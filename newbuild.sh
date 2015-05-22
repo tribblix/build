6f5143ea0d9d418838e3bf2a920fddae545a29bc
Peter Tribble <peter.tribble@gmail.com>
2015-05-22 13:16:09 +0100

    Add postgres

--- newbuild.sh
+++ newbuild.sh
@@ -93,7 +93,15 @@ tribblix_P_opt=""
 tribblix_C_opt=""
 tribblix_M_opt=""
 tribblix_I_opt=""
-tribblix_J_opts=""
+tribblix_J_opt=""
+tribblix_env_opt=""
+do_tribblix_S_opt=""
+do_tribblix_P_opt=""
+do_tribblix_C_opt=""
+do_tribblix_M_opt=""
+do_tribblix_I_opt=""
+do_tribblix_J_opt=""
+do_tribblix_env_opt=""
 
 #
 # this is the list of source files
@@ -101,12 +109,38 @@ tribblix_J_opts=""
 tribblix_srcs=""
 
 . ${BUILD_DIR}/${PKGTOBUILD}/build_opts
-    
+
+#
+# most of the options should be specified without their flags,
+# so add the flags here
+#
+if [ -n "$tribblix_S_opt" ]; then
+    do_tribblix_S_opt="-S"
+fi
+if [ -n "$tribblix_P_opt" ]; then
+    do_tribblix_P_opt="-P"
+fi
+if [ -n "$tribblix_C_opt" ]; then
+    do_tribblix_C_opt="-C"
+fi
+if [ -n "$tribblix_M_opt" ]; then
+    do_tribblix_M_opt="-M"
+fi
+if [ -n "$tribblix_I_opt" ]; then
+    do_tribblix_I_opt="-I"
+fi
+if [ -n "$tribblix_J_opt" ]; then
+    do_tribblix_J_opt="-J"
+fi
+if [ -n "$tribblix_env_opt" ]; then
+    do_tribblix_env_opt="env"
+fi
+
 case $ACTIONS in
     'build')
 	for this_src in $tribblix_srcs
 	do
-	    ${BUILD_DIR}/dobuild $tribblix_64_opt $tribblix_gnu_opt $this_src $tribblix_S_opt $tribblix_P_opt $tribblix_C_opt $tribblix_M_opt $tribblix_I_opt $tribblix_J_opt
+	    $do_tribblix_env_opt $tribblix_env_opt ${BUILD_DIR}/dobuild $tribblix_64_opt $tribblix_gnu_opt $this_src $do_tribblix_S_opt $tribblix_S_opt $do_tribblix_P_opt $tribblix_P_opt $do_tribblix_C_opt "$tribblix_C_opt" $do_tribblix_M_opt $tribblix_M_opt $do_tribblix_I_opt $tribblix_I_opt $do_tribblix_J_opt $tribblix_J_opt
 	done
 	;;
     'package')
@@ -115,7 +149,7 @@ case $ACTIONS in
     'both')
 	for this_src in $tribblix_srcs
 	do
-	    ${BUILD_DIR}/dobuild $tribblix_64_opt $tribblix_gnu_opt $this_src $tribblix_S_opt $tribblix_P_opt $tribblix_C_opt $tribblix_M_opt $tribblix_I_opt $tribblix_J_opt
+	    $do_tribblix_env_opt $tribblix_env_opt ${BUILD_DIR}/dobuild $tribblix_64_opt $tribblix_gnu_opt $this_src $do_tribblix_S_opt $tribblix_S_opt $do_tribblix_P_opt $tribblix_P_opt $do_tribblix_C_opt "$tribblix_C_opt" $do_tribblix_M_opt $tribblix_M_opt $do_tribblix_I_opt $tribblix_I_opt $do_tribblix_J_opt $tribblix_J_opt
 	done
 	${BUILD_DIR}/genpkg ${PKGTOBUILD} $tribblix_srcs
 	;;
