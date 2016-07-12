bfb3ac2d7777fec4979601d2d835477607ce020c
Peter Tribble <peter.tribble@gmail.com>
2016-07-12 12:31:04 +0100

    Update freetype 2.6.5

--- TRIBsys-lib-freetype-2/build.sh
+++ TRIBsys-lib-freetype-2/build.sh
@@ -2,8 +2,5 @@
 #
 # this replaces the OI libraries
 #
-# gotcha for 2.6.4, a "fix" added unresolved symbols to the
-# libraries that we have to undo
-#
-${THOME}/build/dobuild -64 freetype-2.6.4 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no"
-${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.6.4
+${THOME}/build/dobuild -64 freetype-2.6.5 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no"
+${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.6.5
