7c38ce6848d9b3973020f7182301e6424675e9bb
Peter Tribble <peter.tribble@gmail.com>
2016-07-17 19:36:58 +0100

    Update haproxy 1.6.7

--- TRIBblix-haproxy/build.sh
+++ TRIBblix-haproxy/build.sh
@@ -2,15 +2,15 @@
 #
 # haproxy - add smf wrapper
 #
-${THOME}/build/unpack haproxy-1.6.6
-cd haproxy-1.6.6
+${THOME}/build/unpack haproxy-1.6.7
+cd haproxy-1.6.7
 #first fix only necessary for lua
 # gsed -i s:-Wl,--export-dynamic:: Makefile
 #these 2 fixes no longer necessary for 1.6.6
 # gsed -i 's:ifndef ntohll:if 0:' include/common/standard.h
 # gsed -i 's:ifndef htonll:if 0:' include/common/standard.h
 gmake TARGET=solaris USE_PCRE=1 PCRE_INC=/usr/include/pcre USE_OPENSSL=1
-or with lua support
+# or with lua support
 # gmake TARGET=solaris USE_PCRE=1 PCRE_INC=/usr/include/pcre USE_OPENSSL=1 USE_LUA=1
 git clone https://github.com/mmalecki/haproxy-smf-wrapper.git
 cd haproxy-smf-wrapper
