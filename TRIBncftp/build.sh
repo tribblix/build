4b44bbf2dd387edaeae1284b6e30d64713d99089
Peter Tribble <peter.tribble@gmail.com>
2016-07-27 19:39:24 +0100

    Fix some pkginfo typos

--- TRIBncftp/build.sh
+++ TRIBncftp/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
+# you may have to rename the download
+# mv ncftp-3.2.5-src.tar.bz2 ncftp-3.2.5.tar.bz2
+#
 ${THOME}/build/dobuild ncftp-3.2.5
 ${THOME}/build/genpkg TRIBncftp ncftp-3.2.5
