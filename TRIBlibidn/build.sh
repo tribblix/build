185ca5de93471f07853389e5d8808aab2f4ed3f7
Peter Tribble <peter.tribble@gmail.com>
2016-08-01 19:27:36 +0100

    Update libidn 1.33

--- TRIBlibidn/build.sh
+++ TRIBlibidn/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libidn-1.32
-${THOME}/build/genpkg TRIBlibidn libidn-1.32
+${THOME}/build/dobuild -64 libidn-1.33
+${THOME}/build/genpkg TRIBlibidn libidn-1.33
