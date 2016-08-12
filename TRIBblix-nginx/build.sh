3f604fe06844ed3761fb87268a997a8d2f5abedf
Peter Tribble <peter.tribble@gmail.com>
2016-08-12 14:13:43 +0100

    Switch nginx to mainline, enable status

--- TRIBblix-nginx/build.sh
+++ TRIBblix-nginx/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild nginx-1.10.1 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module"
-${THOME}/build/genpkg TRIBblix-nginx nginx-1.10.1
+${THOME}/build/dobuild nginx-1.11.3 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module"
+${THOME}/build/genpkg TRIBblix-nginx nginx-1.11.3
