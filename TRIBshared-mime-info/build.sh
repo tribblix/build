267055fd3b0270012f86458f64da17e4f28ad265
Peter Tribble <peter.tribble@gmail.com>
2016-08-12 19:04:24 +0100

    Update shared-mime-info 1.6

--- TRIBshared-mime-info/build.sh
+++ TRIBshared-mime-info/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu shared-mime-info-1.0
-${THOME}/build/genpkg TRIBshared-mime-info shared-mime-info-1.0
+${THOME}/build/dobuild -gnu shared-mime-info-1.6
+${THOME}/build/genpkg TRIBshared-mime-info shared-mime-info-1.6
