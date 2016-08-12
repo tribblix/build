f605bb7a255fed84eb4a1461285e5f9a90aec8ae
Peter Tribble <peter.tribble@gmail.com>
2016-08-12 16:06:54 +0100

    Update hunspell 1.3.4

--- TRIBhunspell/build.sh
+++ TRIBhunspell/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild hunspell-1.3.2 -C "--disable-static"
-${THOME}/build/genpkg TRIBhunspell hunspell-1.3.2
+${THOME}/build/dobuild hunspell-1.3.4 -C "--disable-static CXXFLAGS=-fpermissive"
+${THOME}/build/genpkg TRIBhunspell hunspell-1.3.4
