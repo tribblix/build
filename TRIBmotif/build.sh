1584df1c6cbf570ef108288a1d7ce51c148dac59
Peter Tribble <peter.tribble@gmail.com>
2016-08-14 22:15:15 +0100

    Update motif 2.3.6

--- TRIBmotif/build.sh
+++ TRIBmotif/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild motif-2.3.5 -J 1
-${THOME}/build/genpkg TRIBmotif motif-2.3.5
+# 2.3.5 and earlier fialed with parallel make and needed -J 1
+#
+${THOME}/build/dobuild motif-2.3.6
+${THOME}/build/genpkg TRIBmotif motif-2.3.6
