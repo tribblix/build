d16bad82427e8242d95cbf6f0cccaf5cd86a8173
Peter Tribble <peter.tribble@gmail.com>
2016-08-17 21:37:49 +0100

    Update libgcrypt 1.7.3 gnupg 1.4.21

--- TRIBgnupg/build.sh
+++ TRIBgnupg/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild gnupg-1.4.20
-${THOME}/build/genpkg TRIBgnupg gnupg-1.4.20
+${THOME}/build/dobuild gnupg-1.4.21
+${THOME}/build/genpkg TRIBgnupg gnupg-1.4.21
--- TRIBlibgcrypt/build.sh
+++ TRIBlibgcrypt/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild libgcrypt-1.7.2
-mv libgcrypt-1.7.2 libgcrypt-1.7.2-32bit
-${THOME}/build/dobuild +64 libgcrypt-1.7.2 -C "--disable-amd64-as-feature-detection --disable-asm"
-mv libgcrypt-1.7.2-32bit libgcrypt-1.7.2
-${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.7.2
+${THOME}/build/dobuild libgcrypt-1.7.3
+mv libgcrypt-1.7.3 libgcrypt-1.7.3-32bit
+${THOME}/build/dobuild +64 libgcrypt-1.7.3 -C "--disable-amd64-as-feature-detection --disable-asm"
+mv libgcrypt-1.7.3-32bit libgcrypt-1.7.3
+${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.7.3
