98d41344127e34226b9e3584aeb141cc5b785ba1
Peter Tribble <peter.tribble@gmail.com>
2016-08-21 20:16:07 +0100

    Update openjpeg 2.1.1

--- TRIBlib-openjpeg/build.sh
+++ TRIBlib-openjpeg/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/unpack openjpeg-2.1.0
-cd openjpeg-2.1.0
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr
+${THOME}/build/unpack openjpeg-2.1.1
+cd openjpeg-2.1.1
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_C_FLAGS="-std=gnu99 -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
 gmake -j 4
 gmake install DESTDIR=/tmp/opj
 ${THOME}/build/create_pkg TRIBlib-openjpeg /tmp/opj
+rm -fr /tmp/opj
