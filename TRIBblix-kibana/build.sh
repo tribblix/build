7a9e195f50bd09f9e1171b66541f950cc68fe0a3
Peter Tribble <peter.tribble@gmail.com>
2016-08-22 20:01:33 +0100

    Add kibana 4.5.4

---
+++ TRIBblix-kibana/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# yes, it's a linux build that we start with, but
+# the platform-specific component is node, which is
+# replaced by the fixit script
+#
+${THOME}/build/pkg_tarball -T TRIBblix-kibana kibana-4.5.4-linux-x86
