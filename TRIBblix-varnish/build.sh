96c2a0c896ecfd7d205a9571d2f064b5060b8f82
Peter Tribble <peter.tribble@gmail.com>
2016-08-26 16:24:42 +0100

    Update varnish 4.1.3

--- TRIBblix-varnish/build.sh
+++ TRIBblix-varnish/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# [4.1.2 is borked]
+# [4.1.2 was problematic]
 zap install TRIBdocutils-python-27
 zap install TRIBlibedit
-env PATH=/usr/gnu/bin:$PATH CPPFLAGS="-I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild varnish-4.1.1 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-2.7/bin/rst2man.py --with-rst2html=/usr/versions/python-2.7/bin/rst2html.py"
-${THOME}/build/genpkg TRIBblix-varnish varnish-4.1.1
+env PATH=/usr/gnu/bin:$PATH CPPFLAGS="-I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild varnish-4.1.3 -M -k -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-2.7/bin/rst2man.py --with-rst2html=/usr/versions/python-2.7/bin/rst2html.py"
+${THOME}/build/genpkg TRIBblix-varnish varnish-4.1.3
