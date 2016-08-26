244f0137d0c57b1a974caad57c141d097ee8c74b
Peter Tribble <peter.tribble@gmail.com>
2016-08-26 14:13:36 +0100

    Update geany 1.28

--- TRIBgeany/build.sh
+++ TRIBgeany/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu geany-1.27 -M 'geany_LDFLAGS='
-${THOME}/build/genpkg TRIBgeany geany-1.27
+${THOME}/build/dobuild -gnu geany-1.28 -M 'geany_LDFLAGS='
+${THOME}/build/genpkg TRIBgeany geany-1.28
