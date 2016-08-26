1c5d0852328f60f7709c3a156f0d78dd5a194949
Peter Tribble <peter.tribble@gmail.com>
2016-08-26 14:24:20 +0100

    Update django 1.10

--- TRIBdjango-python-27/build.sh
+++ TRIBdjango-python-27/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-1.9.8
-cd Django-1.9.8
+${THOME}/build/unpack Django-1.10
+cd Django-1.10
 rm -fr /tmp/p2a
 python setup.py install --root=/tmp/p2a
 ${THOME}/build/create_pkg TRIBdjango-python-27 /tmp/p2a
