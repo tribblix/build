122137c2ebcf3167c6ac0b1769089f26bc5f63b8
Peter Tribble <peter.tribble@gmail.com>
2016-08-26 16:18:12 +0100

    Update farstream 0.2.8

--- TRIBfarstream/build.sh
+++ TRIBfarstream/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild farstream-0.2.7 -C "--disable-python"
-${THOME}/build/genpkg TRIBfarstream farstream-0.2.7
+${THOME}/build/dobuild farstream-0.2.8 -C "--disable-python"
+${THOME}/build/genpkg TRIBfarstream farstream-0.2.8
