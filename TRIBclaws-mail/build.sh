86f5e59f3cc20d20940567e7bf63b4a0c9f85b0c
Peter Tribble <peter.tribble@gmail.com>
2016-08-27 20:32:33 +0100

    Update claws-mail 3.14.0

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 zap uninstall TRIBlibcanberra
-${THOME}/build/dobuild claws-mail-3.13.2 -C --disable-ldap
-${THOME}/build/genpkg TRIBclaws-mail claws-mail-3.13.2
+${THOME}/build/dobuild claws-mail-3.14.0 -C --disable-ldap
+${THOME}/build/genpkg TRIBclaws-mail claws-mail-3.14.0
