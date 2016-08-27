55df435fe1e7ac3018f4ef6a7dfa5f7e05fcfc78
Peter Tribble <peter.tribble@gmail.com>
2016-08-27 18:17:43 +0100

    Sort out desktop caches

---
+++ TRIBsvc-gnome-desktop-cache/build.sh
@@ -0,0 +1,7 @@
+#!/bin/tcsh
+#
+rm -fr /tmp/ts
+mkdir /tmp/ts
+(cd ${THOME}/desktop-cache ; tar cf - usr lib var) | ( cd /tmp/ts ; tar xf -)
+${THOME}/build/create_pkg TRIBsvc-gnome-desktop-cache /tmp/ts
+rm -fr /tmp/ts
