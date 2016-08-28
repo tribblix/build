71fe1b44fbc924d19f92ca0656dfae94db84fc6e
Peter Tribble <peter.tribble@gmail.com>
2016-08-28 19:28:08 +0100

    Document the live media package

---
+++ TRIBsys-install-media-internal/build.sh
@@ -0,0 +1,13 @@
+#!/bin/tcsh
+#
+# assuming the repo is checked out
+# 
+# https://github.com/tribblix/tribblix-media
+#
+rm -fr /tmp/lt2
+mkdir -p /tmp/lt2/sbin
+( cd ${THOME}/tribblix-media ; tar cf - etc lib usr ) | ( cd /tmp/lt2 ; tar xf -)
+gcc -o /tmp/lt2/sbin/listcd -ldevinfo ${THOME}/tribblix-media/listcd.c
+gcc -o /tmp/lt2/sbin/listusb -ldevinfo ${THOME}/tribblix-media/listusb.c
+${THOME}/build/create_pkg TRIBsys-install-media-internal /tmp/lt2
+rm -fr /tmp/lt2
