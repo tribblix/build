1baf47091866de239ef5a5b056c97e97d8f7819f
Peter Tribble <peter.tribble@gmail.com>
2016-08-28 13:37:37 +0100

    Update cups 2.1.4

--- TRIBcups/build.sh
+++ TRIBcups/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
+# need to rename the downloaded tarball, like so:
+#  mv cups-2.1.4-source.tar.gz cups-2.1.4.tar.gz 
+#
 # [needs one of dnssd or avahi]
 # I think the problem here is that we have a broken copy of bonjour. If we
 # didn't have one at all the right thing would happen.
-env CC=gcc CXX=g++ ${THOME}/build/dobuild cups-2.1.3 -C "--sysconfdir=/etc --localstatedir=/var --disable-dnssd"
-cd cups-2.1.3
+env CC=gcc CXX=g++ ${THOME}/build/dobuild cups-2.1.4 -C "--sysconfdir=/etc --localstatedir=/var --disable-dnssd"
+cd cups-2.1.4
 rm -fr /tmp/hc
 gmake install BUILDROOT=/tmp/hc
 ${THOME}/build/create_pkg TRIBcups /tmp/hc
