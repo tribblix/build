2334e329b22804bb7b426557b2100992ae191c3a
Peter Tribble <peter.tribble@gmail.com>
2016-08-28 10:34:35 +0100

    Rebuild djvulibre ted wmaker xplanet against newer tiff

--- TRIBxplanet/build.sh
+++ TRIBxplanet/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xplanet-1.3.0
+# appears to use an incompatible giflib api
+#
+env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild xplanet-1.3.0 -C --with-gif=no
 ${THOME}/build/genpkg TRIBxplanet xplanet-1.3.0
