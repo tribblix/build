490d855922d0987993987c5b58752aa3ba8dc352
Peter Tribble <peter.tribble@gmail.com>
2016-08-31 21:26:38 +0100

    Update xfce weather plugin 0.8.8

--- TRIBxfce-weather-plugin/build.sh
+++ TRIBxfce-weather-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-weather-plugin-0.8.7
-${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.8.7
+${THOME}/build/dobuild -gnu xfce4-weather-plugin-0.8.8
+${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.8.8
