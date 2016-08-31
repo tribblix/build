f99a38bc30a4d73c43306e4ffb13173210958afb
Peter Tribble <peter.tribble@gmail.com>
2016-08-31 21:19:48 +0100

    Add ninja 1.7.1

---
+++ TRIBninja/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git clone git://github.com/ninja-build/ninja.git
+cd ninja
+git checkout release
+./configure.py --bootstrap
+rm -fr /tmp/n1
+mkdir -p /tmp/n1/usr/bin
+cp ninja /tmp/n1/usr/bin
+${THOME}/build/create_pkg TRIBninja /tmp/n1
+rm -fr /tmp/n1
