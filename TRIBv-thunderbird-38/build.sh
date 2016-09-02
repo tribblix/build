be5d8103b8717528b18efa2937e43f8dfe805dad
Peter Tribble <peter.tribble@gmail.com>
2016-09-02 12:17:40 +0100

    Update thunderbird 38.8.0

--- TRIBv-thunderbird-38/build.sh
+++ TRIBv-thunderbird-38/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball TRIBv-thunderbird-38 thunderbird-38.7.2.en-US.solaris-10-fcs-i386
+${THOME}/build/pkg_tarball TRIBv-thunderbird-38 thunderbird-38.8.0.en-US.solaris-10-fcs-i386
