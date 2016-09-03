ddf5872355364e5311f164aa567d9248e3f12aa2
Peter Tribble <peter.tribble@gmail.com>
2016-09-03 21:09:02 +0100

    Update libjpeg-turbo 1.5.0

--- TRIBimage-libjpeg/build.sh
+++ TRIBimage-libjpeg/build.sh
@@ -3,8 +3,8 @@
 # actually libjpeg-turbo
 # no need for the ijg v9 compatibility
 #
-${THOME}/build/dobuild libjpeg-turbo-1.4.2
-mv libjpeg-turbo-1.4.2 libjpeg-turbo-1.4.2-32
-${THOME}/build/dobuild +64 libjpeg-turbo-1.4.2 -C "--host x86_64-pc-solaris"
-mv libjpeg-turbo-1.4.2-32 libjpeg-turbo-1.4.2
-${THOME}/build/genpkg TRIBimage-libjpeg libjpeg-turbo-1.4.2
+${THOME}/build/dobuild libjpeg-turbo-1.5.0
+mv libjpeg-turbo-1.5.0 libjpeg-turbo-1.5.0-32
+${THOME}/build/dobuild +64 libjpeg-turbo-1.5.0 -C "--host x86_64-pc-solaris"
+mv libjpeg-turbo-1.5.0-32 libjpeg-turbo-1.5.0
+${THOME}/build/genpkg TRIBimage-libjpeg libjpeg-turbo-1.5.0
