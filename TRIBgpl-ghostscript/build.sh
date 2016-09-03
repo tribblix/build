6c9f79ef037443149b08add7ec0eb4fc07558f98
Peter Tribble <peter.tribble@gmail.com>
2016-09-03 21:53:32 +0100

    Update ghostscript 9.19

--- TRIBgpl-ghostscript/build.sh
+++ TRIBgpl-ghostscript/build.sh
@@ -2,12 +2,15 @@
 #
 # Update ghostscript
 # Force more system libraries, clean documentation up, disable other
-# things to keep size down to reasonable levels. (Now zap is 11M smaller
-# than before.)
+# things to keep size down to reasonable levels.
 #
-${THOME}/build/dobuild ghostscript-9.15 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
-cd ghostscript-9.15
+# see the BLFS documentation, we build the same way
+#  http://www.linuxfromscratch.org/blfs/view/svn/pst/gs.html
+#
+${THOME}/build/dobuild ghostscript-9.19 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
+cd ghostscript-9.19
 gmake -j4 so
+rm -fr /tmp/ags
 gmake DESTDIR=/tmp/ags install
 gmake DESTDIR=/tmp/ags soinstall
 ginstall -v -m644 base/*.h /tmp/ags/usr/include/ghostscript/
@@ -15,6 +18,7 @@ mkdir -p /tmp/ags/usr/share/ghostscript
 cd /tmp/ags/usr/share/ghostscript
 ${THOME}/build/unpack ghostscript-fonts-std-8.11
 ${THOME}/build/unpack gnu-gs-fonts-other-6.0
-rm 9.15/doc/*.*
+rm 9.19/doc/*.*
 cd
 ${THOME}/build/create_pkg TRIBgpl-ghostscript /tmp/ags
+rm -fr /tmp/ags
