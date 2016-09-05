8fd07d79b3b23cff92ae07da95d3b3544f14fc3f
Peter Tribble <peter.tribble@gmail.com>
2016-09-05 18:48:10 +0100

    Rebuild poppler for tiff4

--- TRIBpoppler/build.sh
+++ TRIBpoppler/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-env CPPFLAGS="-D_XPG6" ${THOME}/build/dobuild -64 poppler-0.26.5 -C "--enable-xpdf-headers"
+# bumping the version requires rebuilding all dependants as the
+# SONAME changes
+#
+env CPPFLAGS="-D_XPG6" ${THOME}/build/dobuild -64 poppler-0.26.5 -C "--enable-xpdf-headers --disable-introspection"
 ${THOME}/build/genpkg TRIBpoppler poppler-0.26.5
