fccf90c5e2c41c99657637745ce5d221b1257896
Peter Tribble <peter.tribble@gmail.com>
2016-09-05 20:45:26 +0100

    Update lcms2 2.8

--- TRIBlib-lcms2/build.sh
+++ TRIBlib-lcms2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 lcms2-2.7
-${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.7
+${THOME}/build/dobuild -64 lcms2-2.8
+${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.8
