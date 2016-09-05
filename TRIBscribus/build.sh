41f814242a3389d8568c92286786c6903afbec9f
Peter Tribble <peter.tribble@gmail.com>
2016-09-05 21:42:07 +0100

    Update scribus 1.4.6

--- TRIBscribus/build.sh
+++ TRIBscribus/build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack scribus-1.4.5
-cd scribus-1.4.5
+${THOME}/build/unpack scribus-1.4.6
+cd scribus-1.4.6
 env PATH=/usr/versions/Qt-4.8/bin:$PATH cmake . -DCMAKE_INSTALL_PREFIX:PATH=/usr -DPYTHON_INCLUDE_DIR=/usr/versions/python-2.7/include/python2.7
 gmake
-rm -fr /tmp/sc
-gmake install DESTDIR=/tmp/sc
-${THOME}/build/create_pkg TRIBscribus /tmp/sc
-rm -fr /tmp/sc
+${THOME}/build/genpkg TRIBscribus
