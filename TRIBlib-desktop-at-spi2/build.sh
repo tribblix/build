e1c3c77fcc762673ab69fc9921275adc674997f2
Peter Tribble <peter.tribble@gmail.com>
2016-09-08 18:34:27 +0100

    Fix atspi dbus location

--- TRIBlib-desktop-at-spi2/build.sh
+++ TRIBlib-desktop-at-spi2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu at-spi2-core-2.20.2 -C --disable-introspection
+${THOME}/build/dobuild -64 -gnu at-spi2-core-2.20.2 -C "--disable-introspection --with-dbus_daemondir=/usr/lib"
 ${THOME}/build/genpkg TRIBlib-desktop-at-spi2 at-spi2-core-2.20.2
