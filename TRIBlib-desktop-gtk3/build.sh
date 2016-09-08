4c2ecc0a579afee479c0db2dfc2fcfbc2cf3ce34
Peter Tribble <peter.tribble@gmail.com>
2016-09-08 18:34:53 +0100

    Update gtk3 3.14.15

--- TRIBlib-desktop-gtk3/build.sh
+++ TRIBlib-desktop-gtk3/build.sh
@@ -3,7 +3,7 @@
 # zap install TRIBlib-desktop-atk-bridge
 # zap install TRIBlib-desktop-at-spi2
 #
-# need to patch gtk/gtksearchenginesimple.c
+# newer versions require updates to cairo and/or pango
 #
-${THOME}/build/dobuild gtk+-3.12.2
-${THOME}/build/genpkg TRIBlib-desktop-gtk3 gtk+-3.12.2
+${THOME}/build/dobuild -64 gtk+-3.14.15 -C "--disable-introspection --disable-cups"
+${THOME}/build/genpkg TRIBlib-desktop-gtk3 gtk+-3.14.15
