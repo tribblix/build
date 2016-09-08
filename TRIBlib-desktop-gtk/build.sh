fccfd17493b6cea5af1dc7c1329676f754891006
Peter Tribble <peter.tribble@gmail.com>
2016-09-08 23:20:00 +0100

    Update gtk+ 2.24.30

--- TRIBlib-desktop-gtk/build.sh
+++ TRIBlib-desktop-gtk/build.sh
@@ -2,5 +2,5 @@
 #
 # gtk 64-bit, --disable-cups as we don't have 64-bit cups or gnutls yet
 #
-${THOME}/build/dobuild -64 gtk+-2.24.28 -C --disable-cups
-${THOME}/build/genpkg TRIBlib-desktop-gtk gtk+-2.24.28
+${THOME}/build/dobuild -64 gtk+-2.24.30 -C "--disable-cups --disable-introspection"
+${THOME}/build/genpkg TRIBlib-desktop-gtk gtk+-2.24.30
