1128cd5aeafe38eb5a9b37b668d18b14ee765ea2
Peter Tribble <peter.tribble@gmail.com>
2016-09-09 19:55:02 +0100

    Add named as a service

---
+++ TRIBsvc-bind9/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# the bind9 package ships the software, which is useful for clients
+# all the work here in populating the package is done in the fixit script
+#
+${THOME}/build/genpkg TRIBsvc-bind9
