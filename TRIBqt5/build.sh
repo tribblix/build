312d893649f90e714493aef07f12ab3c06cccbf7
Peter Tribble <peter.tribble@gmail.com>
2016-09-13 20:08:54 +0100

    Update QT 5.6.1-1

--- TRIBqt5/build.sh
+++ TRIBqt5/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
-${THOME}/build/unpack qt-everywhere-opensource-src-5.2.1
-cd qt-everywhere-opensource-src-5.2.1
-./configure --prefix=/usr/versions/Qt-5.2.1 -opensource --disable-reduce-exports -no-sql-sqlite -confirm-license -platform solaris-g++
+${THOME}/build/unpack qt-everywhere-opensource-src-5.6.1-1
+cd qt-everywhere-opensource-src-5.6.1
+./configure --prefix=/usr/versions/Qt-5 -opensource --disable-reduce-exports -no-sql-sqlite -confirm-license -platform solaris-g++
+# bashisms abound [local] - fix configure qtbase/configure to use bash, also
+gsed -i s:CFG_SSE2=auto:CFG_SSE2=yes: qtbase//configure
 gmake -j 16
-gmake -k -j 16 install INSTALL_ROOT=/tmp/qt5
+# gnu to get a working "install" binary
+env PATH=/usr/gnu/bin:$PATH gmake -k -j 16 install INSTALL_ROOT=/tmp/qt5
 ${THOME}/build/create_pkg TRIBqt5 /tmp/qt5
