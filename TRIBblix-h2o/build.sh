5ba72109d6ef25cf7891978b32243625ade77993
Peter Tribble <peter.tribble@gmail.com>
2016-09-14 20:31:58 +0100

    Update h2o 2.0.4

--- TRIBblix-h2o/build.sh
+++ TRIBblix-h2o/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack h2o-2.0.0
-cd h2o-2.0.0/
+${THOME}/build/unpack h2o-2.0.4
+cd h2o-2.0.4
 cmake -DCMAKE_INSTALL_PREFIX=/opt/tribblix/h2o -DWITH_MRUBY=off
 gmake -j 4
 ${THOME}/build/genpkg TRIBblix-h2o
