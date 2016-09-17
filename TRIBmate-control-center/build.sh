6eae28ce70d89e4046300e84f1c7d9789e507848
Peter Tribble <peter.tribble@gmail.com>
2016-09-17 11:34:41 +0100

    Remove mime conflicts from mate control center

--- TRIBmate-control-center/build.sh
+++ TRIBmate-control-center/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
 # mate-control-center needs mate-menus marco
+#
+# make -k because pt.mo fails
+#
 ${THOME}/build/dobuild -gnu mate-control-center-1.14.1 -M -k
 ${THOME}/build/genpkg TRIBmate-control-center mate-control-center-1.14.1
