82a46ab670b036246789413c523ca84fa5912af7
Peter Tribble <peter.tribble@gmail.com>
2016-09-19 19:15:24 +0100

    Add mod_perl 2.0.9

---
+++ TRIBblix-modperl/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# build mod_perl
+# it's split into 2 packages, one for the apache half, the other for the
+# perl half, as they go in different places
+#
+${THOME}/build/unpack mod_perl-2.0.9
+cd mod_perl-2.0.9
+perl Makefile.PL MP_APXS=/opt/tribblix/apache2/bin/apxs
+make
+#
+# make both packages, assuming fixit will delete the bits
+# not needed in each package
+#
+${THOME}/build/genpkg TRIBblix-modperl
+${THOME}/build/genpkg TRIBlib-perl-5-modperl
