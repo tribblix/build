47eaf5e8ecdd17cebe1e596ecb6c79006423b608
Peter Tribble <peter.tribble@gmail.com>
2016-09-22 09:28:31 +0100

    Update irssi 0.8.20

--- TRIBirssi/build.sh
+++ TRIBirssi/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild irssi-0.8.19 -C --with-perl=no
-${THOME}/build/genpkg TRIBirssi irssi-0.8.19
+${THOME}/build/dobuild irssi-0.8.20 -C --with-perl=no
+${THOME}/build/genpkg TRIBirssi irssi-0.8.20
