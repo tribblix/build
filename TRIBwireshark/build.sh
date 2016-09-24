2b4e204b8c0b410af4f1e71ba7f6837802810536
Peter Tribble <peter.tribble@gmail.com>
2016-09-24 11:11:52 +0100

    Update wireshark 2.0.6

--- TRIBwireshark/build.sh
+++ TRIBwireshark/build.sh
@@ -2,6 +2,8 @@
 #
 # this builds both the main wireshark package and the qt package
 #
-env PATH=${PATH}:/usr/versions/Qt-4.8/bin:/usr/perl5/5.22.0/bin ${THOME}/build/dobuild -gnu wireshark-2.0.4 -C "--with-qt -without-lua --with-gtk2"
-${THOME}/build/genpkg TRIBwireshark wireshark-2.0.4
-${THOME}/build/genpkg TRIBwireshark-qt wireshark-2.0.4
+# need to explicitly tell it which qt as guessing is unreliable
+#
+env PATH=${PATH}:/usr/versions/Qt-4.8/bin:/usr/perl5/5.22.0/bin ${THOME}/build/dobuild -gnu wireshark-2.0.6 -C "--with-qt=4 -without-lua --with-gtk2"
+${THOME}/build/genpkg TRIBwireshark wireshark-2.0.6
+${THOME}/build/genpkg TRIBwireshark-qt wireshark-2.0.6
