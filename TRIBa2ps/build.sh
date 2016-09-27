f7eb11ed0b5717af3fac9bcedb6aafda4a143c20
Peter Tribble <peter.tribble@gmail.com>
2016-09-27 18:51:32 +0100

    A couple of clarifications found in the SPARC build

--- TRIBa2ps/build.sh
+++ TRIBa2ps/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
+# needs gperf
+#
 ${THOME}/build/dobuild a2ps-4.14
 ${THOME}/build/genpkg TRIBa2ps a2ps-4.14
--- TRIBaalib/build.sh
+++ TRIBaalib/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild aalib-1.4.0
+${THOME}/build/dobuild aalib-1.4.0 -C --with-slang-driver=no
 ${THOME}/build/genpkg TRIBaalib aalib-1.4.0
