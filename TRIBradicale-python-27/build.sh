b72842a732aedca3e4ef461cb8e73c44a06bdde3
Peter Tribble <peter.tribble@gmail.com>
2016-09-29 17:24:35 +0100

    Add radicale 1.1.1

---
+++ TRIBradicale-python-27/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# radicale - no dependencies
+#
+${THOME}/build/unpack Radicale-1.1.1
+cd Radicale-1.1.1
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBradicale-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
