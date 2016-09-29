d0a62b992306a51619cf6e3398965a86ee380a33
Peter Tribble <peter.tribble@gmail.com>
2016-09-29 19:47:48 +0100

    Update caddy 0.9.3

--- TRIBcaddy/build.sh
+++ TRIBcaddy/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack caddy-0.8.0
-env GOPATH=`pwd` go get github.com/mholt/caddy
+env GOPATH=`pwd` go get github.com/mholt/caddy/caddy
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/caddyserver
 cp bin/caddy /tmp/cs/usr/bin
-cp README.md LICENSE.txt /tmp/cs/usr/share/caddyserver
+cp src/github.com/mholt/caddy/README.md src/github.com/mholt/caddy/LICENSE.txt /tmp/cs/usr/share/caddyserver
 ${THOME}/build/create_pkg TRIBcaddy /tmp/cs
 rm -fr /tmp/cs
