7d6a7a174b9495afb0885fafb8fbafb397c24a90
Peter Tribble <peter.tribble@gmail.com>
2016-10-02 13:43:46 +0100

    Update freetype 2.7

--- TRIBsys-lib-freetype-2/build.sh
+++ TRIBsys-lib-freetype-2/build.sh
@@ -2,5 +2,5 @@
 #
 # this replaces the OI libraries
 #
-${THOME}/build/dobuild -64 freetype-2.6.5 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no"
-${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.6.5
+${THOME}/build/dobuild -64 freetype-2.7 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no"
+${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.7
