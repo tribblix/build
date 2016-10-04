be4a8c124faed674ff95e97e0e30236c3782b19d
Peter Tribble <peter.tribble@gmail.com>
2016-10-04 14:57:36 +0100

    Update xcowsay 1.4

--- TRIBx11-xcowsay/build.sh
+++ TRIBx11-xcowsay/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xcowsay-1.3
-${THOME}/build/genpkg TRIBx11-xcowsay xcowsay-1.3
+${THOME}/build/dobuild xcowsay-1.4
+${THOME}/build/genpkg TRIBx11-xcowsay xcowsay-1.4
