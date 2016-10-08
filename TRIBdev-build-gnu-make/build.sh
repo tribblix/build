6ddbb95f9cac1b127800f59daea9c59d3b0d456a
Peter Tribble <peter.tribble@gmail.com>
2016-10-08 09:18:42 +0100

    Update gmake 4.2.1

--- TRIBdev-build-gnu-make/build.sh
+++ TRIBdev-build-gnu-make/build.sh
@@ -3,5 +3,5 @@
 # Note that 3.82 introduced major incompatibilities; these have been
 # reverted in 4.1 so that 4.1 should be safe
 #
-${THOME}/build/dobuild make-4.1 -P /usr/gnu
-${THOME}/build/genpkg TRIBdev-build-gnu-make make-4.1
+${THOME}/build/dobuild make-4.2.1 -P /usr/gnu
+${THOME}/build/genpkg TRIBdev-build-gnu-make make-4.2.1
