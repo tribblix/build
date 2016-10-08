fd2a32d54f62997e581e41971197665e09e6cc18
Peter Tribble <peter.tribble@gmail.com>
2016-10-08 13:15:18 +0100

    Update pixman 0.34.0

--- TRIBlib-graphics-pixman/build.sh
+++ TRIBlib-graphics-pixman/build.sh
@@ -2,5 +2,5 @@
 #
 # (may need to pkgrm the original first)
 #
-${THOME}/build/dobuild -64 pixman-0.32.6
-${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.32.6
+${THOME}/build/dobuild -64 pixman-0.34.0
+${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.34.0
