3efa1580b2188147e6112078e6c57119e669650e
Peter Tribble <peter.tribble@gmail.com>
2016-10-08 16:26:25 +0100

    Update ocaml 4.03.0

--- TRIBocaml/build.sh
+++ TRIBocaml/build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 #
-# [patch needed for shared build, should be unnecessary next time]
-#
-${THOME}/build/unpack ocaml-4.02.3
-cd ocaml-4.02.3
+${THOME}/build/unpack ocaml-4.03.0
+cd ocaml-4.03.0
 env PATH=/usr/gnu/bin:$PATH ./configure -cc "gcc -fPIC" -aspp "gcc -c -fPIC" -prefix /usr -mandir /usr/share/man/man1
 env PATH=/usr/gnu/bin:$PATH gmake -j 8 world.opt >& log.world
-env PATH=/usr/gnu/bin:$PATH gmake install DESTDIR=/tmp/oo1
-${THOME}/build/create_pkg TRIBocaml /tmp/oo1
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBocaml
