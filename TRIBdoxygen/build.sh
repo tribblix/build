471d54046d749036b63063ef083e6f99217dd35b
Peter Tribble <peter.tribble@gmail.com>
2016-10-09 21:11:05 +0100

    Update doxygen 1.8.12

--- TRIBdoxygen/build.sh
+++ TRIBdoxygen/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack doxygen-1.8.10
-cd doxygen-1.8.10
+${THOME}/build/unpack doxygen-1.8.12
+cd doxygen-1.8.12
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
