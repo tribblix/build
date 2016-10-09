53ba0a364a788be19d1f43083858c6c9087905bf
Peter Tribble <peter.tribble@gmail.com>
2016-10-09 14:20:17 +0100

    Update wine 1.8.5

--- TRIBwine/build.sh
+++ TRIBwine/build.sh
@@ -2,10 +2,5 @@
 #
 # wine
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-1.8.4
-cd wine-1.8.4
-rm -fr /tmp/w1
-env PATH=/usr/gnu/bin:$PATH gmake install DESTDIR=/tmp/w1
-${THOME}/build/create_pkg TRIBwine /tmp/w1
-cd ..
-rm -fr /tmp/w1
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-1.8.5
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-1.8.5
