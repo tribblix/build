1cb896161ab2553e256a57cf0f5cf7c47f691e3d
Peter Tribble <peter.tribble@gmail.com>
2016-10-10 20:12:53 +0100

    Update gtkmm 2.24.5

--- TRIBlib-desktop-gtkmm/build.sh
+++ TRIBlib-desktop-gtkmm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu gtkmm-2.24.4
-${THOME}/build/genpkg TRIBlib-desktop-gtkmm gtkmm-2.24.4
+${THOME}/build/dobuild -64 -gnu gtkmm-2.24.5
+${THOME}/build/genpkg TRIBlib-desktop-gtkmm gtkmm-2.24.5
