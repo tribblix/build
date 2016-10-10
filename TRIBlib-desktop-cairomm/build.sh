64b52294bae624691c871ead92cdd04d298b6341
Peter Tribble <peter.tribble@gmail.com>
2016-10-10 21:31:59 +0100

    Update cairomm 1.12.0

--- TRIBlib-desktop-cairomm/build.sh
+++ TRIBlib-desktop-cairomm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu cairomm-1.11.2
-${THOME}/build/genpkg TRIBlib-desktop-cairomm cairomm-1.11.2
+${THOME}/build/dobuild -64 -gnu cairomm-1.12.0
+${THOME}/build/genpkg TRIBlib-desktop-cairomm cairomm-1.12.0
