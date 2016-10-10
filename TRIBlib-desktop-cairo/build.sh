4814ec72eaaa434a5cdeb7743febf70027658ddf
Peter Tribble <peter.tribble@gmail.com>
2016-10-10 20:48:22 +0100

    Update cairo 1.14.6

--- TRIBlib-desktop-cairo/build.sh
+++ TRIBlib-desktop-cairo/build.sh
@@ -13,5 +13,5 @@
 # on components that require it, so need to rebuild
 # cairo again after all the other components are built
 #
-${THOME}/build/dobuild -64 cairo-1.12.16 -M -k
-${THOME}/build/genpkg TRIBlib-desktop-cairo cairo-1.12.16
+${THOME}/build/dobuild -64 cairo-1.14.6 -M -k
+${THOME}/build/genpkg TRIBlib-desktop-cairo cairo-1.14.6
