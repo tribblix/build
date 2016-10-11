2e00429d2c7e7411e5d33c509b692360d0f76557
Peter Tribble <peter.tribble@gmail.com>
2016-10-11 10:53:38 +0100

    Use the right python name

--- TRIBboto3-python-35/build.sh
+++ TRIBboto3-python-35/build.sh
@@ -2,9 +2,9 @@
 #
 ${THOME}/build/unpack boto3-1.4.0
 cd boto3-1.4.0
-/usr/versions/python-3.5/bin/python setup.py build
+/usr/versions/python-3.5/bin/python3 setup.py build
 rm -fr /tmp/sut
-/usr/versions/python-3.5/bin/python setup.py install --root=/tmp/sut
+/usr/versions/python-3.5/bin/python3 setup.py install --root=/tmp/sut
 ${THOME}/build/create_pkg TRIBboto3-python-35 /tmp/sut
 cd ..
 rm -fr /tmp/sut
