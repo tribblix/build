c047e67df104a4a9d43f069678db7f497bfcf292
Peter Tribble <peter.tribble@gmail.com>
2016-10-11 10:47:04 +0100

    Update nghttp2 1.15.0

--- TRIBlibnghttp2/build.sh
+++ TRIBlibnghttp2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS="-lsocket -lnsl" TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 nghttp2-1.13.0
-${THOME}/build/genpkg TRIBlibnghttp2 nghttp2-1.13.0
+env LIBS="-lsocket -lnsl" TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 nghttp2-1.15.0
+${THOME}/build/genpkg TRIBlibnghttp2 nghttp2-1.15.0
