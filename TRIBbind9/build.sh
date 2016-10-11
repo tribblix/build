f6edaec2efa0865dd1b1ebb3970330a373e6092f
Peter Tribble <peter.tribble@gmail.com>
2016-10-11 19:40:03 +0100

    Update bind 9.11.0

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -2,5 +2,5 @@
 #
 # must not have TRIBlibedit installed
 #
-${THOME}/build/dobuild bind-9.9.9-P3 -C "--sysconfdir=/etc --localstatedir=/var"
-${THOME}/build/genpkg TRIBbind9 bind-9.9.9-P3
+${THOME}/build/dobuild bind-9.11.0 -C "--sysconfdir=/etc --localstatedir=/var"
+${THOME}/build/genpkg TRIBbind9 bind-9.11.0
