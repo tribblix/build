6b16bd4520a62a830a209380e8cc40b9cfe40573
Peter Tribble <peter.tribble@gmail.com>
2016-10-17 17:15:05 +0100

    Update gnuplot 5.0.5

--- TRIBgnuplot/build.sh
+++ TRIBgnuplot/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild gnuplot-4.6.6
-${THOME}/build/genpkg TRIBgnuplot gnuplot-4.6.6
+${THOME}/build/dobuild gnuplot-5.0.5 -C "--without-gd --with-qt=no"
+${THOME}/build/genpkg TRIBgnuplot gnuplot-5.0.5
