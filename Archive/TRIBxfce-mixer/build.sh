e342234c7372726a188631a863beefc312f1e5a1
Peter Tribble <peter.tribble@gmail.com>
2016-10-19 11:09:21 +0100

    Deprecate jdk 8u92, clear Archive

--- Archive/TRIBxfce-mixer/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# this package is osolete as of xfce 4.12
-#
-# needs gstreamer
-# needs libunique
-#
-${THOME}/build/dobuild -gnu xfce4-mixer-4.10.0
-${THOME}/build/genpkg TRIBxfce-mixer xfce4-mixer-4.10.0
