154746aa65a58945c5e34b540e79be338aa8d29a
Peter Tribble <peter.tribble@gmail.com>
2016-10-19 14:00:31 +0100

    Update unbound 1.5.10

--- TRIBunbound/build.sh
+++ TRIBunbound/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild unbound-1.5.9 -C "--without-libevent --sysconfdir=/etc" -J 1
-${THOME}/build/genpkg TRIBunbound unbound-1.5.9
+${THOME}/build/dobuild unbound-1.5.10 -C "--without-libevent --sysconfdir=/etc" -J 1
+${THOME}/build/genpkg TRIBunbound unbound-1.5.10
