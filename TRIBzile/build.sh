8d338fd293e4e685c0f1d9cbc91c00abaea106b7
Peter Tribble <peter.tribble@gmail.com>
2016-10-19 14:36:34 +0100

    Update zile 2.4.13

--- TRIBzile/build.sh
+++ TRIBzile/build.sh
@@ -2,5 +2,5 @@
 #
 # needs boehm-gc and help2man
 #
-${THOME}/build/dobuild zile-2.4.11
-${THOME}/build/genpkg TRIBzile zile-2.4.11
+${THOME}/build/dobuild zile-2.4.13
+${THOME}/build/genpkg TRIBzile zile-2.4.13
