2a877ee529c999693dfda19c09a196daae8f1853
Peter Tribble <peter.tribble@gmail.com>
2016-10-30 12:09:49 +0000

    Update redis 3.2.5

--- TRIBblix-redis/build.sh
+++ TRIBblix-redis/build.sh
@@ -2,11 +2,12 @@
 #
 # Update redis
 #
-env CC=gcc ${THOME}/build/dobuild redis-3.2.4 -P /opt/tribblix/redis
-cd redis-3.2.4
+env CC=gcc ${THOME}/build/dobuild redis-3.2.5 -P /opt/tribblix/redis
+cd redis-3.2.5
 gmake test
 rm -fr /tmp/rr
 gmake install PREFIX=/tmp/rr/opt/tribblix/redis
 cp COPYING 00-RELEASENOTES /tmp/rr/opt/tribblix/redis
 ${THOME}/build/create_pkg TRIBblix-redis /tmp/rr
+cd ..
 rm -fr /tmp/rr
