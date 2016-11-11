306345dc857aea982bade946b7fafb8b77a23af6
Peter Tribble <peter.tribble@gmail.com>
2016-11-11 10:34:23 +0000

    Switch DBD::Pg to postgres96

--- TRIBlib-perl-5-dbd-pg/build.sh
+++ TRIBlib-perl-5-dbd-pg/build.sh
@@ -2,6 +2,6 @@
 #
 ${THOME}/build/unpack DBD-Pg-3.5.3
 cd DBD-Pg-3.5.3
-env POSTGRES_HOME=/opt/tribblix/postgres94 perl Makefile.PL
+env POSTGRES_HOME=/opt/tribblix/postgres96 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBlib-perl-5-dbd-pg
