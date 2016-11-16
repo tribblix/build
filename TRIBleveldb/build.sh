445a9a258369a903f85a4d6b311c4c44b9fec48c
Peter Tribble <peter.tribble@gmail.com>
2016-11-16 21:16:40 +0000

    Update leveldb 1.19, fix 64-bit build

--- TRIBleveldb/build.sh
+++ TRIBleveldb/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 leveldb-1.18
+env CC=gcc LDFLAGS=-m64 CFLAGS=-m64 CXXFLAGS=-m64 ${THOME}/build/dobuild leveldb-1.19
+mv leveldb-1.19 leveldb-1.19-save
+env CC=gcc ${THOME}/build/dobuild leveldb-1.19
+mv leveldb-1.19-save leveldb-1.19-64bit
 # [have to manually implement make install]
-${THOME}/build/genpkg TRIBleveldb leveldb-1.18
+${THOME}/build/genpkg TRIBleveldb leveldb-1.19
