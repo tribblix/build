afd1a7c4abf88ad295eddbfcdeefcd3b60b3774e
Peter Tribble <peter.tribble@gmail.com>
2016-11-22 07:52:15 +0000

    Update libpcap 1.8.1 tcpdump 4.8.1

--- TRIBtcpdump/build.sh
+++ TRIBtcpdump/build.sh
@@ -3,5 +3,5 @@
 # NB. make sure pcap isn't at the same level, else it pulls pcap from the
 # build area rather than the systems
 #
-${THOME}/build/dobuild tcpdump-4.7.4
-${THOME}/build/genpkg TRIBtcpdump tcpdump-4.7.4
+${THOME}/build/dobuild tcpdump-4.8.1
+${THOME}/build/genpkg TRIBtcpdump tcpdump-4.8.1
