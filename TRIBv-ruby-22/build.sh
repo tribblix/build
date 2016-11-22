fd86ea6f9188b2658af98f4357e48531dba0bf3b
Peter Tribble <peter.tribble@gmail.com>
2016-11-22 09:57:44 +0000

    Update ruby 2.2.6 2.3.3

--- TRIBv-ruby-22/build.sh
+++ TRIBv-ruby-22/build.sh
@@ -3,5 +3,5 @@
 # ruby update (still segfaults on sparc)
 # just can't handle LD_OPTIONS
 #
-${THOME}/build/dobuild ruby-2.2.5 -P /usr/versions/ruby-2.2 -C ac_cv_func_dl_iterate_phdr=no -L
-${THOME}/build/genpkg TRIBv-ruby-22 ruby-2.2.5
+${THOME}/build/dobuild ruby-2.2.6 -P /usr/versions/ruby-2.2 -C ac_cv_func_dl_iterate_phdr=no -L
+${THOME}/build/genpkg TRIBv-ruby-22 ruby-2.2.6
--- TRIBv-ruby-23/build.sh
+++ TRIBv-ruby-23/build.sh
@@ -3,5 +3,5 @@
 # ruby update (still segfaults on sparc)
 # just can't handle LD_OPTIONS
 #
-${THOME}/build/dobuild ruby-2.3.1 -P /usr/versions/ruby-2.3 -C ac_cv_func_dl_iterate_phdr=no -L
-${THOME}/build/genpkg TRIBv-ruby-23 ruby-2.3.1
+${THOME}/build/dobuild ruby-2.3.3 -P /usr/versions/ruby-2.3 -C ac_cv_func_dl_iterate_phdr=no -L
+${THOME}/build/genpkg TRIBv-ruby-23 ruby-2.3.3
