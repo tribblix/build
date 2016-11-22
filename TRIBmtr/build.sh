06fa9f61e436c3fce54f7cb30c00b4803b08d12c
Peter Tribble <peter.tribble@gmail.com>
2016-11-22 21:45:56 +0000

    Update mtr 0.87

--- TRIBmtr/build.sh
+++ TRIBmtr/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS="-D BSD_COMP" ${THOME}/build/dobuild mtr-0.86
-${THOME}/build/genpkg TRIBmtr mtr-0.86
+env CPPFLAGS="-D BSD_COMP" ${THOME}/build/dobuild mtr-0.87
+${THOME}/build/genpkg TRIBmtr mtr-0.87
