beac7562a8479ab565d6f0230df0743bf004ab88
Peter Tribble <peter.tribble@gmail.com>
2016-12-04 12:13:28 +0000

    Extend openjpeg to 64-bits

--- TRIBlib-openjpeg/build.sh
+++ TRIBlib-openjpeg/build.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 #
-${THOME}/build/unpack openjpeg-2.1.2
+case `uname -p` in
+sparc)
+    ARCH64="sparcv9"
+    ;;
+i386)
+    ARCH64="amd64"
+    ;;
+esac
+
+${THOME}/build/unpack -64 openjpeg-2.1.2
 cd openjpeg-2.1.2
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_C_FLAGS="-std=gnu99 -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
 gmake -j 4
-${THOME}/build/genpkg TRIBlib-openjpeg
 cd ..
+cd openjpeg-2.1.2-64bit
+# specifying architecture directories for lib and bin doesn't work
+# so they're corrected by the fixinstall.64 script
+env PKG_CONFIG_PATH=/usr/lib/${ARCH64}/pkgconfig LDFLAGS="-m64" CFLAGS="-m64 -std=gnu99" CXXFLAGS="-m64" cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr
+gmake -j 4
+cd ..
+${THOME}/build/genpkg TRIBlib-openjpeg openjpeg-2.1.2
