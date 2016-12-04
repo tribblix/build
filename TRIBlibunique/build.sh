e5e1888bfe206a052019706f5cc2458eda8266cd
Peter Tribble <peter.tribble@gmail.com>
2016-12-04 12:45:10 +0000

    Extend libunique to 64-bits

--- TRIBlibunique/build.sh
+++ TRIBlibunique/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS="-DG_CONST_RETURN=" ${THOME}/build/dobuild libunique-1.1.6 -C "--enable-bacon=no --enable-maintainer-flags=no"
+env CPPFLAGS="-DG_CONST_RETURN=" ${THOME}/build/dobuild -64 libunique-1.1.6 -C "--enable-bacon=no --enable-maintainer-flags=no"
 ${THOME}/build/genpkg TRIBlibunique libunique-1.1.6
