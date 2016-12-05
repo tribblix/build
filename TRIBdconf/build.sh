d8228d81cd5fca04a51f1dfa5e10d25f65df54e6
Peter Tribble <peter.tribble@gmail.com>
2016-12-05 20:12:46 +0000

    Extend dconf to 64-bits

--- TRIBdconf/build.sh
+++ TRIBdconf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -gnu dconf-0.24.0
+env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -gnu -64 dconf-0.24.0
 env CPPFLAGS=-std=gnu99 ${THOME}/build/genpkg TRIBdconf dconf-0.24.0
