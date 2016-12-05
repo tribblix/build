62dcabb22569e8aa1e382652cef2d96c72606810
Peter Tribble <peter.tribble@gmail.com>
2016-12-05 20:04:13 +0000

    Update flex 2.6.2

--- TRIBflex/build.sh
+++ TRIBflex/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/newbuild.sh both TRIBflex
+# need to work harder to force gnu m4, -gnu isn't enough
+#
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64 flex-2.6.2
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBflex flex-2.6.2
