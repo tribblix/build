5ee606cc45d1df3406cb0c26aefe3d36aa49fcd8
Peter Tribble <peter.tribble@gmail.com>
2016-12-05 20:19:24 +0000

    Extend glib-networking to 64-bits

--- TRIBglib-networking/build.sh
+++ TRIBglib-networking/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu glib-networking-2.30.2 -C "--without-ca-certificates"
+${THOME}/build/dobuild -gnu -64 glib-networking-2.30.2 -C "--without-ca-certificates"
 ${THOME}/build/genpkg TRIBglib-networking glib-networking-2.30.2
