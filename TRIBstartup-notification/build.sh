2ab2011a16487dcbe2bc5c6415523620f8b887d4
Peter Tribble <peter.tribble@gmail.com>
2016-12-06 19:31:34 +0000

    Extend startup-notification to 64-bits

--- TRIBstartup-notification/build.sh
+++ TRIBstartup-notification/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild startup-notification-0.10
+${THOME}/build/dobuild -64 startup-notification-0.10
 ${THOME}/build/genpkg TRIBstartup-notification startup-notification-0.10
