87917b0052c8d18dc9ed126148195e1e305d428b
Peter Tribble <peter.tribble@gmail.com>
2016-12-10 15:26:56 +0000

    Update stunnel 5.38

--- TRIBstunnel/build.sh
+++ TRIBstunnel/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild stunnel-5.32 -C "--sysconfdir=/etc --localstatedir=/var"
-${THOME}/build/genpkg TRIBstunnel stunnel-5.32
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild stunnel-5.38 -C "--sysconfdir=/etc --localstatedir=/var"
+${THOME}/build/genpkg TRIBstunnel stunnel-5.38
