fe98574589851e2ccd58b083cee2f5c4cbf66fbf
Peter Tribble <peter.tribble@gmail.com>
2016-12-11 11:47:53 +0000

    Update scons 2.5.1

--- TRIBscons/build.sh
+++ TRIBscons/build.sh
@@ -2,8 +2,8 @@
 #
 # (built in python)
 #
-${THOME}/build/unpack scons-2.4.1
-cd scons-2.4.1
+${THOME}/build/unpack scons-2.5.1
+cd scons-2.5.1
 python setup.py build
 rm -fr /tmp/sc1
 python setup.py install --root=/tmp/sc1
