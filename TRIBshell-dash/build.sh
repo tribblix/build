c6d5b6eda4d05a0c86b9c2fa44ee4db101cbe9be
Peter Tribble <peter.tribble@gmail.com>
2016-12-11 11:52:18 +0000

    Update dash 0.5.9.1

--- TRIBshell-dash/build.sh
+++ TRIBshell-dash/build.sh
@@ -2,5 +2,5 @@
 #
 # internal scripts fail with our pos awk
 #
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild dash-0.5.9
-${THOME}/build/genpkg TRIBshell-dash dash-0.5.9
+env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild dash-0.5.9.1
+${THOME}/build/genpkg TRIBshell-dash dash-0.5.9.1
