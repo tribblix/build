192f8baf0990b52a736451006ab2006204607113
Peter Tribble <peter.tribble@gmail.com>
2016-12-11 12:13:43 +0000

    Update slang 2.3.1a, extend to 64-bits

--- TRIBslang/build.sh
+++ TRIBslang/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild slang-2.3.0
-cd slang-2.3.0
-gmake
-cd ..
-${THOME}/build/genpkg TRIBslang slang-2.3.0
+${THOME}/build/dobuild -64 slang-2.3.1a -J 1
+${THOME}/build/genpkg TRIBslang slang-2.3.1a
