1fe9b9e8b83d4e3edffd1d5bef79c0aa45809f44
Peter Tribble <peter.tribble@gmail.com>
2016-12-11 12:26:29 +0000

    Update zint 2.5

--- TRIBzint/build.sh
+++ TRIBzint/build.sh
@@ -2,11 +2,6 @@
 #
 # build both the regular and QT packages together
 #
-git clone git://git.code.sf.net/p/zint/code zint-code
-cd zint-code/
-mkdir build
-cd build/
-env PATH=/usr/versions/Qt-4.8/bin:$PATH cmake .. -DCMAKE_INSTALL_PREFIX=/usr
-gmake
-${THOME}/build/genpkg TRIBzint
-${THOME}/build/genpkg TRIBzint-qt
+env PATH=/usr/versions/Qt-4.8/bin:$PATH $THOME/build/cmbuild zint-2.5
+${THOME}/build/genpkg TRIBzint zint-2.5
+${THOME}/build/genpkg TRIBzint-qt zint-2.5
