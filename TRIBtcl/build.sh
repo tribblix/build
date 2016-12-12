02c0ab030c210554deccfa3bbeb6080a725a9cc8
Peter Tribble <peter.tribble@gmail.com>
2016-12-12 20:35:34 +0000

    Extend tcl/tk to 64-bits

--- TRIBtcl/build.sh
+++ TRIBtcl/build.sh
@@ -8,4 +8,17 @@ cd tcl8.6.6/unix
 env CFLAGS=-O ./configure --prefix=/usr
 env AWK=nawk gmake -j 4
 env LD_LIBRARY_PATH=/tmp/tc1/usr/lib gmake install DESTDIR=/tmp/tc1
+
+cd ../..
+
+mkdir -p 64bit
+cd 64bit
+${THOME}/build/unpack tcl8.6.6-src
+cd tcl8.6.6/unix
+env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/amd64 --libdir=/usr/lib/amd64
+env AWK=nawk gmake -j 4
+env LD_LIBRARY_PATH=/tmp/tc1/usr/lib gmake install DESTDIR=/tmp/tc1
+
+cd ../../..
+
 ${THOME}/build/create_pkg TRIBtcl /tmp/tc1
--- TRIBtk/build.sh
+++ TRIBtk/build.sh
@@ -8,6 +8,18 @@ cd tk8.6.6/unix
 env CFLAGS=-O ./configure --prefix=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
+
+cd ../..
+
+mkdir -p 64bit
+cd 64bit
+${THOME}/build/unpack tk8.6.6-src
+cd tk8.6.6/unix
+env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/amd64 --libdir=/usr/lib/amd64
+gmake -j 4
+gmake install DESTDIR=/tmp/tk1
 ${THOME}/build/create_pkg TRIBtk /tmp/tk1
+
+cd ../../..
 
 rm -fr /tmp/tk1 /tmp/tc1
