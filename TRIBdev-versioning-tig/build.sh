7b5276e8b324053bda5260494c78e983f6a66cdd
Peter Tribble <peter.tribble@gmail.com>
2016-12-13 16:02:00 +0000

    Update tig 2.2

--- TRIBdev-versioning-tig/build.sh
+++ TRIBdev-versioning-tig/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS=-I/usr/gnu/include LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" CC=gcc ${THOME}/build/dobuild tig-2.1.1 -C --with-ncurses
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBdev-versioning-tig tig-2.1.1
+env CPPFLAGS=-I/usr/gnu/include TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" CC=gcc ${THOME}/build/dobuild tig-2.2 -C --with-ncurses
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBdev-versioning-tig tig-2.2
