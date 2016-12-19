aa5d15e5cce87c035748657e9ba5a3740dd21747
Peter Tribble <peter.tribble@gmail.com>
2016-12-19 15:52:03 +0000

    Update gpgme 1.8.0, extend to 64-bits, fix python locations

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -3,5 +3,5 @@
 # must not have QT5 installed, else it will try and build against
 # it and fail, but there's no easy way to turn the QT support off
 #
-${THOME}/build/dobuild gpgme-1.7.1
-${THOME}/build/genpkg TRIBgpgme gpgme-1.7.1
+${THOME}/build/dobuild -64 gpgme-1.8.0
+${THOME}/build/genpkg TRIBgpgme gpgme-1.8.0
