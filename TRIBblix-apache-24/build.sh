ff96f1d377259cb0ddeeab3f78dac2709e2bcfa1
Peter Tribble <peter.tribble@gmail.com>
2016-12-20 21:06:54 +0000

    Update apache 2.4.35

--- TRIBblix-apache-24/build.sh
+++ TRIBblix-apache-24/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild httpd-2.4.23 -P /opt/tribblix/apache2 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
-${THOME}/build/genpkg TRIBblix-apache-24 httpd-2.4.23
+${THOME}/build/dobuild httpd-2.4.25 -P /opt/tribblix/apache2 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
+${THOME}/build/genpkg TRIBblix-apache-24 httpd-2.4.25
