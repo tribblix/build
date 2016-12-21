96cbffc2599c50d27fef528f5069093b74b62180
Peter Tribble <peter.tribble@gmail.com>
2016-12-21 20:09:48 +0000

    Update node 7.3.0

--- TRIBv-node-v7/build.sh
+++ TRIBv-node-v7/build.sh
@@ -5,7 +5,7 @@
 # note that we can't use the normal -64 build mechanism as node
 # has its own way of doing it
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v7.2.1 -P /usr/versions/node-v7 -C --without-dtrace
-mv node-v7.2.1 node-v7.2.1-32
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v7.2.1 -P /usr/versions/node-v7 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v7 node-v7.2.1
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v7.3.0 -P /usr/versions/node-v7 -C --without-dtrace
+mv node-v7.3.0 node-v7.3.0-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v7.3.0 -P /usr/versions/node-v7 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v7 node-v7.3.0
