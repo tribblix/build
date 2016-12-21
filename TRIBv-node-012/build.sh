cd4c2af8e2a95cab41847fc8681363e76131ed49
Peter Tribble <peter.tribble@gmail.com>
2016-12-21 20:23:16 +0000

    Update node 0.12.18

--- TRIBv-node-012/build.sh
+++ TRIBv-node-012/build.sh
@@ -2,5 +2,5 @@
 #
 # as of Tribblix 0m17 we don't need to patch in libproc.h
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v0.12.17 -P /usr/versions/node-v0.12 -C --without-dtrace
-${THOME}/build/genpkg TRIBv-node-012 node-v0.12.17
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v0.12.18 -P /usr/versions/node-v0.12 -C --without-dtrace
+${THOME}/build/genpkg TRIBv-node-012 node-v0.12.18
