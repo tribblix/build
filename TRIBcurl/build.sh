2821995589460734999149d1f9da4bfff4c91918
Peter Tribble <peter.tribble@gmail.com>
2016-12-23 11:15:09 +0000

    Update curl 7.52.1

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -2,5 +2,5 @@
 #
 # [needs a postconf patch]
 #
-${THOME}/build/dobuild -64 curl-7.51.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --without-libssh2 --without-nghttp2"
-${THOME}/build/genpkg TRIBcurl curl-7.51.0
+${THOME}/build/dobuild -64 curl-7.52.1 -C "--with-ca-bundle=/etc/openssl/cacert.pem"
+${THOME}/build/genpkg TRIBcurl curl-7.52.1
