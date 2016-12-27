02e5661d49cd8cb9fd1ea1c440a73e27cae4ebef
Peter Tribble <peter.tribble@gmail.com>
2016-12-27 11:38:43 +0000

    Update mawk to 20161120 release

--- TRIBmawk/build.sh
+++ TRIBmawk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild mawk-1.3.4-20141206
-${THOME}/build/genpkg TRIBmawk mawk-1.3.4-20141206
+${THOME}/build/dobuild mawk-1.3.4-20161120
+${THOME}/build/genpkg TRIBmawk mawk-1.3.4-20161120
