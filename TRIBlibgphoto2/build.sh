3cd65e8bc206adf8dbcf90027ed3a8624620f5c5
Peter Tribble <peter.tribble@gmail.com>
2017-01-08 15:41:35 +0000

    Update libgphoto2 2.5.12

--- TRIBlibgphoto2/build.sh
+++ TRIBlibgphoto2/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env LD_ALTEXEC=/usr/gnu/bin/ld ${THOME}/build/dobuild libgphoto2-2.5.10
-env LD_ALTEXEC=/usr/gnu/bin/ld ${THOME}/build/genpkg TRIBlibgphoto2 libgphoto2-2.5.10
+# needs libtool installed
+#
+env LD_ALTEXEC=/usr/gnu/bin/ld ${THOME}/build/dobuild libgphoto2-2.5.12
+env LD_ALTEXEC=/usr/gnu/bin/ld ${THOME}/build/genpkg TRIBlibgphoto2 libgphoto2-2.5.12
