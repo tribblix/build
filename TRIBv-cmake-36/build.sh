a7297691acb4d6497b498562e8d981b7a13dfb9e
Peter Tribble <peter.tribble@gmail.com>
2017-01-08 14:07:07 +0000

    Update cmake 3.6.3

--- TRIBv-cmake-36/build.sh
+++ TRIBv-cmake-36/build.sh
@@ -2,9 +2,10 @@
 #
 #
 #
-${THOME}/build/unpack cmake-3.6.2
-cd cmake-3.6.2
+${THOME}/build/unpack cmake-3.6.3
+cd cmake-3.6.3
 ./bootstrap --prefix=/usr/versions/cmake-3.6
 gmake -j 4
 # [gmake test - all passed]
 ${THOME}/build/genpkg TRIBv-cmake-36
+cd ..
