18b5376379ca09d669f192d327103c0577925e61
Peter Tribble <peter.tribble@gmail.com>
2017-01-09 07:42:49 +0000

    Update libvncserver 0.9.11

--- TRIBlibvncserver/build.sh
+++ TRIBlibvncserver/build.sh
@@ -2,5 +2,5 @@
 #
 # libvncserver
 #
-${THOME}/build/dobuild libvncserver-LibVNCServer-0.9.10
-${THOME}/build/genpkg TRIBlibvncserver libvncserver-LibVNCServer-0.9.10
+${THOME}/build/dobuild libvncserver-LibVNCServer-0.9.11
+${THOME}/build/genpkg TRIBlibvncserver libvncserver-LibVNCServer-0.9.11
