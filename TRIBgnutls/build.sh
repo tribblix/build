695fc4667c09332cfa5ccd2293b152d89873e3e7
Peter Tribble <peter.tribble@gmail.com>
2017-01-10 10:29:54 +0000

    Update gnutls 3.3.26

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # gnutls, now 64-bit (on x86)
-${THOME}/build/dobuild -64 gnutls-3.3.25 -C --disable-doc
+${THOME}/build/dobuild -64 gnutls-3.3.26 -C --disable-doc
 # sparc is 32-bit only because of libgmp errors
-# ${THOME}/build/dobuild gnutls-3.3.25 -C --disable-doc
-${THOME}/build/genpkg TRIBgnutls gnutls-3.3.25
+# ${THOME}/build/dobuild gnutls-3.3.26 -C --disable-doc
+${THOME}/build/genpkg TRIBgnutls gnutls-3.3.26
