ae146a794ab295bc0f1df47a3c02f79558a779a8
Peter Tribble <peter.tribble@gmail.com>
2017-01-14 12:00:09 +0000

    Update speex 1.2.0, incorporating separate speexdsp

--- TRIBlibspeex/build.sh
+++ TRIBlibspeex/build.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild speex-1.2rc1
-${THOME}/build/genpkg TRIBlibspeex speex-1.2rc1
+# note that it looks like the VERSION has gone backwards, it hasn't
+# the older 1.2.1 package was 1.2rc1
+#
+# you need a multi-stage build here
+#
+# if building from scratch, just build and package speex, then install it
+# then build speexdsp and create a package with both, and install that
+# then rebuild and package both
+#
+# if you already have a recent build, then you'll just need to build
+# both, package and install, then repeat
+#
+# if you have an older (1.2rc1) version installed, you need to uninstall
+# it first
+#
+${THOME}/build/dobuild -64 speex-1.2.0
+${THOME}/build/dobuild -64 speexdsp-1.2rc3
+${THOME}/build/genpkg TRIBlibspeex speex-1.2.0 speexdsp-1.2rc3
