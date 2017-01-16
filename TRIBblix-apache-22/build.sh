49c35b5aa0b91e2ee8d79a5cd8aa09d9a4fcacc2
Peter Tribble <peter.tribble@gmail.com>
2017-01-16 11:44:53 +0000

    Update apache httpd 2.2.32

--- TRIBblix-apache-22/build.sh
+++ TRIBblix-apache-22/build.sh
@@ -2,5 +2,5 @@
 #
 # this is used for the illumos build
 #
-${THOME}/build/dobuild httpd-2.2.31 -P /opt/tribblix/apache22 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
-${THOME}/build/genpkg TRIBblix-apache-22 httpd-2.2.31
+${THOME}/build/dobuild httpd-2.2.32 -P /opt/tribblix/apache22 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
+${THOME}/build/genpkg TRIBblix-apache-22 httpd-2.2.32
