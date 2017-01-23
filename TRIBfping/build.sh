cae6b277a44f8832df9425bfea0d3209e0407bc4
Peter Tribble <peter.tribble@gmail.com>
2017-01-23 09:57:06 +0000

    Update fping 3.15

--- TRIBfping/build.sh
+++ TRIBfping/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild fping-3.13 -C --enable-ipv6
-${THOME}/build/genpkg TRIBfping fping-3.13
+${THOME}/build/dobuild fping-3.15 -C --enable-ipv6
+${THOME}/build/genpkg TRIBfping fping-3.15
