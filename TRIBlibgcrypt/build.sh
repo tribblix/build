60d1834844dc3f1dad853225dd4c07a5cadb7393
Peter Tribble <peter.tribble@gmail.com>
2017-01-24 20:44:12 +0000

    Update libgcrypt 1.7.6

--- TRIBlibgcrypt/build.sh
+++ TRIBlibgcrypt/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild libgcrypt-1.7.5
-mv libgcrypt-1.7.5 libgcrypt-1.7.5-32bit
-${THOME}/build/dobuild +64 libgcrypt-1.7.5 -C "--disable-amd64-as-feature-detection --disable-asm"
-mv libgcrypt-1.7.5-32bit libgcrypt-1.7.5
-${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.7.5
+${THOME}/build/dobuild libgcrypt-1.7.6
+mv libgcrypt-1.7.6 libgcrypt-1.7.6-32bit
+${THOME}/build/dobuild +64 libgcrypt-1.7.6 -C "--disable-amd64-as-feature-detection --disable-asm"
+mv libgcrypt-1.7.6-32bit libgcrypt-1.7.6
+${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.7.6
