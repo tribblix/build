956417722cef76f2b606a8923ba909bc4d39e200
Peter Tribble <peter.tribble@gmail.com>
2017-01-25 15:15:31 +0000

    Rebuild expect against the current tcl/tk versions

--- TRIBexpect/build.sh
+++ TRIBexpect/build.sh
@@ -3,8 +3,8 @@
 # needs to be built with the tcl/tk source, not the installed versions
 #
 ${THOME}/build/unpack expect5.45
-${THOME}/build/unpack tcl8.6.4-src
-${THOME}/build/unpack tk8.6.4-src
+${THOME}/build/unpack tcl8.6.6-src
+${THOME}/build/unpack tk8.6.6-src
 cd expect5.45
 ./configure --prefix=/usr
 gmake -j 8
