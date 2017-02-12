d87e96218da9d8b7846006da8697ab46982c78f6
Peter Tribble <peter.tribble@gmail.com>
2017-02-12 13:23:05 +0000

    Update wget 1.19.1

--- TRIBwget/build.sh
+++ TRIBwget/build.sh
@@ -2,5 +2,5 @@
 #
 # (no pcre in the install environment)
 #
-env POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.18 -C '--with-ssl=openssl --disable-pcre'
-${THOME}/build/genpkg TRIBwget wget-1.18
+env POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.19.1 -C '--with-ssl=openssl --disable-pcre'
+${THOME}/build/genpkg TRIBwget wget-1.19.1
