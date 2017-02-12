00773a591820e6dc71fb96194c9cf74bc4ec5937
Peter Tribble <peter.tribble@gmail.com>
2017-02-12 12:25:00 +0000

    Update zint 2.5.1

--- TRIBzint/build.sh
+++ TRIBzint/build.sh
@@ -2,6 +2,6 @@
 #
 # build both the regular and QT packages together
 #
-env PATH=/usr/versions/Qt-4.8/bin:$PATH $THOME/build/cmbuild zint-2.5
-${THOME}/build/genpkg TRIBzint zint-2.5
-${THOME}/build/genpkg TRIBzint-qt zint-2.5
+env PATH=/usr/versions/Qt-4.8/bin:$PATH $THOME/build/cmbuild zint-2.5.1
+${THOME}/build/genpkg TRIBzint zint-2.5.1
+${THOME}/build/genpkg TRIBzint-qt zint-2.5.1
