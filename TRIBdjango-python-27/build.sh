6b40cae919903c6db30e76706ccd0c3b97a38180
Peter Tribble <peter.tribble@gmail.com>
2017-02-12 12:37:39 +0000

    Update django 1.10.5

--- TRIBdjango-python-27/build.sh
+++ TRIBdjango-python-27/build.sh
@@ -2,9 +2,10 @@
 #
 #
 #
-${THOME}/build/unpack Django-1.10.4
-cd Django-1.10.4
+${THOME}/build/unpack Django-1.10.5
+cd Django-1.10.5
 rm -fr /tmp/p2a
 python setup.py install --root=/tmp/p2a
 ${THOME}/build/create_pkg TRIBdjango-python-27 /tmp/p2a
+cd ..
 rm -fr /tmp/p2a
--- TRIBdjango-python-36/build.sh
+++ TRIBdjango-python-36/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-1.10.4
-cd Django-1.10.4
+${THOME}/build/unpack Django-1.10.5
+cd Django-1.10.5
 rm -fr /tmp/p3a
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango-python-36 /tmp/p3a
