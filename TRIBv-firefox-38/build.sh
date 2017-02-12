01f808e5c357c63373b0ced656ca053e6adb91a0
Peter Tribble <peter.tribble@gmail.com>
2017-02-12 17:04:53 +0000

    Deprecate old thunderbird + firefox

--- TRIBv-firefox-38/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/pkg_tarball TRIBv-firefox-38 firefox-38.6.1esr.en-US.solaris-10-fcs-i386
--- TRIBv-thunderbird-38/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/pkg_tarball TRIBv-thunderbird-38 thunderbird-38.8.0.en-US.solaris-10-fcs-i386
