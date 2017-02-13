73e82bde9f8deabf424c0ad6daef84c2370e7c9f
Peter Tribble <peter.tribble@gmail.com>
2017-02-13 10:07:01 +0000

    Fix tracker build and packaging

--- TRIBtracker/build.sh
+++ TRIBtracker/build.sh
@@ -3,5 +3,10 @@
 # (downrev due to sqlite3 - have 3.7.6.3; 0.14.5 => 3.7.0, later => 3.7.9)
 # (disable gstreamer cos it uses gstreamer-0.10)
 #
-env LIBS=-luuid ${THOME}/build/dobuild -gnu tracker-0.14.5 -C "--disable-libgif CFLAGS=-D__EXTENSIONS__ --enable-generic-media-extractor=external"
+# must build without gtk3
+#
+# it claims to no longer need dbus-glib but that's not quite true
+# hence the make -k
+#
+env LIBS=-luuid ${THOME}/build/dobuild -gnu tracker-0.14.5 -C "--disable-libgif CFLAGS=-D__EXTENSIONS__ --enable-generic-media-extractor=external --disable-introspection" -M -k
 ${THOME}/build/genpkg TRIBtracker tracker-0.14.5
