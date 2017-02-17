65b3a6599a33993c58981b8a05f27b5c0a993867
Peter Tribble <peter.tribble@gmail.com>
2017-02-17 09:21:02 +0000

    Update ristretto 0.8.2

--- TRIBxfce-ristretto/build.sh
+++ TRIBxfce-ristretto/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu ristretto-0.8.1
-${THOME}/build/genpkg  TRIBxfce-ristretto ristretto-0.8.1
+${THOME}/build/dobuild -gnu ristretto-0.8.2
+${THOME}/build/genpkg  TRIBxfce-ristretto ristretto-0.8.2
