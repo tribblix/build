1a52403aba52ace90eb9c080397b6ec560db3062
Peter Tribble <peter.tribble@gmail.com>
2017-02-19 12:17:25 +0000

    Add Slony-I 2.2.5

---
+++ TRIBblix-slony96/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# slony for postgres 9.6
+#
+# the binaries are linked against postgres libraries, so this package
+# installs into the same prefix as the postgres version it's built
+# against, and is versioned to match
+#
+$THOME/build/dobuild slony1-2.2.5 -P /opt/tribblix/postgres96 -C "--with-pgconfigdir=/opt/tribblix/postgres96/bin --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-slony96 slony1-2.2.5
