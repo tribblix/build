2de9d5b283dad0ff4589554b7c8c3bb7b9d43665
Peter Tribble <peter.tribble@gmail.com>
2017-02-20 16:27:03 +0000

    Update ORC 0.4.26

--- TRIBorc/build.sh
+++ TRIBorc/build.sh
@@ -2,5 +2,5 @@
 #
 # [needed by libvisual]
 #
-${THOME}/build/dobuild orc-0.4.17
-${THOME}/build/genpkg TRIBorc orc-0.4.17
+${THOME}/build/dobuild -64 orc-0.4.26
+${THOME}/build/genpkg TRIBorc orc-0.4.26
