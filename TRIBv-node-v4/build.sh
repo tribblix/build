ac55d22567b182f33e9dd406112bc4ec24ccb30f
Peter Tribble <peter.tribble@gmail.com>
2017-02-22 16:43:22 +0000

    Update node 4.8.0 6.10.0 7.6.0

--- TRIBv-node-v4/build.sh
+++ TRIBv-node-v4/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v4.7.3 -P /usr/versions/node-v4 -C --without-dtrace
-${THOME}/build/genpkg TRIBv-node-v4 node-v4.7.3
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v4.8.0 -P /usr/versions/node-v4 -C --without-dtrace
+${THOME}/build/genpkg TRIBv-node-v4 node-v4.8.0
