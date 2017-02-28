fe17433dae8957fa60443a062374244847f59c42
Peter Tribble <peter.tribble@gmail.com>
2017-02-28 11:10:13 +0000

    convert the 2 ttf fonts from OI into native packages

---
+++ TRIBfont-ttype-bitstream-vera/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# all the work done by fixinstall
+#
+${THOME}/build/unpack ttf-bitstream-vera-1.10
+${THOME}/build/genpkg TRIBfont-ttype-bitstream-vera ttf-bitstream-vera-1.10
---
+++ TRIBfont-ttype-dejavu/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# all the work done by fixinstall
+#
+${THOME}/build/unpack dejavu-fonts-ttf-2.37
+${THOME}/build/genpkg TRIBfont-ttype-dejavu dejavu-fonts-ttf-2.37
