ed918c754c9307057dfc0379b4933364b702a29a
Peter Tribble <peter.tribble@gmail.com>
2017-03-06 09:35:37 +0000

    Update gnutls 3.3.27

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #
 # gnutls, now 64-bit (on x86)
-${THOME}/build/dobuild -64 gnutls-3.3.26 -C --disable-doc
+#
+# disable tpm because the code isn't even self-consistent and autconf
+# can't work out what to do on its own
+#
+${THOME}/build/dobuild -64 gnutls-3.3.27 -C "--disable-doc --without-tpm"
 # sparc is 32-bit only because of libgmp errors
-# ${THOME}/build/dobuild gnutls-3.3.26 -C --disable-doc
-${THOME}/build/genpkg TRIBgnutls gnutls-3.3.26
+# ${THOME}/build/dobuild gnutls-3.3.27 -C "--disable-doc --without-tpm"
+${THOME}/build/genpkg TRIBgnutls gnutls-3.3.27
