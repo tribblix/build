00a2d5c774e25af188326dae99fe73b7d714cb37
Peter Tribble <peter.tribble@gmail.com>
2017-03-12 16:31:19 +0000

    Update ffmpeg 2.8.11

--- TRIBffmpeg/build.sh
+++ TRIBffmpeg/build.sh
@@ -2,5 +2,5 @@
 #
 # nasm (or yasm) is a build-time dependency - zap install nasm
 #
-env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -64 ffmpeg-2.8.10 -C "--enable-shared --disable-static"
-${THOME}/build/genpkg TRIBffmpeg ffmpeg-2.8.10
+env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -64 ffmpeg-2.8.11 -C "--enable-shared --disable-static"
+${THOME}/build/genpkg TRIBffmpeg ffmpeg-2.8.11
