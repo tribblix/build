9bdfebf5880c9a311202daa34023cc0ed08d19f3
Peter Tribble <peter.tribble@gmail.com>
2017-03-21 21:51:26 +0000

    Add psycopg2 2.7.1

---
+++ TRIBpsycopg2-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack psycopg2-2.7.1
+cd psycopg2-2.7.1
+env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib python setup.py build
+rm -fr /tmp/pil
+env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpsycopg2-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
---
+++ TRIBpsycopg2-36/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack psycopg2-2.7.1
+cd psycopg2-2.7.1
+env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib /usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pil
+env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpsycopg2-36 /tmp/pil
+cd ..
+rm -fr /tmp/pil
