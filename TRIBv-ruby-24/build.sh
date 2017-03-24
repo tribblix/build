40c8f1e7b1d5f4185e3d3745688e0420a3e75c20
Peter Tribble <peter.tribble@gmail.com>
2017-03-24 19:57:05 +0000

    Update ruby 2.4.1

--- TRIBv-ruby-24/build.sh
+++ TRIBv-ruby-24/build.sh
@@ -3,5 +3,5 @@
 # ruby update (still segfaults on sparc)
 # just can't handle LD_OPTIONS
 #
-${THOME}/build/dobuild ruby-2.4.0 -P /usr/versions/ruby-2.4 -C ac_cv_func_dl_iterate_phdr=no -L
-${THOME}/build/genpkg TRIBv-ruby-24 ruby-2.4.0
+${THOME}/build/dobuild ruby-2.4.1 -P /usr/versions/ruby-2.4 -C ac_cv_func_dl_iterate_phdr=no -L
+${THOME}/build/genpkg TRIBv-ruby-24 ruby-2.4.1
