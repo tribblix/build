c0773a0b22e87deb1b3b19785448108e6be0b764
Peter Tribble <peter.tribble@gmail.com>
2017-03-24 10:52:49 +0000

    Updata samba 4.4.12

--- TRIBsamba4/build.sh
+++ TRIBsamba4/build.sh
@@ -2,5 +2,5 @@
 #
 # SPARC needs --without-ldap --without-ads
 #
-${THOME}/build/dobuild samba-4.4.10 -P /usr -C "--enable-fhs --sysconfdir=/etc --localstatedir=/var" -M -k
-${THOME}/build/genpkg TRIBsamba4 samba-4.4.10
+${THOME}/build/dobuild samba-4.4.12 -P /usr -C "--enable-fhs --sysconfdir=/etc --localstatedir=/var" -M -k
+${THOME}/build/genpkg TRIBsamba4 samba-4.4.12
