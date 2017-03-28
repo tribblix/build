8ee550fb50a1fae6635e3451791e14b204c25106
Peter Tribble <peter.tribble@gmail.com>
2017-03-28 08:04:35 +0100

    Update motif 2.3.7

--- TRIBmotif/build.sh
+++ TRIBmotif/build.sh
@@ -2,5 +2,5 @@
 #
 # 2.3.5 and earlier failed with parallel make and needed -J 1
 #
-${THOME}/build/dobuild motif-2.3.6 -L
-${THOME}/build/genpkg TRIBmotif motif-2.3.6
+${THOME}/build/dobuild motif-2.3.7 -L
+${THOME}/build/genpkg TRIBmotif motif-2.3.7
