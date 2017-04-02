46957643de0b22d80a276b4e006cc3dd3aae0f36
Peter Tribble <peter.tribble@gmail.com>
2017-04-02 17:59:38 +0100

    Update libmediaart 1.9.1

--- TRIBlibmediaart/build.sh
+++ TRIBlibmediaart/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libmediaart-1.9.0 -C --disable-introspection
-${THOME}/build/genpkg TRIBlibmediaart libmediaart-1.9.0
+${THOME}/build/dobuild -64 libmediaart-1.9.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBlibmediaart libmediaart-1.9.1
