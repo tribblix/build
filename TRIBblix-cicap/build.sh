08032400f67ff2468c26c0dc96d886c74832f79d
Peter Tribble <peter.tribble@gmail.com>
2017-04-02 10:07:37 +0100

    Update c-icap 0.5.2 and modules 0.4.5

--- TRIBblix-cicap/build.sh
+++ TRIBblix-cicap/build.sh
@@ -4,5 +4,5 @@
 #
 # see also TRIBblix-cicapmod
 #
-${THOME}/build/dobuild c_icap-0.4.4 -P /opt/tribblix/c-icap
-${THOME}/build/genpkg TRIBblix-cicap c_icap-0.4.4
+${THOME}/build/dobuild c_icap-0.5.2 -P /opt/tribblix/c-icap
+${THOME}/build/genpkg TRIBblix-cicap c_icap-0.5.2
--- TRIBblix-cicapmod/build.sh
+++ TRIBblix-cicapmod/build.sh
@@ -2,8 +2,8 @@
 #
 # modules need c-icap and clamav installed
 #
-${THOME}/build/dobuild c_icap_modules-0.4.4 -P /opt/tribblix/c-icap -C "--with-c-icap=/opt/tribblix/c-icap --with-clamav=/opt/tribblix/clamav"
-cd c_icap_modules-0.4.4
+${THOME}/build/dobuild c_icap_modules-0.4.5 -P /opt/tribblix/c-icap -C "--with-c-icap=/opt/tribblix/c-icap --with-clamav=/opt/tribblix/clamav"
+cd c_icap_modules-0.4.5
 rm -fr /tmp/cm
 mkdir -p /tmp/cm/opt/tribblix/c-icap/etc
 gmake install DESTDIR=/tmp/cm
