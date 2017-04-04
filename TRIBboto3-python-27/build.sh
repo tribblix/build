c163b863d6298a3b18d84b3cf5437e710768152a
Peter Tribble <peter.tribble@gmail.com>
2017-04-04 21:42:26 +0100

    Update boto3 to 1.4.4

--- TRIBboto3-python-27/build.sh
+++ TRIBboto3-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.4.0
-cd boto3-1.4.0
+${THOME}/build/unpack boto3-1.4.4
+cd boto3-1.4.4
 python setup.py build
 rm -fr /tmp/sut
 python setup.py install --root=/tmp/sut
--- TRIBboto3-python-36/build.sh
+++ TRIBboto3-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.4.0
-cd boto3-1.4.0
+${THOME}/build/unpack boto3-1.4.4
+cd boto3-1.4.4
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/sut
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/sut
