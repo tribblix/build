862c094c173ea5700612ae80f2960abec553f25b
Peter Tribble <peter.tribble@gmail.com>
2017-04-04 20:59:18 +0100

    Add jmespath python module

---
+++ TRIBjmespath-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack jmespath-0.9.2
+cd jmespath-0.9.2
+python setup.py build
+rm -fr /tmp/pdu
+python setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBjmespath-python-27 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
---
+++ TRIBjmespath-python-36/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack jmespath-0.9.2
+cd jmespath-0.9.2
+/usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pdu
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBjmespath-python-36 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
