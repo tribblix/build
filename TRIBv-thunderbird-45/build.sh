ec5519a7fce53a8ac69874f220a5be95d4de20a0
Peter Tribble <peter.tribble@gmail.com>
2017-04-05 14:40:15 +0100

    Update thunderbird 45.8.0

--- TRIBv-thunderbird-45/build.sh
+++ TRIBv-thunderbird-45/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball TRIBv-thunderbird-45 thunderbird-45.7.0.en-US.solaris-10-fcs-i386
+${THOME}/build/pkg_tarball TRIBv-thunderbird-45 thunderbird-45.8.0.en-US.solaris-10-fcs-i386
