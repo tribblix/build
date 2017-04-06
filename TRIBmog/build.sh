118a600814ecd0f080ec458f923617a71df53cf7
Peter Tribble <peter.tribble@gmail.com>
2017-04-06 21:34:46 +0100

    Add mog 0.6.0

---
+++ TRIBmog/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# needs pygments and file-magic amd mediainfo
+#
+${THOME}/build/unpack mog-0.6.0
+cd mog-0.6.0
+#
+# we need the file from file-magic
+#
+gsed -i "s:\['file':\['file-5.21':" mog/__init__.py
+python setup.py build
+rm -fr /tmp/sut
+python setup.py install --root=/tmp/sut
+${THOME}/build/create_pkg TRIBmog /tmp/sut
+cd ..
+rm -fr /tmp/sut
