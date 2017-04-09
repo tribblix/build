c968bb6a9c76ec9f3504ceb3d579d8d19ad8b508
Peter Tribble <peter.tribble@gmail.com>
2017-04-09 21:56:22 +0100

    Update babl to 0.1.24

--- TRIBlib-babl/build.sh
+++ TRIBlib-babl/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-# babl-pallette.c needs to #include <limits.h> to get INT_MAX
 # the build barfs on awk's crappiness, hence the AWK
 #
-env AWK=nawk ${THOME}/build/dobuild babl-0.1.10
-${THOME}/build/genpkg TRIBlib-babl babl-0.1.10
+env AWK=nawk ${THOME}/build/dobuild -64 babl-0.1.24 -C "--disable-mmx --disable-sse"
+${THOME}/build/genpkg TRIBlib-babl babl-0.1.24
