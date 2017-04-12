d05f5f6ef2be786ffeba9aa1543bb079ee318e82
Peter Tribble <peter.tribble@gmail.com>
2017-04-12 13:13:41 +0100

    Update zabbix 3.0.8

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-2.4.8 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres96/bin/pg_config"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-2.4.8
+${THOME}/build/dobuild zabbix-3.0.8 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres96/bin/pg_config"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-3.0.8
--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-2.4.8 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-2.4.8
+${THOME}/build/dobuild zabbix-3.0.8 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-3.0.8
