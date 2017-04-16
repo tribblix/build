72fcfa700d3aac241819a5fbcc3e170ec61d5353
Peter Tribble <peter.tribble@gmail.com>
2017-04-16 11:14:29 +0100

    Update libfastjson 0.99.4

--- TRIBlibfastjson/build.sh
+++ TRIBlibfastjson/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild libfastjson-0.99.2 -C CFLAGS=-std=gnu99
-${THOME}/build/genpkg TRIBlibfastjson libfastjson-0.99.2
+# need libtool installed for autogen.sh
+#
+${THOME}/build/dobuild -64 libfastjson-0.99.4
+${THOME}/build/genpkg TRIBlibfastjson libfastjson-0.99.4
