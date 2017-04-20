f8a508a6073a793f24fc073fcbaf0aed10e8228d
Peter Tribble <peter.tribble@gmail.com>
2017-04-20 09:55:59 +0100

    Update curl 7.54.0

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -2,5 +2,5 @@
 #
 # [needs a postconf patch]
 #
-${THOME}/build/dobuild -64 curl-7.53.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem"
-${THOME}/build/genpkg TRIBcurl curl-7.53.0
+${THOME}/build/dobuild -64 curl-7.54.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem"
+${THOME}/build/genpkg TRIBcurl curl-7.54.0
