bd0ad6c002b65d29f4f72b7c975dca7614c749bf
Peter Tribble <peter.tribble@gmail.com>
2017-04-22 13:44:10 +0100

    Update ggrep 3.0

--- TRIBtext-gnu-grep/build.sh
+++ TRIBtext-gnu-grep/build.sh
@@ -3,5 +3,5 @@
 # ggrep
 # NB. Must not have gnu iconv installed
 #
-${THOME}/build/dobuild -64 -gnu grep-2.28 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-grep grep-2.28
+${THOME}/build/dobuild -64 -gnu grep-3.0 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-grep grep-3.0
