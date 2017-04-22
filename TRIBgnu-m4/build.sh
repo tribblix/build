20a8e527687d80d3319d78f14f92a9635d9bb536
Peter Tribble <peter.tribble@gmail.com>
2017-04-22 11:08:42 +0100

    Update gm4 1.4.18, and 64-bit

--- TRIBgnu-m4/build.sh
+++ TRIBgnu-m4/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild m4-1.4.17 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-m4 m4-1.4.17
+${THOME}/build/dobuild -64 m4-1.4.18 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-m4 m4-1.4.18
