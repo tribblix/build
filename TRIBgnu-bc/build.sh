5463fd16a686c0ac963c81461ee72255ac63e8c2
Peter Tribble <peter.tribble@gmail.com>
2017-04-22 11:42:01 +0100

    Update gnu bc 1.07.1

--- TRIBgnu-bc/build.sh
+++ TRIBgnu-bc/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild bc-1.06 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-bc bc-1.06
+${THOME}/build/dobuild bc-1.07.1 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-bc bc-1.07.1
