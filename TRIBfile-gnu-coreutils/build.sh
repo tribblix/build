29b3881e80c0855e3cdd82c6a31818ba07767407
Peter Tribble <peter.tribble@gmail.com>
2017-04-22 17:35:09 +0100

    Update coreutils 8.27

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu coreutils-8.26 -P /usr/gnu -C --without-gmp
-${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.26
+${THOME}/build/dobuild -gnu coreutils-8.27 -P /usr/gnu -C --without-gmp
+${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.27
