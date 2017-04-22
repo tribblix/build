f1eb3021ea83ca9a2c5d231fd323c1544903c146
Peter Tribble <peter.tribble@gmail.com>
2017-04-22 16:58:27 +0100

    Update ant 1.9.9 1.10.1

--- TRIBv-apache-ant-110/build.sh
+++ TRIBv-apache-ant-110/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
+rm -fr /tmp/aa
 mkdir -p /tmp/aa/usr/versions
 cd /tmp/aa/usr/versions
-unzip /packages/localsrc/Ant/apache-ant-1.10.0-bin.zip
-cp /packages/localsrc/Ant/jsch-0.1.53.jar apache-ant-1.10.0/lib/
-cp /packages/localsrc/Ant/apache-ivy-2.4.0/ivy-2.4.0.jar apache-ant-1.10.0/lib/
-cp /packages/localsrc/Ant/apache-ivy-2.4.0/lib/{c,o}*.jar apache-ant-1.10.0/lib/
-cp /packages/localsrc/Ant/svnant-1.3.1/lib/svnant.jar apache-ant-1.10.0/lib
-cp /packages/localsrc/Ant/svnant-1.3.1/lib/svnClientAdapter.jar apache-ant-1.10.0/lib
+unzip /packages/localsrc/Ant/apache-ant-1.10.1-bin.zip
+cp /packages/localsrc/Ant/jsch-0.1.54.jar apache-ant-1.10.1/lib/
+cp /packages/localsrc/Ant/apache-ivy-2.4.0/ivy-2.4.0.jar apache-ant-1.10.1/lib/
+cp /packages/localsrc/Ant/apache-ivy-2.4.0/lib/{c,o}*.jar apache-ant-1.10.1/lib/
+cp /packages/localsrc/Ant/svnant-1.3.1/lib/svnant.jar apache-ant-1.10.1/lib
+cp /packages/localsrc/Ant/svnant-1.3.1/lib/svnClientAdapter.jar apache-ant-1.10.1/lib
 ${THOME}/build/create_pkg TRIBv-apache-ant-110 /tmp/aa
 cd
 rm -fr /tmp/aa
