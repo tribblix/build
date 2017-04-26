dccfd98eeb4eb7e0aff1ff929579fd46cbe367ad
Peter Tribble <peter.tribble@gmail.com>
2017-04-26 09:33:38 +0100

    Update zabbix 3.0.9

--- TRIBzabbix-proxy/build.sh
+++ TRIBzabbix-proxy/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.8 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl"
-${THOME}/build/genpkg TRIBzabbix-proxy zabbix-3.0.8
+${THOME}/build/dobuild zabbix-3.0.9 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl"
+${THOME}/build/genpkg TRIBzabbix-proxy zabbix-3.0.9
