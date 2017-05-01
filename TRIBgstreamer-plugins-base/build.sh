5e3854453172f9ac45e83f95fbc4dd721970ce2c
Peter Tribble <peter.tribble@gmail.com>
2017-05-01 22:43:46 +0100

    Refresh gstreamer-0.10, extending to 64-bits

--- TRIBgstreamer-plugins-base/build.sh
+++ TRIBgstreamer-plugins-base/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild -gnu gst-plugins-base-0.10.33
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/genpkg TRIBgstreamer-plugins-base gst-plugins-base-0.10.33
+env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild -64 -gnu gst-plugins-base-0.10.36 -C "--disable-introspection --disable-examples"
+env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/genpkg TRIBgstreamer-plugins-base gst-plugins-base-0.10.36
--- TRIBgstreamer/build.sh
+++ TRIBgstreamer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild -gnu gstreamer-0.10.36
+env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild -64 -gnu gstreamer-0.10.36 -C --disable-introspection
 env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/genpkg TRIBgstreamer gstreamer-0.10.36
