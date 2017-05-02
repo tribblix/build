a2c25f106f437fb5c667e3e61d1d35cd5d5d2210
Peter Tribble <peter.tribble@gmail.com>
2017-05-02 09:38:48 +0100

    Extend farstream to 64-bits

--- TRIBfarstream/build.sh
+++ TRIBfarstream/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild farstream-0.2.8 -C "--disable-python"
+${THOME}/build/dobuild -64 farstream-0.2.8 -C "--disable-python --disable-introspection"
 ${THOME}/build/genpkg TRIBfarstream farstream-0.2.8
