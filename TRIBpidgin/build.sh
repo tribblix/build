61a841a57d848a79f88657aaa47fa92f714f6c39
Peter Tribble <peter.tribble@gmail.com>
2017-05-02 20:20:56 +0100

    Update pidgin 2.12.0

--- TRIBpidgin/build.sh
+++ TRIBpidgin/build.sh
@@ -3,5 +3,5 @@
 # (needs a patch to configure so libgadu doesnt barf on gnutls)
 # it's basically broken and requires older gstreamer/farstream
 #
-env CPPFLAGS=-D__SunOS ${THOME}/build/dobuild -gnu pidgin-2.11.0 -C "--disable-meanwhile --disable-vv --disable-farstream --disable-avahi --disable-nm --disable-tcl --disable-perl"
-${THOME}/build/genpkg TRIBpidgin pidgin-2.11.0
+env TRIBBLIX_CPPFLAGS=-D__SunOS ${THOME}/build/dobuild -gnu pidgin-2.12.0 -C "--disable-meanwhile --disable-vv --disable-farstream --disable-avahi --disable-nm --disable-tcl --disable-perl"
+${THOME}/build/genpkg TRIBpidgin pidgin-2.12.0
