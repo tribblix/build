16e2659505778c6f20cb31842127c5460052bc4d
Peter Tribble <peter.tribble@gmail.com>
2017-05-11 13:46:09 +0100

    Add ConfigArgParse 0.11.0

---
+++ TRIBconfigargparse-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack ConfigArgParse-0.11.0
+cd ConfigArgParse-0.11.0
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBconfigargparse-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
---
+++ TRIBconfigargparse-python-36/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack ConfigArgParse-0.11.0
+cd ConfigArgParse-0.11.0
+/usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBconfigargparse-python-36 /tmp/pil
+cd ..
+rm -fr /tmp/pil
