f4a9802fef166af0001d2c2a2919d30134f91d3d
Peter Tribble <peter.tribble@gmail.com>
2017-05-14 10:14:17 +0100

    Update htmldoc 1.8.30

--- TRIBhtmldoc/build.sh
+++ TRIBhtmldoc/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild htmldoc-1.8.29 -C "--enable-openssl --disable-gnutls"
-cd htmldoc-1.8.29
+${THOME}/build/dobuild htmldoc-1.8.30
+cd htmldoc-1.8.30
 rm -fr /tmp/hd
 gmake install datadir=/tmp/hd/usr/share mandir=/tmp/hd/usr/share/man bindir=/tmp/hd/usr/bin
 ${THOME}/build/create_pkg TRIBhtmldoc /tmp/hd
+cd ..
 rm -fr /tmp/hd
