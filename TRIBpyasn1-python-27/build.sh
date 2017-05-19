fe105921cb46904b40a782207a56c338f6401f5b
Peter Tribble <peter.tribble@gmail.com>
2017-05-19 13:41:02 +0100

    Add python pyasn1

---
+++ TRIBpyasn1-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pyasn1-0.2.3
+cd pyasn1-0.2.3
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpyasn1-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
