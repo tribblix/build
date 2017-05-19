06e35a9fc535bd56513b4d9adb5e45c9f8d81734
Peter Tribble <peter.tribble@gmail.com>
2017-05-19 13:35:54 +0100

    Add python concurrent.futures backport

---
+++ TRIBfutures-python-27/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# this is a backport of the python3 concurrent.futures module
+# it isn't needed for python3, obviously
+#
+${THOME}/build/unpack futures-3.1.1
+cd futures-3.1.1
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBfutures-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
