8e5fa7c18e9a52449eac191efe5cf93d42392606
Peter Tribble <peter.tribble@gmail.com>
2017-05-19 08:51:06 +0100

    Add AWS ecs-cli

---
+++ TRIBaws-ecs-cli/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+
+mkdir ec
+cd ec
+setenv GOPATH `pwd`
+mkdir -p src/github.com/aws
+cd src/github.com/aws
+git clone git@github.com:aws/amazon-ecs-cli.git
+cd amazon-ecs-cli
+
+#
+# a vanilla 'gmake build' fails with Termios errors, because
+# it turns cgo off, so run the build step explicitly
+#
+env CGO_CFLAGS=-std=gnu99 go build -installsuffix cgo -a -ldflags '-s' -o bin/local/ecs-cli ./ecs-cli/
+
+rm -fr /tmp/ee
+mkdir -p /tmp/ee/usr/bin
+cp bin/local/ecs-cli /tmp/ee/usr/bin
+mkdir -p /tmp/ee/usr/share/doc/ecs-cli
+cp LICENSE NOTICE README.md /tmp/ee/usr/share/doc/ecs-cli
+${THOME}/build/create_pkg TRIBaws-ecs-cli /tmp/ee
+rm -fr /tmp/ee
