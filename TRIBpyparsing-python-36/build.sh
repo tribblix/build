b213a870a2be74f8a206940a5b9c134bb53218f0
Peter Tribble <peter.tribble@gmail.com>
2017-05-20 21:57:54 +0100

    Add pyparsing 2.2.0

---
+++ TRIBpyparsing-python-36/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pyparsing-2.2.0
+cd pyparsing-2.2.0
+/usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpyparsing-python-36 /tmp/pil
+cd ..
+rm -fr /tmp/pil
