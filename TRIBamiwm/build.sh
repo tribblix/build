966d7ffa04ccf60436f9915ea288c1a0ff0cd282
Peter Tribble <peter.tribble@gmail.com>
2017-05-21 15:29:53 +0100

    Fix amiwm build

--- TRIBamiwm/build.sh
+++ TRIBamiwm/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild amiwm0.21pl2
+${THOME}/build/dobuild amiwm0.21pl2 -J 1
 cd amiwm0.21pl2
 gmake -n install DESTDIR=/tmp/a | sed 's:/usr:/tmp/a/usr:g' | sed -e s:/tmp/a/usr/bin/ginstall:/usr/bin/ginstall: -e s:/usr/man:/usr/share/man: >! doit.sh
 rm -fr /tmp/a
