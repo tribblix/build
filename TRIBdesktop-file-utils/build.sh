eef3eafd55a8b94caeba73babe92b48927c99741
Peter Tribble <peter.tribble@gmail.com>
2017-05-22 10:30:57 +0100

    Update desktop-file-utils 0.23

--- TRIBdesktop-file-utils/build.sh
+++ TRIBdesktop-file-utils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu desktop-file-utils-0.21
-${THOME}/build/genpkg TRIBdesktop-file-utils desktop-file-utils-0.21
+${THOME}/build/dobuild -gnu desktop-file-utils-0.23
+${THOME}/build/genpkg TRIBdesktop-file-utils desktop-file-utils-0.23
