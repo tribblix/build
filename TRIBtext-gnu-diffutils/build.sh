e4c229e24e81c50ce6d9ef0b3d419091e1d3a535
Peter Tribble <peter.tribble@gmail.com>
2017-05-25 11:15:44 +0100

    Update diffutils 3.6

--- TRIBtext-gnu-diffutils/build.sh
+++ TRIBtext-gnu-diffutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu diffutils-3.5 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-diffutils diffutils-3.5
+${THOME}/build/dobuild -gnu diffutils-3.6 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-diffutils diffutils-3.6
