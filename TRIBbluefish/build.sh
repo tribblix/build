d1967140d4fe5d39e49bf59a1e486d5ffd8cad3a
Peter Tribble <peter.tribble@gmail.com>
2017-05-25 11:09:27 +0100

    Update bluefish 2.2.10

--- TRIBbluefish/build.sh
+++ TRIBbluefish/build.sh
@@ -2,5 +2,5 @@
 #
 # bluefish (mustn't have gtk3 installed)
 #
-${THOME}/build/dobuild -gnu bluefish-2.2.9
-${THOME}/build/genpkg TRIBbluefish bluefish-2.2.9
+${THOME}/build/dobuild -gnu bluefish-2.2.10
+${THOME}/build/genpkg TRIBbluefish bluefish-2.2.10
