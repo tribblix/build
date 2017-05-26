7af288236a6c597c7074fcf62d2ce96e7314ef1a
Peter Tribble <peter.tribble@gmail.com>
2017-05-26 09:30:05 +0100

    Update vlc 2.2.6

--- TRIBvlc/build.sh
+++ TRIBvlc/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild vlc-2.2.4 -C --disable-lua
-${THOME}/build/genpkg TRIBvlc vlc-2.2.4
+${THOME}/build/dobuild vlc-2.2.6 -C --disable-lua
+${THOME}/build/genpkg TRIBvlc vlc-2.2.6
