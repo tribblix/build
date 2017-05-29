94dc4dd47cada2617752d2e1f177ed33fd0a84e5
Peter Tribble <peter.tribble@gmail.com>
2017-05-29 09:44:15 +0100

    Split-out make packages for sparc

--- TRIBdev-build-as/build.sh
+++ TRIBdev-build-as/build.sh
@@ -3,6 +3,8 @@
 # separate package for as (as illumos now supplies make)
 # Save the old binaries like so
 # (cd / ; tar cf ${THOME}/build/patches/binary-as-files.tar -I ${THOME}/build/patches/binary-as-files.txt)
+# or for sparc
+# (cd / ; tar cf ${THOME}/build/patches/binary-as-files.sparc.tar -I ${THOME}/build/patches/binary-as-files.sparc.txt)
 #
 rm -fr /tmp/as1 ; mkdir /tmp/as1
 ${THOME}/build/create_pkg TRIBdev-build-as /tmp/as1
--- TRIBdev-build-sccs/build.sh
+++ TRIBdev-build-sccs/build.sh
@@ -3,6 +3,8 @@
 # separate package for sccs (as illumos now supplies make)
 # Save the old binaries like so
 # (cd / ; tar cf ${THOME}/build/patches/binary-sccs-files.tar -I ${THOME}/build/patches/binary-sccs-files.txt)
+# or for sparc
+# (cd / ; tar cf ${THOME}/build/patches/binary-sccs-files.sparc.tar -I ${THOME}/build/patches/binary-sccs-files.sparc.txt)
 #
 rm -fr /tmp/as1 ; mkdir /tmp/as1
 ${THOME}/build/create_pkg TRIBdev-build-sccs /tmp/as1
