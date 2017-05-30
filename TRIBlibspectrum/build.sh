e318e5ef006a90fa19a6e5a170ab43cc1adf111a
Peter Tribble <peter.tribble@gmail.com>
2017-05-30 13:18:08 +0100

    Add fuse spectrum emulator

---
+++ TRIBlibspectrum/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild libspectrum-1.3.3
+${THOME}/build/genpkg TRIBlibspectrum libspectrum-1.3.3
---
+++ TRIBspectrum-fuse/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild fuse-1.3.5
+${THOME}/build/genpkg TRIBspectrum-fuse fuse-1.3.5
