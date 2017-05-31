2b689b4f41705b9d882deb3eae952fcf0474e7df
Peter Tribble <peter.tribble@gmail.com>
2017-05-31 19:50:12 +0100

    Extend hunspell to 64-bits

--- TRIBhunspell/build.sh
+++ TRIBhunspell/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild hunspell-1.3.4 -C "--disable-static CXXFLAGS=-fpermissive"
+env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 hunspell-1.3.4 -C "--disable-static"
 ${THOME}/build/genpkg TRIBhunspell hunspell-1.3.4
