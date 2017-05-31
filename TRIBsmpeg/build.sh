7c77e98558d8e52a504dc86281b1a7612f81edb6
Peter Tribble <peter.tribble@gmail.com>
2017-05-31 20:44:09 +0100

    Extend smpeg2 to 64-bits

--- TRIBsmpeg/build.sh
+++ TRIBsmpeg/build.sh
@@ -12,5 +12,23 @@ gmake -j 4
 chmod a+x install-sh
 rm -fr /tmp/c1
 gmake install DESTDIR=/tmp/c1
+#
+# now do the same again in 64-bits
+#
+gmake clean
+case `uname -p` in
+sparc)
+    ARCH64="sparcv9"
+    ;;
+i386)
+    ARCH64="amd64"
+    ;;
+esac
+env PKG_CONFIG_PATH=/usr/lib/${ARCH64}/pkgconfig PATH=/usr/bin/${ARCH64}:$PATH LDFLAGS="-m64" CFLAGS="-O -m64" CXXFLAGS="-O -m64" ./configure --prefix=/usr --bindir=/usr/${IPREFIX}/bin/${ARCH64} --libdir=/usr/lib/${ARCH64}
+# edit the Makefile so that CCASFLAGS = -m64
+# (configure lies to you)
+gmake -j 4
+gmake install DESTDIR=/tmp/c1
+
 ${THOME}/build/create_pkg TRIBsmpeg /tmp/c1
 rm -fr /tmp/c1
--- TRIBsmpeg2/build.sh
+++ TRIBsmpeg2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild smpeg2-2.0.0
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64 smpeg2-2.0.0
 ${THOME}/build/genpkg TRIBsmpeg2 smpeg2-2.0.0
