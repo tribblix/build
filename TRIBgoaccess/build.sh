a741c8a1cd169a6069846b7ee639869c483fdf88
Peter Tribble <peter.tribble@gmail.com>
2017-06-02 14:13:18 +0100

    Update goaccess 1.2

--- TRIBgoaccess/build.sh
+++ TRIBgoaccess/build.sh
@@ -2,5 +2,5 @@
 #
 # needs ncurses
 #
-env TRIBBLIX_CFLAGS="-std=gnu99 -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild goaccess-1.0.2 -C --sysconfdir=/etc
-${THOME}/build/genpkg TRIBgoaccess goaccess-1.0.2
+env TRIBBLIX_CFLAGS="-std=gnu99 -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild goaccess-1.2 -C --sysconfdir=/etc
+${THOME}/build/genpkg TRIBgoaccess goaccess-1.2
