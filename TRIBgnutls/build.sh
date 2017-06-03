736d287faee7c103caa222d4f02334cdd9cbdfef
Peter Tribble <peter.tribble@gmail.com>
2017-06-03 15:41:34 +0100

    Fix gmp/nettle/gnutls sparc build

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #
-# gnutls, now 64-bit (on x86)
+# gnutls, now 64-bit
 #
-# disable tpm because the code isn't even self-consistent and autconf
+# disable tpm because the code isn't even self-consistent and autoconf
 # can't work out what to do on its own
 #
 ${THOME}/build/dobuild -64 gnutls-3.3.27 -C "--disable-doc --without-tpm"
-# sparc is 32-bit only because of libgmp errors
-# ${THOME}/build/dobuild gnutls-3.3.27 -C "--disable-doc --without-tpm"
 ${THOME}/build/genpkg TRIBgnutls gnutls-3.3.27
