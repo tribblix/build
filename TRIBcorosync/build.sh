16e5ac604acff0f03b8af2471ffdaef88fbab786
Peter Tribble <peter.tribble@gmail.com>
2017-06-04 14:24:29 +0100

    Update corosync 2.4.2

--- TRIBcorosync/build.sh
+++ TRIBcorosync/build.sh
@@ -2,5 +2,5 @@
 #
 # 64-bit build fails as it picks up 32-bit NSS
 #
-${THOME}/build/dobuild corosync-2.4.1 -C "--localstatedir=/var --sysconfdir=/etc"
-${THOME}/build/genpkg TRIBcorosync corosync-2.4.1
+${THOME}/build/dobuild corosync-2.4.2 -C "--localstatedir=/var --sysconfdir=/etc"
+${THOME}/build/genpkg TRIBcorosync corosync-2.4.2
