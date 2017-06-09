19d1bbb6c80fe32f113b878644079135eb3d5b3b
Peter Tribble <peter.tribble@gmail.com>
2017-06-09 15:42:19 +0100

    Update node 8.1.0

--- TRIBv-node-v8/build.sh
+++ TRIBv-node-v8/build.sh
@@ -5,7 +5,7 @@
 # note that we can't use the normal -64 build mechanism as node
 # has its own way of doing it
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v8.0.0 -P /usr/versions/node-v8 -C --without-dtrace
-mv node-v8.0.0 node-v8.0.0-32
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v8.0.0 -P /usr/versions/node-v8 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v8 node-v8.0.0
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v8.1.0 -P /usr/versions/node-v8 -C --without-dtrace
+mv node-v8.1.0 node-v8.1.0-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v8.1.0 -P /usr/versions/node-v8 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v8 node-v8.1.0
