e9d0566c9ed8bdf9d52cd9d9831d006b8f3f6369
Peter Tribble <peter.tribble@gmail.com>
2017-06-10 13:22:26 +0100

    Add python gnureadline module

---
+++ TRIBgnureadline-python-36/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack gnureadline-6.3.3
+cd gnureadline-6.3.3
+env CFLAGS=-I/usr/gnu/include LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" /usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBgnureadline-python-36 /tmp/pil
+cd ..
+rm -fr /tmp/pil
