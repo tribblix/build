511c72b1415309640e5bcd044de8f79a216528eb
Peter Tribble <peter.tribble@gmail.com>
2017-06-17 21:55:27 +0100

    Update meson 0.41.0

--- TRIBmeson-python-36/build.sh
+++ TRIBmeson-python-36/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack meson-0.33.0
-cd meson-0.33.0
+${THOME}/build/unpack meson-0.41.0
+cd meson-0.41.0
 rm -fr /tmp/p3a
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBmeson-python-36 /tmp/p3a
