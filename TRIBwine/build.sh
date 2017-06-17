896ec226ed50f1825e31b67392e79404023a302e
Peter Tribble <peter.tribble@gmail.com>
2017-06-17 20:50:14 +0100

    Update wine 1.8.7

--- TRIBwine/build.sh
+++ TRIBwine/build.sh
@@ -2,5 +2,5 @@
 #
 # wine
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-1.8.6
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-1.8.6
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-1.8.7
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-1.8.7
