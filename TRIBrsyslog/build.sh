f919f606c5f6a5f2db4e89886b309a55e5b5e4ea
Peter Tribble <peter.tribble@gmail.com>
2017-06-17 21:48:25 +0100

    Update rsyslog 8.27.0

--- TRIBrsyslog/build.sh
+++ TRIBrsyslog/build.sh
@@ -2,5 +2,5 @@
 #
 # new version needs libfastjson rather than json-c
 #
-${THOME}/build/dobuild rsyslog-8.17.0 -C --disable-generate-man-pages
-${THOME}/build/genpkg TRIBrsyslog rsyslog-8.17.0
+${THOME}/build/dobuild rsyslog-8.27.0 -C "--disable-generate-man-pages --sysconfdir=/etc --localstatedir=/var --enable-imsolaris"
+${THOME}/build/genpkg TRIBrsyslog rsyslog-8.27.0
