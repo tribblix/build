ded7d745ab80b402893c0f0b6fbe7d1c9f9506c5
Peter Tribble <peter.tribble@gmail.com>
2017-06-26 15:48:32 +0100

    Update fvwm 2.6.7

--- TRIBfvwm/build.sh
+++ TRIBfvwm/build.sh
@@ -2,5 +2,5 @@
 #
 # This version requires pyxdg (TRIBxdg-python-27)
 #
-${THOME}/build/dobuild fvwm-2.6.6 -C "--disable-bidi --disable-perllib"
-${THOME}/build/genpkg TRIBfvwm fvwm-2.6.6
+${THOME}/build/dobuild fvwm-2.6.7 -C "--disable-bidi --disable-perllib"
+${THOME}/build/genpkg TRIBfvwm fvwm-2.6.7
