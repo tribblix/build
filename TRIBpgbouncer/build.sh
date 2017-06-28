d292174fdf5482e29f5910421c7ffeca8c59c4ec
Peter Tribble <peter.tribble@gmail.com>
2017-06-28 09:30:09 +0100

    fix sacricial typo

--- TRIBpgbouncer/build.sh
+++ TRIBpgbouncer/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# there's a sacricial static libevent built by the preconf script
+# there's a sacrificial static libevent built by the preconf script
 #
 ${THOME}/build/dobuild pgbouncer-1.7.2 -C --with-libevent=/tmp/mc
 ${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.7.2
