1382c7d0aa53cfd0f40c73306a7f16f5088444a0
Peter Tribble <peter.tribble@gmail.com>
2017-07-03 14:14:41 +0100

    Update pv 1.6.6

--- TRIBpipe-viewer/build.sh
+++ TRIBpipe-viewer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild pv-1.6.0 -C --disable-nls
-${THOME}/build/genpkg TRIBpipe-viewer pv-1.6.0
+${THOME}/build/dobuild pv-1.6.6 -C --disable-nls
+${THOME}/build/genpkg TRIBpipe-viewer pv-1.6.6
