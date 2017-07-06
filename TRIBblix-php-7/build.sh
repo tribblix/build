8a046ad4735cb758fe59780186dc5464ae83130e
Peter Tribble <peter.tribble@gmail.com>
2017-07-06 20:14:21 +0100

    Update php 7.0.21

--- TRIBblix-php-7/build.sh
+++ TRIBblix-php-7/build.sh
@@ -12,8 +12,8 @@
 # must have apache24 installed
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-7.0.20
-cd php-7.0.20
+${THOME}/build/unpack php-7.0.21
+cd php-7.0.21
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
