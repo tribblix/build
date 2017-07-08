dad750315ae0f8827f4f22c36cb192a14bbbc62b
Peter Tribble <peter.tribble@gmail.com>
2017-07-08 20:03:06 +0100

    Update nano 2.8.5

--- TRIBeditor-nano/build.sh
+++ TRIBeditor-nano/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 # on sparc at least, incompatible with ncurses being installed
-${THOME}/build/dobuild nano-2.5.3
-${THOME}/build/genpkg TRIBeditor-nano nano-2.5.3
+${THOME}/build/dobuild nano-2.8.5
+${THOME}/build/genpkg TRIBeditor-nano nano-2.8.5
