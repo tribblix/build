70da83af3a60d47322cf32334009c1f155cff6c2
Peter Tribble <peter.tribble@gmail.com>
2017-07-12 10:25:40 +0100

    Update pgpool-II 3.6.5

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,5 @@
 #
 # needs a postconf patch
 #
-env PATH=/opt/tribblix/postgres96/bin:$PATH ${THOME}/build/dobuild pgpool-II-3.6.4
-${THOME}/build/genpkg TRIBpgpool pgpool-II-3.6.4
+env PATH=/opt/tribblix/postgres96/bin:$PATH ${THOME}/build/dobuild pgpool-II-3.6.5
+${THOME}/build/genpkg TRIBpgpool pgpool-II-3.6.5
