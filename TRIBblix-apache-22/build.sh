ec5edac8781b7f9f729b374e78ff28a0d8757cdc
Peter Tribble <peter.tribble@gmail.com>
2017-07-14 20:23:27 +0100

    illumos no longer needs apache to build, now mod_ipp has gone

--- TRIBblix-apache-22/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# this is used for the illumos build
-#
-${THOME}/build/dobuild httpd-2.2.34 -P /opt/tribblix/apache22 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
-${THOME}/build/genpkg TRIBblix-apache-22 httpd-2.2.34
