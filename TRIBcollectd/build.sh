b78b8d951afe0de728e9e86910903ed1f65b7a08
Peter Tribble <peter.tribble@gmail.com>
2017-07-19 11:40:58 +0100

    Update collectd 5.7.2

--- TRIBcollectd/build.sh
+++ TRIBcollectd/build.sh
@@ -2,5 +2,5 @@
 #
 # zap install yajl
 #
-${THOME}/build/dobuild collectd-5.7.1 -C "--disable-static --localstatedir=/var CFLAGS=-std=gnu99 --sysconfdir=/etc --localstatedir=/var"
-${THOME}/build/genpkg TRIBcollectd collectd-5.7.1
+${THOME}/build/dobuild collectd-5.7.2 -C "--disable-static --localstatedir=/var CFLAGS=-std=gnu99 --sysconfdir=/etc --localstatedir=/var"
+${THOME}/build/genpkg TRIBcollectd collectd-5.7.2
