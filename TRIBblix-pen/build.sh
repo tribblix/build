bdb6f7eb58e90139223f664d68c09e7fad3366b3
Peter Tribble <peter.tribble@gmail.com>
2017-07-19 19:47:43 +0100

    Update pen 0.34.1

--- TRIBblix-pen/build.sh
+++ TRIBblix-pen/build.sh
@@ -2,5 +2,5 @@
 #
 # pen
 #
-${THOME}/build/dobuild pen-0.34.0 -P /opt/tribblix/pen
-${THOME}/build/genpkg TRIBblix-pen pen-0.34.0
+${THOME}/build/dobuild pen-0.34.1 -P /opt/tribblix/pen
+${THOME}/build/genpkg TRIBblix-pen pen-0.34.1
