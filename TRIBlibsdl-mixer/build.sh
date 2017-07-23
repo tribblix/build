b76a8c2d265206951fffeecc76b81a51254a58fe
Peter Tribble <peter.tribble@gmail.com>
2017-07-23 18:04:32 +0100

    LibSDL-mixer and LibSDL2-mixer should be 64-bit

--- TRIBlibsdl-mixer/build.sh
+++ TRIBlibsdl-mixer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild SDL_mixer-1.2.12
+${THOME}/build/dobuild -64 SDL_mixer-1.2.12
 ${THOME}/build/genpkg TRIBlibsdl-mixer SDL_mixer-1.2.12
--- TRIBlibsdl2-mixer/build.sh
+++ TRIBlibsdl2-mixer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild SDL2_mixer-2.0.1
+${THOME}/build/dobuild -64 SDL2_mixer-2.0.1
 ${THOME}/build/genpkg TRIBlibsdl2-mixer SDL2_mixer-2.0.1
