a5a998c04152524cf8168de640b40f2e021c46c9
Peter Tribble <peter.tribble@gmail.com>
2017-07-23 16:09:47 +0100

    Update psycopg 2.7.2

--- TRIBpsycopg2-36/build.sh
+++ TRIBpsycopg2-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psycopg2-2.7.1
-cd psycopg2-2.7.1
+${THOME}/build/unpack psycopg2-2.7.2
+cd psycopg2-2.7.2
 env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pil
 env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
