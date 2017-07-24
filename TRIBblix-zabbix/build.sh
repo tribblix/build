940204de2de6c6dcc93cb7e7cd759ec17a43407c
Peter Tribble <peter.tribble@gmail.com>
2017-07-24 09:54:58 +0100

    Update zabbix 3.0.10

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.9 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres96/bin/pg_config"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-3.0.9
+${THOME}/build/dobuild zabbix-3.0.10 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres96/bin/pg_config"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-3.0.10
