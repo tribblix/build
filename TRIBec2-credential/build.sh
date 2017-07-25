a999591ccc709c633f56a926a3913a1b4202203b
Peter Tribble <peter.tribble@gmail.com>
2017-07-25 11:58:58 +0100

    Add ec2 credential setup service

---
+++ TRIBec2-credential/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# copied verbatim from
+#
+# https://github.com/omniti-labs/omnios-build/tree/master/build/ec2-credential
+#
+# everything is done by the fixit script
+#
+${THOME}/build/genpkg TRIBec2-credential
