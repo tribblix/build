8d5d6ebcd75ca4fb67458132bb439ce4a0ac7707
Peter Tribble <peter.tribble@gmail.com>
2017-08-02 10:22:23 +0100

    Update mosh 1.3.2

--- TRIBprotobuf/build.sh
+++ TRIBprotobuf/build.sh
@@ -2,12 +2,12 @@
 #
 # Default build is 64-bit
 #
-${THOME}/build/unpack -64 protobuf-2.5.0
-cd protobuf-2.5.0-64bit
+${THOME}/build/unpack -64 protobuf-2.6.0
+cd protobuf-2.6.0-64bit
 ./configure --prefix=/usr --bindir=/usr/bin/amd64
 gmake -j 4
 gmake install DESTDIR=/tmp/pbf
-cd ../protobuf-2.5.0
+cd ../protobuf-2.6.0
 ./configure --prefix=/usr --disable-64bit-solaris
 gmake -j 4
 gmake install DESTDIR=/tmp/pbf
