0915e1b84b88a2adc5386dd61ff11456640d01cc
Peter Tribble <peter.tribble@gmail.com>
2017-08-02 09:31:30 +0100

    Update ISC dhcp 4.3.6

--- TRIBdhcp-server/build.sh
+++ TRIBdhcp-server/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild dhcp-4.3.5 -C "--sysconfdir=/etc --localstatedir=/var --enable-use-sockets --enable-ipv4-pktinfo " -J 1
-${THOME}/build/genpkg TRIBdhcp-server dhcp-4.3.5
+${THOME}/build/dobuild dhcp-4.3.6 -C "--sysconfdir=/etc --localstatedir=/var --enable-use-sockets --enable-ipv4-pktinfo " -J 1
+${THOME}/build/genpkg TRIBdhcp-server dhcp-4.3.6
