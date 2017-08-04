4559a0f811cb1cbde75d97d2c03f000fd7fd01ce
Peter Tribble <peter.tribble@gmail.com>
2017-08-04 09:56:07 +0100

    Update pyasn1 0.3.1

--- TRIBpyasn1-python-27/build.sh
+++ TRIBpyasn1-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyasn1-0.2.3
-cd pyasn1-0.2.3
+${THOME}/build/unpack pyasn1-0.3.1
+cd pyasn1-0.3.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
