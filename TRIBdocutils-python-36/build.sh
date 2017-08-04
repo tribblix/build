2c7accfd4b73fff78dd5dfb71f0101e281b3abf4
Peter Tribble <peter.tribble@gmail.com>
2017-08-04 10:00:16 +0100

    Update docutils 0.14

--- TRIBdocutils-python-36/build.sh
+++ TRIBdocutils-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack docutils-0.13.1
-cd docutils-0.13.1
+${THOME}/build/unpack docutils-0.14
+cd docutils-0.14
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pdu
