8a83a284ae02c7aa5d9daf05813e22e61af5b6b6
Peter Tribble <peter.tribble@gmail.com>
2017-08-05 11:31:53 +0100

    Update fltk 1.3.4-1

--- TRIBfltk/build.sh
+++ TRIBfltk/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 fltk-1.3.3 -C --enable-shared
-${THOME}/build/genpkg TRIBfltk fltk-1.3.3
+# rename the source tarball
+# mv fltk-1.3.4-1-source.tar.gz fltk-1.3.4-1.tar.gz
+#
+${THOME}/build/dobuild -64 fltk-1.3.4-1 -C --enable-shared
+${THOME}/build/genpkg TRIBfltk fltk-1.3.4-1
