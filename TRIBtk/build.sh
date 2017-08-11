7566cad3019b79d7e15c82b0b2a6198c0584bd39
Peter Tribble <peter.tribble@gmail.com>
2017-08-11 11:54:44 +0100

    Update tcl + tk 8.6.7

--- TRIBtk/build.sh
+++ TRIBtk/build.sh
@@ -3,8 +3,8 @@
 # build tcl and tk together
 #
 
-${THOME}/build/unpack tk8.6.6-src
-cd tk8.6.6/unix
+${THOME}/build/unpack tk8.6.7-src
+cd tk8.6.7/unix
 env CFLAGS=-O ./configure --prefix=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
@@ -13,8 +13,8 @@ cd ../..
 
 mkdir -p 64bit
 cd 64bit
-${THOME}/build/unpack tk8.6.6-src
-cd tk8.6.6/unix
+${THOME}/build/unpack tk8.6.7-src
+cd tk8.6.7/unix
 env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/amd64 --libdir=/usr/lib/amd64
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
