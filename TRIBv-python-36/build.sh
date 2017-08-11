0b974b0590a6d05d0ff9dddbb4fa5e33b56a203d
Peter Tribble <peter.tribble@gmail.com>
2017-08-11 21:05:00 +0100

    Update python 3.6.2

--- TRIBv-python-36/build.sh
+++ TRIBv-python-36/build.sh
@@ -3,6 +3,6 @@
 # NB. TRIBv-python-36 must not be installed, or the make
 # install step will get confused
 #
-${THOME}/build/dobuild Python-3.6.1 -P /usr/versions/python-3.6 -L -C --enable-shared
+${THOME}/build/dobuild Python-3.6.2 -P /usr/versions/python-3.6 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-36 Python-3.6.1
+${THOME}/build/genpkg TRIBv-python-36 Python-3.6.2
