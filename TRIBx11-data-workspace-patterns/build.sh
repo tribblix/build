c2419f6f764f7ac470a52da0b4809503508c7bee
Peter Tribble <peter.tribble@gmail.com>
2017-08-12 15:11:07 +0100

    Package xbitmaps and workspace-patterns ourselves

---
+++ TRIBx11-data-workspace-patterns/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# this is just a set of files
+# you can either grab them from
+#  https://github.com/oracle/solaris-xorg/tree/master/open-src/data/workspace-patterns
+# or if you have the old OI package installed, they'll be in /usr/share/patterns
+# and you can just copy them out into the build area
+#
+rm -fr /tmp/tpat
+mkdir /tmp/tpat
+(cd / ; tar cf - usr/share/patterns) | ( cd /tmp/tpat ; tar xf -)
+${THOME}/build/create_pkg TRIBx11-data-workspace-patterns /tmp/tpat
+rm -fr /tmp/tpat
---
+++ TRIBx11-data-xbitmaps/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild xbitmaps-1.1.1
+${THOME}/build/genpkg TRIBx11-data-xbitmaps xbitmaps-1.1.1
