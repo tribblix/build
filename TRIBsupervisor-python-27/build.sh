f06d8ab0effdc3170e5cb19c6f4fc0286a90bcd0
Peter Tribble <peter.tribble@gmail.com>
2017-08-15 21:16:00 +0100

    Update supervisor 3.3.3

--- TRIBsupervisor-python-27/build.sh
+++ TRIBsupervisor-python-27/build.sh
@@ -2,9 +2,10 @@
 #
 # supervisord (needs meld3)
 #
-${THOME}/build/unpack supervisor-3.1.3
-cd supervisor-3.1.3
+${THOME}/build/unpack supervisor-3.3.3
+cd supervisor-3.3.3
 python setup.py build
 python setup.py install --root=/tmp/pis
 ${THOME}/build/create_pkg TRIBsupervisor-python-27 /tmp/pis
+cd ..
 rm -fr /tmp/pis
