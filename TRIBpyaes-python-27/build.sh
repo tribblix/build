54ff6f57d301f5a3db952db81ff5b22cf5585119
Peter Tribble <peter.tribble@gmail.com>
2017-08-28 13:34:18 +0100

    Add pyaes 1.6.0

---
+++ TRIBpyaes-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pyaes-1.6.0
+cd pyaes-1.6.0
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpyaes-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
