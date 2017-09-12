08ccf99fab607ad34969cfc1f702ad8126db0952
Peter Tribble <peter.tribble@gmail.com>
2017-09-12 09:27:21 +0100

    Update libgd 2.2.5

--- TRIBlibgd/build.sh
+++ TRIBlibgd/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libgd-2.2.4 -C --enable-werror=no
-${THOME}/build/genpkg TRIBlibgd libgd-2.2.4
+${THOME}/build/dobuild -64 libgd-2.2.5 -C --enable-werror=no
+${THOME}/build/genpkg TRIBlibgd libgd-2.2.5
