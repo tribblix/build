35ad0bb5c8bf632e3c2f8dea46fbf40ada487af9
Peter Tribble <peter.tribble@gmail.com>
2017-09-12 21:46:46 +0100

    Update emacs 25.3

--- TRIBv-emacs-25/build.sh
+++ TRIBv-emacs-25/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS=-lcurses ${THOME}/build/dobuild emacs-25.2 -P /usr/versions/emacs-25 -C --with-gif=no
-${THOME}/build/genpkg TRIBv-emacs-25 emacs-25.2
+env LIBS=-lcurses ${THOME}/build/dobuild emacs-25.3 -P /usr/versions/emacs-25 -C --with-gif=no
+${THOME}/build/genpkg TRIBv-emacs-25 emacs-25.3
