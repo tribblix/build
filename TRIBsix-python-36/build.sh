a639aed60ad811271aca11eba79d708b4c14458f
Peter Tribble <peter.tribble@gmail.com>
2017-09-19 21:21:27 +0100

    Update six 1.11.0

--- TRIBsix-python-36/build.sh
+++ TRIBsix-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack six-1.10.0
-cd six-1.10.0
+${THOME}/build/unpack six-1.11.0
+cd six-1.11.0
 rm -fr /tmp/pil
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBsix-python-36 /tmp/pil
