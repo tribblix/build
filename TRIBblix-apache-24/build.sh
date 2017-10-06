ddca5a251e9e26f6f744a4af9cd418111b646cd9
Peter Tribble <peter.tribble@gmail.com>
2017-10-06 10:42:17 +0100

    Update apache 2.4.28

--- TRIBblix-apache-24/build.sh
+++ TRIBblix-apache-24/build.sh
@@ -2,5 +2,5 @@
 #
 # there's a preconf file to unpack apr
 #
-${THOME}/build/dobuild httpd-2.4.27 -P /opt/tribblix/apache2 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
-${THOME}/build/genpkg TRIBblix-apache-24 httpd-2.4.27
+${THOME}/build/dobuild httpd-2.4.28 -P /opt/tribblix/apache2 -C "--enable-mods-shared=most --with-expat=builtin --enable-ssl --enable-proxy --enable-proxy-ajp"
+${THOME}/build/genpkg TRIBblix-apache-24 httpd-2.4.28
