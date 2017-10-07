b9e76310da7124d6255edd3a95c35a58671303de
Peter Tribble <peter.tribble@gmail.com>
2017-10-07 15:01:37 +0100

    Update zabbix 3.0.11

--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.10 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-3.0.10
+${THOME}/build/dobuild zabbix-3.0.11 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-3.0.11
