1566d1bff1f9834a42c51b09189c4c5651cf6b9e
Peter Tribble <peter.tribble@gmail.com>
2017-10-17 14:55:30 +0100

    Update nginx 1.12.2

--- TRIBblix-nginx/build.sh
+++ TRIBblix-nginx/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild nginx-1.12.1 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module"
-${THOME}/build/genpkg TRIBblix-nginx nginx-1.12.1
+${THOME}/build/dobuild nginx-1.12.2 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module"
+${THOME}/build/genpkg TRIBblix-nginx nginx-1.12.2
