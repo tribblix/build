d956885b8c2c119b31859dee7f376b5c3ae0c810
Peter Tribble <peter.tribble@gmail.com>
2017-10-25 11:06:18 +0100

    Update node 4.8.5 6.11.5 8.8.0

--- TRIBv-node-v6/build.sh
+++ TRIBv-node-v6/build.sh
@@ -5,7 +5,7 @@
 # note that we can't use the normal -64 build mechanism as node
 # has its own way of doing it
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v6.11.4 -P /usr/versions/node-v6 -C --without-dtrace
-mv node-v6.11.4 node-v6.11.4-32
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v6.11.4 -P /usr/versions/node-v6 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v6 node-v6.11.4
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v6.11.5 -P /usr/versions/node-v6 -C --without-dtrace
+mv node-v6.11.5 node-v6.11.5-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v6.11.5 -P /usr/versions/node-v6 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v6 node-v6.11.5
