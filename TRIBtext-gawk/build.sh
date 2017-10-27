a3fd8e85d9f7aa52cb00c7156aced64f5a2e9367
Peter Tribble <peter.tribble@gmail.com>
2017-10-27 11:26:04 +0100

    Update gawk 4.2.0

--- TRIBtext-gawk/build.sh
+++ TRIBtext-gawk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu gawk-4.1.4 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gawk gawk-4.1.4
+${THOME}/build/dobuild -64 -gnu gawk-4.2.0 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gawk gawk-4.2.0
