6c70fc1d22840aeb0f868f3e14a3bf9695dca24a
Peter Tribble <peter.tribble@gmail.com>
2017-10-27 11:04:14 +0100

    Update help2man 1.47.5

--- TRIBhelp2man/build.sh
+++ TRIBhelp2man/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild help2man-1.47.4
-${THOME}/build/genpkg TRIBhelp2man help2man-1.47.4
+${THOME}/build/dobuild help2man-1.47.5
+${THOME}/build/genpkg TRIBhelp2man help2man-1.47.5
