5355c3e93dcb2e560711a9f32114e5d0ce2e0c2a
Peter Tribble <peter.tribble@gmail.com>
2017-10-30 09:01:36 +0000

    Update libarchive 3.3.2

--- TRIBlibarchive/build.sh
+++ TRIBlibarchive/build.sh
@@ -2,5 +2,5 @@
 #
 # used by claws mail and samba
 #
-${THOME}/build/dobuild -64 libarchive-3.3.1
-${THOME}/build/genpkg TRIBlibarchive libarchive-3.3.1
+${THOME}/build/dobuild -64 libarchive-3.3.2
+${THOME}/build/genpkg TRIBlibarchive libarchive-3.3.2
