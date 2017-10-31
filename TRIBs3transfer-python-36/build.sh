eb94bc285a1b747a34d614cdc45b30a7ea5ad315
Peter Tribble <peter.tribble@gmail.com>
2017-10-31 09:54:41 +0000

    Update s3transfer 0.1.11

--- TRIBs3transfer-python-36/build.sh
+++ TRIBs3transfer-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack s3transfer-0.1.10
-cd s3transfer-0.1.10
+${THOME}/build/unpack s3transfer-0.1.11
+cd s3transfer-0.1.11
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pdu
