2daa082b16949b6a93a03358c9653ed532fa29bd
Peter Tribble <peter.tribble@gmail.com>
2017-11-02 14:09:11 +0000

    Update ConfigArgParse 0.12.0

--- TRIBconfigargparse-python-36/build.sh
+++ TRIBconfigargparse-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ConfigArgParse-0.11.0
-cd ConfigArgParse-0.11.0
+${THOME}/build/unpack ConfigArgParse-0.12.0
+cd ConfigArgParse-0.12.0
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
