58b37f09af292e4ceea8b5fca900baea1780a8a2
Peter Tribble <peter.tribble@gmail.com>
2017-11-09 15:13:19 +0000

    Update postgres 9.4.15 9.5.10 9.6.6 10.1

--- TRIBblix-postgres10/build.sh
+++ TRIBblix-postgres10/build.sh
@@ -2,8 +2,8 @@
 #
 # postgres 10
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.0 -C --without-readline -M world
-cd postgresql-10.0
+${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.1 -C --without-readline -M world
+cd postgresql-10.1
 rm -fr /tmp/pp
 gmake install-world DESTDIR=/tmp/pp
 ${THOME}/build/create_pkg TRIBblix-postgres10 /tmp/pp
