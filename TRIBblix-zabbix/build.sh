b27dd646f03b1acdfc01b5400d1a1be9c28a582e
Peter Tribble <peter.tribble@gmail.com>
2017-11-10 11:30:46 +0000

    update zabbix 3.0.13

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.12 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres96/bin/pg_config"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-3.0.12
+${THOME}/build/dobuild zabbix-3.0.13 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres96/bin/pg_config"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-3.0.13
--- TRIBzabbix-proxy/build.sh
+++ TRIBzabbix-proxy/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.12 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl"
-${THOME}/build/genpkg TRIBzabbix-proxy zabbix-3.0.12
+${THOME}/build/dobuild zabbix-3.0.13 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl"
+${THOME}/build/genpkg TRIBzabbix-proxy zabbix-3.0.13
