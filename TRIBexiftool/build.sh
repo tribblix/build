279e98f4d6eb53264a40010ae0eea4963976c16c
Peter Tribble <peter.tribble@gmail.com>
2017-11-13 15:14:20 +0000

    Update exiftool 10.65

--- TRIBexiftool/build.sh
+++ TRIBexiftool/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Image-ExifTool-10.57
-cd Image-ExifTool-10.57
+${THOME}/build/unpack Image-ExifTool-10.65
+cd Image-ExifTool-10.65
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBexiftool
