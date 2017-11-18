9fa2a2ab66e1c73605986a0dba836304ed19e399
Peter Tribble <peter.tribble@gmail.com>
2017-11-18 12:41:28 +0000

    Update allmark 0.10.0-dev

--- TRIBblix-allmark/build.sh
+++ TRIBblix-allmark/build.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/tcsh
 #
 # requires go
 #
+setenv GOPATH `pwd`
+mkdir -p src/github.com/andreaskoch
+cd src/github.com/andreaskoch
 git clone https://github.com/andreaskoch/allmark.git
 cd allmark
-env PATH=/usr/versions/go-1.6/bin:$PATH go run make.go -install
+env PATH=/usr/versions/go-1.8/bin:$PATH gmake
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/allmark/bin
-cp bin/allmark /tmp/ee/opt/tribblix/allmark/bin
+cp bin/files/allmark /tmp/ee/opt/tribblix/allmark/bin
 cp README.md /tmp/ee/opt/tribblix/allmark
 ${THOME}/build/create_pkg TRIBblix-allmark /tmp/ee
 rm -fr /tmp/ee
