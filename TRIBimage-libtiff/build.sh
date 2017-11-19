2591a0f765716b0789e8f719e082b54e6bafd2fd
Peter Tribble <peter.tribble@gmail.com>
2017-11-19 21:16:54 +0000

    Update tiff 4.0.9

--- TRIBimage-libtiff/build.sh
+++ TRIBimage-libtiff/build.sh
@@ -4,5 +4,5 @@
 #
 # see TRIBlibtiff-compat for the 3.x legacy files
 #
-${THOME}/build/dobuild -64 tiff-4.0.8 -C "--disable-cxx --disable-static"
-${THOME}/build/genpkg TRIBimage-libtiff tiff-4.0.8
+${THOME}/build/dobuild -64 tiff-4.0.9 -C "--disable-cxx --disable-static"
+${THOME}/build/genpkg TRIBimage-libtiff tiff-4.0.9
