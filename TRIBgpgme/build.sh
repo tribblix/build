8acb0b719750a71d3310488154fda5712715a64d
Peter Tribble <peter.tribble@gmail.com>
2017-12-01 12:16:47 +0000

    Update gpgme 1.9.0

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -3,5 +3,5 @@
 # must not have QT5 installed, else it will try and build against
 # it and fail, but there's no easy way to turn the QT support off
 #
-${THOME}/build/dobuild -64 gpgme-1.8.0
-${THOME}/build/genpkg TRIBgpgme gpgme-1.8.0
+${THOME}/build/dobuild -64 gpgme-1.9.0
+${THOME}/build/genpkg TRIBgpgme gpgme-1.9.0
