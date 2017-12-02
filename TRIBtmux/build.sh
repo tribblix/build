4c4addde5fa34ad15b45ec6e669dae3fefb24165
Peter Tribble <peter.tribble@gmail.com>
2017-12-02 18:09:49 +0000

    Update tmux 2.6

--- TRIBtmux/build.sh
+++ TRIBtmux/build.sh
@@ -4,5 +4,6 @@
 # built a la omnios
 # [no, I don't understand how --disable-static works...]
 ${THOME}/build/dobuild libevent-2.0.22-stable -C "--disable-static --disable-libevent-install"
-env CPPFLAGS="-I`pwd`/libevent-2.0.22-stable/include -I`pwd`/libevent-2.0.22-stable/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.0.22-stable/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild tmux-2.3
-${THOME}/build/genpkg TRIBtmux tmux-2.3
+env CPPFLAGS="-I`pwd`/libevent-2.0.22-stable/include -I`pwd`/libevent-2.0.22-stable/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.0.22-stable/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild tmux-2.6
+${THOME}/build/genpkg TRIBtmux tmux-2.6
+rm -fr libevent-2.0.22-stable
