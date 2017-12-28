700b302593a092975506257760a7b53ff0e73f98
Peter Tribble <peter.tribble@gmail.com>
2017-12-28 14:05:27 +0000

    Update IMageMagick 7.0.7-17

--- TRIBimagemagick/build.sh
+++ TRIBimagemagick/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-# 7.x requires a rebuild of (at least) wmaker, emacs, inkscape 
+# 7.x required a rebuild of (at least) wmaker, emacs, inkscape
+# and doesn't maintain binary compatibility, bumping the SONAME
+# regularly, so disable it as a dependency when we can
 #
 zap uninstall TRIBgraphviz
 zap install openjpeg libwebp
-${THOME}/build/dobuild -64 ImageMagick-7.0.6-7
-${THOME}/build/genpkg TRIBimagemagick ImageMagick-7.0.6-7
+${THOME}/build/dobuild -64 ImageMagick-7.0.7-17
+${THOME}/build/genpkg TRIBimagemagick ImageMagick-7.0.7-17
