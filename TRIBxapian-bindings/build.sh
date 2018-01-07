c166c0736206201ab1fac513aa42e57dd53aa85a
Peter Tribble <peter.tribble@gmail.com>
2018-01-07 18:46:06 +0000

    Update xapian 1.2.25

--- TRIBxapian-bindings/build.sh
+++ TRIBxapian-bindings/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xapian-bindings-1.2.24
-${THOME}/build/genpkg TRIBxapian-bindings xapian-bindings-1.2.24
+${THOME}/build/dobuild xapian-bindings-1.2.25
+${THOME}/build/genpkg TRIBxapian-bindings xapian-bindings-1.2.25
--- TRIBxapian-core/build.sh
+++ TRIBxapian-core/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 xapian-core-1.2.24
-${THOME}/build/genpkg TRIBxapian-core xapian-core-1.2.24
+${THOME}/build/dobuild -64 xapian-core-1.2.25
+${THOME}/build/genpkg TRIBxapian-core xapian-core-1.2.25
