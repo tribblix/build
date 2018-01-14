a2e0f3cfd25c2976ee25d30c40b39b476ba7c156
Peter Tribble <peter.tribble@gmail.com>
2018-01-14 14:24:49 +0000

    Update gtar 1.30

--- TRIBarchiver-gnu-tar/build.sh
+++ TRIBarchiver-gnu-tar/build.sh
@@ -2,5 +2,5 @@
 #
 # gtar
 #
-${THOME}/build/dobuild -gnu tar-1.29 -P /usr/gnu
-${THOME}/build/genpkg TRIBarchiver-gnu-tar tar-1.29
+${THOME}/build/dobuild -gnu tar-1.30 -P /usr/gnu
+${THOME}/build/genpkg TRIBarchiver-gnu-tar tar-1.30
