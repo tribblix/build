33eb6be11ee920ec9798a68d2ce5d2a58c16066c
Peter Tribble <peter.tribble@gmail.com>
2018-01-31 10:54:16 +0000

    Add node v9

---
+++ TRIBv-node-v9/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# with isaexec switching
+#
+# note that we can't use the normal -64 build mechanism as node
+# has its own way of doing it
+#
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v9.4.0 -P /usr/versions/node-v9 -C --without-dtrace
+mv node-v9.4.0 node-v9.4.0-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v9.4.0 -P /usr/versions/node-v9 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v9 node-v9.4.0
