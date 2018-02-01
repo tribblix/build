01bf7fda01f38d21e911bc49adbb078b834c2f23
Peter Tribble <peter.tribble@gmail.com>
2018-02-01 21:52:16 +0000

    Add a simple samba service

---
+++ TRIBsvc-samba4/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# the samba4 package ships the software, which is useful for clients
+# all the work here in populating the package is done in the fixit script
+#
+${THOME}/build/genpkg TRIBsvc-samba4
