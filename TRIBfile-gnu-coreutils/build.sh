38e08bc3ddc2aff43b2700b80887b75189d7c7fe
Peter Tribble <peter.tribble@gmail.com>
2018-02-01 11:27:16 +0000

    Build 64-bit coreutils

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu coreutils-8.29 -P /usr/gnu -C --without-gmp
+${THOME}/build/dobuild -64 -gnu coreutils-8.29 -P /usr/gnu -C --without-gmp
 ${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.29
