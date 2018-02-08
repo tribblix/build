25af2827818c84aa3345ccdf79b7533a5ad1cbc3
Peter Tribble <peter.tribble@gmail.com>
2018-02-08 18:25:54 +0000

    Enable openssl in cmake so https downloads work

--- TRIBv-cmake-36/build.sh
+++ TRIBv-cmake-36/build.sh
@@ -4,6 +4,7 @@
 #
 ${THOME}/build/unpack cmake-3.6.3
 cd cmake-3.6.3
+sed -i 's:DCMAKE_BOOTSTRAP=1:DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=ON:' bootstrap
 ./bootstrap --prefix=/usr/versions/cmake-3.6
 gmake -j 4
 # [gmake test - all passed]
