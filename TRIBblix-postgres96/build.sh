699103a570c67eda90e8530e71e54bed0357aa46
Peter Tribble <peter.tribble@gmail.com>
2018-02-08 14:42:23 +0000

    Update postgres 9.4.16 9.5.11 9.6.7 10.2

--- TRIBblix-postgres96/build.sh
+++ TRIBblix-postgres96/build.sh
@@ -3,7 +3,7 @@
 # postgres 9.6
 #
 ${THOME}/build/newbuild.sh build TRIBblix-postgres96
-cd postgresql-9.6.6
+cd postgresql-9.6.7
 rm -fr /tmp/pp
 gmake install-world DESTDIR=/tmp/pp
 ${THOME}/build/create_pkg TRIBblix-postgres96 /tmp/pp
