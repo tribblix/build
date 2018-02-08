630653a8a1637afb73181dc5c904e415cc3fa2dd
Peter Tribble <peter.tribble@gmail.com>
2018-02-08 18:39:36 +0000

    Get gnu uname -o to say illumos

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu coreutils-8.29 -P /usr/gnu -C --without-gmp
+${THOME}/build/dobuild -64 -gnu coreutils-8.29 -P /usr/gnu -C "--without-gmp gl_cv_host_operating_system=illumos"
 ${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.29
