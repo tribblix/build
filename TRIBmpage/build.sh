88d5c94189c10113b02118c2bc185bf4ef7141a7
Peter Tribble <peter.tribble@gmail.com>
2018-02-12 13:59:53 +0000

    Add mpage 2.5.7

---
+++ TRIBmpage/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack mpage-2.5.7
+cd mpage-2.5.7
+gmake PREFIX=/usr
+gmake install PREFIX=/tmp/mm/usr
+rm -fr /tmp/mm
+${THOME}/build/create_pkg TRIBmpage /tmp/mm
+rm -fr /tmp/mm
+cd ..
