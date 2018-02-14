2faa4b816a9ac30063563d1870c918bdbe98b23d
Peter Tribble <peter.tribble@gmail.com>
2018-02-14 10:20:38 +0000

    Correct mpage build script

--- TRIBmpage/build.sh
+++ TRIBmpage/build.sh
@@ -3,8 +3,8 @@
 ${THOME}/build/unpack mpage-2.5.7
 cd mpage-2.5.7
 gmake PREFIX=/usr
-gmake install PREFIX=/tmp/mm/usr
 rm -fr /tmp/mm
+gmake install PREFIX=/tmp/mm/usr
 ${THOME}/build/create_pkg TRIBmpage /tmp/mm
 rm -fr /tmp/mm
 cd ..
