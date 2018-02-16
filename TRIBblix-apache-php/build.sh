769872c493c57f4cf56772b673fc9c29ef6f2db7
Peter Tribble <peter.tribble@gmail.com>
2018-02-16 14:00:25 +0000

    Add a simple apache+php service

---
+++ TRIBblix-apache-php/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# this is a package that sets up apache+php for a flat-file php
+# application hosted under /var/phpweb. Think grav, for rexample.
+#
+# everything is done by the fixit script
+#
+${THOME}/build/genpkg TRIBblix-apache-php
