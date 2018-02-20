70b918ebbcf530391f41b4da7786f594d7031bc3
Peter Tribble <peter.tribble@gmail.com>
2018-02-20 22:31:25 +0000

    Add skew

---
+++ TRIBskew-python-36/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# can't use a release tarball for pypi as it's hopelessly
+# out of date
+#
+rm -fr skew
+git clone https://github.com/scopely-devops/skew.git
+cd skew
+/usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBskew-python-36 /tmp/pil
+cd ..
+rm -fr /tmp/pil
