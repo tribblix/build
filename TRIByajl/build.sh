57bd6d245ec7c252368b44e97db5ed0336c937de
Peter Tribble <peter.tribble@gmail.com>
2018-02-23 11:05:00 +0000

    Fix the name of the yajl downloaded tarball

--- TRIByajl/build.sh
+++ TRIByajl/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack lloyd-yajl-2.1.0-0-ga0ecdde
-cd lloyd-yajl-66cb08c
+${THOME}/build/unpack yajl-2.1.0
+cd yajl-2.1.0
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/y
