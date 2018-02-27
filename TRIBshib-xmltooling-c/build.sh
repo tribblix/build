8a98b2414006f981fbf312af534ac201488d21d9
Peter Tribble <peter.tribble@gmail.com>
2018-02-27 20:03:08 +0000

    Update xmltooling-c 1.6.4

--- TRIBshib-xmltooling-c/build.sh
+++ TRIBshib-xmltooling-c/build.sh
@@ -2,5 +2,5 @@
 #
 # shibboleth stack requires boost ssl curl apache
 #
-${THOME}/build/dobuild xmltooling-1.6.3 -P /opt/tribblix/shibboleth-sp -C "--disable-static --with-log4shib=/opt/tribblix/shibboleth-sp"
-${THOME}/build/genpkg TRIBshib-xmltooling-c xmltooling-1.6.3
+${THOME}/build/dobuild xmltooling-1.6.4 -P /opt/tribblix/shibboleth-sp -C "--disable-static --with-log4shib=/opt/tribblix/shibboleth-sp"
+${THOME}/build/genpkg TRIBshib-xmltooling-c xmltooling-1.6.4
