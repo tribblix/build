0cd3f1fbbd6d139798bc6eeb11b3323f0ff2f23d
Peter Tribble <peter.tribble@gmail.com>
2018-03-03 14:55:01 +0000

    Update xfce4-panel 4.12.2 Thunar 1.6.14 settings 4.12.2

--- TRIBxfce/build.sh
+++ TRIBxfce/build.sh
@@ -6,10 +6,10 @@ $THOME/build/dobuild -gnu libxfce4ui-4.12.1 -C "--disable-debug --with-vendor-in
 $THOME/build/dobuild -gnu exo-0.10.7 -C "--disable-debug"
 $THOME/build/dobuild -gnu garcon-0.4.0 -C "--disable-debug"
 $THOME/build/dobuild -gnu xfce4-appfinder-4.12.0 -C "--disable-debug"
-$THOME/build/dobuild -gnu xfce4-panel-4.12.1 -C "--disable-debug"
+$THOME/build/dobuild -gnu xfce4-panel-4.12.2 -C "--disable-debug"
 $THOME/build/dobuild -gnu tumbler-0.1.31 -C "--disable-debug"
-$THOME/build/dobuild -gnu Thunar-1.6.11 -C "--disable-debug"
-$THOME/build/dobuild -gnu xfce4-settings-4.12.1 -C "--disable-debug"
+$THOME/build/dobuild -gnu Thunar-1.6.14 -C "--disable-debug"
+$THOME/build/dobuild -gnu xfce4-settings-4.12.2 -C "--disable-debug"
 $THOME/build/dobuild -gnu gtk-xfce-engine-2.10.1 -C "--disable-debug"
 $THOME/build/dobuild -gnu xfce4-session-4.12.1 -C "--disable-debug"
 $THOME/build/dobuild -gnu xfdesktop-4.12.3 -C "--disable-debug"
@@ -23,10 +23,10 @@ rm -fr /tmp/xfu
 (cd exo-0.10.7 ; gmake DESTDIR=/tmp/xfu install)
 (cd garcon-0.4.0 ; gmake DESTDIR=/tmp/xfu install)
 (cd xfce4-appfinder-4.12.0; gmake DESTDIR=/tmp/xfu install)
-(cd xfce4-panel-4.12.1 ; gmake DESTDIR=/tmp/xfu install)
+(cd xfce4-panel-4.12.2 ; gmake DESTDIR=/tmp/xfu install)
 (cd tumbler-0.1.31 ; gmake DESTDIR=/tmp/xfu install)
-(cd Thunar-1.6.11 ; gmake DESTDIR=/tmp/xfu install)
-(cd xfce4-settings-4.12.1 ; gmake DESTDIR=/tmp/xfu install)
+(cd Thunar-1.6.14 ; gmake DESTDIR=/tmp/xfu install)
+(cd xfce4-settings-4.12.2 ; gmake DESTDIR=/tmp/xfu install)
 (cd gtk-xfce-engine-2.10.1 ; gmake DESTDIR=/tmp/xfu install)
 (cd xfce4-session-4.12.1 ; gmake DESTDIR=/tmp/xfu install)
 (cd xfdesktop-4.12.3 ; gmake DESTDIR=/tmp/xfu install)
