33de866603879d8fae464a3f81aba0b37a397d6b
Peter Tribble <peter.tribble@gmail.com>
2018-03-03 13:47:47 +0000

    Add paramiko 2.4.0

---
+++ TRIBparamiko-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack paramiko-2.4.0
+cd paramiko-2.4.0
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBparamiko-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
---
+++ TRIBparamiko-python-36/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack paramiko-2.4.0
+cd paramiko-2.4.0
+/usr/versions/python-3.6/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBparamiko-python-36 /tmp/pil
+cd ..
+rm -fr /tmp/pil
