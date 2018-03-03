0e6b20093a38a0ba72fa5e845cf3c54029fb6544
Peter Tribble <peter.tribble@gmail.com>
2018-03-03 16:42:35 +0000

    Update live555 2018.02.28

--- TRIBlive555/build.sh
+++ TRIBlive555/build.sh
@@ -2,7 +2,7 @@
 #
 #
 #
-${THOME}/build/unpack live.2016.05.20
+${THOME}/build/unpack live.2018.02.28
 cd live
 gsed -i 's:cc:gcc:' config.solaris-32bit 
 cat config.linux-with-shared-libraries | sed 's:-O2:-O2 -DSOLARIS -DXLOCALE_NOT_USED:' | sed 's:-DBSD=1 ::' > config.solaris-shared
@@ -10,5 +10,8 @@ gsed -i -e 's:cc:gcc:' -e 's:c++:g++:' -e 's:ggcc:g++:' config.solaris-shared
 gsed -i -e 's:-shared:-lsocket -lnsl -shared:' config.solaris-shared
 ./genMakefiles solaris-shared
 gmake -j 4 CC=gcc
+rm -fr /tmp/lls
 env PATH=/usr/gnu/bin:$PATH gmake install DESTDIR=/tmp/lls PREFIX=/usr
 ${THOME}/build/create_pkg TRIBlive555 /tmp/lls
+rm -fr /tmp/lls
+cd ..
