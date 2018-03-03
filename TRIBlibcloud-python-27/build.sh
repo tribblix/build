5313eeca9cf3c1672faa75e4eb8e0051d7c0d598
Peter Tribble <peter.tribble@gmail.com>
2018-03-03 14:25:05 +0000

    Add libcloud 2.3.0

---
+++ TRIBlibcloud-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack apache-libcloud-2.3.0
+cd apache-libcloud-2.3.0
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBlibcloud-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
