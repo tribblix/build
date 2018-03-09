c5d94e6a0699b5d520bda9cfdb2c0740f8a44ecf
Peter Tribble <peter.tribble@gmail.com>
2018-03-09 20:16:28 +0000

    Update fontconfig 2.12.6

--- TRIBsys-lib-fontconfig/build.sh
+++ TRIBsys-lib-fontconfig/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 fontconfig-2.11.1 -C "--sysconfdir=/etc --localstatedir=/var"
-${THOME}/build/genpkg TRIBsys-lib-fontconfig fontconfig-2.11.1
+${THOME}/build/dobuild -64 fontconfig-2.12.6 -C "--sysconfdir=/etc --localstatedir=/var"
+${THOME}/build/genpkg TRIBsys-lib-fontconfig fontconfig-2.12.6
 #
-# on sparce, this replaces SUNWfontconfig
+# on sparc, this replaces SUNWfontconfig
 #
