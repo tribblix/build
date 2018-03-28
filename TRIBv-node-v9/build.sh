b1da9d447330c7b5bb6c178e084fcd38e100dd83
Peter Tribble <peter.tribble@gmail.com>
2018-03-28 22:45:43 +0100

    Update node v4.9.0 v6.14.0 v8.11.0 v9.10.0

--- TRIBv-node-v9/build.sh
+++ TRIBv-node-v9/build.sh
@@ -5,7 +5,7 @@
 # note that we can't use the normal -64 build mechanism as node
 # has its own way of doing it
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v9.8.0 -P /usr/versions/node-v9 -C --without-dtrace
-mv node-v9.8.0 node-v9.8.0-32
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v9.8.0 -P /usr/versions/node-v9 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v9 node-v9.8.0
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v9.10.0 -P /usr/versions/node-v9 -C --without-dtrace
+mv node-v9.10.0 node-v9.10.0-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v9.10.0 -P /usr/versions/node-v9 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v9 node-v9.10.0
