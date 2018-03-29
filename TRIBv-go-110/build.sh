2b3b7b53f71aafd2c97fa84f57df3a6b5d5e8435
Peter Tribble <peter.tribble@gmail.com>
2018-03-29 17:19:17 +0100

    Add go 1.10

---
+++ TRIBv-go-110/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# either uninstall gdb, or run make.bash rather than all.bash, as the gdb
+# test fails on Tribblix
+#
+# There have been issues with GOROOT_FINAL causing test failures
+# https://github.com/golang/go/issues/20284
+#
+rm -fr go
+${THOME}/build/unpack go1.10.1.src
+cd go/src
+env GOROOT_FINAL=/usr/versions/go-1.10 GOROOT_BOOTSTRAP=/usr/versions/go-1.9 ./all.bash
+cd ..
+rm -fr /tmp/gg
+mkdir -p /tmp/gg/usr/versions/go-1.10
+tar cf - . | (cd /tmp/gg/usr/versions/go-1.10 ; tar xf -)
+# [need to package godoc and gox]
+# need to not be at a valid GOROOT
+mkdir t
+cd t
+env GOROOT=/tmp/gg/usr/versions/go-1.10 GOPATH=`pwd` ../bin/go get golang.org/x/tools/cmd/godoc
+cp bin/godoc /tmp/gg/usr/versions/go-1.10/bin
+env GOROOT=/tmp/gg/usr/versions/go-1.10 GOPATH=`pwd` ../bin/go get github.com/mitchellh/gox
+cp bin/gox /tmp/gg/usr/versions/go-1.10/bin
+${THOME}/build/create_pkg TRIBv-go-110 /tmp/gg
+rm -fr /tmp/gg
