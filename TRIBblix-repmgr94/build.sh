53bd23f7e43cad1252d34317f27d2c6f1b87db52
Peter Tribble <peter.tribble@gmail.com>
2018-04-04 21:18:13 +0100

    Deprecate postgres 9.4

--- TRIBblix-repmgr94/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# this is a postgres extension, so it's specific to the postgres version
-#
-${THOME}/build/dobuild repmgr-4.0.4 -C PG_CONFIG=/opt/tribblix/postgres94/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr94 repmgr-4.0.4
