2647e28f51062b5a855cf1fc1d53da1e792789ca
Peter Tribble <peter.tribble@gmail.com>
2018-04-04 21:32:02 +0100

    Extend Slony-I to all postgres versions

---
+++ TRIBblix-slony10/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# slony for postgres 10
+#
+# the binaries are linked against postgres libraries, so this package
+# installs into the same prefix as the postgres version it's built
+# against, and is versioned to match
+#
+$THOME/build/dobuild slony1-2.2.6 -P /opt/tribblix/postgres10 -C "--with-pgconfigdir=/opt/tribblix/postgres10/bin --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-slony10 slony1-2.2.6
