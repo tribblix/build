dc72745fbc4c7e26a42c93ad09b222c28dfae714
Peter Tribble <peter.tribble@gmail.com>
2018-04-05 19:30:47 +0100

    Update GMP 6.1.2

--- TRIBlib-gmp/build.sh
+++ TRIBlib-gmp/build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #
 # Fix gmp for 64-bit includes
-# [this is actually the 6.0.0a tarball, renamed as gmp-6.0.0]
 #
-${THOME}/build/dobuild gmp-6.0.0 -C "--enable-cxx ABI=32"
-mv gmp-6.0.0 gmp-6.0.0-32bit
-${THOME}/build/dobuild +64 gmp-6.0.0 -C "--enable-cxx ABI=64"
-mv gmp-6.0.0-32bit gmp-6.0.0
-cd gmp-6.0.0 ; gmake check ; cd ..
-cd gmp-6.0.0-64bit ; gmake check ; cd ..
-${THOME}/build/genpkg TRIBlib-gmp gmp-6.0.0
+${THOME}/build/dobuild gmp-6.1.2 -C "--enable-cxx ABI=32 --disable-static"
+mv gmp-6.1.2 gmp-6.1.2-32bit
+${THOME}/build/dobuild +64 gmp-6.1.2 -C "--enable-cxx ABI=64 --disable-static"
+mv gmp-6.1.2-32bit gmp-6.1.2
+cd gmp-6.1.2 ; gmake check ; cd ..
+cd gmp-6.1.2-64bit ; gmake check ; cd ..
+${THOME}/build/genpkg TRIBlib-gmp gmp-6.1.2
