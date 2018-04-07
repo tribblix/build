26b051d00e86bd5b82745f113407c4983b65adc7
Peter Tribble <peter.tribble@gmail.com>
2018-04-07 11:57:48 +0100

    Update lynx 2.8.9dev.17

--- TRIBlynx/build.sh
+++ TRIBlynx/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild lynx2-8-8 -C --with-ssl
-${THOME}/build/genpkg TRIBlynx lynx2-8-8
+${THOME}/build/dobuild lynx2.8.9dev.17 -C "--with-ssl --sysconfdir=/etc"
+${THOME}/build/genpkg TRIBlynx lynx2.8.9dev.17
