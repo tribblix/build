1feab522a645cb4cb7c290163436f360c36413b3
Peter Tribble <peter.tribble@gmail.com>
2018-04-07 14:32:27 +0100

    Clarify libgcrypt sparc build

--- TRIBlibgcrypt/build.sh
+++ TRIBlibgcrypt/build.sh
@@ -2,6 +2,7 @@
 #
 ${THOME}/build/dobuild libgcrypt-1.8.2
 mv libgcrypt-1.8.2 libgcrypt-1.8.2-32bit
+# sparc just requires --disable-asm
 ${THOME}/build/dobuild +64 libgcrypt-1.8.2 -C "--disable-amd64-as-feature-detection --disable-asm"
 mv libgcrypt-1.8.2-32bit libgcrypt-1.8.2
 ${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.8.2
