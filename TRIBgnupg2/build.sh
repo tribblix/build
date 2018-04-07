cadeb07d893041f3894ecca330429314f8077220
Peter Tribble <peter.tribble@gmail.com>
2018-04-07 11:17:53 +0100

    Update gnupg2 2.2.5

--- TRIBgnupg2/build.sh
+++ TRIBgnupg2/build.sh
@@ -2,5 +2,5 @@
 #
 # gnupg2
 #
-${THOME}/build/dobuild gnupg-2.0.30
-${THOME}/build/genpkg TRIBgnupg2 gnupg-2.0.30
+${THOME}/build/dobuild gnupg-2.2.5 -C --disable-ldap
+${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.5
