2585569a9edb19ee16eff16ebc7fa676f57eb912
Peter Tribble <peter.tribble@gmail.com>
2018-04-12 23:03:36 +0100

    Update zsh to 5.5

--- TRIBshell-zsh/build.sh
+++ TRIBshell-zsh/build.sh
@@ -3,5 +3,5 @@
 # need -L, which enables dynamic modules for the 32-bit build
 # but they still don't build in 64-bit
 #
-${THOME}/build/dobuild -64 zsh-5.4.2 -L
-${THOME}/build/genpkg TRIBshell-zsh zsh-5.4.2
+${THOME}/build/dobuild -64 zsh-5.5 -L
+${THOME}/build/genpkg TRIBshell-zsh zsh-5.5
