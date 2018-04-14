1243812a736e478aeff2f2549f72942dae83dec7
Peter Tribble <peter.tribble@gmail.com>
2018-04-14 14:00:36 +0100

    Update c-ares 1.14.0

--- TRIBlibcares/build.sh
+++ TRIBlibcares/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-$THOME/build/dobuild -64 c-ares-1.12.0
-$THOME/build/genpkg TRIBlibcares c-ares-1.12.0
+$THOME/build/dobuild -64 c-ares-1.14.0
+$THOME/build/genpkg TRIBlibcares c-ares-1.14.0
