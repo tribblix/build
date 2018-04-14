60098fc93273b588c4749ec8857b7dc85e8a74b2
Peter Tribble <peter.tribble@gmail.com>
2018-04-14 16:11:53 +0100

    Update freecol 0.11.6

--- TRIBfreecol/build.sh
+++ TRIBfreecol/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Run the installer. It places files into ~/freecol
-# Then you convert thos into a package
+# Then you convert those into a package
 #
 rm -fr /tmp/jj
 mkdir -p /tmp/jj/usr/bin /tmp/jj/usr/share/applications
@@ -10,7 +10,7 @@ tar cf - freecol | (cd /tmp/jj/usr/share ; tar xf -)
 cat /tmp/jj/usr/share/freecol/freecol | sed 's:^BINDIR.*:BINDIR=/usr/share/freecol:' > /tmp/jj/usr/bin/freecol
 chmod 755 /tmp/jj/usr/bin/freecol
 cp /tmp/jj/usr/share/freecol/freecol.desktop /tmp/jj/usr/share/applications
-gsed -i s:data/freecol.png:/usr/share/freecol/data/freecol.png: /tmp/jj/usr/share/applications/freecol.desktop
+sed -i s:data/freecol.png:/usr/share/freecol/data/freecol.png: /tmp/jj/usr/share/applications/freecol.desktop
 rm -fr /tmp/jj/usr/share/freecol/Uninstaller
 ${THOME}/build/create_pkg TRIBfreecol /tmp/jj
 rm -fr /tmp/jj
