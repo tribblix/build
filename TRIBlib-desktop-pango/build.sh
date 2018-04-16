4a8955aa7af8f3f06a60c366b63e58a3f4a89236
Peter Tribble <peter.tribble@gmail.com>
2018-04-16 19:03:24 +0100

    Fix pango 64-bit build

--- TRIBlib-desktop-pango/build.sh
+++ TRIBlib-desktop-pango/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 pango-1.36.8
+# the +E is so that we can have different pango.modules for 32 and 64-bit
+#
+${THOME}/build/dobuild -64 pango-1.36.8 +E /usr/etc -C "--disable-introspection"
 ${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.36.8
