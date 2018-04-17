19510bef01c8691cf0f7394a0db91305c48e5e01
Peter Tribble <peter.tribble@gmail.com>
2018-04-17 21:39:30 +0100

    Update cmake 3.11.1

---
+++ TRIBcmake/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+#
+#
+${THOME}/build/unpack cmake-3.11.1
+cd cmake-3.11.1
+sed -i 's:DCMAKE_BOOTSTRAP=1:DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=ON:' bootstrap
+./bootstrap --prefix=/usr/versions/cmake-3.11
+gmake -j 4
+# [gmake test - all passed]
+${THOME}/build/genpkg TRIBcmake
+cd ..
