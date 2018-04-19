d4800971bffd782ebea0adba3d6aa9f1b3a080ea
Peter Tribble <peter.tribble@gmail.com>
2018-04-19 11:02:59 +0100

    Clarify illumos-gcc sparc build, and it needs a patch

--- TRIBdev-illumos-gcc/build.sh
+++ TRIBdev-illumos-gcc/build.sh
@@ -8,6 +8,10 @@
 ${THOME}/build/unpack gcc-4.4.4-il-4
 mv gcc-gcc-4.4.4-il-4 gcc-4.4.4-il-4
 cd gcc-4.4.4-il-4
+
+# needed for sparc
+gpatch -p1 < ${THOME}/build/patches/566b8eb0ae46d81c5eefda2dfd5c3c375fe77e4d.patch 
+
 ${THOME}/build/unpack mpc-0.8.1
 ${THOME}/build/unpack gmp-4.3.2
 ${THOME}/build/unpack mpfr-2.4.2
@@ -18,6 +22,9 @@ mv mpfr-2.4.2 mpfr
 mkdir build
 cd build
 
+#
+# sparc is --without-gnu-as --with-as=/usr/ccs/bin/as
+#
 env MSGFMT=/usr/gnu/bin/msgfmt GMSGFMT=/usr/gnu/bin/msgfmt PATH=${PATH}:/usr/gnu/bin LD_OPTIONS="-zignore -zcombreloc -Bdirect -i" \
 ../configure --prefix=/opt/gcc/4.4.4 --enable-languages=c,c++,objc \
   --with-pkgversion="Illumos/Tribblix" \
