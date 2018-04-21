d39768453dd19fd52fabd448b8266d2e341e1750
Peter Tribble <peter.tribble@gmail.com>
2018-04-21 15:01:37 +0100

    Update gpgme 1.11.1

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -3,5 +3,7 @@
 # must not have QT5 installed, else it will try and build against
 # it and fail, but there's no easy way to turn the QT support off
 #
-${THOME}/build/dobuild -64 gpgme-1.10.0
-${THOME}/build/genpkg TRIBgpgme gpgme-1.10.0
+# gpgme 1.11 needs libgpg-error 1.29
+#
+${THOME}/build/dobuild -64 gpgme-1.11.1
+${THOME}/build/genpkg TRIBgpgme gpgme-1.11.1
