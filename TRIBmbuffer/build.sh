3b10b2b8d7bdcfe192c195f4ad99c9e08af93343
Peter Tribble <peter.tribble@gmail.com>
2018-04-22 13:38:21 +0100

    Update mbuffer 20180410

--- TRIBmbuffer/build.sh
+++ TRIBmbuffer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild mbuffer-20160228
-${THOME}/build/genpkg TRIBmbuffer mbuffer-20160228
+${THOME}/build/dobuild mbuffer-20180410 -C --sysconfdir=/etc
+${THOME}/build/genpkg TRIBmbuffer mbuffer-20180410
