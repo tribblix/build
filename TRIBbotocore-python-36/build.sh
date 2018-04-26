09851bb0c52805af94ebec6a14447f13a59afc88
Peter Tribble <peter.tribble@gmail.com>
2018-04-26 16:05:19 +0100

    Update botocore 1.10.9

--- TRIBbotocore-python-36/build.sh
+++ TRIBbotocore-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack botocore-1.8.33
-cd botocore-1.8.33
+${THOME}/build/unpack botocore-1.10.9
+cd botocore-1.10.9
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pdu
