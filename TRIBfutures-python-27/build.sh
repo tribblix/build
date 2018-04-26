3472d7415019cd60623f0c3302990f699fde7584
Peter Tribble <peter.tribble@gmail.com>
2018-04-26 15:53:38 +0100

    Update python futures 3.2.0

--- TRIBfutures-python-27/build.sh
+++ TRIBfutures-python-27/build.sh
@@ -3,8 +3,8 @@
 # this is a backport of the python3 concurrent.futures module
 # it isn't needed for python3, obviously
 #
-${THOME}/build/unpack futures-3.1.1
-cd futures-3.1.1
+${THOME}/build/unpack futures-3.2.0
+cd futures-3.2.0
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
