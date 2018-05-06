865e823661e9793272af084d5d3f733d46004dee
Peter Tribble <peter.tribble@gmail.com>
2018-05-06 11:36:57 +0100

    Update suricata 4.0.4

--- TRIBsuricata/build.sh
+++ TRIBsuricata/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild suricata-3.2.3 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
-${THOME}/build/genpkg -I install-full TRIBsuricata suricata-3.2.3
+${THOME}/build/dobuild suricata-4.0.4 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
+${THOME}/build/genpkg -I install-full TRIBsuricata suricata-4.0.4
