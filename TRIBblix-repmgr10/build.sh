f2f389fdbddb90cad8701d4f5dd03d466b8f0d13
Peter Tribble <peter.tribble@gmail.com>
2018-05-06 10:48:27 +0100

    Update repmgr 4.0.5

--- TRIBblix-repmgr10/build.sh
+++ TRIBblix-repmgr10/build.sh
@@ -2,5 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-${THOME}/build/dobuild repmgr-4.0.4 -C PG_CONFIG=/opt/tribblix/postgres10/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr10 repmgr-4.0.4
+${THOME}/build/dobuild repmgr-4.0.5 -C PG_CONFIG=/opt/tribblix/postgres10/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr10 repmgr-4.0.5
--- TRIBblix-repmgr95/build.sh
+++ TRIBblix-repmgr95/build.sh
@@ -2,5 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-${THOME}/build/dobuild repmgr-4.0.4 -C PG_CONFIG=/opt/tribblix/postgres95/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr95 repmgr-4.0.4
+${THOME}/build/dobuild repmgr-4.0.5 -C PG_CONFIG=/opt/tribblix/postgres95/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr95 repmgr-4.0.5
