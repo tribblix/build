31c97c52f376eb79be5617438e578b80cb73720a
Peter Tribble <peter.tribble@gmail.com>
2018-05-13 12:15:58 +0100

    Add node v10

---
+++ TRIBv-node-v10/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# with isaexec switching
+#
+# note that we can't use the normal -64 build mechanism as node
+# has its own way of doing it
+#
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v10.1.0 -P /usr/versions/node-v10 -C --without-dtrace
+mv node-v10.1.0 node-v10.1.0-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v10.1.0 -P /usr/versions/node-v10 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v10 node-v10.1.0
