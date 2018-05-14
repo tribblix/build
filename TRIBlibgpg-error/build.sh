0278c15625f7d930899dfc7c4b0a47027460300d
Peter Tribble <peter.tribble@gmail.com>
2018-05-14 16:42:53 +0100

    Update libgpg-error 1.31

--- TRIBlibgpg-error/build.sh
+++ TRIBlibgpg-error/build.sh
@@ -2,5 +2,5 @@
 #
 # gpg-error
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64 libgpg-error-1.29
-${THOME}/build/genpkg TRIBlibgpg-error libgpg-error-1.29
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64 libgpg-error-1.31
+${THOME}/build/genpkg TRIBlibgpg-error libgpg-error-1.31
