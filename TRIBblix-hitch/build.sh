928f81ba5b3c3ed4ed57e59483e8c0ad9ab8e490
Peter Tribble <peter.tribble@gmail.com>
2018-05-15 19:46:49 +0100

    Update hitch 1.4.8

--- TRIBblix-hitch/build.sh
+++ TRIBblix-hitch/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS="-lsocket -lnsl" CPPFLAGS="-DBSD_COMP -D__EXTENSIONS__" TRIBBLIX_CFLAGS="-Wno-error=format" ${THOME}/build/dobuild hitch-1.4.6 -P /opt/tribblix/hitch -C --with-rst2man=/usr/versions/python-2.7/bin/rst2man.py
-${THOME}/build/genpkg TRIBblix-hitch hitch-1.4.6
+env LIBS="-lsocket -lnsl" CPPFLAGS="-DBSD_COMP -D__EXTENSIONS__" TRIBBLIX_CFLAGS="-Wno-error=format" ${THOME}/build/dobuild hitch-1.4.8 -P /opt/tribblix/hitch -C --with-rst2man=/usr/versions/python-2.7/bin/rst2man.py
+${THOME}/build/genpkg TRIBblix-hitch hitch-1.4.8
