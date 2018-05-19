484d5121807d880ecd910c155ce7d802c9d52b8e
Peter Tribble <peter.tribble@gmail.com>
2018-05-19 11:05:28 +0100

    Update unbound 1.7.1

--- TRIBunbound/build.sh
+++ TRIBunbound/build.sh
@@ -2,5 +2,5 @@
 #
 # mustn't have pkgsrc installed, else it will pick up the wrong things
 #
-${THOME}/build/dobuild unbound-1.6.7 -C "--without-libevent --sysconfdir=/etc" -J 1
-${THOME}/build/genpkg TRIBunbound unbound-1.6.7
+${THOME}/build/dobuild unbound-1.7.1 -C "--without-libevent --sysconfdir=/etc" -J 1
+${THOME}/build/genpkg TRIBunbound unbound-1.7.1
