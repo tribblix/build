746ffd043b9f5cbe3c7e2e2f20f5ce93cc0d7620
Peter Tribble <peter.tribble@gmail.com>
2018-05-27 09:24:43 +0100

    Update barman 2.4

--- TRIBpgbarman/build.sh
+++ TRIBpgbarman/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack barman-2.3
-cd barman-2.3
+${THOME}/build/unpack barman-2.4
+cd barman-2.4
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
