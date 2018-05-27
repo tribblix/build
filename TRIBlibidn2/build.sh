5e6f4a9355ac0fa635b90722103329f1614ebe39
Peter Tribble <peter.tribble@gmail.com>
2018-05-27 13:56:58 +0100

    Update libidn2 2.0.5

--- TRIBlibidn2/build.sh
+++ TRIBlibidn2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libidn2-2.0.4
-${THOME}/build/genpkg TRIBlibidn2 libidn2-2.0.4
+${THOME}/build/dobuild -64 libidn2-2.0.5
+${THOME}/build/genpkg TRIBlibidn2 libidn2-2.0.5
