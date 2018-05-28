adfe06e7b66fa0595a9fb67d760f0ead94013d07
Peter Tribble <peter.tribble@gmail.com>
2018-05-28 11:26:06 +0100

    Update gdb 7.12.1

--- TRIBdebug-gdb/build.sh
+++ TRIBdebug-gdb/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/dobuild gdb-7.11.1 -C --disable-largefile
-mv gdb-7.11.1 gdb-7.11.1-32
-${THOME}/build/dobuild +64 gdb-7.11.1
-mv gdb-7.11.1-32 gdb-7.11.1
-env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/genpkg TRIBdebug-gdb gdb-7.11.1
+${THOME}/build/dobuild gdb-7.12.1 -C --disable-largefile
+mv gdb-7.12.1 gdb-7.12.1-32
+${THOME}/build/dobuild +64 gdb-7.12.1
+mv gdb-7.12.1-32 gdb-7.12.1
+env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/genpkg TRIBdebug-gdb gdb-7.12.1
