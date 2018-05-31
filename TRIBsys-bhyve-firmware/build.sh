42b475f8139830db345866c6d9d40ab411d0660c
Peter Tribble <peter.tribble@gmail.com>
2018-05-31 08:27:36 +0100

    Add bhyve firmware

---
+++ TRIBsys-bhyve-firmware/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# we cheat a bit here, and use the prebuilt omnios files
+#
+# on an omnios box (or omnios alien zone):
+# pkg install system/bhyve/firmware
+# cd /
+# tar cf bhyve-fw.tar usr/share/bhyve/firmware
+#
+# and then check in that file for the fixit script to unpack
+#
+mkdir tmp1
+cd tmp1
+${THOME}/build/genpkg TRIBsys-bhyve-firmware
+cd ..
+rmdir tmp1
