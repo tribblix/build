d984a875776e8ef2e2d7b308a5decd681b6776eb
Peter Tribble <peter.tribble@gmail.com>
2018-06-06 11:27:40 +0100

    Update lzip 1.20 lzlib 1.10 plzip 1.7

--- TRIBcompress-lzip/build.sh
+++ TRIBcompress-lzip/build.sh
@@ -2,7 +2,7 @@
 #
 # lzip and friends
 #
-${THOME}/build/dobuild lzip-1.16
-${THOME}/build/dobuild lzlib-1.6 -C --enable-shared
-${THOME}/build/dobuild plzip-1.3 -C "CPPFLAGS=-I../lzlib-1.6 LDFLAGS=-L../lzlib-1.6"
-${THOME}/build/genpkg TRIBcompress-lzip lzip-1.16 lzlib-1.6 plzip-1.3
+${THOME}/build/dobuild lzip-1.20
+${THOME}/build/dobuild lzlib-1.10 -C --enable-shared
+${THOME}/build/dobuild plzip-1.7 -C "CPPFLAGS=-I../lzlib-1.10 LDFLAGS=-L../lzlib-1.10"
+${THOME}/build/genpkg TRIBcompress-lzip lzip-1.20 lzlib-1.10 plzip-1.7
