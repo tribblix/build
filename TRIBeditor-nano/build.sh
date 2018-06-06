930d2e53e19e1fc41a383ed7e7b17bddd60c4115
Peter Tribble <peter.tribble@gmail.com>
2018-06-06 11:11:26 +0100

    Update nano 2.9.8

--- TRIBeditor-nano/build.sh
+++ TRIBeditor-nano/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild nano-2.9.7
-${THOME}/build/genpkg TRIBeditor-nano nano-2.9.7
+${THOME}/build/dobuild nano-2.9.8
+${THOME}/build/genpkg TRIBeditor-nano nano-2.9.8
