d8697ef90750bb4b28a6ed66ecd1b088d0db1252
Peter Tribble <peter.tribble@gmail.com>
2018-06-08 21:46:42 +0100

    Update unrar 5.6.4

--- TRIBcompress-unrar/build.sh
+++ TRIBcompress-unrar/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #
-${THOME}/build/unpack unrarsrc-5.4.2
+${THOME}/build/unpack unrarsrc-5.6.4
 cd unrar
 gmake
 rm -fr /tmp/uu
 mkdir -p /tmp/uu/usr/bin
 cp unrar /tmp/uu/usr/bin
 ${THOME}/build/create_pkg TRIBcompress-unrar /tmp/uu
+cd ..
 rm -fr /tmp/uu
