c3d9863a116b2004cde83eded5cb5cf42d3b70d1
Peter Tribble <peter.tribble@gmail.com>
2018-06-10 15:36:04 +0100

    Update openntpd 6.2p3

--- TRIBopenntpd/build.sh
+++ TRIBopenntpd/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild openntpd-6.0p1 -C "--sysconfdir=/etc --localstatedir=/var --with-privsep-user=ntp"
-${THOME}/build/genpkg TRIBopenntpd openntpd-6.0p1
+${THOME}/build/dobuild openntpd-6.2p3 -C "--sysconfdir=/etc --localstatedir=/var --with-privsep-user=ntp"
+${THOME}/build/genpkg TRIBopenntpd openntpd-6.2p3
