aa0b062d1877bbdd2dff5ed33bccb1a1d4c7ef4e
Peter Tribble <peter.tribble@gmail.com>
2018-06-17 19:16:15 +0100

    Update rygel 0.36.1

--- TRIBrygel/build.sh
+++ TRIBrygel/build.sh
@@ -2,5 +2,5 @@
 #
 # rygel [needs gupnp gupnp-av gupnp-dlna vala libmediaart tracker]
 #
-${THOME}/build/dobuild -gnu rygel-0.32.0 -C --disable-introspection
-${THOME}/build/genpkg TRIBrygel rygel-0.32.0
+${THOME}/build/dobuild -gnu rygel-0.36.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBrygel rygel-0.36.1
