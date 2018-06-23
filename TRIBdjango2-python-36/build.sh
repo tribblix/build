df62b0c27f6a5e2eefa35ef2100c3308038873ea
Peter Tribble <peter.tribble@gmail.com>
2018-06-23 13:31:13 +0100

    Add Django 2.0.6

---
+++ TRIBdjango2-python-36/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+#
+#
+${THOME}/build/unpack Django-2.0.6
+cd Django-2.0.6
+rm -fr /tmp/p3a
+/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/p3a
+${THOME}/build/create_pkg TRIBdjango2-python-36 /tmp/p3a
+cd ..
+rm -fr /tmp/p3a
