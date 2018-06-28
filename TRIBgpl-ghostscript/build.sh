8f0426ef94783ca7fbb0c851c362e5ff49a0d32f
Peter Tribble <peter.tribble@gmail.com>
2018-06-28 18:53:26 +0100

    Update GhostScript 9.23

--- TRIBgpl-ghostscript/build.sh
+++ TRIBgpl-ghostscript/build.sh
@@ -7,18 +7,21 @@
 # see the BLFS documentation, we build the same way
 #  http://www.linuxfromscratch.org/blfs/view/svn/pst/gs.html
 #
-${THOME}/build/dobuild ghostscript-9.19 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
-cd ghostscript-9.19
+${THOME}/build/dobuild ghostscript-9.23 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
+cd ghostscript-9.23
 gmake -j4 so
 rm -fr /tmp/ags
 gmake DESTDIR=/tmp/ags install
 gmake DESTDIR=/tmp/ags soinstall
 ginstall -v -m644 base/*.h /tmp/ags/usr/include/ghostscript/
+tar cf - examples | ( cd /tmp/ags/usr/share/ghostscript/9.23 ; tar xf -)
 mkdir -p /tmp/ags/usr/share/ghostscript
 cd /tmp/ags/usr/share/ghostscript
 ${THOME}/build/unpack ghostscript-fonts-std-8.11
 ${THOME}/build/unpack gnu-gs-fonts-other-6.0
-rm 9.19/doc/*.*
+rm -f 9.23/doc
+rm -f 9.23/doc/*.*
+rm -f ../doc/ghostscript/9.23/*.*
 cd
 ${THOME}/build/create_pkg TRIBgpl-ghostscript /tmp/ags
 rm -fr /tmp/ags
