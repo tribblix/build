758e26abe4dca82eeefb5f3cee8c31e72eb6ed1b
Peter Tribble <peter.tribble@gmail.com>
2018-06-28 21:47:42 +0100

    Update R 3.5.0

--- TRIBr/build.sh
+++ TRIBr/build.sh
@@ -5,8 +5,8 @@
 #
 zap install-overlay fortran
 zap install TRIBgnu-libiconv TRIBreadline
-${THOME}/build/unpack R-3.3.2
-cd R-3.3.2
+${THOME}/build/unpack R-3.5.0
+cd R-3.5.0
 env PATH=/usr/gnu/bin:$PATH CPPFLAGS="-I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ./configure --prefix=/usr
 gmake -j 12
 gmake check
@@ -14,4 +14,5 @@ rm -fr /tmp/grs
 env PATH=/usr/gnu/bin:$PATH gmake prefix=/tmp/grs/usr install-strip
 # (strip results in 54M rather than 68M of disk space used)
 ${THOME}/build/create_pkg TRIBr /tmp/grs
+cd ..
 rm -fr /tmp/grs
