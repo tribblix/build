959aac058ccc7b1fe0fb079a86313d8ac294f30b
Peter Tribble <peter.tribble@gmail.com>
2018-07-08 16:40:19 +0100

    Update CDE 2.3.0

--- TRIBcde/build.sh
+++ TRIBcde/build.sh
@@ -2,63 +2,38 @@
 #
 # Since the 2.2.1 release, CDE should build out of the box
 #
-${THOME}/build/unpack cde-src-2.2.4a
-cd cde-2.2.4a
+${THOME}/build/unpack cde-2.3.0
+cd cde-2.3.0
 
-mkdir -p imports/x11/include
-cd imports/x11/include
-ln -s /usr/include/X11 .
-ln -s /usr/X11/include/Xm .
-
-cd ../../..
 # ** edit config/imake/Makefile.ini cc->gcc
 make World
 
-rm -fr /tmp/cde5
-make -k install DESTDIR=/tmp/cde5
-make -k install.man DESTDIR=/tmp/cde5
+#
+# run the CDE installer
+#
+# it won't actually run to completion, but that doesn't matter
+# as it will have done the important thing which is to populate
+# a directory tree under /tmp/111
+#
+rm -fr /tmp/111
+./admin/IntegTools/dbTools/installCDE -s `pwd` -pseudo -destdir /tmp/111 -t /tmp/cde.tar
 
 #
-# FIXME
-#  programs/localized/C/types/dtwm.fp need to end up in
-#    /usr/dt/appconfig/types or /usr/dt/appconfig/types/C
+# clean up
+#
+rm -f /tmp/CDE*
+rm -fr /tmp/cde.tar
 
-( cd doc/C/man ; tar cf - man1 man1m man3 man4 man5 man6 ) | ( cd /tmp/cde5/usr/dt/man ; tar xf - )
-( cd programs ; tar cf - palettes backdrops ) | (cd /tmp/cde5/usr/dt ; tar xf -)
-mkdir -p /tmp/cde5/usr/dt/config/C
-#cp programs/localized/C/config/sys.dtwmrc /tmp/cde5/usr/dt/config/C
-mkdir -p /tmp/cde5/usr/dt/lib/nls/msg/C
-cp programs/localized/C/msg/*.cat /tmp/cde5/usr/dt/lib/nls/msg/C
-mkdir -p /tmp/cde5/usr/dt/app-defaults
-cp programs/localized/C/app-defaults/Dtwm /tmp/cde5/usr/dt/app-defaults
-mkdir -p /tmp/cde5/usr/dt/appconfig/types/C
-cp programs/localized/C/types/*.dt /tmp/cde5/usr/dt/appconfig/types/C
-cp programs/icons/*.pm programs/icons/*.bm /tmp/cde5/C
+#
+# you need to have the DT font aliases defined, else dtterm will look bizarre
+# and lots of other things won't work at all
+#
+mkdir -p /tmp/111/etc/X11/fontpath.d
+ln -s ../../../usr/dt/config/xfonts/C /tmp/111/etc/X11/fontpath.d/cde:pri
 
-cd /tmp/cde5
-mkdir -p usr/dt/config
-mv /tmp/cde5misc usr/dt/config/svc
-mv bin/solaris/dtinfo usr/dt/bin
-rmdir bin/solaris
-mv bin/* usr/dt/bin
-rmdir bin
-mkdir -p usr/dt/app-defaults
-mv app-defaults/* usr/dt/app-defaults
-rmdir app-defaults
-rmdir usr/dt/lib/X11/app-defaults
-ln -s ../../app-defaults usr/dt/lib/X11
-mkdir -p usr/dt/appconfig
-mv appconfig/* usr/dt/appconfig
-rmdir appconfig
-mv infolib usr/dt
-mkdir -p usr/dt/appconfig/icons
-mv C ja usr/dt/appconfig/icons
-cd usr/dt
-mkdir share
-mv man include share
-mkdir -p share/man/man1x
-mv share/man/man1/*.1x share/man/man1x
-ln -s share/* .
-${THOME}/build/create_pkg TRIBcde /tmp/cde5
+#
+# and make the package
+#
+${THOME}/build/create_pkg TRIBcde /tmp/111
 
-rm -fr /tmp/cde5
+rm -fr /tmp/111
