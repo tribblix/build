4a74270e030763bf4da8c1e1d1d8c087c7766e01
Peter Tribble <peter.tribble@gmail.com>
2018-07-11 13:31:20 +0100

    Update boto3 1.7.55

--- TRIBboto3-python-27/build.sh
+++ TRIBboto3-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.7.31
-cd boto3-1.7.31
+${THOME}/build/unpack boto3-1.7.55
+cd boto3-1.7.55
 python setup.py build
 rm -fr /tmp/sut
 python setup.py install --root=/tmp/sut
--- TRIBboto3-python-36/build.sh
+++ TRIBboto3-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.7.31
-cd boto3-1.7.31
+${THOME}/build/unpack boto3-1.7.55
+cd boto3-1.7.55
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/sut
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/sut
