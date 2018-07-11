298cd28892cdb2c4cfdd626119ec1509657fce20
Peter Tribble <peter.tribble@gmail.com>
2018-07-11 13:35:50 +0100

    Update awscli 1.15.56

--- TRIBawscli-python-27/build.sh
+++ TRIBawscli-python-27/build.sh
@@ -2,8 +2,8 @@
 #
 # needs botocore colorama s3transfer docutils rsa yaml
 #
-${THOME}/build/unpack awscli-1.15.31
-cd awscli-1.15.31
+${THOME}/build/unpack awscli-1.15.56
+cd awscli-1.15.56
 python setup.py build
 rm -fr /tmp/sut
 python setup.py install --root=/tmp/sut
--- TRIBawscli-python-36/build.sh
+++ TRIBawscli-python-36/build.sh
@@ -2,8 +2,8 @@
 #
 # needs botocore colorama s3transfer docutils rsa yaml
 #
-${THOME}/build/unpack awscli-1.15.31
-cd awscli-1.15.31
+${THOME}/build/unpack awscli-1.15.56
+cd awscli-1.15.56
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/sut
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/sut
