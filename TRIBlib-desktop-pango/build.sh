b3e28b8e6fbaa91950dd0d6e8377c6c2eed39942
Peter Tribble <peter.tribble@gmail.com>
2018-07-15 14:57:11 +0100

    Update pango 1.40.5

--- TRIBlib-desktop-pango/build.sh
+++ TRIBlib-desktop-pango/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# the +E is so that we can have different pango.modules for 32 and 64-bit
+# stuck at 1.40.5, build failures on newer
 #
-${THOME}/build/dobuild -64 pango-1.36.8 +E /usr/etc -C "--disable-introspection"
-${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.36.8
+${THOME}/build/dobuild -64 pango-1.40.5 -C "--disable-introspection"
+${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.40.5
