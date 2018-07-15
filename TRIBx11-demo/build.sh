0af7ed0411b99d8d8d6b020a30da06f9ea8e78fc
Peter Tribble <peter.tribble@gmail.com>
2018-07-15 20:34:18 +0100

    Builld x11 deoms ourselves

---
+++ TRIBx11-demo/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild ico-1.0.5
+${THOME}/build/dobuild xeyes-1.1.2
+${THOME}/build/dobuild xgc-1.0.5
+${THOME}/build/dobuild xlogo-1.0.4
+${THOME}/build/genpkg TRIBx11-demo ico-1.0.5 xeyes-1.1.2 xgc-1.0.5 xlogo-1.0.4
