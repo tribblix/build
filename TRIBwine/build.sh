7fac268d30dc6dbfb4ca9c9068aefc7ed15df6c1
Peter Tribble <peter.tribble@gmail.com>
2018-07-20 19:32:30 +0100

    update wine 3.0.2

--- TRIBwine/build.sh
+++ TRIBwine/build.sh
@@ -2,5 +2,5 @@
 #
 # wine
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-3.0
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-3.0
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-3.0.2
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-3.0.2
