0232056f14159e432adee037c2df9b4e5b7565e7
Peter Tribble <peter.tribble@gmail.com>
2018-07-20 22:13:29 +0100

    Update subversion 1.9.9

--- TRIBv-subversion-19/build.sh
+++ TRIBv-subversion-19/build.sh
@@ -13,7 +13,7 @@ ln -s . apr-util-1.5.4/bin
 cd serf-1.3.8
 scons APR=../apr-1.5.2 APU=../apr-util-1.5.4 PREFIX=/usr/versions/subversion-1.9 CPPFLAGS=-D__EXTENSIONS__ LINKFLAGS="-L../apr-1.5.2/.libs -L../apr-util-1.5.4/.libs -R/usr/versions/subversion-1.9/lib"
 cd ..
-${THOME}/build/dobuild subversion-1.9.7 -P /usr/versions/subversion-1.9 -C "--with-apr=../apr-1.5.2 --with-apr-util=../apr-util-1.5.4 --with-serf=../serf-1.3.8 CPPFLAGS=-D__EXTENSIONS__" -L
+${THOME}/build/dobuild subversion-1.9.9 -P /usr/versions/subversion-1.9 -C "--with-apr=../apr-1.5.2 --with-apr-util=../apr-util-1.5.4 --with-serf=../serf-1.3.8 CPPFLAGS=-D__EXTENSIONS__" -L
 # symlinks so libtool stupid relinking works
 cd apr-1.5.2
 ln -s .libs/* .
@@ -22,5 +22,5 @@ cd apr-util-1.5.4
 ln -s .libs/* .
 cd ..
 # serf installed by fixinstall
-${THOME}/build/genpkg TRIBv-subversion-19 subversion-1.9.7 apr-1.5.2 apr-util-1.5.4
+${THOME}/build/genpkg TRIBv-subversion-19 subversion-1.9.9 apr-1.5.2 apr-util-1.5.4
 # [phew]
