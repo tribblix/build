7ba163c253204b0c192a4f6ce164bb3d8c072f51
Peter Tribble <peter.tribble@gmail.com>
2018-07-21 14:33:10 +0100

    Build libXv libXvMC ourselves

---
+++ TRIBx11-libxv/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild -64 libXv-1.0.11
+${THOME}/build/genpkg TRIBx11-libxv libXv-1.0.11
---
+++ TRIBx11-libxvmc/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild -64 libXvMC-1.0.10
+${THOME}/build/genpkg TRIBx11-libxvmc libXvMC-1.0.10
