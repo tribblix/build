a32d52739757ea8ecf9cafc2284e545eea498173
Peter Tribble <peter.tribble@gmail.com>
2018-07-29 09:38:21 +0100

    Update flex 2.6.4

--- TRIBflex/build.sh
+++ TRIBflex/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 #
-# with 2.6.x need to work harder to force gnu m4, -gnu isn't enough
-# 2.6.x is broken
-#
-# get the 2.5.39 tarball from sourceforge, the one from github is busted
-#
-${THOME}/build/dobuild -gnu -64 flex-2.5.39
-${THOME}/build/genpkg TRIBflex flex-2.5.39
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64 flex-2.6.4
+${THOME}/build/genpkg TRIBflex flex-2.6.4
