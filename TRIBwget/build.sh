13f3a88f1416c74c0a5aecbc0ce61b7a5a384302
Peter Tribble <peter.tribble@gmail.com>
2018-07-31 09:20:28 +0100

    Rebuild wget, fix sparc build, fix wgetrc location

--- TRIBwget/build.sh
+++ TRIBwget/build.sh
@@ -2,5 +2,5 @@
 #
 # (no pcre in the install environment)
 #
-env POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.19.5 -C '--with-ssl=openssl --disable-pcre'
+env POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.19.5 -C '--sysconfdir=/etc --with-ssl=openssl --disable-pcre'
 ${THOME}/build/genpkg TRIBwget wget-1.19.5
