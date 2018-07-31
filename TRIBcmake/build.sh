2a2267000884015db25662146cc653874d1f46b6
Peter Tribble <peter.tribble@gmail.com>
2018-07-31 09:34:01 +0100

    Update cmake 3.11.4, and fix packaging

--- TRIBcmake/build.sh
+++ TRIBcmake/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack cmake-3.11.1
-cd cmake-3.11.1
+${THOME}/build/unpack cmake-3.11.4
+cd cmake-3.11.4
 sed -i 's:DCMAKE_BOOTSTRAP=1:DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=ON:' bootstrap
 ./bootstrap --prefix=/usr/versions/cmake-3.11
 gmake -j 4
