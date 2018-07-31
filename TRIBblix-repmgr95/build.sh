894406955cd2d0617ec371a771000b61b6a3ac8b
Peter Tribble <peter.tribble@gmail.com>
2018-07-31 18:43:51 +0100

    Remove postgres 9.5

--- TRIBblix-repmgr95/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# this is a postgres extension, so it's specific to the postgres version
-#
-${THOME}/build/dobuild repmgr-4.0.6 -C PG_CONFIG=/opt/tribblix/postgres95/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr95 repmgr-4.0.6
