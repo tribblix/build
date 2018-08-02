40acbab20225eb4bd4bc50ffd93554667e6ac9e8
Peter Tribble <peter.tribble@gmail.com>
2018-08-02 18:34:11 +0100

    Update Illumos SMF and Zones perl modules

--- TRIBlib-perl-5-il-smf/build.sh
+++ TRIBlib-perl-5-il-smf/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Illumos-SMF-0.1.6
-cd Illumos-SMF-0.1.6
+${THOME}/build/unpack Illumos-SMF-0.1.7
+cd Illumos-SMF-0.1.7
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-il-smf Illumos-SMF-0.1.6
+${THOME}/build/genpkg TRIBlib-perl-5-il-smf Illumos-SMF-0.1.7
--- TRIBlib-perl-5-il-zones/build.sh
+++ TRIBlib-perl-5-il-zones/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Illumos-Zones-0.1.4
-cd Illumos-Zones-0.1.4
+${THOME}/build/unpack Illumos-Zones-0.1.7
+cd Illumos-Zones-0.1.7
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-il-zones Illumos-Zones-0.1.4
+${THOME}/build/genpkg TRIBlib-perl-5-il-zones Illumos-Zones-0.1.7
