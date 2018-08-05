49dceff97e67985f020557c98a0bc4820ee1c64b
Peter Tribble <peter.tribble@gmail.com>
2018-08-05 13:25:15 +0100

    Update mercurial 4.7

--- TRIBmercurial/build.sh
+++ TRIBmercurial/build.sh
@@ -3,8 +3,8 @@
 #
 #
 zap install TRIBdocutils-python-27
-${THOME}/build/unpack mercurial-4.5.3
-cd mercurial-4.5.3
+${THOME}/build/unpack mercurial-4.7
+cd mercurial-4.7
 gmake all
 rm -fr /tmp/hg1
 python setup.py install --root=/tmp/hg1
