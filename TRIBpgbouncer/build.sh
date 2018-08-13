192822b07a35baff44c91d36fe4057313233dfb8
Peter Tribble <peter.tribble@gmail.com>
2018-08-13 19:14:49 +0100

    Update pgbouncer 1.9.0

--- TRIBpgbouncer/build.sh
+++ TRIBpgbouncer/build.sh
@@ -2,6 +2,6 @@
 #
 # there's a sacrificial static libevent built by the preconf script
 #
-${THOME}/build/dobuild pgbouncer-1.8.1 -C --with-libevent=/tmp/mc
-${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.8.1
+${THOME}/build/dobuild pgbouncer-1.9.0 -C --with-libevent=/tmp/mc
+${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.9.0
 rm -fr /tmp/mc
