f54bde293b7f012f923b21aa9fb56fdfb37d4757
Peter Tribble <peter.tribble@gmail.com>
2018-08-14 13:42:05 +0100

    Update xmltooling-c to 3.0.2

--- TRIBshib-xmltooling-c/build.sh
+++ TRIBshib-xmltooling-c/build.sh
@@ -2,5 +2,5 @@
 #
 # shibboleth stack requires boost ssl curl apache
 #
-env PKG_CONFIG_PATH=/opt/tribblix/shibboleth-sp/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild xmltooling-3.0.0 -P /opt/tribblix/shibboleth-sp -C "--disable-static --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBshib-xmltooling-c xmltooling-3.0.0
+env PKG_CONFIG_PATH=/opt/tribblix/shibboleth-sp/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild xmltooling-3.0.2 -P /opt/tribblix/shibboleth-sp -C "--disable-static --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBshib-xmltooling-c xmltooling-3.0.2
