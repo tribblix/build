09133752a4e8e62500ae75b51ecf4f23d34e4d5e
Peter Tribble <peter.tribble@gmail.com>
2018-08-26 10:57:38 +0100

    Update libxkbcommon 0.8.2

--- TRIBlibxkbcommon/build.sh
+++ TRIBlibxkbcommon/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libxkbcommon-0.7.0
-${THOME}/build/genpkg TRIBlibxkbcommon libxkbcommon-0.7.0
+${THOME}/build/dobuild -64 libxkbcommon-0.8.2
+${THOME}/build/genpkg TRIBlibxkbcommon libxkbcommon-0.8.2
