de01d8b6ae67c77793a521be26d97f230ce8dbcc
Peter Tribble <peter.tribble@gmail.com>
2018-08-27 19:48:10 +0100

    Build kbd_mode ourselves

---
+++ TRIBsys-keyboard-kbd-mode/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# built by hand
+#
+rm -fr kbd_mode
+cp -r ${THOME}/build/patches/kbd_mode .
+cd kbd_mode
+make CC=gcc
+mkdir -p /tmp/km/usr/share/man/man1
+cp kbd_mode.1 /tmp/km/usr/share/man/man1
+mkdir -p /tmp/km/usr/bin 
+cp kbd_mode /tmp/km/usr/bin
+cd ..
+${THOME}/build/create_pkg TRIBsys-keyboard-kbd-mode /tmp/km
+rm -fr kbd_mode
+rm -fr /tmp/km
