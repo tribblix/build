3ff51ee4ebd628647bbe71b7847bc3cb7ff1ad2b
Peter Tribble <peter.tribble@gmail.com>
2018-09-09 14:11:53 +0100

    Update xscreensaver 5.40

--- TRIBxscreensaver/build.sh
+++ TRIBxscreensaver/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xscreensaver-5.39 -C "--with-gtk"
-cd xscreensaver-5.39
+${THOME}/build/dobuild -gnu xscreensaver-5.40 -C "--with-gtk"
+cd xscreensaver-5.40
 rm -fr /tmp/xs
 gmake install install_prefix=/tmp/xs
 ${THOME}/build/create_pkg TRIBxscreensaver /tmp/xs
