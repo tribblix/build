bfe83eac6d011845004f35edb982a87dda43a88a
Peter Tribble <peter.tribble@gmail.com>
2018-09-10 17:00:42 +0100

    Update zabbix 3.0.21

--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.20 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-3.0.20
+${THOME}/build/dobuild zabbix-3.0.21 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-3.0.21
--- TRIBzabbix-proxy/build.sh
+++ TRIBzabbix-proxy/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-3.0.20 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl"
-${THOME}/build/genpkg TRIBzabbix-proxy zabbix-3.0.20
+${THOME}/build/dobuild zabbix-3.0.21 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl"
+${THOME}/build/genpkg TRIBzabbix-proxy zabbix-3.0.21
