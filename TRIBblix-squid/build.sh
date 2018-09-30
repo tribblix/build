9307b368b8523135b5a0d9c1370b5fe0713bd06c
Peter Tribble <peter.tribble@gmail.com>
2018-09-30 21:22:56 +0100

    Update squid 3.5.28

--- TRIBblix-squid/build.sh
+++ TRIBblix-squid/build.sh
@@ -3,7 +3,9 @@
 # Problems with pod2man and -Werror
 #
 # uninstall TRIBlibtool, otherwise it gets linked against
+# build fails if berkeley-db is installed
 #
 zap uninstall TRIBlibtool
-env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/dobuild squid-3.5.27 -P /opt/tribblix/squid -C "--with-gnutls --disable-strict-error-checking --disable-static --disable-ltdl-install"
-env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/genpkg TRIBblix-squid squid-3.5.27
+zap uninstall TRIBberkeley-db
+env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/dobuild squid-3.5.28 -P /opt/tribblix/squid -C "--with-gnutls --disable-strict-error-checking --disable-static --disable-ltdl-install"
+env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/genpkg TRIBblix-squid squid-3.5.28
