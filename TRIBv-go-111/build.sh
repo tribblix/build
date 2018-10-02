df44f97ef91c6d78439ecb9abdc5404d32498e46
Peter Tribble <peter.tribble@gmail.com>
2018-10-02 18:47:18 +0100

    Update go 1.11.1

--- TRIBv-go-111/build.sh
+++ TRIBv-go-111/build.sh
@@ -6,11 +6,11 @@
 # There have been issues with GOROOT_FINAL causing test failures
 # https://github.com/golang/go/issues/20284
 #
-# initial 1.11 release, not yet default
-# when default, update aliss and reinstate /usr/bin links
+# 1.11 release, not yet default
+# when default, update alias and reinstate /usr/bin links
 #
 rm -fr go
-${THOME}/build/unpack go1.11.src
+${THOME}/build/unpack go1.11.1.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.11 GOROOT_BOOTSTRAP=/usr/versions/go-1.10 ./all.bash
 cd ..
