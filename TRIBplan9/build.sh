e8829ca3935fd6ad2e032f1ffed81e1a4f5f3d79
Peter Tribble <peter.tribble@gmail.com>
2018-10-10 08:25:05 +0100

    Update plan9port

--- TRIBplan9/build.sh
+++ TRIBplan9/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 #
-${THOME}/build/unpack plan9port-20140306
-cd plan9port
+# no versioned releases, built from master which had its latest
+# commit on Oct 5 2018, hence the version number
+#
+${THOME}/build/unpack plan9port-master
+cd plan9port-master
 env PATH=/usr/gnu/bin:$PATH PLAN9_TARGET=/usr/versions/plan9 ./INSTALL -b
 env PATH=/usr/gnu/bin:$PATH PLAN9_TARGET=/usr/versions/plan9 ./INSTALL -c
 cd ..
 mkdir -p t1/usr/versions
-mv plan9port t1/usr/versions/plan9
+mv plan9port-master t1/usr/versions/plan9
 ${THOME}/build/create_pkg TRIBplan9 `pwd`/t1
 rm -fr t1
