25609e5c60cc6ae3669c75d558f23d66d4e42965
Peter Tribble <peter.tribble@gmail.com>
2018-10-12 09:04:25 +0100

    Update php 7.2.11

--- TRIBblix-php-7/build.sh
+++ TRIBblix-php-7/build.sh
@@ -12,8 +12,8 @@
 # must have apache24 installed
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-7.2.10
-cd php-7.2.10
+${THOME}/build/unpack php-7.2.11
+cd php-7.2.11
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
