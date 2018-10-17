2f91ca69dab94f681d56d8d3bee854ec2f0cb938
Peter Tribble <peter.tribble@gmail.com>
2018-10-17 07:28:54 +0100

    Update libssh 0.7.6

--- TRIBlibssh/build.sh
+++ TRIBlibssh/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/cmbuild -64 libssh-0.7.3
-${THOME}/build/genpkg TRIBlibssh libssh-0.7.3
+${THOME}/build/cmbuild -64 libssh-0.7.6
+${THOME}/build/genpkg TRIBlibssh libssh-0.7.6
