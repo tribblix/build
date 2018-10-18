c2704deaf1a8f324b8ce1f48b66465226259daab
Peter Tribble <peter.tribble@gmail.com>
2018-10-18 11:37:43 +0100

    Update libSM 1.2.3

--- TRIBx11-libsm/build.sh
+++ TRIBx11-libsm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libSM-1.2.2 -C "--enable-unix-transport --enable-tcp-transport --enable-ipv6 --without-libuuid"
-${THOME}/build/genpkg TRIBx11-libsm libSM-1.2.2
+${THOME}/build/dobuild -64 libSM-1.2.3 -C "--enable-unix-transport --enable-tcp-transport --enable-ipv6 --without-libuuid"
+${THOME}/build/genpkg TRIBx11-libsm libSM-1.2.3
