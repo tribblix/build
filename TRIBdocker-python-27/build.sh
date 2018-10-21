d78cd6e83b41fe4f8f71787cf5bd15d8505ff943
Peter Tribble <peter.tribble@gmail.com>
2018-10-21 16:26:18 +0100

    Update python docker 3.5.1

--- TRIBdocker-python-27/build.sh
+++ TRIBdocker-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack docker-3.4.1
-cd docker-3.4.1
+${THOME}/build/unpack docker-3.5.1
+cd docker-3.5.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBdocker-python-36/build.sh
+++ TRIBdocker-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack docker-3.4.1
-cd docker-3.4.1
+${THOME}/build/unpack docker-3.5.1
+cd docker-3.5.1
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
