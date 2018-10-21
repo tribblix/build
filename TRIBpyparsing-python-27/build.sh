6b784d38724c6d79ca5d41581f343d1ae00fe4f7
Peter Tribble <peter.tribble@gmail.com>
2018-10-21 19:11:28 +0100

    Update pyparsing 2.2.2

--- TRIBpyparsing-python-27/build.sh
+++ TRIBpyparsing-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyparsing-2.2.0
-cd pyparsing-2.2.0
+${THOME}/build/unpack pyparsing-2.2.2
+cd pyparsing-2.2.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBpyparsing-python-36/build.sh
+++ TRIBpyparsing-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyparsing-2.2.0
-cd pyparsing-2.2.0
+${THOME}/build/unpack pyparsing-2.2.2
+cd pyparsing-2.2.2
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
