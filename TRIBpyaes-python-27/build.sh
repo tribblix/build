dcf7115e94a029e48b89931315cca08d3f8a212b
Peter Tribble <peter.tribble@gmail.com>
2018-10-21 18:50:50 +0100

    Update pyaes 1.6.1

--- TRIBpyaes-python-27/build.sh
+++ TRIBpyaes-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyaes-1.6.0
-cd pyaes-1.6.0
+${THOME}/build/unpack pyaes-1.6.1
+cd pyaes-1.6.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBpyaes-python-36/build.sh
+++ TRIBpyaes-python-36/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyaes-1.6.0
-cd pyaes-1.6.0
+${THOME}/build/unpack pyaes-1.6.1
+cd pyaes-1.6.1
 /usr/versions/python-3.6/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
