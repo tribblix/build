77887fc32a1a145e93de4b4624d95a1f9568cf0a
Peter Tribble <peter.tribble@gmail.com>
2018-10-26 19:35:10 +0100

    Update pgbadger 10.1

--- TRIBpgbadger/build.sh
+++ TRIBpgbadger/build.sh
@@ -2,12 +2,16 @@
 #
 # everything is done by the fixit script
 #
-${THOME}/build/unpack pgbadger-9.2
-cd pgbadger-9.2
+${THOME}/build/unpack pgbadger-10.1
+cd pgbadger-10.1
 # you need to run a make to create the manpage
 perl Makefile.PL
+sed -i 's:sed -i.bak:gsed -i.bak:' Makefile
 make
 rm -fr /tmp/pgb
+#
+# make install puts it inside the perl tree, so copy by hand
+#
 mkdir -p /tmp/pgb/usr/bin
 mkdir -p /tmp/pgb/usr/share/man/man1
 cp pgbadger /tmp/pgb/usr/bin
