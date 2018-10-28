9d1b0fa262f0aeed1579c1ba64a1c20817221c4c
Peter Tribble <peter.tribble@gmail.com>
2018-10-28 19:35:37 +0000

    Fix libspectre build recipe

--- TRIBlibspectre/build.sh
+++ TRIBlibspectre/build.sh
@@ -5,4 +5,4 @@
 # 32-bit only as libgs is 32-bit only
 #
 ${THOME}/build/dobuild libspectre-0.2.8
-${THOME}/build/genpkg TRIBlibspectre libspectre-0.2.8 TRIBlibspectre
+${THOME}/build/genpkg TRIBlibspectre libspectre-0.2.8
