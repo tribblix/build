5717cdf8b66e7fc74a6dd145ae8781e598583b15
Peter Tribble <peter.tribble@gmail.com>
2018-10-28 19:28:02 +0000

    Update libspectre 0.2.8

--- TRIBlibspectre/build.sh
+++ TRIBlibspectre/build.sh
@@ -2,5 +2,7 @@
 #
 # libspectre requires libgs provided by ghostscript
 #
-${THOME}/build/dobuild libspectre-0.2.7
-${THOME}/build/genpkg TRIBlibspectre libspectre-0.2.7 TRIBlibspectre
+# 32-bit only as libgs is 32-bit only
+#
+${THOME}/build/dobuild libspectre-0.2.8
+${THOME}/build/genpkg TRIBlibspectre libspectre-0.2.8 TRIBlibspectre
