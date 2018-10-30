8dcb76e78f3c506658a0f993a66ea89bc4e7097f
Peter Tribble <peter.tribble@gmail.com>
2018-10-30 11:33:33 +0000

    Remove all aws-related python36 modules

--- TRIBsix-python-36/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack six-1.11.0
-cd six-1.11.0
-rm -fr /tmp/pil
-/usr/versions/python-3.6/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsix-python-36 /tmp/pil
-cd ..
-rm -fr /tmp/pil
