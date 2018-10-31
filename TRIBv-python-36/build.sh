3cffc4773b83c0e9863df80c4aaa9fe8b19920f4
Peter Tribble <peter.tribble@gmail.com>
2018-10-31 09:40:01 +0000

    Remove python3.6 completely

--- TRIBv-python-36/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# NB. TRIBv-python-36 must not be installed, or the make
-# install step will get confused
-#
-${THOME}/build/dobuild Python-3.6.7 -P /usr/versions/python-3.6 -L -C --enable-shared
-# Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-36 Python-3.6.7
