4c9ec01eed02a51aed6eb5fb741d56a769fd6e41
Peter Tribble <peter.tribble@gmail.com>
2018-11-01 18:16:04 +0000

    Add simplejson 3.16.0

---
+++ TRIBsimplejson-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack simplejson-3.16.0
+cd simplejson-3.16.0
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBsimplejson-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
