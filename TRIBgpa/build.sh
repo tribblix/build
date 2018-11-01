630875855c8a25757941f2be3bd1ae18ed2aa016
Peter Tribble <peter.tribble@gmail.com>
2018-11-01 15:53:40 +0000

    Update gpa 0.10.0

--- TRIBgpa/build.sh
+++ TRIBgpa/build.sh
@@ -2,5 +2,5 @@
 #
 # gpa
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild gpa-0.9.10
-${THOME}/build/genpkg TRIBgpa gpa-0.9.10
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild gpa-0.10.0
+${THOME}/build/genpkg TRIBgpa gpa-0.10.0
