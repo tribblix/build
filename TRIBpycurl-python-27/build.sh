76ef4dedc847eb2de76b931698c231ec868c703a
Peter Tribble <peter.tribble@gmail.com>
2018-11-01 18:28:23 +0000

    Add pycurl 7.43.0.2

---
+++ TRIBpycurl-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pycurl-7.43.0.2
+cd pycurl-7.43.0.2
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpycurl-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
