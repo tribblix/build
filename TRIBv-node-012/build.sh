3dcc2b76c0310d54a5871a78d0b6c77c3e3640b5
Peter Tribble <peter.tribble@gmail.com>
2018-11-03 21:19:20 +0000

    Deprecate old nodes

--- TRIBv-node-012/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# as of Tribblix 0m17 we don't need to patch in libproc.h
-#
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v0.12.18 -P /usr/versions/node-v0.12 -C --without-dtrace
-${THOME}/build/genpkg TRIBv-node-012 node-v0.12.18
