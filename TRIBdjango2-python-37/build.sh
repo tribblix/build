45cf97e4d83803eb1b59daf0e31dcfac6225e73c
Peter Tribble <peter.tribble@gmail.com>
2018-11-04 14:19:45 +0000

    Update django 2.1.3

--- TRIBdjango2-python-37/build.sh
+++ TRIBdjango2-python-37/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-2.1.2
-cd Django-2.1.2
+${THOME}/build/unpack Django-2.1.3
+cd Django-2.1.3
 rm -fr /tmp/p3a
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango2-python-37 /tmp/p3a
