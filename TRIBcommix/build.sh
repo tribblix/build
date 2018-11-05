0e7cd4113a2208ed3ffd2ddb77518a386c7064fe
Peter Tribble <peter.tribble@gmail.com>
2018-11-05 11:45:05 +0000

    Update commix 2.6.0

--- TRIBcommix/build.sh
+++ TRIBcommix/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 # depends on python
-# needs the gnurealine module
+# needs the gnureadline module
 #
 rm -fr /tmp/ckx
 mkdir -p /tmp/ckx/usr/versions
 cd /tmp/ckx/usr/versions
-${THOME}/build/unpack commix-2.5-20180713
-mv commix-2.5-20180713 commix
+${THOME}/build/unpack commix-2.6-20180921
+mv commix-2.6-20180921 commix
 cd
 ${THOME}/build/create_pkg TRIBcommix /tmp/ckx
 rm -fr /tmp/ckx
