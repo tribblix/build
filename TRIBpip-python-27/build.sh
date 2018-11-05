941542fb5ca9cfcfcb79a67f834f5728279822a3
Peter Tribble <peter.tribble@gmail.com>
2018-11-05 14:14:56 +0000

    Add pip for python2.7

---
+++ TRIBpip-python-27/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# explicit addition of pip is only necessary for python2.X
+#
+# python2.7 has ensurepip, but unlike py3 it's not run by default
+#
+${THOME}/build/unpack pip-18.1
+cd pip-18.1
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpip-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
