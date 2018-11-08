6c60bac10f06843b2d47d924eede4f4db252d94d
Peter Tribble <peter.tribble@gmail.com>
2018-11-08 09:45:44 +0000

    Update gnupg 2.2.11

--- TRIBgnupg2/build.sh
+++ TRIBgnupg2/build.sh
@@ -2,5 +2,5 @@
 #
 # gnupg2
 #
-${THOME}/build/dobuild gnupg-2.2.10 -C --disable-ldap
-${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.10
+${THOME}/build/dobuild gnupg-2.2.11 -C "--disable-ldap --enable-gpg-is-gpg2"
+${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.11
