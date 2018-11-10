c73cd66271561de5d14de2a70ac157cc16f0fc8f
Peter Tribble <peter.tribble@gmail.com>
2018-11-10 15:33:35 +0000

    Update DBD-Pg to 3.7.4

--- TRIBlib-perl-5-dbd-pg/build.sh
+++ TRIBlib-perl-5-dbd-pg/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack DBD-Pg-3.5.3
-cd DBD-Pg-3.5.3
-env POSTGRES_HOME=/opt/tribblix/postgres96 perl Makefile.PL
+${THOME}/build/unpack DBD-Pg-3.7.4
+cd DBD-Pg-3.7.4
+env POSTGRES_HOME=/opt/tribblix/postgres11 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBlib-perl-5-dbd-pg
