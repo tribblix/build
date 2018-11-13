105570616a6c7f10189ff0c433b039587a932520
Peter Tribble <peter.tribble@gmail.com>
2018-11-13 09:12:53 +0000

    Update Radicale 2.1.11

--- TRIBradicale2-python-37/build.sh
+++ TRIBradicale2-python-37/build.sh
@@ -4,8 +4,8 @@
 #
 # radicale 2.x is incompatible with 1.x, and requires python3
 #
-${THOME}/build/unpack Radicale-2.1.10
-cd Radicale-2.1.10
+${THOME}/build/unpack Radicale-2.1.11
+cd Radicale-2.1.11
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
