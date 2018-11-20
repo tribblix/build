da74261e0716f0adc66ab0dc99e423ebc4bf3194
Peter Tribble <peter.tribble@gmail.com>
2018-11-20 10:30:44 +0000

    Enable plperl in all postgres versions

--- TRIBblix-postgres10/build.sh
+++ TRIBblix-postgres10/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 10
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.6 -C --without-readline -M world
+${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.6 -C "--without-readline --with-perl" -M world
 ${THOME}/build/genpkg -I install-world TRIBblix-postgres10 postgresql-10.6
--- TRIBblix-postgres11/build.sh
+++ TRIBblix-postgres11/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 11
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.1 -C --without-readline -M world
+${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.1 -C "--without-readline --with-perl" -M world
 ${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.1
