589c55c276d27b18dce803ff87b7b2a4d6c51aea
Peter Tribble <peter.tribble@gmail.com>
2018-11-22 13:21:33 +0000

    Update pciutils 3.6.2

--- TRIBpciutils/build.sh
+++ TRIBpciutils/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pciutils-3.4.1
-cd pciutils-3.4.1
+${THOME}/build/unpack pciutils-3.6.2
+cd pciutils-3.6.2
 env PATH=/usr/gnu/bin:$PATH gmake PREFIX=/usr OPT="-O2 -DBYTE_ORDER=1234 -DLITTLE_ENDIAN=1234"
 rm -fr /tmp/pp
 env PATH=/usr/gnu/bin:$PATH gmake install PREFIX=/usr DESTDIR=/tmp/pp
