273ab5f686ccbf786bd7dee19bce1237a181d90d
Peter Tribble <peter.tribble@gmail.com>
2018-11-23 19:26:18 +0000

    Update ghostscript 9.26

--- TRIBgpl-ghostscript/build.sh
+++ TRIBgpl-ghostscript/build.sh
@@ -7,14 +7,14 @@
 # see the BLFS documentation, we build the same way
 #  http://www.linuxfromscratch.org/blfs/view/svn/pst/gs.html
 #
-${THOME}/build/dobuild ghostscript-9.24 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
-cd ghostscript-9.24
+${THOME}/build/dobuild ghostscript-9.26 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
+cd ghostscript-9.26
 gmake -j4 so
 rm -fr /tmp/ags
 gmake DESTDIR=/tmp/ags install
 gmake DESTDIR=/tmp/ags soinstall
 ginstall -v -m644 base/*.h /tmp/ags/usr/include/ghostscript/
-tar cf - examples | ( cd /tmp/ags/usr/share/ghostscript/9.24 ; tar xf -)
+tar cf - examples | ( cd /tmp/ags/usr/share/ghostscript/9.26 ; tar xf -)
 mkdir -p /tmp/ags/usr/share/ghostscript
 # it looks odd just having a subset of man pages in german
 # no other non-english languages
@@ -22,9 +22,9 @@ rm -fr /tmp/ags/usr/share/man/de
 cd /tmp/ags/usr/share/ghostscript
 ${THOME}/build/unpack ghostscript-fonts-std-8.11
 ${THOME}/build/unpack gnu-gs-fonts-other-6.0
-rm -f 9.24/doc
-rm -f 9.24/doc/*.*
-rm -f ../doc/ghostscript/9.24/*.*
+rm -f 9.26/doc
+rm -f 9.26/doc/*.*
+rm -f ../doc/ghostscript/9.26/*.*
 cd
 ${THOME}/build/create_pkg TRIBgpl-ghostscript /tmp/ags
 rm -fr /tmp/ags
