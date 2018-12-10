e7713e4da4bcb5be94e20996744be3c4a36fb72b
Peter Tribble <peter.tribble@gmail.com>
2018-12-10 20:30:53 +0000

    Update node 6.15.1 10.14.1

--- TRIBv-node-v10/build.sh
+++ TRIBv-node-v10/build.sh
@@ -5,7 +5,7 @@
 # note that we can't use the normal -64 build mechanism as node
 # has its own way of doing it
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v10.14.0 -P /usr/versions/node-v10 -C --without-dtrace
-mv node-v10.14.0 node-v10.14.0-32
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v10.14.0 -P /usr/versions/node-v10 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v10 node-v10.14.0
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v10.14.1 -P /usr/versions/node-v10 -C --without-dtrace
+mv node-v10.14.1 node-v10.14.1-32
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v10.14.1 -P /usr/versions/node-v10 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v10 node-v10.14.1
