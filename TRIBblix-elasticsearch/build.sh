37cff0e2addc2862906527f4c1fc66ab10fcd6d5
Peter Tribble <peter.tribble@gmail.com>
2018-12-13 16:42:51 +0000

    Update elk stack 5.6.14

--- TRIBblix-elasticsearch/build.sh
+++ TRIBblix-elasticsearch/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball -T TRIBblix-elasticsearch elasticsearch-5.6.13
+# also needs a copy of the jna distribution - see the fixit script
+#
+${THOME}/build/pkg_tarball -T TRIBblix-elasticsearch elasticsearch-5.6.14
