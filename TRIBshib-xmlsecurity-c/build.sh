f0f65eff0cce7a707def1ae09ba50d3b63da3b3e
Peter Tribble <peter.tribble@gmail.com>
2018-12-24 19:49:53 +0000

    Update shibboleth-sp 3.0.3 xmltooling 3.0.3 xerces-c 3.2.2 xml-security 2.0.2

--- TRIBshib-xmlsecurity-c/build.sh
+++ TRIBshib-xmlsecurity-c/build.sh
@@ -2,5 +2,5 @@
 #
 # shibboleth stack requires boost ssl curl apache
 #
-env PKG_CONFIG_PATH=/opt/tribblix/shibboleth-sp/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild xml-security-c-2.0.1 -P /opt/tribblix/shibboleth-sp -C "--disable-static --without-xalan"
-${THOME}/build/genpkg TRIBshib-xmlsecurity-c xml-security-c-2.0.1
+env PKG_CONFIG_PATH=/opt/tribblix/shibboleth-sp/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild xml-security-c-2.0.2 -P /opt/tribblix/shibboleth-sp -C "--disable-static --without-xalan"
+${THOME}/build/genpkg TRIBshib-xmlsecurity-c xml-security-c-2.0.2
