2e5cdc312a1ccf7fd5de811037ca810f3dffc2c0
Peter Tribble <peter.tribble@gmail.com>
2018-12-26 17:53:43 +0000

    Add nicstat

---
+++ TRIBnicstat/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+${THOME}/build/dobuild nicstat-master
+rm -fr /tmp/nii
+mkdir -p /tmp/nii/usr/bin
+mkdir -p /tmp/nii/usr/share/man/man1
+cd nicstat-master
+cp `./nicstat.sh --bin-name` /tmp/nii/usr/bin/nicstat
+cp nicstat.1 /tmp/nii/usr/share/man/man1
+cd ..
+${THOME}/build/create_pkg TRIBnicstat /tmp/nii
+rm -fr /tmp/nii
