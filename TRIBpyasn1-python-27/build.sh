1505078e0db2377e2cbbad3e1d3d3c99fe68ffeb
Peter Tribble <peter.tribble@gmail.com>
2019-01-07 08:59:47 +0000

    Update pyasn1 0.4.5

--- TRIBpyasn1-python-27/build.sh
+++ TRIBpyasn1-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyasn1-0.4.4
-cd pyasn1-0.4.4
+${THOME}/build/unpack pyasn1-0.4.5
+cd pyasn1-0.4.5
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBpyasn1-python-37/build.sh
+++ TRIBpyasn1-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyasn1-0.4.4
-cd pyasn1-0.4.4
+${THOME}/build/unpack pyasn1-0.4.5
+cd pyasn1-0.4.5
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
