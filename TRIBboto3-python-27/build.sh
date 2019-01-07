9118a1b7b91e847b98d34f52e5d9a4b6ae93a72f
Peter Tribble <peter.tribble@gmail.com>
2019-01-07 09:20:51 +0000

    Update boto3 1.9.74

--- TRIBboto3-python-27/build.sh
+++ TRIBboto3-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.9.33
-cd boto3-1.9.33
+${THOME}/build/unpack boto3-1.9.74
+cd boto3-1.9.74
 python setup.py build
 rm -fr /tmp/sut
 python setup.py install --root=/tmp/sut
--- TRIBboto3-python-37/build.sh
+++ TRIBboto3-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.9.33
-cd boto3-1.9.33
+${THOME}/build/unpack boto3-1.9.74
+cd boto3-1.9.74
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/sut
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/sut
