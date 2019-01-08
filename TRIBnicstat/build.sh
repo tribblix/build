ebcf7e2c4db18ad1bd69f73409f15d1d45904b73
Peter Tribble <peter.tribble@gmail.com>
2019-01-08 10:09:21 +0000

    Force nicstat to use gcc

--- TRIBnicstat/build.sh
+++ TRIBnicstat/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild nicstat-master
+${THOME}/build/dobuild nicstat-master -M CC=gcc
 rm -fr /tmp/nii
 mkdir -p /tmp/nii/usr/bin
 mkdir -p /tmp/nii/usr/share/man/man1
