6920d199325448088e7de5ca154b2cd0e5ecf1e3
Peter Tribble <peter.tribble@gmail.com>
2019-01-26 15:01:06 +0000

    Update mysql 5.6.43

--- TRIBblix-mysql-56/build.sh
+++ TRIBblix-mysql-56/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack mysql-5.6.42
-cd mysql-5.6.42
+${THOME}/build/unpack mysql-5.6.43
+cd mysql-5.6.43
 cmake . -DCMAKE_BUILD_TYPE=Release -DBUILD_CONFIG=mysql_release -DFEATURE_SET=community -DWITH_EMBEDDED_SERVER=OFF -DENABLE_DTRACE=OFF -DCMAKE_INSTALL_PREFIX=/opt/tribblix/mysql -DFORCE_UNSUPPORTED_COMPILER=true
 gmake -j 4
 ${THOME}/build/genpkg TRIBblix-mysql-56
