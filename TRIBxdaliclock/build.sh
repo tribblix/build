bb42c9619e90a436dd903101fa3eea685728594a
Peter Tribble <peter.tribble@gmail.com>
2019-02-04 18:28:16 +0000

    Update xdaliclock 2.44

--- TRIBxdaliclock/build.sh
+++ TRIBxdaliclock/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xdaliclock-2.43 -S X11
-cd xdaliclock-2.43/X11
+${THOME}/build/dobuild xdaliclock-2.44 -S X11
+cd xdaliclock-2.44/X11
 rm -fr /tmp/xd
 mkdir -p /tmp/xd/usr/bin /tmp/xd/usr/share/man/man1
 gmake install install_prefix=/tmp/xd
