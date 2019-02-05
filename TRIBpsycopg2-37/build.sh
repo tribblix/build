2f76a86e1fe30dc703ab0ea06c08f49db82ad9a1
Peter Tribble <peter.tribble@gmail.com>
2019-02-05 13:43:10 +0000

    Update psycopg 2.7.7, build against postgres11

--- TRIBpsycopg2-37/build.sh
+++ TRIBpsycopg2-37/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psycopg2-2.7.6
-cd psycopg2-2.7.6
-env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib /usr/versions/python-3.7/bin/python3 setup.py build
+${THOME}/build/unpack psycopg2-2.7.7
+cd psycopg2-2.7.7
+env PATH=/opt/tribblix/postgres11/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres11/lib /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
-env PATH=/opt/tribblix/postgres96/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres96/lib /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+env PATH=/opt/tribblix/postgres11/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres11/lib /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBpsycopg2-37 /tmp/pil
 cd ..
 rm -fr /tmp/pil
