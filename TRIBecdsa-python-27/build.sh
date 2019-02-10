7612303179eff944c3107a678e4ff8c7949edbc6
Peter Tribble <peter.tribble@gmail.com>
2019-02-10 21:58:37 +0000

    Add ecdsa 0.13

---
+++ TRIBecdsa-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack ecdsa-0.13
+cd ecdsa-0.13
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBecdsa-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
