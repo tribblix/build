8cb723a8e287ba532b840ea9da0def35a6724c47
Peter Tribble <peter.tribble@gmail.com>
2019-02-10 20:53:37 +0000

    Build motif in 64-bit

--- TRIBmotif/build.sh
+++ TRIBmotif/build.sh
@@ -2,5 +2,8 @@
 #
 # 2.3.5 and earlier failed with parallel make and needed -J 1
 #
-${THOME}/build/dobuild motif-2.3.8 -L
+# force posix readdir_r, required as that's the only readdir_r
+# available in the LP64 environment
+#
+env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS ${THOME}/build/dobuild -64 motif-2.3.8 -L
 ${THOME}/build/genpkg TRIBmotif motif-2.3.8
