9e13f4234a8391092ab8d76b9429c77593065dc3
Peter Tribble <peter.tribble@gmail.com>
2019-02-12 09:44:19 +0000

    Update Django 1.11.20 2.1.7

--- TRIBdjango-python-27/build.sh
+++ TRIBdjango-python-27/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-1.11.18
-cd Django-1.11.18
+${THOME}/build/unpack Django-1.11.20
+cd Django-1.11.20
 rm -fr /tmp/p2a
 python setup.py install --root=/tmp/p2a
 ${THOME}/build/create_pkg TRIBdjango-python-27 /tmp/p2a
