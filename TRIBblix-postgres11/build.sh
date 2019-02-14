3f515f59a6febf861a2e0fcf151df6f3277961f4
Peter Tribble <peter.tribble@gmail.com>
2019-02-14 14:17:57 +0000

    Update postgres 9.6.12 10.7 11.2

--- TRIBblix-postgres11/build.sh
+++ TRIBblix-postgres11/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 11
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.1 -C "--without-readline --with-perl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.1
+${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.2 -C "--without-readline --with-perl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.2
--- TRIBblix-postgres96/build.sh
+++ TRIBblix-postgres96/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 9.6
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres96 postgresql-9.6.11 -C "--without-readline --with-perl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres96 postgresql-9.6.11
+${THOME}/build/dobuild -P /opt/tribblix/postgres96 postgresql-9.6.12 -C "--without-readline --with-perl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres96 postgresql-9.6.12
