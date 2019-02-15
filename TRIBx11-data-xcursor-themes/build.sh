4b17a1a75b8f6a4bba0bfddb7af7a377df78db5f
Peter Tribble <peter.tribble@gmail.com>
2019-02-15 13:10:56 +0000

    Update xcursor-themes 1.0.6

--- TRIBx11-data-xcursor-themes/build.sh
+++ TRIBx11-data-xcursor-themes/build.sh
@@ -2,5 +2,5 @@
 #
 # requires xcursorgen as a build dependency
 #
-${THOME}/build/dobuild xcursor-themes-1.0.5
-${THOME}/build/genpkg TRIBx11-data-xcursor-themes xcursor-themes-1.0.5
+${THOME}/build/dobuild xcursor-themes-1.0.6
+${THOME}/build/genpkg TRIBx11-data-xcursor-themes xcursor-themes-1.0.6
