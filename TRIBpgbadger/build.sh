a1b5f4f0f5a562dc13b386c90b02902a103888c4
Peter Tribble <peter.tribble@gmail.com>
2019-02-15 13:30:02 +0000

    Update pgbadger 10.3

--- TRIBpgbadger/build.sh
+++ TRIBpgbadger/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pgbadger-10.2
-cd pgbadger-10.2
+${THOME}/build/unpack pgbadger-10.3
+cd pgbadger-10.3
 # you need to run a make to create the manpage
 perl Makefile.PL
 sed -i 's:sed -i.bak:gsed -i.bak:' Makefile
