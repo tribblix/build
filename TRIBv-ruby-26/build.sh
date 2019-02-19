42a57287f0e8308382c9bbb4288543950e904a11
Peter Tribble <peter.tribble@gmail.com>
2019-02-19 15:38:50 +0000

    Add ruby 2.6.1

---
+++ TRIBv-ruby-26/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# just can't handle LD_OPTIONS
+#
+${THOME}/build/dobuild ruby-2.6.1 -P /usr/versions/ruby-2.6 -C ac_cv_func_dl_iterate_phdr=no -L
+${THOME}/build/genpkg TRIBv-ruby-26 ruby-2.6.1
