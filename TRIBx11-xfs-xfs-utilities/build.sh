7f449ddc07e7dd9476e3f5839c976404d1030747
Peter Tribble <peter.tribble@gmail.com>
2019-02-20 21:28:11 +0000

    Update xfsinfo 1.0.6 fslsfonts 1.0.5 fstobdf 1.0.6

--- TRIBx11-xfs-xfs-utilities/build.sh
+++ TRIBx11-xfs-xfs-utilities/build.sh
@@ -3,10 +3,8 @@
 # this is a merge of several smaller packages, that was the way
 # it was inherited.
 #
-# a couple of these are slightly downrev due to dependencies
-#
-${THOME}/build/dobuild fslsfonts-1.0.4
-${THOME}/build/dobuild fstobdf-1.0.5
+${THOME}/build/dobuild fslsfonts-1.0.5
+${THOME}/build/dobuild fstobdf-1.0.6
 ${THOME}/build/dobuild showfont-1.0.5
-${THOME}/build/dobuild xfsinfo-1.0.5
-${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.4 fstobdf-1.0.5 showfont-1.0.5 xfsinfo-1.0.5
+${THOME}/build/dobuild xfsinfo-1.0.6
+${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.5 fstobdf-1.0.6 showfont-1.0.5 xfsinfo-1.0.6
