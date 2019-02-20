73a385309fb0e888c0c92495bafd47dd86ba20d6
Peter Tribble <peter.tribble@gmail.com>
2019-02-20 12:50:22 +0000

    Update xev 1.2.3

--- TRIBx11-diagnostic-xev/build.sh
+++ TRIBx11-diagnostic-xev/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xev-1.2.2
-${THOME}/build/genpkg TRIBx11-diagnostic-xev xev-1.2.2
+${THOME}/build/dobuild xev-1.2.3
+${THOME}/build/genpkg TRIBx11-diagnostic-xev xev-1.2.3
