dee0e4850277962643157ab05dd61be9786b1618
Peter Tribble <peter.tribble@gmail.com>
2019-02-20 12:48:10 +0000

    Update xclock 1.0.8

--- TRIBx11-xclock/build.sh
+++ TRIBx11-xclock/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xclock-1.0.7
-${THOME}/build/genpkg TRIBx11-xclock xclock-1.0.7
+${THOME}/build/dobuild xclock-1.0.8
+${THOME}/build/genpkg TRIBx11-xclock xclock-1.0.8
