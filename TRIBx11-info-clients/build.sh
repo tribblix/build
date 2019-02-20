6a39870b12400f220283472e85c6c8c64c854da6
Peter Tribble <peter.tribble@gmail.com>
2019-02-20 21:41:01 +0000

    Update xwininfo 1.1.4 xvinfo 1.1.4 xlsatoms 1.1.3 xdriinfo 1.0.6 xdpyinfo 1.3.2

--- TRIBx11-info-clients/build.sh
+++ TRIBx11-info-clients/build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #
-# some of these are downrev due to dependencies
-#
-${THOME}/build/dobuild xdpyinfo-1.2.0
-${THOME}/build/dobuild xdriinfo-1.0.5
-${THOME}/build/dobuild xlsatoms-1.1.2
+${THOME}/build/dobuild xdpyinfo-1.3.2
+${THOME}/build/dobuild xdriinfo-1.0.6
+${THOME}/build/dobuild xlsatoms-1.1.3
 ${THOME}/build/dobuild xlsclients-1.1.4
 ${THOME}/build/dobuild xlsfonts-1.0.6
-${THOME}/build/dobuild xvinfo-1.1.2
-${THOME}/build/dobuild xwininfo-1.1.3
-${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.2.0 xdriinfo-1.0.5 xlsatoms-1.1.2 xlsclients-1.1.4 xlsfonts-1.0.6 xvinfo-1.1.2 xwininfo-1.1.3
+${THOME}/build/dobuild xvinfo-1.1.4
+${THOME}/build/dobuild xwininfo-1.1.4
+${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.3.2 xdriinfo-1.0.6 xlsatoms-1.1.3 xlsclients-1.1.4 xlsfonts-1.0.6 xvinfo-1.1.4 xwininfo-1.1.4
