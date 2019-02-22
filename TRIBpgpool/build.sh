bfa6c97bb263dac3d9366a03f3c1f8b9342ad1f0
Peter Tribble <peter.tribble@gmail.com>
2019-02-22 16:44:27 +0000

    Update pgpool 4.0.3

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,5 @@
 #
 # needs a postconf patch
 #
-env PATH=/opt/tribblix/postgres11/bin:$PATH ${THOME}/build/dobuild pgpool-II-4.0.2 -C "--sysconfdir=/etc"
-${THOME}/build/genpkg TRIBpgpool pgpool-II-4.0.2
+env PATH=/opt/tribblix/postgres11/bin:$PATH ${THOME}/build/dobuild pgpool-II-4.0.3 -C "--sysconfdir=/etc"
+${THOME}/build/genpkg TRIBpgpool pgpool-II-4.0.3
