6aabc6a28865984fe431e69efffafa0bdc9e7c55
Peter Tribble <peter.tribble@gmail.com>
2019-02-24 14:59:40 +0000

    Update libfontenc 1.1.4

--- TRIBx11-libfontenc/build.sh
+++ TRIBx11-libfontenc/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
 # historically this package shipped the encodings too, so continue
-# and historically the encodings weren't gzipped, and OI doesn't either
+# historically the encodings weren't gzipped, and OI doesn't either
 #
-${THOME}/build/dobuild -64 libfontenc-1.1.3
+# the encodings come from https://www.x.org/releases/individual/font/
+#
+${THOME}/build/dobuild -64 libfontenc-1.1.4
 ${THOME}/build/dobuild encodings-1.0.4 -C "--disable-gzip-small-encodings --disable-gzip-large-encodings"
-${THOME}/build/genpkg TRIBx11-libfontenc libfontenc-1.1.3 encodings-1.0.4
+${THOME}/build/genpkg TRIBx11-libfontenc libfontenc-1.1.4 encodings-1.0.4
