744f7a7a9ed7a6e37fe6bb3937b83778208280cd
Peter Tribble <peter.tribble@gmail.com>
2019-02-27 10:19:25 +0000

    Fix flex build so it finds m4 sanely

--- TRIBflex/build.sh
+++ TRIBflex/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64 flex-2.6.4
+# mustn't have /usr/gnu/bin in the PATH
+# if you do, it will pick "m4" rather than "gm4"
+# which will cause trouble later
+#
+${THOME}/build/dobuild -64 flex-2.6.4
 ${THOME}/build/genpkg TRIBflex flex-2.6.4
