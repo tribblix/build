0a8e0b58f8ae20aa47d5976b85d2eb5ff7ee8c4d
Peter Tribble <peter.tribble@gmail.com>
2019-02-27 19:59:11 +0000

    Update minio client

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -2,9 +2,14 @@
 #
 mkdir ~/ud/c
 cd ~/ud/c
-env GOPATH=`pwd` /usr/versions/go-1.8/bin/go get -u github.com/minio/mc
+env GOPATH=`pwd` /usr/versions/go-1.10/bin/go get -u github.com/minio/mc
+sed -i 's:darwin:darwin solaris:' src/github.com/minio/mc/vendor/github.com/minio/minio/pkg/cpu/counter_other.go
+env GOPATH=`pwd` /usr/versions/go-1.10/bin/go get -u github.com/minio/mc
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
+mkdir -p /tmp/ee/usr/share/minio-client
 cp bin/mc /tmp/ee/usr/bin
+cp src/github.com/minio/mc/README.md /tmp/ee/usr/share/minio-client
+cp src/github.com/minio/mc/LICENSE /tmp/ee/usr/share/minio-client
 ${THOME}/build/create_pkg TRIBminio-client /tmp/ee
 rm -fr /tmp/ee
