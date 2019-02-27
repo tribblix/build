0ca6ff1a5a216b95a9e11d18b039e451da3b50fa
Peter Tribble <peter.tribble@gmail.com>
2019-02-27 21:54:00 +0000

    Update ctwm 4.0.2

--- TRIBctwm/build.sh
+++ TRIBctwm/build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ctwm-3.8.2
-cd ctwm-3.8.2
-cat Imakefile.local-template | sed 's:#define GNOME:#undef GNOME:' > Imakefile.local
-xmkmf -a
-gsed -i s:/twm:/ctwm: Makefile
-gmake CC=gcc CDEBUGFLAGS=-O MAPFILES_FOR_ALL= MAPFILES_FOR_PROGS=
-rm -fr /tmp/c
-gmake install install.man INSTALL=ginstall DESTDIR=/tmp/c
-mv /tmp/c/usr/share/man/man1/ctwm.1x /tmp/c/usr/share/man/man1/ctwm.1
-${THOME}/build/create_pkg TRIBctwm /tmp/c
-rm -fr /tmp/c
+${THOME}/build/unpack ctwm-4.0.2
+cd ctwm-4.0.2
+sed -i s:std=c99:std=gnu99: cmake_files/compiler_feature_checks.cmake
+gmake CMAKE_EXTRAS="-DCMAKE_INSTALL_PREFIX=/usr"
+#
+# this is an entertaining abuse of genpkg
+#
+${THOME}/build/genpkg TRIBctwm build
