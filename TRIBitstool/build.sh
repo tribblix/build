d465a00e1171f7b802fc384d10faf407ea9e59d7
Peter Tribble <peter.tribble@gmail.com>
2019-03-02 15:53:26 +0000

    Update itstool 2.0.5

--- TRIBitstool/build.sh
+++ TRIBitstool/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild itstool-2.0.2
-${THOME}/build/genpkg TRIBitstool itstool-2.0.2
+${THOME}/build/dobuild itstool-2.0.5
+${THOME}/build/genpkg TRIBitstool itstool-2.0.5
