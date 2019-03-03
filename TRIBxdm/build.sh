a93f32f281dfa883c6c50ca25bc8e400c2728573
Peter Tribble <peter.tribble@gmail.com>
2019-03-03 12:51:40 +0000

    Update xdm 1.1.12

--- TRIBxdm/build.sh
+++ TRIBxdm/build.sh
@@ -2,5 +2,5 @@
 #
 # follow the OI build
 #
-env DEF_SERVER=/usr/bin/X DEF_USER_PATH=/usr/bin:/usr/sbin DEF_SYSTEM_PATH=/usr/bin:/usr/sbin ${THOME}/build/dobuild xdm-1.1.11 -C "--sysconfdir=/etc --localstatedir=/var --with-xdmconfigdir=/etc/X11/xdm --with-piddir=/var/run/xdm --with-authdir=/var/run/xdm --bindir=/usr/sbin --with-xrdb=/usr/bin/xrdb"
-${THOME}/build/genpkg TRIBxdm xdm-1.1.11
+env DEF_SERVER=/usr/bin/X DEF_USER_PATH=/usr/bin:/usr/sbin DEF_SYSTEM_PATH=/usr/bin:/usr/sbin ${THOME}/build/dobuild xdm-1.1.12 -C "--sysconfdir=/etc --localstatedir=/var --with-xdmconfigdir=/etc/X11/xdm --with-piddir=/var/run/xdm --with-authdir=/var/run/xdm --bindir=/usr/sbin --with-xrdb=/usr/bin/xrdb"
+${THOME}/build/genpkg TRIBxdm xdm-1.1.12
