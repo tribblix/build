a90aa126ecaf141841e95393cb920f49e64d3af4
Peter Tribble <peter.tribble@gmail.com>
2019-03-09 13:39:15 +0000

    Update privoxy 3.0.28

--- TRIBblix-privoxy/build.sh
+++ TRIBblix-privoxy/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # rename the tarball
-# mv privoxy-3.0.26-stable-src.tar.gz privoxy-3.0.26-stable.tar.gz
+# mv privoxy-3.0.28-stable-src.tar.gz privoxy-3.0.28-stable.tar.gz
 #
-${THOME}/build/dobuild privoxy-3.0.26-stable -P /opt/tribblix/privoxy
-${THOME}/build/genpkg TRIBblix-privoxy privoxy-3.0.26-stable
+${THOME}/build/dobuild privoxy-3.0.28-stable -P /opt/tribblix/privoxy
+${THOME}/build/genpkg TRIBblix-privoxy privoxy-3.0.28-stable
