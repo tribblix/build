465de70e9bfe4d30b87fd03397e83a3fe6109149
Peter Tribble <peter.tribble@gmail.com>
2019-03-11 11:03:13 +0000

    Update libFS 1.0.8

--- TRIBx11-libfs/build.sh
+++ TRIBx11-libfs/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libFS-1.0.7
-${THOME}/build/genpkg TRIBx11-libfs libFS-1.0.7
+${THOME}/build/dobuild -64 libFS-1.0.8
+${THOME}/build/genpkg TRIBx11-libfs libFS-1.0.8
