823128a09ff3f7e93e58f127531bb031920c0c19
Peter Tribble <peter.tribble@gmail.com>
2019-03-13 20:44:47 +0000

    Update libXcomposite 0.4.5

--- TRIBx11-libxcomposite/build.sh
+++ TRIBx11-libxcomposite/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXcomposite-0.4.4
-${THOME}/build/genpkg TRIBx11-libxcomposite libXcomposite-0.4.4
+${THOME}/build/dobuild -64 libXcomposite-0.4.5
+${THOME}/build/genpkg TRIBx11-libxcomposite libXcomposite-0.4.5
