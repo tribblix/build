78e18de3b01e6de55b5681a00dd81f9f3eb1c004
Peter Tribble <peter.tribble@gmail.com>
2019-03-13 20:49:43 +0000

    Update libXdamage 1.1.5

--- TRIBx11-libxdamage/build.sh
+++ TRIBx11-libxdamage/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXdamage-1.1.4
-${THOME}/build/genpkg TRIBx11-libxdamage libXdamage-1.1.4
+${THOME}/build/dobuild -64 libXdamage-1.1.5
+${THOME}/build/genpkg TRIBx11-libxdamage libXdamage-1.1.5
