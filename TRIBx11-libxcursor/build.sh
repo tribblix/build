a57679d69851da2ef99ee95b86960288d712d909
Peter Tribble <peter.tribble@gmail.com>
2019-03-14 09:16:56 +0000

    Update libXcursor 1.2.0

--- TRIBx11-libxcursor/build.sh
+++ TRIBx11-libxcursor/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXcursor-1.1.15
-${THOME}/build/genpkg TRIBx11-libxcursor libXcursor-1.1.15
+${THOME}/build/dobuild -64 libXcursor-1.2.0
+${THOME}/build/genpkg TRIBx11-libxcursor libXcursor-1.2.0
