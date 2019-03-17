f5ca99861eb5b0ed20689de4ddc39f298a35b592
Peter Tribble <peter.tribble@gmail.com>
2019-03-17 19:29:18 +0000

    Update libXvMC 1.0.11

--- TRIBx11-libxvmc/build.sh
+++ TRIBx11-libxvmc/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXvMC-1.0.10
-${THOME}/build/genpkg TRIBx11-libxvmc libXvMC-1.0.10
+${THOME}/build/dobuild -64 libXvMC-1.0.11
+${THOME}/build/genpkg TRIBx11-libxvmc libXvMC-1.0.11
