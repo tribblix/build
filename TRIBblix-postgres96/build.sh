98f082bc3c4e693cc1d417a99ae39412ee123b2f
Peter Tribble <peter.tribble@gmail.com>
2019-03-17 10:29:52 +0000

    Deprecate postgres 9.6

--- TRIBblix-postgres96/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# postgres 9.6
-#
-${THOME}/build/dobuild -P /opt/tribblix/postgres96 postgresql-9.6.12 -C "--without-readline --with-perl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres96 postgresql-9.6.12
--- TRIBblix-repmgr96/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# this is a postgres extension, so it's specific to the postgres version
-#
-# the tarball name doesn't match the version number
-# mv repmgr-4.2.tar.gz repmgr-4.2.0.tar.gz
-#
-${THOME}/build/dobuild repmgr-4.2.0 -C PG_CONFIG=/opt/tribblix/postgres96/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr96 repmgr-4.2.0
