82b120da1c1ec812a7ea013ff76bd3d20ea44b2d
Peter Tribble <peter.tribble@gmail.com>
2019-03-18 13:07:21 +0000

    Update libXdmcp 1.1.3

--- TRIBx11-libxdmcp/build.sh
+++ TRIBx11-libxdmcp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXdmcp-1.1.2
-${THOME}/build/genpkg TRIBx11-libxdmcp libXdmcp-1.1.2
+${THOME}/build/dobuild -64 libXdmcp-1.1.3
+${THOME}/build/genpkg TRIBx11-libxdmcp libXdmcp-1.1.3
