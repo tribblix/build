020259c0459b78f5a36c1624db229b2ffaaa3766
Peter Tribble <peter.tribble@gmail.com>
2019-03-18 16:20:32 +0000

    Update libXmu 1.1.3

--- TRIBx11-libxmu/build.sh
+++ TRIBx11-libxmu/build.sh
@@ -2,5 +2,5 @@
 #
 # needs libtool to build
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/X11/xcms ${THOME}/build/dobuild -64 libXmu-1.1.2
-${THOME}/build/genpkg TRIBx11-libxmu libXmu-1.1.2
+env TRIBBLIX_CFLAGS=-I/usr/include/X11/xcms ${THOME}/build/dobuild -64 libXmu-1.1.3
+${THOME}/build/genpkg TRIBx11-libxmu libXmu-1.1.3
