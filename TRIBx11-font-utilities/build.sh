61029b3baef147906adbfdda97d1dbc5e21dc890
Peter Tribble <peter.tribble@gmail.com>
2019-03-21 09:26:07 +0000

    Update mkfontscale 1.2.1

--- TRIBx11-font-utilities/build.sh
+++ TRIBx11-font-utilities/build.sh
@@ -6,8 +6,9 @@
 # we don't ship /usr/lib/X11/installalias which appears to be
 # a solaris-only internal thing
 #
+# mkfontdir now distributed by mkfontscale
+#
 ${THOME}/build/dobuild bdftopcf-1.1
-${THOME}/build/dobuild mkfontdir-1.0.7
-${THOME}/build/dobuild mkfontscale-1.1.1
+${THOME}/build/dobuild mkfontscale-1.2.1
 ${THOME}/build/dobuild font-util-1.3.1
-${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontdir-1.0.7 mkfontscale-1.1.1 font-util-1.3.1
+${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.1 font-util-1.3.1
