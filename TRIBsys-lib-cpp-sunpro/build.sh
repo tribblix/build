0abb89826515684fb3150a1670170ba3ab49d1b0
Peter Tribble <peter.tribble@gmail.com>
2019-03-24 14:30:52 +0000

    Package sunpro libC ourselves, under a new name

---
+++ TRIBsys-lib-cpp-sunpro/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# separate package for as (as illumos now supplies make)
+# Save the old binaries like so
+# (cd / ; tar cf ${THOME}/build/patches/binary-libC-files.tar -I ${THOME}/build/patches/binary-libC-files.txt)
+# or for sparc
+# (cd / ; tar cf ${THOME}/build/patches/binary-libC-files.sparc.tar -I ${THOME}/build/patches/binary-libC-files.sparc.txt)
+#
+${THOME}/build/genpkg TRIBsys-lib-cpp-sunpro
