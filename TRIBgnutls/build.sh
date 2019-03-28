4e915a8c4704006f2358ca7ef87960acc0cc1f9a
Peter Tribble <peter.tribble@gmail.com>
2019-03-28 15:19:56 +0000

    Update gnutls 3.3.30

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -5,5 +5,5 @@
 # disable tpm because the code isn't even self-consistent and autoconf
 # can't work out what to do on its own
 #
-${THOME}/build/dobuild -64 gnutls-3.3.28 -C "--disable-doc --without-tpm"
-${THOME}/build/genpkg TRIBgnutls gnutls-3.3.28
+${THOME}/build/dobuild -64 gnutls-3.3.30 -C "--disable-doc --without-tpm"
+${THOME}/build/genpkg TRIBgnutls gnutls-3.3.30
