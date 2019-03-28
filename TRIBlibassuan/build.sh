dfb03df622e529064444dd3497e83d0831f9dfc7
Peter Tribble <peter.tribble@gmail.com>
2019-03-28 13:28:14 +0000

    Update libassuan 2.5.3

--- TRIBlibassuan/build.sh
+++ TRIBlibassuan/build.sh
@@ -2,5 +2,5 @@
 #
 # libassuan
 #
-${THOME}/build/dobuild -64 libassuan-2.5.1
-${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.1
+${THOME}/build/dobuild -64 libassuan-2.5.3
+${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.3
