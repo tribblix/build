60160c6c6c7f71e318468aa0b52df8b124c3962b
Peter Tribble <peter.tribble@gmail.com>
2019-03-29 19:06:01 +0000

    Update groff 1.22.4

--- TRIBtext-groff/build.sh
+++ TRIBtext-groff/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu groff-1.22.3 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-groff groff-1.22.3
+${THOME}/build/dobuild -gnu groff-1.22.4 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-groff groff-1.22.4
