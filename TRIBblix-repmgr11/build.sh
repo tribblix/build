aca8063f4a07b549e71edd96b54bf91fd7d0ef87
Peter Tribble <peter.tribble@gmail.com>
2019-04-04 16:22:49 +0100

    Update repmgr 4.3.0

--- TRIBblix-repmgr11/build.sh
+++ TRIBblix-repmgr11/build.sh
@@ -2,8 +2,7 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-# the tarball name doesn't match the version number
-# mv repmgr-4.2.tar.gz repmgr-4.2.0.tar.gz
+# the tarball name might not match the version number
 #
-${THOME}/build/dobuild repmgr-4.2.0 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr11 repmgr-4.2.0
+${THOME}/build/dobuild repmgr-4.3.0 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr11 repmgr-4.3.0
