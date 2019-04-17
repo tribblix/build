260b8e77f7255fef40dcea029681d7a4f42c0eeb
Peter Tribble <peter.tribble@gmail.com>
2019-04-17 11:05:26 +0100

    Enable ssl in postgres builds

--- TRIBblix-postgres10/build.sh
+++ TRIBblix-postgres10/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 10
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.7 -C "--without-readline --with-perl" -M world
+${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.7 -C "--without-readline --with-perl --with-openssl" -M world
 ${THOME}/build/genpkg -I install-world TRIBblix-postgres10 postgresql-10.7
