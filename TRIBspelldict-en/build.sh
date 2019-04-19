b15b5b56078b12937931829500dfd68b97c58b31
Peter Tribble <peter.tribble@gmail.com>
2019-04-19 19:37:59 +0100

    Update spelling dictionaries, fix symlinks

--- TRIBspelldict-en/build.sh
+++ TRIBspelldict-en/build.sh
@@ -8,19 +8,19 @@ cd /tmp/sp-en/usr/share/hunspell
 #
 # download
 #
-wget https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/hunspell-en_CA-2017.01.22.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/hunspell-en_AU-2017.01.22.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/hunspell-en_US-2017.01.22.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/hunspell-en_GB-ise-2017.01.22.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/hunspell-en_GB-ize-2017.01.22.zip
+wget https://sourceforge.net/projects/wordlist/files/speller/2018.04.16/hunspell-en_CA-2018.04.16.zip
+wget https://sourceforge.net/projects/wordlist/files/speller/2018.04.16/hunspell-en_AU-2018.04.16.zip
+wget https://sourceforge.net/projects/wordlist/files/speller/2018.04.16/hunspell-en_US-2018.04.16.zip
+wget https://sourceforge.net/projects/wordlist/files/speller/2018.04.16/hunspell-en_GB-ise-2018.04.16.zip
+wget https://sourceforge.net/projects/wordlist/files/speller/2018.04.16/hunspell-en_GB-ize-2018.04.16.zip
 #
 # unpack
 #
-unzip -q hunspell-en_AU-2017.01.22.zip
-unzip -q hunspell-en_CA-2017.01.22.zip
-unzip -q hunspell-en_GB-ise-2017.01.22.zip
-unzip -q hunspell-en_GB-ize-2017.01.22.zip
-unzip -q hunspell-en_US-2017.01.22.zip
+unzip -q hunspell-en_AU-2018.04.16.zip
+unzip -q hunspell-en_CA-2018.04.16.zip
+unzip -q hunspell-en_GB-ise-2018.04.16.zip
+unzip -q hunspell-en_GB-ize-2018.04.16.zip
+unzip -q hunspell-en_US-2018.04.16.zip
 rm -f *.zip
 #
 # symlinks
@@ -36,7 +36,7 @@ ln -s en_US.aff en_PH.aff
 ln -s en_US.dic en_PH.dic
 foreach alt ( en_AG en_BS en_BW en_BZ en_DK en_GH en_HK en_IE en_IN en_JM en_NA en_NG en_NZ en_SG en_TT en_ZA en_ZW )
 ln -s en_GB.aff ${alt}.aff
-ln -s en_GB.dci ${alt}.dic
+ln -s en_GB.dic ${alt}.dic
 end
 
 ${THOME}/build/create_pkg TRIBspelldict-en /tmp/sp-en
