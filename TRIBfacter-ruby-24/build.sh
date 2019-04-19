d335f685c6a606902d6c83fa664062ad6ade4958
Peter Tribble <peter.tribble@gmail.com>
2019-04-19 13:58:16 +0100

    Move facter to ruby 2.5

--- TRIBfacter-ruby-24/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack facter-2.4.6
-cd facter-2.4.6
-rm -fr /tmp/ffa
-/usr/versions/ruby-2.4/bin/ruby install.rb --destdir=/tmp/ffa
-${THOME}/build/create_pkg TRIBfacter-ruby-24 /tmp/ffa
-rm -fr /tmp/ffa
-cd ..
