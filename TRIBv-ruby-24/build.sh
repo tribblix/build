36451918b17449d1379fa900943bbdfb2cd43fbe
Peter Tribble <peter.tribble@gmail.com>
2019-04-19 13:50:02 +0100

    Deprecate ruby 2.4

--- TRIBv-ruby-24/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# just can't handle LD_OPTIONS
-#
-${THOME}/build/dobuild ruby-2.4.6 -P /usr/versions/ruby-2.4 -C ac_cv_func_dl_iterate_phdr=no -L
-${THOME}/build/genpkg TRIBv-ruby-24 ruby-2.4.6
