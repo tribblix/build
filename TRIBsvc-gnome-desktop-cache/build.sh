c0c3e86ab69714254efce401708ab4c32dcaa92b
Peter Tribble <peter.tribble@gmail.com>
2019-04-20 12:29:16 +0100

    Update desktop-cache

--- TRIBsvc-gnome-desktop-cache/build.sh
+++ TRIBsvc-gnome-desktop-cache/build.sh
@@ -2,6 +2,6 @@
 #
 rm -fr /tmp/ts
 mkdir /tmp/ts
-(cd ${THOME}/desktop-cache ; tar cf - usr lib var) | ( cd /tmp/ts ; tar xf -)
+(cd ${THOME}/desktop-cache ; tar cf - lib var) | ( cd /tmp/ts ; tar xf -)
 ${THOME}/build/create_pkg TRIBsvc-gnome-desktop-cache /tmp/ts
 rm -fr /tmp/ts
