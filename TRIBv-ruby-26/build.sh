2b8328f8b55da904756c330b8ba9aa057a89816e
Peter Tribble <peter.tribble@gmail.com>
2019-04-22 12:14:22 +0100

    Update ruby 2.6.3

--- TRIBv-ruby-26/build.sh
+++ TRIBv-ruby-26/build.sh
@@ -2,5 +2,5 @@
 #
 # just can't handle LD_OPTIONS
 #
-${THOME}/build/dobuild ruby-2.6.2 -P /usr/versions/ruby-2.6 -C ac_cv_func_dl_iterate_phdr=no -L
-${THOME}/build/genpkg TRIBv-ruby-26 ruby-2.6.2
+${THOME}/build/dobuild ruby-2.6.3 -P /usr/versions/ruby-2.6 -C ac_cv_func_dl_iterate_phdr=no -L
+${THOME}/build/genpkg TRIBv-ruby-26 ruby-2.6.3
