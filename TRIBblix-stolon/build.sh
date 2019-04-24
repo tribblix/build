7f2b9302d6fc14f0da7bbf7ee3770d97e8589972
Peter Tribble <peter.tribble@gmail.com>
2019-04-24 16:26:50 +0100

    Add stolon

---
+++ TRIBblix-stolon/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# needs go 1.11
+#
+mkdir s2
+cd s2
+#
+# a little clunky, use go get to download the source to the right place
+# and then the build script to actually do the compile
+#
+env GOPATH=`pwd` PATH=/usr/versions/go-1.11/bin:$PATH go get -v github.com/sorintlab/stolon
+env GOPATH=`pwd` PATH=/usr/versions/go-1.11/bin:$PATH STOLON_VERSION=0.13.0 src/github.com/sorintlab/stolon/build
+
+rm -fr /tmp/ee
+mkdir -p /tmp/ee/opt/tribblix/stolon/bin
+cp src/github.com/sorintlab/stolon/bin/stolon* /tmp/ee/opt/tribblix/stolon/bin
+cp -R src/github.com/sorintlab/stolon/doc /tmp/ee/opt/tribblix/stolon
+cp src/github.com/sorintlab/stolon/README.md /tmp/ee/opt/tribblix/stolon
+${THOME}/build/create_pkg TRIBblix-stolon /tmp/ee
+rm -fr /tmp/ee
