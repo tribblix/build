77389aa826da7b050206ed0efbbca62670d79e9c
Peter Tribble <peter.tribble@gmail.com>
2019-04-25 17:13:15 +0100

    Add kazoo 2.6.1

---
+++ TRIBkazoo-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack kazoo-2.6.1
+cd kazoo-2.6.1
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBkazoo-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
