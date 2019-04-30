0124fe0f2895a6b2e830f6b4f6ccb188ada7dac7
Peter Tribble <peter.tribble@gmail.com>
2019-04-30 14:27:53 +0100

    Bump gtk3 to 3.16.7

--- TRIBlib-desktop-gtk3/build.sh
+++ TRIBlib-desktop-gtk3/build.sh
@@ -2,8 +2,10 @@
 #
 # zap install TRIBlib-desktop-atk-bridge
 # zap install TRIBlib-desktop-at-spi2
+# zap install TRIBx11-libepoxy (as of 3.16.7)
 #
 # newer versions require updates to cairo and/or pango
+# 3.18.9 requires glib to be updated to 2.45.8 or later
 #
-${THOME}/build/dobuild -64 gtk+-3.14.15 -C "--disable-introspection --disable-cups"
-${THOME}/build/genpkg TRIBlib-desktop-gtk3 gtk+-3.14.15
+${THOME}/build/dobuild -64 -gnu gtk+-3.16.7 -C "--disable-introspection --disable-cups"
+${THOME}/build/genpkg TRIBlib-desktop-gtk3 gtk+-3.16.7
