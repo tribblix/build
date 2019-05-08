7ee96b38f1a31c03a37f59fc778f5c4a6765f230
Peter Tribble <peter.tribble@gmail.com>
2019-05-08 09:26:07 +0100

    Update coreutils 8.31

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu coreutils-8.30 -P /usr/gnu -C "--without-gmp gl_cv_host_operating_system=illumos"
-${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.30
+${THOME}/build/dobuild -64 -gnu coreutils-8.31 -P /usr/gnu -C "--without-gmp gl_cv_host_operating_system=illumos"
+${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.31
