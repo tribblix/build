984f2e80fa8a0872ca792818f7eafeab9d1000f0
Peter Tribble <peter.tribble@gmail.com>
2019-05-08 08:33:11 +0100

    Update bison 3.3.2

--- TRIBbison/build.sh
+++ TRIBbison/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 bison-3.0.5
-${THOME}/build/genpkg TRIBbison bison-3.0.5
+${THOME}/build/dobuild -64 bison-3.3.2
+${THOME}/build/genpkg TRIBbison bison-3.3.2
