5bdf808ff110ae6f03f7d5b24307abce8037f45e
Peter Tribble <peter.tribble@gmail.com>
2019-05-10 08:11:01 +0100

    Update postgres 10.8 11.3

--- TRIBblix-postgres10/build.sh
+++ TRIBblix-postgres10/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 10
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.7 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres10 postgresql-10.7
+${THOME}/build/dobuild -P /opt/tribblix/postgres10 postgresql-10.8 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres10 postgresql-10.8
--- TRIBblix-postgres11/build.sh
+++ TRIBblix-postgres11/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 11
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.2 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.2
+${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.3 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.3
