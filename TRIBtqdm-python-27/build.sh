62af0bf14e273c669d9a0e0dc1961392eeb4801a
Peter Tribble <peter.tribble@gmail.com>
2019-05-11 19:15:58 +0100

    Add tqdm 4.31.1

---
+++ TRIBtqdm-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack tqdm-4.31.1
+cd tqdm-4.31.1
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBtqdm-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
