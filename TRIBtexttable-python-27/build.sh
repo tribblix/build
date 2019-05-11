e3079cf8473bb4823b1676aa9f5d4253f05ad6d9
Peter Tribble <peter.tribble@gmail.com>
2019-05-11 19:08:50 +0100

    Add texttable 1.6.1

---
+++ TRIBtexttable-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack texttable-1.6.1
+cd texttable-1.6.1
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBtexttable-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
