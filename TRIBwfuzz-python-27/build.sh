0d3f3ee4362d96958314fb3587bb04070a780d5d
Peter Tribble <peter.tribble@gmail.com>
2019-05-16 13:55:38 +0100

    Add wfuzz 2.4

---
+++ TRIBwfuzz-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack wfuzz-2.4
+cd wfuzz-2.4
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBwfuzz-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
