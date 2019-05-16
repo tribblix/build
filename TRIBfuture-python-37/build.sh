10645b6e30e663241f5d5cecfb48f78456ba08e2
Peter Tribble <peter.tribble@gmail.com>
2019-05-16 13:53:12 +0100

    Add future 0.17.1

---
+++ TRIBfuture-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack future-0.17.1
+cd future-0.17.1
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBfuture-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
