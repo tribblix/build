05a668d2137aec45f5b331d5b577564e96492fa9
Peter Tribble <peter.tribble@gmail.com>
2019-05-30 08:47:38 +0100

    Update openssl 1.1.1c

--- TRIBv-openssl11/build.sh
+++ TRIBv-openssl11/build.sh
@@ -4,8 +4,8 @@
 # so we can ignore any of the illumos-specific tweaks
 # inherited from Solaris and just do a vanilla build
 #
-${THOME}/build/unpack -64 openssl-1.1.1b
-cd openssl-1.1.1b
+${THOME}/build/unpack -64 openssl-1.1.1c
+cd openssl-1.1.1c
 ./Configure solaris-x86-gcc shared threads zlib --api=1.0.0 --prefix=/usr/versions/openssl11
 env LD_RUN_PATH=/usr/versions/openssl11/lib gmake -j 4
 cd ..
@@ -13,7 +13,7 @@ cd ..
 # you might have thought that asking for a solaris64-x86_64 build
 # would actually do the right thing, but no ...
 #
-cd openssl-1.1.1b-64bit
+cd openssl-1.1.1c-64bit
 env __CNF_CFLAGS=-m64 __CNF_LDFLAGS=-m64 ./Configure solaris64-x86_64-gcc shared threads zlib --api=1.0.0 --prefix=/usr/versions/openssl11 --libdir=lib/amd64
 env LD_RUN_PATH=/usr/versions/openssl11/lib/amd64 gmake -j 4
 cd ..
@@ -21,7 +21,7 @@ cd ..
 #
 # much easier now install understands DESTDIR
 #
-${THOME}/build/genpkg TRIBv-openssl11 openssl-1.1.1b
+${THOME}/build/genpkg TRIBv-openssl11 openssl-1.1.1c
 
 #
 # The sparc configure steps are:
