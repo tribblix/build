80cb2e9106b44dd6e865ec500549aab14d5b7cc5
Peter Tribble <peter.tribble@gmail.com>
2019-06-06 07:41:28 +0100

    Update harfbuzz 1.9.0

--- TRIBharfbuzz/build.sh
+++ TRIBharfbuzz/build.sh
@@ -2,5 +2,5 @@
 #
 # Rebuild harfbuzz, with icu, and 64-bit (which it used to have anyway)
 zap install TRIBlibicu
-${THOME}/build/dobuild -64 harfbuzz-0.9.42
-${THOME}/build/genpkg TRIBharfbuzz harfbuzz-0.9.42
+${THOME}/build/dobuild -64 harfbuzz-1.9.0
+${THOME}/build/genpkg TRIBharfbuzz harfbuzz-1.9.0
