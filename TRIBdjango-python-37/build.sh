1e72e574bb4bd30f333482a55cd54ea64ac9c756
Peter Tribble <peter.tribble@gmail.com>
2019-06-06 08:20:37 +0100

    Update Django 2.2.2 1.11.21

--- TRIBdjango-python-37/build.sh
+++ TRIBdjango-python-37/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-1.11.20
-cd Django-1.11.20
+${THOME}/build/unpack Django-1.11.21
+cd Django-1.11.21
 rm -fr /tmp/p3a
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango-python-37 /tmp/p3a
