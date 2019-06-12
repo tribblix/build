c82a01337d871463a788521f31fc84518455bd84
Peter Tribble <peter.tribble@gmail.com>
2019-06-12 10:36:03 +0100

    Update xcalc 1.1.0

--- TRIBx11-xcalc/build.sh
+++ TRIBx11-xcalc/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xcalc-1.0.7
-${THOME}/build/genpkg TRIBx11-xcalc xcalc-1.0.7
+${THOME}/build/dobuild xcalc-1.1.0
+${THOME}/build/genpkg TRIBx11-xcalc xcalc-1.1.0
