70200f1e11d8aa4a6ce422c90f93e9eeb5371b32
Peter Tribble <peter.tribble@gmail.com>
2019-06-12 10:44:13 +0100

    Update sessreg 1.1.2

--- TRIBx11-session-sessreg/build.sh
+++ TRIBx11-session-sessreg/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild sessreg-1.0.8
-${THOME}/build/genpkg TRIBx11-session-sessreg sessreg-1.0.8
+${THOME}/build/dobuild sessreg-1.1.2
+${THOME}/build/genpkg TRIBx11-session-sessreg sessreg-1.1.2
