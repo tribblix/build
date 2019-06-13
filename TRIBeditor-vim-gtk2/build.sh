0155b30aa2df5777aa28a1db4d73490b6680cc24
Peter Tribble <peter.tribble@gmail.com>
2019-06-13 08:58:38 +0100

    Update vim 8.1.1523

--- TRIBeditor-vim-gtk2/build.sh
+++ TRIBeditor-vim-gtk2/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild vim-8.0.1307 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix"
-${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-8.0.1307
+env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild vim-8.1.1523 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix"
+${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-8.1.1523
 #
 # Athena build looks like:
-# ${THOME}/build/dobuild vim-8.0.1307 -C "--enable-gui=athena --with-compiledby=ptribble@tribblix"
+# ${THOME}/build/dobuild vim-8.1.1523 -C "--enable-gui=athena --with-compiledby=ptribble@tribblix"
 #
