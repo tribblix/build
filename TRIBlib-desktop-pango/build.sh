db01a71e9e0bb42710032c4140ce08d934096f15
Peter Tribble <peter.tribble@gmail.com>
2019-06-20 10:20:38 +0100

    Update pango 1.42.4

--- TRIBlib-desktop-pango/build.sh
+++ TRIBlib-desktop-pango/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# stuck at 1.40.5, build failures on newer
-#
-${THOME}/build/dobuild -64 pango-1.40.5 -C "--disable-introspection"
-${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.40.5
+${THOME}/build/dobuild -64 pango-1.42.4 -C "--disable-introspection"
+${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.42.4
