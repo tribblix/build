922d976e4911019d6cc13f50b4da3d1a9278f93d
Peter Tribble <peter.tribble@gmail.com>
2019-07-03 08:08:02 +0100

    Update irssi 1.2.1

--- TRIBirssi/build.sh
+++ TRIBirssi/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild irssi-1.2.0 -C --with-perl=no
-${THOME}/build/genpkg TRIBirssi irssi-1.2.0
+env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild irssi-1.2.1 -C --with-perl=no
+${THOME}/build/genpkg TRIBirssi irssi-1.2.1
