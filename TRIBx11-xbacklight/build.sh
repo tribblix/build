0c33d98f1cb08e9d34bfe5a66ad206f28f0cba26
Peter Tribble <peter.tribble@gmail.com>
2019-07-15 21:24:24 +0100

    Update xbacklight 1.2.3

--- TRIBx11-xbacklight/build.sh
+++ TRIBx11-xbacklight/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xbacklight-1.2.2
-${THOME}/build/genpkg TRIBx11-xbacklight xbacklight-1.2.2
+${THOME}/build/dobuild xbacklight-1.2.3
+${THOME}/build/genpkg TRIBx11-xbacklight xbacklight-1.2.3
