7d6faa813df0fea3678dd65b1d51e00b4a494915
Peter Tribble <peter.tribble@gmail.com>
2019-07-19 16:03:29 +0100

    Update pyasn1-modules 0.2.5

--- TRIBpyasn1-modules-python-27/build.sh
+++ TRIBpyasn1-modules-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyasn1-modules-0.0.11
-cd pyasn1-modules-0.0.11
+${THOME}/build/unpack pyasn1-modules-0.2.5
+cd pyasn1-modules-0.2.5
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
