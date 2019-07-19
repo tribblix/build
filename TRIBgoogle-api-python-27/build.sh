c6f6d7a5e43ffa4036696beb69f034d9603f3d9b
Peter Tribble <peter.tribble@gmail.com>
2019-07-19 17:03:59 +0100

    Add google-api-python-client 1.7.9

---
+++ TRIBgoogle-api-python-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack google-api-python-client-1.7.9
+cd google-api-python-client-1.7.9
+python setup.py build
+rm -fr /tmp/pil
+python setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBgoogle-api-python-27 /tmp/pil
+cd ..
+rm -fr /tmp/pil
