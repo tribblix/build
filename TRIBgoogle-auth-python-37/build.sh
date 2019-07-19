7161bc784e4a2d0e0663ec7070c8c4b929f2d9b5
Peter Tribble <peter.tribble@gmail.com>
2019-07-19 16:25:37 +0100

    Add google-auth 1.6.3

---
+++ TRIBgoogle-auth-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack google-auth-1.6.3
+cd google-auth-1.6.3
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBgoogle-auth-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
