82ffb4a7926f848209ad2d26854b81c3977e537a
Peter Tribble <peter.tribble@gmail.com>
2019-07-26 12:34:31 +0100

    Update bcrypt 3.1.7

--- TRIBbcrypt-python-27/build.sh
+++ TRIBbcrypt-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack bcrypt-3.1.4
-cd bcrypt-3.1.4
+${THOME}/build/unpack bcrypt-3.1.7
+cd bcrypt-3.1.7
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
