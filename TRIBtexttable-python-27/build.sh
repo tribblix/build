788516f8541bd9101425e305878084bda6cd5a99
Peter Tribble <peter.tribble@gmail.com>
2019-07-26 11:19:20 +0100

    Update texttable 1.6.2

--- TRIBtexttable-python-27/build.sh
+++ TRIBtexttable-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack texttable-1.6.1
-cd texttable-1.6.1
+${THOME}/build/unpack texttable-1.6.2
+cd texttable-1.6.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
