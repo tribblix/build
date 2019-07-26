11ab11e40476d103fbe1f1f0c2550c3c6a4dd459
Peter Tribble <peter.tribble@gmail.com>
2019-07-26 13:03:50 +0100

    Update pycurl 7.43.0.3

--- TRIBpycurl-python-27/build.sh
+++ TRIBpycurl-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycurl-7.43.0.2
-cd pycurl-7.43.0.2
+${THOME}/build/unpack pycurl-7.43.0.3
+cd pycurl-7.43.0.3
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBpycurl-python-37/build.sh
+++ TRIBpycurl-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycurl-7.43.0.2
-cd pycurl-7.43.0.2
+${THOME}/build/unpack pycurl-7.43.0.3
+cd pycurl-7.43.0.3
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
