8fead28f51d3aa9abda331a6e3c9df1516889570
Peter Tribble <peter.tribble@gmail.com>
2019-07-26 12:38:35 +0100

    Update dulwich 0.9.11

--- TRIBdulwich-python-27/build.sh
+++ TRIBdulwich-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack dulwich-0.19.6
-cd dulwich-0.19.6
+${THOME}/build/unpack dulwich-0.19.11
+cd dulwich-0.19.11
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
