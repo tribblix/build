d472ceba2b1c10c5457f9eac030a56c3beb2a569
Peter Tribble <peter.tribble@gmail.com>
2019-07-27 16:49:49 +0100

    Update ecdsa 0.13.2

--- TRIBecdsa-python-27/build.sh
+++ TRIBecdsa-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ecdsa-0.13
-cd ecdsa-0.13
+${THOME}/build/unpack ecdsa-0.13.2
+cd ecdsa-0.13.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBecdsa-python-37/build.sh
+++ TRIBecdsa-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ecdsa-0.13
-cd ecdsa-0.13
+${THOME}/build/unpack ecdsa-0.13.2
+cd ecdsa-0.13.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
