993eb21d52ce4907292822d9addd56585926ba1c
Peter Tribble <peter.tribble@gmail.com>
2019-07-27 16:18:55 +0100

    Update backports.ssl_match_hostname 3.7.0.1

--- TRIBbackportssmh-python-27/build.sh
+++ TRIBbackportssmh-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack backports.ssl_match_hostname-3.5.0.1
-cd backports.ssl_match_hostname-3.5.0.1
+${THOME}/build/unpack backports.ssl_match_hostname-3.7.0.1
+cd backports.ssl_match_hostname-3.7.0.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
