50b49c64f4b563abf11e8efe6a7a16d64b87cbba
Peter Tribble <peter.tribble@gmail.com>
2019-07-28 15:57:55 +0100

    Update jdcal 1.4.1

--- TRIBjdcal-python-37/build.sh
+++ TRIBjdcal-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack jdcal-1.4
-cd jdcal-1.4
+${THOME}/build/unpack jdcal-1.4.1
+cd jdcal-1.4.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
