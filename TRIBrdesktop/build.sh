55ac6a7761bec3f0dc5eee73eb6d6f76f1f0c155
Peter Tribble <peter.tribble@gmail.com>
2019-07-29 18:43:01 +0100

    Update rdesktop 1.8.6

--- TRIBrdesktop/build.sh
+++ TRIBrdesktop/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild rdesktop-1.8.4 -C "--disable-credssp --disable-smartcard"
-${THOME}/build/genpkg  TRIBrdesktop rdesktop-1.8.4
+${THOME}/build/dobuild rdesktop-1.8.6 -C "--disable-credssp --disable-smartcard"
+${THOME}/build/genpkg  TRIBrdesktop rdesktop-1.8.6
