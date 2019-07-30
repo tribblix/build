11dd86dbc63f2733c5e9ed09164bc548a365733d
Peter Tribble <peter.tribble@gmail.com>
2019-07-30 13:30:56 +0100

    Update tcl/tk 8.6.9

--- TRIBtk/build.sh
+++ TRIBtk/build.sh
@@ -3,8 +3,11 @@
 # build tcl and tk together
 #
 
-${THOME}/build/unpack tk8.6.8-src
-cd tk8.6.8/unix
+#
+# the tarbal is called 8.6.9.1, but it's 8.6.9 inside
+#
+${THOME}/build/unpack tk8.6.9.1-src
+cd tk8.6.9/unix
 env CFLAGS=-O ./configure --prefix=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
@@ -13,8 +16,8 @@ cd ../..
 
 mkdir -p 64bit
 cd 64bit
-${THOME}/build/unpack tk8.6.8-src
-cd tk8.6.8/unix
+${THOME}/build/unpack tk8.6.9.1-src
+cd tk8.6.9/unix
 env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/`isainfo -k` --libdir=/usr/lib/`isainfo -k`
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
