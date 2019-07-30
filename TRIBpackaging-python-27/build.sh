df193ba2202993c73f3b6e4c91e1325860102364
Peter Tribble <peter.tribble@gmail.com>
2019-07-30 08:52:51 +0100

    Update packaging-python 19.1

--- TRIBpackaging-python-27/build.sh
+++ TRIBpackaging-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack packaging-19.0
-cd packaging-19.0
+${THOME}/build/unpack packaging-19.1
+cd packaging-19.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
