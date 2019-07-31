82fc2cebf189ee0ad7957406d1c045b363fe8e27
Peter Tribble <peter.tribble@gmail.com>
2019-07-31 13:40:08 +0100

    Update openpyxl 2.6.2

--- TRIBopenpyxl-python-27/build.sh
+++ TRIBopenpyxl-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack openpyxl-2.5.9
-cd openpyxl-2.5.9
+${THOME}/build/unpack openpyxl-2.6.2
+cd openpyxl-2.6.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
