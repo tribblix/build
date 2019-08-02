0d9bc1b2bc2294f94203e38c64ceb7b026c94e4e
Peter Tribble <peter.tribble@gmail.com>
2019-08-02 15:32:47 +0100

    Update pyxdg 0.26

--- TRIBxdg-python-27/build.sh
+++ TRIBxdg-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyxdg-0.25
-cd pyxdg-0.25
+${THOME}/build/unpack pyxdg-0.26
+cd pyxdg-0.26
 python setup.py build
 python setup.py install --root=/tmp/xx1
 ${THOME}/build/create_pkg TRIBxdg-python-27 /tmp/xx1
