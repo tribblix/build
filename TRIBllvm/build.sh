5ad1961746d329192382e4988bb8e372ad895d54
Peter Tribble <peter.tribble@gmail.com>
2019-08-03 09:11:39 +0100

    Fix the unpack and patch for llvm

--- TRIBllvm/build.sh
+++ TRIBllvm/build.sh
@@ -3,7 +3,7 @@
 #
 # LLVM/clang
 #
-# Currently experimental - that's why it's parked out is /usr/versions, it
+# Currently experimental - that's why it's parked out in /usr/versions, it
 # ought to move into /usr when it looks good
 #
 
@@ -23,7 +23,11 @@ cd tools
 ${THOME}/build/unpack cfe-4.0.1.src
 mv cfe-4.0.1.src clang
 cd ..
-
+#
+# this is a combined patch that assumes both llvm and cfe, so we
+# can't use the normal mechanism to apply it during unpack
+#
+gpatch -p1 < ${THOME}/build/patches/llvm4.patch
 
 mkdir build
 cd build
@@ -31,6 +35,6 @@ cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm-4.0.1 -DCMAK
 gmake -j 6
 
 zap install groff
-env PATH=/usr/perl5/5.10.0/bin/:$PATH gmake -k install DESTDIR=/tmp/ll
+env PATH=/usr/perl5/5.22.0/bin/:$PATH gmake -k install DESTDIR=/tmp/ll
 # [fails on pod2html/pod2man]
 ${THOME}/build/create_pkg TRIBllvm /tmp/ll
--- patches/llvm-4.0.1.src.pls
+++
@@ -1,1 +0,0 @@
--p1 llvm4.patch
