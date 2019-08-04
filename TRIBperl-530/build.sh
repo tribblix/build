128f1f3a00b4c67460896a88748a73dd7bf86fec
Peter Tribble <peter.tribble@gmail.com>
2019-08-04 21:49:57 +0100

    Add perl 5.30.0

---
+++ TRIBperl-530/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# System perl (to replace 510), flags from omnios (so we're as
+# compatible as possible)
+#
+${THOME}/build/unpack perl-5.30.0
+cd perl-5.30.0
+./Configure -Dusethreads -Duseshrplib -Dusemultiplicity -Duselargefiles \
+-Duse64bitint -Dmyhostname="tribblix" \
+-Dcc=gcc -Dld=/usr/bin/ld -Dccflags="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -D_TS_ERRNO" \
+-Doptimize="-O3" \
+-Dvendorprefix=/usr/perl5/5.30.0 -Dprefix=/usr/perl5/5.30.0 \
+-Dbin=/usr/perl5/5.30.0/bin \
+-Dsitebin=/usr/perl5/5.30.0/bin \
+-Dvendorbin=/usr/perl5/5.30.0/bin \
+-Dscriptdir=/usr/perl5/5.30.0/bin \
+-Dsitescript=/usr/perl5/5.30.0/bin \
+-Dvendorscript=/usr/perl5/5.30.0/bin \
+-Dprivlib=/usr/perl5/5.30.0/lib \
+-Dsitelib=/usr/perl5/site_perl/5.30.0 \
+-Dvendorlib=/usr/perl5/vendor_perl/5.30.0 \
+-des
+gmake -j 8
+gmake test
+rm -fr /tmp/pp ; gmake install DESTDIR=/tmp/pp
+${THOME}/build/create_pkg TRIBperl-530 /tmp/pp
