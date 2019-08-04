0a02b44f4f44c04ad0048dd400651f3c8d5ff759
Peter Tribble <peter.tribble@gmail.com>
2019-08-04 10:13:31 +0100

    Update attrs 19.1.0

--- TRIBattrs-python-37/build.sh
+++ TRIBattrs-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack attrs-18.2.0
-cd attrs-18.2.0
+${THOME}/build/unpack attrs-19.1.0
+cd attrs-19.1.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
