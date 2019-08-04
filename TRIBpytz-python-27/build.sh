6f56413dead77219ddad5d257be78e6658ee8f24
Peter Tribble <peter.tribble@gmail.com>
2019-08-04 09:56:50 +0100

    Update pytz 2019.2

--- TRIBpytz-python-27/build.sh
+++ TRIBpytz-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pytz-2019.1
-cd pytz-2019.1
+${THOME}/build/unpack pytz-2019.2
+cd pytz-2019.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBpytz-python-37/build.sh
+++ TRIBpytz-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pytz-2019.1
-cd pytz-2019.1
+${THOME}/build/unpack pytz-2019.2
+cd pytz-2019.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
