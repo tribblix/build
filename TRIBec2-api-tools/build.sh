46067f47ef4cd434fb519933773f3234978e85b1
Peter Tribble <peter.tribble@gmail.com>
2019-08-05 11:48:23 +0100

    Note where the EC2 API tools can be obtained from.

--- TRIBec2-api-tools/build.sh
+++ TRIBec2-api-tools/build.sh
@@ -2,4 +2,8 @@
 #
 # the download filename isn't versioned
 #
+# to download, as AWS have hidden it
+#
+# wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
+#
 ${THOME}/build/pkg_tarball TRIBec2-api-tools ec2-api-tools
