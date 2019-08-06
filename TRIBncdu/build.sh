3d0c7e347fc16b2c3c8de9d2288e725d923f2e4c
Peter Tribble <peter.tribble@gmail.com>
2019-08-06 07:53:04 +0100

    Update ncdu 1.14.1

--- TRIBncdu/build.sh
+++ TRIBncdu/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild ncdu-1.13
-${THOME}/build/genpkg TRIBncdu ncdu-1.13
+env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild ncdu-1.14.1
+${THOME}/build/genpkg TRIBncdu ncdu-1.14.1
