cf3b71ec9f326ea2833f41eb87916dba79d713bc
Peter Tribble <peter.tribble@gmail.com>
2019-08-09 10:45:21 +0100

    Update httpie 1.0.2

--- TRIBhttpie-python-37/build.sh
+++ TRIBhttpie-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httpie-0.9.9
-cd httpie-0.9.9
+${THOME}/build/unpack httpie-1.0.2
+cd httpie-1.0.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
