4d04ca191f0ba68cdbbaa4a5db89e0af973f9da2
Peter Tribble <peter.tribble@gmail.com>
2019-08-10 12:56:49 +0100

    Update meson 0.51.1

--- TRIBmeson-python-37/build.sh
+++ TRIBmeson-python-37/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack meson-0.49.2
-cd meson-0.49.2
+${THOME}/build/unpack meson-0.51.1
+cd meson-0.51.1
 rm -fr /tmp/p3a
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBmeson-python-37 /tmp/p3a
