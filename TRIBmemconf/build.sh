2a2403b4921146ba401c560adaad087296ca7d7b
Peter Tribble <peter.tribble@gmail.com>
2019-08-10 17:14:14 +0100

    Update memconf 3.15

--- TRIBmemconf/build.sh
+++ TRIBmemconf/build.sh
@@ -4,7 +4,7 @@
 #
 rm -fr /tmp/pp
 mkdir -p /tmp/pp/usr/bin
-cp memconf.v3.14.pl.gz /tmp/pp/usr/bin/memconf.gz
+cp ${THOME}/tarballs/memconf.v3.15.pl.gz /tmp/pp/usr/bin/memconf.gz
 gunzip /tmp/pp/usr/bin/memconf
 chmod 755 /tmp/pp/usr/bin/memconf
 ${THOME}/build/create_pkg TRIBmemconf /tmp/pp
