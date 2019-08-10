e37d92f27302a5af1c2174f3eec0337bc8ed7324
Peter Tribble <peter.tribble@gmail.com>
2019-08-10 13:22:55 +0100

    Update aide 0.16.2

--- TRIBaide/build.sh
+++ TRIBaide/build.sh
@@ -2,5 +2,5 @@
 #
 # it wants to link everything statically, which will never work for us
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/pcre ${THOME}/build/dobuild aide-0.16.1 -C --disable-static
-${THOME}/build/genpkg TRIBaide aide-0.16.1
+env TRIBBLIX_CFLAGS=-I/usr/include/pcre ${THOME}/build/dobuild aide-0.16.2 -C --disable-static
+${THOME}/build/genpkg TRIBaide aide-0.16.2
