8cbb2145c0746dbfe136d8689bd9fe6e6964e3eb
Peter Tribble <peter.tribble@gmail.com>
2019-08-13 18:24:45 +0100

    Update nginx 1.16.1

--- TRIBblix-nginx/build.sh
+++ TRIBblix-nginx/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild nginx-1.16.0 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module"
-${THOME}/build/genpkg TRIBblix-nginx nginx-1.16.0
+${THOME}/build/dobuild nginx-1.16.1 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module"
+${THOME}/build/genpkg TRIBblix-nginx nginx-1.16.1
