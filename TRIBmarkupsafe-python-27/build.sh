5e232e654a4b5349b3852d70ea1a417b356d34a5
Peter Tribble <peter.tribble@gmail.com>
2019-08-16 10:16:50 +0100

    Update MarkupSafe 1.1.1

--- TRIBmarkupsafe-python-27/build.sh
+++ TRIBmarkupsafe-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack MarkupSafe-1.0
-cd MarkupSafe-1.0
+${THOME}/build/unpack MarkupSafe-1.1.1
+cd MarkupSafe-1.1.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
