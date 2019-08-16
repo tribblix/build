1edc801364020ebd205561250d52fff547178266
Peter Tribble <peter.tribble@gmail.com>
2019-08-16 10:13:42 +0100

    Update sphinxcontrib-websupport 1.1.2

--- TRIBsphinx-ws-python-27/build.sh
+++ TRIBsphinx-ws-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack sphinxcontrib-websupport-1.1.0
-cd sphinxcontrib-websupport-1.1.0
+${THOME}/build/unpack sphinxcontrib-websupport-1.1.2
+cd sphinxcontrib-websupport-1.1.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
