268ca40a3858192d789fcbe4604250972d126cf5
Peter Tribble <peter.tribble@gmail.com>
2019-08-18 19:46:22 +0100

    Update snowballstemmer 1.9.0

--- TRIBsnowballstemmer-python-27/build.sh
+++ TRIBsnowballstemmer-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack snowballstemmer-1.2.1
-cd snowballstemmer-1.2.1
+${THOME}/build/unpack snowballstemmer-1.9.0
+cd snowballstemmer-1.9.0
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBsnowballstemmer-python-37/build.sh
+++ TRIBsnowballstemmer-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack snowballstemmer-1.2.1
-cd snowballstemmer-1.2.1
+${THOME}/build/unpack snowballstemmer-1.9.0
+cd snowballstemmer-1.9.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
