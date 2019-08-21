66b76d3887600e5d372e99caa697de05fb632343
Peter Tribble <peter.tribble@gmail.com>
2019-08-21 17:03:25 +0100

    Document the wallpaper package build

---
+++ TRIBxfce-wallpaper1/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# The image files come from
+# http://petertribble.co.uk/Fun/wallpaper1.html
+# the Tribblix package uses the 1600x1200 images (without the 1600_ prefix)
+#
+rm -fr /tmp/wp
+mkdir -p /tmp/wp/usr/share/backgrounds/xfce
+cd /tmp/wp/usr/share/backgrounds/xfce
+
+wget -o bodiam1.jpg http://petertribble.co.uk/Fun/1600_bodiam1.jpg
+wget -o everest-ride.jpg http://petertribble.co.uk/Fun/1600_everest-ride.jpg
+wget -o fishtank.jpg http://petertribble.co.uk/Fun/1600_fishtank.jpg
+wget -o goddess-engine.jpg http://petertribble.co.uk/Fun/1600_goddess-engine.jpg
+wget -o gozowindow.jpg http://petertribble.co.uk/Fun/1600_gozowindow.jpg
+wget -o lille-wheel.jpg http://petertribble.co.uk/Fun/1600_lille-wheel.jpg
+wget -o rapide.jpg http://petertribble.co.uk/Fun/1600_rapide.jpg
+wget -o signal-box.jpg http://petertribble.co.uk/Fun/1600_signal-box.jpg
+wget -o tree-of-life.jpg http://petertribble.co.uk/Fun/1600_tree-of-life.jpg
+wget -o wcastle1.jpg http://petertribble.co.uk/Fun/1600_wcastle1.jpg
+wget -o wcastle2.jpg http://petertribble.co.uk/Fun/1600_wcastle2.jpg
+
+mkdir -p /tmp/wp/usr/share/mate-background-properties
+cp ${THOME}/build/patches/tribblix-1.xml /tmp/wp/usr/share/mate-background-properties
+${THOME}/build/create_pkg TRIBxfce-wallpaper1 /tmp/wp
+cd
+rm -fr /tmp/wp
---
+++ TRIBxfce-wallpaper2/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# The image files come from
+# http://petertribble.co.uk/Fun/wallpaper2.html
+# the Tribblix package uses the 1600x1200 images (without the 1600_ prefix)
+#
+rm -fr /tmp/wp
+mkdir -p /tmp/wp/usr/share/backgrounds/xfce
+cd /tmp/wp/usr/share/backgrounds/xfce
+
+wget -o mbird1.jpg http://petertribble.co.uk/Fun/1600_mbird1.jpg
+wget -o mcliff.jpg http://petertribble.co.uk/Fun/1600_mcliff.jpg
+wget -o mliners.jpg http://petertribble.co.uk/Fun/1600_mliners.jpg
+wget -o mosaic.jpg http://petertribble.co.uk/Fun/1600_mosaic.jpg
+wget -o mplane.jpg http://petertribble.co.uk/Fun/1600_mplane.jpg
+wget -o mradar.jpg http://petertribble.co.uk/Fun/1600_mradar.jpg
+wget -o naples.jpg http://petertribble.co.uk/Fun/1600_naples.jpg
+wget -o nunsvalley.jpg http://petertribble.co.uk/Fun/1600_nunsvalley.jpg
+wget -o phot.jpg http://petertribble.co.uk/Fun/1600_phot.jpg
+wget -o reykjavik.jpg http://petertribble.co.uk/Fun/1600_reykjavik.jpg
+
+mkdir -p /tmp/wp/usr/share/mate-background-properties
+cp ${THOME}/build/patches/tribblix-2.xml /tmp/wp/usr/share/mate-background-properties
+${THOME}/build/create_pkg TRIBxfce-wallpaper2 /tmp/wp
+cd
+rm -fr /tmp/wp
