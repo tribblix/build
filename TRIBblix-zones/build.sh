7a52a873af2484a6d5f173c9173100ad2e01de89
Peter Tribble <peter.tribble@gmail.com>
2019-08-21 17:02:44 +0100

    Need a build script for TRIBblix-zones

---
+++ TRIBblix-zones/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# this requires the tribblix-zones repo be checked out, eg
+#
+# cd $THOME
+# git clone https://github.com/tribblix/tribblix-zones
+#
+${THOME}/build/create_pkg TRIBblix-zones ${THOME}/tribblix-zones
