f55977ac1f53242f6be5ad6b9fb35b0584d20a28
Peter Tribble <peter.tribble@gmail.com>
2019-08-22 18:59:45 +0100

    Disable SSL2 in wget

--- TRIBwget/build.sh
+++ TRIBwget/build.sh
@@ -2,5 +2,5 @@
 #
 # (no pcre in the install environment)
 #
-env POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.20.3 -C '--sysconfdir=/etc --with-ssl=openssl --disable-pcre'
+env TRIBBLIX_CFLAGS=-DOPENSSL_NO_SSL2 POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.20.3 -C '--sysconfdir=/etc --with-ssl=openssl --disable-pcre'
 ${THOME}/build/genpkg TRIBwget wget-1.20.3
