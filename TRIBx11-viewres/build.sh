26449291705f4392a8a1bb24155205e40727d862
Peter Tribble <peter.tribble@gmail.com>
2019-08-26 16:33:03 +0100

    Update viewres 1.0.6

--- TRIBx11-viewres/build.sh
+++ TRIBx11-viewres/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild viewres-1.0.4
-${THOME}/build/genpkg TRIBx11-viewres viewres-1.0.4
+${THOME}/build/dobuild viewres-1.0.6
+${THOME}/build/genpkg TRIBx11-viewres viewres-1.0.6
