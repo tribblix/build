c056f99e748c32b48473e20b8e2f03cd7282918d
Peter Tribble <peter.tribble@gmail.com>
2019-08-27 16:00:59 +0100

    Update slony 2.2.8

--- TRIBblix-slony10/build.sh
+++ TRIBblix-slony10/build.sh
@@ -6,5 +6,5 @@
 # installs into the same prefix as the postgres version it's built
 # against, and is versioned to match
 #
-$THOME/build/dobuild slony1-2.2.7 -P /opt/tribblix/postgres10 -C "--with-pgconfigdir=/opt/tribblix/postgres10/bin --sysconfdir=/etc"
-$THOME/build/genpkg TRIBblix-slony10 slony1-2.2.7
+$THOME/build/dobuild slony1-2.2.8 -P /opt/tribblix/postgres10 -C "--with-pgconfigdir=/opt/tribblix/postgres10/bin --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-slony10 slony1-2.2.8
