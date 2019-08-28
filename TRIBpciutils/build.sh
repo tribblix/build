d83eb52cecad403c53ff979b7f5b2754efd458a1
Peter Tribble <peter.tribble@gmail.com>
2019-08-28 16:34:29 +0100

    Update the pci id database

--- TRIBpciutils/build.sh
+++ TRIBpciutils/build.sh
@@ -3,6 +3,8 @@
 ${THOME}/build/unpack pciutils-3.6.2
 cd pciutils-3.6.2
 env PATH=/usr/gnu/bin:$PATH gmake PREFIX=/usr OPT="-O2 -DBYTE_ORDER=1234 -DLITTLE_ENDIAN=1234"
+# update the database
+./update-pciids.sh
 rm -fr /tmp/pp
 env PATH=/usr/gnu/bin:$PATH gmake install PREFIX=/usr DESTDIR=/tmp/pp
 ${THOME}/build/create_pkg TRIBpciutils /tmp/pp
