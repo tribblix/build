e9c93f29a146ee6d97541fb6622816672fe03470
Peter Tribble <peter.tribble@gmail.com>
2019-08-31 12:32:32 +0100

    Update phonenumbers 8.10.17

--- TRIBphonenumbers-python-37/build.sh
+++ TRIBphonenumbers-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack phonenumbers-8.10.16
-cd phonenumbers-8.10.16
+${THOME}/build/unpack phonenumbers-8.10.17
+cd phonenumbers-8.10.17
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
