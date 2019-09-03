e2246de2181337aa0f0c4548780cc20731f328cb
Peter Tribble <peter.tribble@gmail.com>
2019-09-03 20:28:56 +0100

    Update gnupg 2.2.17

--- TRIBgnupg2/build.sh
+++ TRIBgnupg2/build.sh
@@ -2,5 +2,5 @@
 #
 # gnupg2
 #
-${THOME}/build/dobuild gnupg-2.2.15 -C "--disable-ldap --enable-gpg-is-gpg2"
-${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.15
+${THOME}/build/dobuild gnupg-2.2.17 -C "--disable-ldap --enable-gpg-is-gpg2"
+${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.17
