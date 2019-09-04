9589702baebe0e24e60b03ccda7b995d093996c6
Peter Tribble <peter.tribble@gmail.com>
2019-09-04 18:33:41 +0100

    Update pycryptodome 3.9.0

--- TRIBpycryptodome-python-27/build.sh
+++ TRIBpycryptodome-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycryptodome-3.8.2
-cd pycryptodome-3.8.2
+${THOME}/build/unpack pycryptodome-3.9.0
+cd pycryptodome-3.9.0
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
