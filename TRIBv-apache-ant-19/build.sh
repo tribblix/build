a1fd18c38f2111a51089c41d58c30ccd24635901
Peter Tribble <peter.tribble@gmail.com>
2019-09-04 21:46:26 +0100

    Tidy up the ant build, update jsch to 0.1.55

--- TRIBv-apache-ant-19/build.sh
+++ TRIBv-apache-ant-19/build.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 #
-rm -fr /tmp/aa
-mkdir -p /tmp/aa/usr/versions
-cd /tmp/aa/usr/versions
-unzip -q /packages/localsrc/Ant/apache-ant-1.9.14-bin.zip
-cp /packages/localsrc/Ant/jsch-0.1.54.jar apache-ant-1.9.14/lib/
-cp /packages/localsrc/Ant/apache-ivy-2.4.0/ivy-2.4.0.jar apache-ant-1.9.14/lib/
-cp /packages/localsrc/Ant/apache-ivy-2.4.0/lib/{c,o}*.jar apache-ant-1.9.14/lib/
-cp /packages/localsrc/Ant/svnant-1.3.1/lib/svnant.jar apache-ant-1.9.14/lib
-cp /packages/localsrc/Ant/svnant-1.3.1/lib/svnClientAdapter.jar apache-ant-1.9.14/lib
-${THOME}/build/create_pkg TRIBv-apache-ant-19 /tmp/aa
-cd
-rm -fr /tmp/aa
+# also needs the following in tarballs for fixit to deploy:
+# apache-ivy-2.4.0-bin-with-deps.zip
+# svnant-1.3.1.zip
+# jsch-0.1.55.jar
+#
+${THOME}/build/pkg_tarball TRIBv-apache-ant-19 apache-ant-1.9.14-bin
