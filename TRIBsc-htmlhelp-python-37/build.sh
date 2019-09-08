c57df7ff38572a2003378236ccc3af640abd4ae5
Peter Tribble <peter.tribble@gmail.com>
2019-09-08 15:44:30 +0100

    Add the contrib modules split out of Sphinx

---
+++ TRIBsc-htmlhelp-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack sphinxcontrib-htmlhelp-1.0.2
+cd sphinxcontrib-htmlhelp-1.0.2
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBsc-htmlhelp-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
