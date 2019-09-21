59f9af75ec83bef317977f92b09a5711b7746791
Peter Tribble <peter.tribble@gmail.com>
2019-09-21 14:13:06 +0100

    Update packaging 19.2

--- TRIBpackaging-python-37/build.sh
+++ TRIBpackaging-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack packaging-19.1
-cd packaging-19.1
+${THOME}/build/unpack packaging-19.2
+cd packaging-19.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
