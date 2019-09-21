d7cd3c3aa9315eea6db3b53096826fc1aa277b21
Peter Tribble <peter.tribble@gmail.com>
2019-09-21 14:06:55 +0100

    Update PySocks 1.7.1

--- TRIBpysocks-python-27/build.sh
+++ TRIBpysocks-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack PySocks-1.7.0
-cd PySocks-1.7.0
+${THOME}/build/unpack PySocks-1.7.1
+cd PySocks-1.7.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
