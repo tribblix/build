8afff18d52fd2cba1324ba3e1b9f0ecf01ce754d
Peter Tribble <peter.tribble@gmail.com>
2019-09-22 16:57:46 +0100

    Update consul 1.6.1

--- TRIBblix-consul/build.sh
+++ TRIBblix-consul/build.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 #
-# (requires go 1.8 or later)
-#
 mkdir ~/ud/c
 cd ~/ud/c
 mkdir -p src/github.com/hashicorp/
 cd src/github.com/hashicorp/
 git clone https://github.com/hashicorp/consul
 cd consul
-# this is the commit for 1.0.0
-git checkout 51ea240df8476e02215d53fbfad5838bf0d44d21
+# this is the commit for 1.6.1
+git checkout 9be6dfc310ddd54627d698537a98827245185290
 # CONSUL_DEV to avoid cross-compiling for all platforms
-env CONSUL_DEV=1 GOPATH=${HOME}/ud/c GOROOT=/usr/versions/go-1.10 PATH=/usr/versions/go-1.10/bin:$PATH gmake
+env CONSUL_DEV=1 GOPATH=${HOME}/ud/c GOROOT=/usr/versions/go-1.12 PATH=/usr/versions/go-1.12/bin:$PATH gmake
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/consul/bin
 cp bin/consul /tmp/ee/opt/tribblix/consul/bin
