cc3d6c0ca935311e2e8abc26a3e0623d0f663484
Peter Tribble <peter.tribble@gmail.com>
2019-09-23 14:21:46 +0100

    Update zabbix 4.0.12

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -4,6 +4,6 @@
 # avoid tying it to the normal preconf as it's only relevant for the server
 #
 ${THOME}/build/patches/zabbix-server.preconf
-${THOME}/build/dobuild zabbix-4.0.11 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres11/bin/pg_config --with-libpcre-include=/usr/include/pcre --with-libevent=/tmp/mc"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-4.0.11
+${THOME}/build/dobuild zabbix-4.0.12 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres11/bin/pg_config --with-libpcre-include=/usr/include/pcre --with-libevent=/tmp/mc"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-4.0.12
 rm -fr /tmp/mc
--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-4.0.11 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre-include=/usr/include/pcre"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-4.0.11
+${THOME}/build/dobuild zabbix-4.0.12 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre-include=/usr/include/pcre"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-4.0.12
