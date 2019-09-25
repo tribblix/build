0e5363786d44d479dfd8aa4bc8b7d6eeb330bb45
Peter Tribble <peter.tribble@gmail.com>
2019-09-25 08:00:29 +0100

    Update pgFormatter 4.1

--- TRIBpgformatter/build.sh
+++ TRIBpgformatter/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pgFormatter-4.0
-cd pgFormatter-4.0
+${THOME}/build/unpack pgFormatter-4.1
+cd pgFormatter-4.1
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBpgformatter
