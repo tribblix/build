b054be188d537c82bef30d29aaf09014adc4c36c
Peter Tribble <peter.tribble@gmail.com>
2019-09-27 14:40:25 +0100

    Update php 7.2.23

--- TRIBblix-php-7/build.sh
+++ TRIBblix-php-7/build.sh
@@ -12,8 +12,8 @@
 # must have apache24 installed
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-7.2.22
-cd php-7.2.22
+${THOME}/build/unpack php-7.2.23
+cd php-7.2.23
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
