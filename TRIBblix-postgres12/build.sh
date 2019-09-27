4db1267f960bb2372073fbf584d00798e2d434dd
Peter Tribble <peter.tribble@gmail.com>
2019-09-27 13:48:18 +0100

    Update postgres12 to rc1

--- TRIBblix-postgres12/build.sh
+++ TRIBblix-postgres12/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 12 beta
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres12 postgresql-12beta3 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres12 postgresql-12beta3
+${THOME}/build/dobuild -P /opt/tribblix/postgres12 postgresql-12rc1 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres12 postgresql-12rc1
