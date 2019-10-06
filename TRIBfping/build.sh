ae91796c9d103ae26e8f348148d5ba6321a8b680
Peter Tribble <peter.tribble@gmail.com>
2019-10-06 19:48:19 +0100

    Update fping 4.2

--- TRIBfping/build.sh
+++ TRIBfping/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild fping-3.16 -C --enable-ipv6
-${THOME}/build/genpkg TRIBfping fping-3.16
+env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild fping-4.2 -C --enable-ipv6
+${THOME}/build/genpkg TRIBfping fping-4.2
