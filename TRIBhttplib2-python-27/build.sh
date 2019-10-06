e17c5ff4ce5734419aa6888ff42ddd3defc013c9
Peter Tribble <peter.tribble@gmail.com>
2019-10-06 10:01:01 +0100

    Update httplib2 0.14.0

--- TRIBhttplib2-python-27/build.sh
+++ TRIBhttplib2-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httplib2-0.13.1
-cd httplib2-0.13.1
+${THOME}/build/unpack httplib2-0.14.0
+cd httplib2-0.14.0
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
