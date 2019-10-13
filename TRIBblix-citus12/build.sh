0fb9970f1942bda0605e982f19756947978d13df
Peter Tribble <peter.tribble@gmail.com>
2019-10-13 12:21:36 +0100

    Update citus 9.0.0, and package for PostgreSQL 12 as well

--- TRIBblix-citus12/build.sh
+++ TRIBblix-citus12/build.sh
@@ -5,5 +5,5 @@
 # there's a patch for
 # https://github.com/citusdata/citus/issues/2679
 #
-${THOME}/build/dobuild citus-9.0.0 -C PG_CONFIG=/opt/tribblix/postgres10/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus10 citus-9.0.0
+${THOME}/build/dobuild citus-9.0.0 -C PG_CONFIG=/opt/tribblix/postgres12/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus12 citus-9.0.0
