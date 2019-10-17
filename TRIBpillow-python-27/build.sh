9c9955b68ef560aae8eb389c8ef0783f1cf8b6f5
Peter Tribble <peter.tribble@gmail.com>
2019-10-17 07:48:09 +0100

    Update Pillow 6.2.0

--- TRIBpillow-python-27/build.sh
+++ TRIBpillow-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Pillow-6.1.0
-cd Pillow-6.1.0
+${THOME}/build/unpack Pillow-6.2.0
+cd Pillow-6.2.0
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
