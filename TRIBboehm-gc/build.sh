6b7b5499c86b3246a8bdb76cc10a098aa481ebe7
Peter Tribble <peter.tribble@gmail.com>
2019-10-20 17:17:26 +0100

    Add 64-bit version of boehm-gc

--- TRIBboehm-gc/build.sh
+++ TRIBboehm-gc/build.sh
@@ -3,5 +3,5 @@
 # (nb symlink from the gc-7.2e tarball)
 # (pkg version is 7.2.5==7.2e)
 #
-${THOME}/build/dobuild gc-7.2
+${THOME}/build/dobuild -64 gc-7.2
 ${THOME}/build/genpkg TRIBboehm-gc gc-7.2
