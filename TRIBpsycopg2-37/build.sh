cba6928d485529d8bd95061a38c495fc44b097bd
Peter Tribble <peter.tribble@gmail.com>
2019-10-22 19:08:54 +0100

    Update psycopg2 2.8.4

--- TRIBpsycopg2-37/build.sh
+++ TRIBpsycopg2-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psycopg2-2.8.3
-cd psycopg2-2.8.3
+${THOME}/build/unpack psycopg2-2.8.4
+cd psycopg2-2.8.4
 env PATH=/opt/tribblix/postgres11/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres11/lib /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 env PATH=/opt/tribblix/postgres11/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres11/lib /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
