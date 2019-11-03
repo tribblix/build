7852a173e7dc12f3e78dea1f2f970bfa52ca6f82
Peter Tribble <peter.tribble@gmail.com>
2019-11-03 09:35:06 +0000

    Update file 5.37

--- TRIBfile-magic/build.sh
+++ TRIBfile-magic/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 file-5.32
-${THOME}/build/genpkg TRIBfile-magic file-5.32
+${THOME}/build/dobuild -64 file-5.37
+${THOME}/build/genpkg TRIBfile-magic file-5.37
