2ca49b0c3159c48699a7d628525c92bc2d6be83b
Peter Tribble <peter.tribble@gmail.com>
2019-11-05 09:00:39 +0000

    Update psutil 5.6.4

--- TRIBpsutil-python-27/build.sh
+++ TRIBpsutil-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psutil-5.6.3
-cd psutil-5.6.3
+${THOME}/build/unpack psutil-5.6.4
+cd psutil-5.6.4
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
