e54cec45efa4fa151d1a2d97117e84ba135262ed
Peter Tribble <peter.tribble@gmail.com>
2019-11-10 12:47:06 +0000

    Update pyparsing 2.4.4

--- TRIBpyparsing-python-37/build.sh
+++ TRIBpyparsing-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyparsing-2.4.2
-cd pyparsing-2.4.2
+${THOME}/build/unpack pyparsing-2.4.4
+cd pyparsing-2.4.4
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
