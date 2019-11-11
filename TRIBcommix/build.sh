1b29664553922726455aff51e6c792d15b1b0888
Peter Tribble <peter.tribble@gmail.com>
2019-11-11 09:31:54 +0000

    Update commix 3.0

--- TRIBcommix/build.sh
+++ TRIBcommix/build.sh
@@ -6,8 +6,8 @@
 rm -fr /tmp/ckx
 mkdir -p /tmp/ckx/usr/versions
 cd /tmp/ckx/usr/versions
-${THOME}/build/unpack commix-2.9-20190626
-mv commix-2.9-20190626 commix
+${THOME}/build/unpack commix-3.0-20191111
+mv commix-3.0-20191111 commix
 cd
 ${THOME}/build/create_pkg TRIBcommix /tmp/ckx
 rm -fr /tmp/ckx
