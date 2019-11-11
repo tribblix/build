9bfdec53a9dc7fbb493f85fee6a24ea605f123a5
Peter Tribble <peter.tribble@gmail.com>
2019-11-11 11:39:09 +0000

    Update nmap 7.80

--- TRIBnmap/build.sh
+++ TRIBnmap/build.sh
@@ -2,5 +2,5 @@
 #
 # zenmap requires pygtk, which we don't have
 #
-${THOME}/build/dobuild nmap-7.50 -C "--without-zenmap --with-libpcre=included"
-${THOME}/build/genpkg TRIBnmap nmap-7.50
+${THOME}/build/dobuild nmap-7.80 -C "--without-zenmap --with-libpcre=included"
+${THOME}/build/genpkg TRIBnmap nmap-7.80
