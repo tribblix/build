4b66d55483f68be265ac23a6d7cb7c1a350acfb0
Peter Tribble <peter.tribble@gmail.com>
2019-11-16 09:56:33 +0000

    Update postgres 11.6 12.1

--- TRIBblix-postgres11/build.sh
+++ TRIBblix-postgres11/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 11
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.5 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.5
+${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.6 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.6
