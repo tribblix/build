439b8e8cef5668223f17eeef90a0d241d1fb8174
Peter Tribble <peter.tribble@gmail.com>
2019-11-17 15:02:39 +0000

    Update clamav 0.102.0

--- TRIBblix-clamav/build.sh
+++ TRIBblix-clamav/build.sh
@@ -5,5 +5,5 @@
 # ensure that libtool isn't installed
 #  zap uninstall libtool
 #
-${THOME}/build/dobuild clamav-0.101.4 -P /opt/tribblix/clamav
-${THOME}/build/genpkg TRIBblix-clamav clamav-0.101.4
+${THOME}/build/dobuild clamav-0.102.0 -P /opt/tribblix/clamav
+${THOME}/build/genpkg TRIBblix-clamav clamav-0.102.0
