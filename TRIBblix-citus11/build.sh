e77e998369fba7b6edb6b58157fcb0660391b6f8
Peter Tribble <peter.tribble@gmail.com>
2019-11-17 15:44:06 +0000

    Update citus 9.0.1

--- TRIBblix-citus11/build.sh
+++ TRIBblix-citus11/build.sh
@@ -5,5 +5,5 @@
 # there's a patch for
 # https://github.com/citusdata/citus/issues/2679
 #
-${THOME}/build/dobuild citus-9.0.0 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus11 citus-9.0.0
+${THOME}/build/dobuild citus-9.0.1 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus11 citus-9.0.1
