71f76792bfe28c64bc145e257a22a8f95dc2aec2
Peter Tribble <peter.tribble@gmail.com>
2019-11-17 10:42:57 +0000

    Update patroni 1.6.1

--- TRIBpatroni-python-27/build.sh
+++ TRIBpatroni-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack patroni-1.6.0
-cd patroni-1.6.0
+${THOME}/build/unpack patroni-1.6.1
+cd patroni-1.6.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
