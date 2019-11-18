a9c19fa1e8f194a5eb16396fe99279e1a1fb1741
Peter Tribble <peter.tribble@gmail.com>
2019-11-18 20:42:04 +0000

    Update libcloud 2.6.1

--- TRIBlibcloud-python-27/build.sh
+++ TRIBlibcloud-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack apache-libcloud-2.6.0
-cd apache-libcloud-2.6.0
+${THOME}/build/unpack apache-libcloud-2.6.1
+cd apache-libcloud-2.6.1
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
