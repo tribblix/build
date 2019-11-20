3c1f06e3622a289dd9d403e3db2c6cfe52a020fe
Peter Tribble <peter.tribble@gmail.com>
2019-11-20 11:31:07 +0000

    Barman should use python3

--- TRIBpgbarman/build.sh
+++ TRIBpgbarman/build.sh
@@ -2,9 +2,9 @@
 #
 ${THOME}/build/unpack barman-2.9
 cd barman-2.9
-python setup.py build
+/usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
-python setup.py install --root=/tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBpgbarman /tmp/pil
 cd ..
 rm -fr /tmp/pil
