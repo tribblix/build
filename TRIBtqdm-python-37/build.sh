a828e29353bce04a44cde53e1419287da64ed2df
Peter Tribble <peter.tribble@gmail.com>
2019-11-28 11:38:17 +0000

    Update tqdm 4.39.0

--- TRIBtqdm-python-37/build.sh
+++ TRIBtqdm-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tqdm-4.38.0
-cd tqdm-4.38.0
+${THOME}/build/unpack tqdm-4.39.0
+cd tqdm-4.39.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
