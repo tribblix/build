130837e133223413b20a41f9a68596df3aa837b5
Peter Tribble <peter.tribble@gmail.com>
2019-11-30 19:47:38 +0000

    Add publicsuffix2 20190812

---
+++ TRIBpublicsuffix2-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack publicsuffix2-2.20190812
+cd publicsuffix2-2.20190812
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpublicsuffix2-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
