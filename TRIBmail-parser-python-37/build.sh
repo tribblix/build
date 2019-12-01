1ea3721b8e8284480ecc20b3cf4d4b36c67c1d27
Peter Tribble <peter.tribble@gmail.com>
2019-12-01 12:34:51 +0000

    mail-parser declares its dependencies incorrectly for python3

--- TRIBmail-parser-python-37/build.sh
+++ TRIBmail-parser-python-37/build.sh
@@ -2,6 +2,11 @@
 #
 ${THOME}/build/unpack mail-parser-3.9.3
 cd mail-parser-3.9.3
+#
+# requirements are plain wrong for python3
+#
+sed -i '/ipaddress/d' mail_parser.egg-info/requires.txt
+sed -i '/ipaddress/d' requirements.txt
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
