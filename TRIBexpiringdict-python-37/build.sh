9986a828b60ad8f07ed0c9ec4565c4946ee7e55c
Peter Tribble <peter.tribble@gmail.com>
2019-12-01 12:42:54 +0000

    expiringdict declares its dependencies incorrectly for python3

--- TRIBexpiringdict-python-37/build.sh
+++ TRIBexpiringdict-python-37/build.sh
@@ -2,6 +2,10 @@
 #
 ${THOME}/build/unpack expiringdict-1.2.0
 cd expiringdict-1.2.0
+#
+# dependencies are wrong for python3
+#
+sed -i '/typing/d' setup.py
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
