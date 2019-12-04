1a0d60e6f8eee1e8e79f3cb875fefa189a6743e9
Peter Tribble <peter.tribble@gmail.com>
2019-12-04 09:29:29 +0000

    Package a copy of elasticsearch6

---
+++ TRIBblix-elasticsearch6/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# this is incompatible with having other versions of elasticsearch
+# installed
+#
+# also needs a copy of the jna distribution - see the fixit script
+#
+${THOME}/build/pkg_tarball -T TRIBblix-elasticsearch6 elasticsearch-6.8.5
