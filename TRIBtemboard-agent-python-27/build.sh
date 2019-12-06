9670e0dccdc340e0c11bacc56b9ed50214881f98
Peter Tribble <peter.tribble@gmail.com>
2019-12-06 09:32:04 +0000

    Update temboard-agent 4.2

--- TRIBtemboard-agent-python-27/build.sh
+++ TRIBtemboard-agent-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack temboard-agent-4.1
-cd temboard-agent-4.1
+${THOME}/build/unpack temboard-agent-4.2
+cd temboard-agent-4.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
