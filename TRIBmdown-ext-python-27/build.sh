f0d3fe6b71989703da8f3da99ad0a5480873d4cc
Peter Tribble <peter.tribble@gmail.com>
2019-12-08 19:58:57 +0000

    Update pymdown-extensions 6.2

--- TRIBmdown-ext-python-27/build.sh
+++ TRIBmdown-ext-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pymdown-extensions-6.1
-cd pymdown-extensions-6.1
+${THOME}/build/unpack pymdown-extensions-6.2
+cd pymdown-extensions-6.2
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
