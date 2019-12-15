0e273143f2b394b0a706e6a280b1f452fa1925c1
Peter Tribble <peter.tribble@gmail.com>
2019-12-15 20:40:39 +0000

    Update terraform 0.12.18

--- TRIBterraform/build.sh
+++ TRIBterraform/build.sh
@@ -2,7 +2,7 @@
 #
 # need to checkout precisely the version we're going to build
 #
-# having build problems with v0.10.x
+# there are also binaries on https://www.terraform.io/downloads.html
 #
 
 mkdir tf
@@ -12,13 +12,13 @@ mkdir -p src/github.com/hashicorp
 cd src/github.com/hashicorp
 git clone git@github.com:hashicorp/terraform.git
 cd terraform
-git checkout v0.9.11
+git checkout v0.12.18
 
 #
 # explicitly specify the target, and create a production build
 # rather than a development build
 #
-env PATH=${GOPATH}/bin:$PATH XC_OS=solaris XC_ARCH=amd64 gmake bin
+env PATH=PATH=/usr/versions/go-1.13/bin:${GOPATH}/bin:$PATH XC_OS=solaris XC_ARCH=amd64 gmake bin
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
