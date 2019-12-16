8307a3a5dc4c959cbf98a45461547cbc5ebef147
Peter Tribble <peter.tribble@gmail.com>
2019-12-16 15:41:07 +0000

    Update Sphinx 2.3.0

--- TRIBsphinx-37/build.sh
+++ TRIBsphinx-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Sphinx-2.2.1
-cd Sphinx-2.2.1
+${THOME}/build/unpack Sphinx-2.3.0
+cd Sphinx-2.3.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pdu
