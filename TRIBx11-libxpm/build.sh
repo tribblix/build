228f63e9f135dd2243032df1eff868ef911fd141
Peter Tribble <peter.tribble@gmail.com>
2019-12-22 11:25:48 +0000

    Update libXpm 3.5.13

--- TRIBx11-libxpm/build.sh
+++ TRIBx11-libxpm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXpm-3.5.12
-${THOME}/build/genpkg TRIBx11-libxpm libXpm-3.5.12
+${THOME}/build/dobuild -64 libXpm-3.5.13
+${THOME}/build/genpkg TRIBx11-libxpm libXpm-3.5.13
