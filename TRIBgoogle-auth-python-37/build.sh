443690cc3fcbb9852a484598c253b301a82c0660
Peter Tribble <peter.tribble@gmail.com>
2019-12-25 20:18:37 +0000

    Update google-auth 1.10.0

--- TRIBgoogle-auth-python-37/build.sh
+++ TRIBgoogle-auth-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-auth-1.7.2
-cd google-auth-1.7.2
+${THOME}/build/unpack google-auth-1.10.0
+cd google-auth-1.10.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
