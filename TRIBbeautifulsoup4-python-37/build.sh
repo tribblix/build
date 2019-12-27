00566c51c0b4000e8442011d4779f5300682b35a
Peter Tribble <peter.tribble@gmail.com>
2019-12-27 19:40:32 +0000

    Update beautifulsoup4 4.8.2

--- TRIBbeautifulsoup4-python-37/build.sh
+++ TRIBbeautifulsoup4-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack beautifulsoup4-4.8.1
-cd beautifulsoup4-4.8.1
+${THOME}/build/unpack beautifulsoup4-4.8.2
+cd beautifulsoup4-4.8.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
