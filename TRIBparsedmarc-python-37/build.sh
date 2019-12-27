0d083d5b049b4d51e7472b218a7218ce6a5a82e1
Peter Tribble <peter.tribble@gmail.com>
2019-12-27 19:38:03 +0000

    Update parsedmarc 6.7.4

--- TRIBparsedmarc-python-37/build.sh
+++ TRIBparsedmarc-python-37/build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack parsedmarc-6.7.3
-cd parsedmarc-6.7.3
+${THOME}/build/unpack parsedmarc-6.7.4
+cd parsedmarc-6.7.4
 #
-# urllib3 is overconstrained
+# check that the dependencies aren't overconstrained
 #
-sed -i s:1.25:1.26: setup.py
-sed -i s:1.25:1.26: parsedmarc.egg-info/requires.txt
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
