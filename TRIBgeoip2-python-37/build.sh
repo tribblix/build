2e44d4fdaba21f6237332071b2b50de8301b2a7b
Peter Tribble <peter.tribble@gmail.com>
2019-12-27 19:36:37 +0000

    Update geoip2 3.0.0

--- TRIBgeoip2-python-37/build.sh
+++ TRIBgeoip2-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack geoip2-2.9.0
-cd geoip2-2.9.0
+${THOME}/build/unpack geoip2-3.0.0
+cd geoip2-3.0.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
