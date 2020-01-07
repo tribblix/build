181dd313a59180e442723a021abe66c436c7c264
Peter Tribble <peter.tribble@gmail.com>
2020-01-07 09:34:18 +0000

    Update Babel 2.8.0

--- TRIBbabel-python-37/build.sh
+++ TRIBbabel-python-37/build.sh
@@ -2,8 +2,8 @@
 #
 # needs pytz
 #
-${THOME}/build/unpack Babel-2.7.0
-cd Babel-2.7.0
+${THOME}/build/unpack Babel-2.8.0
+cd Babel-2.8.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
