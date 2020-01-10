b97bdab49cc94ee8d4188cceb0c4b34771951410
Peter Tribble <peter.tribble@gmail.com>
2020-01-10 15:03:27 +0000

    Update elasticsearch 6.8.6

--- TRIBblix-elasticsearch6/build.sh
+++ TRIBblix-elasticsearch6/build.sh
@@ -5,4 +5,4 @@
 #
 # also needs a copy of the jna distribution - see the fixit script
 #
-${THOME}/build/pkg_tarball -T TRIBblix-elasticsearch6 elasticsearch-6.8.5
+${THOME}/build/pkg_tarball -T TRIBblix-elasticsearch6 elasticsearch-6.8.6
