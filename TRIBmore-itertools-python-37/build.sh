c6fd6e2f466e5a6965f3014fce77a9d524cb2587
Peter Tribble <peter.tribble@gmail.com>
2020-01-10 14:03:16 +0000

    Add more-itertools 8.0.2

---
+++ TRIBmore-itertools-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack more-itertools-8.0.2
+cd more-itertools-8.0.2
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBmore-itertools-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
