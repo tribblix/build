3b243ce17d93361bf1275f7192aba5c6398047cf
Peter Tribble <peter.tribble@gmail.com>
2020-01-13 17:02:26 +0000

    Update Python 3.8.1

--- TRIBv-python-38/build.sh
+++ TRIBv-python-38/build.sh
@@ -3,9 +3,9 @@
 # NB. TRIBv-python-38 must not be installed, or the make
 # install step will get confused
 #
-# 3.8.0 exists only to test that it builds and works
+# 3.8.1 exists only to test that it builds and works
 # no modules have yet been built against this version
 #
-${THOME}/build/dobuild Python-3.8.0 -P /usr/versions/python-3.8 -L -C --enable-shared
+${THOME}/build/dobuild Python-3.8.1 -P /usr/versions/python-3.8 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-38 Python-3.8.0
+${THOME}/build/genpkg TRIBv-python-38 Python-3.8.1
