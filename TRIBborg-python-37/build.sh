b8583eeed503a70aee2784e66e971ec93f6ec9c0
Peter Tribble <peter.tribble@gmail.com>
2020-01-14 11:59:03 +0000

    Add borg 1.1.10

---
+++ TRIBborg-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack borgbackup-1.1.10
+cd borgbackup-1.1.10
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBborg-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
