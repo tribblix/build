355efdaa3d219aaf888a75e052ede7743e08b9bf
Peter Tribble <peter.tribble@gmail.com>
2020-01-19 12:34:16 +0000

    Update rabbitmq 3.7.18 (constrained by erlang compatibility)

--- TRIBblix-rabbitmq/build.sh
+++ TRIBblix-rabbitmq/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball -T TRIBblix-rabbitmq rabbitmq-server-generic-unix-3.7.8
+# 3.7.18 is the last rabbitmq version to support erlang 20.3
+#
+${THOME}/build/pkg_tarball -T TRIBblix-rabbitmq rabbitmq-server-generic-unix-3.7.18
