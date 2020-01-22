a83da5c0ce5894b8a805e02816165716c76ae979
Peter Tribble <peter.tribble@gmail.com>
2020-01-22 22:18:17 +0000

    Add python3 version of meld3

---
+++ TRIBmeld3-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack meld3-2.0.0
+cd meld3-2.0.0
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBmeld3-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
