6eafbdf8caf64a45a9f75beb4f3c8d808f8b2f7b
Peter Tribble <peter.tribble@gmail.com>
2020-01-26 16:27:43 +0000

    Update socat 1.7.3.4

--- TRIBsocat/build.sh
+++ TRIBsocat/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild socat-1.7.3.2
-${THOME}/build/genpkg TRIBsocat socat-1.7.3.2
+${THOME}/build/dobuild socat-1.7.3.4
+${THOME}/build/genpkg TRIBsocat socat-1.7.3.4
