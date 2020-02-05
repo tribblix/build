9acb1ce69683469f5972309959b5476fd331c7db
Peter Tribble <peter.tribble@gmail.com>
2020-02-05 11:26:41 +0000

    Update Django 2.2.10 3.0.3

--- TRIBdjango3-python-37/build.sh
+++ TRIBdjango3-python-37/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-3.0.2
-cd Django-3.0.2
+${THOME}/build/unpack Django-3.0.3
+cd Django-3.0.3
 rm -fr /tmp/p3a
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango3-python-37 /tmp/p3a
