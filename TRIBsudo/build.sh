e2254e7c002be8044c47c19f4327721827dd8bc1
Peter Tribble <peter.tribble@gmail.com>
2020-02-07 12:08:43 +0000

    Update sudo 1.8.31

--- TRIBsudo/build.sh
+++ TRIBsudo/build.sh
@@ -2,8 +2,8 @@
 #
 # take most of the options and the bsm audit patches from OI
 #
-${THOME}/build/dobuild sudo-1.8.28p1 -C "--disable-hardening --with-project --with-rundir=/var/run/sudo --with-pam --with-pam-login --disable-pam-session --with-tty-tickets --without-insults --without-lecture --with-ignore-dot --with-bsm-audit"
+${THOME}/build/dobuild sudo-1.8.31 -C "--disable-hardening --with-project --with-rundir=/var/run/sudo --with-pam --with-pam-login --disable-pam-session --with-tty-tickets --without-insults --without-lecture --with-ignore-dot --with-bsm-audit"
 #
 # hack to avoid the install trying to chown the files
 #
-${THOME}/build/genpkg -I "install INSTALL_OWNER=" TRIBsudo sudo-1.8.28p1
+${THOME}/build/genpkg -I "install INSTALL_OWNER=" TRIBsudo sudo-1.8.31
