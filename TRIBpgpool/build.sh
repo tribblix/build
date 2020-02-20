4b62e3318425cce446fedea66e9a8751f7b9c1b4
Peter Tribble <peter.tribble@gmail.com>
2020-02-20 15:17:49 +0000

    Update pgpool 4.1.1

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,5 @@
 #
 # needs a postconf patch
 #
-env PATH=/opt/tribblix/postgres11/bin:$PATH ${THOME}/build/dobuild pgpool-II-4.1.0 -C "--sysconfdir=/etc --with-openssl"
-${THOME}/build/genpkg TRIBpgpool pgpool-II-4.1.0
+env PATH=/opt/tribblix/postgres11/bin:$PATH ${THOME}/build/dobuild pgpool-II-4.1.1 -C "--sysconfdir=/etc --with-openssl"
+${THOME}/build/genpkg TRIBpgpool pgpool-II-4.1.1
