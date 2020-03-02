ddbeac181cc3dc315fb40ac84574fa7632fdbfa1
Peter Tribble <peter.tribble@gmail.com>
2020-03-02 19:51:00 +0000

    Update pycurl 7.43.0.5

--- TRIBpycurl-python-27/build.sh
+++ TRIBpycurl-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycurl-7.43.0.4
-cd pycurl-7.43.0.4
+${THOME}/build/unpack pycurl-7.43.0.5
+cd pycurl-7.43.0.5
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
