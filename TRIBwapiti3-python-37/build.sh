280fb852b3fd8e29d8e125e45c75aa78f1875d4c
Peter Tribble <peter.tribble@gmail.com>
2020-03-02 18:56:46 +0000

    Update wapiti 3.0.3

--- TRIBwapiti3-python-37/build.sh
+++ TRIBwapiti3-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack wapiti3-3.0.2
-cd wapiti3-3.0.2
+${THOME}/build/unpack wapiti3-3.0.3
+cd wapiti3-3.0.3
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
