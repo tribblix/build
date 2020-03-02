c0cc310c5c1bf196bdb125ed8e274936547280fa
Peter Tribble <peter.tribble@gmail.com>
2020-03-02 19:38:35 +0000

    Update tld 0.11.10

--- TRIBtld-python-37/build.sh
+++ TRIBtld-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tld-0.11.9
-cd tld-0.11.9
+${THOME}/build/unpack tld-0.11.10
+cd tld-0.11.10
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
