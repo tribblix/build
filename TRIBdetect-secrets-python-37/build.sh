ecfbaa550a05f14375937dca85c1275a1da88701
Peter Tribble <peter.tribble@gmail.com>
2020-03-05 09:54:59 +0000

    Add detect_secrets 0.13.0

---
+++ TRIBdetect-secrets-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack detect_secrets-0.13.0
+cd detect_secrets-0.13.0
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBdetect-secrets-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
