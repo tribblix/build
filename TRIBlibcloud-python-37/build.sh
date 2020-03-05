932f026be59807c8b3a6b3740bec1d07beff109b
Peter Tribble <peter.tribble@gmail.com>
2020-03-05 11:00:48 +0000

    Update apache libcloud 2.8.1

--- TRIBlibcloud-python-37/build.sh
+++ TRIBlibcloud-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack apache-libcloud-2.8.0
-cd apache-libcloud-2.8.0
+${THOME}/build/unpack apache-libcloud-2.8.1
+cd apache-libcloud-2.8.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
