3a8f7c28ff724c1f79228801d74c736fdcbeaf8f
Peter Tribble <peter.tribble@gmail.com>
2020-03-07 21:37:38 +0000

    Update mkdocs 1.1

--- TRIBmkdocs-python-37/build.sh
+++ TRIBmkdocs-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack mkdocs-1.0.4
-cd mkdocs-1.0.4
+${THOME}/build/unpack mkdocs-1.1
+cd mkdocs-1.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
