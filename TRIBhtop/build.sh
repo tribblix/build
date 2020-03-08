686837eefea7b112567d9c676f93275202ce77bf
Peter Tribble <peter.tribble@gmail.com>
2020-03-08 19:07:38 +0000

    Only ship 64-bit htop

--- TRIBhtop/build.sh
+++ TRIBhtop/build.sh
@@ -8,12 +8,8 @@
 #
 # requires ncurses 6
 #
-$THOME/build/dobuild htop-2.2.0 -C --disable-unicode
-mv htop-2.2.0 htop-2.2.0-32
-#
 # 64-bit path to pick up 64-bit ncurses-config
 #
 env PATH=/usr/gnu/bin/`isainfo -k`:$PATH $THOME/build/dobuild +64 htop-2.2.0 -C --disable-unicode
-mv htop-2.2.0-32 htop-2.2.0
 
 ${THOME}/build/genpkg TRIBhtop htop-2.2.0
