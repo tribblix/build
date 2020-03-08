4a59519439aee0bdcbcc9d0c8a19aa4867cad832
Peter Tribble <peter.tribble@gmail.com>
2020-03-08 16:55:14 +0000

    Only ship the 64-bit top

--- TRIBtop/build.sh
+++ TRIBtop/build.sh
@@ -4,12 +4,13 @@
 # memory leak. That appears to no longer be the case.
 #
 # the build here is rather manual as top has its own ideas on how to
-# create a dual 32+64 bit build
+# create a dual 32+64 bit build, which we no longer use anyway
 #
 ${THOME}/build/unpack top-3.8beta1
 cd top-3.8beta1
 ${THOME}/build/patches/top-3.8beta1.preconf
 ./configure --prefix=/usr
+${THOME}/build/patches/top-3.8beta1.postconf
 gmake -j 8
 cd ..
 ${THOME}/build/genpkg TRIBtop top-3.8beta1
