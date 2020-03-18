5edaf13448ef3383d9f56945a7fd8b85b0a92060
Peter Tribble <peter.tribble@gmail.com>
2020-03-18 22:48:57 +0000

    Add Node.js v12.16.1

---
+++ TRIBv-node-v12/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# 64-bit only
+#
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v12.16.1 -P /usr/versions/node-v12 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v12 node-v12.16.1
