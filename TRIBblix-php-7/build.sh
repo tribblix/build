5e3a9f7873c01a2deae2511dc7bb17eb1874e707
Peter Tribble <peter.tribble@gmail.com>
2020-03-19 13:23:43 +0000

    Update PHP 7.3.16

--- TRIBblix-php-7/build.sh
+++ TRIBblix-php-7/build.sh
@@ -11,8 +11,8 @@
 # must have apache24 installed
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-7.2.28
-cd php-7.2.28
+${THOME}/build/unpack php-7.3.16
+cd php-7.3.16
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
@@ -20,15 +20,15 @@ cd php-7.2.28
 # support mysql (builtin), postgres (ship as a separate package)
 # grav needs zip and openssl
 #
-env AWK=nawk CFLAGS="-std=gnu99" ./configure --prefix=/opt/tribblix/php --with-ldap=no --with-libxml-dir=/usr --with-apxs2=/opt/tribblix/apache2/bin/apxs --enable-bcmath --enable-mbstring --enable-mysqlnd --with-pgsql=shared,/opt/tribblix/postgres11 --with-pdo-pgsql=shared,/opt/tribblix/postgres11 --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --without-sqlite3 --without-pdo-sqlite --with-curl=/usr --with-gd --with-jpeg-dir=/usr --with-png-dir=/usr --with-zlib-dir=/usr --with-freetype-dir=/usr --enable-sockets --enable-fpm --enable-zip --with-openssl
+env AWK=nawk CFLAGS="-std=gnu99" ./configure --prefix=/opt/tribblix/php --with-ldap=no --with-libxml-dir=/usr --with-apxs2=/opt/tribblix/apache2/bin/apxs --enable-bcmath --enable-mbstring --enable-mysqlnd --with-pgsql=shared,/opt/tribblix/postgres11 --with-pdo-pgsql=shared,/opt/tribblix/postgres11 --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --without-sqlite3 --without-pdo-sqlite --with-curl=/usr --with-gd --with-jpeg-dir=/usr --with-png-dir=/usr --with-zlib-dir=/usr --with-freetype-dir=/usr --enable-sockets --enable-fpm --enable-zip --with-openssl --without-libzip
 gsed -i 's:ext/sockets/ \$:ext/sockets/ -D_XPG4_2 \$:' Makefile
 gmake -j 6
 rm -fr /tmp/m2 /tmp/m2g /tmp/pear
 mkdir -p /tmp/m2/opt/tribblix/apache2/conf
-mkdir -p /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20170718
+mkdir -p /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20180731
 cp /opt/tribblix/apache2/conf/original/httpd.conf /tmp/m2/opt/tribblix/apache2/conf
 gmake install INSTALL_ROOT=/tmp/m2
-mv /tmp/m2/opt/tribblix/php/lib/php/extensions/no-debug-zts-20170718/*pgsql* /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20170718
+mv /tmp/m2/opt/tribblix/php/lib/php/extensions/no-debug-zts-20180731/*pgsql* /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20180731
 ${THOME}/build/create_pkg TRIBblix-php-7 /tmp/m2
 ${THOME}/build/create_pkg TRIBblix-php-7pg /tmp/m2g
 rm -fr /tmp/m2 /tmp/m2g /tmp/pear
