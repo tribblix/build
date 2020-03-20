293eeadbca124b0dc98ed784544830cb3897d85d
Peter Tribble <peter.tribble@gmail.com>
2020-03-20 14:51:31 +0000

    Update pandas 1.0.3

--- TRIBpandas-python-37/build.sh
+++ TRIBpandas-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pandas-0.23.4
-cd pandas-0.23.4
+${THOME}/build/unpack pandas-1.0.3
+cd pandas-1.0.3
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
