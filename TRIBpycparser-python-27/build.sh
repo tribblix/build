048501a91a46f0311deed84f888eab0846ab9739
Peter Tribble <peter.tribble@gmail.com>
2020-03-21 12:07:30 +0000

    Update pycparser 2.20

--- TRIBpycparser-python-27/build.sh
+++ TRIBpycparser-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycparser-2.19
-cd pycparser-2.19
+${THOME}/build/unpack pycparser-2.20
+cd pycparser-2.20
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBpycparser-python-37/build.sh
+++ TRIBpycparser-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycparser-2.19
-cd pycparser-2.19
+${THOME}/build/unpack pycparser-2.20
+cd pycparser-2.20
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
