8e026c94d903ab982cf81feda0760b0aa12e67cf
Peter Tribble <peter.tribble@gmail.com>
2020-03-21 12:43:20 +0000

    Update idna 2.9

--- TRIBidna-python-27/build.sh
+++ TRIBidna-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack idna-2.8
-cd idna-2.8
+${THOME}/build/unpack idna-2.9
+cd idna-2.9
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBidna-python-37/build.sh
+++ TRIBidna-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack idna-2.8
-cd idna-2.8
+${THOME}/build/unpack idna-2.9
+cd idna-2.9
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
