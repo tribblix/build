0e07bb5ab79ea15c52fc6144a217553c70585e29
Peter Tribble <peter.tribble@gmail.com>
2020-03-21 09:13:43 +0000

    Update kazoo 2.7.0

--- TRIBkazoo-python-37/build.sh
+++ TRIBkazoo-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack kazoo-2.6.1
-cd kazoo-2.6.1
+${THOME}/build/unpack kazoo-2.7.0
+cd kazoo-2.7.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
