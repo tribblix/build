42f0921c6de3cd520f90d34f253c76ee430a754f
Peter Tribble <peter.tribble@gmail.com>
2020-03-21 17:05:52 +0000

    Update xkbcomp 1.4.3

--- TRIBx11-keyboard-xkbcomp/build.sh
+++ TRIBx11-keyboard-xkbcomp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xkbcomp-1.4.2
-${THOME}/build/genpkg TRIBx11-keyboard-xkbcomp xkbcomp-1.4.2
+${THOME}/build/dobuild xkbcomp-1.4.3
+${THOME}/build/genpkg TRIBx11-keyboard-xkbcomp xkbcomp-1.4.3
