6808322455269d6787480e40be4bf50449dd5400
Peter Tribble <peter.tribble@gmail.com>
2020-03-22 16:25:14 +0000

    Update ggrep 3.4

--- TRIBtext-gnu-grep/build.sh
+++ TRIBtext-gnu-grep/build.sh
@@ -3,5 +3,5 @@
 # ggrep
 # NB. Must not have gnu iconv installed
 #
-${THOME}/build/dobuild -64 -gnu grep-3.3 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-grep grep-3.3
+${THOME}/build/dobuild -64 -gnu grep-3.4 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-grep grep-3.4
