155ef9f11c1cb81a7961afc255150cd73b52319a
Peter Tribble <peter.tribble@gmail.com>
2020-03-25 14:21:43 +0000

    Update asgiref 3.2.7

--- TRIBasgiref-python-37/build.sh
+++ TRIBasgiref-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack asgiref-3.2.6
-cd asgiref-3.2.6
+${THOME}/build/unpack asgiref-3.2.7
+cd asgiref-3.2.7
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
