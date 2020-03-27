34cb66f00d4afa24700ce2f175ac8e1efeb327bf
Peter Tribble <peter.tribble@gmail.com>
2020-03-27 14:04:02 +0000

    Update seaweedfs 1.69

--- TRIBblix-seaweedfs/build.sh
+++ TRIBblix-seaweedfs/build.sh
@@ -4,13 +4,26 @@
 # git checkout, but use the latest release as the pkg version as that's
 # what weed reports itself
 #
+# this doesn't work, produces binaries that panic
+# setenv GOPATH=`pwd` go get github.com/chrislusf/seaweedfs/weed
+#
 mkdir w
 cd w
-env GOPATH=`pwd` go get github.com/chrislusf/seaweedfs/weed
+git clone github.com/chrislusf/seaweedfs/weed
+cd weed
+env GOPATH=`pwd` gmake
+#
+# you will need to fix:
+#
+# pkg/mod/github.com/\!azure/azure-storage-blob-go@v0.8.0/azblob/zc_mmf_unix.go
+# -> it's unix.Mmap and unix.Munmap, not syscall, and build solaris
+# (or have a separate solaris file)
+#
+# and then run the gmake again
 #
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/seaweedfs/bin
-cp bin/weed /tmp/ee/opt/tribblix/seaweedfs/bin
-cp src/github.com/chrislusf/seaweedfs/README.md /tmp/ee/opt/tribblix/seaweedfs
+cp weed/weed /tmp/ee/opt/tribblix/seaweedfs/bin
+cp README.md /tmp/ee/opt/tribblix/seaweedfs
 ${THOME}/build/create_pkg TRIBblix-seaweedfs /tmp/ee
 rm -fr /tmp/ee
