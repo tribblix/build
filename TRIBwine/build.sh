65700fd020957e349fdf9079bd4ce78dfc887bc1
Peter Tribble <peter.tribble@gmail.com>
2020-03-28 16:45:24 +0000

    Update wine 4.0.3

--- TRIBwine/build.sh
+++ TRIBwine/build.sh
@@ -2,5 +2,5 @@
 #
 # wine
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-3.0.5
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-3.0.5
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu wine-4.0.3
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBwine wine-4.0.3
