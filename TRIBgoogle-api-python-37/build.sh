169bed04f4638ae213bb8665f8cdac4f90b8e2c0
Peter Tribble <peter.tribble@gmail.com>
2020-03-29 13:10:49 +0100

    Update google-api 1.8.0

--- TRIBgoogle-api-python-37/build.sh
+++ TRIBgoogle-api-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-api-python-client-1.7.12
-cd google-api-python-client-1.7.12
+${THOME}/build/unpack google-api-python-client-1.8.0
+cd google-api-python-client-1.8.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
