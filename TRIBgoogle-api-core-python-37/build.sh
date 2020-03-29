875fd898013bc923166bff4d404bf0a489d21b40
Peter Tribble <peter.tribble@gmail.com>
2020-03-29 13:41:17 +0100

    Add google-api-core 1.16.0

---
+++ TRIBgoogle-api-core-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack google-api-core-1.16.0
+cd google-api-core-1.16.0
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBgoogle-api-core-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
