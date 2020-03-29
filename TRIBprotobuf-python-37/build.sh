62c7b1cc0c700cf1271702fd315f3dfaac682df7
Peter Tribble <peter.tribble@gmail.com>
2020-03-29 13:27:47 +0100

    Add protobuf python module

---
+++ TRIBprotobuf-python-37/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# entertainingly, the python module has a filename clash
+# with the regular protobuf download
+#
+${THOME}/build/unpack protobuf-3.11.3
+cd protobuf-3.11.3
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBprotobuf-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
