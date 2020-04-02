82f6f6eca644a8d3523d5b2f904757b337258515
Peter Tribble <peter.tribble@gmail.com>
2020-04-02 11:06:06 +0100

    Add duplicity 0.8.12

---
+++ TRIBduplicity-python-37/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# the backends are soft requirements, I add those we actually need (AWS)
+# and those we already have packages for.
+# paramiko (enables ssh) boto3 (enables S3)
+# other optional packages it can leverage that aren't listed as dependencies
+# rsync rclone ncftp
+#
+${THOME}/build/unpack duplicity-0.8.12.1612
+cd duplicity-0.8.12.1612
+env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBduplicity-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
