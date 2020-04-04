731be05d97b76549c9af4bbb146d47a7ff9739cf
Peter Tribble <peter.tribble@gmail.com>
2020-04-04 14:50:45 +0100

    Update findutils 4.7.0

--- TRIBfile-gnu-findutils/build.sh
+++ TRIBfile-gnu-findutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu findutils-4.6.0 -P /usr/gnu
-${THOME}/build/genpkg TRIBfile-gnu-findutils findutils-4.6.0
+${THOME}/build/dobuild -64 -gnu findutils-4.7.0 -P /usr/gnu
+${THOME}/build/genpkg TRIBfile-gnu-findutils findutils-4.7.0
