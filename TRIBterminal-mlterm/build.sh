8a8d2392e384ac057d2f8714af904d54c1a27185
Peter Tribble <peter.tribble@gmail.com>
2020-04-11 21:56:01 +0100

    Update mlterm 3.8.9

--- TRIBterminal-mlterm/build.sh
+++ TRIBterminal-mlterm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 #
-$THOME/build/dobuild mlterm-3.8.5
-${THOME}/build/genpkg TRIBterminal-mlterm mlterm-3.8.5
+$THOME/build/dobuild mlterm-3.8.9
+${THOME}/build/genpkg TRIBterminal-mlterm mlterm-3.8.9
