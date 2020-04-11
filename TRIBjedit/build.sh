906a49a93794be847f551d0b2f90f255effe1c3f
Peter Tribble <peter.tribble@gmail.com>
2020-04-11 16:36:18 +0100

    Update jEdit 5.5.0

--- TRIBjedit/build.sh
+++ TRIBjedit/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
 rm -fr /tmp/j1 /tmp/jEdit /tmp/man /tmp/bin
-java -jar ${THOME}/tarballs/jedit5.4.0install.jar 
+java -jar ${THOME}/tarballs/jedit5.5.0install.jar 
 # Install under /tmp instead of $HOME, not the API documentation
 mkdir -p /tmp/j1/usr/lib/jEdit
 mkdir -p /tmp/j1/usr/bin
 mkdir -p /tmp/j1/usr/share/man/man1
-mv /tmp/jEdit/5.4.0 /tmp/j1/usr/lib/jEdit
+mv /tmp/jEdit/5.5.0 /tmp/j1/usr/lib/jEdit
 mv /tmp/man/man1/jedit.1 /tmp/j1/usr/share/man/man1
 mv /tmp/bin/jedit /tmp/j1/usr/bin
-gsed -i s:/tmp/jEdit/5.4.0/:/usr/lib/jEdit/5.4.0/: /tmp/j1/usr/bin/jedit
+gsed -i s:/tmp/jEdit/5.5.0/:/usr/lib/jEdit/5.5.0/: /tmp/j1/usr/bin/jedit
 ${THOME}/build/create_pkg TRIBjedit /tmp/j1
 rm -fr /tmp/j1 /tmp/jEdit /tmp/man /tmp/bin
