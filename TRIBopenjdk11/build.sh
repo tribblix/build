42f6688a05cf1992897ec66964da28f2da8f36b2
Peter Tribble <peter.tribble@gmail.com>
2020-04-15 16:24:23 +0100

    Update openjdk11 11.0.7, and switch to the Studio toolchain

--- TRIBopenjdk11/build.sh
+++ TRIBopenjdk11/build.sh
@@ -21,13 +21,34 @@
 # 11.0.5 release
 # wget http://hg.openjdk.java.net/jdk-updates/jdk11u/archive/jdk-11.0.5+10.tar.bz2
 #
+# 11.0.6 release
+# wget http://hg.openjdk.java.net/jdk-updates/jdk11u/archive/jdk-11.0.6+10.tar.bz2
+#
+# to get the build number, go to the jdk11u repo
+# http://hg.openjdk.java.net/jdk-updates/jdk11u/
+# there should be a -ga tag for each release
+# and there should be a build-numbered tag with the same commit
+# and it's that build tag you want to download
+#
 cd ${THOME}/tarballs
-wget http://hg.openjdk.java.net/jdk-updates/jdk11u/archive/jdk-11.0.6+10.tar.bz2
+wget http://hg.openjdk.java.net/jdk-updates/jdk11u/archive/jdk-11.0.7+10.tar.bz2
+#
+# fix the tarball name to match the directory it unpacks into
+# this needed to get the patches to apply correctly
+#
+ln jdk-11.0.7+10.tar.bz2 jdk11u-jdk-11.0.7+10.tar.bz2
 
 cd ~/ud
-${THOME}/build/unpack jdk-11.0.6+10
-cd jdk11u-jdk-11.0.6+10
+${THOME}/build/unpack jdk11u-jdk-11.0.7+10
+cd jdk11u-jdk-11.0.7+10
 
+#
+# as of 11.0.7, switch to a gcc build to replace Stuido
+#
+
+#
+# Not for gcc builds, and ignoring for now as j2ucrypto is deprecated
+# in more recent versions
 #
 # We need a copy of libsoftcrypto.h
 # this was formerly shipped with the source, but got removed as the
@@ -44,12 +65,7 @@ cd jdk11u-jdk-11.0.6+10
 #
 # cp libsoftcrypto.h jdk/src/jdk.crypto.ucrypto/solaris/native/libj2ucrypto
 #
-cp ${THOME}/build/patches/jdk-libsoftcrypto.h src/jdk.crypto.ucrypto/solaris/native/libj2ucrypto/libsoftcrypto.h
-
-#
-# jdk10 needs objcopy, which is in /usr/gnu/bin
-# but the objcopy stuff doesn't actually work
-#
+#cp ${THOME}/build/patches/jdk-libsoftcrypto.h src/jdk.crypto.ucrypto/solaris/native/libj2ucrypto/libsoftcrypto.h
 
 #
 # jdk11 needs autoconf installed
@@ -57,64 +73,26 @@ cp ${THOME}/build/patches/jdk-libsoftcrypto.h src/jdk.crypto.ucrypto/solaris/nat
 zap install autoconf
 
 #
-# jdk10 wants Studio 5.13 == Studio 12.4
-#
-
-#
-# I have no idea what the version string should look like
-#
-
-#
-# for Solaris 10:
-# src/java.desktop/share/native/libfontmanager/harfbuzz/hb-blob.cc
-# the POSIX+C_SOURCE thing is broken - set it to 199506L
-#
-
-#
-# build on Tribblix m20.5 or later, which has the libc compatibility fixes
-#
-# cd $HOME
-# pbzcat /packages/localsrc/Studio/Studio12.4/SolarisStudio12.4-solaris-x86-bin.tar.bz2 | tar xf -
-# mv SolarisStudio12.4-solaris-x86-bin/solarisstudio12.4 .
-#
-# iropt needs libmmheap.so.1
-#
-# OK, they're in the Studio12.4 download
-# ~/SolarisStudio12.4-solaris-x86-bin/patches/system/120754-14
-# SUNWlibmsr/reloc/lib/{,amd64/}libmmheap.so.1
-#
-# cp libmmheap.so.1 ~/solarisstudio12.4/lib/compilers/sys/
-# cp amd64/libmmheap.so.1 ~/solarisstudio12.4/lib/compilers/sys/amd64/
-#
-# it wants objcopy, hence /usr/sfw/bin in the path
-#
-#
-
-#
-# !!!!!!!!!!!!!! YOU MUST FIX THE FOLLOWING !!!!!!!!!!!!!!
-#
-# src/hotspot/os/solaris/perfMemory_solaris.cpp
-# fix the d_fd error -> dd_fd
-#
-# src/java.base/solaris/native/libnio/ch/DevPollArrayWrapper.c
-# need to #include <signal.h> explicitly before devpoll.h
-#
-
-#
 # the gobjcopy stuff doesn't actually work, so disable it
 # --with-native-debug-symbols=none
 #
-env PATH=${HOME}/solarisstudio12.4/bin:/usr/bin:/usr/sbin:/usr/sfw/bin bash ./configure --enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk10 --with-native-debug-symbols=none
-env PATH=${HOME}/solarisstudio12.4/bin:/usr/bin:/usr/sbin:/usr/sfw/bin gmake -k all
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk10 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-hotspot-gtest --disable-dtrace \
+--disable-warnings-as-errors
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
 
 #
 # first testing looks like this:
 #
 # cd build/solaris-x86_64-normal-server-release/images/jdk
 # ./bin/java -version
-#openjdk version "11.0.6-internal" 2020-01-14
-#OpenJDK Runtime Environment (build 11.0.6-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.610)
-#OpenJDK 64-Bit Server VM (build 11.0.6-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.610, mixed mode)
+#openjdk version "11.0.7-internal" 2020-04-14
+#OpenJDK Runtime Environment (build 11.0.7-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.710)
+#OpenJDK 64-Bit Server VM (build 11.0.7-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.710, mixed mode)
 #
 
 rm -fr /tmp/jdk
@@ -130,33 +108,21 @@ ln -s ../versions/openjdk11 .
 #mkdir -p /tmp/jdk/usr/bin
 #cd /tmp/jdk/usr/bin
 #ln -s ../jdk/latest/bin/* .
-#rm amd64
+#rm -f amd64 sparcv9
 cd /tmp/jdk/usr/versions/openjdk11
 rm `find . -name '*.diz'`
 
 #
 # need to create a certificate bundle
 #
-# the jdk11 build appears to create a working one, so leave it be
+# the jdk11 build appears to create a working one, but it uses
+# a horribly outdated set of certificates, so replace it
 #
 # this actually needs to be a JKS keystore, not a PKCS12 keystore
 # https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1739631
 #
-#${THOME}/build/patches/mkcacerts -f /etc/openssl/cacert.pem -o /tmp/cacerts -k /usr/jdk/instances/jdk1.8.0/bin/keytool -s /usr/bin/openssl
+${THOME}/build/patches/mkcacerts -f /etc/openssl/cacert.pem -o /tmp/cacerts -k /usr/jdk/instances/jdk1.8.0/bin/keytool -s /usr/bin/openssl
 #
-#cp /tmp/cacerts /tmp/jdk/usr/versions/openjdk11/lib/security
-
-#
-# edit conf/security/sunpkcs11-solaris.cfg and add the following to disabledMechanisms
-#
-# again, doesn't seem to be necessary
-#
-# # the following mechanisms are disabled due to lack of digest cloning support
-# # need to fix 6414899 first
-#   CKM_MD5
-#   CKM_SHA256
-#   CKM_SHA384
-#   CKM_SHA512
-#
+cp /tmp/cacerts /tmp/jdk/usr/versions/openjdk11/lib/security
 
 ${THOME}/build/create_pkg TRIBopenjdk11 /tmp/jdk
