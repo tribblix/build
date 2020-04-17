4ce06be22b4a1ecce3b5b83bcfa21dd53c4895d3
Peter Tribble <peter.tribble@gmail.com>
2020-04-17 21:50:55 +0100

    Update gtk3 3.22.30

--- TRIBlib-desktop-gtk3/build.sh
+++ TRIBlib-desktop-gtk3/build.sh
@@ -3,6 +3,7 @@
 # zap install TRIBlib-desktop-atk-bridge
 # zap install TRIBlib-desktop-at-spi2
 # zap install TRIBx11-libepoxy (as of 3.16.7)
+# harfbuzz a dependency as of 3.20
 #
-${THOME}/build/dobuild -64 -gnu gtk+-3.18.9 -C "--disable-introspection --disable-cups"
-${THOME}/build/genpkg TRIBlib-desktop-gtk3 gtk+-3.18.9
+${THOME}/build/dobuild -64 -gnu gtk+-3.22.30 -C "--disable-introspection --disable-cups"
+${THOME}/build/genpkg TRIBlib-desktop-gtk3 gtk+-3.22.30
