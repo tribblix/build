899209bb881ece42ccff0211c2186cc124aa43f8
Peter Tribble <peter.tribble@gmail.com>
2020-04-18 12:31:28 +0100

    Fix gobject-introspection to avoid -no-as-needed

--- TRIBgobject-introspection/build.sh
+++ TRIBgobject-introspection/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
-# [fix - build pango to extract the gir files]
+# fix - build pango to extract the gir files
+# note that you need to have gobject-introspection installed
+# first in order for the sacrificial pango build to generate
+# its gir files
 #
 # there may be other packages that don't get picked up correctly
 # gir coverage is rather spotty
 #
-${THOME}/build/dobuild pango-1.36.8
+${THOME}/build/dobuild pango-1.42.4
 ${THOME}/build/dobuild gobject-introspection-1.44.0
 ${THOME}/build/genpkg TRIBgobject-introspection gobject-introspection-1.44.0
---
+++ patches/gobject-introspection-1.44.0.pls
@@ -0,0 +1,1 @@
+-p1 gir-no-as-needed.patch
