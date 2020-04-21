ced264016ebf82ffe0b1a187d4cec8740c29487b
Peter Tribble <peter.tribble@gmail.com>
2020-04-21 19:16:48 +0100

    Add Node.JS v14.0.0

---
+++ TRIBv-node-v14/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# 64-bit only
+#
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v14.0.0 -P /usr/versions/node-v14 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v14 node-v14.0.0
