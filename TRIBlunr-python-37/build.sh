0a9acd10dd38ec305308beff635b3349cb840af4
Peter Tribble <peter.tribble@gmail.com>
2020-04-22 19:00:24 +0100

    Update lunr 0.5.8

--- TRIBlunr-python-37/build.sh
+++ TRIBlunr-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack lunr-0.5.6
-cd lunr-0.5.6
+${THOME}/build/unpack lunr-0.5.8
+cd lunr-0.5.8
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
