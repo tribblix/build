dc7754e14aac2ae2e76f0a25336fa3cdb8eb82fb
Peter Tribble <peter.tribble@gmail.com>
2020-04-22 19:33:33 +0100

    Update libssh 0.9.4

--- TRIBlibssh/build.sh
+++ TRIBlibssh/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/cmbuild -64 libssh-0.7.6
-${THOME}/build/genpkg TRIBlibssh libssh-0.7.6
+${THOME}/build/cmbuild -64 libssh-0.9.4 -C "-DWITH_STACK_PROTECTOR_STRONG=false -DWITH_STACK_PROTECTOR=false"
+${THOME}/build/genpkg TRIBlibssh libssh-0.9.4
