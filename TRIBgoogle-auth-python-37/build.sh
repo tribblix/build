e65883724b692b033c148a1e6d411e670a968403
Peter Tribble <peter.tribble@gmail.com>
2020-04-22 19:07:19 +0100

    Update google-auth 1.14.1 google-api-core 1.17.0

--- TRIBgoogle-auth-python-37/build.sh
+++ TRIBgoogle-auth-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-auth-1.12.0
-cd google-auth-1.12.0
+${THOME}/build/unpack google-auth-1.14.1
+cd google-auth-1.14.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
