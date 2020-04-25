d401d82c2b6a65174a291322b20ee157cba76033
Peter Tribble <peter.tribble@gmail.com>
2020-04-25 10:26:12 +0100

    Update gkrellm 2.3.11

--- TRIBgkrellm/build.sh
+++ TRIBgkrellm/build.sh
@@ -2,11 +2,11 @@
 #
 #
 #
-${THOME}/build/unpack gkrellm-2.3.7
-cd gkrellm-2.3.7
-gsed -i s:/usr/ucb/install:/usr/bin/ginstall: */Makefile
-gsed -i s:chgrp:true: */Makefile
-gsed -i 's:lsocket -lX11:lsocket -lnsl -lX11:' Makefile
+${THOME}/build/unpack gkrellm-2.3.11
+cd gkrellm-2.3.11
+sed -i s:/usr/ucb/install:/usr/bin/ginstall: */Makefile
+sed -i s:chgrp:true: */Makefile
+sed -i 's:lsocket -lX11:lsocket -lnsl -lX11:' Makefile
 gmake solaris LOCALEDIR=/usr/share/locale
 rm -fr /tmp/gk
 gmake install_solaris INSTALL=ginstall DESTDIR=/tmp/gk PREFIX=/usr
