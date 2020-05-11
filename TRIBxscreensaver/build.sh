8a7ef24d71492d672173afc0e4c05c058ff1e093
Peter Tribble <peter.tribble@gmail.com>
2020-05-11 15:47:00 +0100

    Update XScreenSaver 5.44

--- TRIBxscreensaver/build.sh
+++ TRIBxscreensaver/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xscreensaver-5.43 -C "--with-gtk"
-cd xscreensaver-5.43
+${THOME}/build/dobuild -gnu xscreensaver-5.44 -C "--with-gtk"
+cd xscreensaver-5.44
 rm -fr /tmp/xs
 gmake install install_prefix=/tmp/xs
 ${THOME}/build/create_pkg TRIBxscreensaver /tmp/xs
