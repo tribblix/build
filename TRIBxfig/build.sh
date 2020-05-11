48adf5b41ccef6a0e6b4098d1eb539f615d671ca
Peter Tribble <peter.tribble@gmail.com>
2020-05-11 15:35:20 +0100

    Update XFig 3.2.7b

--- TRIBxfig/build.sh
+++ TRIBxfig/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xfig-3.2.7a -C --without-xaw3d
-${THOME}/build/dobuild fig2dev-3.2.7a
-${THOME}/build/genpkg TRIBxfig xfig-3.2.7a fig2dev-3.2.7a
+${THOME}/build/dobuild xfig-3.2.7b -C --without-xaw3d
+${THOME}/build/dobuild fig2dev-3.2.7b
+${THOME}/build/genpkg TRIBxfig xfig-3.2.7b fig2dev-3.2.7b
