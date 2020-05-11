846e2abee33a431800c207227a161e4b71568c1c
Peter Tribble <peter.tribble@gmail.com>
2020-05-11 09:12:12 +0100

    Add pg_activity 1.6.0

---
+++ TRIBpgactivity/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pg_activity-1.6.0
+cd pg_activity-1.6.0
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil --with-man
+${THOME}/build/create_pkg TRIBpgactivity /tmp/pil
+cd ..
+rm -fr /tmp/pil
