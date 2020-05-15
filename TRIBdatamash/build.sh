d27dd1e21e2b71e6b538c8242e4dc2c1d24e378b
Peter Tribble <peter.tribble@gmail.com>
2020-05-15 10:10:46 +0100

    Update datamash 1.7

--- TRIBdatamash/build.sh
+++ TRIBdatamash/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 #
-${THOME}/build/dobuild datamash-1.1.0
-${THOME}/build/genpkg TRIBdatamash datamash-1.1.0
+${THOME}/build/dobuild datamash-1.7
+${THOME}/build/genpkg TRIBdatamash datamash-1.7
