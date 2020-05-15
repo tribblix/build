6a0df750a20464fd70ef5d5f7f00c0dcb28d60e0
Peter Tribble <peter.tribble@gmail.com>
2020-05-15 13:02:26 +0100

    Build our own docbook package

---
+++ TRIBdata-docbook/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# you need to download the files
+# and the fixit script does the rest of the work
+#
+$THOME/build/dobuild sgml-common-0.6.3 -C "--sysconfdir=/etc --with-docdir=/usr/share/nodoc"
+${THOME}/build/genpkg TRIBdata-docbook sgml-common-0.6.3
