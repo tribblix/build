fbef069a2a447bbc1cffc5b0c05b80aaa9473452
Peter Tribble <peter.tribble@gmail.com>
2020-05-16 20:01:34 +0100

    Update python 3.7.7

--- TRIBv-python-37/build.sh
+++ TRIBv-python-37/build.sh
@@ -3,6 +3,6 @@
 # NB. TRIBv-python-37 must not be installed, or the make
 # install step will get confused
 #
-${THOME}/build/dobuild Python-3.7.6 -P /usr/versions/python-3.7 -L -C --enable-shared
+${THOME}/build/dobuild Python-3.7.7 -P /usr/versions/python-3.7 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-37 Python-3.7.6
+${THOME}/build/genpkg TRIBv-python-37 Python-3.7.7
