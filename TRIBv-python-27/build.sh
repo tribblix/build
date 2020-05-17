d860f61a714582bc9a2fc949a8566ccfae15b1e5
Peter Tribble <peter.tribble@gmail.com>
2020-05-17 16:43:38 +0100

    Update python 2.7.18, and split out test files

--- TRIBv-python-27/build.sh
+++ TRIBv-python-27/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild Python-2.7.17 -P /usr/versions/python-2.7 -L -C --enable-shared
-${THOME}/build/genpkg TRIBv-python-27 Python-2.7.17
+${THOME}/build/dobuild Python-2.7.18 -P /usr/versions/python-2.7 -L -C --enable-shared
+${THOME}/build/genpkg TRIBv-python-27 Python-2.7.18
