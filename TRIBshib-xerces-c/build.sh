991ba15dc1e9050c8435494e83fd236903dbbdc2
Peter Tribble <peter.tribble@gmail.com>
2020-05-17 12:32:05 +0100

    Bump and rebuild shibboleth SP stack

--- TRIBshib-xerces-c/build.sh
+++ TRIBshib-xerces-c/build.sh
@@ -2,5 +2,5 @@
 #
 # shibboleth stack requires boost ssl curl apache
 #
-${THOME}/build/dobuild xerces-c-3.2.2 -P /opt/tribblix/shibboleth-sp -C "--disable-static --enable-netaccessor-socket"
-${THOME}/build/genpkg TRIBshib-xerces-c xerces-c-3.2.2
+${THOME}/build/dobuild xerces-c-3.2.3 -P /opt/tribblix/shibboleth-sp -C "--disable-static --enable-netaccessor-socket"
+${THOME}/build/genpkg TRIBshib-xerces-c xerces-c-3.2.3
--- TRIBshib-xmltooling-c/build.sh
+++ TRIBshib-xmltooling-c/build.sh
@@ -2,5 +2,5 @@
 #
 # shibboleth stack requires boost ssl curl apache
 #
-env PKG_CONFIG_PATH=/opt/tribblix/shibboleth-sp/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild xmltooling-3.0.4 -P /opt/tribblix/shibboleth-sp -C "--disable-static --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBshib-xmltooling-c xmltooling-3.0.4
+env PKG_CONFIG_PATH=/opt/tribblix/shibboleth-sp/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild xmltooling-3.1.0 -P /opt/tribblix/shibboleth-sp -C "--disable-static --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBshib-xmltooling-c xmltooling-3.1.0
