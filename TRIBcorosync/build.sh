5e54d73acc149648c4969373dd26af55796bc326
Peter Tribble <peter.tribble@gmail.com>
2020-05-29 10:27:51 +0100

    Update corosync 2.4.5

--- TRIBcorosync/build.sh
+++ TRIBcorosync/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# 64-bit build fails as it picks up 32-bit NSS
+# postconf script to fix up use of the wrong awk
 #
-${THOME}/build/dobuild corosync-2.4.2 -C "--localstatedir=/var --sysconfdir=/etc"
-${THOME}/build/genpkg TRIBcorosync corosync-2.4.2
+${THOME}/build/dobuild corosync-2.4.5 -64 -C "--localstatedir=/var --sysconfdir=/etc"
+${THOME}/build/genpkg TRIBcorosync corosync-2.4.5
