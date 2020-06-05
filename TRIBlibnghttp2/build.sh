fd7e44f48fb0218c8029a514822b3b90bd66eca7
Peter Tribble <peter.tribble@gmail.com>
2020-06-05 11:07:10 +0100

    Update nghttp2 1.41.0, drop the apps

--- TRIBlibnghttp2/build.sh
+++ TRIBlibnghttp2/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-# Need to install TRIBlibcares for the applications to build
+# just the library
 #
-env LIBS="-lsocket -lnsl" TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 nghttp2-1.31.1
-${THOME}/build/genpkg TRIBlibnghttp2 nghttp2-1.31.1
-${THOME}/build/genpkg TRIBnghttp2-apps nghttp2-1.31.1
+${THOME}/build/dobuild -64 nghttp2-1.41.0 -C --enable-lib-only
+${THOME}/build/genpkg TRIBlibnghttp2 nghttp2-1.41.0
--- TRIBnghttp2-apps/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-echo "See TRIBlibnghttp2 - both packages are created from a single build"
