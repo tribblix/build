045dfce29f7cc6736c7ee29e748d787852a0aabb
Peter Tribble <peter.tribble@gmail.com>
2020-06-07 15:50:03 +0100

    Update python docker 4.2.1

--- TRIBdocker-python-37/build.sh
+++ TRIBdocker-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack docker-4.2.0
-cd docker-4.2.0
+${THOME}/build/unpack docker-4.2.1
+cd docker-4.2.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
