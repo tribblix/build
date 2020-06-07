2207fed7806321eeccc013704937b6a1121dd049
Peter Tribble <peter.tribble@gmail.com>
2020-06-07 16:30:59 +0100

    Update citus 9.2.4

--- TRIBblix-citus11/build.sh
+++ TRIBblix-citus11/build.sh
@@ -2,5 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-${THOME}/build/dobuild citus-9.2.3 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus11 citus-9.2.3
+${THOME}/build/dobuild citus-9.2.4 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus11 citus-9.2.4
--- TRIBblix-citus12/build.sh
+++ TRIBblix-citus12/build.sh
@@ -2,5 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-${THOME}/build/dobuild citus-9.2.3 -C PG_CONFIG=/opt/tribblix/postgres12/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus12 citus-9.2.3
+${THOME}/build/dobuild citus-9.2.4 -C PG_CONFIG=/opt/tribblix/postgres12/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus12 citus-9.2.4
