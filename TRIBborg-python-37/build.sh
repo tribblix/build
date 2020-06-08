9337e56c7607b449d0a459e86bb86855ff3deade
Peter Tribble <peter.tribble@gmail.com>
2020-06-08 09:19:28 +0100

    Update borgbackup 1.1.13

--- TRIBborg-python-37/build.sh
+++ TRIBborg-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack borgbackup-1.1.11
-cd borgbackup-1.1.11
+${THOME}/build/unpack borgbackup-1.1.13
+cd borgbackup-1.1.13
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
