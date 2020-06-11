8dd93488aa839275ffb7fdb1c05e55f60725448e
Peter Tribble <peter.tribble@gmail.com>
2020-06-11 10:10:42 +0100

    Update dateparser 0.7.5

--- TRIBdateparser-python-37/build.sh
+++ TRIBdateparser-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack dateparser-0.7.4
-cd dateparser-0.7.4
+${THOME}/build/unpack dateparser-0.7.5
+cd dateparser-0.7.5
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
