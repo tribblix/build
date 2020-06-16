a771bbbffa65df9c0e7af42bf07c3f61d80042c3
Peter Tribble <peter.tribble@gmail.com>
2020-06-16 08:57:44 +0100

    Update file 5.39

--- TRIBfile-magic/build.sh
+++ TRIBfile-magic/build.sh
@@ -2,5 +2,5 @@
 #
 # if the version gets updated, rebuild TRIBmog to match
 #
-${THOME}/build/dobuild -64 file-5.37
-${THOME}/build/genpkg TRIBfile-magic file-5.37
+${THOME}/build/dobuild -64 file-5.39
+${THOME}/build/genpkg TRIBfile-magic file-5.39
