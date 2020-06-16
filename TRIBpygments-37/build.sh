9c46ac8d995b4280d5d39dfa51192735b4934698
Peter Tribble <peter.tribble@gmail.com>
2020-06-16 09:10:07 +0100

    Update Pygments 2.6.1, remove python2 module

--- TRIBpygments-37/build.sh
+++ TRIBpygments-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Pygments-2.5.2
-cd Pygments-2.5.2
+${THOME}/build/unpack Pygments-2.6.1
+cd Pygments-2.6.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pdu
