94b2679f49f0f6d5f39bf538ec0d9e5c29bd947a
Peter Tribble <peter.tribble@gmail.com>
2020-06-16 09:05:45 +0100

    Rebuild mog for new file and switch to python3

--- TRIBmog/build.sh
+++ TRIBmog/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-# needs pygments and file-magic amd mediainfo
+# needs pygments and file-magic and mediainfo
 #
 ${THOME}/build/unpack mog-0.6.0
 cd mog-0.6.0
 #
 # we need the file from file-magic
 #
-gsed -i "s:\['file':\['file-5.37':" mog/__init__.py
-python setup.py build
+gsed -i "s:\['file':\['file-5.39':" mog/__init__.py
+/usr/versions/python-3.7/bin/python3  setup.py build
 rm -fr /tmp/sut
-python setup.py install --root=/tmp/sut
+/usr/versions/python-3.7/bin/python3  setup.py install --root=/tmp/sut
 ${THOME}/build/create_pkg TRIBmog /tmp/sut
 cd ..
 rm -fr /tmp/sut
