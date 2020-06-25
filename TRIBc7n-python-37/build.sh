9bf87e2b5aa553d87695d062f949e0f9198383c4
Peter Tribble <peter.tribble@gmail.com>
2020-06-25 12:56:52 +0100

    Add Cloud Custodian 0.9.3.0

---
+++ TRIBc7n-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack cloud-custodian-0.9.3.0
+cd cloud-custodian-0.9.3.0
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBc7n-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
