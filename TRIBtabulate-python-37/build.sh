2091ce24e9b8cd56393949846b92c0e6276a1aa5
Peter Tribble <peter.tribble@gmail.com>
2020-06-25 12:56:03 +0100

    Add tabulate 0.8.7

---
+++ TRIBtabulate-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack tabulate-0.8.7
+cd tabulate-0.8.7
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBtabulate-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
