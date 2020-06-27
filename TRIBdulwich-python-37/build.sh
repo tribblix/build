9bbba08c0ed5aeecc481282a05b5b372e3a81c3a
Peter Tribble <peter.tribble@gmail.com>
2020-06-27 10:26:02 +0100

    Update dulwich 0.20.5

--- TRIBdulwich-python-37/build.sh
+++ TRIBdulwich-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack dulwich-0.20.3
-cd dulwich-0.20.3
+${THOME}/build/unpack dulwich-0.20.5
+cd dulwich-0.20.5
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
