9da50fd6a81394efff0017a1edb22dc091324d96
Peter Tribble <peter.tribble@gmail.com>
2020-06-27 10:51:13 +0100

    Update netaddr 0.7.20

--- TRIBnetaddr-python-37/build.sh
+++ TRIBnetaddr-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack netaddr-0.7.19
-cd netaddr-0.7.19
+${THOME}/build/unpack netaddr-0.7.20
+cd netaddr-0.7.20
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
