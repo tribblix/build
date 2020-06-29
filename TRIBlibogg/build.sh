e77037c834f650f64175f99fa5ae47ef69653bbe
Peter Tribble <peter.tribble@gmail.com>
2020-06-29 08:09:59 +0100

    Update libogg 1.3.4

--- TRIBlibogg/build.sh
+++ TRIBlibogg/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libogg-1.3.3
-${THOME}/build/genpkg TRIBlibogg libogg-1.3.3
+${THOME}/build/dobuild -64 libogg-1.3.4
+${THOME}/build/genpkg TRIBlibogg libogg-1.3.4
