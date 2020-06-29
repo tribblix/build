42251303c2690cdafbed1f5853fd8fed40e9c5fb
Peter Tribble <peter.tribble@gmail.com>
2020-06-29 10:14:17 +0100

    Update stolon, build system has changed

--- TRIBblix-stolon/build.sh
+++ TRIBblix-stolon/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #
-# needs go 1.11
+# needs go 1.13 or later
 #
 mkdir s2
 cd s2
 #
 # a little clunky, use go get to download the source to the right place
-# and then the build script to actually do the compile
+# and then run make to actually do the compile
 #
-env GOPATH=`pwd` PATH=/usr/versions/go-1.12/bin:$PATH go get -v github.com/sorintlab/stolon
-env GOPATH=`pwd` PATH=/usr/versions/go-1.12/bin:$PATH STOLON_VERSION=0.14.0 src/github.com/sorintlab/stolon/build
+env GOPATH=`pwd` PATH=/usr/versions/go-1.14/bin:$PATH go get -v github.com/sorintlab/stolon
+env GOPATH=`pwd` PATH=/usr/versions/go-1.14/bin:$PATH gmake -C src/github.com/sorintlab/stolon
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/stolon/bin
