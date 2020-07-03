35fd1203d4a877a7865dc755e07015ad84b34120
Peter Tribble <peter.tribble@gmail.com>
2020-07-03 13:56:45 +0100

    Update Zabbix 4.0.22

--- TRIBzabbix-proxy/build.sh
+++ TRIBzabbix-proxy/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-4.0.20 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl --with-libpcre-include=/usr/include/pcre"
-${THOME}/build/genpkg TRIBzabbix-proxy zabbix-4.0.20
+${THOME}/build/dobuild zabbix-4.0.22 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl --with-libpcre-include=/usr/include/pcre"
+${THOME}/build/genpkg TRIBzabbix-proxy zabbix-4.0.22
