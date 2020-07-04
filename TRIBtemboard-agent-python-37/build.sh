d735f35579e100e3717b1eee32fe5a7a88da86d8
Peter Tribble <peter.tribble@gmail.com>
2020-07-04 11:42:51 +0100

    Update temboard-agent 6.0

--- TRIBtemboard-agent-python-37/build.sh
+++ TRIBtemboard-agent-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack temboard-agent-4.2
-cd temboard-agent-4.2
+${THOME}/build/unpack temboard-agent-6.0
+cd temboard-agent-6.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
