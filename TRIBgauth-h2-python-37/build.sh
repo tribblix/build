598373ba1d4cd5a1ce854358c3a30665975f2dc9
Peter Tribble <peter.tribble@gmail.com>
2020-07-10 08:50:01 +0100

    Update python google modules

--- TRIBgauth-h2-python-37/build.sh
+++ TRIBgauth-h2-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-auth-httplib2-0.0.3
-cd google-auth-httplib2-0.0.3
+${THOME}/build/unpack google-auth-httplib2-0.0.4
+cd google-auth-httplib2-0.0.4
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
--- TRIBgoogle-api-core-python-37/build.sh
+++ TRIBgoogle-api-core-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-api-core-1.20.0
-cd google-api-core-1.20.0
+${THOME}/build/unpack google-api-core-1.21.0
+cd google-api-core-1.21.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
