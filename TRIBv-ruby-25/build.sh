17b0cc2347ba68bd35a3b84e17e66bf0682306fe
Peter Tribble <peter.tribble@gmail.com>
2020-07-15 21:36:20 +0100

    Remove ruby 2.5, as it's coming up to EOL

--- TRIBv-ruby-25/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# just can't handle LD_OPTIONS
-# disable -fstack-protector otherwise miniruby fails to link with
-# multiply defined symbols
-#
-${THOME}/build/dobuild ruby-2.5.8 -P /usr/versions/ruby-2.5 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
-${THOME}/build/genpkg TRIBv-ruby-25 ruby-2.5.8
