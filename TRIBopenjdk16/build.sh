96674b818361182869ccf3a90650fd3bf1694c6a
Peter Tribble <peter.tribble@gmail.com>
2020-07-20 13:23:06 +0100

    Add an openjdk16 build

---
+++ TRIBopenjdk16/build.sh
@@ -0,0 +1,99 @@
+#!/bin/tcsh
+#
+# now a single tarball
+#
+cd ${THOME}/tarballs
+wget http://hg.openjdk.java.net/jdk/jdk/archive/jdk-16+6.tar.bz2
+ln jdk-16+6.tar.bz2 jdk-jdk-16+6.tar.bz2
+
+cd ~/ud
+${THOME}/build/unpack jdk-jdk-16+6
+cd jdk-jdk-16+6
+
+#
+# looks like dtrace is busted, illumos and Solaris have diverged
+# enough to trip it up, so --enable-dtrace=no
+#
+
+#
+# jdk16 needs autoconf installed
+#
+zap install autoconf
+
+#
+# the gobjcopy stuff doesn't actually work, so disable it
+# --with-native-debug-symbols=none
+#
+
+#
+# shenandoah doesn't build on SPARC so disable it with
+# --with-jvm-features=-shenandoahgc
+# (Bellsoft fixed shenandoah for Solaris x86 in 12.0.2)
+#
+# we're recognized as solaris, JEP 362 deprecated the solaris and sparc
+# ports, so we need to explicitly re-enable it
+#
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk14 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-dtrace \
+--disable-warnings-as-errors \
+--enable-deprecated-ports=yes
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
+
+
+#
+# first testing looks like this:
+#
+# cd build/solaris-x86_64-server-release/images/jdk
+# ./bin/java -version
+# 
+# openjdk version "16-internal" 2021-03-16
+# OpenJDK Runtime Environment (build 16-internal+0-adhoc.ptribble.jdk-jdk-166)
+# OpenJDK 64-Bit Server VM (build 16-internal+0-adhoc.ptribble.jdk-jdk-166, mixed mode, sharing)
+#
+
+rm -fr /tmp/jdk
+mkdir -p /tmp/jdk/usr/versions/openjdk16
+mkdir -p /tmp/jdk/usr/jdk/instances
+(cd build/solaris-x86_64-server-release/images/jdk; tar cf - *) | ( cd /tmp/jdk/usr/versions/openjdk16 ; tar xf -)
+cd /tmp/jdk/usr/jdk/instances
+ln -s ../../versions/openjdk16 jdk16
+cd /tmp/jdk/usr/jdk
+ln -s ../versions/openjdk16 .
+#jdk16 isn't widely supported, so don't make it the default
+#ln -s openjdk16 latest
+#mkdir -p /tmp/jdk/usr/bin
+#cd /tmp/jdk/usr/bin
+#ln -s ../jdk/latest/bin/* .
+#rm -f amd64 sparcv9
+cd /tmp/jdk/usr/versions/openjdk16
+rm `find . -name '*.diz'`
+
+#
+# need to create a certificate bundle
+#
+# the jdk16 build appears to create a working one, but it uses
+# a horribly outdated set of certificates, so replace it
+#
+# this actually needs to be a JKS keystore, not a PKCS12 keystore
+# https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1739631
+#
+${THOME}/build/patches/mkcacerts -f /etc/openssl/cacert.pem -o /tmp/cacerts -k /usr/jdk/instances/jdk1.8.0/bin/keytool -s /usr/bin/openssl
+#
+cp /tmp/cacerts /tmp/jdk/usr/versions/openjdk16/lib/security
+
+#
+# edit conf/security/sunpkcs11-solaris.cfg and add the following to disabledMechanisms
+#
+# # the following mechanisms are disabled due to lack of digest cloning support
+# # need to fix 6414899 first
+#   CKM_MD5
+#   CKM_SHA256
+#   CKM_SHA384
+#   CKM_SHA512
+#
+
+${THOME}/build/create_pkg TRIBopenjdk16 /tmp/jdk
---
+++ patches/openjdk16/README.txt
@@ -0,0 +1,56 @@
+This is originally derived from the pkgsrc-joyent set for openjdk11,
+considerably modified. Cut from the jdk15 patches as of jdk15+32.
+
+Most patches -p0
+
+JDK16:
+
+One of the reasons for removing SPARC/Solaris suport was to allow more
+radical changes to the codebase. That means it's likely to be more
+difficult to track changes.
+
+openjdk16 fixes (besides fixing line number noise) below
+
+NOTE:
+
+cc1plus: warning: unrecognized command line option '-Wno-cast-function-type'
+That's to do with freetype, I think.
+
+Changes:
+
+1. ExtendedPC has been removed
+fetch_frame_from_context is now an address
+fetch_frame_from_ucontext doesn't exist
+
+This appears to be another studio ("Forte") remnant that got cleaned
+up.
+Needs updates to:
+src/hotspot/os/solaris/os_solaris.hpp
+  remove fetch_frame_from_ucontext prototype
+  remove ucontext_get_ExtendedPC
+src/hotspot/os_cpu/solaris_x86/os_solaris_x86.cpp
+  remove ExtendedPC include
+  remove ucontext_get_ExtendedPC
+  remove fetch_frame_from_ucontext
+  Things that were ExtendedPC are now address
+src/hotspot/os_cpu/solaris_x86/thread_solaris_x86.cpp
+  Things that were ExtendedPC are now address
+src/hotspot/os/solaris/os_solaris.cpp
+  comment + header
+
+deleted
+patch-src_hotspot_os__cpu_solaris__x86_os__solaris__x86.cpp
+as it's merged into the main patch
+(we ought to merge more of the patches)
+
+Build:
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk14 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-dtrace \
+--disable-warnings-as-errors \
+--enable-deprecated-ports=yes
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
