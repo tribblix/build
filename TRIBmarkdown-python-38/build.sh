066863d2b342f51dac4315375cac023ccee2f8a4
Peter Tribble <peter.tribble@gmail.com>
2020-07-25 10:34:42 +0100

    Update Markdown 3.2.2

---
+++ TRIBmarkdown-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack Markdown-3.2.2
+cd Markdown-3.2.2
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBmarkdown-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
