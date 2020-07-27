a4681b4e623ccc4aa15c65e0cd6307670a9396d2
Peter Tribble <peter.tribble@gmail.com>
2020-07-27 19:11:16 +0100

    Remove kibana, unsupportable

--- TRIBblix-kibana/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# yes, it's a linux build that we start with, but
-# the platform-specific component is node, which is
-# replaced by the fixit script
-#
-${THOME}/build/pkg_tarball -T TRIBblix-kibana kibana-5.6.16-linux-x86
