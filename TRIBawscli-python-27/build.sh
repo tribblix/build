b6b171951ba7e3fb36c8d42b8ce918088c9623fc
Peter Tribble <peter.tribble@gmail.com>
2020-07-28 13:40:20 +0100

    Update awscli 1.18.106

--- TRIBawscli-python-27/build.sh
+++ TRIBawscli-python-27/build.sh
@@ -2,8 +2,8 @@
 #
 # needs botocore colorama s3transfer docutils rsa yaml
 #
-${THOME}/build/unpack awscli-1.18.66
-cd awscli-1.18.66
+${THOME}/build/unpack awscli-1.18.106
+cd awscli-1.18.106
 python setup.py build
 rm -fr /tmp/sut
 python setup.py install --root=/tmp/sut
--- TRIBawscli-python-38/build.sh
+++ TRIBawscli-python-38/build.sh
@@ -2,8 +2,8 @@
 #
 # needs botocore colorama s3transfer docutils rsa yaml
 #
-${THOME}/build/unpack awscli-1.18.66
-cd awscli-1.18.66
+${THOME}/build/unpack awscli-1.18.106
+cd awscli-1.18.106
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/sut
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/sut
