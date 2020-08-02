baf909384d2a49045a7370b7cea2d4ecc6bd19b3
Peter Tribble <peter.tribble@gmail.com>
2020-08-02 17:14:36 +0100

    Update importlib_metadata 1.7.0

--- TRIBimportlib-python-37/build.sh
+++ TRIBimportlib-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack importlib_metadata-1.3.0
-cd importlib_metadata-1.3.0
+${THOME}/build/unpack importlib_metadata-1.7.0
+cd importlib_metadata-1.7.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
