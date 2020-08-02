0f993a829a5f681dbc6e37dd1427ed934cdd9cff
Peter Tribble <peter.tribble@gmail.com>
2020-08-02 10:43:40 +0100

    Update python-magic 0.4.18

--- TRIBmagic-python-37/build.sh
+++ TRIBmagic-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack python-magic-0.4.15
-cd python-magic-0.4.15
+${THOME}/build/unpack python-magic-0.4.18
+cd python-magic-0.4.18
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
