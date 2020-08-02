c9175978afd5cd7f46173d3ac1750c50125255f7
Peter Tribble <peter.tribble@gmail.com>
2020-08-02 13:08:02 +0100

    Update libX11 1.6.10

--- TRIBx11-libx11/build.sh
+++ TRIBx11-libx11/build.sh
@@ -3,6 +3,6 @@
 # needs TRIBdev-xorg-macros in order to build
 #
 env CPPFLAGS="-D SUNSOFT -DMOTIFBC -DSUNSOFT_KBD" \
-$THOME/build/dobuild -64 libX11-1.6.9 -C \
-"--enable-xkb --enable-xthreads --enable-static=no --enable-xthread --enable-xf86bigfont --enable-xlocaledir --enable-loadable-i18n --without-xmlto --without-fop --localstatedir=/var --enable-loadable-xcursor"
-${THOME}/build/genpkg TRIBx11-libx11 libX11-1.6.9
+$THOME/build/dobuild -64 libX11-1.6.10 -C \
+"--enable-xkb --enable-xthreads --enable-static=no --enable-xf86bigfont --enable-xlocaledir --enable-loadable-i18n --without-xmlto --without-fop --localstatedir=/var --enable-loadable-xcursor"
+${THOME}/build/genpkg TRIBx11-libx11 libX11-1.6.10
