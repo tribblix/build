7bf9a832d63c63331bf291a798808978b8698def
Peter Tribble <peter.tribble@gmail.com>
2020-08-03 18:32:27 +0100

    Update coreutils 8.32, and purely 64-bit

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu coreutils-8.31 -P /usr/gnu -C "--without-gmp gl_cv_host_operating_system=illumos"
-${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.31
+# we absolutely want a 64-bit date, and it's easier and smaller to
+# make everything 64-bit
+#
+${THOME}/build/dobuild +64only -gnu coreutils-8.32 -P /usr/gnu -C "--without-gmp gl_cv_host_operating_system=illumos"
+${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.32
