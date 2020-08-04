cdd3d57f1977b010c9ab7a78b243d0b11817113c
Peter Tribble <peter.tribble@gmail.com>
2020-08-04 10:13:23 +0100

    Update openpyxl 3.0.4

--- TRIBopenpyxl-python-37/build.sh
+++ TRIBopenpyxl-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack openpyxl-3.0.3
-cd openpyxl-3.0.3
+${THOME}/build/unpack openpyxl-3.0.4
+cd openpyxl-3.0.4
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
