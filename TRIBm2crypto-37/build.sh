f167456b284f183bc404de65580b4d3cf1f50e06
Peter Tribble <peter.tribble@gmail.com>
2020-08-04 19:08:54 +0100

    Update M2Crypto 0.36.0

--- TRIBm2crypto-37/build.sh
+++ TRIBm2crypto-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack M2Crypto-0.35.2
-cd M2Crypto-0.35.2
+${THOME}/build/unpack M2Crypto-0.36.0
+cd M2Crypto-0.36.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
