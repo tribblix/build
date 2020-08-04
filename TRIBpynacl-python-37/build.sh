f097153a9121df7db7a5f7b026fbbeeba97437e0
Peter Tribble <peter.tribble@gmail.com>
2020-08-04 11:23:19 +0100

    Update PyNaCl 1.4.0

--- TRIBpynacl-python-37/build.sh
+++ TRIBpynacl-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack PyNaCl-1.3.0
-cd PyNaCl-1.3.0
+${THOME}/build/unpack PyNaCl-1.4.0
+cd PyNaCl-1.4.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
