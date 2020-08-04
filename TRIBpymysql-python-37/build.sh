b16feeee1cbea7213ba4bc963e199941b7003e2e
Peter Tribble <peter.tribble@gmail.com>
2020-08-04 11:24:46 +0100

    Update PyMySQL 0.10.0

--- TRIBpymysql-python-37/build.sh
+++ TRIBpymysql-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack PyMySQL-0.9.3
-cd PyMySQL-0.9.3
+${THOME}/build/unpack PyMySQL-0.10.0
+cd PyMySQL-0.10.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
---
+++ TRIBpymysql-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack PyMySQL-0.10.0
+cd PyMySQL-0.10.0
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpymysql-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
