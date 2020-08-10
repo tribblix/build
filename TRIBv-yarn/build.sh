2cd49e35c43b8f64bd55e6f4bc7c351b65dbcbd8
Peter Tribble <peter.tribble@gmail.com>
2020-08-10 12:54:04 +0100

    Update yarn 1.22.4

--- TRIBv-yarn/build.sh
+++ TRIBv-yarn/build.sh
@@ -3,4 +3,4 @@
 # logically yarn depends on node, but could use any version,
 # so we don't declare a dependency
 #
-${THOME}/build/pkg_tarball TRIBv-yarn yarn-v1.21.1
+${THOME}/build/pkg_tarball TRIBv-yarn yarn-v1.22.4
