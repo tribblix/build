593b9bacd30b5c17213760e4d4982cd50790d99b
Peter Tribble <peter.tribble@gmail.com>
2020-08-14 10:49:35 +0100

    python3.8 version of pyparsing

---
+++ TRIBpyparsing-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pyparsing-2.4.7
+cd pyparsing-2.4.7
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpyparsing-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
