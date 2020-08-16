7914a2b171a99e4a15bc774cc1f08facaa99ab85
Peter Tribble <peter.tribble@gmail.com>
2020-08-16 10:06:28 +0100

    Update jproc

--- TRIBjproc/build.sh
+++ TRIBjproc/build.sh
@@ -8,11 +8,11 @@
 #
 # then copy the packages
 #
-mv /tmp/TRIBjproc-i386.pkg ${THOME}/pkgs/TRIBjproc.1.1.pkg
-mv /tmp/TRIBjproc-sparc.pkg ${THOME}/pkgs.sparc/TRIBjproc.1.1.pkg
-${THOME}/build/pkg2zap ${THOME}/pkgs/TRIBjproc.1.1.pkg ${THOME}/pkgs
-${THOME}/build/pkg2zap ${THOME}/pkgs.sparc/TRIBjproc.1.1.pkg ${THOME}/pkgs.sparc
-openssl md5 ${THOME}/pkgs/TRIBjproc.1.1.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjproc.1.1.zap.md5
-openssl md5 ${THOME}/pkgs.sparc/TRIBjproc.1.1.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs.sparc/TRIBjproc.1.1.zap.md5
-${THOME}/build/sign-file ${THOME}/pkgs/TRIBjproc.1.1.zap
-${THOME}/build/sign-file ${THOME}/pkgs.sparc/TRIBjproc.1.1.zap
+mv /tmp/TRIBjproc-i386.pkg ${THOME}/pkgs/TRIBjproc.1.2.pkg
+mv /tmp/TRIBjproc-sparc.pkg ${THOME}/pkgs.sparc/TRIBjproc.1.2.pkg
+${THOME}/build/pkg2zap ${THOME}/pkgs/TRIBjproc.1.2.pkg ${THOME}/pkgs
+${THOME}/build/pkg2zap ${THOME}/pkgs.sparc/TRIBjproc.1.2.pkg ${THOME}/pkgs.sparc
+openssl md5 ${THOME}/pkgs/TRIBjproc.1.2.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjproc.1.2.zap.md5
+openssl md5 ${THOME}/pkgs.sparc/TRIBjproc.1.2.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs.sparc/TRIBjproc.1.2.zap.md5
+${THOME}/build/sign-file ${THOME}/pkgs/TRIBjproc.1.2.zap
+${THOME}/build/sign-file ${THOME}/pkgs.sparc/TRIBjproc.1.2.zap
