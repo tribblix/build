77e8d5f6d2d1dbc40d2f8ddc5c7aac134747a5ff
Peter Tribble <peter.tribble@gmail.com>
2020-08-19 08:41:36 +0100

    Update SQLAlchemy 1.3.19

--- TRIBsqlalchemy-python-37/build.sh
+++ TRIBsqlalchemy-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack SQLAlchemy-1.3.18
-cd SQLAlchemy-1.3.18
+${THOME}/build/unpack SQLAlchemy-1.3.19
+cd SQLAlchemy-1.3.19
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
