99007eecaad30295bacbf986c1dcf6b2ffed10eb
Peter Tribble <peter.tribble@gmail.com>
2020-08-22 19:17:01 +0100

    Update Python 3.8.5, and split out test files

--- TRIBv-python-38/build.sh
+++ TRIBv-python-38/build.sh
@@ -3,9 +3,9 @@
 # NB. TRIBv-python-38 must not be installed, or the make
 # install step will get confused
 #
-# 3.8 currently exists only to test that it builds and works
-# few modules have yet been built against this version
+# 3.8 is in testing, 3.7 is still primary
+# not all modules have yet been built against this version
 #
-${THOME}/build/dobuild +64only Python-3.8.3 -P /usr/versions/python-3.8 -L -C --enable-shared
+${THOME}/build/dobuild +64only Python-3.8.5 -P /usr/versions/python-3.8 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-38 Python-3.8.3
+${THOME}/build/genpkg TRIBv-python-38 Python-3.8.5
