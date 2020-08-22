942f68026a1356929892ad67e55cf867e725ab4f
Peter Tribble <peter.tribble@gmail.com>
2020-08-22 19:25:21 +0100

    Update python bcrypt 3.2.0

--- TRIBbcrypt-python-37/build.sh
+++ TRIBbcrypt-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack bcrypt-3.1.7
-cd bcrypt-3.1.7
+${THOME}/build/unpack bcrypt-3.2.0
+cd bcrypt-3.2.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
