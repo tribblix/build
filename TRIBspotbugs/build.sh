99034f7abb045fca045139a64b204065e086983d
Peter Tribble <peter.tribble@gmail.com>
2020-08-25 15:49:27 +0100

    Update SpotBugs 4.1.2

--- TRIBspotbugs/build.sh
+++ TRIBspotbugs/build.sh
@@ -1,31 +1,3 @@
 #!/bin/sh
 #
-# 
-#
-
-
-${THOME}/build/pkg_tarball TRIBspotbugs spotbugs-3.1.12
-
-
-
-
-
-
-exit 0
-#
-# this is the old convoluted build from source version
-#
-${THOME}/build/unpack spotbugs-3.1.8
-cd spotbugs-3.1.8
-./gradlew build
-./gradlew distTar
-
-rm -fr /tmp/gg
-mkdir -p /tmp/gg/usr/versions
-gzcat spotbugs/build/distributions/spotbugs-3.1.8.tgz | ( cd /tmp/gg/usr/versions ; gtar xf -)
-mkdir -p /tmp/gg/usr/bin
-(cd /tmp/gg/usr/bin ; ln -s ../../usr/versions/spotbugs-3.1.8/bin/spotbugs .)
-${THOME}/build/create_pkg TRIBspotbugs /tmp/gg
-rm -fr /tmp/gg
-
-cd ..
+${THOME}/build/pkg_tarball TRIBspotbugs spotbugs-4.1.2
