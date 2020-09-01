9d100e9added6f2758ec055ea35bd3edc2229824
Peter Tribble <peter.tribble@gmail.com>
2020-09-01 16:38:27 +0100

    Update Django 2.2.16 3.1.1

--- TRIBdjango2-python-37/build.sh
+++ TRIBdjango2-python-37/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack Django-2.2.15
-cd Django-2.2.15
+${THOME}/build/unpack Django-2.2.16
+cd Django-2.2.16
 rm -fr /tmp/p3a
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango2-python-37 /tmp/p3a
