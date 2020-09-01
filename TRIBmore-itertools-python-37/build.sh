d1e7167aac7846b86710bc6cf28d2421a2020165
Peter Tribble <peter.tribble@gmail.com>
2020-09-01 17:13:38 +0100

    Update more-itertools 8.5.0

--- TRIBmore-itertools-python-37/build.sh
+++ TRIBmore-itertools-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack more-itertools-8.4.0
-cd more-itertools-8.4.0
+${THOME}/build/unpack more-itertools-8.5.0
+cd more-itertools-8.5.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
