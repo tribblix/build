b1867c891020324cbf5dae0d13bd84ac9cab0a9f
Peter Tribble <peter.tribble@gmail.com>
2020-09-04 15:06:02 +0100

    python38 Jinja module

---
+++ TRIBjinja-python-38/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# needs Babel MarkupSafe
+#
+${THOME}/build/unpack Jinja2-2.11.2
+cd Jinja2-2.11.2
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBjinja-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
