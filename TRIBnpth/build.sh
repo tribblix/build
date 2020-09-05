75085ece348986e4d35a5392de60861f85fdc713
Peter Tribble <peter.tribble@gmail.com>
2020-09-05 09:56:10 +0100

    Update npth 1.6

--- TRIBnpth/build.sh
+++ TRIBnpth/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS="-std=c99" ${THOME}/build/dobuild -64 npth-1.5
-${THOME}/build/genpkg TRIBnpth npth-1.5
+env TRIBBLIX_CFLAGS="-std=c99" ${THOME}/build/dobuild -64 npth-1.6
+${THOME}/build/genpkg TRIBnpth npth-1.6
