5d1ffe5580a26e6b6abfefce4e811235896a4ca3
Peter Tribble <peter.tribble@gmail.com>
2020-09-05 13:53:50 +0100

    python38 modules for configargparse docker-pycreds imagesize

---
+++ TRIBconfigargparse-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack ConfigArgParse-1.2.3
+cd ConfigArgParse-1.2.3
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBconfigargparse-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
