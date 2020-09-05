35fad92e86cb308d88f29cd90eb4b7fdab9c44ac
Peter Tribble <peter.tribble@gmail.com>
2020-09-05 09:49:53 +0100

    Update libgpg-error 1.39

--- TRIBlibgpg-error/build.sh
+++ TRIBlibgpg-error/build.sh
@@ -2,5 +2,5 @@
 #
 # gpg-error
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64 libgpg-error-1.36
-${THOME}/build/genpkg TRIBlibgpg-error libgpg-error-1.36
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64 libgpg-error-1.39
+${THOME}/build/genpkg TRIBlibgpg-error libgpg-error-1.39
