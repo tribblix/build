ead967ae1608bed2f69bdf4867433e5a6655bc37
Peter Tribble <peter.tribble@gmail.com>
2020-09-22 19:22:02 +0100

    Update libcloud 3.2.0

---
+++ TRIBlibcloud-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack apache-libcloud-3.2.0
+cd apache-libcloud-3.2.0
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBlibcloud-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
