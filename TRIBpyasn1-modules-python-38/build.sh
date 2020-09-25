97af68092884fd6c9437f4496ced19ad282f6a9c
Peter Tribble <peter.tribble@gmail.com>
2020-09-25 20:34:32 +0100

    python38 version of pyasn1-modules

---
+++ TRIBpyasn1-modules-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pyasn1-modules-0.2.8
+cd pyasn1-modules-0.2.8
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpyasn1-modules-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
