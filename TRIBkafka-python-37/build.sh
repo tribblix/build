a4f2ee16fe1903d7de1c89619af92e26b9ccffd0
Peter Tribble <peter.tribble@gmail.com>
2020-09-26 21:38:52 +0100

    Update kafka-python 2.0.1

--- TRIBkafka-python-37/build.sh
+++ TRIBkafka-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack kafka-python-1.4.7
-cd kafka-python-1.4.7
+${THOME}/build/unpack kafka-python-2.0.1
+cd kafka-python-2.0.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
