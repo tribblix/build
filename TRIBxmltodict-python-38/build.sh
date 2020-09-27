1ec64a0c47f60595bd9164d2fa40b4c0110fec8d
Peter Tribble <peter.tribble@gmail.com>
2020-09-27 20:38:34 +0100

    python38 version of xmltodict

---
+++ TRIBxmltodict-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack xmltodict-0.12.0
+cd xmltodict-0.12.0
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBxmltodict-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
