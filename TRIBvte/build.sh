76903a36c22c7cdd052879967a74e818b147e8ee
Peter Tribble <peter.tribble@gmail.com>
2020-09-30 17:13:52 +0100

    Update vte 0.42.5 (save files before commit!)

--- TRIBvte/build.sh
+++ TRIBvte/build.sh
@@ -3,5 +3,5 @@
 # this is a patched vte, that handles home/end/pgup/pgdown sanely
 # without requiring shift
 #
-${THOME}/build/dobuild -gnu vte-0.42.5
+${THOME}/build/dobuild -gnu vte-0.42.5 -C "--disable-Bsymbolic --enable-introspection=no --enable-vala=no --without-gnutls"
 ${THOME}/build/genpkg TRIBvte vte-0.42.5
