22a7c6da61f87d4cf3d0977393f11af76c46c1df
Peter Tribble <peter.tribble@gmail.com>
2020-09-30 21:05:25 +0100

    Update xfce taskmanager 1.2.3

--- TRIBxfce-taskmanager/build.sh
+++ TRIBxfce-taskmanager/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-taskmanager-1.1.0
-${THOME}/build/genpkg TRIBxfce-taskmanager xfce4-taskmanager-1.1.0
+${THOME}/build/dobuild -gnu xfce4-taskmanager-1.2.3
+${THOME}/build/genpkg TRIBxfce-taskmanager xfce4-taskmanager-1.2.3
