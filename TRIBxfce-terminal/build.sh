df6abc3500f55a3d993440f3a9527e1750e2e877
Peter Tribble <peter.tribble@gmail.com>
2020-09-30 17:28:41 +0100

    Update xfce-terminal 0.8.9.2

--- TRIBxfce-terminal/build.sh
+++ TRIBxfce-terminal/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu Terminal-0.4.8
-${THOME}/build/genpkg TRIBxfce-terminal Terminal-0.4.8
+${THOME}/build/dobuild -gnu xfce4-terminal-0.8.9.2
+${THOME}/build/genpkg TRIBxfce-terminal xfce4-terminal-0.8.9.2
