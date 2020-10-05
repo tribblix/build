8808a2f6b5e34fa6dc8a000411ef287ddfb71e16
Peter Tribble <peter.tribble@gmail.com>
2020-10-05 20:49:02 +0100

    Update evince 3.30.3

--- TRIBevince/build.sh
+++ TRIBevince/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu evince-2.32.0 -C "--without-keyring --without-gconf --disable-nautilus --disable-help --enable-pixbuf"
-${THOME}/build/genpkg TRIBevince evince-2.32.0
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu evince-3.30.3 -C "--without-keyring --without-gspell --disable-nautilus --enable-compile-warnings=minimum"
+${THOME}/build/genpkg TRIBevince evince-3.30.3
