e4a07e3e0ca06a4597403a18c67bc40e0172ff5d
Peter Tribble <peter.tribble@gmail.com>
2020-10-05 16:28:24 +0100

    Update ristretto 0.10.0

--- TRIBxfce-ristretto/build.sh
+++ TRIBxfce-ristretto/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu ristretto-0.8.3
-${THOME}/build/genpkg  TRIBxfce-ristretto ristretto-0.8.3
+${THOME}/build/dobuild -gnu ristretto-0.10.0
+${THOME}/build/genpkg  TRIBxfce-ristretto ristretto-0.10.0
