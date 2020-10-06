c064d8b6d092c48ecb018f361964c448892f374f
Peter Tribble <peter.tribble@gmail.com>
2020-10-06 08:39:30 +0100

    Update libexif, exif 0.6.22

--- TRIBexif/build.sh
+++ TRIBexif/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild exif-0.6.21
-${THOME}/build/genpkg TRIBexif exif-0.6.21
+${THOME}/build/dobuild exif-0.6.22
+${THOME}/build/genpkg TRIBexif exif-0.6.22
--- TRIBimage-libexif/build.sh
+++ TRIBimage-libexif/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libexif-0.6.21
-${THOME}/build/genpkg TRIBimage-libexif libexif-0.6.21
+${THOME}/build/dobuild -64 libexif-0.6.22
+${THOME}/build/genpkg TRIBimage-libexif libexif-0.6.22
