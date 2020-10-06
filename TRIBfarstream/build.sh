b107d42e0be81af75eb796ce49679543e85c0330
Peter Tribble <peter.tribble@gmail.com>
2020-10-06 13:15:55 +0100

    Update farstream 0.2.9

--- TRIBfarstream/build.sh
+++ TRIBfarstream/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 farstream-0.2.8 -C "--disable-python --disable-introspection"
-${THOME}/build/genpkg TRIBfarstream farstream-0.2.8
+${THOME}/build/dobuild -64 farstream-0.2.9 -C "--disable-python --disable-introspection --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBfarstream farstream-0.2.9
