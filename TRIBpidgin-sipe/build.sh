ab538597ed263b61bf6af3148cbf2f14511aad50
Peter Tribble <peter.tribble@gmail.com>
2020-10-06 15:26:08 +0100

    Update pidgin-sipe 1.25.0

--- TRIBpidgin-sipe/build.sh
+++ TRIBpidgin-sipe/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env LDFLAGS=-R/usr/lib/mps LIBS=-lsocket ${THOME}/build/dobuild -gnu pidgin-sipe-1.22.0 -C "--with-krb5=no"
-${THOME}/build/genpkg TRIBpidgin-sipe pidgin-sipe-1.22.0
+# need --disable-quality-check as otherwise it's confused by type conversions
+#
+env LDFLAGS=-R/usr/lib/mps LIBS=-lsocket ${THOME}/build/dobuild -gnu pidgin-sipe-1.25.0 -C "--with-krb5=no --disable-quality-check"
+${THOME}/build/genpkg TRIBpidgin-sipe pidgin-sipe-1.25.0
