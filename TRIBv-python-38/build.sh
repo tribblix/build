d40ea5bf8e4569ff68d9356edfbc4b827c90ade5
Peter Tribble <peter.tribble@gmail.com>
2020-10-06 19:37:44 +0100

    Update Python 3.8.6, and split out idle/tkinter

--- TRIBv-python-38/build.sh
+++ TRIBv-python-38/build.sh
@@ -6,6 +6,6 @@
 # 3.8 is in testing, 3.7 is still primary
 # not all modules have yet been built against this version
 #
-${THOME}/build/dobuild +64only Python-3.8.5 -P /usr/versions/python-3.8 -L -C --enable-shared
+${THOME}/build/dobuild +64only Python-3.8.6 -P /usr/versions/python-3.8 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-38 Python-3.8.5
+${THOME}/build/genpkg TRIBv-python-38 Python-3.8.6
