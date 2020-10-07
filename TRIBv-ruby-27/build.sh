995edb1b7e52d31921470bf864e7666ae23fa5b7
Peter Tribble <peter.tribble@gmail.com>
2020-10-07 09:02:32 +0100

    Update ruby 2.7.2

--- TRIBv-ruby-27/build.sh
+++ TRIBv-ruby-27/build.sh
@@ -4,5 +4,5 @@
 # disable -fstack-protector otherwise miniruby fails to link with
 # multiply defined symbols
 #
-${THOME}/build/dobuild ruby-2.7.1 -P /usr/versions/ruby-2.7 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
-${THOME}/build/genpkg TRIBv-ruby-27 ruby-2.7.1
+${THOME}/build/dobuild ruby-2.7.2 -P /usr/versions/ruby-2.7 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
+${THOME}/build/genpkg TRIBv-ruby-27 ruby-2.7.2
