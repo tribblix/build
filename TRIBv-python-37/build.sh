7ddccf73756334daa22c75959759317f35fad1f2
Peter Tribble <peter.tribble@gmail.com>
2020-10-08 13:21:14 +0100

    Split out tkinter for python 3.7

--- TRIBv-python-37/build.sh
+++ TRIBv-python-37/build.sh
@@ -3,6 +3,10 @@
 # NB. TRIBv-python-37 must not be installed, or the make
 # install step will get confused
 #
+# NOTE: update the versions of the tests and tkinter packages as well
+# as those components are split out, the packages are automatically
+# created by the fixit script
+#
 ${THOME}/build/dobuild Python-3.7.9 -P /usr/versions/python-3.7 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
 ${THOME}/build/genpkg TRIBv-python-37 Python-3.7.9
--- TRIBv-python-38/build.sh
+++ TRIBv-python-38/build.sh
@@ -6,6 +6,10 @@
 # 3.8 is in testing, 3.7 is still primary
 # not all modules have yet been built against this version
 #
+# NOTE: update the versions of the tests and tkinter packages as well
+# as those components are split out, the packages are automatically
+# created by the fixit script
+#
 ${THOME}/build/dobuild +64only Python-3.8.6 -P /usr/versions/python-3.8 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
 ${THOME}/build/genpkg TRIBv-python-38 Python-3.8.6
