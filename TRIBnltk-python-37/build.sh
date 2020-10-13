59c875639d4904ba519e3cef9e519fc97e8f251d
Peter Tribble <peter.tribble@gmail.com>
2020-10-13 17:08:48 +0100

    Update nltk 3.5

--- TRIBnltk-python-37/build.sh
+++ TRIBnltk-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack nltk-3.4.5
-cd nltk-3.4.5
+${THOME}/build/unpack nltk-3.5
+cd nltk-3.5
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
