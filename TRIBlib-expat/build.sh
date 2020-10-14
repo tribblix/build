0c335e3de23d470c7a1cceefff053a0a9f759659
Peter Tribble <peter.tribble@gmail.com>
2020-10-14 10:13:58 +0100

    Update expat 2.2.10

--- TRIBlib-expat/build.sh
+++ TRIBlib-expat/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 expat-2.2.8
-${THOME}/build/genpkg TRIBlib-expat expat-2.2.8
+${THOME}/build/dobuild -64 expat-2.2.10
+${THOME}/build/genpkg TRIBlib-expat expat-2.2.10
