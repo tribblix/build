ee052b092c27b4847616303b37cfabf01c1ecc19
Peter Tribble <peter.tribble@gmail.com>
2020-10-20 20:33:18 +0100

    Update freetype 2.10.4

--- TRIBsys-lib-freetype-2/build.sh
+++ TRIBsys-lib-freetype-2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 freetype-2.8.1 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no"
-${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.8.1
+${THOME}/build/dobuild -64 freetype-2.10.4 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no --enable-freetype-config"
+${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.10.4
