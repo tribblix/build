120750864230807f19813d70a79248ad8f2b2cc9
Peter Tribble <peter.tribble@gmail.com>
2020-10-23 14:53:43 +0100

    Update openssl (experimental) 1.1.1h

--- TRIBv-openssl11/build.sh
+++ TRIBv-openssl11/build.sh
@@ -4,24 +4,26 @@
 # so we can ignore any of the illumos-specific tweaks
 # inherited from Solaris and just do a vanilla build
 #
-${THOME}/build/unpack -64 openssl-1.1.1g
-cd openssl-1.1.1g
+${THOME}/build/unpack -64 openssl-1.1.1h
+cd openssl-1.1.1h
 ./Configure solaris-x86-gcc shared threads zlib --api=1.0.0 --prefix=/usr/versions/openssl11
+sed -i s:pod2man:/usr/perl5/bin/pod2man: util/process_docs.pl
 env LD_RUN_PATH=/usr/versions/openssl11/lib gmake -j 4
 cd ..
 #
 # you might have thought that asking for a solaris64-x86_64 build
 # would actually do the right thing, but no ...
 #
-cd openssl-1.1.1g-64bit
+cd openssl-1.1.1h-64bit
 env __CNF_CFLAGS=-m64 __CNF_LDFLAGS=-m64 ./Configure solaris64-x86_64-gcc shared threads zlib --api=1.0.0 --prefix=/usr/versions/openssl11 --libdir=lib/amd64
+sed -i s:pod2man:/usr/perl5/bin/pod2man: util/process_docs.pl
 env LD_RUN_PATH=/usr/versions/openssl11/lib/amd64 gmake -j 4
 cd ..
 
 #
 # much easier now install understands DESTDIR
 #
-${THOME}/build/genpkg TRIBv-openssl11 openssl-1.1.1g
+${THOME}/build/genpkg TRIBv-openssl11 openssl-1.1.1h
 
 #
 # The sparc configure steps are:
