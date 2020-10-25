f8f04fa231c096ebb3864f77c1af3b0bec7c0dad
Peter Tribble <peter.tribble@gmail.com>
2020-10-25 16:36:27 +0000

    Update node v14.14.0

--- TRIBv-node-v14/build.sh
+++ TRIBv-node-v14/build.sh
@@ -2,5 +2,5 @@
 #
 # 64-bit only
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v14.13.1 -P /usr/versions/node-v14 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v14 node-v14.13.1
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v14.14.0 -P /usr/versions/node-v14 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v14 node-v14.14.0
