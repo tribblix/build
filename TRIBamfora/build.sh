fec60e5811e47fc88cb54f2644af561a130e07f6
Peter Tribble <peter.tribble@gmail.com>
2020-10-28 13:02:23 +0000

    Add amfora Gemini client

---
+++ TRIBamfora/build.sh
@@ -0,0 +1,17 @@
+#!/bin/tcsh
+#
+# requires go
+#
+git clone https://github.com/makeworld-the-better-one/amfora
+cd amfora
+gmake
+
+rm -fr /tmp/af123
+mkdir -p /tmp/af123/usr/bin
+cp ./amfora /tmp/af123/usr/bin
+mkdir -p /tmp/af123/usr/share/applications
+cp amfora.desktop /tmp/af123/usr/share/applications
+
+${THOME}/build/create_pkg TRIBamfora /tmp/af123
+
+rm -fr /tmp/af123
