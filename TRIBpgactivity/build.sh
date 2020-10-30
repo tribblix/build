1b20bc577205160c7e72a3b5b70cec0c0e4f2fa9
Peter Tribble <peter.tribble@gmail.com>
2020-10-30 14:25:00 +0000

    Update pg_activity 1.6.2

--- TRIBpgactivity/build.sh
+++ TRIBpgactivity/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pg_activity-1.6.0
-cd pg_activity-1.6.0
+${THOME}/build/unpack pg_activity-1.6.2
+cd pg_activity-1.6.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil --with-man
