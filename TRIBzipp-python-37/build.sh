75a33c1cd6830bf1c646a2b9d3c5c151afbaf195
Peter Tribble <peter.tribble@gmail.com>
2020-11-01 19:07:10 +0000

    Update zipp 3.4.0

--- TRIBzipp-python-37/build.sh
+++ TRIBzipp-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack zipp-3.1.0
-cd zipp-3.1.0
+${THOME}/build/unpack zipp-3.4.0
+cd zipp-3.4.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
