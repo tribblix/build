18da6617d1073f1ab72fe5d3cd953acf7f80d8a0
Peter Tribble <peter.tribble@gmail.com>
2020-11-01 10:56:56 +0000

    Update screen 4.8.0

--- TRIBscreen/build.sh
+++ TRIBscreen/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-#
-#
-env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild screen-4.6.2 -C --with-sys-screenrc=/etc/screenrc
-${THOME}/build/genpkg TRIBscreen screen-4.6.2
+${THOME}/build/dobuild screen-4.8.0 -C --with-sys-screenrc=/etc/screenrc
+${THOME}/build/genpkg TRIBscreen screen-4.8.0
---
+++ patches/screen-4.8.0.pls
@@ -0,0 +1,2 @@
+-p1 screen-01.patch
+-p1 screen-02.patch
