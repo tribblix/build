e4bce3b24b6b1eb987cdab14a1d54e94d5583fb0
Peter Tribble <peter.tribble@gmail.com>
2020-11-01 12:59:39 +0000

    Add mcross gemini client 0.5.17

---
+++ TRIBmcross-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack mcross-0.5.17
+cd mcross-0.5.17
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBmcross-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
