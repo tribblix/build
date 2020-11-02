4bdc29ffd38e908a012237a3f7f37fc34fb66a8e
Peter Tribble <peter.tribble@gmail.com>
2020-11-02 21:25:24 +0000

    Update samba 4.10.18

--- TRIBsamba4/build.sh
+++ TRIBsamba4/build.sh
@@ -2,5 +2,5 @@
 #
 # SPARC needs --without-ldap --without-ads
 #
-env CC=gcc TRIBBLIX_CFLAGS="-D_POSIX_PTHREAD_SEMANTICS" TRIBBLIX_LDFLAGS="-lnsl -lsendfile" ${THOME}/build/dobuild samba-4.10.15 -P /usr -C "--enable-fhs --sysconfdir=/etc --localstatedir=/var --disable-python --without-ad-dc" -M -k
-${THOME}/build/genpkg TRIBsamba4 samba-4.10.15
+env CC=gcc TRIBBLIX_CFLAGS="-D_POSIX_PTHREAD_SEMANTICS" TRIBBLIX_LDFLAGS="-lnsl -lsendfile" ${THOME}/build/dobuild samba-4.10.18 -P /usr -C "--enable-fhs --sysconfdir=/etc --localstatedir=/var --disable-python --without-ad-dc" -M -k
+${THOME}/build/genpkg TRIBsamba4 samba-4.10.18
