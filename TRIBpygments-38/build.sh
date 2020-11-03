9fb6ccf2abccf5613de797e367e248aace5c122e
Peter Tribble <peter.tribble@gmail.com>
2020-11-03 19:30:20 +0000

    Update Pygments 2.7.2

---
+++ TRIBpygments-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack Pygments-2.7.2
+cd Pygments-2.7.2
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pdu
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBpygments-38 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
