772f87242f849d3b04c9d07733efac321ef3b97d
Peter Tribble <peter.tribble@gmail.com>
2020-11-08 09:55:53 +0000

    Update pytz 2020.4

--- TRIBpytz-python-38/build.sh
+++ TRIBpytz-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pytz-2020.1
-cd pytz-2020.1
+${THOME}/build/unpack pytz-2020.4
+cd pytz-2020.4
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
