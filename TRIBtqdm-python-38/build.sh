39f5dcf45868b989d241aa982a6326d3adf5037d
Peter Tribble <peter.tribble@gmail.com>
2020-11-08 09:53:10 +0000

    Update tqdm 4.51.0

--- TRIBtqdm-python-38/build.sh
+++ TRIBtqdm-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tqdm-4.50.0
-cd tqdm-4.50.0
+${THOME}/build/unpack tqdm-4.51.0
+cd tqdm-4.51.0
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
