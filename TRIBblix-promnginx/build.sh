8066f460f4a5f4712b978789071fd4a6f2d79b8e
Peter Tribble <peter.tribble@gmail.com>
2020-11-08 12:09:01 +0000

    Update prometheus nginx exporter 0.8.0

--- TRIBblix-promnginx/build.sh
+++ TRIBblix-promnginx/build.sh
@@ -5,8 +5,8 @@
 mkdir pn1
 cd pn1
 setenv GOPATH `pwd`
-go get github.com/nginxinc/nginx-prometheus-exporter
+env PATH=/usr/versions/go-1.15/bin:$PATH go get github.com/nginxinc/nginx-prometheus-exporter
 cd $GOPATH/src/github.com/nginxinc/nginx-prometheus-exporter/
-gmake
+env PATH=/usr/versions/go-1.15/bin:$PATH gmake
 
 ${THOME}/build/genpkg TRIBblix-promnginx
