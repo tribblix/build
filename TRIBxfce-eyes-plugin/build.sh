ca6cfe2a2d3527628b6e232052aeda9748033ce9
Peter Tribble <peter.tribble@gmail.com>
2020-11-09 21:27:57 +0000

    Update Xfce eyes plugin 4.5.0

--- TRIBxfce-eyes-plugin/build.sh
+++ TRIBxfce-eyes-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-eyes-plugin-4.4.5
-${THOME}/build/genpkg TRIBxfce-eyes-plugin xfce4-eyes-plugin-4.4.5
+${THOME}/build/dobuild -gnu xfce4-eyes-plugin-4.5.0
+${THOME}/build/genpkg TRIBxfce-eyes-plugin xfce4-eyes-plugin-4.5.0
