8291dd3cb4132b2357fd29465886b6a8654866b6
Peter Tribble <peter.tribble@gmail.com>
2020-11-09 21:17:54 +0000

    Update Xfce weather plugin 0.10.2

--- TRIBxfce-weather-plugin/build.sh
+++ TRIBxfce-weather-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-weather-plugin-0.8.10
-${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.8.10
+${THOME}/build/dobuild -gnu xfce4-weather-plugin-0.10.2
+${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.10.2
