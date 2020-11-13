7271f9a3ac60cdb156e2a6a083fb38c89410b8de
Peter Tribble <peter.tribble@gmail.com>
2020-11-13 15:47:42 +0000

    Update mercurial 5.6

--- TRIBmercurial/build.sh
+++ TRIBmercurial/build.sh
@@ -2,12 +2,15 @@
 #
 #
 #
-zap install TRIBdocutils-python-27
-${THOME}/build/unpack mercurial-4.7.2
-cd mercurial-4.7.2
-gmake all
+zap install TRIBdocutils-python-37
+${THOME}/build/unpack mercurial-5.6
+cd mercurial-5.6
+sed -i 's:install -d:ginstall -d:' Makefile
+sed -i 's:install -m:ginstall -m:' Makefile
+sed -i 's:install -m:ginstall -m:' doc/Makefile
+gmake PREFIX=/usr PYTHON=python3 all
 rm -fr /tmp/hg1
-python setup.py install --root=/tmp/hg1
+gmake PREFIX=/usr PYTHON=python3 DESTDIR=/tmp/hg1 install
 ${THOME}/build/create_pkg TRIBmercurial /tmp/hg1
 cd ..
 rm -fr /tmp/hg1
