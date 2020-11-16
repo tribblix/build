fc58a262ef0d0663684ac182cdee8558d5faa195
Peter Tribble <peter.tribble@gmail.com>
2020-11-16 18:05:28 +0000

    Update node v12.19.1

--- TRIBv-node-v12/build.sh
+++ TRIBv-node-v12/build.sh
@@ -2,5 +2,5 @@
 #
 # 64-bit only
 #
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v12.19.0 -P /usr/versions/node-v12 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v12 node-v12.19.0
+env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v12.19.1 -P /usr/versions/node-v12 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v12 node-v12.19.1
