8c71bc3202bc8c1d7b2830f5598434d59fb91e58
Peter Tribble <peter.tribble@gmail.com>
2020-11-20 19:02:29 +0000

    Migrate pkg5 to python3

--- TRIBdev-pkg5/build.sh
+++ TRIBdev-pkg5/build.sh
@@ -10,16 +10,16 @@
 #
 # the build needs six and cffi and libzonecfg.h - see build_requires
 #
-# select a fixed checkout, later versions introduce incompatibilities
-# with the Tribblix pythons
-#
 git clone https://github.com/omniosorg/pkg5
 cd pkg5
-git checkout 3e168821b5bb792b69a1f0ef99a4cfeec095ab58
 cd src
 
+#
+# we have a 32-bit python 3.7, not a 64-bit python 3.9
+#
 sed -i 's:build_64 = True:build_64 = False:' setup.py
-
+sed -i s:3.9:3.7: Makefile
+sed -i s:python3.9:python3.7: `grep -rl python3.9 .`
 #
 # use a regular gcc, rather than the mix of plain gcc
 # and illumos gcc specified in the Makefile
@@ -31,30 +31,30 @@ sed -i 's:build_64 = True:build_64 = False:' setup.py
 # successfully having done nothing, rather than that
 # building python35 is true!
 #
+# NB gmake doesn't work
+#
 make install CC=gcc PYTHON35=true
 cd ../proto/root_*
 
 #
 # I have no idea why this is required, but it doesn't work at all
 #
-sed -i 's:psinfo_size = 232:psinfo_size = 236:' usr/lib/python2.7/vendor-packages/pkg/misc.py
+sed -i 's:psinfo_size = 232:psinfo_size = 236:' usr/lib/python3.7/vendor-packages/pkg/misc.py
 
 #
 # we don't need anything to do with running a pkg server or any of the
-# zone stuff, and we need to not hardcode a 64-bit python
+# zone stuff
 #
 rm -fr etc lib var
 rm -fr usr/include
 rm -fr usr/lib/brand usr/lib/zones
 rm usr/lib/pkg.*
-mkdir -p usr/versions/python-2.7/lib
-mv usr/lib/python2.7 usr/versions/python-2.7/lib
-mv usr/versions/python-2.7/lib/python2.7/vendor-packages usr/versions/python-2.7/lib/python2.7/site-packages
+mkdir -p usr/versions/python-3.7/lib
+mv usr/lib/python3.7 usr/versions/python-3.7/lib
+mv usr/versions/python-3.7/lib/python3.7/vendor-packages usr/versions/python-3.7/lib/python3.7/site-packages
 rm -fr usr/share/lib/pkg/web
 rm usr/share/lib/pkg/rad-invoke
 rm -fr usr/share/man/ja_JP.UTF-8 usr/share/man/zh_CN.UTF-8
 rm -fr usr/share/man/man1m
-sed -i s=/usr/bin/amd64/python2.7=/usr/bin/python2.7= usr/bin/*
-sed -i s=/usr/bin/sparcv9/python2.7=/usr/bin/python2.7= usr/bin/*
 
 ${THOME}/build/create_pkg TRIBdev-pkg5 `pwd`
