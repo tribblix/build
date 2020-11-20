1a9101f819c628d28bc44302aeddc3cbd4958ba3
Peter Tribble <peter.tribble@gmail.com>
2020-11-20 12:16:11 +0000

    Update google-api-core 1.23.0 google-api 1.12.8

--- TRIBgoogle-api-core-python-37/build.sh
+++ TRIBgoogle-api-core-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-api-core-1.22.2
-cd google-api-core-1.22.2
+${THOME}/build/unpack google-api-core-1.23.0
+cd google-api-core-1.23.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
