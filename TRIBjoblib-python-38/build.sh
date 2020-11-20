8419e40099bb9b187a37d003aa373961b8c53d94
Peter Tribble <peter.tribble@gmail.com>
2020-11-20 12:21:49 +0000

    Update joblib 0.17.0

--- TRIBjoblib-python-38/build.sh
+++ TRIBjoblib-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack joblib-0.16.0
-cd joblib-0.16.0
+${THOME}/build/unpack joblib-0.17.0
+cd joblib-0.17.0
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
