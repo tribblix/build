de3f9379b318491e824165fecde5dda35e375a41
Peter Tribble <peter.tribble@gmail.com>
2020-11-23 19:50:00 +0000

    Build findutils 64-bit only

--- TRIBfile-gnu-findutils/build.sh
+++ TRIBfile-gnu-findutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu findutils-4.7.0 -P /usr/gnu
+${THOME}/build/dobuild +64only -gnu findutils-4.7.0 -P /usr/gnu
 ${THOME}/build/genpkg TRIBfile-gnu-findutils findutils-4.7.0
