6ef9d809f63fd0f3d382d23f4f73af60d3ff5d07
Peter Tribble <peter.tribble@gmail.com>
2020-11-23 19:56:15 +0000

    Update gsed 4.8

--- TRIBtext-gnu-sed/build.sh
+++ TRIBtext-gnu-sed/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu sed-4.7 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-sed sed-4.7
+${THOME}/build/dobuild +64only -gnu sed-4.8 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-sed sed-4.8
