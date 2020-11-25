ca93ff1b22ef15750317333036df2769b5fa4e8f
Peter Tribble <peter.tribble@gmail.com>
2020-11-25 15:36:57 +0000

    Update libxml2 2.9.9

--- TRIBlibxml2/build.sh
+++ TRIBlibxml2/build.sh
@@ -4,11 +4,11 @@
 #
 # ***MUST have libtool installed or -nostdlib won't take***
 #
-env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.5 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
-${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.5
+env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.9 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
+${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.9
 
 rm -fr /tmp/lxp
-cd libxml2-2.9.5/python
+cd libxml2-2.9.9/python
 python setup.py build
 python setup.py install --root=/tmp/lxp
 ${THOME}/build/create_pkg TRIBlibxml2-python-27 /tmp/lxp
@@ -16,11 +16,20 @@ python setup.py clean
 cd ../..
 
 rm -fr /tmp/lxp
-cd libxml2-2.9.5/python
+cd libxml2-2.9.9/python
 /usr/versions/python-3.7/bin/python3 setup.py build
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/lxp
 ${THOME}/build/create_pkg TRIBlibxml2-python-37 /tmp/lxp
 /usr/versions/python-3.7/bin/python3 setup.py clean
+cd ../..
+
+# python3.8 is 64-bit
+rm -fr /tmp/lxp
+cd libxml2-2.9.9-64bit/python
+/usr/versions/python-3.8/bin/python3 setup.py build
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/lxp
+${THOME}/build/create_pkg TRIBlibxml2-python-38 /tmp/lxp
+/usr/versions/python-3.8/bin/python3 setup.py clean
 cd ../..
 
 rm -fr /tmp/lxp
--- patches/libxml2-2.9.9.pls
+++ patches/libxml2-2.9.9.pls
@@ -1,2 +1,3 @@
 -p1 libxml2.test_errors_name2.patch
 -p1 libxml2.mapfile.patch
+-p1 libxml2-pthread.patch
