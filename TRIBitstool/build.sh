fe5fdf12172cb23a241c3a084fefc92030b49619
Peter Tribble <peter.tribble@gmail.com>
2020-11-26 11:13:27 +0000

    Update itstool 2.0.6

--- TRIBitstool/build.sh
+++ TRIBitstool/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild itstool-2.0.5
-${THOME}/build/genpkg TRIBitstool itstool-2.0.5
+${THOME}/build/dobuild itstool-2.0.6 -C PYTHON=/usr/versions/python-3.7/bin/python3
+${THOME}/build/genpkg TRIBitstool itstool-2.0.6
