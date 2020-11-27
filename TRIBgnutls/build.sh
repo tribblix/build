1426f3647dc4af5265e70b99219172814bfda8b8
Peter Tribble <peter.tribble@gmail.com>
2020-11-27 12:02:31 +0000

    Update gnutls 3.6.15

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -5,5 +5,7 @@
 # disable tpm because the code isn't even self-consistent and autoconf
 # can't work out what to do on its own
 #
-${THOME}/build/dobuild -64 gnutls-3.3.30 -C "--disable-doc --without-tpm"
-${THOME}/build/genpkg TRIBgnutls gnutls-3.3.30
+# ship a copy of the previous shared library for compat
+#
+${THOME}/build/dobuild -64 gnutls-3.6.15 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBgnutls gnutls-3.6.15
