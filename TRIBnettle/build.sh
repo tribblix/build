ac5d64c5ff76a70c5cb223e8c05e7c183b2401d1
Peter Tribble <peter.tribble@gmail.com>
2020-11-27 10:25:08 +0000

    Update nettle 3.6, retaining compat library copies

--- TRIBnettle/build.sh
+++ TRIBnettle/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 nettle-2.7.1
-${THOME}/build/genpkg TRIBnettle nettle-2.7.1
+# there's a 64-bit preconf script
+#
+# we ship the previous versions of the library for binary
+# compatibility with gnutls + squid + claws-mail + libarchive + vnc
+# once all rebuilt the compat copy can be removed
+#
+${THOME}/build/dobuild -64 nettle-3.6 -C --disable-static
+${THOME}/build/genpkg TRIBnettle nettle-3.6
