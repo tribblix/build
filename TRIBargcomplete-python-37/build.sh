400d70bfe9378b92dc7b27811b9018f9f9d3b612
Peter Tribble <peter.tribble@gmail.com>
2020-11-28 10:17:25 +0000

    Update argcomplete 1.12.2

--- TRIBargcomplete-python-37/build.sh
+++ TRIBargcomplete-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack argcomplete-1.12.1
-cd argcomplete-1.12.1
+${THOME}/build/unpack argcomplete-1.12.2
+cd argcomplete-1.12.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
