64c1b08afc993256d1d40161959261848c755b30
Peter Tribble <peter.tribble@gmail.com>
2020-11-30 20:01:53 +0000

    Rebuild EDE for openssl bump, fixing the build as well

--- TRIBede/build.sh
+++ TRIBede/build.sh
@@ -4,5 +4,9 @@
 # needs jam (I used ftjam) installed
 # and edelib
 #
+# it uses pekwm as the window manager
+# it tries to build its own copy, which fails
+# but that's fine, as we have our own copy that works already
+#
 ${THOME}/build/dobuild -gnu ede-2.1 -L
 ${THOME}/build/genpkg TRIBede ede-2.1
