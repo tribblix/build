c9deb3787fdffe70a4155ca4edeb0f4a6b5413d8
Peter Tribble <peter.tribble@gmail.com>
2020-12-01 18:59:03 +0000

    Rebuild rdesktop for openssl bump

--- TRIBrdesktop/build.sh
+++ TRIBrdesktop/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
+# 1.9.0 switched from openssl to gnutls which would be fine
+# but also requires a separate libtasn1
+#
 ${THOME}/build/dobuild rdesktop-1.8.6 -C "--disable-credssp --disable-smartcard"
 ${THOME}/build/genpkg  TRIBrdesktop rdesktop-1.8.6
