a4078ddb823fb0f84857bc452373fc19bf03b103
Peter Tribble <peter.tribble@gmail.com>
2020-12-04 09:20:50 +0000

    Remove h2o, no longer compiles

--- TRIBblix-h2o/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack h2o-2.2.6
-cd h2o-2.2.6
-cmake -DCMAKE_INSTALL_PREFIX=/opt/tribblix/h2o -DWITH_MRUBY=off -DCMAKE_C_FLAGS=-std=gnu99
-gmake -j 4
-${THOME}/build/genpkg TRIBblix-h2o
-cd ..
