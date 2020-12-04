f4e2f4b7eafb9f085831a36d7b3f3cc0a5f2dd68
Peter Tribble <peter.tribble@gmail.com>
2020-12-04 09:40:04 +0000

    Update s-nail 14.9.19

--- TRIBs-nail/build.sh
+++ TRIBs-nail/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild s-nail-14.9.13 -M "VAL_PREFIX=/usr VAL_SYSCONFDIR=/etc all"
-${THOME}/build/genpkg TRIBs-nail s-nail-14.9.13
+${THOME}/build/dobuild s-nail-14.9.19 -M "VAL_PREFIX=/usr VAL_SYSCONFDIR=/etc all"
+${THOME}/build/genpkg TRIBs-nail s-nail-14.9.19
