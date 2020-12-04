b8bc8b23698ee8a0a31da5920f0c74157f4675fc
Peter Tribble <peter.tribble@gmail.com>
2020-12-04 17:47:40 +0000

    Update fossil 2.13

--- TRIBfossil/build.sh
+++ TRIBfossil/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 # rename the tarball
-#  mv fossil-src-2.7.tar.gz fossil-2.7.tar.gz
+#  mv fossil-src-2.13.tar.gz fossil-2.13.tar.gz
 #
 # we've had a while with 2.0 so the sha1/sha3 switch is history
 #
-${THOME}/build/dobuild fossil-2.7 -C --enable-json
-${THOME}/build/genpkg TRIBfossil fossil-2.7
+${THOME}/build/dobuild fossil-2.13 -C --enable-json
+${THOME}/build/genpkg TRIBfossil fossil-2.13
