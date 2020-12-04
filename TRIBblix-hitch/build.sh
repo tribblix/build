4fa53a86f9650bbdef6ff4236ef31caa5e73de7b
Peter Tribble <peter.tribble@gmail.com>
2020-12-04 09:27:42 +0000

    Remove hitch, no longer compiles

--- TRIBblix-hitch/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-env LIBS="-lsocket -lnsl" CPPFLAGS="-DBSD_COMP -D__EXTENSIONS__" TRIBBLIX_CFLAGS="-Wno-error=format" ${THOME}/build/dobuild hitch-1.4.8 -P /opt/tribblix/hitch -C --with-rst2man=/usr/versions/python-2.7/bin/rst2man.py
-${THOME}/build/genpkg TRIBblix-hitch hitch-1.4.8
