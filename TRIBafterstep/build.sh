003a668a941c2d14c6eb53c7dbad337cf805e868
Peter Tribble <peter.tribble@gmail.com>
2020-12-07 15:44:57 +0000

    Rebuild afterstep, 64-bit only, and clean up manpage extensions

--- TRIBafterstep/build.sh
+++ TRIBafterstep/build.sh
@@ -2,5 +2,7 @@
 #
 # [the original tarball is named AfterStep, rename it]
 #
-${THOME}/build/dobuild -64 afterstep-devel-2.2.12 -C "--enable-staticlibs=no --enable-sharedlibs" -J 1 -L
+# there are a couple of patches via debian needed for current gcc
+#
+${THOME}/build/dobuild -64only afterstep-devel-2.2.12 -C "--enable-staticlibs=no --enable-sharedlibs" -L
 env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/genpkg TRIBafterstep afterstep-devel-2.2.12
---
+++ patches/afterstep-devel-2.2.12.pls
@@ -0,0 +1,2 @@
+-p1 Afterstep-Fix-parallel-builds.patch
+-p1 Afterstep-Fix-build-with-gcc-5.patch
