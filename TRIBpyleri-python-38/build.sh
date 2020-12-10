4546e6c62fa058b84edbf5068c4587890c521084
Peter Tribble <peter.tribble@gmail.com>
2020-12-10 21:10:44 +0000

    Update pyleri 1.3.3

--- TRIBpyleri-python-38/build.sh
+++ TRIBpyleri-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyleri-1.3.2
-cd pyleri-1.3.2
+${THOME}/build/unpack pyleri-1.3.3
+cd pyleri-1.3.3
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
