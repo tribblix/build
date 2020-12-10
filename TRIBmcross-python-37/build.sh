83bc3c6f4cdef5ca055134c95a3c6d3d16ad0cca
Peter Tribble <peter.tribble@gmail.com>
2020-12-10 21:35:26 +0000

    Update McRoss 0.5.19

--- TRIBmcross-python-37/build.sh
+++ TRIBmcross-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack mcross-0.5.18
-cd mcross-0.5.18
+${THOME}/build/unpack mcross-0.5.19
+cd mcross-0.5.19
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
