ce51e24b9d4cffe0a21c2d803c9e175b15d3c324
Peter Tribble <peter.tribble@gmail.com>
2020-12-10 14:02:02 +0000

    Update python regex 2020.11.13

--- TRIBregex-python-37/build.sh
+++ TRIBregex-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack regex-2020.10.28
-cd regex-2020.10.28
+${THOME}/build/unpack regex-2020.11.13
+cd regex-2020.11.13
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
