42375b03a8ba4ec719b63da2757191384c68ad35
Peter Tribble <peter.tribble@gmail.com>
2020-12-10 21:18:50 +0000

    Update dulwich 0.20.14

--- TRIBdulwich-python-38/build.sh
+++ TRIBdulwich-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack dulwich-0.20.11
-cd dulwich-0.20.11
+${THOME}/build/unpack dulwich-0.20.14
+cd dulwich-0.20.14
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
