9e3d578702351c119a99738e596a04da40920346
Peter Tribble <peter.tribble@gmail.com>
2020-12-10 13:46:51 +0000

    Update certifi 2020.12.5

--- TRIBcertifi-python-27/build.sh
+++ TRIBcertifi-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack certifi-2020.11.8
-cd certifi-2020.11.8
+${THOME}/build/unpack certifi-2020.12.5
+cd certifi-2020.12.5
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
--- TRIBcertifi-python-37/build.sh
+++ TRIBcertifi-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack certifi-2020.11.8
-cd certifi-2020.11.8
+${THOME}/build/unpack certifi-2020.12.5
+cd certifi-2020.12.5
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
