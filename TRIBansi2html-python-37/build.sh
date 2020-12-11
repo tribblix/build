b416f0db17f9692e2b760dc8186f96d526b2447d
Peter Tribble <peter.tribble@gmail.com>
2020-12-11 14:06:41 +0000

    Update ansi2html 1.6.0

--- TRIBansi2html-python-37/build.sh
+++ TRIBansi2html-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ansi2html-1.5.2
-cd ansi2html-1.5.2
+${THOME}/build/unpack ansi2html-1.6.0
+cd ansi2html-1.6.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
