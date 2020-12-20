5472dfa5de07f81a5189e9547a614c17b5be7fb7
Peter Tribble <peter.tribble@gmail.com>
2020-12-20 15:43:38 +0000

    Update cloud custodian 0.9.9.0

--- TRIBc7n-python-37/build.sh
+++ TRIBc7n-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack cloud-custodian-0.9.3.0
-cd cloud-custodian-0.9.3.0
+${THOME}/build/unpack cloud-custodian-0.9.9.0
+cd cloud-custodian-0.9.9.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
