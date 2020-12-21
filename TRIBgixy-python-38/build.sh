e3add7b14fadb762e817488e598c3ef48e9da44b
Peter Tribble <peter.tribble@gmail.com>
2020-12-21 21:27:21 +0000

    python38 version of gixy

---
+++ TRIBgixy-python-38/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+#${THOME}/build/unpack gixy-0.1.20
+#cd gixy-0.1.20
+git clone https://github.com/yandex/gixy
+cd gixy
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/sut
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/sut
+${THOME}/build/create_pkg TRIBgixy-python-38 /tmp/sut
+cd ..
+rm -fr /tmp/sut
