fd2fb6ba0925f43828f7c52f4f7c3207097ee090
Peter Tribble <peter.tribble@gmail.com>
2020-12-22 11:48:55 +0000

    Update simplejson 3.17.2

--- TRIBsimplejson-python-37/build.sh
+++ TRIBsimplejson-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack simplejson-3.17.0
-cd simplejson-3.17.0
+${THOME}/build/unpack simplejson-3.17.2
+cd simplejson-3.17.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
