18c8e5bfc298af8f086c4bb8c343d0245a68baa7
Peter Tribble <peter.tribble@gmail.com>
2020-12-23 16:24:10 +0000

    Update CMake 3.19.2

--- TRIBcmake/build.sh
+++ TRIBcmake/build.sh
@@ -2,10 +2,10 @@
 #
 #
 #
-${THOME}/build/unpack cmake-3.11.4
-cd cmake-3.11.4
+${THOME}/build/unpack cmake-3.19.2
+cd cmake-3.19.2
 sed -i 's:DCMAKE_BOOTSTRAP=1:DCMAKE_BOOTSTRAP=1 -DCMAKE_USE_OPENSSL=ON:' bootstrap
-./bootstrap --prefix=/usr/versions/cmake-3.11
+./bootstrap --prefix=/usr/versions/cmake-3.19
 gmake -j 4
 # [gmake test - all passed]
 ${THOME}/build/genpkg TRIBcmake
