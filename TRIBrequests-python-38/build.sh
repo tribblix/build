a84be285bfe4c3af3488c42284cbc97aac0fd977
Peter Tribble <peter.tribble@gmail.com>
2020-12-26 16:04:40 +0000

    Update requests 2.25.1

--- TRIBrequests-python-38/build.sh
+++ TRIBrequests-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack requests-2.25.0
-cd requests-2.25.0
+${THOME}/build/unpack requests-2.25.1
+cd requests-2.25.1
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
