77d6d21b37452e7380fcdf0bed9fad47019b215c
Peter Tribble <peter.tribble@gmail.com>
2021-01-09 14:30:29 +0000

    Update beautifulsoup 4.9.3

--- TRIBbeautifulsoup4-python-37/build.sh
+++ TRIBbeautifulsoup4-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack beautifulsoup4-4.8.2
-cd beautifulsoup4-4.8.2
+${THOME}/build/unpack beautifulsoup4-4.9.3
+cd beautifulsoup4-4.9.3
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
