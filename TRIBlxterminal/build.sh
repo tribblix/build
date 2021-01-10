449c4f6a4ca12dfca6c02816455940425c01dfff
Peter Tribble <peter.tribble@gmail.com>
2021-01-10 18:25:10 +0000

    Add LXTerminal 0.3.2

---
+++ TRIBlxterminal/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# explictly tell it what vte and gtk to use
+#
+${THOME}/build/dobuild -gnu lxterminal-0.3.2 -C "--enable-gtk3 vte_CFLAGS=-I/usr/include/vte-2.91 vte_LIBS=-lvte-2.91 LIBS=-lsocket"
+${THOME}/build/genpkg TRIBlxterminal lxterminal-0.3.2
