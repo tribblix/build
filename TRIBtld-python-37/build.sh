c414f6d18996dd1c4b96b177a7460794f851a211
Peter Tribble <peter.tribble@gmail.com>
2021-01-11 10:43:37 +0000

    Update python tld 0.12.4

--- TRIBtld-python-37/build.sh
+++ TRIBtld-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tld-0.12.3
-cd tld-0.12.3
+${THOME}/build/unpack tld-0.12.4
+cd tld-0.12.4
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
