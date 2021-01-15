414dbfe9a8bf41d198a93ce31b794fd2ca914782
Peter Tribble <peter.tribble@gmail.com>
2021-01-15 13:38:25 +0000

    Update Pillow 8.1.0

--- TRIBpillow-python-38/build.sh
+++ TRIBpillow-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Pillow-7.2.0
-cd Pillow-7.2.0
+${THOME}/build/unpack Pillow-8.1.0
+cd Pillow-8.1.0
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
