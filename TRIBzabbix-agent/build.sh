86b0695a6e94fdb3042ce30868dc065bbec8b630
Peter Tribble <peter.tribble@gmail.com>
2021-01-17 14:06:14 +0000

    Update zabbix 4.0.27

--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-4.0.26 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre-include=/usr/include/pcre"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-4.0.26
+${THOME}/build/dobuild zabbix-4.0.27 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre-include=/usr/include/pcre"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-4.0.27
