a6f8e24b865ac7e9fec39af38d4e9b1f9b38ce79
Peter Tribble <peter.tribble@gmail.com>
2021-01-23 14:16:17 +0000

    Update markdown-include 0.6.0

---
+++ TRIBmarkdown-inc-python-38/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack markdown-include-0.6.0
+cd markdown-include-0.6.0
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBmarkdown-inc-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
