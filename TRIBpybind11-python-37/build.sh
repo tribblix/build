61ce2d8c11698c1ee035c96d3750ba4150439928
Peter Tribble <peter.tribble@gmail.com>
2021-01-29 18:45:08 +0000

    Update pybind11 2.6.2

--- TRIBpybind11-python-37/build.sh
+++ TRIBpybind11-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pybind11-2.4.3
-cd pybind11-2.4.3
+${THOME}/build/unpack pybind11-2.6.2
+cd pybind11-2.6.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
