87d31b14ca27b0cfc4ab3b62384a65cf2bd1107c
Peter Tribble <peter.tribble@gmail.com>
2021-02-06 13:44:53 +0000

    Update openssh 8.4p1

--- TRIBnet-ssh/build.sh
+++ TRIBnet-ssh/build.sh
@@ -1,27 +1,12 @@
 #!/bin/sh
 #
-# [can't get it to link with kerberos]
-#
-# openssh updates required because old and new openssh versions
-# aren't interoperable
-#
-# https://www.gentoo.org/support/news-items/2015-08-13-openssh-weak-keys.html
-# Or a command line like:
-# ssh -o PubkeyAcceptedKeyTypes=+ssh-dss
-# 
-# Remember to update any entries in /etc/pam.conf
-# 
-# We needed:
-# 
-#         s/sshd-kbdint/sshd/
-# 
-# to re-enable our two-factor authentication.
-#
-${THOME}/build/dobuild openssh-7.7p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
+${THOME}/build/dobuild openssh-8.4p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
 #
 # Need to split into 3 packages to match the old layout (so upgrades
 # will get all the pieces)
+# Next time, deprecate and remove TRIBnet-ssh-ssh-key as it's no
+# longer necessary
 #
-${THOME}/build/genpkg TRIBnet-ssh openssh-7.7p1
-${THOME}/build/genpkg TRIBnet-ssh-ssh-key openssh-7.7p1
-${THOME}/build/genpkg TRIBsvc-net-ssh openssh-7.7p1
+${THOME}/build/genpkg TRIBnet-ssh openssh-8.4p1
+${THOME}/build/genpkg TRIBnet-ssh-ssh-key openssh-8.4p1
+${THOME}/build/genpkg TRIBsvc-net-ssh openssh-8.4p1
