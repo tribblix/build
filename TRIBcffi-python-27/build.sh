fc2dc71cd11c599b0629590a2ab4ba3d40dc6b13
Peter Tribble <peter.tribble@gmail.com>
2021-02-06 19:48:26 +0000

    Update cffi 1.14.4

--- TRIBcffi-python-27/build.sh
+++ TRIBcffi-python-27/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack cffi-1.14.0
-cd cffi-1.14.0
+${THOME}/build/unpack cffi-1.14.4
+cd cffi-1.14.4
 python setup.py build
 rm -fr /tmp/pil
 python setup.py install --root=/tmp/pil
