d08a735415aaaa8a41c4002c00f268daf0d6ab08
Peter Tribble <peter.tribble@gmail.com>
2021-02-07 14:43:30 +0000

    python39 version of urllib3

---
+++ TRIBidna-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack idna-3.1
+cd idna-3.1
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBidna-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
---
+++ TRIBurllib3-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack urllib3-1.26.3
+cd urllib3-1.26.3
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBurllib3-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
