ce7c15e6f26d24441c4575f8f6a61277f6b48796
Peter Tribble <peter.tribble@gmail.com>
2021-02-07 18:04:20 +0000

    Update socat 1.7.4.1

--- TRIBsocat/build.sh
+++ TRIBsocat/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild socat-1.7.3.4
-${THOME}/build/genpkg TRIBsocat socat-1.7.3.4
+${THOME}/build/dobuild socat-1.7.4.1
+${THOME}/build/genpkg TRIBsocat socat-1.7.4.1
