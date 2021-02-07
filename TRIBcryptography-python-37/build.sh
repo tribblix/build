bf17507d127551a2c6894a3bcb87c43718a5c941
Peter Tribble <peter.tribble@gmail.com>
2021-02-07 14:15:36 +0000

    Update python cryptography 3.3.1

--- TRIBcryptography-python-37/build.sh
+++ TRIBcryptography-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack cryptography-2.9.2
-cd cryptography-2.9.2
+${THOME}/build/unpack cryptography-3.3.1
+cd cryptography-3.3.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
