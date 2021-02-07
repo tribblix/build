2e861c38657d9eff5efbbbcabafd86f220936b32
Peter Tribble <peter.tribble@gmail.com>
2021-02-07 17:22:53 +0000

    Update mkdocs-material 6.2.8

--- TRIBmkdocs-material-python-37/build.sh
+++ TRIBmkdocs-material-python-37/build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 #
-${THOME}/build/unpack mkdocs-material-4.6.3
-cd mkdocs-material-4.6.3
-/usr/versions/python-3.7/bin/python3 setup.py build
+# mkdocs-material and mkdocs-material-extensions have a circular dependency,
+# so package them together
+#
 rm -fr /tmp/pil
+${THOME}/build/unpack mkdocs-material-6.2.8
+cd mkdocs-material-6.2.8
+/usr/versions/python-3.7/bin/python3 setup.py build
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmkdocs-material-python-37 /tmp/pil
 cd ..
+${THOME}/build/unpack mkdocs-material-extensions-1.0.1
+cd mkdocs-material-extensions-1.0.1
+/usr/versions/python-3.7/bin/python3 setup.py build
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+cd ..
+${THOME}/build/create_pkg TRIBmkdocs-material-python-37 /tmp/pil
 rm -fr /tmp/pil
