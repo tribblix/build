de1450485cef623351072cd530ecc407f17d6613
Peter Tribble <peter.tribble@gmail.com>
2021-02-08 13:10:17 +0000

    Update chardet 4.0.0

--- TRIBchardet-python-37/build.sh
+++ TRIBchardet-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack chardet-3.0.4
-cd chardet-3.0.4
+${THOME}/build/unpack chardet-4.0.0
+cd chardet-4.0.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
