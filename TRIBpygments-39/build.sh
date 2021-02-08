482be1e25407577367ac8bfc96f374ddb5bc4b6c
Peter Tribble <peter.tribble@gmail.com>
2021-02-08 13:37:56 +0000

    python39 version of pygments

---
+++ TRIBpygments-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack Pygments-2.7.4
+cd Pygments-2.7.4
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pdu
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBpygments-39 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
