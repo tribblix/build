4b9943355eb9c24f65e527036ad1887fb6e6df91
Peter Tribble <peter.tribble@gmail.com>
2021-02-08 13:38:28 +0000

    Update httpie 2.4.0

--- TRIBhttpie-python-38/build.sh
+++ TRIBhttpie-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httpie-2.3.0
-cd httpie-2.3.0
+${THOME}/build/unpack httpie-2.4.0
+cd httpie-2.4.0
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
