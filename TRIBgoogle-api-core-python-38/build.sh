5970bae9a1613c02d21f999f86c9b6ad1e09c943
Peter Tribble <peter.tribble@gmail.com>
2021-02-10 11:10:28 +0000

    Update google-api-core 1.26.0

--- TRIBgoogle-api-core-python-38/build.sh
+++ TRIBgoogle-api-core-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-api-core-1.25.1
-cd google-api-core-1.25.1
+${THOME}/build/unpack google-api-core-1.26.0
+cd google-api-core-1.26.0
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
