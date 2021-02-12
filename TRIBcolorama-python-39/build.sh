d42d10a3dffed1eefe965701fd37c0bc5f7160ec
Peter Tribble <peter.tribble@gmail.com>
2021-02-12 18:29:51 +0000

    python39 version of RBtools

---
+++ TRIBcolorama-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack colorama-0.4.4
+cd colorama-0.4.4
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pdu
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBcolorama-python-39 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
