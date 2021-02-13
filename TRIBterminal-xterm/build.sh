9ed485758e306e3d315d6a3ed93f98d69b0253a9
Peter Tribble <peter.tribble@gmail.com>
2021-02-13 17:59:55 +0000

    Update xterm 366

--- TRIBterminal-xterm/build.sh
+++ TRIBterminal-xterm/build.sh
@@ -2,5 +2,5 @@
 #
 # options mostly the same as OI
 #
-$THOME/build/dobuild xterm-330 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
-${THOME}/build/genpkg TRIBterminal-xterm xterm-330
+$THOME/build/dobuild xterm-366 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
+${THOME}/build/genpkg TRIBterminal-xterm xterm-366
