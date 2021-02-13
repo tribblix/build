db8f334043f6923551fd45760da8e681faf7e3ce
Peter Tribble <peter.tribble@gmail.com>
2021-02-13 12:46:28 +0000

    Update google-auth 1.26.1

--- TRIBgoogle-auth-python-38/build.sh
+++ TRIBgoogle-auth-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack google-auth-1.25.0
-cd google-auth-1.25.0
+${THOME}/build/unpack google-auth-1.26.1
+cd google-auth-1.26.1
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
