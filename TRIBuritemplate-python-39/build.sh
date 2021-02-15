8893747ee782f4ddedc85c067c9d08567d60d064
Peter Tribble <peter.tribble@gmail.com>
2021-02-15 19:32:22 +0000

    python39 versions of gapi modules

---
+++ TRIBuritemplate-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack uritemplate-3.0.1
+cd uritemplate-3.0.1
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBuritemplate-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
