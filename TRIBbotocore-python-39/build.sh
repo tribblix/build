64197e11917562924ebaf968a3734cb9568c9a8e
Peter Tribble <peter.tribble@gmail.com>
2021-02-15 12:57:45 +0000

    python39 version of awscli

---
+++ TRIBbotocore-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack botocore-1.20.7
+cd botocore-1.20.7
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pdu
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBbotocore-python-39 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
---
+++ TRIBs3transfer-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack s3transfer-0.3.4
+cd s3transfer-0.3.4
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pdu
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
+${THOME}/build/create_pkg TRIBs3transfer-python-39 /tmp/pdu
+cd ..
+rm -fr /tmp/pdu
