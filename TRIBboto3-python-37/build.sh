dfdf0afde9ac9965b3f1a0c0039ea0257935adbd
Peter Tribble <peter.tribble@gmail.com>
2021-02-15 12:50:19 +0000

    Update botocore 1.20.7 s3transfer 0.3.4 awscli 1.19.7 boto3 1.17.7

--- TRIBboto3-python-37/build.sh
+++ TRIBboto3-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boto3-1.16.40
-cd boto3-1.16.40
+${THOME}/build/unpack boto3-1.17.7
+cd boto3-1.17.7
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/sut
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/sut
