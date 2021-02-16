1f4f8e8fd204da037401d85dbbd4e71a0bf3dcb8
Peter Tribble <peter.tribble@gmail.com>
2021-02-16 18:20:00 +0000

    python39 versions of magic and detect-secrets

---
+++ TRIBdetect-secrets-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack detect_secrets-0.14.3
+cd detect_secrets-0.14.3
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBdetect-secrets-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
