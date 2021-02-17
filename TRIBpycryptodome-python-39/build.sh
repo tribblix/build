60cdcf50adf89b2fbfe0b56e1e7d1ed0af8fede5
Peter Tribble <peter.tribble@gmail.com>
2021-02-17 19:16:55 +0000

    python39 modules for patroni

---
+++ TRIBpycryptodome-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack pycryptodome-3.9.9
+cd pycryptodome-3.9.9
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpycryptodome-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
