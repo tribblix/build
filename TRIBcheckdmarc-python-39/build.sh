54d8899d6af6af4c3a14aa3348b488a34100c318
Peter Tribble <peter.tribble@gmail.com>
2021-02-17 19:25:17 +0000

    python39 modules for checkdmarc

---
+++ TRIBcheckdmarc-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack checkdmarc-4.3.1
+cd checkdmarc-4.3.1
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBcheckdmarc-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
