20a3cb7cf641cd18ebe890a9d140d0e751064d16
Peter Tribble <peter.tribble@gmail.com>
2021-02-18 16:01:30 +0000

    Update bind 9.11.28

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -2,5 +2,5 @@
 #
 # must not have TRIBlibedit installed
 #
-${THOME}/build/dobuild bind-9.11.27 -C "--sysconfdir=/etc --localstatedir=/var --without-python"
-${THOME}/build/genpkg TRIBbind9 bind-9.11.27
+${THOME}/build/dobuild bind-9.11.28 -C "--sysconfdir=/etc --localstatedir=/var --without-python"
+${THOME}/build/genpkg TRIBbind9 bind-9.11.28
