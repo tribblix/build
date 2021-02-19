8f588aedacdc670f1acefe036143a01465c649fc
Peter Tribble <peter.tribble@gmail.com>
2021-02-19 09:42:21 +0000

    Update ConfigArgParse 1.3

--- TRIBconfigargparse-python-38/build.sh
+++ TRIBconfigargparse-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ConfigArgParse-1.2.3
-cd ConfigArgParse-1.2.3
+${THOME}/build/unpack ConfigArgParse-1.3
+cd ConfigArgParse-1.3
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
