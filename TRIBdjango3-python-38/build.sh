a38d70a1736c81dd6fee0e252427d7f18a3b71da
Peter Tribble <peter.tribble@gmail.com>
2021-02-20 15:09:15 +0000

    Update Django 3.1.7

--- TRIBdjango3-python-38/build.sh
+++ TRIBdjango3-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Django-3.1.6
-cd Django-3.1.6
+${THOME}/build/unpack Django-3.1.7
+cd Django-3.1.7
 rm -fr /tmp/p3a
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango3-python-38 /tmp/p3a
