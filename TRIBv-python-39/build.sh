6a0b9fdb3e412509f4daac0298b3ba58fb69bde2
Peter Tribble <peter.tribble@gmail.com>
2021-02-22 15:01:37 +0000

    Update Python 3.9.2

--- TRIBv-python-39/build.sh
+++ TRIBv-python-39/build.sh
@@ -5,6 +5,6 @@
 #
 # 3.9 is in testing, 3.7 is still primary
 #
-${THOME}/build/dobuild +64only Python-3.9.1 -P /usr/versions/python-3.9 -L -C --enable-shared
+${THOME}/build/dobuild +64only Python-3.9.2 -P /usr/versions/python-3.9 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-39 Python-3.9.1
+${THOME}/build/genpkg TRIBv-python-39 Python-3.9.2
