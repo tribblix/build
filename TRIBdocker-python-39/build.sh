0f1e06ea5b7ff710af1802ba6cec9a94f293a09e
Peter Tribble <peter.tribble@gmail.com>
2021-02-26 09:04:15 +0000

    Update python docker 4.4.4

--- TRIBdocker-python-39/build.sh
+++ TRIBdocker-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack docker-4.4.3
-cd docker-4.4.3
+${THOME}/build/unpack docker-4.4.4
+cd docker-4.4.4
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
