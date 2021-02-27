71391282019b8046048ef3034aedf09470d61ae4
Peter Tribble <peter.tribble@gmail.com>
2021-02-27 09:45:29 +0000

    Update python crtsh 0.3.1

--- TRIBcrtsh-python-38/build.sh
+++ TRIBcrtsh-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack crtsh-0.2.0
-cd crtsh-0.2.0
+${THOME}/build/unpack crtsh-0.3.1
+cd crtsh-0.3.1
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
