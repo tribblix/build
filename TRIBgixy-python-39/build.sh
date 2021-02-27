a4fc1cb9eea97335a45507aeed36cc110d8c94b1
Peter Tribble <peter.tribble@gmail.com>
2021-02-27 10:57:52 +0000

    python39 version of gixy

---
+++ TRIBgixy-python-39/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+#${THOME}/build/unpack gixy-0.1.20
+#cd gixy-0.1.20
+git clone https://github.com/yandex/gixy
+cd gixy
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/sut
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/sut
+${THOME}/build/create_pkg TRIBgixy-python-39 /tmp/sut
+cd ..
+rm -fr /tmp/sut
