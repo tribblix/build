d9a267a97f58eaa904caa1d7a114ed2cb6f6cb56
Peter Tribble <peter.tribble@gmail.com>
2021-02-28 21:05:11 +0000

    Update mousepad 0.5.3

--- TRIBxfce-mousepad/build.sh
+++ TRIBxfce-mousepad/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 #
-# wants gtksourceview-3.0 by default
-# OK, so you need to pass --disable-gtk3 and it'll pull in
-# gtksourceview-2.0
-#
-env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -gnu mousepad-0.4.2 -C --disable-gtk3
-${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.4.2
+env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -gnu mousepad-0.5.3
+${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.5.3
