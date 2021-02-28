cbf8887338e983de0e045957c23e85d751d6906c
Peter Tribble <peter.tribble@gmail.com>
2021-02-28 14:31:15 +0000

    Update util-macros 1.19.3

--- TRIBdev-xorg-macros/build.sh
+++ TRIBdev-xorg-macros/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild util-macros-1.19.2
-${THOME}/build/genpkg TRIBdev-xorg-macros util-macros-1.19.2
+${THOME}/build/dobuild util-macros-1.19.3
+${THOME}/build/genpkg TRIBdev-xorg-macros util-macros-1.19.3
