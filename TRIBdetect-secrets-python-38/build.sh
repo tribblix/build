7e3fa5a78d844b9a9509619b0d24adfdd2480aeb
Peter Tribble <peter.tribble@gmail.com>
2021-03-01 10:19:37 +0000

    Update detect-secrets 1.0.3

--- TRIBdetect-secrets-python-38/build.sh
+++ TRIBdetect-secrets-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack detect_secrets-1.0.0
-cd detect_secrets-1.0.0
+${THOME}/build/unpack detect_secrets-1.0.3
+cd detect_secrets-1.0.3
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
