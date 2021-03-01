8a669feb9fe95331913a820966b167bbe53941cd
Peter Tribble <peter.tribble@gmail.com>
2021-03-01 19:40:26 +0000

    python38 version of c7n

---
+++ TRIBc7n-python-38/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# get the tarball from https://github.com/cloud-custodian/cloud-custodian
+# rather than PyPi, which only has the wheel and not source
+#
+${THOME}/build/unpack cloud-custodian-0.9.10.0
+cd cloud-custodian-0.9.10.0
+/usr/versions/python-3.8/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBc7n-python-38 /tmp/pil
+cd ..
+rm -fr /tmp/pil
