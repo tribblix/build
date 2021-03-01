b098537dbe5492ad194756033fbffafbfbf34b17
Peter Tribble <peter.tribble@gmail.com>
2021-03-01 09:20:38 +0000

    Remove gtkimageview gtkglext as nothing uses them any more

--- TRIBgtkglext/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild -64 gtkglext-1.2.0
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/genpkg TRIBgtkglext gtkglext-1.2.0
--- TRIBgtkimageview/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# (download the tarball from OI)
-#
-${THOME}/build/dobuild gtkimageview-1.6.4 -C CFLAGS=-D__EXTENSIONS__
-${THOME}/build/genpkg TRIBgtkimageview gtkimageview-1.6.4
