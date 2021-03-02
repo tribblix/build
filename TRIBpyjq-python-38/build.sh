4a64e4e74aac6fbac08cdd860ee474a30c30fb1f
Peter Tribble <peter.tribble@gmail.com>
2021-03-02 19:31:49 +0000

    Update pyjq 2.5.1

--- TRIBpyjq-python-38/build.sh
+++ TRIBpyjq-python-38/build.sh
@@ -17,9 +17,9 @@ rm -fr jq-jq-1.5
 # those aren't makefiles, they're gnu makefiles
 # install appears to repeat the build
 #
-env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.7/bin/python3 setup.py build
+env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
-env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpyjq-python-37 /tmp/pil
+env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBpyjq-python-38 /tmp/pil
 cd ..
 rm -fr /tmp/pil
