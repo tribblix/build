506b4def71bc46550c381da092ed5e173f8d870e
Peter Tribble <peter.tribble@gmail.com>
2021-03-03 21:34:39 +0000

    python39 version of reportlab

---
+++ TRIBreportlab-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack reportlab-3.5.59
+cd reportlab-3.5.59
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBreportlab-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
