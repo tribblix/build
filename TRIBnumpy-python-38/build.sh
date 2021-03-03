5b37bb973c2925d4789916b7d5efa722b491c7ed
Peter Tribble <peter.tribble@gmail.com>
2021-03-03 19:42:51 +0000

    Update NumPy 1.20.1

--- TRIBnumpy-python-38/build.sh
+++ TRIBnumpy-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack numpy-1.18.2
-cd numpy-1.18.2
+${THOME}/build/unpack numpy-1.20.1
+cd numpy-1.20.1
 env CC="gcc -m64" /usr/versions/python-3.8/bin/python3 setup.py build -j 4
 rm -fr /tmp/hg1
 env CC="gcc -m64" /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/hg1
