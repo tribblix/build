554f96aae594aa1b43b84647a7faccbe56b667b2
Peter Tribble <peter.tribble@gmail.com>
2021-03-04 13:14:02 +0000

    Update defusedxml 0.7.0

--- TRIBdefusedxml-python-37/build.sh
+++ TRIBdefusedxml-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack defusedxml-0.6.0
-cd defusedxml-0.6.0
+${THOME}/build/unpack defusedxml-0.7.0
+cd defusedxml-0.7.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
