6016425022e7bbf14414d95e404264c97a0f2931
Peter Tribble <peter.tribble@gmail.com>
2021-03-05 09:45:00 +0000

    Updtae pycryptodome 3.10.1

--- TRIBpycryptodome-python-38/build.sh
+++ TRIBpycryptodome-python-38/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pycryptodome-3.9.9
-cd pycryptodome-3.9.9
+${THOME}/build/unpack pycryptodome-3.10.1
+cd pycryptodome-3.10.1
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
