281034fc4aac3331dfe4afcc1ce2a94feac24c15
Peter Tribble <peter.tribble@gmail.com>
2021-03-06 17:08:05 +0000

    python 38 and 39 versions of maxminddb and geoip2

---
+++ TRIBgeoip2-python-39/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack geoip2-3.0.0
+cd geoip2-3.0.0
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBgeoip2-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
