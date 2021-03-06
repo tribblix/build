254af94898ed24c5a178d0931efba37496cf5511
Peter Tribble <peter.tribble@gmail.com>
2021-03-06 16:49:33 +0000

    Update SciPy 1.6.1

---
+++ TRIBscipy-python-39/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+${THOME}/build/unpack scipy-1.6.1
+cd scipy-1.6.1
+#
+# it's relatively hard work to get the build to be consistently 64-bit
+#
+env CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 /usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+env CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBscipy-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
