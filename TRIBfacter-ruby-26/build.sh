aabf45cbac87dcdf9c4144fd067c633293f1b8ac
Peter Tribble <peter.tribble@gmail.com>
2021-03-06 12:34:22 +0000

    Ruby 2.7 should be the default

--- TRIBfacter-ruby-26/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack facter-2.5.7
-cd facter-2.5.7
-rm -fr /tmp/ffa
-/usr/versions/ruby-2.6/bin/ruby install.rb --destdir=/tmp/ffa
-${THOME}/build/create_pkg TRIBfacter-ruby-26 /tmp/ffa
-rm -fr /tmp/ffa
-cd ..
