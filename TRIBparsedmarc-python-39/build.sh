31aae7a019122491513c37b7b5c29d54ae224a11
Peter Tribble <peter.tribble@gmail.com>
2021-03-06 17:11:55 +0000

    python 38 and 39 versions of parsedmarc

---
+++ TRIBparsedmarc-python-39/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+${THOME}/build/unpack parsedmarc-6.7.4
+cd parsedmarc-6.7.4
+#
+# check that the dependencies aren't overconstrained
+#
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBparsedmarc-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
