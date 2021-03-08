33051a0b125b0de03e4df9d4de61d37bf13dbcca
Peter Tribble <peter.tribble@gmail.com>
2021-03-08 12:59:50 +0000

    Update pyxdg 0.27

--- TRIBxdg-python-37/build.sh
+++ TRIBxdg-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pyxdg-0.26
-cd pyxdg-0.26
+${THOME}/build/unpack pyxdg-0.27
+cd pyxdg-0.27
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/xx1
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/xx1
