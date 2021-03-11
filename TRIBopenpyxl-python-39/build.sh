8ffe477f975d0137ba08bd8c2bf41b7e507255b9
Peter Tribble <peter.tribble@gmail.com>
2021-03-11 08:40:39 +0000

    Update openpyxl 3.0.7

--- TRIBopenpyxl-python-39/build.sh
+++ TRIBopenpyxl-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack openpyxl-3.0.6
-cd openpyxl-3.0.6
+${THOME}/build/unpack openpyxl-3.0.7
+cd openpyxl-3.0.7
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
