fe9ba713320f8acdb9a90f7b24801bc8e5fe1d7b
Peter Tribble <peter.tribble@gmail.com>
2021-03-12 11:34:01 +0000

    Update Go 1.16.2

--- TRIBv-go-116/build.sh
+++ TRIBv-go-116/build.sh
@@ -12,7 +12,7 @@
 # solaris and illumos toolchain, which seems suboptimal
 #
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.16.1.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.16.2.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.16 GOROOT_BOOTSTRAP=/usr/versions/go-1.15 ./all.bash
 cd ..
