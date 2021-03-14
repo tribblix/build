14654031bd2e6102e94183e220aa7235450570d9
Peter Tribble <peter.tribble@gmail.com>
2021-03-14 15:54:51 +0000

    Bump python elastic modules to 7.x

--- TRIBelastic-dsl-python-38/build.sh
+++ TRIBelastic-dsl-python-38/build.sh
@@ -4,8 +4,8 @@
 # it was originally called elasticsearch-dsl-py, now it's just
 # the simple elasticsearch-dsl
 #
-${THOME}/build/unpack elasticsearch-dsl-6.4.0
-cd elasticsearch-dsl-6.4.0
+${THOME}/build/unpack elasticsearch-dsl-7.3.0
+cd elasticsearch-dsl-7.3.0
 /usr/versions/python-3.8/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
--- TRIBelasticsearch-python-39/build.sh
+++ TRIBelasticsearch-python-39/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #
+# the major version here must match elastic-dsl
+#
 # note that elastic have messed with the name and versioning
 # it was originally called elasticsearch-py, now it's got the
 # simple elasticsearchN name where N is the release
 #
-${THOME}/build/unpack elasticsearch6-6.8.2
-cd elasticsearch6-6.8.2
+${THOME}/build/unpack elasticsearch7-7.11.0
+cd elasticsearch7-7.11.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
