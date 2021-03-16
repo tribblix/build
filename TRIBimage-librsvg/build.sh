c56707e21b3cab3edc8469941156656c5bb2353a
Peter Tribble <peter.tribble@gmail.com>
2021-03-16 20:19:41 +0000

    Update librsvg 2.40.21

--- TRIBimage-librsvg/build.sh
+++ TRIBimage-librsvg/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 librsvg-2.36.4 -C "--disable-Bsymbolic --disable-introspection"
-${THOME}/build/genpkg TRIBimage-librsvg librsvg-2.36.4
+# 2.42 requires a cairo update, so stick to 2.40
+#
+${THOME}/build/dobuild -64 librsvg-2.40.21 -C "--disable-Bsymbolic --disable-introspection --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBimage-librsvg librsvg-2.40.21
