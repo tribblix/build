4d0e0085d31492f6128da82cf1490152828b680e
Peter Tribble <peter.tribble@gmail.com>
2021-03-17 09:21:51 +0000

    Update curio 1.5

--- TRIBcurio-python-37/build.sh
+++ TRIBcurio-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack curio-1.4
-cd curio-1.4
+${THOME}/build/unpack curio-1.5
+cd curio-1.5
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
