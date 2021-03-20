3182f41ec1cc835e3d69d05c65c9912480d02279
Peter Tribble <peter.tribble@gmail.com>
2021-03-20 19:49:54 +0000

    Add libzmf

---
+++ TRIBlibzmf/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# wget https://dev-www.libreoffice.org/src/libzmf/libzmf-0.0.2.tar.xz
+#
+${THOME}/build/dobuild libzmf-0.0.2 -C --disable-werror
+${THOME}/build/genpkg TRIBlibzmf libzmf-0.0.2
