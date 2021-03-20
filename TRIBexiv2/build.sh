af1fd45a0f9151f4fea44b92923848bd454b360b
Peter Tribble <peter.tribble@gmail.com>
2021-03-20 11:48:47 +0000

    Update exiv2 0.27.3

--- TRIBexiv2/build.sh
+++ TRIBexiv2/build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 #
-# the 0.26 tarball is called exiv2-0.26-trunk but unpacks into exiv2-trunk
-# so rename the tarball
-# mv exiv2-0.26-trunk.tar.gz exiv2-trunk.tar.gz
-#
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -gnu exiv2-trunk -C --with-libiconv-prefix=/usr/gnu
-${THOME}/build/genpkg TRIBexiv2 exiv2-trunk
+
+env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64 exiv2-0.27.3-Source -C -DCMAKE_BUILD_TYPE=Release
+${THOME}/build/genpkg TRIBexiv2 exiv2-0.27.3-Source
