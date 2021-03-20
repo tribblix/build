19da5a7e92a9f3247550e4ade6af4b68678ade4e
Peter Tribble <peter.tribble@gmail.com>
2021-03-20 09:58:02 +0000

    Update ConfigArgParse 1.4

--- TRIBconfigargparse-python-39/build.sh
+++ TRIBconfigargparse-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ConfigArgParse-1.3
-cd ConfigArgParse-1.3
+${THOME}/build/unpack ConfigArgParse-1.4
+cd ConfigArgParse-1.4
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
