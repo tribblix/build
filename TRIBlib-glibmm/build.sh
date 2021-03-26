29e9e2d09bcb80f7822c6931e0da26fcd63d3a80
Peter Tribble <peter.tribble@gmail.com>
2021-03-26 19:53:16 +0000

    Update glibmm 2.56.1

--- TRIBlib-glibmm/build.sh
+++ TRIBlib-glibmm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu glibmm-2.44.0
-${THOME}/build/genpkg TRIBlib-glibmm glibmm-2.44.0
+${THOME}/build/dobuild -64 -gnu glibmm-2.56.1
+${THOME}/build/genpkg TRIBlib-glibmm glibmm-2.56.1
