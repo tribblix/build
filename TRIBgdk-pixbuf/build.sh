8e255beea0435abe7cc21d681a4ff20158139e3d
Peter Tribble <peter.tribble@gmail.com>
2021-03-26 10:09:04 +0000

    Update gdk-pixbuf 2.36.12

--- TRIBgdk-pixbuf/build.sh
+++ TRIBgdk-pixbuf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 gdk-pixbuf-2.34.0 -C "--with-x11 --disable-introspection"
-${THOME}/build/genpkg TRIBgdk-pixbuf gdk-pixbuf-2.34.0
+${THOME}/build/dobuild -64 gdk-pixbuf-2.36.12 -C "--with-x11 --disable-introspection"
+${THOME}/build/genpkg TRIBgdk-pixbuf gdk-pixbuf-2.36.12
