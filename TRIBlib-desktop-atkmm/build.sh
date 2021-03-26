eed9408bd1a7ce665c5bdc52bc6ce4baf755145b
Peter Tribble <peter.tribble@gmail.com>
2021-03-26 19:55:49 +0000

    Update atkmm 2.24.3

--- TRIBlib-desktop-atkmm/build.sh
+++ TRIBlib-desktop-atkmm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu atkmm-2.22.7
-${THOME}/build/genpkg TRIBlib-desktop-atkmm atkmm-2.22.7
+${THOME}/build/dobuild -64 -gnu atkmm-2.24.3
+${THOME}/build/genpkg TRIBlib-desktop-atkmm atkmm-2.24.3
