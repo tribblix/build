bf65ed28726f521617fd211ed903fd485b94ddfb
Peter Tribble <peter.tribble@gmail.com>
2021-03-26 19:52:38 +0000

    Update fftw 3.3.9, and enable fftw3f

--- TRIBfftw3/build.sh
+++ TRIBfftw3/build.sh
@@ -3,5 +3,5 @@
 # use the same flags to configure as OI
 # --enable-float for fftw3f
 #
-${THOME}/build/dobuild -64 fftw-3.3.5 -C "--enable-float --enable-threads --enable-shared --disable-static --with-combined-threads"
-${THOME}/build/genpkg TRIBfftw3 fftw-3.3.5
+${THOME}/build/dobuild -64 fftw-3.3.9 -C "--enable-float --enable-threads --enable-shared --disable-static --with-combined-threads"
+${THOME}/build/genpkg TRIBfftw3 fftw-3.3.9
