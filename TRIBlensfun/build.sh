a7dfaf933c1980f2db705729d6734ca49292dc0e
Peter Tribble <peter.tribble@gmail.com>
2021-03-27 14:38:23 +0000

    Rebuild lensfun to add 64-bit

--- TRIBlensfun/build.sh
+++ TRIBlensfun/build.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack lensfun-0.3.2
-cd lensfun-0.3.2
-mkdir cmake_build
-cd cmake_build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
-gmake
-rm -fr /tmp/ll
-gmake -k install DESTDIR=/tmp/ll
-${THOME}/build/create_pkg TRIBlensfun /tmp/ll
-rm -fr /tmp/ll
+${THOME}/build/cmbuild -64 lensfun-0.3.2
+${THOME}/build/genpkg TRIBlensfun lensfun-0.3.2
