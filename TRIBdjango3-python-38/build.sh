ed4db2af34c4d95e84205e00a135530a1f397d0f
Peter Tribble <peter.tribble@gmail.com>
2021-03-27 10:10:39 +0000

    Drop python 3.8 modules

--- TRIBdjango3-python-38/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack Django-3.1.7
-cd Django-3.1.7
-rm -fr /tmp/p3a
-/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/p3a
-${THOME}/build/create_pkg TRIBdjango3-python-38 /tmp/p3a
-cd ..
-rm -fr /tmp/p3a
--- TRIBxdg-python-38/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack pyxdg-0.27
-cd pyxdg-0.27
-/usr/versions/python-3.8/bin/python3 setup.py build
-rm -fr /tmp/xx1
-/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/xx1
-${THOME}/build/create_pkg TRIBxdg-python-38 /tmp/xx1
-rm -fr /tmp/xx1
