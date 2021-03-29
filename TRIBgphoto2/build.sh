86442171304172d85784382dab2429a66e0d279b
Peter Tribble <peter.tribble@gmail.com>
2021-03-29 20:09:41 +0100

    Update gphoto 2.5.27

--- TRIBgphoto2/build.sh
+++ TRIBgphoto2/build.sh
@@ -2,5 +2,5 @@
 #
 # needs TRIBlibgphoto2
 #
-${THOME}/build/dobuild gphoto2-2.5.15
-${THOME}/build/genpkg TRIBgphoto2 gphoto2-2.5.15
+${THOME}/build/dobuild gphoto2-2.5.27
+${THOME}/build/genpkg TRIBgphoto2 gphoto2-2.5.27
