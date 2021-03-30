4331289122789e4d28f2dc73f79ad552bb3bf4b7
Peter Tribble <peter.tribble@gmail.com>
2021-03-30 11:45:31 +0100

    Add 64-bit enchant build

--- TRIBenchant/build.sh
+++ TRIBenchant/build.sh
@@ -2,5 +2,5 @@
 #
 # Pidgin wants gtk-spell which requires enchant
 #
-${THOME}/build/dobuild enchant-1.6.0
+${THOME}/build/dobuild -64 enchant-1.6.0 -C "--disable-aspell"
 ${THOME}/build/genpkg TRIBenchant enchant-1.6.0
