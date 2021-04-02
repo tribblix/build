753934a1ede2a4490ef43347f94b67918b79a165
Peter Tribble <peter.tribble@gmail.com>
2021-04-02 12:14:16 +0100

    Update python regex 2021.3.17

--- TRIBregex-python-39/build.sh
+++ TRIBregex-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack regex-2020.11.13
-cd regex-2020.11.13
+${THOME}/build/unpack regex-2021.3.17
+cd regex-2021.3.17
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
