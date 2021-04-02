475cc3d8d65d7721ae17fde4fc7a2e3bdaed199e
Peter Tribble <peter.tribble@gmail.com>
2021-04-02 17:46:36 +0100

    Update minio client

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -2,23 +2,7 @@
 #
 mkdir ~/ud/c
 cd ~/ud/c
-env GO111MODULE=on GOPATH=`pwd` /usr/versions/go-1.13/bin/go get -u github.com/minio/mc
-#
-# you will need to fix:
-#
-# pkg/mod/github.com/minio/minio@v0.0.0-***WHATEVER***/pkg/cpu/counter_other.go
-# -> add solaris to the list of build targets (would be smarter to say !linux)
-#
-# pkg/mod/github.com/minio/mc@v0.0.0-***WHATEVER***/pkg/disk
-# -> copy stat_openbsd.go to stat_solaris.go, change build to solaris,
-# and change syscall to unix
-#
-# pkg/mod/github.com/ncw/directio@v1.0.5/direct_io_unix.go
-# -> remove the syscall entirely so it just stubs openFile
-#
-# and then run the go get again
-#
-env GO111MODULE=on GOPATH=`pwd` /usr/versions/go-1.13/bin/go get -u github.com/minio/mc
+env GO111MODULE=on GOPATH=`pwd` /usr/versions/go-1.15/bin/go get github.com/minio/mc
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
