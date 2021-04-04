15c4e1cccfedb566597304be43cbeba8a35359f6
Peter Tribble <peter.tribble@gmail.com>
2021-04-04 10:15:02 +0100

    Update virtualenv 20.4.3

--- TRIBvirtualenv-python-37/build.sh
+++ TRIBvirtualenv-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack virtualenv-20.4.2
-cd virtualenv-20.4.2
+${THOME}/build/unpack virtualenv-20.4.3
+cd virtualenv-20.4.3
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
