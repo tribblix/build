513cecc4605cb5f400e96824c7cee3cbfa8d5ffb
Peter Tribble <peter.tribble@gmail.com>
2021-04-04 12:33:50 +0100

    Update botocore 1.20.44 s3transfer 0.3.6 awscli 1.19.44 boto3 1.17.44

--- TRIBbotocore-python-39/build.sh
+++ TRIBbotocore-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack botocore-1.20.7
-cd botocore-1.20.7
+${THOME}/build/unpack botocore-1.20.44
+cd botocore-1.20.44
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
