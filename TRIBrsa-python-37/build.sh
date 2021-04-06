879cef683f834d24685df733535161d94b0c3912
Peter Tribble <peter.tribble@gmail.com>
2021-04-06 08:51:10 +0100

    Update python rsa 4.7.2

--- TRIBrsa-python-37/build.sh
+++ TRIBrsa-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack rsa-3.4.2
-cd rsa-3.4.2
+${THOME}/build/unpack rsa-4.7.2
+cd rsa-4.7.2
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pdu
