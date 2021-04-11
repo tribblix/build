0d1be6ec8858ec4761d166b6d0dd68db0a65c898
Peter Tribble <peter.tribble@gmail.com>
2021-04-11 19:13:20 +0100

    Libpng 1.6

--- TRIBimage-libpng/build.sh
+++ TRIBimage-libpng/build.sh
@@ -2,8 +2,8 @@
 #
 # old versions shipped in TRIBlibpng-compat
 #
-# we reverted to 1.4, otherwise we got a mess with both 1.4 and
-# 1.6 libraries linked
+# ship both 1.4 and 1.6
 #
 ${THOME}/build/dobuild -64 libpng-1.4.22 -C --sysconfdir=/etc
-${THOME}/build/genpkg TRIBimage-libpng libpng-1.4.22
+${THOME}/build/dobuild -64 libpng-1.6.37 -C --sysconfdir=/etc
+${THOME}/build/genpkg TRIBimage-libpng libpng-1.4.22 libpng-1.6.37
