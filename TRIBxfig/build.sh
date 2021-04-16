8f6c31de369c818a68158ea686b290b9cb309ccd
Peter Tribble <peter.tribble@gmail.com>
2021-04-16 19:30:26 +0100

    Update xfig 3.2.8a

--- TRIBxfig/build.sh
+++ TRIBxfig/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xfig-3.2.7b -C --without-xaw3d
-${THOME}/build/dobuild fig2dev-3.2.7b
-${THOME}/build/genpkg TRIBxfig xfig-3.2.7b fig2dev-3.2.7b
+# in 3.2.8a there's a file that doesn't correctly handle being
+# not Xaw3d, so the postconf script handles that and will need
+# removing if we ever do use Xaw3d
+#
+${THOME}/build/dobuild xfig-3.2.8a -C --without-xaw3d
+${THOME}/build/dobuild fig2dev-3.2.8a
+${THOME}/build/genpkg TRIBxfig xfig-3.2.8a fig2dev-3.2.8a
