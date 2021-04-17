2090ef7f8709e1e5bbd6cbf2024ea55358c7acc5
Peter Tribble <peter.tribble@gmail.com>
2021-04-17 09:54:31 +0100

    Update xcursorgen 1.0.7

--- TRIBxcursorgen/build.sh
+++ TRIBxcursorgen/build.sh
@@ -2,5 +2,5 @@
 #
 # needed to build xcursor-themes
 #
-${THOME}/build/dobuild xcursorgen-1.0.6
-${THOME}/build/genpkg TRIBxcursorgen xcursorgen-1.0.6
+${THOME}/build/dobuild xcursorgen-1.0.7
+${THOME}/build/genpkg TRIBxcursorgen xcursorgen-1.0.7
