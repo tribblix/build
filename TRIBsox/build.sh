947fe516eaa1bb4d04cf764080a3fb91470ac18e
Peter Tribble <peter.tribble@gmail.com>
2021-04-17 11:45:39 +0100

    Rebuild SoX for libpng bump

--- TRIBsox/build.sh
+++ TRIBsox/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 sox-14.4.2 -C --with-distro=Tribblix
+${THOME}/build/dobuild -64 sox-14.4.2 -C "--disable-openmp --with-distro=Tribblix"
 ${THOME}/build/genpkg TRIBsox sox-14.4.2
