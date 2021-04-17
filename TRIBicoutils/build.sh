45f96b1e3295ed41abf68b0237a0756f4342e685
Peter Tribble <peter.tribble@gmail.com>
2021-04-17 12:24:17 +0100

    Update icoutils 0.32.3

--- TRIBicoutils/build.sh
+++ TRIBicoutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild icoutils-0.31.2
-${THOME}/build/genpkg TRIBicoutils icoutils-0.31.2
+${THOME}/build/dobuild icoutils-0.32.3
+${THOME}/build/genpkg TRIBicoutils icoutils-0.32.3
