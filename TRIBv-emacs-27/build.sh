98e0426e948fc30e304423324d9c763b19a86429
Peter Tribble <peter.tribble@gmail.com>
2021-04-17 10:39:19 +0100

    Update emacs 27.2

--- TRIBv-emacs-27/build.sh
+++ TRIBv-emacs-27/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS=-lcurses ${THOME}/build/dobuild emacs-27.1 -P /usr/versions/emacs-27 -C --with-gif=no
-${THOME}/build/genpkg TRIBv-emacs-27 emacs-27.1
+env LIBS=-lcurses ${THOME}/build/dobuild emacs-27.2 -P /usr/versions/emacs-27 -C --with-gif=no
+${THOME}/build/genpkg TRIBv-emacs-27 emacs-27.2
