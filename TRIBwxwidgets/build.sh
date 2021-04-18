0a70c426605c4ef6fc30db01021cc984cca497c1
Peter Tribble <peter.tribble@gmail.com>
2021-04-18 11:09:21 +0100

    Update wxwidgets 3.1.5

--- TRIBwxwidgets/build.sh
+++ TRIBwxwidgets/build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack wxWidgets-3.1.4
-cd wxWidgets-3.1.4
+${THOME}/build/unpack wxWidgets-3.1.5
+cd wxWidgets-3.1.5
 rm -fr buildgtk
 mkdir buildgtk
 cd buildgtk
 env CXXFLAGS="-std=c++11 -fpermissive" ../configure --with-gtk --prefix=/usr --disable-xlocale
 gmake -j 12
-rm -fr /tmp/wxg
-gmake install DESTDIR=/tmp/wxg
-${THOME}/build/create_pkg TRIBwxwidgets /tmp/wxg
-rm -fr /tmp/wxg
+${THOME}/build/genpkg TRIBwxwidgets
