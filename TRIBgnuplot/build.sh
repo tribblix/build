ff4b045bfb181fc4864ff2608992cc2145e109b5
Peter Tribble <peter.tribble@gmail.com>
2021-04-18 19:34:52 +0100

    Update gnuplot 5.4.1

--- TRIBgnuplot/build.sh
+++ TRIBgnuplot/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild gnuplot-5.2.2 -C "--with-qt=no"
-${THOME}/build/genpkg TRIBgnuplot gnuplot-5.2.2
+${THOME}/build/dobuild gnuplot-5.4.1 -C "--with-qt=no --disable-wxwidgets"
+${THOME}/build/genpkg TRIBgnuplot gnuplot-5.4.1
