7b4d8d15a83eb9bcb4c99d9c083db14bf8776d2a
Peter Tribble <peter.tribble@gmail.com>
2021-04-19 18:42:27 +0100

    Update xfce calculator plugin 0.7.1

--- TRIBxfce-calculator-plugin/build.sh
+++ TRIBxfce-calculator-plugin/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild -gnu xfce4-calculator-plugin-0.6.0
-${THOME}/build/genpkg TRIBxfce-calculator-plugin xfce4-calculator-plugin-0.6.0
+${THOME}/build/dobuild -gnu xfce4-calculator-plugin-0.7.1
+${THOME}/build/genpkg TRIBxfce-calculator-plugin xfce4-calculator-plugin-0.7.1
