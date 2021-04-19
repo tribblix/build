a021cdf87166dfe42e7d00acd730ea3d17034832
Peter Tribble <peter.tribble@gmail.com>
2021-04-19 19:15:56 +0100

    Update xfce sysstemload plugin 1.3.1

--- TRIBxfce-systemload-plugin/build.sh
+++ TRIBxfce-systemload-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LDFLAGS=-lkstat ${THOME}/build/dobuild -gnu xfce4-systemload-plugin-1.1.2
-${THOME}/build/genpkg TRIBxfce-systemload-plugin xfce4-systemload-plugin-1.1.2
+env TRIBBLIX_LDFLAGS=-lkstat ${THOME}/build/dobuild -gnu xfce4-systemload-plugin-1.3.1
+${THOME}/build/genpkg TRIBxfce-systemload-plugin xfce4-systemload-plugin-1.3.1
