e07407e0b3152caf1c3e1b1d6f0b95676ef1c2ee
Peter Tribble <peter.tribble@gmail.com>
2021-04-19 20:27:56 +0100

    Update xfce screenshoter 1.97

--- TRIBxfce-screenshooter/build.sh
+++ TRIBxfce-screenshooter/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-screenshooter-1.8.2
-${THOME}/build/genpkg TRIBxfce-screenshooter xfce4-screenshooter-1.8.2
+${THOME}/build/dobuild -gnu xfce4-screenshooter-1.9.7
+${THOME}/build/genpkg TRIBxfce-screenshooter xfce4-screenshooter-1.9.7
