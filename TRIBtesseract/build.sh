8c9a6e1a6805f59014fd53850a2707d4236d3999
Peter Tribble <peter.tribble@gmail.com>
2021-04-24 13:04:25 +0100

    Rebuild tesseract for libpng bump, update to 3.05.02

--- TRIBtesseract/build.sh
+++ TRIBtesseract/build.sh
@@ -2,5 +2,5 @@
 #
 # needs leptonica
 #
-${THOME}/build/dobuild tesseract-3.05.00
-${THOME}/build/genpkg TRIBtesseract tesseract-3.05.00
+${THOME}/build/dobuild tesseract-3.05.02
+${THOME}/build/genpkg TRIBtesseract tesseract-3.05.02
