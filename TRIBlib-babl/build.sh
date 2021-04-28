b3281ff3db481c181af7cbdfa06f528dfecaba59
Peter Tribble <peter.tribble@gmail.com>
2021-04-28 21:33:40 +0100

    Update babl 0.1.86

--- TRIBlib-babl/build.sh
+++ TRIBlib-babl/build.sh
@@ -2,5 +2,37 @@
 #
 # the build barfs on awk's crappiness, hence the AWK
 #
-env AWK=nawk ${THOME}/build/dobuild -64 babl-0.1.24 -C "--disable-mmx --disable-sse"
-${THOME}/build/genpkg TRIBlib-babl babl-0.1.24
+#env AWK=nawk ${THOME}/build/dobuild -64 babl-0.1.24 -C "--disable-mmx --disable-sse"
+#${THOME}/build/genpkg TRIBlib-babl babl-0.1.24
+
+#
+# new meson build
+#
+${THOME}/build/unpack -64 babl-0.1.86
+cd babl-0.1.86
+meson --prefix=/usr -Denable-gir=false -Denable-mmx=false -Denable-sse=false build
+cd build
+ninja
+cd ..
+cd ..
+cd babl-0.1.86-64bit
+meson --prefix=/usr -Denable-gir=false -Denable-mmx=false -Denable-sse=false --libdir=lib/amd64 build
+cd build
+ninja
+cd ..
+cd ..
+
+rm -fr /tmp/bb
+cd babl-0.1.86
+cd build
+env DESTDIR=/tmp/bb ninja install
+cd ..
+cd ..
+cd babl-0.1.86-64bit
+cd build
+env DESTDIR=/tmp/bb ninja install
+cd ..
+cd ..
+
+${THOME}/build/create_pkg TRIBlib-babl /tmp/bb
+
---
+++ patches/babl-0.1.86.pls
@@ -0,0 +1,1 @@
+-p1 babl-fix-build.patch
