b060f0faf05420d6286cd09a00cd0445ab03bf70
Peter Tribble <peter.tribble@gmail.com>
2021-04-28 22:13:44 +0100

    gexiv2 needs rebuilding to match libexiv2

--- TRIBgexiv2/build.sh
+++ TRIBgexiv2/build.sh
@@ -2,7 +2,6 @@
 #
 # used by gimp
 # 0.12 migrated to meson and the build fails
-# we only have a 32-bit libexiv2
 #
-${THOME}/build/dobuild gexiv2-0.10.10 -C "--disable-dependency-tracking --enable-introspection=no"
+${THOME}/build/dobuild -64 gexiv2-0.10.10 -C "--disable-dependency-tracking --enable-introspection=no"
 ${THOME}/build/genpkg TRIBgexiv2 gexiv2-0.10.10
