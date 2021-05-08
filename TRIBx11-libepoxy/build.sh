3f7340e3f2e8717a278ddafcbbf989529425d52b
Peter Tribble <peter.tribble@gmail.com>
2021-05-08 16:28:11 +0100

    Fix up X11 build dependencies

--- TRIBx11-libepoxy/build.sh
+++ TRIBx11-libepoxy/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libexpoxy-1.5.2 -C --enable-egl=no
+${THOME}/build/dobuild -64 libepoxy-1.5.2 -C --enable-egl=no
 ${THOME}/build/genpkg TRIBx11-libepoxy libepoxy-1.5.2
