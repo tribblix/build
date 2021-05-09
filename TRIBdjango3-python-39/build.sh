fff7026540569e71d99e8c772447c2d5b649d9e1
Peter Tribble <peter.tribble@gmail.com>
2021-05-09 20:19:30 +0100

    Update Django 3.1.10

--- TRIBdjango3-python-39/build.sh
+++ TRIBdjango3-python-39/build.sh
@@ -2,8 +2,8 @@
 #
 # Switch to 3.2 LTS once it's got an update or two
 #
-${THOME}/build/unpack Django-3.1.8
-cd Django-3.1.8
+${THOME}/build/unpack Django-3.1.10
+cd Django-3.1.10
 rm -fr /tmp/p3a
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/p3a
 ${THOME}/build/create_pkg TRIBdjango3-python-39 /tmp/p3a
