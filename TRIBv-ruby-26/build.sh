893ff72a47494349ec35e4bd924893f12e6aaf52
Peter Tribble <peter.tribble@gmail.com>
2021-05-11 13:29:10 +0100

    Update ruby 2.6.7

--- TRIBv-ruby-26/build.sh
+++ TRIBv-ruby-26/build.sh
@@ -4,5 +4,5 @@
 # disable -fstack-protector otherwise miniruby fails to link with
 # multiply defined symbols
 #
-${THOME}/build/dobuild ruby-2.6.6 -P /usr/versions/ruby-2.6 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
-${THOME}/build/genpkg TRIBv-ruby-26 ruby-2.6.6
+${THOME}/build/dobuild ruby-2.6.7 -P /usr/versions/ruby-2.6 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
+${THOME}/build/genpkg TRIBv-ruby-26 ruby-2.6.7
