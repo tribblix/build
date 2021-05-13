c4a1219e1ed7211da3fe7c9a58584b4f67a8ac3c
Peter Tribble <peter.tribble@gmail.com>
2021-05-13 15:23:32 +0100

    Update postgres 11.12 12.7 13.3

--- TRIBblix-postgres12/build.sh
+++ TRIBblix-postgres12/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 12
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres12 postgresql-12.6 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres12 postgresql-12.6
+${THOME}/build/dobuild -P /opt/tribblix/postgres12 postgresql-12.7 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres12 postgresql-12.7
--- TRIBblix-postgres13/build.sh
+++ TRIBblix-postgres13/build.sh
@@ -3,8 +3,8 @@
 # postgres 13
 #
 # this is 64-bit, unlike prior versions
-# python 3.8 and later should use this version
+# python 3.9 and later should use this version
 # but we can't use --with-perl yet as that's only 32-bit
 #
-env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild -P /opt/tribblix/postgres13 postgresql-13.2 -C "--without-readline --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres13 postgresql-13.2
+env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild -P /opt/tribblix/postgres13 postgresql-13.3 -C "--without-readline --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres13 postgresql-13.3
