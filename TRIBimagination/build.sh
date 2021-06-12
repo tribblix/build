171585a2d938047c2630444e67ad354219057c23
Peter Tribble <peter.tribble@gmail.com>
2021-06-12 10:20:46 +0100

    Update imagination 3.6

--- TRIBimagination/build.sh
+++ TRIBimagination/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu imagination-3.5.1
-${THOME}/build/genpkg TRIBimagination imagination-3.5.1
+${THOME}/build/dobuild -gnu imagination-3.6
+${THOME}/build/genpkg TRIBimagination imagination-3.6
