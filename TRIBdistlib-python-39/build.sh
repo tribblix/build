bec302f041adf87e8aade4d32e967ebb4fc9bf21
Peter Tribble <peter.tribble@gmail.com>
2021-06-13 10:01:09 +0100

    Update distlib 0.3.2

--- TRIBdistlib-python-39/build.sh
+++ TRIBdistlib-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack distlib-0.3.1
-cd distlib-0.3.1
+${THOME}/build/unpack distlib-0.3.2
+cd distlib-0.3.2
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
