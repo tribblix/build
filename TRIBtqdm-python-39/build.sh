4eed59dff91f7ce8a1ff3ef2ae87d13968eed0fb
Peter Tribble <peter.tribble@gmail.com>
2021-06-14 10:01:14 +0100

    Update tqdm 4.61.1

--- TRIBtqdm-python-39/build.sh
+++ TRIBtqdm-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tqdm-4.61.0
-cd tqdm-4.61.0
+${THOME}/build/unpack tqdm-4.61.1
+cd tqdm-4.61.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
