8f436e021489efa2da3daf7ad3f2fe9a7d2272cd
Peter Tribble <peter.tribble@gmail.com>
2021-06-16 13:47:37 +0100

    Update bluefish 2.2.12

--- TRIBbluefish/build.sh
+++ TRIBbluefish/build.sh
@@ -2,5 +2,5 @@
 #
 # bluefish
 #
-${THOME}/build/dobuild -gnu bluefish-2.2.11 -C --disable-python
-${THOME}/build/genpkg TRIBbluefish bluefish-2.2.11
+${THOME}/build/dobuild -gnu bluefish-2.2.12 -C --disable-python
+${THOME}/build/genpkg TRIBbluefish bluefish-2.2.12
