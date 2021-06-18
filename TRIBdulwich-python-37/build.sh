4e86a2bfce3c67b99a75dd775f38097e833bfadf
Peter Tribble <peter.tribble@gmail.com>
2021-06-18 12:34:16 +0100

    Update dulwich 0.20.23

--- TRIBdulwich-python-37/build.sh
+++ TRIBdulwich-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack dulwich-0.20.21
-cd dulwich-0.20.21
+${THOME}/build/unpack dulwich-0.20.23
+cd dulwich-0.20.23
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
