32f493c1313a83f6379efc9cb80218b4793d03e5
Peter Tribble <peter.tribble@gmail.com>
2021-06-20 11:55:06 +0100

    Update geany 1.37.1

--- TRIBgeany/build.sh
+++ TRIBgeany/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu geany-1.36 -C --enable-gtk3 -M 'geany_LDFLAGS='
-${THOME}/build/genpkg TRIBgeany geany-1.36
+${THOME}/build/dobuild -gnu geany-1.37.1 -C --enable-gtk3 -M 'geany_LDFLAGS='
+${THOME}/build/genpkg TRIBgeany geany-1.37.1
