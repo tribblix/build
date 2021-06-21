5be9a0c42b4ca93f209e4054400d44a37c53077d
Peter Tribble <peter.tribble@gmail.com>
2021-06-21 15:30:39 +0100

    libmcrypt is unused, deprecate it

--- TRIBblix-libmcrypt/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/dobuild libmcrypt-2.5.8 -P /opt/tribblix/mcrypt
-${THOME}/build/genpkg TRIBblix-libmcrypt libmcrypt-2.5.8
--- TRIBblix-libmhash/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/dobuild mhash-0.9.9.9 -P /opt/tribblix/mcrypt
-${THOME}/build/genpkg TRIBblix-libmhash mhash-0.9.9.9
