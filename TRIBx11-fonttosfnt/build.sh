e5b062ae7aace452b6f9565be98df08ac700ff5f
Peter Tribble <peter.tribble@gmail.com>
2021-07-04 21:52:49 +0100

    Update fonttosfnt 1.2.2

--- TRIBx11-fonttosfnt/build.sh
+++ TRIBx11-fonttosfnt/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild fonttosfnt-1.2.0
-${THOME}/build/genpkg TRIBx11-fonttosfnt fonttosfnt-1.2.0
+${THOME}/build/dobuild fonttosfnt-1.2.2
+${THOME}/build/genpkg TRIBx11-fonttosfnt fonttosfnt-1.2.2
