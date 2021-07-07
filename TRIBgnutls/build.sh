43388ddfb7e5b89679943ad636c1ae748fc6a13a
Peter Tribble <peter.tribble@gmail.com>
2021-07-07 18:45:21 +0100

    Update gnutls 3.6.16

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -7,5 +7,5 @@
 #
 # ship a copy of the previous shared library for compat
 #
-${THOME}/build/dobuild -64 gnutls-3.6.15 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBgnutls gnutls-3.6.15
+${THOME}/build/dobuild -64 gnutls-3.6.16 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBgnutls gnutls-3.6.16
