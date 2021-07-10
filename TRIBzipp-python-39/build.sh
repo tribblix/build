07d06c6d59f5abf3f360db00226931958115e3ee
Peter Tribble <peter.tribble@gmail.com>
2021-07-10 11:43:12 +0100

    Update zipp 3.5.0

--- TRIBzipp-python-39/build.sh
+++ TRIBzipp-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack zipp-3.4.1
-cd zipp-3.4.1
+${THOME}/build/unpack zipp-3.5.0
+cd zipp-3.5.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
