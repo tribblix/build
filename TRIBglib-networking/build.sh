30555b1026fab9fe6f4ac47d4ea580aa20fd3874
Peter Tribble <peter.tribble@gmail.com>
2021-07-10 16:54:57 +0100

    Update glib-networking 2.44.0 (for gnutls update)

--- TRIBglib-networking/build.sh
+++ TRIBglib-networking/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu -64 glib-networking-2.30.2 -C "--without-ca-certificates"
-${THOME}/build/genpkg TRIBglib-networking glib-networking-2.30.2
+${THOME}/build/dobuild -gnu -64 glib-networking-2.44.0 -C "--without-ca-certificates"
+${THOME}/build/genpkg TRIBglib-networking glib-networking-2.44.0
