73a2492491e2285d197f7f4ad92563ac953cf5bb
Peter Tribble <peter.tribble@gmail.com>
2021-07-24 11:41:59 +0100

    Update python elasticsearch 7.13.4

--- TRIBelasticsearch-python-39/build.sh
+++ TRIBelasticsearch-python-39/build.sh
@@ -6,8 +6,8 @@
 # it was originally called elasticsearch-py, now it's got the
 # simple elasticsearchN name where N is the release
 #
-${THOME}/build/unpack elasticsearch7-7.13.3
-cd elasticsearch7-7.13.3
+${THOME}/build/unpack elasticsearch7-7.13.4
+cd elasticsearch7-7.13.4
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
