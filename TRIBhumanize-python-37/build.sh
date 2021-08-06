01838b9194b710ec599d3395f60cdb1192f1793b
Peter Tribble <peter.tribble@gmail.com>
2021-08-06 11:12:24 +0100

    Update humanize 3.11.0

--- TRIBhumanize-python-37/build.sh
+++ TRIBhumanize-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack humanize-3.10.0
-cd humanize-3.10.0
+${THOME}/build/unpack humanize-3.11.0
+cd humanize-3.11.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
