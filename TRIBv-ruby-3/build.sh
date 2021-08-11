29ce894afea9182c6458dd11808cdcccc69f6fc6
Peter Tribble <peter.tribble@gmail.com>
2021-08-11 08:43:29 +0100

    Add ruby 3

---
+++ TRIBv-ruby-3/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# just can't handle LD_OPTIONS
+# disable -fstack-protector otherwise miniruby fails to link with
+# multiply defined symbols
+#
+${THOME}/build/dobuild +64only ruby-3.0.2 -P /usr/versions/ruby-3 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
+${THOME}/build/genpkg TRIBv-ruby-3 ruby-3.0.2
