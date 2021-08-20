0b4072ca3149561e1a7db1547f57056f9390c661
Peter Tribble <peter.tribble@gmail.com>
2021-08-20 16:54:46 +0100

    Make bzip2 64-bit by default

--- TRIBcompress-bzip2/build.sh
+++ TRIBcompress-bzip2/build.sh
@@ -6,7 +6,7 @@
 ${THOME}/build/unpack -64 bzip2-1.0.6
 rm -fr /tmp/bb
 mkdir -p /tmp/bb
-mkdir -p /tmp/bb/usr/bin/amd64
+#mkdir -p /tmp/bb/usr/bin/amd64
 mkdir -p /tmp/bb/usr/lib/amd64
 cd bzip2-1.0.6-64bit
 gsed -i 's:-shared:-shared -m64:' Makefile-libbz2_so
@@ -14,9 +14,10 @@ gsed -i 's:libbz2.so.1.0.6:libbz2.so.1:g' Makefile-libbz2_so
 gmake CFLAGS="-fpic -fPIC -Wall -Winline -O2 -m64" -f Makefile-libbz2_so
 gcc -shared -m64 -Wl,-soname -Wl,libbz2.so.1 -o libbz2.so.1 -nostdlib -lc blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o
 gcc -m64 -o bzip2-shared bzip2.c libbz2.so.1
-cp bzip2-shared /tmp/bb/usr/bin/amd64/bzip2
-ln /tmp/bb/usr/bin/amd64/bzip2 /tmp/bb/usr/bin/amd64/bzcat
-ln /tmp/bb/usr/bin/amd64/bzip2 /tmp/bb/usr/bin/amd64/bunzip2
+gmake CFLAGS="-fpic -fPIC -Wall -Winline -O2 -m64"
+#cp bzip2-shared /tmp/bb/usr/bin/amd64/bzip2
+#ln /tmp/bb/usr/bin/amd64/bzip2 /tmp/bb/usr/bin/amd64/bzcat
+#ln /tmp/bb/usr/bin/amd64/bzip2 /tmp/bb/usr/bin/amd64/bunzip2
 cp libbz2.so.1 /tmp/bb/usr/lib/amd64
 ln -s libbz2.so.1 /tmp/bb/usr/lib/amd64/libbz2.so
 cd ..
@@ -27,8 +28,10 @@ gcc -shared -Wl,-soname -Wl,libbz2.so.1 -o libbz2.so.1 -nostdlib -lc blocksort.o
 gcc -o bzip2-shared bzip2.c libbz2.so.1
 gmake
 gmake install PREFIX=/tmp/bb/usr
-rm /tmp/bb/usr/bin/{bzcat,bzip2,bunzip2}
-cp bzip2-shared /tmp/bb/usr/bin/bzip2
+# use the 64-bit binary
+rm /tmp/bb/usr/bin/{bzcat,bzip2,bunzip2,bzip2recover}
+cp ../bzip2-1.0.6-64bit/bzip2-shared /tmp/bb/usr/bin/bzip2
+cp ../bzip2-1.0.6-64bit/bzip2recover /tmp/bb/usr/bin/bzip2recover
 ln /tmp/bb/usr/bin/bzip2 /tmp/bb/usr/bin/bzcat
 ln /tmp/bb/usr/bin/bzip2 /tmp/bb/usr/bin/bunzip2
 rm /tmp/bb/usr/bin/bzless
