e70de2b385fc18960f5496a1efcd06b9cf7f9fff
Peter Tribble <peter.tribble@gmail.com>
2021-08-27 19:58:59 +0100

    Update Pygments 2.10.0

--- TRIBpygments-39/build.sh
+++ TRIBpygments-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Pygments-2.9.0
-cd Pygments-2.9.0
+${THOME}/build/unpack Pygments-2.10.0
+cd Pygments-2.10.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pdu
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
