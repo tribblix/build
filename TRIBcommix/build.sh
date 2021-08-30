2a886d7890158f9cf256181c0b8f554bbeeb4c42
Peter Tribble <peter.tribble@gmail.com>
2021-08-30 10:25:43 +0100

    Update commix 3.2, migrate to python39

--- TRIBcommix/build.sh
+++ TRIBcommix/build.sh
@@ -3,11 +3,11 @@
 # depends on python
 # needs the gnureadline module
 #
-${THOME}/build/unpack commix-3.1
-cd commix-3.1
-/usr/versions/python-3.7/bin/python3 setup.py build
+${THOME}/build/unpack commix-3.2
+cd commix-3.2
+/usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
-/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/ckx
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/ckx
 ${THOME}/build/create_pkg TRIBcommix /tmp/ckx
 cd ..
 rm -fr /tmp/ckx
