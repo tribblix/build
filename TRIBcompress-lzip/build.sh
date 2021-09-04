02b9d64eac05291b88b242fda9601e856d9a9d0d
Peter Tribble <peter.tribble@gmail.com>
2021-09-04 20:52:39 +0100

    Update lzip plzip lzlib, and build a 64-bit lzlib

--- TRIBcompress-lzip/build.sh
+++ TRIBcompress-lzip/build.sh
@@ -2,7 +2,7 @@
 #
 # lzip and friends
 #
-${THOME}/build/dobuild lzip-1.20
-${THOME}/build/dobuild lzlib-1.10 -C --enable-shared
-${THOME}/build/dobuild plzip-1.7 -C "CPPFLAGS=-I../lzlib-1.10 LDFLAGS=-L../lzlib-1.10"
-${THOME}/build/genpkg TRIBcompress-lzip lzip-1.20 lzlib-1.10 plzip-1.7
+${THOME}/build/dobuild lzip-1.22
+${THOME}/build/dobuild -64 lzlib-1.12 -C --enable-shared +C "CFLAGS+=-m64 --enable-shared"
+${THOME}/build/dobuild plzip-1.9 -C "CPPFLAGS=-I../lzlib-1.12 LDFLAGS=-L../lzlib-1.12"
+${THOME}/build/genpkg TRIBcompress-lzip lzip-1.22 lzlib-1.12 plzip-1.9
