59714a2274128a355b551c1dd0851a231ac56f56
Peter Tribble <peter.tribble@gmail.com>
2021-09-05 11:59:16 +0100

    Update xinit 1.4.1

--- TRIBx11-session-xinit/build.sh
+++ TRIBx11-session-xinit/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xinit-1.3.4 -C "--sysconfdir=/etc --with-xinitdir=/etc/X11/xinit"
-${THOME}/build/genpkg TRIBx11-session-xinit xinit-1.3.4
+${THOME}/build/dobuild xinit-1.4.1 -C "--sysconfdir=/etc --with-xinitdir=/etc/X11/xinit"
+${THOME}/build/genpkg TRIBx11-session-xinit xinit-1.4.1
