3eed3a6744972f532ebfc3f2737387d511bb5581
Peter Tribble <peter.tribble@gmail.com>
2021-09-12 12:57:11 +0100

    Rebuild s3cmd for python3.9

--- TRIBs3cmd/build.sh
+++ TRIBs3cmd/build.sh
@@ -2,11 +2,14 @@
 #
 # needs dateutil and magic
 #
-${THOME}/build/unpack s3cmd-2.1.0
-cd s3cmd-2.1.0
-/usr/versions/python-3.7/bin/python3 setup.py build
+# need a much more recent version that the 2.10 release
+# for python39 compatibility
+#
+${THOME}/build/unpack s3cmd-master
+cd s3cmd-master
+/usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
-/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBs3cmd /tmp/pil
 cd ..
 rm -fr /tmp/pil
