4c040497e61438023a337414f9cc3755eb9654bc
Peter Tribble <peter.tribble@gmail.com>
2021-09-22 08:45:17 +0100

    Update blessed 1.19.0

--- TRIBblessed-python-37/build.sh
+++ TRIBblessed-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack blessed-1.18.1
-cd blessed-1.18.1
+${THOME}/build/unpack blessed-1.19.0
+cd blessed-1.19.0
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
