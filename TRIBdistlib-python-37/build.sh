69beba2f2e11d758950c325957f7d62a5bdcb984
Peter Tribble <peter.tribble@gmail.com>
2021-09-22 09:01:59 +0100

    Update distlib 0.3.3

--- TRIBdistlib-python-37/build.sh
+++ TRIBdistlib-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack distlib-0.3.2
-cd distlib-0.3.2
+${THOME}/build/unpack distlib-0.3.3
+cd distlib-0.3.3
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
