04e3895231251f10b35fd35445f1227f6dcd0320
Peter Tribble <peter.tribble@gmail.com>
2021-09-25 11:46:26 +0100

    Add platformdirs 2.3.0

---
+++ TRIBplatformdirs-python-37/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack platformdirs-2.3.0
+cd platformdirs-2.3.0
+/usr/versions/python-3.7/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBplatformdirs-python-37 /tmp/pil
+cd ..
+rm -fr /tmp/pil
