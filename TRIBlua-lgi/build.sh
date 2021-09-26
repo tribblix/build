e07fd8a524c74dd2a38efc88df55c5560e0a3f02
Peter Tribble <peter.tribble@gmail.com>
2021-09-26 21:40:48 +0100

    Update lgi 0.9.2

--- TRIBlua-lgi/build.sh
+++ TRIBlua-lgi/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CC=gcc ${THOME}/build/dobuild lgi-0.9.0
-${THOME}/build/genpkg TRIBlua-lgi lgi-0.9.0
+env CC=gcc ${THOME}/build/dobuild lgi-0.9.2
+${THOME}/build/genpkg TRIBlua-lgi lgi-0.9.2
