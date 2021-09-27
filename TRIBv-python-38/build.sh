35f4e134f3f8bbdefc37e368b21f2402701d46a9
Peter Tribble <peter.tribble@gmail.com>
2021-09-27 14:15:27 +0100

    Update Python 3.8.12

--- TRIBv-python-38/build.sh
+++ TRIBv-python-38/build.sh
@@ -9,6 +9,6 @@
 # as those components are split out, the packages are automatically
 # created by the fixit script
 #
-${THOME}/build/dobuild +64only Python-3.8.11 -P /usr/versions/python-3.8 -L -C --enable-shared
+${THOME}/build/dobuild +64only Python-3.8.12 -P /usr/versions/python-3.8 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-38 Python-3.8.11
+${THOME}/build/genpkg TRIBv-python-38 Python-3.8.12
