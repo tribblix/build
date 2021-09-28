7d44604031d255b18b79db79c284d04df75b72a7
Peter Tribble <peter.tribble@gmail.com>
2021-09-28 08:18:10 +0100

    Update fossil 2.16

--- TRIBfossil/build.sh
+++ TRIBfossil/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 #
-# rename the tarball
-#  mv fossil-src-2.13.tar.gz fossil-2.13.tar.gz
-#
 # we've had a while with 2.0 so the sha1/sha3 switch is history
 #
-${THOME}/build/dobuild fossil-2.13 -C --enable-json
-${THOME}/build/genpkg TRIBfossil fossil-2.13
+${THOME}/build/dobuild fossil-src-2.16 -C --enable-json
+${THOME}/build/genpkg TRIBfossil fossil-src-2.16
