feece0081fe42def06021b1072dcdd3fcd4f1a53
Peter Tribble <peter.tribble@gmail.com>
2021-10-24 18:54:26 +0100

    Update ant 1.10.12

--- TRIBv-apache-ant-110/build.sh
+++ TRIBv-apache-ant-110/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
 # also needs the following in tarballs for fixit to deploy:
-# apache-ivy-2.4.0-bin-with-deps.zip
+# apache-ivy-2.5.0-bin-with-deps.zip
 # svnant-1.3.1.zip
-# jsch-0.1.55.jar
+# (jsch-0.1.55.jar - now supplied via ivy)
 #
-${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.11-bin
+${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.12-bin
