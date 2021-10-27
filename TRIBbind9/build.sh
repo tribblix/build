64d8f5352c8d6b6fe7fe8d37336cc8b7e2c859e0
Peter Tribble <peter.tribble@gmail.com>
2021-10-27 21:24:10 +0100

    Update bind 9.11.36

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -2,8 +2,5 @@
 #
 # must not have TRIBlibedit or TRIBjson-c installed
 #
-# there's a preconf script that replaces the configure broken by
-# the Solaris fixes with one that actually works
-#
-${THOME}/build/dobuild bind-9.11.35 -C "--sysconfdir=/etc --localstatedir=/var --without-python"
-${THOME}/build/genpkg TRIBbind9 bind-9.11.35
+${THOME}/build/dobuild bind-9.11.36 -C "--sysconfdir=/etc --localstatedir=/var --without-python"
+${THOME}/build/genpkg TRIBbind9 bind-9.11.36
