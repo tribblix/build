478e4adb387fb8e9269197fe151ee7b0a3d7fbfe
Peter Tribble <peter.tribble@gmail.com>
2021-11-15 21:15:45 +0000

    Update pgbouncer 1.16.1

--- TRIBpgbouncer/build.sh
+++ TRIBpgbouncer/build.sh
@@ -2,6 +2,6 @@
 #
 # there's a sacrificial static libevent built by the preconf script
 #
-env PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild pgbouncer-1.16.0
-${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.16.0
+env PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig:/usr/lib/pkgconfig ${THOME}/build/dobuild pgbouncer-1.16.1
+${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.16.1
 rm -fr /tmp/mc
