aa3d5c4202f998ffeb95c1cb77759dba8f7cb5bb
Peter Tribble <peter.tribble@gmail.com>
2021-12-11 11:01:54 +0000

    Update httpie 2.6.0

--- TRIBcharset-norm-python-39/build.sh
+++ TRIBcharset-norm-python-39/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httpie-2.6.0
-cd httpie-2.6.0
+${THOME}/build/unpack charset-normalizer-2.0.9
+cd charset-normalizer-2.0.9
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBhttpie-python-39 /tmp/pil
+${THOME}/build/create_pkg TRIBcharset-norm-python-39 /tmp/pil
 cd ..
 rm -fr /tmp/pil
