442d40e4734d862553687c8e0d820ad78fa83331
Peter Tribble <peter.tribble@gmail.com>
2021-12-11 15:39:49 +0000

    Update xauth 1.1.1

--- TRIBx11-session-xauth/build.sh
+++ TRIBx11-session-xauth/build.sh
@@ -2,5 +2,5 @@
 #
 # mkcookie built by the fixit script
 #
-${THOME}/build/dobuild xauth-1.0.10
-${THOME}/build/genpkg TRIBx11-session-xauth xauth-1.0.10
+${THOME}/build/dobuild xauth-1.1.1
+${THOME}/build/genpkg TRIBx11-session-xauth xauth-1.1.1
