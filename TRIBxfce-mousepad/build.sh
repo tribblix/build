ae097ad1955456eef1481ab8c2be81fc0dfdec75
Peter Tribble <peter.tribble@gmail.com>
2021-12-12 09:53:03 +0000

    Update mousepad 0.5.8

--- TRIBxfce-mousepad/build.sh
+++ TRIBxfce-mousepad/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -gnu mousepad-0.5.7
-${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.5.7
+env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -gnu mousepad-0.5.8
+${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.5.8
