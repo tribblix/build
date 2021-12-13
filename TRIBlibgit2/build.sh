0482db095f6fbb846cc2aa081d76bd262c50a49d
Peter Tribble <peter.tribble@gmail.com>
2021-12-13 20:43:06 +0000

    Update libgit2 1.3.0

--- TRIBlibgit2/build.sh
+++ TRIBlibgit2/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-${THOME}/build/cmbuild -64 libgit2-1.1.0
-${THOME}/build/genpkg TRIBlibgit2 libgit2-1.1.0
+${THOME}/build/cmbuild -64 libgit2-1.3.0
+${THOME}/build/genpkg TRIBlibgit2 libgit2-1.3.0
