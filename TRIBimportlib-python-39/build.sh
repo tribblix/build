d7962fe6f71de0fd4bb6a4abfb5036e06f56a92c
Peter Tribble <peter.tribble@gmail.com>
2021-12-19 10:23:18 +0000

    Update markdown_py 3.3.6

--- TRIBimportlib-python-39/build.sh
+++ TRIBimportlib-python-39/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Markdown-3.3.6
-cd Markdown-3.3.6
+${THOME}/build/unpack importlib_metadata-4.9.0
+cd importlib_metadata-4.9.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmarkdown-python-39 /tmp/pil
+${THOME}/build/create_pkg TRIBimportlib-python-39 /tmp/pil
 cd ..
 rm -fr /tmp/pil
