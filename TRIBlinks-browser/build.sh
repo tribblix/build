63c39693ba8c346d453de164e1c638e04522fd8b
Peter Tribble <peter.tribble@gmail.com>
2021-12-20 09:23:18 +0000

    Update Links browser 2.25

--- TRIBlinks-browser/build.sh
+++ TRIBlinks-browser/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only links-2.23 -C "--enable-graphics --without-openmp"
-${THOME}/build/genpkg TRIBlinks-browser links-2.23
+${THOME}/build/dobuild -64only links-2.25 -C "--enable-graphics --without-openmp"
+${THOME}/build/genpkg TRIBlinks-browser links-2.25
