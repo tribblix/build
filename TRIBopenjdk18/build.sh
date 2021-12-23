6565fcf456d584f676c1dd7f1f83f0ede2b08c5c
Peter Tribble <peter.tribble@gmail.com>
2021-12-23 08:47:40 +0000

    Update jdk 18+29

--- TRIBopenjdk18/build.sh
+++ TRIBopenjdk18/build.sh
@@ -8,8 +8,8 @@
 # the tag is 18+xx, the filename is 18-xx
 #
 cd ~/ud
-${THOME}/build/unpack jdk18-jdk-18-28
-cd jdk18-jdk-18-28
+${THOME}/build/unpack jdk18-jdk-18-29
+cd jdk18-jdk-18-29
 
 #
 # jdk18 needs autoconf installed
@@ -46,8 +46,8 @@ env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
 # ./bin/java -version
 #
 # openjdk version "18-internal" 2022-03-22
-# OpenJDK Runtime Environment (build 18-internal+0-adhoc.ptribble.jdk18-jdk-18-28)
-# OpenJDK 64-Bit Server VM (build 18-internal+0-adhoc.ptribble.jdk18-jdk-18-28, mixed mode, sharing)
+# OpenJDK Runtime Environment (build 18-internal+0-adhoc.ptribble.jdk18-jdk-18-29)
+# OpenJDK 64-Bit Server VM (build 18-internal+0-adhoc.ptribble.jdk18-jdk-18-29, mixed mode, sharing)
 #
 
 rm -fr /tmp/jdk
