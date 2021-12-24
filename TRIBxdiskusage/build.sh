48529c02026a198805a193c6caa989d62a110e00
Peter Tribble <peter.tribble@gmail.com>
2021-12-24 21:29:15 +0000

    Update xdiskusage 1.60

--- TRIBxdiskusage/build.sh
+++ TRIBxdiskusage/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xdiskusage-1.51
-${THOME}/build/genpkg TRIBxdiskusage xdiskusage-1.51
+${THOME}/build/dobuild xdiskusage-1.60
+${THOME}/build/genpkg TRIBxdiskusage xdiskusage-1.60
