dd959407e0062e5f3bf68b1ea7a46a0c552b915e
Peter Tribble <peter.tribble@gmail.com>
2021-12-27 19:59:21 +0000

    Update repmgr 5.3.0, add build for postgres 14

--- TRIBblix-repmgr12/build.sh
+++ TRIBblix-repmgr12/build.sh
@@ -2,7 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-# the tarball name might not match the version number
-#
-${THOME}/build/dobuild repmgr-5.2.0 -C PG_CONFIG=/opt/tribblix/postgres12/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr12 repmgr-5.2.0
+${THOME}/build/dobuild repmgr-5.3.0 -C PG_CONFIG=/opt/tribblix/postgres12/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr12 repmgr-5.3.0
--- TRIBblix-repmgr13/build.sh
+++ TRIBblix-repmgr13/build.sh
@@ -2,7 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-# the tarball name might not match the version number
-#
-${THOME}/build/dobuild +64only repmgr-5.2.0 -C PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr13 repmgr-5.2.0
+${THOME}/build/dobuild +64only repmgr-5.3.0 -C PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr13 repmgr-5.3.0
