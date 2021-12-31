1d189d1547047164c98fa3a7850b4418d74d677e
Peter Tribble <peter.tribble@gmail.com>
2021-12-31 11:16:30 +0000

    Remove python27 version of six

--- TRIBsix-python-27/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack six-1.15.0
-cd six-1.15.0
-rm -fr /tmp/pil
-python setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsix-python-27 /tmp/pil
-cd ..
-rm -fr /tmp/pil
