d037a90d167f63060854f9086e866c8628bcb684
Peter Tribble <peter.tribble@gmail.com>
2021-12-31 10:53:18 +0000

    Update urllib3 1.26.7

--- TRIBurllib3-python-37/build.sh
+++ TRIBurllib3-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack urllib3-1.26.6
-cd urllib3-1.26.6
+${THOME}/build/unpack urllib3-1.26.7
+cd urllib3-1.26.7
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
