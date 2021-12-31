4ccf18f642921cb79578e31ef952f1ff3e10f947
Peter Tribble <peter.tribble@gmail.com>
2021-12-31 10:55:21 +0000

    Remove python38 version of six

--- TRIBsix-python-38/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack six-1.15.0
-cd six-1.15.0
-rm -fr /tmp/pil
-/usr/versions/python-3.8/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsix-python-38 /tmp/pil
-cd ..
-rm -fr /tmp/pil
