b8ebcd20360f3aced4b77f99a6984233f17c5578
Peter Tribble <peter.tribble@gmail.com>
2022-01-02 18:25:18 +0000

    Update abiword 3.0.5

--- TRIBabiword3/build.sh
+++ TRIBabiword3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild abiword-3.0.4 -C "--disable-dependency-tracking --enable-clipart --enable-templates"
-${THOME}/build/genpkg TRIBabiword3 abiword-3.0.4
+${THOME}/build/dobuild abiword-3.0.5 -C "--disable-dependency-tracking --enable-clipart --enable-templates"
+${THOME}/build/genpkg TRIBabiword3 abiword-3.0.5
