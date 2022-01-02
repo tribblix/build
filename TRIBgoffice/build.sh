f9a28b3afa3117c3adfcf6e817e2efa2fe82d8b6
Peter Tribble <peter.tribble@gmail.com>
2022-01-02 16:45:00 +0000

    Update goffice 0.10.50

--- TRIBgoffice/build.sh
+++ TRIBgoffice/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS=-I/usr/include/pcre ${THOME}/build/dobuild -gnu goffice-0.10.48
-${THOME}/build/genpkg TRIBgoffice goffice-0.10.48
+env CPPFLAGS=-I/usr/include/pcre ${THOME}/build/dobuild -gnu goffice-0.10.50
+${THOME}/build/genpkg TRIBgoffice goffice-0.10.50
