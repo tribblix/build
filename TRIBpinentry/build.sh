47768a66ecc3528b34f7cfbd423c0642dfd3e49b
Peter Tribble <peter.tribble@gmail.com>
2022-01-02 16:21:47 +0000

    Update pinentry 1.2.0

--- TRIBpinentry/build.sh
+++ TRIBpinentry/build.sh
@@ -2,5 +2,5 @@
 #
 # pinentry
 #
-${THOME}/build/dobuild pinentry-1.0.0
-${THOME}/build/genpkg TRIBpinentry pinentry-1.0.0
+${THOME}/build/dobuild pinentry-1.2.0 -C LIBS=-lsocket
+${THOME}/build/genpkg TRIBpinentry pinentry-1.2.0
