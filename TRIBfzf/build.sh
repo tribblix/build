c9809f59991f2fac5e6fefaaff42b283f9df79ea
Peter Tribble <peter.tribble@gmail.com>
2022-01-08 12:52:23 +0000

    Add fzf

---
+++ TRIBfzf/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+
+#
+git clone https://github.com/junegunn/fzf.git
+cd fzf
+
+#
+# the Makefile attempts to enumerate all possible build targets
+# which doesn't really work, and our uname output isn't recognised
+#
+sed -i s:x86_64:i86pc: Makefile
+
+gmake bin/fzf
+
+rm -fr /tmp/ll
+mkdir -p /tmp/ll/usr/bin
+mkdir -p /tmp/ll/usr/share/man/man1
+
+ginstall -Dm755 ./bin/fzf* -t /tmp/ll/usr/bin
+cp man/man1/fzf*.1 /tmp/ll/usr/share/man/man1
+cd ..
+
+${THOME}/build/create_pkg TRIBfzf /tmp/ll
+rm -fr /tmp/ll
