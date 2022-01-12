9bd3557ec355f8818afc6c081af46951e93cfea0
Peter Tribble <peter.tribble@gmail.com>
2022-01-12 18:45:09 +0000

    Update ghostscript 9.55.0, and 64-bit

--- TRIBgpl-ghostscript/build.sh
+++ TRIBgpl-ghostscript/build.sh
@@ -7,14 +7,23 @@
 # see the BLFS documentation, we build the same way
 #  http://www.linuxfromscratch.org/blfs/view/svn/pst/gs.html
 #
-${THOME}/build/dobuild ghostscript-9.26 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
-cd ghostscript-9.26
+${THOME}/build/dobuild ghostscript-9.55.0 -64 -C "--disable-compile-inits --with-system-libtiff --disable-gtk --disable-dbus --disable-cups --enable-dynamic LIBS=-lz"
+cd ghostscript-9.55.0-64bit
 gmake -j4 so
+cd ..
+cd ghostscript-9.55.0
+gmake -j4 so
+cd ..
 rm -fr /tmp/ags
+cd ghostscript-9.55.0-64bit
+gmake DESTDIR=/tmp/ags install
+gmake DESTDIR=/tmp/ags soinstall
+cd ..
+cd ghostscript-9.55.0
 gmake DESTDIR=/tmp/ags install
 gmake DESTDIR=/tmp/ags soinstall
 ginstall -v -m644 base/*.h /tmp/ags/usr/include/ghostscript/
-tar cf - examples | ( cd /tmp/ags/usr/share/ghostscript/9.26 ; tar xf -)
+tar cf - examples | ( cd /tmp/ags/usr/share/ghostscript/9.55.0 ; tar xf -)
 mkdir -p /tmp/ags/usr/share/ghostscript
 # it looks odd just having a subset of man pages in german
 # no other non-english languages
@@ -22,9 +31,7 @@ rm -fr /tmp/ags/usr/share/man/de
 cd /tmp/ags/usr/share/ghostscript
 ${THOME}/build/unpack ghostscript-fonts-std-8.11
 ${THOME}/build/unpack gnu-gs-fonts-other-6.0
-rm -f 9.26/doc
-rm -f 9.26/doc/*.*
-rm -f ../doc/ghostscript/9.26/*.*
+rm -f ../doc/ghostscript/9.55.0/*.*
 cd
 ${THOME}/build/create_pkg TRIBgpl-ghostscript /tmp/ags
 rm -fr /tmp/ags
--- patches/ghostscript-9.26.pls
+++
@@ -1,1 +0,0 @@
--p1 ghostpdl.git-41ef9a0bc36b.patch
