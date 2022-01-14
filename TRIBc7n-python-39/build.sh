63927ab51eb4d97ae83a805d6ef4d5a1d7b2ca2a
Peter Tribble <peter.tribble@gmail.com>
2022-01-14 19:41:45 +0000

    Update cloud custodian 0.9.14.0

--- TRIBc7n-python-39/build.sh
+++ TRIBc7n-python-39/build.sh
@@ -3,8 +3,8 @@
 # get the tarball from https://github.com/cloud-custodian/cloud-custodian
 # rather than PyPi, which only has the wheel and not source
 #
-${THOME}/build/unpack cloud-custodian-0.9.10.0
-cd cloud-custodian-0.9.10.0
+${THOME}/build/unpack cloud-custodian-0.9.14.0
+cd cloud-custodian-0.9.14.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
