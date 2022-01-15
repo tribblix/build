d5a2c5fb8c79056ea20b14c5f4d521a95b3340b6
Peter Tribble <peter.tribble@gmail.com>
2022-01-15 21:31:18 +0000

    Update charset-normalizer 2.0.10

--- TRIBcharset-norm-python-39/build.sh
+++ TRIBcharset-norm-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack charset-normalizer-2.0.9
-cd charset-normalizer-2.0.9
+${THOME}/build/unpack charset-normalizer-2.0.10
+cd charset-normalizer-2.0.10
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
