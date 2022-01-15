8c2a8c0ca2280b15514c0c5d9b6a9ceb16b0c05a
Peter Tribble <peter.tribble@gmail.com>
2022-01-15 09:58:25 +0000

    Update texinfo 6.8

--- TRIBgnu-texinfo/build.sh
+++ TRIBgnu-texinfo/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild texinfo-6.5
-${THOME}/build/genpkg TRIBgnu-texinfo texinfo-6.5
+${THOME}/build/dobuild texinfo-6.8
+${THOME}/build/genpkg TRIBgnu-texinfo texinfo-6.8
