0dc240e763e2425f1d2e2280ffb8dc817e5412bb
Peter Tribble <peter.tribble@gmail.com>
2022-01-16 21:34:08 +0000

    Update doctl 1.69.0

--- TRIBdoctl/build.sh
+++ TRIBdoctl/build.sh
@@ -5,6 +5,6 @@ rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/doctl
 cp bin/doctl /tmp/cs/usr/bin
-cp src/github.com/digitalocean/doctl/README.md src/github.com/digitalocean/doctl/LICENSE.txt /tmp/cs/usr/share/doctl
+cp pkg/mod/github.com/digitalocean/doctl@*/README.md pkg/mod/github.com/digitalocean/doctl@*/LICENSE.txt /tmp/cs/usr/share/doctl
 ${THOME}/build/create_pkg TRIBdoctl /tmp/cs
 rm -fr /tmp/cs
