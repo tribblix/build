8a34efa66ce0653afe1956960105e3829cb6c67b
Peter Tribble <peter.tribble@gmail.com>
2022-01-22 16:04:51 +0000

    No longer need python2 version of pyxdg

--- TRIBxdg-python-27/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack pyxdg-0.26
-cd pyxdg-0.26
-python setup.py build
-python setup.py install --root=/tmp/xx1
-${THOME}/build/create_pkg TRIBxdg-python-27 /tmp/xx1
-rm -fr /tmp/xx1
