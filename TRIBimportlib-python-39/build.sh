a85c229020f20fec2eb114f0e3956676cd890388
Peter Tribble <peter.tribble@gmail.com>
2022-01-22 12:49:57 +0000

    Update importlib_metadata 4.10.1

--- TRIBimportlib-python-39/build.sh
+++ TRIBimportlib-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack importlib_metadata-4.10.0
-cd importlib_metadata-4.10.0
+${THOME}/build/unpack importlib_metadata-4.10.1
+cd importlib_metadata-4.10.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
