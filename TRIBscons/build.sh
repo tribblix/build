8a528745fa2260a867f3cf91e92603bdb6284f2a
Peter Tribble <peter.tribble@gmail.com>
2022-01-23 13:53:06 +0000

    Update SCons 4.3.0, and move to python 3.9

--- TRIBscons/build.sh
+++ TRIBscons/build.sh
@@ -2,11 +2,11 @@
 #
 # (built in python)
 #
-${THOME}/build/unpack scons-4.0.1
-cd SCons-4.0.1
-/usr/versions/python-3.7/bin/python3 setup.py build
+${THOME}/build/unpack SCons-4.3.0
+cd SCons-4.3.0
+/usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/sc1
-/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/sc1
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/sc1
 ${THOME}/build/create_pkg TRIBscons /tmp/sc1
 cd ..
 rm -fr /tmp/sc1
