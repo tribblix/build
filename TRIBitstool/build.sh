c90e6e8dccdbb6c981b05401e87635ead388158a
Peter Tribble <peter.tribble@gmail.com>
2022-01-23 14:22:13 +0000

    Update itstool 2.0.7, and migrate to python 3.9

--- TRIBitstool/build.sh
+++ TRIBitstool/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild itstool-2.0.6 -C PYTHON=/usr/versions/python-3.7/bin/python3
-${THOME}/build/genpkg TRIBitstool itstool-2.0.6
+${THOME}/build/dobuild itstool-2.0.7 -C PYTHON=/usr/versions/python-3.9/bin/python3
+${THOME}/build/genpkg TRIBitstool itstool-2.0.7
