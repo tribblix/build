ba1eb65b4510784e937fd896594424de0a50c9aa
Peter Tribble <peter.tribble@gmail.com>
2022-01-25 21:41:03 +0000

    Add Django 4

---
+++ TRIBdjango4-python-39/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# pytz is deprecated, but marked as a dependency for transitional purposes
+#
+${THOME}/build/unpack Django-4.0.1
+cd Django-4.0.1
+/usr/versions/python-3.9/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBdjango4-python-39 /tmp/pil
+cd ..
+rm -fr /tmp/pil
