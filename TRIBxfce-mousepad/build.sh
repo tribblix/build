fba3006ff615c574264f312b56ff8cc6108af5ed
Peter Tribble <peter.tribble@gmail.com>
2022-02-04 10:38:40 +0000

    Rebuild mousepad, fix crashes, make 64-bit

--- TRIBxfce-mousepad/build.sh
+++ TRIBxfce-mousepad/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -gnu mousepad-0.5.8
+# -L seems to be needed to fix crashes
+#
+env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -64only -gnu -L mousepad-0.5.8
 ${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.5.8
