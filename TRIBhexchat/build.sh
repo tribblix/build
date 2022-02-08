aa9b9927ff37c7c22b06e8762f88884298d257cf
Peter Tribble <peter.tribble@gmail.com>
2022-02-08 21:53:28 +0000

    Update hexchat 2.16.0

--- TRIBhexchat/build.sh
+++ TRIBhexchat/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack hexchat-2.14.3
-cd hexchat-2.14.3
-sed -i '/pie: true/d' src/fe-gtk/meson.build
+${THOME}/build/unpack hexchat-2.16.0
+cd hexchat-2.16.0
 
-env LDFLAGS="-lsocket -lnsl" meson build -Dwith-libproxy=false -Dwith-libcanberra=false -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr
+# buildtype=plain avoids SSP failures
+env LDFLAGS="-lsocket -lnsl" meson build -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr -Dbuildtype=plain
 env PATH=/usr/gnu/bin:$PATH ninja -C build
 env DESTDIR=/tmp/hh PATH=/usr/gnu/bin:$PATH ninja -C build install
 ${THOME}/build/create_pkg TRIBhexchat /tmp/hh
