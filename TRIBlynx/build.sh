2f7544bd2528734175bb357e582704740afeca29
Peter Tribble <peter.tribble@gmail.com>
2022-02-08 07:57:19 +0000

    Update lynx 2.9.0dev.10

--- TRIBlynx/build.sh
+++ TRIBlynx/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild lynx2.8.9rel.1 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines"
-${THOME}/build/genpkg -I "install install-help install-doc" TRIBlynx lynx2.8.9rel.1
+${THOME}/build/dobuild -64only lynx2.9.0dev.10 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines"
+${THOME}/build/genpkg -I "install install-help install-doc" TRIBlynx lynx2.9.0dev.10
