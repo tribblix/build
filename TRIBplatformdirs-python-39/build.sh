0dc121c06d28149b141f4e3215c66fa17480cc4b
Peter Tribble <peter.tribble@gmail.com>
2022-02-12 10:01:35 +0000

    Update platformdirs 2.5.0

--- TRIBplatformdirs-python-39/build.sh
+++ TRIBplatformdirs-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack platformdirs-2.4.1
-cd platformdirs-2.4.1
+${THOME}/build/unpack platformdirs-2.5.0
+cd platformdirs-2.5.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
