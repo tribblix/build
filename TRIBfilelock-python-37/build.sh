211b23bd611ec67b711ff67e2aa8c2597e6e2d04
Peter Tribble <peter.tribble@gmail.com>
2022-02-16 19:36:52 +0000

    Update filelock 3.5.1

--- TRIBfilelock-python-37/build.sh
+++ TRIBfilelock-python-37/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack filelock-3.4.2
-cd filelock-3.4.2
+${THOME}/build/unpack filelock-3.5.1
+cd filelock-3.5.1
 /usr/versions/python-3.7/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
