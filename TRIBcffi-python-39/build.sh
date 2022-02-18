b6a00dc07306f707543fde610a6c834668857243
Peter Tribble <peter.tribble@gmail.com>
2022-02-18 13:47:08 +0000

    Fix python cffi 64-bit build

--- TRIBcffi-python-39/build.sh
+++ TRIBcffi-python-39/build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 #
+case `uname -p` in
+sparc)
+  ARCH64="sparcv9"
+  ;;
+i386)
+  ARCH64="amd64"
+  ;;
+esac
+
 ${THOME}/build/unpack cffi-1.14.5
 cd cffi-1.14.5
-/usr/versions/python-3.9/bin/python3 setup.py build
+env PKG_CONFIG_PATH=/usr/lib/${ARCH64}/pkgconfig /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
+env PKG_CONFIG_PATH=/usr/lib/${ARCH64}/pkgconfig /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBcffi-python-39 /tmp/pil
 cd ..
 rm -fr /tmp/pil
