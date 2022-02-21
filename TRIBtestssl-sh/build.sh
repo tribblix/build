d0196247d429e667a09df3c953a37737b7b7a9ff
Peter Tribble <peter.tribble@gmail.com>
2022-02-21 08:33:53 +0000

    Add testssl.sh 3.0.7

---
+++ TRIBtestssl-sh/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_tarball TRIBtestssl-sh testssl.sh-3.0.7
---
+++ patches/testssl.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# this is a wrapper for the real testssl.sh
+#
+# it does a sanity check and then launches the real thing with
+# a fixed environment
+#
+
+if [ ! -x /usr/gnu/bin/grep ]; then
+    if [ ! -x /usr/bin/hexdump ]; then
+	echo "ERROR: hexdump not installed, run the following as root:"
+	echo "  zap install TRIBtext-utilities"
+    fi
+    echo "ERROR: gnu grep not installed, run the following as root:"
+    echo "  zap install TRIBtext-gnu-grep"
+    exit 1
+fi
+
+if [ ! -x /usr/bin/hexdump ]; then
+    echo "ERROR: hexdump not installed, run the following as root:"
+    echo "  zap install TRIBtext-utilities"
+    exit 1
+fi
+
+PATH=/usr/gnu/bin:/usr/bin:/usr/sbin
+export PATH
+exec /usr/versions/testssl.sh/testssl.sh "$@"
