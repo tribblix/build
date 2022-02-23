5c9f5624873592e6679b0e67ea32533d71b7bbd7
Peter Tribble <peter.tribble@gmail.com>
2022-02-23 22:06:02 +0000

    Update hexchat 2.16.1, and build 64-bit

--- TRIBhexchat/build.sh
+++ TRIBhexchat/build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 #
-${THOME}/build/unpack hexchat-2.16.0
-cd hexchat-2.16.0
+${THOME}/build/unpack hexchat-2.16.1
+cd hexchat-2.16.1
 
+#
+# this is a 64-bit build, so diable perl for now as that's
+# 32-bit (and never worked)
+#
+# we ought to pick up the 64-bit python3.9, but while the module builds
+# that gets symbol errors at runtime we ought to look at
+#
 # buildtype=plain avoids SSP failures
-env LDFLAGS="-lsocket -lnsl" meson build -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr -Dbuildtype=plain
+env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig LDFLAGS="-lsocket -lnsl" meson build -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr -Dbuildtype=plain -Dc_args=-m64 -Dcpp_args=-m64 -Dc_link_args="-m64 -lsocket -lnsl" -Dcpp_link_args=-m64 -Dwith-perl=false
 env PATH=/usr/gnu/bin:$PATH ninja -C build
+rm -fr /tmp/hh
 env DESTDIR=/tmp/hh PATH=/usr/gnu/bin:$PATH ninja -C build install
 ${THOME}/build/create_pkg TRIBhexchat /tmp/hh
+rm -fr /tmp/hh
