fbbd77370c3f64b73684859f41abce264b2f558a
Peter Tribble <peter.tribble@gmail.com>
2022-02-25 21:11:31 +0000

    Deprecate python3.7 module for libxml2

--- TRIBlibxml2/build.sh
+++ TRIBlibxml2/build.sh
@@ -7,14 +7,6 @@
 env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.9 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
 ${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.9
 
-rm -fr /tmp/lxp
-cd libxml2-2.9.9/python
-/usr/versions/python-3.7/bin/python3 setup.py build
-/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/lxp
-${THOME}/build/create_pkg TRIBlibxml2-python-37 /tmp/lxp
-/usr/versions/python-3.7/bin/python3 setup.py clean
-cd ../..
-
 # python3.9 is 64-bit
 rm -fr /tmp/lxp
 cd libxml2-2.9.9-64bit/python
