604e611aa161a614cd82e13ec58401bc3cddea93
Peter Tribble <peter.tribble@gmail.com>
2022-02-27 10:46:26 +0000

    Tweak lumina build, add calculator and themes

--- TRIBlumina/build.sh
+++ TRIBlumina/build.sh
@@ -10,4 +10,22 @@ cp ${THOME}/build/patches/LuminaOS-SunOS.cpp src-qt5/core/libLumina/LuminaOS-Sun
 gmake -j 6
 rm -fr /tmp/lld
 gmake install INSTALL_ROOT=/tmp/lld
+cd ..
+
+git clone https://github.com/lumina-desktop/lumina-themes
+cd lumina-themes
+/usr/versions/Qt-5/bin/qmake PREFIX=/usr
+gmake -j 6
+gmake install INSTALL_ROOT=/tmp/lld
+cd ..
+
+$THOME/build/unpack lumina-calculator-1.6.0
+cd lumina-calculator-1.6.0/src-qt5
+/usr/versions/Qt-5/bin/qmake PREFIX=/usr/versions/lumina
+gmake -j 6
+gmake install INSTALL_ROOT=/tmp/lld
+cd ../..
+
+# lumina-pdf needs poppler-qt5, skip for now
+
 ${THOME}/build/create_pkg TRIBlumina /tmp/lld
