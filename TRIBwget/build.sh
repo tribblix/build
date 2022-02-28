c7fa3e0fb4d5fcc227376fd4d9a7562841cb66c0
Peter Tribble <peter.tribble@gmail.com>
2022-02-28 19:52:54 +0000

    Update wget 1.21.3, and make 64-bit

--- TRIBwget/build.sh
+++ TRIBwget/build.sh
@@ -2,5 +2,5 @@
 #
 # (no pcre in the install environment)
 #
-env TRIBBLIX_CFLAGS=-DOPENSSL_NO_SSL2 POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild wget-1.20.3 -C '--sysconfdir=/etc --with-ssl=openssl --disable-pcre'
-${THOME}/build/genpkg TRIBwget wget-1.20.3
+env TRIBBLIX_CFLAGS=-DOPENSSL_NO_SSL2 POD2MAN=/usr/perl5/bin/pod2man ${THOME}/build/dobuild -64only wget-1.21.3 -C '--sysconfdir=/etc --with-ssl=openssl --disable-pcre'
+${THOME}/build/genpkg TRIBwget wget-1.21.3
