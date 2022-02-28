67a479474c12d69e32e315d880c621069ee39da2
Peter Tribble <peter.tribble@gmail.com>
2022-02-28 19:57:29 +0000

    Update rcs 5.10.1, and make 64-bit

--- TRIBrcs/build.sh
+++ TRIBrcs/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild rcs-5.9.4
-${THOME}/build/genpkg TRIBrcs rcs-5.9.4
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only rcs-5.10.1
+${THOME}/build/genpkg TRIBrcs rcs-5.10.1
