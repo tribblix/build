0dcf6434a3b0ef2dba6b72b73ef2cdce15c67161
Peter Tribble <peter.tribble@gmail.com>
2022-02-28 19:46:15 +0000

    Update gawk 5.1.1

--- TRIBtext-gawk/build.sh
+++ TRIBtext-gawk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only -gnu gawk-5.1.0 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gawk gawk-5.1.0
+${THOME}/build/dobuild +64only -gnu gawk-5.1.1 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gawk gawk-5.1.1
