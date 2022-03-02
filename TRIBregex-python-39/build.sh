c9c9e9a6b86ba796bd2e1f84339ae82161587dd9
Peter Tribble <peter.tribble@gmail.com>
2022-03-02 08:40:52 +0000

    Update python regex 2022.3.2

--- TRIBregex-python-39/build.sh
+++ TRIBregex-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack regex-2022.1.18
-cd regex-2022.1.18
+${THOME}/build/unpack regex-2022.3.2
+cd regex-2022.3.2
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
