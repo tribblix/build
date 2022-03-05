16996e3363a41e3f54d8756a0779d0600ca9e81f
Peter Tribble <peter.tribble@gmail.com>
2022-03-05 12:45:37 +0000

    Update fossil 2.18, and make it 64-bit

--- TRIBfossil/build.sh
+++ TRIBfossil/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# we've had a while with 2.0 so the sha1/sha3 switch is history
+# it's not a normal configure, so have to pass flags
+# directly to get 64-bit
 #
-${THOME}/build/dobuild fossil-src-2.16 -C --enable-json
-${THOME}/build/genpkg TRIBfossil fossil-src-2.16
+env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild fossil-src-2.18 -C --json
+${THOME}/build/genpkg TRIBfossil fossil-src-2.18
