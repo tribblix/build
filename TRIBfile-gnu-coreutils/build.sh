6bb2db192655332386f28874755191ac4935bedc
Peter Tribble <peter.tribble@gmail.com>
2022-03-09 13:02:20 +0000

    Update coreutils 9.0

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -3,5 +3,5 @@
 # we absolutely want a 64-bit date, and it's easier and smaller to
 # make everything 64-bit
 #
-${THOME}/build/dobuild +64only -gnu coreutils-8.32 -P /usr/gnu -C "--without-gmp gl_cv_host_operating_system=illumos"
-${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-8.32
+${THOME}/build/dobuild +64only -gnu coreutils-9.0 -P /usr/gnu -C "--without-libgmp gl_cv_host_operating_system=illumos"
+${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-9.0
