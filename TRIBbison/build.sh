e8eece446b456e6cfe3d6ad762035d174b1a8533
Peter Tribble <peter.tribble@gmail.com>
2022-03-12 09:50:38 +0000

    Update bison 3.8.2

--- TRIBbison/build.sh
+++ TRIBbison/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only bison-3.7.4 -C --disable-yacc
-${THOME}/build/genpkg TRIBbison bison-3.7.4
+# explicitly tell the build where gm4 is, otherwise it will pick the
+# 64-bit symlink which we may not support in perpetuity
+#
+env M4=/usr/bin/gm4 ${THOME}/build/dobuild -64only bison-3.8.2 -C --disable-yacc
+${THOME}/build/genpkg TRIBbison bison-3.8.2
