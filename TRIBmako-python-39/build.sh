7c73a807e2962e397f8c71e84adcff954158bb93
Peter Tribble <peter.tribble@gmail.com>
2022-03-12 12:00:44 +0000

    Update Mako 1.2.0

--- TRIBmako-python-39/build.sh
+++ TRIBmako-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Mako-1.1.6
-cd Mako-1.1.6
+${THOME}/build/unpack Mako-1.2.0
+cd Mako-1.2.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
