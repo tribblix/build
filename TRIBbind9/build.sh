a31115ef8701d5ed819de3bc113789f45c0889a4
Peter Tribble <peter.tribble@gmail.com>
2022-03-17 08:17:32 +0000

    Update bind 9.11.37

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -2,5 +2,5 @@
 #
 # must not have TRIBlibedit or TRIBjson-c installed
 #
-${THOME}/build/dobuild bind-9.11.36 -C "--sysconfdir=/etc --localstatedir=/var --without-python"
-${THOME}/build/genpkg TRIBbind9 bind-9.11.36
+${THOME}/build/dobuild bind-9.11.37 -C "--sysconfdir=/etc --localstatedir=/var --without-python"
+${THOME}/build/genpkg TRIBbind9 bind-9.11.37
