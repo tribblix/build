015a8bfe8a3c646c5527e02b47c6a5b3b2816d93
Peter Tribble <peter.tribble@gmail.com>
2022-03-20 19:28:26 +0000

    Rebuild mog with python39

--- TRIBmog/build.sh
+++ TRIBmog/build.sh
@@ -8,9 +8,9 @@ cd mog-0.6.0
 # we need the file from file-magic
 #
 gsed -i "s:\['file':\['file-5.39':" mog/__init__.py
-/usr/versions/python-3.7/bin/python3  setup.py build
+/usr/versions/python-3.9/bin/python3  setup.py build
 rm -fr /tmp/sut
-/usr/versions/python-3.7/bin/python3  setup.py install --root=/tmp/sut
+/usr/versions/python-3.9/bin/python3  setup.py install --root=/tmp/sut
 ${THOME}/build/create_pkg TRIBmog /tmp/sut
 cd ..
 rm -fr /tmp/sut
