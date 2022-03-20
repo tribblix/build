57013f5054b831be4833dd216c61463c1a211cc5
Peter Tribble <peter.tribble@gmail.com>
2022-03-20 16:43:35 +0000

    Update barman 2.19

--- TRIBpgbarman/build.sh
+++ TRIBpgbarman/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack barman-2.17
-cd barman-2.17
-/usr/versions/python-3.7/bin/python3 setup.py build
+${THOME}/build/unpack barman-2.19
+cd barman-2.19
+/usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
-/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/pil
+/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBpgbarman /tmp/pil
 cd ..
 rm -fr /tmp/pil
