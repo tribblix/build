6000c3bde842029446f41ea32066295a5aa465b9
Peter Tribble <peter.tribble@gmail.com>
2022-03-21 16:00:19 +0000

    Rebuild gnome-doc-utils, correct dependencies

--- TRIBgnome-doc-utils/build.sh
+++ TRIBgnome-doc-utils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu gnome-doc-utils-0.20.10
+env PYTHON=/usr/bin/python2.7 ${THOME}/build/dobuild -gnu gnome-doc-utils-0.20.10
 ${THOME}/build/genpkg TRIBgnome-doc-utils gnome-doc-utils-0.20.10
