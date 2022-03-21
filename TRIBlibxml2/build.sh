a6e4c3b7eb6e546b7127af2ebbbd61d18d8b3553
Peter Tribble <peter.tribble@gmail.com>
2022-03-21 15:35:24 +0000

    Revert "Deprecate python2 version of libxml2 module" (Needed for gnome-doc-utils)
    This reverts commit 85a7b22ff8d88476770ed8c49b36d7c7d7544c21.

--- TRIBlibxml2/build.sh
+++ TRIBlibxml2/build.sh
@@ -7,6 +7,15 @@
 env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.9 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
 ${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.9
 
+# we need this for gnome-doc-utils
+rm -fr /tmp/lxp
+cd libxml2-2.9.9/python
+python2.7 setup.py build
+python2.7 setup.py install --root=/tmp/lxp
+${THOME}/build/create_pkg TRIBlibxml2-python-27 /tmp/lxp
+python2.7 setup.py clean
+cd ../..
+
 # python3.9 is 64-bit
 rm -fr /tmp/lxp
 cd libxml2-2.9.9-64bit/python
