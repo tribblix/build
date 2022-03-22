8e786f79007eb847a3bf19a6dc64df88cbb711d9
Peter Tribble <peter.tribble@gmail.com>
2022-03-22 21:42:58 +0000

    Update exiftool 12.40

--- TRIBexiftool/build.sh
+++ TRIBexiftool/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Image-ExifTool-12.07
-cd Image-ExifTool-12.07
+${THOME}/build/unpack Image-ExifTool-12.40
+cd Image-ExifTool-12.40
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBexiftool
