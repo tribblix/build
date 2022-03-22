2ffe7f228f1f35b783bc383e4ad49597c416084a
Peter Tribble <peter.tribble@gmail.com>
2022-03-22 21:59:18 +0000

    Update colordiff 1.0.20

--- TRIBtext-colordiff/build.sh
+++ TRIBtext-colordiff/build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #
-${THOME}/build/unpack  colordiff-1.0.18
-cd colordiff-1.0.18
+${THOME}/build/unpack colordiff-1.0.20
+cd colordiff-1.0.20
 gmake
-gsed -i 's:install :ginstall :' Makefile
-gsed -i 's:diff_cmd=diff:diff_cmd=gdiff:'
+gsed -i 's:diff_cmd=diff:diff_cmd=gdiff:' colordiffrc*
 rm -fr /tmp/cdi
 mkdir -p /tmp/cdi
-gmake install DESTDIR=/tmp/cdi INSTALL_DIR=/usr/bin MAN_DIR=/usr/share/man/man1
+gmake install INSTALL=ginstall DESTDIR=/tmp/cdi INSTALL_DIR=/usr/bin MAN_DIR=/usr/share/man/man1
 ${THOME}/build/create_pkg TRIBtext-colordiff /tmp/cdi
 rm -fr /tmp/cdi
 cd ..
