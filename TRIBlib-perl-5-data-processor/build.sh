5ee44fb575a4707da3cc4ae20c764332a5538bef
Peter Tribble <peter.tribble@gmail.com>
2022-03-22 21:04:02 +0000

    Rebuild perl Illumos-SMF and Illumos-Zones

--- TRIBlib-perl-5-data-processor/build.sh
+++ TRIBlib-perl-5-data-processor/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Data-Processor-0.4.2
-cd Data-Processor-0.4.2
+${THOME}/build/unpack Data-Processor-1.0.9
+cd Data-Processor-1.0.9
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-data-processor Data-Processor-0.4.2
+${THOME}/build/genpkg TRIBlib-perl-5-data-processor Data-Processor-1.0.9
+
