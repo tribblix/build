bf11e86c84da1290939375de926646715702be8d
Peter Tribble <peter.tribble@gmail.com>
2022-03-22 19:58:05 +0000

    Rebuild perl xml-parser

--- TRIBlib-perl-5-xml-parser/build.sh
+++ TRIBlib-perl-5-xml-parser/build.sh
@@ -6,25 +6,25 @@
 # you must run this once, install the package, then run it again
 # to make the final package
 #
-${THOME}/build/unpack XML-SAX-Base-1.08
-${THOME}/build/unpack XML-NamespaceSupport-1.11
-${THOME}/build/unpack XML-SAX-0.99
-${THOME}/build/unpack XML-Parser-2.44
+${THOME}/build/unpack XML-SAX-Base-1.09
+${THOME}/build/unpack XML-NamespaceSupport-1.12
+${THOME}/build/unpack XML-SAX-1.02
+${THOME}/build/unpack XML-Parser-2.46
 ${THOME}/build/unpack XML-SAX-Expat-0.51
-${THOME}/build/unpack XML-Simple-2.20
-cd XML-SAX-Base-1.08
+${THOME}/build/unpack XML-Simple-2.25
+cd XML-SAX-Base-1.09
 perl Makefile.PL
 make
 cd ..
-cd XML-NamespaceSupport-1.11
+cd XML-NamespaceSupport-1.12
 perl Makefile.PL
 make
 cd ..
-cd XML-SAX-0.99
+cd XML-SAX-1.02
 perl Makefile.PL
 make
 cd ..
-cd XML-Parser-2.44
+cd XML-Parser-2.46
 perl Makefile.PL
 make
 cd ..
@@ -32,8 +32,8 @@ cd XML-SAX-Expat-0.51
 perl Makefile.PL
 make
 cd ..
-cd XML-Simple-2.20
+cd XML-Simple-2.25
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-xml-parser XML-SAX-Base-1.08 XML-NamespaceSupport-1.11 XML-SAX-0.99 XML-Parser-2.44 XML-SAX-Expat-0.51 XML-Simple-2.20
+${THOME}/build/genpkg TRIBlib-perl-5-xml-parser XML-SAX-Base-1.09 XML-NamespaceSupport-1.12 XML-SAX-1.02 XML-Parser-2.46 XML-SAX-Expat-0.51 XML-Simple-2.25
