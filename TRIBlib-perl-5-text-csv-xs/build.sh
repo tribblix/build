c7fbe9bc77a3d14e9d28218f950a82117a7a124d
Peter Tribble <peter.tribble@gmail.com>
2022-03-22 20:06:18 +0000

    Rebuild perl Text-CSV_XS

--- TRIBlib-perl-5-text-csv-xs/build.sh
+++ TRIBlib-perl-5-text-csv-xs/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Text-CSV_XS-1.38
-cd Text-CSV_XS-1.38
+${THOME}/build/unpack Text-CSV_XS-1.47
+cd Text-CSV_XS-1.47
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-text-csv-xs Text-CSV_XS-1.38
+${THOME}/build/genpkg TRIBlib-perl-5-text-csv-xs Text-CSV_XS-1.47
