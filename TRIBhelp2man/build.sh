75578b40e86cd07f498d94617e2f270bf8ac3ad6
Peter Tribble <peter.tribble@gmail.com>
2022-03-23 10:51:14 +0000

    Rebuild help2man

--- TRIBhelp2man/build.sh
+++ TRIBhelp2man/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild help2man-1.47.8
-${THOME}/build/genpkg TRIBhelp2man help2man-1.47.8
+${THOME}/build/dobuild help2man-1.49.1
+${THOME}/build/genpkg TRIBhelp2man help2man-1.49.1
