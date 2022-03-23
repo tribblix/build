b2f74bbbd3f60425d060cb87df0a133bf9de1197
Peter Tribble <peter.tribble@gmail.com>
2022-03-23 19:23:19 +0000

    Rebuild Archive::Zip

--- TRIBlib-perl-5-archive-zip/build.sh
+++ TRIBlib-perl-5-archive-zip/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Archive-Zip-1.53
-cd Archive-Zip-1.53
+${THOME}/build/unpack Archive-Zip-1.68
+cd Archive-Zip-1.68
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBlib-perl-5-archive-zip
+cd ..
