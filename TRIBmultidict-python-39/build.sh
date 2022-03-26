c3bfd35690525306b0eed01638547a4ba13925c0
Peter Tribble <peter.tribble@gmail.com>
2022-03-26 20:37:42 +0000

    Update httpie 3.1.0

--- TRIBmultidict-python-39/build.sh
+++ TRIBmultidict-python-39/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httpie-3.1.0
-cd httpie-3.1.0
+${THOME}/build/unpack multidict-6.0.2
+cd multidict-6.0.2
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBhttpie-python-39 /tmp/pil
+${THOME}/build/create_pkg TRIBmultidict-python-39 /tmp/pil
 cd ..
 rm -fr /tmp/pil
