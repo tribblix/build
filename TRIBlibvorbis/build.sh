07589027d7fd72ae0ed0eda3f88641069d257ef9
Peter Tribble <peter.tribble@gmail.com>
2022-03-29 19:28:00 +0100

    Update libvorbis 1.3.7

--- TRIBlibvorbis/build.sh
+++ TRIBlibvorbis/build.sh
@@ -2,5 +2,5 @@
 #
 # (needs ogg)
 #
-${THOME}/build/dobuild -64 libvorbis-1.3.6
-${THOME}/build/genpkg TRIBlibvorbis libvorbis-1.3.6
+${THOME}/build/dobuild -64 libvorbis-1.3.7
+${THOME}/build/genpkg TRIBlibvorbis libvorbis-1.3.7
