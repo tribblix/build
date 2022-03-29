246ed1c80bbfc982fa64794aa5f547190dfde061
Peter Tribble <peter.tribble@gmail.com>
2022-03-29 19:16:13 +0100

    Update flac 1.3.4

--- TRIBlibflac/build.sh
+++ TRIBlibflac/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 flac-1.3.3
-${THOME}/build/genpkg TRIBlibflac flac-1.3.3
+${THOME}/build/dobuild -64 flac-1.3.4
+${THOME}/build/genpkg TRIBlibflac flac-1.3.4
