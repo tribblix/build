8a828734a02637b3a377887c79aae7b3f3978026
Peter Tribble <peter.tribble@gmail.com>
2022-04-04 11:17:49 +0100

    Update appres 1.0.6

--- TRIBx11-appres/build.sh
+++ TRIBx11-appres/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild appres-1.0.4
-${THOME}/build/genpkg TRIBx11-appres appres-1.0.4
+${THOME}/build/dobuild -64only appres-1.0.6
+${THOME}/build/genpkg TRIBx11-appres appres-1.0.6
