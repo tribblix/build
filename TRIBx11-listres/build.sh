ea935d887b480e6a77a59a793def4da81ae45804
Peter Tribble <peter.tribble@gmail.com>
2022-04-04 09:45:03 +0100

    Update listres 1.0.5

--- TRIBx11-listres/build.sh
+++ TRIBx11-listres/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild listres-1.0.3
-${THOME}/build/genpkg TRIBx11-listres listres-1.0.3
+${THOME}/build/dobuild -64only listres-1.0.5
+${THOME}/build/genpkg TRIBx11-listres listres-1.0.5
