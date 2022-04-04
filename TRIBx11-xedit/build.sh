fbb466120dc3dd1e121948d36dd15d3d502f2a11
Peter Tribble <peter.tribble@gmail.com>
2022-04-04 11:59:15 +0100

    Update xedit 1.2.3

--- TRIBx11-xedit/build.sh
+++ TRIBx11-xedit/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xedit-1.2.2
-${THOME}/build/genpkg TRIBx11-xedit xedit-1.2.2
+${THOME}/build/dobuild -64only xedit-1.2.3
+${THOME}/build/genpkg TRIBx11-xedit xedit-1.2.3
