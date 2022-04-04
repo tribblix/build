222a211c25782e785e89388dd74226b4bcfa1a92
Peter Tribble <peter.tribble@gmail.com>
2022-04-04 19:11:12 +0100

    Update xlsfonts 1.0.7

--- TRIBx11-info-clients/build.sh
+++ TRIBx11-info-clients/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xdpyinfo-1.3.2
-${THOME}/build/dobuild xdriinfo-1.0.6
-${THOME}/build/dobuild xlsatoms-1.1.3
-${THOME}/build/dobuild xlsclients-1.1.4
-${THOME}/build/dobuild xlsfonts-1.0.6
-${THOME}/build/dobuild xvinfo-1.1.4
-${THOME}/build/dobuild xwininfo-1.1.5
-${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.3.2 xdriinfo-1.0.6 xlsatoms-1.1.3 xlsclients-1.1.4 xlsfonts-1.0.6 xvinfo-1.1.4 xwininfo-1.1.5
+${THOME}/build/dobuild -64only xdpyinfo-1.3.2
+${THOME}/build/dobuild -64only xdriinfo-1.0.6
+${THOME}/build/dobuild -64only xlsatoms-1.1.3
+${THOME}/build/dobuild -64only xlsclients-1.1.4
+${THOME}/build/dobuild -64only xlsfonts-1.0.7
+${THOME}/build/dobuild -64only xvinfo-1.1.4
+${THOME}/build/dobuild -64only xwininfo-1.1.5
+${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.3.2 xdriinfo-1.0.6 xlsatoms-1.1.3 xlsclients-1.1.4 xlsfonts-1.0.7 xvinfo-1.1.4 xwininfo-1.1.5
