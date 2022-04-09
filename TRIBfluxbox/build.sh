22dd8264ecf3862b198ffad497ebd131ab9c8a9b
Peter Tribble <peter.tribble@gmail.com>
2022-04-09 21:03:38 +0100

    Modernize fluxbox menus

--- TRIBfluxbox/build.sh
+++ TRIBfluxbox/build.sh
@@ -3,5 +3,5 @@
 #  add TRIBtext-locale to provide gencat
 #  unfortunately 1.3.7 barfs in translations, hence -k
 #
-${THOME}/build/dobuild fluxbox-1.3.7 -M "-k"
+${THOME}/build/dobuild -64only fluxbox-1.3.7 -M "-k"
 ${THOME}/build/genpkg TRIBfluxbox fluxbox-1.3.7
