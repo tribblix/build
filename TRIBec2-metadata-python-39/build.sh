26a27f23464a387617c11875bd551546b5057d05
Peter Tribble <peter.tribble@gmail.com>
2022-04-10 17:19:59 +0100

    Update python ec2-metadata 2.9.1

--- TRIBec2-metadata-python-39/build.sh
+++ TRIBec2-metadata-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ec2-metadata-2.9.0
-cd ec2-metadata-2.9.0
+${THOME}/build/unpack ec2-metadata-2.9.1
+cd ec2-metadata-2.9.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
