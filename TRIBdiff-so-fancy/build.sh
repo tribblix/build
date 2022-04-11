646ac5a5c31180dac7062a643a577c8e719495bc
Peter Tribble <peter.tribble@gmail.com>
2022-04-11 19:50:47 +0100

    Add diff-so-fancy

---
+++ TRIBdiff-so-fancy/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# depends on perl
+#
+rm -fr /tmp/ckx
+mkdir -p /tmp/ckx/usr/versions
+cd /tmp/ckx/usr/versions
+#
+git clone https://github.com/so-fancy/diff-so-fancy
+cd diff-so-fancy
+rm -fr .git .circleci .gitmodules .travis.yml test third_party appveyor.yml package*json
+cd
+${THOME}/build/create_pkg TRIBdiff-so-fancy /tmp/ckx
+rm -fr /tmp/ckx
