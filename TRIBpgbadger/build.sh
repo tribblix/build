8f336cd2e58d288e7809982f028eb90f783ea77f
Peter Tribble <peter.tribble@gmail.com>
2022-04-12 10:21:58 +0100

    Update pgBadger 11.8

--- TRIBpgbadger/build.sh
+++ TRIBpgbadger/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pgbadger-11.7
-cd pgbadger-11.7
+${THOME}/build/unpack pgbadger-11.8
+cd pgbadger-11.8
 # you need to run a make to create the manpage
 perl Makefile.PL
 sed -i 's:sed -i.bak:gsed -i.bak:' Makefile
