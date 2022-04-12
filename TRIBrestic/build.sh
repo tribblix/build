4c831d6af2ff1e3573bee3af1a67ad6abe7372bf
Peter Tribble <peter.tribble@gmail.com>
2022-04-12 10:38:28 +0100

    Update restic 0.13.1

--- TRIBrestic/build.sh
+++ TRIBrestic/build.sh
@@ -5,14 +5,16 @@ cd rr
 git clone https://github.com/restic/restic
 cd restic
 #
-# do not use go 1.9.4 for this
+# at least go 1.14
 #
-go run -mod=vendor build.go
+go run build.go
 
 rm -fr /tmp/rr
 mkdir -p /tmp/rr/usr/bin
 cp restic /tmp/rr/usr/bin
 mkdir -p /tmp/rr/usr/share/restic
 cp README.rst LICENSE VERSION /tmp/rr/usr/share/restic
+mkdir -p /tmp/rr/usr/share/man/man1
+/tmp/rr/usr/bin/restic generate --man /tmp/rr/usr/share/man/man1
 $THOME/build/create_pkg TRIBrestic /tmp/rr
 rm -fr /tmp/rr
