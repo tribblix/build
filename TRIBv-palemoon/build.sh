5047cea0cd23732621f4cadeecafe2a2cb13f071
Peter Tribble <peter.tribble@gmail.com>
2022-04-13 15:19:49 +0100

    Update Pale Moon 29.4.6

--- TRIBv-palemoon/build.sh
+++ TRIBv-palemoon/build.sh
@@ -15,7 +15,7 @@
 # https://forum.palemoon.org/viewtopic.php?f=5&t=27369
 #
 # rm -fr palemoon-source
-# $THOME/build/unpack palemoon-29.4.5.source
+# $THOME/build/unpack palemoon-29.4.6.source
 #
 # cd palemoon-source
 # cp $THOME/build/patches/palemoon.mozconfig .mozconfig
@@ -29,9 +29,9 @@
 # but I've found that doesn't work
 #
 # This will create a tarball
-#   palemoon-29.4.5.solaris2.11-x86_64-illumos.tar.xz
+#   palemoon-29.4.6.solaris2.11-x86_64-illumos.tar.xz
 # in the following directory
 #   obj-x86_64-pc-solaris2.11/dist
 # this is the tarball we unpack to create the package
 #
-${THOME}/build/pkg_tarball TRIBv-palemoon palemoon-29.4.5.solaris2.11-x86_64-illumos
+${THOME}/build/pkg_tarball TRIBv-palemoon palemoon-29.4.6.solaris2.11-x86_64-illumos
