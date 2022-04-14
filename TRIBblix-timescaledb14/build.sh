03108909e7ade74a40f42665eff4e116e1921a14
Peter Tribble <peter.tribble@gmail.com>
2022-04-14 08:12:40 +0100

    Update timescaledb 2.6.1

--- TRIBblix-timescaledb14/build.sh
+++ TRIBblix-timescaledb14/build.sh
@@ -4,7 +4,7 @@
 #
 git clone https://github.com/timescale/timescaledb.git
 cd timescaledb
-git checkout 2.6.0
+git checkout 2.6.1
 env PATH=/opt/tribblix/postgres14/bin:$PATH PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig ./bootstrap
 cd build
 gmake
