0d2a701829368dcb61b067d11ce66819b6b87f95
Peter Tribble <peter.tribble@gmail.com>
2022-04-15 12:54:05 +0100

    Update skopeo 1.7.1

--- TRIBskopeo/build.sh
+++ TRIBskopeo/build.sh
@@ -5,20 +5,15 @@ cd sk
 setenv GOPATH `pwd`
 git clone https://github.com/containers/skopeo $GOPATH/src/github.com/containers/skopeo
 cd $GOPATH/src/github.com/containers/skopeo
-gmake binary-local
-
 #
-# fix vendor/github.com/containers/storage/lockfile_unix.go
-# copy TouchedSince from lockfile_linux.go
+# fix vendor/github.com/containers/storage/pkg/homedir/homedir_others.go
+# build !solaris
 #
-# fix vendor/github.com/docker/docker/client/client_unix.go
-# build solaris
-#
-gmake binary-local
+gmake bin/skopeo
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
-cp skopeo /tmp/ee/usr/bin
+cp bin/skopeo /tmp/ee/usr/bin
 mkdir -p /tmp/ee/usr/share/skopeo
 cp LICENSE README.md default-policy.json /tmp/ee/usr/share/skopeo
 ${THOME}/build/create_pkg TRIBskopeo /tmp/ee
