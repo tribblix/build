e7fe7a51bb54c28be07dee9220f0d13b352a2a83
Peter Tribble <peter.tribble@gmail.com>
2022-04-15 14:03:18 +0100

    Update loki

--- TRIBblix-loki/build.sh
+++ TRIBblix-loki/build.sh
@@ -9,21 +9,19 @@ mkdir -p src/github.com/grafana
 cd src/github.com/grafana
 git clone https://github.com/grafana/loki
 cd $GOPATH/src/github.com/grafana/loki
-go build ./cmd/loki
-# edit vendor/github.com/Azure/azure-storage-blob-go/azblob/zc_mmf_unix.go
-# -> it's unix.Mmap and unix.Munmap, not syscall, and build solaris
-go build ./cmd/loki
+#
+# need at least 1.17 for the net module
+#
+/usr/versions/go-1.17/bin/go build ./cmd/loki
 ls -l ./loki
-go build ./cmd/promtail
 # edit vendor/github.com/hpcloud/tail/tail_posix.go - build solaris
-go build ./cmd/promtail
-go build ./cmd/logcli
+/usr/versions/go-1.17/bin/go build ./clients/cmd/promtail
+ls -l ./promtail
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/loki/bin
 cp ./loki /tmp/ee/opt/tribblix/loki/bin
 cp ./promtail /tmp/ee/opt/tribblix/loki/bin
-cp ./logcli /tmp/ee/opt/tribblix/loki/bin
 cp README.md /tmp/ee/opt/tribblix/loki
 ${THOME}/build/create_pkg TRIBblix-loki /tmp/ee
 rm -fr /tmp/ee
