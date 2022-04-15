7edca94d95342d371114858f8382f52d751217b1
Peter Tribble <peter.tribble@gmail.com>
2022-04-15 21:27:08 +0100

    Update gdb 8.3.1

--- TRIBdebug-gdb/build.sh
+++ TRIBdebug-gdb/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #
+# need to build 32-bit with --disable-largefile because of procfs
+# perl to find pod2man for the manual pages
 #
-#
-${THOME}/build/dobuild gdb-7.12.1 -C --disable-largefile
-mv gdb-7.12.1 gdb-7.12.1-32
-${THOME}/build/dobuild +64 gdb-7.12.1
-mv gdb-7.12.1-32 gdb-7.12.1
-env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/genpkg TRIBdebug-gdb gdb-7.12.1
+env PATH=/usr/perl5/5.34/bin:$PATH ${THOME}/build/dobuild gdb-8.3.1 -C --disable-largefile
+mv gdb-8.3.1 gdb-8.3.1-32
+env PATH=/usr/perl5/5.34/bin:$PATH ${THOME}/build/dobuild +64 gdb-8.3.1
+mv gdb-8.3.1-32 gdb-8.3.1
+env PATH=/usr/perl5/5.34/bin:$PATH ${THOME}/build/genpkg TRIBdebug-gdb gdb-8.3.1
