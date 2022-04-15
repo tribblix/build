e03664e18948817c5bf1333909d1b9bf55af882a
Peter Tribble <peter.tribble@gmail.com>
2022-04-15 20:32:42 +0100

    Update wireshark 2.6.20, use QT5

--- TRIBwireshark/build.sh
+++ TRIBwireshark/build.sh
@@ -5,6 +5,6 @@
 # need to explicitly tell it which qt as guessing is unreliable
 # as of 2.2, needs a little extra help with kerberos
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/kerberosv5 PATH=${PATH}:/usr/versions/Qt-4.8/bin:/usr/perl5/5.22.0/bin ${THOME}/build/dobuild -gnu wireshark-2.6.16 -C "--with-qt=4 -without-lua --with-gtk=3"
-${THOME}/build/genpkg TRIBwireshark wireshark-2.6.16
-${THOME}/build/genpkg TRIBwireshark-qt wireshark-2.6.16
+env TRIBBLIX_CFLAGS=-I/usr/include/kerberosv5 PATH=${PATH}:/usr/versions/Qt-5/bin:/usr/perl5/5.34/bin ${THOME}/build/dobuild -gnu wireshark-2.6.20 -C "--with-qt=5 -without-lua --with-gtk=3 --without-maxminddb"
+${THOME}/build/genpkg TRIBwireshark wireshark-2.6.20
+${THOME}/build/genpkg TRIBwireshark-qt wireshark-2.6.20
