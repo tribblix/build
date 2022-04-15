0dd9c835be3e557d921655345ed1d1b0275892a8
Peter Tribble <peter.tribble@gmail.com>
2022-04-15 19:17:55 +0100

    Update squid 5.5

--- TRIBblix-squid/build.sh
+++ TRIBblix-squid/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #
-# Problems with pod2man and -Werror
-#
 # uninstall TRIBlibtool, otherwise it gets linked against
 #
+# build with -k because although it correctly detects that it can't build
+# with kerberos, it still tries to build the kerberos tests, which it can't
+#
 zap uninstall TRIBlibtool
-env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/dobuild squid-3.5.28 -P /opt/tribblix/squid -C "--with-gnutls --disable-strict-error-checking --disable-static --disable-ltdl-install"
-env PATH=/usr/perl5/5.22.0/bin:$PATH ${THOME}/build/genpkg TRIBblix-squid squid-3.5.28
+${THOME}/build/dobuild -64only squid-5.5 -P /opt/tribblix/squid -C "--with-gnutls --disable-static --disable-ltdl-install" -M -k
+${THOME}/build/genpkg TRIBblix-squid squid-5.5
