3c0128fd467563e27ef89598e4ee186470a0869a
Peter Tribble <peter.tribble@gmail.com>
2022-04-17 19:03:12 +0100

    Update collectd 5.12.0

--- TRIBcollectd/build.sh
+++ TRIBcollectd/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# zap install yajl
-#
-${THOME}/build/dobuild -64only collectd-5.7.2 -C "--disable-static --localstatedir=/var CFLAGS=-std=gnu99 --sysconfdir=/etc --localstatedir=/var"
-${THOME}/build/genpkg TRIBcollectd collectd-5.7.2
+${THOME}/build/dobuild -64only collectd-5.12.0 -C "--disable-static --localstatedir=/var --sysconfdir=/etc --localstatedir=/var --enable-rrdtool"
+${THOME}/build/genpkg TRIBcollectd collectd-5.12.0
