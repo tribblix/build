3be57043e02ae4e238025e5423cfcb4830f97fd2
Peter Tribble <peter.tribble@gmail.com>
2022-04-19 10:02:35 +0100

    Update gpgme 1.17.1, and fix build

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 #
-# must not have QT5 installed, else it will try and build against
-# it and fail, but there's no easy way to turn the QT support off
-#
-# gpgme 1.11 needs libgpg-error 1.29
+# QT5 bindings don't work, disabled in the preconf script
 #
 # needs a preconf.64 script to go with --with-libassuan-prefix=/usr
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 ${THOME}/build/dobuild -64 gpgme-1.16.0 -C "--with-libassuan-prefix=/usr --disable-gpg-test"
-${THOME}/build/genpkg TRIBgpgme gpgme-1.16.0
+${THOME}/build/dobuild -64 gpgme-1.17.1 -C "--with-libassuan-prefix=/usr --disable-gpg-test"
+${THOME}/build/genpkg TRIBgpgme gpgme-1.17.1
