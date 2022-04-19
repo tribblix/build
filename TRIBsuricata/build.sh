daa15471a8a213ad560782fae58ee92ff34207e2
Peter Tribble <peter.tribble@gmail.com>
2022-04-19 07:59:11 +0100

    Update suricata 5.0.8

--- TRIBsuricata/build.sh
+++ TRIBsuricata/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild suricata-4.1.5 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
-${THOME}/build/genpkg -I install-full TRIBsuricata suricata-4.1.5
+env TRIBBLIX_LDFLAGS=-lm ${THOME}/build/dobuild -64only suricata-5.0.8 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
+${THOME}/build/genpkg -I install-full TRIBsuricata suricata-5.0.8
