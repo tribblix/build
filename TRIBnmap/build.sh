7a991584366bc1b4b8c051e14f593f7b48eb538a
Peter Tribble <peter.tribble@gmail.com>
2022-04-19 18:44:34 +0100

    Clean up nmap build, no python needed ro shipped

--- TRIBnmap/build.sh
+++ TRIBnmap/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # zenmap requires pygtk, which we don't have
+# ndiff is python2-only. so make sure we don't ship it
 #
-${THOME}/build/dobuild -64only nmap-7.92 -C "--without-zenmap --with-liblua=included --with-libpcre=included PYTHON=python3.9"
+${THOME}/build/dobuild -64only nmap-7.92 -C "--without-zenmap --with-liblua=included --with-libpcre=included PYTHON=python3.9 --without-ndiff"
 ${THOME}/build/genpkg TRIBnmap nmap-7.92
