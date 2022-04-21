a3bed557ec2374b60be058605e076446a2d45094
Peter Tribble <peter.tribble@gmail.com>
2022-04-21 08:19:19 +0100

    Update jdk11 11.0.15

--- TRIBopenjdk11/build.sh
+++ TRIBopenjdk11/build.sh
@@ -57,8 +57,8 @@
 #
 
 cd ~/ud
-${THOME}/build/unpack jdk11u-jdk-11.0.14-ga
-cd jdk11u-jdk-11.0.14-ga
+${THOME}/build/unpack jdk11u-jdk-11.0.15-ga
+cd jdk11u-jdk-11.0.15-ga
 
 #
 # as of 11.0.7, switch to a gcc build to replace Stuido
@@ -95,9 +95,9 @@ env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
 # cd build/solaris-x86_64-normal-server-release/images/jdk
 # ./bin/java -version
 #
-# openjdk version "11.0.14-internal" 2022-01-18
-# OpenJDK Runtime Environment (build 11.0.14-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.14-ga)
-# OpenJDK 64-Bit Server VM (build 11.0.14-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.14-ga, mixed mode)
+# openjdk version "11.0.15-internal" 2022-04-19
+# OpenJDK Runtime Environment (build 11.0.15-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.15-ga)
+# OpenJDK 64-Bit Server VM (build 11.0.15-internal+0-adhoc.ptribble.jdk11u-jdk-11.0.15-ga, mixed mode)
 #
 
 rm -fr /tmp/jdk
