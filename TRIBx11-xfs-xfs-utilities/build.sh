aa729f6da4882c0f4e2f19884ad87e8c369839dc
Peter Tribble <peter.tribble@gmail.com>
2022-04-25 18:30:20 +0100

    Rebuild xfs-utilities 64-bit

--- TRIBx11-xfs-xfs-utilities/build.sh
+++ TRIBx11-xfs-xfs-utilities/build.sh
@@ -3,8 +3,8 @@
 # this is a merge of several smaller packages, that was the way
 # it was inherited.
 #
-${THOME}/build/dobuild fslsfonts-1.0.5
-${THOME}/build/dobuild fstobdf-1.0.6
-${THOME}/build/dobuild showfont-1.0.5
-${THOME}/build/dobuild xfsinfo-1.0.6
+${THOME}/build/dobuild -64only fslsfonts-1.0.5
+${THOME}/build/dobuild -64only fstobdf-1.0.6
+${THOME}/build/dobuild -64only showfont-1.0.5
+${THOME}/build/dobuild -64only xfsinfo-1.0.6
 ${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.5 fstobdf-1.0.6 showfont-1.0.5 xfsinfo-1.0.6
