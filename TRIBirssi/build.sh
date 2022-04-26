56f1f9b600bd68153d72bf2ddeef7c279506e534
Peter Tribble <peter.tribble@gmail.com>
2022-04-26 09:48:09 +0100

    Rebuild irssi 64-bit

--- TRIBirssi/build.sh
+++ TRIBirssi/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild irssi-1.2.3 -C --with-perl=no
+env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only irssi-1.2.3 -C --with-perl=no
 ${THOME}/build/genpkg TRIBirssi irssi-1.2.3
