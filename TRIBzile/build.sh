c3c132825979a3242b76000be0bbc9a710608f29
Peter Tribble <peter.tribble@gmail.com>
2022-04-28 13:27:01 +0100

    Update zile 2.4.15, and build 64-bit

--- TRIBzile/build.sh
+++ TRIBzile/build.sh
@@ -2,5 +2,7 @@
 #
 # needs boehm-gc and help2man
 #
-${THOME}/build/dobuild zile-2.4.14
-${THOME}/build/genpkg TRIBzile zile-2.4.14
+# 2.6 was rewritten in vala, stick to older versions
+#
+${THOME}/build/dobuild -64only zile-2.4.15
+${THOME}/build/genpkg TRIBzile zile-2.4.15
