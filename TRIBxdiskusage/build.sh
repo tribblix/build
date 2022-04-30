cb4fc9b982404aa630bc015fe36992fc29247fc5
Peter Tribble <peter.tribble@gmail.com>
2022-04-30 14:19:27 +0100

    Rebuild xdiskusage 64-bit

--- TRIBxdiskusage/build.sh
+++ TRIBxdiskusage/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xdiskusage-1.60
+# need postconf to get 64-bit to take properly
+#
+${THOME}/build/dobuild -64only xdiskusage-1.60
 ${THOME}/build/genpkg TRIBxdiskusage xdiskusage-1.60
