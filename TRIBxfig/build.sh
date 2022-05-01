224e7b5ebf8ae84902b7644d4cda5214f19fd3d5
Peter Tribble <peter.tribble@gmail.com>
2022-05-01 20:37:57 +0100

    Update xfig and fig2dev to 3.2.8b, and build 64-bit

--- TRIBxfig/build.sh
+++ TRIBxfig/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 #
-# in 3.2.8a there's a file that doesn't correctly handle being
-# not Xaw3d, so the postconf script handles that and will need
-# removing if we ever do use Xaw3d
+# now link against Xaw3d
 #
-${THOME}/build/dobuild xfig-3.2.8a -C --without-xaw3d
-${THOME}/build/dobuild fig2dev-3.2.8a
-${THOME}/build/genpkg TRIBxfig xfig-3.2.8a fig2dev-3.2.8a
+${THOME}/build/dobuild -64only xfig-3.2.8b
+${THOME}/build/dobuild -64only fig2dev-3.2.8b
+${THOME}/build/genpkg TRIBxfig xfig-3.2.8b fig2dev-3.2.8b
---
+++ patches/fig2dev-3.2.8b.pls
@@ -0,0 +1,1 @@
+-p1 fig2dev-wait.patch
