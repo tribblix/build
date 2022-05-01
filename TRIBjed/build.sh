74f27b411c5168f7d812b1b960c94b6ea43fedc0
Peter Tribble <peter.tribble@gmail.com>
2022-05-01 20:12:40 +0100

    Update jed 0.99.20-159, and build 64-bit

--- TRIBjed/build.sh
+++ TRIBjed/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild jed-pre0.99.20-128
-cd jed-pre0.99.20-128
+${THOME}/build/dobuild -64only jed-pre0.99.20-159
+cd jed-pre0.99.20-159-64bit
 gmake xjed
 cd ..
-${THOME}/build/genpkg TRIBjed jed-pre0.99.20-128
+${THOME}/build/genpkg TRIBjed jed-pre0.99.20-159
