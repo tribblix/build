06396ffdf52619f295611ef6b846ea3de14c74f7
Peter Tribble <peter.tribble@gmail.com>
2022-05-01 16:19:23 +0100

    Update mutt 2.2.4

--- TRIBmutt/build.sh
+++ TRIBmutt/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild mutt-2.1.5 -64only -C "--enable-imap --with-ssl  --with-slang --enable-smtp --enable-sidebar --enable-gpgme"
-${THOME}/build/genpkg TRIBmutt mutt-2.1.5
+${THOME}/build/dobuild mutt-2.2.4 -64only -C "--enable-imap --with-ssl  --with-slang --enable-smtp --enable-sidebar --enable-gpgme"
+${THOME}/build/genpkg TRIBmutt mutt-2.2.4
