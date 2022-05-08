31295364d4adc4ac5b380f0621a3c4a909f4e70b
Peter Tribble <peter.tribble@gmail.com>
2022-05-08 14:34:39 +0100

    Update freeglut 3.2.2, and make 64-bit

--- TRIBfreeglut/build.sh
+++ TRIBfreeglut/build.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack freeglut-3.0.0
-cd freeglut-3.0.0
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr      \
-      -DCMAKE_BUILD_TYPE=Release       \
-      -DFREEGLUT_BUILD_DEMOS=OFF       \
-      -DFREEGLUT_BUILD_STATIC_LIBS=OFF \
-      ..
-gmake -j 8
-${THOME}/build/genpkg TRIBfreeglut
+${THOME}/build/cmbuild -64 freeglut-3.2.2 -C "-DFREEGLUT_BUILD_STATIC_LIBS=OFF -DCMAKE_BUILD_TYPE=Release"
+${THOME}/build/genpkg TRIBfreeglut freeglut-3.2.2
---
+++ patches/freeglut-3.2.2.pls
@@ -0,0 +1,1 @@
+-p1 freeglut-fghJoystickRawRead.patch
