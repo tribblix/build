e3adaa992dc5a77faf9d0a7346a491cf5efe38fa
Peter Tribble <peter.tribble@gmail.com>
2022-05-11 10:59:49 +0100

    Update Pale Moon 31.0.0

--- TRIBv-palemoon/build.sh
+++ TRIBv-palemoon/build.sh
@@ -11,13 +11,15 @@
 #   gnm greadelf
 # somewhere in your path
 #
-# the new mechanism uses source tarballs
-# https://forum.palemoon.org/viewtopic.php?f=5&t=27369
-#
-# rm -fr palemoon-source
-# $THOME/build/unpack palemoon-29.4.6.source
-#
+# We're back to the git method
+# mkdir palemoon-source
 # cd palemoon-source
+# git clone https://repo.palemoon.org/MoonchildProductions/Pale-Moon.git ./
+# git submodule init && git submodule update
+# git checkout release && git submodule update
+#
+# and then the build
+#
 # cp $THOME/build/patches/palemoon.mozconfig .mozconfig
 # ./mach build
 # ./mach package
@@ -29,9 +31,9 @@
 # but I've found that doesn't work
 #
 # This will create a tarball
-#   palemoon-29.4.6.solaris2.11-x86_64-illumos.tar.xz
+#   palemoon-31.0.0.solaris2.11-x86_64-illumos.tar.xz
 # in the following directory
 #   obj-x86_64-pc-solaris2.11/dist
 # this is the tarball we unpack to create the package
 #
-${THOME}/build/pkg_tarball TRIBv-palemoon palemoon-29.4.6.solaris2.11-x86_64-illumos
+${THOME}/build/pkg_tarball TRIBv-palemoon palemoon-31.0.0.solaris2.11-x86_64-illumos
