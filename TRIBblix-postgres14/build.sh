9ac1c5bd0272aa06aae310eb5c9c527d18c09996
Peter Tribble <peter.tribble@gmail.com>
2022-05-13 10:35:21 +0100

    Update postgres 14.3

--- TRIBblix-postgres14/build.sh
+++ TRIBblix-postgres14/build.sh
@@ -3,7 +3,6 @@
 # postgres 14
 #
 # this is 64-bit, unlike prior versions
-# but we can't use --with-perl yet as that's only 32-bit
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres14 postgresql-14.2 -C "--without-readline --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres14 postgresql-14.2
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres14 postgresql-14.3 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres14 postgresql-14.3
