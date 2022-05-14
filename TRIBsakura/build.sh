3fc78cd192150d5bd58c3640ce113770e05a1605
Peter Tribble <peter.tribble@gmail.com>
2022-05-14 15:52:52 +0100

    Update sakura 3.8.5

--- TRIBsakura/build.sh
+++ TRIBsakura/build.sh
@@ -2,8 +2,8 @@
 #
 # later versions need a newer vte than we have
 #
-${THOME}/build/unpack sakura-3.5.0
-cd sakura-3.5.0
+${THOME}/build/unpack sakura-3.8.5
+cd sakura-3.8.5
 env PATH=/usr/gnu/bin:$PATH cmake -DCMAKE_INSTALL_PREFIX=/usr .
 env PATH=/usr/gnu/bin:$PATH gmake -j 4
 ${THOME}/build/genpkg TRIBsakura
