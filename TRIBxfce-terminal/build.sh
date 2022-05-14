82a5ffd57673b7ce0cdeda111509b28f828afac5
Peter Tribble <peter.tribble@gmail.com>
2022-05-14 15:48:48 +0100

    Update Xfce terminal 1.0.3

--- TRIBxfce-terminal/build.sh
+++ TRIBxfce-terminal/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-terminal-0.8.9.2
-${THOME}/build/genpkg TRIBxfce-terminal xfce4-terminal-0.8.9.2
+${THOME}/build/dobuild -gnu xfce4-terminal-1.0.3
+${THOME}/build/genpkg TRIBxfce-terminal xfce4-terminal-1.0.3
