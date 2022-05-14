63820450a912ae93251b4ac63e3ba039037731f1
Peter Tribble <peter.tribble@gmail.com>
2022-05-14 20:43:31 +0100

    Update lxterminal 0.4.0

--- TRIBlxterminal/build.sh
+++ TRIBlxterminal/build.sh
@@ -2,5 +2,5 @@
 #
 # explicitly tell it what vte and gtk to use
 #
-${THOME}/build/dobuild -gnu lxterminal-0.3.2 -C "--enable-gtk3 vte_CFLAGS=-I/usr/include/vte-2.91 vte_LIBS=-lvte-2.91 LIBS=-lsocket"
-${THOME}/build/genpkg TRIBlxterminal lxterminal-0.3.2
+${THOME}/build/dobuild -64only -gnu lxterminal-0.4.0 -C "--enable-gtk3 vte_CFLAGS=-I/usr/include/vte-2.91 vte_LIBS=-lvte-2.91 LIBS=-lsocket"
+${THOME}/build/genpkg TRIBlxterminal lxterminal-0.4.0
