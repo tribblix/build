08e3d57f02f2a1e37694dc921afb12086d4e35ec
Peter Tribble <peter.tribble@gmail.com>
2022-05-20 19:43:03 +0100

    Update claws-mail 4.1.0

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -3,5 +3,5 @@
 # maybe reinstate perl once we have a 64-bit perl
 #
 zap uninstall TRIBlibcanberra
-${THOME}/build/dobuild -64only claws-mail-4.0.0 -C "--disable-ldap --disable-perl-plugin"
-${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.0.0
+${THOME}/build/dobuild -64only claws-mail-4.1.0 -C "--disable-ldap --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.1.0
