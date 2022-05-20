6d90319e0284ecbdcf54e7998c1d725fee3ea961
Peter Tribble <peter.tribble@gmail.com>
2022-05-20 09:09:37 +0100

    Fix sakura build to include the man page

--- TRIBsakura/build.sh
+++ TRIBsakura/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 #
-# later versions need a newer vte than we have
-#
 ${THOME}/build/unpack sakura-3.8.5
 cd sakura-3.8.5
-env PATH=/usr/gnu/bin:$PATH cmake -DCMAKE_INSTALL_PREFIX=/usr .
+# path for pod2man to generate the man page
+env PATH=/usr/gnu/bin:${PATH}:/usr/perl5/bin cmake -DCMAKE_INSTALL_PREFIX=/usr .
 env PATH=/usr/gnu/bin:$PATH gmake -j 4
 ${THOME}/build/genpkg TRIBsakura
 
