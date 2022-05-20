f3f50c34271c81bf404aff9061e9d9984d9f33d0
Peter Tribble <peter.tribble@gmail.com>
2022-05-20 19:43:57 +0100

    Fix comment

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #
-# maybe reinstate perl once we have a 64-bit perl
 #
 zap uninstall TRIBlibcanberra
 ${THOME}/build/dobuild -64only claws-mail-4.1.0 -C "--disable-ldap --disable-dependency-tracking"
