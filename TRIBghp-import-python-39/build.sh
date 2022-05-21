90051f9e1526dc2cc882ad2d65626b330ee335cb
Peter Tribble <peter.tribble@gmail.com>
2022-05-21 13:38:50 +0100

    Update ghp-import 2.1.0

--- TRIBghp-import-python-39/build.sh
+++ TRIBghp-import-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ghp-import-2.0.2
-cd ghp-import-2.0.2
+${THOME}/build/unpack ghp-import-2.1.0
+cd ghp-import-2.1.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
