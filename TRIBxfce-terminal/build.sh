08ec3dbbb8f5064ed923c3f7a27d598c9348fb22
Peter Tribble <peter.tribble@gmail.com>
2022-05-22 10:30:40 +0100

    Update xfce terminal 1.0.4, and build 64-bit

--- TRIBxfce-terminal/build.sh
+++ TRIBxfce-terminal/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-terminal-1.0.3
-${THOME}/build/genpkg TRIBxfce-terminal xfce4-terminal-1.0.3
+${THOME}/build/dobuild -64only -gnu xfce4-terminal-1.0.4
+${THOME}/build/genpkg TRIBxfce-terminal xfce4-terminal-1.0.4
