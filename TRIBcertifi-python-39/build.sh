fb1d10827cf4eef7918a98edb89a790ea2b24200
Peter Tribble <peter.tribble@gmail.com>
2022-05-23 13:46:58 +0100

    Update certifi 2022.5.18.1

--- TRIBcertifi-python-39/build.sh
+++ TRIBcertifi-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack certifi-2021.10.8
-cd certifi-2021.10.8
+${THOME}/build/unpack certifi-2022.5.18.1
+cd certifi-2022.5.18.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
