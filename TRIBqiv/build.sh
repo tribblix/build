d921e2136dc21e24302207d2b7a10cf7ce1a1484
Peter Tribble <peter.tribble@gmail.com>
2022-05-28 12:39:44 +0100

    Update qiv 2.3.2

--- TRIBqiv/build.sh
+++ TRIBqiv/build.sh
@@ -2,9 +2,10 @@
 #
 # qiv (needs imlib2)
 #
-${THOME}/build/dobuild qiv-2.3.1
-cd qiv-2.3.1
+${THOME}/build/dobuild qiv-2.3.2
+cd qiv-2.3.2
 rm -fr /tmp/iq
+mkdir -p /tmp/iq/usr/share/man/man1
 gmake install PREFIX=/tmp/iq/usr
 ${THOME}/build/create_pkg TRIBqiv /tmp/iq
 rm -fr /tmp/iq
