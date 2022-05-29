4ee41fc79bd0bffc2dfcd130719d60dca2da0c78
Peter Tribble <peter.tribble@gmail.com>
2022-05-29 15:23:38 +0100

    Add ffuf

---
+++ TRIBffuf/build.sh
@@ -0,0 +1,14 @@
+#!/bin/tcsh
+#
+mkdir ff
+cd ff
+env GOPATH=`pwd` go install github.com/ffuf/ffuf@latest
+
+rm -fr /tmp/ef
+mkdir -p /tmp/ef/usr/bin
+cp bin/ffuf /tmp/ef/usr/bin
+mkdir -p /tmp/ef/usr/share/ffuf
+cd pkg/mod/github.com/ffuf/ffuf@v*
+cp LICENSE README.md /tmp/ef/usr/share/ffuf
+${THOME}/build/create_pkg TRIBffuf /tmp/ef
+rm -fr /tmp/ef
