ffe2ad12be919d7e118842303514016629218cc8
Peter Tribble <peter.tribble@gmail.com>
2022-06-01 22:27:55 +0100

    Update xfce4-taskmanager 1.5.4

--- TRIBxfce-taskmanager/build.sh
+++ TRIBxfce-taskmanager/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-taskmanager-1.2.3
-${THOME}/build/genpkg TRIBxfce-taskmanager xfce4-taskmanager-1.2.3
+${THOME}/build/dobuild -64only -gnu xfce4-taskmanager-1.5.4
+${THOME}/build/genpkg TRIBxfce-taskmanager xfce4-taskmanager-1.5.4
