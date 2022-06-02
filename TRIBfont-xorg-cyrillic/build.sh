4b9d13bcbf0549fabb03be3f4bf83aa765eaf3c4
Peter Tribble <peter.tribble@gmail.com>
2022-06-02 16:25:38 +0100

    Add Xorg cyrillic fonts

---
+++ TRIBfont-xorg-cyrillic/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# built in a similar way to TRIBfont-xorg-core
+#
+# to download
+# for xfont in `cat ${THOME}/build/TRIBfont-xorg-cyrillic/filelist`
+# do
+#    wget $xfont
+# done
+#
+# ** this really is a shell script **
+#
+for xfont in `cat ${THOME}/build/TRIBfont-xorg-cyrillic/filelist | awk -F/ '{print $NF}' | sed s:.tar.bz2::`
+do
+    $THOME/build/dobuild $xfont
+done
+
+
+${THOME}/build/genpkg TRIBfont-xorg-cyrillic `cat ${THOME}/build/TRIBfont-xorg-cyrillic/filelist | awk -F/ '{print $NF}' | sed s:.tar.bz2::`
