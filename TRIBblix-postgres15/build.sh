be75564eb4c923402140e73fdbc4e360a506e369
Peter Tribble <peter.tribble@gmail.com>
2022-06-02 11:19:30 +0100

    Add postgres 15beta1

---
+++ TRIBblix-postgres15/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# postgres 15
+#
+# this is 64-bit
+#
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres15 postgresql-15beta1 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres15 postgresql-15beta1
