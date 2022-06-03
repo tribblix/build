49968546643b19a43b494a65195ba2541f826d9a
Peter Tribble <peter.tribble@gmail.com>
2022-06-03 08:45:37 +0100

    Add Xorg jis-misc fonts

---
+++ TRIBfont-xorg-jis/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# built in a similar way to TRIBfont-xorg-core
+#
+# to download
+# for xfont in `cat ${THOME}/build/TRIBfont-xorg-jis/filelist`
+# do
+#    wget $xfont
+# done
+#
+# ** this really is a shell script **
+#
+# override destination so it doesn't stomp on other fonts (otherwise
+# there's a conflicting fonts.dir)
+#
+for xfont in `cat ${THOME}/build/TRIBfont-xorg-jis/filelist | awk -F/ '{print $NF}' | sed s:.tar.bz2::`
+do
+    $THOME/build/dobuild $xfont -C --with-fontdir=/usr/share/fonts/X11/jis
+done
+
+
+${THOME}/build/genpkg TRIBfont-xorg-jis `cat ${THOME}/build/TRIBfont-xorg-jis/filelist | awk -F/ '{print $NF}' | sed s:.tar.bz2::`
