23ba260b543a4e660915e740077dfca9fd80b120
Peter Tribble <peter.tribble@gmail.com>
2022-06-03 11:38:34 +0100

    Rebuild allmark with (slightly) newer go

--- TRIBblix-allmark/build.sh
+++ TRIBblix-allmark/build.sh
@@ -1,13 +1,13 @@
 #!/bin/tcsh
 #
-# requires go
+# requires go, 1.15 or lower
 #
 setenv GOPATH `pwd`
 mkdir -p src/github.com/andreaskoch
 cd src/github.com/andreaskoch
 git clone https://github.com/andreaskoch/allmark.git
 cd allmark
-env PATH=/usr/versions/go-1.14/bin:$PATH gmake
+env PATH=/usr/versions/go-1.15/bin:$PATH gmake
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/allmark/bin
 cp bin/files/allmark /tmp/ee/opt/tribblix/allmark/bin
