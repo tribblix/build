8a46d6eb89c7f98d4f96c9201633876b915c7ba7
Peter Tribble <peter.tribble@gmail.com>
2022-06-05 14:36:00 +0100

    Add a few of the programs from the X11R6 contrib

---
+++ TRIBx11-contrib/build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# this is a few of the programs from the X11R6 contrib tape
+#
+# https://www.x.org/releases/X11R6/
+#
+# assumes you've unpacked those 4 contrib tarballs and are in the
+# contrib/programs directory
+#
+
+cd xcalendar
+xmkmf -a -DHasGcc3=YES
+make
+cd ..
+cd xcolors
+xmkmf -a -DHasGcc3=YES
+sed -i s:/usr/share/X11:/usr/lib/X11: Makefile
+make
+cd ..
+cd xrubik
+xmkmf -a -DHasGcc3=YES
+make
+cd ..
+cd xdiary
+xmkmf -a -DHasGcc3=YES
+make
+cd ..
+cd AnswerGarden
+rm makefile
+xmkmf -a -DHasGcc3=YES
+sed -i 's:case NULL:case 0:' QA.c
+make
+cd ..
+cd Dclock
+xmkmf -DHasGcc3=YES
+sed -i s:sun/audioio:sys/audioio: Dclock.c
+make
+cd ..
+
+${THOME}/build/genpkg -I "install install.man" TRIBx11-contrib xcalendar xcolors xrubik xdiary AnswerGarden Dclock
