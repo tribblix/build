f49e56fc1ccc840d4cbb41aba74149bea2a0f0b2
Peter Tribble <peter.tribble@gmail.com>
2022-06-07 21:21:20 +0100

    Update libgcrypt 1.8.9

--- TRIBlibgcrypt/build.sh
+++ TRIBlibgcrypt/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild libgcrypt-1.8.8
-mv libgcrypt-1.8.8 libgcrypt-1.8.8-32bit
+${THOME}/build/dobuild libgcrypt-1.8.9
+mv libgcrypt-1.8.9 libgcrypt-1.8.9-32bit
 # sparc just requires --disable-asm
-${THOME}/build/dobuild +64 libgcrypt-1.8.8 -C "--disable-amd64-as-feature-detection --disable-asm"
-mv libgcrypt-1.8.8-32bit libgcrypt-1.8.8
-${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.8.8
+${THOME}/build/dobuild +64 libgcrypt-1.8.9 -C "--disable-amd64-as-feature-detection --disable-asm"
+mv libgcrypt-1.8.9-32bit libgcrypt-1.8.9
+${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.8.9
