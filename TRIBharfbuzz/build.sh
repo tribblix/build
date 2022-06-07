3a075dd5070bee87648e2dbf565d532a249bdcca
Peter Tribble <peter.tribble@gmail.com>
2022-06-07 21:56:38 +0100

    Update harfbuzz 4.3.0

--- TRIBharfbuzz/build.sh
+++ TRIBharfbuzz/build.sh
@@ -2,5 +2,5 @@
 #
 # Rebuild harfbuzz, with icu, and 64-bit (which it used to have anyway)
 zap install TRIBlibicu
-${THOME}/build/dobuild -64 harfbuzz-1.9.0
-${THOME}/build/genpkg TRIBharfbuzz harfbuzz-1.9.0
+$THOME/build/mesonbuild -64 harfbuzz-4.3.0 -C "-Dintrospection=disabled -Dglib=enabled -Dcairo=enabled -Dicu=enabled -Dfreetype=enabled --buildtype=release"
+${THOME}/build/genpkg TRIBharfbuzz harfbuzz-4.3.0
