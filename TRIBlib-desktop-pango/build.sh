6d2e013e6c82a4a108978c829ef0b321de45edd4
Peter Tribble <peter.tribble@gmail.com>
2022-06-07 22:09:54 +0100

    Update pango 1.44.7

--- TRIBlib-desktop-pango/build.sh
+++ TRIBlib-desktop-pango/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 pango-1.42.4 -C "--disable-introspection"
-${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.42.4
+$THOME/build/mesonbuild -64 pango-1.44.7 -C "--buildtype=release -Dintrospection=false"
+${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.44.7
