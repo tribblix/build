9eb37abe01afb92e403f3b60a59f0cb599bf7e24
Peter Tribble <peter.tribble@gmail.com>
2022-06-08 19:01:45 +0100

    Update libsoup 2.62.3

--- TRIBlibsoup/build.sh
+++ TRIBlibsoup/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu -64 libsoup-2.56.1 -C "--without-gnome --disable-more-warnings --disable-introspection --without-krb5-config"
-${THOME}/build/genpkg TRIBlibsoup libsoup-2.56.1
+# later versions add additional dependencies
+#
+${THOME}/build/dobuild -gnu -64 libsoup-2.62.3 -C "--without-gnome --disable-more-warnings --disable-introspection --without-krb5-config"
+${THOME}/build/genpkg TRIBlibsoup libsoup-2.62.3
