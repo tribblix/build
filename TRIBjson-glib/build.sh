c41f281cb555c0b65a4214a7e4ee530b11dcb7d0
Peter Tribble <peter.tribble@gmail.com>
2022-06-08 20:08:31 +0100

    Update json-glib 1.6.6

--- TRIBjson-glib/build.sh
+++ TRIBjson-glib/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 json-glib-1.2.8 -C --disable-introspection
-${THOME}/build/genpkg TRIBjson-glib json-glib-1.2.8
+env PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild -64 json-glib-1.6.6 -C "-Dintrospection=disabled --buildtype=release"
+${THOME}/build/genpkg TRIBjson-glib json-glib-1.6.6
