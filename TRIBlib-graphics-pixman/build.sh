8bcb31297e5e55d2a21df02dc187012ae9d24e18
Peter Tribble <peter.tribble@gmail.com>
2022-06-08 08:02:39 +0100

    Update pixman 0.40.0

--- TRIBlib-graphics-pixman/build.sh
+++ TRIBlib-graphics-pixman/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# (may need to pkgrm the original first)
+# disabling gtk is just for the tests, it doesn't change the
+# functionality
 #
-${THOME}/build/dobuild -64 pixman-0.34.0
-${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.34.0
+${THOME}/build/dobuild -64 pixman-0.40.0 -C --disable-gtk
+${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.40.0
