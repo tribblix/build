62664648064e6e33ff27286f4b1fb7845f467538
Peter Tribble <peter.tribble@gmail.com>
2022-06-12 13:56:11 +0100

    Update libgsf 1.14.49

--- TRIBlib-desktop-gsf/build.sh
+++ TRIBlib-desktop-gsf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu libgsf-1.14.47
-${THOME}/build/genpkg TRIBlib-desktop-gsf libgsf-1.14.47
+${THOME}/build/dobuild -64 -gnu libgsf-1.14.49
+${THOME}/build/genpkg TRIBlib-desktop-gsf libgsf-1.14.49
