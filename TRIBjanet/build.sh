11a466975bccde90d08bc9121c5f446fc4fb80ba
Peter Tribble <peter.tribble@gmail.com>
2022-06-12 13:34:57 +0100

    Add jpm to janet

--- TRIBjanet/build.sh
+++ TRIBjanet/build.sh
@@ -10,5 +10,20 @@ gmake CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__
 #gmake test CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__"
 #
 gmake install DESTDIR=/tmp/jj122 CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__"
+
+#
+# to install jpm you need to have built janet and installed it once
+#
+git clone --depth=1 https://github.com/janet-lang/jpm.git
+cd jpm
+env DESTDIR=/tmp/jj122 PREFIX=/usr janet bootstrap.janet
+
+#
+# to use jpm you need to override things to stop it trying to
+# write into /usr, eg
+#
+# jpm --cc=gcc --cc-link=gcc --modpath=`pwd` --binpath=`pwd`/bin install joy
+#
+
 ${THOME}/build/create_pkg TRIBjanet /tmp/jj122
 rm -fr /tmp/jj122
