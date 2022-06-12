835b80d93930dd9368a167b04a1429341949d033
Peter Tribble <peter.tribble@gmail.com>
2022-06-12 19:17:35 +0100

    Update agedu 20211129.8cd63c5

--- TRIBagedu/build.sh
+++ TRIBagedu/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/newbuild.sh both TRIBagedu
+env TRIBBLIX_LDFLAGS=-lsocket $THOME/build/cmbuild -64only agedu-20211129.8cd63c5
+${THOME}/build/genpkg TRIBagedu agedu-20211129.8cd63c5
