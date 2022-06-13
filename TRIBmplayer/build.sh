e94c7bf7f3fc5cb95a54d8e788ef32e978c91b15
Peter Tribble <peter.tribble@gmail.com>
2022-06-13 17:54:45 +0100

    Update MPlayer 1.5

--- TRIBmplayer/build.sh
+++ TRIBmplayer/build.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 #
-# It's a nightly svn export
+# It's not an autoconf configure
 #
-${THOME}/build/unpack mplayer-export-snapshot
-cd mplayer-export-2017-02-26
-# --disable-ffmpeg_a makes it use the working installed copy of ffmpeg
-# instead of downloading and building its own broken one
-env PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr --enable-gui --disable-ffmpeg_a
-# gui needs sdl, I think
-# wants yasm aka TRIByasm
-# Wants a grep that understands -E, hence path
-gmake -j 4
-# fails on awk
-env PATH=/usr/gnu/bin:$PATH gmake -j 4
-cd ..
-${THOME}/build/genpkg TRIBmplayer mplayer-export-2017-02-26
-# Needs a skin
-# GUI doesn't actually show properly, although standard playback works fine
+# versioning is interesting, it has 2 parts and we used the wrong
+# part of the version number in the past
+#
+env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_CXXFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 $THOME/build/dobuild MPlayer-1.5 -C --enable-gui
+${THOME}/build/genpkg TRIBmplayer MPlayer-1.5
