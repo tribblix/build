b3d5d52608a28dcc461c73764791a1dc5c55c1d9
Peter Tribble <peter.tribble@gmail.com>
2022-06-13 11:15:02 +0100

    Update gimp 2.10.30

--- TRIBgimp/build.sh
+++ TRIBgimp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu gimp-2.10.24 -C "--disable-python --with-bug-report-url=http://www.tribblix.org/feedback.html"
-${THOME}/build/genpkg TRIBgimp gimp-2.10.24
+${THOME}/build/dobuild -gnu gimp-2.10.30 -C "--disable-python --with-bug-report-url=http://www.tribblix.org/feedback.html"
+${THOME}/build/genpkg TRIBgimp gimp-2.10.30
