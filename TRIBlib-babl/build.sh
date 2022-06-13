e06b96447ebb41f340cbb6fe0e7e05a2c5747c18
Peter Tribble <peter.tribble@gmail.com>
2022-06-13 08:55:37 +0100

    Update babl 0.1.92

--- TRIBlib-babl/build.sh
+++ TRIBlib-babl/build.sh
@@ -1,39 +1,6 @@
 #!/bin/sh
 #
-# the build barfs on awk's crappiness, hence the AWK
-#
-#env AWK=nawk ${THOME}/build/dobuild -64 babl-0.1.24 -C "--disable-mmx --disable-sse"
-#${THOME}/build/genpkg TRIBlib-babl babl-0.1.24
-
-#
 # new meson build
 #
-${THOME}/build/unpack -64 babl-0.1.86
-cd babl-0.1.86
-meson --prefix=/usr -Denable-gir=false -Denable-mmx=false -Denable-sse=false build
-cd build
-ninja
-cd ..
-cd ..
-cd babl-0.1.86-64bit
-env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig meson --prefix=/usr -Denable-gir=false -Denable-mmx=false -Denable-sse=false --libdir=lib/amd64 -Dc_args=-m64 -Dc_link_args=-m64 build
-cd build
-ninja
-cd ..
-cd ..
-
-rm -fr /tmp/bb
-cd babl-0.1.86
-cd build
-env DESTDIR=/tmp/bb ninja install
-cd ..
-cd ..
-cd babl-0.1.86-64bit
-cd build
-env DESTDIR=/tmp/bb ninja install
-cd ..
-cd ..
-
-${THOME}/build/create_pkg TRIBlib-babl /tmp/bb
-
-rm -fr /tmp/bb
+$THOME/build/mesonbuild -64 babl-0.1.92 -C "-Denable-gir=false -Denable-mmx=false -Denable-sse=false --buildtype=release"
+${THOME}/build/genpkg TRIBlib-babl babl-0.1.92
