43a2bbbc5d7eae970736d267337e4d409fa4ae22
Peter Tribble <peter.tribble@gmail.com>
2022-06-13 13:41:29 +0100

    Update gnumeric 1.12.52, and build 64-bit

--- TRIBgnumeric/build.sh
+++ TRIBgnumeric/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu gnumeric-1.12.50
-${THOME}/build/genpkg TRIBgnumeric gnumeric-1.12.50
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu gnumeric-1.12.52
+${THOME}/build/genpkg TRIBgnumeric gnumeric-1.12.52
