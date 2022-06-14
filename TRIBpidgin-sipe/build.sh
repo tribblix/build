880a07fdf2fcc88eea4453ffd787b371b3128358
Peter Tribble <peter.tribble@gmail.com>
2022-06-14 13:51:22 +0100

    Rebuild pidgin-sipe 64-bit to match pidgin

--- TRIBpidgin-sipe/build.sh
+++ TRIBpidgin-sipe/build.sh
@@ -2,5 +2,5 @@
 #
 # need --disable-quality-check as otherwise it's confused by type conversions
 #
-env LDFLAGS=-R/usr/lib/mps LIBS=-lsocket ${THOME}/build/dobuild -gnu pidgin-sipe-1.25.0 -C "--with-krb5=no --disable-quality-check"
+env LDFLAGS=-R/usr/lib/mps/`${THOME}/build/getarch` LIBS=-lsocket ${THOME}/build/dobuild -64only -gnu pidgin-sipe-1.25.0 -C "--with-krb5=no --disable-quality-check"
 ${THOME}/build/genpkg TRIBpidgin-sipe pidgin-sipe-1.25.0
