3a6cf04fd84033f96d221bff9ceec6324909936c
Peter Tribble <peter.tribble@gmail.com>
2022-06-14 09:42:40 +0100

    Update libnice 0.1.19

--- TRIBlibnice/build.sh
+++ TRIBlibnice/build.sh
@@ -2,5 +2,5 @@
 #
 # libnice now has only new gstreamer plugins
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 ${THOME}/build/dobuild -64 libnice-0.1.17 -C "--disable-static --disable-introspection --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBlibnice libnice-0.1.17
+$THOME/build/mesonbuild -64 libnice-0.1.19 -C "-Dintrospection=disabled -Dtests=disabled --buildtype=release"
+${THOME}/build/genpkg TRIBlibnice libnice-0.1.19
