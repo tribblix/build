57a56cf45064b3fce42323b0b184d657abb9e952
Peter Tribble <peter.tribble@gmail.com>
2022-06-19 22:06:41 +0100

    Update vlc 3.0.17.4

--- TRIBvlc/build.sh
+++ TRIBvlc/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild vlc-2.2.8 -C --disable-lua
-${THOME}/build/genpkg TRIBvlc vlc-2.2.8
+# can't find procset_t - -D__EXTENSIONS__
+# wants the extended mshgdr struct - -D_XPG4_2
+# doesn't build in 32-bit because the asm isn't handled right
+#
+env LIBS="-lsocket -lnsl -lssp_ns" TRIBBLIX_CFLAGS="-D_XPG4_2 -D__EXTENSIONS__" $THOME/build/dobuild -64only vlc-3.0.17.4 -C "--disable-lua --disable-live555 --disable-smbclient BUILDCC=gcc"
+${THOME}/build/genpkg TRIBvlc vlc-3.0.17.4
