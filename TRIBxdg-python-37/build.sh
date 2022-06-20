06a9dd841077060a572890a527473b4766955f7c
Peter Tribble <peter.tribble@gmail.com>
2022-06-20 18:37:51 +0100

    Deprecate TRIBxdg-python-37

--- TRIBxdg-python-37/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack pyxdg-0.27
-cd pyxdg-0.27
-/usr/versions/python-3.7/bin/python3 setup.py build
-rm -fr /tmp/xx1
-/usr/versions/python-3.7/bin/python3 setup.py install --root=/tmp/xx1
-${THOME}/build/create_pkg TRIBxdg-python-37 /tmp/xx1
-rm -fr /tmp/xx1
