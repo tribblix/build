4d8bb28620daec3bcc4acee91658ccc5ef52a339
Peter Tribble <peter.tribble@gmail.com>
2022-06-20 18:28:26 +0100

    Update reportlab 3.6.10

--- TRIBreportlab-python-39/build.sh
+++ TRIBreportlab-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack reportlab-3.6.9
-cd reportlab-3.6.9
+${THOME}/build/unpack reportlab-3.6.10
+cd reportlab-3.6.10
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
