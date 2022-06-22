0d015c62a53e0b30a9d8ba870ea02879baafafeb
Peter Tribble <peter.tribble@gmail.com>
2022-06-22 18:39:06 +0100

    Update libssh 0.9.6

--- TRIBlibssh/build.sh
+++ TRIBlibssh/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
+# turn off SSP until we can work out how to persuade the 32-bit build
+# to pick up libssp_ns
 #
-#
-${THOME}/build/cmbuild -64 libssh-0.9.4 -C "-DWITH_STACK_PROTECTOR_STRONG=false -DWITH_STACK_PROTECTOR=false"
-${THOME}/build/genpkg TRIBlibssh libssh-0.9.4
+${THOME}/build/cmbuild -64 libssh-0.9.6 -C "-DWITH_STACK_PROTECTOR_STRONG=false -DWITH_STACK_PROTECTOR=false"
+${THOME}/build/genpkg TRIBlibssh libssh-0.9.6
