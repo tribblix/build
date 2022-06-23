f38da3dee11083a3808766b8fa3a142e264b34cc
Peter Tribble <peter.tribble@gmail.com>
2022-06-23 09:15:08 +0100

    Update htmldoc 1.9.15

--- TRIBhtmldoc/build.sh
+++ TRIBhtmldoc/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild htmldoc-1.8.30
-cd htmldoc-1.8.30
-rm -fr /tmp/hd
-gmake install datadir=/tmp/hd/usr/share mandir=/tmp/hd/usr/share/man bindir=/tmp/hd/usr/bin
-${THOME}/build/create_pkg TRIBhtmldoc /tmp/hd
-cd ..
-rm -fr /tmp/hd
+${THOME}/build/dobuild -64only htmldoc-1.9.15
+${THOME}/build/genpkg TRIBhtmldoc htmldoc-1.9.15
