c3526268a907c247164588c43fba3d8a8e0bfafe
Peter Tribble <peter.tribble@gmail.com>
2022-06-25 15:31:24 +0100

    Package mcross for python3.10 (not fully functional)

---
+++ TRIBmcross-python-310/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# pages come up blank on python 3.10, so experimental
+#
+${THOME}/build/unpack mcross-0.5.19
+cd mcross-0.5.19
+/usr/versions/python-3.10/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBmcross-python-310 /tmp/pil
+cd ..
+rm -fr /tmp/pil
