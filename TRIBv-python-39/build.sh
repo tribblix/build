bf9d77b965f0c4d489f1e5fb1e0cd780ed0ff70c
Peter Tribble <peter.tribble@gmail.com>
2022-06-25 15:13:50 +0100

    Update Python 3.9.13

--- TRIBv-python-39/build.sh
+++ TRIBv-python-39/build.sh
@@ -5,6 +5,6 @@
 #
 # 3.9 is 64-bit
 #
-${THOME}/build/dobuild +64only Python-3.9.12 -P /usr/versions/python-3.9 -L -C --enable-shared
+${THOME}/build/dobuild +64only Python-3.9.13 -P /usr/versions/python-3.9 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-39 Python-3.9.12
+${THOME}/build/genpkg TRIBv-python-39 Python-3.9.13
