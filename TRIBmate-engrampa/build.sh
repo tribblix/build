a5b7f106f23b64d0523cd006fadf9bb55bde15a8
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 14:56:07 +0100

    Update engrampa 1.24.2

--- TRIBmate-engrampa/build.sh
+++ TRIBmate-engrampa/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu engrampa-1.22.3
-${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.22.3
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu engrampa-1.24.2
+${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.24.2
