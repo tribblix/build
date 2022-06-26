d4edb0fb283ee61bc97d8ca0ff9b72e0ab97115c
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 14:34:40 +0100

    Update caja 1.24.1

--- TRIBmate-caja/build.sh
+++ TRIBmate-caja/build.sh
@@ -2,5 +2,5 @@
 #
 # needs mate-desktop installing for the build
 #
-${THOME}/build/dobuild -gnu caja-1.22.3 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-caja caja-1.22.3
+${THOME}/build/dobuild -64 -gnu caja-1.24.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-caja caja-1.24.1
