161ae774a952559d28c0514161b7744a9420b93d
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 11:07:30 +0100

    Update mate-themes 3.22.23

--- TRIBmate-theme/build.sh
+++ TRIBmate-theme/build.sh
@@ -3,5 +3,5 @@
 # mate-themes is released separately and matches gtk
 # rather than mate releases
 #
-${THOME}/build/dobuild -gnu mate-themes-3.22.21
-${THOME}/build/genpkg TRIBmate-theme mate-themes-3.22.21
+${THOME}/build/dobuild -gnu mate-themes-3.22.23
+${THOME}/build/genpkg TRIBmate-theme mate-themes-3.22.23
