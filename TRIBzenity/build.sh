8befbdde4a22e121b848740e6b5c069d8398f70e
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 16:11:12 +0100

    Fix zenity to no longer require gnome-doc-utils

--- TRIBzenity/build.sh
+++ TRIBzenity/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# zenity (gnome-doc-utils is a build dependency)
+# zenity (patched to no longer require gnome-doc-utils)
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild zenity-2.32.1 -C --disable-scrollkeeper
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild zenity-2.32.1 -C "--disable-scrollkeeper"
 ${THOME}/build/genpkg TRIBzenity zenity-2.32.1
