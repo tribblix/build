5928c90d65e9566fd4e1ef5af9f3866826921dc9
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 14:40:45 +0100

    Update atril 1.24.1

--- TRIBmate-atril/build.sh
+++ TRIBmate-atril/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu atril-1.22.3 -C --without-keyring
-${THOME}/build/genpkg TRIBmate-atril atril-1.22.3
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu atril-1.24.1 -C --without-keyring
+${THOME}/build/genpkg TRIBmate-atril atril-1.24.1
