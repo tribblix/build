4f53f19f2386f1288000ed3787b71f85320a19af
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 15:33:17 +0100

    Update mate-icon-theme 1.24.0

--- TRIBmate-icon-theme/build.sh
+++ TRIBmate-icon-theme/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu mate-icon-theme-1.22.2
-${THOME}/build/genpkg TRIBmate-icon-theme mate-icon-theme-1.22.2
+${THOME}/build/dobuild -gnu mate-icon-theme-1.24.0
+${THOME}/build/genpkg TRIBmate-icon-theme mate-icon-theme-1.24.0
