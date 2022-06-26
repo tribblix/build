fe02f03f08aa49058c11f627e9cfed1567a415b1
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 12:29:51 +0100

    Update mate-menus 1.24.1

--- TRIBmate-menus/build.sh
+++ TRIBmate-menus/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu mate-menus-1.22.1 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-menus mate-menus-1.22.1
+${THOME}/build/dobuild -64 -gnu mate-menus-1.24.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-menus mate-menus-1.24.1
