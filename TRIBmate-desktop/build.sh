e7342c53a6941cb091dd5272824fda7438aba686
Peter Tribble <peter.tribble@gmail.com>
2022-06-26 11:50:20 +0100

    Update mate-common-1.24.2 mate-desktop-1.24.1 libmatemixer-1.24.1

--- TRIBmate-desktop/build.sh
+++ TRIBmate-desktop/build.sh
@@ -3,7 +3,7 @@
 # bundle mate-common and libmatemixer in with this rather than creating a
 # separate package
 #
-${THOME}/build/dobuild -gnu libmatemixer-1.22.0
-${THOME}/build/dobuild mate-common-1.22.2
-${THOME}/build/dobuild -gnu mate-desktop-1.22.2 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-desktop mate-common-1.22.2 mate-desktop-1.22.2 libmatemixer-1.22.0
+${THOME}/build/dobuild -64 -gnu libmatemixer-1.24.1
+${THOME}/build/dobuild mate-common-1.24.2
+${THOME}/build/dobuild -64 -gnu mate-desktop-1.24.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-desktop mate-common-1.24.2 mate-desktop-1.24.1 libmatemixer-1.24.1
