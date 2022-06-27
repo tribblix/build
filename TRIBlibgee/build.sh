42b3fbd47e7406a0535693eba69d672fac05d14b
Peter Tribble <peter.tribble@gmail.com>
2022-06-27 20:20:20 +0100

    Update libgee 0.20.5

--- TRIBlibgee/build.sh
+++ TRIBlibgee/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libgee-0.20.0
-${THOME}/build/genpkg TRIBlibgee libgee-0.20.0
+${THOME}/build/dobuild -64 libgee-0.20.5 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBlibgee libgee-0.20.5
