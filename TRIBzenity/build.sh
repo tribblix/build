b40bda9ba173fd68c04e9851c7a5a6425e56bfbc
Peter Tribble <peter.tribble@gmail.com>
2022-06-27 19:23:21 +0100

    Update zenity 3.42.1

--- TRIBzenity/build.sh
+++ TRIBzenity/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# zenity (patched to no longer require gnome-doc-utils)
-#
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild zenity-2.32.1 -C "--disable-scrollkeeper"
-${THOME}/build/genpkg TRIBzenity zenity-2.32.1
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64only zenity-3.42.1
+${THOME}/build/genpkg TRIBzenity zenity-3.42.1
