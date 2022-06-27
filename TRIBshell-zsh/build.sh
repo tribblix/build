d9466aeecedfc31f0b7d683876a1b7426c63eaf9
Peter Tribble <peter.tribble@gmail.com>
2022-06-27 18:17:49 +0100

    Update zsh 5.9

--- TRIBshell-zsh/build.sh
+++ TRIBshell-zsh/build.sh
@@ -2,5 +2,5 @@
 #
 # need a preconf script and -L, which enables dynamic modules
 #
-${THOME}/build/dobuild +64only zsh-5.8 -L
-${THOME}/build/genpkg TRIBshell-zsh zsh-5.8
+${THOME}/build/dobuild +64only zsh-5.9 -L
+${THOME}/build/genpkg TRIBshell-zsh zsh-5.9
