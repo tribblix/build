281566c9151b41c0da117b206435423e6691351b
Peter Tribble <peter.tribble@gmail.com>
2022-06-27 18:03:04 +0100

    Update dash 0.5.11.5

--- TRIBshell-dash/build.sh
+++ TRIBshell-dash/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# internal scripts fail with our pos awk
-#
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/dobuild dash-0.5.9.1
-${THOME}/build/genpkg TRIBshell-dash dash-0.5.9.1
+${THOME}/build/dobuild -64only dash-0.5.11.5 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBshell-dash dash-0.5.11.5
---
+++ patches/dash-0.5.11.5.pls
@@ -0,0 +1,1 @@
+-p1 dash-configure-stat64.patch
