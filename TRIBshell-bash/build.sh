ff7e947b44255f62415b67f9954d81aec4afdfcf
Peter Tribble <peter.tribble@gmail.com>
2022-06-28 16:36:15 +0100

    Update bash 5.1.16

--- TRIBshell-bash/build.sh
+++ TRIBshell-bash/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only bash-5.0
-${THOME}/build/genpkg TRIBshell-bash bash-5.0
+${THOME}/build/dobuild +64only bash-5.1.16
+${THOME}/build/genpkg TRIBshell-bash bash-5.1.16
