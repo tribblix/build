44c7b7f5a43c64111968e871aad641f77a2e970f
Peter Tribble <peter.tribble@gmail.com>
2022-06-28 18:36:41 +0100

    Update gssdp 1.0.5

--- TRIBgssdp/build.sh
+++ TRIBgssdp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 gssdp-1.0.2 -C --disable-introspection
-${THOME}/build/genpkg TRIBgssdp gssdp-1.0.2
+${THOME}/build/dobuild -64 gssdp-1.0.5 -C "--disable-introspection --disable-dependency-tracking --without-gtk"
+${THOME}/build/genpkg TRIBgssdp gssdp-1.0.5
