db710ebad436d9f1a613da3ad9b62e087f16a223
Peter Tribble <peter.tribble@gmail.com>
2022-06-28 20:21:15 +0100

    Fix gupnp 64-bit pc file

--- TRIBgupnp/build.sh
+++ TRIBgupnp/build.sh
@@ -2,5 +2,7 @@
 #
 # gupnp [needs gssdp]
 #
+# stay downrev; 1.0.7 for example requires GLib2 2.66
+#
 ${THOME}/build/dobuild -64 gupnp-1.0.3 -C "UUID_CFLAGS=-I/usr/include/uuid UUID_LIBS=-luuid --disable-introspection"
 ${THOME}/build/genpkg TRIBgupnp gupnp-1.0.3
