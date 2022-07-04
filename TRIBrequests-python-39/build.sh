7ef78519925b1a37c85bd167e75affbe6b63d6d4
Peter Tribble <peter.tribble@gmail.com>
2022-07-04 20:34:09 +0100

    Update requests 2.28.1

--- TRIBrequests-python-39/build.sh
+++ TRIBrequests-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack requests-2.26.0
-cd requests-2.26.0
+${THOME}/build/unpack requests-2.28.1
+cd requests-2.28.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
