06121ae1bc6a9b43fdf44710662dd9bf42fdc916
Peter Tribble <peter.tribble@gmail.com>
2022-07-04 09:06:47 +0100

    Update brotli 1.0.9

--- TRIBbrotli/build.sh
+++ TRIBbrotli/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/cmbuild -64 brotli-1.0.4
-${THOME}/build/genpkg TRIBbrotli brotli-1.0.4
+${THOME}/build/cmbuild -64 brotli-1.0.9
+${THOME}/build/genpkg TRIBbrotli brotli-1.0.9
