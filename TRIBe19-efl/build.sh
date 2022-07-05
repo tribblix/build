b9487d1d4efa0acfb621144743ad0a9c761151a0
Peter Tribble <peter.tribble@gmail.com>
2022-07-05 22:03:53 +0100

    Update efl 1.18.5

--- TRIBe19-efl/build.sh
+++ TRIBe19-efl/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 #
-zap install TRIBgiflib
-zap install TRIBlibsndfile
-zap install TRIBbullet
-${THOME}/build/dobuild efl-1.12.3 -C "--disable-pulseaudio --enable-lua-old --enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-aba"
-${THOME}/build/genpkg TRIBe19-efl efl-1.12.3
+$THOME/build/dobuild -64 efl-1.18.5 -C "--with-profile=release --disable-pulseaudio --enable-fribidi --enable-harfbuzz --disable-libeeze --enable-lua-old --disable-cserve --enable-physics --disable-systemd --enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-abb"
+${THOME}/build/genpkg TRIBe19-efl efl-1.18.5
---
+++ patches/efl-1.18.5.pls
@@ -0,0 +1,1 @@
+-p1 efl-openssl11.patch
