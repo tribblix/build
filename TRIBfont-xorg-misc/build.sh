6de2f81127c5f026dcbeb19786d06369e551591d
Peter Tribble <peter.tribble@gmail.com>
2022-07-06 19:09:13 +0100

    Add some more misc fonts

---
+++ TRIBfont-xorg-misc/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# built in a similar way to TRIBfont-xorg-core
+#
+# to download
+# for xfont in `cat ${THOME}/build/TRIBfont-xorg-misc/filelist`
+# do
+#    wget $xfont
+# done
+#
+# ** this really is a shell script **
+#
+# override destination so it doesn't stomp on other fonts (otherwise
+# there's a conflicting fonts.dir)
+#
+for xfont in `cat ${THOME}/build/TRIBfont-xorg-misc/filelist | awk -F/ '{print $NF}' | sed s:.tar.bz2::`
+do
+    $THOME/build/dobuild $xfont -C --with-fontdir=/usr/share/fonts/X11/extra-misc
+done
+
+
+${THOME}/build/genpkg TRIBfont-xorg-misc `cat ${THOME}/build/TRIBfont-xorg-misc/filelist | awk -F/ '{print $NF}' | sed s:.tar.bz2::`
