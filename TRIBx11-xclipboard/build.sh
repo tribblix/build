064f356cc674033b62333c3522eeea861d6417e3
Peter Tribble <peter.tribble@gmail.com>
2022-07-12 09:06:46 +0100

    Update xclipboard 1.1.4

--- TRIBx11-xclipboard/build.sh
+++ TRIBx11-xclipboard/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xclipboard-1.1.3
-${THOME}/build/genpkg TRIBx11-xclipboard xclipboard-1.1.3
+${THOME}/build/dobuild -64only xclipboard-1.1.4
+${THOME}/build/genpkg TRIBx11-xclipboard xclipboard-1.1.4
