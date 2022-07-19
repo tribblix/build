37228bf29cbbeb0e4b00e82442f2b444511790d7
Peter Tribble <peter.tribble@gmail.com>
2022-07-19 10:24:35 +0100

    Update Radicale 3.1.8

--- TRIBradicale-python-39/build.sh
+++ TRIBradicale-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Radicale-3.1.7
-cd Radicale-3.1.7
+${THOME}/build/unpack Radicale-3.1.8
+cd Radicale-3.1.8
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
