0d80bc24d1b0f7c69c51a06147518ba8f410dbfb
Peter Tribble <peter.tribble@gmail.com>
2022-07-22 20:28:22 +0100

    Update ristretto 0.123, and build 64-bit

--- TRIBxfce-ristretto/build.sh
+++ TRIBxfce-ristretto/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu ristretto-0.12.2
-${THOME}/build/genpkg TRIBxfce-ristretto ristretto-0.12.2
+${THOME}/build/dobuild -gnu -64only ristretto-0.12.3
+${THOME}/build/genpkg TRIBxfce-ristretto ristretto-0.12.3
