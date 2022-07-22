6da1c43fa5d4017c795c9a1e9de471f833b18089
Peter Tribble <peter.tribble@gmail.com>
2022-07-22 12:26:01 +0100

    Update xconsole 1.0.8

--- TRIBx11-xconsole/build.sh
+++ TRIBx11-xconsole/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xconsole-1.0.7
-${THOME}/build/genpkg TRIBx11-xconsole xconsole-1.0.7
+${THOME}/build/dobuild -64only xconsole-1.0.8
+${THOME}/build/genpkg TRIBx11-xconsole xconsole-1.0.8
