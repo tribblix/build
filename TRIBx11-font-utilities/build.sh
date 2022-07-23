54d3e422990516ba8935091a2eda444ed964e650
Peter Tribble <peter.tribble@gmail.com>
2022-07-23 10:45:14 +0100

    Update font-util 1.3.3

--- TRIBx11-font-utilities/build.sh
+++ TRIBx11-font-utilities/build.sh
@@ -5,7 +5,7 @@
 #
 # font-utils is in the font directory, not the app directory
 #
-${THOME}/build/dobuild bdftopcf-1.1
-${THOME}/build/dobuild mkfontscale-1.2.2
-${THOME}/build/dobuild font-util-1.3.2
-${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.2 font-util-1.3.2
+${THOME}/build/dobuild -64only bdftopcf-1.1
+${THOME}/build/dobuild -64only mkfontscale-1.2.2
+${THOME}/build/dobuild -64only font-util-1.3.3
+${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.2 font-util-1.3.3
