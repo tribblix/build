102fe3b478e9c784cbb82ac232871ad708e95571
Peter Tribble <peter.tribble@gmail.com>
2022-07-27 08:50:50 +0100

    Update pybind11 2.10.0

--- TRIBpybind11-python-39/build.sh
+++ TRIBpybind11-python-39/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pybind11-2.9.2
-${THOME}/build/unpack pybind11_global-2.9.2
+${THOME}/build/unpack pybind11-2.10.0
+${THOME}/build/unpack pybind11_global-2.10.0
 rm -fr /tmp/pil
-cd pybind11-2.9.2
+cd pybind11-2.10.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
 cd ..
-cd pybind11_global-2.9.2
+cd pybind11_global-2.10.0
 /usr/versions/python-3.9/bin/python3 setup.py build
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
 cd ..
