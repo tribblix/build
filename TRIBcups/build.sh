f22af4e208404b55dc3b837740477d500ef15484
Peter Tribble <peter.tribble@gmail.com>
2022-07-30 10:30:21 +0100

    Update cups 2.4.2, and 64-bit

--- TRIBcups/build.sh
+++ TRIBcups/build.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 #
 # may need to rename the downloaded tarball, like so:
-#  mv cups-2.1.4-source.tar.gz cups-2.1.4.tar.gz 
+#  mv cups-2.4.2-source.tar.gz cups-2.4.2.tar.gz 
 #
-# cups 2.1 needed a preconf
-# we don't want main() in a shared library
-# gsed -i 's:-G:-shared:' configure
-# which no longer seems necessary as of 2.2
+
 #
-# [needs one of dnssd or avahi]
-# I think the problem here is that we have a broken copy of bonjour. If we
-# didn't have one at all the right thing would happen.
-env CC=gcc CXX=g++ ${THOME}/build/dobuild cups-2.2.6 -C "--sysconfdir=/etc --localstatedir=/var --disable-dnssd --disable-gssapi"
-cd cups-2.2.6
-rm -fr /tmp/hc
-gmake install BUILDROOT=/tmp/hc
-${THOME}/build/create_pkg TRIBcups /tmp/hc
-rm -fr /tmp/hc
-cd ..
+# it's a little bit more complicated than normal, in that we need to
+# add --with-archflags=-m64 to get the 64-bit build to work
+#
+# don't use openssl, the code hasn't been modernized for opensll 1.1.1
+# and we've built against gnutls in the past
+#
+env CC=gcc CXX=g++ TRIBBLIX_CFLAGS="-D_XOPEN_SOURCE=600 -D__EXTENSIONS__" TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64 cups-2.4.2 -C "--sysconfdir=/etc --localstatedir=/var --disable-gssapi --with-tls=gnutls" +C "--sysconfdir=/etc --localstatedir=/var --disable-gssapi --with-tls=gnutls --with-archflags=-m64"
+$THOME/build/genpkg TRIBcups cups-2.4.2
