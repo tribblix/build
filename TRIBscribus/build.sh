ce9dd551b52772e446e3b99c77f63bf8d222d9c8
Peter Tribble <peter.tribble@gmail.com>
2022-07-31 14:04:15 +0100

    Update scribus 1.5.4

--- TRIBscribus/build.sh
+++ TRIBscribus/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #
-${THOME}/build/unpack scribus-1.4.6
-cd scribus-1.4.6
-env PATH=/usr/versions/Qt-4.8/bin:$PATH cmake . -DCMAKE_INSTALL_PREFIX:PATH=/usr -DPYTHON_INCLUDE_DIR=/usr/versions/python-2.7/include/python2.7
-gmake
-${THOME}/build/genpkg TRIBscribus
+# later versions require a poppler upgrade
+# unfortunately scripting support is stuck at python2
+# gcc7 miscompiles something so it won't even start
+#
+# manual fixes:
+# scribus/ui/scresizecursor.cpp include QPainterPath as well as QPainter
+# comment swab() in stdlib.h to avoid duplicate definition conflict
+#
+env PATH=/usr/versions/Qt-5/bin:$PATH $THOME/build/cmbuild scribus-1.5.4 -C "-DPYTHON_LIBRARY=/usr/versions/python-2.7/lib/libpython2.7.so -DPYTHON_INCLUDE_DIR=/usr/versions/python-2.7/include/python2.7"
+${THOME}/build/genpkg TRIBscribus scribus-1.5.4
