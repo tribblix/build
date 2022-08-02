3f344f34ed2160ffaff37082c0c48a39a4bd4324
Peter Tribble <peter.tribble@gmail.com>
2022-08-02 20:41:54 +0100

    Update librevenge 0.0.4, and 64-bit

--- TRIBlibrevenge/build.sh
+++ TRIBlibrevenge/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild librevenge-0.0.2 -C --disable-werror 
-${THOME}/build/genpkg TRIBlibrevenge librevenge-0.0.2
+env TRIBBLIX_CFLAGS=-pthreads TRIBBLIX_CXXFLAGS=-pthreads ${THOME}/build/dobuild -64 librevenge-0.0.4 -C --disable-werror 
+${THOME}/build/genpkg TRIBlibrevenge librevenge-0.0.4
