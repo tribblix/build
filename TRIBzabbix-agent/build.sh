447beefe3cd48024c2898a789e643707f0c66f2b
Peter Tribble <peter.tribble@gmail.com>
2022-08-02 11:55:57 +0100

    Update zabbix 4.0.43

--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-4.0.38 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre-include=/usr/include/pcre"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-4.0.38
+${THOME}/build/dobuild -64only zabbix-4.0.43 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre-include=/usr/include/pcre"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-4.0.43
--- TRIBzabbix-proxy/build.sh
+++ TRIBzabbix-proxy/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild zabbix-4.0.38 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl --with-libpcre-include=/usr/include/pcre"
-${THOME}/build/genpkg TRIBzabbix-proxy zabbix-4.0.38
+${THOME}/build/dobuild -64only zabbix-4.0.43 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl --with-libpcre-include=/usr/include/pcre"
+${THOME}/build/genpkg TRIBzabbix-proxy zabbix-4.0.43
