d0ac3004e7fdad277612a50ee9e859d281e4339b
Peter Tribble <peter.tribble@gmail.com>
2022-08-05 09:30:41 +0100

    Update Django 3.2.15 4.0.7

--- TRIBdjango4-python-39/build.sh
+++ TRIBdjango4-python-39/build.sh
@@ -2,8 +2,8 @@
 #
 # pytz is deprecated, but marked as a dependency for transitional purposes
 #
-${THOME}/build/unpack Django-4.0.6
-cd Django-4.0.6
+${THOME}/build/unpack Django-4.0.7
+cd Django-4.0.7
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
