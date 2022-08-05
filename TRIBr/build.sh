6709f1ee313d783b91fc938e65a6831601eb2642
Peter Tribble <peter.tribble@gmail.com>
2022-08-05 11:07:15 +0100

    Update R 4.2.1

--- TRIBr/build.sh
+++ TRIBr/build.sh
@@ -5,18 +5,10 @@
 #
 zap install-overlay fortran
 zap install TRIBgnu-libiconv TRIBreadline
-${THOME}/build/unpack R-3.6.3
-cd R-3.6.3
-env PATH=/usr/gnu/bin:$PATH CPPFLAGS="-I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ./configure --prefix=/usr
-gmake -j 12
+env TRIBBLIX_CFLAGS=-I/usr/gnu/include TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" $THOME/build/dobuild -gnu R-4.2.1 -C --disable-openmp
+
+cd R-4.2.1
 gmake check
-rm -fr /tmp/grs
-# there's a broken dependency here, install requires
-# this even though it can't make is
-touch doc/NEWS.pdf
-env PATH=/usr/gnu/bin:$PATH gmake prefix=/tmp/grs/usr install-strip
-rm /tmp/grs/usr/lib/R/doc/NEWS.pdf
-# (strip results in 64M rather than 80M of disk space used)
-${THOME}/build/create_pkg TRIBr /tmp/grs
 cd ..
-rm -fr /tmp/grs
+
+$THOME/build/genpkg TRIBr R-4.2.1 
