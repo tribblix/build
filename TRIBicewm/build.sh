a02dc9001e97d633bfe6b9ebbe378c388c4c59b0
Peter Tribble <peter.tribble@gmail.com>
2022-08-06 12:47:25 +0100

    Update IceWM 2.9.9

--- TRIBicewm/build.sh
+++ TRIBicewm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only icewm-2.9.6
-${THOME}/build/genpkg TRIBicewm icewm-2.9.6
+${THOME}/build/dobuild -64only icewm-2.9.9
+${THOME}/build/genpkg TRIBicewm icewm-2.9.9
