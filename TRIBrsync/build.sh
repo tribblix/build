63a50dab7bb77cbc3db2c1f75eae7dec4df0fbda
Peter Tribble <peter.tribble@gmail.com>
2022-08-11 08:38:29 +0100

    Update rsync 3.2.5pre2

--- TRIBrsync/build.sh
+++ TRIBrsync/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only rsync-3.2.5pre1 -C "--disable-xxhash"
-${THOME}/build/genpkg TRIBrsync rsync-3.2.5pre1
+${THOME}/build/dobuild -64only rsync-3.2.5pre2 -C "--disable-xxhash"
+${THOME}/build/genpkg TRIBrsync rsync-3.2.5pre2
