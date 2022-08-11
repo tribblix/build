c2ae7bfe7d93d795c2cc74a169ae8ffe465d7ca2
Peter Tribble <peter.tribble@gmail.com>
2022-08-11 08:17:28 +0100

    Update jdk19 19+35

--- TRIBopenjdk19/build.sh
+++ TRIBopenjdk19/build.sh
@@ -5,8 +5,8 @@
 #
 
 cd ~/ud
-${THOME}/build/unpack jdk19-jdk-19-34
-cd jdk19-jdk-19-34
+${THOME}/build/unpack jdk19-jdk-19-35
+cd jdk19-jdk-19-35
 
 #
 # jdk19 needs autoconf installed
@@ -43,8 +43,8 @@ env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
 # ./bin/java -version
 #
 # openjdk version "19-internal" 2022-09-20
-# OpenJDK Runtime Environment (build 19-internal-adhoc.ptribble.jdk19-jdk-19-34)
-# OpenJDK 64-Bit Server VM (build 19-internal-adhoc.ptribble.jdk19-jdk-19-34, mixed mode, sharing)
+# OpenJDK Runtime Environment (build 19-internal-adhoc.ptribble.jdk19-jdk-19-35)
+# OpenJDK 64-Bit Server VM (build 19-internal-adhoc.ptribble.jdk19-jdk-19-35, mixed mode, sharing)
 #
 
 rm -fr /tmp/jdk
