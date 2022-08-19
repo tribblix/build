7aa886640eb80bb6eb4d08b8fc08da0cbb9c91a9
Peter Tribble <peter.tribble@gmail.com>
2022-08-19 11:38:09 +0100

    Update bind 9.16.32

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -4,5 +4,5 @@
 #
 # the preconf file builds a private static libuv
 #
-env LIBUV_CFLAGS=-I`pwd`/bind-9.16.31-64bit/libuv-v1.40.0-64bit/include LIBUV_LIBS="`pwd`/bind-9.16.31-64bit/libuv-v1.40.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_CFLAGS=-D_XPG4_2 ${THOME}/build/dobuild -64only bind-9.16.31 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip"
-${THOME}/build/genpkg TRIBbind9 bind-9.16.31
+env LIBUV_CFLAGS=-I`pwd`/bind-9.16.32-64bit/libuv-v1.40.0-64bit/include LIBUV_LIBS="`pwd`/bind-9.16.32-64bit/libuv-v1.40.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_CFLAGS=-D_XPG4_2 ${THOME}/build/dobuild -64only bind-9.16.32 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip"
+${THOME}/build/genpkg TRIBbind9 bind-9.16.32
