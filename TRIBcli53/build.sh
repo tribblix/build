9833c923fd486a17da9159ad95c77c3b02a8de35
Peter Tribble <peter.tribble@gmail.com>
2022-08-23 18:05:43 +0100

    Update cli53

--- TRIBcli53/build.sh
+++ TRIBcli53/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #
-env GOPATH=`pwd` go get github.com/barnybug/cli53/cmd/cli53
+
+#
+# go get doesn't work with newer versions of go
+#
+env GOPATH=`pwd` go install github.com/barnybug/cli53/cmd/cli53@latest
+
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/cli53
 cp bin/cli53 /tmp/cs/usr/bin
-cp src/github.com/barnybug/cli53/README.md src/github.com/barnybug/cli53/LICENSE /tmp/cs/usr/share/cli53
+cp pkg/mod/github.com/barnybug/cli53@*/README.md pkg/mod/github.com/barnybug/cli53@*/LICENSE /tmp/cs/usr/share/cli53
 ${THOME}/build/create_pkg TRIBcli53 /tmp/cs
 rm -fr /tmp/cs
