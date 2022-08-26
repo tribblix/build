759c6e3d8e9376d9bf4cf17cb0a8691368e783ab
Peter Tribble <peter.tribble@gmail.com>
2022-08-26 13:26:17 +0100

    Update zipp 3.8.1

--- TRIBzipp-python-39/build.sh
+++ TRIBzipp-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack zipp-3.7.0
-cd zipp-3.7.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBzipp-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBzipp-python-39 zipp-3.8.1
