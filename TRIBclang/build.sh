6a6b64f005c992cd808f53d001a3754a871b9bad
Peter Tribble <peter.tribble@gmail.com>
2022-08-28 19:25:35 +0100

    Rebuild clang for gcc10 switch

--- TRIBclang/build.sh
+++ TRIBclang/build.sh
@@ -12,7 +12,7 @@ cd clang-14.0.1.src
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGCC_INSTALL_PREFIX=/usr/versions/gcc-7 -DCLANG_VENDOR="Tribblix" -DCLANG_DEFAULT_LINKER=/usr/bin/ld -DCLANG_DEFAULT_RTLIB=libgcc -DCLANG_DEFAULT_CXX_STDLIB=libstdc++ -DLLVM_DIR=/usr/versions/llvm/lib/cmake/llvm ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGCC_INSTALL_PREFIX=/usr/versions/gcc-10 -DCLANG_VENDOR="Tribblix" -DCLANG_DEFAULT_LINKER=/usr/bin/ld -DCLANG_DEFAULT_RTLIB=libgcc -DCLANG_DEFAULT_CXX_STDLIB=libstdc++ -DLLVM_DIR=/usr/versions/llvm/lib/cmake/llvm ..
 # this will take a while...
 # need to find the llvm libraries at build time
 env LD_LIBRARY_PATH=/usr/versions/llvm/lib gmake -j 6
