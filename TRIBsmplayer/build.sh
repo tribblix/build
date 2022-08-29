d8a318b3b538c54e2b8275d6e030ae357fdc0d93
Peter Tribble <peter.tribble@gmail.com>
2022-08-29 19:50:54 +0100

    Add smplayer

---
+++ TRIBsmplayer/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+#
+$THOME/build/unpack smplayer-22.2.0
+$THOME/build/unpack smplayer-skins-20.11.0
+$THOME/build/unpack smplayer-themes-20.11.0
+cd smplayer-skins-20.11.0
+sed -i 's:install :ginstall :' Makefile
+env PATH=/usr/versions/Qt-5/bin:$PATH gmake PREFIX=/usr CC=gcc
+cd ..
+cd smplayer-themes-20.11.0
+sed -i 's:install :ginstall :' Makefile
+env PATH=/usr/versions/Qt-5/bin:$PATH gmake PREFIX=/usr CC=gcc
+cd ..
+cd smplayer-22.2.0
+sed -i 's: make: gmake:' Makefile
+sed -i 's:install :ginstall :' Makefile
+sed -i 's:install :ginstall :' install_appdata.sh
+sed -i 's:tar :gtar :g' Makefile
+sed -i 's:-pthread:-lnsl -lsocket:' webserver/Makefile
+sed -i 's:Another OS:Tribblix:' src/smplayer.cpp
+env PATH=/usr/versions/Qt-5/bin:$PATH gmake -j 6 PREFIX=/usr CC=gcc
+cd ..
+${THOME}/build/genpkg -M PREFIX=/usr TRIBsmplayer smplayer-22.2.0 smplayer-skins-20.11.0 smplayer-themes-20.11.0
