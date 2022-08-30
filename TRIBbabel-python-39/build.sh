a24479178799e805180272e4492e16e973f5350a
Peter Tribble <peter.tribble@gmail.com>
2022-08-30 18:17:08 +0100

    Update Babel 2.10.3

--- TRIBbabel-python-39/build.sh
+++ TRIBbabel-python-39/build.sh
@@ -2,8 +2,8 @@
 #
 # needs pytz
 #
-${THOME}/build/unpack Babel-2.9.1
-cd Babel-2.9.1
+${THOME}/build/unpack Babel-2.10.3
+cd Babel-2.10.3
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
