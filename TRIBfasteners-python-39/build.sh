36999214b0f825fba4daa90d777f04d13a5ecd4a
Peter Tribble <peter.tribble@gmail.com>
2022-08-30 09:10:21 +0100

    Update fasteners 0.17.3

--- TRIBfasteners-python-39/build.sh
+++ TRIBfasteners-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack fasteners-0.16.3
-cd fasteners-0.16.3
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBfasteners-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBfasteners-python-39 fasteners-0.17.3
