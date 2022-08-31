50085a7e706eeb8f096f1dc5651ba6392aa734a5
Peter Tribble <peter.tribble@gmail.com>
2022-08-31 09:53:45 +0100

    Update psutil 5.9.1

--- TRIBpsutil-python-39/build.sh
+++ TRIBpsutil-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psutil-5.9.0
-cd psutil-5.9.0
+${THOME}/build/unpack psutil-5.9.1
+cd psutil-5.9.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
