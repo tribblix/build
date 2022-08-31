556460710bc77a3fc15d1c86bd22705f8ecfc0f9
Peter Tribble <peter.tribble@gmail.com>
2022-08-31 11:43:02 +0100

    Update showfont 1.0.6

--- TRIBx11-xfs-xfs-utilities/build.sh
+++ TRIBx11-xfs-xfs-utilities/build.sh
@@ -5,6 +5,6 @@
 #
 ${THOME}/build/dobuild -64only fslsfonts-1.0.5
 ${THOME}/build/dobuild -64only fstobdf-1.0.6
-${THOME}/build/dobuild -64only showfont-1.0.5
+${THOME}/build/dobuild -64only showfont-1.0.6
 ${THOME}/build/dobuild -64only xfsinfo-1.0.6
-${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.5 fstobdf-1.0.6 showfont-1.0.5 xfsinfo-1.0.6
+${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.5 fstobdf-1.0.6 showfont-1.0.6 xfsinfo-1.0.6
