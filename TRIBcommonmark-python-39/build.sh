874c10c881da292294b6a11d15f098fa13ddddbd
Peter Tribble <peter.tribble@gmail.com>
2022-09-01 14:06:55 +0100

    Update httpie 3.2.1, add rich and commonmark

--- TRIBcommonmark-python-39/build.sh
+++ TRIBcommonmark-python-39/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httpie-3.2.1
-cd httpie-3.2.1
+${THOME}/build/unpack commonmark-0.9.1
+cd commonmark-0.9.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBhttpie-python-39 /tmp/pil
+${THOME}/build/create_pkg TRIBcommonmark-python-39 /tmp/pil
 cd ..
 rm -fr /tmp/pil
--- TRIBrich-python-39/build.sh
+++ TRIBrich-python-39/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httpie-3.2.1
-cd httpie-3.2.1
+${THOME}/build/unpack rich-12.5.1
+cd rich-12.5.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBhttpie-python-39 /tmp/pil
+${THOME}/build/create_pkg TRIBrich-python-39 /tmp/pil
 cd ..
 rm -fr /tmp/pil
