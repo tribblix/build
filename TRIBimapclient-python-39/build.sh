26306f64b7ab61f3a2b32687e4257f3f5afc437c
Peter Tribble <peter.tribble@gmail.com>
2022-09-01 18:06:58 +0100

    Update imapclient 2.3.1

--- TRIBimapclient-python-39/build.sh
+++ TRIBimapclient-python-39/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack IMAPClient-2.2.0
-cd IMAPClient-2.2.0
+${THOME}/build/unpack IMAPClient-2.3.1
+cd IMAPClient-2.3.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
