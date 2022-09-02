9b522686cf83bd8201ee1d165e5a8a652b6f307d
Peter Tribble <peter.tribble@gmail.com>
2022-09-02 08:55:41 +0100

    Update libXau 1.0.10

--- TRIBx11-libxau/build.sh
+++ TRIBx11-libxau/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXau-1.0.9 -C "--enable-xthreads --with-pic"
-${THOME}/build/genpkg TRIBx11-libxau libXau-1.0.9
+${THOME}/build/dobuild -64 libXau-1.0.10 -C "--enable-xthreads --with-pic"
+${THOME}/build/genpkg TRIBx11-libxau libXau-1.0.10
