ffda00f306a2d3dd837c1c050f4249656256c928
Peter Tribble <peter.tribble@gmail.com>
2022-09-04 14:47:22 +0100

    Update cpuid 1.8.2

--- TRIBcpuid/build.sh
+++ TRIBcpuid/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild cpuid-1.6.4 -M prefix=/usr
-${THOME}/build/genpkg TRIBcpuid cpuid-1.6.4
+env EXTRA_CFLAGS=-m64 ${THOME}/build/dobuild cpuid-1.8.2
+env EXTRA_CFLAGS=-m64 ${THOME}/build/genpkg TRIBcpuid cpuid-1.8.2
