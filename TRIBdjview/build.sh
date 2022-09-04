8e3d3e20d43b803253b57deea4222c35c3ac65e9
Peter Tribble <peter.tribble@gmail.com>
2022-09-04 16:46:01 +0100

    Update djview 4.12, and use Qt5

--- TRIBdjview/build.sh
+++ TRIBdjview/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:/usr/versions/Qt-4.8/bin:$PATH ${THOME}/build/dobuild djview-4.10.6
-env PATH=/usr/gnu/bin:/usr/versions/Qt-4.8/bin:$PATH ${THOME}/build/genpkg TRIBdjview djview-4.10.6
+# the tarball needs a rename from djview-4.12.tar.gz to djview4-4.12.tar.gz
+#
+env PATH=/usr/gnu/bin:/usr/versions/Qt-5/bin:$PATH ${THOME}/build/dobuild djview4-4.12
+env PATH=/usr/gnu/bin:/usr/versions/Qt-5/bin:$PATH ${THOME}/build/genpkg TRIBdjview djview4-4.12
