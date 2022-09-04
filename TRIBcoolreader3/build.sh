546138cb92c88b480cd7d108f1a69a87c7e0832b
Peter Tribble <peter.tribble@gmail.com>
2022-09-04 21:23:15 +0100

    Update coolreader 3.2.58 (don't remove RPATH)

--- TRIBcoolreader3/build.sh
+++ TRIBcoolreader3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -B -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/cmake" coolreader-cr3.2.58
+${THOME}/build/cmbuild -B -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE" coolreader-cr3.2.58
 ${THOME}/build/genpkg TRIBcoolreader3 coolreader-cr3.2.58
