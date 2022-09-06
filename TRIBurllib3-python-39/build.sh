c880eeef9745d274d2bdd6bf6856e1b04d319e53
Peter Tribble <peter.tribble@gmail.com>
2022-09-06 08:48:54 +0100

    Update urllib3 1.26.12

--- TRIBurllib3-python-39/build.sh
+++ TRIBurllib3-python-39/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #
-${THOME}/build/unpack urllib3-1.26.9
-cd urllib3-1.26.9
+# the dependencies around [secure] will need looking at
+# we still declare them, but urllib3 shouldn't use them
+# see https://github.com/urllib3/urllib3/issues/2680
+#
+${THOME}/build/unpack urllib3-1.26.12
+cd urllib3-1.26.12
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
