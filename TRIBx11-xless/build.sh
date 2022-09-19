6877ea5e7b81b8bc670cd05f45b73a4e764cc40d
Peter Tribble <peter.tribble@gmail.com>
2022-09-19 09:19:26 +0100

    Rebuild XLess with modern patches, 64 bit, and removing deprecated libXp dependency

--- TRIBx11-xless/build.sh
+++ TRIBx11-xless/build.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 #
-# (modified /usr/lib/X11/config/site.def to add HasGcc3 YES)
+# source and patch from https://launchpad.net/ubuntu/+source/xless/1.7-14.4
+# modified the patch to change a few locations, but kept the code changes
+# the same as debian
 #
-${THOME}/build/unpack xless-1.7
-cd xless-1.7
-xmkmf -a
-gmake
-rm -fr /tmp/xl
-mkdir -p /tmp/xl/usr/bin /tmp/xl/usr/lib/X11/app-defaults /tmp/xl/usr/share/man/man1
-cp xless /tmp/xl/usr/bin
-cp xless.help /tmp/xl/usr/lib/X11
-cp XLess.ad /tmp/xl/usr/lib/X11/app-defaults/XLess
-cp XLess-co.ad /tmp/xl/usr/lib/X11/app-defaults/XLess-color
-cp xless._man /tmp/xl/usr/share/man/man1/xless.1
-${THOME}/build/create_pkg TRIBx11-xless /tmp/xl
-rm -fr /tmp/xl
+${THOME}/build/dobuild xless-1.7
+${THOME}/build/genpkg TRIBx11-xless xless-1.7
