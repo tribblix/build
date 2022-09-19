a26c91fa70449f69a992e1cd8598a7659a2ace8f
Peter Tribble <peter.tribble@gmail.com>
2022-09-19 21:04:29 +0100

    Update openconnect 8.20

--- TRIBopenconnect/build.sh
+++ TRIBopenconnect/build.sh
@@ -4,11 +4,5 @@
 # where it's going to be packaged.
 # And it needs tun/tap installed (the headers at least)
 #
-${THOME}/build/dobuild openconnect-6.00 -C --with-vpnc-script=/etc/vpnc/vpnc-script
-cd openconnect-6.00
-rm -fr /tmp/oc
-mkdir -p /tmp/oc/etc/vpnc
-cp ${THOME}/tarballs/vpnc-script /tmp/oc/etc/vpnc
-chmod 755 /tmp/oc/etc/vpnc/vpnc-script
-gmake install DESTDIR=/tmp/oc
-${THOME}/build/create_pkg TRIBopenconnect /tmp/oc
+${THOME}/build/dobuild openconnect-8.20 -C "--with-vpnc-script=/etc/vpnc/vpnc-script --disable-dependency-tracking --without-gnutls --with-system-cafile=/etc/ssl/cacert.pem"
+${THOME}/build/genpkg TRIBopenconnect openconnect-8.20
