ebd97e2db73341ebf56623b99e01dde692fd9a05
Peter Tribble <peter.tribble@gmail.com>
2022-09-19 20:33:49 +0100

    Deprecate ant 1.9

--- TRIBv-apache-ant-19/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# also needs the following in tarballs for fixit to deploy:
-# apache-ivy-2.4.0-bin-with-deps.zip
-# svnant-1.3.1.zip
-# jsch-0.1.55.jar
-#
-${THOME}/build/pkg_tarball TRIBv-apache-ant-19 apache-ant-1.9.16-bin
