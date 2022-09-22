b5b09cfe64529bbd0f9511ec699a6db9ebe86433
Peter Tribble <peter.tribble@gmail.com>
2022-09-22 21:13:16 +0100

    Update fastbencode 0.0.12

--- TRIBfastbencode-python-39/build.sh
+++ TRIBfastbencode-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack fastbencode-0.0.9
-cd fastbencode-0.0.9
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBfastbencode-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBfastbencode-python-39 fastbencode-0.0.12
