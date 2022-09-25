97e42a4ac2865e6273225e18b672e2b30c90ca7c
Peter Tribble <peter.tribble@gmail.com>
2022-09-25 10:25:36 +0100

    Update joblib 1.2.0

--- TRIBjoblib-python-39/build.sh
+++ TRIBjoblib-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack joblib-1.1.0
-cd joblib-1.1.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBjoblib-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBjoblib-python-39 joblib-1.2.0
