c1e2cea6c3d5134c08600e854fa99a2193e387c5
Peter Tribble <peter.tribble@gmail.com>
2022-09-26 21:32:23 +0100

    Bump ghc to 8.4.4

--- TRIBhaskell/build.sh
+++ TRIBhaskell/build.sh
@@ -21,33 +21,38 @@ exit 0
 # isn't good enough to ship, but it's good enough to bootstrap
 # the build. We unpack it and install it into /tmp/local
 #
-$THOME/build/unpack ghc-8.0.2-i386-unknown-solaris2
-cd ghc-8.0.2
-ln -s /usr/lib/libgmp.so ./libgmp.so.3
-ln -s /usr/gnu/lib/libncurses.so.5 .
-env LD_LIBRARY_PATH=`pwd` ./configure --prefix=/tmp/local
-env LD_LIBRARY_PATH=`pwd` gmake install
-cd ..
-rm -fr ghc-8.0.2
-cd /tmp/local/lib/ghc-8.0.2/rts
-ln -s /usr/lib/libgmp.so ./libgmp.so.3
-ln -s /usr/gnu/lib/libncurses.so.5 .
+#$THOME/build/unpack ghc-8.0.2-i386-unknown-solaris2
+#cd ghc-8.0.2
+#ln -s /usr/lib/libgmp.so ./libgmp.so.3
+#ln -s /usr/gnu/lib/libncurses.so.5 .
+#env LD_LIBRARY_PATH=`pwd` ./configure --prefix=/tmp/local
+#env LD_LIBRARY_PATH=`pwd` gmake install
+#cd ..
+#rm -fr ghc-8.0.2
+#cd /tmp/local/lib/ghc-8.0.2/rts
+#ln -s /usr/lib/libgmp.so ./libgmp.so.3
+#ln -s /usr/gnu/lib/libncurses.so.5 .
+
+#
+# subsequent builds will not need the bootstrap build installing
+# but there's a chain of updates
+# 8.4.4 can be built with 8.0.2
+#
 
 #
 # then we use that to do the build
 #
-$THOME/build/unpack ghc-8.0.2-src
-cd ghc-8.0.2
-env PATH=${PATH}:/tmp/local/bin ./configure --prefix=/usr/versions/haskell
-env PATH=${PATH}:/tmp/local/bin gmake -j 8
+$THOME/build/unpack ghc-8.4.4
+cd ghc-8.4.4
+./configure --prefix=/usr/versions/haskell
+gmake -j 8
 
 #
-# subsequent builds will not need the bootstrap build installing
-# or the PATH modifications
+# will take several hours...
 #
 
 #
 # and then we can create a package
 #
-env PATH=${PATH}:/tmp/local/bin $THOME/build/genpkg TRIBhaskell
+$THOME/build/genpkg TRIBhaskell
 cd ..
