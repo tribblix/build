7ba070b5ee009b582332e0dc8a4e169543c7a468
Peter Tribble <peter.tribble@gmail.com>
2022-09-27 17:48:12 +0100

    Update mkdocs 1.3.1

--- TRIBmkdocs-python-39/build.sh
+++ TRIBmkdocs-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack mkdocs-1.2.3
-cd mkdocs-1.2.3
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmkdocs-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBmkdocs-python-39 mkdocs-1.3.1
