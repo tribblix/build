e275e2dc0113d141068b266af184ae8fbca9485c
Peter Tribble <peter.tribble@gmail.com>
2022-09-27 09:11:04 +0100

    Update attrs 22.1.0

--- TRIBattrs-python-39/build.sh
+++ TRIBattrs-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack attrs-21.4.0
-cd attrs-21.4.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBattrs-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBattrs-python-39 attrs-22.1.0
