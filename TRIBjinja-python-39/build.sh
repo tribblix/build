a3028e8b492bdd0f7ea2d1d2c886e8301a67c440
Peter Tribble <peter.tribble@gmail.com>
2022-09-27 18:26:34 +0100

    Update Jinja2 3.1.2

--- TRIBjinja-python-39/build.sh
+++ TRIBjinja-python-39/build.sh
@@ -2,11 +2,4 @@
 #
 # needs Babel MarkupSafe
 #
-${THOME}/build/unpack Jinja2-2.11.3
-cd Jinja2-2.11.3
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBjinja-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBjinja-python-39 Jinja2-3.1.2
