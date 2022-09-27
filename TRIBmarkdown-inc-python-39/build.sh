4e783e94837e0e6c07795546ed9cdbb76a24e1c4
Peter Tribble <peter.tribble@gmail.com>
2022-09-27 11:58:10 +0100

    Update markdown 3.4.1 markdown-include 0.7.0

--- TRIBmarkdown-inc-python-39/build.sh
+++ TRIBmarkdown-inc-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack markdown-include-0.6.0
-cd markdown-include-0.6.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmarkdown-inc-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBmarkdown-inc-python-39 markdown-include-0.7.0
--- TRIBmarkdown-python-39/build.sh
+++ TRIBmarkdown-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Markdown-3.3.7
-cd Markdown-3.3.7
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmarkdown-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBmarkdown-python-39 Markdown-3.4.1
