e10c68093ce8970be469f54a1248c210ff84cd3d
Peter Tribble <peter.tribble@gmail.com>
2022-09-27 18:26:09 +0100

    Update MarkupSafe 2.1.1

--- TRIBmarkupsafe-python-39/build.sh
+++ TRIBmarkupsafe-python-39/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/unpack MarkupSafe-1.1.1
+${THOME}/build/pkg_setup_py TRIBmarkupsafe-python-39 MarkupSafe-2.1.1
 cd MarkupSafe-1.1.1
 /usr/versions/python-3.9/bin/python3 setup.py build
 rm -fr /tmp/pil
