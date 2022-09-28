a2947030de7703656d3c6972f1c9f59c703f9d5b
Peter Tribble <peter.tribble@gmail.com>
2022-09-28 11:28:42 +0100

    Fix markupsafe build

--- TRIBmarkupsafe-python-39/build.sh
+++ TRIBmarkupsafe-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
 ${THOME}/build/pkg_setup_py TRIBmarkupsafe-python-39 MarkupSafe-2.1.1
-cd MarkupSafe-1.1.1
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmarkupsafe-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
