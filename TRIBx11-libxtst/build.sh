9328f20eeeb67dc21cccade84a21aef7915e89a4
Peter Tribble <peter.tribble@gmail.com>
2022-09-29 21:17:27 +0100

    Update libXtst 1.2.4

--- TRIBx11-libxtst/build.sh
+++ TRIBx11-libxtst/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXtst-1.2.3
-${THOME}/build/genpkg TRIBx11-libxtst libXtst-1.2.3
+${THOME}/build/dobuild -64 libXtst-1.2.4
+${THOME}/build/genpkg TRIBx11-libxtst libXtst-1.2.4
