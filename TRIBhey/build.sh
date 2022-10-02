a3d1f91df83e7994069b2db387fb45d13280554d
Peter Tribble <peter.tribble@gmail.com>
2022-10-02 12:39:25 +0100

    Build hey with newer go

--- TRIBhey/build.sh
+++ TRIBhey/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-env GOPATH=`pwd` go get github.com/rakyll/hey
+env GOPATH=`pwd` go install github.com/rakyll/hey@latest
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/hey
 cp bin/hey /tmp/cs/usr/bin
-cp src/github.com/rakyll/hey/README.md src/github.com/rakyll/hey/LICENSE /tmp/cs/usr/share/hey
+cp pkg/mod/github.com/rakyll/hey@*/README.md pkg/mod/github.com/rakyll/hey@*/LICENSE /tmp/cs/usr/share/hey
 ${THOME}/build/create_pkg TRIBhey /tmp/cs
 rm -fr /tmp/cs
