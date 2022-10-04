765e5d66c6544be4bed560620effb2fa20943248
Peter Tribble <peter.tribble@gmail.com>
2022-10-04 16:54:09 +0100

    Update Django 3.2.16 4.0.8

--- TRIBdjango3-python-39/build.sh
+++ TRIBdjango3-python-39/build.sh
@@ -2,10 +2,4 @@
 #
 # 3.2 is an LTS release
 #
-${THOME}/build/unpack Django-3.2.15
-cd Django-3.2.15
-rm -fr /tmp/p3a
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/p3a
-${THOME}/build/create_pkg TRIBdjango3-python-39 /tmp/p3a
-cd ..
-rm -fr /tmp/p3a
+${THOME}/build/pkg_setup_py TRIBdjango3-python-39 Django-3.2.16
--- TRIBdjango4-python-39/build.sh
+++ TRIBdjango4-python-39/build.sh
@@ -2,11 +2,4 @@
 #
 # pytz is deprecated, but marked as a dependency for transitional purposes
 #
-${THOME}/build/unpack Django-4.0.7
-cd Django-4.0.7
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBdjango4-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBdjango4-python-39 Django-4.0.8
