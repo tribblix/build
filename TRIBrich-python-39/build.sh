ec74cec259814ef88ac0387e4a832842f649cc13
Peter Tribble <peter.tribble@gmail.com>
2022-10-05 08:07:29 +0100

    Update rich 12.6.0

--- TRIBrich-python-39/build.sh
+++ TRIBrich-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack rich-12.5.1
-cd rich-12.5.1
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBrich-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBrich-python-39 rich-12.6.0
