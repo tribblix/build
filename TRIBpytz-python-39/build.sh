20399682931eda91ff438abf4943d141821cf95d
Peter Tribble <peter.tribble@gmail.com>
2022-10-08 19:28:57 +0100

    Update pytz 2022.4

--- TRIBpytz-python-39/build.sh
+++ TRIBpytz-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pytz-2022.2.1
-cd pytz-2022.2.1
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpytz-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBpytz-python-39 pytz-2022.4
