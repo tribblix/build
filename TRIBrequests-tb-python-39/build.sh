60e48959106025a28dcc50cca603820febdd991d
Peter Tribble <peter.tribble@gmail.com>
2022-10-12 13:29:42 +0100

    Update requests-toolbelt 0.10.0

--- TRIBrequests-tb-python-39/build.sh
+++ TRIBrequests-tb-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack requests-toolbelt-0.9.1
-cd requests-toolbelt-0.9.1
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBrequests-tb-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBrequests-tb-python-39 requests-toolbelt-0.10.0
