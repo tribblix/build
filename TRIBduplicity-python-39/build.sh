beeb6dcc72c6f6c3425e525f168876a8a629547b
Peter Tribble <peter.tribble@gmail.com>
2022-10-16 19:54:51 +0100

    Update duplicity 1.0.1

--- TRIBduplicity-python-39/build.sh
+++ TRIBduplicity-python-39/build.sh
@@ -6,11 +6,4 @@
 # other optional packages it can leverage that aren't listed as dependencies
 # rsync rclone ncftp
 #
-${THOME}/build/unpack duplicity-0.8.23
-cd duplicity-0.8.23
-env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-env PATH=/usr/gnu/bin:$PATH /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBduplicity-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/pkg_setup_py TRIBduplicity-python-39 duplicity-1.0.1
