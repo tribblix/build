8a31e698973c62ee034aa48acf7bdad9c9fb4509
Peter Tribble <peter.tribble@gmail.com>
2022-10-16 11:52:07 +0100

    Package wfuzz for python3.10

---
+++ TRIBchardet-python-310/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack chardet-4.0.0
+cd chardet-4.0.0
+/usr/versions/python-3.10/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBchardet-python-310 /tmp/pil
+cd ..
+rm -fr /tmp/pil
