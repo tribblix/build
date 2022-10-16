421e0adb239b53f1f4a9c1245a0bf9268b1d9330
Peter Tribble <peter.tribble@gmail.com>
2022-10-16 14:12:09 +0100

    Update docutils 0.16

--- TRIBdocutils-python-39/build.sh
+++ TRIBdocutils-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack docutils-0.15.2
-cd docutils-0.15.2
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pdu
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
-${THOME}/build/create_pkg TRIBdocutils-python-39 /tmp/pdu
-cd ..
-rm -fr /tmp/pdu
+${THOME}/build/pkg_setup_py TRIBdocutils-python-39 docutils-0.16
