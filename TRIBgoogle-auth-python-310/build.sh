2c369944f56dcae095fd963e462b463f4a66c38f
Peter Tribble <peter.tribble@gmail.com>
2022-10-18 08:17:25 +0100

    Update google-auth 2.13.0

--- TRIBgoogle-auth-python-310/build.sh
+++ TRIBgoogle-auth-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-310 google-auth-2.12.0
+${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-310 google-auth-2.13.0
--- TRIBgoogle-auth-python-39/build.sh
+++ TRIBgoogle-auth-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-39 google-auth-2.12.0
+${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-39 google-auth-2.13.0
