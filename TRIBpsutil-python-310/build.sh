3b51878e125729d0044ebc73741c51c5c7b06daf
Peter Tribble <peter.tribble@gmail.com>
2022-10-19 15:08:04 +0100

    Update psutil 5.9.3

--- TRIBpsutil-python-310/build.sh
+++ TRIBpsutil-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psutil-5.9.2
-cd psutil-5.9.2
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpsutil-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBpsutil-python-310 psutil-5.9.3
--- TRIBpsutil-python-39/build.sh
+++ TRIBpsutil-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack psutil-5.9.2
-cd psutil-5.9.2
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpsutil-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBpsutil-python-39 psutil-5.9.3
