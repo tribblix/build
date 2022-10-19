24201b31f2de5ff391cde0dd860989a57ac4ff3a
Peter Tribble <peter.tribble@gmail.com>
2022-10-19 11:32:10 +0100

    Update google-api-python-client 2.65.0

--- TRIBgoogle-api-python-310/build.sh
+++ TRIBgoogle-api-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-api-python-310 google-api-python-client-2.64.0
+${THOME}/build/pkg_setup_py TRIBgoogle-api-python-310 google-api-python-client-2.65.0
