fb1cfe1f9a0605e7d22c2e9eaead302eadea49ae
Peter Tribble <peter.tribble@gmail.com>
2022-10-19 20:14:51 +0100

    Update nginx 1.22.1

--- TRIBblix-nginx/build.sh
+++ TRIBblix-nginx/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
+# it's not a standard configure, so a 64-bit dobuild doesn't work as
+# it sets --bindir which isn't recognized
 #
+# while the build would pick up -m64 for CFLAGS, it doesn't pick
+# it up for LDFLAGS so the link fails; hence we tell it directly
+# via $CC (and don't use --with-cc any more)
 #
-${THOME}/build/dobuild nginx-1.20.2 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-cc=gcc --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module"
-${THOME}/build/genpkg TRIBblix-nginx nginx-1.20.2
+env CC="gcc -m64" ${THOME}/build/dobuild nginx-1.22.1 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module"
+${THOME}/build/genpkg TRIBblix-nginx nginx-1.22.1
