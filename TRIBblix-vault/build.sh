d9f5bea09829b38428bf70d091be21cb6367e1bb
Peter Tribble <peter.tribble@gmail.com>
2022-10-24 21:02:04 +0100

    Update vault v1.12.0

--- TRIBblix-vault/build.sh
+++ TRIBblix-vault/build.sh
@@ -1,16 +1,16 @@
-#!/bin/tcsh
+#!/bin/sh
 #
-# (requires go 1.9)
+# (requires go 1.19)
 #
 mkdir ~/ud/v
 cd ~/ud/v
-setenv GOPATH `pwd`
-cd $GOPATH/src/github.com/hashicorp
 git clone https://github.com/hashicorp/vault.git
 cd vault
+git checkout v1.12.0
+
 gmake bootstrap
-# needs gnu grep
-env PATH=/usr/gnu/bin:$PATH gmake dev
+# needs gnu grep for the version check to work
+env PATH=/usr/versions/go-1.19/bin:/usr/gnu/bin:$PATH gmake
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/vault/bin
