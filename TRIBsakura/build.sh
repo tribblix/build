258d4ada2099b507d6fce09052232b7a965d492c
Peter Tribble <peter.tribble@gmail.com>
2022-10-24 22:29:24 +0100

    Build sakura 64-bit

--- TRIBsakura/build.sh
+++ TRIBsakura/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack sakura-3.8.5
-cd sakura-3.8.5
-# path for pod2man to generate the man page
-env PATH=/usr/gnu/bin:${PATH}:/usr/perl5/bin cmake -DCMAKE_INSTALL_PREFIX=/usr .
-env PATH=/usr/gnu/bin:$PATH gmake -j 4
-${THOME}/build/genpkg TRIBsakura
-
+env PATH=/usr/gnu/bin:${PATH}:/usr/perl5/bin ${THOME}/build/cmbuild -64only sakura-3.8.5
+${THOME}/build/genpkg TRIBsakura sakura-3.8.5
