afae5bed47a43fc87fa992f1abf95255a82c585f
Peter Tribble <peter.tribble@gmail.com>
2022-10-25 09:14:22 +0100

    Update colorama 0.4.6

--- TRIBcolorama-python-39/build.sh
+++ TRIBcolorama-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack colorama-0.4.5
-cd colorama-0.4.5
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pdu
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pdu
-${THOME}/build/create_pkg TRIBcolorama-python-39 /tmp/pdu
-cd ..
-rm -fr /tmp/pdu
+${THOME}/build/pkg_pep518 TRIBcolorama-python-39 colorama-0.4.6
