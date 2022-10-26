d439e9bf483a831d479b7f713c99e4f83b761a2a
Peter Tribble <peter.tribble@gmail.com>
2022-10-26 10:07:01 +0100

    Update curio 1.6

--- TRIBcurio-python-39/build.sh
+++ TRIBcurio-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack curio-1.5
-cd curio-1.5
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBcurio-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBcurio-python-39 curio-1.6
