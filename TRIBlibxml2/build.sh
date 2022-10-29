59456a93f5e8772f3c7e86dd23640d35b2d53e38
Peter Tribble <peter.tribble@gmail.com>
2022-10-29 13:49:00 +0100

    Update libxml2 2.9.14, remove python2, add python3.10, and clean up

--- TRIBlibxml2/build.sh
+++ TRIBlibxml2/build.sh
@@ -4,25 +4,25 @@
 #
 # ***MUST have libtool installed or -nostdlib won't take***
 #
-env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.9 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
-${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.9
-
-# we need this for gnome-doc-utils
-rm -fr /tmp/lxp
-cd libxml2-2.9.9/python
-python2.7 setup.py build
-python2.7 setup.py install --root=/tmp/lxp
-${THOME}/build/create_pkg TRIBlibxml2-python-27 /tmp/lxp
-python2.7 setup.py clean
-cd ../..
+env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.14 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
+${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.14
 
 # python3.9 is 64-bit
 rm -fr /tmp/lxp
-cd libxml2-2.9.9-64bit/python
+cd libxml2-2.9.14-64bit/python
 /usr/versions/python-3.9/bin/python3 setup.py build
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/lxp
 ${THOME}/build/create_pkg TRIBlibxml2-python-39 /tmp/lxp
 /usr/versions/python-3.9/bin/python3 setup.py clean
+cd ../..
+
+# python3.10 is 64-bit
+rm -fr /tmp/lxp
+cd libxml2-2.9.14-64bit/python
+/usr/versions/python-3.10/bin/python3 setup.py build
+/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/lxp
+${THOME}/build/create_pkg TRIBlibxml2-python-310 /tmp/lxp
+/usr/versions/python-3.10/bin/python3 setup.py clean
 cd ../..
 
 rm -fr /tmp/lxp
--- patches/libxml2-2.9.14.pls
+++ patches/libxml2-2.9.14.pls
@@ -1,4 +1,2 @@
 -p1 libxml2.test_errors_name2.patch
 -p1 libxml2.mapfile.patch
--p1 libxml2-pthread.patch
--p1 libxml2-python39.patch
