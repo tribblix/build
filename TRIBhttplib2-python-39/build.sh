84a4b5fccdc81f0425dd5dc04a23b25fafadbdea
Peter Tribble <peter.tribble@gmail.com>
2022-10-30 20:12:49 +0000

    Update httplib2 0.21.0

--- TRIBhttplib2-python-39/build.sh
+++ TRIBhttplib2-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack httplib2-0.20.4
-cd httplib2-0.20.4
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBhttplib2-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBhttplib2-python-39 httplib2-0.21.0
