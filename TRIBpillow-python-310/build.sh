f00b211a6406f505c9dda8638748b3cd66836afc
Peter Tribble <peter.tribble@gmail.com>
2022-10-31 17:40:53 +0000

    Update Pillow 9.3.0

--- TRIBpillow-python-310/build.sh
+++ TRIBpillow-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpillow-python-310 Pillow-9.2.0
+${THOME}/build/pkg_setup_py TRIBpillow-python-310 Pillow-9.3.0
--- TRIBpillow-python-39/build.sh
+++ TRIBpillow-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpillow-python-39 Pillow-9.2.0
+${THOME}/build/pkg_setup_py TRIBpillow-python-39 Pillow-9.3.0
