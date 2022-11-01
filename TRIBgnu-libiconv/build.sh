dc2e6cd9cb3b89faea25bc3c954d91d25c6844aa
Peter Tribble <peter.tribble@gmail.com>
2022-11-01 11:30:58 +0000

    Update gnu libiconv 1.17

--- TRIBgnu-libiconv/build.sh
+++ TRIBgnu-libiconv/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libiconv-1.16 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-libiconv libiconv-1.16
+${THOME}/build/dobuild -64 libiconv-1.17 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-libiconv libiconv-1.17
