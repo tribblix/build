f2ce07b4f83ec7a02ae6bcd5fa06edca234c2d0e
Peter Tribble <peter.tribble@gmail.com>
2022-11-04 13:52:20 +0000

    Repackage ant to update ivy to 2.5.1

--- TRIBv-apache-ant-110/build.sh
+++ TRIBv-apache-ant-110/build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #
 # also needs the following in tarballs for fixit to deploy:
-# apache-ivy-2.5.0-bin-with-deps.zip
+# apache-ivy-2.5.1-bin-with-deps.zip
 # svnant-1.3.1.zip
-# (jsch-0.1.55.jar - now supplied via ivy)
 #
 ${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.12-bin
