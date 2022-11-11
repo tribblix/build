20d5d3cb8e39c752c992a42c42d5105f1b1c169d
Peter Tribble <peter.tribble@gmail.com>
2022-11-11 16:18:23 +0000

    Update postgresql 11.18 13.9 14.6 15.1

--- TRIBblix-postgres11/build.sh
+++ TRIBblix-postgres11/build.sh
@@ -4,5 +4,5 @@
 #
 # now without perl as we're 32-bit but perl is now 64-bit
 #
-${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.17 -C "--without-readline --without-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.17
+${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.18 -C "--without-readline --without-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.18
