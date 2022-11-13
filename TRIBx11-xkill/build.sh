cb2ce34d3d31d141218b74d431123749cb67940f
Peter Tribble <peter.tribble@gmail.com>
2022-11-13 21:37:32 +0000

    Update xkill 1.0.6

--- TRIBx11-xkill/build.sh
+++ TRIBx11-xkill/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xkill-1.0.5
-${THOME}/build/genpkg TRIBx11-xkill xkill-1.0.5
+${THOME}/build/dobuild -64only xkill-1.0.6
+${THOME}/build/genpkg TRIBx11-xkill xkill-1.0.6
