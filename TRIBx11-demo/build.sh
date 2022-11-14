8328c4bff25073717b533356db759e95fcecd414
Peter Tribble <peter.tribble@gmail.com>
2022-11-14 21:39:38 +0000

    Update xgc 1.0.6 xlogo 1.0.6

--- TRIBx11-demo/build.sh
+++ TRIBx11-demo/build.sh
@@ -2,6 +2,6 @@
 #
 ${THOME}/build/dobuild -64only ico-1.0.6
 ${THOME}/build/dobuild -64only xeyes-1.2.0
-${THOME}/build/dobuild -64only xgc-1.0.5
-${THOME}/build/dobuild -64only xlogo-1.0.5
-${THOME}/build/genpkg TRIBx11-demo ico-1.0.6 xeyes-1.2.0 xgc-1.0.5 xlogo-1.0.5
+${THOME}/build/dobuild -64only xgc-1.0.6
+${THOME}/build/dobuild -64only xlogo-1.0.6
+${THOME}/build/genpkg TRIBx11-demo ico-1.0.6 xeyes-1.2.0 xgc-1.0.6 xlogo-1.0.6
