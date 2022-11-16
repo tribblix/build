327482653d14fc7ab3c64dbbc83610ce0072f428
Peter Tribble <peter.tribble@gmail.com>
2022-11-16 10:00:03 +0000

    Update python msgpack 1.0.4

--- TRIBmsgpack-python-310/build.sh
+++ TRIBmsgpack-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack msgpack-0.6.2
-cd msgpack-0.6.2
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmsgpack-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBmsgpack-python-310 msgpack-1.0.4
