3a6b5d6e61bc8b1f1a18fd27fea5b70c5356664e
Peter Tribble <peter.tribble@gmail.com>
2022-11-16 09:14:27 +0000

    Update googleapis-common-protos 1.57.0

--- TRIBgapi-common-python-310/build.sh
+++ TRIBgapi-common-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgapi-common-python-310 googleapis-common-protos-1.56.4
+${THOME}/build/pkg_setup_py TRIBgapi-common-python-310 googleapis-common-protos-1.57.0
