d085de81580efe928ecc43cea9ad0a2489fd50d0
Peter Tribble <peter.tribble@gmail.com>
2022-11-19 20:54:47 +0000

    Update xfce4-places-plugin 1.8.3

--- TRIBxfce-places-plugin/build.sh
+++ TRIBxfce-places-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-places-plugin-1.8.2
-${THOME}/build/genpkg TRIBxfce-places-plugin xfce4-places-plugin-1.8.2
+${THOME}/build/dobuild -gnu xfce4-places-plugin-1.8.3
+${THOME}/build/genpkg TRIBxfce-places-plugin xfce4-places-plugin-1.8.3
