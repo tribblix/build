58cc1f1872c2a54d7113d4109b24093ebb6496d7
Peter Tribble <peter.tribble@gmail.com>
2022-11-20 14:39:04 +0000

    Update xfce4-screenshooter 1.10.0

--- TRIBxfce-screenshooter/build.sh
+++ TRIBxfce-screenshooter/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-screenshooter-1.9.7
-${THOME}/build/genpkg TRIBxfce-screenshooter xfce4-screenshooter-1.9.7
+${THOME}/build/dobuild -gnu -64only xfce4-screenshooter-1.10.0
+${THOME}/build/genpkg TRIBxfce-screenshooter xfce4-screenshooter-1.10.0
