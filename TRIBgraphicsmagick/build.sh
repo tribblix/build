97ac06c2ac9dbef7ccaccdc983126ec3b96b9db7
Peter Tribble <peter.tribble@gmail.com>
2022-11-23 21:20:44 +0000

    Update GraphicsMagick 1.3.38

--- TRIBgraphicsmagick/build.sh
+++ TRIBgraphicsmagick/build.sh
@@ -3,5 +3,5 @@
 zap uninstall TRIBgraphviz
 zap install openjpeg
 # assumes a different webp than we have
-${THOME}/build/dobuild GraphicsMagick-1.3.32 -C "--without-webp --enable-shared --disable-static"
-${THOME}/build/genpkg TRIBgraphicsmagick GraphicsMagick-1.3.32
+${THOME}/build/dobuild -64 GraphicsMagick-1.3.38 -C "--without-webp --enable-shared --disable-static --disable-openmp"
+${THOME}/build/genpkg TRIBgraphicsmagick GraphicsMagick-1.3.38
