fafd8dfa726b72088f38cc2b9931d7e3b306e4a0
Peter Tribble <peter.tribble@gmail.com>
2022-11-24 11:36:12 +0000

    Update libXrandr 1.5.3

--- TRIBx11-libxrandr/build.sh
+++ TRIBx11-libxrandr/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXrandr-1.5.2
-${THOME}/build/genpkg TRIBx11-libxrandr libXrandr-1.5.2
+${THOME}/build/dobuild -64 libXrandr-1.5.3
+${THOME}/build/genpkg TRIBx11-libxrandr libXrandr-1.5.3
