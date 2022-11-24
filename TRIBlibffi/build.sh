bb982457f41b72d1b7495b93c8cfb868809adcc9
Peter Tribble <peter.tribble@gmail.com>
2022-11-24 20:23:26 +0000

    Update libffi 3.4.4

--- TRIBlibffi/build.sh
+++ TRIBlibffi/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
+# the SONAME gets bumped, so ship the old version as well for binary
+# compatibility
+#
 ${THOME}/build/dobuild -64 libffi-3.2.1
-${THOME}/build/genpkg TRIBlibffi libffi-3.2.1
+${THOME}/build/dobuild -64 libffi-3.4.4 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBlibffi libffi-3.2.1 libffi-3.4.4
---
+++ patches/ffi.h
@@ -0,0 +1,5 @@
+#if defined(_LP64) || defined (__amd64) || defined (__sparcv9)
+#include <ffi-64.h>
+#else
+#include <ffi-32.h>
+#endif
