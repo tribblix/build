69d0f1a4360a8b85ea40e2863dd1af07df24fedb
Peter Tribble <peter.tribble@gmail.com>
2022-11-28 17:39:47 +0000

    Update Tcl/Tk 8.6.13

--- TRIBtcl/build.sh
+++ TRIBtcl/build.sh
@@ -3,20 +3,20 @@
 # build tcl and tk together
 #
 rm -fr /tmp/tk1 /tmp/tc1
-${THOME}/build/unpack tcl8.6.10-src
-cd tcl8.6.10/unix
+${THOME}/build/unpack tcl8.6.13-src
+cd tcl8.6.13/unix
 env CFLAGS=-O ./configure --prefix=/usr
-env AWK=nawk gmake -j 4
+gmake -j 4
 env LD_LIBRARY_PATH=/tmp/tc1/usr/lib gmake install DESTDIR=/tmp/tc1
 
 cd ../..
 
 mkdir -p 64bit
 cd 64bit
-${THOME}/build/unpack tcl8.6.10-src
-cd tcl8.6.10/unix
+${THOME}/build/unpack tcl8.6.13-src
+cd tcl8.6.13/unix
 env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/`isainfo -k` --libdir=/usr/lib/`isainfo -k`
-env AWK=nawk gmake -j 4
+gmake -j 4
 env LD_LIBRARY_PATH=/tmp/tc1/usr/lib gmake install DESTDIR=/tmp/tc1
 
 cd ../../..
--- TRIBtk/build.sh
+++ TRIBtk/build.sh
@@ -2,8 +2,8 @@
 #
 # build tcl and tk together
 #
-${THOME}/build/unpack tk8.6.10-src
-cd tk8.6.10/unix
+${THOME}/build/unpack tk8.6.13-src
+cd tk8.6.13/unix
 env CFLAGS=-O ./configure --prefix=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
@@ -12,13 +12,14 @@ cd ../..
 
 mkdir -p 64bit
 cd 64bit
-${THOME}/build/unpack tk8.6.10-src
-cd tk8.6.10/unix
+${THOME}/build/unpack tk8.6.13-src
+cd tk8.6.13/unix
 env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/`isainfo -k` --libdir=/usr/lib/`isainfo -k`
 gmake -j 4
 gmake install DESTDIR=/tmp/tk1
-${THOME}/build/create_pkg TRIBtk /tmp/tk1
 
 cd ../../..
+
+${THOME}/build/create_pkg TRIBtk /tmp/tk1
 
 rm -fr /tmp/tk1 /tmp/tc1
