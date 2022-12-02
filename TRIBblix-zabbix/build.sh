3cc905df44818775f6b237407b3e0385d7370d87
Peter Tribble <peter.tribble@gmail.com>
2022-12-02 17:15:32 +0000

    Update Zabbix 6.0.11

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 # we need a temporary copy of libevent
-# avoid tying it to the normal preconf as it's only relevant for the server
+# avoid tying it to the normal preconf as it's not relevant for the agent
 #
 ${THOME}/build/patches/zabbix-server.preconf
-${THOME}/build/dobuild -64only zabbix-4.0.44 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres13/bin/pg_config --with-libpcre-include=/usr/include/pcre --with-libevent=/tmp/mc"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-4.0.44
+${THOME}/build/dobuild -64only zabbix-6.0.11 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres13/bin/pg_config --with-libpcre-include=/usr/include/pcre --with-libevent=/tmp/mc"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-6.0.11
 rm -fr /tmp/mc
--- TRIBzabbix-proxy/build.sh
+++ TRIBzabbix-proxy/build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only zabbix-4.0.44 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl --with-libpcre-include=/usr/include/pcre"
-${THOME}/build/genpkg TRIBzabbix-proxy zabbix-4.0.44
+# we need a temporary copy of libevent
+# avoid tying it to the normal preconf as it's not relevant for the agent
+#
+${THOME}/build/patches/zabbix-server.preconf
+${THOME}/build/dobuild -64only zabbix-6.0.11 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-proxy --with-sqlite3 --with-libcurl --with-libpcre-include=/usr/include/pcre --with-libevent=/tmp/mc"
+${THOME}/build/genpkg TRIBzabbix-proxy zabbix-6.0.11
+rm -fr /tmp/mc
