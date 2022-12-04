e61de0550d91e4a9bad362d4e9b4d093ea90d185
Peter Tribble <peter.tribble@gmail.com>
2022-12-04 09:36:17 +0000

    Update xfd 1.1.4

--- TRIBx11-xfd/build.sh
+++ TRIBx11-xfd/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xfd-1.1.3
-${THOME}/build/genpkg TRIBx11-xfd xfd-1.1.3
+${THOME}/build/dobuild -64only xfd-1.1.4
+${THOME}/build/genpkg TRIBx11-xfd xfd-1.1.4
