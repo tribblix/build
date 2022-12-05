2dfec10a30ed9dbe1d617241b3b10b670110e16d
Peter Tribble <peter.tribble@gmail.com>
2022-12-05 08:06:30 +0000

    Update multidict 6.0.3

--- TRIBmultidict-python-310/build.sh
+++ TRIBmultidict-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack multidict-6.0.2
-cd multidict-6.0.2
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBmultidict-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBmultidict-python-310 multidict-6.0.3
