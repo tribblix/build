8e7ecba6f5eba797d75d48a10f47a5db4dd24837
Peter Tribble <peter.tribble@gmail.com>
2022-12-07 21:38:13 +0000

    Update jtreg

--- TRIBjtreg/build.sh
+++ TRIBjtreg/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 
-hg clone https://hg.openjdk.java.net/code-tools/jtreg
+git clone https://github.com/openjdk/jtreg
 cd jtreg
-env PATH=/usr/gnu/bin:$PATH sh make/build-all.sh /usr/jdk/instances/jdk1.8.0
+env PATH=/usr/gnu/bin:$PATH bash make/build.sh --jdk /usr/jdk/instances/jdk11
 
 rm -fr /tmp/jtb
 mkdir -p /tmp/jtb/usr/versions
