a59e8e2274b225d0f2b831e9cd939d0b7bffa3d9
Peter Tribble <peter.tribble@gmail.com>
2022-12-10 14:15:26 +0000

    Update watchdog 2.2.0

--- TRIBwatchdog-python-39/build.sh
+++ TRIBwatchdog-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack watchdog-2.1.9
-cd watchdog-2.1.9
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBwatchdog-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBwatchdog-python-39 watchdog-2.2.0
