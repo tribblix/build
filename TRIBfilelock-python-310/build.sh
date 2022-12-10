eee9d44fcf9e4f2a7e97d6643a83e27feec40ac2
Peter Tribble <peter.tribble@gmail.com>
2022-12-10 14:05:35 +0000

    Update filelock 3.8.2

--- TRIBfilelock-python-310/build.sh
+++ TRIBfilelock-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack filelock-3.8.0
-cd filelock-3.8.0
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBfilelock-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBfilelock-python-310 filelock-3.8.2
