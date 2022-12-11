10f535ac409983ade8d2545a2046e6079b20f3f7
Peter Tribble <peter.tribble@gmail.com>
2022-12-11 12:04:04 +0000

    Update urllib3 1.26.13

--- TRIBurllib3-python-39/build.sh
+++ TRIBurllib3-python-39/build.sh
@@ -4,11 +4,4 @@
 # we still declare them, but urllib3 shouldn't use them
 # see https://github.com/urllib3/urllib3/issues/2680
 #
-${THOME}/build/unpack urllib3-1.26.12
-cd urllib3-1.26.12
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBurllib3-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBurllib3-python-39 urllib3-1.26.13
