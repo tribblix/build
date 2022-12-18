702cc333faa45b5931e92e1883321cb6de49e0e2
Peter Tribble <peter.tribble@gmail.com>
2022-12-18 13:08:08 +0000

    Add hurl

---
+++ TRIBhurl/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+
+#
+git clone https://github.com/Orange-OpenSource/hurl
+cd hurl
+cargo build --release
+
+rm -fr /tmp/ll
+
+mkdir -p /tmp/ll/usr/bin
+ginstall -s -Dm755 ./target/release/hurl -t /tmp/ll/usr/bin
+
+mkdir -p /tmp/ll/usr/share/hurl
+cp LICENSE README.md /tmp/ll/usr/share/hurl
+
+mkdir -p /tmp/ll/usr/share/man/man1
+cp docs/manual/*.1 /tmp/ll/usr/share/man/man1
+
+${THOME}/build/create_pkg TRIBhurl /tmp/ll
+rm -fr /tmp/ll
