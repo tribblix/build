64776295076eba2348be0123b2efbb95817e32e6
Peter Tribble <peter.tribble@gmail.com>
2022-12-18 11:40:24 +0000

    Update repmgr 5.3.3, and build for postgres 15

--- TRIBblix-repmgr11/build.sh
+++ TRIBblix-repmgr11/build.sh
@@ -2,5 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-${THOME}/build/dobuild repmgr-5.3.2 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr11 repmgr-5.3.2
+${THOME}/build/dobuild repmgr-5.3.3 -C PG_CONFIG=/opt/tribblix/postgres11/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr11 repmgr-5.3.3
--- TRIBblix-repmgr13/build.sh
+++ TRIBblix-repmgr13/build.sh
@@ -2,5 +2,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-${THOME}/build/dobuild +64only repmgr-5.3.2 -C PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr13 repmgr-5.3.2
+${THOME}/build/dobuild +64only repmgr-5.3.3 -C PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config
+${THOME}/build/genpkg TRIBblix-repmgr13 repmgr-5.3.3
