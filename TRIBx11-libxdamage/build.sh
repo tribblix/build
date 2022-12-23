d3d48a39780f2f3d6bcbbc51d75ced60910fb0b6
Peter Tribble <peter.tribble@gmail.com>
2022-12-23 18:29:38 +0000

    Update libXdamage 1.1.6

--- TRIBx11-libxdamage/build.sh
+++ TRIBx11-libxdamage/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXdamage-1.1.5
-${THOME}/build/genpkg TRIBx11-libxdamage libXdamage-1.1.5
+${THOME}/build/dobuild -64 libXdamage-1.1.6
+${THOME}/build/genpkg TRIBx11-libxdamage libXdamage-1.1.6
