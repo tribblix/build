0d431df4d6b6ae3b259b4307d696cc94431eadb6
Peter Tribble <peter.tribble@gmail.com>
2022-12-23 20:15:37 +0000

    Update libXres 1.2.2

--- TRIBx11-libxres/build.sh
+++ TRIBx11-libxres/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXres-1.2.1
-${THOME}/build/genpkg TRIBx11-libxres libXres-1.2.1
+${THOME}/build/dobuild -64 libXres-1.2.2
+${THOME}/build/genpkg TRIBx11-libxres libXres-1.2.2
