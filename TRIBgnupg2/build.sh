a44a7c7f2c9c0d8469ae8174d98d89ab9fc0026c
Peter Tribble <peter.tribble@gmail.com>
2022-12-24 21:27:55 +0000

    Update gnupg 2.2.41

--- TRIBgnupg2/build.sh
+++ TRIBgnupg2/build.sh
@@ -4,7 +4,5 @@
 #
 # has a postconf script
 #
-# there's a patch for 2.2.40 that will not be needed for later versions
-#
-${THOME}/build/dobuild gnupg-2.2.40 -64only -C "--disable-ldap --enable-gpg-is-gpg2"
-${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.40
+${THOME}/build/dobuild gnupg-2.2.41 -64only -C "--disable-ldap --enable-gpg-is-gpg2"
+${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.41
