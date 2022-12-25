f7de632a7281da33ef941d2f3b191a5678ddd454
Peter Tribble <peter.tribble@gmail.com>
2022-12-25 20:53:27 +0000

    Update gnumeric 1.12.53

--- TRIBgnumeric/build.sh
+++ TRIBgnumeric/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu gnumeric-1.12.52
-${THOME}/build/genpkg TRIBgnumeric gnumeric-1.12.52
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu gnumeric-1.12.53
+${THOME}/build/genpkg TRIBgnumeric gnumeric-1.12.53
