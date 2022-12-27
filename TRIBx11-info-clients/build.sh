7bef51a64e40190f64712a746798d7005f8c9d22
Peter Tribble <peter.tribble@gmail.com>
2022-12-27 16:52:49 +0000

    Update xdriinfo 1.0.7

--- TRIBx11-info-clients/build.sh
+++ TRIBx11-info-clients/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
 ${THOME}/build/dobuild -64only xdpyinfo-1.3.3
-${THOME}/build/dobuild -64only xdriinfo-1.0.6
+${THOME}/build/dobuild -64only xdriinfo-1.0.7
 ${THOME}/build/dobuild -64only xlsatoms-1.1.4
 ${THOME}/build/dobuild -64only xlsclients-1.1.5
 ${THOME}/build/dobuild -64only xlsfonts-1.0.7
 ${THOME}/build/dobuild -64only xvinfo-1.1.5
 ${THOME}/build/dobuild -64only xwininfo-1.1.5
-${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.3.3 xdriinfo-1.0.6 xlsatoms-1.1.4 xlsclients-1.1.5 xlsfonts-1.0.7 xvinfo-1.1.5 xwininfo-1.1.5
+${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.3.3 xdriinfo-1.0.7 xlsatoms-1.1.4 xlsclients-1.1.5 xlsfonts-1.0.7 xvinfo-1.1.5 xwininfo-1.1.5
