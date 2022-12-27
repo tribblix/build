8640940f9dc46700c139543a7084a7e630f86007
Peter Tribble <peter.tribble@gmail.com>
2022-12-27 20:30:16 +0000

    Update pgpool 4.2.11

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,5 @@
 #
 # needs a postconf patch
 #
-env PATH=/opt/tribblix/postgres13/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.2.10 -C "--sysconfdir=/etc --with-openssl"
-${THOME}/build/genpkg TRIBpgpool pgpool-II-4.2.10
+env PATH=/opt/tribblix/postgres13/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.2.11 -C "--sysconfdir=/etc --with-openssl"
+${THOME}/build/genpkg TRIBpgpool pgpool-II-4.2.11
