54e4596eb962bbcdde849352ed7b221a12849130
Peter Tribble <peter.tribble@gmail.com>
2022-12-27 15:29:43 +0000

    Update stunnel 5.67

--- TRIBstunnel/build.sh
+++ TRIBstunnel/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild stunnel-5.60 -C "--sysconfdir=/etc --localstatedir=/var --disable-libwrap"
-${THOME}/build/genpkg TRIBstunnel stunnel-5.60
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only stunnel-5.67 -C "--sysconfdir=/etc --localstatedir=/var --disable-libwrap"
+${THOME}/build/genpkg TRIBstunnel stunnel-5.67
