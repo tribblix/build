e1d9a68b4be499d3561d722b45517111529d97d8
Peter Tribble <peter.tribble@gmail.com>
2022-12-28 12:52:00 +0000

    Update multidict 6.0.4

--- TRIBmultidict-python-310/build.sh
+++ TRIBmultidict-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmultidict-python-310 multidict-6.0.3
+${THOME}/build/pkg_setup_py TRIBmultidict-python-310 multidict-6.0.4
--- TRIBmultidict-python-39/build.sh
+++ TRIBmultidict-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmultidict-python-39 multidict-6.0.3
+${THOME}/build/pkg_setup_py TRIBmultidict-python-39 multidict-6.0.4
