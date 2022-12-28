73586e4daa7fc983129c66ab2b9f2411066628a3
Peter Tribble <peter.tribble@gmail.com>
2022-12-28 13:01:18 +0000

    Update supervisord 4.2.5

--- TRIBsupervisor-python-39/build.sh
+++ TRIBsupervisor-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack supervisor-4.2.4
-cd supervisor-4.2.4
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pis
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pis
-${THOME}/build/create_pkg TRIBsupervisor-python-39 /tmp/pis
-cd ..
-rm -fr /tmp/pis
+${THOME}/build/pkg_setup_py TRIBsupervisor-python-39 supervisor-4.2.5
