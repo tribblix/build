8d54858eb364c0132aa993e2d1152427697e36dd
Peter Tribble <peter.tribble@gmail.com>
2023-01-10 08:26:53 +0000

    Update janet 1.26.0

--- TRIBjanet/build.sh
+++ TRIBjanet/build.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 #
 #
-${THOME}/build/unpack janet-1.22.0
-cd janet-1.22.0
+${THOME}/build/unpack janet-1.26.0
+cd janet-1.26.0
+sed -i 's:ln -sf -T:ln -sf:' Makefile 
 gmake CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__"
 #
 # optional, but this ought to pass
 #
 #gmake test CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__"
 #
-gmake install DESTDIR=/tmp/jj122 CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__"
+rm -fr /tmp/jj126
+gmake install DESTDIR=/tmp/jj126 CC=gcc PREFIX=/usr CLIBS="-lsocket -lnsl -lm" CFLAGS="-O2 -D__EXTENSIONS__"
 
 #
 # to install jpm you need to have built janet and installed it once
 #
 git clone --depth=1 https://github.com/janet-lang/jpm.git
 cd jpm
-env DESTDIR=/tmp/jj122 PREFIX=/usr janet bootstrap.janet
+env DESTDIR=/tmp/jj126 PREFIX=/usr janet bootstrap.janet
 
 #
 # to use jpm you need to override things to stop it trying to
@@ -25,5 +27,5 @@ env DESTDIR=/tmp/jj122 PREFIX=/usr janet bootstrap.janet
 # jpm --cc=gcc --cc-link=gcc --modpath=`pwd` --binpath=`pwd`/bin install joy
 #
 
-${THOME}/build/create_pkg TRIBjanet /tmp/jj122
-rm -fr /tmp/jj122
+${THOME}/build/create_pkg TRIBjanet /tmp/jj126
+rm -fr /tmp/jj126
