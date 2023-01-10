a057a46fd727f952f06d985600e9f3f974120ff4
Peter Tribble <peter.tribble@gmail.com>
2023-01-10 18:08:35 +0000

    Update fossil 2.20

--- TRIBfossil/build.sh
+++ TRIBfossil/build.sh
@@ -3,5 +3,5 @@
 # it's not a normal configure, so have to pass flags
 # directly to get 64-bit
 #
-env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild fossil-src-2.18 -C --json
-${THOME}/build/genpkg TRIBfossil fossil-src-2.18
+env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild fossil-src-2.20 -C --json
+${THOME}/build/genpkg TRIBfossil fossil-src-2.20
