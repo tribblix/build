6cf33a456ba008bc3540cae81af5b5818aa72f5a
Peter Tribble <peter.tribble@gmail.com>
2023-01-10 18:15:47 +0000

    Update sphinxcontrib_applehelp 1.0.3

--- TRIBsc-applehelp-python-310/build.sh
+++ TRIBsc-applehelp-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack sphinxcontrib-applehelp-1.0.2
-cd sphinxcontrib-applehelp-1.0.2
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsc-applehelp-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBsc-applehelp-python-310 sphinxcontrib.applehelp-1.0.3
