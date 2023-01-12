415df140bc3898666123a44c314bfa326c747ed4
Peter Tribble <peter.tribble@gmail.com>
2023-01-12 21:45:03 +0000

    Update platformdirs 2.6.2

--- TRIBplatformdirs-python-310/build.sh
+++ TRIBplatformdirs-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBplatformdirs-python-310 platformdirs-2.6.0
+${THOME}/build/pkg_pep518 TRIBplatformdirs-python-310 platformdirs-2.6.2
--- TRIBplatformdirs-python-39/build.sh
+++ TRIBplatformdirs-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBplatformdirs-python-39 platformdirs-2.6.0
+${THOME}/build/pkg_pep518 TRIBplatformdirs-python-39 platformdirs-2.6.2
