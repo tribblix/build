bff00e12aa14c4d5b7b0cf267f41f38747714a1f
Peter Tribble <peter.tribble@gmail.com>
2023-01-14 12:13:05 +0000

    Update alabaster 0.7.13

--- TRIBalabaster-python-39/build.sh
+++ TRIBalabaster-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack alabaster-0.7.12
-cd alabaster-0.7.12
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBalabaster-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBalabaster-python-39 alabaster-0.7.13
