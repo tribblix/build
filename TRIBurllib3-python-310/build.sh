3f92d4f72aba64184e904d849d1310cd27ddd92a
Peter Tribble <peter.tribble@gmail.com>
2023-01-14 12:23:05 +0000

    Update urllib3 1.26.14

--- TRIBurllib3-python-310/build.sh
+++ TRIBurllib3-python-310/build.sh
@@ -4,4 +4,4 @@
 # we still declare them, but urllib3 shouldn't use them
 # see https://github.com/urllib3/urllib3/issues/2680
 #
-${THOME}/build/pkg_setup_py TRIBurllib3-python-310 urllib3-1.26.13
+${THOME}/build/pkg_setup_py TRIBurllib3-python-310 urllib3-1.26.14
