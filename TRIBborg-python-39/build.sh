c3caf0c3cd2127176e80bb4cdb1bd6a2736d2217
Peter Tribble <peter.tribble@gmail.com>
2023-01-14 22:28:26 +0000

    Update borgbackup 1.2.3

--- TRIBborg-python-39/build.sh
+++ TRIBborg-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack borgbackup-1.2.2
-cd borgbackup-1.2.2
-env PKG_CONFIG_PATH=/usr/lib/`${THOME}/build/getarch`/pkgconfig /usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBborg-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+env PKG_CONFIG_PATH=/usr/lib/`${THOME}/build/getarch`/pkgconfig ${THOME}/build/pkg_setup_py TRIBborg-python-39 borgbackup-1.2.3
