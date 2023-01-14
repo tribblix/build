56c4c519a61f5861d2d8c6d97fe7472beb5e6d33
Peter Tribble <peter.tribble@gmail.com>
2023-01-14 22:19:39 +0000

    Update quilt 0.67

--- TRIBquilt/build.sh
+++ TRIBquilt/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-$THOME/build/dobuild -gnu quilt-0.65 -C "--without-date --with-grep=/usr/gnu/bin/grep --with-sed=/usr/gnu/bin/sed --without-getopt --with-diff=/usr/gnu/bin/diff"
-cd quilt-0.65
+$THOME/build/dobuild -gnu quilt-0.67 -C "--without-date --with-grep=/usr/gnu/bin/grep --with-sed=/usr/gnu/bin/sed --without-getopt --with-diff=/usr/gnu/bin/diff"
+cd quilt-0.67
 rm -fr /tmp/qqt4
 gmake install BUILD_ROOT=/tmp/qqt4
 ${THOME}/build/create_pkg TRIBquilt /tmp/qqt4
