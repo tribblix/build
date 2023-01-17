1ebbf4e0716c3623596f9ed04aa327302b73783d
Peter Tribble <peter.tribble@gmail.com>
2023-01-17 08:06:48 +0000

    Update wcwidth 0.2.6

--- TRIBwcwidth-python-39/build.sh
+++ TRIBwcwidth-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack wcwidth-0.2.5
-cd wcwidth-0.2.5
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBwcwidth-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBwcwidth-python-39 wcwidth-0.2.6
