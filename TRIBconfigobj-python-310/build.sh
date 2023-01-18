1fd6497878868770045ee85752ad9c0bfc6f85b7
Peter Tribble <peter.tribble@gmail.com>
2023-01-18 19:52:26 +0000

    Update configobj 5.0.7

--- TRIBconfigobj-python-310/build.sh
+++ TRIBconfigobj-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack configobj-5.0.6
-cd configobj-5.0.6
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBconfigobj-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBconfigobj-python-310 configobj-5.0.7
--- TRIBconfigobj-python-39/build.sh
+++ TRIBconfigobj-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack configobj-5.0.6
-cd configobj-5.0.6
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBconfigobj-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBconfigobj-python-39 configobj-5.0.7
