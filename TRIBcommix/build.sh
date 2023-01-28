e2459bfe346dc1d5ef8362e5d4d3468f4766eb03
Peter Tribble <peter.tribble@gmail.com>
2023-01-28 21:49:51 +0000

    Update commix 3.6

--- TRIBcommix/build.sh
+++ TRIBcommix/build.sh
@@ -3,10 +3,10 @@
 # depends on python
 # needs the gnureadline module
 #
-${THOME}/build/unpack commix-3.3
-cd commix-3.3
+${THOME}/build/unpack commix-3.6
+cd commix-3.6
 /usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
+rm -fr /tmp/ckx
 /usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/ckx
 ${THOME}/build/create_pkg TRIBcommix /tmp/ckx
 cd ..
