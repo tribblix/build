2c6a40bd983cb80d32d96651cef812d2fd8f6757
Peter Tribble <peter.tribble@gmail.com>
2023-02-02 20:11:27 +0000

    Update sphinxcontrib-htmlhelp 2.0.1

--- TRIBsc-htmlhelp-python-39/build.sh
+++ TRIBsc-htmlhelp-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack sphinxcontrib-htmlhelp-2.0.0
-cd sphinxcontrib-htmlhelp-2.0.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsc-htmlhelp-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBsc-htmlhelp-python-39 sphinxcontrib-htmlhelp-2.0.1
