bd83bc199594ba4f9f19472c9d418caecfe2c34c
Peter Tribble <peter.tribble@gmail.com>
2023-02-02 21:15:22 +0000

    Update mate-control-center 1.24.2

--- TRIBmate-control-center/build.sh
+++ TRIBmate-control-center/build.sh
@@ -2,5 +2,5 @@
 #
 # make -k because pt.mo fails
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu mate-control-center-1.22.2
-${THOME}/build/genpkg TRIBmate-control-center mate-control-center-1.22.2
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu mate-control-center-1.24.2
+${THOME}/build/genpkg TRIBmate-control-center mate-control-center-1.24.2
---
+++ patches/mate-control-center-1.24.2.pls
@@ -0,0 +1,1 @@
+-p1 mate-control-center.patch
