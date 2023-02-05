3c53d02d21c31ea636b4dd45773c68bcfc8bf873
Peter Tribble <peter.tribble@gmail.com>
2023-02-05 20:37:55 +0000

    Update google-api-python-client 2.76.0

--- TRIBgoogle-api-python-39/build.sh
+++ TRIBgoogle-api-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-api-python-39 google-api-python-client-2.74.0
+${THOME}/build/pkg_setup_py TRIBgoogle-api-python-39 google-api-python-client-2.76.0
