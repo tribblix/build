4e26a7c930204d143b036e0a7d9944ac3e5282a1
Peter Tribble <peter.tribble@gmail.com>
2023-02-05 20:33:44 +0000

    Update blessed 1.20.0

--- TRIBblessed-python-310/build.sh
+++ TRIBblessed-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack blessed-1.19.1
-cd blessed-1.19.1
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBblessed-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBblessed-python-310 blessed-1.20.0
