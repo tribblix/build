895fc4d215fac6e6cd0cadab5e8603f8a45ee13a
Peter Tribble <peter.tribble@gmail.com>
2023-02-06 08:03:15 +0000

    Update dnspython 2.3.0

--- TRIBdnspython-python-39/build.sh
+++ TRIBdnspython-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack dnspython-2.2.1
-cd dnspython-2.2.1
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBdnspython-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBdnspython-python-39 dnspython-2.3.0
