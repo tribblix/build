452217937d6101f428ce0346ec6a068e7e83efdb
Peter Tribble <peter.tribble@gmail.com>
2023-02-10 12:20:13 +0000

    Update ffuf 2.0.0

--- TRIBffuf/build.sh
+++ TRIBffuf/build.sh
@@ -2,13 +2,13 @@
 #
 mkdir ff
 cd ff
-env GOPATH=`pwd` go install github.com/ffuf/ffuf@latest
+env GOPATH=`pwd` go install github.com/ffuf/ffuf/v2@latest
 
 rm -fr /tmp/ef
 mkdir -p /tmp/ef/usr/bin
 cp bin/ffuf /tmp/ef/usr/bin
 mkdir -p /tmp/ef/usr/share/ffuf
-cd pkg/mod/github.com/ffuf/ffuf@v*
+cd pkg/mod/github.com/ffuf/ffuf/v2@v*
 cp LICENSE README.md /tmp/ef/usr/share/ffuf
 ${THOME}/build/create_pkg TRIBffuf /tmp/ef
 rm -fr /tmp/ef
