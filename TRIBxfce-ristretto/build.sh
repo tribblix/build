f1c4a9240be37ab488f8645695587b832003461f
Peter Tribble <peter.tribble@gmail.com>
2023-02-10 14:21:20 +0000

    Update ristretto 0.13.0

--- TRIBxfce-ristretto/build.sh
+++ TRIBxfce-ristretto/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu -64only ristretto-0.12.3
-${THOME}/build/genpkg TRIBxfce-ristretto ristretto-0.12.3
+${THOME}/build/dobuild -gnu -64only ristretto-0.13.0
+${THOME}/build/genpkg TRIBxfce-ristretto ristretto-0.13.0
