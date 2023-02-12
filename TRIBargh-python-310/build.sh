6cbaa2425e70240a86e602eb4658012bc9534cb5
Peter Tribble <peter.tribble@gmail.com>
2023-02-12 15:49:26 +0000

    Update argh 0.27.2

--- TRIBargh-python-310/build.sh
+++ TRIBargh-python-310/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack argh-0.26.2
-cd argh-0.26.2
-/usr/versions/python-3.10/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.10/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBargh-python-310 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBargh-python-310 argh-0.27.2
--- TRIBargh-python-39/build.sh
+++ TRIBargh-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack argh-0.26.2
-cd argh-0.26.2
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBargh-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBargh-python-39 argh-0.27.2
