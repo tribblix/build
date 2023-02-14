f240e1c7704015ff42c2a3204e33dea482726fcd
Peter Tribble <peter.tribble@gmail.com>
2023-02-14 14:58:48 +0000

    Update PyNaCl 1.5.0

--- TRIBpynacl-python-39/build.sh
+++ TRIBpynacl-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack PyNaCl-1.4.0
-cd PyNaCl-1.4.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpynacl-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBpynacl-python-39 PyNaCl-1.5.0
