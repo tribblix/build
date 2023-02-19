9eeb5f706e565450da2928270d0c5ddadc6095f5
Peter Tribble <peter.tribble@gmail.com>
2023-02-19 17:23:11 +0000

    Update constype 1.0.5

--- TRIBdiagnostic-constype/build.sh
+++ TRIBdiagnostic-constype/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild constype-1.0.4
-${THOME}/build/genpkg TRIBdiagnostic-constype constype-1.0.4
+${THOME}/build/dobuild -64only constype-1.0.5
+${THOME}/build/genpkg TRIBdiagnostic-constype constype-1.0.5
