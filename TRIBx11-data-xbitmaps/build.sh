26d4585eece029e14b5d9eff94441d86c0e95328
Peter Tribble <peter.tribble@gmail.com>
2023-02-24 09:27:09 +0000

    Update xbitmaps 1.1.3

--- TRIBx11-data-xbitmaps/build.sh
+++ TRIBx11-data-xbitmaps/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xbitmaps-1.1.1
-${THOME}/build/genpkg TRIBx11-data-xbitmaps xbitmaps-1.1.1
+${THOME}/build/dobuild xbitmaps-1.1.3
+${THOME}/build/genpkg TRIBx11-data-xbitmaps xbitmaps-1.1.3
