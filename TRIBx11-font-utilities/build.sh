63f9de3cf99bd0ed987d5382d629b4d4b4c3d681
Peter Tribble <peter.tribble@gmail.com>
2023-03-01 20:25:03 +0000

    Ship 32 and 64-bit pc files for font-util

--- TRIBx11-font-utilities/build.sh
+++ TRIBx11-font-utilities/build.sh
@@ -5,7 +5,9 @@
 #
 # font-utils is in the font directory, not the app directory
 #
+# do not build font-util -64only, we need both the 32 and 64 bit pc file
+#
 ${THOME}/build/dobuild -64only bdftopcf-1.1
 ${THOME}/build/dobuild -64only mkfontscale-1.2.2
-${THOME}/build/dobuild -64only font-util-1.4.0
+${THOME}/build/dobuild -64 font-util-1.4.0
 ${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.2 font-util-1.4.0
