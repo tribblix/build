5a5e664a07bdd4eb53d6261b684b428f4d5f953d
Peter Tribble <peter.tribble@gmail.com>
2023-03-04 09:34:10 +0000

    Rebuild fping 64-bit

--- TRIBfping/build.sh
+++ TRIBfping/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild fping-5.1 -C --enable-ipv6
+env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild -64only fping-5.1 -C --enable-ipv6
 ${THOME}/build/genpkg TRIBfping fping-5.1
