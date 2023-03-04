f6af30fcab82140d0bc57ec30e9196969eb9b582
Peter Tribble <peter.tribble@gmail.com>
2023-03-04 10:18:25 +0000

    Update gnu gettext 0.21.1

--- TRIBgnu-gettext/build.sh
+++ TRIBgnu-gettext/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild gettext-0.19.7 -P /usr/gnu -C "--with-included-glib --with-included-libcroco --disable-curses --without-libiconv-prefix"
-${THOME}/build/genpkg TRIBgnu-gettext gettext-0.19.7
+# this package ships a number of shared libraries which have the version number
+# embedded, but fortunately I haven't (yet) found any consumers so updating
+# should be safe
+#
+${THOME}/build/dobuild -64only gettext-0.21.1 -P /usr/gnu -C "--with-included-glib --with-included-libcroco --disable-curses --without-libiconv-prefix --disable-libasprintf --disable-openmp --with-included-libunistring"
+${THOME}/build/genpkg TRIBgnu-gettext gettext-0.21.1
