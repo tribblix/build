b930a2bc88d806c86c2bf3e366326200eac36caf
Peter Tribble <peter.tribble@gmail.com>
2023-03-09 11:39:10 +0000

    Update illumos gcc10 to 10.4.0-il-1

--- TRIBdev-illumos-gcc10/build.sh
+++ TRIBdev-illumos-gcc10/build.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 #
-# this gcc10 build is designed purely as a compiler for illumos-gate
-# at some point in the future it will be promoted to the primary compiler
-# for Tribblix, which would be repackaged in a different location, split
-# into dev and runtime packages, with fortran added, at which point this
-# package might be obsolete
+# this gcc10 build is designed purely as the compiler for illumos-gate
 #
 # while it builds on sparc, illumos-gate will not work with gcc10 on sparc
-# so it's only useful to find the gcc10 bugs in the source
+# so it's only useful there to find the gcc10 bugs in the source
 #
 # download the tarball like so
 #
-# wget https://github.com/illumos/gcc/archive/gcc-10.3.0-il-1.tar.gz
+# wget https://github.com/illumos/gcc/archive/gcc-10.4.0-il-1.tar.gz
 #
 
-${THOME}/build/unpack gcc-10.3.0-il-1
-mv gcc-gcc-10.3.0-il-1 gcc-10.3.0-il-1
-cd gcc-10.3.0-il-1
+${THOME}/build/unpack gcc-10.4.0-il-1
+mv gcc-gcc-10.4.0-il-1 gcc-10.4.0-il-1
+cd gcc-10.4.0-il-1
 
 #
 # these are the required versions for an in-tree build
@@ -38,8 +34,8 @@ cd build
 ###x86
 # last few flags to match OmniOS
 env MSGFMT=/usr/gnu/bin/msgfmt GMSGFMT=/usr/gnu/bin/msgfmt PATH=${PATH}:/usr/gnu/bin LD_OPTIONS="-zignore -zcombreloc -Bdirect -i" \
-../configure --prefix=/opt/gcc/10.3.0 --enable-languages=c,c++,objc \
-  --with-pkgversion="illumos/Tribblix 10.3.0" \
+../configure --prefix=/opt/gcc/10.4.0 --enable-languages=c,c++,objc \
+  --with-pkgversion="illumos/Tribblix 10.4.0" \
   --with-bugurl="http://www.tribblix.org/feedback.html" \
   --without-gnu-ld --with-ld=/usr/bin/ld \
   --with-as=/usr/gnu/bin/as --with-gnu-as \
@@ -50,8 +46,8 @@ env MSGFMT=/usr/gnu/bin/msgfmt GMSGFMT=/usr/gnu/bin/msgfmt PATH=${PATH}:/usr/gnu
 
 ###sparc
 env MSGFMT=/usr/gnu/bin/msgfmt GMSGFMT=/usr/gnu/bin/msgfmt PATH=${PATH}:/usr/gnu/bin LD_OPTIONS="-zignore -zcombreloc -Bdirect -i" \
-../configure --prefix=/opt/gcc/10.3.0 --enable-languages=c,c++,objc \
-  --with-pkgversion="illumos/Tribblix 10.3.0" \
+../configure --prefix=/opt/gcc/10.4.0 --enable-languages=c,c++,objc \
+  --with-pkgversion="illumos/Tribblix 10.4.0" \
   --with-bugurl="http://www.tribblix.org/feedback.html" \
   --without-gnu-ld --with-ld=/usr/bin/ld \
   --with-as=/usr/ccs/bin/as --without-gnu-as
@@ -67,11 +63,11 @@ env MSGFMT=/usr/gnu/bin/msgfmt GMSGFMT=/usr/gnu/bin/msgfmt PATH=${PATH}:/usr/gnu
 gmake -j 8
 
 #
-# plain install is ~700M on x86
-# with install-strip we get down to 239M
+# plain install is 1.33G on x86
+# with install-strip we get down to 410M
 #
 rm -fr /tmp/gc10
 gmake DESTDIR=/tmp/gc10 install-strip
-rm -fr /tmp/gc10/opt/gcc/10.3.0/lib/gcc/i386-pc-solaris2.11/10.3.0/include-fixed
+rm -fr /tmp/gc10/opt/gcc/10.4.0/lib/gcc/i386-pc-solaris2.11/10.4.0/include-fixed
 ${THOME}/build/create_pkg TRIBdev-illumos-gcc10 /tmp/gc10
 rm -fr /tmp/gc10
