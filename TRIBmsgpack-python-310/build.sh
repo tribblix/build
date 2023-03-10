2b483ef59891c75cd6e5ffa041c31d3d3c9ef82d
Peter Tribble <peter.tribble@gmail.com>
2023-03-10 09:35:06 +0000

    Update python msgpack 1.0.5

--- TRIBmsgpack-python-310/build.sh
+++ TRIBmsgpack-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmsgpack-python-310 msgpack-1.0.4
+${THOME}/build/pkg_setup_py TRIBmsgpack-python-310 msgpack-1.0.5
--- TRIBmsgpack-python-39/build.sh
+++ TRIBmsgpack-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmsgpack-python-39 msgpack-1.0.4
+${THOME}/build/pkg_setup_py TRIBmsgpack-python-39 msgpack-1.0.5
