a9622ddb6feb25f4d8e713a8a671750a7877a5b7
Peter Tribble <peter.tribble@gmail.com>
2023-03-11 11:14:41 +0000

    Update iso-codes 4.13.0

--- TRIBiso-codes/build.sh
+++ TRIBiso-codes/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# you might need to get the tarball from launchpad
-# https://launchpad.net/ubuntu/+source/iso-codes/4.11.0-1
+# easier to pull the tarball from the omnios mirror
+# https://mirrors.omnios.org/iso-codes/
 #
 ${THOME}/build/newbuild.sh both TRIBiso-codes
