59dce6f62dbaa48c30fa663f402259607e370bd0
Peter Tribble <peter.tribble@gmail.com>
2023-03-16 18:37:11 +0000

    Update php 8.1.17

--- TRIBblix-php-8/build.sh
+++ TRIBblix-php-8/build.sh
@@ -11,8 +11,8 @@
 # must have apache24 installed
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-8.1.16
-cd php-8.1.16
+${THOME}/build/unpack php-8.1.17
+cd php-8.1.17
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
