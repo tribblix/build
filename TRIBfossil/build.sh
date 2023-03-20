745b7c53be4eb8171b8d96f64e46aed9ec8b29b1
Peter Tribble <peter.tribble@gmail.com>
2023-03-20 08:27:11 +0000

    Fix up fossil build on SPARC

--- TRIBfossil/build.sh
+++ TRIBfossil/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # it's not a normal configure, so have to pass flags
-# directly to get 64-bit
+# directly as well to get 64-bit
 #
-env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild fossil-src-2.20 -C --json
+env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 ${THOME}/build/dobuild -64only fossil-src-2.20 -C --json
 ${THOME}/build/genpkg TRIBfossil fossil-src-2.20
