1803425c4427b8a808b554ec24f6e87e87d569b4
Peter Tribble <peter.tribble@gmail.com>
2023-03-21 09:05:39 +0000

    The atk build recipe was out of touch with reality

--- TRIBlib-desktop-atk/build.sh
+++ TRIBlib-desktop-atk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 atk-2.38.0 -C --disable-introspection
+${THOME}/build/mesonbuild -64 -gnu atk-2.38.0 -C "--buildtype=release -Dintrospection=false"
 ${THOME}/build/genpkg TRIBlib-desktop-atk atk-2.38.0
