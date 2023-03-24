ba5ae07c285d37ed6dafdd84b24bd2006a0d247c
Peter Tribble <peter.tribble@gmail.com>
2023-03-24 20:31:04 +0000

    Remove postgres11

--- TRIBblix-postgres11/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# postgres 11
-#
-# now without perl as we're 32-bit but perl is now 64-bit
-#
-${THOME}/build/dobuild -P /opt/tribblix/postgres11 postgresql-11.19 -C "--without-readline --without-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres11 postgresql-11.19
