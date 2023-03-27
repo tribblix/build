4363a4bc473f9bbf9bf773545c7c73a973c3a505
Peter Tribble <peter.tribble@gmail.com>
2023-03-27 11:49:45 +0100

    Rebuild libwv, la rework, add 64-bit

--- TRIBlibwv/build.sh
+++ TRIBlibwv/build.sh
@@ -3,6 +3,5 @@
 # you'll have to get the tarball from debian or similar; the wvware
 # site on sourceforge goes up to 1.2.4
 #
-${THOME}/build/dobuild wv-1.2.9
-rm wv-1.2.9/install-sh ; ln -s /usr/bin/ginstall wv-1.2.9/install-sh
+${THOME}/build/dobuild -64 wv-1.2.9
 ${THOME}/build/genpkg TRIBlibwv wv-1.2.9
