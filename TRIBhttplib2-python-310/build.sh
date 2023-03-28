8c0c1f46da80618f4b9aee5fa62d5186b205984d
Peter Tribble <peter.tribble@gmail.com>
2023-03-28 19:39:53 +0100

    Update httplib2 0.22.0

--- TRIBhttplib2-python-310/build.sh
+++ TRIBhttplib2-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBhttplib2-python-310 httplib2-0.21.0
+${THOME}/build/pkg_setup_py TRIBhttplib2-python-310 httplib2-0.22.0
--- TRIBhttplib2-python-39/build.sh
+++ TRIBhttplib2-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBhttplib2-python-39 httplib2-0.21.0
+${THOME}/build/pkg_setup_py TRIBhttplib2-python-39 httplib2-0.22.0
