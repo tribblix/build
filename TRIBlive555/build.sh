796ae4952a6869663b12f84378543099e01971a5
Peter Tribble <peter.tribble@gmail.com>
2023-03-28 22:06:53 +0100

    Update live555 2023.01.19, and make 64-bit

--- TRIBlive555/build.sh
+++ TRIBlive555/build.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 #
+# slightly hacked about to force a 64-bit build
 #
-#
-${THOME}/build/unpack live.2020.12.13
+${THOME}/build/unpack live.2023.01.19
 cd live
-gsed -i 's:cc:gcc:' config.solaris-32bit 
-cat config.linux-with-shared-libraries | sed 's:-O2:-O2 -DSOLARIS -DXLOCALE_NOT_USED:' | sed 's:-DBSD=1 ::' > config.solaris-shared
-gsed -i -e 's:cc:gcc:' -e 's:c++:g++:' -e 's:ggcc:g++:' config.solaris-shared
-gsed -i -e 's:-shared:-lsocket -lnsl -shared:' config.solaris-shared
+cat config.linux-with-shared-libraries | sed 's:-O2:-m64 -O2 -DSOLARIS -DNEWLOCALE_NOT_USED:' | sed 's:-DBSD=1 ::' > config.solaris-shared
+sed -i -e 's:-o:-m64 -o:' config.solaris-shared
+sed -i -e 's:-shared:-lsocket -lnsl -shared:' config.solaris-shared
 ./genMakefiles solaris-shared
 gmake -j 4 CC=gcc
 rm -fr /tmp/lls
-env PATH=/usr/gnu/bin:$PATH gmake install DESTDIR=/tmp/lls PREFIX=/usr
+env PATH=/usr/gnu/bin:$PATH gmake install DESTDIR=/tmp/lls PREFIX=/usr LIBDIR=/usr/lib/`${THOME}/build/getarch`
 ${THOME}/build/create_pkg TRIBlive555 /tmp/lls
 rm -fr /tmp/lls
 cd ..
