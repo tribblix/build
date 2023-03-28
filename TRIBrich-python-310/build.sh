54537bb027b2d3ca4c89ec6403e964791915cd4a
Peter Tribble <peter.tribble@gmail.com>
2023-03-28 19:28:11 +0100

    Update rich 13.3.3

--- TRIBrich-python-310/build.sh
+++ TRIBrich-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBrich-python-310 rich-13.3.1
+${THOME}/build/pkg_pep518 TRIBrich-python-310 rich-13.3.3
--- TRIBrich-python-39/build.sh
+++ TRIBrich-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBrich-python-39 rich-13.3.1
+${THOME}/build/pkg_pep518 TRIBrich-python-39 rich-13.3.3
