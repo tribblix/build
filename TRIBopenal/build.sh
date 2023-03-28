f57f19c25c6c7562fdbb1f592134e2e51d76d23b
Peter Tribble <peter.tribble@gmail.com>
2023-03-28 21:28:15 +0100

    Improve the openal build, and add 64-bit

--- TRIBopenal/build.sh
+++ TRIBopenal/build.sh
@@ -2,10 +2,5 @@
 #
 # [seems orphaned, no accurate idea where to get it from]
 #
-${THOME}/build/unpack openal-soft-1.15.1
-cd openal-soft-1.15.1
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr
-gmake -j 8
-${THOME}/build/genpkg TRIBopenal
+${THOME}/build/cmbuild -64 openal-soft-1.15.1 -C "-DCMAKE_BUILD_TYPE=Release -DEXAMPLES=NO" +C "-DCMAKE_BUILD_TYPE=Release -DEXAMPLES=NO -D LIB_SUFFIX=/`$THOME/build/getarch`"
+${THOME}/build/genpkg TRIBopenal openal-soft-1.15.1
