e918afb3de8ece25097ad82a72ed05b0ebd1e4f1
Peter Tribble <peter.tribble@gmail.com>
2023-03-29 17:57:51 +0100

    Update babl 0.1.98 gegl 0.4.44

--- TRIBlib-gegl/build.sh
+++ TRIBlib-gegl/build.sh
@@ -4,6 +4,6 @@
 #
 # we need GNU msgfmt (hence PATH) but our own ar, hence AR
 #
-env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild -64 gegl-0.4.36 -C "-Dintrospection=false --buildtype=release -Dworkshop=true -Dlibav=disabled"
+env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild -64 gegl-0.4.44 -C "-Dintrospection=false --buildtype=release -Dworkshop=true -Dlibav=disabled -Dopenmp=disabled"
 
-${THOME}/build/genpkg TRIBlib-gegl gegl-0.4.36
+${THOME}/build/genpkg TRIBlib-gegl gegl-0.4.44
