8aec4f0718c6c235867e274511fb7a95c32edeeb
Peter Tribble <peter.tribble@gmail.com>
2023-03-30 21:37:55 +0100

    Update tcpdump 4.99.3

--- TRIBtcpdump/build.sh
+++ TRIBtcpdump/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# NB. make sure pcap isn't at the same level, else it pulls pcap from the
-# build area rather than the systems
+# NB. make sure libpcap isn't at the same level, else it pulls it from the
+# build area rather than the system
 #
-${THOME}/build/dobuild tcpdump-4.9.3
-${THOME}/build/genpkg TRIBtcpdump tcpdump-4.9.3
+${THOME}/build/dobuild -64only tcpdump-4.99.3
+${THOME}/build/genpkg TRIBtcpdump tcpdump-4.99.3
