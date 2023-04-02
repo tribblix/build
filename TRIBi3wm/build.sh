9897feda8d52ee3e35bfcf6199ad7ed42a926ffd
Peter Tribble <peter.tribble@gmail.com>
2023-04-02 21:30:10 +0100

    Update i3wm 4.22

--- TRIBi3wm/build.sh
+++ TRIBi3wm/build.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 #
-# i3 switched to autoconf, but a slightly funky way of doing it
+# i3 now switched to meson
 #
-env LIBS="-lsocket -lnsl -lgen" ${THOME}/build/dobuild i3-4.18.2 -M "-C i386-pc-solaris2.11" -C --enable-mans
-cd i3-4.18.2
-rm -fr /tmp/i3
-cd i386-pc-solaris2.11
-gmake install DESTDIR=/tmp/i3
-cd ..
-mkdir -p /tmp/i3/usr/share/man/man1
-cp man/*.1 /tmp/i3/usr/share/man/man1
-${THOME}/build/create_pkg TRIBi3wm /tmp/i3
-rm -fr /tmp/i3
-cd ..
+env TRIBBLIX_LDFLAGS=-lsocket TRIBBLIX_CFLAGS=-D__EXTENSIONS__ ${THOME}/build/mesonbuild -64only i3-4.22
+$THOME/build/genpkg TRIBi3wm i3-4.22
