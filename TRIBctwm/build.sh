44621d970a7ef28a03dd265851d478c39a4f8723
Peter Tribble <peter.tribble@gmail.com>
2023-04-02 20:06:01 +0100

    Update ctwm 4.1.0

--- TRIBctwm/build.sh
+++ TRIBctwm/build.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ctwm-4.0.3
-cd ctwm-4.0.3
-sed -i s:std=c99:std=gnu99: cmake_files/compiler_feature_checks.cmake
-gmake CMAKE_EXTRAS="-DCMAKE_INSTALL_PREFIX=/usr"
-#
-# this is an entertaining abuse of genpkg
-#
-${THOME}/build/genpkg TRIBctwm build
+$THOME/build/cmbuild -64only ctwm-4.1.0
+${THOME}/build/genpkg TRIBctwm ctwm-4.1.0
