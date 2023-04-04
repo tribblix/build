4cd7a220e3ffcd5f78f1419b583334648f3fd00d
Peter Tribble <peter.tribble@gmail.com>
2023-04-04 13:23:21 +0100

    Remove facter

--- TRIBfacter-ruby-27/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack facter-2.5.7
-cd facter-2.5.7
-rm -fr /tmp/ffa
-/usr/versions/ruby-2.7/bin/ruby install.rb --destdir=/tmp/ffa
-${THOME}/build/create_pkg TRIBfacter-ruby-27 /tmp/ffa
-rm -fr /tmp/ffa
-cd ..
