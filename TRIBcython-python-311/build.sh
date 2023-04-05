422f3d6c1731000b71d619c51e33af960bd715e1
Peter Tribble <peter.tribble@gmail.com>
2023-04-05 19:58:30 +0100

    Update cython 0.29.34

---
+++ TRIBcython-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBcython-python-311 Cython-0.29.34
--- TRIBcython-python-39/build.sh
+++ TRIBcython-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Cython-0.29.32
-cd Cython-0.29.32
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBcython-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBcython-python-39 Cython-0.29.34
