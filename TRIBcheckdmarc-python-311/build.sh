f284d070d1f4e7bb769367f54ae66a4517122d4a
Peter Tribble <peter.tribble@gmail.com>
2023-04-05 21:46:49 +0100

    Build checkdmarc for python3.11

---
+++ TRIBcheckdmarc-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBcheckdmarc-python-311 checkdmarc-4.5.2
---
+++ TRIBdnspython-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBdnspython-python-311 dnspython-2.3.0
