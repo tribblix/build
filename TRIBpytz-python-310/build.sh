47e218c0cc0ea32d44c4834f74b7bb9e924d1791
Peter Tribble <peter.tribble@gmail.com>
2023-04-05 19:18:25 +0100

    Update pytz 2023.3

--- TRIBpytz-python-310/build.sh
+++ TRIBpytz-python-310/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpytz-python-310 pytz-2023.2
+${THOME}/build/pkg_setup_py TRIBpytz-python-310 pytz-2023.3
---
+++ TRIBpytz-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBpytz-python-311 pytz-2023.3
