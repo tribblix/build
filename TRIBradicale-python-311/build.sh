e550bd53c3740c35713cac0aa60070d991220580
Peter Tribble <peter.tribble@gmail.com>
2023-04-06 18:52:08 +0100

    Build radicale for python3.11

---
+++ TRIBradicale-python-311/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+${THOME}/build/unpack Radicale-3.1.8
+cd Radicale-3.1.8
+/usr/versions/python-3.11/bin/python3 setup.py build
+rm -fr /tmp/pil
+/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
+${THOME}/build/create_pkg TRIBradicale-python-311 /tmp/pil
+cd ..
+rm -fr /tmp/pil
