92d77c2ba5d317c204042be0c66f4072073843cc
Peter Tribble <peter.tribble@gmail.com>
2023-04-07 20:13:46 +0100

    Need a 64-bit libpeas

--- TRIBlibpeas/build.sh
+++ TRIBlibpeas/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #
-${THOME}/build/unpack libpeas-1.26.0
-cd libpeas-1.26.0
-mkdir build
-cd build
-# would like -Dintrospection=false but the build doesn't
-# actually support that, despite it being listed
-meson --prefix=/usr -Ddemos=false ..
-env PATH=/usr/gnu/bin:$PATH CC=gcc ninja
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC=gcc $THOME/build/mesonbuild libpeas-1.26.0 -C -Ddemos=false
+mv libpeas-1.26.0 libpeas-1.26.0-32bit
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC="gcc -m64" $THOME/build/mesonbuild +64 libpeas-1.26.0 -C -Ddemos=false
+mv libpeas-1.26.0-32bit libpeas-1.26.0
 
 rm -fr /tmp/lps
-env PATH=/usr/gnu/bin:$PATH CC=gcc DESTDIR=/tmp/lps ninja install
+
+cd libpeas-1.26.0
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC=gcc DESTDIR=/tmp/lps ninja -C build install
+cd ..
+cd libpeas-1.26.0-64bit
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC="gcc -m64" DESTDIR=/tmp/lps ninja -C build install
+cd ..
 
 ${THOME}/build/create_pkg TRIBlibpeas /tmp/lps
 rm -fr /tmp/lps
