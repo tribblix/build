e542a138fba4adc01e5a4866dec0064b6fa55817
Peter Tribble <peter.tribble@gmail.com>
2023-04-07 20:21:08 +0100

    Want 64-bit pluma

--- TRIBmate-pluma/build.sh
+++ TRIBmate-pluma/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu pluma-1.24.2 -C "--disable-introspection LIBS=-lsocket"
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu -64only pluma-1.24.2 -C "--disable-introspection LIBS=-lsocket"
 ${THOME}/build/genpkg TRIBmate-pluma pluma-1.24.2
