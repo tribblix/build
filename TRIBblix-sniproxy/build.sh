90704c7e2b4c32d623e317112d35a705dfa02491
Peter Tribble <peter.tribble@gmail.com>
2023-04-08 16:49:21 +0100

    Update sniproxy 0.6.1

--- TRIBblix-sniproxy/build.sh
+++ TRIBblix-sniproxy/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-$THOME/build/unpack sniproxy-0.6.0
-cd sniproxy-0.6.0
+$THOME/build/unpack sniproxy-0.6.1
+cd sniproxy-0.6.1
 ./autogen.sh
-env LIBS="-lsocket -lnsl" CPPFLAGS="-D__EXTENSIONS__ -D_XPG4_2" CC="gcc -std=gnu99" ./configure --prefix=/opt/tribblix/sniproxy
+env LIBS="-lsocket -lnsl" CPPFLAGS="-D__EXTENSIONS__ -D_XPG4_2 -m64" LDFLAGS=-m64 ./configure --prefix=/opt/tribblix/sniproxy
 gmake -j 4
 ${THOME}/build/genpkg TRIBblix-sniproxy
 cd ..
