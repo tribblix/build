3a31b2ba76c3fad13ba04c4953f517b9846c6861
Peter Tribble <peter.tribble@gmail.com>
2023-04-10 15:53:04 +0100

    Update python3.9 virtualenv 20.21.0

--- TRIBvirtualenv-python-39/build.sh
+++ TRIBvirtualenv-python-39/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack virtualenv-20.4.7
-cd virtualenv-20.4.7
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBvirtualenv-python-39 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBvirtualenv-python-39 virtualenv-20.21.0
