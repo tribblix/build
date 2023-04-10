8a65b26f76c35511ffd1c4f7b8bd6630b43f2fad
Peter Tribble <peter.tribble@gmail.com>
2023-04-10 09:44:34 +0100

    Add vips 8.14.2

---
+++ TRIBlibvips/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# only current consumers are go-based, so restrict to x86 and 64-bit only
+#
+env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64only vips-8.14.2 -C -Dintrospection=false
+${THOME}/build/genpkg TRIBlibvips vips-8.14.2
