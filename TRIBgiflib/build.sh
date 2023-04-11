499f4f1bcb4591c3ee7de26b7d9edb46f4a16c28
Peter Tribble <peter.tribble@gmail.com>
2023-04-11 17:49:06 +0100

    Tidy up giflib

--- TRIBgiflib/build.sh
+++ TRIBgiflib/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-#
+# later versions appear to not be binary compatible (although
+# they should be)
 #
 ${THOME}/build/dobuild -64 giflib-5.1.4
 ${THOME}/build/genpkg TRIBgiflib giflib-5.1.4
