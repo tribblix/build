b4cc115dc06a86689ab5675a5ad34bc48b10ce9c
Peter Tribble <peter.tribble@gmail.com>
2023-04-11 17:53:00 +0100

    Update libwebp 1.3.0 (with compat)

--- TRIBlibwebp/build.sh
+++ TRIBlibwebp/build.sh
@@ -2,5 +2,9 @@
 #
 # webp
 #
+# we build and ship the old version for compatibility with applications
+# built against it; once they have all been rebuild it can be removed
+#
 ${THOME}/build/dobuild -64 libwebp-0.5.1
-${THOME}/build/genpkg TRIBlibwebp libwebp-0.5.1
+${THOME}/build/dobuild -64 libwebp-1.3.0 -C "--disable-gl"
+${THOME}/build/genpkg TRIBlibwebp libwebp-0.5.1 libwebp-1.3.0
