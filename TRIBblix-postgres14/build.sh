f8f2c15f714448734c0f1c5f9ed0fbd34a98aa0d
Peter Tribble <peter.tribble@gmail.com>
2023-04-13 19:53:41 +0100

    Deprecate postgres14

--- TRIBblix-postgres14/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# postgres 14
-#
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres14 postgresql-14.7 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres14 postgresql-14.7
