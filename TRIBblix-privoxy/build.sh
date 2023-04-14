fb70109e37fea75e017f28dd2a82b057249fad39
Peter Tribble <peter.tribble@gmail.com>
2023-04-14 13:43:39 +0100

    Update privoxy 3.0.34

--- TRIBblix-privoxy/build.sh
+++ TRIBblix-privoxy/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #
 # rename the tarball
-# mv privoxy-3.0.28-stable-src.tar.gz privoxy-3.0.28-stable.tar.gz
+# mv privoxy-3.0.34-stable-src.tar.gz privoxy-3.0.34-stable.tar.gz
 #
-${THOME}/build/dobuild privoxy-3.0.28-stable -P /opt/tribblix/privoxy
-${THOME}/build/genpkg TRIBblix-privoxy privoxy-3.0.28-stable
+# there's a preconf script to generate the configure script
+# the makefile will do it for you, but loses the arguments we
+# pass in that case
+#
+${THOME}/build/dobuild -64only privoxy-3.0.34-stable -P /opt/tribblix/privoxy
+${THOME}/build/genpkg TRIBblix-privoxy privoxy-3.0.34-stable
