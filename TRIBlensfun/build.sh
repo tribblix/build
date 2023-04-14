9d48d20250d702f30a2011b295debaa670930003
Peter Tribble <peter.tribble@gmail.com>
2023-04-14 21:37:40 +0100

    Update lensfun 0.3.3

--- TRIBlensfun/build.sh
+++ TRIBlensfun/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64 lensfun-0.3.2
-${THOME}/build/genpkg TRIBlensfun lensfun-0.3.2
+${THOME}/build/cmbuild -64 lensfun-0.3.3 -C -DBUILD_LENSTOOL=ON
+${THOME}/build/genpkg TRIBlensfun lensfun-0.3.3
