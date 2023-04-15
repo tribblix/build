8b326ab550ab850fb3260fffbf90cb52ea5d9661
Peter Tribble <peter.tribble@gmail.com>
2023-04-15 10:04:25 +0100

    Disable check for libdnet

--- TRIBlibdnet/build.sh
+++ TRIBlibdnet/build.sh
@@ -2,5 +2,5 @@
 #
 # there's a preconf file to patch out use of 'sun' as a variable name
 #
-${THOME}/build/dobuild -64 libdnet-libdnet-1.16.4 -C "ac_cv_dnet_linux_pf_packet=no"
+${THOME}/build/dobuild -64 libdnet-libdnet-1.16.4 -C "ac_cv_dnet_linux_pf_packet=no --disable-check"
 ${THOME}/build/genpkg TRIBlibdnet libdnet-libdnet-1.16.4
