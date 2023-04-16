7f2d6b4e4ecb00f98f51d77866bc1a4d60f6ee2d
Peter Tribble <peter.tribble@gmail.com>
2023-04-16 09:46:05 +0100

    Update libicu 68.2

--- TRIBlibicu/build.sh
+++ TRIBlibicu/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #
 # [in tarballs, rename the tarball to icu.tar.gz]
-mv icu4c-50_1_2-src.tgz icu.tar.gz
-env TRIBBLIX_CXXFLAGS=-D_XPG6 ${THOME}/build/dobuild -64 icu -S source
+# mv icu4c-68_2-src.tgz icu.tar.gz
+#
+# the old 50.1 shared libaries are copied in by the fixit step
+# for compatibility
+#
+${THOME}/build/dobuild -64 icu -S source
 rm -fr /tmp/ii
 cd icu-64bit/source
 gmake install DESTDIR=/tmp/ii
@@ -10,3 +14,4 @@ cd ../../icu/source
 gmake install DESTDIR=/tmp/ii
 cd ../..
 ${THOME}/build/create_pkg TRIBlibicu /tmp/ii
+rm -fr /tmp/ii
