b547c14b3bbab014116e1c2a8d992d2caa4a2bf4
Peter Tribble <peter.tribble@gmail.com>
2023-04-17 11:11:13 +0100

    Update mkdocs_material_extensions 1.1.1

--- TRIBmkdocs-material-python-311/build.sh
+++ TRIBmkdocs-material-python-311/build.sh
@@ -4,4 +4,4 @@
 # so package them together
 #
 
-${THOME}/build/pkg_pep518 TRIBmkdocs-material-python-311 mkdocs_material-8.5.7 mkdocs-material-extensions-1.0.3
+${THOME}/build/pkg_pep518 TRIBmkdocs-material-python-311 mkdocs_material-8.5.7 mkdocs_material_extensions-1.1.1
