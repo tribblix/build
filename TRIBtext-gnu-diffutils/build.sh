cf5f31119793bd7e36c934e2ac4d0c2177cd2ded
Peter Tribble <peter.tribble@gmail.com>
2023-04-19 18:36:32 +0100

    Update diffutils 3.9

--- TRIBtext-gnu-diffutils/build.sh
+++ TRIBtext-gnu-diffutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only -gnu diffutils-3.8 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-diffutils diffutils-3.8
+${THOME}/build/dobuild +64only -gnu diffutils-3.9 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-diffutils diffutils-3.9
