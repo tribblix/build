d8852c61b0dfac29b23c085cfb153098b2715243
Peter Tribble <peter.tribble@gmail.com>
2023-04-24 18:55:49 +0100

    Remove unused cicap

--- TRIBblix-cicap/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# c-icap
-#
-# see also TRIBblix-cicapmod
-#
-${THOME}/build/dobuild c_icap-0.5.6 -P /opt/tribblix/c-icap -C --with-ldap=no
-${THOME}/build/genpkg TRIBblix-cicap c_icap-0.5.6
