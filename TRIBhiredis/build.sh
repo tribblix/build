9de98642baebf21f8c105e5569ab90c74e634ba3
Peter Tribble <peter.tribble@gmail.com>
2023-04-24 18:22:57 +0100

    Update hiredis 1.1.0

--- TRIBhiredis/build.sh
+++ TRIBhiredis/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild hiredis-0.13.3
-cd hiredis-0.13.3
-rm -fr /tmp/rr
-gmake install PREFIX=/tmp/rr/usr
-${THOME}/build/create_pkg TRIBhiredis /tmp/rr
-cd ..
-rm -fr /tmp/rr
+${THOME}/build/cmbuild -64 hiredis-1.1.0
+${THOME}/build/genpkg TRIBhiredis hiredis-1.1.0
