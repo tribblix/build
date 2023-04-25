b88341a0f02743af4c5b05d582ccf3be40b0974e
Peter Tribble <peter.tribble@gmail.com>
2023-04-25 16:12:47 +0100

    Update wheel 0.40.0

--- TRIBwheel-python-311/build.sh
+++ TRIBwheel-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBwheel-python-311 wheel-0.38.4
+${THOME}/build/pkg_setup_py TRIBwheel-python-311 wheel-0.40.0
--- TRIBwheel-python-39/build.sh
+++ TRIBwheel-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBwheel-python-39 wheel-0.38.4
+${THOME}/build/pkg_setup_py TRIBwheel-python-39 wheel-0.40.0
