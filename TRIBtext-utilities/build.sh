5ebb62b086a50d8d6f02face9490aa86b9e18f6e
Peter Tribble <peter.tribble@gmail.com>
2023-04-26 11:58:11 +0100

    The hexdump man page was in the wrong place

--- TRIBtext-utilities/build.sh
+++ TRIBtext-utilities/build.sh
@@ -8,8 +8,8 @@ cd util-linux-2.37.4
 gmake hexdump
 rm -fr /tmp/tu
 gmake install-binPROGRAMS DESTDIR=/tmp/tu bin_PROGRAMS="hexdump"
-# manually otherwise it install all the other man pages
-mkdir -p /tmp/tu/usr/share/man/man/man1
-/usr/bin/ginstall -c -m 644 text-utils/hexdump.1 /tmp/tu/usr/share/man/man/man1
+# manually otherwise it installs all the other man pages
+mkdir -p /tmp/tu/usr/share/man/man1
+/usr/bin/ginstall -c -m 644 text-utils/hexdump.1 /tmp/tu/usr/share/man/man1
 ${THOME}/build/create_pkg TRIBtext-utilities /tmp/tu
 rm -fr /tmp/tu
