df587ddfeb92d3f2a17eec9516e20ff50e23a842
Peter Tribble <peter.tribble@gmail.com>
2023-04-26 17:48:40 +0100

    Update adwaita-icon-theme 3.38.0

--- TRIBadwaita-icon-theme/build.sh
+++ TRIBadwaita-icon-theme/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu adwaita-icon-theme-3.32.0
-${THOME}/build/genpkg TRIBadwaita-icon-theme adwaita-icon-theme-3.32.0
+${THOME}/build/dobuild -gnu adwaita-icon-theme-3.38.0
+${THOME}/build/genpkg TRIBadwaita-icon-theme adwaita-icon-theme-3.38.0
