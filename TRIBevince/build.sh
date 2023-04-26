63f1ffe7901c559125b276de1feee9b0a48e67e3
Peter Tribble <peter.tribble@gmail.com>
2023-04-26 17:26:26 +0100

    Update evince to 3.36.9, and enable PS and XPS

--- TRIBevince/build.sh
+++ TRIBevince/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu evince-3.30.3 -C "--without-keyring --without-gspell --disable-nautilus --enable-compile-warnings=minimum"
-${THOME}/build/genpkg TRIBevince evince-3.30.3
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -gnu evince-3.36.9 -C "--without-keyring --without-gspell --disable-nautilus --enable-compile-warnings=minimum --with-systemduserunitdir=no --enable-ps"
+${THOME}/build/genpkg TRIBevince evince-3.36.9
