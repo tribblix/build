20eea976866492331fc4ed9196c672e969b1576e
Peter Tribble <peter.tribble@gmail.com>
2023-04-26 10:46:16 +0100

    Update pekwm 0.3.0

--- TRIBpekwm/build.sh
+++ TRIBpekwm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64only pekwm-0.2.1
-${THOME}/build/genpkg TRIBpekwm pekwm-0.2.1
+${THOME}/build/cmbuild -64only pekwm-0.3.0
+${THOME}/build/genpkg TRIBpekwm pekwm-0.3.0
