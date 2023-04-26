f43680400d2ebe244c8980b73e4c396e47a91752
Peter Tribble <peter.tribble@gmail.com>
2023-04-26 11:04:20 +0100

    Update amiwm 0.22pl2

--- TRIBamiwm/build.sh
+++ TRIBamiwm/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild amiwm0.21pl2 -J 1
-cd amiwm0.21pl2
-gmake -n install DESTDIR=/tmp/a | sed 's:/usr:/tmp/a/usr:g' | sed -e s:/tmp/a/usr/bin/ginstall:/usr/bin/ginstall: -e s:/usr/man:/usr/share/man: >! doit.sh
+# need to set CC as it doesn't otherwise pass -m64 when linking
+#
+env CC="gcc -m64" ${THOME}/build/dobuild -64only amiwm0.22pl2 -J 1
+cd amiwm0.22pl2-64bit
+# install doesn't follow DESTDIR
+gmake -n install | sed 's:/usr:/tmp/a/usr:g' | sed -e s:/tmp/a/usr/bin/ginstall:/usr/bin/ginstall: -e s:/usr/man:/usr/share/man: -e s=:/tmp/a=:=g >! doit.sh
 rm -fr /tmp/a
 sh doit.sh
 ${THOME}/build/create_pkg TRIBamiwm /tmp/a
