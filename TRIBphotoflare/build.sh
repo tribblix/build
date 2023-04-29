70dd4716eb7f3c8fa8e93c7fee3c9aa180eaadad
Peter Tribble <peter.tribble@gmail.com>
2023-04-29 12:11:52 +0100

    Update photoflare 1.6.12

--- TRIBphotoflare/build.sh
+++ TRIBphotoflare/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
 #
-$THOME/build/unpack photoflare-1.6.10
-cd photoflare-1.6.10
+$THOME/build/unpack photoflare-1.6.12
+cd photoflare-1.6.12
 env PATH=/usr/versions/Qt-5/bin:$PATH qmake Photoflare.pro PREFIX=/usr INCLUDEPATH=/usr/include/ImageMagick-6 LIBS="-lMagick++-6.Q16 /usr/versions/gcc-10/lib/libgomp.a"
 gmake -j 6
 rm -fr /tmp/phf
