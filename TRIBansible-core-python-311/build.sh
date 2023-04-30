0a3cfa0e5de31ddf3437940f0a43a57825c2b03c
Peter Tribble <peter.tribble@gmail.com>
2023-04-30 17:24:15 +0100

    Update ansible-core 2.14.5 ansible 7.5.0

--- TRIBansible-core-python-311/build.sh
+++ TRIBansible-core-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBansible-core-python-311 ansible-core-2.14.4
+${THOME}/build/pkg_setup_py TRIBansible-core-python-311 ansible-core-2.14.5
--- TRIBansible-python-311/build.sh
+++ TRIBansible-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBansible-python-311 ansible-7.4.0
+${THOME}/build/pkg_setup_py TRIBansible-python-311 ansible-7.5.0
