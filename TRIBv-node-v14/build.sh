0482648c833d9ca4f92d22aaeca66f820d3b6ca5
Peter Tribble <peter.tribble@gmail.com>
2023-05-01 09:40:59 +0100

    Remove node v14

--- TRIBv-node-v14/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# 64-bit only
-#
-env CC=gcc CXX=g++ ${THOME}/build/dobuild node-v14.21.3 -P /usr/versions/node-v14 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v14 node-v14.21.3
