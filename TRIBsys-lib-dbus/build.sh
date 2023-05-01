514a59bfba2b3fc8a4919e8eae7025b9bd7ce63e
Peter Tribble <peter.tribble@gmail.com>
2023-05-01 18:49:33 +0100

    Update dbus 1.12.24

--- TRIBsys-lib-dbus/build.sh
+++ TRIBsys-lib-dbus/build.sh
@@ -6,11 +6,11 @@
 # manpages require xmlto to be installed
 # build requires autoconf-archive to be installed
 #
-${THOME}/build/dobuild -64 dbus-1.10.26 -C "--sysconfdir=/etc --localstatedir=/var --with-dbus-user=root --disable-static --libexecdir=/usr/lib --with-dbus-daemondir=/usr/lib"
+${THOME}/build/dobuild -64 dbus-1.12.24 -C "--sysconfdir=/etc --localstatedir=/var --with-dbus-user=root --disable-static --libexecdir=/usr/lib --with-dbus-daemondir=/usr/lib --disable-dependency-tracking"
 
 #
 # now build packages
 #
-${THOME}/build/genpkg TRIBsys-lib-dbus dbus-1.10.26
-${THOME}/build/genpkg TRIBsys-libdbus dbus-1.10.26
-${THOME}/build/genpkg TRIBsys-lib-dbus-dbus-x11 dbus-1.10.26
+${THOME}/build/genpkg TRIBsys-lib-dbus dbus-1.12.24
+${THOME}/build/genpkg TRIBsys-libdbus dbus-1.12.24
+${THOME}/build/genpkg TRIBsys-lib-dbus-dbus-x11 dbus-1.12.24
--- patches/dbus-1.12.24.pls
+++ patches/dbus-1.12.24.pls
@@ -1,8 +1,8 @@
 -p1 dbus-01-nosystemservice.patch
--p1 dbus-02-closefrom.patch
 -p1 dbus-03-consoleuser.patch
 -p1 dbus-04-cleanup-libs.patch
 -p1 dbus-05-compile.patch
 -p1 dbus-06-m4.patch
 -p1 dbus-07-remove-setuid-check.patch
 -p1 dbus-09-dbus-daemon-full-path.patch
+-p1 dbus-10-remove-inline-from-some-headers.patch
