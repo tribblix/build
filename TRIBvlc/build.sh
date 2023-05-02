1521e08f1366ec9773f2bb62d097e5f16cbfc0d8
Peter Tribble <peter.tribble@gmail.com>
2023-05-02 20:52:59 +0100

    Update vlc 3.0.18

--- TRIBvlc/build.sh
+++ TRIBvlc/build.sh
@@ -4,5 +4,5 @@
 # wants the extended mshgdr struct - -D_XPG4_2
 # doesn't build in 32-bit because the asm isn't handled right
 #
-env LIBS="-lsocket -lnsl -lssp_ns" TRIBBLIX_CFLAGS="-D_XPG4_2 -D__EXTENSIONS__" $THOME/build/dobuild -64only vlc-3.0.17.4 -C "--disable-lua --disable-live555 --disable-smbclient BUILDCC=gcc"
-${THOME}/build/genpkg TRIBvlc vlc-3.0.17.4
+env LIBS="-lsocket -lnsl -lssp_ns" TRIBBLIX_CFLAGS="-D_XPG4_2 -D__EXTENSIONS__" $THOME/build/dobuild -64only vlc-3.0.18 -C "--disable-lua --disable-live555 --disable-smbclient BUILDCC=gcc"
+${THOME}/build/genpkg TRIBvlc vlc-3.0.18
