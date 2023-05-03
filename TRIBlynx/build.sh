d2ec33509e897943ff3341fde72f53dcc94d6a51
Peter Tribble <peter.tribble@gmail.com>
2023-05-03 15:12:27 +0100

    Update lynx 2.9.0dev.12

--- TRIBlynx/build.sh
+++ TRIBlynx/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only lynx2.9.0dev.10 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines"
-${THOME}/build/genpkg -I "install install-help install-doc" TRIBlynx lynx2.9.0dev.10
+# the curses logic is a bit convoluted and needs a little help
+# otherwise you get missing symbol delete_screen
+#
+env TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only lynx2.9.0dev.12 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines --with-screen=ncurses --with-curses-dir=/usr/gnu"
+${THOME}/build/genpkg -I "install install-help install-doc" TRIBlynx lynx2.9.0dev.12
