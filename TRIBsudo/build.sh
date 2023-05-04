873c369fc5199882f8633bd27750e8841403365f
Peter Tribble <peter.tribble@gmail.com>
2023-05-04 15:02:46 +0100

    Update sudo 1.9.13p3

--- TRIBsudo/build.sh
+++ TRIBsudo/build.sh
@@ -2,8 +2,8 @@
 #
 # take most of the options and the bsm audit patches from OI
 #
-${THOME}/build/dobuild -64only sudo-1.9.12p2 -C "--disable-hardening --with-project --with-rundir=/var/run/sudo --with-pam --with-pam-login --disable-pam-session --with-tty-tickets --without-insults --without-lecture --with-ignore-dot --with-bsm-audit"
+${THOME}/build/dobuild -64only sudo-1.9.13p3 -C "--disable-hardening --with-project --with-rundir=/var/run/sudo --with-pam --with-pam-login --disable-pam-session --with-tty-tickets --without-insults --without-lecture --with-ignore-dot --with-bsm-audit"
 #
 # hack to avoid the install trying to chown the files
 #
-${THOME}/build/genpkg -I "install INSTALL_OWNER=" TRIBsudo sudo-1.9.12p2
+${THOME}/build/genpkg -I "install INSTALL_OWNER=" TRIBsudo sudo-1.9.13p3
