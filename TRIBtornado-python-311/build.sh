f38ef741dff35c9c30a15d2e604043837772bafb
Peter Tribble <peter.tribble@gmail.com>
2023-05-04 11:21:44 +0100

    Update tornado 6.3.1

--- TRIBtornado-python-311/build.sh
+++ TRIBtornado-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tornado-6.2
-cd tornado-6.2
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBtornado-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBtornado-python-311 tornado-6.3.1
