8f728f207c0d04af27522e35d937e7de7eff679e
Peter Tribble <peter.tribble@gmail.com>
2023-05-06 14:46:40 +0100

    Update mawk 1.3.4-20230404

--- TRIBmawk/build.sh
+++ TRIBmawk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only mawk-1.3.4-20200120
-${THOME}/build/genpkg TRIBmawk mawk-1.3.4-20200120
+${THOME}/build/dobuild -64only mawk-1.3.4-20230404
+${THOME}/build/genpkg TRIBmawk mawk-1.3.4-20230404
