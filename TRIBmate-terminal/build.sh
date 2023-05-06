861021779e074e5487b73ae0c91ab316e4b88b59
Peter Tribble <peter.tribble@gmail.com>
2023-05-06 10:13:37 +0100

    Update mate-terminal 1.26.0

--- TRIBmate-terminal/build.sh
+++ TRIBmate-terminal/build.sh
@@ -2,5 +2,5 @@
 #
 # 
 #
-env PATH=/usr/gnu/bin:$PATH $THOME/build/dobuild -64only mate-terminal-1.24.1
-${THOME}/build/genpkg TRIBmate-terminal mate-terminal-1.24.1
+env PATH=/usr/gnu/bin:$PATH $THOME/build/dobuild -64only mate-terminal-1.26.0
+${THOME}/build/genpkg TRIBmate-terminal mate-terminal-1.26.0
