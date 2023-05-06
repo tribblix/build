c6a4db79bf0e8d021b25d3640371dd05c6c48780
Peter Tribble <peter.tribble@gmail.com>
2023-05-06 11:14:11 +0100

    Add the freedesktop sound theme

---
+++ TRIBfreedesktop-sound-theme/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# download from
+# https://people.freedesktop.org/~mccann/dist/sound-theme-freedesktop-0.8.tar.bz2
+#
+${THOME}/build/dobuild -gnu sound-theme-freedesktop-0.8
+${THOME}/build/genpkg TRIBfreedesktop-sound-theme sound-theme-freedesktop-0.8
