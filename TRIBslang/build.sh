f537fc3dab81297085c50427be25ecd1d3ed22b4
Peter Tribble <peter.tribble@gmail.com>
2023-05-06 22:14:22 +0100

    Update slang 2.3.3

--- TRIBslang/build.sh
+++ TRIBslang/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 slang-2.3.2 -J 1
-${THOME}/build/genpkg TRIBslang slang-2.3.2
+${THOME}/build/dobuild -64 slang-2.3.3 -J 1
+${THOME}/build/genpkg TRIBslang slang-2.3.3
