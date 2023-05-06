8d09fa2d03c469896ee65227f52c7ea59c8e008f
Peter Tribble <peter.tribble@gmail.com>
2023-05-06 09:35:15 +0100

    Update caja 1.26.1, and 64-bit only

--- TRIBmate-caja/build.sh
+++ TRIBmate-caja/build.sh
@@ -2,5 +2,5 @@
 #
 # needs mate-desktop installing for the build
 #
-${THOME}/build/dobuild -64 -gnu caja-1.24.1 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-caja caja-1.24.1
+${THOME}/build/dobuild -64only -gnu caja-1.26.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-caja caja-1.26.1
