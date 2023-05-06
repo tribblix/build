50c4651be1f73d5239fd81708685c92d9f0d561b
Peter Tribble <peter.tribble@gmail.com>
2023-05-06 11:33:21 +0100

    Update mate-desktop 1.26.1 mate-common 1.26.0 libmatemixer 1.26.0, and enable oss

--- TRIBmate-desktop/build.sh
+++ TRIBmate-desktop/build.sh
@@ -3,7 +3,7 @@
 # bundle mate-common and libmatemixer in with this rather than creating a
 # separate package
 #
-${THOME}/build/dobuild -64 -gnu libmatemixer-1.24.1
-${THOME}/build/dobuild mate-common-1.24.2
-${THOME}/build/dobuild -64 -gnu mate-desktop-1.24.1 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-desktop mate-common-1.24.2 mate-desktop-1.24.1 libmatemixer-1.24.1
+${THOME}/build/dobuild -64 -gnu libmatemixer-1.26.0 -C --enable-oss
+${THOME}/build/dobuild mate-common-1.26.0
+${THOME}/build/dobuild -64 -gnu mate-desktop-1.26.1 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-desktop mate-common-1.26.0 mate-desktop-1.26.1 libmatemixer-1.26.0
