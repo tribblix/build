18a5552cffa9a86b189ba1c1f098bd7c89bc4447
Peter Tribble <peter.tribble@gmail.com>
2023-05-08 15:46:04 +0100

    Update jkstat 1.4.0

--- TRIBjkstat/build.sh
+++ TRIBjkstat/build.sh
@@ -8,11 +8,11 @@
 #
 # then copy the packages
 #
-mv /tmp/TRIBjkstat-i386.pkg ${THOME}/pkgs/TRIBjkstat.1.3.0.pkg
-mv /tmp/TRIBjkstat-sparc.pkg ${THOME}/pkgs.sparc/TRIBjkstat.1.3.0.pkg
-${THOME}/build/pkg2zap ${THOME}/pkgs/TRIBjkstat.1.3.0.pkg ${THOME}/pkgs
-${THOME}/build/pkg2zap ${THOME}/pkgs.sparc/TRIBjkstat.1.3.0.pkg ${THOME}/pkgs.sparc
-openssl md5 ${THOME}/pkgs/TRIBjkstat.1.3.0.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjkstat.1.3.0.zap.md5
-openssl md5 ${THOME}/pkgs.sparc/TRIBjkstat.1.3.0.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs.sparc/TRIBjkstat.1.3.0.zap.md5
-${THOME}/build/sign-file ${THOME}/pkgs/TRIBjkstat.1.3.0.zap
-${THOME}/build/sign-file ${THOME}/pkgs.sparc/TRIBjkstat.1.3.0.zap
+mv /tmp/TRIBjkstat-i386.pkg ${THOME}/pkgs/TRIBjkstat.1.4.0.pkg
+mv /tmp/TRIBjkstat-sparc.pkg ${THOME}/pkgs.sparc/TRIBjkstat.1.4.0.pkg
+${THOME}/build/pkg2zap ${THOME}/pkgs/TRIBjkstat.1.4.0.pkg ${THOME}/pkgs
+${THOME}/build/pkg2zap ${THOME}/pkgs.sparc/TRIBjkstat.1.4.0.pkg ${THOME}/pkgs.sparc
+openssl md5 ${THOME}/pkgs/TRIBjkstat.1.4.0.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjkstat.1.4.0.zap.md5
+openssl md5 ${THOME}/pkgs.sparc/TRIBjkstat.1.4.0.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs.sparc/TRIBjkstat.1.4.0.zap.md5
+${THOME}/build/sign-file ${THOME}/pkgs/TRIBjkstat.1.4.0.zap
+${THOME}/build/sign-file ${THOME}/pkgs.sparc/TRIBjkstat.1.4.0.zap
