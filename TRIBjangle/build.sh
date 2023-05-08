3d9e7ec96501c74689279aa0f2e3f45aa366c6e1
Peter Tribble <peter.tribble@gmail.com>
2023-05-08 15:54:37 +0100

    Update jangle 0.15

--- TRIBjangle/build.sh
+++ TRIBjangle/build.sh
@@ -8,8 +8,8 @@
 #
 # then copy the packages
 #
-mv /tmp/TRIBjangle.pkg /tmp/TRIBjangle.0.14.pkg
-${THOME}/build/pkg2zap /tmp/TRIBjangle.0.14.pkg ${THOME}/pkgs
-openssl md5 ${THOME}/pkgs/TRIBjangle.0.14.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjangle.0.14.zap.md5
-${THOME}/build/sign-file ${THOME}/pkgs/TRIBjangle.0.14.zap
-ln ${THOME}/pkgs/TRIBjangle.0.14.zap* ${THOME}/pkgs.sparc
+mv /tmp/TRIBjangle.pkg /tmp/TRIBjangle.0.15.pkg
+${THOME}/build/pkg2zap /tmp/TRIBjangle.0.15.pkg ${THOME}/pkgs
+openssl md5 ${THOME}/pkgs/TRIBjangle.0.15.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjangle.0.15.zap.md5
+${THOME}/build/sign-file ${THOME}/pkgs/TRIBjangle.0.15.zap
+ln ${THOME}/pkgs/TRIBjangle.0.15.zap* ${THOME}/pkgs.sparc
