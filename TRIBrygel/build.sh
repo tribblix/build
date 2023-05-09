0a33936a67fd78f7e15a65030113e5b814a7b5a3
Peter Tribble <peter.tribble@gmail.com>
2023-05-09 14:19:40 +0100

    Rebuild rygel against newer tracker

--- TRIBrygel/build.sh
+++ TRIBrygel/build.sh
@@ -2,5 +2,5 @@
 #
 # rygel [needs gupnp gupnp-av gupnp-dlna vala libmediaart tracker]
 #
-${THOME}/build/dobuild -gnu rygel-0.36.1 -C --disable-introspection
+${THOME}/build/dobuild -gnu rygel-0.36.1 -C "--disable-introspection --without-ui"
 ${THOME}/build/genpkg TRIBrygel rygel-0.36.1
