a33d51c37440e181bd47f400fca4ffbf89a28dad
Peter Tribble <peter.tribble@gmail.com>
2023-05-10 15:04:27 +0100

    Update log4cplus 2.0.8

--- TRIBlog4cplus/build.sh
+++ TRIBlog4cplus/build.sh
@@ -2,5 +2,5 @@
 #
 # needed by kea
 #
-${THOME}/build/dobuild log4cplus-1.2.0
-${THOME}/build/genpkg TRIBlog4cplus log4cplus-1.2.0
+${THOME}/build/dobuild -64 log4cplus-2.0.8
+${THOME}/build/genpkg TRIBlog4cplus log4cplus-2.0.8
