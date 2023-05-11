4322e64f395d818e0089f74f017331b1b8097c1f
Peter Tribble <peter.tribble@gmail.com>
2023-05-11 16:11:47 +0100

    Update postgres 13.11 15.3

--- TRIBblix-postgres13/build.sh
+++ TRIBblix-postgres13/build.sh
@@ -5,5 +5,5 @@
 # this is 64-bit, unlike prior versions
 # python 3.9 and later should use this version
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres13 postgresql-13.10 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres13 postgresql-13.10
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres13 postgresql-13.11 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres13 postgresql-13.11
