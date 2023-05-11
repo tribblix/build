f48bd669cfd572dcfb41529a1651fa95c880a6a8
Peter Tribble <peter.tribble@gmail.com>
2023-05-11 12:53:55 +0100

    Add a compatibility layer for Xsun on sparc

---
+++ TRIBopenxsun-compat/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# on sparc, Xsun is shipped in the SUNWopenxsun-server package
+# this package supplies the missing files, some of which were
+# missing entirely and some of which were in other obsoleted
+# packages
+#
+# all the work done by the fixit script, files are in ../patches/openXsun
+#
+${THOME}/build/genpkg TRIBopenxsun-compat
