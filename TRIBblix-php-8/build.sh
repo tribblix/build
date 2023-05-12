ad1acf0e8dff705b7fcb9b151a0a2bee74d5d9c2
Peter Tribble <peter.tribble@gmail.com>
2023-05-12 10:57:56 +0100

    Update PHP 8.1.19

--- TRIBblix-php-8/build.sh
+++ TRIBblix-php-8/build.sh
@@ -13,8 +13,8 @@
 # needs a tsrm patch to avoid crashes
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-8.1.18
-cd php-8.1.18
+${THOME}/build/unpack php-8.1.19
+cd php-8.1.19
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
