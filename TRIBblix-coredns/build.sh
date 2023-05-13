a141569a6b8feef0ba0ae77b7e47cdfd1a709f5f
Peter Tribble <peter.tribble@gmail.com>
2023-05-13 16:52:04 +0100

    Update coredns 1.10.1

--- TRIBblix-coredns/build.sh
+++ TRIBblix-coredns/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# requires at least go1.18
+# requires at least go1.20
 #
-$THOME/build/unpack coredns-1.10.0
-cd coredns-1.10.0
-env PATH=/usr/versions/go-1.18/bin:$PATH gmake
+$THOME/build/unpack coredns-1.10.1
+cd coredns-1.10.1
+env PATH=/usr/versions/go-1.20/bin:$PATH gmake
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/coredns/bin
