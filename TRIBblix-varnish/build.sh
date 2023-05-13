281a73aa0728a107903c8e775b7df23463774915
Peter Tribble <peter.tribble@gmail.com>
2023-05-13 19:49:07 +0100

    Update varnish 6.0.11

--- TRIBblix-varnish/build.sh
+++ TRIBblix-varnish/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 
-${THOME}/build/dobuild -64only varnish-6.0.10 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-3.9/bin/rst2man.py --with-rst2html=/usr/versions/python-3.9/bin/rst2html.py --with-sphinx-build=/usr/versions/python-3.9/bin/sphinx-build"
-${THOME}/build/genpkg TRIBblix-varnish varnish-6.0.10
+${THOME}/build/dobuild -64only varnish-6.0.11 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-3.11/bin/rst2man.py --with-rst2html=/usr/versions/python-3.11/bin/rst2html.py --with-sphinx-build=/usr/versions/python-3.11/bin/sphinx-build"
+${THOME}/build/genpkg TRIBblix-varnish varnish-6.0.11
