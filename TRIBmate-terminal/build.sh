4e4bb480c49d217c73d06731c0f0cfddf808d04d
Peter Tribble <peter.tribble@gmail.com>
2023-05-14 20:38:53 +0100

    Note additional dependency for mate-terminal

--- TRIBmate-terminal/build.sh
+++ TRIBmate-terminal/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# 
+# this depends on mate-desktop, via the file
+# /usr/share/glib-2.0/schemas/org.mate.interface.gschema.xml
 #
 env PATH=/usr/gnu/bin:$PATH $THOME/build/dobuild -64only mate-terminal-1.26.0
 ${THOME}/build/genpkg TRIBmate-terminal mate-terminal-1.26.0
