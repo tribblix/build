cdd6d1f3c4a55dbbcd46fe95def7069ca71670af
Peter Tribble <peter.tribble@gmail.com>
2023-05-17 08:28:46 +0100

    Update the ffmpeg build

--- TRIBffmpeg/build.sh
+++ TRIBffmpeg/build.sh
@@ -2,13 +2,9 @@
 #
 # yasm (fallback to nasm) is a build-time dependency - zap install yasm
 #
-# this is a split build, as the SONAME carries version information
-# therefore, the 32-bit build is 2.8 for compatibility with old consumers
-# while the 64-bit build can be newer
+# this is a split build, the binaries are 64-bit, overwriting the 32-bit ones
 #
-# disable ffserver in the 32-bit build, it's been removed
-# the binaries are 64-bit, overwriting the 32-bit ones
-#
-env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild ffmpeg-2.8.20 -C "--enable-shared --disable-static --disable-ffserver"
+env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild ffmpeg-4.4.2 -C "--strip=/usr/gnu/bin/strip --enable-shared --disable-static"
+mv ffmpeg-4.4.2 ffmpeg-4.4.2-32
 env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -64only ffmpeg-4.4.2 -C "--strip=/usr/gnu/bin/strip --enable-shared --disable-static"
-${THOME}/build/genpkg TRIBffmpeg ffmpeg-2.8.20 ffmpeg-4.4.2
+${THOME}/build/genpkg TRIBffmpeg ffmpeg-4.4.2-32 ffmpeg-4.4.2
