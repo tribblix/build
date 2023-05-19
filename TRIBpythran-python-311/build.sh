21788fff63d6615163958c570b3dfb4162c9e346
Peter Tribble <peter.tribble@gmail.com>
2023-05-19 08:40:40 +0100

    Update pythran 0.13.1

--- TRIBpythran-python-311/build.sh
+++ TRIBpythran-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpythran-python-311 pythran-0.12.1
+${THOME}/build/pkg_setup_py TRIBpythran-python-311 pythran-0.13.1
--- patches/pythran-0.12.1.pls
+++
@@ -1,1 +0,0 @@
--p1 pythran-exp.patch
