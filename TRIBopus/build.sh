b679ee32d7ea04295b698a709d661a831ea5a44d
Peter Tribble <peter.tribble@gmail.com>
2023-05-20 19:59:15 +0100

    Update opus 1.4

--- TRIBopus/build.sh
+++ TRIBopus/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 opus-1.3.1
-${THOME}/build/genpkg TRIBopus opus-1.3.1
+${THOME}/build/dobuild -64 opus-1.4 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBopus opus-1.4
