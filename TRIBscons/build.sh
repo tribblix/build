b717f6e3c19c1fa4236323daf760d8bb1436d825
Peter Tribble <peter.tribble@gmail.com>
2023-05-21 19:56:10 +0100

    Update SCons 4.5.2

--- TRIBscons/build.sh
+++ TRIBscons/build.sh
@@ -2,11 +2,4 @@
 #
 # (built in python)
 #
-${THOME}/build/unpack SCons-4.3.0
-cd SCons-4.3.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/sc1
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/sc1
-${THOME}/build/create_pkg TRIBscons /tmp/sc1
-cd ..
-rm -fr /tmp/sc1
+${THOME}/build/pkg_setup_py -v 39 TRIBscons SCons-4.5.2
