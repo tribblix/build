91f87b7e883c8442ff53ec8da12299da220e1581
Peter Tribble <peter.tribble@gmail.com>
2023-05-21 19:38:47 +0100

    Update pg_activity 3.4.0

--- TRIBpgactivity/build.sh
+++ TRIBpgactivity/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pg_activity-3.0.3
-cd pg_activity-3.0.3
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpgactivity /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 -v 39 TRIBpgactivity pg_activity-3.4.0
