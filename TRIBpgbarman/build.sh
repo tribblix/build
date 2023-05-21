9dad5bbbf8745a1149185b1330d14be8dda023a3
Peter Tribble <peter.tribble@gmail.com>
2023-05-21 20:03:31 +0100

    Update Barman 3.5.0

--- TRIBpgbarman/build.sh
+++ TRIBpgbarman/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack barman-3.4.0
-cd barman-3.4.0
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBpgbarman /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py -v 39 TRIBpgbarman barman-3.5.0
