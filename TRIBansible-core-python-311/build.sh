ff5578873c37548dc74377434ac9ec115d8cf3e8
Peter Tribble <peter.tribble@gmail.com>
2023-05-24 09:36:42 +0100

    Update ansible-core 2.15.0, and drop python39

--- TRIBansible-core-python-311/build.sh
+++ TRIBansible-core-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBansible-core-python-311 ansible-core-2.14.5
+${THOME}/build/pkg_setup_py TRIBansible-core-python-311 ansible-core-2.15.0
--- TRIBansible-core-python-39/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/pkg_setup_py TRIBansible-core-python-39 ansible-core-2.14.5
