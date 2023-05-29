1323c8bba7243fe97ca12c02c8699dc61af27470
Peter Tribble <peter.tribble@gmail.com>
2023-05-29 18:00:36 +0100

    Rebuild crtsh with go119

--- TRIBcrtsh/build.sh
+++ TRIBcrtsh/build.sh
@@ -3,17 +3,20 @@
 
 mkdir c
 cd c
-setenv GOPATH `pwd`
 
-go get -u github.com/knqyf263/crtsh
+git clone https://github.com/knqyf263/crtsh
+cd crtsh
+env GOPATH=`pwd`/.. go install
+cd ..
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
 cp bin/crtsh /tmp/ee/usr/bin
 mkdir -p /tmp/ee/usr/share/doc/crtsh
-cp src/github.com/knqyf263/crtsh/LICENSE src/github.com/knqyf263/crtsh/README.md /tmp/ee/usr/share/doc/crtsh
+cp crtsh/LICENSE crtsh/README.md /tmp/ee/usr/share/doc/crtsh
 ${THOME}/build/create_pkg TRIBcrtsh /tmp/ee
 rm -fr /tmp/ee
 
 cd ..
+chmod -R u+rw c
 rm -fr c
