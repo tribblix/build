5f7d9d02fffd65516ea0d9fa81c3cb9e994284b7
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 18:45:43 +0100

    Add pngquant 2.18.0

---
+++ TRIBpngquant/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# later versions (3.x) are rust-based, stay with the 2.x branch
+# for wider portability
+#
+# rename the download
+# mv pngquant-2.18.0-src.tar.gz pngquant-2.18.0.tar.gz
+#
+# it's not an autoconf configure, but it understands the arguments that
+# dobuild uses well enough
+#
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only pngquant-2.18.0 -C --with-libimagequant
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBpngquant pngquant-2.18.0
