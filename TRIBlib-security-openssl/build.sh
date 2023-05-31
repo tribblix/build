98c76d270a154a22be1c7e469d22b1db6d8ef031
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 22:09:56 +0100

    Update openssl 1.1.1u

--- TRIBlib-security-openssl/build.sh
+++ TRIBlib-security-openssl/build.sh
@@ -7,26 +7,26 @@
 # the fixit script copies in the 1.0 libraries for binary
 # compatibility
 #
-${THOME}/build/unpack -64 openssl-1.1.1t
-cd openssl-1.1.1t
+${THOME}/build/unpack -64 openssl-1.1.1u
+cd openssl-1.1.1u
 env __CNF_LDFLAGS="-z aslr -z ignore" ./Configure solaris-x86-gcc shared threads zlib --api=1.0.0 --prefix=/usr
 sed -i s:pod2man:/usr/perl5/bin/pod2man: util/process_docs.pl
-gmake -j 4
+gmake -j 6
 cd ..
 #
 # you might have thought that asking for a solaris64-x86_64 build
 # would actually do the right thing, but no ...
 #
-cd openssl-1.1.1t-64bit
+cd openssl-1.1.1u-64bit
 env __CNF_CFLAGS=-m64 __CNF_LDFLAGS="-m64 -z aslr -z ignore" ./Configure solaris64-x86_64-gcc shared threads zlib --api=1.0.0 --prefix=/usr --libdir=lib/amd64
 sed -i s:pod2man:/usr/perl5/bin/pod2man: util/process_docs.pl
-gmake -j 4
+gmake -j 6
 cd ..
 
 #
 # much easier now install understands DESTDIR
 #
-${THOME}/build/genpkg TRIBlib-security-openssl openssl-1.1.1t
+${THOME}/build/genpkg TRIBlib-security-openssl openssl-1.1.1u
 
 #
 # The sparc configure steps are:
