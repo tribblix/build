732a599d916976ac054454a56dd6849c645fff8c
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 15:50:38 +0100

    Add libimagequant 2.18.0

---
+++ TRIBlibimagequant/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# later versions (4.x) are rust-based, stay with the 2.x branch
+# for wider portability
+#
+# it's not an autoconf configure, but it understands the arguments that
+# dobuild uses well enough
+#
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64 libimagequant-2.18.0
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBlibimagequant libimagequant-2.18.0
