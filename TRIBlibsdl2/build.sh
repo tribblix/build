0cb8bb4400ab5487e51fd260b24f75bac814d7d5
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 13:47:26 +0100

    Update libSDL2 2.26.5

--- TRIBlibsdl2/build.sh
+++ TRIBlibsdl2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 SDL2-2.0.4
-${THOME}/build/genpkg TRIBlibsdl2 SDL2-2.0.4
+env TRIBBLIX_LDFLAGS="-L/usr/lib/mesa -R/usr/lib/mesa" ${THOME}/build/dobuild -64 SDL-release-2.26.5 -C SORT=/usr/gnu/bin/sort
+${THOME}/build/genpkg TRIBlibsdl2 SDL-release-2.26.5
---
+++ patches/SDL_config.h
@@ -0,0 +1,5 @@
+#if defined(_LP64) || defined (__amd64) || defined (__sparcv9)
+#include <SDL2/SDL_config-64.h>
+#else
+#include <SDL2/SDL_config-32.h>
+#endif
