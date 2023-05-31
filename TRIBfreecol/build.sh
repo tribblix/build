00a949075df575df70d5ec30096f773ccbf88241
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 19:58:02 +0100

    Update freecol 1.1.0

--- TRIBfreecol/build.sh
+++ TRIBfreecol/build.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 #
-# Run the installer. It places files into ~/freecol
-# Then you convert those into a package
+# download the zip and just run from that
 #
-rm -fr /tmp/jj
-mkdir -p /tmp/jj/usr/bin /tmp/jj/usr/share/applications
-cd
-tar cf - freecol | (cd /tmp/jj/usr/share ; tar xf -)
-cat /tmp/jj/usr/share/freecol/freecol | sed 's:^BINDIR.*:BINDIR=/usr/share/freecol:' > /tmp/jj/usr/bin/freecol
-chmod 755 /tmp/jj/usr/bin/freecol
-cp /tmp/jj/usr/share/freecol/freecol.desktop /tmp/jj/usr/share/applications
-sed -i s:data/freecol.png:/usr/share/freecol/data/freecol.png: /tmp/jj/usr/share/applications/freecol.desktop
-rm -fr /tmp/jj/usr/share/freecol/Uninstaller
-${THOME}/build/create_pkg TRIBfreecol /tmp/jj
-rm -fr /tmp/jj
+${THOME}/build/pkg_tarball TRIBfreecol freecol-1.1.0
