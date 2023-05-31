4d466902bac79bfdba157f43d30a020fe49fb835
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 18:23:55 +0100

    Add tuxpaint stamps

---
+++ TRIBtuxpaint-stamps/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# just a Makefile, set variables to bypass its settings
+#
+$THOME/build/unpack tuxpaint-stamps-2023.05.19
+cd tuxpaint-stamps-2023.05.19
+
+rm -fr /tmp/txps
+# needs the trailing /
+gmake install-all DATA_PREFIX=/tmp/txps/usr/share/tuxpaint/
+
+${THOME}/build/create_pkg TRIBtuxpaint-stamps /tmp/txps
+rm -fr /tmp/txps
