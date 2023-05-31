3b93840b49a3ac14ea1af6f5c3d586ed53809133
Peter Tribble <peter.tribble@gmail.com>
2023-05-31 10:41:08 +0100

    Need to explicitly specify gcc for plan9 on sparc

--- TRIBplan9/build.sh
+++ TRIBplan9/build.sh
@@ -3,8 +3,14 @@
 # no versioned releases, built from master which had its latest
 # commit on Mar 31 2023, hence the version number
 #
+#
 ${THOME}/build/unpack plan9port-master
 cd plan9port-master
+#
+# need this on sparc, x86 defaults to gcc already
+#
+echo CC9=gcc > LOCAL.config
+#
 env PATH=/usr/gnu/bin:$PATH PLAN9_TARGET=/usr/versions/plan9 ./INSTALL -b
 env PATH=/usr/gnu/bin:$PATH PLAN9_TARGET=/usr/versions/plan9 ./INSTALL -c
 cd ..
