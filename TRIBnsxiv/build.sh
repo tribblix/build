160bc7e3af00499ef3a90011e027ecff530055f5
Peter Tribble <peter.tribble@gmail.com>
2023-06-02 17:03:16 +0100

    Update nsxiv v31

--- TRIBnsxiv/build.sh
+++ TRIBnsxiv/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #
+# you may have to repackage the tarball to get the correct directory name in it
 # there's a preconf script to change the PREFIX
 #
-${THOME}/build/dobuild nsxiv-29 -M "CC=gcc HAVE_INOTIFY=0 HAVE_LIBWEBP=0"
-cd nsxiv-29
+env LDLIBS=-m64 CPPFLAGS=-m64 ${THOME}/build/dobuild nsxiv-31 -M "CC=gcc HAVE_INOTIFY=0"
+cd nsxiv-31
 rm -fr /tmp/nlr
-gmake install-all DESTDIR=/tmp/nlr CC=gcc HAVE_INOTIFY=0 HAVE_LIBWEBP=0
+env LDLIBS=-m64 CPPFLAGS=-m64 gmake install-all DESTDIR=/tmp/nlr CC=gcc HAVE_INOTIFY=0
 cd ..
 ${THOME}/build/create_pkg TRIBnsxiv /tmp/nlr
 rm -fr /tmp/nlr
