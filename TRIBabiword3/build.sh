fdb1f94986db8b258ee4a9f87c848434d5bafcfa
Peter Tribble <peter.tribble@gmail.com>
2023-06-02 16:10:24 +0100

    Rebuild abiword 64-bit

--- TRIBabiword3/build.sh
+++ TRIBabiword3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild abiword-3.0.5 -C "--disable-dependency-tracking --enable-clipart --enable-templates"
+${THOME}/build/dobuild -64only abiword-3.0.5 -C "--disable-dependency-tracking --enable-clipart --enable-templates"
 ${THOME}/build/genpkg TRIBabiword3 abiword-3.0.5
