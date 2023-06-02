e04254e084282908c76a08d27a9bcda575adabde
Peter Tribble <peter.tribble@gmail.com>
2023-06-02 15:56:09 +0100

    Update tmux 3.3a

--- TRIBtmux/build.sh
+++ TRIBtmux/build.sh
@@ -3,7 +3,7 @@
 # tmux/libevent
 # built a la omnios
 # [no, I don't understand how --disable-static works...]
-${THOME}/build/dobuild libevent-2.1.11-stable -C "--disable-static --disable-libevent-install"
-env CPPFLAGS="-I`pwd`/libevent-2.1.11-stable/include -I`pwd`/libevent-2.1.11-stable/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.1.11-stable/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild tmux-2.9
-${THOME}/build/genpkg TRIBtmux tmux-2.9
-rm -fr libevent-2.1.11-stable
+${THOME}/build/dobuild +64only libevent-2.1.11-stable -C "--disable-static --disable-libevent-install"
+env CPPFLAGS="-I`pwd`/libevent-2.1.11-stable-64bit/include -I`pwd`/libevent-2.1.11-stable-64bit/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.1.11-stable-64bit/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild -64only tmux-3.3a
+${THOME}/build/genpkg TRIBtmux tmux-3.3a
+rm -fr libevent-2.1.11-stable-64bit
