45dd4b028691606997e9540104a1c1a337c9ce0a
Peter Tribble <peter.tribble@gmail.com>
2023-06-04 18:11:56 +0100

    Update vim 9.0

--- TRIBeditor-vim-gtk2/build.sh
+++ TRIBeditor-vim-gtk2/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild vim-8.1.1523 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix"
-${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-8.1.1523
+env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vim90 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix --disable-canberra" -M -k
+${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim90
 #
 # Athena build looks like:
-# ${THOME}/build/dobuild vim-8.1.1523 -C "--enable-gui=athena --with-compiledby=ptribble@tribblix"
+# ${THOME}/build/dobuild vim90 -C "--enable-gui=athena --with-compiledby=ptribble@tribblix"
 #
--- TRIBeditor-vim/build.sh
+++ TRIBeditor-vim/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
+# rename the download
+# mv vim-9.0.tar.bz2 vim90.tar.bz2
+#
 # we ship gvim and the desktop integration pieces in the
 # TRIBeditor-vim-gtk2 package
 #
 # Now most of the space is actually the shared stuff
 # Wrapper? [exec -a $0 /usr/bin/vim.variant "$@"]
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild vim-8.1.1523 -C "--with-compiledby=ptribble@tribblix --enable-gui=no --without-x"
-${THOME}/build/genpkg TRIBeditor-vim vim-8.1.1523
+env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vim90 -C "--with-compiledby=ptribble@tribblix --enable-gui=no --without-x --disable-canberra" -M -k
+${THOME}/build/genpkg TRIBeditor-vim vim90
