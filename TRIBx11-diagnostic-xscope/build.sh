7fd8ec38d9491fd4d50ab8bf9c46c6c04020be6f
Peter Tribble <peter.tribble@gmail.com>
2023-06-04 12:20:14 +0100

    Update xscope 1.4.4

--- TRIBx11-diagnostic-xscope/build.sh
+++ TRIBx11-diagnostic-xscope/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xscope-1.4.3 -C "CPPFLAGS=-DFD_SETSIZE=256"
-${THOME}/build/genpkg TRIBx11-diagnostic-xscope xscope-1.4.3
+${THOME}/build/dobuild -64only xscope-1.4.4 -C "CPPFLAGS=-DFD_SETSIZE=256"
+${THOME}/build/genpkg TRIBx11-diagnostic-xscope xscope-1.4.4
