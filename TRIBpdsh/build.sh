a36ed46b574cd25dac64331ba468c2184ced0e0d
Peter Tribble <peter.tribble@gmail.com>
2023-06-04 13:32:57 +0100

    Update pdsh 2.34

--- TRIBpdsh/build.sh
+++ TRIBpdsh/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 #
-${THOME}/build/dobuild pdsh-2.33 -C "--with-ssh --without-torgue --without-slurm --with-rcmd-rank-list=ssh,rsh,exec"
-${THOME}/build/genpkg TRIBpdsh pdsh-2.33
+${THOME}/build/dobuild -64only pdsh-2.34 -C "--with-ssh --without-torgue --without-slurm --with-rcmd-rank-list=ssh,rsh,exec"
+${THOME}/build/genpkg TRIBpdsh pdsh-2.34
