0c4acbe0f0f2309eabffe9d8beda4c34e85e4728
Peter Tribble <peter.tribble@gmail.com>
2023-06-06 18:11:05 +0100

    Rebuild WindowMaker 64-bit

--- TRIBwmaker/build.sh
+++ TRIBwmaker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild WindowMaker-0.95.9 -C --disable-magick
+${THOME}/build/dobuild -64only WindowMaker-0.95.9 -C --disable-magick
 ${THOME}/build/genpkg TRIBwmaker WindowMaker-0.95.9
