8ebb10ee2eaa9e8f79fb43e05343b555ca5ec573
Peter Tribble <peter.tribble@gmail.com>
2023-06-08 13:14:05 +0100

    Rebuild libsmooth to add 64-bit

--- TRIBlibsmooth/build.sh
+++ TRIBlibsmooth/build.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 #
-${THOME}/build/unpack smooth-0.9.10
+${THOME}/build/unpack -64 smooth-0.9.10
 cd smooth-0.9.10
-gmake -j 4 CC=gcc prefix=/usr
+gmake -j 4 CC=gcc CXX=g++ prefix=/usr
 cd ..
-${THOME}/build/genpkg -M "CC=gcc prefix=/usr" TRIBlibsmooth smooth-0.9.10
+cd smooth-0.9.10-64bit
+gmake -j 4 CC="gcc -m64" CXX="g++ -m64" prefix=/usr libdir=/usr/lib/`$THOME/build/getarch`
+cd ..
+
+rm -fr /tmp/smth
+cd smooth-0.9.10
+gmake install CC=gcc CXX=g++ prefix=/usr DESTDIR=/tmp/smth
+cd ..
+cd smooth-0.9.10-64bit
+gmake install CC="gcc -m64" CXX="g++ -m64" prefix=/usr libdir=/usr/lib/`$THOME/build/getarch` DESTDIR=/tmp/smth
+cd ..
+
+${THOME}/build/create_pkg TRIBlibsmooth /tmp/smth
+rm -fr /tmp/smth
