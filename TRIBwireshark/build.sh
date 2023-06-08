9191b007e5c3a66347fec376755b741cb998a3ce
Peter Tribble <peter.tribble@gmail.com>
2023-06-08 08:41:51 +0100

    Update wireshark 3.6.14

--- TRIBwireshark/build.sh
+++ TRIBwireshark/build.sh
@@ -2,9 +2,8 @@
 #
 # this builds both the main wireshark package and the qt package
 #
-# need to explicitly tell it which qt as guessing is unreliable
-# as of 2.2, needs a little extra help with kerberos
+# needs a little extra help with kerberos
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/kerberosv5 PATH=${PATH}:/usr/versions/Qt-5/bin:/usr/perl5/5.34/bin ${THOME}/build/dobuild -gnu wireshark-2.6.20 -C "--with-qt=5 -without-lua --with-gtk=3 --without-maxminddb"
-${THOME}/build/genpkg TRIBwireshark wireshark-2.6.20
-${THOME}/build/genpkg TRIBwireshark-qt wireshark-2.6.20
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" PATH=${PATH}:/usr/versions/Qt-5/bin:/usr/perl5/5.34/bin ${THOME}/build/cmbuild -gnu wireshark-3.6.14
+${THOME}/build/genpkg TRIBwireshark wireshark-3.6.14
+${THOME}/build/genpkg TRIBwireshark-qt wireshark-3.6.14
