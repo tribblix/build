8d1fc1035acb84a523ce6efbde6cfe92930d5cb1
Peter Tribble <peter.tribble@gmail.com>
2023-06-10 09:16:31 +0100

    Extend libboca to 64-bit

--- TRIBlibboca/build.sh
+++ TRIBlibboca/build.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 #
-${THOME}/build/unpack boca-1.0.7
+${THOME}/build/unpack -64 boca-1.0.7
 cd boca-1.0.7
 sed -i s:winegcc:XXwinegccXX: Makefile-options
 gmake -j 4 CC=gcc prefix=/usr
 cd ..
-${THOME}/build/genpkg -M "CC=gcc prefix=/usr" TRIBlibboca boca-1.0.7
+cd boca-1.0.7-64bit
+sed -i s:winegcc:XXwinegccXX: Makefile-options
+gmake -j 4 CC="gcc -m64" CXX="g++ -m64" prefix=/usr libdir=/usr/lib/`$THOME/build/getarch`
+cd ..
+
+rm -fr /tmp/iboca
+cd boca-1.0.7
+gmake install CC=gcc prefix=/usr DESTDIR=/tmp/iboca
+cd ..
+cd boca-1.0.7-64bit
+gmake install CC="gcc -m64" CXX="g++ -m64" prefix=/usr libdir=/usr/lib/`$THOME/build/getarch` DESTDIR=/tmp/iboca
+cd ..
+
+${THOME}/build/create_pkg TRIBlibboca /tmp/iboca
+rm -fr /tmp/iboca
