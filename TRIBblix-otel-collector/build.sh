bbf1018899de70d43ce926cdecd9a3d22377e742
Peter Tribble <peter.tribble@gmail.com>
2023-06-11 09:56:42 +0100

    Update OpenTelemetry collector 0.79.0

--- TRIBblix-otel-collector/build.sh
+++ TRIBblix-otel-collector/build.sh
@@ -2,16 +2,12 @@
 #
 git clone https://github.com/open-telemetry/opentelemetry-collector
 cd opentelemetry-collector
-#
-# edit pkg/errors/abort.go
-# add golang.org/x/sys/unix to import
-# Getpid and Getpgid are unix not syscall
-#
-gmake otelcorecol
+
+env PATH=/usr/versions/go-1.20/bin:$PATH gmake otelcorecol
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/otel/bin
-cp bin/otelcorecol_solaris_amd64 /tmp/ee/opt/tribblix/otel/bin/otelcorecol
+cp bin/otelcorecol_illumos_amd64 /tmp/ee/opt/tribblix/otel/bin/otelcorecol
 cp README.md /tmp/ee/opt/tribblix/otel
 ${THOME}/build/create_pkg TRIBblix-otel-collector /tmp/ee
 rm -fr /tmp/ee
