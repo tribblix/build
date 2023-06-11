76674476896b5da036e37a8b16b77e739d6b5de2
Peter Tribble <peter.tribble@gmail.com>
2023-06-11 11:44:37 +0100

    Update minio server

--- TRIBminio-server/build.sh
+++ TRIBminio-server/build.sh
@@ -4,7 +4,11 @@ mkdir ~/ud/c
 cd ~/ud/c
 
 #
-env GOPATH=`pwd` go install github.com/minio/minio@latest
+# containerd/console is broken, so pick a tag that works
+# https://github.com/minio/minio/issues/17398
+# https://github.com/containerd/console/issues/75
+#
+env GOPATH=`pwd` PATH=/usr/versions/go-1.20/bin:$PATH go install github.com/minio/minio@RELEASE.2023-05-18T00-05-36Z
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
