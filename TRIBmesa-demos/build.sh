b496878172be2fd55e56ce45eda60f94af7b0a6e
Peter Tribble <peter.tribble@gmail.com>
2023-06-12 13:27:06 +0100

    Add mesa-demos 8.5.0

---
+++ TRIBmesa-demos/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# alternate prefix to avoid dumping a lot of potentially conflicting
+# commands into /usr/bin
+#
+env TRIBBLIX_LDFLAGS="-L/usr/lib/mesa/`$THOME/build/getarch` -R/usr/lib/mesa/`$THOME/build/getarch`" $THOME/build/dobuild -64only mesa-demos-8.5.0 -P /usr/versions/mesa-demos -C "--enable-gles1=no --enable-autotools --with-system-data-files"
+${THOME}/build/genpkg TRIBmesa-demos mesa-demos-8.5.0
