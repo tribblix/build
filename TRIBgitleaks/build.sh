fc2bab6d665b22f92b781c429056b94117dc4aae
Peter Tribble <peter.tribble@gmail.com>
2023-06-12 08:19:20 +0100

    Update gitleaks v8.16.4

--- TRIBgitleaks/build.sh
+++ TRIBgitleaks/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 #
-git clone https://github.com/zricethezav/gitleaks.git
+git clone https://github.com/gitleaks/gitleaks.git
 cd gitleaks
-# explicitly demands this version of go
-# and needs gnu sort
-env PATH=/usr/versions/go-1.19/bin:/usr/gnu/bin:$PATH gmake build
+# needs gnu sort
+env PATH=/usr/versions/go-1.20/bin:/usr/gnu/bin:$PATH gmake build
 
 rm -fr /tmp/cgl
 mkdir -p /tmp/cgl/usr/bin
