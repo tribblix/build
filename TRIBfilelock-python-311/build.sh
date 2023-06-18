c22db5e62bd330bffb450f486179581eec397d6a
Peter Tribble <peter.tribble@gmail.com>
2023-06-18 09:17:53 +0100

    Update filelock 3.12.2

--- TRIBfilelock-python-311/build.sh
+++ TRIBfilelock-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBfilelock-python-311 filelock-3.12.1
+${THOME}/build/pkg_pep518 TRIBfilelock-python-311 filelock-3.12.2
--- TRIBfilelock-python-39/build.sh
+++ TRIBfilelock-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBfilelock-python-39 filelock-3.12.1
+${THOME}/build/pkg_pep518 TRIBfilelock-python-39 filelock-3.12.2
