3fd619c2b149754c2ea8ab102cda481c7d610a83
Peter Tribble <peter.tribble@gmail.com>
2023-06-19 10:03:27 +0100

    Update python protobuf 4.23.3

--- TRIBprotobuf-python-39/build.sh
+++ TRIBprotobuf-python-39/build.sh
@@ -3,6 +3,4 @@
 # entertainingly, the python module has a filename clash
 # with the regular protobuf download
 #
-# this is a pure python build, without the C++ extensions
-#
-${THOME}/build/pkg_setup_py TRIBprotobuf-python-39 protobuf-4.21.12
+${THOME}/build/pkg_setup_py TRIBprotobuf-python-39 protobuf-4.23.3
