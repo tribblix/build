33bdf01eb206c40fccd3dbcca1ab9c4f64cb2d15
Peter Tribble <peter.tribble@gmail.com>
2023-06-19 15:58:59 +0100

    Update Cython 0.29.35

--- TRIBcython-python-311/build.sh
+++ TRIBcython-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBcython-python-311 Cython-0.29.34
+${THOME}/build/pkg_setup_py TRIBcython-python-311 Cython-0.29.35
--- TRIBcython-python-39/build.sh
+++ TRIBcython-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBcython-python-39 Cython-0.29.34
+${THOME}/build/pkg_setup_py TRIBcython-python-39 Cython-0.29.35
