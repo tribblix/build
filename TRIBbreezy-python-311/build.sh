b95198bdff21e3d9e10c1828150012c73c6e3e34
Peter Tribble <peter.tribble@gmail.com>
2023-06-19 17:54:59 +0100

    Update breezy 3.3.3

--- TRIBbreezy-python-311/build.sh
+++ TRIBbreezy-python-311/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack breezy-3.3.2
-cd breezy-3.3.2
+${THOME}/build/unpack breezy-3.3.3
+cd breezy-3.3.3
 # needs gnu msgfmt
 env PATH=/usr/gnu/bin:$PATH LD_RUN_PATH=/usr/versions/python-3.11/lib /usr/versions/python-3.11/bin/python3 setup.py build
 rm -fr /tmp/pil
