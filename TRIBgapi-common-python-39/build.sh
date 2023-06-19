cc13f3007ecdfb10da68199ca48271fc07a70339
Peter Tribble <peter.tribble@gmail.com>
2023-06-19 09:48:05 +0100

    Update googleapis-common-protos 1.59.1

--- TRIBgapi-common-python-39/build.sh
+++ TRIBgapi-common-python-39/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgapi-common-python-39 googleapis-common-protos-1.59.0
+${THOME}/build/pkg_setup_py TRIBgapi-common-python-39 googleapis-common-protos-1.59.1
