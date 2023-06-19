a4c9b3aa16321d3f4ddba1909610d4fd52089f5a
Peter Tribble <peter.tribble@gmail.com>
2023-06-19 11:35:12 +0100

    Update Python 3.9.17

--- TRIBv-python-39/build.sh
+++ TRIBv-python-39/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
+# 3.9 is 64-bit
+#
+${THOME}/build/dobuild +64only Python-3.9.17 -P /usr/versions/python-3.9 -L -C --enable-shared
+#
 # NB. TRIBv-python-39 must not be installed, or the make
 # install step will get confused
 #
-# 3.9 is 64-bit
+# zap d TRIBv-python-39-tests TRIBv-python-39-tkinter TRIBv-python-39
 #
-${THOME}/build/dobuild +64only Python-3.9.16 -P /usr/versions/python-3.9 -L -C --enable-shared
 # Horrid hack to work around deficiencies in setuptools
-${THOME}/build/genpkg TRIBv-python-39 Python-3.9.16
+${THOME}/build/genpkg TRIBv-python-39 Python-3.9.17
