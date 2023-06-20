19b6893152dced512ba9279c8c0582aead057a5f
Peter Tribble <peter.tribble@gmail.com>
2023-06-20 16:37:00 +0100

    Update gnucobol 3.1.2

--- TRIBgnucobol/build.sh
+++ TRIBgnucobol/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild gnucobol-2.2 -C --with-db=no
-${THOME}/build/genpkg TRIBgnucobol gnucobol-2.2
+${THOME}/build/dobuild -64only gnucobol-3.1.2 -C --with-db=no
+${THOME}/build/genpkg TRIBgnucobol gnucobol-3.1.2
