bde92de02ad83f59ece0c103b3e7bf021a05c0ff
Peter Tribble <peter.tribble@gmail.com>
2023-06-21 13:28:42 +0100

    Update nltk 3.8.1

--- TRIBnltk-python-311/build.sh
+++ TRIBnltk-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack nltk-3.7
-cd nltk-3.7
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBnltk-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBnltk-python-311 nltk-3.8.1
