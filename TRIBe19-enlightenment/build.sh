c49fb4798b1a8a89edc3c748d2acb14bf719ca89
Peter Tribble <peter.tribble@gmail.com>
2023-06-23 17:10:35 +0100

    Update enlightenment 0.21.11

--- TRIBe19-enlightenment/build.sh
+++ TRIBe19-enlightenment/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild enlightenment-0.21.9 -C "--disable-connman --disable-cpufreq --disable-battery --disable-mixer --disable-bluez4 --disable-temperature --disable-device-udev --disable-mount-udisks --disable-device-hal"
-${THOME}/build/genpkg TRIBe19-enlightenment enlightenment-0.21.9
+${THOME}/build/dobuild -64only enlightenment-0.21.11 -C "--disable-connman --disable-cpufreq --disable-battery --disable-mixer --disable-bluez4 --disable-temperature --disable-device-udev --disable-mount-udisks --disable-device-hal"
+${THOME}/build/genpkg TRIBe19-enlightenment enlightenment-0.21.11
