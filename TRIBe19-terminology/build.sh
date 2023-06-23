ebc91e9ce6aba011ee3814b973dc98742f1a4406
Peter Tribble <peter.tribble@gmail.com>
2023-06-23 15:42:08 +0100

    Update terminology 1.7.0

--- TRIBe19-terminology/build.sh
+++ TRIBe19-terminology/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild terminology-1.1.1
-${THOME}/build/genpkg TRIBe19-terminology terminology-1.1.1
+# 1.7.0 is the latest compatible with EFL 1.20
+#
+${THOME}/build/mesonbuild -64only -gnu terminology-1.7.0
+${THOME}/build/genpkg TRIBe19-terminology terminology-1.7.0
