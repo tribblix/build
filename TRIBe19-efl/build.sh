dda948dbd1f9961f3974432dbcbd6e20cff84a17
Peter Tribble <peter.tribble@gmail.com>
2023-06-23 15:10:01 +0100

    Update EFL 1.20.7

--- TRIBe19-efl/build.sh
+++ TRIBe19-efl/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-$THOME/build/dobuild -64 efl-1.18.5 -C "--with-profile=release --disable-pulseaudio --enable-fribidi --enable-harfbuzz --disable-libeeze --enable-lua-old --disable-cserve --enable-physics --disable-systemd --enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-abb"
-${THOME}/build/genpkg TRIBe19-efl efl-1.18.5
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl -z muldefs" $THOME/build/dobuild -64 efl-1.20.7 -C "--with-profile=release --disable-pulseaudio --enable-fribidi --enable-harfbuzz --disable-libeeze --enable-lua-old --disable-cserve --enable-physics --disable-systemd --enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-abb"
+${THOME}/build/genpkg TRIBe19-efl efl-1.20.7
