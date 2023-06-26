5bffd1c01bd571483ee08e4ee504eb67f4793abd
Peter Tribble <peter.tribble@gmail.com>
2023-06-26 12:59:22 +0100

    Update fuse 1.6.0

--- TRIBspectrum-fuse/build.sh
+++ TRIBspectrum-fuse/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild fuse-1.5.7 -C --enable-desktop-integration
-${THOME}/build/genpkg TRIBspectrum-fuse fuse-1.5.7
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only fuse-1.6.0 -C --enable-desktop-integration
+${THOME}/build/genpkg TRIBspectrum-fuse fuse-1.6.0
