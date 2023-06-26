65c34e51f942481f0d0feac906f84597bd66db8e
Peter Tribble <peter.tribble@gmail.com>
2023-06-26 14:53:45 +0100

    Build edelib 64-bit

--- TRIBedelib/build.sh
+++ TRIBedelib/build.sh
@@ -3,8 +3,19 @@
 # desktop based on fltk
 # needs jam (I used ftjam) installed
 #
-${THOME}/build/dobuild edelib-2.1 -C --enable-shared
+${THOME}/build/dobuild -64 edelib-2.1 -C --enable-shared
+cd edelib-2.1-64bit
+jam
+cd ..
 cd edelib-2.1
 jam
+cd ..
+
+cd edelib-2.1-64bit
 jam install DESTDIR=/tmp/el
+cd ..
+cd edelib-2.1
+jam install DESTDIR=/tmp/el
+cd ..
+
 ${THOME}/build/create_pkg TRIBedelib /tmp/el
