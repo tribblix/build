88796b0e322ca6bd88e97cc4c143597e397caa00
Peter Tribble <peter.tribble@gmail.com>
2023-06-26 09:17:15 +0100

    Update suricata 5.0.9

--- TRIBsuricata/build.sh
+++ TRIBsuricata/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS=-lm ${THOME}/build/dobuild -64only suricata-5.0.8 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
-${THOME}/build/genpkg -I install-full TRIBsuricata suricata-5.0.8
+# rust code in 5.0.10 doesn't build
+#
+env PATH=/usr/versions/python-3.11/bin:$PATH TRIBBLIX_LDFLAGS=-lm ${THOME}/build/dobuild -64only suricata-5.0.9 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
+${THOME}/build/genpkg -I install-full TRIBsuricata suricata-5.0.9
