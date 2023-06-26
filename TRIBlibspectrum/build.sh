aa522cd34e325f54a30426caac3ef33f56f2ec3f
Peter Tribble <peter.tribble@gmail.com>
2023-06-26 12:46:47 +0100

    Update libspectrum 1.4.4, with a 64-bit 1.5.0 build

--- TRIBlibspectrum/build.sh
+++ TRIBlibspectrum/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild libspectrum-1.4.1
-${THOME}/build/genpkg TRIBlibspectrum libspectrum-1.4.1
+# temporary split build; the SONAME changes so we keep the older build
+# for binary compatibility just in case there are old 32-bit clients
+#
+${THOME}/build/dobuild libspectrum-1.4.4
+${THOME}/build/dobuild +64 libspectrum-1.5.0
+${THOME}/build/genpkg TRIBlibspectrum libspectrum-1.4.4 libspectrum-1.5.0
