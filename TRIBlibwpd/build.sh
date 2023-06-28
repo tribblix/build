cd1d568f5f1a2ec418338f9b49104c65af5bd8a1
Peter Tribble <peter.tribble@gmail.com>
2023-06-28 13:35:04 +0100

    Update libwpd and add 64-bit

--- TRIBlibwpd/build.sh
+++ TRIBlibwpd/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild libwpd-0.10.0
-${THOME}/build/genpkg TRIBlibwpd libwpd-0.10.0
+# need to build the 32-bit library as the old version with the old ABI
+# for binary compatibility
+#
+env TRIBBLIX_CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ${THOME}/build/dobuild libwpd-0.10.0
+${THOME}/build/dobuild +64 libwpd-0.10.3
+${THOME}/build/genpkg TRIBlibwpd libwpd-0.10.0 libwpd-0.10.3
