c3eac8b8aafb6c7b4a79690ef8df8982cf175a15
Peter Tribble <peter.tribble@gmail.com>
2023-06-28 13:35:25 +0100

    Update libwpg and add 64-bit

--- TRIBlibwpg/build.sh
+++ TRIBlibwpg/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
+# use the old version in 32-bit for binary compatibility
+#
 ${THOME}/build/dobuild libwpg-0.3.0
-${THOME}/build/genpkg TRIBlibwpg libwpg-0.3.0
+${THOME}/build/dobuild +64 libwpg-0.3.4
+${THOME}/build/genpkg TRIBlibwpg libwpg-0.3.0 libwpg-0.3.4
