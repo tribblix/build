67f4f191effedb0570a08806ddfe82b595b6f1b1
Peter Tribble <peter.tribble@gmail.com>
2023-06-28 11:06:39 +0100

    Update cabextract 1.11

--- TRIBcabextract/build.sh
+++ TRIBcabextract/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild cabextract-1.6
-${THOME}/build/genpkg TRIBcabextract cabextract-1.6
+${THOME}/build/dobuild -64only cabextract-1.11
+${THOME}/build/genpkg TRIBcabextract cabextract-1.11
