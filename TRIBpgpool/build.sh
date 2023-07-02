4655baa248af61549b7f2856b7447b4f26e15e32
Peter Tribble <peter.tribble@gmail.com>
2023-07-02 21:35:38 +0100

    Build pgpool against postgres 15

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,5 @@
 #
 # needs a postconf patch
 #
-env PATH=/opt/tribblix/postgres13/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.2.13 -C "--sysconfdir=/etc --with-openssl"
+env PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.2.13 -C "--sysconfdir=/etc --with-openssl"
 ${THOME}/build/genpkg TRIBpgpool pgpool-II-4.2.13
