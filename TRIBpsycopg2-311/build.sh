a06670e5fd377e410bec6aef66f58293ae95c3d9
Peter Tribble <peter.tribble@gmail.com>
2023-07-02 21:26:39 +0100

    Build psycopg2 against postgres 15

--- TRIBpsycopg2-311/build.sh
+++ TRIBpsycopg2-311/build.sh
@@ -2,9 +2,9 @@
 #
 ${THOME}/build/unpack psycopg2-2.9.6
 cd psycopg2-2.9.6
-env PATH=/opt/tribblix/postgres13/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres13/lib /usr/versions/python-3.11/bin/python3 setup.py build
+env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib /usr/versions/python-3.11/bin/python3 setup.py build
 rm -fr /tmp/pil
-env PATH=/opt/tribblix/postgres13/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres13/lib /usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
+env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib /usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
 ${THOME}/build/create_pkg TRIBpsycopg2-311 /tmp/pil
 cd ..
 rm -fr /tmp/pil
