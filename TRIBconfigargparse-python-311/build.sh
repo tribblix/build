081383b19df93ffd5e86e250e858420322aa3cbe
Peter Tribble <peter.tribble@gmail.com>
2023-07-02 11:28:43 +0100

    Update ConfigArgParse 1.5.5

--- TRIBconfigargparse-python-311/build.sh
+++ TRIBconfigargparse-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ConfigArgParse-1.5.3
-cd ConfigArgParse-1.5.3
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBconfigargparse-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBconfigargparse-python-311 ConfigArgParse-1.5.5
