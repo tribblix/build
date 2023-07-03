9ef6120f51424dcc49d3e1bb4ceed44aafd73be8
Peter Tribble <peter.tribble@gmail.com>
2023-07-03 13:51:59 +0100

    Add google-cloud-storage python module

---
+++ TRIBgcs-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBgcs-python-311 google-cloud-storage-2.10.0
---
+++ TRIBgoog-res-media-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBgoog-res-media-python-311 google-resumable-media-2.5.0
