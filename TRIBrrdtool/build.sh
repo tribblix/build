28901d4612019e2043aecb6f41c02e9ebb2cf242
Peter Tribble <peter.tribble@gmail.com>
2023-07-04 15:39:01 +0100

    Clean up rrdtool build

--- TRIBrrdtool/build.sh
+++ TRIBrrdtool/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only rrdtool-1.8.0 -C "--disable-lua --disable-tcl"
+# the python modules fail to configure
+# disable ruby as its install doesn't honor DESTDIR
+#
+${THOME}/build/dobuild -64only rrdtool-1.8.0 -C "--disable-lua --disable-tcl --disable-python --disable-ruby"
 ${THOME}/build/genpkg TRIBrrdtool rrdtool-1.8.0
