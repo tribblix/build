eab7764a33ebcea08fa3bfa86f67b3f65538cbd2
Peter Tribble <peter.tribble@gmail.com>
2023-07-04 20:38:11 +0100

    Rebuild hexchat, disable python as it doesn't work

--- TRIBhexchat/build.sh
+++ TRIBhexchat/build.sh
@@ -4,14 +4,13 @@ ${THOME}/build/unpack hexchat-2.16.1
 cd hexchat-2.16.1
 
 #
-# this is a 64-bit build, so disable perl for now as that's
-# 32-bit (and never worked)
+# disable perl for now as it's never worked
 #
-# we ought to pick up the 64-bit python3.9, but while the module builds
+# we ought to pick up the 64-bit python, but while the module builds
 # that gets symbol errors at runtime we ought to look at
 #
 # buildtype=plain avoids SSP failures
-env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig LDFLAGS="-lsocket -lnsl" meson build -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr -Dbuildtype=plain -Dc_args=-m64 -Dcpp_args=-m64 -Dc_link_args="-m64 -lsocket -lnsl" -Dcpp_link_args=-m64 -Dwith-perl=false
+env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig LDFLAGS="-lsocket -lnsl" meson build -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr -Dbuildtype=plain -Dc_args=-m64 -Dcpp_args=-m64 -Dc_link_args="-m64 -lsocket -lnsl" -Dcpp_link_args=-m64 -Dwith-perl=false -Dwith-python=false -Dlibcanberra=disabled
 env PATH=/usr/gnu/bin:$PATH ninja -C build
 rm -fr /tmp/hh
 env DESTDIR=/tmp/hh PATH=/usr/gnu/bin:$PATH ninja -C build install
