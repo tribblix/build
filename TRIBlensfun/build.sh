28c77aa4f6503400907807b58ffa26e0582fb9d9
Peter Tribble <peter.tribble@gmail.com>
2023-07-06 19:24:26 +0100

    Rebuild lensfun with python 3.11

--- TRIBlensfun/build.sh
+++ TRIBlensfun/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
+# see fixit - there are python pieces, although we don't declare a dependency
+#
 ${THOME}/build/cmbuild -64 lensfun-0.3.3 -C -DBUILD_LENSTOOL=ON
 ${THOME}/build/genpkg TRIBlensfun lensfun-0.3.3
