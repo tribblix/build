b001e20617ce305b1aab145badfdc7101f1cb0c9
Peter Tribble <peter.tribble@gmail.com>
2023-07-06 21:15:09 +0100

    Update NEdit 5.7

--- TRIBnedit/build.sh
+++ TRIBnedit/build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 #
-${THOME}/build/unpack nedit-5.5
-cd nedit-5.5
+${THOME}/build/unpack nedit-5.7
+cd nedit-5.7
 make solarisx86
 # make solaris on sparc
+rm -fr /tmp/ned1
 mkdir -p /tmp/ned1/usr/bin
 mkdir -p /tmp/ned1/usr/share/nedit
 mkdir -p /tmp/ned1/usr/share/man/man1
 cp source/nedit /tmp/ned1/usr/bin
 cp source/nc /tmp/ned1/usr/bin/nedit-client
+cd doc
+env PATH=/usr/perl5/bin:$PATH make VERSION='NEdit 5.7' all
+cd ..
 cp README COPYRIGHT ReleaseNotes doc/nedit.doc doc/nedit.html doc/faq.txt /tmp/ned1/usr/share/nedit
 cp doc/nedit.man /tmp/ned1/usr/share/man/man1/nedit.1
 cp doc/nc.man /tmp/ned1/usr/share/man/man1/nedit-client.1
