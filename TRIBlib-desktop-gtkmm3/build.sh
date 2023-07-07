acc485e7797edcf7e93344c02c82d1a234d617e4
Peter Tribble <peter.tribble@gmail.com>
2023-07-07 10:49:15 +0100

    Update gtkmm 3.24.7

--- TRIBlib-desktop-gtkmm3/build.sh
+++ TRIBlib-desktop-gtkmm3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu gtkmm-3.22.3
-${THOME}/build/genpkg TRIBlib-desktop-gtkmm3 gtkmm-3.22.3
+${THOME}/build/mesonbuild -64 -gnu gtkmm-3.24.7 -C "-Dbuild-tests=false -Dbuild-demos=false"
+${THOME}/build/genpkg TRIBlib-desktop-gtkmm3 gtkmm-3.24.7
