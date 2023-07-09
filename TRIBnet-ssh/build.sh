9bed0b684d4d39ff8fba4a66769859e3fc280b38
Peter Tribble <peter.tribble@gmail.com>
2023-07-09 16:56:01 +0100

    Update OpenSSH to 8.9p1

--- TRIBnet-ssh/build.sh
+++ TRIBnet-ssh/build.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild openssh-8.4p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
+${THOME}/build/dobuild -64only openssh-8.9p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
 #
-# Need to split into 3 packages to match the old layout (so upgrades
-# will get all the pieces)
-# Next time, deprecate and remove TRIBnet-ssh-ssh-key as it's no
-# longer necessary
+# Need to split to match the old layout
+# TRIBnet-ssh-ssh-key is now deprecated and forcibly removed in m31
 #
-${THOME}/build/genpkg TRIBnet-ssh openssh-8.4p1
-${THOME}/build/genpkg TRIBnet-ssh-ssh-key openssh-8.4p1
-${THOME}/build/genpkg TRIBsvc-net-ssh openssh-8.4p1
+${THOME}/build/genpkg TRIBnet-ssh openssh-8.9p1
+${THOME}/build/genpkg TRIBsvc-net-ssh openssh-8.9p1
