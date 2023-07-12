414ef760dec16031cef87ba87e51b461c642a07a
Peter Tribble <peter.tribble@gmail.com>
2023-07-12 08:30:36 +0100

    Drop more orphaned python 3.9 modules

--- TRIBcertifi-python-39/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/pkg_setup_py TRIBcertifi-python-39 certifi-2023.5.7
--- TRIBxdg-python-39/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack pyxdg-0.28
-cd pyxdg-0.28
-/usr/versions/python-3.9/bin/python3 setup.py build
-rm -fr /tmp/xx1
-/usr/versions/python-3.9/bin/python3 setup.py install --root=/tmp/xx1
-${THOME}/build/create_pkg TRIBxdg-python-39 /tmp/xx1
-rm -fr /tmp/xx1
