9080feaa668ca5a3ca569030c4b3ec05e4fd23bc
Peter Tribble <peter.tribble@gmail.com>
2023-07-15 15:51:31 +0100

    Update tarsnap 1.0.40

--- TRIBtarsnap/build.sh
+++ TRIBtarsnap/build.sh
@@ -3,5 +3,7 @@
 # there is no pre-built tarsnap for illumos
 # so we have to build from source
 #
-${THOME}/build/dobuild tarsnap-autoconf-1.0.39 -C "--sysconfdir=/etc --localstatedir=/var"
-${THOME}/build/genpkg TRIBtarsnap tarsnap-autoconf-1.0.39
+# patch configure so we don't get a broken RPATH
+#
+${THOME}/build/dobuild -64only tarsnap-autoconf-1.0.40 -C "--sysconfdir=/etc --localstatedir=/var"
+${THOME}/build/genpkg TRIBtarsnap tarsnap-autoconf-1.0.40
---
+++ patches/tarsnap-autoconf-1.0.40.pls
@@ -0,0 +1,1 @@
+-p1 tarsnap-conf.patch 
