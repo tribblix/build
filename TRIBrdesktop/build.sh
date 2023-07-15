d4bebd8c9090917b94bc7b967fe477d43c4941ba
Peter Tribble <peter.tribble@gmail.com>
2023-07-15 15:16:39 +0100

    Update rdesktop 1.9.0

--- TRIBrdesktop/build.sh
+++ TRIBrdesktop/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 #
-# 1.9.0 switched from openssl to gnutls which would be fine
-# but also requires a separate libtasn1
-#
-${THOME}/build/dobuild rdesktop-1.8.6 -C "--disable-credssp --disable-smartcard"
-${THOME}/build/genpkg  TRIBrdesktop rdesktop-1.8.6
+${THOME}/build/dobuild -64only rdesktop-1.9.0 -C "--disable-credssp --disable-smartcard"
+${THOME}/build/genpkg  TRIBrdesktop rdesktop-1.9.0
