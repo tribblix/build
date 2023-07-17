cd1aed553b7d0f522c95ac5e86ff082b1cadc309
Peter Tribble <peter.tribble@gmail.com>
2023-07-17 22:10:28 +0100

    Build libetpan 64-bit only

--- TRIBlibetpan/build.sh
+++ TRIBlibetpan/build.sh
@@ -2,12 +2,11 @@
 #
 # the postconf script replaces the configure step
 # arguments to configure need to go in the postconf script
-# (and in the 64-bit postconf too)
 #
 # this library seems to bump its SONAME every time
 # so consumers will need to be rebuilt
 #
 # zap install TRIBlibtool
-${THOME}/build/dobuild -64 libetpan-1.9.4
+${THOME}/build/dobuild +64 libetpan-1.9.4
 ${THOME}/build/genpkg TRIBlibetpan libetpan-1.9.4
 #zap uninstall TRIBlibtool
