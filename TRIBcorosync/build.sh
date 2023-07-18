908253685583051493996ac14be5c64cf007ccbc
Peter Tribble <peter.tribble@gmail.com>
2023-07-18 08:15:27 +0100

    Update corosync 2.4.6, and build 64-bit only

--- TRIBcorosync/build.sh
+++ TRIBcorosync/build.sh
@@ -2,5 +2,5 @@
 #
 # postconf script to fix up use of the wrong awk
 #
-${THOME}/build/dobuild corosync-2.4.5 -64 -C "--localstatedir=/var --sysconfdir=/etc"
-${THOME}/build/genpkg TRIBcorosync corosync-2.4.5
+${THOME}/build/dobuild -64only corosync-2.4.6 -C "--localstatedir=/var --sysconfdir=/etc"
+${THOME}/build/genpkg TRIBcorosync corosync-2.4.6
