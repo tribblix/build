ff4bcd34679ee340153d167f6b1357f9c38724a5
Peter Tribble <peter.tribble@gmail.com>
2023-07-18 09:06:56 +0100

    Update libgit2 1.3.2, and only build 64-bit

--- TRIBlibgit2/build.sh
+++ TRIBlibgit2/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-${THOME}/build/cmbuild -64 libgit2-1.3.0
-${THOME}/build/genpkg TRIBlibgit2 libgit2-1.3.0
+#
+# bumps the SONAME, so upgrades to a newer 1.X require
+# consumers to be rebuilt
+#
+${THOME}/build/cmbuild -64only libgit2-1.3.2 -C -DBUILD_CLAR=OFF
+${THOME}/build/genpkg TRIBlibgit2 libgit2-1.3.2
