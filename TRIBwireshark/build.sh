e564a929cc6698ad71bbfdd132812369709746de
Peter Tribble <peter.tribble@gmail.com>
2023-07-18 13:13:23 +0100

    Update wireshark 3.6.15, build 64-bit, fix gui build

--- TRIBwireshark/build.sh
+++ TRIBwireshark/build.sh
@@ -4,6 +4,8 @@
 #
 # needs a little extra help with kerberos
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" PATH=${PATH}:/usr/versions/Qt-5/bin:/usr/perl5/5.34/bin ${THOME}/build/cmbuild -gnu wireshark-3.6.14
-${THOME}/build/genpkg TRIBwireshark wireshark-3.6.14
-${THOME}/build/genpkg TRIBwireshark-qt wireshark-3.6.14
+
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" PATH=${PATH}:/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:/usr/perl5/5.34/bin ${THOME}/build/cmbuild -64only -gnu wireshark-3.6.15 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
+
+${THOME}/build/genpkg TRIBwireshark wireshark-3.6.15
+${THOME}/build/genpkg TRIBwireshark-qt wireshark-3.6.15
