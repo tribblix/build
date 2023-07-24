f12c9213c2c11c71977fc55231a22088cef525ed
Peter Tribble <peter.tribble@gmail.com>
2023-07-24 08:33:51 +0100

    Update jangle

--- TRIBjangle/build.sh
+++ TRIBjangle/build.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 #
 # In jangle itself:
-./build packages deps
-#
-# that creates a cross-platform package
-#  *** make sure the pkg version matches that here ***
-#
-# then copy the packages
-#
-mv /tmp/TRIBjangle.pkg /tmp/TRIBjangle.0.15.pkg
-${THOME}/build/pkg2zap /tmp/TRIBjangle.0.15.pkg ${THOME}/pkgs
-openssl md5 ${THOME}/pkgs/TRIBjangle.0.15.zap | /usr/bin/awk '{print $NF}' > ${THOME}/pkgs/TRIBjangle.0.15.zap.md5
-${THOME}/build/sign-file ${THOME}/pkgs/TRIBjangle.0.15.zap
-ln ${THOME}/pkgs/TRIBjangle.0.15.zap* ${THOME}/pkgs.sparc
+echo "Checkout jangle and"
+echo ./build packages deps
