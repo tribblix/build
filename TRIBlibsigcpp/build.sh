80640f67ebb3ed98295d3c2f5ed51ffa29271549
Peter Tribble <peter.tribble@gmail.com>
2023-07-26 13:54:22 +0100

    Update libsigc++ 2.10.8

--- TRIBlibsigcpp/build.sh
+++ TRIBlibsigcpp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu libsigc++-2.10.0
-${THOME}/build/genpkg TRIBlibsigcpp libsigc++-2.10.0
+${THOME}/build/mesonbuild -64 -gnu libsigc++-2.10.8
+${THOME}/build/genpkg TRIBlibsigcpp libsigc++-2.10.8
