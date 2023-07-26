b6837cd1f9d5433c92144662f406ac3002825e6d
Peter Tribble <peter.tribble@gmail.com>
2023-07-26 16:18:18 +0100

    Update inkscape 1.2.2

--- TRIBinkscape/build.sh
+++ TRIBinkscape/build.sh
@@ -2,5 +2,5 @@
 #
 # install the *mm packages, sigc++, TRIBboehm-gc TRIBlibgsl TRIBboost
 #
-${THOME}/build/cmbuild -gnu -C "--enable-dbusapi --enable-lcms --enable-poppler-cairo" inkscape-0.92.3
-${THOME}/build/genpkg TRIBinkscape inkscape-0.92.3
+$THOME/build/cmbuild -gnu inkscape-1.2.2_2022-12-01_b0a8486541 -C -DWITH_OPENMP=OFF
+${THOME}/build/genpkg TRIBinkscape inkscape-1.2.2_2022-12-01_b0a8486541
---
+++ patches/inkscape-1.2.2_2022-12-01_b0a8486541.pls
@@ -0,0 +1,1 @@
+-p1 inkscape-path.patch
