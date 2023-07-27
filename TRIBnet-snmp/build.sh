a0a6deb334d954af42845970e8a0930cc5fa16f8
Peter Tribble <peter.tribble@gmail.com>
2023-07-27 11:24:29 +0100

    Update net-snmp 5.9.3

--- TRIBnet-snmp/build.sh
+++ TRIBnet-snmp/build.sh
@@ -4,10 +4,22 @@
 #  - hal discovery addons
 #  - libfmd_snmp (also 64-bit)
 #
+
+#
+# we supply (temporarily) the library pieces from 5.7.3 for backwards
+# compatibility
+#
 ${THOME}/build/unpack -64 net-snmp-5.7.3
+${THOME}/build/unpack -64 net-snmp-5.9.3
+
+#
+# first 5.7.3; we disable the installation of some bits to speed things up and
+# simplify cleanup, but don't disable anything that would change the build
+#
 cd net-snmp-5.7.3
 env LDFLAGS=-Wl,-zignore CFLAGS=-O CC=gcc CXX=g++ \
     ./configure --prefix=/usr --with-defaults \
+    --disable-manuals --disable-scripts --disable-mibs \
     --with-default-snmp-version=3 \
     --with-logfile=/var/log/snmpd.log \
     --with-persistent-directory=/var/net-snmp \
@@ -18,12 +30,11 @@ env LDFLAGS=-Wl,-zignore CFLAGS=-O CC=gcc CXX=g++ \
     --with-pkcs \
     --disable-embedded-perl \
     --without-perl-modules \
+    --without-kmem-usage --without-elf \
     --disable-static \
     --with-sys-contact=root@localhost \
     --with-transports="UDP TCP UDPIPv6 TCPIPv6" \
     --with-mib-modules="host disman/event-mib ucd-snmp/diskio udp-mib tcp-mib if-mib"
-perl -pi -e 's#^^(archive_cmds=.*)"$#$1 -nostdlib"#g;' libtool
-
 #
 # fix library dependencies, configure guesses wrong
 #
@@ -38,6 +49,7 @@ cd ..
 cd net-snmp-5.7.3-64bit
 env LDFLAGS="-Wl,-zignore -m64" CFLAGS="-O -m64" CXXFLAGS="-O -m64" CC=gcc CXX=g++ \
     ./configure --prefix=/usr --with-defaults \
+    --disable-manuals --disable-scripts --disable-mibs \
     --libdir=/usr/lib/`isainfo -k` \
     --with-default-snmp-version=3 \
     --with-logfile=/var/log/snmpd.log \
@@ -53,14 +65,75 @@ env LDFLAGS="-Wl,-zignore -m64" CFLAGS="-O -m64" CXXFLAGS="-O -m64" CC=gcc CXX=g
     --with-sys-contact=root@localhost \
     --with-transports="UDP TCP UDPIPv6 TCPIPv6" \
     --with-mib-modules="host disman/event-mib ucd-snmp/diskio udp-mib tcp-mib if-mib"
-perl -pi -e 's#^^(archive_cmds=.*)"$#$1 -nostdlib"#g;' libtool
-
 #
 # fix library dependencies, configure guesses wrong
 #
 sed -i s:-lkvm:: agent/Makefile
 sed -i s:-lelf:: agent/Makefile
 sed -i 's:lkstat:lkstat -lsocket -lnsl:' snmplib/Makefile
+
+gmake -j 8
+
+cd ..
+
+cd net-snmp-5.9.3
+env LDFLAGS=-Wl,-zignore CFLAGS=-O CC=gcc CXX=g++ \
+    ./configure --prefix=/usr --with-defaults \
+    --with-mibdirs=/etc/net-snmp/snmp/mibs --datadir=/etc/net-snmp \
+    --with-default-snmp-version=3 \
+    --with-logfile=/var/log/snmpd.log \
+    --with-persistent-directory=/var/net-snmp \
+    --enable-agentx-dom-sock-only \
+    --enable-ucd-snmp-compatibility \
+    --enable-ipv6 \
+    --enable-mfd-rewrites \
+    --without-pkcs \
+    --enable-blumenthal-aes \
+    --disable-embedded-perl \
+    --without-perl-modules \
+    --without-kmem-usage --without-elf \
+    --disable-static \
+    --with-sys-contact=root@localhost \
+    --with-transports="UDP TCP UDPIPv6 TCPIPv6" \
+    --with-mib-modules="host disman/event-mib ucd-snmp/diskio udp-mib tcp-mib if-mib"
+#
+# fix library dependencies, configure guesses wrong
+#
+sed -i s:-lkvm:: agent/Makefile
+sed -i s:-lelf:: agent/Makefile
+sed -i 's:lkstat -lsocket:lkstat -lsocket -lnsl:' snmplib/Makefile
+
+gmake -j 8
+
+cd ..
+
+cd net-snmp-5.9.3-64bit
+env LDFLAGS="-Wl,-zignore -m64" CFLAGS="-O -m64" CXXFLAGS="-O -m64" CC=gcc CXX=g++ \
+    ./configure --prefix=/usr --with-defaults \
+    --libdir=/usr/lib/`isainfo -k` \
+    --with-mibdirs=/etc/net-snmp/snmp/mibs --datadir=/etc/net-snmp \
+    --with-default-snmp-version=3 \
+    --with-logfile=/var/log/snmpd.log \
+    --with-persistent-directory=/var/net-snmp \
+    --enable-agentx-dom-sock-only \
+    --enable-ucd-snmp-compatibility \
+    --enable-ipv6 \
+    --enable-mfd-rewrites \
+    --without-pkcs \
+    --enable-blumenthal-aes \
+    --disable-embedded-perl \
+    --without-perl-modules \
+    --without-kmem-usage --without-elf \
+    --disable-static \
+    --with-sys-contact=root@localhost \
+    --with-transports="UDP TCP UDPIPv6 TCPIPv6" \
+    --with-mib-modules="host disman/event-mib ucd-snmp/diskio udp-mib tcp-mib if-mib"
+#
+# fix library dependencies, configure guesses wrong
+#
+sed -i s:-lkvm:: agent/Makefile
+sed -i s:-lelf:: agent/Makefile
+sed -i 's:lkstat -lsocket:lkstat -lsocket -lnsl:' snmplib/Makefile
 
 gmake -j 8
 
@@ -75,6 +148,21 @@ cd net-snmp-5.7.3-64bit
 gmake install DESTDIR=/tmp/ns
 cd ..
 cd net-snmp-5.7.3
+gmake install DESTDIR=/tmp/ns
+cd ..
+#
+# clean up the pieces we don't want
+#
+rm -fr /tmp/ns/usr/include /tmp/ns/usr/bin /tmp/ns/usr/sbin /tmp/ns/usr/share
+rm -f /tmp/ns/usr/lib/lib*.la /tmp/ns/usr/lib/lib*.so
+rm -f /tmp/ns/usr/lib/*/lib*.la /tmp/ns/usr/lib/*/lib*.so
+#
+# and now put in the proper new version
+#
+cd net-snmp-5.9.3
+gmake install DESTDIR=/tmp/ns
+cd ..
+cd net-snmp-5.9.3-64bit
 gmake install DESTDIR=/tmp/ns
 cd ..
 ${THOME}/build/create_pkg TRIBnet-snmp /tmp/ns
