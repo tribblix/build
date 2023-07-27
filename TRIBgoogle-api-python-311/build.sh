f1bafed9f82f1a877bb56c3c3aba0d2d45a7122a
Peter Tribble <peter.tribble@gmail.com>
2023-07-27 15:16:34 +0100

    Update google-auth 2.22.0 google-api-python-client 2.95.0

--- TRIBgoogle-api-python-311/build.sh
+++ TRIBgoogle-api-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-api-python-311 google-api-python-client-2.92.0
+${THOME}/build/pkg_setup_py TRIBgoogle-api-python-311 google-api-python-client-2.95.0
