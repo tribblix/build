9e2967a58fa582e64c9e630570e0d4d8ca6d54aa
Peter Tribble <peter.tribble@gmail.com>
2023-07-30 10:53:17 +0100

    Build gegl 32-bit only

--- TRIBlib-gegl/build.sh
+++ TRIBlib-gegl/build.sh
@@ -3,7 +3,8 @@
 # new meson build
 #
 # we need GNU msgfmt (hence PATH) but our own ar, hence AR
+# no 32-bit consumers
 #
-env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild -64 gegl-0.4.44 -C "-Dintrospection=false --buildtype=release -Dworkshop=true -Dlibav=disabled -Dopenmp=disabled"
+env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild +64 gegl-0.4.44 -C "-Dintrospection=false --buildtype=release -Dworkshop=true -Dlibav=disabled -Dopenmp=disabled -Dsdl2=disabled -Dlibspiro=disabled"
 
 ${THOME}/build/genpkg TRIBlib-gegl gegl-0.4.44
