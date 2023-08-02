d74dd0dc2c7f9f4a203628935b1eb500d72dd400
Peter Tribble <peter.tribble@gmail.com>
2023-08-02 12:53:24 +0100

    Drop 32-bit libspectrum

--- TRIBlibspectrum/build.sh
+++ TRIBlibspectrum/build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #
-# temporary split build; the SONAME changes so we keep the older build
-# for binary compatibility just in case there are old 32-bit clients
+# it really does depend on libaudiofile, it's linked in using the full
+# pathname which might not get picked up
 #
-${THOME}/build/dobuild libspectrum-1.4.4
 ${THOME}/build/dobuild +64 libspectrum-1.5.0
-${THOME}/build/genpkg TRIBlibspectrum libspectrum-1.4.4 libspectrum-1.5.0
+${THOME}/build/genpkg TRIBlibspectrum libspectrum-1.5.0
