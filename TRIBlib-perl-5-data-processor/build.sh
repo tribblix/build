5ca77de32c6b36d497e278b761e28cb1b72bdd91
Peter Tribble <peter.tribble@gmail.com>
2023-08-02 16:09:22 +0100

    Drop some unused perl modules

--- TRIBlib-perl-5-data-processor/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack Data-Processor-1.0.9
-cd Data-Processor-1.0.9
-perl Makefile.PL
-make
-cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-data-processor Data-Processor-1.0.9
-
--- TRIBlib-perl-5-il-zones/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/unpack Illumos-Zones-0.1.7
-cd Illumos-Zones-0.1.7
-perl Makefile.PL
-make
-cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-il-zones Illumos-Zones-0.1.7
