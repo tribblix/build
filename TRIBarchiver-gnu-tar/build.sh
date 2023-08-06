a5cc032a83934caeb70d6e9b9393960b901a0ae7
Peter Tribble <peter.tribble@gmail.com>
2023-08-06 21:41:14 +0100

    Update gtar 1.35

--- TRIBarchiver-gnu-tar/build.sh
+++ TRIBarchiver-gnu-tar/build.sh
@@ -2,5 +2,5 @@
 #
 # gtar
 #
-${THOME}/build/dobuild -64only -gnu tar-1.34 -P /usr/gnu
-${THOME}/build/genpkg TRIBarchiver-gnu-tar tar-1.34
+${THOME}/build/dobuild -64only -gnu tar-1.35 -P /usr/gnu
+${THOME}/build/genpkg TRIBarchiver-gnu-tar tar-1.35
