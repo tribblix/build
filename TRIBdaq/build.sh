22694f7f7de9d560bbb024697135bf1b16d89cc3
Peter Tribble <peter.tribble@gmail.com>
2023-08-06 21:19:04 +0100

    Remove unused daq

--- TRIBdaq/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# The normal Solaris build, as followed by OI, disables static libraries
-# and patches snort to compensate. We install the static libs and leave
-# snort alone
-#
-${THOME}/build/dobuild -64 daq-2.0.7 -J 1
-${THOME}/build/genpkg TRIBdaq daq-2.0.7
