aafcf3399c5f45cbab2446bed7e974257e8b2db1
Peter Tribble <peter.tribble@gmail.com>
2023-08-08 14:42:52 +0100

    Add Hugo 0.117.0

---
+++ TRIBhugo/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+env GOPATH=`pwd` go install github.com/gohugoio/hugo@latest
+
+rm -fr /tmp/ll
+mkdir -p /tmp/ll/usr/bin
+mkdir -p /tmp/ll/usr/share/hugo
+
+ginstall -Dm755 ./bin/hugo -t /tmp/ll/usr/bin
+cp pkg/mod/github.com/gohugoio/hugo@v*/README.md /tmp/ll/usr/share/hugo
+cp pkg/mod/github.com/gohugoio/hugo@v*/LICENSE /tmp/ll/usr/share/hugo
+cd ..
+
+${THOME}/build/create_pkg TRIBhugo /tmp/ll
+rm -fr /tmp/ll
