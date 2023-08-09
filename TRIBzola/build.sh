77a99f0c0274d9c09789c26577977be8be0f9c11
Peter Tribble <peter.tribble@gmail.com>
2023-08-09 07:58:18 +0100

    Add zola 0.17.2

---
+++ TRIBzola/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+
+#
+git clone https://github.com/getzola/zola.git
+cd zola
+cargo build --release
+
+rm -fr /tmp/ll
+
+mkdir -p /tmp/ll/usr/bin
+ginstall -s -Dm755 ./target/release/zola -t /tmp/ll/usr/bin
+
+mkdir -p /tmp/ll/usr/share/zola
+cp LICENSE README.md /tmp/ll/usr/share/zola
+
+${THOME}/build/create_pkg TRIBzola /tmp/ll
+rm -fr /tmp/ll
