0ea06ac7ab7634859cb464acf39e488641b3aa3d
Peter Tribble <peter.tribble@gmail.com>
2023-08-11 13:59:43 +0100

    Add optipng 0.7.7

---
+++ TRIBoptipng/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# it's not an autoconf configure, so needs a little chicanery
+#
+env TRIBBLIX_CFLAGS="-m64" TRIBBLIX_LDFLAGS="-m64" $THOME/build/dobuild optipng-0.7.7 -C "--prefix=/usr --with-system-libs"
+${THOME}/build/genpkg TRIBoptipng optipng-0.7.7
