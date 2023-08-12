72142c98d6d8522fe906aa92ac9beed8f87f9710
Peter Tribble <peter.tribble@gmail.com>
2023-08-12 15:55:32 +0100

    Update tesseract 5.3.2

--- TRIBtesseract/build.sh
+++ TRIBtesseract/build.sh
@@ -2,5 +2,7 @@
 #
 # needs leptonica
 #
-${THOME}/build/dobuild tesseract-3.05.02
-${THOME}/build/genpkg TRIBtesseract tesseract-3.05.02
+# downloads the training data during install, see the fixit script
+#
+env TRIBBLIX_LDFLAGS="-lsocket" $THOME/build/cmbuild -64only tesseract-5.3.2 -C  -DTESSDATA_PREFIX=/usr/share
+${THOME}/build/genpkg TRIBtesseract tesseract-5.3.2
