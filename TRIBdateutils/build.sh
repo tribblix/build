d2dbf2d32e3d8026ea15a58f8fba7e74e1c9f821
Peter Tribble <peter.tribble@gmail.com>
2023-08-15 11:54:12 +0100

    Update dateutils 0.4.10

--- TRIBdateutils/build.sh
+++ TRIBdateutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild dateutils-0.4.0
-${THOME}/build/genpkg TRIBdateutils dateutils-0.4.0
+${THOME}/build/dobuild dateutils-0.4.10
+${THOME}/build/genpkg TRIBdateutils dateutils-0.4.10
