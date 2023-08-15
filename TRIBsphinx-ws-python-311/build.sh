ca33a8486e3dde4d7d0a8363b47fe0ab5017eb11
Peter Tribble <peter.tribble@gmail.com>
2023-08-15 10:38:32 +0100

    Update sphinxcontrib-websupport 1.2.6

--- TRIBsphinx-ws-python-311/build.sh
+++ TRIBsphinx-ws-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack sphinxcontrib-websupport-1.2.4
-cd sphinxcontrib-websupport-1.2.4
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsphinx-ws-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBsphinx-ws-python-311 sphinxcontrib_websupport-1.2.6
