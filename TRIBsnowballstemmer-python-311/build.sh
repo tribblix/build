d70c9070a98671176d0beeee7722a8b2ddad4fbc
Peter Tribble <peter.tribble@gmail.com>
2023-08-17 10:51:51 +0100

    Update snowballstemmer 2.2.0

--- TRIBsnowballstemmer-python-311/build.sh
+++ TRIBsnowballstemmer-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack snowballstemmer-1.9.1
-cd snowballstemmer-1.9.1
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBsnowballstemmer-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBsnowballstemmer-python-311 snowballstemmer-2.2.0
