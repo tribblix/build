e6c8b53c6a6fc56bff3463c4314709bb93c44d8b
Peter Tribble <peter.tribble@gmail.com>
2023-08-18 12:16:51 +0100

    Update nettle 3.9.1

--- TRIBnettle/build.sh
+++ TRIBnettle/build.sh
@@ -3,8 +3,10 @@
 # there's a 64-bit preconf script
 #
 # we ship the previous versions of the library for binary
-# compatibility with gnutls + squid + claws-mail + libarchive + vnc
+# compatibility with gnutls + squid + vnc
 # once all rebuilt the compat copy can be removed
 #
-${THOME}/build/dobuild -64 nettle-3.6 -C --disable-static
-${THOME}/build/genpkg TRIBnettle nettle-3.6
+# 3.9.1 claims binary compatibility with our previous 3.6
+#
+${THOME}/build/dobuild -64 nettle-3.9.1 -C --disable-static
+${THOME}/build/genpkg TRIBnettle nettle-3.9.1
