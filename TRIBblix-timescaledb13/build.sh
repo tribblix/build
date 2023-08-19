0ea29b4562bdc71a8a2515b6fd52d41a96f6f8c5
Peter Tribble <peter.tribble@gmail.com>
2023-08-19 14:52:34 +0100

    Update timescaledb 2.11.2

--- TRIBblix-timescaledb13/build.sh
+++ TRIBblix-timescaledb13/build.sh
@@ -2,8 +2,8 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-$THOME/build/unpack timescaledb-2.10.1
-cd timescaledb-2.10.1
+$THOME/build/unpack timescaledb-2.11.2
+cd timescaledb-2.11.2
 env PATH=/opt/tribblix/postgres13/bin:$PATH PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig ./bootstrap
 cd build
 gmake
