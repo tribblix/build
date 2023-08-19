bb15adf39daff09b91be09de3f197f63bf892e63
Peter Tribble <peter.tribble@gmail.com>
2023-08-19 12:00:53 +0100

    Update haproxy 2.6.15

--- TRIBblix-haproxy2/build.sh
+++ TRIBblix-haproxy2/build.sh
@@ -5,10 +5,11 @@
 # the assumption is that there will only be one haproxy installed,
 # as v1 and v2 are installed in the same locations
 #
-${THOME}/build/unpack haproxy-2.6.12
-cd haproxy-2.6.12
+${THOME}/build/unpack haproxy-2.6.15
+cd haproxy-2.6.15
 
 gmake -j 4 CC=gcc TARGET=solaris USE_PCRE2=1 USE_OPENSSL=1 ARCH=64
 
 sed -i 's:PREFIX = /usr/local:PREFIX = /opt/tribblix/haproxy:' Makefile 
 ${THOME}/build/genpkg TRIBblix-haproxy2
+cd ..
