1583f4eabe9595ea1b4e8210c66c38473daa8840
Peter Tribble <peter.tribble@gmail.com>
2023-08-20 18:54:26 +0100

    Update pgpool 4.4.4 (4.2 to 4.4 to match postgres15 default)

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,5 @@
 #
 # needs a patch and postconf
 #
-env PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.2.14 -C "--sysconfdir=/etc --with-openssl"
-${THOME}/build/genpkg TRIBpgpool pgpool-II-4.2.14
+env PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.4 -C "--sysconfdir=/etc --with-openssl"
+${THOME}/build/genpkg TRIBpgpool pgpool-II-4.4.4
