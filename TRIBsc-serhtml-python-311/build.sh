d4b81d03fa60ac0c6480f5ccf0e9a88450ad60e3
Peter Tribble <peter.tribble@gmail.com>
2023-08-21 08:14:57 +0100

    Update sphinxcontrib-serializinghtml 1.1.9

--- TRIBsc-serhtml-python-311/build.sh
+++ TRIBsc-serhtml-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBsc-serhtml-python-311 sphinxcontrib_serializinghtml-1.1.8
+${THOME}/build/pkg_pep518 TRIBsc-serhtml-python-311 sphinxcontrib_serializinghtml-1.1.9
