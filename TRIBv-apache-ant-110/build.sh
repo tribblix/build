99d65c9b57594d700f9bb062ed0c52c6ecd8e338
Peter Tribble <peter.tribble@gmail.com>
2023-08-21 13:28:44 +0100

    Update ant 1.10.14 (with ivy 2.5.2)

--- TRIBv-apache-ant-110/build.sh
+++ TRIBv-apache-ant-110/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # also needs the following in tarballs for fixit to deploy:
-# apache-ivy-2.5.1-bin-with-deps.zip
+# apache-ivy-2.5.2-bin-with-deps.zip
 # svnant-1.3.1.zip
 #
-${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.13-bin
+${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.14-bin
