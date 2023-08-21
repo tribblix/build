57887f704b9062655acdff89f8a6ac5eeda14bfb
Peter Tribble <peter.tribble@gmail.com>
2023-08-21 13:15:52 +0100

    Update gpgme 1.22.0

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 #
-# QT5 bindings don't work, disabled in the preconf script
-#
-# needs a preconf.64 script to go with --with-libassuan-prefix=/usr
-#
-${THOME}/build/dobuild -64 gpgme-1.20.0 -C "--with-libassuan-prefix=/usr --disable-gpg-test"
-${THOME}/build/genpkg TRIBgpgme gpgme-1.20.0
+${THOME}/build/dobuild -64only gpgme-1.22.0 -C --disable-gpg-test
+${THOME}/build/genpkg TRIBgpgme gpgme-1.22.0
