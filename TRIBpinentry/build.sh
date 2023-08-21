5e68b3e387347681fab1ae85111ab6bc901491bf
Peter Tribble <peter.tribble@gmail.com>
2023-08-21 13:01:31 +0100

    Update pinentry 1.2.1

--- TRIBpinentry/build.sh
+++ TRIBpinentry/build.sh
@@ -2,5 +2,5 @@
 #
 # pinentry
 #
-${THOME}/build/dobuild pinentry-1.2.0 -C LIBS=-lsocket
-${THOME}/build/genpkg TRIBpinentry pinentry-1.2.0
+${THOME}/build/dobuild -64only pinentry-1.2.1 -C "--disable-libsecret --disable-pinentry-efl LIBS=-lsocket"
+${THOME}/build/genpkg TRIBpinentry pinentry-1.2.1
