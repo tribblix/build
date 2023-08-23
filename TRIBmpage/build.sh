0808a101afe8c0d0e0742ae3bd4aac00c0a95a6b
Peter Tribble <peter.tribble@gmail.com>
2023-08-23 10:35:17 +0100

    Update mpage 2.5.8

--- TRIBmpage/build.sh
+++ TRIBmpage/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/unpack mpage-2.5.7
-cd mpage-2.5.7
-gmake PREFIX=/usr
+${THOME}/build/unpack mpage-2.5.8
+cd mpage-2.5.8
+gmake PREFIX=/usr CC="gcc -m64"
 rm -fr /tmp/mm
 gmake install PREFIX=/tmp/mm/usr
 ${THOME}/build/create_pkg TRIBmpage /tmp/mm
