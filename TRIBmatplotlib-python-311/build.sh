0d9c62bbaf65e18dcd92bc595eca62ca869347d4
Peter Tribble <peter.tribble@gmail.com>
2023-08-26 14:40:36 +0100

    Add matplotlib 3.7.2

---
+++ TRIBmatplotlib-python-311/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# there's a patch to create the mplsetup.cfg file to force the use of
+# the system freetype
+#
+${THOME}/build/pkg_setup_py TRIBmatplotlib-python-311 matplotlib-3.7.2
---
+++ patches/matplotlib-3.7.2.pls
@@ -0,0 +1,1 @@
+-p1 mplsetup.patch 
