be417ff3752d4dcb29ea223c1c8861703527d9f8
Peter Tribble <peter.tribble@gmail.com>
2023-08-26 13:36:49 +0100

    Update numpy 1.25.2

--- TRIBnumpy-python-311/build.sh
+++ TRIBnumpy-python-311/build.sh
@@ -2,9 +2,9 @@
 #
 # needs a patch to restore sanity to the compilation environment
 #
-${THOME}/build/unpack numpy-1.24.2
-cd numpy-1.24.2
-$THOME/build/patches/numpy-1.24.2.preconf
+${THOME}/build/unpack numpy-1.25.2
+cd numpy-1.25.2
+$THOME/build/patches/numpy-1.25.2.preconf
 env CXX="g++ -m64" CC="gcc -m64" /usr/versions/python-3.11/bin/python3 setup.py build -j 4
 rm -fr /tmp/hg1
 env CXX="g++ -m64" CC="gcc -m64" /usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/hg1
---
+++ patches/numpy-1.25.2.pls
@@ -0,0 +1,1 @@
+-p1 numpy-setup.patch
