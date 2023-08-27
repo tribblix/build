66972a78f14d9d89817d11f0b215a7d919e42be4
Peter Tribble <peter.tribble@gmail.com>
2023-08-27 13:04:26 +0100

    Rebuild geeqie 64-bit

--- TRIBgeeqie/build.sh
+++ TRIBgeeqie/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS="-lsocket" $THOME/build/mesonbuild -gnu geeqie-2.0.1
+env TRIBBLIX_LDFLAGS="-lsocket" $THOME/build/mesonbuild -64only -gnu geeqie-2.0.1
 ${THOME}/build/genpkg TRIBgeeqie geeqie-2.0.1
