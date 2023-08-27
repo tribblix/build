b866d0db53fb62ea62f5e254e9b0479001c400fb
Peter Tribble <peter.tribble@gmail.com>
2023-08-27 13:50:00 +0100

    Build aspell 64-bit only

--- TRIBaspell/build.sh
+++ TRIBaspell/build.sh
@@ -5,8 +5,5 @@
 #
 # (which is why there is no user-friendly alias)
 #
-env CPPFLAGS="-I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild aspell-0.60.8 -C "--sysconfdir=/etc --with-curses=ncurses"
-mv aspell-0.60.8 aspell-0.60.8-32bit
-env CPPFLAGS="-I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/dobuild +64 aspell-0.60.8 -C "--sysconfdir=/etc --with-curses=ncurses"
-mv aspell-0.60.8-32bit aspell-0.60.8
+env CPPFLAGS="-I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/dobuild -64only aspell-0.60.8 -C "--sysconfdir=/etc --with-curses=ncurses"
 ${THOME}/build/genpkg TRIBaspell aspell-0.60.8
