a97995fbf5643643e4714f48717f5e7607dd232d
Peter Tribble <peter.tribble@gmail.com>
2023-08-29 07:52:09 +0100

    Update clamav 0.103.10

--- TRIBblix-clamav/build.sh
+++ TRIBblix-clamav/build.sh
@@ -8,5 +8,5 @@
 # libjson enables clamsubmit which is optional and we've not shipped
 # in the past
 #
-${THOME}/build/dobuild -64only clamav-0.103.9 -P /opt/tribblix/clamav -C --without-libjson
-${THOME}/build/genpkg TRIBblix-clamav clamav-0.103.9
+${THOME}/build/dobuild -64only clamav-0.103.10 -P /opt/tribblix/clamav -C --without-libjson
+${THOME}/build/genpkg TRIBblix-clamav clamav-0.103.10
