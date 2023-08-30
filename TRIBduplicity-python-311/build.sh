89ae7df509c3b68318e050ac69332ecd24d0258d
Peter Tribble <peter.tribble@gmail.com>
2023-08-30 08:34:53 +0100

    Update duplicity 2.1.0

--- TRIBduplicity-python-311/build.sh
+++ TRIBduplicity-python-311/build.sh
@@ -6,4 +6,4 @@
 # other optional packages it can leverage that aren't listed as dependencies
 # rsync rclone ncftp
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/pkg_setup_py TRIBduplicity-python-311 duplicity-2.0.2
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/pkg_setup_py TRIBduplicity-python-311 duplicity-2.1.0
