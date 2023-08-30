f1342349b5c8d557e0fa898b51eb57c912db0c3b
Peter Tribble <peter.tribble@gmail.com>
2023-08-30 12:39:21 +0100

    Update importlib_metadata 5.2.0

--- TRIBimportlib-python-311/build.sh
+++ TRIBimportlib-python-311/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/unpack importlib_metadata-4.10.1
-cd importlib_metadata-4.10.1
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBimportlib-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+# downrev because that's what c7n wants
+#
+${THOME}/build/pkg_pep518 TRIBimportlib-python-311 importlib_metadata-5.2.0
