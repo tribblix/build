3ebf4ed7e21422e98c28125c67c21389f131ba47
Peter Tribble <peter.tribble@gmail.com>
2023-08-31 13:33:08 +0100

    Update pandas 2.1.0

--- TRIBpandas-python-311/build.sh
+++ TRIBpandas-python-311/build.sh
@@ -15,11 +15,17 @@
 # to build its own numpy, which would fail as it doesn't have the
 # modifications we need to make it build successfully
 #
-${THOME}/build/unpack pandas-2.0.3
-cd pandas-2.0.3
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
+# but that also means that we have to have all the modules that pip
+# needs installed ahead of time, see build_require
+#
+# PATH so it can find the cython executable
+# Need to pass -m64 down to meson
+#
+${THOME}/build/unpack pandas-2.1.0
+cd pandas-2.1.0
+
+env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.11/bin:$PATH /usr/versions/python-3.11/bin/python3 -mpip install --no-deps --isolated --no-input --exists-action=a --disable-pip-version-check --no-warn-script-location --compile --no-build-isolation --root=/tmp/pil -I .
+
 ${THOME}/build/create_pkg TRIBpandas-python-311 /tmp/pil
 cd ..
 rm -fr /tmp/pil
