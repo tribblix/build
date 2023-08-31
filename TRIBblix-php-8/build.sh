3a64edcce7a6ba3f1010caa61c6eff4ce0347ae8
Peter Tribble <peter.tribble@gmail.com>
2023-08-31 19:18:00 +0100

    Update php 8.1.23, exable exif, and intl as a separate package

--- TRIBblix-php-8/build.sh
+++ TRIBblix-php-8/build.sh
@@ -13,8 +13,8 @@
 # needs a tsrm patch to avoid crashes
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-8.1.22
-cd php-8.1.22
+${THOME}/build/unpack php-8.1.23
+cd php-8.1.23
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
@@ -23,20 +23,25 @@ cd php-8.1.22
 # grav needs zip and openssl, but zip now means libzip
 # mbregex needs oniguruma, so disable it
 # nextcloud needs zlib, wants bz2
+# exif appears relatively common
+# intl is also common, build shared so it's packaged separately
 #
 # --build gets the architecture right on x86, otherwise it uses 32-bit asm
 # on sparc, need --without-pcre-jit
 #
-env CFLAGS="-std=gnu99" PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig ./configure --prefix=/opt/tribblix/php --with-ldap=no --with-apxs2=/opt/tribblix/apache2/bin/apxs --enable-bcmath --enable-mbstring --enable-mysqlnd --with-pgsql=shared,/opt/tribblix/postgres15 --with-pdo-pgsql=shared,/opt/tribblix/postgres15 --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --without-sqlite3 --without-pdo-sqlite --with-curl=/usr --enable-gd --with-jpeg --with-bz2 --with-zlib --with-zlib-dir=/usr --with-freetype --enable-sockets --enable-fpm --with-zip --with-openssl --disable-mbregex --build=x86_64-pc-solaris2.11 CFLAGS=-m64 LDFLAGS=-m64
+env CFLAGS="-std=gnu99" PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig ./configure --prefix=/opt/tribblix/php --with-ldap=no --with-apxs2=/opt/tribblix/apache2/bin/apxs --enable-bcmath --enable-mbstring --enable-mysqlnd --with-pgsql=shared,/opt/tribblix/postgres15 --with-pdo-pgsql=shared,/opt/tribblix/postgres15 --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --without-sqlite3 --without-pdo-sqlite --with-curl=/usr --enable-intl=shared --enable-gd --enable-exif --with-jpeg --with-bz2 --with-zlib --with-zlib-dir=/usr --with-freetype --enable-sockets --enable-fpm --with-zip --with-openssl --disable-mbregex --build=x86_64-pc-solaris2.11 CFLAGS=-m64 LDFLAGS=-m64 ICU_CFLAGS=-m64
 gsed -i 's:ext/sockets/ \$:ext/sockets/ -D_XPG4_2 \$:' Makefile
 gmake -j 6
-rm -fr /tmp/m2 /tmp/m2g /tmp/pear
+rm -fr /tmp/m2 /tmp/m2g /tmp/m2i /tmp/pear
 mkdir -p /tmp/m2/opt/tribblix/apache2/conf
 mkdir -p /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902
+mkdir -p /tmp/m2i/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902
 cp /opt/tribblix/apache2/conf/original/httpd.conf /tmp/m2/opt/tribblix/apache2/conf
 gmake install INSTALL_ROOT=/tmp/m2
-mv /tmp/m2/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902/*pgsql* /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902
+mv /tmp/m2/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902/*pgsql*.so /tmp/m2g/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902
+mv /tmp/m2/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902/*intl*.so /tmp/m2i/opt/tribblix/php/lib/php/extensions/no-debug-zts-20210902
 ${THOME}/build/create_pkg TRIBblix-php-8 /tmp/m2
 ${THOME}/build/create_pkg TRIBblix-php-8pg /tmp/m2g
-rm -fr /tmp/m2 /tmp/m2g /tmp/pear
+${THOME}/build/create_pkg TRIBblix-php-8intl /tmp/m2i
+rm -fr /tmp/m2 /tmp/m2g /tmp/m2i /tmp/pear
 cd ..
