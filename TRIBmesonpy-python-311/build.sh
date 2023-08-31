0a426013dec80c35a08a749cd25d264d3c075c19
Peter Tribble <peter.tribble@gmail.com>
2023-08-31 13:31:48 +0100

    Add meson-python 0.13.2

---
+++ TRIBmesonpy-python-311/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# note that this isn't meson, it's the meson-python aka mesonpy
+# module
+#
+# and note that MesonPy on PyPi is something completely different
+#
+# used by the pandas build
+#
+${THOME}/build/pkg_pep518 TRIBmesonpy-python-311 meson_python-0.13.2
---
+++ TRIBproj-metadata-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# needed by meson-python
+#
+${THOME}/build/pkg_pep518 TRIBproj-metadata-python-311 pyproject-metadata-0.7.1
