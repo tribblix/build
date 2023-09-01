fc42b647c064815036609e09d1f4c4101001f99e
Peter Tribble <peter.tribble@gmail.com>
2023-09-01 12:43:55 +0100

    Update borg 1.2.6

--- TRIBborg-python-311/build.sh
+++ TRIBborg-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-env PKG_CONFIG_PATH=/usr/lib/`${THOME}/build/getarch`/pkgconfig ${THOME}/build/pkg_setup_py TRIBborg-python-311 borgbackup-1.2.5
+env PKG_CONFIG_PATH=/usr/lib/`${THOME}/build/getarch`/pkgconfig ${THOME}/build/pkg_setup_py TRIBborg-python-311 borgbackup-1.2.6
