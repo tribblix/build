72b1028e54542e3ac8621671fa0f9b347355761a
Peter Tribble <peter.tribble@gmail.com>
2023-09-03 18:08:52 +0100

    Drop node-v16

--- TRIBv-node-v16/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# 64-bit only
-#
-env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v16.20.2 -P /usr/versions/node-v16 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v16 node-v16.20.2
