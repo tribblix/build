16d7e6cd89f7284e94a6360fc8fb67878d9fbd97
Peter Tribble <peter.tribble@gmail.com>
2023-09-03 18:02:09 +0100

    Fix OpenBLAS build recipe

--- TRIBopenblas/build.sh
+++ TRIBopenblas/build.sh
@@ -3,9 +3,13 @@
 # we only build 64-bit as the only consumers are scipy/numpy which are
 # in a 64-bit python
 #
-${THOME}/build/unpack -64 OpenBLAS-0.3.21
+${THOME}/build/unpack OpenBLAS-0.3.21
 cd OpenBLAS-0.3.21
-gmake -j PREFIX=/usr TARGET=PRESCOTT NO_STATIC=1 EXTRALIB+="-lm -lgfortran"
+#
+# Intel - TARGET=PRESCOTT
+# Sparc - TARGET=SPARC
+#
+gmake -j PREFIX=/usr BINARY=64 TARGET=PRESCOTT NO_STATIC=1 EXTRALIB+="-lm -lgfortran"
 rm -fr /tmp/mm
 env PATH=/usr/gnu/bin:$PATH gmake install PREFIX=/tmp/mm/usr BINARY=64 TARGET=PRESCOTT NO_STATIC=1 EXTRALIB+="-lm -lgfortran"
 cd ..
