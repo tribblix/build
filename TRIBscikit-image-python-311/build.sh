53f21815110370328c202c504576434c26fc931e
Peter Tribble <peter.tribble@gmail.com>
2023-09-04 15:52:50 +0100

    Add scikit-image 2.1.0

---
+++ TRIBscikit-image-python-311/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# a pep518 build requires --no-build-isolation otherwise it tries
+# to build its own numpy, which would fail as it doesn't have the
+# modifications we need to make it build successfully
+#
+# but that also means that we have to have all the modules that pip
+# needs installed ahead of time, see build_require
+#
+# PATH so it can find the cython executable
+# Need to pass -m64 down to meson
+#
+${THOME}/build/unpack scikit-image-0.21.0
+cd scikit-image-0.21.0
+
+env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.11/bin:$PATH /usr/versions/python-3.11/bin/python3 -mpip install --no-deps --isolated --no-input --exists-action=a --disable-pip-version-check --no-warn-script-location --compile --no-build-isolation --root=/tmp/pil -I .
+
+${THOME}/build/create_pkg TRIBscikit-image-python-311 /tmp/pil
+cd ..
+rm -fr /tmp/pil
