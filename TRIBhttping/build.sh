3affea29f805313fd12c469cf125d6cd6e99c23e
Peter Tribble <peter.tribble@gmail.com>
2023-09-05 16:25:10 +0100

    Update httping 2.9

--- TRIBhttping/build.sh
+++ TRIBhttping/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CC=gcc LDFLAGS="-lsocket -lnsl -lresolv" ${THOME}/build/dobuild httping-2.5
-${THOME}/build/genpkg TRIBhttping httping-2.5
+env CC="gcc -m64" LDFLAGS="-lresolv -lsocket -lnsl" ${THOME}/build/dobuild HTTPing-2.9
+${THOME}/build/genpkg TRIBhttping HTTPing-2.9
