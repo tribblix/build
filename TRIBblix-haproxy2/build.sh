a3b4efbed3aa43073e37b4da55143c325cd2b6c4
Peter Tribble <peter.tribble@gmail.com>
2023-09-05 14:31:02 +0100

    Enable haproxy built-in prometheus exporter

--- TRIBblix-haproxy2/build.sh
+++ TRIBblix-haproxy2/build.sh
@@ -8,7 +8,7 @@
 ${THOME}/build/unpack haproxy-2.6.15
 cd haproxy-2.6.15
 
-gmake -j 4 CC=gcc TARGET=solaris USE_PCRE2=1 USE_OPENSSL=1 ARCH=64
+gmake -j 4 CC=gcc TARGET=solaris USE_PCRE2=1 USE_OPENSSL=1 ARCH=64 USE_PROMEX=1
 
 sed -i 's:PREFIX = /usr/local:PREFIX = /opt/tribblix/haproxy:' Makefile 
 ${THOME}/build/genpkg TRIBblix-haproxy2
