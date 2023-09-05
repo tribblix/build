f5d186d18fae74021195d511a99dbec0a6ca0e1a
Peter Tribble <peter.tribble@gmail.com>
2023-09-05 16:11:04 +0100

    Update cmake 3.27.4

--- TRIBcmake/build.sh
+++ TRIBcmake/build.sh
@@ -2,8 +2,8 @@
 #
 #
 #
-${THOME}/build/unpack cmake-3.27.1
-cd cmake-3.27.1
+${THOME}/build/unpack cmake-3.27.4
+cd cmake-3.27.4
 ./bootstrap --prefix=/usr/versions/cmake-3.27 -- -DCMAKE_USE_OPENSSL=ON
 gmake -j 4
 # [gmake test - all passed]
