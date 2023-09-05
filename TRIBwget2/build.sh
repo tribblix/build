c682a499b1639f411cbf01793e61c306e381821e
Peter Tribble <peter.tribble@gmail.com>
2023-09-05 09:09:39 +0100

    Update wget2 2.1.0

--- TRIBwget2/build.sh
+++ TRIBwget2/build.sh
@@ -2,5 +2,5 @@
 #
 # wget2 pulls in a lot more dependencies than v1 did
 #
-${THOME}/build/dobuild -64only wget2-2.0.1 -C '--sysconfdir=/etc'
-${THOME}/build/genpkg TRIBwget2 wget2-2.0.1
+${THOME}/build/dobuild -64only wget2-2.1.0 -C '--sysconfdir=/etc'
+${THOME}/build/genpkg TRIBwget2 wget2-2.1.0
