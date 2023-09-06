94b3f9a2644755a60ec7272fbd5dccfe78ac5e44
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 15:33:29 +0100

    Update Net::SSLeay 1.92

--- TRIBlib-perl-5-net-ssleay/build.sh
+++ TRIBlib-perl-5-net-ssleay/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Net-SSLeay-1.81
-cd Net-SSLeay-1.81
+${THOME}/build/unpack Net-SSLeay-1.92
+cd Net-SSLeay-1.92
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBlib-perl-5-net-ssleay
