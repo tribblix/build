d612f7dfaea07b0fd264a59e55570103961f09be
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 15:42:19 +0100

    Update neon 0.32.5

--- TRIBneon/build.sh
+++ TRIBneon/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 neon-0.32.2 -C "--with-ssl=openssl --enable-shared --disable-static"
-${THOME}/build/genpkg TRIBneon neon-0.32.2
+${THOME}/build/dobuild -64only neon-0.32.5 -C "--with-ssl=openssl --enable-shared --disable-static"
+${THOME}/build/genpkg TRIBneon neon-0.32.5
