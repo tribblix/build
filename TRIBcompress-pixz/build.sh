d1e08696d501ce998781384d7627c969ad638059
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 13:26:44 +0100

    Update pixz 1.0.7, and make 64-bit

--- TRIBcompress-pixz/build.sh
+++ TRIBcompress-pixz/build.sh
@@ -3,5 +3,5 @@
 # install libarchive first
 # zap install libarchive
 #
-${THOME}/build/dobuild pixz-1.0.6
-${THOME}/build/genpkg TRIBcompress-pixz pixz-1.0.6
+${THOME}/build/dobuild -64only pixz-1.0.7
+${THOME}/build/genpkg TRIBcompress-pixz pixz-1.0.7
