cfed6e8965cd34837c1d24cf3a89964f9764ac2b
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 11:34:30 +0100

    Update libzip 1.10.1

--- TRIBlibzip/build.sh
+++ TRIBlibzip/build.sh
@@ -2,5 +2,5 @@
 #
 # the consumer is php, so use the same TLS implementation (openssl)
 #
-${THOME}/build/cmbuild -64only libzip-1.10.0 -C -DENABLE_GNUTLS=OFF
-${THOME}/build/genpkg TRIBlibzip libzip-1.10.0
+${THOME}/build/cmbuild -64only libzip-1.10.1 -C -DENABLE_GNUTLS=OFF
+${THOME}/build/genpkg TRIBlibzip libzip-1.10.1
