f1a2778934582a10138a7c0987f5b085800947fa
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 10:16:15 +0100

    Update aria2 1.36.0

--- TRIBaria2/build.sh
+++ TRIBaria2/build.sh
@@ -2,5 +2,5 @@
 #
 # aria2
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild aria2-1.35.0 -C "--with-openssl --without-nettle --without-sqlite3 --without-libgmp --without-gnutls --with-ca-bundle=/etc/openssl/cacert.pem"
-${THOME}/build/genpkg TRIBaria2 aria2-1.35.0
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only aria2-1.36.0 -C "--with-openssl --without-nettle --without-libcares --without-sqlite3 --without-libgmp --without-gnutls --with-ca-bundle=/etc/openssl/cacert.pem"
+${THOME}/build/genpkg TRIBaria2 aria2-1.36.0
