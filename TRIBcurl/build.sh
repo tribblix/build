ed7a85c64e19bde6486cc869a7309331ccb7eac0
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 10:31:48 +0100

    Update curl 8.2.1

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# [needs a postconf patch]
-#
-${THOME}/build/dobuild -64 curl-7.88.1 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --without-zstd --with-openssl --without-brotli"
-${THOME}/build/genpkg TRIBcurl curl-7.88.1
+${THOME}/build/dobuild -64 curl-8.2.1 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --without-zstd --with-openssl --without-brotli"
+${THOME}/build/genpkg TRIBcurl curl-8.2.1
