81af10ae0936393c56bdf3428723b6dfd63d1c54
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 13:19:41 +0100

    Update libarchive 3.7.1

--- TRIBlibarchive/build.sh
+++ TRIBlibarchive/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# used by claws mail evince samba
-#
-${THOME}/build/dobuild -64 libarchive-3.6.2
-${THOME}/build/genpkg TRIBlibarchive libarchive-3.6.2
+${THOME}/build/dobuild -64 libarchive-3.7.1
+${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.1
