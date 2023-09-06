10b03f74c17371b99cae1ff5f40e1f44855fa93a
Peter Tribble <peter.tribble@gmail.com>
2023-09-06 11:46:31 +0100

    Update libpcap 1.10.4

--- TRIBlibpcap/build.sh
+++ TRIBlibpcap/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libpcap-1.10.3
-${THOME}/build/genpkg TRIBlibpcap libpcap-1.10.3
+${THOME}/build/dobuild -64 libpcap-1.10.4
+${THOME}/build/genpkg TRIBlibpcap libpcap-1.10.4
