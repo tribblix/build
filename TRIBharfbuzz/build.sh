067ff1e4c93698f4cb763b8e6e4796c7f0c8a130
Peter Tribble <peter.tribble@gmail.com>
2023-09-07 19:24:50 +0100

    Build harfbuzz with graphite2

--- TRIBharfbuzz/build.sh
+++ TRIBharfbuzz/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# Rebuild harfbuzz, with icu, and 64-bit (which it used to have anyway)
+# with icu (which it used to have anyway)
+# with graphite2 (for LibreOffice)
+#
 zap install TRIBlibicu
-$THOME/build/mesonbuild -64 harfbuzz-4.3.0 -C "-Dintrospection=disabled -Dglib=enabled -Dcairo=enabled -Dicu=enabled -Dfreetype=enabled"
+$THOME/build/mesonbuild -64 harfbuzz-4.3.0 -C "-Dintrospection=disabled -Dglib=enabled -Dcairo=enabled -Dicu=enabled -Dfreetype=enabled -Dgraphite2=enabled"
 ${THOME}/build/genpkg TRIBharfbuzz harfbuzz-4.3.0
