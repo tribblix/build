da2323d2f8136443c04b660f7896d10bef37d35b
Peter Tribble <peter.tribble@gmail.com>
2023-09-08 15:27:44 +0100

    Update erlang/OTP 26.0.2

--- TRIBerlang/build.sh
+++ TRIBerlang/build.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 #
-# current RabbitMQ and LeoFS both want something in the 19-21 range
-# go for 20 as 21 is still a little new
+# for testing only; seeing a number of failures in the test suite
 #
-# Erlang 18.3, use our 64-bit (the --enable-m64-build flag is broken)
-# Erlang 20.3 you need both our flags and the erlang 64-bit flag
+# current RabbitMQ needs 25 minimum, but supports 26
 #
-# needs gnu m4 and make being gmake
-# but you can't change PATH as that pulls in the wrong ld
+# you need both our flags and the erlang 64-bit flag
 #
-env M4=/usr/gnu/bin/m4 MAKE=gmake ${THOME}/build/dobuild otp_src_20.3
-mv otp_src_20.3 otp_src_20.3-32bit
-env M4=/usr/gnu/bin/m4 MAKE=gmake ${THOME}/build/dobuild +64 otp_src_20.3 -C --enable-m64-build
-mv otp_src_20.3-32bit otp_src_20.3
+# the DED_LD stuff is because erlang "knows" that you need to link
+# with -64 on solaris, but passing -64 to gcc doesn't work
+#
+env DED_LD="ld" DED_LDFLAGS="-m64 -G" DED_LD_FLAG_RUNTIME_LIBRARY_PATH="-R" \
+${THOME}/build/dobuild -64only otp_src_26.0.2 -C --enable-m64-build
 #
 # [tests:
-# cd otp_src_20.3
+# cd otp_src_26.0.2-64bit
 # env ERL_TOP=`pwd` gmake release_tests
 # env ERL_TOP=`pwd` sh 'cd release/tests/test_server; $ERL_TOP/bin/erl -s ts install -s ts smoke_test batch -s init stop'
 # ls -l `pwd`/release/tests/test_server/index.html
 # cd ..
 # ]
 #
-env M4=/usr/gnu/bin/m4 MAKE=gmake ${THOME}/build/genpkg TRIBerlang otp_src_20.3
+env DED_LD="ld" DED_LDFLAGS="-m64 -G" DED_LD_FLAG_RUNTIME_LIBRARY_PATH="-R" \
+${THOME}/build/genpkg TRIBerlang otp_src_26.0.2
