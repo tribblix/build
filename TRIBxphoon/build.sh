6a69c309a7039917d1869bf3ee16d62c1f57362c
Peter Tribble <peter.tribble@gmail.com>
2023-09-09 14:21:53 +0100

    Fix xphoon pkginfo, and make 64-bit

--- TRIBxphoon/build.sh
+++ TRIBxphoon/build.sh
@@ -5,6 +5,7 @@ cd xphoon
 sed -i s:OS_SYSV:SVR4: date_parse.c
 # yes gsed for the newline
 gsed -i 's:<sys/ioctl.h>:<sys/ioctl.h>\n#include <sys/termio.h>:' xphoon.c
+sed -i 's:-ansi:-m64 -ansi:' Imakefile
 xmkmf -DHasGcc3=YES
 make
 cd ..
