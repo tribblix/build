b479c49f9640959054d705a3b29c9458e6e532e6
Peter Tribble <peter.tribble@gmail.com>
2023-09-12 20:26:02 +0100

    Add Octave 8.3.0

---
+++ TRIBoctave/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# initial build ofr testing; we could do with adding hdf5, glpk, qhull
+#
+env FFLAGS=-m64 ${THOME}/build/dobuild -64only octave-8.3.0 -C "--disable-openmp --disable-readline --disable-dependency-tracking"
+
+${THOME}/build/genpkg TRIBoctave octave-8.3.0
