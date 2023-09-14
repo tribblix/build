5cb9a0987463d97834b87ae7d1d3cb9aa9997500
Peter Tribble <peter.tribble@gmail.com>
2023-09-14 22:21:05 +0100

    Update libwebp 1.3.2

--- TRIBlibwebp/build.sh
+++ TRIBlibwebp/build.sh
@@ -6,5 +6,5 @@
 # built against it; once they have all been rebuild it can be removed
 #
 ${THOME}/build/dobuild -64 libwebp-0.5.1
-${THOME}/build/dobuild -64 libwebp-1.3.0 -C "--disable-gl"
-${THOME}/build/genpkg TRIBlibwebp libwebp-0.5.1 libwebp-1.3.0
+${THOME}/build/dobuild -64 libwebp-1.3.2 -C "--disable-gl"
+${THOME}/build/genpkg TRIBlibwebp libwebp-0.5.1 libwebp-1.3.2
