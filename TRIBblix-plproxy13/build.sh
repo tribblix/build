f377b2f5e56d80608eb839d48da4f1da1661707c
Peter Tribble <peter.tribble@gmail.com>
2023-09-14 15:59:30 +0100

    Drop unmaintained plproxy

--- TRIBblix-plproxy13/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# this is a postgres extension, so it's specific to the postgres version
-#
-${THOME}/build/dobuild +64only plproxy-2.10.0 -M "PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config"
-${THOME}/build/genpkg -I "install PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config" TRIBblix-plproxy13 plproxy-2.10.0
