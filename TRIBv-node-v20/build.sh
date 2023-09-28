359c2c6bfbaee4a649ab83982d22aa08ccdca91e
Peter Tribble <peter.tribble@gmail.com>
2023-09-28 14:54:19 +0100

    Update node v20.7.0

--- TRIBv-node-v20/build.sh
+++ TRIBv-node-v20/build.sh
@@ -2,5 +2,5 @@
 #
 # 64-bit only
 #
-env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v20.6.0 -P /usr/versions/node-v20 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v20 node-v20.6.0
+env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v20.7.0 -P /usr/versions/node-v20 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v20 node-v20.7.0
