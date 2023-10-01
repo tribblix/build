175717b2c3ef6d34de119ff241c9256d356362fd
Peter Tribble <peter.tribble@gmail.com>
2023-10-01 11:06:02 +0100

    Update cycler 0.12.0

--- TRIBcycler-python-311/build.sh
+++ TRIBcycler-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack cycler-0.11.0
-cd cycler-0.11.0
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBcycler-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_pep518 TRIBcycler-python-311 cycler-0.12.0
