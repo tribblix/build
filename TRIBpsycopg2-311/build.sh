a7cb1004abe7f974807d3784ba9112f0a5146507
Peter Tribble <peter.tribble@gmail.com>
2023-10-02 10:25:44 +0100

    Update psycopg2 2.9.8

--- TRIBpsycopg2-311/build.sh
+++ TRIBpsycopg2-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg2-311 psycopg2-2.9.7
+env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg2-311 psycopg2-2.9.8
