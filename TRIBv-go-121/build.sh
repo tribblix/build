9595fb0019549b9004b22788b302a9c3889bb979
Peter Tribble <peter.tribble@gmail.com>
2023-10-05 21:42:12 +0100

    Update Go 1.21.2

--- TRIBv-go-121/build.sh
+++ TRIBv-go-121/build.sh
@@ -12,7 +12,7 @@
 #
 chmod -R u+w go
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.21.1.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.21.2.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.21 GOROOT_BOOTSTRAP=/usr/versions/go-1.20 ./all.bash
 cd ..
