38ea2852c4d151552a631cfd1513929bad9a744e
Peter Tribble <peter.tribble@gmail.com>
2023-10-09 11:24:04 +0100

    Update protocol buffers 3.20.1, and ship 64-bit only

--- TRIBprotobuf/build.sh
+++ TRIBprotobuf/build.sh
@@ -2,13 +2,28 @@
 #
 # Default build is 64-bit
 #
-${THOME}/build/unpack -64 protobuf-2.6.1
-cd protobuf-2.6.1-64bit
-./configure --prefix=/usr --bindir=/usr/bin/`$THOME/build/getarch`
+
+#
+# old version for binary compatibility only needed on x86
+#
+${THOME}/build/unpack protobuf-2.6.1
+cd protobuf-2.6.1
+./configure --prefix=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/pbf
-cd ../protobuf-2.6.1
-./configure --prefix=/usr --disable-64bit-solaris
+rm -fr /tmp/pbf/usr/include /tmp/pbf/usr/bin
+rm -fr /tmp/pbf/usr/lib/amd64/pkgconfig
+rm -f /tmp/pbf/usr/lib/amd64/*.{a,la,so}
+cd ..
+
+#
+# current version
+#
+${THOME}/build/unpack protobuf-cpp-3.20.1
+cd protobuf-3.20.1
+./configure --prefix=/usr
 gmake -j 4
 gmake install DESTDIR=/tmp/pbf
+cd ..
+
 ${THOME}/build/create_pkg TRIBprotobuf /tmp/pbf
