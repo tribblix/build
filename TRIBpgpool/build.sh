87fc119eeac008147e978ad6ceea17f0b39aacbc
Peter Tribble <peter.tribble@gmail.com>
2023-10-09 13:42:38 +0100

    Rebuild pgpool so that pgproto gets linked correctly

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -2,5 +2,8 @@
 #
 # needs a patch and postconf
 #
-env PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.4 -C "--sysconfdir=/etc --with-openssl"
+# specify LD_LRUN_PATH so that pgproto also picks it up and can find
+# libpq at runtime
+#
+env LD_RUN_PATH=/opt/tribblix/postgres15/lib PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.4 -C "--sysconfdir=/etc --with-openssl"
 ${THOME}/build/genpkg TRIBpgpool pgpool-II-4.4.4
