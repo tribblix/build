44cacbf1cd3d17a02cfc6331f0988b801455a911
Peter Tribble <peter.tribble@gmail.com>
2023-10-09 13:55:14 +0100

    Update XEphem 4.1.0

--- TRIBxephem/build.sh
+++ TRIBxephem/build.sh
@@ -2,16 +2,16 @@
 #
 #
 #
-${THOME}/build/unpack xephem-3.7.7
-cd xephem-3.7.7/GUI/xephem
+${THOME}/build/unpack XEphem-4.1.0
+cd XEphem-4.1.0/GUI/xephem
 gsed -i 's:-lm:-lsocket -lnsl -lm:' Makefile
-gmake CC=gcc MOTIFI=/usr/include MOTIFL=""
+gmake CC="gcc -m64" MOTIFI=/usr/include MOTIFL=""
 rm -fr /tmp/xep1
 mkdir -p /tmp/xep1/usr/bin
 mkdir -p /tmp/xep1/usr/share/xephem
 mkdir -p /tmp/xep1/usr/share/man/man1
 cp xephem /tmp/xep1/usr/bin
 cp -R auxil catalogs fifos fits gallery help lo /tmp/xep1/usr/share/xephem
-cp xephem.1 /tmp/xep1/usr/share/man/man1
+cp xephem.man /tmp/xep1/usr/share/man/man1/xephem.1
 ${THOME}/build/create_pkg TRIBxephem /tmp/xep1
 rm -fr /tmp/xep1
