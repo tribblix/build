694b16fa142f5c542a107194fbb24cf97b5363d6
Peter Tribble <peter.tribble@gmail.com>
2023-10-10 19:22:26 +0100

    Add jupyter-server 2.7.3

---
+++ TRIBjupyter-server-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBjupyter-server-python-311 jupyter_server-2.7.3
---
+++ TRIBwebsocket-client-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBwebsocket-client-python-311 websocket-client-1.6.4
