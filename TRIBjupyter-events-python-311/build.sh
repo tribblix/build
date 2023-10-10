60cf90eec3b2c89782e8e131fd81b62317eccb16
Peter Tribble <peter.tribble@gmail.com>
2023-10-10 18:12:27 +0100

    Add jupyter-events 0.7.0

---
+++ TRIBjupyter-events-python-311/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# this depends on jsonschema[format-nongpl], see the file
+# TRIBjsonschema4-python-311/depend.format-nongpl
+#
+${THOME}/build/pkg_pep518 TRIBjupyter-events-python-311 jupyter_events-0.7.0
