39f49a014cdf3e6e20d4199ec668dc91bee43373
Peter Tribble <peter.tribble@gmail.com>
2023-10-10 10:08:16 +0100

    Tweak duplicity dependencies

--- TRIBduplicity-python-311/build.sh
+++ TRIBduplicity-python-311/build.sh
@@ -2,10 +2,13 @@
 #
 # note that it declares setup-scm as a runtime dependency, so we do too
 #
-# the backends are soft requirements, I add those we actually need (AWS)
-# and those we already have packages for.
-# paramiko (enables ssh) boto3 (enables S3)
+# the backends are soft requirements, the dependencies declared here
+# are a basic subset, the overlay can add additional packages
+# which may vary by platform, but we add boto3 as it's the default
+#
 # other optional packages it can leverage that aren't listed as dependencies
 # rsync rclone ncftp
+# paramiko *or* pexpect for ssh
+# requests is used by multiple backends
 #
 env PATH=/usr/gnu/bin:$PATH ${THOME}/build/pkg_setup_py TRIBduplicity-python-311 duplicity-2.1.3
