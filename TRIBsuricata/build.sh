f67a17aeb4511733860896c52d7f71b13175ec4a
Peter Tribble <peter.tribble@gmail.com>
2023-10-11 22:04:20 +0100

    Drop suricata, supported versions don't build

--- TRIBsuricata/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# rust code in 5.0.10 doesn't build
-#
-env PATH=/usr/versions/python-3.11/bin:$PATH TRIBBLIX_LDFLAGS=-lm ${THOME}/build/dobuild -64only suricata-5.0.9 -C "--with-libpcre-includes=/usr/include/pcre --enable-gccmarch-native=no --sysconfdir=/etc/ --localstatedir=/var/"
-${THOME}/build/genpkg -I install-full TRIBsuricata suricata-5.0.9
