35644ea98a9d39ddfb71322ed325a1c46caef5d8
Peter Tribble <peter.tribble@gmail.com>
2023-10-11 16:50:58 +0100

    Add cups-pdf

---
+++ TRIBcups-pdf/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+${THOME}/build/unpack cups-pdf_3.0.1
+# note that tarball and contents are slightly different names
+cd cups-pdf-3.0.1/src
+# setpdfwrite is removed from gs
+gpatch < ${THOME}/build/patches/cups-pdf-new-gs.patch
+gcc -O -m64 -s cups-pdf.c -o cups-pdf -lcups
+cd ..
+
+rm -fr /tmp/xs
+
+mkdir -p /tmp/xs/usr/lib/cups/backend
+cp src/cups-pdf /tmp/xs/usr/lib/cups/backend
+chmod 0700 /tmp/xs/usr/lib/cups/backend/cups-pdf
+
+mkdir -p /tmp/xs/etc/cups
+cp extra/cups-pdf.conf /tmp/xs/etc/cups
+chmod 640 /tmp/xs/etc/cups/cups-pdf.conf
+
+mkdir -p /tmp/xs/usr/share/cups/model
+cp extra/CUPS-PDF_opt.ppd /tmp/xs/usr/share/cups/model/CUPS-PDF.ppd
+chmod 444 /tmp/xs/usr/share/cups/model/CUPS-PDF.ppd
+
+mkdir -p /tmp/xs/var/spool/cups-pdf/SPOOL
+
+${THOME}/build/create_pkg TRIBcups-pdf /tmp/xs
+rm -fr /tmp/xs
