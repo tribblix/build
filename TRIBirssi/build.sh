4d9acc8f86871da15c307a3ab8b092f4d03a9759
Peter Tribble <peter.tribble@gmail.com>
2023-10-11 14:55:56 +0100

    Update irssi 1.4.5, and enable perl

--- TRIBirssi/build.sh
+++ TRIBirssi/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only irssi-1.4.4 -C --with-perl=no
-${THOME}/build/genpkg TRIBirssi irssi-1.4.4
+env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only irssi-1.4.5 -C "--with-perl=yes --with-perl-lib=/usr/perl5/site_perl/5.34"
+${THOME}/build/genpkg TRIBirssi irssi-1.4.5
