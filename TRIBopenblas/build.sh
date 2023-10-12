bc528482b1ca479572eeef28f9f0edfe444873c4
Peter Tribble <peter.tribble@gmail.com>
2023-10-12 10:50:48 +0100

    Fix OpenBLAS on sparc

--- TRIBopenblas/build.sh
+++ TRIBopenblas/build.sh
@@ -6,6 +6,12 @@
 ${THOME}/build/unpack OpenBLAS-0.3.21
 cd OpenBLAS-0.3.21
 #
+# this generates an assembler error on sparc, so put in the old
+# 0.3.20 version, caused by
+# https://github.com/OpenMathLib/OpenBLAS/commit/06ef015234584f57d6743a78df7a7a05f6b39c11
+#
+cp ${THOME}/build/patches/dnrm2.S kernel/sparc
+#
 # Intel - TARGET=PRESCOTT
 # Sparc - TARGET=SPARC
 #
