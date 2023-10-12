c5724841a7a48dba909a87352a28d093d6158a5a
Peter Tribble <peter.tribble@gmail.com>
2023-10-12 17:15:40 +0100

    Rebuild openjpeg, force static off so that installed cmake files are self-consistent

--- TRIBlib-openjpeg/build.sh
+++ TRIBlib-openjpeg/build.sh
@@ -2,6 +2,9 @@
 #
 # specifying architecture directories for lib and bin doesn't work
 # so they're corrected by the fixinstall.64 script
-cd ..
-env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild -64 openjpeg-2.5.0
+#
+# need to not build static libs in the first place so the installed
+# cmake config is consistent
+#
+env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild -64 openjpeg-2.5.0 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF"
 ${THOME}/build/genpkg TRIBlib-openjpeg openjpeg-2.5.0
