732603d617535b9ad552c0c6d4c4e0586f7e7f1a
Peter Tribble <peter.tribble@gmail.com>
2023-10-12 15:51:12 +0100

    Rebuild readline, explicitly requiring libcurses

--- TRIBreadline/build.sh
+++ TRIBreadline/build.sh
@@ -2,5 +2,10 @@
 #
 # try parking in /usr/gnu to partially hide it
 #
-${THOME}/build/dobuild -64 readline-6.3 -P /usr/gnu
+# notes that we need to explicitly link against curses
+# that's what it actually does implicitly anyway, but if we don't
+# explicitly then it doesn't pull in the shared library, relying on
+# the consumer accidentally doing so
+#
+env TRIBBLIX_LDFLAGS=-lcurses ${THOME}/build/dobuild -64 readline-6.3 -P /usr/gnu -C --with-curses
 ${THOME}/build/genpkg TRIBreadline readline-6.3
