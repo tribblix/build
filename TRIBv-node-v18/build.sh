31942fed5464f896e7fab2a57429be86bba6ef58
Peter Tribble <peter.tribble@gmail.com>
2023-10-15 20:32:39 +0100

    Update node v18.18.2

--- TRIBv-node-v18/build.sh
+++ TRIBv-node-v18/build.sh
@@ -2,5 +2,5 @@
 #
 # 64-bit only
 #
-env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v18.18.0 -P /usr/versions/node-v18 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v18 node-v18.18.0
+env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v18.18.2 -P /usr/versions/node-v18 -J 6 -C "--without-dtrace --dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v18 node-v18.18.2
