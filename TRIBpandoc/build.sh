3ad6471c31af6f5ee92eb1940ca3da91d0b347e5
Peter Tribble <peter.tribble@gmail.com>
2023-10-17 09:54:50 +0100

    Add pandoc

---
+++ TRIBpandoc/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+
+#
+# Custom cabal method as described at
+# https://pandoc.org/installing.html#compiling-from-source
+#
+cabal update
+
+$THOME/build/unpack pandoc-cli-0.1.1.1
+cd pandoc-cli-0.1.1.1
+
+cabal configure --prefix=/usr --flags=embed_data_files
+cabal build
+cabal install --installdir=/tmp/pd/usr/bin --install-method=copy -fembed_data_files
+strip /tmp/pd/usr/bin/pandoc
+
+#
+# the man page is in the library, not the cli package
+#
+$THOME/build/unpack pandoc-3.1.8
+mkdir -p /tmp/pd/usr/share/man/man1
+cp pandoc-3.1.8/man/pandoc.1 /tmp/pd/usr/share/man/man1
+
+$THOME/build/create_pkg TRIBpandoc /tmp/pd
