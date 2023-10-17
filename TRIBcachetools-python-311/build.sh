68e73d586b063d17334ed2581dd0b490140249f7
Peter Tribble <peter.tribble@gmail.com>
2023-10-17 20:13:14 +0100

    Update cachetools 5.3.1

--- TRIBcachetools-python-311/build.sh
+++ TRIBcachetools-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack cachetools-4.2.4
-cd cachetools-4.2.4
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBcachetools-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBcachetools-python-311 cachetools-5.3.1
