b74ece474bd00c72251d076cb0297dc40fc71372
Peter Tribble <peter.tribble@gmail.com>
2023-10-17 11:03:41 +0100

    Add eza 0.14.2

---
+++ TRIBeza/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+
+#
+git clone https://github.com/eza-community/eza
+cd eza
+env PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig cargo build --release
+
+rm -fr /tmp/ll
+
+mkdir -p /tmp/ll/usr/bin
+ginstall -s -Dm755 ./target/release/eza -t /tmp/ll/usr/bin
+
+# the man page requires pandoc or similar
+mkdir -p /tmp/ll/usr/share/man/man1
+mkdir -p /tmp/ll/usr/share/man/man5
+pandoc --to=man man/eza.1.md --output=/tmp/ll/usr/share/man/man1/eza.1
+pandoc --to=man man/eza_colors.5.md --output=/tmp/ll/usr/share/man/man5/eza_colors.5
+pandoc --to=man man/eza_colors-explanation.5.md --output=/tmp/ll/usr/share/man/man5/eza_colors-explanation.5
+
+mkdir -p /tmp/ll/usr/share/eza
+cp LICENCE README.md /tmp/ll/usr/share/eza
+
+mkdir -p /tmp/ll/usr/share/bash-completion/completions
+cp completions/bash/eza /tmp/ll/usr/share/bash-completion/completions
+mkdir -p /tmp/ll/usr/share/fish/completions
+cp completions/fish/eza.fish /tmp/ll/usr/share/fish/completions
+mkdir -p /tmp/ll/usr/share/zsh/site-functions
+cp completions/zsh/_eza /tmp/ll/usr/share/zsh/site-functions
+
+${THOME}/build/create_pkg TRIBeza /tmp/ll
+rm -fr /tmp/ll
