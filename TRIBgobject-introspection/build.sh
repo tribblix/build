9edf2aaf29cd5a4dabc6d9125d170f3f486d040a
Peter Tribble <peter.tribble@gmail.com>
2023-10-18 14:21:59 +0100

    Update gobject-introspection 1.72.1, rebuild for python3.11, update libs, add 64-bit pango typelib

--- TRIBgobject-introspection/build.sh
+++ TRIBgobject-introspection/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 #
-# fix - build pango to extract the gir files
 # note that you need to have gobject-introspection installed
-# first in order for the sacrificial pango build to generate
-# its gir files
+# first in order for the sacrificial builds to generate the gir files
 #
 # also need gtk3 and gdk-pixbuf
 # but the gtk3 one needs atk and gdk-pixbuf built *and* installed first
@@ -13,19 +11,28 @@
 #
 # the builds used here should be the same as the regular package build
 # for that component, but without introspection disabled (that's the point)
-# and there's no need for the 64-bit build either
 #
 # these builds need to match the versions in fixinstall
 #
+# note that if you have to update gobject-introspection you'll
+# probably have to go round the loop from scratch
+#
 ${THOME}/build/dobuild gdk-pixbuf-2.36.12 -C --with-x11
-${THOME}/build/dobuild atk-2.20.0
-${THOME}/build/dobuild -gnu gtk+-3.22.30 -C --disable-cups
+${THOME}/build/mesonbuild atk-2.38.0
+${THOME}/build/dobuild -gnu gtk+-3.24.34 -C --disable-cups
 env CC=gcc ${THOME}/build/mesonbuild pango-1.44.7
+
+#
+# 64-bit, gdk just SEGV's, Pango needs extra help
+#
+mv pango-1.44.7 pango-1.44.7-32bit
+env CC="gcc -m64" ${THOME}/build/mesonbuild +64 pango-1.44.7
+mv pango-1.44.7-32bit pango-1.44.7
 
 #
 # the old version is only compatible with python2, so this makes sure
 # we not only link against python2 but put it in the shebang too
 #
 env PYTHON=/usr/bin/python2 ${THOME}/build/dobuild gobject-introspection-1.44.0
-$THOME/build/mesonbuild +64 gobject-introspection-1.72.0
-${THOME}/build/genpkg TRIBgobject-introspection gobject-introspection-1.44.0 gobject-introspection-1.72.0
+$THOME/build/mesonbuild +64 gobject-introspection-1.72.1
+${THOME}/build/genpkg TRIBgobject-introspection gobject-introspection-1.44.0 gobject-introspection-1.72.1
