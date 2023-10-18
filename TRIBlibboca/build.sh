b524a37b5763e96e03d104d90c5c451433284ca1
Peter Tribble <peter.tribble@gmail.com>
2023-10-18 16:20:24 +0100

    libboca only needs to be 64-bit

--- TRIBlibboca/build.sh
+++ TRIBlibboca/build.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 #
-${THOME}/build/unpack -64 boca-1.0.7
+${THOME}/build/unpack boca-1.0.7
 cd boca-1.0.7
-sed -i s:winegcc:XXwinegccXX: Makefile-options
-gmake -j 4 CC=gcc prefix=/usr
-cd ..
-cd boca-1.0.7-64bit
 sed -i s:winegcc:XXwinegccXX: Makefile-options
 gmake -j 4 CC="gcc -m64" CXX="g++ -m64" prefix=/usr libdir=/usr/lib/`$THOME/build/getarch`
 cd ..
 
 rm -fr /tmp/iboca
 cd boca-1.0.7
-gmake install CC=gcc prefix=/usr DESTDIR=/tmp/iboca
-cd ..
-cd boca-1.0.7-64bit
 gmake install CC="gcc -m64" CXX="g++ -m64" prefix=/usr libdir=/usr/lib/`$THOME/build/getarch` DESTDIR=/tmp/iboca
 cd ..
 
