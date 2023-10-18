f71a5b67a39674398cefe98952abc89c2964e120
Peter Tribble <peter.tribble@gmail.com>
2023-10-18 19:12:12 +0100

    Update ncdu 1.19, and build 64-bit

--- TRIBncdu/build.sh
+++ TRIBncdu/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib -R/usr/gnu/lib" ${THOME}/build/dobuild ncdu-1.14.1
-${THOME}/build/genpkg TRIBncdu ncdu-1.14.1
+env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only ncdu-1.19
+${THOME}/build/genpkg TRIBncdu ncdu-1.19
