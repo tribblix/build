9e1f7cd6b20b968ea7dfb8633a7f17e9c07554bb
Peter Tribble <peter.tribble@gmail.com>
2023-10-18 16:38:39 +0100

    gpicview can be 64-bit

--- TRIBgpicview/build.sh
+++ TRIBgpicview/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu gpicview-0.2.5 -C --enable-gtk3
+${THOME}/build/dobuild -gnu -64only gpicview-0.2.5 -C --enable-gtk3
 ${THOME}/build/genpkg TRIBgpicview gpicview-0.2.5
