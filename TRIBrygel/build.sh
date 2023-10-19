3949ed7b971ed6bfee00e66ed7c6d86c042f5534
Peter Tribble <peter.tribble@gmail.com>
2023-10-19 21:47:02 +0100

    Update rygel 0.36.2, and build 64-bit

--- TRIBrygel/build.sh
+++ TRIBrygel/build.sh
@@ -2,5 +2,5 @@
 #
 # rygel [needs gupnp gupnp-av gupnp-dlna vala libmediaart tracker]
 #
-${THOME}/build/dobuild -gnu rygel-0.36.1 -C "--disable-introspection --without-ui"
-${THOME}/build/genpkg TRIBrygel rygel-0.36.1
+${THOME}/build/dobuild -gnu -64only rygel-0.36.2 -C "--disable-introspection --without-ui"
+${THOME}/build/genpkg TRIBrygel rygel-0.36.2
