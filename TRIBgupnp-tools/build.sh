8b290eab36bf12cbb6a90a4f55eb72b7ad35a693
Peter Tribble <peter.tribble@gmail.com>
2023-10-19 21:02:50 +0100

    Rebuild gupnp-tools 64-bit, fix gupnp-network-light

--- TRIBgupnp-tools/build.sh
+++ TRIBgupnp-tools/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar ${THOME}/build/mesonbuild -64 gupnp-tools-0.8.15
+# there's a postconf file to make gupnp-network-light work
+#
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only gupnp-tools-0.8.15
 ${THOME}/build/genpkg TRIBgupnp-tools gupnp-tools-0.8.15
