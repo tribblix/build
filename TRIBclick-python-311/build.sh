7e9433c9ba8f0d404668b751e3cc2b0d4a4e3e1f
Peter Tribble <peter.tribble@gmail.com>
2023-10-21 13:28:46 +0100

    Update click 8.1.7

--- TRIBclick-python-311/build.sh
+++ TRIBclick-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack click-7.1.2
-cd click-7.1.2
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBclick-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBclick-python-311 click-8.1.7
