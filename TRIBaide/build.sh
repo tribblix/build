ffd4bce894bb3afe11c86d4f6055284372ac13c0
Peter Tribble <peter.tribble@gmail.com>
2023-10-23 18:59:24 +0100

    Update aide 0.18.6

--- TRIBaide/build.sh
+++ TRIBaide/build.sh
@@ -2,5 +2,5 @@
 #
 # patch to disable pie
 #
-${THOME}/build/dobuild -64only aide-0.18.2
-${THOME}/build/genpkg TRIBaide aide-0.18.2
+${THOME}/build/dobuild -64only aide-0.18.6
+${THOME}/build/genpkg TRIBaide aide-0.18.6
