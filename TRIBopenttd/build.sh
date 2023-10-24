cf9794ba97f5045b35341e34bdf351519b2c33d0
Peter Tribble <peter.tribble@gmail.com>
2023-10-24 10:59:42 +0100

    Need openttd to be downrev on sparc

--- TRIBopenttd/build.sh
+++ TRIBopenttd/build.sh
@@ -2,5 +2,8 @@
 #
 # needs TRIBlibsdl2
 #
+# current requires charconv, but doen't check for it - that came in with gcc8
+# so we need to be downrev on sparc which only has gcc7
+#
 env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild openttd-13.4
 $THOME/build/genpkg TRIBopenttd openttd-13.4
