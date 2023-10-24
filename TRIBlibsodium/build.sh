89da66dcddca030b61aae2c1991ae202b6b4ff22
Peter Tribble <peter.tribble@gmail.com>
2023-10-24 16:07:27 +0100

    Modify PyNaCl to use a system libsodium

---
+++ TRIBlibsodium/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# the intention is to build pynacl against the system libsodium provided here,
+# to reduce fragility in the pynacl build
+#
+# the downloaded tarball unpacks into libsodium-stable, so
+# repack it to create something more useful
+#
+${THOME}/build/dobuild +64 libsodium-1.0.19
+${THOME}/build/genpkg TRIBlibsodium libsodium-1.0.19
--- TRIBpynacl-python-311/build.sh
+++ TRIBpynacl-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpynacl-python-311 PyNaCl-1.5.0
+env SODIUM_INSTALL=system ${THOME}/build/pkg_setup_py TRIBpynacl-python-311 PyNaCl-1.5.0
