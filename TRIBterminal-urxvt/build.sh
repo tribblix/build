d4b550788fa7c0e571f1de56ca03c3e19e6cb01f
Peter Tribble <peter.tribble@gmail.com>
2023-10-25 09:17:58 +0100

    Fix urxvt build for sparc

--- TRIBterminal-urxvt/build.sh
+++ TRIBterminal-urxvt/build.sh
@@ -3,6 +3,6 @@
 # urxvt used to bundle libptytty, we now have to build it separately
 # as a preconf script
 #
-env TRIBBLIX_PKG_CONFIG_PATH=/tmp/libptytty/lib/amd64/pkgconfig ${THOME}/build/dobuild -64only rxvt-unicode-9.31
+env TRIBBLIX_PKG_CONFIG_PATH=/tmp/libptytty/lib/`${THOME}/build/getarch`/pkgconfig ${THOME}/build/dobuild -64only rxvt-unicode-9.31
 ${THOME}/build/genpkg TRIBterminal-urxvt rxvt-unicode-9.31
 rm -fr /tmp/libptytty
