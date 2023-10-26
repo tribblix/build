b2d873381c7acd3a2f36dd52013aeefbc737db5b
Peter Tribble <peter.tribble@gmail.com>
2023-10-26 15:23:05 +0100

    Update XScreenSaver 6.08

--- TRIBxscreensaver/build.sh
+++ TRIBxscreensaver/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only -gnu xscreensaver-6.04 -C "--with-gtk"
-cd xscreensaver-6.04-64bit
+${THOME}/build/dobuild -64only -gnu xscreensaver-6.08 -C "--with-gtk"
+cd xscreensaver-6.08-64bit
 rm -fr /tmp/xs
 gmake install install_prefix=/tmp/xs
 ${THOME}/build/create_pkg TRIBxscreensaver /tmp/xs
