14d5537f0298015ecdb755d8b1f16dc32e3d76e7
Peter Tribble <peter.tribble@gmail.com>
2023-10-26 09:10:03 +0100

    Add dependencies for pelican importer

--- TRIBpelican-python-311/build.sh
+++ TRIBpelican-python-311/build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 #
+# the following are optional dependencies that we pull in for
+# the pelican importer:
+#
+# BeautifulSoup4 lxml feedparser
+#
+# and pandoc, which makes this only useful on x86
+#
 ${THOME}/build/pkg_pep518 TRIBpelican-python-311 pelican-4.8.0
