a48eb2934c1a2a2f3c78f60fd81aa9483b04e1d4
Peter Tribble <peter.tribble@gmail.com>
2023-10-27 14:02:42 +0100

    Update prometheus bind exporter 0.7.0

--- TRIBblix-prombind/build.sh
+++ TRIBblix-prombind/build.sh
@@ -1,27 +1,21 @@
 #!/bin/tcsh
 #
 
-$THOME/build/unpack bind_exporter-0.5.0
-cd bind_exporter-0.5.0
-
 mkdir p1
 cd p1
 setenv GOPATH `pwd`
-mkdir -p $GOPATH/src/github.com/prometheus
-cd $GOPATH/src/github.com/prometheus
 
 #
 # we need to build the promu tool first, as otherwise the main build
 # tries to download it which fails as there isn't a prebuilt one
-# for solaris
 #
 git clone https://github.com/prometheus/promu.git
 cd promu
-env PATH=/usr/versions/go-1.20/bin:$PATH gmake build
+env PATH=/usr/versions/go-1.21/bin:$PATH gmake build
 cd ..
 
-$THOME/build/unpack bind_exporter-0.6.1
-cd bind_exporter-0.6.1
-env PATH=/usr/versions/go-1.20/bin:$PATH gmake build
+$THOME/build/unpack bind_exporter-0.7.0
+cd bind_exporter-0.7.0
+env PATH=/usr/versions/go-1.21/bin:$PATH gmake build
 
 ${THOME}/build/genpkg TRIBblix-prombind
