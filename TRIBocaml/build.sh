a9c909c98308b54fc0000b227d130e8319233975
Peter Tribble <peter.tribble@gmail.com>
2023-10-27 12:09:11 +0100

    Update ocaml 4.14.1

--- TRIBocaml/build.sh
+++ TRIBocaml/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack ocaml-4.03.0
-cd ocaml-4.03.0
-env PATH=/usr/gnu/bin:$PATH ./configure -cc "gcc -fPIC" -aspp "gcc -c -fPIC" -prefix /usr -mandir /usr/share/man/man1
-env PATH=/usr/gnu/bin:$PATH gmake -j 8 world.opt >& log.world
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/genpkg TRIBocaml
+env CC="gcc -m64" $THOME/build/dobuild -64only ocaml-4.14.1
+${THOME}/build/genpkg TRIBocaml ocaml-4.14.1
