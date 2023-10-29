a35d0a4a12fa2bac4f589da5e28e563a76d2a563
Peter Tribble <peter.tribble@gmail.com>
2023-10-29 10:50:55 +0000

    Update gnuplot 5.4.10

--- TRIBgnuplot/build.sh
+++ TRIBgnuplot/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only gnuplot-5.4.4 -C "--with-qt=no --disable-wxwidgets"
-${THOME}/build/genpkg TRIBgnuplot gnuplot-5.4.4
+${THOME}/build/dobuild -64only gnuplot-5.4.10 -C "--with-qt=no --disable-wxwidgets --with-readline=builtin --with-x"
+${THOME}/build/genpkg TRIBgnuplot gnuplot-5.4.10
