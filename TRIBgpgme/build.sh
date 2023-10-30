db5bbdfbcef46d19d5b522c92f02451d53af80ba
Peter Tribble <peter.tribble@gmail.com>
2023-10-30 09:14:44 +0000

    Update gpgme 1.23.1

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only gpgme-1.22.0 -C --disable-gpg-test
-${THOME}/build/genpkg TRIBgpgme gpgme-1.22.0
+${THOME}/build/dobuild -64only gpgme-1.23.1 -C --disable-gpg-test
+${THOME}/build/genpkg TRIBgpgme gpgme-1.23.1
