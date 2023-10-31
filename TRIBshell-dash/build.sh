b8c89f5aa4d6e15f83196786d697da1759079962
Peter Tribble <peter.tribble@gmail.com>
2023-10-31 18:23:35 +0000

    Update dash 0.5.12

--- TRIBshell-dash/build.sh
+++ TRIBshell-dash/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only dash-0.5.11.5 -C --disable-dependency-tracking
-${THOME}/build/genpkg TRIBshell-dash dash-0.5.11.5
+${THOME}/build/dobuild -64only dash-0.5.12
+${THOME}/build/genpkg TRIBshell-dash dash-0.5.12
