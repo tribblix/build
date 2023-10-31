c6c739b84227b70804f20d3cd19235277cf56c43
Peter Tribble <peter.tribble@gmail.com>
2023-10-31 18:01:30 +0000

    Update vim 9.0.2081

--- TRIBeditor-vim/build.sh
+++ TRIBeditor-vim/build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #
-# rename the download
-# mv vim-9.0.tar.bz2 vim90.tar.bz2
-#
 # we ship gvim and the desktop integration pieces in the
 # TRIBeditor-vim-gtk2 package
 #
 # Now most of the space is actually the shared stuff
 # Wrapper? [exec -a $0 /usr/bin/vim.variant "$@"]
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vim90 -C "--with-compiledby=ptribble@tribblix --enable-gui=no --without-x --disable-canberra" -M -k
-${THOME}/build/genpkg TRIBeditor-vim vim90
+# need to make with -k because msgfmt can't cope with one or more files
+#
+env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vim-9.0.2081 -C "--with-compiledby=ptribble@tribblix --enable-gui=no --without-x --disable-canberra --disable-libsodium" -M -k
+${THOME}/build/genpkg TRIBeditor-vim vim-9.0.2081
