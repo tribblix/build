72904745a66cad7c6f86b3bbe3ddd336534c2cd0
Peter Tribble <peter.tribble@gmail.com>
2023-11-01 16:22:06 +0000

    Rebuild json-c for la rework, and 64-bit only

--- TRIBjson-c/build.sh
+++ TRIBjson-c/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-#  needs autoconf
-#
 # the github downloads have obscure names
 #
-${THOME}/build/dobuild -64 json-c-json-c-0.12.1-20160607 -J 1
+# updates bump the SONAME, forcing a rebuild of libmypaint and repmgr
+#
+${THOME}/build/dobuild +64 json-c-json-c-0.12.1-20160607 -J 1
 ${THOME}/build/genpkg TRIBjson-c json-c-json-c-0.12.1-20160607
