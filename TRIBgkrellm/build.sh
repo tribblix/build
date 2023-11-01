1f9b0bcf7e756aa4d03c25421606bfef062dfec4
Peter Tribble <peter.tribble@gmail.com>
2023-11-01 22:14:54 +0000

    Rebuild gkrellm 64-bit

--- TRIBgkrellm/build.sh
+++ TRIBgkrellm/build.sh
@@ -4,11 +4,9 @@
 #
 ${THOME}/build/unpack gkrellm-2.3.11
 cd gkrellm-2.3.11
-sed -i s:/usr/ucb/install:/usr/bin/ginstall: */Makefile
-sed -i s:chgrp:true: */Makefile
-sed -i 's:lsocket -lX11:lsocket -lnsl -lX11:' Makefile
-gmake solaris LOCALEDIR=/usr/share/locale
+# CPPFLAGS, as CFLAGS is overridden by the Makefiles
+env PATH=/usr/bin/`$THOME/build/getarch`:$PATH PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig gmake -j 4 solaris LOCALEDIR=/usr/share/locale CPPFLAGS=-m64 LDFLAGS=-m64
 rm -fr /tmp/gk
-gmake install_solaris INSTALL=ginstall DESTDIR=/tmp/gk PREFIX=/usr
+gmake install INSTALL=ginstall DESTDIR=/tmp/gk PREFIX=/usr PKGCONFIGDIR=/tmp/gk/usr/lib/`$THOME/build/getarch`/pkgconfig
 ${THOME}/build/create_pkg TRIBgkrellm /tmp/gk
 rm -fr /tmp/gk
