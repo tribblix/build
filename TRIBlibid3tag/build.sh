bea8a145953ccd8bf160af3ffc98d9c7d19e8000
Peter Tribble <peter.tribble@gmail.com>
2023-11-01 20:45:13 +0000

    Update libid3tag 0.16.3

--- TRIBlibid3tag/build.sh
+++ TRIBlibid3tag/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libid3tag-0.15.1b
-${THOME}/build/genpkg TRIBlibid3tag libid3tag-0.15.1b
+# you may need to repack the tarball as it's shipped without the version
+#
+${THOME}/build/cmbuild -64 libid3tag-0.16.3
+${THOME}/build/genpkg TRIBlibid3tag libid3tag-0.16.3
