475e0d32f39c16ae36a16e1f9825bf0fe59d29e6
Peter Tribble <peter.tribble@gmail.com>
2023-11-03 15:20:21 +0000

    Update libidn2 2.3.4

--- TRIBlibidn2/build.sh
+++ TRIBlibidn2/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libidn2-2.3.0
-${THOME}/build/genpkg TRIBlibidn2 libidn2-2.3.0
+# note that all consumers of libidn use v1, not this libidn2
+#
+${THOME}/build/dobuild -64 libidn2-2.3.4 -C --with-included-libunistring
+${THOME}/build/genpkg TRIBlibidn2 libidn2-2.3.4
