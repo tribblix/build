7f09342502997086e10ace6eb4b99e0664605446
Peter Tribble <peter.tribble@gmail.com>
2023-11-05 14:36:59 +0000

    Update gawk 5.3.0

--- TRIBtext-gawk/build.sh
+++ TRIBtext-gawk/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only -gnu gawk-5.2.2 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gawk gawk-5.2.2
+${THOME}/build/dobuild +64only -gnu gawk-5.3.0 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gawk gawk-5.3.0
