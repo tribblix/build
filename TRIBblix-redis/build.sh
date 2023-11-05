a6835a5d40482de0a50785970d7b274460b6e5aa
Peter Tribble <peter.tribble@gmail.com>
2023-11-05 14:24:57 +0000

    Update redis 6.2.14

--- TRIBblix-redis/build.sh
+++ TRIBblix-redis/build.sh
@@ -5,8 +5,8 @@
 #
 # the redis build is implicitly 64-bit already
 #
-env CC=gcc BUILD_TLS=yes ${THOME}/build/dobuild redis-6.2.13 -P /opt/tribblix/redis
-cd redis-6.2.13
+env CC=gcc BUILD_TLS=yes ${THOME}/build/dobuild redis-6.2.14 -P /opt/tribblix/redis
+cd redis-6.2.14
 # some of the tests might fail in tcl
 gmake test CC=gcc BUILD_TLS=yes
 rm -fr /tmp/rr
