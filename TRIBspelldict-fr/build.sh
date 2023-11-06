0fc672323ca93987e031fa3b036712506d8bdfd7
Peter Tribble <peter.tribble@gmail.com>
2023-11-06 13:53:34 +0000

    Add french hunspell dictionary

---
+++ TRIBspelldict-fr/build.sh
@@ -0,0 +1,8 @@
+#!/bin/tcsh
+#
+# https://www.dicollecte.org/download/fr/hunspell-french-dictionaries-v7.0.zip
+# but that site appears to have gone AWOL
+#
+# use the libreoffice dictionaries instead, see the fixit scrip
+#
+${THOME}/build/genpkg TRIBspelldict-fr /tmp/sp-fr
