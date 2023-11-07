510acb75d526bd2a04f59180ee92a3957cbac97f
Peter Tribble <peter.tribble@gmail.com>
2023-11-07 15:10:23 +0000

    Rebuild qiv 64-bit

--- TRIBqiv/build.sh
+++ TRIBqiv/build.sh
@@ -2,8 +2,8 @@
 #
 # qiv (needs imlib2)
 #
-${THOME}/build/dobuild qiv-2.3.3
-cd qiv-2.3.3
+env CC="gcc -m64" ${THOME}/build/dobuild -64only qiv-2.3.3
+cd qiv-2.3.3-64bit
 rm -fr /tmp/iq
 gmake install PREFIX=/tmp/iq/usr INSTALL=ginstall
 ${THOME}/build/create_pkg TRIBqiv /tmp/iq
