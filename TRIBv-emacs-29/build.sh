e162ca6697d04368e1ca894698cb5f2f6e7b87f6
Peter Tribble <peter.tribble@gmail.com>
2023-11-07 09:07:28 +0000

    Add emacs 29.1

---
+++ TRIBv-emacs-29/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-29.1 -P /usr/versions/emacs-29 -C "--with-xft --with-xpm --without-sqlite3"
+${THOME}/build/genpkg TRIBv-emacs-29 emacs-29.1
