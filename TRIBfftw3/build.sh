cb86490bedaa18e84dc752f9f82b64c90f98f3ec
Peter Tribble <peter.tribble@gmail.com>
2023-11-07 14:40:53 +0000

    Update fftw 3.3.10, and remove 32-bit

--- TRIBfftw3/build.sh
+++ TRIBfftw3/build.sh
@@ -3,5 +3,5 @@
 # use the same flags to configure as OI
 # --enable-float for fftw3f
 #
-${THOME}/build/dobuild -64 fftw-3.3.9 -C "--enable-float --enable-threads --enable-shared --disable-static --with-combined-threads"
-${THOME}/build/genpkg TRIBfftw3 fftw-3.3.9
+${THOME}/build/dobuild -64only fftw-3.3.10 -C "--enable-float --enable-threads --enable-shared --disable-static --with-combined-threads"
+${THOME}/build/genpkg TRIBfftw3 fftw-3.3.10
