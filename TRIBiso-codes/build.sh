bca1334630f7b4062d7fdce62f87a34c416f3696
Peter Tribble <peter.tribble@gmail.com>
2023-11-08 20:42:12 +0000

    Update iso-codes 4.15.0

--- TRIBiso-codes/build.sh
+++ TRIBiso-codes/build.sh
@@ -3,4 +3,5 @@
 # easier to pull the tarball from the omnios mirror
 # https://mirrors.omnios.org/iso-codes/
 #
-${THOME}/build/newbuild.sh both TRIBiso-codes
+${THOME}/build/dobuild iso-codes-v4.15.0
+${THOME}/build/genpkg TRIBiso-codes iso-codes-v4.15.0
