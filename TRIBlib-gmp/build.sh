e40432578ef2fc6564aee9f89e42b0c7905b3013
Peter Tribble <peter.tribble@gmail.com>
2023-11-08 20:06:42 +0000

    Update gmp 6.3.0

--- TRIBlib-gmp/build.sh
+++ TRIBlib-gmp/build.sh
@@ -2,10 +2,7 @@
 #
 # Fix gmp for 64-bit includes
 #
-${THOME}/build/dobuild gmp-6.1.2 -C "--enable-cxx ABI=32 --disable-static"
-mv gmp-6.1.2 gmp-6.1.2-32bit
-${THOME}/build/dobuild +64 gmp-6.1.2 -C "--enable-cxx ABI=64 --disable-static"
-mv gmp-6.1.2-32bit gmp-6.1.2
-cd gmp-6.1.2 ; gmake check ; cd ..
-cd gmp-6.1.2-64bit ; gmake check ; cd ..
-${THOME}/build/genpkg TRIBlib-gmp gmp-6.1.2
+${THOME}/build/dobuild -64 gmp-6.3.0 -C "--enable-cxx ABI=32 --disable-static" +C "--enable-cxx ABI=64 --disable-static"
+# cd gmp-6.3.0 ; gmake check ; cd ..
+# cd gmp-6.3.0-64bit ; gmake check ; cd ..
+${THOME}/build/genpkg TRIBlib-gmp gmp-6.3.0
