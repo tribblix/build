4c9555dd41d97a78afd439128d3ac0aa5c36de17
Peter Tribble <peter.tribble@gmail.com>
2023-11-09 15:07:34 +0000

    Update postgres 13.13 15.5 16.1

--- TRIBblix-postgres16/build.sh
+++ TRIBblix-postgres16/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 16
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres16 postgresql-16.0 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres16 postgresql-16.0
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres16 postgresql-16.1 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres16 postgresql-16.1
