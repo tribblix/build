3a383d96389620d5c9c1ad3830995ce753e46347
Peter Tribble <peter.tribble@gmail.com>
2023-11-09 20:12:15 +0000

    Update git 2.42.1

--- TRIBdev-versioning-git/build.sh
+++ TRIBdev-versioning-git/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/newbuild.sh both TRIBdev-versioning-git
+${THOME}/build/dobuild -64only git-2.42.1 -C --with-perl=/usr/perl5/5.34/bin/perl
+${THOME}/build/genpkg TRIBdev-versioning-git git-2.42.1
