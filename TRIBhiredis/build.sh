ff0b5318b5b07b59af3a0dba029260eb4e10c7fb
Peter Tribble <peter.tribble@gmail.com>
2023-11-10 15:37:58 +0000

    Update hiredis 1.2.0, 64-bit only, enable ssl, tidy up

--- TRIBhiredis/build.sh
+++ TRIBhiredis/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64 hiredis-1.1.0
-${THOME}/build/genpkg TRIBhiredis hiredis-1.1.0
+${THOME}/build/cmbuild -64only hiredis-1.2.0 -C "-DENABLE_SSL=ON"
+${THOME}/build/genpkg TRIBhiredis hiredis-1.2.0
