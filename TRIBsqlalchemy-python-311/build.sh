9ed22c2bbdae6a309a0813a4b55e42b2551c0e21
Peter Tribble <peter.tribble@gmail.com>
2023-11-10 12:10:39 +0000

    Add SQLAlchemy 2.0.23

---
+++ TRIBsqlalchemy-python-311/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# it does depend on greenlet, although pip doesn't pick that up as
+# there's an architecture switch that doesn't work the way they
+# expect on solaris
+#
+${THOME}/build/pkg_pep518 TRIBsqlalchemy-python-311 SQLAlchemy-2.0.23
