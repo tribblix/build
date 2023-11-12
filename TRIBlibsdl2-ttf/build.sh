77a537e588712735b83a8160ce010a79abbad37e
Peter Tribble <peter.tribble@gmail.com>
2023-11-12 10:46:34 +0000

    Update libsdl-ttf 2.0.18

--- TRIBlibsdl2-ttf/build.sh
+++ TRIBlibsdl2-ttf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 SDL2_ttf-2.0.14
-${THOME}/build/genpkg TRIBlibsdl2-ttf SDL2_ttf-2.0.14
+${THOME}/build/dobuild -64 SDL2_ttf-2.0.18 -C "--enable-freetype-builtin=no --enable-harfbuzz=yes --enable-harfbuzz-builtin=no"
+${THOME}/build/genpkg TRIBlibsdl2-ttf SDL2_ttf-2.0.18
