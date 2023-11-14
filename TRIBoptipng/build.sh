f460a88d0d54fe8d837d18a12918456156fa3afb
Peter Tribble <peter.tribble@gmail.com>
2023-11-14 11:35:08 +0000

    Update optipng 0.7.8

--- TRIBoptipng/build.sh
+++ TRIBoptipng/build.sh
@@ -2,5 +2,5 @@
 #
 # it's not an autoconf configure, so needs a little chicanery
 #
-env TRIBBLIX_CFLAGS="-m64" TRIBBLIX_LDFLAGS="-m64" $THOME/build/dobuild optipng-0.7.7 -C "--prefix=/usr --with-system-libs"
-${THOME}/build/genpkg TRIBoptipng optipng-0.7.7
+env TRIBBLIX_CFLAGS="-m64" TRIBBLIX_LDFLAGS="-m64" $THOME/build/dobuild optipng-0.7.8 -C "--prefix=/usr --with-system-libs"
+${THOME}/build/genpkg TRIBoptipng optipng-0.7.8
