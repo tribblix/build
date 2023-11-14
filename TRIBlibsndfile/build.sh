f1796a3b1e0716887c8b6b9e83f5da6ca0a00ebe
Peter Tribble <peter.tribble@gmail.com>
2023-11-14 11:27:23 +0000

    Update libsndfile

--- TRIBlibsndfile/build.sh
+++ TRIBlibsndfile/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/gitarchive https://github.com/erikd/libsndfile
+${THOME}/build/gitarchive https://github.com/libsndfile/libsndfile
 ${THOME}/build/cmbuild -64 libsndfile -C "-DBUILD_SHARED_LIBS=on -DBUILD_STATIC_LIBS=no"
 ${THOME}/build/genpkg TRIBlibsndfile libsndfile
