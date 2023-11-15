cdd0dffc9f396220edd434b2bd0e9b422acccb48
Peter Tribble <peter.tribble@gmail.com>
2023-11-15 16:06:56 +0000

    Update axpbox

--- TRIBaxpbox/build.sh
+++ TRIBaxpbox/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/cmbuild https://github.com/lenticularis39/axpbox
+env TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/cmbuild -64only https://github.com/lenticularis39/axpbox
 ${THOME}/build/genpkg TRIBaxpbox axpbox
