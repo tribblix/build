923474e933f5925ae946a02bba3bf29d141a1d1a
Peter Tribble <peter.tribble@gmail.com>
2023-11-17 12:58:05 +0000

    Update jsonschema-specifications 2023.11.1

--- TRIBjsonschema-spec-python-311/build.sh
+++ TRIBjsonschema-spec-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBjsonschema-spec-python-311 jsonschema_specifications-2023.7.1
+${THOME}/build/pkg_pep518 TRIBjsonschema-spec-python-311 jsonschema_specifications-2023.11.1
