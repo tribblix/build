b3036a6d2fc7481341d876750fa71537a7a349db
Peter Tribble <peter.tribble@gmail.com>
2023-11-18 12:07:21 +0000

    Fix gmp build on sparc

--- TRIBlib-gmp/build.sh
+++ TRIBlib-gmp/build.sh
@@ -3,6 +3,13 @@
 # Fix gmp for 64-bit includes
 #
 ${THOME}/build/dobuild -64 gmp-6.3.0 -C "--enable-cxx ABI=32 --disable-static" +C "--enable-cxx ABI=64 --disable-static"
+#
+# for sparc it tries to optimize for the current build host, which is
+# absolutely not what you want, especially as the T4 pulls in VIS
+# unconditionally, so force the build target to be a generic ultrasparc
+#
+# ${THOME}/build/dobuild -64 gmp-6.3.0 -C "--build=ultrasparc-sun-solaris2.11 --enable-cxx ABI=32 --disable-static" +C "--build=ultrasparc-sun-solaris2.11 --enable-cxx ABI=64 --disable-static"
+#
 # cd gmp-6.3.0 ; gmake check ; cd ..
 # cd gmp-6.3.0-64bit ; gmake check ; cd ..
 ${THOME}/build/genpkg TRIBlib-gmp gmp-6.3.0
