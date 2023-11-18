4a83057c1bc04b8b597c1541038476585e845fa8
Peter Tribble <peter.tribble@gmail.com>
2023-11-18 18:32:21 +0000

    Update dehydrated 0.7.1

--- TRIBdehydrated/build.sh
+++ TRIBdehydrated/build.sh
@@ -2,18 +2,18 @@
 #
 # pull source, copy out the files we need
 #
-git clone https://github.com/lukas2511/dehydrated.git
+git clone https://github.com/dehydrated-io/dehydrated.git
 #
 # get the matching commit
 #
 cd dehydrated
-git checkout v0.6.5
+git checkout v0.7.1
 rm -fr /tmp/dhd
 mkdir -p /tmp/dhd/usr/bin
 mkdir -p /tmp/dhd/usr/share/man/man1
 mkdir -p /tmp/dhd/usr/share/dehydrated/docs
 cp dehydrated /tmp/dhd/usr/bin
-cp dehydrated.1 /tmp/dhd/usr/share/man/man1
+cp docs/man/dehydrated.1 /tmp/dhd/usr/share/man/man1
 cp LICENSE README.md /tmp/dhd/usr/share/dehydrated
 cp -r docs/examples /tmp/dhd/usr/share/dehydrated
 cp docs/*.* /tmp/dhd/usr/share/dehydrated/docs
