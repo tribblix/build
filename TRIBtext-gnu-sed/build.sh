d6b80c88929bdb574b2f4ff93531c9c902e4ffbc
Peter Tribble <peter.tribble@gmail.com>
2023-11-18 18:44:08 +0000

    Update gnu sed 4.9

--- TRIBtext-gnu-sed/build.sh
+++ TRIBtext-gnu-sed/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only -gnu sed-4.8 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-sed sed-4.8
+${THOME}/build/dobuild -64only -gnu sed-4.9 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-sed sed-4.9
