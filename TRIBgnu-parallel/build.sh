9eb6b28ebcdf350680dcbf8a78d10404bb4d0788
Peter Tribble <peter.tribble@gmail.com>
2023-11-18 18:56:17 +0000

    Update gnu parallel 20231022

--- TRIBgnu-parallel/build.sh
+++ TRIBgnu-parallel/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild parallel-20230422 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-parallel parallel-20230422
+${THOME}/build/dobuild parallel-20231022 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-parallel parallel-20231022
