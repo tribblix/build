756c89be47fed8d05b9462f4b1d92fc8843ce5c6
Peter Tribble <peter.tribble@gmail.com>
2023-11-19 22:02:04 +0000

    Update LibRaw 0.21.1

--- TRIBlibraw/build.sh
+++ TRIBlibraw/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 LibRaw-0.19.0
-${THOME}/build/genpkg TRIBlibraw LibRaw-0.19.0
+${THOME}/build/dobuild -64 LibRaw-0.21.1 -L -C "--enable-jasper --disable-openmp --enable-shared --disable-static"
+${THOME}/build/genpkg TRIBlibraw LibRaw-0.21.1
