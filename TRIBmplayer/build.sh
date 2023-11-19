a1600340daa6a20280815561a03f6606ea2e996a
Peter Tribble <peter.tribble@gmail.com>
2023-11-19 16:19:29 +0000

    Rebuild mplayer and boca for libcdio bump

--- TRIBmplayer/build.sh
+++ TRIBmplayer/build.sh
@@ -7,5 +7,5 @@
 #
 # Disable SDL as it's SDL1
 #
-env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_CXXFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 $THOME/build/dobuild -64only MPlayer-1.5 -C "--enable-gui --disable-sdl"
+env TRIBBLIX_CFLAGS=-m64 TRIBBLIX_CXXFLAGS=-m64 TRIBBLIX_LDFLAGS=-m64 $THOME/build/dobuild -64only MPlayer-1.5 -C "--enable-gui --disable-sdl --disable-smb"
 ${THOME}/build/genpkg TRIBmplayer MPlayer-1.5
