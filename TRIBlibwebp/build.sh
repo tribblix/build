4d58120549d19cb78150b47b5a41630765bfc1a2
Peter Tribble <peter.tribble@gmail.com>
2023-11-19 20:48:15 +0000

    Drop old libwebp compat

--- TRIBlibwebp/build.sh
+++ TRIBlibwebp/build.sh
@@ -5,6 +5,5 @@
 # we build and ship the old version for compatibility with applications
 # built against it; once they have all been rebuild it can be removed
 #
-${THOME}/build/dobuild -64 libwebp-0.5.1
 ${THOME}/build/dobuild -64 libwebp-1.3.2 -C "--disable-gl"
-${THOME}/build/genpkg TRIBlibwebp libwebp-0.5.1 libwebp-1.3.2
+${THOME}/build/genpkg TRIBlibwebp libwebp-1.3.2
