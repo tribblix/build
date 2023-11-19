b768b5534b38e7812049bd149fbc1f8393eb7c96
Peter Tribble <peter.tribble@gmail.com>
2023-11-19 14:41:53 +0000

    Update geany 2.0

--- TRIBgeany/build.sh
+++ TRIBgeany/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only -gnu geany-1.38 -C --enable-gtk3 -M 'geany_LDFLAGS='
-${THOME}/build/genpkg TRIBgeany geany-1.38
+${THOME}/build/dobuild -64only -gnu geany-2.0 -C --enable-gtk3 -M 'geany_LDFLAGS='
+${THOME}/build/genpkg TRIBgeany geany-2.0
