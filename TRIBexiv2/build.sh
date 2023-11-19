04a63455931515fb0f86066ca076886d76b8d28f
Peter Tribble <peter.tribble@gmail.com>
2023-11-19 17:42:59 +0000

    Update exiv2 0.28.1

--- TRIBexiv2/build.sh
+++ TRIBexiv2/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 
-env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64 exiv2-0.27.3-Source
-${THOME}/build/genpkg TRIBexiv2 exiv2-0.27.3-Source
+env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only exiv2-0.28.1 -C "-DEXIV2_ENABLE_INIH=OFF"
+${THOME}/build/genpkg TRIBexiv2 exiv2-0.28.1
---
+++ patches/exiv2-0.28.1.pls
@@ -0,0 +1,1 @@
+-p1 exiv2-futils.patch
