eb8504143f199d7cd9a1443d81d2e6db3dee0f38
Peter Tribble <peter.tribble@gmail.com>
2023-11-19 13:54:12 +0000

    Drop postgres 13

--- TRIBblix-citus13/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# this is a postgres extension, so it's specific to the postgres version
-#
-# there is a citus-stat patch
-#
-${THOME}/build/dobuild +64only citus-11.3.0 -C PG_CONFIG=/opt/tribblix/postgres13/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus13 citus-11.3.0
