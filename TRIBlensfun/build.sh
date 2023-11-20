a6302c847c22805fbdb005bc50d1f16415899e00
Peter Tribble <peter.tribble@gmail.com>
2023-11-20 21:55:13 +0000

    Update lensfun 0.3.4, 64-bit only, and fix the python install properly

--- TRIBlensfun/build.sh
+++ TRIBlensfun/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# see fixit - there are python pieces, although we don't declare a dependency
+# there are python pieces, although we don't declare a dependency
+# these are installed manually - see fixit and fixinstall - as the
+# cmake build installs a deprecated egg and puts it in the wrong place
 #
-${THOME}/build/cmbuild -64 lensfun-0.3.3 -C -DBUILD_LENSTOOL=ON
-${THOME}/build/genpkg TRIBlensfun lensfun-0.3.3
+${THOME}/build/cmbuild -64only lensfun-0.3.4 -C -DBUILD_LENSTOOL=ON
+${THOME}/build/genpkg TRIBlensfun lensfun-0.3.4
