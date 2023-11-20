7d50a09d704a3ac7bad5f24bab04c1307eb6c674
Peter Tribble <peter.tribble@gmail.com>
2023-11-20 16:30:21 +0000

    Update libgee 0.20.6, and make 64-bit only

--- TRIBlibgee/build.sh
+++ TRIBlibgee/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libgee-0.20.5 -C --disable-dependency-tracking
-${THOME}/build/genpkg TRIBlibgee libgee-0.20.5
+${THOME}/build/dobuild -64only libgee-0.20.6 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBlibgee libgee-0.20.6
