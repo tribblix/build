4374b6a6f7aad0ca3cfe9bd52f5ffc501ccb299a
Peter Tribble <peter.tribble@gmail.com>
2023-11-20 18:50:09 +0000

    Rebuild babl 64-bit only

--- TRIBlib-babl/build.sh
+++ TRIBlib-babl/build.sh
@@ -2,5 +2,5 @@
 #
 # new meson build
 #
-$THOME/build/mesonbuild -64 babl-0.1.98 -C "-Denable-gir=false -Denable-mmx=false -Denable-sse=false"
+$THOME/build/mesonbuild -64only babl-0.1.98 -C "-Denable-gir=false -Denable-mmx=false -Denable-sse=false"
 ${THOME}/build/genpkg TRIBlib-babl babl-0.1.98
