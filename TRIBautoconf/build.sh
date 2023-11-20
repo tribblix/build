24155e5c63ddd990aa9398de1a68d5989f272af2
Peter Tribble <peter.tribble@gmail.com>
2023-11-20 19:39:34 +0000

    Update autoconf 2.71

--- TRIBautoconf/build.sh
+++ TRIBautoconf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu autoconf-2.69
-${THOME}/build/genpkg TRIBautoconf autoconf-2.69
+${THOME}/build/dobuild -gnu autoconf-2.71
+${THOME}/build/genpkg TRIBautoconf autoconf-2.71
