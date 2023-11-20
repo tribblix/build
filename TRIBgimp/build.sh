1cf643acffd1ffa0ad138759b48b1675e8dad951
Peter Tribble <peter.tribble@gmail.com>
2023-11-20 18:50:31 +0000

    Update gimp 2.10.36

--- TRIBgimp/build.sh
+++ TRIBgimp/build.sh
@@ -2,5 +2,5 @@
 #
 # specify the build target so it picks up the correct assembler
 #
-${THOME}/build/dobuild -gnu -64only gimp-2.10.34 -C "--disable-python --with-bug-report-url=http://www.tribblix.org/feedback.html --build=x86_64-pc-solaris2.11"
-${THOME}/build/genpkg TRIBgimp gimp-2.10.34
+${THOME}/build/dobuild -gnu -64only gimp-2.10.36 -C "--disable-python --with-bug-report-url=http://www.tribblix.org/feedback.html --build=x86_64-pc-solaris2.11"
+${THOME}/build/genpkg TRIBgimp gimp-2.10.36
