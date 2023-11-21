9ad117018485f76ab8f0a325a029bd9c3fc06dd5
Peter Tribble <peter.tribble@gmail.com>
2023-11-21 20:28:26 +0000

    No longer need to specify xpg4 for AfterStep

--- TRIBafterstep/build.sh
+++ TRIBafterstep/build.sh
@@ -5,4 +5,4 @@
 # there are a couple of patches via debian needed for current gcc
 #
 ${THOME}/build/dobuild -64only afterstep-devel-2.2.12 -C "--enable-staticlibs=no --enable-sharedlibs" -L
-env PATH=/usr/xpg4/bin:$PATH ${THOME}/build/genpkg TRIBafterstep afterstep-devel-2.2.12
+${THOME}/build/genpkg TRIBafterstep afterstep-devel-2.2.12
