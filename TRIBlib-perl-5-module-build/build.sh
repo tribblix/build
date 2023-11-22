fd610c94557cb278cc2e7769beff8877ce66535d
Peter Tribble <peter.tribble@gmail.com>
2023-11-22 14:32:50 +0000

    Add Perl Module::Build

---
+++ TRIBlib-perl-5-module-build/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+${THOME}/build/unpack Module-Build-0.4234
+cd Module-Build-0.4234
+perl Build.PL
+./Build
+./Build install --destdir /tmp/hoa
+${THOME}/build/create_pkg TRIBlib-perl-5-module-build /tmp/hoa
+cd ..
