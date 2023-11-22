2fa9ac029df8f1fdea49228fcd8c53fc5a514d40
Peter Tribble <peter.tribble@gmail.com>
2023-11-22 21:54:40 +0000

    Rebuild dcraw against newer jasper, and make 64-bit

--- TRIBdcraw/build.sh
+++ TRIBdcraw/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
+# a mirror of the source is at https://github.com/ncruces/dcraw
+# but what you want is the tarball from https://dechifro.org/dcraw/archive/
+# because that has the message files included
+#
 ${THOME}/build/unpack dcraw-9.28.0
 cd dcraw
 #
@@ -7,7 +11,7 @@ cd dcraw
 #
 gpatch -p1 < ${THOME}/build/patches/dcraw.patch
 #
-gcc -O4 -o dcraw dcraw.c -Wall -Wno-unused-result -Wno-array-bounds -Wno-maybe-uninitialized -Wno-unused-but-set-variable -lm -ljasper -ljpeg -llcms2 -DLOCALEDIR=\"/usr/share/locale\"
+gcc -O4 -m64 -o dcraw dcraw.c -Wall -Wno-unused-result -Wno-array-bounds -Wno-maybe-uninitialized -Wno-unused-but-set-variable -lm -ljasper -ljpeg -llcms2 -DLOCALEDIR=\"/usr/share/locale\"
 rm -fr /tmp/dr
 mkdir -p /tmp/dr/usr/bin /tmp/dr/usr/share/man/man1
 cp dcraw /tmp/dr/usr/bin
