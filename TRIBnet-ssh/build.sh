557038826f656104e1277181b42c55303e5c9123
Peter Tribble <peter.tribble@gmail.com>
2023-11-23 14:21:17 +0000

    Update OpenSSH 9.5p1

--- TRIBnet-ssh/build.sh
+++ TRIBnet-ssh/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only openssh-9.4p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
+${THOME}/build/dobuild -64only openssh-9.5p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
 #
 # Need to split to match the old layout
 # TRIBnet-ssh-ssh-key is now deprecated and forcibly removed in m31
 #
-${THOME}/build/genpkg TRIBnet-ssh openssh-9.4p1
-${THOME}/build/genpkg TRIBsvc-net-ssh openssh-9.4p1
+${THOME}/build/genpkg TRIBnet-ssh openssh-9.5p1
+${THOME}/build/genpkg TRIBsvc-net-ssh openssh-9.5p1
