ba650c4d167096fdbddf4605358392148ad9a31c
Peter Tribble <peter.tribble@gmail.com>
2023-11-23 17:54:41 +0000

    Add TripleA

---
+++ TRIBtriplea/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Run the installer. It places files into ~/TripleA
+# Then convert those files into a package
+#
+rm -fr /tmp/jj
+mkdir -p /tmp/jj/usr/bin /tmp/jj/usr/share
+cd
+tar cf - TripleA | (cd /tmp/jj/usr/share ; tar xf -)
+ln -s ../../usr/share/TripleA/TripleA /tmp/jj/usr/bin
+rm -f /tmp/jj/usr/share/TripleA/error.log
+rm -f /tmp/jj/usr/share/TripleA/uninstall
+${THOME}/build/create_pkg TRIBtriplea /tmp/jj
+rm -fr /tmp/jj
