429a7b383563f3e83282869808b8282c328b10ef
Peter Tribble <peter.tribble@gmail.com>
2023-11-25 13:14:45 +0000

    Add gumbo-parser for claws

---
+++ TRIBlibgumbo/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This is a fork of the archived and unmaintained original which was
+# at https://github.com/google/gumbo-parser
+#
+# note that claws requires 0.12+, so the original won't work
+#
+# note that the file path in the downloaded tarball don't include the
+# version number
+#
+${THOME}/build/dobuild -64only gumbo-parser
+${THOME}/build/genpkg TRIBlibgumbo gumbo-parser
