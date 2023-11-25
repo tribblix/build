16cad359bbf9e6e925fc4cc39a96e7e884d25d09
Peter Tribble <peter.tribble@gmail.com>
2023-11-25 13:17:51 +0000

    Update claws 4.2.0, with more plugins

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
+# needs a preconf patch otherwise it fails to find libetpan
 #
 zap uninstall TRIBlibcanberra
-${THOME}/build/dobuild -64only claws-mail-4.1.1 -C "--disable-ldap --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.1.1
+${THOME}/build/dobuild -64only claws-mail-4.2.0 -C "--disable-ldap --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.2.0
