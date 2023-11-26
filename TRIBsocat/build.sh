1b4bc37bd39b5225772bff36489b6248cafb82da
Peter Tribble <peter.tribble@gmail.com>
2023-11-26 17:20:39 +0000

    Update socat 1.8.0.0

--- TRIBsocat/build.sh
+++ TRIBsocat/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only socat-1.7.4.4
-${THOME}/build/genpkg TRIBsocat socat-1.7.4.4
+${THOME}/build/dobuild -64only socat-1.8.0.0
+${THOME}/build/genpkg TRIBsocat socat-1.8.0.0
