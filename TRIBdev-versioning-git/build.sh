2b7cc8ea230cd75c2a3d27d9a9de22bc600db5e4
Peter Tribble <peter.tribble@gmail.com>
2023-11-26 10:30:08 +0000

    Update git 2.43.0

--- TRIBdev-versioning-git/build.sh
+++ TRIBdev-versioning-git/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only git-2.42.1 -C --with-perl=/usr/perl5/5.34/bin/perl
-${THOME}/build/genpkg TRIBdev-versioning-git git-2.42.1
+# the manual pages are unpacked with the fixit script, so remember
+# to update the version there too
+#
+${THOME}/build/dobuild -64only git-2.43.0 -C --with-perl=/usr/perl5/5.34/bin/perl
+${THOME}/build/genpkg TRIBdev-versioning-git git-2.43.0
