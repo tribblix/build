6108ddb07d8d6b5977bff4bfc9b4aa7ca61fa1f7
Peter Tribble <peter.tribble@gmail.com>
2023-11-27 14:27:08 +0000

    Update Qt-6 6.2.6 (still only for testing)

--- TRIBqt6/build.sh
+++ TRIBqt6/build.sh
@@ -9,24 +9,29 @@
 #
 # if we get the 64-bit build fixed. update the fixit file as well
 #
-${THOME}/build/unpack qt-everywhere-src-6.2.4
+${THOME}/build/unpack qt-everywhere-src-6.2.6
 #
 # 32-bit build is nice and clean
 #
-cd qt-everywhere-src-6.2.4
-env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -no-sql-sqlite -no-pch -xcb -xcb-xlib -bundled-xcb-xinput -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11
+cd qt-everywhere-src-6.2.6
+env PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11
+mkdir qtbase/translations
+env PATH=/usr/gnu/bin:$PATH gmake -j 8
+cd ..
+
 #
 # 64-bit build doesn't work, I think something isn't passing the
 # 64-bit all the way through
 #
-#env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -no-sql-sqlite -no-pch -xcb -xcb-xlib -bundled-xcb-xinput -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11 -bindir /usr/versions/Qt-6/bin -libdir /usr/versions/Qt-6/lib/amd64 -plugindir /usr/versions/Qt-6/plugins/amd64 -qmldir /usr/versions/Qt-6/qml/amd64 -platform solaris-g++-64 CMAKE_PREFIX_PATH=`pwd`/qtbase/lib/amd64/cmake
-env PATH=/usr/gnu/bin:$PATH gmake -j 8
-cd ..
+#cd qt-everywhere-src-6.2.6-64bit
+#env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11 -bindir /usr/versions/Qt-6/bin -libdir /usr/versions/Qt-6/lib/amd64 -plugindir /usr/versions/Qt-6/plugins/amd64 -qmldir /usr/versions/Qt-6/qml/amd64 -platform solaris-g++-64 CMAKE_PREFIX_PATH=`pwd`/qtbase/lib/amd64/cmake
+#mkdir qtbase/translations
+#env PATH=/usr/gnu/bin:$PATH gmake -j 8
+#cd ..
 
 # gnu to get a working "install" binary
 rm -fr /tmp/qt6
-cd qt-everywhere-src-6.2.4
-#env PATH=/usr/gnu/bin:$PATH gmake -j 8 install INSTALL_ROOT=/tmp/qt6
+cd qt-everywhere-src-6.2.6
 env PATH=/usr/gnu/bin:$PATH gmake -j 8 install DESTDIR=/tmp/qt6
 cd ..
 ${THOME}/build/create_pkg TRIBqt6 /tmp/qt6
