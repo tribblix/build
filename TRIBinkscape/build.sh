5d05961c456b58a81b7c271fb11ad32105e2cbc5
Peter Tribble <peter.tribble@gmail.com>
2023-11-27 15:15:40 +0000

    Rebuild inkscape for ImageMagick bump, it uses GraphicsMagick, and make 64-bit

--- TRIBinkscape/build.sh
+++ TRIBinkscape/build.sh
@@ -2,5 +2,5 @@
 #
 # install the *mm packages, sigc++, TRIBboehm-gc TRIBlibgsl TRIBboost
 #
-$THOME/build/cmbuild -gnu inkscape-1.2.2_2022-12-01_b0a8486541 -C -DWITH_OPENMP=OFF
+$THOME/build/cmbuild -gnu -64only inkscape-1.2.2_2022-12-01_b0a8486541 -C "-DWITH_OPENMP=OFF -DWITH_INTERNAL_2GEOM=ON"
 ${THOME}/build/genpkg TRIBinkscape inkscape-1.2.2_2022-12-01_b0a8486541
