35fc1e54bdeb59831b518257f6f42055172ed43f
Peter Tribble <peter.tribble@gmail.com>
2023-11-27 08:21:56 +0000

    Rebuild vte 64-bit only

--- TRIBvte/build.sh
+++ TRIBvte/build.sh
@@ -3,5 +3,5 @@
 # this is a patched vte, that handles home/end/pgup/pgdown sanely
 # without requiring shift
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64 vte-0.52.4 -C "--disable-Bsymbolic --enable-introspection=no --enable-vala=no"
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vte-0.52.4 -C "--disable-Bsymbolic --enable-introspection=no --enable-vala=no"
 ${THOME}/build/genpkg TRIBvte vte-0.52.4
