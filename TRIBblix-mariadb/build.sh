c1974682dec757101200b350bb38d2cb3d2cd464
Peter Tribble <peter.tribble@gmail.com>
2023-11-30 19:02:48 +0000

    Update mariadb 10.11.6, and configure as a service

--- TRIBblix-mariadb/build.sh
+++ TRIBblix-mariadb/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/cmbuild -64only mariadb-10.11.5 -P /opt/tribblix/mariadb
-${THOME}/build/genpkg TRIBblix-mariadb mariadb-10.11.5
+${THOME}/build/cmbuild -64only mariadb-10.11.6 -P /opt/tribblix/mariadb
+${THOME}/build/genpkg TRIBblix-mariadb mariadb-10.11.6
