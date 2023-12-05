d9d6605964f602ac410d3a9c09f6ef6cce4bc1e0
Peter Tribble <peter.tribble@gmail.com>
2023-12-05 20:55:55 +0000

    Update go 1.21.5

--- TRIBv-go-121/build.sh
+++ TRIBv-go-121/build.sh
@@ -8,11 +8,10 @@
 # the tarball has some test files with UTF-8 filenames
 #
 # As of 1.20, this MUST be GOOS=illumos or the build fails
-# so you need an illumos bootstrap compiler
 #
 chmod -R u+w go
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.21.4.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.21.5.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.21 GOROOT_BOOTSTRAP=/usr/versions/go-1.20 ./all.bash
 cd ..
@@ -21,8 +20,6 @@ mkdir -p /tmp/gg/usr/versions/go-1.21
 tar cf - . | (cd /tmp/gg/usr/versions/go-1.21 ; tar xf -)
 # [need to package godoc and gox]
 # need to not be at a valid GOROOT
-# go get is no more
-# https://golang.org/doc/go-get-install-deprecation
 mkdir t
 cd t
 env GOROOT=/tmp/gg/usr/versions/go-1.21 GOPATH=`pwd` ../bin/go install golang.org/x/tools/cmd/godoc@latest
