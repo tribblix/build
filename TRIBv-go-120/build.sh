a8b4c726789b77336199db221e7c31e3beb128d2
Peter Tribble <peter.tribble@gmail.com>
2023-12-05 20:44:35 +0000

    Update go 1.20.12

--- TRIBv-go-120/build.sh
+++ TRIBv-go-120/build.sh
@@ -3,7 +3,7 @@
 # either uninstall gdb, or run make.bash rather than all.bash, as the gdb
 # test fails on Tribblix
 #
-# 1.20 release, now default
+# 1.20 release, no longer default
 #
 # the tarball has some test files with UTF-8 filenames
 #
@@ -12,7 +12,7 @@
 #
 chmod -R u+w go
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.20.11.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.20.12.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.20 GOROOT_BOOTSTRAP=/usr/versions/go-1.19 ./all.bash
 cd ..
