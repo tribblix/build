5c9b3091aac636aad376f0b4dee7e9ce6df9a534
Peter Tribble <peter.tribble@gmail.com>
2023-12-05 09:10:03 +0000

    Rebuild tmux against libevent 2.1.12

--- TRIBtmux/build.sh
+++ TRIBtmux/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #
-# tmux/libevent
-# built a la omnios
-# [no, I don't understand how --disable-static works...]
-${THOME}/build/dobuild +64only libevent-2.1.11-stable -C "--disable-static --disable-libevent-install"
-env CPPFLAGS="-I`pwd`/libevent-2.1.11-stable-64bit/include -I`pwd`/libevent-2.1.11-stable-64bit/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.1.11-stable-64bit/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild -64only tmux-3.3a
+# tmux needs a sacrificial libevent
+#
+${THOME}/build/dobuild +64only libevent-2.1.12-stable -C "--enable-static --disable-shared --disable-libevent-install"
+env CPPFLAGS="-I`pwd`/libevent-2.1.12-stable-64bit/include -I`pwd`/libevent-2.1.12-stable-64bit/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.1.12-stable-64bit/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild -64only tmux-3.3a
 ${THOME}/build/genpkg TRIBtmux tmux-3.3a
-rm -fr libevent-2.1.11-stable-64bit
+rm -fr libevent-2.1.12-stable-64bit
