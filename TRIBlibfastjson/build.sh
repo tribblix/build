e4495cd14fb71274201a44b0554a8489c46101d4
Peter Tribble <peter.tribble@gmail.com>
2023-12-07 14:35:46 +0000

    Update libfastjson 0.99.9.1, and make 64-bit only

--- TRIBlibfastjson/build.sh
+++ TRIBlibfastjson/build.sh
@@ -2,5 +2,5 @@
 #
 # need libtool installed for autogen.sh
 #
-${THOME}/build/dobuild -64 libfastjson-0.99.8
-${THOME}/build/genpkg TRIBlibfastjson libfastjson-0.99.8
+${THOME}/build/dobuild -64only libfastjson-0.99.9.1 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBlibfastjson libfastjson-0.99.9.1
