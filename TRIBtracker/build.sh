fe5e82ccaefb94d5d265b8c2401a22b4bbad0bad
Peter Tribble <peter.tribble@gmail.com>
2023-12-09 13:31:28 +0000

    Make tracker 64-bit only

--- TRIBtracker/build.sh
+++ TRIBtracker/build.sh
@@ -2,5 +2,5 @@
 #
 # must build without gtk3
 #
-env LIBS=-luuid ${THOME}/build/dobuild -gnu -64 tracker-1.13.0 -C "--disable-introspection --disable-hal"
+env LIBS=-luuid ${THOME}/build/dobuild -gnu -64only tracker-1.13.0 -C "--disable-introspection --disable-hal --disable-static"
 ${THOME}/build/genpkg TRIBtracker tracker-1.13.0
