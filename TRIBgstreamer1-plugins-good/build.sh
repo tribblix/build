a7d27f6f69cfe64b94e814f75f3bcb9a1e8ddcf6
Peter Tribble <peter.tribble@gmail.com>
2023-12-09 16:39:54 +0000

    Update GStreamer 1.20.7

--- TRIBgstreamer1-plugins-good/build.sh
+++ TRIBgstreamer1-plugins-good/build.sh
@@ -3,5 +3,5 @@
 # qt5 is an undeclared dependency as we don't really want to pull
 # all of that in
 #
-env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64 gst-plugins-good-1.20.3 -C "-Dexamples=disabled -Dtests=disabled"
-${THOME}/build/genpkg TRIBgstreamer1-plugins-good gst-plugins-good-1.20.3
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64 gst-plugins-good-1.20.7 -C "-Dexamples=disabled -Dtests=disabled"
+${THOME}/build/genpkg TRIBgstreamer1-plugins-good gst-plugins-good-1.20.7
--- TRIBgstreamer1/build.sh
+++ TRIBgstreamer1/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
+# note that we have to install both 32-bit and 64-bit files into /usr/libexec
+# as gst-plugin-scanner populates the registry and needs to match
 #
-#
-env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64 gstreamer-1.20.3 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled"
-${THOME}/build/genpkg TRIBgstreamer1 gstreamer-1.20.3
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64 gstreamer-1.20.7 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled" +C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled -Dlibexecdir=/usr/libexec/`$THOME/build/getarch`"
+${THOME}/build/genpkg TRIBgstreamer1 gstreamer-1.20.7
