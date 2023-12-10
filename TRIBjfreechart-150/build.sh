3629fdc7a5c9012fada0b8f798be752dc40f70f5
Peter Tribble <peter.tribble@gmail.com>
2023-12-10 10:27:40 +0000

    No longer need to ship 2 versions of jfreechart

--- TRIBjfreechart-150/build.sh
+++ TRIBjfreechart-150/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
+# the "150" in the name originally meant "version 1.5.0" but should now
+# be interpreted as "version 1.5.x"
+#
 # get the fixit script to do all the work
 #
 ${THOME}/build/genpkg TRIBjfreechart-150
