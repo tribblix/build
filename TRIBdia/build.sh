9874d22d1b87522476cef705e3b881db89058a89
Peter Tribble <peter.tribble@gmail.com>
2023-12-10 16:53:31 +0000

    Rebuild dia 64-bit

--- TRIBdia/build.sh
+++ TRIBdia/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu dia-0.97.3
+# latest versions are at https://download.gnome.org/sources/dia/
+#
+${THOME}/build/dobuild -gnu -64only dia-0.97.3
 ${THOME}/build/genpkg TRIBdia dia-0.97.3
