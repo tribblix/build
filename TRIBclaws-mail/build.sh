48ae2b41900d4bf15cf5cd0fb6b94f76a45c98bc
Peter Tribble <peter.tribble@gmail.com>
2023-12-11 14:21:19 +0000

    Teach claws where the certificate store is

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
 # needs a preconf patch otherwise it fails to find libetpan
+# the preconf patch also fixes up claws' incorrect notions of
+# where the certificate bundle happens to live
 #
 zap uninstall TRIBlibcanberra
 ${THOME}/build/dobuild -64only claws-mail-4.2.0 -C "--disable-ldap --disable-dependency-tracking"
