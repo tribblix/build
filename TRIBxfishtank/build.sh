8c69f7fd74ed48eda157d83a331324d215831ea8
Peter Tribble <peter.tribble@gmail.com>
2023-12-12 17:39:31 +0000

    Build xfishtank 64-bit

--- TRIBxfishtank/build.sh
+++ TRIBxfishtank/build.sh
@@ -2,6 +2,7 @@
 #
 ${THOME}/build/unpack xfishtank
 cd xfishtank
-xmkmf -DHasGcc3=YES
+xmkmf -DHasGcc3=YES -DAMD64Architecture=YES -DDefaultCCOptions="-m64"
+# xmkmf -DHasGcc3=YES -DSparc64Architecture=YES -DDefaultCCOptions="-m64"
 gmake
 ${THOME}/build/genpkg TRIBxfishtank
