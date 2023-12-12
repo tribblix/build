9998df813c164627790795bc537220d77d1e5a2c
Peter Tribble <peter.tribble@gmail.com>
2023-12-12 14:36:13 +0000

    Update qpdf 11.6.4

--- TRIBqpdf/build.sh
+++ TRIBqpdf/build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 #
-
-$THOME/build/cmbuild -64only qpdf-11.6.3
-${THOME}/build/genpkg TRIBqpdf qpdf-11.6.3
+$THOME/build/cmbuild -64only qpdf-11.6.4
+${THOME}/build/genpkg TRIBqpdf qpdf-11.6.4
