d03bf66acdecd36199fefe02d49bd82ddcef75ea
Peter Tribble <peter.tribble@gmail.com>
2023-12-12 21:13:08 +0000

    Update libspectre 0.2.12, and drop 32-bit

--- TRIBlibspectre/build.sh
+++ TRIBlibspectre/build.sh
@@ -2,5 +2,5 @@
 #
 # libspectre requires libgs provided by ghostscript
 #
-${THOME}/build/dobuild -64 libspectre-0.2.9
-${THOME}/build/genpkg TRIBlibspectre libspectre-0.2.9
+${THOME}/build/dobuild -64only libspectre-0.2.12
+${THOME}/build/genpkg TRIBlibspectre libspectre-0.2.12
