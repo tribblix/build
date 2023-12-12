dba8e8348152ee838ba2115c6765953422df7ab5
Peter Tribble <peter.tribble@gmail.com>
2023-12-12 20:30:20 +0000

    Drop 32-bit imlib2, no more consumers

--- TRIBimlib2/build.sh
+++ TRIBimlib2/build.sh
@@ -2,5 +2,5 @@
 #
 # it tries to build 32-bit assembler on 64-bit, so need to be specific
 #
-${THOME}/build/dobuild -64 imlib2-1.12.1 -C --enable-mmx +C --enable-amd64
+${THOME}/build/dobuild -64only imlib2-1.12.1 +C --enable-amd64
 ${THOME}/build/genpkg TRIBimlib2 imlib2-1.12.1
