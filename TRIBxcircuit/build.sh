afb93f053befffa279f050ca11197998992a19b9
Peter Tribble <peter.tribble@gmail.com>
2023-12-12 15:49:40 +0000

    Build xcircuit 64-bit

--- TRIBxcircuit/build.sh
+++ TRIBxcircuit/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xcircuit-3.10.30
+# a 64-bit build is slightly tricky as not everything picks up the
+# flags, not everything should be relocated, and there's a fixit script
+# to correct the remainder
+#
+env CC="gcc -m64" ${THOME}/build/dobuild +64only xcircuit-3.10.30
 ${THOME}/build/genpkg TRIBxcircuit xcircuit-3.10.30
