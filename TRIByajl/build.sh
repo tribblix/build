34c3486fb0f16c906d2d18a6cfd1f557f2a715d4
Peter Tribble <peter.tribble@gmail.com>
2023-12-14 08:53:50 +0000

    Drop 32-bit yajl

--- TRIByajl/build.sh
+++ TRIByajl/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-$THOME/build/cmbuild -64 yajl-2.1.0 +C "-D LIB_SUFFIX=/`$THOME/build/getarch`"
+$THOME/build/cmbuild -64only yajl-2.1.0 +C "-D LIB_SUFFIX=/`$THOME/build/getarch`"
 ${THOME}/build/genpkg TRIByajl yajl-2.1.0
