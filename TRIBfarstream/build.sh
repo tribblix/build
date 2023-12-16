60aecc9d1455455e7d1d78ea0a8c401517eaf020
Peter Tribble <peter.tribble@gmail.com>
2023-12-16 09:59:49 +0000

    Drop 32-bit farstream

--- TRIBfarstream/build.sh
+++ TRIBfarstream/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 farstream-0.2.9 -C "--disable-introspection --disable-dependency-tracking"
+${THOME}/build/dobuild -64only farstream-0.2.9 -C "--disable-introspection --disable-dependency-tracking"
 ${THOME}/build/genpkg TRIBfarstream farstream-0.2.9
