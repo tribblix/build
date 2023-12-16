404d16c387c75599346c5a07b6e60395bd6905bb
Peter Tribble <peter.tribble@gmail.com>
2023-12-16 09:54:04 +0000

    Update jansson 2.14, and drop 32-bit

--- TRIBjansson/build.sh
+++ TRIBjansson/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 jansson-2.11
-${THOME}/build/genpkg TRIBjansson jansson-2.11
+${THOME}/build/dobuild -64only jansson-2.14
+${THOME}/build/genpkg TRIBjansson jansson-2.14
