4fe1c96d30fee11646af3685431451d637c82a95
Peter Tribble <peter.tribble@gmail.com>
2023-12-16 16:42:55 +0000

    Drop 32-bit libunistring

--- TRIBlibunistring/build.sh
+++ TRIBlibunistring/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libunistring-1.0
+# 1.1 bumps the SONAME
+#
+${THOME}/build/dobuild -64only libunistring-1.0
 ${THOME}/build/genpkg TRIBlibunistring libunistring-1.0
