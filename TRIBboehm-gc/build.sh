c5af5b29e557ae0538c0a13368c6026b23991233
Peter Tribble <peter.tribble@gmail.com>
2023-12-17 13:49:51 +0000

    Update boehm-gc 7.2g, and drop 32-bit

--- TRIBboehm-gc/build.sh
+++ TRIBboehm-gc/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #
-# (nb symlink from the gc-7.2e tarball)
-# (pkg version is 7.2.5==7.2e)
+# some of the archives are doubly gzipped, so you need to gunzip them
+# and rename with the gz suffix
 #
-${THOME}/build/dobuild -64 gc-7.2
+# later versions have removed some symbols from the library
+# so I've been very conservative regarding updates
+#
+# and the 7.2x tarballs are all copies of gc-7.2, so need to
+# rm gc-7.2.tar.gz; ln -s gc-7.2g.tar.gz gc-7.2.tar.gz
+#
+env TRIBBLIX_CFLAGS=-fcommon ${THOME}/build/dobuild -64only gc-7.2
 ${THOME}/build/genpkg TRIBboehm-gc gc-7.2
