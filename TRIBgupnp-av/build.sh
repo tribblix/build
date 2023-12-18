919deb9252b12730641143a5f5ce2112c29853a8
Peter Tribble <peter.tribble@gmail.com>
2023-12-18 17:18:26 +0000

    Drop 32-bit gupnp-av and gupnp-dlna

--- TRIBgupnp-av/build.sh
+++ TRIBgupnp-av/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 gupnp-av-0.12.11 -C "--disable-introspection --disable-dependency-tracking"
+${THOME}/build/dobuild -64only gupnp-av-0.12.11 -C "--disable-introspection --disable-dependency-tracking"
 ${THOME}/build/genpkg TRIBgupnp-av gupnp-av-0.12.11
