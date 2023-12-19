feaa4b716d566f6a7f4aba54c5521a5901c108ad
Peter Tribble <peter.tribble@gmail.com>
2023-12-19 08:52:19 +0000

    Update libheif 1.17.5

--- TRIBlibheif/build.sh
+++ TRIBlibheif/build.sh
@@ -3,5 +3,5 @@
 # would like to add the OpenJPEG support too, but the openjpeg install
 # is broken and mixes up 32- and 64-bit objects
 #
-${THOME}/build/cmbuild -64 libheif-1.17.1 -C "-DWITH_JPEG_ENCODER=YES -DWITH_JPEG_DECODER=YES"
-${THOME}/build/genpkg TRIBlibheif libheif-1.17.1
+${THOME}/build/cmbuild -64 libheif-1.17.5 -C "-DWITH_JPEG_ENCODER=YES -DWITH_JPEG_DECODER=YES"
+${THOME}/build/genpkg TRIBlibheif libheif-1.17.5
