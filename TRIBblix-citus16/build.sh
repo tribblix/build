f2bfc0b6684e1517152dbeee728fc55d1d1880e8
Peter Tribble <peter.tribble@gmail.com>
2023-12-19 10:18:07 +0000

    Update citus 12.1.0, and add postgres16

---
+++ TRIBblix-citus16/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# this is a postgres extension, so it's specific to the postgres version
+#
+# there is a citus-stat patch
+#
+${THOME}/build/dobuild +64only citus-12.1.0 -C PG_CONFIG=/opt/tribblix/postgres16/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus16 citus-12.1.0
