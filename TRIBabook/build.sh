5877288d174c703216d7724a420cd8b58c35f613
Peter Tribble <peter.tribble@gmail.com>
2023-12-20 18:23:15 +0000

    Remove abook

--- TRIBabook/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# needs readline installed
-#
-env LD_RUN_PATH=/usr/gnu/lib ${THOME}/build/dobuild abook-0.5.6 -C "--with-readline=/usr/gnu"
-${THOME}/build/genpkg TRIBabook abook-0.5.6
