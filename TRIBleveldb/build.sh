e6be66671e3e01847f64dada6a037c1268f8744d
Peter Tribble <peter.tribble@gmail.com>
2023-12-20 18:42:41 +0000

    Update leveldb 1.23

--- TRIBleveldb/build.sh
+++ TRIBleveldb/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 #
-env CC=gcc LDFLAGS=-m64 CFLAGS=-m64 CXXFLAGS=-m64 ${THOME}/build/dobuild leveldb-1.19
-mv leveldb-1.19 leveldb-1.19-save
-env CC=gcc ${THOME}/build/dobuild leveldb-1.19
-mv leveldb-1.19-save leveldb-1.19-64bit
-# [have to manually implement make install]
-${THOME}/build/genpkg TRIBleveldb leveldb-1.19
+$THOME/build/cmbuild -64 leveldb-1.23 -C "-DLEVELDB_BUILD_BENCHMARKS=OFF -DLEVELDB_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF"
+${THOME}/build/genpkg TRIBleveldb leveldb-1.23
