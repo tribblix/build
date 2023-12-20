320bdf1a9ea611094f488089e9fb2ad8e3a531c9
Peter Tribble <peter.tribble@gmail.com>
2023-12-20 19:57:11 +0000

    Update lzo 2.10 lzop 1.04, and make 64-bit only

--- TRIBcompress-lzo/build.sh
+++ TRIBcompress-lzo/build.sh
@@ -2,5 +2,5 @@
 #
 # lzo library - also see lzop
 #
-${THOME}/build/dobuild -64 lzo-2.09 -C "--enable-shared --disable-static"
-${THOME}/build/genpkg TRIBcompress-lzo lzo-2.09
+${THOME}/build/dobuild -64only lzo-2.10 -C "--enable-shared --disable-static"
+${THOME}/build/genpkg TRIBcompress-lzo lzo-2.10
--- TRIBcompress-lzop/build.sh
+++ TRIBcompress-lzop/build.sh
@@ -2,5 +2,5 @@
 #
 # requires lzo
 #
-${THOME}/build/dobuild lzop-1.03
-${THOME}/build/genpkg TRIBcompress-lzop lzop-1.03
+${THOME}/build/dobuild -64only lzop-1.04
+${THOME}/build/genpkg TRIBcompress-lzop lzop-1.04
