dac2f6a490d1572ae0917778455ff5334f704c6f
Peter Tribble <peter.tribble@gmail.com>
2023-12-20 18:46:30 +0000

    Remove unused quilt

--- TRIBquilt/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-$THOME/build/dobuild -gnu quilt-0.67 -C "--without-date --with-grep=/usr/gnu/bin/grep --with-sed=/usr/gnu/bin/sed --without-getopt --with-diff=/usr/gnu/bin/diff"
-cd quilt-0.67
-rm -fr /tmp/qqt4
-gmake install BUILD_ROOT=/tmp/qqt4
-${THOME}/build/create_pkg TRIBquilt /tmp/qqt4
-rm -fr /tmp/qqt4
-cd ..
