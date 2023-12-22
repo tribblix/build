51a7a29a5e5cb3e6d9c4b2305a63dfc401e93494
Peter Tribble <peter.tribble@gmail.com>
2023-12-22 16:59:50 +0000

    Drop sniproxy, deprecated upstream

--- TRIBblix-sniproxy/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-$THOME/build/unpack sniproxy-0.6.1
-cd sniproxy-0.6.1
-./autogen.sh
-env LIBS="-lsocket -lnsl" CPPFLAGS="-D__EXTENSIONS__ -D_XPG4_2 -m64" LDFLAGS=-m64 ./configure --prefix=/opt/tribblix/sniproxy
-gmake -j 4
-${THOME}/build/genpkg TRIBblix-sniproxy
-cd ..
