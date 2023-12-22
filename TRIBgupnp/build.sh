dfcaf89e5d64ede015fa6b0bcef04b1758dbabf1
Peter Tribble <peter.tribble@gmail.com>
2023-12-22 19:29:49 +0000

    Drop 32-bit gupnp

--- TRIBgupnp/build.sh
+++ TRIBgupnp/build.sh
@@ -4,5 +4,5 @@
 #
 # stay downrev; 1.0.7 requires GLib2 2.66
 #
-${THOME}/build/dobuild -64 gupnp-1.0.6 -C "UUID_CFLAGS=-I/usr/include/uuid UUID_LIBS=-luuid --disable-introspection --disable-dependency-tracking"
+${THOME}/build/dobuild -64only gupnp-1.0.6 -C "UUID_CFLAGS=-I/usr/include/uuid UUID_LIBS=-luuid --disable-introspection --disable-dependency-tracking"
 ${THOME}/build/genpkg TRIBgupnp gupnp-1.0.6
