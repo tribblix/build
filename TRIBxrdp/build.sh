eb33949a9b10db94c8a368304e152c2786960f96
Peter Tribble <peter.tribble@gmail.com>
2023-12-22 16:20:34 +0000

    Update xrdp 0.9.23.1

--- TRIBxrdp/build.sh
+++ TRIBxrdp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS $THOME/build/dobuild +64only xrdp-0.9.23 -C LIBS=-lsocket
-${THOME}/build/genpkg  TRIBxrdp xrdp-0.9.23
+env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS $THOME/build/dobuild +64only xrdp-0.9.23.1 -C LIBS=-lsocket
+${THOME}/build/genpkg  TRIBxrdp xrdp-0.9.23.1
