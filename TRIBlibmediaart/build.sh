5ea056d8bc665f5d852e54d8397f1ccede306c6a
Peter Tribble <peter.tribble@gmail.com>
2023-12-22 10:23:31 +0000

    Drop 32-bit libmediaart

--- TRIBlibmediaart/build.sh
+++ TRIBlibmediaart/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/mesonbuild -64 libmediaart-1.9.6 -C -Dintrospection=false
+${THOME}/build/mesonbuild -64only libmediaart-1.9.6 -C -Dintrospection=false
 ${THOME}/build/genpkg TRIBlibmediaart libmediaart-1.9.6
