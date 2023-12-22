79862f2ff0ee2adc50de038912958567ce56a236
Peter Tribble <peter.tribble@gmail.com>
2023-12-22 10:45:44 +0000

    Drop 32-bit potrace

--- TRIBpotrace/build.sh
+++ TRIBpotrace/build.sh
@@ -3,5 +3,5 @@
 # A dependency for newer inkscape
 # Specify gcc, it will pick clang on its own
 #
-${THOME}/build/dobuild -64 potrace-1.16 -C "CC=gcc --with-libpotrace"
+${THOME}/build/dobuild -64only potrace-1.16 -C "CC=gcc --with-libpotrace"
 ${THOME}/build/genpkg TRIBpotrace potrace-1.16
