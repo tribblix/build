9381ef96a309d4c84704c9c6862fafa5aa843537
Peter Tribble <peter.tribble@gmail.com>
2023-12-22 20:57:57 +0000

    Update urllib3 1.26.18

--- TRIBurllib3-python-311/build.sh
+++ TRIBurllib3-python-311/build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 #
-# the dependencies around [secure] will need looking at
-# we still declare them, but urllib3 shouldn't use them
-# see https://github.com/urllib3/urllib3/issues/2680
-#
-${THOME}/build/pkg_setup_py TRIBurllib3-python-311 urllib3-1.26.15
+${THOME}/build/pkg_setup_py TRIBurllib3-python-311 urllib3-1.26.18
