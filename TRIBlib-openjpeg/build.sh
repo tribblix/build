7742c9b04d59fee9213618a6ae3de0b7f41e457b
Peter Tribble <peter.tribble@gmail.com>
2023-12-24 21:36:23 +0000

    Try and fix the openjpeg cmake install properly

--- TRIBlib-openjpeg/build.sh
+++ TRIBlib-openjpeg/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# specifying architecture directories for lib and bin doesn't work
-# so they're corrected by the fixinstall.64 script
+# the normal cmake bin and lib directories aren't honored, it uses its own
 #
 # need to not build static libs in the first place so the installed
 # cmake config is consistent
 #
-env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild -64 openjpeg-2.5.0 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF"
+env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +64 openjpeg-2.5.0 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF -DOPENJPEG_INSTALL_BIN_DIR=bin/`$THOME/build/getarch` -DOPENJPEG_INSTALL_LIB_DIR=lib/`$THOME/build/getarch`"
+env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +32 openjpeg-2.5.0 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF"
 ${THOME}/build/genpkg TRIBlib-openjpeg openjpeg-2.5.0
