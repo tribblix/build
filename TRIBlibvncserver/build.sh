2fc8d4c100626d42cd3cd0ba6ae4e1132ddc9ba9
Peter Tribble <peter.tribble@gmail.com>
2023-12-24 18:33:45 +0000

    Drop 32-bit libvncserver

--- TRIBlibvncserver/build.sh
+++ TRIBlibvncserver/build.sh
@@ -2,5 +2,5 @@
 #
 # libvncserver
 #
-env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/cmbuild -64 libvncserver-LibVNCServer-0.9.14
+env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/cmbuild -64only libvncserver-LibVNCServer-0.9.14
 ${THOME}/build/genpkg TRIBlibvncserver libvncserver-LibVNCServer-0.9.14
