205c260af5b41c296b59e21f712393d96db314f4
Peter Tribble <peter.tribble@gmail.com>
2023-12-25 17:10:38 +0000

    Update zint 2.13.0

--- TRIBzint/build.sh
+++ TRIBzint/build.sh
@@ -2,6 +2,6 @@
 #
 # build both the regular and QT packages together
 #
-env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only zint-2.12.0-src -C -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake
-${THOME}/build/genpkg TRIBzint zint-2.12.0-src
-${THOME}/build/genpkg TRIBzint-qt zint-2.12.0-src
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only zint-2.13.0-src -C -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake
+${THOME}/build/genpkg TRIBzint zint-2.13.0-src
+${THOME}/build/genpkg TRIBzint-qt zint-2.13.0-src
