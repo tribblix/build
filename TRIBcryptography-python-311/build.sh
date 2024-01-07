010b2da58ecdc01a9d58d3e5984c6ba7621eadcc
Peter Tribble <peter.tribble@gmail.com>
2024-01-07 13:11:18 +0000

    Update python cryptography 41.0.7

--- TRIBcryptography-python-311/build.sh
+++ TRIBcryptography-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # current versions require a rust toolchain to build
 #
-${THOME}/build/pkg_setup_py TRIBcryptography-python-311 cryptography-41.0.5
+${THOME}/build/pkg_setup_py TRIBcryptography-python-311 cryptography-41.0.7
