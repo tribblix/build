d93a9ef8ff3b103de5272fbcae2e2fe8a94c770b
Peter Tribble <peter.tribble@gmail.com>
2024-01-11 18:47:10 +0000

    Update mbuffer 20240107

--- TRIBmbuffer/build.sh
+++ TRIBmbuffer/build.sh
@@ -2,5 +2,5 @@
 #
 # depends on libgcrypt, but via dlopen rather than being linked in
 #
-${THOME}/build/dobuild -64only mbuffer-20220418 -C --sysconfdir=/etc
-${THOME}/build/genpkg TRIBmbuffer mbuffer-20220418
+${THOME}/build/dobuild -64only mbuffer-20240107 -C --sysconfdir=/etc
+${THOME}/build/genpkg TRIBmbuffer mbuffer-20240107
