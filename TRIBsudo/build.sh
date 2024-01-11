f1cc1eb90ff8edff78f7d513fbc7912babbbdc46
Peter Tribble <peter.tribble@gmail.com>
2024-01-11 17:42:01 +0000

    Update sudo 1.9.15p5

--- TRIBsudo/build.sh
+++ TRIBsudo/build.sh
@@ -4,8 +4,8 @@
 #
 # there's a custom config file installed by the fixit script
 #
-${THOME}/build/dobuild -64only sudo-1.9.15p4 -C "--disable-hardening --with-project --with-rundir=/var/run/sudo --with-pam --with-pam-login --disable-pam-session --with-tty-tickets --without-insults --without-lecture --with-ignore-dot --with-bsm-audit"
+${THOME}/build/dobuild -64only sudo-1.9.15p5 -C "--disable-hardening --with-project --with-rundir=/var/run/sudo --with-pam --with-pam-login --disable-pam-session --with-tty-tickets --without-insults --without-lecture --with-ignore-dot --with-bsm-audit"
 #
 # hack to avoid the install trying to chown the files
 #
-${THOME}/build/genpkg -I "install INSTALL_OWNER=" TRIBsudo sudo-1.9.15p4
+${THOME}/build/genpkg -I "install INSTALL_OWNER=" TRIBsudo sudo-1.9.15p5
