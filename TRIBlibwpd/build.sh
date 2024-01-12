8e862729b40adaea166fc17ef91da814efbda6d9
Peter Tribble <peter.tribble@gmail.com>
2024-01-12 13:55:49 +0000

    Drop 32-bit libwpd

--- TRIBlibwpd/build.sh
+++ TRIBlibwpd/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 #
-# need to build the 32-bit library as the old version with the old ABI
-# for binary compatibility
-#
-env TRIBBLIX_CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ${THOME}/build/dobuild libwpd-0.10.0
-${THOME}/build/dobuild +64 libwpd-0.10.3
-${THOME}/build/genpkg TRIBlibwpd libwpd-0.10.0 libwpd-0.10.3
+${THOME}/build/dobuild -64only libwpd-0.10.3
+${THOME}/build/genpkg TRIBlibwpd libwpd-0.10.3
