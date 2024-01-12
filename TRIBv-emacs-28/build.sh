c805d7a54e9e3f707fb08eadda573acaba307c5a
Peter Tribble <peter.tribble@gmail.com>
2024-01-12 15:35:41 +0000

    Tidy up emacs install

--- TRIBv-emacs-28/build.sh
+++ TRIBv-emacs-28/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-28.2 -P /usr/versions/emacs-28 -C "--with-xft --with-xpm"
+env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-28.2 -P /usr/versions/emacs-28 -C "--without-xft --with-xpm --with-cairo"
 ${THOME}/build/genpkg TRIBv-emacs-28 emacs-28.2
