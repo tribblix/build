7e0d1665d594e5578dbc4bb56c470b119809f527
Peter Tribble <peter.tribble@gmail.com>
2024-01-13 12:22:49 +0000

    Update jupyterlab_pygments 0.3.0

--- TRIBjupyter-pygments-python-311/build.sh
+++ TRIBjupyter-pygments-python-311/build.sh
@@ -3,4 +3,4 @@
 # a build from source is somewhat complex and fails on Tribblix,
 # so install directly from the trivial wheel instead
 #
-${THOME}/build/pkg_wheel TRIBjupyter-pygments-python-311 jupyterlab_pygments-0.2.2-py2.py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBjupyter-pygments-python-311 jupyterlab_pygments-0.3.0-py3-none-any.whl
