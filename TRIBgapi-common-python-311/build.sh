712e029dce6c53c06fec7109318c99836348ec69
Peter Tribble <peter.tribble@gmail.com>
2024-01-13 10:44:54 +0000

    Update gapi modules

--- TRIBgapi-common-python-311/build.sh
+++ TRIBgapi-common-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgapi-common-python-311 googleapis-common-protos-1.60.0
+${THOME}/build/pkg_setup_py TRIBgapi-common-python-311 googleapis-common-protos-1.62.0
--- TRIBgoogle-auth-python-311/build.sh
+++ TRIBgoogle-auth-python-311/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-311 google-auth-2.23.0
+# 2.23.0 needed urllib3 <2, 2.23.1 needed urllib3>=2
+# current have urllib3 as optional but support all versions
+#
+${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-311 google-auth-2.26.2
