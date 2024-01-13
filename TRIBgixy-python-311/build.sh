9912cdb0491084b5287c6e0f348f9628d135028f
Peter Tribble <peter.tribble@gmail.com>
2024-01-13 13:01:37 +0000

    Update gixy 0.1.21

--- TRIBgixy-python-311/build.sh
+++ TRIBgixy-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-git clone https://github.com/yandex/gixy
-cd gixy
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/sut
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/sut
-${THOME}/build/create_pkg TRIBgixy-python-311 /tmp/sut
-cd ..
-rm -fr /tmp/sut gixy
+${THOME}/build/pkg_setup_py TRIBgixy-python-311 gixy-0.1.21
