1885410b5b5de290520af5fc3292f628f9b50ae1
Peter Tribble <peter.tribble@gmail.com>
2024-01-14 10:37:08 +0000

    Drop 32-bit dconf

--- TRIBdconf/build.sh
+++ TRIBdconf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env AR=/usr/bin/ar ${THOME}/build/mesonbuild -gnu -64 dconf-0.40.0 -C -Dbash_completion=false
+env AR=/usr/bin/ar ${THOME}/build/mesonbuild -gnu -64only dconf-0.40.0 -C -Dbash_completion=false
 ${THOME}/build/genpkg TRIBdconf dconf-0.40.0
