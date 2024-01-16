6441cbc9fbf1bed0db35b746eabf30fd4fc1cf3b
Peter Tribble <peter.tribble@gmail.com>
2024-01-16 18:25:59 +0000

    Update restic 0.16.3

--- TRIBrestic/build.sh
+++ TRIBrestic/build.sh
@@ -11,7 +11,7 @@ rm -fr /tmp/rr
 mkdir -p /tmp/rr/usr/bin
 cp restic /tmp/rr/usr/bin
 mkdir -p /tmp/rr/usr/share/restic
-cp README.rst LICENSE VERSION /tmp/rr/usr/share/restic
+cp README.md LICENSE VERSION /tmp/rr/usr/share/restic
 mkdir -p /tmp/rr/usr/share/man/man1
 /tmp/rr/usr/bin/restic generate --man /tmp/rr/usr/share/man/man1
 $THOME/build/create_pkg TRIBrestic /tmp/rr
