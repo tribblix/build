6cd1e1fc5275ab1b4d7769b0f226ab523fa4f098
Peter Tribble <peter.tribble@gmail.com>
2024-01-16 19:17:45 +0000

    Update bat 0.24.0

--- TRIBbat/build.sh
+++ TRIBbat/build.sh
@@ -5,15 +5,12 @@
 git clone https://github.com/sharkdp/bat.git
 cd bat
 env AR=/usr/gnu/bin/ar cargo build --release
-cd ..
 
 rm -fr /tmp/ll
 mkdir -p /tmp/ll/usr/bin
 mkdir -p /tmp/ll/usr/share/man/man1
 
-cd bat
 ginstall -s -Dm755 ./target/release/bat -t /tmp/ll/usr/bin
-mkdir -p /tmp/ll/usr/share/man/man1
 cp target/release/build/bat-*/out/assets/manual/bat.1 /tmp/ll/usr/share/man/man1
 cd ..
 
