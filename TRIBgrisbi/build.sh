ad19e319dbe4bdd1942c9839a8391b9c63774d15
Peter Tribble <peter.tribble@gmail.com>
2024-01-18 13:24:27 +0000

    Update grisbi 3.0.2

--- TRIBgrisbi/build.sh
+++ TRIBgrisbi/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu -64only grisbi-2.0.5
-${THOME}/build/genpkg TRIBgrisbi grisbi-2.0.5
+${THOME}/build/dobuild -64only grisbi-3.0.2 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBgrisbi grisbi-3.0.2
