5b5e9161c69d0f02c295d5d2c53b4bc1b7f99b58
Peter Tribble <peter.tribble@gmail.com>
2024-01-18 08:49:50 +0000

    Update libmateweather 1.26.3

--- TRIBmate-weather/build.sh
+++ TRIBmate-weather/build.sh
@@ -2,5 +2,5 @@
 #
 # actually libmateweather
 #
-${THOME}/build/dobuild -64only -gnu libmateweather-1.26.1
-${THOME}/build/genpkg TRIBmate-weather libmateweather-1.26.1
+${THOME}/build/dobuild -64only -gnu libmateweather-1.26.3
+${THOME}/build/genpkg TRIBmate-weather libmateweather-1.26.3
