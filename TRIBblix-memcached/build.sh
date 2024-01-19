90e17b99af59ccc5fe86315b697dfab14d99c8a3
Peter Tribble <peter.tribble@gmail.com>
2024-01-19 09:11:45 +0000

    Update memcached 1.6.23

--- TRIBblix-memcached/build.sh
+++ TRIBblix-memcached/build.sh
@@ -3,6 +3,6 @@
 # Hack! temporary static copy of libevent placed in /tmp/mc
 # Remember to update the preconf file and have a copy of libevent-2.1.12-stable
 #
-${THOME}/build/dobuild -64only memcached-1.6.22 -P /opt/tribblix/memcached -C --with-libevent=/tmp/mc
-${THOME}/build/genpkg TRIBblix-memcached memcached-1.6.22
+${THOME}/build/dobuild -64only memcached-1.6.23 -P /opt/tribblix/memcached -C --with-libevent=/tmp/mc
+${THOME}/build/genpkg TRIBblix-memcached memcached-1.6.23
 rm -fr /tmp/mc
