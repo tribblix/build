e555641198d70144c06a48dda0a2f0572e74bed3
Peter Tribble <peter.tribble@gmail.com>
2024-01-19 21:30:46 +0000

    Make the wmaker-themes package match what it says here

--- TRIBwmaker-themes/build.sh
+++ TRIBwmaker-themes/build.sh
@@ -2,15 +2,16 @@
 #
 # some of the wmaker theme backgrounds are added to xfce as well
 #
+rm -fr /tmp/pth
 mkdir -p /tmp/pth/usr/share/WindowMaker/Themes
 mkdir -p /tmp/pth/usr/share/backgrounds/xfce
 cd /tmp/pth/usr/share/WindowMaker/Themes
-${THOME}/build/unpack osol-wmaker-theme
-${THOME}/build/unpack niagara-wmaker-theme
-${THOME}/build/unpack raystone-wmaker-theme
+git clone https://github.com/ptribble/tribblix-wmaker-themes .
+rm -fr .git README.md
 cd /tmp/pth/usr/share/backgrounds/xfce
 ln -s ../../WindowMaker/Themes/OpenSolaris.themed/stream.jpg wmaker-osol.jpg
 ln -s ../../WindowMaker/Themes/Niagara.themed/niagara_us.jpg wmaker-niagara.jpg
 ln -s ../../WindowMaker/Themes/RayStone.themed/hallway.jpg wmaker-raystone.jpg
 cd
 ${THOME}/build/create_pkg TRIBwmaker-themes /tmp/pth
+rm -fr /tmp/pth
