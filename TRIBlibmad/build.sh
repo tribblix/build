8589353ceb91dc350772116080bbd4243d336824
Peter Tribble <peter.tribble@gmail.com>
2024-01-25 08:18:03 +0000

    libmad actually uses CMake now

--- TRIBlibmad/build.sh
+++ TRIBlibmad/build.sh
@@ -5,5 +5,5 @@
 # you may need to recreate the tarball, as codeberg downloads unpack
 # without the version number
 #
-${THOME}/build/dobuild -64only libmad-0.16.4
+${THOME}/build/cmbuild -64only libmad-0.16.4
 ${THOME}/build/genpkg TRIBlibmad libmad-0.16.4
