ed9012ed17296dfce9cdce4f55ee79162b72048f
Peter Tribble <peter.tribble@gmail.com>
2024-01-27 09:30:23 +0000

    Update lzlib 1.14 plzip 1.11

--- TRIBcompress-lzip/build.sh
+++ TRIBcompress-lzip/build.sh
@@ -3,6 +3,6 @@
 # lzip and friends
 #
 ${THOME}/build/dobuild lzip-1.23 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
-${THOME}/build/dobuild -64 lzlib-1.13 -C --enable-shared +C "CFLAGS+=-m64 --enable-shared"
-${THOME}/build/dobuild plzip-1.10 -C "CPPFLAGS=-I../lzlib-1.13-64bit LDFLAGS=-L../lzlib-1.13-64bit CXXFLAGS+=-m64"
-${THOME}/build/genpkg TRIBcompress-lzip lzip-1.23 lzlib-1.13 plzip-1.10
+${THOME}/build/dobuild -64 lzlib-1.14 -C --enable-shared +C "CFLAGS+=-m64 --enable-shared"
+${THOME}/build/dobuild plzip-1.11 -C "CPPFLAGS=-I../lzlib-1.14-64bit LDFLAGS=-L../lzlib-1.14-64bit CXXFLAGS+=-m64"
+${THOME}/build/genpkg TRIBcompress-lzip lzip-1.23 lzlib-1.14 plzip-1.11
