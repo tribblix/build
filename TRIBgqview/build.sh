ca70528a4b3e185f526f9d38eea1afc23f898423
Peter Tribble <peter.tribble@gmail.com>
2024-01-28 21:20:48 +0000

    Build gqview 64-bit

--- TRIBgqview/build.sh
+++ TRIBgqview/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS="-lm -lsocket" ${THOME}/build/dobuild gqview-2.0.4
+env LIBS="-lm -lsocket" ${THOME}/build/dobuild -64only gqview-2.0.4
 ${THOME}/build/genpkg TRIBgqview gqview-2.0.4
