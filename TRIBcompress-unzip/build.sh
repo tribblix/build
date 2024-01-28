fbc221919a3dd6c194755c075ce3b7ce57b9c0d2
Peter Tribble <peter.tribble@gmail.com>
2024-01-28 21:08:45 +0000

    Build unzip 64-bit

--- TRIBcompress-unzip/build.sh
+++ TRIBcompress-unzip/build.sh
@@ -6,7 +6,7 @@ ${THOME}/build/unpack unzip60
 cd unzip60
 ${THOME}/build/unpack bzip2-1.0.6
 mv bzip2-1.0.6/* bzip2
-gmake -f unix/Makefile D_USE_BZ2=-DUSE_BZIP2 L_BZ2=-lbz2 generic_gcc
+gmake -f unix/Makefile D_USE_BZ2=-DUSE_BZIP2 L_BZ2=-lbz2 generic CC="gcc -m64"
 rm -fr /tmp/unzip
 gmake -f unix/Makefile install prefix=/tmp/unzip/usr
 ${THOME}/build/create_pkg TRIBcompress-unzip /tmp/unzip
