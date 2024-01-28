00993478d44fa84eeb6ecae7ffee4467d34640c9
Peter Tribble <peter.tribble@gmail.com>
2024-01-28 21:13:21 +0000

    Build zip 64-bit

--- TRIBcompress-zip/build.sh
+++ TRIBcompress-zip/build.sh
@@ -2,7 +2,7 @@
 #
 ${THOME}/build/unpack zip30
 cd zip30
-gmake -f unix/Makefile generic_gcc
+gmake -f unix/Makefile generic CC="gcc -m64"
 rm -fr /tmp/zip
 gmake -f unix/Makefile install prefix=/tmp/zip/usr
 ${THOME}/build/create_pkg TRIBcompress-zip /tmp/zip
