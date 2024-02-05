07a9a24c673c37fd182f27b3b9c0a4977c621ec0
Peter Tribble <peter.tribble@gmail.com>
2024-02-05 17:09:56 +0000

    Update bitmap 1.1.1

--- TRIBx11-bitmap/build.sh
+++ TRIBx11-bitmap/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only bitmap-1.1.0
-${THOME}/build/genpkg TRIBx11-bitmap bitmap-1.1.0
+${THOME}/build/dobuild -64only bitmap-1.1.1
+${THOME}/build/genpkg TRIBx11-bitmap bitmap-1.1.1
