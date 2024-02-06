94e507dcbbf1b89bbe935d16238d0e34a03a1458
Peter Tribble <peter.tribble@gmail.com>
2024-02-06 18:16:49 +0000

    Add diffoscope (experimental)

---
+++ TRIBdiffoscope-python-311/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# there are a lot of potential dependencies we could add
+#
+# [cmdline] argcomplete progressbar
+# also pypdf defusedxml
+#
+${THOME}/build/pkg_setup_py TRIBdiffoscope-python-311 diffoscope-255
---
+++ TRIBlibarchive-c-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBlibarchive-c-python-311 libarchive-c-5.0
