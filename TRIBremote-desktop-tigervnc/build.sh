7841ce031dd513e1cf5e8933c2c1bb71603d12af
Peter Tribble <peter.tribble@gmail.com>
2024-02-07 18:17:51 +0000

    Update TigerVNC Viewer v1.13.1

--- TRIBremote-desktop-tigervnc/build.sh
+++ TRIBremote-desktop-tigervnc/build.sh
@@ -2,5 +2,7 @@
 #
 # our own tigervnc
 #
-${THOME}/build/cmbuild -64only -gnu tigervnc-1.10.1
-${THOME}/build/genpkg TRIBremote-desktop-tigervnc tigervnc-1.10.1
+# we turn off H264 to avoid pulling ffmpeg
+#
+env TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/cmbuild -64only -gnu tigervnc-1.13.1 -C -DENABLE_H264=OFF
+${THOME}/build/genpkg TRIBremote-desktop-tigervnc tigervnc-1.13.1
