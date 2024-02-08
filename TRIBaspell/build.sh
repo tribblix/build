c3b86f441e75498e6a2559ed90d027fdeee3ab3e
Peter Tribble <peter.tribble@gmail.com>
2024-02-08 16:25:40 +0000

    Update aspell 0.60.8.1

--- TRIBaspell/build.sh
+++ TRIBaspell/build.sh
@@ -3,7 +3,5 @@
 # aspell is a build dependency for dictionaries, it is not intended
 # to be supported in its own right
 #
-# (which is why there is no user-friendly alias)
-#
-env CPPFLAGS="-I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/dobuild -64only aspell-0.60.8 -C "--sysconfdir=/etc --with-curses=ncurses"
-${THOME}/build/genpkg TRIBaspell aspell-0.60.8
+env CPPFLAGS="-I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/dobuild -64only aspell-0.60.8.1 -C "--sysconfdir=/etc --with-curses=ncurses"
+${THOME}/build/genpkg TRIBaspell aspell-0.60.8.1
