3cf9ddc6797a2beed8e7c8f98549a577cd14d4a0
Peter Tribble <peter.tribble@gmail.com>
2024-02-08 15:39:27 +0000

    Update PostgreSQL 15.6 16.2

--- TRIBblix-postgres15/build.sh
+++ TRIBblix-postgres15/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 15
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres15 postgresql-15.5 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres15 postgresql-15.5
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres15 postgresql-15.6 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres15 postgresql-15.6
--- TRIBblix-postgres16/build.sh
+++ TRIBblix-postgres16/build.sh
@@ -2,5 +2,5 @@
 #
 # postgres 16
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres16 postgresql-16.1 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres16 postgresql-16.1
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres16 postgresql-16.2 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres16 postgresql-16.2
