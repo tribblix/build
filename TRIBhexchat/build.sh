9559bb4c4e02fd8bdd91ee400061b488ab9ff399
Peter Tribble <peter.tribble@gmail.com>
2024-02-08 11:50:08 +0000

    Update hexchat 2.16.2

--- TRIBhexchat/build.sh
+++ TRIBhexchat/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/unpack hexchat-2.16.1
-cd hexchat-2.16.1
 
 #
 # disable perl for now as it's never worked
@@ -9,10 +7,5 @@ cd hexchat-2.16.1
 # we ought to pick up the 64-bit python, but while the module builds
 # that gets symbol errors at runtime we ought to look at
 #
-# buildtype=plain avoids SSP failures
-env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig LDFLAGS="-lsocket -lnsl" meson build -Dwith-lua=false -Dwith-sysinfo=false -Dprefix=/usr -Dbuildtype=plain -Dc_args=-m64 -Dcpp_args=-m64 -Dc_link_args="-m64 -lsocket -lnsl" -Dcpp_link_args=-m64 -Dwith-perl=false -Dwith-python=false -Dlibcanberra=disabled
-env PATH=/usr/gnu/bin:$PATH ninja -C build
-rm -fr /tmp/hh
-env DESTDIR=/tmp/hh PATH=/usr/gnu/bin:$PATH ninja -C build install
-${THOME}/build/create_pkg TRIBhexchat /tmp/hh
-rm -fr /tmp/hh
+env AR=/usr/bin/ar TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/mesonbuild -gnu -64only hexchat-2.16.2 -C "-Dwith-lua=false -Dwith-sysinfo=false -Dwith-perl=false -Dwith-python=false -Dlibcanberra=disabled"
+${THOME}/build/genpkg TRIBhexchat hexchat-2.16.2
