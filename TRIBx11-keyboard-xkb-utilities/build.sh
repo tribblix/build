042553a899d9f3c3dc1ec6bca73073335768cfab
Peter Tribble <peter.tribble@gmail.com>
2024-02-08 18:03:13 +0000

    Update xkbutils 1.0.6 xkbprint 1.0.6 setxkbmap 1.3.4

--- TRIBx11-keyboard-xkb-utilities/build.sh
+++ TRIBx11-keyboard-xkb-utilities/build.sh
@@ -3,7 +3,7 @@
 # historically this was a merged package and I can't see any benefit
 # from fragmenting it
 #
-${THOME}/build/dobuild -64only xkbutils-1.0.5
-${THOME}/build/dobuild -64only xkbprint-1.0.5
-${THOME}/build/dobuild -64only setxkbmap-1.3.3
-${THOME}/build/genpkg TRIBx11-keyboard-xkb-utilities xkbutils-1.0.5 xkbprint-1.0.5 setxkbmap-1.3.3
+${THOME}/build/dobuild -64only xkbutils-1.0.6
+${THOME}/build/dobuild -64only xkbprint-1.0.6
+${THOME}/build/dobuild -64only setxkbmap-1.3.4
+${THOME}/build/genpkg TRIBx11-keyboard-xkb-utilities xkbutils-1.0.6 xkbprint-1.0.6 setxkbmap-1.3.4
