3a50de374b53d50c0e609555f02241b1f0ed1021
Peter Tribble <peter.tribble@gmail.com>
2024-02-09 21:25:07 +0000

    Update nginx-prometheus-exporter 1.1.0

--- TRIBblix-promnginx/build.sh
+++ TRIBblix-promnginx/build.sh
@@ -1,9 +1,9 @@
-#!/bin/tcsh
+#!/bin/sh
 #
 
-$THOME/build/unpack nginx-prometheus-exporter-0.11.0
-cd nginx-prometheus-exporter-0.11.0
-gmake
+$THOME/build/unpack nginx-prometheus-exporter-1.1.0
+cd nginx-prometheus-exporter-1.1.0
+env PATH=/usr/gnu/bin:$PATH gmake nginx-prometheus-exporter
 
 ${THOME}/build/genpkg TRIBblix-promnginx
 cd ..
