11a08f63091168a87568cd999b52e9642ae869be
Peter Tribble <peter.tribble@gmail.com>
2024-02-09 11:55:30 +0000

    Rebuild gstreamer, with separate 32 and 64-bit registry files

--- TRIBgstreamer1/build.sh
+++ TRIBgstreamer1/build.sh
@@ -3,5 +3,8 @@
 # note that we have to install both 32-bit and 64-bit files into /usr/libexec
 # as gst-plugin-scanner populates the registry and needs to match
 #
+# there are 32 and 64-bit postconf patches to ensure that there are different
+# registries for 32 and 64-bit applications, otherwise one will crash
+#
 env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64 gstreamer-1.20.7 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled" +C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled -Dlibexecdir=/usr/libexec/`$THOME/build/getarch`"
 ${THOME}/build/genpkg TRIBgstreamer1 gstreamer-1.20.7
