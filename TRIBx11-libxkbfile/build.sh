815002ae5622003a321556cf6933ce8fa7e24b87
Peter Tribble <peter.tribble@gmail.com>
2024-02-09 09:32:01 +0000

    Update libxkbfile 1.1.3

--- TRIBx11-libxkbfile/build.sh
+++ TRIBx11-libxkbfile/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libxkbfile-1.1.1
-${THOME}/build/genpkg TRIBx11-libxkbfile libxkbfile-1.1.1
+# we patch the SONAME, and do so for both autotools and meson
+#
+${THOME}/build/mesonbuild -64 libxkbfile-1.1.3
+${THOME}/build/genpkg TRIBx11-libxkbfile libxkbfile-1.1.3
