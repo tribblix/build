8a9905a5319d31bc9ad1c4684bf595404e438f8b
Peter Tribble <peter.tribble@gmail.com>
2024-02-09 19:52:27 +0000

    Update vault 1.15.5

--- TRIBblix-vault/build.sh
+++ TRIBblix-vault/build.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 #
-# (requires go 1.19)
-#
 mkdir ~/ud/v
 cd ~/ud/v
 git clone https://github.com/hashicorp/vault.git
 cd vault
-git checkout v1.12.5
+git checkout v1.15.5
 
-# newer versions fail to make bootstrap
-gmake bootstrap
 # needs gnu grep for the version check to work
-env PATH=/usr/versions/go-1.20/bin:/usr/gnu/bin:$PATH gmake
+env PATH=/usr/versions/go-1.21/bin:/usr/gnu/bin:$PATH gmake bootstrap
+env PATH=/usr/versions/go-1.21/bin:/usr/gnu/bin:$PATH gmake
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/vault/bin
