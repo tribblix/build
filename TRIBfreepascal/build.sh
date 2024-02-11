5490c0850580b4ed5d698574ad7c9cb2a3d6f993
Peter Tribble <peter.tribble@gmail.com>
2024-02-11 09:39:05 +0000

    Add Free Pascal 3.2.2

---
+++ TRIBfreepascal/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# to bootstrap the build, first install the binary download
+# (fpc-3.2.2.sparc-solaris.tar or fpc-3.2.2.x86_64-solaris.tar)
+# into /tmp/f and add a couple of symlinks
+#  ln -s /usr/gnu/bin/ld /tmp/f/bin/gld
+#  ln -s /usr/gnu/bin/strip /tmp/f/bin/gstrip
+#
+# the fixit script will add the required symlinks to the installed version
+#
+${THOME}/build/unpack fpc-3.2.2.source
+cd fpc-3.2.2
+env PATH=/tmp/f/bin:/usr/gnu/bin:$PATH fpcmake Makefile.fpc
+env PATH=/tmp/f/bin:/usr/gnu/bin:$PATH gmake all OVERRIDEVERSIONCHECK=1
+env PATH=/tmp/f/bin:/usr/gnu/bin:$PATH gmake install PREFIX=/tmp/hhh/usr/versions/fpc
+${THOME}/build/create_pkg TRIBfreepascal /tmp/hhh
+rm -fr /tmp/hhh
