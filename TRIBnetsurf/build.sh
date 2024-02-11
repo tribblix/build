473e17ca2e3460c84b2d613a40ead525b29aadd6
Peter Tribble <peter.tribble@gmail.com>
2024-02-11 22:06:02 +0000

    Update NetSurf 3.11

--- TRIBnetsurf/build.sh
+++ TRIBnetsurf/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-$THOME/build/unpack netsurf-all-3.10
-cd netsurf-all-3.10
+$THOME/build/unpack netsurf-all-3.11
+cd netsurf-all-3.11
 #
 # we need TRIBlib-perl-5-html-parser for utils/split-messages.pl
 #
@@ -10,11 +10,11 @@ cd netsurf-all-3.10
 #
 gmake -j 4 build CC=gcc BUILD=i386-pc-solaris2.11 PREFIX=/usr AR=ar INSTALL=ginstall BUILD_CC=gcc TARGET=gtk3
 # passing INSTALL is ignored
-rm -fr /tmp/nn310
-env PATH=/usr/gnu/bin:$PATH gmake install CC=gcc BUILD=i386-pc-solaris2.11 PREFIX=/usr DESTDIR=/tmp/nn310 INSTALL=ginstall BUILD_CC=gcc TARGET=gtk3
-mkdir -p /tmp/nn310/usr/share/man/man1
-cp netsurf/docs/netsurf-gtk.1 /tmp/nn310/usr/share/man/man1
-mkdir -p /tmp/nn310/usr/share/applications
-cp netsurf/frontends/gtk/res/netsurf-gtk.desktop /tmp/nn310/usr/share/applications
-$THOME/build/create_pkg TRIBnetsurf /tmp/nn310
-rm -fr /tmp/nn310
+rm -fr /tmp/nn311
+env PATH=/usr/gnu/bin:$PATH gmake install CC=gcc BUILD=i386-pc-solaris2.11 PREFIX=/usr DESTDIR=/tmp/nn311 INSTALL=ginstall BUILD_CC=gcc TARGET=gtk3
+mkdir -p /tmp/nn311/usr/share/man/man1
+cp netsurf/docs/netsurf-gtk.1 /tmp/nn311/usr/share/man/man1
+mkdir -p /tmp/nn311/usr/share/applications
+cp netsurf/frontends/gtk/res/netsurf-gtk.desktop /tmp/nn311/usr/share/applications
+$THOME/build/create_pkg TRIBnetsurf /tmp/nn311
+rm -fr /tmp/nn311
--- patches/netsurf-all-3.10.pls
+++
@@ -1,2 +0,0 @@
--p1 netsurf-cflags.patch
--p1 netsurf-endian.patch
