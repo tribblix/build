783ee9d8bf58246000e6d7696c11837896534696
Peter Tribble <peter.tribble@gmail.com>
2024-02-12 20:32:07 +0000

    Update OpenEXR 3.1.12

--- TRIBopenexr/build.sh
+++ TRIBopenexr/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 #
-${THOME}/build/cmbuild -64only openexr-3.1.5
-${THOME}/build/genpkg TRIBopenexr openexr-3.1.5
+${THOME}/build/cmbuild -64only openexr-3.1.12
+${THOME}/build/genpkg TRIBopenexr openexr-3.1.12
