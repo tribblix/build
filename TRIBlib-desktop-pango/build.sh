346849abe7cdbccd28b31c1ffc4e43cf5447b508
Peter Tribble <peter.tribble@gmail.com>
2024-02-13 18:54:51 +0000

    Update pango 1.50.14

--- TRIBlib-desktop-pango/build.sh
+++ TRIBlib-desktop-pango/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-$THOME/build/mesonbuild -64 pango-1.44.7 -C "-Dintrospection=false"
-${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.44.7
+$THOME/build/mesonbuild -64 pango-1.50.14 -C "-Dintrospection=disabled"
+${THOME}/build/genpkg TRIBlib-desktop-pango pango-1.50.14
