b432c9b437855f497689a8d7e5cacc6c8483bc6d
Peter Tribble <peter.tribble@gmail.com>
2024-02-14 17:02:40 +0000

    Update nginx 1.25.4

--- TRIBblix-nginx/build.sh
+++ TRIBblix-nginx/build.sh
@@ -7,5 +7,5 @@
 # it up for LDFLAGS so the link fails; hence we tell it directly
 # via $CC (and don't use --with-cc any more)
 #
-env CC="gcc -m64" ${THOME}/build/dobuild nginx-1.25.3 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module --with-http_v3_module"
-${THOME}/build/genpkg TRIBblix-nginx nginx-1.25.3
+env CC="gcc -m64" ${THOME}/build/dobuild nginx-1.25.4 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module --with-http_v3_module"
+${THOME}/build/genpkg TRIBblix-nginx nginx-1.25.4
