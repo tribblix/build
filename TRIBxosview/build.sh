c114ab8aeb926e126e64044fe16d5c5773c78625
Peter Tribble <peter.tribble@gmail.com>
2024-02-15 22:06:35 +0000

    Update xosview 1.24

--- TRIBxosview/build.sh
+++ TRIBxosview/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xosview-1.23 -M "PLATFORM=sunos5 PREFIX=/usr CXXFLAGS=-m64 LDFLAGS=-m64"
-${THOME}/build/genpkg -M "PLATFORM=sunos5 PREFIX=/usr" TRIBxosview xosview-1.23
+${THOME}/build/dobuild xosview-1.24 -M "PLATFORM=sunos5 PREFIX=/usr CXXFLAGS=-m64 LDFLAGS=-m64"
+${THOME}/build/genpkg -M "PLATFORM=sunos5 PREFIX=/usr" TRIBxosview xosview-1.24
