ee7bc134021e039395c3a3208441bb3866dba784
Peter Tribble <peter.tribble@gmail.com>
2024-02-15 21:53:37 +0000

    Update planner 0.14.92

--- TRIBplanner/build.sh
+++ TRIBplanner/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env AR=/usr/bin/ar $THOME/build/mesonbuild -64only -gnu planner-0.14.91 -C -Dexamples=true
-${THOME}/build/genpkg TRIBplanner planner-0.14.91
+env AR=/usr/bin/ar $THOME/build/mesonbuild -64only -gnu planner-0.14.92 -C -Dexamples=true
+${THOME}/build/genpkg TRIBplanner planner-0.14.92
