5a69882f6ebd64712e058e4327c73b5cfac1d092
Peter Tribble <peter.tribble@gmail.com>
2024-02-15 21:41:12 +0000

    Update sakura 3.8.7

--- TRIBsakura/build.sh
+++ TRIBsakura/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:${PATH}:/usr/perl5/bin ${THOME}/build/cmbuild -64only sakura-3.8.5
-${THOME}/build/genpkg TRIBsakura sakura-3.8.5
+${THOME}/build/cmbuild -64only -gnu sakura-3.8.7
+${THOME}/build/genpkg TRIBsakura sakura-3.8.7
