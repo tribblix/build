0420bc7ac11cfcc14da56ebf311f8d7338ca8428
Peter Tribble <peter.tribble@gmail.com>
2024-02-16 11:30:00 +0000

    Package jdk22

---
+++ TRIBopenjdk22-src/build.sh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+#
+# a separate package for src.zip to live in to keep the main package smaller
+#
+echo "See the main package"
---
+++ TRIBopenjdk22/build.sh
@@ -0,0 +1,88 @@
+#!/bin/tcsh
+#
+# jdk mainline has been migrated to git
+# go to https://github.com/openjdk/jdk22/tags
+#
+
+cd ~/ud
+${THOME}/build/unpack jdk22-jdk-22-35
+cd jdk22-jdk-22-35
+
+#
+# jdk22 needs autoconf installed
+#
+zap install autoconf
+
+#
+# looks like dtrace is busted, illumos and Solaris have diverged
+# enough to trip it up, so --enable-dtrace=no
+#
+# the gobjcopy stuff doesn't actually work, so disable it
+# --with-native-debug-symbols=none
+#
+# we're recognized as solaris, JEP 362 deprecated the solaris and sparc
+# ports, so we need to explicitly re-enable it
+#
+# use jdk21 as the boot jdk
+#
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk21 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-dtrace \
+--disable-warnings-as-errors \
+--with-source-date=current \
+--with-jobs=3 \
+DATE=/usr/gnu/bin/date \
+STRIP=/usr/gnu/bin/strip
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake all
+
+#
+# first testing looks like this:
+#
+# cd build/solaris-x86_64-server-release/images/jdk
+# ./bin/java -version
+#
+# openjdk version "22-internal" 2024-03-19
+# OpenJDK Runtime Environment (build 22-internal-adhoc.ptribble.jdk22-jdk-22-35)
+# OpenJDK 64-Bit Server VM (build 22-internal-adhoc.ptribble.jdk22-jdk-22-35, mixed mode, sharing)
+#
+
+rm -fr /tmp/jdk
+mkdir -p /tmp/jdk/usr/versions/openjdk22
+mkdir -p /tmp/jdk/usr/jdk/instances
+(cd build/solaris-x86_64-server-release/images/jdk; tar cf - *) | ( cd /tmp/jdk/usr/versions/openjdk22 ; tar xf -)
+cd /tmp/jdk/usr/jdk/instances
+ln -s ../../versions/openjdk22 jdk22
+cd /tmp/jdk/usr/jdk
+ln -s ../versions/openjdk22 .
+#jdk22 isn't LTS, don't make it the default
+
+#
+# need to create a certificate bundle
+#
+# the jdk22 build appears to create a working one, but it uses
+# a horribly outdated set of certificates, so replace it
+#
+# this actually needs to be a JKS keystore, not a PKCS12 keystore
+# https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1739631
+#
+${THOME}/build/patches/mkcacerts -f /etc/openssl/cacert.pem -o /tmp/cacerts -k /usr/jdk/instances/jdk1.8.0/bin/keytool -s /usr/bin/openssl
+#
+cp /tmp/cacerts /tmp/jdk/usr/versions/openjdk22/lib/security
+
+#
+# edit conf/security/sunpkcs11-solaris.cfg and add the following to disabledMechanisms
+#
+# # the following mechanisms are disabled due to lack of digest cloning support
+# # need to fix 6414899 first
+#   CKM_MD5
+#   CKM_SHA256
+#   CKM_SHA384
+#   CKM_SHA512
+#
+
+(cd /tmp/jdk/usr/versions/openjdk22/conf/security/ ; gpatch -p1 < ${THOME}/build/patches/sunpkcs11-solaris.cfg.patch)
+
+${THOME}/build/create_pkg TRIBopenjdk22 /tmp/jdk
