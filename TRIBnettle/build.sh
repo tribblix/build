aec1f5916cd5466afdbae1687a6cc173edd3a745
Peter Tribble <peter.tribble@gmail.com>
2024-02-16 20:50:09 +0000

    Drop old nettle compat

--- TRIBnettle/build.sh
+++ TRIBnettle/build.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 #
-# we ship the previous versions of the library for binary
-# compatibility with gnutls + squid + vnc
-# once all rebuilt the compat copy can be removed
-#
-# 3.9.1 claims binary compatibility with our previous 3.6
-#
 ${THOME}/build/dobuild -64 nettle-3.9.1 -C --disable-static
 ${THOME}/build/genpkg TRIBnettle nettle-3.9.1
