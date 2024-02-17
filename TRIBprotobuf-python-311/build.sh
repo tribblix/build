61b8a73f86b4953d297f45f9ba867357d96d1551
Peter Tribble <peter.tribble@gmail.com>
2024-02-17 19:15:36 +0000

    Update python protobuf 4.25.3

--- TRIBprotobuf-python-311/build.sh
+++ TRIBprotobuf-python-311/build.sh
@@ -3,4 +3,4 @@
 # entertainingly, the python module has a filename clash
 # with the regular protobuf download
 #
-${THOME}/build/pkg_setup_py TRIBprotobuf-python-311 protobuf-4.25.2
+${THOME}/build/pkg_setup_py TRIBprotobuf-python-311 protobuf-4.25.3
