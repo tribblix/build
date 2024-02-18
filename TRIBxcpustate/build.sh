52450d9676dbdcf94758decdda7ab23d553a02c0
Peter Tribble <peter.tribble@gmail.com>
2024-02-18 20:55:01 +0000

    Rebuild xcpustate 64-bit, and modernize

--- TRIBxcpustate/build.sh
+++ TRIBxcpustate/build.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild xcpustate-2.9
-cd xcpustate-2.9
-rm -fr /tmp/xc
-mkdir -p /tmp/xc/usr/bin /tmp/xc/usr/share/man/man1
-cp xcpustate /tmp/xc/usr/bin
-cp xcpustate.man /tmp/xc/usr/share/man/man1/xcpustate.1
-${THOME}/build/create_pkg TRIBxcpustate /tmp/xc
-rm -fr /tmp/xc
+${THOME}/build/dobuild xcpustate-2.9p1
+${THOME}/build/genpkg TRIBxcpustate xcpustate-2.9p1
