06b7de8c065a9012ca15b56c5a6d2538c8ba23bf
Peter Tribble <peter.tribble@gmail.com>
2024-02-19 14:55:48 +0000

    Update Liberation Fonts 2.1.5

--- TRIBfont-ttype-liberation/build.sh
+++ TRIBfont-ttype-liberation/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/unpack liberation-fonts-ttf-2.00.1
-${THOME}/build/genpkg TRIBfont-ttype-liberation liberation-fonts-ttf-2.00.1
+# packaging requires mkfontscale from x11-font-utilities
+# package requires fontconfig to provide the fc-cache service
+#
+${THOME}/build/unpack liberation-fonts-ttf-2.1.5
+${THOME}/build/genpkg TRIBfont-ttype-liberation liberation-fonts-ttf-2.1.5
