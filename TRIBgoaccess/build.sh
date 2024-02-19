18fdea6fee8f713d799b15e5b7a0015bafe59762
Peter Tribble <peter.tribble@gmail.com>
2024-02-19 16:03:15 +0000

    Update goaccess 1.9.1

--- TRIBgoaccess/build.sh
+++ TRIBgoaccess/build.sh
@@ -4,5 +4,5 @@
 # cannot enable utf8 as that requires ncursesw
 # patch to disable use of gmtoff
 #
-env TRIBBLIX_CFLAGS="-std=gnu99 -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only goaccess-1.7.2 -C "--sysconfdir=/etc"
-${THOME}/build/genpkg TRIBgoaccess goaccess-1.7.2
+env TRIBBLIX_CFLAGS="-std=gnu99 -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only goaccess-1.9.1 -C "--sysconfdir=/etc"
+${THOME}/build/genpkg TRIBgoaccess goaccess-1.9.1
