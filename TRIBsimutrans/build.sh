2d7d1686509b56126c5700552aa04bdfc386563d
Peter Tribble <peter.tribble@gmail.com>
2024-02-20 16:46:45 +0000

    Update simutrans

--- TRIBsimutrans/build.sh
+++ TRIBsimutrans/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# patch to avoid using the native type of label_t
+#
 env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only https://github.com/aburch/simutrans
 
 ${THOME}/build/genpkg TRIBsimutrans simutrans
