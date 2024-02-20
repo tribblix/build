b726c75c2fa338a686abcd0994441a941f23ef6a
Peter Tribble <peter.tribble@gmail.com>
2024-02-20 10:07:09 +0000

    Update freeciv 3.0.10

--- TRIBfreeciv/build.sh
+++ TRIBfreeciv/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-$THOME/build/dobuild -64only freeciv-3.0.9 -C "--enable-ipv6=test --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBfreeciv freeciv-3.0.9
+$THOME/build/dobuild -64only freeciv-3.0.10 -C "--enable-ipv6=test --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBfreeciv freeciv-3.0.10
