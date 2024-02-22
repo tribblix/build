80c492f7ae626dc39f3da697a76357e1acc44763
Peter Tribble <peter.tribble@gmail.com>
2024-02-22 16:37:10 +0000

    Update libgphoto2 2.5.31

--- TRIBlibgphoto2/build.sh
+++ TRIBlibgphoto2/build.sh
@@ -4,5 +4,5 @@
 #
 # 64-bit only, like the only consumer which is gphoto2
 #
-env CONFIG_SHELL=/bin/bash  ${THOME}/build/dobuild +64 libgphoto2-2.5.29
-${THOME}/build/genpkg TRIBlibgphoto2 libgphoto2-2.5.29
+env CONFIG_SHELL=/bin/bash  ${THOME}/build/dobuild +64 libgphoto2-2.5.31
+${THOME}/build/genpkg TRIBlibgphoto2 libgphoto2-2.5.31
