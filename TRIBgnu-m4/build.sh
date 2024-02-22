450aefda66d543b09b64f9f8e2b1286f2f2af8a1
Peter Tribble <peter.tribble@gmail.com>
2024-02-22 16:19:08 +0000

    Update gm4 1.4.19

--- TRIBgnu-m4/build.sh
+++ TRIBgnu-m4/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only m4-1.4.18 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-m4 m4-1.4.18
+${THOME}/build/dobuild -64only m4-1.4.19 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-m4 m4-1.4.19
