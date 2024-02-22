a4ab32cdda993a85a35aedc4232b406da6344d57
Peter Tribble <peter.tribble@gmail.com>
2024-02-22 14:51:10 +0000

    Check for hybrid iso presence on usb media

--- TRIBsys-install-media-internal/build.sh
+++ TRIBsys-install-media-internal/build.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh
+#!/bin/sh
 #
 # assuming the repo is checked out
 #
@@ -9,5 +9,6 @@ mkdir -p /tmp/lt2/sbin
 ( cd ${THOME}/tribblix-media ; tar cf - etc lib boot ) | ( cd /tmp/lt2 ; tar xf -)
 gcc -o /tmp/lt2/sbin/listcd -ldevinfo ${THOME}/tribblix-media/listcd.c
 gcc -o /tmp/lt2/sbin/listusb -ldevinfo ${THOME}/tribblix-media/listusb.c
+strip /tmp/lt2/sbin/list*
 ${THOME}/build/create_pkg TRIBsys-install-media-internal /tmp/lt2
 rm -fr /tmp/lt2
