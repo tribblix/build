b342ad5d3b96536890dfea8b588795b516e31d23
Peter Tribble <peter.tribble@gmail.com>
2024-02-23 15:29:28 +0000

    Update scikit-learn 1.4.1.post1

--- TRIBscikit-learn-python-311/build.sh
+++ TRIBscikit-learn-python-311/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-#
-# a pep518 build requires --no-build-isolation otherwise it tries
+# a pep518 build requires -n == --no-build-isolation otherwise it tries
 # to build its own numpy, which would fail as it doesn't have the
 # modifications we need to make it build successfully
 #
@@ -15,12 +14,4 @@
 # if it doesn't, you may need to edit openmp_helpers.py and make
 # check_openmp_support() unconditionally return False
 #
-${THOME}/build/unpack scikit-learn-1.3.2
-cd scikit-learn-1.3.2
-
-rm -fr /tmp/pil
-env CC="gcc -m64" LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.11/bin:$PATH /usr/versions/python-3.11/bin/python3 -mpip install --no-deps --isolated --no-input --exists-action=a --disable-pip-version-check --no-warn-script-location --compile --no-build-isolation --root=/tmp/pil -I .
-
-${THOME}/build/create_pkg TRIBscikit-learn-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+env CC="gcc -m64" LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.11/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscikit-learn-python-311 scikit-learn-1.4.1.post1
