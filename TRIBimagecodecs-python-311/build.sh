75c969eeaf53f76dae940e4fa23f82b4507db387
Peter Tribble <peter.tribble@gmail.com>
2024-02-23 15:04:32 +0000

    Update imagecodecs 2024.1.1

--- TRIBimagecodecs-python-311/build.sh
+++ TRIBimagecodecs-python-311/build.sh
@@ -4,4 +4,4 @@
 # need to copy in a custom setup.cfg so as to skip the codecs that
 # won't build due to us not having the external libraries they need
 #
-${THOME}/build/pkg_setup_py TRIBimagecodecs-python-311 imagecodecs-2023.9.18
+${THOME}/build/pkg_setup_py TRIBimagecodecs-python-311 imagecodecs-2024.1.1
