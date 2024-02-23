5d7a8c4335d0eabeb72e6e18514ef3602b9c145f
Peter Tribble <peter.tribble@gmail.com>
2024-02-23 09:10:06 +0000

    Update lib4package 0.2.0

--- TRIBlib4package-python-311/build.sh
+++ TRIBlib4package-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_wheel TRIBlib4package-python-311 lib4package-0.1.0-py2.py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBlib4package-python-311 lib4package-0.2.0-py2.py3-none-any.whl
