15191a1c199a7d4fbe1455dfeb46c3fb6e29bfb7
Peter Tribble <peter.tribble@gmail.com>
2024-02-24 10:08:43 +0000

    Update xterm 390

--- TRIBterminal-xterm/build.sh
+++ TRIBterminal-xterm/build.sh
@@ -2,5 +2,5 @@
 #
 # options mostly the same as OI
 #
-$THOME/build/dobuild -64only xterm-387 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
-${THOME}/build/genpkg TRIBterminal-xterm xterm-387
+$THOME/build/dobuild -64only xterm-390 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
+${THOME}/build/genpkg TRIBterminal-xterm xterm-390
