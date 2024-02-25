30a2a3ba27992528ef1b1079bb91bbb9e5af7067
Peter Tribble <peter.tribble@gmail.com>
2024-02-25 10:16:56 +0000

    Update libarchive 3.7.2, and drop 32-bit

--- TRIBlibarchive/build.sh
+++ TRIBlibarchive/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libarchive-3.7.1
-${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.1
+${THOME}/build/dobuild -64only libarchive-3.7.2
+${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.2
