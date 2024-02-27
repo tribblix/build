c5f9632b68eb74cc860e1d710619b6dc944eca7a
Peter Tribble <peter.tribble@gmail.com>
2024-02-27 14:42:36 +0000

    Update vim 9.1.0139

--- TRIBeditor-vim-gtk2/build.sh
+++ TRIBeditor-vim-gtk2/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vim-9.0.2081 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix --disable-canberra --disable-libsodium" -M -k
-${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-9.0.2081
+env TRIBBLIX_CFLAGS=-std=gnu99 PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only vim-9.1.0139 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix --disable-canberra --disable-libsodium" -M -k
+${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-9.1.0139
 #
 # Athena build looks like:
 # ${THOME}/build/dobuild vim90 -C "--enable-gui=athena --with-compiledby=ptribble@tribblix"
