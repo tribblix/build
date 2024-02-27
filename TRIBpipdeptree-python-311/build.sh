e2db52652b1d937c661989c2781382d102ada72f
Peter Tribble <peter.tribble@gmail.com>
2024-02-27 08:30:44 +0000

    Make a note of pipdeptree and graphviz

--- TRIBpipdeptree-python-311/build.sh
+++ TRIBpipdeptree-python-311/build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 #
+# The graphviz extra is useful for graphical output, using the graphiviz
+# module provided by the TRIBgraphviz-python-311 package
+#
+# That isn't declared as a dependency nor is it included in an overlay
+# because it would pull in graphviz and most of the desktop stack which
+# would not be what's wanted by default
+#
 ${THOME}/build/pkg_pep518 TRIBpipdeptree-python-311 pipdeptree-2.15.1
