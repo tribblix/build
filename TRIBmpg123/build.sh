4747475aac46551ab4c29b66426f573b365fa9b0
Peter Tribble <peter.tribble@gmail.com>
2024-02-27 14:24:29 +0000

    Update mpg123 1.32.5

--- TRIBmpg123/build.sh
+++ TRIBmpg123/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" ${THOME}/build/dobuild -64 mpg123-1.32.4 \
+env TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" ${THOME}/build/dobuild -64 mpg123-1.32.5 \
     -C "--disable-static --enable-shared --enable-int-quality --enable-fifo --enable-network --enable-ipv6=yes --with-optimization=3 --with-cpu=sse" \
     +C "--disable-static --enable-shared --enable-int-quality --enable-fifo --enable-network --enable-ipv6=yes --with-optimization=3 --with-cpu=x86-64"
 #
 # sparc
 # -C "--disable-static --enable-shared --enable-int-quality --enable-fifo --enable-network --enable-ipv6=yes --with-optimization=3"
 #
-${THOME}/build/genpkg TRIBmpg123 mpg123-1.32.4
+${THOME}/build/genpkg TRIBmpg123 mpg123-1.32.5
