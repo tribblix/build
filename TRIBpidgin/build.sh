8708d1ef4b58165dceb1bfae36e3c235df9d279b
Peter Tribble <peter.tribble@gmail.com>
2024-02-27 14:07:27 +0000

    Update pidgin 2.14.13

--- TRIBpidgin/build.sh
+++ TRIBpidgin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only -gnu pidgin-2.14.12 -C "--disable-gevolution --disable-meanwhile --disable-avahi --disable-nm --disable-cyrus-sasl"
-${THOME}/build/genpkg TRIBpidgin pidgin-2.14.12
+${THOME}/build/dobuild -64only -gnu pidgin-2.14.13 -C "--disable-gevolution --disable-meanwhile --disable-avahi --disable-nm --disable-cyrus-sasl"
+${THOME}/build/genpkg TRIBpidgin pidgin-2.14.13
