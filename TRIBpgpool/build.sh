4a57dd8e4aac0818dc7ce450cfa32d5224447329
Peter Tribble <peter.tribble@gmail.com>
2024-02-29 15:34:21 +0000

    Update pgpool 4.4.6

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -5,5 +5,5 @@
 # specify LD_LRUN_PATH so that pgproto also picks it up and can find
 # libpq at runtime
 #
-env LD_RUN_PATH=/opt/tribblix/postgres15/lib PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.5 -C "--sysconfdir=/etc --with-openssl"
-${THOME}/build/genpkg TRIBpgpool pgpool-II-4.4.5
+env LD_RUN_PATH=/opt/tribblix/postgres15/lib PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.6 -C "--sysconfdir=/etc --with-openssl"
+${THOME}/build/genpkg TRIBpgpool pgpool-II-4.4.6
