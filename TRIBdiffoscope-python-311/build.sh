ecefc5a1439d026d31bb89e11eb79b9ea7501b67
Peter Tribble <peter.tribble@gmail.com>
2024-03-01 18:50:27 +0000

    Update diffoscope 259

--- TRIBdiffoscope-python-311/build.sh
+++ TRIBdiffoscope-python-311/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
-# there are a lot of potential dependencies we could add
+# there are a lot of potential dependencies we could add, just
+# declare the ones we already have available
 #
 # [cmdline] argcomplete progressbar
 # also pypdf defusedxml
 #
-${THOME}/build/pkg_setup_py TRIBdiffoscope-python-311 diffoscope-258
+${THOME}/build/pkg_setup_py TRIBdiffoscope-python-311 diffoscope-259
