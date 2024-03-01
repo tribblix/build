16ccc6a40b0980503833e4dccc78b5c989a6475d
Peter Tribble <peter.tribble@gmail.com>
2024-03-01 12:20:42 +0000

    Update Xarchiver 0.5.4.23

--- TRIBxarchiver/build.sh
+++ TRIBxarchiver/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xarchiver-0.5.4
-${THOME}/build/genpkg TRIBxarchiver xarchiver-0.5.4
+${THOME}/build/dobuild -gnu -64only xarchiver-0.5.4.23
+${THOME}/build/genpkg TRIBxarchiver xarchiver-0.5.4.23
