780897761dfc19472b7c4e1f7dbf85e6ea3b28d1
Peter Tribble <peter.tribble@gmail.com>
2024-03-01 09:16:21 +0000

    Update python-dateutil 2.9.0

--- TRIBdateutil-python-311/build.sh
+++ TRIBdateutil-python-311/build.sh
@@ -2,12 +2,4 @@
 #
 # python-dateutil - needs six
 #
-${THOME}/build/unpack python-dateutil-2.8.2
-cd python-dateutil-2.8.2
-/usr/versions/python-3.11/bin/python3 setup.py build
-/usr/versions/python-3.11/bin/python3 ./updatezinfo.py
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBdateutil-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBdateutil-python-311 python-dateutil-2.9.0
