e0cf7da89a96a82f05e9af4b32edd2cdbfa0ec49
Peter Tribble <peter.tribble@gmail.com>
2024-03-02 10:07:11 +0000

    Update jed pre0.99.20-180

--- TRIBjed/build.sh
+++ TRIBjed/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only jed-pre0.99.20-180
+# specify JED_ROOT otherwise it will install stuff directly into /usr/jed
+#
+env JED_ROOT=/usr/share/jed ${THOME}/build/dobuild -64only jed-pre0.99.20-180
 cd jed-pre0.99.20-180-64bit
 gmake xjed
 cd ..
