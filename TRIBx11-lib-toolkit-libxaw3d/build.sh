ce6a170eb0bd3cdcf9525aa38de1315f1953631a
Peter Tribble <peter.tribble@gmail.com>
2024-03-03 20:40:34 +0000

    Update libXaw3d 1.6.6

--- TRIBx11-lib-toolkit-libxaw3d/build.sh
+++ TRIBx11-lib-toolkit-libxaw3d/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXaw3d-1.6.5
-${THOME}/build/genpkg TRIBx11-lib-toolkit-libxaw3d libXaw3d-1.6.5
+${THOME}/build/dobuild -64 libXaw3d-1.6.6
+${THOME}/build/genpkg TRIBx11-lib-toolkit-libxaw3d libXaw3d-1.6.6
