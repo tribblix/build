5ddc3ddf5fa6c9ef2585e2a2eb51dbd2b8502531
Peter Tribble <peter.tribble@gmail.com>
2024-03-03 20:28:37 +0000

    Update editres 1.0.9

--- TRIBx11-editres/build.sh
+++ TRIBx11-editres/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only editres-1.0.8
-${THOME}/build/genpkg TRIBx11-editres editres-1.0.8
+${THOME}/build/dobuild -64only editres-1.0.9
+${THOME}/build/genpkg TRIBx11-editres editres-1.0.9
