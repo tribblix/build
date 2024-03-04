fd7c32b81cbebd9acb586236f29a61ee25e7852d
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 19:04:10 +0000

    Update lcms 2.16

--- TRIBlib-lcms2/build.sh
+++ TRIBlib-lcms2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 lcms2-2.15
-${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.15
+${THOME}/build/dobuild -64 lcms2-2.16
+${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.16
