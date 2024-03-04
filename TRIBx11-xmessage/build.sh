c5150c7840a3281c19dd58ffd6ceb07cc54bdb07
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 07:48:26 +0000

    Update xmessage 1.0.7

--- TRIBx11-xmessage/build.sh
+++ TRIBx11-xmessage/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xmessage-1.0.6
-${THOME}/build/genpkg TRIBx11-xmessage xmessage-1.0.6
+${THOME}/build/dobuild -64only xmessage-1.0.7
+${THOME}/build/genpkg TRIBx11-xmessage xmessage-1.0.7
