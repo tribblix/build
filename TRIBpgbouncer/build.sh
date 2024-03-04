6ef1123b50a9c8cbd3aaf5511d0c1c2bd5bce4c1
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 15:47:07 +0000

    Update pgbouncer 1.22.1

--- TRIBpgbouncer/build.sh
+++ TRIBpgbouncer/build.sh
@@ -2,6 +2,6 @@
 #
 # there's a sacrificial static libevent built by the preconf script
 #
-env TRIBBLIX_PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig ${THOME}/build/dobuild -64only pgbouncer-1.22.0 -C --without-cares
-${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.22.0
+env TRIBBLIX_PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig ${THOME}/build/dobuild -64only pgbouncer-1.22.1 -C --without-cares
+${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.22.1
 rm -fr /tmp/mc
