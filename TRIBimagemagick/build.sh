ab6c632140f6d26162d33958005952549468dd0d
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 17:00:22 +0000

    Update ImageMagick 7.1.1-29

--- TRIBimagemagick/build.sh
+++ TRIBimagemagick/build.sh
@@ -9,5 +9,5 @@ zap install openjpeg libwebp
 # need to disable openmp, otherwise it pulls in libgomp which we
 # ship with the compiler rather than the runtime
 #
-${THOME}/build/dobuild -64 ImageMagick-7.1.1-21 -C "--disable-openmp ac_cv_have_linux_sendfile=no"
-${THOME}/build/genpkg TRIBimagemagick ImageMagick-7.1.1-21
+${THOME}/build/dobuild -64only ImageMagick-7.1.1-29 -C "--disable-openmp ac_cv_have_linux_sendfile=no"
+${THOME}/build/genpkg TRIBimagemagick ImageMagick-7.1.1-29
--- patches/magick-baseconfig.h
+++
@@ -1,5 +0,0 @@
-#if defined(_LP64) || defined (__amd64) || defined (__sparcv9)
-#include <MagickCore/magick-baseconfig-64.h>
-#else
-#include <MagickCore/magick-baseconfig-32.h>
-#endif
