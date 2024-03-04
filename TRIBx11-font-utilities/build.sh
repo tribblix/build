cb0df57735a821c10163a410bfd8fdb992639c04
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 09:03:53 +0000

    Update mkfontscale 1.2.3 font-util 1.4.1

--- TRIBx11-font-utilities/build.sh
+++ TRIBx11-font-utilities/build.sh
@@ -8,6 +8,6 @@
 # do not build font-util -64only, we need both the 32 and 64 bit pc file
 #
 ${THOME}/build/dobuild -64only bdftopcf-1.1
-${THOME}/build/dobuild -64only mkfontscale-1.2.2
-${THOME}/build/dobuild -64 font-util-1.4.0
-${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.2 font-util-1.4.0
+${THOME}/build/dobuild -64only mkfontscale-1.2.3
+${THOME}/build/dobuild -64 font-util-1.4.1
+${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.3 font-util-1.4.1
