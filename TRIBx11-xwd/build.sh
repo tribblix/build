021f4710ecb47900f139e1511fae28f65eccaa6c
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 07:36:01 +0000

    Update xpr 1.2.0

--- TRIBx11-xwd/build.sh
+++ TRIBx11-xwd/build.sh
@@ -2,5 +2,5 @@
 #
 ${THOME}/build/dobuild -64only xwd-1.0.9
 ${THOME}/build/dobuild -64only xwud-1.0.6
-${THOME}/build/dobuild -64only xpr-1.1.0
-${THOME}/build/genpkg TRIBx11-xwd xwd-1.0.9 xwud-1.0.6 xpr-1.1.0
+${THOME}/build/dobuild -64only xpr-1.2.0
+${THOME}/build/genpkg TRIBx11-xwd xwd-1.0.9 xwud-1.0.6 xpr-1.2.0
