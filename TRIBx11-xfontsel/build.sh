d2b1ac3973d188e7576d26598f82dedca4fd5dab
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 08:58:26 +0000

    Update xfontsel 1.1.1

--- TRIBx11-xfontsel/build.sh
+++ TRIBx11-xfontsel/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xfontsel-1.1.0
-${THOME}/build/genpkg TRIBx11-xfontsel xfontsel-1.1.0
+${THOME}/build/dobuild -64only xfontsel-1.1.1
+${THOME}/build/genpkg TRIBx11-xfontsel xfontsel-1.1.1
