e0334275064739396d0b80b04f18cb06f89c94a7
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 19:35:19 +0000

    Update libnice 0.1.21

--- TRIBlibnice/build.sh
+++ TRIBlibnice/build.sh
@@ -2,5 +2,5 @@
 #
 # libnice now has only new gstreamer plugins
 #
-$THOME/build/mesonbuild -64 libnice-0.1.19 -C "-Dintrospection=disabled -Dtests=disabled"
-${THOME}/build/genpkg TRIBlibnice libnice-0.1.19
+$THOME/build/mesonbuild -64 libnice-0.1.21 -C "-Dintrospection=disabled -Dtests=disabled"
+${THOME}/build/genpkg TRIBlibnice libnice-0.1.21
