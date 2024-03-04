2ed9ac939cb465c1190e391cf9390b7495ec041f
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 18:36:44 +0000

    Rebuild webalizer 64-bit

--- TRIBwebalizer/build.sh
+++ TRIBwebalizer/build.sh
@@ -5,5 +5,5 @@
 # need to rename the downloaded tarball
 #  mv webalizer-2.23-08-src.tar.bz2 webalizer-2.23-08.tar.bz2
 #
-$THOME/build/dobuild webalizer-2.23-08
+$THOME/build/dobuild -64only webalizer-2.23-08
 ${THOME}/build/genpkg TRIBwebalizer webalizer-2.23-08
