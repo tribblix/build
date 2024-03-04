8231658ce3b621ab17bd44b18aa757dfec44a981
Peter Tribble <peter.tribble@gmail.com>
2024-03-04 11:05:35 +0000

    Extra note for building timg on sparc

--- TRIBtimg/build.sh
+++ TRIBtimg/build.sh
@@ -3,6 +3,8 @@
 # enabling GraphicsMagick leads to a SEGV with libjpeg-turbo 2 or later
 # our librsvg is too old
 #
+# sparc needs -DWITH_POPPLER=OFF as gcc7 is too old
+#
 # to see the rather spiffy sixel output, use 'mlterm or 'xterm -ti vt340'
 #
 ${THOME}/build/cmbuild -64only timg-1.6.0 -C "-DWITH_RSVG=OFF -DWITH_GRAPHICSMAGICK=OFF"
