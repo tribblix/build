60554c590071391554f27f08691252e0007b8a6c
Peter Tribble <peter.tribble@gmail.com>
2024-03-05 12:45:00 +0000

    Update SQLAlchemy 2.0.28

--- TRIBsqlalchemy-python-311/build.sh
+++ TRIBsqlalchemy-python-311/build.sh
@@ -4,4 +4,4 @@
 # there's an architecture switch that doesn't work the way they
 # expect on solaris
 #
-${THOME}/build/pkg_pep518 TRIBsqlalchemy-python-311 SQLAlchemy-2.0.27
+${THOME}/build/pkg_pep518 TRIBsqlalchemy-python-311 SQLAlchemy-2.0.28
