b24cca333fca009e2232ed9aa8c02f6ba609ab99
Peter Tribble <peter.tribble@gmail.com>
2024-03-06 18:40:58 +0000

    Update libXext 1.3.6

--- TRIBx11-libxext/build.sh
+++ TRIBx11-libxext/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXext-1.3.5 -C CPPFLAGS=-DSUNSOFT
-${THOME}/build/genpkg TRIBx11-libxext libXext-1.3.5
+${THOME}/build/dobuild -64 libXext-1.3.6 -C CPPFLAGS=-DSUNSOFT
+${THOME}/build/genpkg TRIBx11-libxext libXext-1.3.6
