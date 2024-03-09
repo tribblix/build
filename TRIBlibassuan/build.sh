77036e9fa876a1bd2a9e298dee3b00fdaa79f072
Peter Tribble <peter.tribble@gmail.com>
2024-03-09 19:02:27 +0000

    Update libassuan 2.5.7

--- TRIBlibassuan/build.sh
+++ TRIBlibassuan/build.sh
@@ -2,5 +2,5 @@
 #
 # libassuan
 #
-${THOME}/build/dobuild +64 libassuan-2.5.6
-${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.6
+${THOME}/build/dobuild +64 libassuan-2.5.7
+${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.7
