df3fa1a9340b1ccaea470a1ac78b32269194152c
Peter Tribble <peter.tribble@gmail.com>
2024-03-10 09:52:50 +0000

    Update google-auth 2.28.2

--- TRIBgoogle-auth-python-311/build.sh
+++ TRIBgoogle-auth-python-311/build.sh
@@ -3,4 +3,4 @@
 # 2.23.0 needed urllib3 <2, 2.23.1 needed urllib3>=2
 # current have urllib3 as optional but support all versions
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-311 google-auth-2.28.1
+${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-311 google-auth-2.28.2
