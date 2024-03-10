010cca7efad84fd9874d46d394e6de95c6a2f8c2
Peter Tribble <peter.tribble@gmail.com>
2024-03-10 13:35:36 +0000

    Update rclone 1.66.0

--- TRIBrclone/build.sh
+++ TRIBrclone/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-$THOME/build/unpack rclone-v1.65.2
-cd rclone-v1.65.2
+$THOME/build/unpack rclone-v1.66.0
+cd rclone-v1.66.0
 env PATH=/usr/gnu/bin:/usr/versions/go-1.21/bin:$PATH gmake
 
 rm -fr /tmp/cs
