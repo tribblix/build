2cb0364feaf366152f458d4a6591165f9becec06
Peter Tribble <peter.tribble@gmail.com>
2024-03-11 10:05:01 +0000

    Update iceauth 1.0.10

--- TRIBx11-iceauth/build.sh
+++ TRIBx11-iceauth/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only iceauth-1.0.9
-${THOME}/build/genpkg TRIBx11-iceauth iceauth-1.0.9
+${THOME}/build/dobuild -64only iceauth-1.0.10
+${THOME}/build/genpkg TRIBx11-iceauth iceauth-1.0.10
