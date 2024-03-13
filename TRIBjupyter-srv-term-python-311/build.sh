95f99062eda3218120871fd42965bc343cc17c13
Peter Tribble <peter.tribble@gmail.com>
2024-03-13 10:08:30 +0000

    Update jupyter_server_terminals 0.5.3

--- TRIBjupyter-srv-term-python-311/build.sh
+++ TRIBjupyter-srv-term-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBjupyter-srv-term-python-311 jupyter_server_terminals-0.5.2
+${THOME}/build/pkg_pep518 TRIBjupyter-srv-term-python-311 jupyter_server_terminals-0.5.3
