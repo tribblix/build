4d8067248b36faa2104df9bbbcf7c53ec1d01f9c
Peter Tribble <peter.tribble@gmail.com>
2024-03-13 08:10:59 +0000

    Update googleapis-common-protos 1.63.0

--- TRIBgapi-common-python-311/build.sh
+++ TRIBgapi-common-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgapi-common-python-311 googleapis-common-protos-1.62.0
+${THOME}/build/pkg_setup_py TRIBgapi-common-python-311 googleapis-common-protos-1.63.0
