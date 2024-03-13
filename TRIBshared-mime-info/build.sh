c8b6429c65bc2feae3b6a9e558374733fe803ce5
Peter Tribble <peter.tribble@gmail.com>
2024-03-13 14:43:45 +0000

    Update shared-mime-info 2.4

--- TRIBshared-mime-info/build.sh
+++ TRIBshared-mime-info/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu shared-mime-info-1.6
-${THOME}/build/genpkg TRIBshared-mime-info shared-mime-info-1.6
+# the files installed into /usr/share/mime/application should be group
+# root, as enforced by ../fixproto, otherwise the mime-cache service
+# will change the group
+#
+${THOME}/build/mesonbuild -gnu -64only shared-mime-info-2.4 -C -Dupdate-mimedb=true
+${THOME}/build/genpkg TRIBshared-mime-info shared-mime-info-2.4
