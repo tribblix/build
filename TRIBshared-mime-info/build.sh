ea9d504912d749979eca6cb84d95eade0de1ee34
Peter Tribble <peter.tribble@gmail.com>
2024-03-14 14:05:46 +0000

    Hold shared-mimeinfo on sparc as newer version requires newer gcc

--- TRIBshared-mime-info/build.sh
+++ TRIBshared-mime-info/build.sh
@@ -4,5 +4,7 @@
 # root, as enforced by ../fixproto, otherwise the mime-cache service
 # will change the group
 #
+# sparc downrev as current build requires later gcc features
+#
 ${THOME}/build/mesonbuild -gnu -64only shared-mime-info-2.4 -C -Dupdate-mimedb=true
 ${THOME}/build/genpkg TRIBshared-mime-info shared-mime-info-2.4
