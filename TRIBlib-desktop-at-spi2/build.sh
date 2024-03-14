241ab266cc72848e7a08e3e27a3940bc24b7d594
Peter Tribble <peter.tribble@gmail.com>
2024-03-14 22:03:54 +0000

    Fix dbus for at-spi2-core

--- TRIBlib-desktop-at-spi2/build.sh
+++ TRIBlib-desktop-at-spi2/build.sh
@@ -2,5 +2,5 @@
 #
 # newer versions need newer glib2
 #
-${THOME}/build/mesonbuild -64 -gnu at-spi2-core-2.40.3 -C "-Dintrospection=no -Ddbus_daemon=/usr/lib"
+${THOME}/build/mesonbuild -64 -gnu at-spi2-core-2.40.3 -C "-Dintrospection=no -Ddbus_daemon=/usr/lib/dbus-daemon"
 ${THOME}/build/genpkg TRIBlib-desktop-at-spi2 at-spi2-core-2.40.3
