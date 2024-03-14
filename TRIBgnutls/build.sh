433d87e64a3a219315dfc15aaf037d3e026669db
Peter Tribble <peter.tribble@gmail.com>
2024-03-14 17:39:37 +0000

    Drop old gnutls compat

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -5,7 +5,5 @@
 # disable tpm because the code isn't even self-consistent and autoconf
 # can't work out what to do on its own
 #
-# ship a copy of the previous shared library for compat
-#
 ${THOME}/build/dobuild -64 gnutls-3.6.16 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs"
 ${THOME}/build/genpkg TRIBgnutls gnutls-3.6.16
