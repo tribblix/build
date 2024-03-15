568590de1ee95f0a7cd14fb5016436a3ee58ff58
Peter Tribble <peter.tribble@gmail.com>
2024-03-15 18:34:56 +0000

    Update libpaper 2.2.5

--- TRIBlibpaper/build.sh
+++ TRIBlibpaper/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only libpaper-2.1.3
-${THOME}/build/genpkg TRIBlibpaper libpaper-2.1.3
+${THOME}/build/dobuild -64only libpaper-2.2.5
+${THOME}/build/genpkg TRIBlibpaper libpaper-2.2.5
