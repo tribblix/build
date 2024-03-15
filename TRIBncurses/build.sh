6270dc3e5f6dd7433efd20ae7ffed1d2bd5b68d7
Peter Tribble <peter.tribble@gmail.com>
2024-03-15 08:56:40 +0000

    Drop ncurses5

--- TRIBncurses/build.sh
+++ TRIBncurses/build.sh
@@ -4,18 +4,7 @@
 #
 rm -fr /tmp/nc
 #
-# first make and install the old abi-compatible version
-#
-env LD_RUN_PATH=/usr/gnu/lib/`isainfo -k` ${THOME}/build/dobuild +64 ncurses-6.4 -P /usr/gnu -C "--with-shared --with-abi-version=5"
-cd ncurses-6.4-64bit
-env LD_RUN_PATH=/usr/gnu/lib/`isainfo -k` gmake install DESTDIR=/tmp/nc
-cd ..
-env LD_RUN_PATH=/usr/gnu/lib ${THOME}/build/dobuild ncurses-6.4 -P /usr/gnu -C "--with-shared --with-abi-version=5"
-cd ncurses-6.4
-env LD_RUN_PATH=/usr/gnu/lib gmake install DESTDIR=/tmp/nc
-cd ..
-#
-# now the new version
+# just the new version
 #
 env LD_RUN_PATH=/usr/gnu/lib/`isainfo -k` ${THOME}/build/dobuild +64 ncurses-6.4 -P /usr/gnu -C --with-shared
 cd ncurses-6.4-64bit
