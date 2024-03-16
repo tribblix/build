3e43cfb8f22ccf7956a43ee56b2d158221fd9bf8
Peter Tribble <peter.tribble@gmail.com>
2024-03-16 09:37:03 +0000

    Add oci cli and modules

---
+++ TRIBoci-python-311/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# bundle all the bits together
+# it's a massive package, ~50k files, 560M uncompressed, 119M zap
+#
+${THOME}/build/pkg_setup_py TRIBoci-python-311 circuitbreaker-1.4.0 terminaltables-3.1.10 oci-2.124.1 oci-cli-3.37.13
