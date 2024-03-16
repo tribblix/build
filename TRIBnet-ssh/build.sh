65dd8c8b75671cb06a67992f0902dd7517dea93a
Peter Tribble <peter.tribble@gmail.com>
2024-03-16 17:25:58 +0000

    Update openssh 9.7p1

--- TRIBnet-ssh/build.sh
+++ TRIBnet-ssh/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only openssh-9.6p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
+${THOME}/build/dobuild -64only openssh-9.7p1 -C "--sysconfdir=/etc/ssh --libexecdir=/usr/lib/ssh --with-solaris-contracts --with-solaris-projects --with-ssl-engine --with-pam"
 #
 # split into client and server packages
 #
-${THOME}/build/genpkg TRIBnet-ssh openssh-9.6p1
-${THOME}/build/genpkg TRIBsvc-net-ssh openssh-9.6p1
+${THOME}/build/genpkg TRIBnet-ssh openssh-9.7p1
+${THOME}/build/genpkg TRIBsvc-net-ssh openssh-9.7p1
