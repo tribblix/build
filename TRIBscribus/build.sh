32d3b1c8640c0edf2d54f34fd10c4095c9a97662
Peter Tribble <peter.tribble@gmail.com>
2024-03-16 20:36:42 +0000

    Update scribus 1.6.1

--- TRIBscribus/build.sh
+++ TRIBscribus/build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 #
-# later versions require a poppler upgrade
-# unfortunately scripting support is stuck at python2
-# gcc7 miscompiles something so it won't even start
-#
-# manual fixes:
-# scribus/ui/scresizecursor.cpp include QPainterPath as well as QPainter
-# comment swab() in stdlib.h to avoid duplicate definition conflict
-#
-env PATH=/usr/versions/Qt-5/bin:$PATH $THOME/build/cmbuild scribus-1.5.4 -C "-DPYTHON_LIBRARY=/usr/versions/python-2.7/lib/libpython2.7.so -DPYTHON_INCLUDE_DIR=/usr/versions/python-2.7/include/python2.7"
-${THOME}/build/genpkg TRIBscribus scribus-1.5.4
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only scribus-1.6.1 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DWANT_CPP17=ON"
+
+${THOME}/build/genpkg TRIBscribus scribus-1.6.1
