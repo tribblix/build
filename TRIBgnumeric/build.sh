b01cff48224da370a80cf5a85a3ebe4d7db3f75b
Peter Tribble <peter.tribble@gmail.com>
2024-03-16 08:53:37 +0000

    Update goffice 0.10.57 gnumeric 1.12.57

--- TRIBgnumeric/build.sh
+++ TRIBgnumeric/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu gnumeric-1.12.56
-${THOME}/build/genpkg TRIBgnumeric gnumeric-1.12.56
+env PATH=/usr/gnu/bin:$PATH ${THOME}/build/dobuild -64only -gnu gnumeric-1.12.57
+${THOME}/build/genpkg TRIBgnumeric gnumeric-1.12.57
--- TRIBgoffice/build.sh
+++ TRIBgoffice/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env CPPFLAGS=-I/usr/include/pcre ${THOME}/build/dobuild +64 -gnu goffice-0.10.56
-${THOME}/build/genpkg TRIBgoffice goffice-0.10.56
+${THOME}/build/dobuild +64 -gnu goffice-0.10.57
+${THOME}/build/genpkg TRIBgoffice goffice-0.10.57
