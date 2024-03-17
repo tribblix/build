c322da7463953b021d15fdf9232d3a2d9e8c45b7
Peter Tribble <peter.tribble@gmail.com>
2024-03-17 09:39:18 +0000

    Update vips 8.15.2

--- TRIBlibvips/build.sh
+++ TRIBlibvips/build.sh
@@ -2,5 +2,5 @@
 #
 # only current consumers are go-based, so restrict to x86 and 64-bit only
 #
-env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64only vips-8.14.2 -C "-Dintrospection=false -Dimagequant=disabled"
-${THOME}/build/genpkg TRIBlibvips vips-8.14.2
+env AR=/usr/bin/ar PATH=/usr/gnu/bin:$PATH ${THOME}/build/mesonbuild -64only vips-8.15.2 -C "-Dintrospection=disabled -Dimagequant=disabled"
+${THOME}/build/genpkg TRIBlibvips vips-8.15.2
