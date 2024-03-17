0eda0b7c71112f80992e0c9ecbf1f9c194a25621
Peter Tribble <peter.tribble@gmail.com>
2024-03-17 19:24:52 +0000

    Update libpeas 1.36.0, and drop 32-bit

--- TRIBlibpeas/build.sh
+++ TRIBlibpeas/build.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 #
-env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC=gcc $THOME/build/mesonbuild libpeas-1.26.0 -C -Ddemos=false
-mv libpeas-1.26.0 libpeas-1.26.0-32bit
-env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC="gcc -m64" $THOME/build/mesonbuild +64 libpeas-1.26.0 -C -Ddemos=false
-mv libpeas-1.26.0-32bit libpeas-1.26.0
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC="gcc -m64" $THOME/build/mesonbuild +64 libpeas-1.36.0 -C -Ddemos=false
 
 rm -fr /tmp/lps
 
-cd libpeas-1.26.0
-env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC=gcc DESTDIR=/tmp/lps ninja -C build install
-cd ..
-cd libpeas-1.26.0-64bit
-env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC="gcc -m64" DESTDIR=/tmp/lps ninja -C build install
+cd libpeas-1.36.0-64bit
+env PATH=/usr/gnu/bin:$PATH AR=/usr/bin/ar CC="gcc -m64" DESTDIR=/tmp/lps ninja -C tribblix_build install
 cd ..
 
 ${THOME}/build/create_pkg TRIBlibpeas /tmp/lps
