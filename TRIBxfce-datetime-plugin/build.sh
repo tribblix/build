135dd98023c92728986d0fdb63a0463504bf2215
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 17:57:27 +0000

    Enable 64-bit xfce datetime plugin

--- TRIBxfce-datetime-plugin/build.sh
+++ TRIBxfce-datetime-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-datetime-plugin-0.8.3
+${THOME}/build/dobuild -64 -gnu xfce4-datetime-plugin-0.8.3
 ${THOME}/build/genpkg TRIBxfce-datetime-plugin xfce4-datetime-plugin-0.8.3
