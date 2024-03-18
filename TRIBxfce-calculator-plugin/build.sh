b8d7e33c74f6bf981b3f90eb4037f971b776dee8
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 17:03:19 +0000

    Enable 64-bit xfce calculator plugin

--- TRIBxfce-calculator-plugin/build.sh
+++ TRIBxfce-calculator-plugin/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild -gnu xfce4-calculator-plugin-0.7.2
+${THOME}/build/dobuild -64 -gnu xfce4-calculator-plugin-0.7.2
 ${THOME}/build/genpkg TRIBxfce-calculator-plugin xfce4-calculator-plugin-0.7.2
