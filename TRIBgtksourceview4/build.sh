7a372299791410e21e272fe773722a907b7c5fc2
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 13:11:48 +0000

    Don't disable gir in gtksourceview-4

--- TRIBgtksourceview4/build.sh
+++ TRIBgtksourceview4/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS=-lssp_ns AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only -gnu gtksourceview-4.8.4 -C -Dgir=false
+env CC="gcc -m64" TRIBBLIX_LDFLAGS=-lssp_ns AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only -gnu gtksourceview-4.8.4
 ${THOME}/build/genpkg TRIBgtksourceview4 gtksourceview-4.8.4
