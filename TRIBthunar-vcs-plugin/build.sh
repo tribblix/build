e3b58bf7e6059618abbd03b2d3cb95082607a0ce
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 18:06:15 +0000

    Enable 64-bit thunar-vcs-plugin

--- TRIBthunar-vcs-plugin/build.sh
+++ TRIBthunar-vcs-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu thunar-vcs-plugin-0.2.0
+${THOME}/build/dobuild -64 -gnu thunar-vcs-plugin-0.2.0
 ${THOME}/build/genpkg TRIBthunar-vcs-plugin thunar-vcs-plugin-0.2.0
