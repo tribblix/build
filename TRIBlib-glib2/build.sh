613a50e4dc3d930dcdeb62e216adf7112c13f19d
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 13:23:51 +0000

    Update glib2 2.64.6

--- TRIBlib-glib2/build.sh
+++ TRIBlib-glib2/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #
+# later versions appear to break Xfce with /usr/libexec/at-spi-bus-launcher
+# reporting "Failed to launch bus: Failed to connect to session bus"
+#
 
-env TRIBBLIX_LDFLAGS="-lsocket" TRIBBLIX_CFLAGS="-Wno-error=format -Wno-error=format-nonliteral -D_POSIX_PTHREAD_SEMANTICS" PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild -64 glib-2.62.6 -C "-Dxattr=false -Dman=true -Ddtrace=false"
+env TRIBBLIX_LDFLAGS="-lsocket" TRIBBLIX_CFLAGS="-Wno-error=format -Wno-error=format-nonliteral -D_POSIX_PTHREAD_SEMANTICS" PATH=/usr/gnu/bin:$PATH $THOME/build/mesonbuild -64 glib-2.64.6 -C "-Dxattr=false -Dman=true -Ddtrace=false"
 
-$THOME/build/genpkg TRIBlib-glib2 glib-2.62.6
+$THOME/build/genpkg TRIBlib-glib2 glib-2.64.6
