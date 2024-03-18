bd6570fe1fdf94b15878819f40afec9d62c0e98f
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 17:46:42 +0000

    Enable 64-bit xfce places plugin

--- TRIBxfce-places-plugin/build.sh
+++ TRIBxfce-places-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-places-plugin-1.8.3
+${THOME}/build/dobuild -64 -gnu xfce4-places-plugin-1.8.3
 ${THOME}/build/genpkg TRIBxfce-places-plugin xfce4-places-plugin-1.8.3
