1eb6cb1e5d11872447cf7755280f806d960e3903
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 17:05:36 +0000

    Enable 64-bit xfce clipman plugin

--- TRIBxfce-clipman-plugin/build.sh
+++ TRIBxfce-clipman-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu xfce4-clipman-plugin-1.6.6
+${THOME}/build/dobuild -64 -gnu xfce4-clipman-plugin-1.6.6
 ${THOME}/build/genpkg TRIBxfce-clipman-plugin xfce4-clipman-plugin-1.6.6
