7c2c591d16d0d2296b23ee4cb44a6a5aff393463
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 13:04:41 +0000

    Update hunspell 1.7.2, with 1.3.4 compat

--- TRIBhunspell/build.sh
+++ TRIBhunspell/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 hunspell-1.3.4 -C "--disable-static"
-${THOME}/build/genpkg TRIBhunspell hunspell-1.3.4
+env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 hunspell-1.7.2 -C "--disable-static"
+${THOME}/build/genpkg TRIBhunspell hunspell-1.7.2
