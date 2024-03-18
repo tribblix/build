34a251d4bff323887deae2a8b2a3a21de604305c
Peter Tribble <peter.tribble@gmail.com>
2024-03-18 16:43:22 +0000

    Enable 64-bit xfce diskperf plugin

--- TRIBxfce-diskperf-plugin/build.sh
+++ TRIBxfce-diskperf-plugin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS=-lkstat ${THOME}/build/dobuild -gnu xfce4-diskperf-plugin-2.6.3
+env TRIBBLIX_LDFLAGS=-lkstat ${THOME}/build/dobuild -64 -gnu xfce4-diskperf-plugin-2.6.3
 ${THOME}/build/genpkg TRIBxfce-diskperf-plugin xfce4-diskperf-plugin-2.6.3
