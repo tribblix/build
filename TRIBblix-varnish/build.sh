523a59c2cfed098a610b4f5119beb66c7d1aaab3
Peter Tribble <peter.tribble@gmail.com>
2024-03-19 21:02:38 +0000

    Update varnish 7.4.3

--- TRIBblix-varnish/build.sh
+++ TRIBblix-varnish/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 
-${THOME}/build/dobuild -64only varnish-7.4.2 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-3.11/bin/rst2man.py --with-rst2html=/usr/versions/python-3.11/bin/rst2html.py --with-sphinx-build=/usr/versions/python-3.11/bin/sphinx-build --disable-dependency-tracking CC=gcc"
-${THOME}/build/genpkg TRIBblix-varnish varnish-7.4.2
+${THOME}/build/dobuild -64only varnish-7.4.3 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-3.11/bin/rst2man.py --with-rst2html=/usr/versions/python-3.11/bin/rst2html.py --with-sphinx-build=/usr/versions/python-3.11/bin/sphinx-build --disable-dependency-tracking CC=gcc"
+${THOME}/build/genpkg TRIBblix-varnish varnish-7.4.3
