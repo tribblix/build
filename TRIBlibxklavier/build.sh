0d764295730326e7fcd0a40c541acd1d3a8ba6d0
Peter Tribble <peter.tribble@gmail.com>
2024-03-20 18:40:15 +0000

    Drop 32-bit libxklavier

--- TRIBlibxklavier/build.sh
+++ TRIBlibxklavier/build.sh
@@ -2,5 +2,5 @@
 #
 # needs iso-codes
 #
-${THOME}/build/dobuild -64 libxklavier-5.4 -C --disable-introspection
+${THOME}/build/dobuild -64only libxklavier-5.4 -C --disable-introspection
 ${THOME}/build/genpkg TRIBlibxklavier libxklavier-5.4
