2c1b2b64d77a8a24bf84b82c8259d955510f8763
Peter Tribble <peter.tribble@gmail.com>
2024-03-20 18:35:21 +0000

    Update libwnck 3.30.0, drop 32-bit

--- TRIBlibwnck3/build.sh
+++ TRIBlibwnck3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu libwnck-3.24.1 -C "--disable-introspection --program-suffix=3"
-${THOME}/build/genpkg TRIBlibwnck3 libwnck-3.24.1
+${THOME}/build/dobuild -64only -gnu libwnck-3.30.0 -C "--disable-introspection --program-suffix=3"
+${THOME}/build/genpkg TRIBlibwnck3 libwnck-3.30.0
