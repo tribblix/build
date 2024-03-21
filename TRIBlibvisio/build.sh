11387b9dcd50bc3e52b029e16d801d762785a119
Peter Tribble <peter.tribble@gmail.com>
2024-03-21 19:31:54 +0000

    Update libvisio 0.1.7, and drop 32-bit

--- TRIBlibvisio/build.sh
+++ TRIBlibvisio/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-pthreads TRIBBLIX_CXXFLAGS=-pthreads ${THOME}/build/dobuild -64 libvisio-0.1.1 -C LIBS=-lboost_system
-${THOME}/build/genpkg TRIBlibvisio libvisio-0.1.1
+env TRIBBLIX_CFLAGS=-pthreads TRIBBLIX_CXXFLAGS=-pthreads ${THOME}/build/dobuild -64only libvisio-0.1.7
+${THOME}/build/genpkg TRIBlibvisio libvisio-0.1.7
