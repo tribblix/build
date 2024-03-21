3870c9471ccc95bf7cf1844e3aa9d893ceba5a70
Peter Tribble <peter.tribble@gmail.com>
2024-03-21 17:48:29 +0000

    Drop 32-bit libcanberra

--- TRIBlibcanberra/build.sh
+++ TRIBlibcanberra/build.sh
@@ -2,5 +2,5 @@
 #
 # only used by mate, so restrict to gtk3
 #
-${THOME}/build/dobuild -64 libcanberra-0.30 -C "--disable-gtk --enable-gtk3"
+${THOME}/build/dobuild -64only libcanberra-0.30 -C "--disable-gtk --enable-gtk3"
 ${THOME}/build/genpkg TRIBlibcanberra libcanberra-0.30
