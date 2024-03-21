9482b68af8cb3a96a990a5be1c80dc9f4c01ce1e
Peter Tribble <peter.tribble@gmail.com>
2024-03-21 19:18:38 +0000

    Drop 32-bit libzmf

--- TRIBlibzmf/build.sh
+++ TRIBlibzmf/build.sh
@@ -2,5 +2,5 @@
 #
 # wget https://dev-www.libreoffice.org/src/libzmf/libzmf-0.0.2.tar.xz
 #
-${THOME}/build/dobuild -64 libzmf-0.0.2 -C --disable-werror
+${THOME}/build/dobuild -64only libzmf-0.0.2 -C --disable-werror
 ${THOME}/build/genpkg TRIBlibzmf libzmf-0.0.2
