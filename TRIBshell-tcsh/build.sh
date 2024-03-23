b5c41694fc68eb7195135deba6e6604b148d5b19
Peter Tribble <peter.tribble@gmail.com>
2024-03-23 10:17:47 +0000

    Update tcsh 6.24.11

--- TRIBshell-tcsh/build.sh
+++ TRIBshell-tcsh/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only tcsh-6.24.10
-${THOME}/build/genpkg TRIBshell-tcsh tcsh-6.24.10
+${THOME}/build/dobuild -64only tcsh-6.24.11
+${THOME}/build/genpkg TRIBshell-tcsh tcsh-6.24.11
