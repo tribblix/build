cbdbc1af7ec6b453f9d1c799981a99ae63231df8
Peter Tribble <peter.tribble@gmail.com>
2024-03-23 17:14:52 +0000

    Rebuild coolreader3 64-bit

--- TRIBcoolreader3/build.sh
+++ TRIBcoolreader3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild +B -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE" coolreader-cr3.2.58
+env PATH=/usr/versions/Qt-5/bin`${THOME}/build/getarch`:$PATH ${THOME}/build/cmbuild +B -64only -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE" coolreader-cr3.2.58
 ${THOME}/build/genpkg TRIBcoolreader3 coolreader-cr3.2.58
