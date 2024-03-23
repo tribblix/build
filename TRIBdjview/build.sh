a0172d53179281a300855628eacbb223356977c0
Peter Tribble <peter.tribble@gmail.com>
2024-03-23 17:21:14 +0000

    Rebuild djview 64-bit

--- TRIBdjview/build.sh
+++ TRIBdjview/build.sh
@@ -2,5 +2,5 @@
 #
 # the tarball needs a rename from djview-4.12.tar.gz to djview4-4.12.tar.gz
 #
-env PATH=/usr/gnu/bin:/usr/versions/Qt-5/bin:$PATH ${THOME}/build/dobuild djview4-4.12
-env PATH=/usr/gnu/bin:/usr/versions/Qt-5/bin:$PATH ${THOME}/build/genpkg TRIBdjview djview4-4.12
+env PATH=/usr/gnu/bin:/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH ${THOME}/build/dobuild -64only djview4-4.12
+env PATH=/usr/gnu/bin:/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH ${THOME}/build/genpkg TRIBdjview djview4-4.12
