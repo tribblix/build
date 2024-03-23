9ba1baca2511fb0cc2df192c1419ae58d9df46ba
Peter Tribble <peter.tribble@gmail.com>
2024-03-23 18:00:13 +0000

    Rebuild smplayer 64-bit

--- TRIBsmplayer/build.sh
+++ TRIBsmplayer/build.sh
@@ -8,11 +8,11 @@ $THOME/build/unpack smplayer-skins-20.11.0
 $THOME/build/unpack smplayer-themes-20.11.0
 cd smplayer-skins-20.11.0
 sed -i 's:install :ginstall :' Makefile
-env PATH=/usr/versions/Qt-5/bin:$PATH gmake PREFIX=/usr CC=gcc
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH gmake PREFIX=/usr CC=gcc
 cd ..
 cd smplayer-themes-20.11.0
 sed -i 's:install :ginstall :' Makefile
-env PATH=/usr/versions/Qt-5/bin:$PATH gmake PREFIX=/usr CC=gcc
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH gmake PREFIX=/usr CC=gcc
 cd ..
 cd smplayer-22.2.0
 sed -i 's: make: gmake:' Makefile
@@ -21,6 +21,6 @@ sed -i 's:install :ginstall :' install_appdata.sh
 sed -i 's:tar :gtar :g' Makefile
 sed -i 's:-pthread:-lnsl -lsocket:' webserver/Makefile
 sed -i 's:Another OS:Tribblix:' src/smplayer.cpp
-env PATH=/usr/versions/Qt-5/bin:$PATH gmake -j 6 PREFIX=/usr CC=gcc
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH gmake -j 6 PREFIX=/usr CC=gcc
 cd ..
 ${THOME}/build/genpkg -M PREFIX=/usr TRIBsmplayer smplayer-22.2.0 smplayer-skins-20.11.0 smplayer-themes-20.11.0
