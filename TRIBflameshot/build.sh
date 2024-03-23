53a1d5a6bc53aae7f133ab950cf9c441573e3a22
Peter Tribble <peter.tribble@gmail.com>
2024-03-23 17:10:47 +0000

    Rebuild flameshot 64-bit

--- TRIBflameshot/build.sh
+++ TRIBflameshot/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 #
-env PATH=/usr/versions/Qt-5/bin:$PATH $THOME/build/cmbuild flameshot-12.1.0 -C -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE
+env PATH=/usr/versions/Qt-5/bin`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only flameshot-12.1.0 -C "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake"
 ${THOME}/build/genpkg TRIBflameshot flameshot-12.1.0
