29ac519863cbaf725706e05cbab30d093451cbef
Peter Tribble <peter.tribble@gmail.com>
2024-03-25 07:58:15 +0000

    Update automake 1.16.5

--- TRIBautomake/build.sh
+++ TRIBautomake/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 ${THOME}/build/dobuild -gnu automake-1.15
-${THOME}/build/dobuild -gnu automake-1.16.1
-${THOME}/build/genpkg TRIBautomake automake-1.15 automake-1.16.1
+${THOME}/build/dobuild -gnu automake-1.16.5
+${THOME}/build/genpkg TRIBautomake automake-1.15 automake-1.16.5
