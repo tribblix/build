2b13409bea194df1be188ae316da1f5e74ead4d5
Peter Tribble <peter.tribble@gmail.com>
2024-03-25 11:33:02 +0000

    Update emacs29 29.3

--- TRIBv-emacs-29/build.sh
+++ TRIBv-emacs-29/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-29.2 -P /usr/versions/emacs-29 -C "--with-xft --with-xpm --without-sqlite3"
-${THOME}/build/genpkg TRIBv-emacs-29 emacs-29.2
+env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-29.3 -P /usr/versions/emacs-29 -C "--with-xft --with-xpm --without-sqlite3"
+${THOME}/build/genpkg TRIBv-emacs-29 emacs-29.3
