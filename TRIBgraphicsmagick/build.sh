beff0b97a8556d148c6a7301e3acec1bbb8dbb9b
Peter Tribble <peter.tribble@gmail.com>
2024-03-25 11:39:50 +0000

    Update GraphicsMagick 1.3.43

--- TRIBgraphicsmagick/build.sh
+++ TRIBgraphicsmagick/build.sh
@@ -2,5 +2,5 @@
 #
 zap uninstall TRIBgraphviz
 #
-${THOME}/build/dobuild -64only GraphicsMagick-1.3.42 -C "--enable-shared --disable-static --disable-openmp"
-${THOME}/build/genpkg TRIBgraphicsmagick GraphicsMagick-1.3.42
+${THOME}/build/dobuild -64only GraphicsMagick-1.3.43 -C "--enable-shared --disable-static --disable-openmp"
+${THOME}/build/genpkg TRIBgraphicsmagick GraphicsMagick-1.3.43
