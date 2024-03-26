a3c4f2799270c7169f2ef194444a9d64682b581f
Peter Tribble <peter.tribble@gmail.com>
2024-03-26 13:44:24 +0000

    Add libpsl 0.21.5

---
+++ TRIBlibpsl/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# It will pick up libicu as the runtime by default, but we want to avoid
+# such a large package being pulled in by curl
+# The alternative is libidn{,2} but that pulls in libunistring and again
+# the dependency tree starts to expand
+# Which is why we --disable-runtime
+#
+${THOME}/build/dobuild -64 libpsl-0.21.5 -C "--disable-static --disable-dependency-tracking --enable-builtin --disable-runtime"
+${THOME}/build/genpkg TRIBlibpsl libpsl-0.21.5
