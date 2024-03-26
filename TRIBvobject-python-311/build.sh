dfbe1ea2d4a35c6890842474af964bae720c61bb
Peter Tribble <peter.tribble@gmail.com>
2024-03-26 13:51:24 +0000

    Update vobject 0.9.7

--- TRIBvobject-python-311/build.sh
+++ TRIBvobject-python-311/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/unpack vobject-0.9.6.1
-cd vobject-0.9.6.1
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBvobject-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+${THOME}/build/pkg_setup_py TRIBvobject-python-311 vobject-0.9.7
