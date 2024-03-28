0276b1c294027001317193688717de858795c05a
Peter Tribble <peter.tribble@gmail.com>
2024-03-28 14:08:33 +0000

    Update rabbitmq 3.13.0

--- TRIBblix-rabbitmq/build.sh
+++ TRIBblix-rabbitmq/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-# 3.12.4 needs otp 25 or 26
+# 3.13.0 needs otp 26
 #
-${THOME}/build/pkg_tarball -T TRIBblix-rabbitmq rabbitmq-server-generic-unix-3.12.4
+${THOME}/build/pkg_tarball -T TRIBblix-rabbitmq rabbitmq-server-generic-unix-3.13.0
