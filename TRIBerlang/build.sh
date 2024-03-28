4b1517efd57b390c977a9b8cf63bc7bbafc5fdbb
Peter Tribble <peter.tribble@gmail.com>
2024-03-28 14:02:38 +0000

    Update erlang 26.2.3

--- TRIBerlang/build.sh
+++ TRIBerlang/build.sh
@@ -10,10 +10,10 @@
 # with -64 on solaris, but passing -64 to gcc doesn't work
 #
 env DED_LD="ld" DED_LDFLAGS="-m64 -G" DED_LD_FLAG_RUNTIME_LIBRARY_PATH="-R" \
-${THOME}/build/dobuild -64only otp_src_26.0.2 -C --enable-m64-build
+${THOME}/build/dobuild -64only otp_src_26.2.3 -C --enable-m64-build
 #
 # [tests:
-# cd otp_src_26.0.2-64bit
+# cd otp_src_26.2.3-64bit
 # env ERL_TOP=`pwd` gmake release_tests
 # env ERL_TOP=`pwd` sh 'cd release/tests/test_server; $ERL_TOP/bin/erl -s ts install -s ts smoke_test batch -s init stop'
 # ls -l `pwd`/release/tests/test_server/index.html
@@ -21,4 +21,4 @@ ${THOME}/build/dobuild -64only otp_src_26.0.2 -C --enable-m64-build
 # ]
 #
 env DED_LD="ld" DED_LDFLAGS="-m64 -G" DED_LD_FLAG_RUNTIME_LIBRARY_PATH="-R" \
-${THOME}/build/genpkg TRIBerlang otp_src_26.0.2
+${THOME}/build/genpkg TRIBerlang otp_src_26.2.3
