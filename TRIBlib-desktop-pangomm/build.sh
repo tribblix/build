1806ace0c857b0e71820bab45c8abff5b3c20cc9
Peter Tribble <peter.tribble@gmail.com>
2024-03-29 14:00:08 +0000

    Update pangomm 2.46.4, and drop 32-bit

--- TRIBlib-desktop-pangomm/build.sh
+++ TRIBlib-desktop-pangomm/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/mesonbuild -64 -gnu pangomm-2.42.2
-${THOME}/build/genpkg TRIBlib-desktop-pangomm pangomm-2.42.2
+# note that 2.48 changes ABI and changes dependencies
+#
+${THOME}/build/mesonbuild +64 -gnu pangomm-2.46.4
+${THOME}/build/genpkg TRIBlib-desktop-pangomm pangomm-2.46.4
