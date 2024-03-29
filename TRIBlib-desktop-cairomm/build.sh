15f9f6cb9a0bdc1a21bf6737ea2ab230f18f425d
Peter Tribble <peter.tribble@gmail.com>
2024-03-29 14:03:55 +0000

    Drop 32-bit cairomm

--- TRIBlib-desktop-cairomm/build.sh
+++ TRIBlib-desktop-cairomm/build.sh
@@ -3,5 +3,5 @@
 # building tests requires boost, and fails on sparc, so force
 # in order to get consistent behaviour
 #
-${THOME}/build/mesonbuild -64 -gnu -B tbuild cairomm-1.14.5 -C -Dbuild-tests=false
+${THOME}/build/mesonbuild +64 -gnu cairomm-1.14.5 -C -Dbuild-tests=false
 ${THOME}/build/genpkg TRIBlib-desktop-cairomm cairomm-1.14.5
