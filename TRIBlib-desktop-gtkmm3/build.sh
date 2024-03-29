8ff7b528434d9189f51ca7d7cbe5accef4d4107d
Peter Tribble <peter.tribble@gmail.com>
2024-03-29 13:49:56 +0000

    Update gtkmm3 3.24.9, and drop 32-bit

--- TRIBlib-desktop-gtkmm3/build.sh
+++ TRIBlib-desktop-gtkmm3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/mesonbuild -64 -gnu gtkmm-3.24.7 -C "-Dbuild-tests=false -Dbuild-demos=false"
-${THOME}/build/genpkg TRIBlib-desktop-gtkmm3 gtkmm-3.24.7
+${THOME}/build/mesonbuild +64 -gnu gtkmm-3.24.9 -C "-Dbuild-tests=false -Dbuild-demos=false"
+${THOME}/build/genpkg TRIBlib-desktop-gtkmm3 gtkmm-3.24.9
