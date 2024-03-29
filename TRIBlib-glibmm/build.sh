6e34a8fa01f31e51ed4a4967239bbb3bd94c88ff
Peter Tribble <peter.tribble@gmail.com>
2024-03-29 14:19:12 +0000

    Update glibmm 2.64.5, and drop 32-bit

--- TRIBlib-glibmm/build.sh
+++ TRIBlib-glibmm/build.sh
@@ -2,5 +2,5 @@
 #
 # there's a critical patch
 #
-${THOME}/build/dobuild -64 -gnu glibmm-2.62.0
-${THOME}/build/genpkg TRIBlib-glibmm glibmm-2.62.0
+${THOME}/build/mesonbuild +64 -gnu glibmm-2.64.5
+${THOME}/build/genpkg TRIBlib-glibmm glibmm-2.64.5
