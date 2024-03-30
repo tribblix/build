0145366a9f999c9f8240f4d707af3349d7fe4c8c
Peter Tribble <peter.tribble@gmail.com>
2024-03-30 10:21:31 +0000

    Update python-snappy 0.7.1 (but not sparc)

--- TRIBsnappy-python-311/build.sh
+++ TRIBsnappy-python-311/build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBsnappy-python-311 python-snappy-0.6.1
+
+#
+# the new version is a wrapper round cramjam, but cramjam is written in rust
+#
+${THOME}/build/pkg_setup_py TRIBsnappy-python-311 python-snappy-0.7.1
+
+#
+# old build for sparc
+#
+#${THOME}/build/pkg_setup_py TRIBsnappy-python-311 python-snappy-0.6.1
