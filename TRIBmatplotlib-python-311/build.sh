9c0bd2fd35b243ede596c2e9ed8a8cda9d6fafd3
Peter Tribble <peter.tribble@gmail.com>
2024-04-04 12:58:52 +0100

    Update matplotlib 3.8.4

--- TRIBmatplotlib-python-311/build.sh
+++ TRIBmatplotlib-python-311/build.sh
@@ -3,4 +3,4 @@
 # there's a patch to create the mplsetup.cfg file to force the use of
 # the system freetype
 #
-${THOME}/build/pkg_setup_py TRIBmatplotlib-python-311 matplotlib-3.8.3
+${THOME}/build/pkg_setup_py TRIBmatplotlib-python-311 matplotlib-3.8.4
