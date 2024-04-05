477aca33b75a3a35b7b930782079bfbae90550e4
Peter Tribble <peter.tribble@gmail.com>
2024-04-05 10:02:32 +0100

    update xdm 1.1.16

--- TRIBxdm/build.sh
+++ TRIBxdm/build.sh
@@ -2,5 +2,5 @@
 #
 # follow the OI build
 #
-env DEF_SERVER=/usr/bin/X DEF_USER_PATH=/usr/bin:/usr/sbin DEF_SYSTEM_PATH=/usr/bin:/usr/sbin ${THOME}/build/dobuild -64only xdm-1.1.15 -C "--sysconfdir=/etc --localstatedir=/var --with-xdmconfigdir=/etc/X11/xdm --with-xdmlibdir=/usr/lib/X11/xdm --with-piddir=/var/run/xdm --with-authdir=/var/run/xdm --bindir=/usr/bin --sbindir=/usr/sbin --with-xrdb=/usr/bin/xrdb ADMIN_MAN_SUFFIX=8"
-${THOME}/build/genpkg TRIBxdm xdm-1.1.15
+env DEF_SERVER=/usr/bin/X DEF_USER_PATH=/usr/bin:/usr/sbin DEF_SYSTEM_PATH=/usr/bin:/usr/sbin ${THOME}/build/dobuild -64only xdm-1.1.16 -C "--sysconfdir=/etc --localstatedir=/var --with-xdmconfigdir=/etc/X11/xdm --with-xdmlibdir=/usr/lib/X11/xdm --with-piddir=/var/run/xdm --with-authdir=/var/run/xdm --bindir=/usr/bin --sbindir=/usr/sbin --with-xrdb=/usr/bin/xrdb ADMIN_MAN_SUFFIX=8"
+${THOME}/build/genpkg TRIBxdm xdm-1.1.16
