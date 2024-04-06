66e43c141886c2342833153fbb6a103d807445e1
Peter Tribble <peter.tribble@gmail.com>
2024-04-06 14:41:53 +0100

    Drop 32-bit libtasn1

--- TRIBlibtasn1/build.sh
+++ TRIBlibtasn1/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #
-# used by current qemu
-#
-${THOME}/build/dobuild -64 libtasn1-4.19.0
+${THOME}/build/dobuild -64only libtasn1-4.19.0
 ${THOME}/build/genpkg TRIBlibtasn1 libtasn1-4.19.0
