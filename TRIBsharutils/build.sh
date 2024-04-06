cc197d0318c49651876e8db3aefb6f99ff41d16e
Peter Tribble <peter.tribble@gmail.com>
2024-04-06 15:58:55 +0100

    Rebuild sharutils 64-bit

--- TRIBsharutils/build.sh
+++ TRIBsharutils/build.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-${THOME}/build/dobuild sharutils-4.15.2 -C --disable-uucode
+env TRIBBLIX_CFLAGS=-fcommon ${THOME}/build/dobuild -64only sharutils-4.15.2 -C --disable-uucode
 ${THOME}/build/genpkg TRIBsharutils sharutils-4.15.2
