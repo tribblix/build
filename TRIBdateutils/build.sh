946ba0e1f1299e33aa30d4cae4d3788f0169ed04
Peter Tribble <peter.tribble@gmail.com>
2024-04-06 16:50:54 +0100

    Update dateutils 0.4.11

--- TRIBdateutils/build.sh
+++ TRIBdateutils/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild dateutils-0.4.10
-${THOME}/build/genpkg TRIBdateutils dateutils-0.4.10
+${THOME}/build/dobuild -64only dateutils-0.4.11 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBdateutils dateutils-0.4.11
