a3600ca37a59fcf3c8b0c64e1914cc66069fb92b
Peter Tribble <peter.tribble@gmail.com>
2024-04-06 15:09:54 +0100

    Update IceWM 3.4.7

--- TRIBicewm/build.sh
+++ TRIBicewm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only icewm-3.4.5
-${THOME}/build/genpkg TRIBicewm icewm-3.4.5
+${THOME}/build/dobuild -64only icewm-3.4.7
+${THOME}/build/genpkg TRIBicewm icewm-3.4.7
