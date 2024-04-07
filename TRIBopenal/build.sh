79e1c4dec547c7fd0ff797a73ef77d2355708afe
Peter Tribble <peter.tribble@gmail.com>
2024-04-07 18:13:01 +0100

    Update OpenAL 1.23.1

--- TRIBopenal/build.sh
+++ TRIBopenal/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# [seems orphaned, no accurate idea where to get it from]
+# necessary preconf
 #
-${THOME}/build/cmbuild -64 openal-soft-1.15.1 -C "-DEXAMPLES=NO" +C "-DEXAMPLES=NO -D LIB_SUFFIX=/`$THOME/build/getarch`"
-${THOME}/build/genpkg TRIBopenal openal-soft-1.15.1
+${THOME}/build/cmbuild -64 openal-soft-1.23.1 -C "-DALSOFT_UTILS=NO -DALSOFT_EXAMPLES=NO"
+${THOME}/build/genpkg TRIBopenal openal-soft-1.23.1
