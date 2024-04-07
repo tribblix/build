3d4153a7d0d5cbc314ff6c9bbbd22d2b4259642a
Peter Tribble <peter.tribble@gmail.com>
2024-04-07 18:44:36 +0100

    Update OpenEXR 3.1.13

--- TRIBopenexr/build.sh
+++ TRIBopenexr/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# 3.2 bumps the SONAME
 #
-${THOME}/build/cmbuild -64only openexr-3.1.12
-${THOME}/build/genpkg TRIBopenexr openexr-3.1.12
+${THOME}/build/cmbuild -64only openexr-3.1.13
+${THOME}/build/genpkg TRIBopenexr openexr-3.1.13
