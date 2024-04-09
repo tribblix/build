3add1cfa1141d496470b6299d347b0070e820f34
Peter Tribble <peter.tribble@gmail.com>
2024-04-09 14:03:39 +0100

    Enable write_prometheus plugin for collectd

--- TRIBcollectd/build.sh
+++ TRIBcollectd/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only collectd-5.12.0 -C "--disable-static --localstatedir=/var --sysconfdir=/etc --localstatedir=/var --enable-rrdtool --disable-werror"
+${THOME}/build/dobuild -64only collectd-5.12.0 -C "--disable-static --localstatedir=/var --sysconfdir=/etc --localstatedir=/var --enable-rrdtool --enable-write_prometheus --disable-werror"
 ${THOME}/build/genpkg TRIBcollectd collectd-5.12.0
