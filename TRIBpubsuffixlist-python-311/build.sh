44fe477791ac440f623d95d022d17f5c8723c6e2
Peter Tribble <peter.tribble@gmail.com>
2024-04-09 08:15:11 +0100

    Update pubsuffixlist 0.10.0.20240403

--- TRIBpubsuffixlist-python-311/build.sh
+++ TRIBpubsuffixlist-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpubsuffixlist-python-311 publicsuffixlist-0.10.0.20240328
+${THOME}/build/pkg_setup_py TRIBpubsuffixlist-python-311 publicsuffixlist-0.10.0.20240403
