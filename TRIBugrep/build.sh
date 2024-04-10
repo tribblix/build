7f7550a85dbfa2e55ad9b3749e567c8156a6d54b
Peter Tribble <peter.tribble@gmail.com>
2024-04-10 18:11:15 +0100

    Update ugrep 5.1.4

--- TRIBugrep/build.sh
+++ TRIBugrep/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only ugrep-4.5.2
-${THOME}/build/genpkg TRIBugrep ugrep-4.5.2
+${THOME}/build/dobuild -64only ugrep-5.1.4 -C "--enable-sse2 --disable-avx"
+${THOME}/build/genpkg TRIBugrep ugrep-5.1.4
