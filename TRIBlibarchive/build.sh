93cd886ea40bf2380399997c52e7ee372ffe5414
Peter Tribble <peter.tribble@gmail.com>
2024-04-10 08:41:29 +0100

    Update libarchive 3.7.3

--- TRIBlibarchive/build.sh
+++ TRIBlibarchive/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only libarchive-3.7.2
-${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.2
+${THOME}/build/dobuild -64only libarchive-3.7.3
+${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.3
