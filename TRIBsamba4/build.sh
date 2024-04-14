71a4f5e63d44d769ab01c824b655f1f62b2581ff
Peter Tribble <peter.tribble@gmail.com>
2024-04-14 09:04:18 +0100

    Update samba 4.18.11

--- TRIBsamba4/build.sh
+++ TRIBsamba4/build.sh
@@ -5,5 +5,5 @@
 # disable spotlight otherwise it pulls in gnome tracker and the dependency
 # tree expands
 #
-env CC=gcc TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -D_POSIX_PTHREAD_SEMANTICS -DHAVE_AUXV_T -DHCRYPTO_DEF_PROVIDER=hcrypto" TRIBBLIX_LDFLAGS="-lnsl -lsendfile -lssp_ns -R/usr/gnu/lib/`$THOME/build/getarch` -L/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only samba-4.18.10 -P /usr -C "--enable-fhs --sysconfdir=/etc --localstatedir=/var --disable-python --without-ad-dc --disable-spotlight" -M -k
-${THOME}/build/genpkg TRIBsamba4 samba-4.18.10
+env CC=gcc TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -D_POSIX_PTHREAD_SEMANTICS -DHAVE_AUXV_T -DHCRYPTO_DEF_PROVIDER=hcrypto" TRIBBLIX_LDFLAGS="-lnsl -lsendfile -lssp_ns -R/usr/gnu/lib/`$THOME/build/getarch` -L/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only samba-4.18.11 -P /usr -C "--enable-fhs --sysconfdir=/etc --localstatedir=/var --disable-python --without-ad-dc --disable-spotlight" -M -k
+${THOME}/build/genpkg TRIBsamba4 samba-4.18.11
