61162a74253c477443e9214d9ce761f3cb1b3b45
Peter Tribble <peter.tribble@gmail.com>
2024-04-16 16:11:42 +0100

    Add pydantic 2.7.0

---
+++ TRIBannot-types-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBannot-types-python-311 annotated_types-0.6.0
---
+++ TRIBpydantic-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# build together, as they're joined at the hip and version locked
+#
+${THOME}/build/pkg_pep518 TRIBpydantic-python-311 pydantic_core-2.18.1 pydantic-2.7.0
