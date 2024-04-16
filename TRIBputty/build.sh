eb8a3f595fc7b491ae644bb7c39f4c9c2dda8602
Peter Tribble <peter.tribble@gmail.com>
2024-04-16 08:02:15 +0100

    Update putty 0.81

--- TRIBputty/build.sh
+++ TRIBputty/build.sh
@@ -2,5 +2,5 @@
 #
 # putty
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only putty-0.80
-${THOME}/build/genpkg TRIBputty putty-0.80
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only putty-0.81
+${THOME}/build/genpkg TRIBputty putty-0.81
