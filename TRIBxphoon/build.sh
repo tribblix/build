c6b433d89666f6af3f1fce7e58ab317a3c6960b1
Peter Tribble <peter.tribble@gmail.com>
2024-04-16 20:37:16 +0100

    Convert xphoon to imakebuild

--- TRIBxphoon/build.sh
+++ TRIBxphoon/build.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack xphoon_14Aug2014
-cd xphoon
-sed -i s:OS_SYSV:SVR4: date_parse.c
-# yes gsed for the newline
-gsed -i 's:<sys/ioctl.h>:<sys/ioctl.h>\n#include <sys/termio.h>:' xphoon.c
-sed -i 's:-ansi:-m64 -ansi:' Imakefile
-xmkmf -DHasGcc3=YES
-make
-cd ..
-${THOME}/build/genpkg TRIBxphoon xphoon
+${THOME}/build/imakebuild -64only -n xphoon xphoon_14Aug2014
+${THOME}/build/genpkg TRIBxphoon xphoon_14Aug2014
