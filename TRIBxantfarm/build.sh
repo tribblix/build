5fdcbf313cecdb514d9914993a3d57c3a8b93df4
Peter Tribble <peter.tribble@gmail.com>
2024-04-16 20:21:57 +0100

    Convert xantfarm to imakebuild, and build 64-bit

--- TRIBxantfarm/build.sh
+++ TRIBxantfarm/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack xantfarm_14Aug2014
-cd xantfarm
-sed -i s:SYSV:SVR4: xantfarm.c
-xmkmf -DHasGcc3=YES
-make
-cd ..
-${THOME}/build/genpkg TRIBxantfarm xantfarm
+${THOME}/build/imakebuild -64only -n xantfarm xantfarm_14Aug2014
+${THOME}/build/genpkg TRIBxantfarm xantfarm_14Aug2014
