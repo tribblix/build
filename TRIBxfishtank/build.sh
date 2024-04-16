50460b000323a2ebd78a5ba98ee0b4a0671af1d5
Peter Tribble <peter.tribble@gmail.com>
2024-04-16 20:28:46 +0100

    Convert xfishtank to imakebuild

--- TRIBxfishtank/build.sh
+++ TRIBxfishtank/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/unpack xfishtank
-cd xfishtank
-xmkmf -DHasGcc3=YES -DAMD64Architecture=YES -DDefaultCCOptions="-m64"
-# xmkmf -DHasGcc3=YES -DSparc64Architecture=YES -DDefaultCCOptions="-m64"
-gmake
-${THOME}/build/genpkg TRIBxfishtank
+${THOME}/build/imakebuild -64only xfishtank
+${THOME}/build/genpkg TRIBxfishtank xfishtank
