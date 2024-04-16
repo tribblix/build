a248a92472d249e89890fa7a6a567a8ce85f7b3e
Peter Tribble <peter.tribble@gmail.com>
2024-04-16 08:20:11 +0100

    Update pyzmq 26.0.0

--- TRIBpyzmq-python-311/build.sh
+++ TRIBpyzmq-python-311/build.sh
@@ -3,5 +3,6 @@
 # builds the bundled native libzmq, rather than using a system one
 #
 # the setup.py build fails, as it builds things quite differently
+# need to pass flags through to cmake
 #
-${THOME}/build/pkg_pep518 TRIBpyzmq-python-311 pyzmq-25.1.2
+env CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 ${THOME}/build/pkg_pep518 TRIBpyzmq-python-311 pyzmq-26.0.0
