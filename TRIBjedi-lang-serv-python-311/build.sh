011097579fd73aa1e352fdddf6034d27220c862a
Peter Tribble <peter.tribble@gmail.com>
2024-04-17 08:01:58 +0100

    Update jedi-language-server 0.41.4

--- TRIBjedi-lang-serv-python-311/build.sh
+++ TRIBjedi-lang-serv-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBjedi-lang-serv-python-311 jedi_language_server-0.41.3
+${THOME}/build/pkg_pep518 TRIBjedi-lang-serv-python-311 jedi_language_server-0.41.4
