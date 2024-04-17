ab68207ffeab9d27244e96e3a6a4c917e636e592
Peter Tribble <peter.tribble@gmail.com>
2024-04-17 18:52:22 +0100

    Need to explicitly turn off tests in taglib

--- TRIBtaglib/build.sh
+++ TRIBtaglib/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64 taglib-1.13 -C "-DBUILD_SHARED_LIBS=on -DBUILD_STATIC_LIBS=no"
+${THOME}/build/cmbuild -64 taglib-1.13 -C "-DBUILD_SHARED_LIBS=on -DBUILD_STATIC_LIBS=no -DBUILD_TESTING=no"
 ${THOME}/build/genpkg TRIBtaglib taglib-1.13
