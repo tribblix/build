1b6f8bcde6ef3dc450c458f33ab7087af865914e
Peter Tribble <peter.tribble@gmail.com>
2024-04-18 15:46:57 +0100

    Add hatchling

---
+++ TRIBhatchling-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBhatchling-python-311 hatchling-1.24.1
---
+++ TRIBtrove-class-python-311/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBtrove-class-python-311 trove-classifiers-2024.4.10
