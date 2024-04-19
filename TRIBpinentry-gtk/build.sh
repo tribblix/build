7ffbc56ee07f10653ba5f26fc350aa79f759e722
Peter Tribble <peter.tribble@gmail.com>
2024-04-19 11:25:03 +0100

    Split the pinentry package for dependency handling

---
+++ TRIBpinentry-gtk/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# pinentry wrapper
+#
+echo "Built as part of TRIBpinentry"
--- TRIBpinentry/build.sh
+++ TRIBpinentry/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # pinentry
+#
+# for dependency purposes, split the gui implementations
+# into separate packages
 #
 ${THOME}/build/dobuild -64only pinentry-1.3.0 -C "--disable-libsecret --disable-pinentry-efl LIBS=-lsocket"
 ${THOME}/build/genpkg TRIBpinentry pinentry-1.3.0
