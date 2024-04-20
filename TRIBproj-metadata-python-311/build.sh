1ac24becbd8be73624f39935cd7c15dabc5ddfd9
Peter Tribble <peter.tribble@gmail.com>
2024-04-20 14:28:34 +0100

    Update pyproject-metadata 0.8.0

--- TRIBproj-metadata-python-311/build.sh
+++ TRIBproj-metadata-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # needed by meson-python
 #
-${THOME}/build/pkg_pep518 TRIBproj-metadata-python-311 pyproject-metadata-0.7.1
+${THOME}/build/pkg_pep518 TRIBproj-metadata-python-311 pyproject_metadata-0.8.0
