3e53fdfcd5c635578ee0f7f4f22064e8d64fc355
Peter Tribble <peter.tribble@gmail.com>
2024-04-20 09:46:37 +0100

    Update json-c 0.17, with compat

--- TRIBjson-c/build.sh
+++ TRIBjson-c/build.sh
@@ -2,7 +2,8 @@
 #
 # the github downloads have obscure names
 #
-# updates bump the SONAME, forcing a rebuild of libmypaint and repmgr
+# updates bump the SONAME, forcing a rebuild of any consumers
+# so ship a copy of the previous build for compat, see fixit
 #
-${THOME}/build/dobuild +64 json-c-json-c-0.12.1-20160607 -J 1
-${THOME}/build/genpkg TRIBjson-c json-c-json-c-0.12.1-20160607
+${THOME}/build/cmbuild +64 json-c-json-c-0.17-20230812
+${THOME}/build/genpkg TRIBjson-c json-c-json-c-0.17-20230812
