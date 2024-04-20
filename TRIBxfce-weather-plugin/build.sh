2dbe11b5686ddd36734943e1a30a3b57c68bd94f
Peter Tribble <peter.tribble@gmail.com>
2024-04-20 11:27:28 +0100

    Update xfce weather plugin 0.11.2

--- TRIBxfce-weather-plugin/build.sh
+++ TRIBxfce-weather-plugin/build.sh
@@ -2,5 +2,5 @@
 #
 # 0.11.2 requires json-c, which we don't have in 32-bit
 #
-${THOME}/build/dobuild -64only -gnu xfce4-weather-plugin-0.11.1
-${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.11.1
+${THOME}/build/dobuild -64only -gnu xfce4-weather-plugin-0.11.2
+${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.11.2
