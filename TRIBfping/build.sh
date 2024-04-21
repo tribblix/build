ce32d8b7e8b16a79ee013758184bff5357ad8a7d
Peter Tribble <peter.tribble@gmail.com>
2024-04-21 21:09:11 +0100

    Update fping 5.2

--- TRIBfping/build.sh
+++ TRIBfping/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild -64only fping-5.1 -C --enable-ipv6
-${THOME}/build/genpkg TRIBfping fping-5.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+env CPPFLAGS="-D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild -64only fping-5.2 -C --enable-ipv6
+${THOME}/build/genpkg TRIBfping fping-5.2
