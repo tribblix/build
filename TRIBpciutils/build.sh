0ab12d83b78e2cb71e87594a275ba7e849ee2e65
Peter Tribble <peter.tribble@gmail.com>
2024-04-21 21:05:01 +0100

    Update pciutils 3.12.0

--- TRIBpciutils/build.sh
+++ TRIBpciutils/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack pciutils-3.6.3
-cd pciutils-3.6.3
-env PATH=/usr/gnu/bin:$PATH gmake PREFIX=/usr OPT="-O2 -DBYTE_ORDER=1234 -DLITTLE_ENDIAN=1234"
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack pciutils-3.12.0
+cd pciutils-3.12.0
+env PATH=/usr/gnu/bin:$PATH gmake -j 4 CC=gcc PREFIX=/usr OPT="-m64 -O2 -DBYTE_ORDER=1234 -DLITTLE_ENDIAN=1234"
 # update the database
 ./update-pciids.sh
 rm -fr /tmp/pp
