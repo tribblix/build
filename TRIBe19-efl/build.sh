de2f22c33e19181ef7dae46e0f16298930990d5f
Peter Tribble <peter.tribble@gmail.com>
2024-04-22 10:08:31 +0100

    Update efl 1.21.1 enlightenment 0.22.4

--- TRIBe19-efl/build.sh
+++ TRIBe19-efl/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl -z muldefs" $THOME/build/dobuild -64only efl-1.20.7 -C "--with-profile=release --disable-pulseaudio --enable-fribidi --enable-harfbuzz --disable-libeeze --enable-lua-old --disable-cserve --enable-physics --disable-systemd --enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-abb"
-${THOME}/build/genpkg TRIBe19-efl efl-1.20.7
+# SPDX-License-Identifier: CDDL-1.0
+#
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl -z muldefs" $THOME/build/dobuild -64only efl-1.21.1 -C "--with-profile=release --disable-pulseaudio --enable-fribidi --enable-harfbuzz --disable-libeeze --enable-lua-old --disable-cserve --enable-physics --disable-systemd --enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-abb"
+${THOME}/build/genpkg TRIBe19-efl efl-1.21.1
--- TRIBe19-enlightenment/build.sh
+++ TRIBe19-enlightenment/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only enlightenment-0.21.11 -C "--disable-connman --disable-cpufreq --disable-battery --disable-mixer --disable-bluez4 --disable-temperature --disable-device-udev --disable-mount-udisks --disable-device-hal"
-${THOME}/build/genpkg TRIBe19-enlightenment enlightenment-0.21.11
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only enlightenment-0.22.4 -C "--disable-connman --disable-cpufreq --disable-battery --disable-mixer --disable-bluez4 --disable-temperature --disable-device-udev --disable-mount-udisks --disable-device-hal --disable-sysinfo"
+${THOME}/build/genpkg TRIBe19-enlightenment enlightenment-0.22.4
