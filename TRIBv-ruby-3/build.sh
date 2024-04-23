a510908cb393786468b627730743489622478f79
Peter Tribble <peter.tribble@gmail.com>
2024-04-23 16:38:07 +0100

    Update ruby 3.2.4

--- TRIBv-ruby-3/build.sh
+++ TRIBv-ruby-3/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # just can't handle LD_OPTIONS
 # disable -fstack-protector otherwise miniruby fails to link with
 # multiply defined symbols
 #
-${THOME}/build/dobuild +64only ruby-3.2.3 -P /usr/versions/ruby-3 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
-${THOME}/build/genpkg TRIBv-ruby-3 ruby-3.2.3
+${THOME}/build/dobuild +64only ruby-3.2.4 -P /usr/versions/ruby-3 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
+${THOME}/build/genpkg TRIBv-ruby-3 ruby-3.2.4
