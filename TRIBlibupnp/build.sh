cefe1966267beb3be8c045fdf086aa9466f05210
Peter Tribble <peter.tribble@gmail.com>
2024-04-24 08:16:17 +0100

    Update libupnp 1.4.19

--- TRIBlibupnp/build.sh
+++ TRIBlibupnp/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only libupnp-1.14.18
-${THOME}/build/genpkg TRIBlibupnp libupnp-1.14.18
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libupnp-1.14.19
+${THOME}/build/genpkg TRIBlibupnp libupnp-1.14.19
