43ef0ff96611e954b1512fb1e19328af2ea560dc
Peter Tribble <peter.tribble@gmail.com>
2024-04-24 14:25:45 +0100

    Drop openntpd, use chrony instead

--- TRIBopenntpd/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-#
-#
-${THOME}/build/dobuild -64only openntpd-6.8p1 -C "--sysconfdir=/etc --localstatedir=/var --with-privsep-user=ntp ac_cv_header_sys_sensors_h=no"
-${THOME}/build/genpkg TRIBopenntpd openntpd-6.8p1
