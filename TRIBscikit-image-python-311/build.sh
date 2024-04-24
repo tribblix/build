a27efba3ee612969bcdfa344ef6a6bde13662d41
Peter Tribble <peter.tribble@gmail.com>
2024-04-24 08:11:48 +0100

    Update scikit-image 0.23.2

--- TRIBscikit-image-python-311/build.sh
+++ TRIBscikit-image-python-311/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # a pep518 build requires -N = --no-build-isolation otherwise it tries
 # to build its own numpy, which would fail as it doesn't have the
 # modifications we need to make it build successfully
@@ -10,4 +12,4 @@
 # PATH so it can find the cython executable
 # Need to pass -m64 down to meson
 #
-env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.11/bin:$PATH  ${THOME}/build/pkg_pep518 -N TRIBscikit-image-python-311 scikit_image-0.23.1
+env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.11/bin:$PATH  ${THOME}/build/pkg_pep518 -N TRIBscikit-image-python-311 scikit_image-0.23.2
