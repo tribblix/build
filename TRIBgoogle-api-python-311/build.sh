d3d99ddb636cf419bc163d0a82bfa4fc0a50af5f
Peter Tribble <peter.tribble@gmail.com>
2024-04-24 07:51:54 +0100

    Update google-api-python-client 2.127.0

--- TRIBgoogle-api-python-311/build.sh
+++ TRIBgoogle-api-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-api-python-311 google-api-python-client-2.126.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBgoogle-api-python-311 google-api-python-client-2.127.0
