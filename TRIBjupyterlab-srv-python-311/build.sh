82b4aee4832623ee6440c8a9f1c59fbc53b4e566
Peter Tribble <peter.tribble@gmail.com>
2024-04-24 07:49:08 +0100

    Update jupyterlab-server 2.27.1

--- TRIBjupyterlab-srv-python-311/build.sh
+++ TRIBjupyterlab-srv-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBjupyterlab-srv-python-311 jupyterlab_server-2.27.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBjupyterlab-srv-python-311 jupyterlab_server-2.27.1
