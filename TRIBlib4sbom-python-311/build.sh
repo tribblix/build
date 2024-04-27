5bdcc8d0a77309331aed19285c8c801dc6e7bfb9
Peter Tribble <peter.tribble@gmail.com>
2024-04-27 19:49:52 +0100

    Update lib4sbom 0.7.1

--- TRIBlib4sbom-python-311/build.sh
+++ TRIBlib4sbom-python-311/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_wheel TRIBlib4sbom-python-311 lib4sbom-0.7.0-py2.py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBlib4sbom-python-311 lib4sbom-0.7.1-py2.py3-none-any.whl
