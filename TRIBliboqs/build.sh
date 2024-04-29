db296fa09c0242209f8bb794e0a269f751b53c55
Peter Tribble <peter.tribble@gmail.com>
2024-04-29 17:42:03 +0100

    Add liboqs 0.10.0

---
+++ TRIBliboqs/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild -64only liboqs-0.10.0 -C "-DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF"
+${THOME}/build/genpkg TRIBliboqs liboqs-0.10.0
