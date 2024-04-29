0fb7d92e013bf4abf82ecc6b04539cb2d0c1e840
Peter Tribble <peter.tribble@gmail.com>
2024-04-29 08:45:45 +0100

    Update python regex 2024.4.28

--- TRIBregex-python-311/build.sh
+++ TRIBregex-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBregex-python-311 regex-2024.4.16
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBregex-python-311 regex-2024.4.28
