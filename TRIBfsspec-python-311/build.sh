ec286f756a53e393562cb31c71437db3f938f3c7
Peter Tribble <peter.tribble@gmail.com>
2024-04-29 21:44:38 +0100

    Add fsspec 2024.3.1

---
+++ TRIBfsspec-python-311/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# there's a huge list of optional extras, we don't declare any of them
+# as dependencies directly
+#
+${THOME}/build/pkg_pep518 TRIBfsspec-python-311 fsspec-2024.3.1
