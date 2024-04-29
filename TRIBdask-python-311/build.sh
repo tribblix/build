4e5cdfe6359e82e48a4f0c1bd02eb6ee148adb86
Peter Tribble <peter.tribble@gmail.com>
2024-04-29 21:47:51 +0100

    Add dask and distributed 2024.4.2

---
+++ TRIBdask-python-311/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# dask and distributed are locked together version-wise so package
+# them together
+#
+# the dependency list is thus for the combinbation of both modules
+#
+# there's a distributed patch necessary to handle RLIMIT
+#
+${THOME}/build/pkg_pep518 TRIBdask-python-311 dask-2024.4.2 distributed-2024.4.2
