e59db0605310c8ac6e3d1905a91372463522d5f9
Peter Tribble <peter.tribble@gmail.com>
2024-04-29 16:55:32 +0100

    Add Node v22

---
+++ TRIBv-node-v22/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# 64-bit only
+#
+env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v22.0.0 -P /usr/versions/node-v22 -J 6 -C "--dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v22 node-v22.0.0
---
+++ patches/node-v22.0.0.pls
@@ -0,0 +1,1 @@
+-p1 node22-madvise.patch
