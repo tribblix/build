efbf8ca3cbb84d2997a8955ab13bdcd7013facde
Peter Tribble <peter.tribble@gmail.com>
2024-04-30 15:02:09 +0100

    Enable parquet in arrow

--- TRIBapache-arrow/build.sh
+++ TRIBapache-arrow/build.sh
@@ -4,8 +4,8 @@
 #
 # building with -DARROW_PARQUET=ON fails, see
 # https://github.com/apache/arrow/issues/41338
-# I can't see how the code can possibly work, given that char
-# should always be int8_t or uint8_t depending on signedness
+# the "fix" is to force int8_t to be explicitly 'signed char'
+# rather than 'char' (see /usr/include/sys/int_types.h)
 #
-$THOME/build/cmbuild -64only -S cpp apache-arrow-16.0.0 -C "-DARROW_JEMALLOC=OFF -DARROW_BUILD_STATIC=OFF -DARROW_COMPUTE=ON -DARROW_FILESYSTEM=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_JSON=ON"
+$THOME/build/cmbuild -64only -S cpp apache-arrow-16.0.0 -C "-DARROW_JEMALLOC=OFF -DARROW_BUILD_STATIC=OFF -DARROW_COMPUTE=ON -DARROW_FILESYSTEM=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_JSON=ON -DARROW_PARQUET=O"
 ${THOME}/build/genpkg TRIBapache-arrow apache-arrow-16.0.0
