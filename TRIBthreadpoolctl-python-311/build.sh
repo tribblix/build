0ca041d249b3eebb1a8d5ed47ad79671ad8badc1
Peter Tribble <peter.tribble@gmail.com>
2024-04-30 08:20:09 +0100

    Update threadpoolctl 3.5.0

--- TRIBthreadpoolctl-python-311/build.sh
+++ TRIBthreadpoolctl-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBthreadpoolctl-python-311 threadpoolctl-3.4.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBthreadpoolctl-python-311 threadpoolctl-3.5.0
