c4cffdeb3ed8fae2e9190b124dabbe6a90d50a6c
Peter Tribble <peter.tribble@gmail.com>
2024-04-30 08:25:06 +0100

    Update nbconvert 7.16.4

--- TRIBnbconvert-python-311/build.sh
+++ TRIBnbconvert-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBnbconvert-python-311 nbconvert-7.16.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBnbconvert-python-311 nbconvert-7.16.4
