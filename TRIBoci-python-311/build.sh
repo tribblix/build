a52bb0d3ec38de886ca946c7bb497728e6238ea0
Peter Tribble <peter.tribble@gmail.com>
2024-04-30 08:36:23 +0100

    Update oci 2.126.1 oci-cli 3.40.1

--- TRIBoci-python-311/build.sh
+++ TRIBoci-python-311/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # bundle all the bits together
 # it's a massive package, ~50k files, 560M uncompressed, 119M zap
 #
-${THOME}/build/pkg_setup_py TRIBoci-python-311 circuitbreaker-1.4.0 terminaltables-3.1.10 oci-2.124.1 oci-cli-3.37.13
+${THOME}/build/pkg_setup_py TRIBoci-python-311 circuitbreaker-1.4.0 terminaltables-3.1.10 oci-2.126.1 oci-cli-3.40.1
