1c62cd3d42e76a890fc9c62424f2baf9882685cf
Peter Tribble <peter.tribble@gmail.com>
2024-05-02 14:26:45 +0100

    Update mate-terminal 1.28.1

--- TRIBmate-terminal/build.sh
+++ TRIBmate-terminal/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this depends on mate-desktop, via the file
 # /usr/share/glib-2.0/schemas/org.mate.interface.gschema.xml
 #
-$THOME/build/dobuild +gnu -64only mate-terminal-1.26.1
-${THOME}/build/genpkg TRIBmate-terminal mate-terminal-1.26.1
+$THOME/build/dobuild +gnu -64only mate-terminal-1.28.1
+${THOME}/build/genpkg TRIBmate-terminal mate-terminal-1.28.1
