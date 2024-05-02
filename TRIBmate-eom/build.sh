ce39745fb4980ce0a4db2739a07275bffd0076b0
Peter Tribble <peter.tribble@gmail.com>
2024-05-02 14:34:51 +0100

    Update eom 1.28.0

--- TRIBmate-eom/build.sh
+++ TRIBmate-eom/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # eom (needs itstool, newer intltool, mate-icon-theme, mate-desktop)
 #
-${THOME}/build/dobuild +gnu -64only eom-1.26.1
-${THOME}/build/genpkg TRIBmate-eom eom-1.26.1
+${THOME}/build/dobuild +gnu -64only eom-1.28.0
+${THOME}/build/genpkg TRIBmate-eom eom-1.28.0
