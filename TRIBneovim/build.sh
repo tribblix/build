01d53785684fed0512877e4612caa31355d8af4d
Peter Tribble <peter.tribble@gmail.com>
2024-05-02 14:57:12 +0100

    Add neovim 0.9.5

---
+++ TRIBneovim/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# use dobuild to go through the Makefile, which handles all the prerequisites
+# correctly, rather than cmbuild directly, which doesn't
+#
+${THOME}/build/dobuild +gnu -64only neovim-0.9.5 -M CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=/usr
+${THOME}/build/genpkg TRIBneovim neovim-0.9.5
