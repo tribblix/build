0c0faee35bbb7dcea668bfa0abfbffe94425f383
Peter Tribble <peter.tribble@gmail.com>
2024-05-02 14:19:16 +0100

    Update engrampa 1.28.1

--- TRIBmate-engrampa/build.sh
+++ TRIBmate-engrampa/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only +gnu engrampa-1.26.2
-${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.26.2
+${THOME}/build/dobuild -64only +gnu engrampa-1.28.1
+${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.28.1
