a0dc38af046319606321c2773a7b8cccc6099465
Peter Tribble <peter.tribble@gmail.com>
2024-05-03 08:53:58 +0100

    Update libopus 1.5.2

--- TRIBopus/build.sh
+++ TRIBopus/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 opus-1.4 -C --disable-dependency-tracking
-${THOME}/build/genpkg TRIBopus opus-1.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 opus-1.5.2 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBopus opus-1.5.2
