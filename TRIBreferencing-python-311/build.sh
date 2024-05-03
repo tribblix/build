56311c93910be7c197ce036c467af331985f3d71
Peter Tribble <peter.tribble@gmail.com>
2024-05-03 08:17:27 +0100

    Update referencing 0.35.1

--- TRIBreferencing-python-311/build.sh
+++ TRIBreferencing-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBreferencing-python-311 referencing-0.35.0
+${THOME}/build/pkg_pep518 TRIBreferencing-python-311 referencing-0.35.1
