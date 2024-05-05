6c5839e88f8e8714b51717e87a1b7856c2f2cee7
Peter Tribble <peter.tribble@gmail.com>
2024-05-05 19:17:01 +0100

    Update wireshark 3.6.22

--- TRIBwireshark-qt/build.sh
+++ TRIBwireshark-qt/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-#
+# SPDX-License-Identifier: CDDL-1.0
 #
 echo "See TRIBwireshark"
--- TRIBwireshark/build.sh
+++ TRIBwireshark/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this builds both the main wireshark package and the qt package
 #
 # needs a little extra help with kerberos
 #
 
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" PATH=${PATH}:/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:/usr/perl5/5.34/bin ${THOME}/build/cmbuild -64only -gnu wireshark-3.6.15 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" PATH=${PATH}:/usr/versions/Qt-5/bin/`${THOME}/build/getarch` ${THOME}/build/cmbuild -64only -gnu wireshark-3.6.22 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
 
 #
 # sparc doesn't yet have Qt, so only build tshark, disabling the wireshark gui
 #
-#env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" PATH=${PATH}:/usr/perl5/5.34/bin ${THOME}/build/cmbuild -64only -gnu wireshark-3.6.15 -C "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DBUILD_wireshark=OFF"
+#env TRIBBLIX_LDFLAGS="-lsocket -lnsl" TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -I/usr/include/kerberosv5" ${THOME}/build/cmbuild -64only -gnu wireshark-3.6.22 -C "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DBUILD_wireshark=OFF"
 
-${THOME}/build/genpkg TRIBwireshark wireshark-3.6.15
-${THOME}/build/genpkg TRIBwireshark-qt wireshark-3.6.15
+${THOME}/build/genpkg TRIBwireshark wireshark-3.6.22
+${THOME}/build/genpkg TRIBwireshark-qt wireshark-3.6.22
