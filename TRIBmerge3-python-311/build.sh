0fd067fd40c183a9f83f748a0a7f1596c6a48f89
Peter Tribble <peter.tribble@gmail.com>
2024-05-05 18:24:36 +0100

    Update merge3 0.0.15

--- TRIBmerge3-python-311/build.sh
+++ TRIBmerge3-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmerge3-python-311 merge3-0.0.14
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBmerge3-python-311 merge3-0.0.15
