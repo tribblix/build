a28f030b6f74e0ca29a1bbe20e0fc39fc26c8c2e
Peter Tribble <peter.tribble@gmail.com>
2024-05-07 07:56:03 +0100

    Update detect-secrets 1.5.0

--- TRIBdetect-secrets-python-311/build.sh
+++ TRIBdetect-secrets-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBdetect-secrets-python-311 detect_secrets-1.4.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBdetect-secrets-python-311 detect_secrets-1.5.0
