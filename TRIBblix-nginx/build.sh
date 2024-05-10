0493a89c71707330847264080305480b6e03f2be
Peter Tribble <peter.tribble@gmail.com>
2024-05-10 09:07:15 +0100

    Update nginx 1.26.0

--- TRIBblix-nginx/build.sh
+++ TRIBblix-nginx/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # it's not a standard configure, so a 64-bit dobuild doesn't work as
 # it sets --bindir which isn't recognized
 #
@@ -7,5 +9,5 @@
 # it up for LDFLAGS so the link fails; hence we tell it directly
 # via $CC (and don't use --with-cc any more)
 #
-env CC="gcc -m64" ${THOME}/build/dobuild nginx-1.25.4 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module --with-http_v3_module"
-${THOME}/build/genpkg TRIBblix-nginx nginx-1.25.4
+env CC="gcc -m64" ${THOME}/build/dobuild nginx-1.26.0 -P /opt/tribblix/nginx -C "--user=nginx --group=nginx --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --with-http_realip_module --with-http_v3_module"
+${THOME}/build/genpkg TRIBblix-nginx nginx-1.26.0
