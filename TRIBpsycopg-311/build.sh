2040ec1f0f9d6fe8514151ed898beb5fb2d93874
Peter Tribble <peter.tribble@gmail.com>
2024-05-11 21:49:25 +0100

    Update psycopg 3.1.19

--- TRIBpsycopg-311/build.sh
+++ TRIBpsycopg-311/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is really psycopg3, but the module is just called psycopg
 #
 # we ship the optional components providing the [c] and [pool] extras,
@@ -9,4 +11,4 @@
 # just a binary download from pypi and illumos isn't provided, but the
 # [c] extra, aka "local installation", should be equivalent
 #
-env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg-311 psycopg-3.1.18 psycopg-pool-3.2.1 psycopg-c-3.1.18
+env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg-311 psycopg-3.1.19 psycopg_pool-3.2.2 psycopg_c-3.1.19
