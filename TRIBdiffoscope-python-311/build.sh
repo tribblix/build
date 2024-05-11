0c00fc6d977ec3c1d0244ffa40accf1bd00f0ad2
Peter Tribble <peter.tribble@gmail.com>
2024-05-11 21:39:50 +0100

    Update diffoscope 266

--- TRIBdiffoscope-python-311/build.sh
+++ TRIBdiffoscope-python-311/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # there are a lot of potential dependencies we could add, just
 # declare the ones we already have available
 #
 # [cmdline] argcomplete progressbar
 # also pypdf defusedxml
 #
-${THOME}/build/pkg_setup_py TRIBdiffoscope-python-311 diffoscope-265
+${THOME}/build/pkg_setup_py TRIBdiffoscope-python-311 diffoscope-266
