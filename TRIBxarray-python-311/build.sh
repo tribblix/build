5bb13303040111922e97c1835f8f9693c3b06de6
Peter Tribble <peter.tribble@gmail.com>
2024-05-14 14:37:11 +0100

    Update xarray 2024.5.0

--- TRIBxarray-python-311/build.sh
+++ TRIBxarray-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBxarray-python-311 xarray-2024.3.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBxarray-python-311 xarray-2024.5.0
