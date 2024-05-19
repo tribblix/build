c72e1f4d0a52aa4a2eae0671891ffe730e20a544
Peter Tribble <peter.tribble@gmail.com>
2024-05-19 21:32:02 +0100

    Update Pgpool-II 4.4.7

--- TRIBpgpool/build.sh
+++ TRIBpgpool/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs a patch and postconf
 #
 # specify LD_LRUN_PATH so that pgproto also picks it up and can find
 # libpq at runtime
 #
-env LD_RUN_PATH=/opt/tribblix/postgres15/lib PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.6 -C "--sysconfdir=/etc --with-openssl"
-${THOME}/build/genpkg TRIBpgpool pgpool-II-4.4.6
+env LD_RUN_PATH=/opt/tribblix/postgres15/lib PATH=/opt/tribblix/postgres15/bin:$PATH ${THOME}/build/dobuild -64only pgpool-II-4.4.7 -C "--sysconfdir=/etc --with-openssl"
+${THOME}/build/genpkg TRIBpgpool pgpool-II-4.4.7
