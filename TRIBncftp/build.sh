f0634de41c5f7f75d1ef26c622fdb14eaae87128
Peter Tribble <peter.tribble@gmail.com>
2024-05-19 22:06:48 +0100

    Update ncftp 3.2.7

--- TRIBncftp/build.sh
+++ TRIBncftp/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 #
-# you may have to rename the download
-# mv ncftp-3.2.6-src.tar.xz ncftp-3.2.6.tar.xz
+# SPDX-License-Identifier: CDDL-1.0
 #
 # by default, it will find clang if it's installed
 # while not a problem, force gcc for consistency
 #
-env CC=gcc ${THOME}/build/dobuild -64only ncftp-3.2.6
-${THOME}/build/genpkg TRIBncftp ncftp-3.2.6
+env CC=gcc ${THOME}/build/dobuild -64only -n ncftp-3.2.7 ncftp-3.2.7-src
+${THOME}/build/genpkg TRIBncftp ncftp-3.2.7-src
