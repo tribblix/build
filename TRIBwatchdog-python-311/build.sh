1384bbd2ab56cc8b08014c8610c83cf8f6e19933
Peter Tribble <peter.tribble@gmail.com>
2024-05-27 12:08:09 +0100

    Update watchdog 4.0.1

--- TRIBwatchdog-python-311/build.sh
+++ TRIBwatchdog-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBwatchdog-python-311 watchdog-4.0.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBwatchdog-python-311 watchdog-4.0.1
