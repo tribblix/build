6148d8919834e52f3340d74e016b4d732f1acc1e
Peter Tribble <peter.tribble@gmail.com>
2024-05-27 20:29:37 +0100

    Update xterm 392

--- TRIBterminal-xterm/build.sh
+++ TRIBterminal-xterm/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # options mostly the same as OI
 #
-$THOME/build/dobuild -64only xterm-390 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
-${THOME}/build/genpkg TRIBterminal-xterm xterm-390
+# there's a postconf script for termios
+#
+$THOME/build/dobuild -64only xterm-392 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
+${THOME}/build/genpkg TRIBterminal-xterm xterm-392
