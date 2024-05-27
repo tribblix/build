600d4e33232bce7f72303edae3586d0ce76c1d7f
Peter Tribble <peter.tribble@gmail.com>
2024-05-27 17:58:22 +0100

    Update node v20.13.1

--- TRIBv-node-v20/build.sh
+++ TRIBv-node-v20/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # 64-bit only
 #
-env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v20.12.2 -P /usr/versions/node-v20 -J 6 -C "--dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v20 node-v20.12.2
+env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v20.13.1 -P /usr/versions/node-v20 -J 6 -C "--dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v20 node-v20.13.1
