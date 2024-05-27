d234f95389cdaabf07018c28216ee4d77ee0c3ae
Peter Tribble <peter.tribble@gmail.com>
2024-05-27 13:18:08 +0100

    Update rsyslog 8.2404.0

--- TRIBrsyslog/build.sh
+++ TRIBrsyslog/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # new version needs libfastjson rather than json-c
 #
-${THOME}/build/dobuild rsyslog-8.2402.0 -64only -C "--disable-generate-man-pages --sysconfdir=/etc --localstatedir=/var --enable-imsolaris"
-${THOME}/build/genpkg TRIBrsyslog rsyslog-8.2402.0
+${THOME}/build/dobuild rsyslog-8.2404.0 -64only -C "--disable-generate-man-pages --sysconfdir=/etc --localstatedir=/var --enable-imsolaris"
+${THOME}/build/genpkg TRIBrsyslog rsyslog-8.2404.0
