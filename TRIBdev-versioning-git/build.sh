ad9aeb9c9bff64df907194dd616faface15deaef
Peter Tribble <peter.tribble@gmail.com>
2024-05-27 13:15:19 +0100

    Update git 2.45.1

--- TRIBdev-versioning-git/build.sh
+++ TRIBdev-versioning-git/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the manual pages are unpacked with the fixit script, so remember
 # to update the version there too
 #
-${THOME}/build/dobuild -64only git-2.43.2 -C --with-perl=/usr/perl5/5.34/bin/perl
-${THOME}/build/genpkg TRIBdev-versioning-git git-2.43.2
+${THOME}/build/dobuild -64only git-2.45.1 -C --with-perl=/usr/perl5/5.34/bin/perl
+${THOME}/build/genpkg TRIBdev-versioning-git git-2.45.1
