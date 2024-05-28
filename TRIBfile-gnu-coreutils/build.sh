5f8952d118f2d5d2dcd29fef44ceafcb2d5d6f8d
Peter Tribble <peter.tribble@gmail.com>
2024-05-28 08:55:50 +0100

    Update gnu coreutils 9.5

--- TRIBfile-gnu-coreutils/build.sh
+++ TRIBfile-gnu-coreutils/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only -gnu coreutils-9.3 -P /usr/gnu -C "--without-libgmp gl_cv_host_operating_system=illumos"
-${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-9.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild +64only -gnu coreutils-9.5 -P /usr/gnu -C "--without-libgmp --without-openssl gl_cv_host_operating_system=illumos"
+${THOME}/build/genpkg TRIBfile-gnu-coreutils coreutils-9.5
