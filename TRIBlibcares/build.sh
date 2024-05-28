3254d3c9b0e8612da39796efd697e1e45b9b1f56
Peter Tribble <peter.tribble@gmail.com>
2024-05-28 09:19:48 +0100

    Update c-ares 1.29.0

--- TRIBlibcares/build.sh
+++ TRIBlibcares/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # 64-bit only, as is only consumer (wireshark)
 #
-$THOME/build/dobuild +64 c-ares-1.28.1
-$THOME/build/genpkg TRIBlibcares c-ares-1.28.1
+$THOME/build/dobuild +64 c-ares-1.29.0
+$THOME/build/genpkg TRIBlibcares c-ares-1.29.0
