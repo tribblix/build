2c5bd19c6fbf9dca1f71d0f042d85fbadad1443b
Peter Tribble <peter.tribble@gmail.com>
2024-05-28 12:12:11 +0100

    Update mariadb 10.11.8

--- TRIBblix-mariadb/build.sh
+++ TRIBblix-mariadb/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-#
-${THOME}/build/cmbuild -64only mariadb-10.11.6 -P /opt/tribblix/mariadb
-${THOME}/build/genpkg TRIBblix-mariadb mariadb-10.11.6
+${THOME}/build/cmbuild -64only mariadb-10.11.8 -P /opt/tribblix/mariadb
+${THOME}/build/genpkg TRIBblix-mariadb mariadb-10.11.8
--- patches/mariadb-10.11.6.pls
+++
@@ -1,3 +0,0 @@
--p1 mariadb-fix-pam-include-order.patch
--p1 mariadb-remove-concurrency-variable.patch
--p1 mariadb-strerror.patch
