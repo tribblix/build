331531c317f5be3fb8e93c3dddc462d083762860
Peter Tribble <peter.tribble@gmail.com>
2024-05-28 19:07:59 +0100

    Update JRuby 9.4.7.0

--- TRIBv-jruby-9k/build.sh
+++ TRIBv-jruby-9k/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball TRIBv-jruby-9k jruby-dist-9.4.6.0-bin
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_tarball TRIBv-jruby-9k jruby-dist-9.4.7.0-bin
