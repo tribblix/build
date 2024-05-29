fd50ad13ee1d3cbea72584c2aee5e65f178f8891
Peter Tribble <peter.tribble@gmail.com>
2024-05-29 15:01:45 +0100

    Update jupyter_client 8.6.2

--- TRIBjupyter-client-python-311/build.sh
+++ TRIBjupyter-client-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBjupyter-client-python-311 jupyter_client-8.6.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBjupyter-client-python-311 jupyter_client-8.6.2
