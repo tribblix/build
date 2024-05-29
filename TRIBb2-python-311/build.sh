d5372a0e85bdac9cde2a3d1006507c0407e3653c
Peter Tribble <peter.tribble@gmail.com>
2024-05-29 17:45:09 +0100

    Add b2 cli 4.0.1

---
+++ TRIBb2-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBb2-python-311 b2-4.0.1
---
+++ TRIBrst2ansi-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBrst2ansi-python-311 rst2ansi-0.1.5
