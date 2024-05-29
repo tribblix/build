4f13adff5989f0b6395bf249890d40df8a5be987
Peter Tribble <peter.tribble@gmail.com>
2024-05-29 20:30:41 +0100

    Update openpyxl 3.1.3

--- TRIBopenpyxl-python-311/build.sh
+++ TRIBopenpyxl-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBopenpyxl-python-311 openpyxl-3.1.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBopenpyxl-python-311 openpyxl-3.1.3
