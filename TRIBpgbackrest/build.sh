0ad138df2acba252baea5dc4975431742132659a
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 16:13:58 +0100

    Update pgBackRest 2.52

--- TRIBpgbackrest/build.sh
+++ TRIBpgbackrest/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS="-R/opt/tribblix/postgres15/lib -lsocket -lnsl" TRIBBLIX_CFLAGS=-D__EXTENSIONS__ PATH=/opt/tribblix/postgres15/bin:$PATH $THOME/build/dobuild -64only -S src pgbackrest-release-2.50
-sed -i 's:install -:ginstall -:' pgbackrest-release-2.50-64bit/src/Makefile
-${THOME}/build/genpkg TRIBpgbackrest pgbackrest-release-2.50
+# SPDX-License-Identifier: CDDL-1.0
+#
+env TRIBBLIX_LDFLAGS="-R/opt/tribblix/postgres15/lib -lsocket -lnsl" TRIBBLIX_CFLAGS=-D__EXTENSIONS__ PATH=/opt/tribblix/postgres15/bin:$PATH $THOME/build/dobuild -64only -S src pgbackrest-release-2.52
+sed -i 's:install -:ginstall -:' pgbackrest-release-2.52-64bit/src/Makefile
+${THOME}/build/genpkg TRIBpgbackrest pgbackrest-release-2.52
