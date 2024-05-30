26541f4c432ca0c0f73b9e7fe4020e4a3078d561
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 12:53:31 +0100

    Update OpenJPEG 2.5.2

--- TRIBlib-openjpeg/build.sh
+++ TRIBlib-openjpeg/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the normal cmake bin and lib directories aren't honored, it uses its own
 #
 # need to not build static libs in the first place so the installed
 # cmake config is consistent
 #
-env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +64 openjpeg-2.5.0 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF -DOPENJPEG_INSTALL_BIN_DIR=bin/`$THOME/build/getarch` -DOPENJPEG_INSTALL_LIB_DIR=lib/`$THOME/build/getarch`"
-env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +32 openjpeg-2.5.0 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF"
-${THOME}/build/genpkg TRIBlib-openjpeg openjpeg-2.5.0
+env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +64 openjpeg-2.5.2 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF -DOPENJPEG_INSTALL_BIN_DIR=bin/`$THOME/build/getarch` -DOPENJPEG_INSTALL_LIB_DIR=lib/`$THOME/build/getarch`"
+env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +32 openjpeg-2.5.2 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF"
+${THOME}/build/genpkg TRIBlib-openjpeg openjpeg-2.5.2
