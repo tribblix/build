62e782be4ea5d64bc07e854cc0c27882da7b8fc6
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 11:46:29 +0100

    Update libsecret 0.21.4

--- TRIBlibsecret/build.sh
+++ TRIBlibsecret/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only -gnu libsecret-0.20.5 -C "-Dintrospection=false -Dgtk_doc=false"
-${THOME}/build/genpkg TRIBlibsecret libsecret-0.20.5
+env AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only -gnu libsecret-0.21.4 -C "-Dintrospection=false -Dgtk_doc=false"
+${THOME}/build/genpkg TRIBlibsecret libsecret-0.21.4
