23b711d6bea60d9a8a697d6c633545b161c7ffbd
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 18:35:04 +0100

    Add PostgreSQL 17beta1

---
+++ TRIBblix-postgres17/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# postgres 17
+#
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres17 postgresql-17beta1 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres17 postgresql-17beta1
