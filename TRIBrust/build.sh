dd54eaf4e50cb6dc805a9909ae1048ab8c8bd3b0
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 15:05:30 +0100

    Update rust 1.78.0

--- TRIBrust/build.sh
+++ TRIBrust/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # we're now stable, so grab a stable build from
 # https://forge.rust-lang.org/infra/other-installation-methods.html
 #
-${THOME}/build/unpack rust-1.76.0-x86_64-unknown-illumos
-cd rust-1.76.0-x86_64-unknown-illumos
+${THOME}/build/unpack rust-1.78.0-x86_64-unknown-illumos
+cd rust-1.78.0-x86_64-unknown-illumos
 
 rm -fr /tmp/rrt
 bash ./install.sh --prefix=/usr/versions/rust --destdir=/tmp/rrt
