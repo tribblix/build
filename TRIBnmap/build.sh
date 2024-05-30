baac3b06da848ef5abfed6df3de8f0c4f2b7ca44
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 11:32:38 +0100

    Update nmap 7.95

--- TRIBnmap/build.sh
+++ TRIBnmap/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # zenmap requires pygtk, which we don't have
 #
-${THOME}/build/dobuild -64only nmap-7.94 -C "--without-zenmap --with-liblua=included --with-libpcre=included PYTHON=python3.11"
-${THOME}/build/genpkg TRIBnmap nmap-7.94
+env CC=gcc ${THOME}/build/dobuild -64only nmap-7.95 -C "--without-zenmap --with-liblua=included --with-libpcre PYTHON=python3.11"
+${THOME}/build/genpkg TRIBnmap nmap-7.95
