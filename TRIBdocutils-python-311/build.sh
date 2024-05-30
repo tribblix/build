000c1ddea9147817d4cc8fc283a8ec8a481f1b27
Peter Tribble <peter.tribble@gmail.com>
2024-05-30 17:48:33 +0100

    Update docutils 0.19 (intermediate)

--- TRIBdocutils-python-311/build.sh
+++ TRIBdocutils-python-311/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBdocutils-python-311 docutils-0.16
+# SPDX-License-Identifier: CDDL-1.0
+#
+# awscli wants 0.16, but seems happy with later versions
+# we can't please everybody
+#
+${THOME}/build/pkg_setup_py TRIBdocutils-python-311 docutils-0.19
