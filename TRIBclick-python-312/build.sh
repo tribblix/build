0ba0123b9bc7a8b0cb99891e66682133502e8e8a
Peter Tribble <peter.tribble@gmail.com>
2024-05-31 11:41:40 +0100

    Package pipx for python 3.12

---
+++ TRIBclick-python-312/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_setup_py TRIBclick-python-312 click-8.1.7
---
+++ TRIBplatformdirs-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBplatformdirs-python-312 platformdirs-4.2.2
