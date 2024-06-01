e8dc2c9cf5aeec407da815c316a10a518e31a158
Peter Tribble <peter.tribble@gmail.com>
2024-06-01 11:27:59 +0100

    Update dask 2024.5.2 distributed 2024.5.2 dask-expr 1.1.2

--- TRIBdask-python-311/build.sh
+++ TRIBdask-python-311/build.sh
@@ -12,4 +12,4 @@
 #
 # there's a distributed patch necessary to handle RLIMIT
 #
-${THOME}/build/pkg_pep518 TRIBdask-python-311 dask-2024.5.1 distributed-2024.5.1
+${THOME}/build/pkg_pep518 TRIBdask-python-311 dask-2024.5.2 distributed-2024.5.2
