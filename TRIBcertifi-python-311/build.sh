66609c8981a244c7d8ae89139537ac923bac38ab
Peter Tribble <peter.tribble@gmail.com>
2024-06-02 10:48:50 +0100

    Update certifi 2024.6.2

--- TRIBcertifi-python-311/build.sh
+++ TRIBcertifi-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBcertifi-python-311 certifi-2024.2.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBcertifi-python-311 certifi-2024.6.2
