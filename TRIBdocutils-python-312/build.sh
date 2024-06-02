d08b06fc3a9d60344aa1dc74e0271af9a5dc2387
Peter Tribble <peter.tribble@gmail.com>
2024-06-02 18:23:28 +0100

    Update docutils 0.21.2

--- TRIBdocutils-python-312/build.sh
+++ TRIBdocutils-python-312/build.sh
@@ -5,4 +5,4 @@
 # awscli wants 0.16, but seems happy with later versions
 # we can't please everybody
 #
-${THOME}/build/pkg_setup_py TRIBdocutils-python-312 docutils-0.19
+${THOME}/build/pkg_pep518 TRIBdocutils-python-312 docutils-0.21.2
