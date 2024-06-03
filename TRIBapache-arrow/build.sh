37999a8b7a52970ef67cc5982bb6edf332361cb5
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 16:13:37 +0100

    Update apache arrow 16.1.0

--- TRIBapache-arrow/build.sh
+++ TRIBapache-arrow/build.sh
@@ -4,8 +4,9 @@
 #
 # building with -DARROW_PARQUET=ON fails, see
 # https://github.com/apache/arrow/issues/41338
-# the "fix" is to force int8_t to be explicitly 'signed char'
+# the fix is to force int8_t to be explicitly 'signed char'
 # rather than 'char' (see /usr/include/sys/int_types.h)
+# as in https://www.illumos.org/issues/16523
 #
-$THOME/build/cmbuild -64only -S cpp apache-arrow-16.0.0 -C "-DARROW_JEMALLOC=OFF -DARROW_BUILD_STATIC=OFF -DARROW_COMPUTE=ON -DARROW_FILESYSTEM=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_JSON=ON -DARROW_PARQUET=O"
-${THOME}/build/genpkg TRIBapache-arrow apache-arrow-16.0.0
+$THOME/build/cmbuild -64only -S cpp apache-arrow-16.1.0 -C "-DARROW_JEMALLOC=OFF -DARROW_BUILD_STATIC=OFF -DARROW_COMPUTE=ON -DARROW_FILESYSTEM=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_JSON=ON -DARROW_PARQUET=ON"
+${THOME}/build/genpkg TRIBapache-arrow apache-arrow-16.1.0
