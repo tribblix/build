87245b717086dbfdd4af59b03797e79cdbf9388b
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 18:19:10 +0100

    Package statsmodels for python 3.12

---
+++ TRIBpatsy-python-312/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# consumer is statsmodels which requires scipy
+# hence this not available on sparc
+#
+${THOME}/build/pkg_setup_py TRIBpatsy-python-312 patsy-0.5.6
---
+++ TRIBstatsmodels-python-312/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 -N TRIBstatsmodels-python-312 statsmodels-0.14.2
