b59f6eb870c18be915120935b29867df0a2c2f17
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 16:33:58 +0100

    Package dask-expr for python 3.12

---
+++ TRIBdask-expr-python-312/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# This is built and packaged separately from the main dask package, as
+# it expands the dependency tree, and not all dependencies are (yet)
+# available on sparc
+#
+# it's version-locked to dask, so this will need updating if the main
+# dask package is
+#
+${THOME}/build/pkg_pep518 TRIBdask-expr-python-312 dask_expr-1.1.2
