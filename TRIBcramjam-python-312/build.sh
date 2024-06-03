bc15f5438f9b68699003b4c35e8295409896769e
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 09:14:14 +0100

    Package snappy for python 3.12

---
+++ TRIBcramjam-python-312/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBcramjam-python-312 cramjam-2.8.3
---
+++ TRIBsnappy-python-312/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+
+#
+# the new version is a wrapper round cramjam, but cramjam is written in rust
+#
+${THOME}/build/pkg_setup_py TRIBsnappy-python-312 python-snappy-0.7.1
+
+#
+# old build for sparc
+#
+#${THOME}/build/pkg_setup_py TRIBsnappy-python-312 python-snappy-0.6.1
