0d980d720bba3868128ccfe4ce730b4770c611af
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 12:20:53 +0100

    Package virtualenv for python 3.12

---
+++ TRIBdistlib-python-312/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBdistlib-python-312 distlib-0.3.8
---
+++ TRIBvirtualenv-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBvirtualenv-python-312 virtualenv-20.26.2
