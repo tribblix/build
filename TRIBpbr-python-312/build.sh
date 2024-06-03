12d63b77934020e8dd3a7a9604795402b4f46bc9
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 20:36:21 +0100

    Add stevedore 5.2.0

---
+++ TRIBpbr-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpbr-python-312 pbr-6.0.0
---
+++ TRIBstevedore-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBstevedore-python-312 stevedore-5.2.0
