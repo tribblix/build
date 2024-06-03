90a5638b156ab91f114df029fea955a50cd4867c
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 21:03:10 +0100

    Update citus 12.1.3

--- TRIBblix-citus15/build.sh
+++ TRIBblix-citus15/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a postgres extension, so it's specific to the postgres version
 #
 # there is a citus-stat patch
 #
-${THOME}/build/dobuild +64only citus-12.1.2 -C PG_CONFIG=/opt/tribblix/postgres15/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus15 citus-12.1.2
+${THOME}/build/dobuild +64only citus-12.1.3 -C PG_CONFIG=/opt/tribblix/postgres15/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus15 citus-12.1.3
