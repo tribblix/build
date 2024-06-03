103a066f91b9671baca9f7b592ff74f7d0e05f73
Peter Tribble <peter.tribble@gmail.com>
2024-06-03 13:51:47 +0100

    Package scikit-learn for python 3.12

---
+++ TRIBpooch-python-312/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+${THOME}/build/pkg_pep518 TRIBpooch-python-312 pooch-1.8.1
---
+++ TRIBthreadpoolctl-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBthreadpoolctl-python-312 threadpoolctl-3.5.0
