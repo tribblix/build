8da601cbb7b3666e5dd4c20f219ccb4c9512a837
Peter Tribble <peter.tribble@gmail.com>
2024-06-05 09:27:24 +0100

    Update matwm2 0.1.2

--- TRIBmatwm2/build.sh
+++ TRIBmatwm2/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-git clone https://github.com/segin/matwm2
-cd matwm2
-sed -i 's:install :ginstall :' Makefile.in
-env CC=gcc ./configure --prefix=/usr --mandir=/usr/share/man
-gmake
-${THOME}/build/genpkg TRIBmatwm2
-cd ..
+# SPDX-License-Identifier: CDDL-1.0
+#
+# There's a patch to get configure to be consistent with the way
+# we call it
+#
+$THOME/build/dobuild -64only matwm2-0.1.2 -C "--cc=gcc --mandir=/usr/share/man"
+${THOME}/build/genpkg TRIBmatwm2 matwm2-0.1.2
