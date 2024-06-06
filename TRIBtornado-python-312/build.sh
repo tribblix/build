f27ac8a528277556cce89a491e54be7f0a875de5
Peter Tribble <peter.tribble@gmail.com>
2024-06-06 20:21:26 +0100

    Update tornado 6.4.1

--- TRIBtornado-python-312/build.sh
+++ TRIBtornado-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBtornado-python-312 tornado-6.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBtornado-python-312 tornado-6.4.1
