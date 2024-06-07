bea977ab4bbf18e5b7ff38e8b18ee5d5c511b435
Peter Tribble <peter.tribble@gmail.com>
2024-06-07 09:52:52 +0100

    Add pytesseract 0.3.10

---
+++ TRIBpytesseract-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpytesseract-python-311 pytesseract-0.3.10
---
+++ TRIBpytesseract-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpytesseract-python-312 pytesseract-0.3.10
