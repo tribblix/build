9cd3664e7c8638e79bfb63c3251ce1eb77b857af
Peter Tribble <peter.tribble@gmail.com>
2024-06-07 20:27:46 +0100

    Update tesseract 5.3.4, and build shared

--- TRIBtesseract/build.sh
+++ TRIBtesseract/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs leptonica
 #
 # downloads the training data during install, see the fixit script
 #
-env TRIBBLIX_LDFLAGS="-lsocket" $THOME/build/cmbuild -64only tesseract-5.3.2 -C  -DTESSDATA_PREFIX=/usr/share
-${THOME}/build/genpkg TRIBtesseract tesseract-5.3.2
+env TRIBBLIX_LDFLAGS="-lsocket" $THOME/build/cmbuild -64only tesseract-5.3.4 -C  "-DTESSDATA_PREFIX=/usr/share -DBUILD_SHARED_LIBS=on -DBUILD_STATIC_LIBS=no"
+${THOME}/build/genpkg TRIBtesseract tesseract-5.3.4
