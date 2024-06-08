91bba180d4d9e5686d20efc5ddf1ec288a651123
Peter Tribble <peter.tribble@gmail.com>
2024-06-08 17:03:40 +0100

    Add pdf2image 1.17.0

---
+++ TRIBpdf2image-python-311/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# uses pdftocairo and pdftoppm, hence dependency on poppler
+#
+${THOME}/build/pkg_setup_py TRIBpdf2image-python-311 pdf2image-1.17.0
