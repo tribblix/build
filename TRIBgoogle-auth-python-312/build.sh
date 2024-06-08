e6b2c259810928886bb6b1c6022d6892db1b785e
Peter Tribble <peter.tribble@gmail.com>
2024-06-08 14:06:32 +0100

    Update google-auth 2.30.0

--- TRIBgoogle-auth-python-312/build.sh
+++ TRIBgoogle-auth-python-312/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # 2.23.0 needed urllib3 <2, 2.23.1 needed urllib3>=2
 # current have urllib3 as optional but support all versions
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-312 google-auth-2.29.0
+${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-312 google-auth-2.30.0
