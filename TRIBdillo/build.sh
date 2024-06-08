10d62fa690569a721d8ec522ef64b65f8e162d41
Peter Tribble <peter.tribble@gmail.com>
2024-06-08 13:41:09 +0100

    Update dillo 3.1.1

--- TRIBdillo/build.sh
+++ TRIBdillo/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only dillo-3.1.0 -C --enable-tls
-${THOME}/build/genpkg TRIBdillo dillo-3.1.0
+${THOME}/build/dobuild -64only dillo-3.1.1 -C --enable-tls
+${THOME}/build/genpkg TRIBdillo dillo-3.1.1
