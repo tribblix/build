41944e277d8d436f4237ca10ffccee40cef332de
Peter Tribble <peter.tribble@gmail.com>
2024-06-09 19:58:26 +0100

    Add uwsgitop 0.12

---
+++ TRIBuwsgitop-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBuwsgitop-python-311 uwsgitop-0.12
---
+++ TRIBuwsgitop-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBuwsgitop-python-312 uwsgitop-0.12
