aeaf6415f435a69f3cbd882de9ce13669e665684
Peter Tribble <peter.tribble@gmail.com>
2024-06-09 15:19:28 +0100

    Rebuild cppunit for la rework, drop 32-bit

--- TRIBcppunit/build.sh
+++ TRIBcppunit/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 cppunit-1.13.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+# updates bump the SONAME
+#
+# need to build the old version using the old ABI for compatibility
+#
+env TRIBBLIX_CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ${THOME}/build/dobuild +64 cppunit-1.13.2
 ${THOME}/build/genpkg TRIBcppunit cppunit-1.13.2
