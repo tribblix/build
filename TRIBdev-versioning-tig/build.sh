c58a81731adac6a081d7c34aeb68263839a9b3b4
Peter Tribble <peter.tribble@gmail.com>
2024-06-09 11:49:24 +0100

    Update tig 2.5.10

--- TRIBdev-versioning-tig/build.sh
+++ TRIBdev-versioning-tig/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env CPPFLAGS=-I/usr/gnu/include TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" CC=gcc ${THOME}/build/dobuild -64only tig-2.5.9 -C --with-ncurses
-${THOME}/build/genpkg -gnu TRIBdev-versioning-tig tig-2.5.9
+# SPDX-License-Identifier: CDDL-1.0
+#
+env CPPFLAGS=-I/usr/gnu/include TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" CC=gcc ${THOME}/build/dobuild -64only tig-2.5.10 -C --with-ncurses
+${THOME}/build/genpkg -gnu TRIBdev-versioning-tig tig-2.5.10
