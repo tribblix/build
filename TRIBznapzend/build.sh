c943dea42a3cd0bfd6085172618505609f6081f2
Peter Tribble <peter.tribble@gmail.com>
2024-06-09 12:09:43 +0100

    Add znapzend 0.22.0

---
+++ TRIBznapzend/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# park in /usr/versions to avoid splatting stuff randomly over /usr
+#
+${THOME}/build/dobuild znapzend-0.22.0 -P /usr/versions/znapzend
+${THOME}/build/genpkg TRIBznapzend znapzend-0.22.0
