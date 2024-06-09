9bec7c74a075d348e0c9385b001c6e5d02f33b3b
Peter Tribble <peter.tribble@gmail.com>
2024-06-09 12:40:38 +0100

    Update graphviz 11.0.0

--- TRIBgraphviz/build.sh
+++ TRIBgraphviz/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only graphviz-9.0.0 -C "--disable-dependency-tracking --enable-man-pdfs=no"
-${THOME}/build/genpkg TRIBgraphviz graphviz-9.0.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only graphviz-11.0.0 -C "--disable-dependency-tracking --enable-man-pdfs=no"
+${THOME}/build/genpkg TRIBgraphviz graphviz-11.0.0
