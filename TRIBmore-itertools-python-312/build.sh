c74b164cc0acb9a3aaf7cacb99494b68a5fd99b5
Peter Tribble <peter.tribble@gmail.com>
2024-06-10 16:08:53 +0100

    Update more-itertools 10.3.0

--- TRIBmore-itertools-python-312/build.sh
+++ TRIBmore-itertools-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # this one includes a setup.py, but not one that works correctly
 #
-$THOME/build/pkg_pep518 TRIBmore-itertools-python-311 more-itertools-10.3.0
+$THOME/build/pkg_pep518 TRIBmore-itertools-python-312 more-itertools-10.3.0
