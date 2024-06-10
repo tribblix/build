e5b573dfcfe8a5c72a60c619abcb1606d692a1d8
Peter Tribble <peter.tribble@gmail.com>
2024-06-10 20:54:59 +0100

    Update caja 1.28.0

--- TRIBmate-caja/build.sh
+++ TRIBmate-caja/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs mate-desktop installing for the build
 #
-${THOME}/build/dobuild -64only -gnu caja-1.26.3 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-caja caja-1.26.3
+${THOME}/build/dobuild -64only -gnu caja-1.28.0 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-caja caja-1.28.0
