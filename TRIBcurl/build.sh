40bb4788e3e9a0e8c4a420eb6efac1a84efefdeb
Peter Tribble <peter.tribble@gmail.com>
2024-06-11 16:13:11 +0100

    Update curl 8.8.0

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 curl-8.7.1 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli --without-libpsl"
-${THOME}/build/genpkg TRIBcurl curl-8.7.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 curl-8.8.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli --without-libpsl"
+${THOME}/build/genpkg TRIBcurl curl-8.8.0
