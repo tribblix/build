0a891ad3df1cfb9a94d42cf4bb7a26527ece7add
Peter Tribble <peter.tribble@gmail.com>
2024-06-11 19:25:24 +0100

    Add libjxl 0.10.2

---
+++ TRIBimage-libjxl/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# there's a preconf script to download its dependencies
+# and a patch to align with the illumos toolchain
+#
+${THOME}/build/cmbuild -64only libjxl-0.10.2 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE"
+${THOME}/build/genpkg TRIBimage-libjxl libjxl-0.10.2
---
+++ patches/libjxl-0.10.2.pls
@@ -0,0 +1,1 @@
+-p0 libjxl.patch
