3dfc927330f7198c570a87a760b78bbc8410cf85
Peter Tribble <peter.tribble@gmail.com>
2024-06-12 16:01:03 +0100

    Update findutils 4.10.0

--- TRIBfile-gnu-findutils/build.sh
+++ TRIBfile-gnu-findutils/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +64only -gnu findutils-4.9.0 -P /usr/gnu
-${THOME}/build/genpkg TRIBfile-gnu-findutils findutils-4.9.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild +64only -gnu findutils-4.10.0 -P /usr/gnu
+${THOME}/build/genpkg TRIBfile-gnu-findutils findutils-4.10.0
