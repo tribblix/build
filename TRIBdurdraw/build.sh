3b67cdf19d1202c3472ff02f0b585557a89f5fe4
Peter Tribble <peter.tribble@gmail.com>
2024-06-12 14:01:25 +0100

    Add durdraw 0.27.1

---
+++ TRIBdurdraw/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# (built in python)
+#
+# only works in 3.12 due to the way it uses curses
+#
+${THOME}/build/pkg_setup_py -v 312 TRIBdurdraw durdraw-0.27.1
