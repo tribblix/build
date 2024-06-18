8b93c115f4a70c5e0c8823f18f6b6abf4e29a129
Peter Tribble <peter.tribble@gmail.com>
2024-06-18 19:49:14 +0100

    Add pysyncobj 0.3.12

---
+++ TRIBpysyncobj-python-311/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpysyncobj-python-311 pysyncobj-0.3.12
---
+++ TRIBpysyncobj-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpysyncobj-python-312 pysyncobj-0.3.12
