ac4278ebc84042bc26d189a19ffb947985041000
Peter Tribble <peter.tribble@gmail.com>
2024-06-18 19:51:18 +0100

    Update patroni 3.3.1

--- TRIBpatroni-python-312/build.sh
+++ TRIBpatroni-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpatroni-python-312 patroni-3.3.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpatroni-python-312 patroni-3.3.1
