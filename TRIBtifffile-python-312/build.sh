455e12435afcf6ab4c33dd4cc97d139da49d2cf3
Peter Tribble <peter.tribble@gmail.com>
2024-06-19 18:30:34 +0100

    Update tifffile 2024.6.18

--- TRIBtifffile-python-312/build.sh
+++ TRIBtifffile-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBtifffile-python-312 tifffile-2024.5.22
+${THOME}/build/pkg_setup_py TRIBtifffile-python-312 tifffile-2024.6.18
