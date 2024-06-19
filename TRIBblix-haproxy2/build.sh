3746777eb167937e769115a5da807e103c82a8eb
Peter Tribble <peter.tribble@gmail.com>
2024-06-19 19:57:41 +0100

    Update haproxy 2.6.18

--- TRIBblix-haproxy2/build.sh
+++ TRIBblix-haproxy2/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # haproxy2, installed in the same location as haproxy1,
 # the package for which is now deprecated and removed
 #
-${THOME}/build/unpack haproxy-2.6.17
-cd haproxy-2.6.17
+${THOME}/build/unpack haproxy-2.6.18
+cd haproxy-2.6.18
 
 gmake -j 4 CC=gcc TARGET=solaris USE_PCRE2=1 USE_OPENSSL=1 ARCH=64 USE_PROMEX=1
 
