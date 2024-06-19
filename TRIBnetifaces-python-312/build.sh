56f42fa54c9bc0388726390a88cf3b173a69cfce
Peter Tribble <peter.tribble@gmail.com>
2024-06-19 13:42:38 +0100

    Add python netifaces module

---
+++ TRIBnetifaces-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# there's a patch to help find getifaddrs
+#
+env CC="gcc -m64" ${THOME}/build/pkg_setup_py TRIBnetifaces-python-312 netifaces-0.11.0
