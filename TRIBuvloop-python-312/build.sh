fbf6a9284c338572fdb1d3af11a9284c4042970a
Peter Tribble <peter.tribble@gmail.com>
2024-06-20 08:48:05 +0100

    Add uvloop 0.19.0

---
+++ TRIBuvloop-python-312/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# unfortunately, SO_REUSUEPORT appears insufficently guarded
+# there's a patch and preconf to do that correctly
+# https://github.com/MagicStack/uvloop/issues/550
+#
+${THOME}/build/pkg_pep518 TRIBuvloop-python-312 uvloop-0.19.0
---
+++ patches/uvloop-0.19.0.pls
@@ -0,0 +1,1 @@
+-p1 uvloop-reuseport.patch
