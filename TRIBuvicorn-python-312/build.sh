a16885d74ea101f72dc55b53fe2e3a940b4ca9a4
Peter Tribble <peter.tribble@gmail.com>
2024-06-20 10:39:15 +0100

    Add uvicorn 0.30.1

---
+++ TRIBuvicorn-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# dependencies are not only the basic but also all of [standard]
+#
+${THOME}/build/pkg_pep518 TRIBuvicorn-python-312 uvicorn-0.30.1
