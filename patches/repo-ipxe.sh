536a2e7fcca20d763e4151553332c36b2152f836
Peter Tribble <peter.tribble@gmail.com>
2024-06-22 15:44:43 +0100

    Tidy up repo-ipxe

--- patches/repo-ipxe.sh
+++ patches/repo-ipxe.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+# }}}
+#
+# Copyright 2024 Peter Tribble
+#
+
+#
 # this script unpacks an ISO image and creates an environment suitable
 # for booting using ipxe
 #
@@ -45,8 +64,8 @@ if [ ! -f "${ISOFILE}" ]; then
     exit 1
 fi
 
-RELNAME=`echo $RELVER | sed 's:^0m:m:'`
-case `/bin/id -u` in
+RELNAME=${RELVER/0m/m}
+case $(/bin/id -u) in
     0)
 	TDIR=/mnt$RELVER
 	;;
@@ -56,48 +75,48 @@ case `/bin/id -u` in
 	;;
 esac
 
-if [ -d ${REPODIR}/${RELNAME} ]; then
+if [ -d "${REPODIR}/${RELNAME}" ]; then
     echo "Directory ${REPODIR}/${RELNAME} already exists"
 fi
 
-LDEV=`/usr/sbin/lofiadm -a ${ISOFILE}`
-mkdir -p $TDIR
-mount -F hsfs -o ro $LDEV $TDIR
+LDEV=$(/usr/sbin/lofiadm -a "${ISOFILE}")
+mkdir -p "$TDIR"
+mount -F hsfs -o ro "${LDEV}" "$TDIR"
 
 #
 # are the files where we expect them
 #
-if [ -f ${TDIR}/platform/i86pc/boot_archive ]; then
+if [ -f "${TDIR}/platform/i86pc/boot_archive" ]; then
     #
     # this is what Tribblix looks like
     #
-    mkdir -p ${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64
-    cp ${TDIR}/platform/i86pc/boot_archive ${REPODIR}/${RELNAME}/platform/i86pc
-    cp ${TDIR}/platform/i86pc/kernel/amd64/unix ${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix
-    mv ${REPODIR}/${RELNAME}/platform/i86pc/boot_archive ${REPODIR}/${RELNAME}/platform/i86pc/boot_archive.gz
-    gunzip ${REPODIR}/${RELNAME}/platform/i86pc/boot_archive.gz
-    chmod a+r ${REPODIR}/${RELNAME}/platform/i86pc/boot_archive
-    chmod a+r ${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix
-    cat > ${REPODIR}/${RELNAME}/ipxe.txt <<EOF
+    mkdir -p "${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64"
+    cp "${TDIR}/platform/i86pc/boot_archive" "${REPODIR}/${RELNAME}/platform/i86pc"
+    cp "${TDIR}/platform/i86pc/kernel/amd64/unix" "${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix"
+    mv "${REPODIR}/${RELNAME}/platform/i86pc/boot_archive" "${REPODIR}/${RELNAME}/platform/i86pc/boot_archive.gz"
+    gunzip "${REPODIR}/${RELNAME}/platform/i86pc/boot_archive.gz"
+    chmod a+r "${REPODIR}/${RELNAME}/platform/i86pc/boot_archive"
+    chmod a+r "${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix"
+    cat > "${REPODIR}/${RELNAME}/ipxe.txt" <<EOF
 #!ipxe
 dhcp
 kernel /${RELNAME}/platform/i86pc/kernel/amd64/unix
 initrd /${RELNAME}/platform/i86pc/boot_archive
 boot
 EOF
-elif [ -f ${TDIR}/platform/i86pc/amd64/boot_archive.gz ]; then
+elif [ -f "${TDIR}/platform/i86pc/amd64/boot_archive.gz" ]; then
     #
     # OmniOSce is slightly different; the boot archive on media
     # has the 64-bit path and the .gz suffix
     #
-    mkdir -p ${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64
-    mkdir -p ${REPODIR}/${RELNAME}/platform/i86pc/amd64
-    cp ${TDIR}/platform/i86pc/amd64/boot_archive.gz ${REPODIR}/${RELNAME}/platform/i86pc/amd64
-    cp ${TDIR}/platform/i86pc/kernel/amd64/unix ${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix
-    gunzip ${REPODIR}/${RELNAME}/platform/i86pc/amd64/boot_archive.gz
-    chmod a+r ${REPODIR}/${RELNAME}/platform/i86pc/amd64/boot_archive
-    chmod a+r ${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix
-    cat > ${REPODIR}/${RELNAME}/ipxe.txt <<EOF
+    mkdir -p "${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64"
+    mkdir -p "${REPODIR}/${RELNAME}/platform/i86pc/amd64"
+    cp "${TDIR}/platform/i86pc/amd64/boot_archive.gz" "${REPODIR}/${RELNAME}/platform/i86pc/amd64"
+    cp "${TDIR}/platform/i86pc/kernel/amd64/unix" "${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix"
+    gunzip "${REPODIR}/${RELNAME}/platform/i86pc/amd64/boot_archive.gz"
+    chmod a+r "${REPODIR}/${RELNAME}/platform/i86pc/amd64/boot_archive"
+    chmod a+r "${REPODIR}/${RELNAME}/platform/i86pc/kernel/amd64/unix"
+    cat > "${REPODIR}/${RELNAME}/ipxe.txt" <<EOF
 #!ipxe
 dhcp
 kernel /${RELNAME}/platform/i86pc/kernel/amd64/unix
@@ -109,7 +128,6 @@ fi
 #
 # clean up
 #
-umount $TDIR
-rmdir $TDIR
-/usr/sbin/lofiadm -d $LDEV
-
+umount "$TDIR"
+rmdir "$TDIR"
+/usr/sbin/lofiadm -d "${LDEV}"
