cfe926f80d9423278d86d0e6dbc55205fc399872
Peter Tribble <peter.tribble@gmail.com>
2024-06-22 10:48:54 +0100

    Update pycairo 1.26.1

--- TRIBpycairo-python-311/build.sh
+++ TRIBpycairo-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpycairo-python-311 pycairo-1.26.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpycairo-python-311 pycairo-1.26.1
