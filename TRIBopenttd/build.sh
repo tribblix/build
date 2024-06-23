fbf9726b8b64990b010b5b3d87d61c90cf8da735
Peter Tribble <peter.tribble@gmail.com>
2024-06-23 13:22:40 +0100

    Rebuild openttd 64-bit

--- TRIBopenttd/build.sh
+++ TRIBopenttd/build.sh
@@ -5,5 +5,5 @@
 # current requires charconv, but doesn't check for it - that came in with gcc8
 # so we need to be downrev on sparc which only has gcc7
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild openttd-13.4
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only openttd-13.4
 $THOME/build/genpkg TRIBopenttd openttd-13.4
