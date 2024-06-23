d056b1eb6fb0e6863d53ebba6dcc6df077bfd565
Peter Tribble <peter.tribble@gmail.com>
2024-06-23 13:22:19 +0100

    Explicltly disable static in libSDL2

--- TRIBlibsdl2/build.sh
+++ TRIBlibsdl2/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env TRIBBLIX_LDFLAGS="-L/usr/lib/mesa -R/usr/lib/mesa" ${THOME}/build/dobuild -64 SDL-release-2.26.5 -C SORT=/usr/gnu/bin/sort
+# SPDX-License-Identifier: CDDL-1.0
+#
+env TRIBBLIX_LDFLAGS="-L/usr/lib/mesa -R/usr/lib/mesa" ${THOME}/build/dobuild -64 SDL-release-2.26.5 -C "--disable-static SORT=/usr/gnu/bin/sort"
 ${THOME}/build/genpkg TRIBlibsdl2 SDL-release-2.26.5
