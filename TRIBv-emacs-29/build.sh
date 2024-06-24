eda478522a7b109dee921675a2d019ad9a135fc0
Peter Tribble <peter.tribble@gmail.com>
2024-06-24 09:02:58 +0100

    Update emacs 29.4

--- TRIBv-emacs-29/build.sh
+++ TRIBv-emacs-29/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-29.3 -P /usr/versions/emacs-29 -C "--with-xft --with-xpm --without-sqlite3"
-${THOME}/build/genpkg TRIBv-emacs-29 emacs-29.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-29.4 -P /usr/versions/emacs-29 -C "--with-xft --with-xpm --without-sqlite3"
+${THOME}/build/genpkg TRIBv-emacs-29 emacs-29.4
