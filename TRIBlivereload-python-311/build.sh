f89663bc088d8b84b0444e17ba9963a529763ee4
Peter Tribble <peter.tribble@gmail.com>
2024-06-24 10:22:57 +0100

    Update livereload 2.7.0

--- TRIBlivereload-python-311/build.sh
+++ TRIBlivereload-python-311/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/unpack livereload-2.6.3
-cd livereload-2.6.3
-/usr/versions/python-3.11/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBlivereload-python-311 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBlivereload-python-311 livereload-2.7.0
--- TRIBlivereload-python-312/build.sh
+++ TRIBlivereload-python-312/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/unpack livereload-2.6.3
-cd livereload-2.6.3
-/usr/versions/python-3.12/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.12/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBlivereload-python-312 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBlivereload-python-312 livereload-2.7.0
