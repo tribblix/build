1ae3ccd79154b923da7b683d67e04b3141f51ccb
Peter Tribble <peter.tribble@gmail.com>
2024-06-24 09:11:56 +0100

    Emacs security patch

--- TRIBv-emacs-28/build.sh
+++ TRIBv-emacs-28/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-28.2 -P /usr/versions/emacs-28 -C "--without-xft --with-xpm --with-cairo"
 ${THOME}/build/genpkg TRIBv-emacs-28 emacs-28.2
