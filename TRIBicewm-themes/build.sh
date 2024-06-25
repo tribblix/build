fc6653547ea5f5be249696526b05bf04ace5cead
Peter Tribble <peter.tribble@gmail.com>
2024-06-25 08:50:55 +0100

    Update IceWM 3.6.0

--- TRIBicewm-themes/build.sh
+++ TRIBicewm-themes/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only icewm-extra-themes-1.2
-${THOME}/build/genpkg TRIBicewm-themes icewm-extra-themes-1.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+# the version number in pkginfo is made up, the old repo was 1.2
+# and the new repo doesn't have any numbering
+#
+#${THOME}/build/dobuild -64only icewm-extra
+#${THOME}/build/genpkg TRIBicewm-themes icewm-extra
+${THOME}/build/pkg_tarball -P usr/share/icewm TRIBicewm-themes icewm-extra
--- TRIBicewm/build.sh
+++ TRIBicewm/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only icewm-3.5.0
-${THOME}/build/genpkg TRIBicewm icewm-3.5.0
+${THOME}/build/dobuild -64only icewm-3.6.0
+${THOME}/build/genpkg TRIBicewm icewm-3.6.0
