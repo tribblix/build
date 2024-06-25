ce06385597cf2054d7d9567aa8a0c22ad67b3a14
Peter Tribble <peter.tribble@gmail.com>
2024-06-25 18:10:06 +0100

    Rebuild Qt6 64-bit

--- TRIBqt6/build.sh
+++ TRIBqt6/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # need to make sure the qt filename matches the unpacked directory name,
 # sometimes an extra "opensource" appears in the tarball
 #
@@ -11,23 +13,13 @@
 #
 ${THOME}/build/unpack qt-everywhere-src-6.2.6
 #
-# 32-bit build is nice and clean
+# 64-bit build needs a fair amount of work to get 64-bit consistently
 #
 cd qt-everywhere-src-6.2.6
-env PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11
+env CXX="g++ -m64" CC="gcc -m64" PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:/usr/bin/amd64:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -skip qtwebengine -skip qtquick3dphysics -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11 -bindir /usr/versions/Qt-6/bin -libdir /usr/versions/Qt-6/lib/amd64 -plugindir /usr/versions/Qt-6/plugins/amd64 -qmldir /usr/versions/Qt-6/qml/amd64 -platform solaris-g++-64 CMAKE_PREFIX_PATH=`pwd`/qtbase/lib/amd64/cmake CMAKE_LIBRARY_ARCHITECTURE=amd64
 mkdir qtbase/translations
-env PATH=/usr/gnu/bin:$PATH gmake -j 8
+env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:/usr/bin/amd64:$PATH gmake -j 8
 cd ..
-
-#
-# 64-bit build doesn't work, I think something isn't passing the
-# 64-bit all the way through
-#
-#cd qt-everywhere-src-6.2.6-64bit
-#env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11 -bindir /usr/versions/Qt-6/bin -libdir /usr/versions/Qt-6/lib/amd64 -plugindir /usr/versions/Qt-6/plugins/amd64 -qmldir /usr/versions/Qt-6/qml/amd64 -platform solaris-g++-64 CMAKE_PREFIX_PATH=`pwd`/qtbase/lib/amd64/cmake
-#mkdir qtbase/translations
-#env PATH=/usr/gnu/bin:$PATH gmake -j 8
-#cd ..
 
 # gnu to get a working "install" binary
 rm -fr /tmp/qt6
