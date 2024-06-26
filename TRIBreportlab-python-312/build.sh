fd10302d6a7803c390b21f5b127204114cca9822
Peter Tribble <peter.tribble@gmail.com>
2024-06-26 08:23:53 +0100

    Update reportlab 4.2.2

--- TRIBreportlab-python-312/build.sh
+++ TRIBreportlab-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBreportlab-python-312 reportlab-4.2.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBreportlab-python-312 reportlab-4.2.2
