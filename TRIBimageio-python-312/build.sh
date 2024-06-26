e7198bcbac6a52e165ccaa6ebb74aa9972d8ee57
Peter Tribble <peter.tribble@gmail.com>
2024-06-26 10:26:38 +0100

    Update imageio 2.34.2

--- TRIBimageio-python-312/build.sh
+++ TRIBimageio-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBimageio-python-312 imageio-2.34.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBimageio-python-312 imageio-2.34.2
