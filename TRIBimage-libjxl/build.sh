96da660165020f1df71913c018b5934a1b46b402
Peter Tribble <peter.tribble@gmail.com>
2024-06-27 21:41:14 +0100

    Update libjxl 0.10.3

--- TRIBimage-libjxl/build.sh
+++ TRIBimage-libjxl/build.sh
@@ -5,5 +5,5 @@
 # there's a preconf script to download its dependencies
 # and a patch to align with the illumos toolchain
 #
-${THOME}/build/cmbuild -64only libjxl-0.10.2 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE"
-${THOME}/build/genpkg TRIBimage-libjxl libjxl-0.10.2
+${THOME}/build/cmbuild -64only libjxl-0.10.3 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE"
+${THOME}/build/genpkg TRIBimage-libjxl libjxl-0.10.3
