347f59f266ff2df8eece0f0c512d3a222a40d49b
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 16:44:46 +0100

    Update python-snappy 0.7.2

--- TRIBsnappy-python-311/build.sh
+++ TRIBsnappy-python-311/build.sh
@@ -6,4 +6,4 @@
 #
 # the new version is a wrapper round cramjam, but cramjam is written in rust
 #
-${THOME}/build/pkg_setup_py TRIBsnappy-python-311 python-snappy-0.7.1
+${THOME}/build/pkg_setup_py TRIBsnappy-python-311 python_snappy-0.7.2
