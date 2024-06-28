e3b204efcf4a73202cf0fff951677a2f74e08459
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 19:50:57 +0100

    Update Vault 1.17.1

--- TRIBblix-vault/build.sh
+++ TRIBblix-vault/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 mkdir ~/ud/v
 cd ~/ud/v
 git clone https://github.com/hashicorp/vault.git
 cd vault
-git checkout v1.15.5
+git checkout v1.17.1
 
 # needs gnu grep for the version check to work
-env PATH=/usr/versions/go-1.21/bin:/usr/gnu/bin:$PATH gmake bootstrap
-env PATH=/usr/versions/go-1.21/bin:/usr/gnu/bin:$PATH gmake
+env PATH=/usr/versions/go-1.22/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake bootstrap
+env PATH=/usr/versions/go-1.22/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/vault/bin
