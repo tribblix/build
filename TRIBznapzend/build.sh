489213d07813aa349a1ca90c3c8b4b2f24cf37d2
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 09:42:49 +0100

    Update znapzend 0.23.2

--- TRIBznapzend/build.sh
+++ TRIBznapzend/build.sh
@@ -4,5 +4,5 @@
 #
 # park in /usr/versions to avoid splatting stuff randomly over /usr
 #
-${THOME}/build/dobuild znapzend-0.22.0 -P /usr/versions/znapzend
-${THOME}/build/genpkg TRIBznapzend znapzend-0.22.0
+${THOME}/build/dobuild znapzend-0.23.2 -P /usr/versions/znapzend
+${THOME}/build/genpkg TRIBznapzend znapzend-0.23.2
