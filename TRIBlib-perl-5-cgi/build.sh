69fc8462f4e1bc87d36286f82c0628550cffd3a2
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 09:54:35 +0100

    Update perl CGI 4.66

--- TRIBlib-perl-5-cgi/build.sh
+++ TRIBlib-perl-5-cgi/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
-# cgi.pm needs html-parser
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack CGI-4.54
-cd CGI-4.54
+# cgi.pm needs URI and html-parser
+#
+${THOME}/build/unpack CGI-4.66
+cd CGI-4.66
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBlib-perl-5-cgi
