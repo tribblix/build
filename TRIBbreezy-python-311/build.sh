b27c89364e34e1d07109f561eebe292946243a07
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 09:28:38 +0100

    Update breezy 3.3.8

--- TRIBbreezy-python-311/build.sh
+++ TRIBbreezy-python-311/build.sh
@@ -3,4 +3,5 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # needs gnu msgfmt
-env LD_RUN_PATH=/usr/versions/python-3.11/lib ${THOME}/build/pkg_setup_py -gnu TRIBbreezy-python-311 breezy-3.3.7
+#
+env LD_RUN_PATH=/usr/versions/python-3.11/lib ${THOME}/build/pkg_setup_py -gnu TRIBbreezy-python-311 breezy-3.3.8
--- TRIBbreezy-python-312/build.sh
+++ TRIBbreezy-python-312/build.sh
@@ -3,4 +3,5 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # needs gnu msgfmt
-env LD_RUN_PATH=/usr/versions/python-3.12/lib ${THOME}/build/pkg_setup_py -gnu TRIBbreezy-python-312 breezy-3.3.7
+#
+env LD_RUN_PATH=/usr/versions/python-3.12/lib ${THOME}/build/pkg_setup_py -gnu TRIBbreezy-python-312 breezy-3.3.8
