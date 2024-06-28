cf41ba754291a96e59ab116b35672e6b786c9871
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 16:14:57 +0100

    Enable libjxl plugins

--- TRIBimage-libjxl/build.sh
+++ TRIBimage-libjxl/build.sh
@@ -5,5 +5,8 @@
 # there's a preconf script to download its dependencies
 # and a patch to align with the illumos toolchain
 #
-${THOME}/build/cmbuild -64only libjxl-0.10.3 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE"
+# we build the plugins and package, but don't declare a runtime dependency
+# the dependency is only used in the event it's resolved, so is unnecessary
+#
+${THOME}/build/cmbuild -64only libjxl-0.10.3 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE -DJPEGXL_ENABLE_PLUGINS=TRUE"
 ${THOME}/build/genpkg TRIBimage-libjxl libjxl-0.10.3
