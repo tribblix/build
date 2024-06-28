796fac8a270e16447444d71829cbcac3bfadc65d
Peter Tribble <peter.tribble@gmail.com>
2024-06-28 16:21:27 +0100

    Update nettle 3.10

--- TRIBnettle/build.sh
+++ TRIBnettle/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 nettle-3.9.1 -C --disable-static
-${THOME}/build/genpkg TRIBnettle nettle-3.9.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 nettle-3.10 -C --disable-static
+${THOME}/build/genpkg TRIBnettle nettle-3.10
