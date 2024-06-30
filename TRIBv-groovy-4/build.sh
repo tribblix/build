9ce11d06f1830e7897bd6a45cbe649f1fc40813d
Peter Tribble <peter.tribble@gmail.com>
2024-06-30 18:00:55 +0100

    Update Groovy 4.0.22

--- TRIBv-groovy-4/build.sh
+++ TRIBv-groovy-4/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball TRIBv-groovy-4 apache-groovy-binary-4.0.21
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_tarball TRIBv-groovy-4 apache-groovy-binary-4.0.22
