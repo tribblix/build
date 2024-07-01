a5c6df4066987fe2ea5104e58af343e87ad820c8
Peter Tribble <peter.tribble@gmail.com>
2024-07-01 13:55:00 +0100

    Go back to the old closed cpp

--- TRIBdev-macro-cpp/build.sh
+++ TRIBdev-macro-cpp/build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 #
-# this version doesn't work on sparc for the gate build
+# SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/dobuild https://github.com/illumos/cpp
-${THOME}/build/genpkg TRIBdev-macro-cpp cpp
+# the source version was shipped as package version 1.0.0
+# but doesn't work for the gate build, or cde
+# the closed binary was originally shipped as version 0.9.0
+#
+#$THOME/build/dobuild https://github.com/illumos/cpp
+#${THOME}/build/genpkg TRIBdev-macro-cpp cpp
+#
+# Save the old binary like so
+# (cd / ; tar cf ${THOME}/build/patches/binary-cpp.tar usr/lib/cpp)
+# or for sparc
+# (cd / ; tar cf ${THOME}/build/patches/binary-cpp.sparc.tar usr/lib/cpp)
+#
+${THOME}/build/genpkg TRIBdev-macro-cpp
