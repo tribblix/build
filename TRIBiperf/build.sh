1248926f6bd3dff3f28af538226833ac3e311b23
Peter Tribble <peter.tribble@gmail.com>
2024-07-01 13:42:49 +0100

    Update iperf 3.17.1

--- TRIBiperf/build.sh
+++ TRIBiperf/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-#
-${THOME}/build/dobuild -64only iperf-3.16
-${THOME}/build/genpkg TRIBiperf iperf-3.16
+${THOME}/build/dobuild -64only iperf-3.17.1 -C --enable-static=no
+${THOME}/build/genpkg TRIBiperf iperf-3.17.1
