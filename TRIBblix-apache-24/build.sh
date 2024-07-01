3089347cce35ab8dc29918d7af07ff15556fe01d
Peter Tribble <peter.tribble@gmail.com>
2024-07-01 14:03:54 +0100

    Update Apache httpd 2.4.60

--- TRIBblix-apache-24/build.sh
+++ TRIBblix-apache-24/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # there's a preconf file to unpack apr
 #
 # explicitly disable md to avoid pulling in jansson and curl
 #
-${THOME}/build/dobuild +64only httpd-2.4.59 -P /opt/tribblix/apache2 -C "--enable-mods-shared=most --disable-md --enable-ssl --enable-proxy --enable-proxy-ajp"
-${THOME}/build/genpkg TRIBblix-apache-24 httpd-2.4.59
+${THOME}/build/dobuild +64only httpd-2.4.60 -P /opt/tribblix/apache2 -C "--enable-mods-shared=most --disable-md --enable-ssl --enable-proxy --enable-proxy-ajp"
+${THOME}/build/genpkg TRIBblix-apache-24 httpd-2.4.60
