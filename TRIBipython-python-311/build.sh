403af4fda5df65777d8fa8d0d1c771b709c4935d
Peter Tribble <peter.tribble@gmail.com>
2024-07-01 09:02:43 +0100

    Update IPython 8.26.0

--- TRIBipython-python-311/build.sh
+++ TRIBipython-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBipython-python-311 ipython-8.25.0
+${THOME}/build/pkg_setup_py TRIBipython-python-311 ipython-8.26.0
--- TRIBipython-python-312/build.sh
+++ TRIBipython-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBipython-python-312 ipython-8.25.0
+${THOME}/build/pkg_setup_py TRIBipython-python-312 ipython-8.26.0
