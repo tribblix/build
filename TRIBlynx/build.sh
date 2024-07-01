459c4059c042e06e6c8ea3dae6508a827981a9a5
Peter Tribble <peter.tribble@gmail.com>
2024-07-01 16:11:49 +0100

    Update lynx 2.9.2

--- TRIBlynx/build.sh
+++ TRIBlynx/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the curses logic is a bit convoluted and needs a little help
 # otherwise you get missing symbol delete_screen
 #
-env TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only lynx2.9.0 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines --with-screen=ncurses --with-curses-dir=/usr/gnu"
-${THOME}/build/genpkg -I "install install-help" TRIBlynx lynx2.9.0
+# don't have gnu iconv installed, as it will only be partially detected
+# and won't link
+#
+env TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only lynx2.9.2 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines --with-screen=ncurses --with-curses-dir=/usr/gnu"
+${THOME}/build/genpkg -I "install install-help" TRIBlynx lynx2.9.2
