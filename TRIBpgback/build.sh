06b894875e696dbab46dc79de71a54ebe1e6a383
Peter Tribble <peter.tribble@gmail.com>
2024-07-01 14:42:41 +0100

    Update pg_back to 2.4.0 (on x86)

--- TRIBpgback/build.sh
+++ TRIBpgback/build.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 #
-# v2 is written in go; stick with v1 for simplicity and
-# SPARC support
+# SPDX-License-Identifier: CDDL-1.0
+#
+# v2 is written in go; stick with v1 on SPARC
 #
 # everything is done by the fixit script
 #
-${THOME}/build/genpkg TRIBpgback
+mkdir p
+cd p
+env GOPATH=`pwd` go install github.com/orgrim/pg_back@latest
+
+#
+# copy the binary
+#
+rm -fr /tmp/pgb
+mkdir -p /tmp/pgb/usr/bin
+cp bin/pg_back /tmp/pgb/usr/bin
+
+#
+# put in a sensible configuration file
+#
+mkdir -p /tmp/pgb/etc/postgresql
+cp pkg/mod/github.com/orgrim/pg_back@v*/pg_back.conf /tmp/pgb/etc/postgresql
+sed -i 's:bin_directory =:bin_directory = /opt/tribblix/postgres15/bin:' /tmp/pgb/etc/postgresql/pg_back.conf
+
+${THOME}/build/create_pkg TRIBpgback /tmp/pgb
+rm -fr /tmp/pgb
