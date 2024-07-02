d8ac5966263b80c6f22d92c08de4c64c2f15b636
Peter Tribble <peter.tribble@gmail.com>
2024-07-02 20:00:00 +0100

    Update vultr-cli 3.2.0

--- TRIBvultr-cli/build.sh
+++ TRIBvultr-cli/build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #
-# Note the v2 in the path
+# SPDX-License-Identifier: CDDL-1.0
 #
-env GOPATH=`pwd` go install github.com/vultr/vultr-cli/v2@latest
+# Note the v3 in the path
+#
+env GOPATH=`pwd` go install github.com/vultr/vultr-cli/v3@latest
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/vultr-cli
 cp bin/vultr-cli /tmp/cs/usr/bin
-cp pkg/mod/github.com/vultr/vultr-cli/v2@*/README.md pkg/mod/github.com/vultr/vultr-cli/v2@*/LICENSE /tmp/cs/usr/share/vultr-cli
+cp pkg/mod/github.com/vultr/vultr-cli/v3@*/README.md pkg/mod/github.com/vultr/vultr-cli/v3@*/LICENSE /tmp/cs/usr/share/vultr-cli
 ${THOME}/build/create_pkg TRIBvultr-cli /tmp/cs
 rm -fr /tmp/cs
