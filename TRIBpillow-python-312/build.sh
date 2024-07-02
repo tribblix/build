0f0a8a738284b802e6c46c0602f6508952ac0d25
Peter Tribble <peter.tribble@gmail.com>
2024-07-02 08:15:21 +0100

    Update Pillow 10.4.0

--- TRIBpillow-python-312/build.sh
+++ TRIBpillow-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpillow-python-312 pillow-10.3.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpillow-python-312 pillow-10.4.0
