7271242cc7b30ef5131b57cd93f48b43af3e9d3c
Peter Tribble <peter.tribble@gmail.com>
2024-07-02 08:21:13 +0100

    Update ipykernel 6.29.5

--- TRIBipykernel-python-311/build.sh
+++ TRIBipykernel-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBipykernel-python-311 ipykernel-6.29.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBipykernel-python-311 ipykernel-6.29.5
