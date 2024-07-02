77e0bae657103f3556ed7510a7d7b7c3e3df0f78
Peter Tribble <peter.tribble@gmail.com>
2024-07-02 19:07:04 +0100

    Add forgejo 7.0.4

---
+++ TRIBblix-forgejo/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+$THOME/build/unpack forgejo-src-7.0.4
+cd forgejo-src-7.0.4
+#
+# download the proper tarball, not the automated source dump
+# this way, the assets are pregenerated and all you have to do
+# is build the binary
+#
+env TAGS="bindata timetzdata sqlite sqlite_unlock_notify" gmake build
+
+#
+# yes, the executable is called gitea, because forgejo is a gitea
+# fork, and it's that way for compatibility
+# we add a symlink so that both names work
+#
+rm -fr /tmp/ee
+mkdir -p /tmp/ee/opt/tribblix/forgejo/bin
+cp gitea /tmp/ee/opt/tribblix/forgejo/bin
+ln -s gitea /tmp/ee/opt/tribblix/forgejo/bin/forgejo
+cp README.md /tmp/ee/opt/tribblix/forgejo
+${THOME}/build/create_pkg TRIBblix-forgejo /tmp/ee
+rm -fr /tmp/ee
