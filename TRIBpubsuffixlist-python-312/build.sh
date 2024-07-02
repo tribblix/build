24784b29b62e7a09e9aca7932b0d330f69609b87
Peter Tribble <peter.tribble@gmail.com>
2024-07-02 08:31:16 +0100

    Update publicsuffixlist 1.0.1.20240702

--- TRIBpubsuffixlist-python-312/build.sh
+++ TRIBpubsuffixlist-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBpubsuffixlist-python-312 publicsuffixlist-0.10.1.20240618
+${THOME}/build/pkg_setup_py TRIBpubsuffixlist-python-312 publicsuffixlist-1.0.1.20240702
