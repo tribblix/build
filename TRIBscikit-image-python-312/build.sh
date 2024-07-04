c37aea302df4c90a9ab59e6be875da0373fbde6c
Peter Tribble <peter.tribble@gmail.com>
2024-07-04 18:41:58 +0100

    Update scikit-image 0.24.0

--- TRIBscikit-image-python-312/build.sh
+++ TRIBscikit-image-python-312/build.sh
@@ -12,4 +12,4 @@
 # PATH so it can find the cython executable
 # Need to pass -m64 down to meson
 #
-env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH  ${THOME}/build/pkg_pep518 -N TRIBscikit-image-python-312 scikit_image-0.23.2
+env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH  ${THOME}/build/pkg_pep518 -N TRIBscikit-image-python-312 scikit_image-0.24.0
