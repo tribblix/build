32e3f63803891f2d95cd5c0081c4697c1d80d65a
Peter Tribble <peter.tribble@gmail.com>
2024-07-04 13:22:56 +0100

    Add openstack swift for testing

---
+++ TRIBswift-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# part of openstack
+#
+${THOME}/build/pkg_setup_py TRIBswift-python-312 swift-2.33.0
---
+++ TRIBswiftclient-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBswiftclient-python-312 python-swiftclient-4.5.0
