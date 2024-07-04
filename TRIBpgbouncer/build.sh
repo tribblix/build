d5818a139c6185b3b23d81959eda6379ca1a3024
Peter Tribble <peter.tribble@gmail.com>
2024-07-04 10:18:27 +0100

    Update pgbouncer 1.23.0

--- TRIBpgbouncer/build.sh
+++ TRIBpgbouncer/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # there's a sacrificial static libevent built by the preconf script
 #
-env TRIBBLIX_PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig ${THOME}/build/dobuild -64only pgbouncer-1.22.1 -C --without-cares
-${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.22.1
+env TRIBBLIX_PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig ${THOME}/build/dobuild -64only pgbouncer-1.23.0 -C --without-cares
+${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.23.0
 rm -fr /tmp/mc
