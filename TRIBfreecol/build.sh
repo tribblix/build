f665903b2c993610310be65528f1f4006692fd86
Peter Tribble <peter.tribble@gmail.com>
2024-07-05 08:59:46 +0100

    Update freecol 1.2.0

--- TRIBfreecol/build.sh
+++ TRIBfreecol/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # download the zip and just run from that
 #
-${THOME}/build/pkg_tarball TRIBfreecol freecol-1.1.0
+${THOME}/build/pkg_tarball TRIBfreecol freecol-1.2.0
