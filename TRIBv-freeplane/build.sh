09ce4f7cc966b1200fe9dd581751e6e758c6a581
Peter Tribble <peter.tribble@gmail.com>
2024-07-06 09:55:39 +0100

    Update freeplane 1.11.14

--- TRIBv-freeplane/build.sh
+++ TRIBv-freeplane/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_tarball TRIBv-freeplane freeplane_bin-1.11.12
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_tarball TRIBv-freeplane freeplane_bin-1.11.14
