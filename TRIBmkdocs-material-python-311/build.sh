d93eca1bccd7149f7e6e36b92316b094f99c0a63
Peter Tribble <peter.tribble@gmail.com>
2024-07-07 17:48:50 +0100

    Update mkdocs-material 9.5.28

--- TRIBmkdocs-material-python-311/build.sh
+++ TRIBmkdocs-material-python-311/build.sh
@@ -5,4 +5,4 @@
 # mkdocs-material and mkdocs-material-extensions have a circular dependency,
 # so package them together
 #
-${THOME}/build/pkg_pep518 TRIBmkdocs-material-python-311 mkdocs_material-9.5.25 mkdocs_material_extensions-1.3.1
+${THOME}/build/pkg_pep518 TRIBmkdocs-material-python-311 mkdocs_material-9.5.28 mkdocs_material_extensions-1.3.1
