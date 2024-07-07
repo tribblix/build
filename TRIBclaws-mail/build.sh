60cce395a5c04323df671f0d2cbdfdd4b955a2a3
Peter Tribble <peter.tribble@gmail.com>
2024-07-07 10:54:40 +0100

    Update claws-mail 4.3.0

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs a preconf patch otherwise it fails to find libetpan
 # the preconf patch also fixes up claws' incorrect notions of
 # where the certificate bundle happens to live
 #
 zap uninstall TRIBlibcanberra
-${THOME}/build/dobuild -64only claws-mail-4.2.0 -C "--disable-ldap --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.2.0
+${THOME}/build/dobuild -64only claws-mail-4.3.0 -C "--disable-ldap --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.3.0
