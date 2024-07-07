bc839808a155736383f55895a1b2563135f22bd8
Peter Tribble <peter.tribble@gmail.com>
2024-07-07 17:24:33 +0100

    Rebuild inkscape to pick up newer hunspell

--- TRIBinkscape/build.sh
+++ TRIBinkscape/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # install the *mm packages, sigc++, TRIBboehm-gc TRIBlibgsl TRIBboost
 #
 $THOME/build/cmbuild -gnu -64only inkscape-1.2.2_2022-12-01_b0a8486541 -C "-DWITH_OPENMP=OFF -DWITH_INTERNAL_2GEOM=ON"
