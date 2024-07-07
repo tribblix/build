0c26d9fa736fd5e86c5b62f54354c1bffc87b26a
Peter Tribble <peter.tribble@gmail.com>
2024-07-07 10:34:50 +0100

    Rebuild enchant to pick up newer hunspell

--- TRIBenchant/build.sh
+++ TRIBenchant/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/dobuild -64only enchant-1.6.1 -C "--disable-aspell --with-myspell-dir=/usr/share/hunspell"
 ${THOME}/build/genpkg TRIBenchant enchant-1.6.1
