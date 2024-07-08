f6310592097d6c6e4039ebcdc489019b1f545689
Peter Tribble <peter.tribble@gmail.com>
2024-07-08 20:14:58 +0100

    Update loki 2.9.9

--- TRIBblix-loki/build.sh
+++ TRIBblix-loki/build.sh
@@ -11,17 +11,18 @@ mkdir -p src/github.com/grafana
 cd src/github.com/grafana
 git clone https://github.com/grafana/loki
 cd $GOPATH/src/github.com/grafana/loki
+git checkout v2.9.9
 #
 # fix vendor/go.etcd.io/etcd/client/pkg/v3/transport/sockopt_unix.go
 # just error for the REUSEPORT case
 # import "errors"
 # return errors.New("Unsupported\n")
 #
-env PATH=/usr/versions/go-1.21/bin:$PATH go build ./cmd/loki
+env PATH=/usr/versions/go-1.22/bin:$PATH go build ./cmd/loki
 ls -l ./loki
 # vendor/github.com/grafana/tail/watch/file_posix.go - build solaris
 # vendor/github.com/grafana/tail/tail_posix.go - build solaris
-env PATH=/usr/versions/go-1.21/bin:$PATH go build ./clients/cmd/promtail
+env PATH=/usr/versions/go-1.22/bin:$PATH go build ./clients/cmd/promtail
 ls -l ./promtail
 
 rm -fr /tmp/ee
