1f74e4f9a4f00056b403f430f359f35a288b36cb
Peter Tribble <peter.tribble@gmail.com>
2024-07-08 17:58:17 +0100

    Update terraform 1.7.5, and build with go 1.22

--- TRIBterraform/build.sh
+++ TRIBterraform/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # need to checkout precisely the version we're going to build
 #
 # there are also binaries on https://www.terraform.io/downloads.html
@@ -9,8 +11,8 @@ mkdir tf
 cd tf
 git clone https://github.com/hashicorp/terraform
 cd terraform
-git checkout v1.7.3
-env PATH=/usr/versions/go-1.21/bin:$PATH go install .
+git checkout v1.7.5
+env PATH=/usr/versions/go-1.22/bin:$PATH go install .
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
