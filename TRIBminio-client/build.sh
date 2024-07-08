ce96df504e7f56dd8240081d717512edc45ad391
Peter Tribble <peter.tribble@gmail.com>
2024-07-08 17:18:09 +0100

    Rebuild minio client with go 1.22

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # need to pin to a release before containerd/console was wrecked
 #
 mkdir ~/ud/c
 cd ~/ud/c
-env GOPATH=`pwd` PATH=/usr/versions/go-1.21/bin:$PATH go install github.com/minio/mc@RELEASE.2023-07-21T20-44-27Z
+env GOPATH=`pwd` PATH=/usr/versions/go-1.22/bin:$PATH go install github.com/minio/mc@RELEASE.2023-07-21T20-44-27Z
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
