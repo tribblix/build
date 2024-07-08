dccf3f588b9ccf307664872c4bae2ae0c4d4fcf7
Peter Tribble <peter.tribble@gmail.com>
2024-07-08 11:07:56 +0100

    Update gdbm 1.24

--- TRIBgdbm/build.sh
+++ TRIBgdbm/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only gdbm-1.23
-${THOME}/build/genpkg TRIBgdbm gdbm-1.23
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only gdbm-1.24
+${THOME}/build/genpkg TRIBgdbm gdbm-1.24
