9625a018f7d5a8edbad5144cb567860169520601
Peter Tribble <peter.tribble@gmail.com>
2024-07-08 20:25:34 +0100

    Update humanize 4.10.0

--- TRIBhumanize-python-312/build.sh
+++ TRIBhumanize-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBhumanize-python-312 humanize-4.9.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBhumanize-python-312 humanize-4.10.0
