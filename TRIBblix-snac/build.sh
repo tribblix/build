02d820e122c87b82ae4c1c9824d1ea4346e1a52b
Peter Tribble <peter.tribble@gmail.com>
2024-07-09 07:53:32 +0100

    Add snac 2.56

---
+++ TRIBblix-snac/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+env CC=gcc CFLAGS="-O -m64" LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64only https://codeberg.org/grunfink/snac2
+sed -i -e 's:=/usr/local:=/opt/tribblix/snac:' -e 's: $(PREFIX: $(DESTDIR)$(PREFIX:' -e 's:install -:ginstall -:' snac2-64bit/Makefile
+${THOME}/build/genpkg TRIBblix-snac snac2
