c57bcad21829e9d8694c8ad5ce017fefd83087ae
Peter Tribble <peter.tribble@gmail.com>
2024-07-10 22:14:22 +0100

    Update Django 4.2.14 5.0.7

--- TRIBdjango5-python-311/build.sh
+++ TRIBdjango5-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBdjango5-python-311 Django-5.0.6
+${THOME}/build/pkg_setup_py TRIBdjango5-python-311 Django-5.0.7
--- TRIBdjango5-python-312/build.sh
+++ TRIBdjango5-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBdjango5-python-312 Django-5.0.6
+${THOME}/build/pkg_setup_py TRIBdjango5-python-312 Django-5.0.7
