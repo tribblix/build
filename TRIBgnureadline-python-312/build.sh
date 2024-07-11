d77bfb81f212097a36e49e9dc6247cf331dff34c
Peter Tribble <peter.tribble@gmail.com>
2024-07-11 13:24:55 +0100

    Update gnureadline 8.2.10

--- TRIBgnureadline-python-312/build.sh
+++ TRIBgnureadline-python-312/build.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/unpack gnureadline-8.1.2
-cd gnureadline-8.1.2
+# SPDX-License-Identifier: CDDL-1.0
 #
 # we need -m64 in CFLAGS here because it's passed through to the
 # build of the private copy of readline
 #
-env CFLAGS="-m64 -I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" /usr/versions/python-3.12/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.12/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBgnureadline-python-312 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+env CFLAGS="-m64 -I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/pkg_setup_py TRIBgnureadline-python-312 gnureadline-8.2.10
