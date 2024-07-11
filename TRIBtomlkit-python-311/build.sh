2d56cd75ce4a37b2588de94c02ff986a9aa7de46
Peter Tribble <peter.tribble@gmail.com>
2024-07-11 08:08:54 +0100

    Update tomlkit 0.13.0

--- TRIBtomlkit-python-311/build.sh
+++ TRIBtomlkit-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBtomlkit-python-311 tomlkit-0.12.5
+${THOME}/build/pkg_pep518 TRIBtomlkit-python-311 tomlkit-0.13.0
--- TRIBtomlkit-python-312/build.sh
+++ TRIBtomlkit-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBtomlkit-python-312 tomlkit-0.12.5
+${THOME}/build/pkg_pep518 TRIBtomlkit-python-312 tomlkit-0.13.0
