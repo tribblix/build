1baad3269c1188fdb00d13a19ba5bbdc57177892
Peter Tribble <peter.tribble@gmail.com>
2024-07-11 15:37:41 +0100

    Update harfbuzz 9.0.0

--- TRIBharfbuzz/build.sh
+++ TRIBharfbuzz/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # with icu (which it used to have anyway)
 # with graphite2 (for LibreOffice)
 #
+# harfbuzz make strong guarantees about ABI compatibility, so updating
+# should always be safe
+#
 zap install TRIBlibicu
-$THOME/build/mesonbuild -64 harfbuzz-4.3.0 -C "-Dintrospection=disabled -Dglib=enabled -Dcairo=enabled -Dicu=enabled -Dfreetype=enabled -Dgraphite2=enabled"
-${THOME}/build/genpkg TRIBharfbuzz harfbuzz-4.3.0
+$THOME/build/mesonbuild -64 harfbuzz-9.0.0 -C "-Dintrospection=disabled -Dglib=enabled -Dcairo=enabled -Dicu=enabled -Dfreetype=enabled -Dgraphite2=enabled"
+${THOME}/build/genpkg TRIBharfbuzz harfbuzz-9.0.0
