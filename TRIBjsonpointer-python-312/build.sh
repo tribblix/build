52f965532717b90b460e7a0451c9635990392f51
Peter Tribble <peter.tribble@gmail.com>
2024-07-12 13:32:16 +0100

    Update jsonpointer 3.0.0

--- TRIBjsonpointer-python-312/build.sh
+++ TRIBjsonpointer-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBjsonpointer-python-312 jsonpointer-2.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBjsonpointer-python-312 jsonpointer-3.0.0
