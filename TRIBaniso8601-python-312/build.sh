46d8740ea7113d237ed88bcd00700084eff3eb30
Peter Tribble <peter.tribble@gmail.com>
2024-07-12 13:18:20 +0100

    Add Flask-RESTful 0.3.10

---
+++ TRIBaniso8601-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBaniso8601-python-312 aniso8601-9.0.1
---
+++ TRIBflask-restful-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBflask-restful-python-312 Flask-RESTful-0.3.10
