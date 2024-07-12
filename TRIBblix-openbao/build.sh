1f65a29e8967c81ad3baac0efe3909a535f836c0
Peter Tribble <peter.tribble@gmail.com>
2024-07-12 15:57:38 +0100

    Add openbao

---
+++ TRIBblix-openbao/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+mkdir ~/ud/v
+cd ~/ud/v
+git clone https://github.com/openbao/openbao.git
+cd openbao
+
+# needs gnu grep for the version check to work
+#
+# ~/go/pkg/mod/gotest.tools/gotestsum@v1.10.0/internal/filewatcher/term_unix.go
+# tcSet -> unix.TCSETS
+# tcGet -> unix.TCGETS
+env PATH=/usr/versions/go-1.22/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake bootstrap
+env PATH=/usr/versions/go-1.22/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake
+
+rm -fr /tmp/ee
+mkdir -p /tmp/ee/opt/tribblix/openbao/bin
+cp bin/bao /tmp/ee/opt/tribblix/openbao/bin
+cp README.md /tmp/ee/opt/tribblix/openbao
+${THOME}/build/create_pkg TRIBblix-openbao /tmp/ee
+rm -fr /tmp/ee
