da83a6aaaba0aea18a08bbfd1a5e8cfff5977dc7
Peter Tribble <peter.tribble@gmail.com>
2024-07-14 21:03:29 +0100

    Add Hetzner cloud cli

---
+++ TRIBhcloud/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+mkdir h
+cd h
+env GOPATH=`pwd` go install github.com/hetznercloud/cli/cmd/hcloud@latest
+rm -fr /tmp/cs
+mkdir -p /tmp/cs/usr/bin
+mkdir -p /tmp/cs/usr/share/hcloud
+cp bin/hcloud /tmp/cs/usr/bin
+cp pkg/mod/github.com/hetznercloud/cli@*/README.md pkg/mod/github.com/hetznercloud/cli@*/LICENSE /tmp/cs/usr/share/hcloud
+${THOME}/build/create_pkg TRIBhcloud /tmp/cs
+rm -fr /tmp/cs
