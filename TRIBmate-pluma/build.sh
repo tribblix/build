9510a274a4eef6b4375e3cc87f2dda8b12fe7799
Peter Tribble <peter.tribble@gmail.com>
2024-07-15 14:33:11 +0100

    Update pluma 1.28.0

--- TRIBmate-pluma/build.sh
+++ TRIBmate-pluma/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild +gnu -64only pluma-1.26.1 -C "--disable-introspection LIBS=-lsocket"
-${THOME}/build/genpkg TRIBmate-pluma pluma-1.26.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild +gnu -64only pluma-1.28.0 -C "--disable-introspection LIBS=-lsocket"
+${THOME}/build/genpkg TRIBmate-pluma pluma-1.28.0
