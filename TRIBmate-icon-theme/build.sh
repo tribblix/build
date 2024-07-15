abdd043024cba53a311f9a1d07a89300e2cd7847
Peter Tribble <peter.tribble@gmail.com>
2024-07-15 14:41:28 +0100

    Update mate-icon-theme 1.28.0

--- TRIBmate-icon-theme/build.sh
+++ TRIBmate-icon-theme/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu mate-icon-theme-1.26.0
-${THOME}/build/genpkg TRIBmate-icon-theme mate-icon-theme-1.26.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -gnu mate-icon-theme-1.28.0
+${THOME}/build/genpkg TRIBmate-icon-theme mate-icon-theme-1.28.0
