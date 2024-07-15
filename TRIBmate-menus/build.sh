f38c53dbf6ecd28b0c06186f1b959ef2e65e9751
Peter Tribble <peter.tribble@gmail.com>
2024-07-15 14:47:00 +0100

    Update mate-menus 1.28.0

--- TRIBmate-menus/build.sh
+++ TRIBmate-menus/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only -gnu mate-menus-1.26.1 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-menus mate-menus-1.26.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only -gnu mate-menus-1.28.0 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-menus mate-menus-1.28.0
