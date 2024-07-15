ba857f3cd49321a7c918ba5ea5e90d99122af7c5
Peter Tribble <peter.tribble@gmail.com>
2024-07-15 14:08:25 +0100

    Update mate-common-1.28.0 mate-desktop-1.28.2 libmatemixer-1.28.0

--- TRIBmate-desktop/build.sh
+++ TRIBmate-desktop/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # bundle mate-common and libmatemixer in with this rather than creating a
 # separate package
 #
-${THOME}/build/dobuild -64only -gnu libmatemixer-1.26.1 -C --enable-oss
-${THOME}/build/dobuild mate-common-1.26.0
-${THOME}/build/dobuild -64only -gnu mate-desktop-1.26.2 -C --disable-introspection
-${THOME}/build/genpkg TRIBmate-desktop mate-common-1.26.0 mate-desktop-1.26.2 libmatemixer-1.26.1
+${THOME}/build/dobuild -64only -gnu libmatemixer-1.28.0 -C --enable-oss
+${THOME}/build/dobuild mate-common-1.28.0
+${THOME}/build/dobuild -64only -gnu mate-desktop-1.28.2 -C --disable-introspection
+${THOME}/build/genpkg TRIBmate-desktop mate-common-1.28.0 mate-desktop-1.28.2 libmatemixer-1.28.0
