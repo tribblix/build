d4349fee010c2d996f2351d88f04e30e7545be03
Peter Tribble <peter.tribble@gmail.com>
2024-07-15 14:23:38 +0100

    Update marco 1.28.1

--- TRIBmate-marco/build.sh
+++ TRIBmate-marco/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only -gnu marco-1.26.2
-${THOME}/build/genpkg TRIBmate-marco marco-1.26.2
+${THOME}/build/dobuild -64only -gnu marco-1.28.1
+${THOME}/build/genpkg TRIBmate-marco marco-1.28.1
