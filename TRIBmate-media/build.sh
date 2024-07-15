9b5a58061499088b73e5c0505e956ee74050bd72
Peter Tribble <peter.tribble@gmail.com>
2024-07-15 14:37:29 +0100

    Update mate-media 1.28.1

--- TRIBmate-media/build.sh
+++ TRIBmate-media/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # wants the freedesktop sound them TRIBfreedesktop-sound-theme
 # not listed as a hard dependency here, but pulled in via the mate overlay
 #
-${THOME}/build/dobuild -64only +gnu mate-media-1.26.2 -C --disable-dependency-tracking
-${THOME}/build/genpkg TRIBmate-media mate-media-1.26.2
+${THOME}/build/dobuild -64only +gnu mate-media-1.28.1 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBmate-media mate-media-1.28.1
