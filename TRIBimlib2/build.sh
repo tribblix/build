635ac6ee8cad1c3230bc26e1863d3e88e4446743
Peter Tribble <peter.tribble@gmail.com>
2024-07-16 21:31:05 +0100

    Update imlib2 1.12.3

--- TRIBimlib2/build.sh
+++ TRIBimlib2/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # it tries to build 32-bit assembler on 64-bit, so need to be specific
 #
-${THOME}/build/dobuild -64only imlib2-1.12.1 +C --enable-amd64
-${THOME}/build/genpkg TRIBimlib2 imlib2-1.12.1
+${THOME}/build/dobuild -64only imlib2-1.12.3 +C --enable-amd64
+${THOME}/build/genpkg TRIBimlib2 imlib2-1.12.3
