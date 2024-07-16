5972d147841440771cd251f11eaac893805fdded
Peter Tribble <peter.tribble@gmail.com>
2024-07-16 09:41:54 +0100

    Update JasPer 4.2.4, drop compat and 32-bit

--- TRIBgraphics-jasper/build.sh
+++ TRIBgraphics-jasper/build.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 #
-# this is the old compatibility build; we install the things we don't want to
-# keep into known locations and the fixit script knows to clean them up
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 jasper-1.900.1 -C "--enable-shared --disable-static --disable-opengl --program-suffix=.19001 --includedir=/usr/include.19001 --mandir=/usr/man.19001"
-#
-# this is the new build; disable opengl to keep the dependency tree
+# disable opengl to keep the dependency tree
 # tight; ditto heif which is marked experimental
 #
-$THOME/build/cmbuild -64 jasper-4.1.0 -C "-DJAS_ENABLE_OPENGL=OFF -DJAS_ENABLE_LIBHEIF=OFF"
-${THOME}/build/genpkg TRIBgraphics-jasper jasper-1.900.1 jasper-4.1.0
+$THOME/build/cmbuild -64only jasper-4.2.4 -C "-DJAS_ENABLE_OPENGL=OFF -DJAS_ENABLE_LIBHEIF=OFF -DALLOW_IN_SOURCE_BUILD=TRUE"
+${THOME}/build/genpkg TRIBgraphics-jasper jasper-4.2.4
