658890884adeb1c816356ab2e225e870ffa2159a
Peter Tribble <peter.tribble@gmail.com>
2024-07-16 13:19:49 +0100

    Update libmateweather 1.28.0

--- TRIBmate-weather/build.sh
+++ TRIBmate-weather/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # actually libmateweather
 #
-${THOME}/build/dobuild -64only -gnu libmateweather-1.26.3
-${THOME}/build/genpkg TRIBmate-weather libmateweather-1.26.3
+${THOME}/build/dobuild -64only -gnu libmateweather-1.28.0
+${THOME}/build/genpkg TRIBmate-weather libmateweather-1.28.0
