5731c510d4512e56765ea96c165fa633c2f716ed
Peter Tribble <peter.tribble@gmail.com>
2024-07-16 10:12:17 +0100

    Update psycopg 3.2.1

--- TRIBpsycopg-312/build.sh
+++ TRIBpsycopg-312/build.sh
@@ -11,4 +11,4 @@
 # just a binary download from pypi and illumos isn't provided, but the
 # [c] extra, aka "local installation", should be equivalent
 #
-env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg-312 psycopg-3.1.19 psycopg_pool-3.2.2 psycopg_c-3.1.19
+env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg-312 psycopg-3.2.1 psycopg_pool-3.2.2 psycopg_c-3.2.1
