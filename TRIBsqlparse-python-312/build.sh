0a6badc8196623977298354a9091f05a0235e0bd
Peter Tribble <peter.tribble@gmail.com>
2024-07-16 08:17:50 +0100

    Update sqlparse 0.5.1

--- TRIBsqlparse-python-312/build.sh
+++ TRIBsqlparse-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBsqlparse-python-312 sqlparse-0.5.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBsqlparse-python-312 sqlparse-0.5.1
