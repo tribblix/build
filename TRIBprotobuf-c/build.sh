29adf53698596d4e0cdab950c96068f18acdc964
Peter Tribble <peter.tribble@gmail.com>
2024-07-16 19:15:10 +0100

    Rebuild protobuf-c to match protobuf signature changes

--- TRIBprotobuf-c/build.sh
+++ TRIBprotobuf-c/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/dobuild -64only protobuf-c-1.5.0 -C --disable-dependency-tracking
 ${THOME}/build/genpkg TRIBprotobuf-c protobuf-c-1.5.0
