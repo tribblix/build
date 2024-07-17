8b579b5a0a5392ed7e47e6a1b46d9358dec409f3
Peter Tribble <peter.tribble@gmail.com>
2024-07-17 13:36:21 +0100

    Update borgbackup 1.4.0

--- TRIBborg-python-311/build.sh
+++ TRIBborg-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBborg-python-311 borgbackup-1.2.8
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBborg-python-311 borgbackup-1.4.0
