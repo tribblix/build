c664a923acd86869ac5ea355c583bb4cf9291cb2
Peter Tribble <peter.tribble@gmail.com>
2024-07-18 21:13:05 +0100

    Switch sbom to python 3.12

--- TRIBsbom2dot-python-312/build.sh
+++ TRIBsbom2dot-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_wheel TRIBsbom2dot-python-312 sbom2dot-0.3.0-py2.py3-none-any.whl
--- TRIBsbom4files-python-311/build.sh
+++ TRIBsbom4files-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_wheel TRIBsbom4files-python-311 sbom4files-0.4.0-py2.py3-none-any.whl
