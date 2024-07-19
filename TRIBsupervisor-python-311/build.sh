263cb58672223b7c3e257cd7f4db48831c0f24b5
Peter Tribble <peter.tribble@gmail.com>
2024-07-19 09:03:06 +0100

    Switch supervisord to python 3.12

--- TRIBsupervisor-python-311/build.sh
+++ TRIBsupervisor-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_setup_py TRIBsupervisor-python-311 supervisor-4.2.5
--- TRIBsupervisor-python-312/build.sh
+++ TRIBsupervisor-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_setup_py TRIBsupervisor-python-312 supervisor-4.2.5
