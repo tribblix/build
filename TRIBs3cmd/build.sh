35eddebc04d415917d08c68f2ed2f584caeaacd2
Peter Tribble <peter.tribble@gmail.com>
2024-07-20 15:42:18 +0100

    Switch s3cmd to python 3.12

--- TRIBs3cmd/build.sh
+++ TRIBs3cmd/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs dateutil and magic
 #
-${THOME}/build/pkg_setup_py -v 311 TRIBs3cmd s3cmd-2.4.0
+${THOME}/build/pkg_setup_py -v 312 TRIBs3cmd s3cmd-2.4.0
