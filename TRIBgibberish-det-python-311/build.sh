2ddf60414240d41a695d06a7b61308cfc024f73d
Peter Tribble <peter.tribble@gmail.com>
2024-07-20 14:06:45 +0100

    Switch gibberish_detector to python 3.12

--- TRIBgibberish-det-python-311/build.sh
+++ TRIBgibberish-det-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_setup_py TRIBgibberish-det-python-311 gibberish_detector-0.1.1
--- TRIBgibberish-det-python-312/build.sh
+++ TRIBgibberish-det-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_setup_py TRIBgibberish-det-python-312 gibberish_detector-0.1.1
