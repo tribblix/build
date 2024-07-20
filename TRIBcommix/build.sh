673ffb3773a79b5fce56cd1459d73198e4d1c6dc
Peter Tribble <peter.tribble@gmail.com>
2024-07-20 19:10:37 +0100

    Switch commix to python 3.12

--- TRIBcommix/build.sh
+++ TRIBcommix/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # depends on python
 # needs the gnureadline module
 #
-${THOME}/build/pkg_setup_py -v 311 TRIBcommix commix-3.9
+${THOME}/build/pkg_setup_py -v 312 TRIBcommix commix-3.9
