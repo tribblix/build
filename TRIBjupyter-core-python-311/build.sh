c39f958ba65029385ba1b8dd47db5d58128a44cd
Peter Tribble <peter.tribble@gmail.com>
2024-07-20 21:17:01 +0100

    Switch jupyter_core to python 3.12

--- TRIBjupyter-core-python-311/build.sh
+++ TRIBjupyter-core-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_pep518 TRIBjupyter-core-python-311 jupyter_core-5.7.2
--- TRIBjupyter-core-python-312/build.sh
+++ TRIBjupyter-core-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_pep518 TRIBjupyter-core-python-312 jupyter_core-5.7.2
