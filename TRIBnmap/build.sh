1847818a27755751fafc24f3fea94ecf3dd4f05d
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 14:54:22 +0100

    Switch nmap to python 3.12

--- TRIBnmap/build.sh
+++ TRIBnmap/build.sh
@@ -4,5 +4,5 @@
 #
 # zenmap requires pygtk, which we don't have
 #
-env CC=gcc ${THOME}/build/dobuild -64only nmap-7.95 -C "--without-zenmap --with-liblua=included --with-libpcre PYTHON=python3.11"
+env CC=gcc ${THOME}/build/dobuild -64only nmap-7.95 -C "--without-zenmap --with-liblua=included --with-libpcre PYTHON=python3.12"
 ${THOME}/build/genpkg TRIBnmap nmap-7.95
