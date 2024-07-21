8fc7375a20c7ac84141f94541cb40810aa82a83b
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 14:28:55 +0100

    Update SCons 4.8.0, and switch to python 3.12

--- TRIBscons/build.sh
+++ TRIBscons/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # (built in python)
 #
-${THOME}/build/pkg_setup_py -v 311 TRIBscons SCons-4.7.0
+${THOME}/build/pkg_setup_py -v 312 TRIBscons SCons-4.8.0
