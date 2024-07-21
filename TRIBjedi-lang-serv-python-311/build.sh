b573b0876ad55eca886d8166bbddbb9db5c96189
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 10:13:52 +0100

    Switch jedi-language-server to python 3.12

--- TRIBjedi-lang-serv-python-311/build.sh
+++ TRIBjedi-lang-serv-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_pep518 TRIBjedi-lang-serv-python-311 jedi_language_server-0.41.4
--- TRIBjedi-lang-serv-python-312/build.sh
+++ TRIBjedi-lang-serv-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/pkg_pep518 TRIBjedi-lang-serv-python-312 jedi_language_server-0.41.4
