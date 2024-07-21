83d7cdef1944411080ca41be634b87971640df51
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 16:42:11 +0100

    Switch pluma-plugins to python 3.12

--- TRIBmate-pluma-plugins/build.sh
+++ TRIBmate-pluma-plugins/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild +gnu -64only pluma-plugins-1.28.0 -C --disable-dependency-tracking
+${THOME}/build/dobuild +gnu -64only pluma-plugins-1.28.0 -C "--disable-dependency-tracking PYTHON=/usr/bin/python3.12"
 ${THOME}/build/genpkg TRIBmate-pluma-plugins pluma-plugins-1.28.0
