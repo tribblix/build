a432702d424450cf77ef689e1db291ca52c8ce3f
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 14:04:58 +0100

    Switch gnu units to python 3.12

--- TRIBgnu-units/build.sh
+++ TRIBgnu-units/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# units_cur is a python script
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only units-2.23 -P /usr/gnu
+# units_cur is a python script, tell it precisely which python to use
+#
+${THOME}/build/dobuild -64only units-2.23 -P /usr/gnu -C PYTHON=/usr/bin/python3.12
 ${THOME}/build/genpkg TRIBgnu-units units-2.23
