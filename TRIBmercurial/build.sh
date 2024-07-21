12d85975da0f7268f3d71a4f944b0922034cb0a8
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 14:23:34 +0100

    Update mercurial 6.8, and switch to python 3.12

--- TRIBmercurial/build.sh
+++ TRIBmercurial/build.sh
@@ -2,15 +2,15 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-zap install TRIBdocutils-python-311
-${THOME}/build/unpack mercurial-6.7.4
-cd mercurial-6.7.4
+zap install TRIBdocutils-python-312
+${THOME}/build/unpack mercurial-6.8
+cd mercurial-6.8
 sed -i 's:install -d:ginstall -d:' Makefile
 sed -i 's:install -m:ginstall -m:' Makefile
 sed -i 's:install -m:ginstall -m:' doc/Makefile
-gmake PREFIX=/usr PYTHON=python3.11 all
+gmake PREFIX=/usr PYTHON=python3.12 all
 rm -fr /tmp/hg1
-gmake PREFIX=/usr PYTHON=python3.11 DESTDIR=/tmp/hg1 install
+gmake PREFIX=/usr PYTHON=python3.12 DESTDIR=/tmp/hg1 install
 ${THOME}/build/create_pkg TRIBmercurial /tmp/hg1
 cd ..
 rm -fr /tmp/hg1
