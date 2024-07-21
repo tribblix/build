7a8a99cd1e96e3b653b711f440ff0998f0df5c73
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 13:55:36 +0100

    Switch duviz to python 3.12

--- TRIBduviz/build.sh
+++ TRIBduviz/build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 rm -fr /tmp/dvp
 mkdir -p /tmp/dvp/usr/bin
 cd /tmp/dvp/usr/bin
 wget https://raw.github.com/soxofaan/duviz/master/duviz.py
 ln -s duviz.py duviz
 chmod a+x duviz.py
-sed -i 's:/usr/bin/env python:/usr/bin/python3.11:' duviz.py
+sed -i 's:/usr/bin/env python:/usr/bin/python3.12:' duviz.py
 ${THOME}/build/create_pkg TRIBduviz /tmp/dvp
 cd
 rm -fr /tmp/dvp
