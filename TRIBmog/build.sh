cf8d47128499801e2c12ac1038992184bbac95b7
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 13:52:27 +0100

    Switch mog to python 3.12

--- TRIBmog/build.sh
+++ TRIBmog/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs pygments and file-magic and mediainfo
 #
 ${THOME}/build/unpack mog-0.6.0
@@ -8,9 +10,9 @@ cd mog-0.6.0
 # we need the file from file-magic
 #
 gsed -i "s:\['file':\['file-5.45':" mog/__init__.py
-/usr/versions/python-3.11/bin/python3  setup.py build
+/usr/versions/python-3.12/bin/python3  setup.py build
 rm -fr /tmp/sut
-/usr/versions/python-3.11/bin/python3  setup.py install --root=/tmp/sut
+/usr/versions/python-3.12/bin/python3  setup.py install --root=/tmp/sut
 ${THOME}/build/create_pkg TRIBmog /tmp/sut
 cd ..
 rm -fr /tmp/sut
