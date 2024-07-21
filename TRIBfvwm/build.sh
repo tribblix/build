01f939ee2444a35d6773d3ae3aa5f94d3ce3edc5
Peter Tribble <peter.tribble@gmail.com>
2024-07-21 19:30:09 +0100

    Switch fvwm to python 3.12

--- TRIBfvwm/build.sh
+++ TRIBfvwm/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# This version requires pyxdg (TRIBxdg-python-311)
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only fvwm-2.7.0 -C "--disable-bidi --disable-perllib"
+# This version requires pyxdg (TRIBxdg-python-312)
+#
+${THOME}/build/dobuild -64only fvwm-2.7.0 -C "--disable-bidi"
 ${THOME}/build/genpkg TRIBfvwm fvwm-2.7.0
