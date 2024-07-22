6c44a82352f4914a825c899cafb887218b88e035
Peter Tribble <peter.tribble@gmail.com>
2024-07-22 07:59:47 +0100

    Update pure-eval 0.2.3

--- TRIBpure-eval-python-311/build.sh
+++ TRIBpure-eval-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpure-eval-python-311 pure_eval-0.2.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpure-eval-python-311 pure_eval-0.2.3
