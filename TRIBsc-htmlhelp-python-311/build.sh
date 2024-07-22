b5513d44afa4dbc8615e542ba6b165b894679fa5
Peter Tribble <peter.tribble@gmail.com>
2024-07-22 07:51:07 +0100

    Update sphinxcontrib-htmlhelp 2.0.6

--- TRIBsc-htmlhelp-python-311/build.sh
+++ TRIBsc-htmlhelp-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBsc-htmlhelp-python-311 sphinxcontrib_htmlhelp-2.0.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBsc-htmlhelp-python-311 sphinxcontrib_htmlhelp-2.0.6
