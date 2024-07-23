9c730f11b6c7dab90429ef1ad514f23675d55dca
Peter Tribble <peter.tribble@gmail.com>
2024-07-23 10:24:48 +0100

    Use python 3.12 to bootstrap ninja

--- TRIBninja/build.sh
+++ TRIBninja/build.sh
@@ -2,12 +2,12 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# look at src/version.cc to check the version string
+# check out release, look at src/version.cc to check the version string
 #
 git clone https://github.com/ninja-build/ninja.git
 cd ninja
 git checkout release
-./configure.py --bootstrap
+python3.12 ./configure.py --bootstrap
 rm -fr /tmp/n1
 mkdir -p /tmp/n1/usr/bin
 cp ninja /tmp/n1/usr/bin
