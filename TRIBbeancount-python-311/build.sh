e61e10919184392714a9fa3ca2cb33bc4a1c2d6d
Peter Tribble <peter.tribble@gmail.com>
2024-07-23 09:18:40 +0100

    Polish beancount a bit

--- TRIBbeancount-python-311/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_setup_py TRIBbeancount-python-311 beancount-3.0.0
--- TRIBbeancount-python-312/build.sh
+++ TRIBbeancount-python-312/build.sh
@@ -4,5 +4,7 @@
 #
 # the requirements aren't terribly well specified, but see also what's in
 # beancount/scripts/deps.py to add magic ply dateutil requests
+# beangulp needs beautifulsoup chardet click lxml magic
+# beanprice needs dateutil requests
 #
-${THOME}/build/pkg_setup_py TRIBbeancount-python-312 beancount-3.0.0
+${THOME}/build/pkg_setup_py TRIBbeancount-python-312 beancount-3.0.0 beanprice-1.2.1 beangulp-0.1.1
