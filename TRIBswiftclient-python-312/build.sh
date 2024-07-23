79bd9ecc64da8784effbad0d8e78b91c45b1ad94
Peter Tribble <peter.tribble@gmail.com>
2024-07-23 08:03:26 +0100

    Update python-swiftclient 4.6.0

--- TRIBswiftclient-python-312/build.sh
+++ TRIBswiftclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBswiftclient-python-312 python-swiftclient-4.5.0
+${THOME}/build/pkg_pep518 TRIBswiftclient-python-312 python-swiftclient-4.6.0
