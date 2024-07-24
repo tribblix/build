3573c4a9e8779c8a055365f9b96ce45f5bdfacc8
Peter Tribble <peter.tribble@gmail.com>
2024-07-24 08:48:11 +0100

    Update pv 1.8.12

--- TRIBpipe-viewer/build.sh
+++ TRIBpipe-viewer/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only pv-1.8.10 -C --disable-nls
-${THOME}/build/genpkg TRIBpipe-viewer pv-1.8.10
+${THOME}/build/dobuild -64only pv-1.8.12 -C --disable-nls
+${THOME}/build/genpkg TRIBpipe-viewer pv-1.8.12
