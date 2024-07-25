b5a1eab718a89dab6910cda5cad2f9a1ebff3f73
Peter Tribble <peter.tribble@gmail.com>
2024-07-25 11:22:07 +0100

    Don't disable introspection in pluma

--- TRIBmate-pluma/build.sh
+++ TRIBmate-pluma/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild +gnu -64only pluma-1.28.0 -C "--disable-introspection LIBS=-lsocket"
+${THOME}/build/dobuild +gnu -64only pluma-1.28.0 -C LIBS=-lsocket
 ${THOME}/build/genpkg TRIBmate-pluma pluma-1.28.0
