b471ec8f23f85887238d2714f833061c5f1557d4
Peter Tribble <peter.tribble@gmail.com>
2024-07-27 13:40:35 +0100

    Typo in wfuzz comment

--- TRIBwfuzz-python-312/build.sh
+++ TRIBwfuzz-python-312/build.sh
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# needs patches for compatiblity with current ecosystem
+# needs patches for compatibility with current ecosystem
 #
 ${THOME}/build/pkg_setup_py TRIBwfuzz-python-312 wfuzz-3.1.0
