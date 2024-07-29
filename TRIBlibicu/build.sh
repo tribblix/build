261787b3567625c493e444b468a3de039079134d
Peter Tribble <peter.tribble@gmail.com>
2024-07-29 10:45:56 +0100

    Update icu4c 74.2, with 50 and 68 compat

--- TRIBlibicu/build.sh
+++ TRIBlibicu/build.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 #
-# [in tarballs, rename the tarball to icu.tar.gz]
-# mv icu4c-68_2-src.tgz icu.tar.gz
+# SPDX-License-Identifier: CDDL-1.0
 #
-# the old 50.1 shared libraries are copied in by the fixit step
-# for compatibility
+# the old shared libraries are copied in by the fixit step
+# for compatibility, as the SONAME gets bumped each release
 #
-${THOME}/build/dobuild -64 icu -S source
-rm -fr /tmp/ii
-cd icu-64bit/source
-gmake install DESTDIR=/tmp/ii
-cd ../../icu/source
-gmake install DESTDIR=/tmp/ii
-cd ../..
-${THOME}/build/create_pkg TRIBlibicu /tmp/ii
-rm -fr /tmp/ii
+${THOME}/build/dobuild -64 -n icu icu4c-74_2-src -S source -C "CC=gcc CXX=g++ --disable-tests --disable-examples"
+${THOME}/build/genpkg TRIBlibicu icu4c-74_2-src
