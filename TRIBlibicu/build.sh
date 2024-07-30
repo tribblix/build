f1cc59c611c2a75266eeec3dbdb1242d0db757ee
Peter Tribble <peter.tribble@gmail.com>
2024-07-30 10:32:22 +0100

    Bring icu4c back to 72.1, for wider compatibility

--- TRIBlibicu/build.sh
+++ TRIBlibicu/build.sh
@@ -5,5 +5,5 @@
 # the old shared libraries are copied in by the fixit step
 # for compatibility, as the SONAME gets bumped each release
 #
-${THOME}/build/dobuild -64 -n icu icu4c-74_2-src -S source -C "CC=gcc CXX=g++ --disable-tests --disable-examples"
-${THOME}/build/genpkg TRIBlibicu icu4c-74_2-src
+${THOME}/build/dobuild -64 -n icu icu4c-72_1-src -S source -C "CC=gcc CXX=g++ --disable-tests --disable-examples"
+${THOME}/build/genpkg TRIBlibicu icu4c-72_1-src
