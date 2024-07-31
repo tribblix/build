d5c56fa6259ebaef384d4e3cf81a3b371081a3f9
Peter Tribble <peter.tribble@gmail.com>
2024-07-31 13:01:47 +0100

    Rebuild libxslt with python 3.12

--- TRIBlibxslt/build.sh
+++ TRIBlibxslt/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # follow the OI build, as that's what we've had until now
+#
+# delivers some python pieces, see fixit, but we don't declare a dependency
+#
 ${THOME}/build/dobuild -64only libxslt-1.1.39 -C "--disable-static --with-pic --without-crypto"
 ${THOME}/build/genpkg TRIBlibxslt libxslt-1.1.39
