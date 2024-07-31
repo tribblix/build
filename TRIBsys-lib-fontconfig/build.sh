6261cb74bde042077dfa9c43ea5467df96b535d3
Peter Tribble <peter.tribble@gmail.com>
2024-07-31 10:06:24 +0100

    Update fontconfig 2.15.0

--- TRIBsys-lib-fontconfig/build.sh
+++ TRIBsys-lib-fontconfig/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 fontconfig-2.14.2 -C "--sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBsys-lib-fontconfig fontconfig-2.14.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 fontconfig-2.15.0 -C "--sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBsys-lib-fontconfig fontconfig-2.15.0
