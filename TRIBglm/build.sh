8e60c4f9fb2037a9776a3511b84d038e556f3e51
Peter Tribble <peter.tribble@gmail.com>
2024-07-31 22:49:36 +0100

    Add glm 1.0.1

---
+++ TRIBglm/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild -64only glm-1.0.1 -C "-DGLM_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON"
+${THOME}/build/genpkg TRIBglm glm-1.0.1
