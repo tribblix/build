4cd6eec64092553881742203d316151003a8ea13
Peter Tribble <peter.tribble@gmail.com>
2024-07-31 13:53:53 +0100

    Update scribus 1.6.2

--- TRIBscribus/build.sh
+++ TRIBscribus/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
-env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only scribus-1.6.1 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DWANT_CPP17=ON"
+# SPDX-License-Identifier: CDDL-1.0
+#
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only scribus-1.6.2 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DWANT_CPP17=ON"
 
-${THOME}/build/genpkg TRIBscribus scribus-1.6.1
+${THOME}/build/genpkg TRIBscribus scribus-1.6.2
