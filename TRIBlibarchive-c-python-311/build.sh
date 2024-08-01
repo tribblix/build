1a0233ef4fa1c561fb4895819c034753d6055dbd
Peter Tribble <peter.tribble@gmail.com>
2024-08-01 15:48:50 +0100

    Drop python 3.11 version of diffoscope

--- TRIBlibarchive-c-python-311/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/pkg_setup_py TRIBlibarchive-c-python-311 libarchive-c-5.1
--- TRIBprogressbar-python-311/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# used by diffoscope - it wants this, not progressbar3 or progressbar4
-#
-${THOME}/build/pkg_setup_py TRIBprogressbar-python-311 progressbar-2.5
