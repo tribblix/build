7e08b533cb59b762aa0128f7c5a6d4f2f4ec597d
Peter Tribble <peter.tribble@gmail.com>
2024-08-01 15:32:43 +0100

    Switch pkg5 to python 3.12 (first try)

--- TRIBdev-pkg5/build.sh
+++ TRIBdev-pkg5/build.sh
@@ -21,19 +21,26 @@ git clone https://github.com/omniosorg/pkg5
 cd pkg5
 # this one works, newer ones seen to fail building pydantic
 # with rust errors
-git checkout b73cf9dac236c95dcf1b62b9966606183c2ad2ca
+#
+# this was the python3.11 checkout
+#
+#git checkout b73cf9dac236c95dcf1b62b9966606183c2ad2ca
+#
+# and this is the one used for python 3.12 on m35
+#
+git checkout 145220ee6ceb537305371f55d6b50328f2e0f113
 cd src
 
 #
-# we have 64-bit python 3.11, which may not be the default in the source
+# we have 64-bit python 3.12, which may not be the default in the source
 #
-#sed -i s:3.10:3.11: Makefile.com
-#sed -i s:python3.10:python3.11: `grep -rl python3.11 .`
+#sed -i s:3.11:3.12: Makefile.com
+#sed -i s:python3.11:python3.12: `grep -rl python3.11 .`
 
 #
 # use a regular gcc
 #
-sed -i s:gcc-12:gcc: Makefile.com
+sed -i s:gcc-13:gcc: Makefile.com
 #
 # don't do the ctfconvert piece
 #
@@ -41,7 +48,7 @@ sed -i s:/opt/onbld/bin/i386/ctfconvert:true: Makefile.com
 #
 # align the PYTHONPATH with Tribblix so it can find modules
 #
-sed -i 's=:/usr/lib/python$PYVER/vendor-packages=:/usr/versions/python-3.11/lib/python3.11/site-packages=' ../tools/installmodules
+sed -i 's=:/usr/lib/python$PYVER/vendor-packages=:/usr/versions/python-3.12/lib/python3.12/site-packages=' ../tools/installmodules
 #
 # pkg has an odd way of fiddling the module path instead of using
 # the normal vendoring mechanism
@@ -49,6 +56,10 @@ sed -i 's=:/usr/lib/python$PYVER/vendor-packages=:/usr/versions/python-3.11/lib/
 sed -i '/strip_site()$/d' modules/site_paths/__init__.py
 sed -i '/sys.path,/d' modules/site_paths/__init__.py
 sed -i '/sys.path.extend/d' modules/site_paths/__init__.py
+#
+# this really doesn't work
+#
+sed -i 's:-L/lib ::' brand/ipkg/Makefile
 #
 # NB gmake doesn't work
 #
@@ -58,7 +69,7 @@ cd ../proto/root_*
 #
 # I have no idea why this is required, but it doesn't work at all
 #
-sed -i 's:psinfo_size = 232:psinfo_size = 236:' usr/lib/python3.11/vendor-packages/pkg/misc.py
+sed -i 's:psinfo_size = 232:psinfo_size = 236:' usr/lib/python3.12/vendor-packages/pkg/misc.py
 
 #
 # we don't need anything to do with running a pkg server or any of the
@@ -68,9 +79,9 @@ rm -fr etc lib var
 rm -fr usr/include
 rm -fr usr/lib/brand usr/lib/zones
 rm usr/lib/pkg.*
-mkdir -p usr/versions/python-3.11/lib
-mv usr/lib/python3.11 usr/versions/python-3.11/lib
-mv usr/versions/python-3.11/lib/python3.11/vendor-packages usr/versions/python-3.11/lib/python3.11/site-packages
+mkdir -p usr/versions/python-3.12/lib
+mv usr/lib/python3.12 usr/versions/python-3.12/lib
+mv usr/versions/python-3.12/lib/python3.12/vendor-packages usr/versions/python-3.12/lib/python3.12/site-packages
 rm -fr usr/share/lib/pkg/web
 rm -f usr/share/lib/pkg/rad-invoke
 rm -fr usr/share/man/ja_JP.UTF-8 usr/share/man/zh_CN.UTF-8
@@ -85,6 +96,6 @@ mv usr/share/man/man7/pkg.keep usr/share/man/man7/pkg.7
 #
 # spaces...
 #
-mv 'usr/lib/pkg/python3.11/jaraco/text/Lorem ipsum.txt' 'usr/lib/pkg/python3.11/jaraco/text/Lorem_ipsum.txt'
+mv 'usr/lib/pkg/python3.12/jaraco/text/Lorem ipsum.txt' 'usr/lib/pkg/python3.12/jaraco/text/Lorem_ipsum.txt'
 
 ${THOME}/build/create_pkg TRIBdev-pkg5 `pwd`
