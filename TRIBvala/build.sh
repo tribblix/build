23caf868f346ccf02e908b673c13fa3d354de3c4
Peter Tribble <peter.tribble@gmail.com>
2024-08-02 17:02:58 +0100

    Drop 32-bit vala

--- TRIBvala/build.sh
+++ TRIBvala/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 vala-0.56.1 -C --disable-valadoc
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only vala-0.56.1 -C --disable-valadoc
 ${THOME}/build/genpkg TRIBvala vala-0.56.1
