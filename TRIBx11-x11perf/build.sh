4e7679ded4045c0a687e969ccfef42c872757af3
Peter Tribble <peter.tribble@gmail.com>
2024-08-02 10:11:35 +0100

    Update x11perf 1.7.0

--- TRIBx11-x11perf/build.sh
+++ TRIBx11-x11perf/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only x11perf-1.6.2
-${THOME}/build/genpkg TRIBx11-x11perf x11perf-1.6.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only x11perf-1.7.0
+${THOME}/build/genpkg TRIBx11-x11perf x11perf-1.7.0
