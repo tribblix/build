1fbf8bead542167b9e522612789553da2260d14e
Peter Tribble <peter.tribble@gmail.com>
2024-08-02 18:05:28 +0100

    Drop old json-c compat

--- TRIBjson-c/build.sh
+++ TRIBjson-c/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #
-# the github downloads have obscure names
+# SPDX-License-Identifier: CDDL-1.0
 #
-# updates bump the SONAME, forcing a rebuild of any consumers
-# so ship a copy of the previous build for compat, see fixit
+# updates bump the SONAME
 #
 ${THOME}/build/cmbuild +64 json-c-json-c-0.17-20230812
 ${THOME}/build/genpkg TRIBjson-c json-c-json-c-0.17-20230812
