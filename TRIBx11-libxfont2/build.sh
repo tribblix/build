934ab143dd736f7fa664abc0bafe47c38f2ff4a5
Peter Tribble <peter.tribble@gmail.com>
2024-08-02 14:13:05 +0100

    Update libXfont2 2.0.7

--- TRIBx11-libxfont2/build.sh
+++ TRIBx11-libxfont2/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXfont2-2.0.6 +P /usr/lib/xorg -C "--enable-freetype --enable-fc --enable-builtins --with-bzip2"
-${THOME}/build/genpkg TRIBx11-libxfont2 libXfont2-2.0.6
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 libXfont2-2.0.7 +P /usr/lib/xorg -C "--enable-freetype --enable-fc --enable-builtins --with-bzip2"
+${THOME}/build/genpkg TRIBx11-libxfont2 libXfont2-2.0.7
