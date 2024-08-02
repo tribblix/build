cbd6a8d52663dbcf776345c89db383486e77e6e7
Peter Tribble <peter.tribble@gmail.com>
2024-08-02 18:02:27 +0100

    Rebuild repmgr against newer json-c

--- TRIBblix-repmgr16/build.sh
+++ TRIBblix-repmgr16/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a postgres extension, so it's specific to the postgres version
 #
 ${THOME}/build/dobuild +64only repmgr-5.4.1 -C PG_CONFIG=/opt/tribblix/postgres16/bin/pg_config
