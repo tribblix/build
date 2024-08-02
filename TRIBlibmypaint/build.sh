56794691bdb388673f73ff2e8252b3e3dc2bb909
Peter Tribble <peter.tribble@gmail.com>
2024-08-02 17:58:11 +0100

    Rebuild libmypaint against newer json-c

--- TRIBlibmypaint/build.sh
+++ TRIBlibmypaint/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
 ${THOME}/build/dobuild +64 -gnu libmypaint-1.6.1 -C --enable-introspection=no
 ${THOME}/build/genpkg TRIBlibmypaint libmypaint-1.6.1
