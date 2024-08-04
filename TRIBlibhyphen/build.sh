53e97c9f672a1cda59f21c6ba4a8d12c96921e55
Peter Tribble <peter.tribble@gmail.com>
2024-08-04 11:20:20 +0100

    Drop 32-bit libhyphen

--- TRIBlibhyphen/build.sh
+++ TRIBlibhyphen/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the easiest way to get the tarball is from libreoffice, it will be
 # external/tarballs/5ade6ae2a99bc1e9e57031ca88d36dad-hyphen-2.8.8.tar.gz
 #
-${THOME}/build/dobuild -64 hyphen-2.8.8 -C "--disable-static"
+${THOME}/build/dobuild -64only hyphen-2.8.8 -C "--disable-static"
 ${THOME}/build/genpkg TRIBlibhyphen hyphen-2.8.8
