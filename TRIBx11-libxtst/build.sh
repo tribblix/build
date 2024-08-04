5115530f1fc1ff5e194f6f7ca7bd60ac3be262c9
Peter Tribble <peter.tribble@gmail.com>
2024-08-04 19:59:02 +0100

    Update libXtst 1.2.5

--- TRIBx11-libxtst/build.sh
+++ TRIBx11-libxtst/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXtst-1.2.4
-${THOME}/build/genpkg TRIBx11-libxtst libXtst-1.2.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 libXtst-1.2.5
+${THOME}/build/genpkg TRIBx11-libxtst libXtst-1.2.5
