2f73e5fa80ec1251bdf1401222a5f1d104599b98
Peter Tribble <peter.tribble@gmail.com>
2024-08-04 13:53:49 +0100

    Bump wine to 4.0.4

--- TRIBwine/build.sh
+++ TRIBwine/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # wine
 #
-${THOME}/build/dobuild +gnu wine-4.0.3
-${THOME}/build/genpkg -gnu TRIBwine wine-4.0.3
+${THOME}/build/dobuild +gnu wine-4.0.4
+${THOME}/build/genpkg -gnu TRIBwine wine-4.0.4
