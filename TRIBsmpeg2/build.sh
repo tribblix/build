cbd19ae588505f3f1bd5c40a8464ba45937f5a16
Peter Tribble <peter.tribble@gmail.com>
2024-08-04 11:30:38 +0100

    Drop 32-bit smpeg2

--- TRIBsmpeg2/build.sh
+++ TRIBsmpeg2/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this used to overlap with TRIBsmpeg, but that is no more
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64 smpeg-main
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only smpeg-main
 ${THOME}/build/genpkg TRIBsmpeg2 smpeg-main
