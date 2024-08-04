56c5278eb47b09d35a5e1acfd8c25c731e8b54cb
Peter Tribble <peter.tribble@gmail.com>
2024-08-04 22:12:50 +0100

    Update shellcheck 0.10.0

--- TRIBshellcheck/build.sh
+++ TRIBshellcheck/build.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 git clone https://github.com/koalaman/shellcheck
 cd shellcheck
-/usr/bin/cabal install --bindir=/tmp/cbls/usr/bin
+/usr/bin/cabal install --installdir=/tmp/cbls/usr/bin --install-method=copy
+
+# it can be ridiculously big
+strip /tmp/cbls/usr/bin/shellcheck
+
+# this bit requires pandoc
+./manpage
+mkdir -p /tmp/cbls/usr/share/man/man1
+cp shellcheck.1 /tmp/cbls/usr/share/man/man1
 
 ${THOME}/build/create_pkg TRIBshellcheck /tmp/cbls
 rm -fr /tmp/cbls
