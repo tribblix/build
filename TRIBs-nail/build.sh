b489968f8129b052c91e36dbf97a49b0c01804b8
Peter Tribble <peter.tribble@gmail.com>
2024-08-04 11:53:34 +0100

    Update s-nail v14.9.25

--- TRIBs-nail/build.sh
+++ TRIBs-nail/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild s-nail-14.9.24 -M "VAL_PREFIX=/usr VAL_SYSCONFDIR=/etc CC=gcc EXTRA_CFLAGS=-m64 EXTRA_LDFLAGS=-m64 all"
-${THOME}/build/genpkg TRIBs-nail s-nail-14.9.24
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild s-nail-14.9.25 -M "VAL_PREFIX=/usr VAL_SYSCONFDIR=/etc CC=gcc EXTRA_CFLAGS=-m64 EXTRA_LDFLAGS=-m64 all"
+${THOME}/build/genpkg TRIBs-nail s-nail-14.9.25
