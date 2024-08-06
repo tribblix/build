fa76c9e20dc4d05ef74fbca1cf03445ff4a9f6be
Peter Tribble <peter.tribble@gmail.com>
2024-08-06 10:41:27 +0100

    Update multitail 7.1.3, and build 64-bit

--- TRIBmultitail/build.sh
+++ TRIBmultitail/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild multitail-7.0.0 -M "-f makefile.solaris_gcc"
-${THOME}/build/genpkg -M "-f makefile.solaris_gcc" TRIBmultitail multitail-7.0.0
+# can download the tarball from the tags on github
+#
+${THOME}/build/dobuild multitail-7.1.3 -M "-f makefile.solaris_gcc EXTRA_CFLAGS=-m64 EXTRA_LDFLAGS=-m64"
+${THOME}/build/genpkg -M "-f makefile.solaris_gcc" TRIBmultitail multitail-7.1.3
