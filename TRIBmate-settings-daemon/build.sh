d78f2ee494f32e1347f1b95d2cde4d49d32f7f1e
Peter Tribble <peter.tribble@gmail.com>
2024-08-06 16:04:47 +0100

    Update mate-settings-daemon 1.28.0

--- TRIBmate-settings-daemon/build.sh
+++ TRIBmate-settings-daemon/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # mate-settings-daemon (needs mate-desktop installed)
-${THOME}/build/dobuild -64only -gnu mate-settings-daemon-1.26.1 -C --disable-rfkill
-${THOME}/build/genpkg TRIBmate-settings-daemon mate-settings-daemon-1.26.1
+#
+${THOME}/build/dobuild -64only -gnu mate-settings-daemon-1.28.0 -C --disable-rfkill
+${THOME}/build/genpkg TRIBmate-settings-daemon mate-settings-daemon-1.28.0
