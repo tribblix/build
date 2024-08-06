22517df2266f0508fbf15966873d2572198ebdfa
Peter Tribble <peter.tribble@gmail.com>
2024-08-06 13:18:55 +0100

    Rebuild htop

--- TRIBhtop/build.sh
+++ TRIBhtop/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # htop support for solaris/illumos is experimental
 #
 # Thanks to Guy M Broome <gmbroome@vcu.edu>
 #
-# the build here is rather manual
-#
-# requires ncurses 6
-#
-$THOME/build/dobuild +gnu +64 htop-2.2.0 -C --disable-unicode
+env TRIBBLIX_CFLAGS=-fcommon $THOME/build/dobuild +gnu -64only htop-2.2.0
 
 ${THOME}/build/genpkg TRIBhtop htop-2.2.0
