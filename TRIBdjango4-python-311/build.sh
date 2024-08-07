ebdcb1a4ca4c305cc0d7ca3623bec000a6b8e06b
Peter Tribble <peter.tribble@gmail.com>
2024-08-07 09:21:31 +0100

    Update Django 4.2.15

--- TRIBdjango4-python-311/build.sh
+++ TRIBdjango4-python-311/build.sh
@@ -4,4 +4,4 @@
 #
 # pytz is deprecated, but marked as a dependency for transitional purposes
 #
-${THOME}/build/pkg_setup_py TRIBdjango4-python-311 Django-4.2.14
+${THOME}/build/pkg_setup_py TRIBdjango4-python-311 Django-4.2.15
