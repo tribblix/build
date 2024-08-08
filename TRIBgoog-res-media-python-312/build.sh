494752afd7f576fafd083961101b775f012c4c3e
Peter Tribble <peter.tribble@gmail.com>
2024-08-08 10:18:35 +0100

    Update google-resumable-media 2.7.2

--- TRIBgoog-res-media-python-312/build.sh
+++ TRIBgoog-res-media-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBgoog-res-media-python-312 google-resumable-media-2.7.1
+${THOME}/build/pkg_setup_py TRIBgoog-res-media-python-312 google_resumable_media-2.7.2
