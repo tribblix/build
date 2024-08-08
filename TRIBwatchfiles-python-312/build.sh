889b38aff73fd1ddecd69c7e1fa839a88a75d8c5
Peter Tribble <peter.tribble@gmail.com>
2024-08-08 10:33:55 +0100

    Update watchfiles 0.23.0

--- TRIBwatchfiles-python-312/build.sh
+++ TRIBwatchfiles-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBwatchfiles-python-312 watchfiles-0.22.0
+${THOME}/build/pkg_pep518 TRIBwatchfiles-python-312 watchfiles-0.23.0
