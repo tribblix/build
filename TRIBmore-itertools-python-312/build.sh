129640c9f80320fcd895681916674d5320ca3ab3
Peter Tribble <peter.tribble@gmail.com>
2024-08-09 17:26:38 +0100

    Update more-itertools 10.4.0

--- TRIBmore-itertools-python-312/build.sh
+++ TRIBmore-itertools-python-312/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# this one includes a setup.py, but not one that works correctly
-#
-$THOME/build/pkg_pep518 TRIBmore-itertools-python-312 more-itertools-10.3.0
+$THOME/build/pkg_pep518 TRIBmore-itertools-python-312 more-itertools-10.4.0
