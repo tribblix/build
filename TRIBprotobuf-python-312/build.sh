850912cc72ba2006fe22c2dbc8b7d26b78d1329b
Peter Tribble <peter.tribble@gmail.com>
2024-08-09 17:43:58 +0100

    Update python protobuf 5.27.3

--- TRIBprotobuf-python-312/build.sh
+++ TRIBprotobuf-python-312/build.sh
@@ -2,7 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# entertainingly, the python module has a filename clash
-# with the regular protobuf download
-#
-${THOME}/build/pkg_setup_py TRIBprotobuf-python-312 protobuf-5.27.2
+${THOME}/build/pkg_setup_py TRIBprotobuf-python-312 protobuf-5.27.3
