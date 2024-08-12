0eb695a74d4d65c2ceb809e511dbeab486779158
Peter Tribble <peter.tribble@gmail.com>
2024-08-12 09:14:13 +0100

    Update webcolors 24.8.0

--- TRIBwebcolors-python-312/build.sh
+++ TRIBwebcolors-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBwebcolors-python-312 webcolors-24.6.0
+${THOME}/build/pkg_pep518 -N TRIBwebcolors-python-312 webcolors-24.8.0
