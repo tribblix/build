3a02707fab1f9ed7c8d74d6e5e1d9acd7e47a495
Peter Tribble <peter.tribble@gmail.com>
2024-08-12 09:11:19 +0100

    Update nltk 3.8.2

--- TRIBnltk-python-312/build.sh
+++ TRIBnltk-python-312/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # there's a fixit file to correct permissions
 #
-${THOME}/build/pkg_setup_py TRIBnltk-python-312 nltk-3.8.1
+${THOME}/build/pkg_setup_py TRIBnltk-python-312 nltk-3.8.2
