5a0591c34f7d7f7678e9dfeb7c6494eb67fcba1d
Peter Tribble <peter.tribble@gmail.com>
2024-08-13 10:38:23 +0100

    Update PyWavelets 1.7.0

--- TRIBpywavelets-python-312/build.sh
+++ TRIBpywavelets-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBpywavelets-python-312 pywavelets-1.6.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+env LDFLAGS=-m64 CXXFLAGS=-m64 CFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBpywavelets-python-312 pywavelets-1.7.0
