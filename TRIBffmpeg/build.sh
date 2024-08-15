d721e1eacfc26e005f492c70c7b241ad5d6abe23
Peter Tribble <peter.tribble@gmail.com>
2024-08-15 17:46:17 +0100

    No longer need 32-bit ffmpeg

--- TRIBffmpeg/build.sh
+++ TRIBffmpeg/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # yasm (fallback to nasm) is a build-time dependency - zap install yasm
 #
-# this is a split build, the binaries are 64-bit, overwriting the 32-bit ones
+# documentation is fragile as it requires a specific version of texinfo,
+# so disable it
 #
-env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild ffmpeg-4.4.2 -C "--strip=/usr/gnu/bin/strip --enable-shared --disable-static"
-mv ffmpeg-4.4.2 ffmpeg-4.4.2-32
-env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -64only ffmpeg-4.4.2 -C "--strip=/usr/gnu/bin/strip --enable-shared --disable-static"
-${THOME}/build/genpkg TRIBffmpeg ffmpeg-4.4.2-32 ffmpeg-4.4.2
+# SDL2 requires a much older version than we have
+#
+env CPPFLAGS=-std=gnu99 ${THOME}/build/dobuild -64only ffmpeg-4.4.2 -C "--strip=/usr/gnu/bin/strip --enable-shared --disable-static --disable-doc"
+${THOME}/build/genpkg TRIBffmpeg ffmpeg-4.4.2
