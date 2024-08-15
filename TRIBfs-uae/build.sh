98bf3060360aa626be735766ac5e5ae0613da02f
Peter Tribble <peter.tribble@gmail.com>
2024-08-15 20:54:17 +0100

    Rebuild fs-uae 64-bit

--- TRIBfs-uae/build.sh
+++ TRIBfs-uae/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild fs-uae-3.1.66 -C "--with-libmpeg2=builtin --disable-jit"
+# SPDX-License-Identifier: CDDL-1.0
+#
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only fs-uae-3.1.66 -C "--with-libmpeg2=builtin --disable-jit"
 ${THOME}/build/genpkg TRIBfs-uae fs-uae-3.1.66
