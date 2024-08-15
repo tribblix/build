2d853c1a77d7ff37b0c60b90efcc38bb7663c1f2
Peter Tribble <peter.tribble@gmail.com>
2024-08-15 18:08:26 +0100

    No longer need 32-bit libmodplug

--- TRIBlibmodplug/build.sh
+++ TRIBlibmodplug/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS=-O $THOME/build/dobuild -64 https://github.com/christopho/libmodplug
+# SPDX-License-Identifier: CDDL-1.0
+#
+env TRIBBLIX_CFLAGS=-O $THOME/build/dobuild -64only https://github.com/christopho/libmodplug
 ${THOME}/build/genpkg TRIBlibmodplug libmodplug
