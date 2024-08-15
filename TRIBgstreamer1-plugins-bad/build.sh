1cf6d435dd62fc27f0bb0a253805a638b8ac6484
Peter Tribble <peter.tribble@gmail.com>
2024-08-15 17:55:29 +0100

    No longer need 32-bit gstreamer1-plugins-bad

--- TRIBgstreamer1-plugins-bad/build.sh
+++ TRIBgstreamer1-plugins-bad/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu -64 gst-plugins-bad-1.20.7 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled"
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu +64 gst-plugins-bad-1.20.7 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled"
 ${THOME}/build/genpkg TRIBgstreamer1-plugins-bad gst-plugins-bad-1.20.7
