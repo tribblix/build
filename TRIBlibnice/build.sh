60b7aaa5f7e62d716d5a0ea255fc787f228da421
Peter Tribble <peter.tribble@gmail.com>
2024-08-15 18:15:17 +0100

    Update libnice 0.1.22, drop 32-bit

--- TRIBlibnice/build.sh
+++ TRIBlibnice/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # libnice now has only new gstreamer plugins
 #
-$THOME/build/mesonbuild -64 libnice-0.1.21 -C "-Dintrospection=disabled -Dtests=disabled"
-${THOME}/build/genpkg TRIBlibnice libnice-0.1.21
+$THOME/build/mesonbuild -64only libnice-0.1.22 -C "-Dintrospection=disabled -Dtests=disabled -Dexamples=disabled"
+${THOME}/build/genpkg TRIBlibnice libnice-0.1.22
