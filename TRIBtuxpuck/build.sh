4107d9fa5bfc291ebeda701ae7099e41bae4caa5
Peter Tribble <peter.tribble@gmail.com>
2024-08-16 22:07:32 +0100

    Rebuild tuxpuck 64-bit

--- TRIBtuxpuck/build.sh
+++ TRIBtuxpuck/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # If you download the source from launchpad you'll need to rename it
 #  mv tuxpuck_0.8.2.orig.tar.gz tuxpuck-0.8.2.tar.gz
 #
-${THOME}/build/dobuild tuxpuck-0.8.2 -J 1
+env CFLAGS="-m64" ${THOME}/build/dobuild -64only tuxpuck-0.8.2 -J 1
 ${THOME}/build/genpkg TRIBtuxpuck tuxpuck-0.8.2
