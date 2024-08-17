3c3f152fd7e053fac5acffadf6970e1163b42642
Peter Tribble <peter.tribble@gmail.com>
2024-08-17 10:07:15 +0100

    Drop 32-bit SDL_mixer, fix dependencies

--- TRIBlibsdl-mixer/build.sh
+++ TRIBlibsdl-mixer/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 SDL_mixer-1.2.12
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild +64 SDL_mixer-1.2.12
 ${THOME}/build/genpkg TRIBlibsdl-mixer SDL_mixer-1.2.12
