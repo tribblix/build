55721aa7ea0b4a3ed20d31916ab6b9a69795c29b
Peter Tribble <peter.tribble@gmail.com>
2024-08-17 11:58:10 +0100

    Drop 32-bit libexif

--- TRIBimage-libexif/build.sh
+++ TRIBimage-libexif/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libexif-0.6.24
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild +64 libexif-0.6.24
 ${THOME}/build/genpkg TRIBimage-libexif libexif-0.6.24
