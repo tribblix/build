35f38eeec4069542a694222ee09497abe13f3886
Peter Tribble <peter.tribble@gmail.com>
2024-08-17 13:24:30 +0100

    Update unbound 1.21.0

--- TRIBunbound/build.sh
+++ TRIBunbound/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # mustn't have pkgsrc installed, else it will pick up the wrong things
 #
-${THOME}/build/dobuild -64only unbound-1.20.0 -C "--without-libevent --sysconfdir=/etc" -J 1
-${THOME}/build/genpkg TRIBunbound unbound-1.20.0
+${THOME}/build/dobuild -64only unbound-1.21.0 -C "--without-libevent --sysconfdir=/etc" -J 1
+${THOME}/build/genpkg TRIBunbound unbound-1.21.0
