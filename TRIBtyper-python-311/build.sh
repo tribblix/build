a57a251f8c426946860dead129c13deb64cfdb2d
Peter Tribble <peter.tribble@gmail.com>
2024-08-18 19:54:13 +0100

    Update typer 0.12.4

--- TRIBtyper-python-311/build.sh
+++ TRIBtyper-python-311/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBtyper-python-311 typer-0.12.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBtyper-python-311 typer-0.12.4
--- TRIBtyper-python-312/build.sh
+++ TRIBtyper-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBtyper-python-312 typer-0.12.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBtyper-python-312 typer-0.12.4
