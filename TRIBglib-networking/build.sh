35e8a20821af682ae0cb7790a86ce1f38888637f
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 21:43:46 +0100

    Drop 32-bit glib-networking

--- TRIBglib-networking/build.sh
+++ TRIBglib-networking/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu -64 glib-networking-2.44.0 -C "--without-ca-certificates"
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -gnu -64only glib-networking-2.44.0 -C "--without-ca-certificates"
 ${THOME}/build/genpkg TRIBglib-networking glib-networking-2.44.0
