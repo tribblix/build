bcd2dfc1c4a12537f0ea46314865e7d094fdbf86
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 21:18:17 +0100

    Drop 32-bit gssdp

--- TRIBgssdp/build.sh
+++ TRIBgssdp/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 gssdp-1.0.5 -C "--disable-introspection --disable-dependency-tracking --without-gtk"
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only gssdp-1.0.5 -C "--disable-introspection --disable-dependency-tracking --without-gtk"
 ${THOME}/build/genpkg TRIBgssdp gssdp-1.0.5
