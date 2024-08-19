333caed9197103f8199f443bf08bbae5064eb83c
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 13:34:56 +0100

    Missing files in openxsun-compat

--- TRIBopenxsun-compat/build.sh
+++ TRIBopenxsun-compat/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # on sparc, Xsun is shipped in the SUNWopenxsun-server package
 # this package supplies the missing files, some of which were
 # missing entirely and some of which were in other obsoleted
