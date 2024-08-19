74ec5d0492c17c94844daa5189d12ed7f3fbae77
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 16:15:56 +0100

    Update libFS 1.0.10, drop 32-bit

--- TRIBx11-libfs/build.sh
+++ TRIBx11-libfs/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libFS-1.0.9
-${THOME}/build/genpkg TRIBx11-libfs libFS-1.0.9
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libFS-1.0.10
+${THOME}/build/genpkg TRIBx11-libfs libFS-1.0.10
