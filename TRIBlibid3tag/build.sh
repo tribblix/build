d0ddcd9afae3e69751ebe2c30ebfa343c987c0a1
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 20:24:54 +0100

    Drop 32-bit libid3tag

--- TRIBlibid3tag/build.sh
+++ TRIBlibid3tag/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # you may need to repack the tarball as it's shipped without the version
 #
-${THOME}/build/cmbuild -64 libid3tag-0.16.3
+${THOME}/build/cmbuild -64only libid3tag-0.16.3
 ${THOME}/build/genpkg TRIBlibid3tag libid3tag-0.16.3
