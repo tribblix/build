c8e9be5df1ce365647f533590062bb8fc537056f
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 08:27:52 +0100

    Update cachetools 5.5.0

--- TRIBcachetools-python-312/build.sh
+++ TRIBcachetools-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBcachetools-python-312 cachetools-5.4.0
+${THOME}/build/pkg_setup_py TRIBcachetools-python-312 cachetools-5.5.0
