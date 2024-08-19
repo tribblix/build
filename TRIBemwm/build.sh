6e3b98050c17158a89c2733c3b57876fd51d006b
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 18:59:50 +0100

    Update emwm 1.2

--- TRIBemwm/build.sh
+++ TRIBemwm/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the Makefile is patched to enable 64-bit
 #
-$THOME/build/dobuild emwm-src-1.1
-$THOME/build/dobuild emwm-utils-src-1.1
-${THOME}/build/genpkg TRIBemwm emwm-src-1.1 emwm-utils-src-1.1
+$THOME/build/dobuild emwm-src-1.2
+$THOME/build/dobuild emwm-utils-src-1.2
+${THOME}/build/genpkg TRIBemwm emwm-src-1.2 emwm-utils-src-1.2
