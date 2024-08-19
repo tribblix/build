dd6de8eb9d7729f3759c189b007acaf44f7251a9
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 21:21:22 +0100

    Drop 32-bit libopusenc

--- TRIBlibopusenc/build.sh
+++ TRIBlibopusenc/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libopusenc-0.2.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libopusenc-0.2.1
 ${THOME}/build/genpkg TRIBlibopusenc libopusenc-0.2.1
