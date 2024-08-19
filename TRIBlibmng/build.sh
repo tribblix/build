c69f0a2e14f625a17c484e00a89f50c32471d2f7
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 20:52:43 +0100

    Drop 32-bit libmng

--- TRIBlibmng/build.sh
+++ TRIBlibmng/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libmng-2.0.3 -C --disable-static
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild +64 libmng-2.0.3 -C --disable-static
 ${THOME}/build/genpkg TRIBlibmng libmng-2.0.3
