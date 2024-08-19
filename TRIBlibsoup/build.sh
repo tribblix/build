a493f61bbf23d65e3f75f67ab0ea708e67127403
Peter Tribble <peter.tribble@gmail.com>
2024-08-19 21:34:47 +0100

    Drop 32-bit libsoup

--- TRIBlibsoup/build.sh
+++ TRIBlibsoup/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # later versions add additional dependencies
 #
-${THOME}/build/dobuild -gnu -64 libsoup-2.62.3 -C "--without-gnome --disable-more-warnings --disable-introspection --without-krb5-config"
+# glib-networking is a dependency, but indirectly via gio
+# so isn't properly detected by autodepend
+#
+${THOME}/build/dobuild -gnu -64only libsoup-2.62.3 -C "--without-gnome --disable-more-warnings --disable-introspection --without-krb5-config"
 ${THOME}/build/genpkg TRIBlibsoup libsoup-2.62.3
