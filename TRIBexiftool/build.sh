7a412631fb323d92068798220d7ee9da1efd8734
Peter Tribble <peter.tribble@gmail.com>
2024-08-21 21:01:39 +0100

    Update ExifTool 12.93

--- TRIBexiftool/build.sh
+++ TRIBexiftool/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Image-ExifTool-12.68
-cd Image-ExifTool-12.68
+# SPDX-License-Identifier: CDDL-1.0
+#
+# dependencies are optional, half come with perl itself
+# Archive::Zip we already had, Compress::Raw:Lzma was easy
+# others send you down the rabbit hole
+#
+${THOME}/build/unpack Image-ExifTool-12.93
+cd Image-ExifTool-12.93
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBexiftool
