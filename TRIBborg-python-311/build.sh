f35885645c3b905391e071779d503c64dc676ef5
Peter Tribble <peter.tribble@gmail.com>
2024-08-21 08:43:04 +0100

    Remove python 3.11 borg

--- TRIBborg-python-311/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_setup_py TRIBborg-python-311 borgbackup-1.4.0
--- TRIBmsgpack-python-311/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-${THOME}/build/pkg_setup_py TRIBmsgpack-python-311 msgpack-1.0.8
