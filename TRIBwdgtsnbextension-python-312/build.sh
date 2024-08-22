1d608b0aa9d9012a9399e9fcd3b6cbfd060f0177
Peter Tribble <peter.tribble@gmail.com>
2024-08-22 11:54:51 +0100

    Update widgetsnbextension 4.0.12

--- TRIBwdgtsnbextension-python-312/build.sh
+++ TRIBwdgtsnbextension-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBwdgtsnbextension-python-312 widgetsnbextension-4.0.11
+${THOME}/build/pkg_pep518 TRIBwdgtsnbextension-python-312 widgetsnbextension-4.0.12
