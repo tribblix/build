578b55a942487a2e02d8df5fad79e6f90a00fe4d
Peter Tribble <peter.tribble@gmail.com>
2024-08-23 21:06:12 +0100

    Drop 32-bit startup-notification

--- TRIBstartup-notification/build.sh
+++ TRIBstartup-notification/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 startup-notification-0.10
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only startup-notification-0.10
 ${THOME}/build/genpkg TRIBstartup-notification startup-notification-0.10
