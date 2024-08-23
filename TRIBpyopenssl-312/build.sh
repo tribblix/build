508d17d9a11008cdbb60a8fd61a40904072f3f8e
Peter Tribble <peter.tribble@gmail.com>
2024-08-23 09:18:09 +0100

    Update pyOpenSSL 24.2.1

--- TRIBpyopenssl-312/build.sh
+++ TRIBpyopenssl-312/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # despite the name, the actual openssl bindings are in the
 # cryptography module, not this one
 #
-${THOME}/build/pkg_setup_py TRIBpyopenssl-312 pyOpenSSL-24.1.0
+${THOME}/build/pkg_setup_py TRIBpyopenssl-312 pyopenssl-24.2.1
