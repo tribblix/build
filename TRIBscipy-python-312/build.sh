11d2b191f65eedafd5b53f328a42da25c94c1a4c
Peter Tribble <peter.tribble@gmail.com>
2024-08-23 13:32:27 +0100

    Update scipy 1.14.1

--- TRIBscipy-python-312/build.sh
+++ TRIBscipy-python-312/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# 1.14.0 build pins some dependencies
+# 1.14.1 build pins some dependencies
 # meson-python>=0.15.0,<0.19.0
 # Cython>=3.0.8,<3.1.0
 # pybind11>=2.12.0,<2.13.0
@@ -16,4 +16,4 @@
 #
 # ideally that would be fixed further up
 #
-env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscipy-python-312 scipy-1.14.0
+env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscipy-python-312 scipy-1.14.1
