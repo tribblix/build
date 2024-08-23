de28694b610f4ace63ce6596ca072c51919fb659
Peter Tribble <peter.tribble@gmail.com>
2024-08-23 21:15:00 +0100

    Drop 32-bit libxdg-basedir

--- TRIBlibxdg-basedir/build.sh
+++ TRIBlibxdg-basedir/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libxdg-basedir-1.2.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libxdg-basedir-1.2.0
 ${THOME}/build/genpkg TRIBlibxdg-basedir libxdg-basedir-1.2.0
