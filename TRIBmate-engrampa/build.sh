8f46b67e35bf26b7091a0f1cc4f5e08daf6a18a9
Peter Tribble <peter.tribble@gmail.com>
2024-08-24 20:03:15 +0100

    Update engrampa 1.28.2

--- TRIBmate-engrampa/build.sh
+++ TRIBmate-engrampa/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only +gnu engrampa-1.28.1
-${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.28.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only +gnu engrampa-1.28.2
+${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.28.2
