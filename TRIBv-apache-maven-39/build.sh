f0569244d873a93fbad37f45e98299d76e4037d6
Peter Tribble <peter.tribble@gmail.com>
2024-08-24 19:54:46 +0100

    Update maven 3.9.9

--- TRIBv-apache-maven-39/build.sh
+++ TRIBv-apache-maven-39/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_tarball TRIBv-apache-maven-39 apache-maven-3.9.7-bin
+${THOME}/build/pkg_tarball TRIBv-apache-maven-39 apache-maven-3.9.9-bin
