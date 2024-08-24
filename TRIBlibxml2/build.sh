88b6de6567bfaafb14e685b916839092d015100c
Peter Tribble <peter.tribble@gmail.com>
2024-08-24 11:55:58 +0100

    Drop python 3.11 libxml2

--- TRIBlibxml2/build.sh
+++ TRIBlibxml2/build.sh
@@ -9,15 +9,6 @@
 env TRIBBLIX_LDFLAGS="-z direct -z defs -z ignore" ${THOME}/build/dobuild -64 libxml2-2.9.14 -C "--disable-static --with-pic --with-threads --without-lzma --without-python"
 ${THOME}/build/genpkg TRIBlibxml2 libxml2-2.9.14
 
-# python3.11 is 64-bit
-rm -fr /tmp/lxp
-cd libxml2-2.9.14-64bit/python
-/usr/versions/python-3.11/bin/python3 setup.py build
-/usr/versions/python-3.11/bin/python3 setup.py install --root=/tmp/lxp
-${THOME}/build/create_pkg TRIBlibxml2-python-311 /tmp/lxp
-/usr/versions/python-3.11/bin/python3 setup.py clean
-cd ../..
-
 # python3.12 is 64-bit
 rm -fr /tmp/lxp
 cd libxml2-2.9.14-64bit/python
