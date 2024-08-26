ed394ec90fe5c6e8509bfaa101aaa2ab271ebbf2
Peter Tribble <peter.tribble@gmail.com>
2024-08-26 11:01:14 +0100

    Update paginate 0.5.7

--- TRIBpaginate-python-312/build.sh
+++ TRIBpaginate-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpaginate-python-312 paginate-0.5.6
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpaginate-python-312 paginate-0.5.7
