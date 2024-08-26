50bf6f6ca49dfcc182a99d3d6a4f3ace5757710d
Peter Tribble <peter.tribble@gmail.com>
2024-08-26 13:54:12 +0100

    Update sphinxcontrib-websupport 2.0.0

--- TRIBsphinx-ws-python-312/build.sh
+++ TRIBsphinx-ws-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBsphinx-ws-python-312 sphinxcontrib_websupport-1.2.7
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBsphinx-ws-python-312 sphinxcontrib_websupport-2.0.0
