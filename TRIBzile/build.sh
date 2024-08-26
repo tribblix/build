f482b4a3ed2c907929fe415bd6a70e6de15d5cc3
Peter Tribble <peter.tribble@gmail.com>
2024-08-26 19:50:03 +0100

    Drop unused zile, doesn't build

--- TRIBzile/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# needs boehm-gc and help2man
-#
-# 2.6 was rewritten in vala, stick to older versions
-#
-${THOME}/build/dobuild -64only zile-2.4.15
-${THOME}/build/genpkg TRIBzile zile-2.4.15
