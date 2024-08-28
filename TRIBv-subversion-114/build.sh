387cbbf55ccdc335891e041af96ba0452200d360
Peter Tribble <peter.tribble@gmail.com>
2024-08-28 21:02:44 +0100

    Rebuild subversion with apr 1.7.5

--- TRIBv-subversion-114/build.sh
+++ TRIBv-subversion-114/build.sh
@@ -5,13 +5,13 @@
 #
 # Need to disable the LD_OPTIONS stuff else it all goes pear shaped
 #
-${THOME}/build/dobuild +64only apr-1.7.4 -P /usr/versions/subversion-1.14 -L
-${THOME}/build/dobuild +64only apr-util-1.6.3 -P /usr/versions/subversion-1.14 -C --with-apr=../apr-1.7.4-64bit -L
+${THOME}/build/dobuild +64only apr-1.7.5 -P /usr/versions/subversion-1.14 -L
+${THOME}/build/dobuild +64only apr-util-1.6.3 -P /usr/versions/subversion-1.14 -C --with-apr=../apr-1.7.5-64bit -L
 ${THOME}/build/unpack serf-1.3.10
-ln -s . apr-1.7.4-64bit/bin
+ln -s . apr-1.7.5-64bit/bin
 ln -s . apr-util-1.6.3-64bit/bin
 cd serf-1.3.10
-scons APR=../apr-1.7.4-64bit APU=../apr-util-1.6.3-64bit PREFIX=/usr/versions/subversion-1.14 CPPFLAGS="-m64 -D__EXTENSIONS__" LINKFLAGS="-m64 -L../apr-1.7.4-64bit/.libs -L../apr-util-1.6.3-64bit/.libs -R/usr/versions/subversion-1.14/lib"
+scons APR=../apr-1.7.5-64bit APU=../apr-util-1.6.3-64bit PREFIX=/usr/versions/subversion-1.14 CPPFLAGS="-m64 -D__EXTENSIONS__" LINKFLAGS="-m64 -L../apr-1.7.5-64bit/.libs -L../apr-util-1.6.3-64bit/.libs -R/usr/versions/subversion-1.14/lib"
 #
 # subversion uses the .pc file, but it has the final installed
 # locations which would be wrong
@@ -21,14 +21,14 @@ sed -i s:/usr/versions/subversion-1.14/lib:`pwd`: serf-1.pc
 sed -i s:/usr/versions/subversion-1.14:`pwd`: serf-1.pc
 sed -i s:/include/serf-1:: serf-1.pc
 cd ..
-${THOME}/build/dobuild +64only subversion-1.14.3 -P /usr/versions/subversion-1.14 -C "--with-apr=../apr-1.7.4-64bit --with-apr-util=../apr-util-1.6.3-64bit --with-serf=../serf-1.3.10 CPPFLAGS=-D__EXTENSIONS__ --with-lz4=internal --with-utf8proc=internal --without-gnome-keyring" -L
+${THOME}/build/dobuild +64only subversion-1.14.3 -P /usr/versions/subversion-1.14 -C "--with-apr=../apr-1.7.5-64bit --with-apr-util=../apr-util-1.6.3-64bit --with-serf=../serf-1.3.10 CPPFLAGS=-D__EXTENSIONS__ --with-lz4=internal --with-utf8proc=internal --without-gnome-keyring" -L
 # symlinks so libtool stupid relinking works
-cd apr-1.7.4-64bit
+cd apr-1.7.5-64bit
 ln -s .libs/* .
 cd ..
 cd apr-util-1.6.3-64bit
 ln -s .libs/* .
 cd ..
 # serf installed by fixinstall
-${THOME}/build/genpkg TRIBv-subversion-114 subversion-1.14.3 apr-1.7.4 apr-util-1.6.3
+${THOME}/build/genpkg TRIBv-subversion-114 subversion-1.14.3 apr-1.7.5 apr-util-1.6.3
 # [phew]
