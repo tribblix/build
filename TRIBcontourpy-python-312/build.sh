639ee8eab147f67e942c23eaf4b567f209c918fe
Peter Tribble <peter.tribble@gmail.com>
2024-08-28 08:32:18 +0100

    Update contourpy 1.3.0

--- TRIBcontourpy-python-312/build.sh
+++ TRIBcontourpy-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-env CXX="g++ -m64" CC="gcc -m64" ${THOME}/build/pkg_pep518 TRIBcontourpy-python-312 contourpy-1.2.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+env CXX="g++ -m64" CC="gcc -m64" ${THOME}/build/pkg_pep518 TRIBcontourpy-python-312 contourpy-1.3.0
