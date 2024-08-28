81d25c951fa3ca3c4a87491586b237b8b9681860
Peter Tribble <peter.tribble@gmail.com>
2024-08-28 10:20:53 +0100

    Update dust 1.1.1

--- TRIBdust/build.sh
+++ TRIBdust/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 
 #
-$THOM/build/unpack dust-0.9.0
-cd dust-0.9.0
+$THOME/build/unpack dust-1.1.1
+cd dust-1.1.1
 cargo build --release
 
 rm -fr /tmp/ll
@@ -16,6 +18,7 @@ cp LICENSE README.md /tmp/ll/usr/share/dust
 
 mkdir -p /tmp/ll/usr/share/man/man1
 cp man-page/dust.1 /tmp/ll/usr/share/man/man1
+cd ..
 
 ${THOME}/build/create_pkg TRIBdust /tmp/ll
 rm -fr /tmp/ll
