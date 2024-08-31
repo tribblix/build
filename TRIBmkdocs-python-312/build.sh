0f8e4f776bbecc32c2eac5fbe4173baab2b6b5a1
Peter Tribble <peter.tribble@gmail.com>
2024-08-31 09:21:28 +0100

    Update mkdocs 1.6.1

--- TRIBmkdocs-python-312/build.sh
+++ TRIBmkdocs-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # ship mkdocs-get-deps in the same package
 #
-${THOME}/build/pkg_pep518 TRIBmkdocs-python-312 mkdocs-1.6.0 mkdocs_get_deps-0.2.0
+${THOME}/build/pkg_pep518 TRIBmkdocs-python-312 mkdocs-1.6.1 mkdocs_get_deps-0.2.0
