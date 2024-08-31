9178a783869a6970bf927cb65c1523c4c2c3a8b8
Peter Tribble <peter.tribble@gmail.com>
2024-08-31 09:29:08 +0100

    Update dask 2024.8.2

--- TRIBdask-python-312/build.sh
+++ TRIBdask-python-312/build.sh
@@ -12,4 +12,4 @@
 #
 # there's a distributed patch necessary to handle RLIMIT
 #
-${THOME}/build/pkg_pep518 -N TRIBdask-python-312 dask-2024.8.1 distributed-2024.8.1
+${THOME}/build/pkg_pep518 -N TRIBdask-python-312 dask-2024.8.2 distributed-2024.8.2
