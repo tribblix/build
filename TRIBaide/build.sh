c8740677b82ce99f313bc934a190940234864ada
Peter Tribble <peter.tribble@gmail.com>
2024-09-01 16:56:07 +0100

    Update aide 0.18.8

--- TRIBaide/build.sh
+++ TRIBaide/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# patch to disable pie
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only aide-0.18.6
-${THOME}/build/genpkg TRIBaide aide-0.18.6
+# there's a patch to disable pie
+#
+${THOME}/build/dobuild -64only aide-0.18.8
+${THOME}/build/genpkg TRIBaide aide-0.18.8
