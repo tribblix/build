e10f5bfc5ab42b014bff047d600cea67be989476
Peter Tribble <peter.tribble@gmail.com>
2024-09-01 14:04:55 +0100

    Add osprofiler 4.2.0

---
+++ TRIBoslo-concurrency-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBoslo-concurrency-python-312 oslo.concurrency-6.1.0
---
+++ TRIBosprofiler-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBosprofiler-python-312 osprofiler-4.2.0
