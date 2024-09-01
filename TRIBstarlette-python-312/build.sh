edbb2955b662c4bb355cce68be0f4e9f95da40f8
Peter Tribble <peter.tribble@gmail.com>
2024-09-01 10:39:46 +0100

    Update starlette 0.38.3

--- TRIBstarlette-python-312/build.sh
+++ TRIBstarlette-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBstarlette-python-312 starlette-0.38.2
+${THOME}/build/pkg_pep518 -N TRIBstarlette-python-312 starlette-0.38.3
