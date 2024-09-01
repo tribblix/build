0ffd5afb487747052774d315dacbe3a0ba60759b
Peter Tribble <peter.tribble@gmail.com>
2024-09-01 10:36:08 +0100

    Update freetype-py 2.5.1

--- TRIBfreetype-python-312/build.sh
+++ TRIBfreetype-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBfreetype-python-312 freetype-py-2.4.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBfreetype-python-312 freetype-py-2.5.1
