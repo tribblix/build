2fb7d2f71801ff495548859d749662bf98ecd64c
Peter Tribble <peter.tribble@gmail.com>
2024-09-02 10:31:07 +0100

    Add Routes 2.5.1

---
+++ TRIBrepoze-lru-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBrepoze-lru-python-312 repoze.lru-0.7
---
+++ TRIBroutes-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBroutes-python-312 Routes-2.5.1
