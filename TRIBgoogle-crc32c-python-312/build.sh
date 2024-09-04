72cae45806d3173114374fd0af437978df2b91bd
Peter Tribble <peter.tribble@gmail.com>
2024-09-04 09:03:58 +0100

    Update google-crc32c 1.6.0

--- TRIBgoogle-crc32c-python-312/build.sh
+++ TRIBgoogle-crc32c-python-312/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a pure python build, disabling the C extension
 # you don't need to explicitly force it, but by doing so you avoid
 # the scary errors when it tries and fails
 #
-env CRC32C_PURE_PYTHON=1 ${THOME}/build/pkg_setup_py TRIBgoogle-crc32c-python-312 google-crc32c-1.5.0
+env CRC32C_PURE_PYTHON=1 ${THOME}/build/pkg_setup_py TRIBgoogle-crc32c-python-312 google_crc32c-1.6.0
