889d076f1213504f0847f3a9501891db2add5ddd
Peter Tribble <peter.tribble@gmail.com>
2024-09-04 08:44:49 +0100

    Update kiwisolver 1.4.6

--- TRIBkiwisolver-python-312/build.sh
+++ TRIBkiwisolver-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBkiwisolver-python-312 kiwisolver-1.4.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBkiwisolver-python-312 kiwisolver-1.4.6
