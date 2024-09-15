a0001ee2a8f0b6e5b89f1ce30885756646cc0932
Peter Tribble <peter.tribble@gmail.com>
2024-09-15 18:33:14 +0100

    Update postgres 17rc1

--- TRIBblix-postgres17/build.sh
+++ TRIBblix-postgres17/build.sh
@@ -4,5 +4,5 @@
 #
 # postgres 17
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres17 postgresql-17beta3 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres17 postgresql-17beta3
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres17 postgresql-17rc1 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres17 postgresql-17rc1
