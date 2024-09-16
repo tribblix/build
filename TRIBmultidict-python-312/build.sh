0fe8d9c250cad9af95640b6108ac2cf282391444
Peter Tribble <peter.tribble@gmail.com>
2024-09-16 18:37:31 +0100

    Update multidict 6.1.0

--- TRIBmultidict-python-312/build.sh
+++ TRIBmultidict-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmultidict-python-312 multidict-6.0.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBmultidict-python-312 multidict-6.1.0
