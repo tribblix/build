9d46dc548397d9a1c1b79c113b6227cb7d537343
Peter Tribble <peter.tribble@gmail.com>
2024-09-19 08:13:40 +0100

    Update cyclonedx-python-lib 7.6.1

--- TRIBcyclonedx-python-312/build.sh
+++ TRIBcyclonedx-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBcyclonedx-python-312 cyclonedx_python_lib-7.6.0
+${THOME}/build/pkg_pep518 TRIBcyclonedx-python-312 cyclonedx_python_lib-7.6.1
