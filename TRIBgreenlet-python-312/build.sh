8f1f6cf5101a910d7574b49291b4843e80259b70
Peter Tribble <peter.tribble@gmail.com>
2024-09-20 17:33:57 +0100

    Update greenlet 3.1.0

--- TRIBgreenlet-python-312/build.sh
+++ TRIBgreenlet-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgreenlet-python-312 greenlet-3.0.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBgreenlet-python-312 greenlet-3.1.0
