14007d55d3753cf21beed4ec6350cd93d2fe4e61
Peter Tribble <peter.tribble@gmail.com>
2024-09-21 16:11:30 +0100

    Update configobj 5.0.9

--- TRIBconfigobj-python-312/build.sh
+++ TRIBconfigobj-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBconfigobj-python-312 configobj-5.0.8
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBconfigobj-python-312 configobj-5.0.9
