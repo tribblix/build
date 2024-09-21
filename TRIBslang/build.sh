498d9355478733a6dc3446fb1dcecec964604034
Peter Tribble <peter.tribble@gmail.com>
2024-09-21 17:00:28 +0100

    Update slang pre2.3.4-14

--- TRIBslang/build.sh
+++ TRIBslang/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only slang-pre2.3.4-11 -J 1 -C --without-onig
-${THOME}/build/genpkg TRIBslang slang-pre2.3.4-11
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only slang-pre2.3.4-14 -J 1 -C --without-onig
+${THOME}/build/genpkg TRIBslang slang-pre2.3.4-14
