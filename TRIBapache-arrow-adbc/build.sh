c93edbf03feee9e8f5588ef3c9c9aabbad4477e2
Peter Tribble <peter.tribble@gmail.com>
2024-09-21 12:37:53 +0100

    Add Apache Arrow ADBC

---
+++ TRIBapache-arrow-adbc/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# the aim here is to fully enable the [postgresql] extra for pandas,
+# which requires the python adbc-driver-postgresql module
+#
+# there's a little patch to make the postgresql build work on illumos
+#
+env TRIBBLIX_PKG_CONFIG_PATH=/opt/tribblix/postgres15/lib/pkgconfig $THOME/build/cmbuild -64only -S c apache-arrow-adbc-14 -C "-DADBC_BUILD_SHARED=ON -DADBC_BUILD_STATIC=OFF -DADBC_DRIVER_MANAGER=ON -DADBC_DRIVER_POSTGRESQL=ON"
+${THOME}/build/genpkg TRIBapache-arrow-adbc apache-arrow-adbc-14
