1e5e0edb84f360adaf15a77a4bd15cdb2b2ddad6
Peter Tribble <peter.tribble@gmail.com>
2024-09-21 17:03:08 +0100

    Update jed pre0.99.20-183

--- TRIBjed/build.sh
+++ TRIBjed/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # specify JED_ROOT otherwise it will install stuff directly into /usr/jed
 #
-env JED_ROOT=/usr/share/jed ${THOME}/build/dobuild -64only jed-pre0.99.20-180
-cd jed-pre0.99.20-180-64bit
+env JED_ROOT=/usr/share/jed ${THOME}/build/dobuild -64only jed-pre0.99.20-183
+cd jed-pre0.99.20-183-64bit
 gmake xjed
 cd ..
-${THOME}/build/genpkg TRIBjed jed-pre0.99.20-180
+${THOME}/build/genpkg TRIBjed jed-pre0.99.20-183
