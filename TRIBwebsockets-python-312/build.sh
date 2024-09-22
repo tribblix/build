c9e95929e882efdeed4db2509175132ac48dcccb
Peter Tribble <peter.tribble@gmail.com>
2024-09-22 20:07:20 +0100

    Update websockets 13.1

--- TRIBwebsockets-python-312/build.sh
+++ TRIBwebsockets-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwebsockets-python-312 websockets-13.0.1
+${THOME}/build/pkg_pep518 -N TRIBwebsockets-python-312 websockets-13.1
