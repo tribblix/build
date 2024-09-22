6642b3e42ff8b8de35a59aa1499f598de14e9719
Peter Tribble <peter.tribble@gmail.com>
2024-09-22 21:05:03 +0100

    Drop 32-bit libXres

--- TRIBx11-libxres/build.sh
+++ TRIBx11-libxres/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXres-1.2.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libXres-1.2.2
 ${THOME}/build/genpkg TRIBx11-libxres libXres-1.2.2
