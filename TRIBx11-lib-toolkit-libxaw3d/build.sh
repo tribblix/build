3c37ca2318152b06fea9c024ebdc812cb676ebab
Peter Tribble <peter.tribble@gmail.com>
2024-09-22 21:08:38 +0100

    Drop 32-bit libXaw3d

--- TRIBx11-lib-toolkit-libxaw3d/build.sh
+++ TRIBx11-lib-toolkit-libxaw3d/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libXaw3d-1.6.6
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libXaw3d-1.6.6
 ${THOME}/build/genpkg TRIBx11-lib-toolkit-libxaw3d libXaw3d-1.6.6
