47646ac33e14d029062a3645e920a60c89fcc39b
Peter Tribble <peter.tribble@gmail.com>
2024-09-23 08:36:51 +0100

    Update libzip 1.11.1

--- TRIBlibzip/build.sh
+++ TRIBlibzip/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the consumer is php, so use the same TLS implementation (openssl)
 #
-${THOME}/build/cmbuild -64only libzip-1.10.1 -C -DENABLE_GNUTLS=OFF
-${THOME}/build/genpkg TRIBlibzip libzip-1.10.1
+${THOME}/build/cmbuild -64only libzip-1.11.1 -C -DENABLE_GNUTLS=OFF
+${THOME}/build/genpkg TRIBlibzip libzip-1.11.1
