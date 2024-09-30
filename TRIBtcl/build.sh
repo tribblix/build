9dc7e05712423986ca37e77e602d511b68de76bc
Peter Tribble <peter.tribble@gmail.com>
2024-09-30 13:34:02 +0100

    Update Tcl/Tk 8.6.15

--- TRIBtcl/build.sh
+++ TRIBtcl/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # build tcl and tk together
 #
 rm -fr /tmp/tk1 /tmp/tc1
-${THOME}/build/unpack tcl8.6.14-src
-cd tcl8.6.14/unix
+${THOME}/build/unpack tcl8.6.15-src
+cd tcl8.6.15/unix
 env CFLAGS=-O ./configure --prefix=/usr
 gmake -j 4
 env LD_LIBRARY_PATH=/tmp/tc1/usr/lib gmake install DESTDIR=/tmp/tc1
@@ -13,8 +15,8 @@ cd ../..
 
 mkdir -p 64bit
 cd 64bit
-${THOME}/build/unpack tcl8.6.14-src
-cd tcl8.6.14/unix
+${THOME}/build/unpack tcl8.6.15-src
+cd tcl8.6.15/unix
 env CFLAGS=-O ./configure --prefix=/usr --enable-64bit --bindir=/usr/bin/`isainfo -k` --libdir=/usr/lib/`isainfo -k`
 gmake -j 4
 env LD_LIBRARY_PATH=/tmp/tc1/usr/lib gmake install DESTDIR=/tmp/tc1
