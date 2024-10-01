08c1d809bfc52e3ec2a29ca75e8e4ac5564d0267
Peter Tribble <peter.tribble@gmail.com>
2024-10-01 08:35:50 +0100

    Update setuptools 75.1.0

--- TRIBsetuptools-python-312/build.sh
+++ TRIBsetuptools-python-312/build.sh
@@ -9,4 +9,4 @@
 # and unpack that in postinstall, like we used to do for python
 # itself in prior releases
 #
-${THOME}/build/pkg_pep518 -N TRIBsetuptools-python-312 setuptools-74.1.1
+${THOME}/build/pkg_pep518 -N TRIBsetuptools-python-312 setuptools-75.1.0
