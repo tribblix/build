16aa04d22defe4c017ab0429de6de1c47d92e9ad
Peter Tribble <peter.tribble@gmail.com>
2024-10-01 09:00:39 +0100

    Update pydantic 2.9.2

--- TRIBpydantic-python-312/build.sh
+++ TRIBpydantic-python-312/build.sh
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # build together, as they're joined at the hip and version locked
+# (note that pydantic_core may need to be slightly downrev to match)
 #
-${THOME}/build/pkg_pep518 TRIBpydantic-python-312 pydantic_core-2.20.1 pydantic-2.8.2
+${THOME}/build/pkg_pep518 TRIBpydantic-python-312 pydantic_core-2.23.4 pydantic-2.9.2
