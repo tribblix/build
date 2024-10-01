edc4b1dd5f75d7a623bf3554a6a986dca912777d
Peter Tribble <peter.tribble@gmail.com>
2024-10-01 14:35:46 +0100

    Update libical 3.0.18

--- TRIBlibical/build.sh
+++ TRIBlibical/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64only libical-3.0.17 -C "-DSHARED_ONLY=yes -DICAL_BUILD_DOCS=false"
-${THOME}/build/genpkg TRIBlibical libical-3.0.17
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild -64only libical-3.0.18 -C "-DSHARED_ONLY=yes -DICAL_BUILD_DOCS=false"
+${THOME}/build/genpkg TRIBlibical libical-3.0.18
