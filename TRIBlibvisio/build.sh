5e4f630a4a77b8cae3963549c5f276589784f751
Peter Tribble <peter.tribble@gmail.com>
2024-10-01 22:03:42 +0100

    Rebuild libvisio with new icu

--- TRIBlibvisio/build.sh
+++ TRIBlibvisio/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 env TRIBBLIX_CFLAGS=-pthreads TRIBBLIX_CXXFLAGS=-pthreads ${THOME}/build/dobuild -64only libvisio-0.1.7
 ${THOME}/build/genpkg TRIBlibvisio libvisio-0.1.7
