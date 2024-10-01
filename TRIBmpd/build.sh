947a7f6f7aca0b33dde1844dcb3b634ae7200b22
Peter Tribble <peter.tribble@gmail.com>
2024-10-01 14:48:50 +0100

    Rebuild mpd against current libicu

--- TRIBmpd/build.sh
+++ TRIBmpd/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # modplug and sndfile are incompatible and break the build
 #
 ${THOME}/build/mesonbuild -64only mpd-0.23.15 -C -Dmodplug=disabled
