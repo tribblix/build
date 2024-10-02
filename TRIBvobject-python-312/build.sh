78d9a4a7f243b615735e531d3edf99970818a71d
Peter Tribble <peter.tribble@gmail.com>
2024-10-02 08:07:50 +0100

    Update vobject 0.9.8

--- TRIBvobject-python-312/build.sh
+++ TRIBvobject-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBvobject-python-312 vobject-0.9.7
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBvobject-python-312 vobject-0.9.8
