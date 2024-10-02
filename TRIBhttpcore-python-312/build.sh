77800c5d3efb5426213d794acd6459539b6dcc91
Peter Tribble <peter.tribble@gmail.com>
2024-10-02 08:25:47 +0100

    Update httpcore 1.0.6

--- TRIBhttpcore-python-312/build.sh
+++ TRIBhttpcore-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBhttpcore-python-312 httpcore-1.0.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBhttpcore-python-312 httpcore-1.0.6
