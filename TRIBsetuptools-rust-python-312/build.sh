620a337cd484167191c88bc4aaa9100250f15f38
Peter Tribble <peter.tribble@gmail.com>
2024-10-02 08:11:26 +0100

    Update setuptools-rust 1.10.2

--- TRIBsetuptools-rust-python-312/build.sh
+++ TRIBsetuptools-rust-python-312/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # limited to x86 as rust is only present there
 #
-${THOME}/build/pkg_pep518 -N TRIBsetuptools-rust-python-312 setuptools_rust-1.10.1
+${THOME}/build/pkg_pep518 -N TRIBsetuptools-rust-python-312 setuptools_rust-1.10.2
