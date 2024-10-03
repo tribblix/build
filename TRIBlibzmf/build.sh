7a93c0bdaadb100b6aeb00236498bb4d610f7563
Peter Tribble <peter.tribble@gmail.com>
2024-10-03 08:37:53 +0100

    Rebuild libzmf with new icu

--- TRIBlibzmf/build.sh
+++ TRIBlibzmf/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # wget https://dev-www.libreoffice.org/src/libzmf/libzmf-0.0.2.tar.xz
 #
 ${THOME}/build/dobuild -64only libzmf-0.0.2 -C --disable-werror
