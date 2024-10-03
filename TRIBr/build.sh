5b29ab2413f3773da50019c53b600ca0c3baa732
Peter Tribble <peter.tribble@gmail.com>
2024-10-03 13:17:57 +0100

    Update R 4.4.1

--- TRIBr/build.sh
+++ TRIBr/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # note that readline can cause build failures elsewhere
 # so shouldn't be left installed on a build system
 #
 zap install-overlay fortran
 zap install TRIBgnu-libiconv TRIBreadline
-env TRIBBLIX_CFLAGS=-I/usr/gnu/include TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" FFLAGS=-m64 $THOME/build/dobuild -gnu -64only R-4.3.1 -C --disable-openmp
+env TRIBBLIX_CFLAGS=-I/usr/gnu/include TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" FFLAGS=-m64 $THOME/build/dobuild -gnu -64only R-4.4.1 -C --disable-openmp
 
-cd R-4.3.1-64bit
+cd R-4.4.1-64bit
 gmake check
 cd ..
 
-$THOME/build/genpkg TRIBr R-4.3.1
+$THOME/build/genpkg TRIBr R-4.4.1
