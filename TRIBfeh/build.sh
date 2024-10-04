c6110adf97d8d674676a70b06de8dcc8180f9efa
Peter Tribble <peter.tribble@gmail.com>
2024-10-04 15:22:01 +0100

    Update feh 3.10.3

--- TRIBfeh/build.sh
+++ TRIBfeh/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs a patch to fix the makefile
 #
-${THOME}/build/dobuild feh-3.9.1
-${THOME}/build/genpkg TRIBfeh feh-3.9.1
+${THOME}/build/dobuild feh-3.10.3
+${THOME}/build/genpkg TRIBfeh feh-3.10.3
