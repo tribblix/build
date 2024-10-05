082d87e0b518096f29098dd588b436055ccf4182
Peter Tribble <peter.tribble@gmail.com>
2024-10-05 09:52:19 +0100

    Update gdk-pixbuf 2.42.12

--- TRIBgdk-pixbuf/build.sh
+++ TRIBgdk-pixbuf/build.sh
@@ -4,8 +4,5 @@
 #
 # needs python in the path to find rst2man to generate man pages
 #
-# this version drops gdk-pixbuf-xlib, but it's deprecated and I can find
-# no consumers in Tribblix
-#
-env AR=/usr/bin/ar PATH=${PATH}:/usr/versions/python-3.12/bin ${THOME}/build/mesonbuild -64 -gnu gdk-pixbuf-2.42.10 -C '-Dtests=false -Dinstalled_tests=false -Dintrospection=disabled'
-${THOME}/build/genpkg TRIBgdk-pixbuf gdk-pixbuf-2.42.10
+env AR=/usr/bin/ar PATH=${PATH}:/usr/versions/python-3.12/bin ${THOME}/build/mesonbuild -64 -gnu gdk-pixbuf-2.42.12 -C '-Dothers=enabled -Dtests=false -Dinstalled_tests=false -Dintrospection=disabled'
+${THOME}/build/genpkg TRIBgdk-pixbuf gdk-pixbuf-2.42.12
