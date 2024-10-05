deaa19a366ecbe44d268d5c5b29a8dede9654bf3
Peter Tribble <peter.tribble@gmail.com>
2024-10-05 13:16:43 +0100

    Update snac 2.59

--- TRIBblix-snac/build.sh
+++ TRIBblix-snac/build.sh
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# there's a patch to fix a variable named "sun"
-#
-env CC=gcc CFLAGS="-O -m64" LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64only -n snac2 snac2-2.57
-sed -i -e 's:=/usr/local:=/opt/tribblix/snac:' -e 's: $(PREFIX: $(DESTDIR)$(PREFIX:' -e 's:install -:ginstall -:' snac2-2.57-64bit/Makefile
-${THOME}/build/genpkg TRIBblix-snac snac2-2.57
+env CC=gcc CFLAGS="-O -m64" LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64only -n snac2 snac2-2.59
+sed -i -e 's:=/usr/local:=/opt/tribblix/snac:' -e 's: $(PREFIX: $(DESTDIR)$(PREFIX:' -e 's:install -:ginstall -:' snac2-2.59-64bit/Makefile
+${THOME}/build/genpkg TRIBblix-snac snac2-2.59
