0b5cdb13e53b9f246345c4389146ce99d4839dbf
Peter Tribble <peter.tribble@gmail.com>
2024-10-06 09:00:55 +0100

    Update dnspython 2.7.0

--- TRIBdnspython-python-312/build.sh
+++ TRIBdnspython-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBdnspython-python-312 dnspython-2.6.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBdnspython-python-312 dnspython-2.7.0
