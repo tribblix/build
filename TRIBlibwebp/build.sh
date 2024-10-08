8a6c6c59ea27a4dced30312761b7740f2cad398d
Peter Tribble <peter.tribble@gmail.com>
2024-10-08 09:03:37 +0100

    Update libwebp 1.4.0

--- TRIBlibwebp/build.sh
+++ TRIBlibwebp/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # webp
 #
-${THOME}/build/dobuild -64 libwebp-1.3.2 -C "--disable-gl"
-${THOME}/build/genpkg TRIBlibwebp libwebp-1.3.2
+${THOME}/build/dobuild -64 libwebp-1.4.0
+${THOME}/build/genpkg TRIBlibwebp libwebp-1.4.0
