9e56a53279a2bfe1404e483d71818c18530738c4
Peter Tribble <peter.tribble@gmail.com>
2024-10-08 08:41:18 +0100

    Update mc 4.8.32

--- TRIBmc/build.sh
+++ TRIBmc/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only mc-4.8.31
-${THOME}/build/genpkg TRIBmc mc-4.8.31
+${THOME}/build/dobuild -64only mc-4.8.32
+${THOME}/build/genpkg TRIBmc mc-4.8.32
