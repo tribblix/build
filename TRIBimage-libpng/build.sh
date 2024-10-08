3bb50fef03351dff9b7ca0ec1901651c6e9c3f49
Peter Tribble <peter.tribble@gmail.com>
2024-10-08 08:52:13 +0100

    Update libpng 1.6.44

--- TRIBimage-libpng/build.sh
+++ TRIBimage-libpng/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # old versions shipped in TRIBlibpng-compat
 #
 # ship both 1.4 and 1.6
 #
 ${THOME}/build/dobuild -64 libpng-1.4.22 -C --sysconfdir=/etc
-${THOME}/build/dobuild -64 libpng-1.6.43 -C --sysconfdir=/etc
-${THOME}/build/genpkg TRIBimage-libpng libpng-1.4.22 libpng-1.6.43
+${THOME}/build/dobuild -64 libpng-1.6.44 -C --sysconfdir=/etc
+${THOME}/build/genpkg TRIBimage-libpng libpng-1.4.22 libpng-1.6.44
