58132543da4de39c8be148262681fb5e4fc52e5b
Peter Tribble <peter.tribble@gmail.com>
2024-10-10 09:51:38 +0100

    Need to disable GL for libwebp on sparc

--- TRIBlibwebp/build.sh
+++ TRIBlibwebp/build.sh
@@ -4,5 +4,8 @@
 #
 # webp
 #
-${THOME}/build/dobuild -64 libwebp-1.4.0
+# explicitly disable GL, as it's accidentally picked up on sparc
+# if we don't
+#
+${THOME}/build/dobuild -64 libwebp-1.4.0 -C --disable-gl
 ${THOME}/build/genpkg TRIBlibwebp libwebp-1.4.0
