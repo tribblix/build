cade46d33601cc0ac84ff6f14d8da9cdc7803af3
Peter Tribble <peter.tribble@gmail.com>
2024-10-10 08:07:55 +0100

    Update distlib 0.3.9

--- TRIBdistlib-python-312/build.sh
+++ TRIBdistlib-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBdistlib-python-312 distlib-0.3.8
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBdistlib-python-312 distlib-0.3.9
