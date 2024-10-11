c8197197e01a858dc86f6352f42511e0172907ce
Peter Tribble <peter.tribble@gmail.com>
2024-10-11 09:29:38 +0100

    Update jsonschema-specifications 2024.10.1

--- TRIBjsonschema-spec-python-312/build.sh
+++ TRIBjsonschema-spec-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBjsonschema-spec-python-312 jsonschema_specifications-2023.12.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBjsonschema-spec-python-312 jsonschema_specifications-2024.10.1
