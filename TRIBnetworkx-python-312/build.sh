1a03f34c73a06097c61014b6b8a9a3d4ef3b494d
Peter Tribble <peter.tribble@gmail.com>
2024-10-11 09:24:39 +0100

    Update networkx 3.4

--- TRIBnetworkx-python-312/build.sh
+++ TRIBnetworkx-python-312/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the dependencies we declare are those for networkx[default]
 #
-${THOME}/build/pkg_pep518 TRIBnetworkx-python-312 networkx-3.3
+${THOME}/build/pkg_pep518 TRIBnetworkx-python-312 networkx-3.4
