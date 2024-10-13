39371fed0ac0d510640a5e7180305dfbf10fbacc
Peter Tribble <peter.tribble@gmail.com>
2024-10-13 14:59:32 +0100

    Update xkbprint 1.0.7

--- TRIBx11-keyboard-xkb-utilities/build.sh
+++ TRIBx11-keyboard-xkb-utilities/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
-# historically this was a merged package and I can't see any benefit
+# SPDX-License-Identifier: CDDL-1.0
+#
+# historically this was a merged package and I see no benefit
 # from fragmenting it
 #
 ${THOME}/build/dobuild -64only xkbutils-1.0.6
-${THOME}/build/dobuild -64only xkbprint-1.0.6
+${THOME}/build/dobuild -64only xkbprint-1.0.7
 ${THOME}/build/dobuild -64only setxkbmap-1.3.4
-${THOME}/build/genpkg TRIBx11-keyboard-xkb-utilities xkbutils-1.0.6 xkbprint-1.0.6 setxkbmap-1.3.4
+${THOME}/build/genpkg TRIBx11-keyboard-xkb-utilities xkbutils-1.0.6 xkbprint-1.0.7 setxkbmap-1.3.4
