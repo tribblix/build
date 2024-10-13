690f13301227020e6666dc8785b3de918de78b68
Peter Tribble <peter.tribble@gmail.com>
2024-10-13 14:52:32 +0100

    Update bdftopcf 1.1.2

--- TRIBx11-font-utilities/build.sh
+++ TRIBx11-font-utilities/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a merge of several smaller packages, that was the way
 # it was inherited.
 #
@@ -7,7 +9,7 @@
 #
 # do not build font-util -64only, we need both the 32 and 64 bit pc file
 #
-${THOME}/build/dobuild -64only bdftopcf-1.1
+${THOME}/build/dobuild -64only bdftopcf-1.1.2
 ${THOME}/build/dobuild -64only mkfontscale-1.2.3
 ${THOME}/build/dobuild -64 font-util-1.4.1
-${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1 mkfontscale-1.2.3 font-util-1.4.1
+${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1.2 mkfontscale-1.2.3 font-util-1.4.1
