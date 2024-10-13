08555ec0b3c0519f2888cac35fcf40e256929b30
Peter Tribble <peter.tribble@gmail.com>
2024-10-13 14:22:44 +0100

    Udpate networkx 3.4.1

--- TRIBnetworkx-python-312/build.sh
+++ TRIBnetworkx-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # the dependencies we declare are those for networkx[default]
 #
-${THOME}/build/pkg_pep518 TRIBnetworkx-python-312 networkx-3.4
+${THOME}/build/pkg_pep518 TRIBnetworkx-python-312 networkx-3.4.1
