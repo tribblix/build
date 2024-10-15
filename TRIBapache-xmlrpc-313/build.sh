9efda8f9c29c0766b0ea6fa5d8e0c6fee9ea976a
Peter Tribble <peter.tribble@gmail.com>
2024-10-15 20:19:22 +0100

    Need a build recipe for apache-xmlrpc

---
+++ TRIBapache-xmlrpc-313/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# this is an old archoved project, get apache-xmlrpc-3.1.3-bin.tar.gz
+# from https://archive.apache.org/dist/ws/xmlrpc/binaries/
+#
+# get the fixit script to do all the work
+#
+${THOME}/build/genpkg TRIBapache-xmlrpc-313
