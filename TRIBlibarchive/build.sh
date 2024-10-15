26f777f88a8ec9e4cfb64fa5e74b42712920cfa8
Peter Tribble <peter.tribble@gmail.com>
2024-10-15 15:43:11 +0100

    Update libarchive 3.7.7

--- TRIBlibarchive/build.sh
+++ TRIBlibarchive/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libarchive-3.7.4
-${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.4
+${THOME}/build/dobuild -64only libarchive-3.7.7
+${THOME}/build/genpkg TRIBlibarchive libarchive-3.7.7
