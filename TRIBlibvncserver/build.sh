baa0be32fe328d384f9aca1da0fbace29b59efc9
Peter Tribble <peter.tribble@gmail.com>
2024-10-15 10:04:04 +0100

    Rebuild libvncserver so that dependencies are correct

--- TRIBlibvncserver/build.sh
+++ TRIBlibvncserver/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # libvncserver
 #
 env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/cmbuild -64only libvncserver-LibVNCServer-0.9.14
