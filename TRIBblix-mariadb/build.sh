b3ed062476b2e38c04e02bbf7ffbfdae6249187c
Peter Tribble <peter.tribble@gmail.com>
2024-10-15 13:19:38 +0100

    Update mariadb 10.11.9

--- TRIBblix-mariadb/build.sh
+++ TRIBblix-mariadb/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only mariadb-10.11.8 -P /opt/tribblix/mariadb
-${THOME}/build/genpkg TRIBblix-mariadb mariadb-10.11.8
+${THOME}/build/cmbuild -64only mariadb-10.11.9 -P /opt/tribblix/mariadb
+${THOME}/build/genpkg TRIBblix-mariadb mariadb-10.11.9
