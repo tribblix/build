fc8b32a224b52e1c652f1ee061de69a2fc8f0a15
Peter Tribble <peter.tribble@gmail.com>
2024-10-16 15:08:55 +0100

    Update ncdu 1.20

--- TRIBncdu/build.sh
+++ TRIBncdu/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
+# use the 'legacy' 1.x branch; 2.x requires zig
 #
-env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncurses -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only ncdu-1.19
-${THOME}/build/genpkg TRIBncdu ncdu-1.19
+env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only ncdu-1.20
+${THOME}/build/genpkg TRIBncdu ncdu-1.20
