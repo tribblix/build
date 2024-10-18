e11941896359f10f743432fa85d79da11dfe6a1b
Peter Tribble <peter.tribble@gmail.com>
2024-10-18 18:53:03 +0100

    Update psycopg2 2.9.10

--- TRIBpsycopg2-312/build.sh
+++ TRIBpsycopg2-312/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the binary package duplicates the actual functionality of the regular
 # one, so 'import psycopg2' will work either way, but building and
 # installing both means that pip will see both
 #
-env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg2-312 psycopg2-2.9.9 psycopg2-binary-2.9.9
+env PATH=/opt/tribblix/postgres15/bin:$PATH LD_RUN_PATH=/opt/tribblix/postgres15/lib $THOME/build/pkg_setup_py TRIBpsycopg2-312 psycopg2-2.9.10 psycopg2-binary-2.9.10
