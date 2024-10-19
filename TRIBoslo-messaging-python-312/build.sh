8b548dc9bef1fbe187e26e50ef3a187475abbe82
Peter Tribble <peter.tribble@gmail.com>
2024-10-19 10:08:54 +0100

    Update oslo.messaging 14.9.1

--- TRIBoslo-messaging-python-312/build.sh
+++ TRIBoslo-messaging-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-messaging-python-312 oslo.messaging-14.9.0
+${THOME}/build/pkg_setup_py TRIBoslo-messaging-python-312 oslo.messaging-14.9.1
