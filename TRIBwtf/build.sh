d1e812e9e62e89bca8aec9558cf3b0cc7b03f71f
Peter Tribble <peter.tribble@gmail.com>
2024-10-22 10:09:45 +0100

    Update wtf 20230906

--- TRIBwtf/build.sh
+++ TRIBwtf/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # look at fixinstall
 #
-${THOME}/build/unpack wtf-20230804
-${THOME}/build/genpkg TRIBwtf wtf-20230804
+${THOME}/build/unpack wtf-20230906
+${THOME}/build/genpkg TRIBwtf wtf-20230906
