010bf7e7c1c92892fe6f39baa400e0e1339308e3
Peter Tribble <peter.tribble@gmail.com>
2024-10-23 10:17:37 +0100

    Update MarkupSafe 3.0.2

--- TRIBmarkupsafe-python-312/build.sh
+++ TRIBmarkupsafe-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBmarkupsafe-python-312 MarkupSafe-2.1.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBmarkupsafe-python-312 markupsafe-3.0.2
