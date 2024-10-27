80c8af9869e0a3fbd39b7bc89a11d7be2bbb7fb1
Peter Tribble <peter.tribble@gmail.com>
2024-10-27 08:33:14 +0000

    Update meson-python 0.17.1

--- TRIBmesonpy-python-312/build.sh
+++ TRIBmesonpy-python-312/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # note that this isn't meson, it's the meson-python aka mesonpy
 # module
 #
@@ -7,4 +9,4 @@
 #
 # used by the pandas and scikit-image build
 #
-${THOME}/build/pkg_pep518 TRIBmesonpy-python-312 meson_python-0.16.0
+${THOME}/build/pkg_pep518 TRIBmesonpy-python-312 meson_python-0.17.1
