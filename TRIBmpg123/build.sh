c8cf748b5339e69c3f7d860489e26b492a6c33a7
Peter Tribble <peter.tribble@gmail.com>
2024-10-28 10:43:40 +0000

    Update mpg123 1.32.8

--- TRIBmpg123/build.sh
+++ TRIBmpg123/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #
-env TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" ${THOME}/build/dobuild -64 mpg123-1.32.5 \
+# SPDX-License-Identifier: CDDL-1.0
+#
+# uses assembler, so sparc build is different
+#
+env TRIBBLIX_CFLAGS="-D__EXTENSIONS__ -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" ${THOME}/build/dobuild -64 mpg123-1.32.8 \
     -C "--disable-static --enable-shared --enable-int-quality --enable-fifo --enable-network --enable-ipv6=yes --with-optimization=3 --with-cpu=sse" \
     +C "--disable-static --enable-shared --enable-int-quality --enable-fifo --enable-network --enable-ipv6=yes --with-optimization=3 --with-cpu=x86-64"
 #
-# sparc
-# -C "--disable-static --enable-shared --enable-int-quality --enable-fifo --enable-network --enable-ipv6=yes --with-optimization=3"
-#
-${THOME}/build/genpkg TRIBmpg123 mpg123-1.32.5
+${THOME}/build/genpkg TRIBmpg123 mpg123-1.32.8
