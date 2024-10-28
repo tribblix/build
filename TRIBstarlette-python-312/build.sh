10b88906dcc3116ad723e95b38aa5deb4840e3bf
Peter Tribble <peter.tribble@gmail.com>
2024-10-28 08:37:08 +0000

    Update starlette 0.41.2

--- TRIBstarlette-python-312/build.sh
+++ TRIBstarlette-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # check the version is within the range permitted by fastapi before updating
 #
-${THOME}/build/pkg_pep518 -N TRIBstarlette-python-312 starlette-0.41.0
+${THOME}/build/pkg_pep518 -N TRIBstarlette-python-312 starlette-0.41.2
