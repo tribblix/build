06505d764cf338e8f76b2a9602de8a7e185d8fa6
Peter Tribble <peter.tribble@gmail.com>
2024-10-28 10:23:40 +0000

    Update nghttp2 1.64.0

--- TRIBlibnghttp2/build.sh
+++ TRIBlibnghttp2/build.sh
@@ -4,5 +4,5 @@
 #
 # just the library
 #
-${THOME}/build/dobuild -64 nghttp2-1.63.0 -C --enable-lib-only
-${THOME}/build/genpkg TRIBlibnghttp2 nghttp2-1.63.0
+${THOME}/build/dobuild -64 nghttp2-1.64.0 -C --enable-lib-only
+${THOME}/build/genpkg TRIBlibnghttp2 nghttp2-1.64.0
