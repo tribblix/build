8353dca597745466f9640cb946b0d3a58833368a
Peter Tribble <peter.tribble@gmail.com>
2024-10-30 10:55:58 +0000

    Drop 32-bit libxkbfile

--- TRIBx11-libxkbfile/build.sh
+++ TRIBx11-libxkbfile/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # we patch the SONAME, and do so for both autotools and meson
 #
-${THOME}/build/mesonbuild -64 libxkbfile-1.1.3
+${THOME}/build/mesonbuild -64only libxkbfile-1.1.3
 ${THOME}/build/genpkg TRIBx11-libxkbfile libxkbfile-1.1.3
