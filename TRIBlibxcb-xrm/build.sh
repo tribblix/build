5f91390c496ec96b4941f2069cd4cf20c87e6d30
Peter Tribble <peter.tribble@gmail.com>
2024-10-30 10:46:13 +0000

    Drop 32-bit xcb-util-xrm

--- TRIBlibxcb-xrm/build.sh
+++ TRIBlibxcb-xrm/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # packaged separately as it's distributed independently
 # used by the i3 window manager
 #
-${THOME}/build/dobuild -64 xcb-util-xrm-1.3
+${THOME}/build/dobuild -64only xcb-util-xrm-1.3
 ${THOME}/build/genpkg TRIBlibxcb-xrm xcb-util-xrm-1.3
