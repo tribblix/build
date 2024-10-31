c58bc703e88343421512b124b07dd27f9e990cf7
Peter Tribble <peter.tribble@gmail.com>
2024-10-31 09:13:15 +0000

    Update linode-cli 5.54.0

--- TRIBlinode-cli-python-312/build.sh
+++ TRIBlinode-cli-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBlinode-cli-python-312 linode_cli-5.53.1
+${THOME}/build/pkg_setup_py TRIBlinode-cli-python-312 linode_cli-5.54.0
