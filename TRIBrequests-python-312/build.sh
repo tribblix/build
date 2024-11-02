5932a1f6aef64f7d298d82f939049068d7e3bc02
Peter Tribble <peter.tribble@gmail.com>
2024-11-02 10:41:27 +0000

    Update requests 2.32.3 (now unpinned by httpie)

--- TRIBrequests-python-312/build.sh
+++ TRIBrequests-python-312/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBrequests-python-312 requests-2.31.0
+# in addition to the mandatory dependencies, we add PySocks, as
+# httpie requires the socks extra
+#
+${THOME}/build/pkg_setup_py TRIBrequests-python-312 requests-2.32.3
