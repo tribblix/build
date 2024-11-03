49d072fc12f8aaaa2d11db3871973cc41b65ab20
Peter Tribble <peter.tribble@gmail.com>
2024-11-03 12:06:37 +0000

    Update libidn 1.42, drop 32-bit

--- TRIBlibidn/build.sh
+++ TRIBlibidn/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libidn-1.41
-${THOME}/build/genpkg TRIBlibidn libidn-1.41
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libidn-1.42
+${THOME}/build/genpkg TRIBlibidn libidn-1.42
