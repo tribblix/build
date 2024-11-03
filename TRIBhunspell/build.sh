6a76cf8e525d87c3973ceef74626506f87d680c9
Peter Tribble <peter.tribble@gmail.com>
2024-11-03 11:47:07 +0000

    Drop 32-bit hunspell

--- TRIBhunspell/build.sh
+++ TRIBhunspell/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64 hunspell-1.7.2 -C "--disable-static"
+env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64only hunspell-1.7.2 -C "--disable-static"
 ${THOME}/build/genpkg TRIBhunspell hunspell-1.7.2
