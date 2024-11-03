b950294cfb458a5f08d993e056611ba07dfbcb76
Peter Tribble <peter.tribble@gmail.com>
2024-11-03 17:36:28 +0000

    Update libX11 1.8.10

--- TRIBx11-libx11/build.sh
+++ TRIBx11-libx11/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
-# needs TRIBdev-xorg-macros in order to build
+# SPDX-License-Identifier: CDDL-1.0
+#
+# for historical reasons, and to maintain compatibility, we add most of
+# the patches and additional features that Solaris did in the past
 #
 env CPPFLAGS="-D SUNSOFT -DMOTIFBC -DSUNSOFT_KBD" \
-$THOME/build/dobuild -64 libX11-1.6.12 -C \
+$THOME/build/dobuild -64 libX11-1.8.10 -C \
 "--enable-xkb --enable-xthreads --enable-static=no --enable-xf86bigfont --enable-xlocaledir --enable-loadable-i18n --without-xmlto --without-fop --localstatedir=/var --enable-loadable-xcursor"
-${THOME}/build/genpkg TRIBx11-libx11 libX11-1.6.12
+${THOME}/build/genpkg TRIBx11-libx11 libX11-1.8.10
