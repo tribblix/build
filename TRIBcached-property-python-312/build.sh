6fa309650a36d6dc6e6d2b162ea2441897382f12
Peter Tribble <peter.tribble@gmail.com>
2024-11-03 09:38:08 +0000

    Update cached-property 2.0.1

--- TRIBcached-property-python-312/build.sh
+++ TRIBcached-property-python-312/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/unpack cached-property-1.5.2
-cd cached-property-1.5.2
-/usr/versions/python-3.12/bin/python3 setup.py build
-rm -fr /tmp/pil
-/usr/versions/python-3.12/bin/python3 setup.py install --root=/tmp/pil
-${THOME}/build/create_pkg TRIBcached-property-python-312 /tmp/pil
-cd ..
-rm -fr /tmp/pil
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBcached-property-python-312 cached_property-2.0.1
