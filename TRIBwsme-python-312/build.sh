f460a8e4b1b05c47bb7c6703ee50e30ebdefd2aa
Peter Tribble <peter.tribble@gmail.com>
2024-11-03 12:22:50 +0000

    Rebuild wsme package to fix errant permissions

--- TRIBwsme-python-312/build.sh
+++ TRIBwsme-python-312/build.sh
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # we add simplegeneric as a private dependency
+# note that simplegeneric permissions need fixing, see the fixit file
 #
 ${THOME}/build/pkg_setup_py TRIBwsme-python-312 WSME-0.12.1 simplegeneric-0.8.1
