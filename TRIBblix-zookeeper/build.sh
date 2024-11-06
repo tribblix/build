6c5f56f34ae767a3d13af9f22ae71f82dc28537d
Peter Tribble <peter.tribble@gmail.com>
2024-11-06 16:24:20 +0000

    Update Apache ZooKeeper 3.9.3

--- TRIBblix-zookeeper/build.sh
+++ TRIBblix-zookeeper/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_tarball -T TRIBblix-zookeeper apache-zookeeper-3.8.4-bin
+${THOME}/build/pkg_tarball -T TRIBblix-zookeeper apache-zookeeper-3.9.3-bin
