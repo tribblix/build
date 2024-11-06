006a414bd27cc3187b9c9d2a070add9bccd1fdf0
Peter Tribble <peter.tribble@gmail.com>
2024-11-06 21:46:50 +0000

    mog should use pkg_setup_py

--- TRIBmog/build.sh
+++ TRIBmog/build.sh
@@ -3,16 +3,6 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # needs pygments and file-magic and mediainfo
+# there's a preconf script so we use the correct file command
 #
-${THOME}/build/unpack mog-0.6.0
-cd mog-0.6.0
-#
-# we need the file from file-magic
-#
-gsed -i "s:\['file':\['file-5.45':" mog/__init__.py
-/usr/versions/python-3.12/bin/python3 setup.py build
-rm -fr /tmp/sut
-/usr/versions/python-3.12/bin/python3 setup.py install --root=/tmp/sut
-${THOME}/build/create_pkg TRIBmog /tmp/sut
-cd ..
-rm -fr /tmp/sut
+${THOME}/build/pkg_setup_py -v 312 TRIBmog mog-0.6.0
