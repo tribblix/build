2f18d5df94cd2a514407dab08e7ebf9057b75100
Peter Tribble <peter.tribble@gmail.com>
2024-11-08 09:32:28 +0000

    Add github-archive 6.1.2

---
+++ TRIBgithub-archive-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBgithub-archive-python-312 github-archive-6.1.2
---
+++ TRIBwoodchips-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBwoodchips-python-312 woodchips-1.0.0
