8eceabbd92858333b2640de2d465d5f19a2376ed
Peter Tribble <peter.tribble@gmail.com>
2024-11-08 17:59:44 +0000

    Update pixman 0.44.0

--- TRIBlib-graphics-pixman/build.sh
+++ TRIBlib-graphics-pixman/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # disabling gtk is just for the tests, it doesn't change the
 # functionality
 #
-${THOME}/build/mesonbuild -64 pixman-0.43.4 -C "-Dgtk=disabled -Ddemos=disabled"
-${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.43.4
+${THOME}/build/mesonbuild -64 pixman-0.44.0 -C "-Dgtk=disabled -Ddemos=disabled"
+${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.44.0
