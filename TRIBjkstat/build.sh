e6aa3528be87260fb286600eb21ea2225c9fff58
Peter Tribble <peter.tribble@gmail.com>
2024-11-11 12:58:14 +0000

    Update jkstat

--- TRIBjkstat/build.sh
+++ TRIBjkstat/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # In jkstat itself:
 ./build packages deps
 #
@@ -8,7 +10,8 @@
 #
 # then copy the packages
 #
-mv /tmp/TRIBjkstat-i386.pkg ${THOME}/pkgs/TRIBjkstat.1.6.0.pkg
-mv /tmp/TRIBjkstat-sparc.pkg ${THOME}/pkgs.sparc/TRIBjkstat.1.6.0.pkg
-${THOME}/build/pkg2zap ${THOME}/pkgs/TRIBjkstat.1.6.0.pkg ${THOME}/pkgs
-${THOME}/build/pkg2zap ${THOME}/pkgs.sparc/TRIBjkstat.1.6.0.pkg ${THOME}/pkgs.sparc
+mkdir -p /tmp/pct
+mv /tmp/TRIBjkstat-i386.pkg /tmp/pct/TRIBjkstat.1.7.0.pkg
+mv /tmp/TRIBjkstat-sparc.pkg ${THOME}/pkgs.sparc/TRIBjkstat.1.7.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBjkstat.1.7.0.pkg /tmp/pct
+${THOME}/build/pkg2zap ${THOME}/pkgs.sparc/TRIBjkstat.1.7.0.pkg ${THOME}/pkgs.sparc
