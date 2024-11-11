3ff82580f01930ff0a158c9a44a3a3075beb3bab
Peter Tribble <peter.tribble@gmail.com>
2024-11-11 10:52:39 +0000

    Update jattack

--- TRIBjattack/build.sh
+++ TRIBjattack/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the jattack build creates /tmp/TRIBjattack.pkg, which is then converted
 #
 ${THOME}/build/create_zap_from_pkg TRIBjattack /tmp/TRIBjattack.pkg
