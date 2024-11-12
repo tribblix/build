346a0eb865ac3cc978d5d3d010f1b185ae9500a4
Peter Tribble <peter.tribble@gmail.com>
2024-11-12 08:46:25 +0000

    Update libgpg-error 1.51

--- TRIBlibgpg-error/build.sh
+++ TRIBlibgpg-error/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64 libgpg-error-1.50
-${THOME}/build/genpkg TRIBlibgpg-error libgpg-error-1.50
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64 libgpg-error-1.51
+${THOME}/build/genpkg TRIBlibgpg-error libgpg-error-1.51
