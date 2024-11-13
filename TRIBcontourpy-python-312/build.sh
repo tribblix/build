38423b18443152e0c9fbb8bff1c8deb1a44d0a7f
Peter Tribble <peter.tribble@gmail.com>
2024-11-13 08:39:26 +0000

    Update contourpy 1.3.1

--- TRIBcontourpy-python-312/build.sh
+++ TRIBcontourpy-python-312/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env CXX="g++ -m64" CC="gcc -m64" ${THOME}/build/pkg_pep518 TRIBcontourpy-python-312 contourpy-1.3.0
+# without build isolation fails; the build requirement on pybind11 is
+# incompatible with that for scipy
+#
+env CXX="g++ -m64" CC="gcc -m64" ${THOME}/build/pkg_pep518 TRIBcontourpy-python-312 contourpy-1.3.1
