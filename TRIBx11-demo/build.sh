fc475408628ac00a233f1040e96d046e5f78302c
Peter Tribble <peter.tribble@gmail.com>
2024-11-14 08:19:22 +0000

    Update xlogo 1.0.7

--- TRIBx11-demo/build.sh
+++ TRIBx11-demo/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/dobuild -64only ico-1.0.6
 ${THOME}/build/dobuild -64only xeyes-1.3.0
 ${THOME}/build/dobuild -64only xgc-1.0.6
-${THOME}/build/dobuild -64only xlogo-1.0.6
-${THOME}/build/genpkg TRIBx11-demo ico-1.0.6 xeyes-1.3.0 xgc-1.0.6 xlogo-1.0.6
+${THOME}/build/dobuild -64only xlogo-1.0.7
+${THOME}/build/genpkg TRIBx11-demo ico-1.0.6 xeyes-1.3.0 xgc-1.0.6 xlogo-1.0.7
