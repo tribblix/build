22917a6d289f44733cbcb51c13f718e1e1d2e828
Peter Tribble <peter.tribble@gmail.com>
2024-11-16 19:15:45 +0000

    Fix broken libcurl.pc files

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# there's a fixit for 8.11.0 which ships utterly broken pc files
+#
 ${THOME}/build/dobuild -64 curl-8.11.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli"
 ${THOME}/build/genpkg TRIBcurl curl-8.11.0
