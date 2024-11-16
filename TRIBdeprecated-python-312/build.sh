b3cd320481e3184a797d56204c636a6df19e10c4
Peter Tribble <peter.tribble@gmail.com>
2024-11-16 18:21:07 +0000

    Update Deprecated 1.2.15

--- TRIBdeprecated-python-312/build.sh
+++ TRIBdeprecated-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBdeprecated-python-312 Deprecated-1.2.14
+${THOME}/build/pkg_pep518 TRIBdeprecated-python-312 deprecated-1.2.15
