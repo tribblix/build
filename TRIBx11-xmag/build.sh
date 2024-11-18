c2033e21d9d18a852aa4303c2ef01e8c14c9a68a
Peter Tribble <peter.tribble@gmail.com>
2024-11-18 08:47:15 +0000

    Update xmag 1.0.8

--- TRIBx11-xmag/build.sh
+++ TRIBx11-xmag/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xmag-1.0.7
-${THOME}/build/genpkg TRIBx11-xmag xmag-1.0.7
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only xmag-1.0.8
+${THOME}/build/genpkg TRIBx11-xmag xmag-1.0.8
