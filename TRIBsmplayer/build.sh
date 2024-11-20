a0e1c45e353f05177b745170b87cfb1b3424d44d
Peter Tribble <peter.tribble@gmail.com>
2024-11-20 20:28:20 +0000

    Fix smplayer build so simple_web_server is 64-bit

--- TRIBsmplayer/build.sh
+++ TRIBsmplayer/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a front-end to MPlayer, hence the dependency on TRIBmplayer
 # 23.12.0 builds but hangs on startup
 #
@@ -19,7 +21,7 @@ sed -i 's: make: gmake:' Makefile
 sed -i 's:install :ginstall :' Makefile
 sed -i 's:install :ginstall :' install_appdata.sh
 sed -i 's:tar :gtar :g' Makefile
-sed -i 's:-pthread:-lnsl -lsocket:' webserver/Makefile
+sed -i 's:-pthread:-lnsl -lsocket -m64:' webserver/Makefile
 sed -i 's:Another OS:Tribblix:' src/smplayer.cpp
 env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH gmake -j 6 PREFIX=/usr CC=gcc
 cd ..
