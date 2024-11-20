94f96aa6df84bcc9f566ec72a4ffd608786222ef
Peter Tribble <peter.tribble@gmail.com>
2024-11-20 20:12:18 +0000

    Fix qvwm build so qv_deanimate is 64-bit

--- TRIBqvwm/build.sh
+++ TRIBqvwm/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-env TRIBBLIX_CXXFLAGS=-Wno-narrowing TRIBBLIX_LDFLAGS=-lXpm ${THOME}/build/dobuild -64only qvwm-1.1.12-10-IKu
+# SPDX-License-Identifier: CDDL-1.0
+#
+# need to set CC as qv_deanimate doesn't pick the flags up properly
+#
+env CC="gcc -m64" TRIBBLIX_CXXFLAGS=-Wno-narrowing TRIBBLIX_LDFLAGS=-lXpm ${THOME}/build/dobuild -64only qvwm-1.1.12-10-IKu
 ${THOME}/build/genpkg TRIBqvwm qvwm-1.1.12-10-IKu
