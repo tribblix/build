eae6a004bfa79155f346f965361f6d99778e6865
Peter Tribble <peter.tribble@gmail.com>
2024-11-20 18:15:41 +0000

    Rebuild ninja 64-bit

--- TRIBninja/build.sh
+++ TRIBninja/build.sh
@@ -7,7 +7,7 @@
 git clone https://github.com/ninja-build/ninja.git
 cd ninja
 git checkout release
-python3.12 ./configure.py --bootstrap
+env CFLAGS=-m64 python3.12 ./configure.py --bootstrap
 rm -fr /tmp/n1
 mkdir -p /tmp/n1/usr/bin
 cp ninja /tmp/n1/usr/bin
