b655d9545a75e090252a92d1c7e075ae277c7c09
Peter Tribble <peter.tribble@gmail.com>
2024-11-20 12:45:59 +0000

    Update xmlto 0.0.29

--- TRIBxmlto/build.sh
+++ TRIBxmlto/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs gnu-getopt
 #
-${THOME}/build/dobuild xmlto-0.0.28 -C GETOPT=/usr/gnu/bin/getopt
-${THOME}/build/genpkg TRIBxmlto xmlto-0.0.28
+${THOME}/build/dobuild -A -64only xmlto-0.0.29 -C GETOPT=/usr/gnu/bin/getopt
+${THOME}/build/genpkg TRIBxmlto xmlto-0.0.29
