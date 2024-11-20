8b853c55b8fbe24685ddbe59f140111fac3aaf7b
Peter Tribble <peter.tribble@gmail.com>
2024-11-20 17:45:43 +0000

    Update vale 3.9.1

--- TRIBvale/build.sh
+++ TRIBvale/build.sh
@@ -2,12 +2,12 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/unpack vale-3.7.1
-cd vale-3.7.1
+$THOME/build/unpack vale-3.9.1
+cd vale-3.9.1
 
 # like the Makefile, but that will only autodetect the version
 # from a git checkout, so we specify explicitly
-/usr/versions/go-1.22/bin/go build -ldflags "-s -w -X main.version=v3.7.1" -o bin/ ./cmd/vale
+/usr/versions/go-1.23/bin/go build -ldflags "-s -w -X main.version=v3.9.1" -o bin/ ./cmd/vale
 
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
