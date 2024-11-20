9acaee93feced630d66a186e0f595ddd867720f5
Peter Tribble <peter.tribble@gmail.com>
2024-11-20 21:48:28 +0000

    Update desktop-file-utils 0.28

--- TRIBdesktop-file-utils/build.sh
+++ TRIBdesktop-file-utils/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu desktop-file-utils-0.23
-${THOME}/build/genpkg TRIBdesktop-file-utils desktop-file-utils-0.23
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/mesonbuild -64only desktop-file-utils-0.28
+${THOME}/build/genpkg TRIBdesktop-file-utils desktop-file-utils-0.28
