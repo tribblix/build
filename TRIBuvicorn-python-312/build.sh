79ce37e161b264d7878b20139f5b9c76470d07ab
Peter Tribble <peter.tribble@gmail.com>
2024-11-21 11:55:46 +0000

    Update uvicorn 0.32.1

--- TRIBuvicorn-python-312/build.sh
+++ TRIBuvicorn-python-312/build.sh
@@ -5,4 +5,4 @@
 # dependencies are not only the basic but also all of [standard]
 # except on sparc, which misses watchfiles which depends on rust
 #
-${THOME}/build/pkg_pep518 TRIBuvicorn-python-312 uvicorn-0.32.0
+${THOME}/build/pkg_pep518 TRIBuvicorn-python-312 uvicorn-0.32.1
