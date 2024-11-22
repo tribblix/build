9bb9c2478ff6aa1c63ffd6963a054cc62a59546f
Peter Tribble <peter.tribble@gmail.com>
2024-11-22 17:46:15 +0000

    Rebuild kbd_mode 64-bit

--- TRIBsys-keyboard-kbd-mode/build.sh
+++ TRIBsys-keyboard-kbd-mode/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # built by hand
 #
 rm -fr kbd_mode
 cp -r ${THOME}/build/patches/kbd_mode .
 cd kbd_mode
-make CC=gcc
+make CC="gcc -m64"
 mkdir -p /tmp/km/usr/share/man/man1
 cp kbd_mode.1 /tmp/km/usr/share/man/man1
 mkdir -p /tmp/km/usr/bin
