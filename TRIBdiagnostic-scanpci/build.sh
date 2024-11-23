7b3c257f55f03214bef33bed1dc0d361c57c46af
Peter Tribble <peter.tribble@gmail.com>
2024-11-23 16:03:59 +0000

    Update libpciaccess 0.18.1

--- TRIBdiagnostic-scanpci/build.sh
+++ TRIBdiagnostic-scanpci/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # scanpci installed by fixinstall
 #
-${THOME}/build/dobuild -64 libpciaccess-0.17
-${THOME}/build/genpkg TRIBdiagnostic-scanpci libpciaccess-0.17
+${THOME}/build/dobuild -64 libpciaccess-0.18.1
+${THOME}/build/genpkg TRIBdiagnostic-scanpci libpciaccess-0.18.1
