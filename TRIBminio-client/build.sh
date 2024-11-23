31d7df9851fbb67fe1e956179357d067bbcd6845
Peter Tribble <peter.tribble@gmail.com>
2024-11-23 21:37:55 +0000

    Update minio-client 2024.11.17

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -2,17 +2,15 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# need to pin to a release before containerd/console was wrecked
-#
 mkdir ~/ud/c
 cd ~/ud/c
-env GOPATH=`pwd` PATH=/usr/versions/go-1.22/bin:$PATH go install github.com/minio/mc@RELEASE.2023-07-21T20-44-27Z
+env GOPATH=`pwd` PATH=/usr/versions/go-1.23/bin:$PATH go install github.com/minio/mc@RELEASE.2024-11-17T19-35-25Z
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
 mkdir -p /tmp/ee/usr/share/minio-client
 cp bin/mc /tmp/ee/usr/bin
-cp pkg/mod/github.com/minio/mc@*/README.md /tmp/ee/usr/share/minio-client
-cp pkg/mod/github.com/minio/mc@*/LICENSE /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20241117*/README.md /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20241117*/LICENSE /tmp/ee/usr/share/minio-client
 ${THOME}/build/create_pkg TRIBminio-client /tmp/ee
 rm -fr /tmp/ee
