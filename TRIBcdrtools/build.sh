7fdb4297ed4d9d8172b77ea79d2f574ccd5cd8a1
Peter Tribble <peter.tribble@gmail.com>
2024-11-26 18:12:26 +0000

    Rebuild cdrtools 64-bit

--- TRIBcdrtools/build.sh
+++ TRIBcdrtools/build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 ${THOME}/build/unpack cdrtools-3.01
 cd cdrtools-3.01
-env INS_BASE=/usr make -e
+env INS_BASE=/usr make -e CCOM=gcc64
 rm -fr /tmp/cdr
-env INS_BASE=/tmp/cdr/usr INS_RBASE=/tmp/cdr make -e install
+env INS_BASE=/tmp/cdr/usr INS_RBASE=/tmp/cdr make -e install CCOM=gcc64
+
+#
+# tidy up
+#
 rm -fr /tmp/cdr/etc /tmp/cdr/usr/sbin /tmp/cdr/usr/include
 rm -fr /tmp/cdr/usr/share/doc /tmp/cdr/usr/share/man/man4
 rm -fr /tmp/cdr/usr/lib
@@ -14,4 +20,7 @@ rm -f /tmp/cdr/usr/bin/iso* /tmp/cdr/usr/share/man/man8/iso*
 rm -f /tmp/cdr/usr/bin/btc* /tmp/cdr/usr/share/man/man1/btc*
 rm -f /tmp/cdr/usr/bin/dev* /tmp/cdr/usr/share/man/man8/dev*
 rm -fr /tmp/cdr/usr/share/man/man3
+# this needs lame, which we don't have
+rm -f /tmp/cdr/usr/bin/cdda2mp3 /tmp/cdr/usr/share/man/man1/cdda2mp3.1
+
 ${THOME}/build/create_pkg TRIBcdrtools /tmp/cdr
