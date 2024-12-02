a1803d780b86e1865d438432dca8954d325a53f3
Peter Tribble <peter.tribble@gmail.com>
2024-12-02 20:39:05 +0000

    Deprecate old png tiff compat packages

--- TRIBlibpng-compat/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Add new version I built, ship straight copies of older versions
-#
-rm -fr /tmp/lp
-mkdir /tmp/lp
-${THOME}/build/create_pkg TRIBlibpng-compat /tmp/lp
-rm -fr /tmp/lp
--- TRIBlibtiff-compat/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# this provides the legacy 3.x files for binary compatibility
-#
-rm -fr /tmp/lp
-mkdir /tmp/lp
-${THOME}/build/create_pkg TRIBlibtiff-compat /tmp/lp
-rm -fr /tmp/lp
