bf9fc8e990957cdd28b54665c417082459da2e5f
Peter Tribble <peter.tribble@gmail.com>
2024-12-05 08:16:06 +0000

    Update six 1.17.0

--- TRIBsix-python-311/build.sh
+++ TRIBsix-python-311/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBsix-python-311 six-1.16.0
+${THOME}/build/pkg_setup_py TRIBsix-python-311 six-1.17.0
--- TRIBsix-python-312/build.sh
+++ TRIBsix-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBsix-python-312 six-1.16.0
+${THOME}/build/pkg_setup_py TRIBsix-python-312 six-1.17.0
