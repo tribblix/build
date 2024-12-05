1f1090b8eeb355ce71e721b4710bb73047fec6b2
Peter Tribble <peter.tribble@gmail.com>
2024-12-05 09:08:34 +0000

    Update oclock 1.0.6

--- TRIBx11-oclock/build.sh
+++ TRIBx11-oclock/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only oclock-1.0.5
-${THOME}/build/genpkg TRIBx11-oclock oclock-1.0.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only oclock-1.0.6
+${THOME}/build/genpkg TRIBx11-oclock oclock-1.0.6
