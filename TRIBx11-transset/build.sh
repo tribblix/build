d672b229d165e6e86a6cdaa25c24b913110fdca3
Peter Tribble <peter.tribble@gmail.com>
2024-12-05 09:06:12 +0000

    Update transset 1.0.4

--- TRIBx11-transset/build.sh
+++ TRIBx11-transset/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only transset-1.0.3
-${THOME}/build/genpkg TRIBx11-transset transset-1.0.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only transset-1.0.4
+${THOME}/build/genpkg TRIBx11-transset transset-1.0.4
