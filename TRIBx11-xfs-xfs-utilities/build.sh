ac02bd2fb86360cbdd95d712234c51e8fdc8b34a
Peter Tribble <peter.tribble@gmail.com>
2024-12-05 09:11:36 +0000

    Update fslsfonts 1.0.7

--- TRIBx11-xfs-xfs-utilities/build.sh
+++ TRIBx11-xfs-xfs-utilities/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a merge of several smaller packages, that was the way
 # it was inherited.
 #
-${THOME}/build/dobuild -64only fslsfonts-1.0.6
+${THOME}/build/dobuild -64only fslsfonts-1.0.7
 ${THOME}/build/dobuild -64only fstobdf-1.0.7
 ${THOME}/build/dobuild -64only showfont-1.0.6
 ${THOME}/build/dobuild -64only xfsinfo-1.0.7
-${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.6 fstobdf-1.0.7 showfont-1.0.6 xfsinfo-1.0.7
+${THOME}/build/genpkg TRIBx11-xfs-xfs-utilities fslsfonts-1.0.7 fstobdf-1.0.7 showfont-1.0.6 xfsinfo-1.0.7
