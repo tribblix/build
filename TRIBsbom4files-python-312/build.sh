79c9132d58432540faf6ad9259e84d470f9164d7
Peter Tribble <peter.tribble@gmail.com>
2024-12-10 13:37:23 +0000

    Update sbom4python 0.12.0 sbom4files 0.4.5 lib4sbom 0.8.0 lib4package 0.3.0

--- TRIBsbom4files-python-312/build.sh
+++ TRIBsbom4files-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_wheel TRIBsbom4files-python-312 sbom4files-0.4.4-py2.py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBsbom4files-python-312 sbom4files-0.4.5-py2.py3-none-any.whl
