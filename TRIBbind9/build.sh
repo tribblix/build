bccb0a85ccb48b064f10792fab60ca66d3fe93e5
Peter Tribble <peter.tribble@gmail.com>
2024-12-11 18:55:33 +0000

    Update bind 9.18.32

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -7,5 +7,5 @@
 # the preconf file builds a private static libuv, although as of 9.18.19
 # the libuv must be built in a separate tree
 #
-env LIBUV_CFLAGS=-I/tmp/libuv-v1.48.0-64bit/include LIBUV_LIBS="/tmp/libuv-v1.48.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_CFLAGS=-D_XPG4_2 TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64only bind-9.18.31 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBbind9 bind-9.18.31
+env LIBUV_CFLAGS=-I/tmp/libuv-v1.49.2-64bit/include LIBUV_LIBS="/tmp/libuv-v1.49.2-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_CFLAGS=-D_XPG4_2 TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64only bind-9.18.32 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBbind9 bind-9.18.32
