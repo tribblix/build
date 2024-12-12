c81bb5945cb4185e478199672c3297c8d7a642df
Peter Tribble <peter.tribble@gmail.com>
2024-12-12 19:29:01 +0000

    Converting jattack package to zap should be done in the build repo

--- TRIBjattack/build.sh
+++ TRIBjattack/build.sh
@@ -2,6 +2,14 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# the jattack build creates /tmp/TRIBjattack.pkg, which is then converted
+# in jattack
 #
-${THOME}/build/create_zap_from_pkg TRIBjattack /tmp/TRIBjattack.pkg
+./jattack package
+#
+#  *** make sure the pkg version matches that here ***
+#
+# then copy the package into our own format
+#
+mkdir -p /tmp/pct
+mv /tmp/TRIBjattack.pkg /tmp/pct/TRIBjattack.1.6.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBjattack.1.6.0.pkg /tmp/pct
