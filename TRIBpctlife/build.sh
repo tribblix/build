843c1bf86742ee184413ab69962da55eae0942ec
Peter Tribble <peter.tribble@gmail.com>
2024-12-12 19:10:44 +0000

    Converting pctlife package to zap should be done in the build repo

--- TRIBpctlife/build.sh
+++ TRIBpctlife/build.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-echo "In a pctlife checkout, run './life package'"
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# in a pctlife checkout
+#
+./life package
+#
+#  *** make sure the pkg version matches that here ***
+#
+# then copy the package into our own format
+#
+mkdir -p /tmp/pct
+mv /tmp/TRIBpctlife.pkg /tmp/pct/TRIBpctlife.1.7.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBpctlife.1.7.0.pkg /tmp/pct
