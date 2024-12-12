2cf1b83ba863e4b1243795634ab1d985778987ae
Peter Tribble <peter.tribble@gmail.com>
2024-12-12 19:17:06 +0000

    Converting jangle package to zap should be done in the build repo

--- TRIBjangle/build.sh
+++ TRIBjangle/build.sh
@@ -3,5 +3,13 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # In jangle itself:
-echo "Checkout jangle and"
-echo ./build packages deps
+#
+./build packages deps
+#
+#  *** make sure the pkg version matches that here ***
+#
+# then copy the package into our own format
+#
+mkdir -p /tmp/pct
+mv /tmp/TRIBjangle.pkg /tmp/pct/TRIBjangle.0.17.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBjangle.0.17.pkg /tmp/pct
