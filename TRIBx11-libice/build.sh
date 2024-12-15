45dee505be1e1aa2abe8a290d85f3ee5b62b9fe0
Peter Tribble <peter.tribble@gmail.com>
2024-12-15 16:28:17 +0000

    Update libICE 1.1.2

--- TRIBx11-libice/build.sh
+++ TRIBx11-libice/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libICE-1.0.10 -C "--enable-unix-transport --enable-tcp-transport --disable-local-transport --enable-ipv6"
-${THOME}/build/genpkg TRIBx11-libice libICE-1.0.10
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 libICE-1.1.2 -C "--enable-unix-transport --enable-tcp-transport --disable-local-transport --enable-ipv6"
+${THOME}/build/genpkg TRIBx11-libice libICE-1.1.2
