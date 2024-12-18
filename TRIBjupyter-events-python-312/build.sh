8ea1d5c483dce926d6a531600db7e8e7b412e415
Peter Tribble <peter.tribble@gmail.com>
2024-12-18 08:46:41 +0000

    Update jupyter-events 0.11.0

--- TRIBjupyter-events-python-312/build.sh
+++ TRIBjupyter-events-python-312/build.sh
@@ -3,4 +3,4 @@
 # this depends on jsonschema[format-nongpl], see the file
 # TRIBjsonschema4-python-312/depend.format-nongpl
 #
-${THOME}/build/pkg_pep518 TRIBjupyter-events-python-312 jupyter_events-0.10.0
+${THOME}/build/pkg_pep518 -N TRIBjupyter-events-python-312 jupyter_events-0.11.0
