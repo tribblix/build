52bfbea3516c3457100a8146a583be4ca5a57a80
Peter Tribble <peter.tribble@gmail.com>
2024-12-18 19:41:49 +0000

    Update citus 12.1.5

--- TRIBblix-citus15/build.sh
+++ TRIBblix-citus15/build.sh
@@ -6,5 +6,5 @@
 #
 # there is a citus-stat patch
 #
-${THOME}/build/dobuild +64only citus-12.1.4 -C PG_CONFIG=/opt/tribblix/postgres15/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus15 citus-12.1.4
+${THOME}/build/dobuild +64only citus-12.1.5 -C PG_CONFIG=/opt/tribblix/postgres15/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus15 citus-12.1.5
