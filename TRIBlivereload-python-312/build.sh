81ca4d57146f478eb69d250fadde9f4ba682396b
Peter Tribble <peter.tribble@gmail.com>
2024-12-18 20:02:34 +0000

    Update livereload 2.7.1

--- TRIBlivereload-python-312/build.sh
+++ TRIBlivereload-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBlivereload-python-312 livereload-2.7.0
+${THOME}/build/pkg_setup_py TRIBlivereload-python-312 livereload-2.7.1
