90d05137a20595d09a80a8448b93289b4e29f18d
Peter Tribble <peter.tribble@gmail.com>
2024-12-20 19:50:16 +0000

    Package httpclient5

---
+++ TRIBapache-httpclient5/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+rm -fr /tmp/hc5
+mkdir -p /tmp/hc5
+cd /tmp/hc5
+$THOME/build/unpack httpcomponents-client-5.4.1-bin
+# just shuffle the files we need to the right place
+mkdir -p usr/share/httpcomponents-client-5
+mv lib/httpcore5-5.3.1.jar usr/share/httpcomponents-client-5
+mv lib/httpcore5-h2-5.3.1.jar usr/share/httpcomponents-client-5
+mv lib/httpclient5-5.4.1.jar usr/share/httpcomponents-client-5
+rm -fr javadoc lib
+mv *.txt usr/share/httpcomponents-client-5
+cd
+$THOME/build/create_pkg TRIBapache-httpclient5 /tmp/hc5
