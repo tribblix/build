6f69e6c2fb5ffdb9694b185b44426545aa666495
Peter Tribble <peter.tribble@gmail.com>
2024-12-23 13:47:44 +0000

    Drop unused spiritvnc

--- TRIBspiritvnc/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/dobuild -64only spiritvnc-fltk-no-libssh2-0.6.4
-${THOME}/build/genpkg TRIBspiritvnc spiritvnc-fltk-no-libssh2-0.6.4
