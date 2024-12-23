c808142981dbc527d61f1f70db49081325bf6131
Peter Tribble <peter.tribble@gmail.com>
2024-12-23 15:57:04 +0000

    Update LuaJIT 2.1

--- TRIBluajit/build.sh
+++ TRIBluajit/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild LuaJIT-2.0.3
-${THOME}/build/genpkg TRIBluajit LuaJIT-2.0.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+# documentation says to always build from current git
+#
+${THOME}/build/dobuild -64only https://github.com/LuaJIT/LuaJIT
+${THOME}/build/genpkg TRIBluajit LuaJIT
