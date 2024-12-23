7e680394f2e4d93e1415ea918b37434450a2981e
Peter Tribble <peter.tribble@gmail.com>
2024-12-23 13:45:39 +0000

    Drop unused pidgin-sipe

--- TRIBpidgin-sipe/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# need --disable-quality-check as otherwise it's confused by type conversions
-#
-env LDFLAGS=-R/usr/lib/mps/`${THOME}/build/getarch` LIBS=-lsocket ${THOME}/build/dobuild -64only -gnu pidgin-sipe-1.25.0 -C "--with-krb5=no --disable-quality-check"
-${THOME}/build/genpkg TRIBpidgin-sipe pidgin-sipe-1.25.0
