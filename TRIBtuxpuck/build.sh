953889f9d3d8e554fd9819de282bf424ba236a52
Peter Tribble <peter.tribble@gmail.com>
2024-12-23 13:46:41 +0000

    Drop unused tuxpuck

--- TRIBtuxpuck/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# If you download the source from launchpad you'll need to rename it
-#  mv tuxpuck_0.8.2.orig.tar.gz tuxpuck-0.8.2.tar.gz
-#
-env CFLAGS="-m64" ${THOME}/build/dobuild -64only tuxpuck-0.8.2 -J 1
-${THOME}/build/genpkg TRIBtuxpuck tuxpuck-0.8.2
