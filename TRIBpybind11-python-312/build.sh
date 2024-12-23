e8f54fc0c4b2d0deb84ea4bc76df1eea5241da72
Peter Tribble <peter.tribble@gmail.com>
2024-12-23 10:55:00 +0000

    Update matplotlib 3.10.0

--- TRIBpybind11-python-312/build.sh
+++ TRIBpybind11-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/pkg_setup_py TRIBpybind11-python-312 pybind11-2.12.0 pybind11_global-2.12.0
+$THOME/build/pkg_setup_py TRIBpybind11-python-312 pybind11-2.13.6 pybind11_global-2.13.6
