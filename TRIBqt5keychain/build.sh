a375433e45833c159ff3a5a6e7c77dc98fb10592
Peter Tribble <peter.tribble@gmail.com>
2024-12-23 14:47:21 +0000

    Update qt5keychain 0.14.3

--- TRIBqt5keychain/build.sh
+++ TRIBqt5keychain/build.sh
@@ -4,5 +4,5 @@
 #
 # needed for the nextcloud client
 #
-env PATH=/usr/versions/Qt-5/bin/amd64:$PATH $THOME/build/cmbuild -64only qtkeychain-0.13.2 -P /usr/versions/Qt-5 -C -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/amd64/cmake
-${THOME}/build/genpkg TRIBqt5keychain qtkeychain-0.13.2
+env PATH=/usr/versions/Qt-5/bin/amd64:$PATH $THOME/build/cmbuild -64only qtkeychain-0.14.3 -P /usr/versions/Qt-5 -C -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/amd64/cmake
+${THOME}/build/genpkg TRIBqt5keychain qtkeychain-0.14.3
