20c7cf37e9229c27bcac9861be2f744764aec52e
Peter Tribble <peter.tribble@gmail.com>
2025-01-07 09:35:11 +0000

    Update snac 2.68

--- TRIBblix-snac/build.sh
+++ TRIBblix-snac/build.sh
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env CC=gcc CFLAGS="-O -m64" LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64only -n snac2 snac2-2.66
-sed -i -e 's:=/usr/local:=/opt/tribblix/snac:' -e 's: $(PREFIX: $(DESTDIR)$(PREFIX:' -e 's:install -:ginstall -:' snac2-2.66-64bit/Makefile
-${THOME}/build/genpkg TRIBblix-snac snac2-2.66
+env CC=gcc CFLAGS="-O -m64" LDFLAGS="-lsocket -lnsl" ${THOME}/build/dobuild -64only -n snac2 snac2-2.68
+sed -i -e 's:=/usr/local:=/opt/tribblix/snac:' -e 's: $(PREFIX: $(DESTDIR)$(PREFIX:' -e 's:install -:ginstall -:' snac2-2.68-64bit/Makefile
+${THOME}/build/genpkg TRIBblix-snac snac2-2.68
