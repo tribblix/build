a6fb9a78ab68f90b827290f5abfb6f7689c6bb2f
Peter Tribble <peter.tribble@gmail.com>
2025-01-07 14:02:23 +0000

    Update xinit 1.4.3

--- TRIBx11-session-xinit/build.sh
+++ TRIBx11-session-xinit/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xinit-1.4.2 -C "--sysconfdir=/etc --with-xinitdir=/etc/X11/xinit"
-${THOME}/build/genpkg TRIBx11-session-xinit xinit-1.4.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only xinit-1.4.3 -C "--sysconfdir=/etc --with-xinitdir=/etc/X11/xinit"
+${THOME}/build/genpkg TRIBx11-session-xinit xinit-1.4.3
