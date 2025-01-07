9a06d3f0c6fc27a488968cf77f75517202a5afb6
Peter Tribble <peter.tribble@gmail.com>
2025-01-07 08:49:42 +0000

    Update jedi-language-server 0.43.1

--- TRIBjedi-lang-serv-python-312/build.sh
+++ TRIBjedi-lang-serv-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBjedi-lang-serv-python-312 jedi_language_server-0.42.0
+${THOME}/build/pkg_pep518 TRIBjedi-lang-serv-python-312 jedi_language_server-0.43.1
