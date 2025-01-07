34d548fbb3da39ff9e57186c55d8c10fd73c8687
Peter Tribble <peter.tribble@gmail.com>
2025-01-07 18:07:33 +0000

    Rebuild Mosaic 64-bit

--- TRIBmosaic/build.sh
+++ TRIBmosaic/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the download can't be grabbed by wget; use a browser
 #
 # there were missing files in ck11, restored in ck13
@@ -7,11 +9,12 @@
 ${THOME}/build/unpack mosaic27ck13
 cd mosaic-ck
 
+# Created makefiles/Makefile.illumos and edited Makefile
 cp ${THOME}/build/patches/mosaic.Makefile Makefile
 cp ${THOME}/build/patches/mosaic.Makefile.illumos makefiles/Makefile.illumos
-# Created makefiles/Makefile.illumos and edited Makefile
 
-gmake illumos
+# slightly abusing prereleaseflags to avoid trampling over cflags
+gmake illumos prereleaseflags=-m64 ldflags=-m64
 rm -fr /tmp/mmm
 mkdir -p /tmp/mmm/usr/bin
 cp src/Mosaic /tmp/mmm/usr/bin
