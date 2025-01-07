d3efd16a64998969d2c13cd1eec805d7472c2857
Peter Tribble <peter.tribble@gmail.com>
2025-01-07 21:38:18 +0000

    Update tkdiff 5.7

--- TRIBtkdiff/build.sh
+++ TRIBtkdiff/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #
-${THOME}/build/unpack tkdiff-5-6
-cd tkdiff-5-6
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack tkdiff-5-7
+cd tkdiff-5-7
+rm -fr /tmp/tk1
 mkdir -p /tmp/tk1/usr/bin
 mkdir -p /tmp/tk1/usr/share/tkdiff
 cp tkdiff /tmp/tk1/usr/bin
 cp *.txt /tmp/tk1/usr/share/tkdiff
 chmod 644 /tmp/tk1/usr/share/tkdiff/*
+cd ..
 ${THOME}/build/create_pkg TRIBtkdiff /tmp/tk1
+rm -fr /tmp/tk1
