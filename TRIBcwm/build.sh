d60cfd4d380bf7dbfbd06912c104f0f8abbb70ff
Peter Tribble <peter.tribble@gmail.com>
2025-01-08 19:15:46 +0000

    Rebuild cwm 64-bit

--- TRIBcwm/build.sh
+++ TRIBcwm/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# there's a patch to eliminate non-portable code
+# there's a patch to eliminate non-portable code and enable 64-bit
 #
 env CC=gcc ${THOME}/build/dobuild -64only https://github.com/leahneukirchen/cwm
 env PATH=/usr/gnu/bin:$PATH PREFIX=/usr ${THOME}/build/genpkg TRIBcwm cwm
