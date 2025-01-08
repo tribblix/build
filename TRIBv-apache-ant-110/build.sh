5638f222dcb3533f33b4dc0f7b7cc9b77e5afb8e
Peter Tribble <peter.tribble@gmail.com>
2025-01-08 09:46:53 +0000

    Update ivy (in ant)

--- TRIBv-apache-ant-110/build.sh
+++ TRIBv-apache-ant-110/build.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # also needs the following in tarballs for fixit to deploy:
-# apache-ivy-2.5.2-bin-with-deps.zip
+# apache-ivy-2.5.3-bin-with-deps.zip
 # svnant-1.3.1.zip
 #
 ${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.15-bin
