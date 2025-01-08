be6f9a75258a45806a12f81e4b22944e786101db
Peter Tribble <peter.tribble@gmail.com>
2025-01-08 10:25:30 +0000

    Remove pglast, broken

--- TRIBpglast-python-312/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# there's a patch to fix up the bundled pg_config.h which is unfortunately
-# static rather than dynamically generated
-#
-env CC=gcc PATH=/usr/gnu/bin:$PATH ${THOME}/build/pkg_setup_py TRIBpglast-python-312 pglast-7.2
