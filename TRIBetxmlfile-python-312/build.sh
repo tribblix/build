98b816b24eb4bf386c0f96d14f9a37494da9a36f
Peter Tribble <peter.tribble@gmail.com>
2025-01-09 13:23:56 +0000

    Update et_xmlfile 2.0.0

--- TRIBetxmlfile-python-312/build.sh
+++ TRIBetxmlfile-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBetxmlfile-python-312 et_xmlfile-1.1.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBetxmlfile-python-312 et_xmlfile-2.0.0
