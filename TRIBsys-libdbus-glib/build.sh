a6706e6f70b49bfc81eb02f6c24f1401cd1295b0
Peter Tribble <peter.tribble@gmail.com>
2025-01-10 16:45:59 +0000

    Update dbus-glib 0.112

--- TRIBsys-libdbus-glib/build.sh
+++ TRIBsys-libdbus-glib/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 -gnu dbus-glib-0.110
-${THOME}/build/genpkg TRIBsys-libdbus-glib dbus-glib-0.110
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 -gnu dbus-glib-0.112
+${THOME}/build/genpkg TRIBsys-libdbus-glib dbus-glib-0.112
