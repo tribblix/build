78ff7250e2ae4cc1bad247e44d7c01b53b604f2c
Peter Tribble <peter.tribble@gmail.com>
2025-01-10 19:11:38 +0000

    Update dbus 1.12.28

--- TRIBsys-lib-dbus/build.sh
+++ TRIBsys-lib-dbus/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is a combined build that produces 3 packages
 # we follow the OI build, patches, and packaging
 #
 # manpages require xmlto to be installed
 # build requires autoconf-archive to be installed
 #
-${THOME}/build/dobuild -64 dbus-1.12.24 -C "--sysconfdir=/etc --localstatedir=/var --with-dbus-user=root --disable-static --libexecdir=/usr/lib --with-dbus-daemondir=/usr/lib --disable-dependency-tracking"
+${THOME}/build/dobuild -64 dbus-1.12.28 -C "--sysconfdir=/etc --localstatedir=/var --with-dbus-user=root --disable-static --libexecdir=/usr/lib --with-dbus-daemondir=/usr/lib --disable-dependency-tracking"
 
 #
 # now build packages
 #
-${THOME}/build/genpkg TRIBsys-lib-dbus dbus-1.12.24
-${THOME}/build/genpkg TRIBsys-libdbus dbus-1.12.24
-${THOME}/build/genpkg TRIBsys-lib-dbus-dbus-x11 dbus-1.12.24
+${THOME}/build/genpkg TRIBsys-lib-dbus dbus-1.12.28
+${THOME}/build/genpkg TRIBsys-libdbus dbus-1.12.28
+${THOME}/build/genpkg TRIBsys-lib-dbus-dbus-x11 dbus-1.12.28
