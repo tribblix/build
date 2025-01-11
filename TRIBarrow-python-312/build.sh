5e8d44ee47ebc687fe61f400a0d453a44246a95b
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 10:49:32 +0000

    Update python arrow 1.3.0

--- TRIBarrow-python-312/build.sh
+++ TRIBarrow-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBarrow-python-312 arrow-1.2.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBarrow-python-312 arrow-1.3.0
---
+++ TRIBtypes-dateutil-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBtypes-dateutil-python-312 types_python_dateutil-2.9.0.20241206
