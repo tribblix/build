85b0df4055be23e178c7a6c203c0e30ca280b4f2
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 21:12:47 +0000

    Update Try::Tiny 0.32

--- TRIBlib-perl-5-try-tiny/build.sh
+++ TRIBlib-perl-5-try-tiny/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Try-Tiny-0.31
-cd Try-Tiny-0.31
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Try-Tiny-0.32
+cd Try-Tiny-0.32
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-try-tiny Try-Tiny-0.31
+${THOME}/build/genpkg TRIBlib-perl-5-try-tiny Try-Tiny-0.32
