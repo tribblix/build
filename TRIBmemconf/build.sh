deddd6fac8b2d9d627e3d40dcd3097c9157dfbd5
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 14:23:20 +0000

    Update memconf 3.17.1

--- TRIBmemconf/build.sh
+++ TRIBmemconf/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-#
+# SPDX-License-Identifier: CDDL-1.0
 #
 rm -fr /tmp/pp
 mkdir -p /tmp/pp/usr/bin
-cp ${THOME}/tarballs/memconf.v3.16.pl.gz /tmp/pp/usr/bin/memconf.gz
+cp ${THOME}/tarballs/memconf.v3.17.1.pl.gz /tmp/pp/usr/bin/memconf.gz
 gunzip /tmp/pp/usr/bin/memconf
 chmod 755 /tmp/pp/usr/bin/memconf
 ${THOME}/build/create_pkg TRIBmemconf /tmp/pp
