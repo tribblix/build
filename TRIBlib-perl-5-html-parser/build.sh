6505f5414ba8d3b527026808dec1c2511ca90c67
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 21:30:29 +0000

    Update HTML::Parser 3.83

--- TRIBlib-perl-5-html-parser/build.sh
+++ TRIBlib-perl-5-html-parser/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # HTML-Parser needs html-tagset
 #
-${THOME}/build/unpack HTML-Parser-3.77
-cd HTML-Parser-3.77
+${THOME}/build/unpack HTML-Parser-3.83
+cd HTML-Parser-3.83
 perl Makefile.PL
 make
-${THOME}/build/genpkg TRIBlib-perl-5-html-parser
+cd ..
+${THOME}/build/genpkg TRIBlib-perl-5-html-parser HTML-Parser-3.83
