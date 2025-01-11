ff37f16c43348ef045886a0abb507d4a03b7e27e
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 21:33:20 +0000

    Update HTML::Tagset 3.24

--- TRIBlib-perl-5-html-tagset/build.sh
+++ TRIBlib-perl-5-html-tagset/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack HTML-Tagset-3.20
-cd HTML-Tagset-3.20
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack HTML-Tagset-3.24
+cd HTML-Tagset-3.24
 perl Makefile.PL
 make
-${THOME}/build/genpkg TRIBlib-perl-5-html-tagset
+cd ..
+${THOME}/build/genpkg TRIBlib-perl-5-html-tagset HTML-Tagset-3.24
