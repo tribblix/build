4a7e443843588dfbba1f06173a049ed85a2a7e6c
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 21:45:59 +0000

    Update i3wm 4.24

--- TRIBi3wm/build.sh
+++ TRIBi3wm/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # i3 now switched to meson
 #
 # if you update this, also update TRIBlib-perl-5-anyevent-i3
 #
-env TRIBBLIX_LDFLAGS=-lsocket TRIBBLIX_CFLAGS=-D__EXTENSIONS__ ${THOME}/build/mesonbuild -64only i3-4.23
-$THOME/build/genpkg TRIBi3wm i3-4.23
+env TRIBBLIX_LDFLAGS=-lsocket TRIBBLIX_CFLAGS=-D__EXTENSIONS__ ${THOME}/build/mesonbuild -64only i3-4.24
+$THOME/build/genpkg TRIBi3wm i3-4.24
--- TRIBlib-perl-5-anyevent-i3/build.sh
+++ TRIBlib-perl-5-anyevent-i3/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # this is actually shipped with the i3 tarball
 #
-${THOME}/build/unpack i3-4.23
-cd i3-4.23
+${THOME}/build/unpack i3-4.24
+cd i3-4.24
 cd AnyEvent-I3
 perl Makefile.PL
 make
