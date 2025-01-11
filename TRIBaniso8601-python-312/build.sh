b480e9cc420e0a6f8be1c91f0eb91d0fca0a81ac
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 10:26:20 +0000

    Update aniso8601 10.0.0

--- TRIBaniso8601-python-312/build.sh
+++ TRIBaniso8601-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBaniso8601-python-312 aniso8601-9.0.1
+${THOME}/build/pkg_setup_py TRIBaniso8601-python-312 aniso8601-10.0.0
