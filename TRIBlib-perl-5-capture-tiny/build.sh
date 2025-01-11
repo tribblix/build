8139956ce395cbe2a110f10b6675c722b1d223f8
Peter Tribble <peter.tribble@gmail.com>
2025-01-11 21:14:55 +0000

    Update Capture::Tiny 0.50

--- TRIBlib-perl-5-capture-tiny/build.sh
+++ TRIBlib-perl-5-capture-tiny/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Capture-Tiny-0.48
-cd Capture-Tiny-0.48
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Capture-Tiny-0.50
+cd Capture-Tiny-0.50
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-capture-tiny Capture-Tiny-0.48
+${THOME}/build/genpkg TRIBlib-perl-5-capture-tiny Capture-Tiny-0.50
