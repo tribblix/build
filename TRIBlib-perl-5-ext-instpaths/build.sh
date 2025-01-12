70357de2153e88bc59b6f24c5906420b88d539da
Peter Tribble <peter.tribble@gmail.com>
2025-01-12 20:20:44 +0000

    Update ExtUtils::InstallPaths 0.014

--- TRIBlib-perl-5-ext-instpaths/build.sh
+++ TRIBlib-perl-5-ext-instpaths/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # required by Build::Tiny
 #
-${THOME}/build/unpack ExtUtils-InstallPaths-0.012
-cd ExtUtils-InstallPaths-0.012
+${THOME}/build/unpack ExtUtils-InstallPaths-0.014
+cd ExtUtils-InstallPaths-0.014
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-ext-instpaths ExtUtils-InstallPaths-0.012
+${THOME}/build/genpkg TRIBlib-perl-5-ext-instpaths ExtUtils-InstallPaths-0.014
