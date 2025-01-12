2d7190b4a23fa444593e853e5a220794c81865eb
Peter Tribble <peter.tribble@gmail.com>
2025-01-12 20:15:00 +0000

    Update ExtUtils::Helpers 0.028

--- TRIBlib-perl-5-extutils-helpers/build.sh
+++ TRIBlib-perl-5-extutils-helpers/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # required by Build::Tiny
 #
-${THOME}/build/unpack ExtUtils-Helpers-0.026
-cd ExtUtils-Helpers-0.026
+${THOME}/build/unpack ExtUtils-Helpers-0.028
+cd ExtUtils-Helpers-0.028
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-extutils-helpers ExtUtils-Helpers-0.026
+${THOME}/build/genpkg TRIBlib-perl-5-extutils-helpers ExtUtils-Helpers-0.028
