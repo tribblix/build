0cfc7265bbce3a4e4f7c5da0d04cba228b8fbff4
Peter Tribble <peter.tribble@gmail.com>
2025-01-12 10:17:40 +0000

    Update tifffile 2025.1.10

--- TRIBtifffile-python-312/build.sh
+++ TRIBtifffile-python-312/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBtifffile-python-312 tifffile-2024.12.12
+# declared dependencies cover direct and the [codecs] and [plot] extras
+# the scikit-image overlay includes the [all] extra
+#
+${THOME}/build/pkg_setup_py TRIBtifffile-python-312 tifffile-2025.1.10
