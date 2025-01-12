6c0877890e53d29a029a0a02affa52a18244e645
Peter Tribble <peter.tribble@gmail.com>
2025-01-12 20:17:26 +0000

    Update ExtUtils::Config 0.010

--- TRIBlib-perl-5-extutils-config/build.sh
+++ TRIBlib-perl-5-extutils-config/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # required by Build::Tiny
 #
-${THOME}/build/unpack ExtUtils-Config-0.008
-cd ExtUtils-Config-0.008
+${THOME}/build/unpack ExtUtils-Config-0.010
+cd ExtUtils-Config-0.010
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-extutils-config ExtUtils-Config-0.008
+${THOME}/build/genpkg TRIBlib-perl-5-extutils-config ExtUtils-Config-0.010
