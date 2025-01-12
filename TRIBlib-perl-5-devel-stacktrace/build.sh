5c11d06349fd3b1e400186732b91ed9f26929d83
Peter Tribble <peter.tribble@gmail.com>
2025-01-12 20:11:38 +0000

    Update Devel::StackTrace 2.05

--- TRIBlib-perl-5-devel-stacktrace/build.sh
+++ TRIBlib-perl-5-devel-stacktrace/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Devel-StackTrace-2.04
-cd Devel-StackTrace-2.04
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Devel-StackTrace-2.05
+cd Devel-StackTrace-2.05
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-devel-stacktrace Devel-StackTrace-2.04
+${THOME}/build/genpkg TRIBlib-perl-5-devel-stacktrace Devel-StackTrace-2.05
