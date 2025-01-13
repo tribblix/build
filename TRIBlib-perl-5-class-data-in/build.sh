354db55d8edcbefe24f333cbeaf4d3105e80792b
Peter Tribble <peter.tribble@gmail.com>
2025-01-13 08:30:24 +0000

    Update Class::Data::Inheritable 0.10

--- TRIBlib-perl-5-class-data-in/build.sh
+++ TRIBlib-perl-5-class-data-in/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Class-Data-Inheritable-0.09
-cd Class-Data-Inheritable-0.09
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Class-Data-Inheritable-0.10
+cd Class-Data-Inheritable-0.10
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-class-data-in Class-Data-Inheritable-0.09
+${THOME}/build/genpkg TRIBlib-perl-5-class-data-in Class-Data-Inheritable-0.10
