a8274faa8c93c998ba0d16b82e31d63e5ad59a7a
Peter Tribble <peter.tribble@gmail.com>
2025-01-13 18:08:00 +0000

    Update libedit 20250104

--- TRIBlibedit/build.sh
+++ TRIBlibedit/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only libedit-20230828-3.1
-${THOME}/build/genpkg TRIBlibedit libedit-20230828-3.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libedit-20250104-3.1
+${THOME}/build/genpkg TRIBlibedit libedit-20250104-3.1
