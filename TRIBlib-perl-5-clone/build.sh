17c436a584e659033c31fe1528248fc1525ffda3
Peter Tribble <peter.tribble@gmail.com>
2025-01-13 08:34:10 +0000

    Update perl Clone 0.47

--- TRIBlib-perl-5-clone/build.sh
+++ TRIBlib-perl-5-clone/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Clone-0.46
-cd Clone-0.46
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Clone-0.47
+cd Clone-0.47
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-clone Clone-0.46
+${THOME}/build/genpkg TRIBlib-perl-5-clone Clone-0.47
