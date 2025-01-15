bb9e71870b4ccc17654970c231f43ec83c2113ab
Peter Tribble <peter.tribble@gmail.com>
2025-01-15 09:06:05 +0000

    Update pyOpenSSL 25.0.0

--- TRIBpyopenssl-312/build.sh
+++ TRIBpyopenssl-312/build.sh
@@ -5,4 +5,4 @@
 # despite the name, the actual openssl bindings are in the
 # cryptography module, not this one
 #
-${THOME}/build/pkg_setup_py TRIBpyopenssl-312 pyopenssl-24.3.0
+${THOME}/build/pkg_setup_py TRIBpyopenssl-312 pyopenssl-25.0.0
