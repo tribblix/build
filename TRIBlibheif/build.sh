0ee5bfc5efd5c6dd658cbd5b8132b99b3368e8fd
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 17:42:10 +0000

    Update libheif 1.19.5

--- TRIBlibheif/build.sh
+++ TRIBlibheif/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild +64 libheif-1.17.6 -C "-DWITH_JPEG_ENCODER=YES -DWITH_JPEG_DECODER=YES -DWITH_OpenJPEG_ENCODER=YES -DWITH_OpenJPEG_DECODER=YES -DCMAKE_PREFIX_PATH=/usr/lib/`${THOME}/build/getarch`/openjpeg-2.5"
-${THOME}/build/cmbuild +32 libheif-1.17.6 -C "-DWITH_JPEG_ENCODER=YES -DWITH_JPEG_DECODER=YES -DWITH_OpenJPEG_ENCODER=YES -DWITH_OpenJPEG_DECODER=YES"
-${THOME}/build/genpkg TRIBlibheif libheif-1.17.6
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild +64 libheif-1.19.5 -C "-DWITH_JPEG_ENCODER=YES -DWITH_JPEG_DECODER=YES -DWITH_OpenJPEG_ENCODER=YES -DWITH_OpenJPEG_DECODER=YES -DCMAKE_PREFIX_PATH=/usr/lib/`${THOME}/build/getarch`/cmake/openjpeg-2.5"
+${THOME}/build/cmbuild +32 libheif-1.19.5 -C "-DWITH_JPEG_ENCODER=YES -DWITH_JPEG_DECODER=YES -DWITH_OpenJPEG_ENCODER=YES -DWITH_OpenJPEG_DECODER=YES"
+${THOME}/build/genpkg TRIBlibheif libheif-1.19.5
