420ef651940bcb5cbbf85ca7349b503e24948cb4
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 15:32:34 +0000

    Rebuild ggobi 64-bit

--- TRIBggobi/build.sh
+++ TRIBggobi/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-env CPPFLAGS="-std=c99 -D_XOPEN_SOURCE=600 -D__EXTENSIONS__" ${THOME}/build/dobuild ggobi-2.1.11
+# SPDX-License-Identifier: CDDL-1.0
+#
+env CPPFLAGS="-std=c99 -D_XOPEN_SOURCE=600 -D__EXTENSIONS__" ${THOME}/build/dobuild -64only ggobi-2.1.11
 ${THOME}/build/genpkg TRIBggobi ggobi-2.1.11
