0f9f83fff4d10f74c678a4cb304801a78fd2b800
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 16:30:35 +0000

    Rebuild xless 64-bit

--- TRIBx11-xless/build.sh
+++ TRIBx11-xless/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # source and patch from https://launchpad.net/ubuntu/+source/xless/1.7-14.4
 # modified the patch to change a few locations, but kept the code changes
 # the same as debian
 #
-${THOME}/build/dobuild xless-1.7
+${THOME}/build/dobuild -64only xless-1.7
 ${THOME}/build/genpkg TRIBx11-xless xless-1.7
