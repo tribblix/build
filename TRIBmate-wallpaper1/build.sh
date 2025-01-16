e3a32bf843b8e86b50b8799484f198a9e2b23c6a
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 15:09:00 +0000

    Chase missing site for some MATE wallpapers

--- TRIBmate-wallpaper1/build.sh
+++ TRIBmate-wallpaper1/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
+# images downloaded by the fixit script
+#
 mkdir -p /tmp/ss
 ${THOME}/build/create_pkg TRIBmate-wallpaper1 /tmp/ss
 rm -fr /tmp/ss
