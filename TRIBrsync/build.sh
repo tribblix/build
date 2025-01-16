fc0fdb0082e2ab346bf60ec8203a33b659ff69ff
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 09:30:13 +0000

    Update rsync 3.4.1

--- TRIBrsync/build.sh
+++ TRIBrsync/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only rsync-3.4.0 -C "--disable-xxhash --with-included-popt"
-${THOME}/build/genpkg TRIBrsync rsync-3.4.0
+${THOME}/build/dobuild -64only rsync-3.4.1 -C "--disable-xxhash --with-included-popt"
+${THOME}/build/genpkg TRIBrsync rsync-3.4.1
