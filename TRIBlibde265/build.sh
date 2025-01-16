07419f106b1dc240d2cdbd3034d4836db21ea788
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 17:29:16 +0000

    Update libde265 1.0.15

--- TRIBlibde265/build.sh
+++ TRIBlibde265/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # disable the example utilities, which would pull in libSDL and Qt
 #
-${THOME}/build/dobuild -64 libde265-1.0.14 -C "--disable-sherlock265 --disable-dec265"
-${THOME}/build/genpkg TRIBlibde265 libde265-1.0.14
+${THOME}/build/dobuild -64 libde265-1.0.15 -C "--disable-sherlock265 --disable-dec265"
+${THOME}/build/genpkg TRIBlibde265 libde265-1.0.15
