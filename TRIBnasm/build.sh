071a2ade222f8511e506877a49a875c4daf8a18e
Peter Tribble <peter.tribble@gmail.com>
2025-01-16 15:23:23 +0000

    Update nasm 2.16.03

--- TRIBnasm/build.sh
+++ TRIBnasm/build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild nasm-2.14.02
-${THOME}/build/genpkg TRIBnasm nasm-2.14.02
+# SPDX-License-Identifier: CDDL-1.0
+#
+# to run the test, cd into the source and
+# gmake golden test
+#
+${THOME}/build/dobuild -64only nasm-2.16.03
+${THOME}/build/genpkg TRIBnasm nasm-2.16.03
