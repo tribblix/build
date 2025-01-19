57d1a6e1fa14cc5d2ddf9bd27fc0e73ebf9f5960
Peter Tribble <peter.tribble@gmail.com>
2025-01-19 11:09:42 +0000

    Update numpy 2.2.2

--- TRIBnumpy-python-312/build.sh
+++ TRIBnumpy-python-312/build.sh
@@ -2,6 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# needs patching to restore sanity to the compilation environment
+# the -D__STDC_NO_ATOMICS__=1 is to force Cython not to pull in stdatomic.h
+# from C++ code, to avoid '_Atomic' does not name a type
 #
-env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "-Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas -Csetup-args=-Dallow-noblas=false" TRIBnumpy-python-312 numpy-1.26.4
+# ideally that would be fixed further up
+#
+env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "-Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas -Csetup-args=-Dallow-noblas=false" TRIBnumpy-python-312 numpy-2.2.2
