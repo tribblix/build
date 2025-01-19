d43df6c84a690d169f7662e11e63631eaa636edd
Peter Tribble <peter.tribble@gmail.com>
2025-01-19 12:00:11 +0000

    Update libjpeg-turbo 3.0.4

--- TRIBimage-libjpeg/build.sh
+++ TRIBimage-libjpeg/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # actually libjpeg-turbo
 # no need for the ijg v9 compatibility
 #
-${THOME}/build/cmbuild -64 libjpeg-turbo-3.0.2
-${THOME}/build/genpkg TRIBimage-libjpeg libjpeg-turbo-3.0.2
+${THOME}/build/cmbuild -64 libjpeg-turbo-3.0.4
+${THOME}/build/genpkg TRIBimage-libjpeg libjpeg-turbo-3.0.4
