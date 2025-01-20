12ebc1caa76fda2967988fefba5402f9dd9d3fed
Peter Tribble <peter.tribble@gmail.com>
2025-01-20 15:40:53 +0000

    Drop 32-bit gst-plugins-good

--- TRIBgstreamer1-plugins-good/build.sh
+++ TRIBgstreamer1-plugins-good/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # qt5 is an undeclared dependency as we don't really want to pull
 # all of that in
 #
-env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu -64 gst-plugins-good-1.20.7 -C "-Dexamples=disabled -Dtests=disabled"
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu +64 gst-plugins-good-1.20.7 -C "-Dexamples=disabled -Dtests=disabled"
 ${THOME}/build/genpkg TRIBgstreamer1-plugins-good gst-plugins-good-1.20.7
