5794a3188428f429c6a6351d73f532871139dd7b
Peter Tribble <peter.tribble@gmail.com>
2025-01-21 13:00:42 +0000

    Update taglib 1.13.1, drop 32-bit

--- TRIBtaglib/build.sh
+++ TRIBtaglib/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64 taglib-1.13 -C "-DBUILD_SHARED_LIBS=on -DBUILD_STATIC_LIBS=no -DBUILD_TESTING=no"
-${THOME}/build/genpkg TRIBtaglib taglib-1.13
+# SPDX-License-Identifier: CDDL-1.0
+#
+# v2 is binary incompatible
+#
+${THOME}/build/cmbuild +64 taglib-1.13.1 -C "-DBUILD_SHARED_LIBS=on -DBUILD_STATIC_LIBS=no -DBUILD_TESTING=no"
+${THOME}/build/genpkg TRIBtaglib taglib-1.13.1
