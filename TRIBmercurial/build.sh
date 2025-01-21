2a2ea50bd6fd949a6cf530161154222b417911f7
Peter Tribble <peter.tribble@gmail.com>
2025-01-21 21:47:09 +0000

    Update mercurial 6.9.1, and build like a normal python module

--- TRIBmercurial/build.sh
+++ TRIBmercurial/build.sh
@@ -2,15 +2,26 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-zap install TRIBdocutils-python-312
-${THOME}/build/unpack mercurial-6.9
-cd mercurial-6.9
-sed -i 's:install -d:ginstall -d:' Makefile
-sed -i 's:install -m:ginstall -m:' Makefile
-sed -i 's:install -m:ginstall -m:' doc/Makefile
-gmake PREFIX=/usr PYTHON=python3.12 all
-rm -fr /tmp/hg1
-gmake PREFIX=/usr PYTHON=python3.12 DESTDIR=/tmp/hg1 install
-${THOME}/build/create_pkg TRIBmercurial /tmp/hg1
-cd ..
-rm -fr /tmp/hg1
+
+#
+# in the new scheme, we build hg as a normal python module, so that
+# it shows up in the list of modules and can be used directly by
+# other python modules
+#
+$THOME/build/pkg_setup_py -v 312 TRIBmercurial mercurial-6.9.1
+
+#
+# the old manual scheme installed hg separately
+#
+#zap install TRIBdocutils-python-312
+#${THOME}/build/unpack mercurial-6.9.1
+#cd mercurial-6.9.1
+#sed -i 's:install -d:ginstall -d:' Makefile
+#sed -i 's:install -m:ginstall -m:' Makefile
+#sed -i 's:install -m:ginstall -m:' doc/Makefile
+#gmake PREFIX=/usr PYTHON=python3.12 all
+#rm -fr /tmp/hg1
+#gmake PREFIX=/usr PYTHON=python3.12 DESTDIR=/tmp/hg1 install
+#${THOME}/build/create_pkg TRIBmercurial /tmp/hg1
+#cd ..
+#rm -fr /tmp/hg1
