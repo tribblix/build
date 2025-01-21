3817d992f32d4ab814232743e55e24e8f15e6903
Peter Tribble <peter.tribble@gmail.com>
2025-01-21 07:46:43 +0000

    Update DBI 1.647

--- TRIBlib-perl-5-dbi/build.sh
+++ TRIBlib-perl-5-dbi/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack DBI-1.646
-cd DBI-1.646
+${THOME}/build/unpack DBI-1.647
+cd DBI-1.647
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-dbi DBI-1.646
+${THOME}/build/genpkg TRIBlib-perl-5-dbi DBI-1.647
