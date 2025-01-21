4d73dd96b7e6a14bbee2a31d540921aa0a295d19
Peter Tribble <peter.tribble@gmail.com>
2025-01-21 20:47:26 +0000

    Add pecan 1.5.1

---
+++ TRIBlogutils-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBlogutils-python-312 logutils-0.3.5
---
+++ TRIBpecan-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpecan-python-312 pecan-1.5.1
