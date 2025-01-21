2408bbeb3585a954594a86b2cf1d924e9e42bf46
Peter Tribble <peter.tribble@gmail.com>
2025-01-21 13:16:00 +0000

    Drop 32-bit speex

--- TRIBlibspeex/build.sh
+++ TRIBlibspeex/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # you need a multi-stage build here
 #
 # if building from scratch, just build and package speex, then install it
@@ -9,6 +11,6 @@
 # if you already have a recent build, then you'll just need to build
 # both, package and install, then repeat
 #
-${THOME}/build/dobuild -64 speex-1.2.1
-${THOME}/build/dobuild -64 speexdsp-1.2.1
+${THOME}/build/dobuild -64only speex-1.2.1
+${THOME}/build/dobuild -64only speexdsp-1.2.1
 ${THOME}/build/genpkg TRIBlibspeex speex-1.2.1 speexdsp-1.2.1
