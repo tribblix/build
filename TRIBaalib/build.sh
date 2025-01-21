08adc2f39692d57041b17cb75223d0c10e7e5b96
Peter Tribble <peter.tribble@gmail.com>
2025-01-21 13:06:43 +0000

    Drop 32-bit aalib

--- TRIBaalib/build.sh
+++ TRIBaalib/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 aalib-1.4.0 -C --with-slang-driver=no
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only aalib-1.4.0 -C --with-slang-driver=no
 ${THOME}/build/genpkg TRIBaalib aalib-1.4.0
