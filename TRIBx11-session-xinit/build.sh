0f1283993b4e426c392ceffc2a837cff3c31bee8
Peter Tribble <peter.tribble@gmail.com>
2025-01-23 21:13:54 +0000

    xinit 1.4.3 broken, revert to 1.4.2

--- TRIBx11-session-xinit/build.sh
+++ TRIBx11-session-xinit/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xinit-1.4.3 -C "--sysconfdir=/etc --with-xinitdir=/etc/X11/xinit"
-${THOME}/build/genpkg TRIBx11-session-xinit xinit-1.4.3
+${THOME}/build/dobuild -64only xinit-1.4.2 -C "--sysconfdir=/etc --with-xinitdir=/etc/X11/xinit"
+${THOME}/build/genpkg TRIBx11-session-xinit xinit-1.4.2
