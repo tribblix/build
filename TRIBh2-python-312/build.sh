6298982ad6bde4b505b08cdf04b3b0d2f9376654
Peter Tribble <peter.tribble@gmail.com>
2025-01-23 08:39:11 +0000

    Update hpack 4.1.0 hyperframe 6.1.0

--- TRIBh2-python-312/build.sh
+++ TRIBh2-python-312/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # ship these 3 together to avoid exploding the number of packages
-# hpack and hyperframe are used by h2, and all some from the same
+# hpack and hyperframe are used by h2, and all come from the same
 # main project
 #
-${THOME}/build/pkg_setup_py TRIBh2-python-312 hpack-4.0.0 hyperframe-6.0.1 h2-4.1.0
+${THOME}/build/pkg_pep518 -N TRIBh2-python-312 hpack-4.1.0 hyperframe-6.1.0 h2-4.1.0
