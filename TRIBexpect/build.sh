4f7f87fea4dd6998b9046e1c5060c55a1b1e0821
Peter Tribble <peter.tribble@gmail.com>
2025-01-25 13:37:30 +0000

    Update expect 5.45.4

--- TRIBexpect/build.sh
+++ TRIBexpect/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needs to be built with the tcl/tk source, not the installed versions
 #
-${THOME}/build/unpack expect5.45
-${THOME}/build/unpack tcl8.6.6-src
-${THOME}/build/unpack tk8.6.6-src
-cd expect5.45
-./configure --prefix=/usr
-gmake -j 8
-${THOME}/build/genpkg TRIBexpect
+${THOME}/build/unpack tcl8.6.15-src
+${THOME}/build/unpack tk8.6.15-src
+
+${THOME}/build/dobuild expect5.45.4
+${THOME}/build/genpkg TRIBexpect expect5.45.4
