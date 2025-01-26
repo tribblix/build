2b67b49a237ef34e64444ce3f824498fe2fae969
Peter Tribble <peter.tribble@gmail.com>
2025-01-26 19:21:11 +0000

    Update Pidgin 2.14.14

--- TRIBpidgin/build.sh
+++ TRIBpidgin/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only -gnu pidgin-2.14.13 -C "--disable-gevolution --disable-meanwhile --disable-avahi --disable-nm --disable-cyrus-sasl"
-${THOME}/build/genpkg TRIBpidgin pidgin-2.14.13
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only -gnu pidgin-2.14.14 -C "--disable-gevolution --disable-meanwhile --disable-avahi --disable-nm --disable-cyrus-sasl"
+${THOME}/build/genpkg TRIBpidgin pidgin-2.14.14
