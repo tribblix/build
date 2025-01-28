20cfb2bdd4f17bb812cf29587d18f528edf58738
Peter Tribble <peter.tribble@gmail.com>
2025-01-28 16:20:59 +0000

    Update qpdf 11.9.1

--- TRIBqpdf/build.sh
+++ TRIBqpdf/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-$THOME/build/cmbuild -64only qpdf-11.9.0
-${THOME}/build/genpkg TRIBqpdf qpdf-11.9.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+$THOME/build/cmbuild -64only qpdf-11.9.1 -C "-DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF"
+${THOME}/build/genpkg TRIBqpdf qpdf-11.9.1
