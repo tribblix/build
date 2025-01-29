d51b758696d0e345ea2b91cc6acae7c2e6d2cf13
Peter Tribble <peter.tribble@gmail.com>
2025-01-29 08:26:56 +0000

    Update codespell 2.4.1

--- TRIBcodespell-python-312/build.sh
+++ TRIBcodespell-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBcodespell-python-312 codespell-2.4.0
+${THOME}/build/pkg_pep518 -N TRIBcodespell-python-312 codespell-2.4.1
