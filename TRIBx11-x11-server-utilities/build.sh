197363e45c6d975401db9eace532b0eb58167fa7
Peter Tribble <peter.tribble@gmail.com>
2025-02-03 20:13:57 +0000

    Update xhost 1.0.10

--- TRIBx11-x11-server-utilities/build.sh
+++ TRIBx11-x11-server-utilities/build.sh
@@ -17,7 +17,7 @@
 ${THOME}/build/dobuild -64only rgb-1.1.0 -C "--with-rgb-db-dir=/usr/lib/X11/rgb --with-rgb-db-type=ndbm"
 ${THOME}/build/dobuild -64only xcmsdb-1.0.7
 ${THOME}/build/dobuild -64only xgamma-1.0.7
-${THOME}/build/dobuild -64only xhost-1.0.9
+${THOME}/build/dobuild -64only xhost-1.0.10
 ${THOME}/build/dobuild -64only xinput-1.6.4
 ${THOME}/build/dobuild -64only xmodmap-1.0.11
 ${THOME}/build/dobuild -64only xprop-1.2.7
@@ -31,4 +31,4 @@ ${THOME}/build/dobuild -64only xsetroot-1.1.3
 # xmakemap installed by the fixit script
 #
 
-${THOME}/build/genpkg TRIBx11-x11-server-utilities rgb-1.1.0 xcmsdb-1.0.7 xgamma-1.0.7 xhost-1.0.9 xinput-1.6.4 xmodmap-1.0.11 xprop-1.2.7 xrandr-1.5.3 xrdb-1.2.2 xrefresh-1.1.0 xset-1.2.5 xsetroot-1.1.3
+${THOME}/build/genpkg TRIBx11-x11-server-utilities rgb-1.1.0 xcmsdb-1.0.7 xgamma-1.0.7 xhost-1.0.10 xinput-1.6.4 xmodmap-1.0.11 xprop-1.2.7 xrandr-1.5.3 xrdb-1.2.2 xrefresh-1.1.0 xset-1.2.5 xsetroot-1.1.3
