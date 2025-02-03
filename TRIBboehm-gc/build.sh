d481494cd339872708ab1e0088e8b2f696749f1a
Peter Tribble <peter.tribble@gmail.com>
2025-02-03 21:30:34 +0000

    Update boehm-gc 8.2.8

--- TRIBboehm-gc/build.sh
+++ TRIBboehm-gc/build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # some of the archives are doubly gzipped, so you need to gunzip them
 # and rename with the gz suffix
 #
-# later versions have removed some symbols from the library
-# so I've been very conservative regarding updates
+# later versions have changed some symbols from global to local
+# so need to be conservative regarding updates
 #
-# and the 7.2x tarballs are all copies of gc-7.2, so need to
+# and the 7.2x tarballs were all copies of gc-7.2, so need to
 # rm gc-7.2.tar.gz; ln -s gc-7.2g.tar.gz gc-7.2.tar.gz
 #
-env TRIBBLIX_CFLAGS=-fcommon ${THOME}/build/dobuild -64only gc-7.2
-${THOME}/build/genpkg TRIBboehm-gc gc-7.2
+${THOME}/build/dobuild -64only gc-8.2.8
+${THOME}/build/genpkg TRIBboehm-gc gc-8.2.8
