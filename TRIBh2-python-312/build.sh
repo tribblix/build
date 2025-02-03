6a452639c67a454eb32910d5d2dcb41f40552f0a
Peter Tribble <peter.tribble@gmail.com>
2025-02-03 18:17:24 +0000

    Update h2 4.2.0

--- TRIBh2-python-312/build.sh
+++ TRIBh2-python-312/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # ship these 3 together to avoid exploding the number of packages
 # hpack and hyperframe are used by h2, and all come from the same
 # main project
 #
-${THOME}/build/pkg_pep518 -N TRIBh2-python-312 hpack-4.1.0 hyperframe-6.1.0 h2-4.1.0
+${THOME}/build/pkg_pep518 -N TRIBh2-python-312 hpack-4.1.0 hyperframe-6.1.0 h2-4.2.0
