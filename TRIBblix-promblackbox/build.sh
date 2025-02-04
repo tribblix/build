fce45d833069c7ffd2c13a4a94cff7c549b6639b
Peter Tribble <peter.tribble@gmail.com>
2025-02-04 14:37:13 +0000

    Add the prometheus blackbox exporter 0.25.0

---
+++ TRIBblix-promblackbox/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+
+mkdir p1
+cd p1
+
+#
+# we need to build the promu tool first, as otherwise the main build
+# tries to download it which fails as there isn't a prebuilt one
+#
+git clone https://github.com/prometheus/promu.git
+cd promu
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.23/bin:$PATH gmake build
+cd ..
+
+#
+$THOME/build/unpack blackbox_exporter-0.25.0
+cd blackbox_exporter-0.25.0
+#
+# cp collector/uname_bsd.go collector/uname_solaris.go
+# and change to build on solaris
+# and add solaris to the build of collector/uname.go
+#
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.23/bin:$PATH gmake build
+cd ..
+
+${THOME}/build/genpkg TRIBblix-promblackbox
