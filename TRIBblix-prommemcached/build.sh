c921f3828bb47af3715d79e3f2de0c86cc2250ac
Peter Tribble <peter.tribble@gmail.com>
2025-02-04 14:43:54 +0000

    Add the prometheus memcached exporter 0.15.0

---
+++ TRIBblix-prommemcached/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+
+mkdir p1
+cd p1
+
+#
+# we need to build the promu tool first, as otherwise the main build
+# tries to download it which fails as there isn't a prebuilt one
+#
+git clone https://github.com/prometheus/promu.git
+cd promu
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.23/bin:$PATH gmake build
+cd ..
+
+#
+$THOME/build/unpack memcached_exporter-0.15.0
+cd memcached_exporter-0.15.0
+
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.23/bin:$PATH gmake build
+cd ..
+
+${THOME}/build/genpkg TRIBblix-prommemcached
