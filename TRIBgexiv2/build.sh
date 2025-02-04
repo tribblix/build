d71235ad055113148467b645637f588615762463
Peter Tribble <peter.tribble@gmail.com>
2025-02-04 10:52:57 +0000

    Update gexiv2 0.14.3

--- TRIBgexiv2/build.sh
+++ TRIBgexiv2/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # used by gimp
 #
-$THOME/build/mesonbuild -64only gexiv2-0.14.2 -C "-Dvapi=false -Dintrospection=false -Dpython3=false"
-${THOME}/build/genpkg TRIBgexiv2 gexiv2-0.14.2
+$THOME/build/mesonbuild -64only gexiv2-0.14.3 -C "-Dvapi=false -Dintrospection=false -Dpython3=false"
+${THOME}/build/genpkg TRIBgexiv2 gexiv2-0.14.3
