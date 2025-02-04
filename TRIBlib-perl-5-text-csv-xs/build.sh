a2a15db0cb0302065bf74995a6b42328949ea68f
Peter Tribble <peter.tribble@gmail.com>
2025-02-04 08:20:36 +0000

    Update Text::CSV_XS 1.60

--- TRIBlib-perl-5-text-csv-xs/build.sh
+++ TRIBlib-perl-5-text-csv-xs/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Text-CSV_XS-1.59
-cd Text-CSV_XS-1.59
+${THOME}/build/unpack Text-CSV_XS-1.60
+cd Text-CSV_XS-1.60
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-text-csv-xs Text-CSV_XS-1.59
+${THOME}/build/genpkg TRIBlib-perl-5-text-csv-xs Text-CSV_XS-1.60
