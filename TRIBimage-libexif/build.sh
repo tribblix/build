3f7bb0593aef18b4298c3aa84efe193f54090dd5
Peter Tribble <peter.tribble@gmail.com>
2025-02-04 10:28:45 +0000

    Update libexif 0.6.25

--- TRIBimage-libexif/build.sh
+++ TRIBimage-libexif/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild +64 libexif-0.6.24
-${THOME}/build/genpkg TRIBimage-libexif libexif-0.6.24
+${THOME}/build/dobuild +64 libexif-0.6.25
+${THOME}/build/genpkg TRIBimage-libexif libexif-0.6.25
