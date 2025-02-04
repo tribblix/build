c9fde2d4c0bbf099c5aa1902643a52cba9e0790f
Peter Tribble <peter.tribble@gmail.com>
2025-02-04 08:24:02 +0000

    Update paramiko 3.5.1

--- TRIBparamiko-python-312/build.sh
+++ TRIBparamiko-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # paramiko[all] adds invoke and pyasn1 over the fixed dependencies
 #
-${THOME}/build/pkg_setup_py TRIBparamiko-python-312 paramiko-3.5.0
+${THOME}/build/pkg_setup_py TRIBparamiko-python-312 paramiko-3.5.1
