9d6c4dfbd4045fd0d33bfdd1b5416be176331f2b
Peter Tribble <peter.tribble@gmail.com>
2025-02-05 09:33:24 +0000

    Update zlib 1.3.1

--- TRIBlib-zlib/build.sh
+++ TRIBlib-zlib/build.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # It must go in /lib as wget needs it
 # omnios has LDSHARED="gcc -shared -nostdlib" CFLAGS="-DNO_VIZ"
 # oi has much more in LDSHARED and forces -O2
 # -Wl,-z -Wl,direct -Wl,-z -Wl,text -Wl,-z -Wl,defs -Wl,-h -Wl,libz.so.1
 # It's not a real configure script
 #
-${THOME}/build/unpack -64 zlib-1.2.13
-cd zlib-1.2.13
+${THOME}/build/unpack -64 zlib-1.3.1
+cd zlib-1.3.1
 env CFLAGS="-DNO_VIZ -O2" ./configure --prefix=/usr --sharedlibdir=/lib
 gmake LDSHARED="gcc -shared -nostdlib -Wl,-z -Wl,direct -Wl,-z -Wl,text -Wl,-z -Wl,defs -Wl,-h -Wl,libz.so.1 -Wl,-M -Wl,${THOME}/build/patches/zlib.mapfile"
 cd ..
-cd zlib-1.2.13-64bit
+cd zlib-1.3.1-64bit
 env CFLAGS="-DNO_VIZ -O2" ./configure --64 --prefix=/usr --libdir=/usr/lib/`isainfo -k` --sharedlibdir=/lib/`isainfo -k`
 gmake LDSHARED="gcc -shared -nostdlib -Wl,-z -Wl,direct -Wl,-z -Wl,text -Wl,-z -Wl,defs -Wl,-h -Wl,libz.so.1 -Wl,-M -Wl,${THOME}/build/patches/zlib.mapfile"
 cd ..
-${THOME}/build/genpkg TRIBlib-zlib zlib-1.2.13
+${THOME}/build/genpkg TRIBlib-zlib zlib-1.3.1
