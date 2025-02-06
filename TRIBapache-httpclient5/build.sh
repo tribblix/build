9fd9794a54a487860f0e2a2682fbb336346e44a9
Peter Tribble <peter.tribble@gmail.com>
2025-02-06 20:29:51 +0000

    Update HttpComponents Client 5.4.2

--- TRIBapache-httpclient5/build.sh
+++ TRIBapache-httpclient5/build.sh
@@ -5,12 +5,12 @@
 rm -fr /tmp/hc5
 mkdir -p /tmp/hc5
 cd /tmp/hc5
-$THOME/build/unpack httpcomponents-client-5.4.1-bin
+$THOME/build/unpack httpcomponents-client-5.4.2-bin
 # just shuffle the files we need to the right place
 mkdir -p usr/share/httpcomponents-client-5
-mv lib/httpcore5-5.3.1.jar usr/share/httpcomponents-client-5
-mv lib/httpcore5-h2-5.3.1.jar usr/share/httpcomponents-client-5
-mv lib/httpclient5-5.4.1.jar usr/share/httpcomponents-client-5
+mv lib/httpcore5-5.3.3.jar usr/share/httpcomponents-client-5
+mv lib/httpcore5-h2-5.3.3.jar usr/share/httpcomponents-client-5
+mv lib/httpclient5-5.4.2.jar usr/share/httpcomponents-client-5
 rm -fr javadoc lib
 mv *.txt usr/share/httpcomponents-client-5
 cd
