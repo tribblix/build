1eab69eb83c6e4b7fc2a32ad4fe354ba47bf2d41
Peter Tribble <peter.tribble@gmail.com>
2025-02-09 21:18:26 +0000

    Add fava 1.30.1, fixing up beancount to suit

---
+++ TRIBfava-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# dependencies have the [excel] extra added
+#
+${THOME}/build/pkg_pep518 -N TRIBfava-python-312 fava-1.30.1
---
+++ TRIBtatsu-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBtatsu-python-312 tatsu-5.13.1
