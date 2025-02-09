e2d2ab00ba37145fa776b4a7c8195e495262ef5c
Peter Tribble <peter.tribble@gmail.com>
2025-02-09 19:58:52 +0000

    Update putty 0.83

--- TRIBputty/build.sh
+++ TRIBputty/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only putty-0.82
-${THOME}/build/genpkg TRIBputty putty-0.82
+env TRIBBLIX_LDFLAGS="-lsocket -lnsl" $THOME/build/cmbuild -64only putty-0.83
+${THOME}/build/genpkg TRIBputty putty-0.83
