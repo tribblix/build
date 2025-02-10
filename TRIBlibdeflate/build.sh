7425f3e02d68238733202edb3c94a524f1fbef0c
Peter Tribble <peter.tribble@gmail.com>
2025-02-10 08:58:06 +0000

    Update libdeflate 1.23, drop 32-bit

--- TRIBlibdeflate/build.sh
+++ TRIBlibdeflate/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64 libdeflate-1.19
-${THOME}/build/genpkg TRIBlibdeflate libdeflate-1.19
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild -64only libdeflate-1.23
+${THOME}/build/genpkg TRIBlibdeflate libdeflate-1.23
