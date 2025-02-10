2f84fe364f15580338f63e5fe5c3a36f92161fa7
Peter Tribble <peter.tribble@gmail.com>
2025-02-10 08:26:54 +0000

    Update YAML::PP 0.39.0

--- TRIBlib-perl-5-yaml-pp/build.sh
+++ TRIBlib-perl-5-yaml-pp/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack YAML-PP-v0.38.1
-cd YAML-PP-v0.38.1
+${THOME}/build/unpack YAML-PP-v0.39.0
+cd YAML-PP-v0.39.0
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-yaml-pp YAML-PP-v0.38.1
+${THOME}/build/genpkg TRIBlib-perl-5-yaml-pp YAML-PP-v0.39.0
