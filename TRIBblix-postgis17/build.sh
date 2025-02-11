8d405f9249c8e21b030774bda2aa46608fae3a4c
Peter Tribble <peter.tribble@gmail.com>
2025-02-11 15:57:15 +0000

    Update PostGIS 3.5.2, and add to postgres 17

--- TRIBblix-postgis17/build.sh
+++ TRIBblix-postgis17/build.sh
@@ -2,11 +2,11 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# postgis for postgres 16
+# postgis for postgres 17
 #
 # the binaries are linked against postgres libraries, so this package
 # installs into the same prefix as the postgres version it's built
 # against, and is versioned to match
 #
-$THOME/build/dobuild +64only postgis-3.5.2 -P /opt/tribblix/postgres16 -C "--with-pgconfig=/opt/tribblix/postgres16/bin/pg_config --sysconfdir=/etc"
-$THOME/build/genpkg TRIBblix-postgis16 postgis-3.5.2
+$THOME/build/dobuild +64only postgis-3.5.2 -P /opt/tribblix/postgres17 -C "--with-pgconfig=/opt/tribblix/postgres17/bin/pg_config --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-postgis17 postgis-3.5.2
