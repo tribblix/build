111ec567e4cc356557bf55dfa8d5bb9a78c7b696
Peter Tribble <peter.tribble@gmail.com>
2025-02-11 21:06:25 +0000

    Update git-lfs 3.6.1

--- TRIBgit-lfs/build.sh
+++ TRIBgit-lfs/build.sh
@@ -5,8 +5,8 @@
 # it's a straight makefile
 # ignore the env noise, it's non-portable
 #
-${THOME}/build/dobuild git-lfs-3.5.1
+${THOME}/build/dobuild git-lfs-3.6.1
 #
 # there's no make install, so fixinstall does all the work
 #
-${THOME}/build/genpkg TRIBgit-lfs git-lfs-3.5.1
+${THOME}/build/genpkg TRIBgit-lfs git-lfs-3.6.1
