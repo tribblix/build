ea73846766f5c4921271ce1fe758006e32169068
Peter Tribble <peter.tribble@gmail.com>
2025-02-11 14:48:44 +0000

    Update GDAL 3.10.1

--- TRIBgdal/build.sh
+++ TRIBgdal/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only gdal-3.9.2
-${THOME}/build/genpkg TRIBgdal gdal-3.9.2
+# updates bump the SONAME
+#
+${THOME}/build/cmbuild -64only gdal-3.10.1
+${THOME}/build/genpkg TRIBgdal gdal-3.10.1
