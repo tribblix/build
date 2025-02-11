cb6f7c14a0ee2ad4a199a67783bbacebba54f207
Peter Tribble <peter.tribble@gmail.com>
2025-02-11 19:37:43 +0000

    Rebuild amfora with go 1.23

--- TRIBamfora/build.sh
+++ TRIBamfora/build.sh
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# requires go
-#
 git clone https://github.com/makeworld-the-better-one/amfora
 cd amfora
 gmake
@@ -13,6 +11,8 @@ mkdir -p /tmp/af123/usr/bin
 cp ./amfora /tmp/af123/usr/bin
 mkdir -p /tmp/af123/usr/share/applications
 cp amfora.desktop /tmp/af123/usr/share/applications
+mkdir -p /tmp/af123/usr/share/man/man1
+cp amfora.1 /tmp/af123/usr/share/man/man1
 
 ${THOME}/build/create_pkg TRIBamfora /tmp/af123
 
