7075df7afedcb4b66eb6c2f743b086b9f5805eb9
Peter Tribble <peter.tribble@gmail.com>
2025-02-11 15:49:06 +0000

    Update GEOS 3.13.0

--- TRIBgeos/build.sh
+++ TRIBgeos/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only geos-3.12.2 -C "-DBUILD_TESTING=OFF -DBUILD_DOCUMENTATION=OFF"
-${THOME}/build/genpkg TRIBgeos geos-3.12.2
+# any update bumps the SONAME
+#
+${THOME}/build/cmbuild -64only geos-3.13.0 -C "-DBUILD_TESTING=OFF -DBUILD_DOCUMENTATION=OFF"
+${THOME}/build/genpkg TRIBgeos geos-3.13.0
