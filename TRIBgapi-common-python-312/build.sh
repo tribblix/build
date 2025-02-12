abf94418e723bca63c05409d64c4289cf50c6062
Peter Tribble <peter.tribble@gmail.com>
2025-02-12 21:14:14 +0000

    Update googleapis-common-protos 1.67.0

--- TRIBgapi-common-python-312/build.sh
+++ TRIBgapi-common-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBgapi-common-python-312 googleapis_common_protos-1.66.0
+${THOME}/build/pkg_setup_py TRIBgapi-common-python-312 googleapis_common_protos-1.67.0
