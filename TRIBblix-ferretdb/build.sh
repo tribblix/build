eab5757891b31c7fd4221d2fce117f647bd0e9b3
Peter Tribble <peter.tribble@gmail.com>
2025-02-12 19:56:43 +0000

    Add FerretDB 2.0.0rc1

---
+++ TRIBblix-ferretdb/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+
+#
+git clone https://github.com/FerretDB/FerretDB
+cd FerretDB
+
+#
+cd tools
+go generate -x
+cd ..
+
+#
+# Edit Taskfile.yml to add (ne OS "illumos") to RACE_FLAG just like windows
+#  (yes, explicitly illumos)
+#
+./bin/task init
+
+#
+# ./bin/task build-production
+#  [needs docker, so doesn't work]
+# ./bin/task build-host
+# or it's really just as simple as
+echo 'bin' >! build/version/package.txt
+go build -v -trimpath -o=bin/ferretdb ./cmd/ferretdb
+
+#
+rm -fr /tmp/fdb123
+mkdir -p /tmp/fdb123/opt/tribblix/ferretdb/bin
+cp ./bin/ferretdb /tmp/fdb123/opt/tribblix/ferretdb/bin
+cp README.md LICENSE /tmp/fdb123/opt/tribblix/ferretdb
+
+${THOME}/build/create_pkg TRIBblix-ferretdb /tmp/fdb123
+
+rm -fr /tmp/fdb123
