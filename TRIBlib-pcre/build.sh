7ec82ae066d248366407f5d990c3fcef8627bc02
Peter Tribble <peter.tribble@gmail.com>
2025-02-13 08:19:17 +0000

    Update pcre 8.45

--- TRIBlib-pcre/build.sh
+++ TRIBlib-pcre/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# [must rebuild ggrep abiword gnumeric i3 nmap swig afterwards]
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 pcre-8.42 -C "--includedir=/usr/include/pcre --localstatedir=/var --disable-static --enable-cpp --enable-rebuild-chartables --enable-utf8 --enable-unicode-properties --enable-newline-is-any --disable-stack-for-recursion --enable-pcregrep-libz --enable-pcregrep-libbz2 --with-posix-malloc-threshold=20 --with-link-size=4 --with-match-limit=10000000 --with-pic"
-${THOME}/build/genpkg TRIBlib-pcre pcre-8.42
+${THOME}/build/dobuild -64 pcre-8.45 -C "--includedir=/usr/include/pcre --localstatedir=/var --disable-static --enable-cpp --enable-rebuild-chartables --enable-utf8 --enable-unicode-properties --enable-newline-is-any --disable-stack-for-recursion --enable-pcregrep-libz --enable-pcregrep-libbz2 --with-posix-malloc-threshold=20 --with-link-size=4 --with-match-limit=10000000 --with-pic"
+${THOME}/build/genpkg TRIBlib-pcre pcre-8.45
