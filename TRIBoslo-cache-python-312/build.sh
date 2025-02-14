b671e27adabffd60f9772f53e7acdd701456b025
Peter Tribble <peter.tribble@gmail.com>
2025-02-14 08:47:16 +0000

    Update oslo.cache 3.10.0

--- TRIBoslo-cache-python-312/build.sh
+++ TRIBoslo-cache-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-cache-python-312 oslo.cache-3.9.0
+${THOME}/build/pkg_setup_py TRIBoslo-cache-python-312 oslo_cache-3.10.0
