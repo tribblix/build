44f7d717f4aef0819257cfd4d4e76937c28b3ee8
Peter Tribble <peter.tribble@gmail.com>
2025-02-15 18:24:48 +0000

    Drop node v18

--- TRIBv-node-v18/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# 64-bit only
-#
-env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v18.20.6 -P /usr/versions/node-v18 -J 6 -C "--without-dtrace --dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v18 node-v18.20.6
