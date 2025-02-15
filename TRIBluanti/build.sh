45dc130dcd4f6c25752abdaa894d8d2295462cbc
Peter Tribble <peter.tribble@gmail.com>
2025-02-15 13:10:53 +0000

    Update Luanti 5.11.0

--- TRIBluanti/build.sh
+++ TRIBluanti/build.sh
@@ -12,6 +12,6 @@
 # it really is using ncurses
 #
 
-env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch` -lsocket -lnsl -liconv -lncursesw" TRIBBLIX_CXXFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include -fpermissive" $THOME/build/cmbuild +gnu -64only luanti-5.10.0 -C "-DRUN_IN_PLACE=FALSE -DCURSES_NEED_NCURSES=TRUE"
+env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch` -lsocket -lnsl -liconv -lncursesw" TRIBBLIX_CXXFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include -fpermissive" $THOME/build/cmbuild +gnu -64only luanti-5.11.0 -C "-DRUN_IN_PLACE=FALSE -DCURSES_NEED_NCURSES=TRUE"
 
-${THOME}/build/genpkg TRIBluanti luanti-5.10.0
+${THOME}/build/genpkg TRIBluanti luanti-5.11.0
