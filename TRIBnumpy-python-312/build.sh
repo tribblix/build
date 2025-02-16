9bff119af0dca277c7606049da45297c288a227f
Peter Tribble <peter.tribble@gmail.com>
2025-02-16 10:58:27 +0000

    Update numpy 2.2.3

--- TRIBnumpy-python-312/build.sh
+++ TRIBnumpy-python-312/build.sh
@@ -13,4 +13,4 @@
 # and will rebuild everything against 2.x before bumping this package
 # back to 2.x
 #
-env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "-Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas -Csetup-args=-Dallow-noblas=false" TRIBnumpy-python-312 numpy-2.2.2
+env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "-Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas -Csetup-args=-Dallow-noblas=false" TRIBnumpy-python-312 numpy-2.2.3
