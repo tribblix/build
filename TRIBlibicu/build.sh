dfbfb8fe464f95867d6effcd8e08536bb32a1606
Peter Tribble <peter.tribble@gmail.com>
2025-02-16 12:28:06 +0000

    Drop icu 50+68 compat

--- TRIBlibicu/build.sh
+++ TRIBlibicu/build.sh
@@ -2,8 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# the old shared libraries are copied in by the fixit step
-# for compatibility, as the SONAME gets bumped each release
+# the SONAME gets bumped each release so, if necessary, the old
+# shared libraries will need to be copied in by the fixit step in
+# order for binaries linked against the previous version to run
 #
 ${THOME}/build/dobuild -64 -n icu icu4c-72_1-src -S source -C "CC=gcc CXX=g++ --disable-tests --disable-examples"
 ${THOME}/build/genpkg TRIBlibicu icu4c-72_1-src
