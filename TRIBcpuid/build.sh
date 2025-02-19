d782fe9f07ed488d5a3e9dbf1890e83ce2a1f6c9
Peter Tribble <peter.tribble@gmail.com>
2025-02-19 13:28:02 +0000

    Update cpuid 1.8.3

--- TRIBcpuid/build.sh
+++ TRIBcpuid/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-env EXTRA_CFLAGS=-m64 ${THOME}/build/dobuild cpuid-1.8.2
-env EXTRA_CFLAGS=-m64 ${THOME}/build/genpkg TRIBcpuid cpuid-1.8.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+# there's a postconf script to fix the install step
+#
+env EXTRA_CFLAGS=-m64 ${THOME}/build/dobuild cpuid-1.8.3
+env EXTRA_CFLAGS=-m64 ${THOME}/build/genpkg TRIBcpuid cpuid-1.8.3
