e3653c03d4e0f9dc2aa531c9f8ddf22119687287
Peter Tribble <peter.tribble@gmail.com>
2025-02-20 08:01:00 +0000

    Update Sphinx 8.2.0

---
+++ TRIBroman-numerals-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals_py-3.0.0
--- TRIBsphinx-312/build.sh
+++ TRIBsphinx-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBsphinx-312 sphinx-8.1.3
+${THOME}/build/pkg_pep518 -N TRIBsphinx-312 sphinx-8.2.0
