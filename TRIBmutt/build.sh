0bacf863c4f84e7b33daec1215c6260eeab57aef
Peter Tribble <peter.tribble@gmail.com>
2025-02-21 09:17:48 +0000

    Update mutt 2.2.14

--- TRIBmutt/build.sh
+++ TRIBmutt/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild mutt-2.2.13 -64only -C "--enable-imap --with-ssl --with-slang --enable-smtp --enable-sidebar --enable-gpgme"
-${THOME}/build/genpkg TRIBmutt mutt-2.2.13
+${THOME}/build/dobuild mutt-2.2.14 -64only -C "--enable-imap --with-ssl --with-slang --enable-smtp --enable-sidebar --enable-gpgme"
+${THOME}/build/genpkg TRIBmutt mutt-2.2.14
