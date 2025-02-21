11f09ea33c4d9afa10f8cad96ee5588c4f6b27a5
Peter Tribble <peter.tribble@gmail.com>
2025-02-21 08:57:19 +0000

    Update google-cloud-core 2.4.2

--- TRIBgoog-cloud-core-python-312/build.sh
+++ TRIBgoog-cloud-core-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBgoog-cloud-core-python-312 google-cloud-core-2.4.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBgoog-cloud-core-python-312 google_cloud_core-2.4.2
