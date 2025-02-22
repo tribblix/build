0fa0a2ce573f0189705efee5057454d023ba8120
Peter Tribble <peter.tribble@gmail.com>
2025-02-22 14:16:08 +0000

    Update oslo.service 4.1.0

--- TRIBoslo-service-python-312/build.sh
+++ TRIBoslo-service-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-service-python-312 oslo.service-4.0.0
+${THOME}/build/pkg_setup_py TRIBoslo-service-python-312 oslo_service-4.1.0
