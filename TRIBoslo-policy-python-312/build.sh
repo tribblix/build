b24770c07becb87cb95052978f38ef79349413be
Peter Tribble <peter.tribble@gmail.com>
2025-02-22 14:05:14 +0000

    Update oslo.policy 4.5.1

--- TRIBoslo-policy-python-312/build.sh
+++ TRIBoslo-policy-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-policy-python-312 oslo.policy-4.5.0
+${THOME}/build/pkg_setup_py TRIBoslo-policy-python-312 oslo_policy-4.5.1
