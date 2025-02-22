b66a23c81c431ce0ebeb00dece29fd8fe4be4375
Peter Tribble <peter.tribble@gmail.com>
2025-02-22 09:24:07 +0000

    Update roman-numerals-py 3.1.0

--- TRIBroman-numerals-python-312/build.sh
+++ TRIBroman-numerals-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals_py-3.0.0
+${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals_py-3.1.0
