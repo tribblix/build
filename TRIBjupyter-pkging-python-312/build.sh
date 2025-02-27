6ea3647bcab17ef1d6b614801d469ae9718b700f
Peter Tribble <peter.tribble@gmail.com>
2025-02-27 08:30:10 +0000

    Fix missing dependencies on setuptools

--- TRIBjupyter-pkging-python-312/build.sh
+++ TRIBjupyter-pkging-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBjupyter-pkging-python-312 jupyter_packaging-0.12.3
+${THOME}/build/pkg_pep518 -N TRIBjupyter-pkging-python-312 jupyter_packaging-0.12.3
