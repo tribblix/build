c7744cb61499d352fac042064bf6663a8153a89e
Peter Tribble <peter.tribble@gmail.com>
2025-02-27 12:32:41 +0000

    Repackage smartmontools with new man page locations

--- TRIBsmartmontools/build.sh
+++ TRIBsmartmontools/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# a preconf switch puts the manual pages in the right place for IPD4
+#
 ${THOME}/build/dobuild -64only smartmontools-7.4 -C --sysconfdir=/etc
 ${THOME}/build/genpkg TRIBsmartmontools smartmontools-7.4
