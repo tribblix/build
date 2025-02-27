1ad0606150877abc3da80ba7cb982bf691d6e88a
Peter Tribble <peter.tribble@gmail.com>
2025-02-27 08:15:12 +0000

    Update oslo.db 17.2.1

--- TRIBoslo-db-python-312/build.sh
+++ TRIBoslo-db-python-312/build.sh
@@ -7,4 +7,4 @@
 #
 # note that testtools adds setuptools as a requirement
 #
-${THOME}/build/pkg_pep518 TRIBoslo-db-python-312 oslo_db-17.2.0 testresources-2.0.1 testscenarios-0.5.0 testtools-2.7.2
+${THOME}/build/pkg_pep518 TRIBoslo-db-python-312 oslo_db-17.2.1 testresources-2.0.1 testscenarios-0.5.0 testtools-2.7.2
