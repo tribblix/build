b47ed4700aa17b3acf7217035da43d90794ac802
Peter Tribble <peter.tribble@gmail.com>
2025-02-28 14:53:41 +0000

    Update commons-logging in apache-xmlrpc package

--- TRIBapache-xmlrpc-313/build.sh
+++ TRIBapache-xmlrpc-313/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# this is an old archoved project, get apache-xmlrpc-3.1.3-bin.tar.gz
+# this is an old archived project, get apache-xmlrpc-3.1.3-bin.tar.gz
 # from https://archive.apache.org/dist/ws/xmlrpc/binaries/
 #
 # get the fixit script to do all the work
