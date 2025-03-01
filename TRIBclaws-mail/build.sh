be69c29cb8d62207491872eb598f4542b7892720
Peter Tribble <peter.tribble@gmail.com>
2025-03-01 14:49:56 +0000

    Update claws-mail 4.3.1

--- TRIBclaws-mail/build.sh
+++ TRIBclaws-mail/build.sh
@@ -7,5 +7,5 @@
 # where the certificate bundle happens to live
 #
 zap uninstall TRIBlibcanberra
-${THOME}/build/dobuild -64only claws-mail-4.3.0 -C "--disable-ldap --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.3.0
+${THOME}/build/dobuild -64only claws-mail-4.3.1 -C "--disable-ldap --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBclaws-mail claws-mail-4.3.1
