14a4eafc477b36b63ec82fc8ee200384a3873cd9
Peter Tribble <peter.tribble@gmail.com>
2025-03-01 15:54:25 +0000

    Drop postgres16

--- TRIBblix-postgres16/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# postgres 16
-#
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres16 postgresql-16.8 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres16 postgresql-16.8
--- TRIBblix-repmgr16/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# this is a postgres extension, so it's specific to the postgres version
-#
-${THOME}/build/dobuild +64only repmgr-5.5.0 -C PG_CONFIG=/opt/tribblix/postgres16/bin/pg_config
-${THOME}/build/genpkg TRIBblix-repmgr16 repmgr-5.5.0
