6246ac6a5edc6345027958415bb92ed63894b2ce
Peter Tribble <peter.tribble@gmail.com>
2025-03-01 12:16:35 +0000

    Repackage linkchecker to fix man pages

--- TRIBlinkchecker-python-312/build.sh
+++ TRIBlinkchecker-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBlinkchecker-python-312 LinkChecker-10.5.0
+${THOME}/build/pkg_pep518 -N TRIBlinkchecker-python-312 LinkChecker-10.5.0
