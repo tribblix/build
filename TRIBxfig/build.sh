71de1df1e15abb0d121fc5cf28fa483a67cef458
Peter Tribble <peter.tribble@gmail.com>
2025-03-02 20:02:00 +0000

    Update xfig 3.2.9a

--- TRIBxfig/build.sh
+++ TRIBxfig/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # now link against Xaw3d
 #
-${THOME}/build/dobuild -64only xfig-3.2.9
-${THOME}/build/dobuild -64only fig2dev-3.2.9
-${THOME}/build/genpkg TRIBxfig xfig-3.2.9 fig2dev-3.2.9
+${THOME}/build/dobuild -64only xfig-3.2.9a
+${THOME}/build/dobuild -64only fig2dev-3.2.9a
+${THOME}/build/genpkg TRIBxfig xfig-3.2.9a fig2dev-3.2.9a
