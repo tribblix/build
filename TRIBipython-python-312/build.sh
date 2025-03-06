3b70230fc25e3aa751a767e8d379b76d387df1c2
Peter Tribble <peter.tribble@gmail.com>
2025-03-06 13:04:49 +0000

    Update IPython 9.0.1

--- TRIBipython-python-312/build.sh
+++ TRIBipython-python-312/build.sh
@@ -2,4 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBipython-python-312 ipython-8.32.0
+# ipython-pygments-lexers used to be included in IPython, now spun out,
+# but package together
+#
+# ipython build uses setuptool, ipython-pygments-lexers uses flit-core
+#
+${THOME}/build/pkg_pep518 -N TRIBipython-python-312 ipython-9.0.1 ipython_pygments_lexers-1.1.1
