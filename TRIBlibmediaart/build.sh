f75cd25d474f2b6010a07c96ea99bd8adc39bc89
Peter Tribble <peter.tribble@gmail.com>
2025-03-06 08:59:55 +0000

    Update libmediaart 1.9.7

--- TRIBlibmediaart/build.sh
+++ TRIBlibmediaart/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/mesonbuild -64only libmediaart-1.9.6 -C -Dintrospection=false
-${THOME}/build/genpkg TRIBlibmediaart libmediaart-1.9.6
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/mesonbuild -64only libmediaart-1.9.7 -C -Dintrospection=false
+${THOME}/build/genpkg TRIBlibmediaart libmediaart-1.9.7
