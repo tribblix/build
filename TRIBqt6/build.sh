4eda56e928d626ef91abe51516f6344ea690f8be
Peter Tribble <peter.tribble@gmail.com>
2025-03-08 21:18:38 +0000

    Update Qt 6.6.3

--- TRIBqt6/build.sh
+++ TRIBqt6/build.sh
@@ -11,19 +11,17 @@
 #
 # if we get the 64-bit build fixed. update the fixit file as well
 #
-${THOME}/build/unpack qt-everywhere-src-6.2.6
+${THOME}/build/unpack qt-everywhere-src-6.6.3
 #
 # 64-bit build needs a fair amount of work to get 64-bit consistently
 #
-cd qt-everywhere-src-6.2.6
-env CXX="g++ -m64" CC="gcc -m64" PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:/usr/bin/amd64:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -skip qtwebengine -skip qtquick3dphysics -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11 -bindir /usr/versions/Qt-6/bin -libdir /usr/versions/Qt-6/lib/amd64 -plugindir /usr/versions/Qt-6/plugins/amd64 -qmldir /usr/versions/Qt-6/qml/amd64 -platform solaris-g++-64 CMAKE_PREFIX_PATH=`pwd`/qtbase/lib/amd64/cmake CMAKE_LIBRARY_ARCHITECTURE=amd64
+cd qt-everywhere-src-6.6.3
+env LANG=C.UTF-8 CXX="g++ -m64" CC="gcc -m64" PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:/usr/bin/amd64:$PATH ./configure --prefix=/usr/versions/Qt-6 -opensource --disable-reduce-exports -xcb -xcb-xlib -qpa xcb -release -system-pcre -system-zlib -system-freetype -system-libpng -system-libjpeg -system-webp -system-tiff -jasper -no-sql-sqlite -no-eglfs -skip qtwebengine -skip qtquick3dphysics -no-pch -confirm-license -nomake examples -nomake tests -cmake-generator 'Unix Makefiles' -c++std c++11 -bindir /usr/versions/Qt-6/bin -libdir /usr/versions/Qt-6/lib/amd64 -plugindir /usr/versions/Qt-6/plugins/amd64 -qmldir /usr/versions/Qt-6/qml/amd64 -platform solaris-g++-64 CMAKE_PREFIX_PATH=`pwd`/qtbase/lib/amd64/cmake CMAKE_LIBRARY_ARCHITECTURE=amd64
 mkdir qtbase/translations
-env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:/usr/bin/amd64:$PATH gmake -j 8
-cd ..
+env LANG=C.UTF-8  PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig PATH=/usr/gnu/bin:/usr/bin/amd64:$PATH gmake -j 8
 
 # gnu to get a working "install" binary
 rm -fr /tmp/qt6
-cd qt-everywhere-src-6.2.6
 env PATH=/usr/gnu/bin:$PATH gmake -j 8 install DESTDIR=/tmp/qt6
 cd ..
 ${THOME}/build/create_pkg TRIBqt6 /tmp/qt6
--- patches/qt-everywhere-src-6.6.3.pls
+++ patches/qt-everywhere-src-6.6.3.pls
@@ -1,5 +1,8 @@
 -p1 qt6/01-Assertions.h.patch
 -p1 qt6/02-open62541.c.patch
 -p1 qt6/03-qssgrenderparticles.cpp.patch
--p1 qt6/04-qv4assemblercommon_p.h.patch
 -p1 qt6/05-lupdateCmakeLists.txt.patch
+-p1 qt6/06-qelfparser_p.cpp.patch
+-p1 qt6/08-qplugin.h.patch
+-p1 qt6/11-qt_version_tag.patch
+-p1 qt6/14-qv4stacklimits.cpp.patch
