35d6a015a3cf711a287df2be9a641a1eaadac972
Peter Tribble <peter.tribble@gmail.com>
2025-03-08 09:45:23 +0000

    Update apache-arrow-adbc 17

--- TRIBapache-arrow-adbc/build.sh
+++ TRIBapache-arrow-adbc/build.sh
@@ -7,5 +7,5 @@
 #
 # there's a little patch to make the postgresql build work on illumos
 #
-env TRIBBLIX_PKG_CONFIG_PATH=/opt/tribblix/postgres15/lib/pkgconfig $THOME/build/cmbuild -64only -S c apache-arrow-adbc-15 -C "-DADBC_BUILD_SHARED=ON -DADBC_BUILD_STATIC=OFF -DADBC_DRIVER_MANAGER=ON -DADBC_DRIVER_POSTGRESQL=ON"
-${THOME}/build/genpkg TRIBapache-arrow-adbc apache-arrow-adbc-15
+env TRIBBLIX_PKG_CONFIG_PATH=/opt/tribblix/postgres15/lib/pkgconfig $THOME/build/cmbuild -64only -S c apache-arrow-adbc-17 -C "-DADBC_BUILD_SHARED=ON -DADBC_BUILD_STATIC=OFF -DADBC_DRIVER_MANAGER=ON -DADBC_DRIVER_POSTGRESQL=ON"
+${THOME}/build/genpkg TRIBapache-arrow-adbc apache-arrow-adbc-17
