7985ff1747fffd946dfaf43216316093999fdd96
Peter Tribble <peter.tribble@gmail.com>
2025-03-09 10:35:42 +0000

    Rebuild netsurf 64-bit

--- TRIBnetsurf/build.sh
+++ TRIBnetsurf/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
+
+#
+# Note: If updating from 32-bit to 64-bit, you'll need to clear the cache
+#   rm -fr ~/.cache/netsurf
+# otherwise it won't work and you get an InitFailed error
+#
+
 $THOME/build/unpack netsurf-all-3.11
 cd netsurf-all-3.11
 #
@@ -8,10 +17,10 @@ cd netsurf-all-3.11
 # no configure, just make, but a set of Makefiles that make
 # a bunch of bad guesses
 #
-gmake -j 4 build CC=gcc BUILD=i386-pc-solaris2.11 PREFIX=/usr AR=ar INSTALL=ginstall BUILD_CC=gcc TARGET=gtk3
+gmake -j 4 build CC="gcc -m64" BUILD=i386-pc-solaris2.11 PREFIX=/usr AR=ar INSTALL=ginstall BUILD_CC="gcc -m64" TARGET=gtk3
 # passing INSTALL is ignored
 rm -fr /tmp/nn311
-env PATH=/usr/gnu/bin:$PATH gmake install CC=gcc BUILD=i386-pc-solaris2.11 PREFIX=/usr DESTDIR=/tmp/nn311 INSTALL=ginstall BUILD_CC=gcc TARGET=gtk3
+env PATH=/usr/gnu/bin:$PATH gmake install CC="gcc -m64" BUILD=i386-pc-solaris2.11 PREFIX=/usr DESTDIR=/tmp/nn311 INSTALL=ginstall BUILD_CC="gcc -m64" TARGET=gtk3
 mkdir -p /tmp/nn311/usr/share/man/man1
 cp netsurf/docs/netsurf-gtk.1 /tmp/nn311/usr/share/man/man1
 mkdir -p /tmp/nn311/usr/share/applications
