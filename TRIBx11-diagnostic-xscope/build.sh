7050767faf644aa2ea6b37c22ba352cf3f504b3d
Peter Tribble <peter.tribble@gmail.com>
2025-03-09 09:40:43 +0000

    Update xscope 1.4.5

--- TRIBx11-diagnostic-xscope/build.sh
+++ TRIBx11-diagnostic-xscope/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xscope-1.4.4 -C "CPPFLAGS=-DFD_SETSIZE=256"
-${THOME}/build/genpkg TRIBx11-diagnostic-xscope xscope-1.4.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only xscope-1.4.5 -C "CPPFLAGS=-DFD_SETSIZE=256"
+${THOME}/build/genpkg TRIBx11-diagnostic-xscope xscope-1.4.5
