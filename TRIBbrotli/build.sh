5cbdd4ff0c76a4333e7887c727309ecbbc9e82bc
Peter Tribble <peter.tribble@gmail.com>
2025-03-11 10:49:40 +0000

    Update brotli 1.1.0, drop 32-bit

--- TRIBbrotli/build.sh
+++ TRIBbrotli/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-#
-${THOME}/build/cmbuild -64 brotli-1.0.9
-${THOME}/build/genpkg TRIBbrotli brotli-1.0.9
+${THOME}/build/cmbuild -64only brotli-1.1.0
+${THOME}/build/genpkg TRIBbrotli brotli-1.1.0
