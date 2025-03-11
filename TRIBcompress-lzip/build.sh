4ba663cd26d094914b5051bc96b9cd806eb09861
Peter Tribble <peter.tribble@gmail.com>
2025-03-11 12:12:42 +0000

    Update lzip 1.25 lzlib 1.15 plzip 1.12, drop 32-bit, add lunzip

--- TRIBcompress-lzip/build.sh
+++ TRIBcompress-lzip/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # lzip and friends
 #
-${THOME}/build/dobuild lzip-1.23 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
-${THOME}/build/dobuild -64 lzlib-1.14 -C --enable-shared +C "CFLAGS+=-m64 --enable-shared"
-${THOME}/build/dobuild plzip-1.11 -C "CPPFLAGS=-I../lzlib-1.14-64bit LDFLAGS=-L../lzlib-1.14-64bit CXXFLAGS+=-m64"
-${THOME}/build/genpkg TRIBcompress-lzip lzip-1.23 lzlib-1.14 plzip-1.11
+${THOME}/build/dobuild lzip-1.25 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
+${THOME}/build/dobuild -64only lzlib-1.15 -C "CFLAGS+=-m64 --disable-static --enable-shared"
+${THOME}/build/dobuild plzip-1.12 -C "CPPFLAGS=-I../lzlib-1.15-64bit LDFLAGS=-L../lzlib-1.15-64bit CXXFLAGS+=-m64"
+${THOME}/build/dobuild lunzip-1.15 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
+${THOME}/build/genpkg TRIBcompress-lzip lzip-1.25 lzlib-1.15 plzip-1.12 lunzip-1.15
