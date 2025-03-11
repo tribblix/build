b89280b3cd9e4173c896e2cbe38ca9c891d25b7f
Peter Tribble <peter.tribble@gmail.com>
2025-03-11 20:41:08 +0000

    Update LibVNCServer 0.9.15

--- TRIBlibvncserver/build.sh
+++ TRIBlibvncserver/build.sh
@@ -4,5 +4,5 @@
 #
 # libvncserver
 #
-env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/cmbuild -64only libvncserver-LibVNCServer-0.9.14
-${THOME}/build/genpkg TRIBlibvncserver libvncserver-LibVNCServer-0.9.14
+env LDFLAGS="-lsocket -lnsl" TRIBBLIX_LDFLAGS="-lsocket -lnsl" ${THOME}/build/cmbuild -64only libvncserver-LibVNCServer-0.9.15
+${THOME}/build/genpkg TRIBlibvncserver libvncserver-LibVNCServer-0.9.15
