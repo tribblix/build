91c7a8ead3f44d5688d4aa5fcc8233c2dd1e9a05
Peter Tribble <peter.tribble@gmail.com>
2025-03-11 09:52:10 +0000

    Drop emacs 29

--- TRIBv-emacs-29/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-29.4 -P /usr/versions/emacs-29 -C "--with-xft --with-xpm --without-sqlite3"
-${THOME}/build/genpkg TRIBv-emacs-29 emacs-29.4
