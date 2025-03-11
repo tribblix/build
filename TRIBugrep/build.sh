e61e40e69f2106c7c3794a6a28fab207c2b8bf92
Peter Tribble <peter.tribble@gmail.com>
2025-03-11 10:43:20 +0000

    Update ugrep 7.3.0

--- TRIBugrep/build.sh
+++ TRIBugrep/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only ugrep-5.1.4 -C "--enable-sse2 --disable-avx"
-${THOME}/build/genpkg TRIBugrep ugrep-5.1.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only ugrep-7.3.0 -C "--enable-sse2 --disable-avx"
+${THOME}/build/genpkg TRIBugrep ugrep-7.3.0
