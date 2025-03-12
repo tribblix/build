3386c3931c23c4814addb8c61d6418f3224323e3
Peter Tribble <peter.tribble@gmail.com>
2025-03-12 20:05:03 +0000

    spelldict builds shouldn't use tcsh

--- TRIBspelldict-de/build.sh
+++ TRIBspelldict-de/build.sh
@@ -1,26 +1,5 @@
-#!/bin/tcsh
+#!/bin/sh
 #
-$THOME/build/unpack igerman98-20161207
-cd igerman98-20161207
-sed -i 's:-i latin1:-i ISO8859-1:' Makefile
-gmake hunspell-all AFFIX_EXPANDER=aspell
-
-rm -fr /tmp/sp-de
-mkdir -p /tmp/sp-de/usr/share/hunspell
-cp hunspell/de_{AT,CH,DE}.{aff,dic} /tmp/sp-de/usr/share/hunspell
-cd /tmp/sp-de/usr/share/hunspell
+# SPDX-License-Identifier: CDDL-1.0
 #
-# symlinks
-#
-# these aliases derived from the OI list
-#
-ln -s de_CH.aff de_de_LI.aff
-ln -s de_CH.dic de_de_LI.dic
-foreach alt ( de_BE de_LU )
-ln -s de_DE.aff ${alt}.aff
-ln -s de_DE.dic ${alt}.dic
-end
-
-${THOME}/build/create_pkg TRIBspelldict-de /tmp/sp-de
-cd
-rm -fr /tmp/sp-de
+${THOME}/build/genpkg TRIBspelldict-de
--- TRIBspelldict-en/build.sh
+++ TRIBspelldict-en/build.sh
@@ -1,44 +1,7 @@
-#!/bin/tcsh
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
 #
 # we use the "normal" (not "large") SCOWL lists
 #
-rm -fr /tmp/sp-en
-mkdir -p /tmp/sp-en/usr/share/hunspell
-cd /tmp/sp-en/usr/share/hunspell
-#
-# download
-#
-wget https://sourceforge.net/projects/wordlist/files/speller/2020.12.07/hunspell-en_CA-2020.12.07.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2020.12.07/hunspell-en_AU-2020.12.07.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2020.12.07/hunspell-en_US-2020.12.07.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2020.12.07/hunspell-en_GB-ise-2020.12.07.zip
-wget https://sourceforge.net/projects/wordlist/files/speller/2020.12.07/hunspell-en_GB-ize-2020.12.07.zip
-#
-# unpack
-#
-unzip -q hunspell-en_AU-2020.12.07.zip
-unzip -q hunspell-en_CA-2020.12.07.zip
-unzip -q hunspell-en_GB-ise-2020.12.07.zip
-unzip -q hunspell-en_GB-ize-2020.12.07.zip
-unzip -q hunspell-en_US-2020.12.07.zip
-rm -f *.zip
-#
-# symlinks
-#
-# for british english, prefer "ise"
-#
-ln -s en_GB-ise.aff en_GB.aff
-ln -s en_GB-ise.dic en_GB.dic
-#
-# these aliases derived from the OI list
-#
-ln -s en_US.aff en_PH.aff
-ln -s en_US.dic en_PH.dic
-foreach alt ( en_AG en_BS en_BW en_BZ en_DK en_GH en_HK en_IE en_IN en_JM en_NA en_NG en_NZ en_SG en_TT en_ZA en_ZW )
-ln -s en_GB.aff ${alt}.aff
-ln -s en_GB.dic ${alt}.dic
-end
-
-${THOME}/build/create_pkg TRIBspelldict-en /tmp/sp-en
-cd
-rm -fr /tmp/sp-en
+${THOME}/build/genpkg TRIBspelldict-en
