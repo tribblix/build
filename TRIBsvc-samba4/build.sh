bdddf24fe3c77b29d4620e84664fdee1f89d50ad
Peter Tribble <peter.tribble@gmail.com>
2025-03-13 20:52:34 +0000

    Create a working samba service

--- TRIBsvc-samba4/build.sh
+++ TRIBsvc-samba4/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # the samba4 package ships the software, which is useful for clients
 # all the work here in populating the package is done in the fixit script
 #
