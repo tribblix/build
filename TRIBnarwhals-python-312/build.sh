6d6cfce07fad5112de948bd680521c3554603b40
Peter Tribble <peter.tribble@gmail.com>
2025-03-13 09:12:23 +0000

    Update bokeh 3.7.0, add narwhals

---
+++ TRIBnarwhals-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# there are a lot of possible dependencies, but all are in extras
+#
+${THOME}/build/pkg_pep518 -N TRIBnarwhals-python-312 narwhals-1.30.0
