0aba1cdb49421fa9e42770a62101eea9579a3985
Peter Tribble <peter.tribble@gmail.com>
2025-03-14 19:45:50 +0000

    REmove errantly named package

--- TRIBkj6jobwidgets/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-env LANG=C.UTF-8 PATH=/usr/versions/Qt-6/bin/amd64:$PATH $THOME/build/cmbuild -64only kconfig-6.11.0 -P /usr/versions/Qt-6 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-6/lib/amd64/cmake -DKF_IGNORE_PLATFORM_CHECK=TRUE"
-${THOME}/build/genpkg TRIBkf6config kconfig-6.11.0
