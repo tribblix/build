b8b913c9c53fe6541196e057238dad86b2e7501f
Peter Tribble <peter.tribble@gmail.com>
2025-03-14 16:13:29 +0000

    Add KF6 ConfigWidgets Codecs ColorScheme

---
+++ TRIBkf6colorscheme/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+env LANG=C.UTF-8 PATH=/usr/versions/Qt-6/bin/amd64:$PATH $THOME/build/cmbuild -64only kcolorscheme-6.11.0 -P /usr/versions/Qt-6 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-6/lib/amd64/cmake -DKF_IGNORE_PLATFORM_CHECK=TRUE"
+${THOME}/build/genpkg TRIBkf6colorscheme kcolorscheme-6.11.0
