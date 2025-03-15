584b74d220bb29a726cdc8196e03638192c9d170
Peter Tribble <peter.tribble@gmail.com>
2025-03-15 15:35:07 +0000

    Add picmi

---
+++ TRIBkde-picmi/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+env LANG=C.UTF-8 PATH=/usr/versions/Qt-6/bin/amd64:/usr/gnu/bin:$PATH $THOME/build/cmbuild -64only picmi-24.12.3 -P /usr/versions/Qt-6 -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-6/lib/amd64/cmake -DKF_IGNORE_PLATFORM_CHECK=TRUE"
+${THOME}/build/genpkg TRIBkde-picmi picmi-24.12.3
