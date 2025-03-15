07d52caeb03f481cc629edcc89cfa00af0349dda
Peter Tribble <peter.tribble@gmail.com>
2025-03-15 10:19:04 +0000

    Comment typo

--- TRIBsnappy/build.sh
+++ TRIBsnappy/build.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # the old library needs to be built with the old ABI for compatibility
-# this can be droped if snappy is updated, as that will require all
+# this can be dropped if snappy is updated, as that will require all
 # consumers to be rebuilt in any case
 #
 env TRIBBLIX_CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ${THOME}/build/dobuild +64 snappy-1.1.3
