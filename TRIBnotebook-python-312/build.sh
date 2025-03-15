84901840246cff19fd21137e01dd120c93450ead
Peter Tribble <peter.tribble@gmail.com>
2025-03-15 09:47:11 +0000

    Update notebook 7.3.3

--- TRIBnotebook-python-312/build.sh
+++ TRIBnotebook-python-312/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# needs hatch-jupyter-builder to go along with -N
-#
-${THOME}/build/pkg_pep518 -N TRIBnotebook-python-312 notebook-7.3.2
+${THOME}/build/pkg_pep518 -N TRIBnotebook-python-312 notebook-7.3.3
