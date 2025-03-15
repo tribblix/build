f24764ebbfe9dde248646f3359aed0c4504c19e5
Peter Tribble <peter.tribble@gmail.com>
2025-03-15 09:41:42 +0000

    Update breezy 3.3.10

--- TRIBbreezy-python-312/build.sh
+++ TRIBbreezy-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # needs rust and gnu msgfmt
 #
-env LD_RUN_PATH=/usr/versions/python-3.12/lib ${THOME}/build/pkg_setup_py -gnu TRIBbreezy-python-312 breezy-3.3.9
+env LD_RUN_PATH=/usr/versions/python-3.12/lib ${THOME}/build/pkg_setup_py -gnu TRIBbreezy-python-312 breezy-3.3.10
