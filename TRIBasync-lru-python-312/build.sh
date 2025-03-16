1400db01e17fb711548c6899106e5d2f4d45912d
Peter Tribble <peter.tribble@gmail.com>
2025-03-16 18:12:46 +0000

    Update async-lru 2.0.5

--- TRIBasync-lru-python-312/build.sh
+++ TRIBasync-lru-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBasync-lru-python-312 async-lru-2.0.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBasync-lru-python-312 async_lru-2.0.5
