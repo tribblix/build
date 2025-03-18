085221dc324e3c9d6d54a58071014d7e86af4dba
Peter Tribble <peter.tribble@gmail.com>
2025-03-18 19:38:48 +0000

    Update minio-client 2025-03-12

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -4,13 +4,13 @@
 #
 mkdir ~/ud/c
 cd ~/ud/c
-env GOPATH=`pwd` PATH=/usr/versions/go-1.23/bin:$PATH go install github.com/minio/mc@RELEASE.2024-11-17T19-35-25Z
+env GOPATH=`pwd` PATH=/usr/versions/go-1.23/bin:$PATH go install github.com/minio/mc@RELEASE.2025-03-12T17-29-24Z
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
 mkdir -p /tmp/ee/usr/share/minio-client
-cp bin/mc /tmp/ee/usr/bin
-cp pkg/mod/github.com/minio/mc@v0.0.0-20241117*/README.md /tmp/ee/usr/share/minio-client
-cp pkg/mod/github.com/minio/mc@v0.0.0-20241117*/LICENSE /tmp/ee/usr/share/minio-client
+cp bin/mc /tmp/ee/usr/bin/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20250312*/README.md /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20250312*/LICENSE /tmp/ee/usr/share/minio-client
 ${THOME}/build/create_pkg TRIBminio-client /tmp/ee
 rm -fr /tmp/ee
