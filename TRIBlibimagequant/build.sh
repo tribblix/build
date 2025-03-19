5af219066a2ebfbd2bc2718cff0f008d86404336
Peter Tribble <peter.tribble@gmail.com>
2025-03-19 16:27:18 +0000

    Drop 32-bit libimagequant

--- TRIBlibimagequant/build.sh
+++ TRIBlibimagequant/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # later versions (4.x) are rust-based, stay with the 2.x branch
 # for wider portability
 #
 # it's not an autoconf configure, but it understands the arguments that
 # dobuild uses well enough
 #
-${THOME}/build/dobuild -64 libimagequant-2.18.0
+${THOME}/build/dobuild -64only libimagequant-2.18.0
 ${THOME}/build/genpkg -gnu TRIBlibimagequant libimagequant-2.18.0
