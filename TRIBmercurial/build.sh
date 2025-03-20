25729465b32e5bd5181de1c477569b09f3ed6bc1
Peter Tribble <peter.tribble@gmail.com>
2025-03-20 08:36:56 +0000

    Update mercurial 6.9.4

--- TRIBmercurial/build.sh
+++ TRIBmercurial/build.sh
@@ -8,4 +8,4 @@
 # it shows up in the list of modules and can be used directly by
 # other python modules
 #
-$THOME/build/pkg_setup_py -v 312 TRIBmercurial mercurial-6.9.3
+$THOME/build/pkg_setup_py -v 312 TRIBmercurial mercurial-6.9.4
