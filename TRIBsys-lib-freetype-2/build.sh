0a61d1bd88cfa827a4ad361cb19cceaea1f187e5
Peter Tribble <peter.tribble@gmail.com>
2025-03-21 14:07:34 +0000

    Update freetype 2.13.3

--- TRIBsys-lib-freetype-2/build.sh
+++ TRIBsys-lib-freetype-2/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 freetype-2.10.4 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no --enable-freetype-config"
-${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.10.4
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 freetype-2.13.3 -C "-enable-biarch-config --with-zlib --with-pic --with-harfbuzz=no --enable-freetype-config --with-brotli=no"
+${THOME}/build/genpkg TRIBsys-lib-freetype-2 freetype-2.13.3
