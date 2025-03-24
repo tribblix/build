9f223b9b7ff54bca3ad2f336a1adafcd53c54781
Peter Tribble <peter.tribble@gmail.com>
2025-03-24 08:26:58 +0000

    Update pdfminer.six 20250324

--- TRIBpdfminer-six-python-312/build.sh
+++ TRIBpdfminer-six-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBpdfminer-six-python-312 pdfminer.six-20240706
+${THOME}/build/pkg_setup_py TRIBpdfminer-six-python-312 pdfminer.six-20250324
