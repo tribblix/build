9cf1699da11f1a7e884815894d80d88cf19a2a88
Peter Tribble <peter.tribble@gmail.com>
2025-03-24 09:21:37 +0000

    Update Zabbix 6.0.39

--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only zabbix-6.0.36 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre2"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-6.0.36
+${THOME}/build/dobuild -64only zabbix-6.0.39 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre2"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-6.0.39
