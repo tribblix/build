67a9625de286b128a052637a9dff9b355144e5c5
Peter Tribble <peter.tribble@gmail.com>
2025-03-26 09:23:53 +0000

    Roll setuptools-scm back to 8.2.0 (8.2.1 was yanked)

--- TRIBsetup-scm-python-312/build.sh
+++ TRIBsetup-scm-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBsetup-scm-python-312 setuptools_scm-8.2.1
+${THOME}/build/pkg_pep518 TRIBsetup-scm-python-312 setuptools_scm-8.2.0
