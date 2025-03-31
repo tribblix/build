f69043e43cc528e3686a64d90da8d6e163aad85b
Peter Tribble <peter.tribble@gmail.com>
2025-03-31 17:53:39 +0100

    Update pyasn1_modules 0.4.2

--- TRIBpyasn1-modules-python-312/build.sh
+++ TRIBpyasn1-modules-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBpyasn1-modules-python-312 pyasn1_modules-0.4.1
+${THOME}/build/pkg_setup_py TRIBpyasn1-modules-python-312 pyasn1_modules-0.4.2
