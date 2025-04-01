a9b4c299c085976ad5956da6e92e968d1ec818a8
Peter Tribble <peter.tribble@gmail.com>
2025-04-01 09:17:56 +0100

    Update pycodestyle 2.13.0

--- TRIBpycodestyle-python-312/build.sh
+++ TRIBpycodestyle-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBpycodestyle-python-312 pycodestyle-2.12.1
+${THOME}/build/pkg_setup_py TRIBpycodestyle-python-312 pycodestyle-2.13.0
