c05f01e29df9c33b1fa0aecf2bf6ea9706964cf6
Peter Tribble <peter.tribble@gmail.com>
2025-04-01 09:34:50 +0100

    Update virtualenv 20.30.0

--- TRIBvirtualenv-python-312/build.sh
+++ TRIBvirtualenv-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBvirtualenv-python-312 virtualenv-20.29.3
+${THOME}/build/pkg_pep518 -N TRIBvirtualenv-python-312 virtualenv-20.30.0
