abfc2fbc77fad044939479fd818537f6ed8537b5
Peter Tribble <peter.tribble@gmail.com>
2025-04-04 08:57:46 +0100

    Update Test::Deep 1.205

--- TRIBlib-perl-5-test-deep/build.sh
+++ TRIBlib-perl-5-test-deep/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Test-Deep-1.204
-cd Test-Deep-1.204
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Test-Deep-1.205
+cd Test-Deep-1.205
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-test-deep Test-Deep-1.204
+${THOME}/build/genpkg TRIBlib-perl-5-test-deep Test-Deep-1.205
