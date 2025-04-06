ba506beb8d61eeb496c806f9d202df1d365e36ce
Peter Tribble <peter.tribble@gmail.com>
2025-04-06 18:42:02 +0100

    Update boolean.py 5.0

--- TRIBboolean-py-python-312/build.sh
+++ TRIBboolean-py-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBboolean-py-python-312 boolean.py-4.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBboolean-py-python-312 boolean_py-5.0
