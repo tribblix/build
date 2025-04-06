2bfb004dd71415f99981f422ba484923d9f9895c
Peter Tribble <peter.tribble@gmail.com>
2025-04-06 16:59:15 +0100

    Update at-spi2-core 2.42.1

--- TRIBlib-desktop-at-spi2/build.sh
+++ TRIBlib-desktop-at-spi2/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# newer versions need newer glib2
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/mesonbuild -64 -gnu at-spi2-core-2.40.3 -C "-Dintrospection=no -Ddbus_daemon=/usr/lib/dbus-daemon"
-${THOME}/build/genpkg TRIBlib-desktop-at-spi2 at-spi2-core-2.40.3
+${THOME}/build/mesonbuild -64 -gnu at-spi2-core-2.42.1 -C "-Dintrospection=no -Ddbus_daemon=/usr/lib/dbus-daemon"
+${THOME}/build/genpkg TRIBlib-desktop-at-spi2 at-spi2-core-2.42.1
