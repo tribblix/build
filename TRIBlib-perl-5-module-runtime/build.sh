d53314f0ec50001418fe368617d16579410d1fa9
Peter Tribble <peter.tribble@gmail.com>
2025-04-08 09:19:43 +0100

    Update Module::Runtime 0.017

--- TRIBlib-perl-5-module-runtime/build.sh
+++ TRIBlib-perl-5-module-runtime/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Module-Runtime-0.016
-cd Module-Runtime-0.016
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Module-Runtime-0.017
+cd Module-Runtime-0.017
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-module-runtime Module-Runtime-0.016
+${THOME}/build/genpkg TRIBlib-perl-5-module-runtime Module-Runtime-0.017
