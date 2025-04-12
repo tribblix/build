69fc6515907df4a73746204630695b26012016ed
Peter Tribble <peter.tribble@gmail.com>
2025-04-12 10:05:45 +0100

    Update pydantic 2.11.3

--- TRIBpydantic-python-312/build.sh
+++ TRIBpydantic-python-312/build.sh
@@ -5,4 +5,4 @@
 # build together, as they're joined at the hip and version locked
 # note that pydantic needs an *exact* version of pydantic_core
 #
-${THOME}/build/pkg_pep518 TRIBpydantic-python-312 pydantic_core-2.27.2 pydantic-2.10.6
+${THOME}/build/pkg_pep518 TRIBpydantic-python-312 pydantic_core-2.33.1 pydantic-2.11.3
---
+++ TRIBtyping-ins-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBtyping-ins-python-312 typing_inspection-0.4.0
