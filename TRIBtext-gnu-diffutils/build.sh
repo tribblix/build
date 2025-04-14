d121e29400c9aadd0d7965961175d4d834e71371
Peter Tribble <peter.tribble@gmail.com>
2025-04-14 08:40:19 +0100

    Update diffutils 3.12

--- TRIBtext-gnu-diffutils/build.sh
+++ TRIBtext-gnu-diffutils/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild +64only -gnu diffutils-3.11 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-diffutils diffutils-3.11
+${THOME}/build/dobuild +64only -gnu diffutils-3.12 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-diffutils diffutils-3.12
