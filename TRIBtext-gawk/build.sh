62cc381653e573d6bb0a3309c9d7a46bca1e1316
Peter Tribble <peter.tribble@gmail.com>
2025-04-14 09:23:48 +0100

    Update gawk 5.3.2

--- TRIBtext-gawk/build.sh
+++ TRIBtext-gawk/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild +64only -gnu gawk-5.3.1 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gawk gawk-5.3.1
+${THOME}/build/dobuild +64only -gnu gawk-5.3.2 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gawk gawk-5.3.2
