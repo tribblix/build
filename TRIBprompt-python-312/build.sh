f741bbc6e464f86e6a49b2c7ce58c2e798b7abae
Peter Tribble <peter.tribble@gmail.com>
2025-04-16 15:36:32 +0100

    Update prompt_toolkit 3.0.51

--- TRIBprompt-python-312/build.sh
+++ TRIBprompt-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBprompt-python-312 prompt_toolkit-3.0.50
+${THOME}/build/pkg_pep518 -N TRIBprompt-python-312 prompt_toolkit-3.0.51
