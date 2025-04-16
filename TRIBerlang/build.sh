fcbea2069636b36b8b7f2a487674442458e8b6de
Peter Tribble <peter.tribble@gmail.com>
2025-04-16 19:04:52 +0100

    Update Erlang 26.2.5.11

--- TRIBerlang/build.sh
+++ TRIBerlang/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # for testing only; seeing a number of failures in the test suite
 #
 # current RabbitMQ needs 25 minimum, but supports 26
@@ -10,10 +12,10 @@
 # with -64 on solaris, but passing -64 to gcc doesn't work
 #
 env DED_LD="ld" DED_LDFLAGS="-m64 -G" DED_LD_FLAG_RUNTIME_LIBRARY_PATH="-R" \
-${THOME}/build/dobuild -64only otp_src_26.2.3 -C --enable-m64-build
+${THOME}/build/dobuild -64only otp_src_26.2.5.11 -C --enable-m64-build
 #
 # [tests:
-# cd otp_src_26.2.3-64bit
+# cd otp_src_26.2.5.11-64bit
 # env ERL_TOP=`pwd` gmake release_tests
 # env ERL_TOP=`pwd` sh 'cd release/tests/test_server; $ERL_TOP/bin/erl -s ts install -s ts smoke_test batch -s init stop'
 # ls -l `pwd`/release/tests/test_server/index.html
@@ -21,4 +23,4 @@ ${THOME}/build/dobuild -64only otp_src_26.2.3 -C --enable-m64-build
 # ]
 #
 env DED_LD="ld" DED_LDFLAGS="-m64 -G" DED_LD_FLAG_RUNTIME_LIBRARY_PATH="-R" \
-${THOME}/build/genpkg TRIBerlang otp_src_26.2.3
+${THOME}/build/genpkg TRIBerlang otp_src_26.2.5.11
