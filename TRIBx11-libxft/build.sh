19db5fe2318bf1d74836faeb3a3c6c2ea68545cc
Peter Tribble <peter.tribble@gmail.com>
2025-04-22 10:11:58 +0100

    Update libXft 2.3.9

--- TRIBx11-libxft/build.sh
+++ TRIBx11-libxft/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXft-2.3.8 -C "--disable-static --with-pic --localstatedir=/var"
-${THOME}/build/genpkg TRIBx11-libxft libXft-2.3.8
+${THOME}/build/dobuild -64 libXft-2.3.9 -C "--disable-static --with-pic --localstatedir=/var"
+${THOME}/build/genpkg TRIBx11-libxft libXft-2.3.9
