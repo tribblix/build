a562cf8ace3ed29c430c3559f963287fd1ed569b
Peter Tribble <peter.tribble@gmail.com>
2025-04-22 09:37:20 +0100

    Update numpy 2.2.5

--- TRIBnumpy-python-312/build.sh
+++ TRIBnumpy-python-312/build.sh
@@ -9,8 +9,6 @@
 #
 # version compatibility in numpy is somewhat unusual, in that you can't
 # build against numpy 1.x and run with 2.x, but you can build with 2.x
-# and run with 1.x; as a result I've pinned numpy back to 1.x for m35
-# and will rebuild everything against 2.x before bumping this package
-# back to 2.x
+# and run with 1.x
 #
-env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "-Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas -Csetup-args=-Dallow-noblas=false" TRIBnumpy-python-312 numpy-2.2.3
+env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "-Csetup-args=-Dblas=openblas -Csetup-args=-Dlapack=openblas -Csetup-args=-Dallow-noblas=false" TRIBnumpy-python-312 numpy-2.2.5
