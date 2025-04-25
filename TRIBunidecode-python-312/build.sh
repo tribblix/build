3c50b6e363d69d18b2292924eebd8912600cc4e2
Peter Tribble <peter.tribble@gmail.com>
2025-04-25 09:36:10 +0100

    Update Unidecode 1.4.0

--- TRIBunidecode-python-312/build.sh
+++ TRIBunidecode-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBunidecode-python-312 Unidecode-1.3.8
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBunidecode-python-312 Unidecode-1.4.0
