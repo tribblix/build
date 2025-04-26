59f31a1fa4a52c41706dd4ccdbe2806ebe1a48e9
Peter Tribble <peter.tribble@gmail.com>
2025-04-26 21:49:16 +0100

    Add tor 0.4.8.16

---
+++ TRIBtor/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# uses the same trick as memcached to build a static libevent 
+#
+${THOME}/build/dobuild -64only tor-0.4.8.16 -C "--enable-static-libevent --with-libevent-dir=/tmp/mc --sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBtor tor-0.4.8.16
+rm -fr /tmp/mc
