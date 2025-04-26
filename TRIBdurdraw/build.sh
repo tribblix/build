87852227711210b25700a83c10d26e2baa76ecc4
Peter Tribble <peter.tribble@gmail.com>
2025-04-26 10:46:54 +0100

    Update durdraw 0.29.0

--- TRIBdurdraw/build.sh
+++ TRIBdurdraw/build.sh
@@ -6,4 +6,4 @@
 #
 # only works in 3.12 due to the way it uses curses
 #
-${THOME}/build/pkg_setup_py -v 312 TRIBdurdraw durdraw-0.28.0
+${THOME}/build/pkg_pep518 -N -v 312 TRIBdurdraw durdraw-0.29.0
