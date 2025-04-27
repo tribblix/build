ff45a83c3a43600b481e25ab919a3037e5e09d3d
Peter Tribble <peter.tribble@gmail.com>
2025-04-27 10:04:12 +0100

    Update patsy 1.0.1

--- TRIBpatsy-python-312/build.sh
+++ TRIBpatsy-python-312/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # consumer is statsmodels which requires scipy
 # hence this not available on sparc
 #
-${THOME}/build/pkg_setup_py TRIBpatsy-python-312 patsy-0.5.6
+${THOME}/build/pkg_setup_py TRIBpatsy-python-312 patsy-1.0.1
