ad7c744d86491042c8f7dd20e49c9e0a93c207fb
Peter Tribble <peter.tribble@gmail.com>
2025-04-29 20:09:53 +0100

    Update sbom2dot 0.3.2

--- TRIBsbom2dot-python-312/build.sh
+++ TRIBsbom2dot-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_wheel TRIBsbom2dot-python-312 sbom2dot-0.3.1-py2.py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBsbom2dot-python-312 sbom2dot-0.3.2-py2.py3-none-any.whl
