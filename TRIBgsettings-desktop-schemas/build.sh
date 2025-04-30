7336b0cd0b45c3c2be142fdc5af7f9fa821bcdff
Peter Tribble <peter.tribble@gmail.com>
2025-04-30 10:02:48 +0100

    Update gsettings-desktop-schemas 3.38.0, and fix rc script

--- TRIBgsettings-desktop-schemas/build.sh
+++ TRIBgsettings-desktop-schemas/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needed for evince, mate etc
 #
-${THOME}/build/dobuild -gnu gsettings-desktop-schemas-3.6.1
-${THOME}/build/genpkg TRIBgsettings-desktop-schemas gsettings-desktop-schemas-3.6.1
+${THOME}/build/mesonbuild -gnu gsettings-desktop-schemas-3.38.0 -C -Dintrospection=false
+${THOME}/build/genpkg TRIBgsettings-desktop-schemas gsettings-desktop-schemas-3.38.0
