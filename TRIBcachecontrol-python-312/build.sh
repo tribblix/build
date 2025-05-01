a1e3c7ae32f2e86d0c5fb65dd703503413122711
Peter Tribble <peter.tribble@gmail.com>
2025-05-01 08:54:51 +0100

    Update CacheControl 0.14.3

--- TRIBcachecontrol-python-312/build.sh
+++ TRIBcachecontrol-python-312/build.sh
@@ -3,4 +3,4 @@
 # pip-audit requires CacheControl[filecache] which adds filelock
 # as a dependency
 #
-${THOME}/build/pkg_pep518 -N TRIBcachecontrol-python-312 cachecontrol-0.14.2
+${THOME}/build/pkg_pep518 -N TRIBcachecontrol-python-312 cachecontrol-0.14.3
