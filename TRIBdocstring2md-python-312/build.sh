05e56f399535961dc1aef5158f99ed6d5fb7d9c6
Peter Tribble <peter.tribble@gmail.com>
2025-05-03 11:49:01 +0100

    Udpate docstring-to-markdown 0.17

--- TRIBdocstring2md-python-312/build.sh
+++ TRIBdocstring2md-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBdocstring2md-python-312 docstring_to_markdown-0.16
+${THOME}/build/pkg_pep518 -N TRIBdocstring2md-python-312 docstring_to_markdown-0.17
