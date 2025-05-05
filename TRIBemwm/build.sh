d6c6ac883ffddc1ceb09fc3d02479004819628e8
Peter Tribble <peter.tribble@gmail.com>
2025-05-05 13:36:41 +0100

    Update emwm 1.3

--- TRIBemwm/build.sh
+++ TRIBemwm/build.sh
@@ -4,6 +4,6 @@
 #
 # the Makefile is patched to enable 64-bit
 #
-$THOME/build/dobuild emwm-src-1.2
-$THOME/build/dobuild emwm-utils-src-1.2
-${THOME}/build/genpkg TRIBemwm emwm-src-1.2 emwm-utils-src-1.2
+$THOME/build/dobuild emwm-src-1.3
+$THOME/build/dobuild emwm-utils-src-1.3
+${THOME}/build/genpkg TRIBemwm emwm-src-1.3 emwm-utils-src-1.3
