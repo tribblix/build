a3dbc6753ba8a7c432c7476f76fbbaaf5bc11da8
Peter Tribble <peter.tribble@gmail.com>
2025-05-05 13:29:01 +0100

    Update IceWM 3.7.4

--- TRIBicewm-themes/build.sh
+++ TRIBicewm-themes/build.sh
@@ -5,6 +5,4 @@
 # the version number in pkginfo is made up, the old repo was 1.2
 # and the new repo doesn't have any numbering
 #
-#${THOME}/build/dobuild -64only icewm-extra
-#${THOME}/build/genpkg TRIBicewm-themes icewm-extra
 ${THOME}/build/pkg_tarball -P usr/share/icewm TRIBicewm-themes icewm-extra
