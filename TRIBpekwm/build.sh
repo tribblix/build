094db376a8b2e55a60821d9feec7685cbdade881
Peter Tribble <peter.tribble@gmail.com>
2025-05-05 13:43:28 +0100

    Update pekwm 0.3.2

--- TRIBpekwm/build.sh
+++ TRIBpekwm/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/cmbuild -64only pekwm-0.3.0
-${THOME}/build/genpkg TRIBpekwm pekwm-0.3.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild -64only pekwm-release-0.3.2
+${THOME}/build/genpkg TRIBpekwm pekwm-release-0.3.2
