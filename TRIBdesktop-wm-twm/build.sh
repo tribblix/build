34d84dc5ac8110e0afde62b3ae691e306059ff89
Peter Tribble <peter.tribble@gmail.com>
2025-05-05 11:37:05 +0100

    Update twm 1.0.13

--- TRIBdesktop-wm-twm/build.sh
+++ TRIBdesktop-wm-twm/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only twm-1.0.12
-${THOME}/build/genpkg TRIBdesktop-wm-twm twm-1.0.12
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only twm-1.0.13
+${THOME}/build/genpkg TRIBdesktop-wm-twm twm-1.0.13
