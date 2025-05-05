3c5ff8f832092d13e8935604c01bd31d53c7f033
Peter Tribble <peter.tribble@gmail.com>
2025-05-05 11:00:13 +0100

    Update perl PPIx::Utils 0.004

--- TRIBlib-perl-5-ppix-utils/build.sh
+++ TRIBlib-perl-5-ppix-utils/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack PPIx-Utils-0.003
-cd PPIx-Utils-0.003
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack PPIx-Utils-0.004
+cd PPIx-Utils-0.004
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-ppix-utils PPIx-Utils-0.003
+${THOME}/build/genpkg TRIBlib-perl-5-ppix-utils PPIx-Utils-0.004
