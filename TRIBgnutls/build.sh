6d841a9f66355e5cc3a78b0d0e0e706cae288d1f
Peter Tribble <peter.tribble@gmail.com>
2025-05-13 20:35:56 +0100

    Drop 32-bit gnutls

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# gnutls, now 64-bit
+# SPDX-License-Identifier: CDDL-1.0
 #
 # disable tpm because the code isn't even self-consistent and autoconf
 # can't work out what to do on its own
 #
-${THOME}/build/dobuild -64 gnutls-3.6.16 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs"
+${THOME}/build/dobuild -64only gnutls-3.6.16 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs"
 ${THOME}/build/genpkg TRIBgnutls gnutls-3.6.16
