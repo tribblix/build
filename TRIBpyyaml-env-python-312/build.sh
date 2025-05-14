5dc1634c7c59157632c238be1f5bca7b8d5eef80
Peter Tribble <peter.tribble@gmail.com>
2025-05-14 12:46:33 +0100

    Update pyyaml_env_tag 1.1

--- TRIBpyyaml-env-python-312/build.sh
+++ TRIBpyyaml-env-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpyyaml-env-python-312 pyyaml_env_tag-1.0
+${THOME}/build/pkg_pep518 -N TRIBpyyaml-env-python-312 pyyaml_env_tag-1.1
