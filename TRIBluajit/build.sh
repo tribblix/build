8d0221a94350c7fa5d36fdb0dfc1c24e94bac19c
Peter Tribble <peter.tribble@gmail.com>
2025-05-15 14:00:58 +0100

    Drop unused luajit

--- TRIBluajit/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# documentation says to always build from current git
-#
-${THOME}/build/dobuild -64only https://github.com/LuaJIT/LuaJIT
-${THOME}/build/genpkg TRIBluajit LuaJIT
