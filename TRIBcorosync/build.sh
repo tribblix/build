698c278fd8aa580f55fdc19986ddfffbaa0216f2
Peter Tribble <peter.tribble@gmail.com>
2025-05-15 14:03:06 +0100

    Drop old and unused corsync

--- TRIBcorosync/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# postconf script to fix up use of the wrong awk
-#
-${THOME}/build/dobuild -64only corosync-2.4.6 -C "--localstatedir=/var --sysconfdir=/etc"
-${THOME}/build/genpkg TRIBcorosync corosync-2.4.6
