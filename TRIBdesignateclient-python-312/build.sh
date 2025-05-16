b93e28806dba5b30988a65dd201de4001f3cc4a7
Peter Tribble <peter.tribble@gmail.com>
2025-05-16 20:11:11 +0100

    Update python-designateclient 6.3.0

--- TRIBdesignateclient-python-312/build.sh
+++ TRIBdesignateclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBdesignateclient-python-312 python_designateclient-6.2.0
+${THOME}/build/pkg_setup_py TRIBdesignateclient-python-312 python_designateclient-6.3.0
