1b6f0b95381130f44c671e7807ae53c56c6b2d44
Peter Tribble <peter.tribble@gmail.com>
2025-05-17 19:55:57 +0100

    Update oslo.context 6.0.0

--- TRIBoslo-context-python-312/build.sh
+++ TRIBoslo-context-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-context-python-312 oslo_context-5.7.1
+${THOME}/build/pkg_setup_py TRIBoslo-context-python-312 oslo_context-6.0.0
