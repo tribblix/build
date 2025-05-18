5da9ea38f38376d7fc8294cee816b6d40839a190
Peter Tribble <peter.tribble@gmail.com>
2025-05-18 20:21:17 +0100

    Update sessreg 1.1.4

--- TRIBx11-session-sessreg/build.sh
+++ TRIBx11-session-sessreg/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only sessreg-1.1.3
-${THOME}/build/genpkg TRIBx11-session-sessreg sessreg-1.1.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only sessreg-1.1.4
+${THOME}/build/genpkg TRIBx11-session-sessreg sessreg-1.1.4
