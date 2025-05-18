a55222c8d192fb56382a6f6707e76d791cc42a38
Peter Tribble <peter.tribble@gmail.com>
2025-05-18 20:50:38 +0100

    Update vorbis-tools 1.4.3

--- TRIBvorbis-tools/build.sh
+++ TRIBvorbis-tools/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only vorbis-tools-1.4.2
-${THOME}/build/genpkg TRIBvorbis-tools vorbis-tools-1.4.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only vorbis-tools-1.4.3
+${THOME}/build/genpkg TRIBvorbis-tools vorbis-tools-1.4.3
