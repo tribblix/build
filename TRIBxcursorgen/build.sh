8f783cb57dfa31463b855c509789db2f91df5e6a
Peter Tribble <peter.tribble@gmail.com>
2025-05-26 13:33:59 +0100

    Update xcursorgen 1.0.9

--- TRIBxcursorgen/build.sh
+++ TRIBxcursorgen/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # needed to build xcursor-themes
 #
-${THOME}/build/dobuild -64only xcursorgen-1.0.8
-${THOME}/build/genpkg TRIBxcursorgen xcursorgen-1.0.8
+${THOME}/build/mesonbuild -64only xcursorgen-1.0.9
+${THOME}/build/genpkg TRIBxcursorgen xcursorgen-1.0.9
