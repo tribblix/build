edbdeefaacf2361320a81643a30daf49e73ad9c7
Peter Tribble <peter.tribble@gmail.com>
2025-05-26 10:06:56 +0100

    Update python-openstackclient 8.1.0

--- TRIBopenstackclient-python-312/build.sh
+++ TRIBopenstackclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBopenstackclient-python-312 python_openstackclient-8.0.0
+${THOME}/build/pkg_setup_py TRIBopenstackclient-python-312 python_openstackclient-8.1.0
