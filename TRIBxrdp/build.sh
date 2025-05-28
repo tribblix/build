ffc74bfae256f52ef58313836cd328f7b3cf2cb4
Peter Tribble <peter.tribble@gmail.com>
2025-05-28 11:55:49 +0100

    Update xrdp 0.10.3

--- TRIBxrdp/build.sh
+++ TRIBxrdp/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS $THOME/build/dobuild +64only xrdp-0.9.26 -C LIBS=-lsocket
-${THOME}/build/genpkg TRIBxrdp xrdp-0.9.26
+env TRIBBLIX_CFLAGS="-D_XPG4_2 -D_POSIX_PTHREAD_SEMANTICS -D__EXTENSIONS__" $THOME/build/dobuild +64only xrdp-0.10.3 -C LIBS=-lsocket
+${THOME}/build/genpkg TRIBxrdp xrdp-0.10.3
