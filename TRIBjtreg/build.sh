c65ea043294244a1aec598d8726dfd113065e358
Peter Tribble <peter.tribble@gmail.com>
2025-05-30 14:36:12 +0100

    Update jtreg 7.5.2

--- TRIBjtreg/build.sh
+++ TRIBjtreg/build.sh
@@ -2,10 +2,15 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# we want to ship a version that will be valid for testing all versions
+# of openjdk, with emphasis on mainline
+#
+# look for JTREG_MINIMUM_VERSION in make/autoconf/lib-tests.m4
+#
 
 git clone https://github.com/openjdk/jtreg
 cd jtreg
-git checkout jtreg-7.5.1+1
+git checkout jtreg-7.5.2+1
 
 env PATH=/usr/gnu/bin:$PATH bash make/build.sh --jdk /usr/jdk/instances/jdk11
 
