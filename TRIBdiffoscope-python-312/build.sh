770bf9580bfd5b975078bf977a764554a3b89dd5
Peter Tribble <peter.tribble@gmail.com>
2025-05-31 09:33:14 +0100

    Update diffoscope 297

--- TRIBdiffoscope-python-312/build.sh
+++ TRIBdiffoscope-python-312/build.sh
@@ -8,4 +8,4 @@
 # [cmdline] argcomplete progressbar
 # also pypdf defusedxml distro python-debian
 #
-${THOME}/build/pkg_setup_py TRIBdiffoscope-python-312 diffoscope-296
+${THOME}/build/pkg_setup_py TRIBdiffoscope-python-312 diffoscope-297
