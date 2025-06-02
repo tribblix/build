d18f0de6a913c7ced30ff7051e08bc087c1f3d30
Peter Tribble <peter.tribble@gmail.com>
2025-06-02 10:30:03 +0100

    Update libppd 2.1.1

--- TRIBlibppd/build.sh
+++ TRIBlibppd/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
-
-$THOME/build/dobuild -64only libppd-2.0.0
-${THOME}/build/genpkg TRIBlibppd libppd-2.0.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+$THOME/build/dobuild -64only libppd-2.1.1
+${THOME}/build/genpkg TRIBlibppd libppd-2.1.1
