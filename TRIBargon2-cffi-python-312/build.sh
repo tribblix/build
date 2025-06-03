3d760878915903592b0cb97d535d4a499c464ad5
Peter Tribble <peter.tribble@gmail.com>
2025-06-03 08:36:05 +0100

    Update argon2-cffi 25.1.0

--- TRIBargon2-cffi-python-312/build.sh
+++ TRIBargon2-cffi-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_pep518 TRIBargon2-cffi-python-312 argon2_cffi-23.1.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBargon2-cffi-python-312 argon2_cffi-25.1.0
