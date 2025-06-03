5c5cb9317a07526e778b277df55a43c697103507
Peter Tribble <peter.tribble@gmail.com>
2025-06-03 08:21:53 +0100

    Update uritemplate 4.2.0

--- TRIBuritemplate-python-312/build.sh
+++ TRIBuritemplate-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBuritemplate-python-312 uritemplate-4.1.1
+${THOME}/build/pkg_setup_py TRIBuritemplate-python-312 uritemplate-4.2.0
