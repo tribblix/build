0421502bfb2f649f1f7a37909e5a698f15f5bb9d
Peter Tribble <peter.tribble@gmail.com>
2025-06-05 18:13:46 +0100

    Update osv-scanner v2.0.2

--- TRIBosv-scanner/build.sh
+++ TRIBosv-scanner/build.sh
@@ -2,14 +2,14 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env GOPATH=`pwd` PATH=/usr/versions/go-1.23/bin:$PATH go install github.com/google/osv-scanner/cmd/osv-scanner@v1
+env GOPATH=`pwd` PATH=/usr/versions/go-1.24/bin:$PATH go install github.com/google/osv-scanner/v2/cmd/osv-scanner@latest
 
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/osv-scanner
 
 cp bin/osv-scanner /tmp/cs/usr/bin
-cp pkg/mod/github.com/google/osv-scanner@v1*/README.md pkg/mod/github.com/google/osv-scanner@v1*/LICENSE /tmp/cs/usr/share/osv-scanner
+cp pkg/mod/github.com/google/osv-scanner/v2@*/README.md pkg/mod/github.com/google/osv-scanner/v2@*/LICENSE /tmp/cs/usr/share/osv-scanner
 ${THOME}/build/create_pkg TRIBosv-scanner /tmp/cs
 
 rm -fr /tmp/cs
