e58a065d55c35c138e302fc47f318ccf9e390c97
Peter Tribble <peter.tribble@gmail.com>
2025-06-05 08:18:38 +0100

    Update IO::Socket::SSL 2.090

--- TRIBlib-perl-5-io-socket-ssl/build.sh
+++ TRIBlib-perl-5-io-socket-ssl/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack IO-Socket-SSL-2.089
-cd IO-Socket-SSL-2.089
+${THOME}/build/unpack IO-Socket-SSL-2.090
+cd IO-Socket-SSL-2.090
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-io-socket-ssl IO-Socket-SSL-2.089
+${THOME}/build/genpkg TRIBlib-perl-5-io-socket-ssl IO-Socket-SSL-2.090
