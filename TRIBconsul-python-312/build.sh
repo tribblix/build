71c39120abb5e911d49df8b79c99afdcb0f0bbf7
Peter Tribble <peter.tribble@gmail.com>
2025-06-07 09:43:21 +0100

    Update patroni 4.0.6

--- TRIBconsul-python-312/build.sh
+++ TRIBconsul-python-312/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBconsul-python-312 python-consul-1.1.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+# this used to be python-consul, but that's now unmaintained
+# switched to the maintained replacement py-consul
+#
+${THOME}/build/pkg_setup_py TRIBconsul-python-312 py_consul-1.6.0
