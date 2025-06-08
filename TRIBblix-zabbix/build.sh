dd23af3b460aefb045419699b750a084b39fd3b6
Peter Tribble <peter.tribble@gmail.com>
2025-06-08 21:00:07 +0100

    Update Zabbix 6.0.40

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -6,6 +6,6 @@
 # avoid tying it to the normal preconf as it's not relevant for the agent
 #
 ${THOME}/build/patches/zabbix-server.preconf
-${THOME}/build/dobuild -64only zabbix-6.0.39 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres15/bin/pg_config --with-libpcre2 --with-libevent=/tmp/mc"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-6.0.39
+${THOME}/build/dobuild -64only zabbix-6.0.40 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres15/bin/pg_config --with-libpcre2 --with-libevent=/tmp/mc"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-6.0.40
 rm -fr /tmp/mc
