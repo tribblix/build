6303ed18d047ad32b58c54b58d0001776ca36568
Peter Tribble <peter.tribble@gmail.com>
2025-06-15 11:00:11 +0100

    Update vim 1.1457

--- TRIBeditor-vim-gtk2/build.sh
+++ TRIBeditor-vim-gtk2/build.sh
@@ -4,5 +4,5 @@
 #
 # the shared pieces are shipped in the TRIBeditor-vim package
 #
-env TRIBBLIX_CFLAGS=-std=gnu99 ${THOME}/build/dobuild +gnu -64only vim-9.1.1199 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix --disable-canberra" -M -k
-${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-9.1.1199
+env TRIBBLIX_CFLAGS=-std=gnu99 ${THOME}/build/dobuild +gnu -64only vim-9.1.1457 -C "--enable-gui=gtk2 --with-compiledby=ptribble@tribblix --disable-canberra" -M -k
+${THOME}/build/genpkg TRIBeditor-vim-gtk2 vim-9.1.1457
