26f6047cc0f5860a3ff7e2a85752c4fb42766cc4
Peter Tribble <peter.tribble@gmail.com>
2025-06-16 15:42:35 +0100

    Switch DBD-Pg to postgres17

--- TRIBlib-perl-5-dbd-pg/build.sh
+++ TRIBlib-perl-5-dbd-pg/build.sh
@@ -4,7 +4,7 @@
 #
 ${THOME}/build/unpack DBD-Pg-3.18.0
 cd DBD-Pg-3.18.0
-env POSTGRES_HOME=/opt/tribblix/postgres15 perl Makefile.PL
+env POSTGRES_HOME=/opt/tribblix/postgres17 perl Makefile.PL
 make
 cd ..
 ${THOME}/build/genpkg TRIBlib-perl-5-dbd-pg DBD-Pg-3.18.0
