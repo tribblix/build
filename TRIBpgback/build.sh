d72908c3dbe5647a90ac5a1e2e55a6a504e69a24
Peter Tribble <peter.tribble@gmail.com>
2025-06-16 15:23:36 +0100

    Switch pg_back to postgres17

--- TRIBpgback/build.sh
+++ TRIBpgback/build.sh
@@ -20,7 +20,7 @@ cp bin/pg_back /tmp/pgb/usr/bin
 #
 mkdir -p /tmp/pgb/etc/postgresql
 cp pkg/mod/github.com/orgrim/pg_back@v*/pg_back.conf /tmp/pgb/etc/postgresql
-sed -i 's:bin_directory =:bin_directory = /opt/tribblix/postgres15/bin:' /tmp/pgb/etc/postgresql/pg_back.conf
+sed -i 's:bin_directory =:bin_directory = /opt/tribblix/postgres17/bin:' /tmp/pgb/etc/postgresql/pg_back.conf
 
 ${THOME}/build/create_pkg TRIBpgback /tmp/pgb
 rm -fr /tmp/pgb
