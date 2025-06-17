d3a9ec2f9f030a61ea429f8b7abfc6d4d32ad03b
Peter Tribble <peter.tribble@gmail.com>
2025-06-17 09:46:32 +0100

    Drop unmaintained gixy

--- TRIBcached-property-python-312/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_setup_py TRIBcached-property-python-312 cached_property-2.0.1
--- TRIBconfigargparse-python-312/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_setup_py TRIBconfigargparse-python-312 configargparse-1.7.1
