6dd27b8088cc41c52e964d3761a7277b5671abb3
Peter Tribble <peter.tribble@gmail.com>
2025-06-19 12:12:19 +0100

    Update Ruby 3.4.4

--- TRIBv-ruby-3/build.sh
+++ TRIBv-ruby-3/build.sh
@@ -6,5 +6,7 @@
 # disable -fstack-protector otherwise miniruby fails to link with
 # multiply defined symbols
 #
-${THOME}/build/dobuild +64only ruby-3.3.8 -P /usr/versions/ruby-3 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
-${THOME}/build/genpkg TRIBv-ruby-3 ruby-3.3.8
+# NOTE: if you bump the minor version, update pkg_gem to match
+#
+${THOME}/build/dobuild +64only ruby-3.4.4 -P /usr/versions/ruby-3 -C "ac_cv_func_dl_iterate_phdr=no stack_protector=no" -L
+${THOME}/build/genpkg TRIBv-ruby-3 ruby-3.4.4
