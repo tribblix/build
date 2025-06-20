f228713cbbb14fe3d0e6df4962da68b8bbe5ca70
Peter Tribble <peter.tribble@gmail.com>
2025-06-20 17:08:29 +0100

    Add adbc-driver-sqlite

---
+++ TRIBadbc-sqlite-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# dependencies added for the [dbapi] extension
+#
+env ADBC_SQLITE_LIBRARY=/usr/lib/amd64/libadbc_driver_sqlite.so ${THOME}/build/pkg_setup_py TRIBadbc-sqlite-python-312 adbc_driver_sqlite-1.6.0
