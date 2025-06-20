126ae5c41f4e33cba912df82a94c157e48733abe
Peter Tribble <peter.tribble@gmail.com>
2025-06-20 14:12:22 +0100

    Drop xcoral, fails 64-bit

--- TRIBxcoral/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# need -J 1 otherwise word.h will be constructed after it's used
-#
-${THOME}/build/dobuild xcoral-4.02.7 -J 1
-$THOME/build/genpkg TRIBxcoral xcoral-4.02.7
