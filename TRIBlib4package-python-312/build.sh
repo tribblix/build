78f533e2d6751c71667c52d288d8b06361498e65
Peter Tribble <peter.tribble@gmail.com>
2025-06-22 09:52:27 +0100

    Update lib4package 0.3.3

--- TRIBlib4package-python-312/build.sh
+++ TRIBlib4package-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_wheel TRIBlib4package-python-312 lib4package-0.3.2-py2.py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBlib4package-python-312 lib4package-0.3.3-py2.py3-none-any.whl
