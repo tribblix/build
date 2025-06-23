4f05ab1a71958fb899eecf91295ef7bcfd1f741c
Peter Tribble <peter.tribble@gmail.com>
2025-06-23 09:39:19 +0100

    Update snowballstemmer 3.0.1

--- TRIBsnowballstemmer-python-312/build.sh
+++ TRIBsnowballstemmer-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBsnowballstemmer-python-312 snowballstemmer-2.2.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBsnowballstemmer-python-312 snowballstemmer-3.0.1
