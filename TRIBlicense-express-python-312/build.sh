80272d76836e85879f1127ed6c4e0a1e5062905f
Peter Tribble <peter.tribble@gmail.com>
2025-06-25 17:34:13 +0100

    Update license-expression 30.4.3

--- TRIBlicense-express-python-312/build.sh
+++ TRIBlicense-express-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBlicense-express-python-312 license_expression-30.4.1
+${THOME}/build/pkg_pep518 -N TRIBlicense-express-python-312 license_expression-30.4.3
