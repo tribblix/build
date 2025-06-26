d2e870eaf66ac430e3dea65905bc6aded329e672
Peter Tribble <peter.tribble@gmail.com>
2025-06-26 09:21:00 +0100

    Update sox to sox_ng 14.4.4

--- TRIBsox/build.sh
+++ TRIBsox/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only sox-14.4.2 -C "--disable-openmp --with-distro=Tribblix"
-${THOME}/build/genpkg TRIBsox sox-14.4.2
+# SPDX-License-Identifier: CDDL-1.0
+#
+env TRIBBLIX_CFLAGS=-I/usr/include/opus ${THOME}/build/dobuild -64only sox_ng-14.4.4 -C "--disable-openmp --with-distro=Tribblix"
+${THOME}/build/genpkg TRIBsox sox_ng-14.4.4
