a26850494a4351079c670d5cfe279fa3ee2013f3
Peter Tribble <peter.tribble@gmail.com>
2025-06-26 11:49:35 +0100

    Switch umlet to jdk21

--- TRIBv-umlet/build.sh
+++ TRIBv-umlet/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# note that different architectures are packaged separately as the java
+# dependency (and the path inside the wrapper) differs
+#
 ${THOME}/build/pkg_tarball TRIBv-umlet umlet-standalone-15.1
