3fcc9fb4579b348e9caba2f383a0b4eb12956799
Peter Tribble <peter.tribble@gmail.com>
2025-06-27 20:34:33 +0100

    Update libssh2 1.11.1

--- TRIBlibssh2/build.sh
+++ TRIBlibssh2/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-#
-${THOME}/build/dobuild -64 libssh2-1.11.0
-${THOME}/build/genpkg TRIBlibssh2 libssh2-1.11.0
+${THOME}/build/dobuild -64 libssh2-1.11.1
+${THOME}/build/genpkg TRIBlibssh2 libssh2-1.11.1
