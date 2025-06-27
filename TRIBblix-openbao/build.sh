62c56f2a69519d066368b4692f50423b2341c885
Peter Tribble <peter.tribble@gmail.com>
2025-06-27 18:09:57 +0100

    Update openbao 2.3.1

--- TRIBblix-openbao/build.sh
+++ TRIBblix-openbao/build.sh
@@ -6,15 +6,14 @@ mkdir ~/ud/v
 cd ~/ud/v
 git clone https://github.com/openbao/openbao.git
 cd openbao
-git checkout v2.1.1
+git checkout v2.3.1
 
 # needs gnu grep for the version check to work
 #
-# ~/go/pkg/mod/gotest.tools/gotestsum@v1.10.0/internal/filewatcher/term_unix.go
-# tcSet -> unix.TCSETS
-# tcGet -> unix.TCGETS
-env PATH=/usr/versions/go-1.23/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake bootstrap
-env PATH=/usr/versions/go-1.23/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake
+# bbolt 1.4.1 is broken, patch https://github.com/etcd-io/bbolt/pull/990
+#
+env PATH=/usr/versions/go-1.24/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake bootstrap
+env PATH=/usr/versions/go-1.24/bin:/usr/gnu/bin:${HOME}/go/bin:$PATH gmake
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/openbao/bin
