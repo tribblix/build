d5d0b1714974228148690a6a4a862f5775a3b6f9
Peter Tribble <peter.tribble@gmail.com>
2025-06-29 10:00:19 +0100

    Update beniget 0.4.2.post1

--- TRIBbeniget-python-312/build.sh
+++ TRIBbeniget-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBbeniget-python-312 beniget-0.4.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBbeniget-python-312 beniget-0.4.2.post1
