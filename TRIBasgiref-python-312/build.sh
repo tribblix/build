5b9d862a22e9734aa31b5c7c8887562c7853c758
Peter Tribble <peter.tribble@gmail.com>
2025-07-04 13:59:09 +0100

    Update asgiref 3.9.0

--- TRIBasgiref-python-312/build.sh
+++ TRIBasgiref-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBasgiref-python-312 asgiref-3.8.1
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBasgiref-python-312 asgiref-3.9.0
