dfc1bdefc1e7d66d4da41b737023ceadfdb862bd
Peter Tribble <peter.tribble@gmail.com>
2025-07-07 16:54:10 +0100

    Update libde265 1.0.16 libheif 1.20.1, and drop 32-bit

--- TRIBlibde265/build.sh
+++ TRIBlibde265/build.sh
@@ -4,5 +4,5 @@
 #
 # disable the example utilities, which would pull in libSDL and Qt
 #
-${THOME}/build/dobuild -64 libde265-1.0.15 -C "--disable-sherlock265 --disable-dec265"
-${THOME}/build/genpkg TRIBlibde265 libde265-1.0.15
+${THOME}/build/dobuild +64 libde265-1.0.16 -C "--disable-sherlock265 --disable-dec265"
+${THOME}/build/genpkg TRIBlibde265 libde265-1.0.16
