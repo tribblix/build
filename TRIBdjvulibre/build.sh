477acd341a25de7b3657582143d08620f11397e2
Peter Tribble <peter.tribble@gmail.com>
2025-07-07 13:49:54 +0100

    Update DjVuLibre 3.5.29

--- TRIBdjvulibre/build.sh
+++ TRIBdjvulibre/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only djvulibre-3.5.28
-${THOME}/build/genpkg TRIBdjvulibre djvulibre-3.5.28
+${THOME}/build/dobuild -64only djvulibre-3.5.29
+${THOME}/build/genpkg TRIBdjvulibre djvulibre-3.5.29
