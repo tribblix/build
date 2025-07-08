216a933c31d7d242419934c6331333be72253859
Peter Tribble <peter.tribble@gmail.com>
2025-07-08 20:41:00 +0100

    Update python adbc modules to 1.7.0

--- TRIBadbc-manager-python-312/build.sh
+++ TRIBadbc-manager-python-312/build.sh
@@ -2,7 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# required by the adbc postgresql driver
+# required by the adbc postgresql and sqlite drivers
 # dependencies added for the [dbapi] extension
 #
-${THOME}/build/pkg_setup_py TRIBadbc-manager-python-312 adbc_driver_manager-1.6.0
+# needs a patch. just like adbc itself
+#
+${THOME}/build/pkg_setup_py TRIBadbc-manager-python-312 adbc_driver_manager-1.7.0
---
+++ patches/adbc_driver_manager-1.7.0.pls
@@ -0,0 +1,1 @@
+-p1 adbc_driver_manager.patch
