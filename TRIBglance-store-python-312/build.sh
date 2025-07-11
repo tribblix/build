32edb572e3167ea90e6116d2e2545eac6846ab4e
Peter Tribble <peter.tribble@gmail.com>
2025-07-11 09:16:31 +0100

    Update glance_store 5.0.0

--- TRIBglance-store-python-312/build.sh
+++ TRIBglance-store-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # add dependencies for [swift]
 #
-${THOME}/build/pkg_setup_py TRIBglance-store-python-312 glance_store-4.10.0
+${THOME}/build/pkg_setup_py TRIBglance-store-python-312 glance_store-5.0.0
