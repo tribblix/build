bc41e4a937f0115f5fa01202cae4213714eaf968
Peter Tribble <peter.tribble@gmail.com>
2025-07-11 09:13:43 +0100

    Update Glances 4.3.3

--- TRIBglances-python-312/build.sh
+++ TRIBglances-python-312/build.sh
@@ -7,6 +7,6 @@
 #
 # the "ip" extra declares a dependency on netifaces2 which is
 # non-portable and doesn't work, but netifaces might actually
-# work
+# work well enough
 #
-${THOME}/build/pkg_pep518 -N TRIBglances-python-312 glances-4.3.2
+${THOME}/build/pkg_pep518 -N TRIBglances-python-312 glances-4.3.3
