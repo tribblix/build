24bf94ef86c9734d81ab240c20fdf38d3dc6a932
Peter Tribble <peter.tribble@gmail.com>
2025-07-12 09:22:11 +0100

    Update Perl Test::Differences 0.72

--- TRIBlib-perl-5-test-differences/build.sh
+++ TRIBlib-perl-5-test-differences/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Test-Differences-0.71
-cd Test-Differences-0.71
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Test-Differences-0.72
+cd Test-Differences-0.72
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-test-differences Test-Differences-0.71
+${THOME}/build/genpkg TRIBlib-perl-5-test-differences Test-Differences-0.72
