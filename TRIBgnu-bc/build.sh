1ddb91a46ac920291041879a5a00ba3f302e6b63
Peter Tribble <peter.tribble@gmail.com>
2025-07-14 18:58:01 +0100

    Update gnu bc 1.0.8.2

--- TRIBgnu-bc/build.sh
+++ TRIBgnu-bc/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only bc-1.08.1 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-bc bc-1.08.1
+${THOME}/build/dobuild -64only bc-1.08.2 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-bc bc-1.08.2
