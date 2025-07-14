3004ace32a5507392e341b0b3018803118ceff95
Peter Tribble <peter.tribble@gmail.com>
2025-07-14 20:40:47 +0100

    Update tcsh 6.24.16

--- TRIBshell-tcsh/build.sh
+++ TRIBshell-tcsh/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only tcsh-6.24.15
-${THOME}/build/genpkg TRIBshell-tcsh tcsh-6.24.15
+${THOME}/build/dobuild -64only tcsh-6.24.16
+${THOME}/build/genpkg TRIBshell-tcsh tcsh-6.24.16
