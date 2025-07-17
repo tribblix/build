13774b9bec6b41ab52e92f45ed81df8134e34bbe
Peter Tribble <peter.tribble@gmail.com>
2025-07-17 18:28:29 +0100

    Add a package for postgres 18

---
+++ TRIBblix-postgres18/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# postgres 18
+#
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres18 postgresql-18beta2 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres18 postgresql-18beta2
