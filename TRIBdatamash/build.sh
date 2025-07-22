561e72dbdeea2a6f3947763c1eb1aed41d93224e
Peter Tribble <peter.tribble@gmail.com>
2025-07-22 22:31:26 +0100

    Update datamash 1.9

--- TRIBdatamash/build.sh
+++ TRIBdatamash/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only datamash-1.8
-${THOME}/build/genpkg TRIBdatamash datamash-1.8
+${THOME}/build/dobuild -64only datamash-1.9
+${THOME}/build/genpkg TRIBdatamash datamash-1.9
