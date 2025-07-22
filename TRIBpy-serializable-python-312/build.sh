38d0a9b02b2be9422661522672e9503e7eba5397
Peter Tribble <peter.tribble@gmail.com>
2025-07-22 17:05:27 +0100

    Update py-serializable 2.1.0

--- TRIBpy-serializable-python-312/build.sh
+++ TRIBpy-serializable-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpy-serializable-python-312 py_serializable-2.0.0
+${THOME}/build/pkg_pep518 -N TRIBpy-serializable-python-312 py_serializable-2.1.0
