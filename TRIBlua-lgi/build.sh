022807328817ffe7b370f00ceae3c4191a109ef5
Peter Tribble <peter.tribble@gmail.com>
2025-07-24 10:13:37 +0100

    Drop 32-bit lua-lgi

--- TRIBlua-lgi/build.sh
+++ TRIBlua-lgi/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-env CC=gcc ${THOME}/build/dobuild lgi-0.9.2
-mv lgi-0.9.2 lgi-0.9.2-32bit
+# SPDX-License-Identifier: CDDL-1.0
+#
 env CC="gcc -m64" PKG_CONFIG_PATH=/usr/lib/`${THOME}/build/getarch`/pkgconfig ${THOME}/build/dobuild +64 lgi-0.9.2
-mv lgi-0.9.2-32bit lgi-0.9.2
 ${THOME}/build/genpkg TRIBlua-lgi lgi-0.9.2
