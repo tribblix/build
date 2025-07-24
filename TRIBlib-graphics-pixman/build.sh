d670ee69b20a136efc5cf82e395e1064e0a17a89
Peter Tribble <peter.tribble@gmail.com>
2025-07-24 10:00:43 +0100

    Update pixman 0.46.4

--- TRIBlib-graphics-pixman/build.sh
+++ TRIBlib-graphics-pixman/build.sh
@@ -5,5 +5,5 @@
 # disabling gtk is just for the tests, it doesn't change the
 # functionality
 #
-${THOME}/build/mesonbuild -64 pixman-0.46.0 -C "-Dgtk=disabled -Ddemos=disabled -Dopenmp=disabled"
-${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.46.0
+${THOME}/build/mesonbuild -64 pixman-0.46.4 -C "-Dgtk=disabled -Ddemos=disabled -Dopenmp=disabled"
+${THOME}/build/genpkg TRIBlib-graphics-pixman pixman-0.46.4
