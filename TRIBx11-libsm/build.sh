b62005c8d1af1bb3a6de7b9f0caa8fe1c6b4bee8
Peter Tribble <peter.tribble@gmail.com>
2025-07-24 10:24:14 +0100

    Update libSM 1.2.6

--- TRIBx11-libsm/build.sh
+++ TRIBx11-libsm/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libSM-1.2.3 -C "--enable-unix-transport --enable-tcp-transport --enable-ipv6 --without-libuuid"
-${THOME}/build/genpkg TRIBx11-libsm libSM-1.2.3
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 libSM-1.2.6 -C "--enable-unix-transport --enable-tcp-transport --enable-ipv6 --without-libuuid"
+${THOME}/build/genpkg TRIBx11-libsm libSM-1.2.6
