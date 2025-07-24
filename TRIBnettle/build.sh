4e2c95b9d9a2eef45ead419988021c9aeed6e5de
Peter Tribble <peter.tribble@gmail.com>
2025-07-24 09:51:00 +0100

    Update nettle 3.10.2, drop 32-bit

--- TRIBnettle/build.sh
+++ TRIBnettle/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 nettle-3.10.1 -C --disable-static
-${THOME}/build/genpkg TRIBnettle nettle-3.10.1
+${THOME}/build/dobuild -64only nettle-3.10.2 -C --disable-static
+${THOME}/build/genpkg TRIBnettle nettle-3.10.2
