82a92af870bd451f250cdf558b3adf74a55cc961
Peter Tribble <peter.tribble@gmail.com>
2025-07-25 16:01:04 +0100

    Add dnsviz

---
+++ TRIBdnsviz-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBdnsviz-python-312 dnsviz-0.11.1
---
+++ TRIBpygraphviz-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpygraphviz-python-312 pygraphviz-1.14
