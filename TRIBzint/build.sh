45d072a454db3905b27fa982a8122644a864d15d
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 18:55:01 +0100

    Update zint 2.15.0

--- TRIBzint/build.sh
+++ TRIBzint/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # build both the regular and QT packages together
 #
-env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only zint-2.13.0-src -C -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake
-${THOME}/build/genpkg TRIBzint zint-2.13.0-src
-${THOME}/build/genpkg TRIBzint-qt zint-2.13.0-src
+env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only zint-2.15.0-src -C "-DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
+${THOME}/build/genpkg TRIBzint zint-2.15.0-src
+${THOME}/build/genpkg TRIBzint-qt zint-2.15.0-src
