fa0b738a7a54a7ffca1a9a97fdf7c7405a0cf8ee
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 19:58:19 +0100

    Update gnu grep 3.12

--- TRIBtext-gnu-grep/build.sh
+++ TRIBtext-gnu-grep/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # ggrep
 # NB. Must not have gnu iconv installed
 #
-${THOME}/build/dobuild -64only -gnu grep-3.11 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-grep grep-3.11
+${THOME}/build/dobuild -64only -gnu grep-3.12 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-grep grep-3.12
