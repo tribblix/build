4d2b699ca915030c757af43d566a50f8042529e3
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 09:02:51 +0100

    Package jdk25

---
+++ TRIBopenjdk25/build.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# jdk has been migrated to git
+# go to https://github.com/openjdk/jdk/tags
+#
+
+cd ~/ud
+${THOME}/build/unpack jdk-jdk-25-33
+cd jdk-jdk-25-33
+
+#
+# jdk25 needs autoconf installed
+#
+zap install autoconf
+
+#
+# looks like dtrace is busted, illumos and Solaris have diverged
+# enough to trip it up, so --enable-dtrace=no
+#
+# LOCALE so locale detection fails and we correctly fall back to
+# the C locale, so that sort doesn't fail
+#
+# the gobjcopy stuff doesn't actually work, so disable it
+# --with-native-debug-symbols=none
+#
+# use jdk24 as the boot jdk
+#
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
+--enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk24 \
+--with-native-debug-symbols=none \
+--with-toolchain-type=gcc \
+--disable-dtrace \
+--disable-warnings-as-errors \
+--with-source-date=current \
+--with-jobs=4 \
+DATE=/usr/gnu/bin/date \
+LOCALE=/bin/true \
+STRIP=/usr/gnu/bin/strip
+
+env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake product-images
+
+#
+# first testing looks like this:
+#
+# cd build/solaris-x86_64-server-release/images/jdk
+# ./bin/java -version
+#
+# openjdk version "25-internal" 2025-09-16
+# OpenJDK Runtime Environment (build 25-internal-adhoc.ptribble.jdk-jdk-25-33)
+# OpenJDK 64-Bit Server VM (build 25-internal-adhoc.ptribble.jdk-jdk-25-33, mixed mode, sharing)
+#
+
+rm -fr /tmp/jdk
+mkdir -p /tmp/jdk/usr/versions/openjdk25
+mkdir -p /tmp/jdk/usr/jdk/instances
+(cd build/solaris-x86_64-server-release/images/jdk; tar cf - *) | ( cd /tmp/jdk/usr/versions/openjdk25 ; tar xf -)
+cd /tmp/jdk/usr/jdk/instances
+ln -s ../../versions/openjdk25 jdk25
+cd /tmp/jdk/usr/jdk
+ln -s ../versions/openjdk25 .
+#jdk25 is LTS, but don't make it the default yet
+
+#
+# need to create a certificate bundle
+#
+# the jdk25 build appears to create a working one, but it uses
+# a horribly outdated set of certificates, so replace it
+#
+# this actually needs to be a JKS keystore, not a PKCS12 keystore
+# https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1739631
+#
+${THOME}/build/patches/mkcacerts -f /etc/openssl/cacert.pem -o /tmp/cacerts -k /usr/jdk/instances/jdk1.8.0/bin/keytool -s /usr/bin/openssl
+#
+cp /tmp/cacerts /tmp/jdk/usr/versions/openjdk25/lib/security
+
+#
+# edit conf/security/sunpkcs11-solaris.cfg and add the following to disabledMechanisms
+#
+# # the following mechanisms are disabled due to lack of digest cloning support
+# # need to fix 6414899 first
+#   CKM_MD5
+#   CKM_SHA256
+#   CKM_SHA384
+#   CKM_SHA512
+#
+
+(cd /tmp/jdk/usr/versions/openjdk25/conf/security/ ; gpatch -p1 < ${THOME}/build/patches/sunpkcs11-solaris.cfg.patch)
+
+${THOME}/build/create_pkg TRIBopenjdk25 /tmp/jdk
+
+cd
+rm -fr /tmp/jdk
