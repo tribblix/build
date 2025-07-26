947a9b9c6243d0e2f831ce5ed27554b2ed2d1ef4
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 19:47:12 +0100

    Update xapian 1.4.29

--- TRIBxapian-core/build.sh
+++ TRIBxapian-core/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xapian-core-1.4.25
-${THOME}/build/genpkg TRIBxapian-core xapian-core-1.4.25
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only xapian-core-1.4.29
+${THOME}/build/genpkg TRIBxapian-core xapian-core-1.4.29
