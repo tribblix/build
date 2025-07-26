cff83cff44c0715ff98cd1a0b80353a99e400d68
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 18:34:03 +0100

    Update xauth 1.1.4

--- TRIBx11-session-xauth/build.sh
+++ TRIBx11-session-xauth/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # mkcookie built by the fixit script
 #
-${THOME}/build/dobuild -64only xauth-1.1.3
-${THOME}/build/genpkg TRIBx11-session-xauth xauth-1.1.3
+${THOME}/build/dobuild -64only xauth-1.1.4
+${THOME}/build/genpkg TRIBx11-session-xauth xauth-1.1.4
