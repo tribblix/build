03e3715b50d698c8f92900685b27c7e3d6f72e50
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 20:05:20 +0100

    Update libmicrohttpd 1.0.2

--- TRIBlibmicrohttpd/build.sh
+++ TRIBlibmicrohttpd/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only libmicrohttpd-0.9.77
-${THOME}/build/genpkg TRIBlibmicrohttpd libmicrohttpd-0.9.77
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only libmicrohttpd-1.0.2
+${THOME}/build/genpkg TRIBlibmicrohttpd libmicrohttpd-1.0.2
