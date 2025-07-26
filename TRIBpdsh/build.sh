c2f9fac31594b2225237664d3fb5c21b17128096
Peter Tribble <peter.tribble@gmail.com>
2025-07-26 20:22:41 +0100

    Update pdsh 2.35

--- TRIBpdsh/build.sh
+++ TRIBpdsh/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only pdsh-2.34 -C "--with-ssh --without-torgue --without-slurm --with-rcmd-rank-list=ssh,rsh,exec"
-${THOME}/build/genpkg TRIBpdsh pdsh-2.34
+${THOME}/build/dobuild -64only pdsh-2.35 -C "--with-ssh --without-torgue --without-slurm --with-rcmd-rank-list=ssh,rsh,exec"
+${THOME}/build/genpkg TRIBpdsh pdsh-2.35
