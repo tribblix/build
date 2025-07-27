2d8817e98598571f16c8ca509a1db8a0a5274bce
Peter Tribble <peter.tribble@gmail.com>
2025-07-27 09:28:32 +0100

    Update contourpy 1.3.3

--- TRIBcontourpy-python-312/build.sh
+++ TRIBcontourpy-python-312/build.sh
@@ -5,4 +5,4 @@
 # without build isolation fails; the build requirement on pybind11 is
 # incompatible with that for scipy
 #
-env CXX="g++ -m64" CC="gcc -m64" ${THOME}/build/pkg_pep518 TRIBcontourpy-python-312 contourpy-1.3.2
+env CXX="g++ -m64" CC="gcc -m64" ${THOME}/build/pkg_pep518 TRIBcontourpy-python-312 contourpy-1.3.3
