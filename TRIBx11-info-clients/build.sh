293b5a447f68109be3ab1c189fb06bfc1d9e242c
Peter Tribble <peter.tribble@gmail.com>
2025-07-27 09:21:04 +0100

    Update xdpyinfo 1.4.0 xdriinfo 1.0.8

--- TRIBx11-info-clients/build.sh
+++ TRIBx11-info-clients/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64only xdpyinfo-1.3.4
-${THOME}/build/dobuild -64only xdriinfo-1.0.7
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64only xdpyinfo-1.4.0
+${THOME}/build/dobuild -64only xdriinfo-1.0.8
 ${THOME}/build/dobuild -64only xlsatoms-1.1.4
 ${THOME}/build/dobuild -64only xlsclients-1.1.5
 ${THOME}/build/dobuild -64only xlsfonts-1.0.8
 ${THOME}/build/dobuild -64only xvinfo-1.1.5
 ${THOME}/build/dobuild -64only xwininfo-1.1.6 -C --with-xcb-errors
-${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.3.4 xdriinfo-1.0.7 xlsatoms-1.1.4 xlsclients-1.1.5 xlsfonts-1.0.8 xvinfo-1.1.5 xwininfo-1.1.6
+${THOME}/build/genpkg TRIBx11-info-clients xdpyinfo-1.4.0 xdriinfo-1.0.8 xlsatoms-1.1.4 xlsclients-1.1.5 xlsfonts-1.0.8 xvinfo-1.1.5 xwininfo-1.1.6
