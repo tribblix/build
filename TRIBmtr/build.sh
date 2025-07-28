3f7b6d4ba58291bb88bdb549c5aa75a27b6b8588
Peter Tribble <peter.tribble@gmail.com>
2025-07-28 09:47:04 +0100

    Update mtr 0.96

--- TRIBmtr/build.sh
+++ TRIBmtr/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env CPPFLAGS="-D BSD_COMP -D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild -64only mtr-0.95
-${THOME}/build/genpkg TRIBmtr mtr-0.95
+# you must use the tarball from github, not the download site
+#
+env CPPFLAGS="-D BSD_COMP -D_XPG4_2 -D__EXTENSIONS__" ${THOME}/build/dobuild -64only mtr-0.96
+${THOME}/build/genpkg TRIBmtr mtr-0.96
