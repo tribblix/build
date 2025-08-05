92420c8702450b4f96c8747924e97444e2b26220
Peter Tribble <peter.tribble@gmail.com>
2025-08-05 12:05:56 +0100

    Drop 32-bit libXfont2

--- TRIBx11-libxfont2/build.sh
+++ TRIBx11-libxfont2/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXfont2-2.0.7 +P /usr/lib/xorg -C "--enable-freetype --enable-fc --enable-builtins --with-bzip2"
+${THOME}/build/dobuild -64only libXfont2-2.0.7 +P /usr/lib/xorg -C "--enable-freetype --enable-fc --enable-builtins --with-bzip2"
 ${THOME}/build/genpkg TRIBx11-libxfont2 libXfont2-2.0.7
