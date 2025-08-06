e91e073078d3837a6ed05ca18ba8bd981d49706b
Peter Tribble <peter.tribble@gmail.com>
2025-08-06 19:29:34 +0100

    Update trove-classifiers 2025.8.6.13

--- TRIBtrove-class-python-312/build.sh
+++ TRIBtrove-class-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBtrove-class-python-312 trove_classifiers-2025.5.9.12
+${THOME}/build/pkg_pep518 TRIBtrove-class-python-312 trove_classifiers-2025.8.6.13
