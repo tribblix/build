dd562bef18678513c78431d69752b39d02f32126
Peter Tribble <peter.tribble@gmail.com>
2025-08-07 11:11:34 +0100

    Update nginx-prometheus-exporter 1.4.2

--- TRIBblix-promnginx/build.sh
+++ TRIBblix-promnginx/build.sh
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 
-$THOME/build/unpack nginx-prometheus-exporter-1.4.1
-cd nginx-prometheus-exporter-1.4.1
+$THOME/build/unpack nginx-prometheus-exporter-1.4.2
+cd nginx-prometheus-exporter-1.4.2
 env PATH=/usr/gnu/bin:$PATH gmake nginx-prometheus-exporter
 
 ${THOME}/build/genpkg TRIBblix-promnginx
