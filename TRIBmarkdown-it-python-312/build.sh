402322736878b983de10a41e20cb87096767b3ec
Peter Tribble <peter.tribble@gmail.com>
2025-08-12 09:50:30 +0100

    Update markdown-it-py 4.0.0

--- TRIBmarkdown-it-python-312/build.sh
+++ TRIBmarkdown-it-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBmarkdown-it-python-312 markdown-it-py-3.0.0
+${THOME}/build/pkg_pep518 -N TRIBmarkdown-it-python-312 markdown_it_py-4.0.0
