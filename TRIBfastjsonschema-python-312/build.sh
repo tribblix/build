ad0617481b8e21dba5d350ee6d9bbaa4180344d3
Peter Tribble <peter.tribble@gmail.com>
2025-08-15 10:29:48 +0100

    Update fastjsonschema 2.21.2

--- TRIBfastjsonschema-python-312/build.sh
+++ TRIBfastjsonschema-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBfastjsonschema-python-312 fastjsonschema-2.21.1
+${THOME}/build/pkg_setup_py TRIBfastjsonschema-python-312 fastjsonschema-2.21.2
