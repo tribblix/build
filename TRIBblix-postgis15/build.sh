eee3b3cde56990dc0745b2ee2612e8fdace24021
Peter Tribble <peter.tribble@gmail.com>
2025-08-15 13:15:58 +0100

    Update PostGIS 3.5.3

--- TRIBblix-postgis15/build.sh
+++ TRIBblix-postgis15/build.sh
@@ -8,5 +8,5 @@
 # installs into the same prefix as the postgres version it's built
 # against, and is versioned to match
 #
-$THOME/build/dobuild +64only postgis-3.5.2 -P /opt/tribblix/postgres15 -C "--with-pgconfig=/opt/tribblix/postgres15/bin/pg_config --sysconfdir=/etc"
-$THOME/build/genpkg TRIBblix-postgis15 postgis-3.5.2
+$THOME/build/dobuild +64only postgis-3.5.3 -P /opt/tribblix/postgres15 -C "--with-pgconfig=/opt/tribblix/postgres15/bin/pg_config --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-postgis15 postgis-3.5.3
--- TRIBblix-postgis17/build.sh
+++ TRIBblix-postgis17/build.sh
@@ -8,5 +8,5 @@
 # installs into the same prefix as the postgres version it's built
 # against, and is versioned to match
 #
-$THOME/build/dobuild +64only postgis-3.5.2 -P /opt/tribblix/postgres17 -C "--with-pgconfig=/opt/tribblix/postgres17/bin/pg_config --sysconfdir=/etc"
-$THOME/build/genpkg TRIBblix-postgis17 postgis-3.5.2
+$THOME/build/dobuild +64only postgis-3.5.3 -P /opt/tribblix/postgres17 -C "--with-pgconfig=/opt/tribblix/postgres17/bin/pg_config --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-postgis17 postgis-3.5.3
