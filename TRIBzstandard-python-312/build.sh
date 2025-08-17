3958eb612e9ed9fa9ea571ced129b9ce42310014
Peter Tribble <peter.tribble@gmail.com>
2025-08-17 20:28:29 +0100

    Update python zstandard 0.24.0

--- TRIBzstandard-python-312/build.sh
+++ TRIBzstandard-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBzstandard-python-312 zstandard-0.23.0
+${THOME}/build/pkg_setup_py TRIBzstandard-python-312 zstandard-0.24.0
