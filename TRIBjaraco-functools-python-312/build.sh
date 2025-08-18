6917139a527cac3ac0b9fd2dadbd62ad6ff51187
Peter Tribble <peter.tribble@gmail.com>
2025-08-18 21:44:12 +0100

    Update jaraco.functools 4.3.0

--- TRIBjaraco-functools-python-312/build.sh
+++ TRIBjaraco-functools-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBjaraco-functools-python-312 jaraco_functools-4.2.1
+${THOME}/build/pkg_pep518 TRIBjaraco-functools-python-312 jaraco_functools-4.3.0
