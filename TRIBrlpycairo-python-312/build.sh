38c8ff1f3f678bb4c518a04d299c6975324110d6
Peter Tribble <peter.tribble@gmail.com>
2025-08-18 10:15:02 +0100

    Update rlPyCairo 0.4.0

--- TRIBrlpycairo-python-312/build.sh
+++ TRIBrlpycairo-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_wheel TRIBrlpycairo-python-312 rlPyCairo-0.3.0-py3-none-any.whl
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_wheel TRIBrlpycairo-python-312 rlpycairo-0.4.0-py3-none-any.whl
