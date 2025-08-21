95042054ed59010fe7e7e0729581a0954a27a2ec
Peter Tribble <peter.tribble@gmail.com>
2025-08-21 19:33:14 +0100

    Update jupyter-server 2.17.0

--- TRIBjupyter-server-python-312/build.sh
+++ TRIBjupyter-server-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBjupyter-server-python-312 jupyter_server-2.16.0
+${THOME}/build/pkg_pep518 -N TRIBjupyter-server-python-312 jupyter_server-2.17.0
