c82544dbedee0a94e1ba12a87e32f399ef7c4d0a
Peter Tribble <peter.tribble@gmail.com>
2025-08-22 09:11:48 +0100

    Update types-python-dateutil 2.9.0.20250822

--- TRIBtypes-dateutil-python-312/build.sh
+++ TRIBtypes-dateutil-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBtypes-dateutil-python-312 types_python_dateutil-2.9.0.20250809
+${THOME}/build/pkg_pep518 -N TRIBtypes-dateutil-python-312 types_python_dateutil-2.9.0.20250822
