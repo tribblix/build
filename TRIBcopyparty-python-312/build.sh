e598c71c16d87ee6259abe1b99a4a91ae32792f5
Peter Tribble <peter.tribble@gmail.com>
2025-08-25 20:46:12 +0100

    Add copyparty 1.19.5

---
+++ TRIBcopyparty-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# dependencies include the extras [pwhash,audiotags,thumbnails,zeromq]
+#
+${THOME}/build/pkg_pep518 -N TRIBcopyparty-python-312 copyparty-1.19.5
