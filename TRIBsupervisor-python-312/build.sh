0a7a8b1c3d05fa33a10902c0c4909ebe7284a1dd
Peter Tribble <peter.tribble@gmail.com>
2025-08-26 11:10:41 +0100

    Update supervisord 4.3.0

--- TRIBsupervisor-python-312/build.sh
+++ TRIBsupervisor-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBsupervisor-python-312 supervisor-4.2.5
+${THOME}/build/pkg_setup_py TRIBsupervisor-python-312 supervisor-4.3.0
