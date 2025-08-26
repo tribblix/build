3b043138f24dd38158bdae27f6cd62cc0aa6f453
Peter Tribble <peter.tribble@gmail.com>
2025-08-26 11:00:37 +0100

    Update oslo.utils 9.1.0

--- TRIBoslo-utils-python-312/build.sh
+++ TRIBoslo-utils-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-utils-python-312 oslo_utils-9.0.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-utils-python-312 oslo_utils-9.1.0
