fb6405f845cf10a36d8ed84091b2cc1c2e54048d
Peter Tribble <peter.tribble@gmail.com>
2025-08-26 10:50:13 +0100

    Update oslo.reports 3.6.0

--- TRIBoslo-reports-python-312/build.sh
+++ TRIBoslo-reports-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-reports-python-312 oslo_reports-3.5.1
+${THOME}/build/pkg_pep518 -N TRIBoslo-reports-python-312 oslo_reports-3.6.0
