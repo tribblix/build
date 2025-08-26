b12bf1438e6fbf240520a0f23443bf087bb52157
Peter Tribble <peter.tribble@gmail.com>
2025-08-26 08:56:47 +0100

    Update typing_extensions 4.15.0

--- TRIBtyping-ext-python-312/build.sh
+++ TRIBtyping-ext-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBtyping-ext-python-312 typing_extensions-4.14.1
+${THOME}/build/pkg_pep518 -N TRIBtyping-ext-python-312 typing_extensions-4.15.0
