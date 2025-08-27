33f5c31dc17210cc351e2e0a433603df2742adfa
Peter Tribble <peter.tribble@gmail.com>
2025-08-27 11:57:14 +0100

    Update double-conversion 3.3.1, drop 32-bit

--- TRIBdouble-conversion/build.sh
+++ TRIBdouble-conversion/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-${THOME}/build/cmbuild -64 double-conversion-3.1.5 -C -DBUILD_SHARED_LIBS=ON
-${THOME}/build/genpkg TRIBdouble-conversion double-conversion-3.1.5
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/cmbuild +64 double-conversion-3.3.1 -C -DBUILD_SHARED_LIBS=ON
+${THOME}/build/genpkg TRIBdouble-conversion double-conversion-3.3.1
