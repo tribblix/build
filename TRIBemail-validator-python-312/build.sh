295ce5a5c498cf969e443b44a7782d63f51e1485
Peter Tribble <peter.tribble@gmail.com>
2025-08-27 09:21:35 +0100

    Update email_validator 2.3.0

--- TRIBemail-validator-python-312/build.sh
+++ TRIBemail-validator-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBemail-validator-python-312 email_validator-2.2.0
+${THOME}/build/pkg_pep518 TRIBemail-validator-python-312 email_validator-2.3.0
