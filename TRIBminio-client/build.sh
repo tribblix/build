81e74b9d157f1c32725107e6c34ab1a2e83924cd
Peter Tribble <peter.tribble@gmail.com>
2025-08-27 10:17:38 +0100

    Update minio client 2025.07.21

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -4,13 +4,16 @@
 #
 mkdir ~/ud/c
 cd ~/ud/c
-env GOPATH=`pwd` PATH=/usr/versions/go-1.23/bin:$PATH go install github.com/minio/mc@RELEASE.2025-03-12T17-29-24Z
+env GOPATH=`pwd` PATH=/usr/versions/go-1.25/bin:$PATH go install github.com/minio/mc@RELEASE.2025-07-21T05-28-08Z
 
+#
+# install under a different name as Midnight Commander is also called mc
+#
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
 mkdir -p /tmp/ee/usr/share/minio-client
 cp bin/mc /tmp/ee/usr/bin/minio-client
-cp pkg/mod/github.com/minio/mc@v0.0.0-20250312*/README.md /tmp/ee/usr/share/minio-client
-cp pkg/mod/github.com/minio/mc@v0.0.0-20250312*/LICENSE /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20250721*/README.md /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20250721*/LICENSE /tmp/ee/usr/share/minio-client
 ${THOME}/build/create_pkg TRIBminio-client /tmp/ee
 rm -fr /tmp/ee
