bbe16bd32a47b4201993576bb384d02ac8ae204e
Peter Tribble <peter.tribble@gmail.com>
2025-08-28 09:34:43 +0100

    Update jupyter-lsp 2.3.0

--- TRIBjupyter-lsp-python-312/build.sh
+++ TRIBjupyter-lsp-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBjupyter-lsp-python-312 jupyter_lsp-2.2.6
+${THOME}/build/pkg_setup_py TRIBjupyter-lsp-python-312 jupyter_lsp-2.3.0
