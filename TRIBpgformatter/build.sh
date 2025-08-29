591edc29b3199a3b12f05f0e60d7b6b3aa75b4dd
Peter Tribble <peter.tribble@gmail.com>
2025-08-29 12:42:34 +0100

    Update pgFormatter 5.7

--- TRIBpgformatter/build.sh
+++ TRIBpgformatter/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack pgFormatter-5.6
-cd pgFormatter-5.6
+${THOME}/build/unpack pgFormatter-5.7
+cd pgFormatter-5.7
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBpgformatter
