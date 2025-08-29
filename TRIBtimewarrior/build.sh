3def3f20432c8d270782e24c1aa605b7d03013a4
Peter Tribble <peter.tribble@gmail.com>
2025-08-29 13:31:04 +0100

    Update Timewarrior 1.9.1

--- TRIBtimewarrior/build.sh
+++ TRIBtimewarrior/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only timew-1.7.1
-${THOME}/build/genpkg TRIBtimewarrior timew-1.7.1
+${THOME}/build/cmbuild -64only timew-1.9.1
+${THOME}/build/genpkg TRIBtimewarrior timew-1.9.1
