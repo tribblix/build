0c179e2bf8c857351b4f58b55312a726432aa83d
Peter Tribble <peter.tribble@gmail.com>
2025-08-29 09:34:57 +0100

    Update python-barbicanclient 7.2.0

--- TRIBbarbicanclient-python-312/build.sh
+++ TRIBbarbicanclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBbarbicanclient-python-312 python_barbicanclient-7.1.0
+${THOME}/build/pkg_setup_py TRIBbarbicanclient-python-312 python_barbicanclient-7.2.0
