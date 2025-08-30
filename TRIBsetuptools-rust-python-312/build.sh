ecf3b9eee7d50f93ce2e8496d1de4eb4752b9c68
Peter Tribble <peter.tribble@gmail.com>
2025-08-30 09:32:01 +0100

    Update setuptools-rust 1.12.0

--- TRIBsetuptools-rust-python-312/build.sh
+++ TRIBsetuptools-rust-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # limited to x86 as rust is only present there
 #
-${THOME}/build/pkg_pep518 -N TRIBsetuptools-rust-python-312 setuptools_rust-1.11.1
+${THOME}/build/pkg_pep518 -N TRIBsetuptools-rust-python-312 setuptools_rust-1.12.0
