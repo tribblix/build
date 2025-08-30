45f83c64c7a8bc4ff220f93ddb7620ddb969920d
Peter Tribble <peter.tribble@gmail.com>
2025-08-30 10:09:02 +0100

    Update httplib2 0.30.0

--- TRIBhttplib2-python-312/build.sh
+++ TRIBhttplib2-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBhttplib2-python-312 httplib2-0.22.0
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBhttplib2-python-312 httplib2-0.30.0
