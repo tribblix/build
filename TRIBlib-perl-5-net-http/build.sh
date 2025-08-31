4b9c119cdb9baf3702aeb29d8fc674f87a4d0879
Peter Tribble <peter.tribble@gmail.com>
2025-08-31 10:03:20 +0100

    Update Net::HTTP 6.24

--- TRIBlib-perl-5-net-http/build.sh
+++ TRIBlib-perl-5-net-http/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Net-HTTP-6.23
-cd Net-HTTP-6.23
+${THOME}/build/unpack Net-HTTP-6.24
+cd Net-HTTP-6.24
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-net-http Net-HTTP-6.23
+${THOME}/build/genpkg TRIBlib-perl-5-net-http Net-HTTP-6.24
