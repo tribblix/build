bdce30cce172eb6ac0d3c586dc4b2d12974d48ac
Peter Tribble <peter.tribble@gmail.com>
2025-08-31 13:52:54 +0100

    Add pyproject-dependencies 1.3.2

---
+++ TRIBbuild-python-312/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# this is more internal, so inline pyproject_hooks
+#
+${THOME}/build/pkg_pep518 -N TRIBbuild-python-312 build-1.3.0 pyproject_hooks-1.2.0
---
+++ TRIBpyproject-deps-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpyproject-deps-python-312 pyproject_dependencies-1.3.2
