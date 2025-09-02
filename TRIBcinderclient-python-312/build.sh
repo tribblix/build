31f2ceabf6cba28235ef9e7d18509152b6491c73
Peter Tribble <peter.tribble@gmail.com>
2025-09-02 09:12:59 +0100

    Update python-cinderclient 9.8.0

--- TRIBcinderclient-python-312/build.sh
+++ TRIBcinderclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBcinderclient-python-312 python_cinderclient-9.7.0
+${THOME}/build/pkg_setup_py TRIBcinderclient-python-312 python_cinderclient-9.8.0
