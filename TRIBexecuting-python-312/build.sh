42c5795f20fdd79f9ed117fbaaa86e33c0b59f9b
Peter Tribble <peter.tribble@gmail.com>
2025-09-02 09:08:30 +0100

    Update executing 2.2.1

--- TRIBexecuting-python-312/build.sh
+++ TRIBexecuting-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBexecuting-python-312 executing-2.2.0
+${THOME}/build/pkg_pep518 -N TRIBexecuting-python-312 executing-2.2.1
