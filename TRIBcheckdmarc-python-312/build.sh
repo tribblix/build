f118448352cc7969609c2e86a4892d8b341d4080
Peter Tribble <peter.tribble@gmail.com>
2025-09-04 09:33:48 +0100

    Update checkdmarc 5.10.5

--- TRIBcheckdmarc-python-312/build.sh
+++ TRIBcheckdmarc-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBcheckdmarc-python-312 checkdmarc-5.10.3
+${THOME}/build/pkg_pep518 -N TRIBcheckdmarc-python-312 checkdmarc-5.10.5
