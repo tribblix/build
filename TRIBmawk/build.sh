e821d908cf7c218a555d893be2356d07474dcadd
Peter Tribble <peter.tribble@gmail.com>
2025-09-06 09:44:18 +0100

    Update mawk 1.3.4-20250131

--- TRIBmawk/build.sh
+++ TRIBmawk/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only mawk-1.3.4-20240905
-${THOME}/build/genpkg TRIBmawk mawk-1.3.4-20240905
+${THOME}/build/dobuild -64only mawk-1.3.4-20250131
+${THOME}/build/genpkg TRIBmawk mawk-1.3.4-20250131
