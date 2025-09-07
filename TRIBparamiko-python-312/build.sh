e0a8148afc69d29f1fd0850ae4e35009569a1b0a
Peter Tribble <peter.tribble@gmail.com>
2025-09-07 11:47:39 +0100

    Update paramiko 4.0.0

--- TRIBparamiko-python-312/build.sh
+++ TRIBparamiko-python-312/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# paramiko[all] adds invoke and pyasn1 over the fixed dependencies
-#
-${THOME}/build/pkg_setup_py TRIBparamiko-python-312 paramiko-3.5.1
+${THOME}/build/pkg_pep518 -N TRIBparamiko-python-312 paramiko-4.0.0
