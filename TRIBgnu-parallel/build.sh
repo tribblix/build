32a1d27e970ff8bb0259f922f8c5eca68955339b
Peter Tribble <peter.tribble@gmail.com>
2025-09-08 13:59:40 +0100

    Update gnu parallel 20250822

--- TRIBgnu-parallel/build.sh
+++ TRIBgnu-parallel/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild parallel-20250622 -P /usr/gnu
-${THOME}/build/genpkg TRIBgnu-parallel parallel-20250622
+${THOME}/build/dobuild parallel-20250822 -P /usr/gnu
+${THOME}/build/genpkg TRIBgnu-parallel parallel-20250822
