cf6fe93adfc9ada2966e806dd53824ff9d2a3869
Peter Tribble <peter.tribble@gmail.com>
2025-09-08 11:36:38 +0100

    Update libXScrnSaver 1.2.5

--- TRIBx11-libxscrnsaver/build.sh
+++ TRIBx11-libxscrnsaver/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libXScrnSaver-1.2.4
-${THOME}/build/genpkg TRIBx11-libxscrnsaver libXScrnSaver-1.2.4
+${THOME}/build/dobuild -64only libXScrnSaver-1.2.5
+${THOME}/build/genpkg TRIBx11-libxscrnsaver libXScrnSaver-1.2.5
