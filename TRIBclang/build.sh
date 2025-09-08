ad73e9c4c8a7ccf5abd6f386de560ea142be05a4
Peter Tribble <peter.tribble@gmail.com>
2025-09-08 10:12:05 +0100

    Rebuild llvm and clang 64-bit

--- TRIBclang/build.sh
+++ TRIBclang/build.sh
@@ -15,10 +15,10 @@ cd llvm-project-18.1.8.src/clang
 
 mkdir tribblix_build
 cd tribblix_build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGCC_INSTALL_PREFIX=/usr/versions/gcc-10 -DCLANG_VENDOR="Tribblix" -DCLANG_DEFAULT_RTLIB=libgcc -DCLANG_DEFAULT_CXX_STDLIB=libstdc++ -DLLVM_DIR=/usr/versions/llvm/lib/cmake/llvm -DLLVM_INCLUDE_TESTS=OFF ..
+env CXXFLAGS=-m64 CFLAGS=-m64 LDFLAGS=-m64 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGCC_INSTALL_PREFIX=/usr/versions/gcc-10 -DCLANG_VENDOR="Tribblix" -DCLANG_DEFAULT_RTLIB=libgcc -DCLANG_DEFAULT_CXX_STDLIB=libstdc++ -DLLVM_DIR=/usr/versions/llvm/lib/cmake/llvm -DLLVM_INCLUDE_TESTS=OFF ..
 # this will take a while...
 # need to find the llvm libraries at build time
-env LD_LIBRARY_PATH=/usr/versions/llvm/lib gmake -j 6
+env LD_LIBRARY_PATH=/usr/versions/llvm/lib gmake -j 4
 
 ${THOME}/build/genpkg TRIBclang
 cd ../..
--- TRIBllvm/build.sh
+++ TRIBllvm/build.sh
@@ -13,11 +13,20 @@
 ${THOME}/build/unpack llvm-project-18.1.8.src
 cd llvm-project-18.1.8.src/llvm
 
+#
+# need to disable assembly, as (a) it doesn't build properly, and
+# (b) symbol visibility is messed up - this only impacts BLAKE3
+#
 mkdir tribblix_build
 cd tribblix_build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_INSTALL_UTILS=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON ..
+env CXXFLAGS=-m64 CFLAGS=-m64 LDFLAGS=-m64 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/versions/llvm -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_INSTALL_UTILS=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_DISABLE_ASSEMBLY_FILES=ON ..
 # this will take a while...
-gmake -j 6
+#
+# while you want to make the parallelism as high as possible to shorten
+# build times, it's quite memory intensive, so the maximum effective
+# value is half your memory in GB, otherwise it will start swapping
+#
+gmake -j 4
 
 ${THOME}/build/genpkg TRIBllvm
 cd ../..
