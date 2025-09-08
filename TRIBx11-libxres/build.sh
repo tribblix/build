d386a90125f4c4da88d9aad4aebc44525ecc4395
Peter Tribble <peter.tribble@gmail.com>
2025-09-08 11:49:44 +0100

    Update libXres 1.2.3

--- TRIBx11-libxres/build.sh
+++ TRIBx11-libxres/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libXres-1.2.2
-${THOME}/build/genpkg TRIBx11-libxres libXres-1.2.2
+${THOME}/build/dobuild -64only libXres-1.2.3
+${THOME}/build/genpkg TRIBx11-libxres libXres-1.2.3
