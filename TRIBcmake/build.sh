f4170a1164490644ebf72aac43d52701a7c2a1d6
Peter Tribble <peter.tribble@gmail.com>
2025-09-09 15:32:14 +0100

    Update cmake 3.31.8

--- TRIBcmake/build.sh
+++ TRIBcmake/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack cmake-3.29.3
-cd cmake-3.29.3
-./bootstrap --prefix=/usr/versions/cmake-3.29 -- -DCMAKE_USE_OPENSSL=ON
+${THOME}/build/unpack cmake-3.31.8
+cd cmake-3.31.8
+./bootstrap --prefix=/usr/versions/cmake-3 -- -DCMAKE_USE_OPENSSL=ON
 gmake -j 4
 # [gmake test - all passed]
 ${THOME}/build/genpkg TRIBcmake
