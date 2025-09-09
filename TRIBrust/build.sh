466e290726456186a41d66079db358697e78ec72
Peter Tribble <peter.tribble@gmail.com>
2025-09-09 13:58:03 +0100

    Update rust 1.89.0

--- TRIBrust/build.sh
+++ TRIBrust/build.sh
@@ -5,8 +5,8 @@
 # we're now stable, so grab a stable build from
 # https://forge.rust-lang.org/infra/other-installation-methods.html
 #
-${THOME}/build/unpack rust-1.87.0-x86_64-unknown-illumos
-cd rust-1.87.0-x86_64-unknown-illumos
+${THOME}/build/unpack rust-1.89.0-x86_64-unknown-illumos
+cd rust-1.89.0-x86_64-unknown-illumos
 
 rm -fr /tmp/rrt
 bash ./install.sh --prefix=/usr/versions/rust --destdir=/tmp/rrt
