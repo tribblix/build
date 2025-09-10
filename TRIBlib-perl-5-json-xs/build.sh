437a3fc71035c214c94b1a631d02be26693a2467
Peter Tribble <peter.tribble@gmail.com>
2025-09-10 11:44:47 +0100

    Update JSON::XS 4.04

--- TRIBlib-perl-5-json-xs/build.sh
+++ TRIBlib-perl-5-json-xs/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack JSON-XS-4.03
-cd JSON-XS-4.03
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack JSON-XS-4.04
+cd JSON-XS-4.04
 env PERL_CANARY_STABILITY_NOPROMPT=1 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-json-xs JSON-XS-4.03
+${THOME}/build/genpkg TRIBlib-perl-5-json-xs JSON-XS-4.04
