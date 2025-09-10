a5ab6f0f4a5b3f9cbfcc494d6e64417377bde41c
Peter Tribble <peter.tribble@gmail.com>
2025-09-10 11:52:21 +0100

    Update pycparser 2.23

--- TRIBpycparser-python-312/build.sh
+++ TRIBpycparser-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBpycparser-python-312 pycparser-2.22
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBpycparser-python-312 pycparser-2.23
