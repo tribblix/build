ae586aa995df75f7cf19b3a2087fa51353b34842
Peter Tribble <peter.tribble@gmail.com>
2025-09-11 12:01:28 +0100

    Update pctlife

--- TRIBpctlife/build.sh
+++ TRIBpctlife/build.sh
@@ -4,12 +4,12 @@
 #
 # in a pctlife checkout
 #
-./life package
+./build package
 #
 #  *** make sure the pkg version matches that here ***
 #
 # then copy the package into our own format
 #
 mkdir -p /tmp/pct
-mv /tmp/TRIBpctlife.pkg /tmp/pct/TRIBpctlife.1.7.0.pkg
-${THOME}/build/pkg2zap /tmp/pct/TRIBpctlife.1.7.0.pkg /tmp/pct
+mv /tmp/TRIBpctlife.pkg /tmp/pct/TRIBpctlife.1.8.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBpctlife.1.8.0.pkg /tmp/pct
