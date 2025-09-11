cffee84ffab7403bc9177e6fe568a33879c5fdbd
Peter Tribble <peter.tribble@gmail.com>
2025-09-11 19:40:04 +0100

    Update gutenprint 5.3.5

--- TRIBgutenprint/build.sh
+++ TRIBgutenprint/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 gutenprint-5.3.4
-$THOME/build/genpkg TRIBgutenprint gutenprint-5.3.4
+${THOME}/build/dobuild -64only gutenprint-5.3.5
+$THOME/build/genpkg TRIBgutenprint gutenprint-5.3.5
