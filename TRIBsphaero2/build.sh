772a7c0d3403433f0531d532ec0c6baa17c3de3a
Peter Tribble <peter.tribble@gmail.com>
2025-09-11 12:13:05 +0100

    Update sphaero2

--- TRIBsphaero2/build.sh
+++ TRIBsphaero2/build.sh
@@ -11,5 +11,5 @@
 # then copy the package into our own format
 #
 mkdir -p /tmp/pct
-mv /tmp/TRIBsphaero2.pkg /tmp/pct/TRIBsphaero2.1.8.0.pkg
-${THOME}/build/pkg2zap /tmp/pct/TRIBsphaero2.1.8.0.pkg /tmp/pct
+mv /tmp/TRIBsphaero2.pkg /tmp/pct/TRIBsphaero2.1.9.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBsphaero2.1.9.0.pkg /tmp/pct
