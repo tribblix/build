3ba80e3aeeff4ec97c799d54cefbcd858da5fce8
Peter Tribble <peter.tribble@gmail.com>
2025-09-11 10:38:24 +0100

    Update feedparser 6.0.12

--- TRIBfeedparser-python-312/build.sh
+++ TRIBfeedparser-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBfeedparser-python-312 feedparser-6.0.11
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBfeedparser-python-312 feedparser-6.0.12
