b43b4362d1746005492e2059d9299da3bdbc1f2e
Peter Tribble <peter.tribble@gmail.com>
2025-09-11 16:43:47 +0100

    Update atril 1.28.1

--- TRIBmate-atril/build.sh
+++ TRIBmate-atril/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only +gnu atril-1.28.0 -C --without-keyring
-${THOME}/build/genpkg TRIBmate-atril atril-1.28.0
+${THOME}/build/dobuild -64only +gnu atril-1.28.1 -C --without-keyring
+${THOME}/build/genpkg TRIBmate-atril atril-1.28.1
