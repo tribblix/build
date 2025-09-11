ad0eec9f0ba3def706095780fa2ba9ba0a9bfad3
Peter Tribble <peter.tribble@gmail.com>
2025-09-11 12:08:42 +0100

    Update jattack

--- TRIBjattack/build.sh
+++ TRIBjattack/build.sh
@@ -4,12 +4,12 @@
 #
 # in jattack
 #
-./jattack package
+./build package
 #
 #  *** make sure the pkg version matches that here ***
 #
 # then copy the package into our own format
 #
 mkdir -p /tmp/pct
-mv /tmp/TRIBjattack.pkg /tmp/pct/TRIBjattack.1.6.0.pkg
-${THOME}/build/pkg2zap /tmp/pct/TRIBjattack.1.6.0.pkg /tmp/pct
+mv /tmp/TRIBjattack.pkg /tmp/pct/TRIBjattack.1.7.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBjattack.1.7.0.pkg /tmp/pct
