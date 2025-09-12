f27ad50b7609e63300e1166d79465280ac6ccc01
Peter Tribble <peter.tribble@gmail.com>
2025-09-12 09:47:22 +0100

    Update zope.interface 8.0

--- TRIBzope-interface-python-312/build.sh
+++ TRIBzope-interface-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBzope-interface-python-312 zope.interface-7.2
+${THOME}/build/pkg_pep518 -N TRIBzope-interface-python-312 zope_interface-8.0
