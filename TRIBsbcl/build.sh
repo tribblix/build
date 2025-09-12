8289951d04a859dd7be8e01c3c72bb392dd4c253
Peter Tribble <peter.tribble@gmail.com>
2025-09-12 11:10:39 +0100

    Update sbcl 2.5.8

--- TRIBsbcl/build.sh
+++ TRIBsbcl/build.sh
@@ -14,16 +14,24 @@
 #env INSTALL_ROOT=/tmp/l sh install.sh
 #cd ..
 #env GNUMAKE=gmake SBCL_HOME=/tmp/l/lib/sbcl PATH=/tmp/l/bin:$PATH sh make.sh --prefix=/usr/versions/sbcl-XXX
+#
+#${THOME}/build/unpack sbcl-2.0.4-sparc-solaris-binary
+#cd sbcl-2.0.4-sparc-solaris
+#env INSTALL_ROOT=/tmp/l sh install.sh
+#cd ..
+#env GNUMAKE=gmake SBCL_HOME=/tmp/l/lib/sbcl PATH=/tmp/l/bin:$PATH sh make.sh --prefix=/usr/versions/sbcl-XXX
 
 #
 # subsequent updates are a lot simpler, but you have to make
 # sure the previous package is installed
 #
-${THOME}/build/unpack sbcl-2.5.7-source
-cd sbcl-2.5.7
-env GNUMAKE=gmake sh make.sh --prefix=/usr/versions/sbcl-2.5.7
+# must rebuild maxima if sbcl is updated
+#
+${THOME}/build/unpack sbcl-2.5.8-source
+cd sbcl-2.5.8
+env GNUMAKE=gmake sh make.sh --prefix=/usr/versions/sbcl-2.5.8
 rm -fr /tmp/l2
-env INSTALL_ROOT=/tmp/l2/usr/versions/sbcl-2.5.7 sh install.sh
+env INSTALL_ROOT=/tmp/l2/usr/versions/sbcl-2.5.8 sh install.sh
 ${THOME}/build/create_pkg TRIBsbcl /tmp/l2
 rm -fr /tmp/l2
 cd ..
