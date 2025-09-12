6468b395ed56ed2af31c67237b32d7614c443e69
Peter Tribble <peter.tribble@gmail.com>
2025-09-12 21:43:00 +0100

    Update ocrmypdf 16.11.0

--- TRIBocrmypdf-python-312/build.sh
+++ TRIBocrmypdf-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBocrmypdf-python-312 ocrmypdf-16.10.4
+${THOME}/build/pkg_pep518 -N TRIBocrmypdf-python-312 ocrmypdf-16.11.0
