2b495440e05d2eb7b9f1bc895d0b111c98bc3f12
Peter Tribble <peter.tribble@gmail.com>
2025-09-13 16:05:42 +0100

    Update s3transfer 0.14.0 botocore 1.40.30 boto3 1.40.30 awscli 1.42.30

--- TRIBs3transfer-python-312/build.sh
+++ TRIBs3transfer-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBs3transfer-python-312 s3transfer-0.13.1
+${THOME}/build/pkg_setup_py TRIBs3transfer-python-312 s3transfer-0.14.0
