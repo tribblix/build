7a059f8edf28b3e2c85c3d6867971073a907dabd
Peter Tribble <peter.tribble@gmail.com>
2025-09-15 18:28:55 +0100

    Update varnish 7.7.3

--- TRIBblix-varnish/build.sh
+++ TRIBblix-varnish/build.sh
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 
-${THOME}/build/dobuild -64only varnish-7.7.2 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-3.12/bin/rst2man --with-rst2html=/usr/versions/python-3.12/bin/rst2html --with-sphinx-build=/usr/versions/python-3.12/bin/sphinx-build --disable-dependency-tracking CC=gcc"
-${THOME}/build/genpkg TRIBblix-varnish varnish-7.7.2
+${THOME}/build/dobuild -64only varnish-7.7.3 -P /opt/tribblix/varnish -C "--with-rst2man=/usr/versions/python-3.12/bin/rst2man --with-rst2html=/usr/versions/python-3.12/bin/rst2html --with-sphinx-build=/usr/versions/python-3.12/bin/sphinx-build --disable-dependency-tracking CC=gcc"
+${THOME}/build/genpkg TRIBblix-varnish varnish-7.7.3
