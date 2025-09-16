ab8df3b6a94ccbeaab1aaa07271ce1622122d869
Peter Tribble <peter.tribble@gmail.com>
2025-09-16 09:21:19 +0100

    Update beancount 3.2.0

--- TRIBbeancount-python-312/build.sh
+++ TRIBbeancount-python-312/build.sh
@@ -7,4 +7,4 @@
 # beangulp needs beautifulsoup chardet click lxml magic
 # beanprice needs dateutil requests
 #
-env CFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBbeancount-python-312 beancount-3.1.0 beanprice-2.0.0 beangulp-0.2.0 beanquery-0.2.0
+env CFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBbeancount-python-312 beancount-3.2.0 beanprice-2.0.0 beangulp-0.2.0 beanquery-0.2.0
