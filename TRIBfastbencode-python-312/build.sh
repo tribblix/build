04ff54f3f802c0b01282d58f1ef2803d3313b4d5
Peter Tribble <peter.tribble@gmail.com>
2025-09-16 09:16:47 +0100

    Update fastbencode 0.3.6

--- TRIBfastbencode-python-312/build.sh
+++ TRIBfastbencode-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBfastbencode-python-312 fastbencode-0.3.5
+${THOME}/build/pkg_pep518 -N TRIBfastbencode-python-312 fastbencode-0.3.6
