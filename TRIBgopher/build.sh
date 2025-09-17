9eacc79cb7f57c95f0d5a52f1b76f00804d2e667
Peter Tribble <peter.tribble@gmail.com>
2025-09-17 22:27:02 +0100

    Update gopher 3.0.19

--- TRIBgopher/build.sh
+++ TRIBgopher/build.sh
@@ -5,8 +5,8 @@
 # needs a bit of help passing flags into subdirectories
 # make install doesn't seem to work properly at all
 #
-${THOME}/build/dobuild -64only gopher-release-3.0.17 -M LDFLAGS=-m64
-cd gopher-release-3.0.17-64bit
+${THOME}/build/dobuild -64only gopher-release-3.0.19 -M LDFLAGS=-m64
+cd gopher-release-3.0.19-64bit
 rm -fr /tmp/ggf
 mkdir -p /tmp/ggf/usr/bin
 mkdir -p /tmp/ggf/usr/etc/gopher
