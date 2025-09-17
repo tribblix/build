17f97f6ff93723ca98303480e0ec11587fa1ccbf
Peter Tribble <peter.tribble@gmail.com>
2025-09-17 18:15:55 +0100

    Update libssh 0.11.3

--- TRIBlibssh/build.sh
+++ TRIBlibssh/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS ${THOME}/build/cmbuild +64 libssh-0.11.2 -C -DWITH_EXAMPLES=OFF
-${THOME}/build/genpkg TRIBlibssh libssh-0.11.2
+env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS ${THOME}/build/cmbuild +64 libssh-0.11.3 -C -DWITH_EXAMPLES=OFF
+${THOME}/build/genpkg TRIBlibssh libssh-0.11.3
