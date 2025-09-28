6482e64f51e5b5cf8b674b4c8b0ed16f71cd03e6
Peter Tribble <peter.tribble@gmail.com>
2025-09-28 19:42:53 +0100

    Update wcwidth 0.2.14

--- TRIBwcwidth-python-312/build.sh
+++ TRIBwcwidth-python-312/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
-${THOME}/build/pkg_setup_py TRIBwcwidth-python-312 wcwidth-0.2.13
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBwcwidth-python-312 wcwidth-0.2.14
