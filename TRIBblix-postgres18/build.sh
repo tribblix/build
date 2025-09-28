f683651190d2de7efe60472b3f18dc36fabfff90
Peter Tribble <peter.tribble@gmail.com>
2025-09-28 21:45:24 +0100

    Update PostgreSQL 18.0

--- TRIBblix-postgres18/build.sh
+++ TRIBblix-postgres18/build.sh
@@ -4,5 +4,5 @@
 #
 # postgres 18
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres18 postgresql-18rc1 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres18 postgresql-18rc1
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres18 postgresql-18.0 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres18 postgresql-18.0
