c21ad64a46750c65289fbd52031a71c97b0c2cb6
Peter Tribble <peter.tribble@gmail.com>
2025-09-29 11:24:17 +0100

    Update pyperclip 1.11.0

--- TRIBpyperclip-python-312/build.sh
+++ TRIBpyperclip-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpyperclip-python-312 pyperclip-1.10.0
+${THOME}/build/pkg_pep518 -N TRIBpyperclip-python-312 pyperclip-1.11.0
