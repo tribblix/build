fa28f00fe9906ce27337a0ba5e288d9329f260dd
Peter Tribble <peter.tribble@gmail.com>
2025-09-29 10:50:58 +0100

    Update ExtUtils::InstallPaths 0.015

--- TRIBlib-perl-5-ext-instpaths/build.sh
+++ TRIBlib-perl-5-ext-instpaths/build.sh
@@ -4,9 +4,9 @@
 #
 # required by Build::Tiny
 #
-${THOME}/build/unpack ExtUtils-InstallPaths-0.014
-cd ExtUtils-InstallPaths-0.014
+${THOME}/build/unpack ExtUtils-InstallPaths-0.015
+cd ExtUtils-InstallPaths-0.015
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-ext-instpaths ExtUtils-InstallPaths-0.014
+${THOME}/build/genpkg TRIBlib-perl-5-ext-instpaths ExtUtils-InstallPaths-0.015
