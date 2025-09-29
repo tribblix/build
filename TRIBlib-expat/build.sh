010ecf9aa0bb7c0fd30d550e2a2c77f6aba62d95
Peter Tribble <peter.tribble@gmail.com>
2025-09-29 15:34:38 +0100

    Update expat 2.7.3

--- TRIBlib-expat/build.sh
+++ TRIBlib-expat/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 expat-2.7.2
-${THOME}/build/genpkg TRIBlib-expat expat-2.7.2
+${THOME}/build/dobuild -64 expat-2.7.3
+${THOME}/build/genpkg TRIBlib-expat expat-2.7.3
