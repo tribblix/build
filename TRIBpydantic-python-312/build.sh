372686c7ccb45d81040625cf4cf5cb0179171dd9
Peter Tribble <peter.tribble@gmail.com>
2025-09-30 18:46:16 +0100

    Update pydantic 2.11.9

--- TRIBpydantic-python-312/build.sh
+++ TRIBpydantic-python-312/build.sh
@@ -5,4 +5,4 @@
 # build together, as they're joined at the hip and version locked
 # note that pydantic needs an *exact* version of pydantic_core
 #
-${THOME}/build/pkg_pep518 -N TRIBpydantic-python-312 pydantic_core-2.33.2 pydantic-2.11.7
+${THOME}/build/pkg_pep518 -N TRIBpydantic-python-312 pydantic_core-2.33.2 pydantic-2.11.9
