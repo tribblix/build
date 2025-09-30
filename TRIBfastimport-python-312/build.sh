2b1659242274897a1b67aa4c8c7b7e852c9b53a1
Peter Tribble <peter.tribble@gmail.com>
2025-09-30 18:51:57 +0100

    Update fastimport 0.9.16

--- TRIBfastimport-python-312/build.sh
+++ TRIBfastimport-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBfastimport-python-312 fastimport-0.9.14
+${THOME}/build/pkg_pep518 -N TRIBfastimport-python-312 fastimport-0.9.16
