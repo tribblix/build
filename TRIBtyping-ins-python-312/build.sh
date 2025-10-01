4e0b3e6324d86dc981d73a84e86c7203acd8bf6a
Peter Tribble <peter.tribble@gmail.com>
2025-10-01 09:24:15 +0100

    Update typing-inspection 0.4.2

--- TRIBtyping-ins-python-312/build.sh
+++ TRIBtyping-ins-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBtyping-ins-python-312 typing_inspection-0.4.1
+${THOME}/build/pkg_pep518 -N TRIBtyping-ins-python-312 typing_inspection-0.4.2
