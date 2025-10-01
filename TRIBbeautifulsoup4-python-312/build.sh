03f6af94c7a867811e155690b05cece3f4aa079f
Peter Tribble <peter.tribble@gmail.com>
2025-10-01 09:36:14 +0100

    Update beautifulsoup4 4.14.2

--- TRIBbeautifulsoup4-python-312/build.sh
+++ TRIBbeautifulsoup4-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBbeautifulsoup4-python-312 beautifulsoup4-4.13.5
+${THOME}/build/pkg_pep518 -N TRIBbeautifulsoup4-python-312 beautifulsoup4-4.14.2
