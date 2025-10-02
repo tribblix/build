a0977da056ce85d70ca988a345a2268b6a9fa2d2
Peter Tribble <peter.tribble@gmail.com>
2025-10-02 09:11:01 +0100

    Update designate 21.0.0

--- TRIBdesignate-python-312/build.sh
+++ TRIBdesignate-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBdesignate-python-312 designate-20.0.0
+${THOME}/build/pkg_pep518 -N TRIBdesignate-python-312 designate-21.0.0
