6863f6908a8748723bab0e49c0a731a0ff54cdd6
Peter Tribble <peter.tribble@gmail.com>
2025-10-04 20:14:29 +0100

    Update libogg 1.3.6

--- TRIBlibogg/build.sh
+++ TRIBlibogg/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -64 libogg-1.3.5
-${THOME}/build/genpkg TRIBlibogg libogg-1.3.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -64 libogg-1.3.6
+${THOME}/build/genpkg TRIBlibogg libogg-1.3.6
