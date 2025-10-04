43d17182de15b9401975f7afcf3e01bb47ca82a1
Peter Tribble <peter.tribble@gmail.com>
2025-10-04 10:29:37 +0100

    Update xorriso 1.5.6.pl02

--- TRIBxorriso-gui/build.sh
+++ TRIBxorriso-gui/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # ship the gui in a separate package
 #
 # this package automatically built via TRIBxorriso
--- TRIBxorriso/build.sh
+++ TRIBxorriso/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
-# the tarball is misnamed
-# mv xorriso-1.5.4.pl02.tar.gz xorriso-1.5.4.tar.gz
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xorriso-1.5.4
-${THOME}/build/genpkg TRIBxorriso xorriso-1.5.4
+# the tarball is misnamed
+#
+${THOME}/build/dobuild -64only -n xorriso-1.5.6 xorriso-1.5.6.pl02
+${THOME}/build/genpkg TRIBxorriso xorriso-1.5.6.pl02
