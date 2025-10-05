56281da68a59ba54f2561e57ee8ca121f2376dd4
Peter Tribble <peter.tribble@gmail.com>
2025-10-05 14:54:56 +0100

    Update haproxy 2.6.23

--- TRIBblix-haproxy2/build.sh
+++ TRIBblix-haproxy2/build.sh
@@ -5,8 +5,8 @@
 # haproxy2, installed in the same location as haproxy1,
 # the package for which is now deprecated and removed
 #
-${THOME}/build/unpack haproxy-2.6.22
-cd haproxy-2.6.22
+${THOME}/build/unpack haproxy-2.6.23
+cd haproxy-2.6.23
 
 gmake -j 4 CC=gcc TARGET=solaris USE_PCRE2=1 USE_OPENSSL=1 ARCH=64 USE_PROMEX=1
 
