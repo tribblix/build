1300ac78275ef19dad1ba8733ee437cf0f48ff1b
Peter Tribble <peter.tribble@gmail.com>
2025-10-06 09:24:16 +0100

    Be specific for hey version

--- TRIBhey/build.sh
+++ TRIBhey/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env GOPATH=`pwd` go install github.com/rakyll/hey@latest
+env GOPATH=`pwd` go install github.com/rakyll/hey@v0.1.4
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/hey
