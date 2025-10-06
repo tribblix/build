d1e31e007c2a0524024b207a4d5d2e974b764253
Peter Tribble <peter.tribble@gmail.com>
2025-10-06 09:15:14 +0100

    Update python-json-logger 4.0.0

--- TRIBjson-logger-python-312/build.sh
+++ TRIBjson-logger-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBjson-logger-python-312 python_json_logger-3.3.0
+${THOME}/build/pkg_pep518 -N TRIBjson-logger-python-312 python_json_logger-4.0.0
