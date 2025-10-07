865f42d4805a1e353a53d80f769368104f464c53
Peter Tribble <peter.tribble@gmail.com>
2025-10-07 20:11:23 +0100

    Update go 1.24.8

--- TRIBv-go-124/build.sh
+++ TRIBv-go-124/build.sh
@@ -13,7 +13,7 @@
 #
 chmod -R u+w go
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.24.7.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.24.8.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.24 GOROOT_BOOTSTRAP=/usr/versions/go-1.23 ./all.bash
 cd ..
