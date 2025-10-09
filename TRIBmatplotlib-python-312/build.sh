5da5f0366d0e131235fdef611b28bd46afb0dc9f
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 09:51:54 +0100

    Update matplotlib 3.10.7

--- TRIBmatplotlib-python-312/build.sh
+++ TRIBmatplotlib-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # force the use of the system freetype
 #
-env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C --config-settings=setup-args=-Dsystem-freetype=true TRIBmatplotlib-python-312 matplotlib-3.10.6
+env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C --config-settings=setup-args=-Dsystem-freetype=true TRIBmatplotlib-python-312 matplotlib-3.10.7
