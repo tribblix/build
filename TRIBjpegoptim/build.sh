b7f14bf5ddd5a9c11540a5f60eb0f197dd08b3a2
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 13:47:23 +0100

    Update jpegoptim 1.5.6

--- TRIBjpegoptim/build.sh
+++ TRIBjpegoptim/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-$THOME/build/dobuild -64only jpegoptim-1.5.5
-${THOME}/build/genpkg TRIBjpegoptim jpegoptim-1.5.5
+# SPDX-License-Identifier: CDDL-1.0
+#
+$THOME/build/dobuild -64only jpegoptim-1.5.6
+${THOME}/build/genpkg TRIBjpegoptim jpegoptim-1.5.6
