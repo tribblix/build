b1e332766704cb155ab7a4846bff745d38383b0d
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 09:40:01 +0100

    Update platformdirs 4.5.0

--- TRIBplatformdirs-python-312/build.sh
+++ TRIBplatformdirs-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBplatformdirs-python-312 platformdirs-4.4.0
+${THOME}/build/pkg_pep518 -N TRIBplatformdirs-python-312 platformdirs-4.5.0
