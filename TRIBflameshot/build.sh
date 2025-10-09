2f922a8b2638c1bec305f503966ada0dacf35ce8
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 18:14:23 +0100

    Update flameshot 13.1.0

--- TRIBflameshot/build.sh
+++ TRIBflameshot/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 #
-env PATH=/usr/versions/Qt-5/bin`${THOME}/build/getarch`:$PATH $THOME/build/cmbuild -64only flameshot-12.1.0 -C "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DCMAKE_PREFIX_PATH=/usr/versions/Qt-5/lib/`${THOME}/build/getarch`/cmake"
-${THOME}/build/genpkg TRIBflameshot flameshot-12.1.0
+env PATH=/usr/versions/Qt-6/bin:$PATH $THOME/build/cmbuild -64only flameshot-13.1.0 -C "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DCMAKE_PREFIX_PATH=/usr/versions/Qt-6/lib/`${THOME}/build/getarch`/cmake"
+${THOME}/build/genpkg TRIBflameshot flameshot-13.1.0
