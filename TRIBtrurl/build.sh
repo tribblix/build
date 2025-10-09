df01ba70b26aefbb4e6088e9bd476a5c3e89e5b1
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 15:33:07 +0100

    Update trurl 0.16.1

--- TRIBtrurl/build.sh
+++ TRIBtrurl/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env CC="gcc -m64" ${THOME}/build/dobuild -64only trurl-0.16 -M PREFIX=/usr
-${THOME}/build/genpkg -M PREFIX=/usr TRIBtrurl trurl-0.16
+env CC="gcc -m64" ${THOME}/build/dobuild -64only trurl-0.16.1 -M PREFIX=/usr
+${THOME}/build/genpkg -M PREFIX=/usr TRIBtrurl trurl-0.16.1
