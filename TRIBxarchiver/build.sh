e0dc4ac3eb0efe878f020ad2eb061d6f758573ab
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 10:26:07 +0100

    Update xarchiver 0.5.4.26

--- TRIBxarchiver/build.sh
+++ TRIBxarchiver/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #
-${THOME}/build/dobuild -gnu -64only xarchiver-0.5.4.23
-${THOME}/build/genpkg TRIBxarchiver xarchiver-0.5.4.23
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/dobuild -gnu -64only xarchiver-0.5.4.26
+${THOME}/build/genpkg TRIBxarchiver xarchiver-0.5.4.26
