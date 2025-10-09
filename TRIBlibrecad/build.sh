5581a0430c4cedca4f61993d2547437a129169ef
Peter Tribble <peter.tribble@gmail.com>
2025-10-09 22:00:09 +0100

    Update LibreCAD 2.2.1.2

--- TRIBlibrecad/build.sh
+++ TRIBlibrecad/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/unpack LibreCAD-2.2.1.1
-cd LibreCAD-2.2.1.1
+$THOME/build/unpack LibreCAD-2.2.1.2
+cd LibreCAD-2.2.1.2
 
 env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH qmake -r
 env PATH=/usr/versions/Qt-5/bin/`${THOME}/build/getarch`:$PATH gmake -j 4
