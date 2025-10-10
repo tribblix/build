62669ec25d91b4b2557aa46e8f1f6bb327d1a727
Peter Tribble <peter.tribble@gmail.com>
2025-10-10 09:21:38 +0100

    Update httptools 0.7.1

--- TRIBhttptools-python-312/build.sh
+++ TRIBhttptools-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBhttptools-python-312 httptools-0.6.4
+${THOME}/build/pkg_pep518 -N TRIBhttptools-python-312 httptools-0.7.1
