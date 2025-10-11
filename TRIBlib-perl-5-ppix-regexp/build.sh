c2c3536b395b1f451d5302c74090206d491f0d59
Peter Tribble <peter.tribble@gmail.com>
2025-10-11 09:04:33 +0100

    Update PPIx::Regexp 0.091

--- TRIBlib-perl-5-ppix-regexp/build.sh
+++ TRIBlib-perl-5-ppix-regexp/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack PPIx-Regexp-0.090
-cd PPIx-Regexp-0.090
+${THOME}/build/unpack PPIx-Regexp-0.091
+cd PPIx-Regexp-0.091
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-ppix-regexp PPIx-Regexp-0.090
+${THOME}/build/genpkg TRIBlib-perl-5-ppix-regexp PPIx-Regexp-0.091
