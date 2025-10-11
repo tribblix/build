18fc2eb4f7b68b8b284414f47ea7d651fa432535
Peter Tribble <peter.tribble@gmail.com>
2025-10-11 09:01:18 +0100

    Update CGI 4.71

--- TRIBlib-perl-5-cgi/build.sh
+++ TRIBlib-perl-5-cgi/build.sh
@@ -4,9 +4,9 @@
 #
 # cgi.pm needs URI and html-parser
 #
-${THOME}/build/unpack CGI-4.70
-cd CGI-4.70
+${THOME}/build/unpack CGI-4.71
+cd CGI-4.71
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-cgi CGI-4.70
+${THOME}/build/genpkg TRIBlib-perl-5-cgi CGI-4.71
