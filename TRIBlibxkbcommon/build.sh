a745cfaaa488e942db4804aff9f62196f730569f
Peter Tribble <peter.tribble@gmail.com>
2025-10-11 08:57:28 +0100

    Update xkbcommon 1.12.0

--- TRIBlibxkbcommon/build.sh
+++ TRIBlibxkbcommon/build.sh
@@ -5,5 +5,5 @@
 # xkbregistry pulls in libxml2 as an extra dependency, and wasn't
 # present in the earlier versions we shipped
 #
-$THOME/build/mesonbuild -64only libxkbcommon-xkbcommon-1.10.0 -C "-Denable-wayland=false -Denable-xkbregistry=false"
-${THOME}/build/genpkg TRIBlibxkbcommon libxkbcommon-xkbcommon-1.10.0
+$THOME/build/mesonbuild -64only libxkbcommon-xkbcommon-1.12.0 -C "-Denable-wayland=false -Denable-xkbregistry=false"
+${THOME}/build/genpkg TRIBlibxkbcommon libxkbcommon-xkbcommon-1.12.0
