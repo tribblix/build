3cf5afd78acc4a264ca2cf325d35818fd80b22ea
Peter Tribble <peter.tribble@gmail.com>
2025-10-11 08:59:37 +0100

    Update Mojolicious 9.42

--- TRIBlib-perl-5-mojolicious/build.sh
+++ TRIBlib-perl-5-mojolicious/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Mojolicious-9.41
-cd Mojolicious-9.41
+${THOME}/build/unpack Mojolicious-9.42
+cd Mojolicious-9.42
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-mojolicious Mojolicious-9.41
+${THOME}/build/genpkg TRIBlib-perl-5-mojolicious Mojolicious-9.42
