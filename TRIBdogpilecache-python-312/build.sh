fcab1f86fa60adea110d0cfdff7d3e401068df88
Peter Tribble <peter.tribble@gmail.com>
2025-10-12 10:05:38 +0100

    Update dogpile.cache 1.5.0

--- TRIBdogpilecache-python-312/build.sh
+++ TRIBdogpilecache-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBdogpilecache-python-312 dogpile_cache-1.4.1
+${THOME}/build/pkg_pep518 -N TRIBdogpilecache-python-312 dogpile_cache-1.5.0
