8b83cf75d1d777ab6bef24fe3790fa675cd64800
Peter Tribble <peter.tribble@gmail.com>
2025-10-12 19:12:17 +0100

    Update psycopg2 2.9.11

--- TRIBpsycopg2-312/build.sh
+++ TRIBpsycopg2-312/build.sh
@@ -9,4 +9,4 @@
 # note that python already links with -R (for ncurses) so will ignore
 # LD_RUN_PATH, hence LDFLAGS so it can find libpq at runtime
 #
-env LDFLAGS=-R/opt/tribblix/postgres17/lib PATH=/opt/tribblix/postgres17/bin:$PATH $THOME/build/pkg_setup_py TRIBpsycopg2-312 psycopg2-2.9.10 psycopg2-binary-2.9.10
+env LDFLAGS=-R/opt/tribblix/postgres17/lib PATH=/opt/tribblix/postgres17/bin:$PATH $THOME/build/pkg_setup_py TRIBpsycopg2-312 psycopg2-2.9.11 psycopg2-binary-2.9.11
