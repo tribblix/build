e6d1d365075294b1764bd391b92e9e196e0ef6a2
Peter Tribble <peter.tribble@gmail.com>
2025-10-13 08:53:20 +0100

    Update pdfly 0.5.0

---
+++ TRIBasn1crypto-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBasn1crypto-python-312 asn1crypto-1.5.1
--- TRIBpdfly-python-312/build.sh
+++ TRIBpdfly-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpdfly-python-312 pdfly-0.4.0
+${THOME}/build/pkg_pep518 -N TRIBpdfly-python-312 pdfly-0.5.0
