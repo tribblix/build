6ef1b7312ed68a8631374c235a2a18be20c5f787
Peter Tribble <peter.tribble@gmail.com>
2025-10-14 08:29:45 +0100

    Update charset-normalizer 3.4.4

--- TRIBcharset-norm-python-312/build.sh
+++ TRIBcharset-norm-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBcharset-norm-python-312 charset_normalizer-3.4.3
+${THOME}/build/pkg_pep518 -N TRIBcharset-norm-python-312 charset_normalizer-3.4.4
