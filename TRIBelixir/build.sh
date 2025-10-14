bd011a33de61f8476e3279ebec6e7f51064578d3
Peter Tribble <peter.tribble@gmail.com>
2025-10-14 13:12:44 +0100

    Add Elixir 1.18.4

---
+++ TRIBelixir/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# simply unpack, but make sure that you get the right version
+# to match the installed erlang version
+#
+$THOME/build/genpkg TRIBelixir
