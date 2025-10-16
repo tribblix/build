02968e1f31addeb1e12329c61104c46299569667
Peter Tribble <peter.tribble@gmail.com>
2025-10-16 17:18:35 +0100

    Update RabbitMQ 3.13.7

--- TRIBblix-rabbitmq/build.sh
+++ TRIBblix-rabbitmq/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
-# 3.13.0 needs otp 26
+# SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_tarball -T TRIBblix-rabbitmq rabbitmq-server-generic-unix-3.13.0
+# 3.13.X needs otp 26
+#
+${THOME}/build/pkg_tarball -T TRIBblix-rabbitmq rabbitmq-server-generic-unix-3.13.7
