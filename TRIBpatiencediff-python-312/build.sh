008bd5329a9211d2fff302c30da9be70a92230e9
Peter Tribble <peter.tribble@gmail.com>
2025-10-16 13:53:30 +0100

    Tidy up sparc requirements for patiencediff

--- TRIBpatiencediff-python-312/build.sh
+++ TRIBpatiencediff-python-312/build.sh
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# latest buyild needs rust, hold sparc downrev
+# latest build needs rust, hold sparc downrev
 #
 ${THOME}/build/pkg_pep518 -N TRIBpatiencediff-python-312 patiencediff-0.2.18
