1486e1ad9b981cdd9027de8033d65cf064e51c45
Peter Tribble <peter.tribble@gmail.com>
2025-10-16 17:31:46 +0100

    Update pg_exporter 1.0.2

--- TRIBblix-pg-exporter/build.sh
+++ TRIBblix-pg-exporter/build.sh
@@ -6,9 +6,12 @@
 mkdir p1
 cd p1
 
-$THOME/build/unpack pg_exporter-1.0.1
-cd pg_exporter-1.0.1
-env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.24/bin:$PATH go build -o pg_exporter
+$THOME/build/unpack pg_exporter-1.0.2
+cd pg_exporter-1.0.2
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.25/bin:$PATH go build -o pg_exporter
 
-${THOME}/build/genpkg TRIBblix-pg-exporter
+#
+# make install incorrectly rebuilds it, so need the same environment
+#
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.25/bin:$PATH ${THOME}/build/genpkg TRIBblix-pg-exporter
 cd ..
