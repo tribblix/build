c18a1afcc008a3005436f102429c6f07eef59aa7
Peter Tribble <peter.tribble@gmail.com>
2025-10-16 12:01:51 +0100

    Update patiencediff 0.2.18

--- TRIBpatiencediff-python-312/build.sh
+++ TRIBpatiencediff-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBpatiencediff-python-312 patiencediff-0.2.15
+# latest buyild needs rust, hold sparc downrev
+#
+${THOME}/build/pkg_pep518 -N TRIBpatiencediff-python-312 patiencediff-0.2.18
