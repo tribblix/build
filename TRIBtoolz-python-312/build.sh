c361edaab8ba75ac25b27c71d2b5d7f21b63690a
Peter Tribble <peter.tribble@gmail.com>
2025-10-17 08:51:11 +0100

    Update toolz 1.1.0

--- TRIBtoolz-python-312/build.sh
+++ TRIBtoolz-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBtoolz-python-312 toolz-1.0.0
+# without -N, as otherwise the versioning is handled incorrectly
+#
+${THOME}/build/pkg_pep518 TRIBtoolz-python-312 toolz-1.1.0
