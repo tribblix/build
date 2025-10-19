8217f5948a2c64d48098a2751adb064b003a6e39
Peter Tribble <peter.tribble@gmail.com>
2025-10-19 20:07:43 +0100

    Update iniconfig 2.3.0

--- TRIBiniconfig-python-312/build.sh
+++ TRIBiniconfig-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBiniconfig-python-312 iniconfig-2.1.0
+${THOME}/build/pkg_pep518 -N TRIBiniconfig-python-312 iniconfig-2.3.0
