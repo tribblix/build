b5b577555fc6b7f3f6bd6eb66702b6e8ce811a26
Peter Tribble <peter.tribble@gmail.com>
2025-10-19 17:30:34 +0100

    Update B::Keywords 1.29

--- TRIBlib-perl-5-b-keywords/build.sh
+++ TRIBlib-perl-5-b-keywords/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack B-Keywords-1.28
-cd B-Keywords-1.28
+${THOME}/build/unpack B-Keywords-1.29
+cd B-Keywords-1.29
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-b-keywords B-Keywords-1.28
+${THOME}/build/genpkg TRIBlib-perl-5-b-keywords B-Keywords-1.29
