bd7f95fba39ac42f125916586e7281d97de4563e
Peter Tribble <peter.tribble@gmail.com>
2025-10-20 09:13:32 +0100

    Update setuptools-scm 9.2.2

--- TRIBsetup-scm-python-312/build.sh
+++ TRIBsetup-scm-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBsetup-scm-python-312 setuptools_scm-9.2.1
+${THOME}/build/pkg_pep518 -N TRIBsetup-scm-python-312 setuptools_scm-9.2.2
