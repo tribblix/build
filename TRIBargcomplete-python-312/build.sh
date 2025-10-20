84fd134f0f626e1db4b28e7bc3f399eb88c197dc
Peter Tribble <peter.tribble@gmail.com>
2025-10-20 09:09:59 +0100

    Update argcomplete 3.6.3

--- TRIBargcomplete-python-312/build.sh
+++ TRIBargcomplete-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBargcomplete-python-312 argcomplete-3.6.2
+${THOME}/build/pkg_pep518 -N TRIBargcomplete-python-312 argcomplete-3.6.3
