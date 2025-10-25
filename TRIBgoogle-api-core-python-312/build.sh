92fbe415bdce3f7803ceef51f789c2252f9ddeb4
Peter Tribble <peter.tribble@gmail.com>
2025-10-25 11:53:40 +0100

    Update google-api-core 2.27.0

--- TRIBgoogle-api-core-python-312/build.sh
+++ TRIBgoogle-api-core-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBgoogle-api-core-python-312 google_api_core-2.26.0
+${THOME}/build/pkg_pep518 -N TRIBgoogle-api-core-python-312 google_api_core-2.27.0
