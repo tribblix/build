d8bae276813bb8888b8ea45082e76642ca2e30e4
Peter Tribble <peter.tribble@gmail.com>
2025-10-25 12:44:49 +0100

    Update openjdk 8u472

--- TRIBopenjdk8/build.sh
+++ TRIBopenjdk8/build.sh
@@ -15,11 +15,11 @@
 # check the build number corresponding to GA here
 # https://github.com/openjdk/jdk8u/tags
 # so you use the correct --with-build-number below
-# ga for 8u462 was b08
+# ga for 8u472 was b08
 #
 cd ~/ud
-${THOME}/build/unpack jdk8u-jdk8u462-ga
-cd jdk8u-jdk8u462-ga
+${THOME}/build/unpack jdk8u-jdk8u472-ga
+cd jdk8u-jdk8u472-ga
 
 #
 # The fcs is magic to hide the milestone from the version string.
@@ -29,7 +29,7 @@ cd jdk8u-jdk8u462-ga
 # new gcc build
 #
 env PATH=/usr/gnu/bin:/usr/bin:/usr/sfw/bin bash ./configure \
---with-milestone=fcs --with-update-version=462 --with-build-number=b08 \
+--with-milestone=fcs --with-update-version=472 --with-build-number=b08 \
 --enable-unlimited-crypto --with-boot-jdk=/usr/jdk/instances/jdk1.8.0 \
 --with-toolchain-type=gcc \
 --with-native-debug-symbols=none \
@@ -46,9 +46,9 @@ env PATH=/usr/gnu/bin:/usr/bin:/usr/sfw/bin gmake all USE_GCC=1 AS=/usr/gnu/bin/
 #
 # cd build/solaris-x86_64-normal-server-release/images/j2sdk-image
 # ./bin/java -version
-# openjdk version "1.8.0_462"
-# OpenJDK Runtime Environment (build 1.8.0_462-b08)
-# OpenJDK 64-Bit Server VM (build 25.462-b08, mixed mode)
+# openjdk version "1.8.0_472"
+# OpenJDK Runtime Environment (build 1.8.0_472-b08)
+# OpenJDK 64-Bit Server VM (build 25.472-b08, mixed mode)
 #
 
 #
