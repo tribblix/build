493e0a75b2c13f9188e6401912b381597fbb5ef6
Peter Tribble <peter.tribble@gmail.com>
2025-10-26 10:20:02 +0000

    Update pyexcel 0.7.4

--- TRIBpyexcel-python-312/build.sh
+++ TRIBpyexcel-python-312/build.sh
@@ -9,4 +9,4 @@
 # pyexcel-ods3 and pyexcel-xlsx, and is why it doesn't include others
 # such as pyexcel-xls
 #
-${THOME}/build/pkg_setup_py TRIBpyexcel-python-312 lml-0.2.0 pyexcel_io-0.6.7 pyexcel-0.7.3 pyexcel-ezodf-0.3.4 pyexcel-ods3-0.6.1 pyexcel_xlsx-0.6.1
+${THOME}/build/pkg_setup_py TRIBpyexcel-python-312 lml-0.2.0 pyexcel_io-0.6.7 pyexcel-0.7.4 pyexcel-ezodf-0.3.4 pyexcel-ods3-0.6.1 pyexcel_xlsx-0.6.1
