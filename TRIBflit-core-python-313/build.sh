3ff8bbad18c82b458d8fbfc02b4d724e80e71978
Peter Tribble <peter.tribble@gmail.com>
2025-10-27 12:55:03 +0000

    Add poetry and flit core for python 3.13

---
+++ TRIBflit-core-python-313/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# there's a potential bootstrapping issue, so using -N here
+# isn't advised
+#
+${THOME}/build/pkg_pep518 TRIBflit-core-python-313 flit_core-3.12.0
---
+++ TRIBpoetry-core-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpoetry-core-python-313 poetry_core-2.2.1
