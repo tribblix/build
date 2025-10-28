22b326652bc1230cfa9367334468d2ed99d02190
Peter Tribble <peter.tribble@gmail.com>
2025-10-28 20:28:31 +0000

    Update SeaweedFS 3.99

--- TRIBblix-seaweedfs/build.sh
+++ TRIBblix-seaweedfs/build.sh
@@ -8,9 +8,9 @@ mkdir w
 cd w
 git clone https://github.com/seaweedfs/seaweedfs
 cd seaweedfs
-git checkout 3.97
+git checkout 3.99
 cd weed
-env PATH=/usr/versions/go-1.24/bin:$PATH GOPATH=`pwd` gmake
+env PATH=/usr/versions/go-1.25/bin:$PATH GOPATH=`pwd` gmake
 cd ..
 #
 rm -fr /tmp/ee
