f279217cbee67a82fa914d83d88fdc1fef9063e5
Peter Tribble <peter.tribble@gmail.com>
2025-10-28 09:51:07 +0000

    Update HTTP::Message 7.01

--- TRIBlib-perl-5-http-message/build.sh
+++ TRIBlib-perl-5-http-message/build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # http-message needs io-html
 # http-message needs http-date
 # http-message need encode-locale
 # http-message need lwp-mediatypes
 #
-${THOME}/build/unpack HTTP-Message-7.00
-cd HTTP-Message-7.00
+${THOME}/build/unpack HTTP-Message-7.01
+cd HTTP-Message-7.01
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-http-message HTTP-Message-7.00
+${THOME}/build/genpkg TRIBlib-perl-5-http-message HTTP-Message-7.01
