9de1485a309d227951e29fd584a442114366267d
Peter Tribble <peter.tribble@gmail.com>
2025-10-28 20:16:08 +0000

    Update ncdu 1.22

--- TRIBncdu/build.sh
+++ TRIBncdu/build.sh
@@ -4,5 +4,5 @@
 #
 # use the 'legacy' 1.x branch; 2.x requires zig
 #
-env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only ncdu-1.20
-${THOME}/build/genpkg TRIBncdu ncdu-1.20
+env TRIBBLIX_CFLAGS="-I/usr/gnu/include/ncursesw -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only ncdu-1.22
+${THOME}/build/genpkg TRIBncdu ncdu-1.22
