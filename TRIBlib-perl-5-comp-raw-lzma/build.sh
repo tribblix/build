3a91a0d7c4b8aa596bcb4f0a748d54438cf02459
Peter Tribble <peter.tribble@gmail.com>
2025-10-28 09:47:43 +0000

    Update Compress::Raw::Lzma 2.214

--- TRIBlib-perl-5-comp-raw-lzma/build.sh
+++ TRIBlib-perl-5-comp-raw-lzma/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Compress-Raw-Lzma-2.213
-cd Compress-Raw-Lzma-2.213
+${THOME}/build/unpack Compress-Raw-Lzma-2.214
+cd Compress-Raw-Lzma-2.214
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-comp-raw-lzma Compress-Raw-Lzma-2.213
+${THOME}/build/genpkg TRIBlib-perl-5-comp-raw-lzma Compress-Raw-Lzma-2.214
