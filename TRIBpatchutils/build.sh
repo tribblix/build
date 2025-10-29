83ff589dc7da538c5b179cea7a63f3aea2ed93ce
Peter Tribble <peter.tribble@gmail.com>
2025-10-29 10:08:17 +0000

    Update patchutils 0.4.4

--- TRIBpatchutils/build.sh
+++ TRIBpatchutils/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
 #
-#
-${THOME}/build/dobuild -64only patchutils-0.4.2
-${THOME}/build/genpkg TRIBpatchutils patchutils-0.4.2
+${THOME}/build/dobuild -64only patchutils-0.4.4
+${THOME}/build/genpkg TRIBpatchutils patchutils-0.4.4
