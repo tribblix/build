f691b6264ffcd6dd7bbe89494be61304e1fafce8
Peter Tribble <peter.tribble@gmail.com>
2025-10-29 12:35:15 +0000

    Package setuptools for python 3.13

---
+++ TRIBsetuptools-python-313/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# as of python 3.12, packaged separately rather than bundled
+# with python
+#
+# contains filenames with spaces, so we ship those as a tarball
+# and unpack that in postinstall, like we used to do for python
+# itself in prior releases
+#
+${THOME}/build/pkg_pep518 -N TRIBsetuptools-python-313 setuptools-80.9.0
