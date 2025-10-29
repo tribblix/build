ace5634a489b4c9c69fa0eec7cc2f03f0294a620
Peter Tribble <peter.tribble@gmail.com>
2025-10-29 10:00:46 +0000

    Update less 685

--- TRIBtext-less/build.sh
+++ TRIBtext-less/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only less-679
-${THOME}/build/genpkg TRIBtext-less less-679
+${THOME}/build/dobuild -64only less-685
+${THOME}/build/genpkg TRIBtext-less less-685
