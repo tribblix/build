43303e1d927443b63d4eb07a98e2277e1747117d
Peter Tribble <peter.tribble@gmail.com>
2025-10-30 14:09:23 +0000

    Drop the empty separate xephyr package

--- TRIBx11-server-xephyr/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# this is an empty package so that if you update the old
-# bits get removed
-#
-rm -fr /tmp/c1
-mkdir -p /tmp/c1
-${THOME}/build/create_pkg TRIBx11-server-xephyr /tmp/c1
-rm -fr /tmp/c1
