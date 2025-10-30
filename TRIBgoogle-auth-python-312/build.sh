68c051d251b3b7d011ce9c815ce484a4d3a1b77e
Peter Tribble <peter.tribble@gmail.com>
2025-10-30 22:40:26 +0000

    Update google-auth 2.42.1

--- TRIBgoogle-auth-python-312/build.sh
+++ TRIBgoogle-auth-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-312 google_auth-2.42.0
+${THOME}/build/pkg_setup_py TRIBgoogle-auth-python-312 google_auth-2.42.1
