d3100f2bce54dce5befca65c456ffacdabf64406
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 09:52:29 +0000

    Update snappy 1.2.2

--- TRIBsnappy/build.sh
+++ TRIBsnappy/build.sh
@@ -2,9 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# the old library needs to be built with the old ABI for compatibility
-# this can be dropped if snappy is updated, as that will require all
-# consumers to be rebuilt in any case
-#
-env TRIBBLIX_CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ${THOME}/build/dobuild +64 snappy-1.1.3
-${THOME}/build/genpkg TRIBsnappy snappy-1.1.3
+${THOME}/build/cmbuild +64 snappy-1.2.2 -C "-DSNAPPY_BUILD_TESTS=OFF -DSNAPPY_BUILD_BENCHMARKS=OFF -DBUILD_SHARED_LIBS=ON"
+${THOME}/build/genpkg TRIBsnappy snappy-1.2.2
