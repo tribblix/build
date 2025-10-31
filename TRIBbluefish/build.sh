7825dace51b6e9f2223cd5ce85dcd5c3f5812fcd
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 11:57:15 +0000

    Update bluefish 2.2.18

--- TRIBbluefish/build.sh
+++ TRIBbluefish/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -gnu -64only bluefish-2.2.16 -C --disable-python
-${THOME}/build/genpkg TRIBbluefish bluefish-2.2.16
+${THOME}/build/dobuild -gnu -64only bluefish-2.2.18 -C --disable-python
+${THOME}/build/genpkg TRIBbluefish bluefish-2.2.18
