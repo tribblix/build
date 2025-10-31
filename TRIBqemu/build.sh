4dc5f29e8b9d3fca27f67fd6688bc0c021b51314
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 11:34:08 +0000

    Update qemu 8.1.5

--- TRIBqemu/build.sh
+++ TRIBqemu/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# SPDX-License-Identifier: CDDL-1.0
+#
 # big jump from 2.6 to 8
 #
 
@@ -9,7 +11,7 @@
 #
 
 env TRIBBLIX_CFLAGS="-D_POSIX_C_SOURCE=200112L -D_POSIX_PTHREAD_SEMANTICS -D__EXTENSIONS__=1 -O2" \
-$THOME/build/dobuild -64only qemu-8.1.0 -P /usr/versions/qemu8 \
+$THOME/build/dobuild -64only qemu-8.1.5 -P /usr/versions/qemu8 \
 -C "--cc=gcc --localstatedir=/var/qemu --enable-slirp -Ddebug=false"
 
-${THOME}/build/genpkg TRIBqemu qemu-8.1.0
+${THOME}/build/genpkg TRIBqemu qemu-8.1.5
