9d6722cb06587b22cdd954b9f73871718810293f
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 11:39:56 +0000

    Update gpgme 1.24.3

--- TRIBgpgme/build.sh
+++ TRIBgpgme/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only gpgme-1.24.2 -C --disable-gpg-test
-${THOME}/build/genpkg TRIBgpgme gpgme-1.24.2
+${THOME}/build/dobuild -64only gpgme-1.24.3 -C --disable-gpg-test
+${THOME}/build/genpkg TRIBgpgme gpgme-1.24.3
