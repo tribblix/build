19386e5e46560933c7298be95098d13ec6b2dbe8
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 09:39:50 +0000

    Update fsspec 2025.10.0

--- TRIBfsspec-python-312/build.sh
+++ TRIBfsspec-python-312/build.sh
@@ -5,4 +5,4 @@
 # there's a huge list of optional extras, we don't declare any of them
 # as dependencies directly
 #
-${THOME}/build/pkg_pep518 -N TRIBfsspec-python-312 fsspec-2025.9.0
+${THOME}/build/pkg_pep518 -N TRIBfsspec-python-312 fsspec-2025.10.0
