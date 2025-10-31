be99d621f2f6b750916c05d22c6ec17b5c7395c0
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 11:45:14 +0000

    Update pinentry 1.3.2

--- TRIBpinentry/build.sh
+++ TRIBpinentry/build.sh
@@ -7,5 +7,5 @@
 # for dependency purposes, split the gui implementations
 # into separate packages
 #
-${THOME}/build/dobuild -64only pinentry-1.3.0 -C "--disable-libsecret --disable-pinentry-efl LIBS=-lsocket"
-${THOME}/build/genpkg TRIBpinentry pinentry-1.3.0
+${THOME}/build/dobuild -64only pinentry-1.3.2 -C "--disable-libsecret --disable-pinentry-efl LIBS=-lsocket"
+${THOME}/build/genpkg TRIBpinentry pinentry-1.3.2
