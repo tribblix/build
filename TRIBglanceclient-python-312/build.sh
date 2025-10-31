310af254a5e0d5bd2eef9f2f5f65995cfee6d1d7
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 13:49:44 +0000

    Add glanceclient 4.10.0

---
+++ TRIBglanceclient-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_setup_py TRIBglanceclient-python-312 python_glanceclient-4.10.0
---
+++ TRIBwarlock-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBwarlock-python-312 warlock-2.1.0
