f688f0db74ba27aa45ada4bee717a78eeb836804
Peter Tribble <peter.tribble@gmail.com>
2025-10-31 11:51:35 +0000

    Update gnupg 2.4.8

--- TRIBgnupg2/build.sh
+++ TRIBgnupg2/build.sh
@@ -6,5 +6,5 @@
 #
 # disabling the internal ccid driver avoids pulling in libusb
 #
-${THOME}/build/dobuild gnupg-2.2.43 -64only -C "--disable-ldap --enable-gpg-is-gpg2 --disable-ccid-driver"
-${THOME}/build/genpkg TRIBgnupg2 gnupg-2.2.43
+${THOME}/build/dobuild gnupg-2.4.8 -64only -C "--disable-ldap --enable-gpg-is-gpg2 --disable-ccid-driver"
+${THOME}/build/genpkg TRIBgnupg2 gnupg-2.4.8
