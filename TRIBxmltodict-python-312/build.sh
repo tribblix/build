22b2130c684d191e119a6406c700903a209d9b79
Peter Tribble <peter.tribble@gmail.com>
2025-11-01 09:50:20 +0000

    Update xmltodict 1.0.2

--- TRIBxmltodict-python-312/build.sh
+++ TRIBxmltodict-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBxmltodict-python-312 xmltodict-0.15.1
+${THOME}/build/pkg_pep518 -N TRIBxmltodict-python-312 xmltodict-1.0.2
