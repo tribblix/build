b6fc53b1f2e543e3ce6273078c7b92f615169e48
Peter Tribble <peter.tribble@gmail.com>
2025-11-01 13:14:16 +0000

    Update pcre2 10.47

--- TRIBlib-pcre2/build.sh
+++ TRIBlib-pcre2/build.sh
@@ -4,5 +4,5 @@
 #
 # this is pcre2, the original pcre is in TRIBlib-pcre
 #
-${THOME}/build/dobuild -64 pcre2-10.45 -C "--localstatedir=/var --disable-static --enable-rebuild-chartables --enable-newline-is-any --disable-stack-for-recursion --with-link-size=4 --with-match-limit=10000000 --with-pic --enable-pcre2grep-libz --enable-pcre2grep-libbz2 --enable-pcre2-8 --enable-pcre2-16 --enable-unicode"
-${THOME}/build/genpkg TRIBlib-pcre2 pcre2-10.45
+${THOME}/build/dobuild -64 pcre2-10.47 -C "--localstatedir=/var --disable-static --enable-rebuild-chartables --enable-newline-is-any --disable-stack-for-recursion --with-link-size=4 --with-match-limit=10000000 --with-pic --enable-pcre2grep-libz --enable-pcre2grep-libbz2 --enable-pcre2-8 --enable-pcre2-16 --enable-unicode"
+${THOME}/build/genpkg TRIBlib-pcre2 pcre2-10.47
