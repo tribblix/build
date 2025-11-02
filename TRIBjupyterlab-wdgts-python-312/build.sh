4e790429620e73311f08e484b2af257d747c4994
Peter Tribble <peter.tribble@gmail.com>
2025-11-02 09:02:00 +0000

    Update jupyterlab_widgets 3.0.16

--- TRIBjupyterlab-wdgts-python-312/build.sh
+++ TRIBjupyterlab-wdgts-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBjupyterlab-wdgts-python-312 jupyterlab_widgets-3.0.15
+${THOME}/build/pkg_pep518 -N TRIBjupyterlab-wdgts-python-312 jupyterlab_widgets-3.0.16
