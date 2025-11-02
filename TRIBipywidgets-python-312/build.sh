c4c8d8f50fa75d6888e514a1d0b934d3d1dd021f
Peter Tribble <peter.tribble@gmail.com>
2025-11-02 09:04:27 +0000

    Update ipywidgets 8.1.8

--- TRIBipywidgets-python-312/build.sh
+++ TRIBipywidgets-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBipywidgets-python-312 ipywidgets-8.1.7
+${THOME}/build/pkg_pep518 -N TRIBipywidgets-python-312 ipywidgets-8.1.8
