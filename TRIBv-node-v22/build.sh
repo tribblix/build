fb5bec31df194e31d18f0d66524ef917118349c6
Peter Tribble <peter.tribble@gmail.com>
2025-11-03 12:13:06 +0000

    Update node v22.21.1

--- TRIBv-node-v22/build.sh
+++ TRIBv-node-v22/build.sh
@@ -4,5 +4,5 @@
 #
 # 64-bit only
 #
-env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v22.21.0 -P /usr/versions/node-v22 -J 6 -C "--dest-cpu=x64"
-${THOME}/build/genpkg TRIBv-node-v22 node-v22.21.0
+env CC_host="gcc -m64" CC=gcc CXX=g++ ${THOME}/build/dobuild node-v22.21.1 -P /usr/versions/node-v22 -J 6 -C "--dest-cpu=x64"
+${THOME}/build/genpkg TRIBv-node-v22 node-v22.21.1
