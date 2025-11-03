a21698b5d6e9837138b69381c9b0e0265a320d50
Peter Tribble <peter.tribble@gmail.com>
2025-11-03 09:19:00 +0000

    Update glm 1.0.2

--- TRIBglm/build.sh
+++ TRIBglm/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only glm-1.0.1 -C "-DGLM_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON"
-${THOME}/build/genpkg TRIBglm glm-1.0.1
+${THOME}/build/cmbuild -64only glm-1.0.2 -C "-DGLM_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON"
+${THOME}/build/genpkg TRIBglm glm-1.0.2
