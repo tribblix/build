613db3ffe4dbdc1318b4c1987f0913e366e0d0bb
Peter Tribble <peter.tribble@gmail.com>
2025-11-03 18:22:32 +0000

    Update cloudpickle 3.1.2

--- TRIBcloudpickle-python-312/build.sh
+++ TRIBcloudpickle-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBcloudpickle-python-312 cloudpickle-3.1.1
+${THOME}/build/pkg_pep518 -N TRIBcloudpickle-python-312 cloudpickle-3.1.2
