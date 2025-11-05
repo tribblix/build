62ce229aca1f91b844c10167e22e537832224aee
Peter Tribble <peter.tribble@gmail.com>
2025-11-05 20:07:10 +0000

    Update go 1.25.4, and make default

--- TRIBv-go-125/build.sh
+++ TRIBv-go-125/build.sh
@@ -5,13 +5,13 @@
 # either uninstall gdb, or run make.bash rather than all.bash, as the gdb
 # test fails on Tribblix
 #
-# 1.25 release, not yet default
+# 1.25 release, now default
 #
 # the tarball has some test files with UTF-8 filenames
 #
 chmod -R u+w go
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.25.3.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.25.4.src
 cd go/src
 env GOROOT_FINAL=/usr/versions/go-1.25 GOROOT_BOOTSTRAP=/usr/versions/go-1.24 ./all.bash
 cd ..
