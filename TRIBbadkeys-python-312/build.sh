9ec478bf5babd4122e45518d6c18840c213c2ad1
Peter Tribble <peter.tribble@gmail.com>
2025-11-07 14:30:48 +0000

    Update badkeys 0.0.15

--- TRIBbadkeys-python-312/build.sh
+++ TRIBbadkeys-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBbadkeys-python-312 badkeys-0.0.14
+# dependencies include the [ssh,dkim] extras
+#
+${THOME}/build/pkg_pep518 -N TRIBbadkeys-python-312 badkeys-0.0.15
