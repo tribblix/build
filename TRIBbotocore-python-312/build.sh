d3bd288f985dfe803f8f9625517ed6b1e8da106a
Peter Tribble <peter.tribble@gmail.com>
2025-11-09 09:15:18 +0000

    Update botocore 1.40.69 boto3 1.40.69 awscli 1.42.69

--- TRIBbotocore-python-312/build.sh
+++ TRIBbotocore-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBbotocore-python-312 botocore-1.40.64
+${THOME}/build/pkg_pep518 -N TRIBbotocore-python-312 botocore-1.40.69
