eddde69cc465b791af2ef8941afd55bb95635c6a
Peter Tribble <peter.tribble@gmail.com>
2025-11-11 08:39:01 +0000

    Update annotated-doc 0.0.4

--- TRIBannotated-doc-python-312/build.sh
+++ TRIBannotated-doc-python-312/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# must use -N, as it uses uv-build which needs patching to build
-#
-${THOME}/build/pkg_pep518 -N TRIBannotated-doc-python-312 annotated_doc-0.0.3
+${THOME}/build/pkg_pep518 TRIBannotated-doc-python-312 annotated_doc-0.0.4
