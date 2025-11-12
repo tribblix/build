68ef49f1765ab8ee804056f5037c1005662d4e43
Peter Tribble <peter.tribble@gmail.com>
2025-11-12 08:45:17 +0000

    Update endesive 2.19.2

--- TRIBendesive-python-312/build.sh
+++ TRIBendesive-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_wheel TRIBendesive-python-312 endesive-2.19.1-py3-none-any.whl
+${THOME}/build/pkg_wheel TRIBendesive-python-312 endesive-2.19.2-py3-none-any.whl
