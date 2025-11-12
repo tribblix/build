0d09ec201e227cdee911ca2db2d645d31dd29c81
Peter Tribble <peter.tribble@gmail.com>
2025-11-12 08:40:58 +0000

    Update pymdown-extensions 10.17.1

--- TRIBmdown-ext-python-312/build.sh
+++ TRIBmdown-ext-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBmdown-ext-python-312 pymdown_extensions-10.16.1
+${THOME}/build/pkg_pep518 -N TRIBmdown-ext-python-312 pymdown_extensions-10.17.1
