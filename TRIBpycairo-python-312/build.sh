e4eeaeb4c82bd13ff0daedd349a169d7463a075c
Peter Tribble <peter.tribble@gmail.com>
2025-11-12 08:48:53 +0000

    Update pycairo 1.29.0

--- TRIBpycairo-python-312/build.sh
+++ TRIBpycairo-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 ${THOME}/build/pkg_pep518 TRIBpycairo-python-312 pycairo-1.28.0
+env CFLAGS=-m64 CXXFLAGS=-m64 LDFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBpycairo-python-312 pycairo-1.29.0
