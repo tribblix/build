a5aa5f3c8e85af0eefe231b503d00ed58d7cb645
Peter Tribble <peter.tribble@gmail.com>
2025-11-13 09:35:13 +0000

    Update linode_metadata 0.3.2

--- TRIBlinode-metadata-python-312/build.sh
+++ TRIBlinode-metadata-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBlinode-metadata-python-312 linode_metadata-0.3.1
+${THOME}/build/pkg_pep518 -N TRIBlinode-metadata-python-312 linode_metadata-0.3.2
