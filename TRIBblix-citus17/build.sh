40b6640e3f660532f5907c99581182aaeec6e963
Peter Tribble <peter.tribble@gmail.com>
2025-11-13 14:48:14 +0000

    Update citus 13.2.0

--- TRIBblix-citus17/build.sh
+++ TRIBblix-citus17/build.sh
@@ -6,5 +6,5 @@
 #
 # there is a citus-stat patch
 #
-${THOME}/build/dobuild +64only citus-13.1.0 -C PG_CONFIG=/opt/tribblix/postgres17/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus17 citus-13.1.0
+${THOME}/build/dobuild +64only citus-13.2.0 -C PG_CONFIG=/opt/tribblix/postgres17/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus17 citus-13.2.0
