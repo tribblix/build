41f042f49cb35bbb536a5c04c4cb1b78c434d2a8
Peter Tribble <peter.tribble@gmail.com>
2025-11-15 10:15:01 +0000

    Update IceWM 3.9.0

--- TRIBicewm/build.sh
+++ TRIBicewm/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only icewm-3.8.0
-${THOME}/build/genpkg TRIBicewm icewm-3.8.0
+${THOME}/build/dobuild -64only icewm-3.9.0
+${THOME}/build/genpkg TRIBicewm icewm-3.9.0
