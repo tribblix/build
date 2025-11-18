62cececc6a7168ad457a5a57e37accaddb558ae9
Peter Tribble <peter.tribble@gmail.com>
2025-11-18 17:25:30 +0000

    Update fx 39.2.0

--- TRIBfx/build.sh
+++ TRIBfx/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env GOPATH=`pwd` go install github.com/antonmedv/fx@38.0.0
+env GOPATH=`pwd` PATH=/usr/versions/go-1.25/bin:$PATH go install github.com/antonmedv/fx@39.2.0
 rm -fr /tmp/cs
 mkdir -p /tmp/cs/usr/bin
 mkdir -p /tmp/cs/usr/share/fx
