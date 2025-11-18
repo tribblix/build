45aae11783405ced2af487f661e31f155e0a7a86
Peter Tribble <peter.tribble@gmail.com>
2025-11-18 19:24:04 +0000

    typo diistribution

--- TRIBmeson-python-312/build.sh
+++ TRIBmeson-python-312/build.sh
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# no source diistribution was made available for 1.9.1
+# no source distribution was made available for 1.9.1
 #
 ${THOME}/build/pkg_wheel TRIBmeson-python-312 meson-1.9.1-py3-none-any.whl
