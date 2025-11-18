5fdfc3c78981c7a928c8212066efb463197bbc62
Peter Tribble <peter.tribble@gmail.com>
2025-11-18 17:47:22 +0000

    Update fzf 0.67.0

--- TRIBfzf/build.sh
+++ TRIBfzf/build.sh
@@ -6,7 +6,7 @@
 #
 git clone https://github.com/junegunn/fzf.git
 cd fzf
-git checkout v0.65.1
+git checkout v0.67.0
 
 #
 # the Makefile attempts to enumerate all possible build targets
@@ -14,7 +14,7 @@ git checkout v0.65.1
 #
 sed -i s:x86_64:i86pc: Makefile
 
-env PATH=/usr/versions/go-1.24/bin:$PATH gmake bin/fzf
+env PATH=/usr/versions/go-1.25/bin:$PATH gmake bin/fzf
 
 rm -fr /tmp/ll
 mkdir -p /tmp/ll/usr/bin
