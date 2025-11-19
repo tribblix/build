d7c095944ba632760b28fa5c9d0bf8a34e883a64
Peter Tribble <peter.tribble@gmail.com>
2025-11-19 21:39:39 +0000

    Update liboqs 0.15.0

--- TRIBliboqs/build.sh
+++ TRIBliboqs/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only liboqs-0.14.0 -C "-DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF"
-${THOME}/build/genpkg TRIBliboqs liboqs-0.14.0
+${THOME}/build/cmbuild -64only liboqs-0.15.0 -C "-DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF"
+${THOME}/build/genpkg TRIBliboqs liboqs-0.15.0
