7a2e3c75cf6c55f6149a0cd71fcb49d28a135a61
Peter Tribble <peter.tribble@gmail.com>
2025-11-19 08:17:19 +0000

    Update castellan 5.5.0

--- TRIBcastellan-python-312/build.sh
+++ TRIBcastellan-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBcastellan-python-312 castellan-5.4.1
+${THOME}/build/pkg_pep518 -N TRIBcastellan-python-312 castellan-5.5.0
