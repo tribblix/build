f1e17de229b5086a04a1084eaa934b6c0ce3ecf8
Peter Tribble <peter.tribble@gmail.com>
2025-11-19 08:27:32 +0000

    Update python-ironicclient 5.14.0

--- TRIBironicclient-python-312/build.sh
+++ TRIBironicclient-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBironicclient-python-312 python_ironicclient-5.13.0
+# the [cli] extra adds openstackclient as a dependency
+#
+${THOME}/build/pkg_setup_py TRIBironicclient-python-312 python_ironicclient-5.14.0
