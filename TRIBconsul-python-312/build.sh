63c0104ebc5b145491f1c1b1363b08169c87268f
Peter Tribble <peter.tribble@gmail.com>
2025-11-20 10:05:52 +0000

    Update py-consul 1.6.1

--- TRIBconsul-python-312/build.sh
+++ TRIBconsul-python-312/build.sh
@@ -5,4 +5,4 @@
 # this used to be python-consul, but that's now unmaintained
 # switched to the maintained replacement py-consul
 #
-${THOME}/build/pkg_setup_py TRIBconsul-python-312 py_consul-1.6.0
+${THOME}/build/pkg_pep518 -N TRIBconsul-python-312 py_consul-1.6.1
