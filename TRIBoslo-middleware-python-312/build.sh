7f29e5c818e90739bdeb042d08aecf4c730a50bb
Peter Tribble <peter.tribble@gmail.com>
2025-11-20 10:58:26 +0000

    Update oslo.middleware 7.0.0

--- TRIBoslo-middleware-python-312/build.sh
+++ TRIBoslo-middleware-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBoslo-middleware-python-312 oslo_middleware-6.6.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-middleware-python-312 oslo_middleware-7.0.0
