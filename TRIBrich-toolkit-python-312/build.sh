47a4091a616591f6dc562546929983ff620132cf
Peter Tribble <peter.tribble@gmail.com>
2025-11-20 10:03:07 +0000

    Update rich-toolkit 0.16.0

--- TRIBrich-toolkit-python-312/build.sh
+++ TRIBrich-toolkit-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBrich-toolkit-python-312 rich_toolkit-0.15.1
+${THOME}/build/pkg_pep518 -N TRIBrich-toolkit-python-312 rich_toolkit-0.16.0
