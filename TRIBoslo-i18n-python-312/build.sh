095c8ad80c4c4f61be0ec77d4a16745557a46fb1
Peter Tribble <peter.tribble@gmail.com>
2025-11-20 19:37:28 +0000

    Update oslo.i18n 6.7.1

--- TRIBoslo-i18n-python-312/build.sh
+++ TRIBoslo-i18n-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-i18n-python-312 oslo_i18n-6.7.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-i18n-python-312 oslo_i18n-6.7.1
