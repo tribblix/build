0f4644adc32152878d10ee397ff60ad7fe5843ad
Peter Tribble <peter.tribble@gmail.com>
2025-11-21 17:02:03 +0000

    Update os-service-types 1.8.2

--- TRIBos-service-types-python-312/build.sh
+++ TRIBos-service-types-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBos-service-types-python-312 os_service_types-1.8.1
+${THOME}/build/pkg_pep518 -N TRIBos-service-types-python-312 os_service_types-1.8.2
