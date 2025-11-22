91caa1fb88fcdefda8f1f521463d6c0370aac024
Peter Tribble <peter.tribble@gmail.com>
2025-11-22 13:57:09 +0000

    Update xyzservices 2025.11.0

--- TRIBxyzservices-python-312/build.sh
+++ TRIBxyzservices-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBxyzservices-python-312 xyzservices-2025.10.0
+${THOME}/build/pkg_pep518 -N TRIBxyzservices-python-312 xyzservices-2025.11.0
