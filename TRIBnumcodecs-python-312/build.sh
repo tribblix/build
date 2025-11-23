2f90bcc4ab2ee198da90b272078f05e284b45ad3
Peter Tribble <peter.tribble@gmail.com>
2025-11-23 12:04:49 +0000

    Update numcodecs 0.16.5

--- TRIBnumcodecs-python-312/build.sh
+++ TRIBnumcodecs-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBnumcodecs-python-312 numcodecs-0.15.1
+${THOME}/build/pkg_pep518 -N TRIBnumcodecs-python-312 numcodecs-0.16.5
