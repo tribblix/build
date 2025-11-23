d3aa0ff49492196da29d9bea8a2376687d889bc3
Peter Tribble <peter.tribble@gmail.com>
2025-11-23 08:58:12 +0000

    Update scipy 1.16.3

--- TRIBscipy-python-312/build.sh
+++ TRIBscipy-python-312/build.sh
@@ -16,4 +16,4 @@
 #
 # ideally that would be fixed further up
 #
-env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscipy-python-312 scipy-1.16.1
+env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscipy-python-312 scipy-1.16.3
