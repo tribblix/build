1b0dd21e4b57c5dd15e13da42e243d04b3539d27
Peter Tribble <peter.tribble@gmail.com>
2025-11-27 08:02:56 +0000

    Update hatchling 1.28.0

--- TRIBhatchling-python-312/build.sh
+++ TRIBhatchling-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBhatchling-python-312 hatchling-1.27.0 hatch_fancy_pypi_readme-25.1.0
+${THOME}/build/pkg_pep518 TRIBhatchling-python-312 hatchling-1.28.0 hatch_fancy_pypi_readme-25.1.0
