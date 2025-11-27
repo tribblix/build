e50aca23f53578b9da8174ad758f8acbb1d64b3e
Peter Tribble <peter.tribble@gmail.com>
2025-11-27 08:26:45 +0000

    Update weechat 4.7.2

--- TRIBweechat/build.sh
+++ TRIBweechat/build.sh
@@ -5,5 +5,5 @@
 # need -lresolv -lsocket -lnsl for res_init
 # there's a preconf script to help it find ncurses in the first place
 #
-env TRIBBLIX_CFLAGS="-I/usr/gnu/include -D_XPG6 -D__EXTENSIONS__" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch` -liconv -lresolv -lsocket -lnsl" ${THOME}/build/cmbuild -gnu -64only weechat-4.7.0 -C "-DENABLE_CJSON=OFF -DENABLE_PHP=OFF -DENABLE_PYTHON=OFF -DENABLE_RUBY=OFF -DENABLE_LUA=OFF -DENABLE_GUILE=OFF"
-${THOME}/build/genpkg TRIBweechat weechat-4.7.0
+env TRIBBLIX_CFLAGS="-I/usr/gnu/include -D_XPG6 -D__EXTENSIONS__" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch` -liconv -lresolv -lsocket -lnsl" ${THOME}/build/cmbuild -gnu -64only weechat-4.7.2 -C "-DENABLE_CJSON=OFF -DENABLE_PHP=OFF -DENABLE_PYTHON=OFF -DENABLE_RUBY=OFF -DENABLE_LUA=OFF -DENABLE_GUILE=OFF"
+${THOME}/build/genpkg TRIBweechat weechat-4.7.2
