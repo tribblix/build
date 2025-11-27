814a2e56e6fb2e4789bdde762c451cb704db043b
Peter Tribble <peter.tribble@gmail.com>
2025-11-27 08:56:46 +0000

    Update ninja 1.13.2

--- TRIBninja/build.sh
+++ TRIBninja/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/unpack ninja-1.13.1
-cd ninja-1.13.1
+$THOME/build/unpack ninja-1.13.2
+cd ninja-1.13.2
 env CFLAGS=-m64 python3.12 ./configure.py --bootstrap
 rm -fr /tmp/n1
 mkdir -p /tmp/n1/usr/bin
