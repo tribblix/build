473bbd608503a42a9313e8c05c16bf7c0abc6c1f
Peter Tribble <peter.tribble@gmail.com>
2025-11-28 09:22:27 +0000

    Update incremental 24.11.0

--- TRIBincremental-python-312/build.sh
+++ TRIBincremental-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBincremental-python-312 incremental-24.7.2
+${THOME}/build/pkg_pep518 -N TRIBincremental-python-312 incremental-24.11.0
