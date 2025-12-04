50af8a86316b53b593d91a6fcdc307892e77665a
Peter Tribble <peter.tribble@gmail.com>
2025-12-04 12:02:17 +0000

    Update Django 4.2.27 5.2.8

--- TRIBdjango4-python-312/build.sh
+++ TRIBdjango4-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # pytz is deprecated, but marked as a dependency for transitional purposes
 #
-${THOME}/build/pkg_pep518 -N TRIBdjango4-python-312 django-4.2.26
+${THOME}/build/pkg_pep518 -N TRIBdjango4-python-312 django-4.2.27
--- TRIBdjango5-python-312/build.sh
+++ TRIBdjango5-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBdjango5-python-312 django-5.2.8
+${THOME}/build/pkg_pep518 -N TRIBdjango5-python-312 django-5.2.9
