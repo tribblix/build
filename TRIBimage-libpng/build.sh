fe2eef9fed4263e50d2a16d62fc0af62c1980918
Peter Tribble <peter.tribble@gmail.com>
2025-12-04 13:54:49 +0000

    Update libpng 1.6.52

--- TRIBimage-libpng/build.sh
+++ TRIBimage-libpng/build.sh
@@ -7,5 +7,5 @@
 # ship both 1.4 and 1.6
 #
 ${THOME}/build/dobuild -64 libpng-1.4.22 -C --sysconfdir=/etc
-${THOME}/build/dobuild -64 libpng-1.6.51 -C --sysconfdir=/etc
-${THOME}/build/genpkg TRIBimage-libpng libpng-1.4.22 libpng-1.6.51
+${THOME}/build/dobuild -64 libpng-1.6.52 -C --sysconfdir=/etc
+${THOME}/build/genpkg TRIBimage-libpng libpng-1.4.22 libpng-1.6.52
