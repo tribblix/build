0b66f22ca2e80fdc3923af35d643cbfb062777ce
Peter Tribble <peter.tribble@gmail.com>
2025-12-04 09:37:21 +0000

    Update github-archive 7.0.2

--- TRIBgithub-archive-python-312/build.sh
+++ TRIBgithub-archive-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBgithub-archive-python-312 github_archive-7.0.1
+${THOME}/build/pkg_pep518 -N TRIBgithub-archive-python-312 github_archive-7.0.2
