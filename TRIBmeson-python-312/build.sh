531eddd9a8494d4031019784982a1543da2d7fc4
Peter Tribble <peter.tribble@gmail.com>
2025-12-05 13:41:36 +0000

    Update meson 1.9.2

--- TRIBmeson-python-312/build.sh
+++ TRIBmeson-python-312/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# no source distribution was made available for 1.9.1
-#
-${THOME}/build/pkg_wheel TRIBmeson-python-312 meson-1.9.1-py3-none-any.whl
+${THOME}/build/pkg_pep518 -N TRIBmeson-python-312 meson-1.9.2
