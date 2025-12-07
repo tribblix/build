e7be5b5c5d317f4ca3cc1661f36f68602bbec16e
Peter Tribble <peter.tribble@gmail.com>
2025-12-07 17:20:41 +0000

    Update statsmodels 0.14.6

--- TRIBstatsmodels-python-312/build.sh
+++ TRIBstatsmodels-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBstatsmodels-python-312 statsmodels-0.14.5
+${THOME}/build/pkg_pep518 -N -q TRIBstatsmodels-python-312 statsmodels-0.14.6
