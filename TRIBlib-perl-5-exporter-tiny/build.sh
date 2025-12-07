55e5c30941257d64091956cb001ecfe3a3b77dc9
Peter Tribble <peter.tribble@gmail.com>
2025-12-07 17:57:44 +0000

    Update Exporter::Tiny 1.006003

--- TRIBlib-perl-5-exporter-tiny/build.sh
+++ TRIBlib-perl-5-exporter-tiny/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-${THOME}/build/unpack Exporter-Tiny-1.006002
-cd Exporter-Tiny-1.006002
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/unpack Exporter-Tiny-1.006003
+cd Exporter-Tiny-1.006003
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-exporter-tiny Exporter-Tiny-1.006002
+${THOME}/build/genpkg TRIBlib-perl-5-exporter-tiny Exporter-Tiny-1.006003
