241919b08e8c890188979d9e5597b6be1533c017
Peter Tribble <peter.tribble@gmail.com>
2025-12-08 17:21:36 +0000

    Update c-ares 1.34.6

--- TRIBlibcares/build.sh
+++ TRIBlibcares/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/dobuild +64 c-ares-1.34.5
-$THOME/build/genpkg TRIBlibcares c-ares-1.34.5
+$THOME/build/dobuild +64 c-ares-1.34.6
+$THOME/build/genpkg TRIBlibcares c-ares-1.34.6
