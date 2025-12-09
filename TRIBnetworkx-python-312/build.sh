1a497b480c93251d460e47fe89c34037ca5fa8d5
Peter Tribble <peter.tribble@gmail.com>
2025-12-09 10:01:49 +0000

    Update networkx 3.6.1

--- TRIBnetworkx-python-312/build.sh
+++ TRIBnetworkx-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # the dependencies we declare are those for networkx[default]
 #
-${THOME}/build/pkg_pep518 -N TRIBnetworkx-python-312 networkx-3.6
+${THOME}/build/pkg_pep518 -N TRIBnetworkx-python-312 networkx-3.6.1
