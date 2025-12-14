d488a783cbfe6e4473fb159091f0099dcc2f5b9c
Peter Tribble <peter.tribble@gmail.com>
2025-12-14 18:53:25 +0000

    Update fonttools 4.61.1

--- TRIBfonttools-python-312/build.sh
+++ TRIBfonttools-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBfonttools-python-312 fonttools-4.61.0
+${THOME}/build/pkg_pep518 -N TRIBfonttools-python-312 fonttools-4.61.1
