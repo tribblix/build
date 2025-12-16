f0ac73b7c8e8540270aeb1683919a780cf665b04
Peter Tribble <peter.tribble@gmail.com>
2025-12-16 09:09:34 +0000

    Update google-crc32c 1.8.0

--- TRIBgoogle-crc32c-python-312/build.sh
+++ TRIBgoogle-crc32c-python-312/build.sh
@@ -6,4 +6,4 @@
 # you don't need to explicitly force it, but by doing so you avoid
 # the scary errors when it tries and fails
 #
-env CRC32C_PURE_PYTHON=1 ${THOME}/build/pkg_setup_py TRIBgoogle-crc32c-python-312 google_crc32c-1.7.1
+env CRC32C_PURE_PYTHON=1 ${THOME}/build/pkg_pep518 -N TRIBgoogle-crc32c-python-312 google_crc32c-1.8.0
