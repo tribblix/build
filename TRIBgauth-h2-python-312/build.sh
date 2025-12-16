493b8e325984561fc2207c285f0704444b37dd95
Peter Tribble <peter.tribble@gmail.com>
2025-12-16 09:06:12 +0000

    Update google-auth-httplib2 0.3.0

--- TRIBgauth-h2-python-312/build.sh
+++ TRIBgauth-h2-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBgauth-h2-python-312 google_auth_httplib2-0.2.1
+${THOME}/build/pkg_pep518 -N TRIBgauth-h2-python-312 google_auth_httplib2-0.3.0
