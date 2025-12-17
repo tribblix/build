cf7d6bb84e2ad7f3ed679e578505b2fbf6a6c532
Peter Tribble <peter.tribble@gmail.com>
2025-12-17 13:57:18 +0000

    Update Sphinx 9.0.4

--- TRIBroman-numerals-python-312/build.sh
+++ TRIBroman-numerals-python-312/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals_py-3.1.0
+# this was roman-numerals-py, which is now deprecated
+# sphinx now depends directly on roman-numerals
+#
+${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals-4.0.0
--- TRIBsphinx-312/build.sh
+++ TRIBsphinx-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBsphinx-312 sphinx-8.2.3
+${THOME}/build/pkg_pep518 -N TRIBsphinx-312 sphinx-9.0.4
