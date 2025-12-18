73253c4c228ed19cd1bde1d769745e4555327fa4
Peter Tribble <peter.tribble@gmail.com>
2025-12-18 08:51:21 +0000

    Update linode-cli 5.65.0

--- TRIBlinode-cli-python-312/build.sh
+++ TRIBlinode-cli-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # there's a preconf file as well
 #
-${THOME}/build/pkg_pep518 -N TRIBlinode-cli-python-312 linode_cli-5.64.0
+${THOME}/build/pkg_pep518 -N TRIBlinode-cli-python-312 linode_cli-5.65.0
---
+++ TRIBpytimeparse-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpytimeparse-python-312 pytimeparse-1.1.8
