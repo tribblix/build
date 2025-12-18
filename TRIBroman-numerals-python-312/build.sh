25ba86cbcfaae719705e5e017a23dd0a671513c2
Peter Tribble <peter.tribble@gmail.com>
2025-12-18 08:41:10 +0000

    Update roman-numerals 4.1.0

--- TRIBroman-numerals-python-312/build.sh
+++ TRIBroman-numerals-python-312/build.sh
@@ -5,4 +5,4 @@
 # this was roman-numerals-py, which is now deprecated
 # sphinx now depends directly on roman-numerals
 #
-${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals-4.0.0
+${THOME}/build/pkg_pep518 -N TRIBroman-numerals-python-312 roman_numerals-4.1.0
