63daa83f78952c0ff13f62fd1f342d2eb3aab371
Peter Tribble <peter.tribble@gmail.com>
2025-12-18 14:52:05 +0000

    Update oslo.limit 2.9.2

--- TRIBoslo-limit-python-312/build.sh
+++ TRIBoslo-limit-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-limit-python-312 oslo_limit-2.9.1
+${THOME}/build/pkg_pep518 -N TRIBoslo-limit-python-312 oslo_limit-2.9.2
