be42ae91ed3cc1329f6c266ad5a544ba91e4d431
Peter Tribble <peter.tribble@gmail.com>
2025-12-18 08:40:04 +0000

    Update ActiveMQ 6.2.0

--- TRIBblix-activemq/build.sh
+++ TRIBblix-activemq/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_tarball -T TRIBblix-activemq apache-activemq-6.1.8-bin
+${THOME}/build/pkg_tarball -T TRIBblix-activemq apache-activemq-6.2.0-bin
