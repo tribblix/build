be8b0b63e91f61269f81490ed364025898ce71da
Peter Tribble <peter.tribble@gmail.com>
2025-12-19 09:40:01 +0000

    Update voluptuous 0.16.0

--- TRIBvoluptuous-python-312/build.sh
+++ TRIBvoluptuous-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBvoluptuous-python-312 voluptuous-0.15.2
+${THOME}/build/pkg_pep518 -N TRIBvoluptuous-python-312 voluptuous-0.16.0
