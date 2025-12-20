4c33254ff0d41e70e979c35115dec5b8ec3a1c5a
Peter Tribble <peter.tribble@gmail.com>
2025-12-20 20:58:38 +0000

    Update node_exporter 1.10.2

--- TRIBblix-promnode/build.sh
+++ TRIBblix-promnode/build.sh
@@ -12,18 +12,18 @@ cd p1
 #
 git clone https://github.com/prometheus/promu.git
 cd promu
-env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.24/bin:$PATH gmake build
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.25/bin:$PATH gmake build
 cd ..
 
 #
-$THOME/build/unpack node_exporter-1.9.1
-cd node_exporter-1.9.1
+$THOME/build/unpack node_exporter-1.10.2
+cd node_exporter-1.10.2
 #
 # cp collector/uname_bsd.go collector/uname_solaris.go
 # and change to build on solaris
 # and add solaris to the build of collector/uname.go
 #
-env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.24/bin:$PATH gmake build
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.25/bin:$PATH gmake build
 cd ..
 
 ${THOME}/build/genpkg TRIBblix-promnode
