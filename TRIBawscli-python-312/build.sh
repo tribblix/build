66537481d2768de8cd7f60774ff31db2de4d9ada
Peter Tribble <peter.tribble@gmail.com>
2025-12-20 17:17:04 +0000

    Update botocore 1.42.14 boto3 1.42.14 awscli 1.44.4

--- TRIBawscli-python-312/build.sh
+++ TRIBawscli-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # needs botocore colorama s3transfer docutils rsa yaml
 #
-${THOME}/build/pkg_pep518 -N TRIBawscli-python-312 awscli-1.43.15
+${THOME}/build/pkg_pep518 -N TRIBawscli-python-312 awscli-1.44.4
