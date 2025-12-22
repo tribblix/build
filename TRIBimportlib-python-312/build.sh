8b05b4ece74c68d8403620f725fb7b8801aeba2d
Peter Tribble <peter.tribble@gmail.com>
2025-12-22 08:41:29 +0000

    Update importlib_metadata 8.7.1

--- TRIBimportlib-python-312/build.sh
+++ TRIBimportlib-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBimportlib-python-312 importlib_metadata-8.7.0
+${THOME}/build/pkg_pep518 -N TRIBimportlib-python-312 importlib_metadata-8.7.1
