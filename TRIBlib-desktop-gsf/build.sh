a79ed9e92c46d71481290dd176e4218495b15e31
Peter Tribble <peter.tribble@gmail.com>
2025-12-22 15:50:47 +0000

    Update libgsf 1.14.54

--- TRIBlib-desktop-gsf/build.sh
+++ TRIBlib-desktop-gsf/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only -gnu libgsf-1.14.53
-${THOME}/build/genpkg TRIBlib-desktop-gsf libgsf-1.14.53
+${THOME}/build/dobuild -64only -gnu libgsf-1.14.54
+${THOME}/build/genpkg TRIBlib-desktop-gsf libgsf-1.14.54
