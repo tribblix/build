6418d56edda93145ec4972c8f098d4d438f9414c
Peter Tribble <peter.tribble@gmail.com>
2025-12-22 15:10:27 +0000

    Update testtools 2.8.2

--- TRIBoslo-db-python-312/build.sh
+++ TRIBoslo-db-python-312/build.sh
@@ -5,6 +5,4 @@
 # to simplify matters, bundle the 3 test modules into this packages as well,
 # as nothing else (that I can see) uses them
 #
-# note that testtools adds setuptools as a requirement
-#
-${THOME}/build/pkg_pep518 TRIBoslo-db-python-312 oslo_db-17.4.0 testresources-2.0.2 testscenarios-0.5.0 testtools-2.8.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-db-python-312 oslo_db-17.4.0 testresources-2.0.2 testscenarios-0.5.0 testtools-2.8.2
