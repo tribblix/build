2e5fe64678d83dcc445fcbafa1651dbab325085b
Peter Tribble <peter.tribble@gmail.com>
2025-12-22 15:46:45 +0000

    Update xterm 406

--- TRIBterminal-xterm/build.sh
+++ TRIBterminal-xterm/build.sh
@@ -6,5 +6,5 @@
 #
 # there's a postconf script for termios
 #
-$THOME/build/dobuild -64only xterm-401 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
-${THOME}/build/genpkg TRIBterminal-xterm xterm-401
+$THOME/build/dobuild -64only xterm-406 -C "--enable-256-color --enable-88-color --enable-wide-chars --disable-setuid --disable-setgid --enable-luit --disable-imake"
+${THOME}/build/genpkg TRIBterminal-xterm xterm-406
