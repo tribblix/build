0645f845645cd30402409f061b995200f188b268
Peter Tribble <peter.tribble@gmail.com>
2025-12-23 20:57:01 +0000

    Update libjxl 0.10.4

--- TRIBimage-libjxl/build.sh
+++ TRIBimage-libjxl/build.sh
@@ -8,5 +8,5 @@
 # we build the plugins and package, but don't declare a runtime dependency
 # the dependency is only used in the event it's resolved, so is unnecessary
 #
-${THOME}/build/cmbuild -64only libjxl-0.10.3 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE -DJPEGXL_ENABLE_PLUGINS=TRUE"
-${THOME}/build/genpkg TRIBimage-libjxl libjxl-0.10.3
+${THOME}/build/cmbuild -64only libjxl-0.10.4 -C "-DBUILD_TESTING=OFF -DJPEGXL_FORCE_SYSTEM_BROTLI=TRUE -DJPEGXL_FORCE_SYSTEM_LCMS2=TRUE -DJPEGXL_ENABLE_PLUGINS=TRUE"
+${THOME}/build/genpkg TRIBimage-libjxl libjxl-0.10.4
