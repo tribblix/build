a9cf602d18df129d5af4389b02bce05735f3527f
Peter Tribble <peter.tribble@gmail.com>
2025-12-24 15:55:54 +0000

    Update oslo.serialization 5.9.0

--- TRIBoslo-serial-python-312/build.sh
+++ TRIBoslo-serial-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-serial-python-312 oslo_serialization-5.8.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-serial-python-312 oslo_serialization-5.9.0
