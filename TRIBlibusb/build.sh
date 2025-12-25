6d2207991eedf318e5d7ff5c7e9b5c4248549b6f
Peter Tribble <peter.tribble@gmail.com>
2025-12-25 20:40:44 +0000

    Update libusb 1.0.29

--- TRIBlibusb/build.sh
+++ TRIBlibusb/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libusb-1.0.27
-${THOME}/build/genpkg TRIBlibusb libusb-1.0.27
+${THOME}/build/dobuild -64 libusb-1.0.29
+${THOME}/build/genpkg TRIBlibusb libusb-1.0.29
