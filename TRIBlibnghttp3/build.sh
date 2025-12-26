c12978a0c538ffe100db110cc1c9408c9e1c6d12
Peter Tribble <peter.tribble@gmail.com>
2025-12-26 09:31:06 +0000

    Update nghttp3 1.14.0

--- TRIBlibnghttp3/build.sh
+++ TRIBlibnghttp3/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 nghttp3-1.12.0
-${THOME}/build/genpkg TRIBlibnghttp3 nghttp3-1.12.0
+${THOME}/build/dobuild -64 nghttp3-1.14.0
+${THOME}/build/genpkg TRIBlibnghttp3 nghttp3-1.14.0
