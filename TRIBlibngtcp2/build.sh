45edf22cc8f14598f156f9d03f5a75a8ee9a327b
Peter Tribble <peter.tribble@gmail.com>
2025-12-26 09:28:28 +0000

    Update ngtcp2 1.19.0

--- TRIBlibngtcp2/build.sh
+++ TRIBlibngtcp2/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 ngtcp2-1.17.0
-${THOME}/build/genpkg TRIBlibngtcp2 ngtcp2-1.17.0
+${THOME}/build/dobuild -64 ngtcp2-1.19.0
+${THOME}/build/genpkg TRIBlibngtcp2 ngtcp2-1.19.0
