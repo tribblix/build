52ce75895446084c5ea444650aa4abfb4367de30
Peter Tribble <peter.tribble@gmail.com>
2025-12-26 10:11:47 +0000

    Update doxygen 1.15.0

--- TRIBdoxygen/build.sh
+++ TRIBdoxygen/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only doxygen-1.9.8
-${THOME}/build/genpkg TRIBdoxygen doxygen-1.9.8
+# there's a preconf script to stop invalid compiler flags
+#
+${THOME}/build/cmbuild -64only doxygen-1.15.0
+${THOME}/build/genpkg TRIBdoxygen doxygen-1.15.0
