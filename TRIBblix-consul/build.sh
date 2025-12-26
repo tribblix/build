2b42a7c1b2562a27d6282d169f4a908493d1774f
Peter Tribble <peter.tribble@gmail.com>
2025-12-26 16:28:18 +0000

    Update consul 1.22.2

--- TRIBblix-consul/build.sh
+++ TRIBblix-consul/build.sh
@@ -9,9 +9,9 @@ cd src/github.com/hashicorp/
 git clone https://github.com/hashicorp/consul
 cd consul
 #
-git checkout v1.21.5
+git checkout v1.22.2
 #
-env GOPATH=${HOME}/ud/c PATH=/usr/versions/go-1.24/bin:$PATH gmake
+env GOPATH=${HOME}/ud/c PATH=/usr/versions/go-1.25/bin:$PATH gmake
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/consul/bin
 cp bin/consul /tmp/ee/opt/tribblix/consul/bin
