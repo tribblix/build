a11d40be10940438fd39a999627b0e14781a8cc2
Peter Tribble <peter.tribble@gmail.com>
2026-01-08 20:50:51 +0000

    Update libtasn1 4.21.0

--- TRIBlibtasn1/build.sh
+++ TRIBlibtasn1/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libtasn1-4.19.0
-${THOME}/build/genpkg TRIBlibtasn1 libtasn1-4.19.0
+${THOME}/build/dobuild -64only libtasn1-4.21.0
+${THOME}/build/genpkg TRIBlibtasn1 libtasn1-4.21.0
