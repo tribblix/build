4c089572e1d1f08b4dbb15bfd7396a7d5fc1d807
Peter Tribble <peter.tribble@gmail.com>
2026-01-09 11:48:26 +0000

    Update python-manilaclient 5.7.1

--- TRIBmanilaclient-python-312/build.sh
+++ TRIBmanilaclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBmanilaclient-python-312 python_manilaclient-5.7.0
+${THOME}/build/pkg_pep518 -N TRIBmanilaclient-python-312 python_manilaclient-5.7.1
