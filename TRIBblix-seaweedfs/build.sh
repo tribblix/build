638b23512f58b5ab1eb9c3f7a6c1623c095cfd11
Peter Tribble <peter.tribble@gmail.com>
2026-01-09 15:21:34 +0000

    Update seaweedfs 4.05

--- TRIBblix-seaweedfs/build.sh
+++ TRIBblix-seaweedfs/build.sh
@@ -2,20 +2,13 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# check out the latest version
-#
 mkdir w
 cd w
-git clone https://github.com/seaweedfs/seaweedfs
-cd seaweedfs
-git checkout 3.99
-cd weed
-env PATH=/usr/versions/go-1.25/bin:$PATH GOPATH=`pwd` gmake
-cd ..
-#
+env PATH=/usr/versions/go-1.25/bin:$PATH GOPATH=`pwd` go install github.com/seaweedfs/seaweedfs/weed@4.05
+
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/seaweedfs/bin
-cp weed/bin/weed /tmp/ee/opt/tribblix/seaweedfs/bin
-cp README.md LICENSE /tmp/ee/opt/tribblix/seaweedfs
+cp bin/weed /tmp/ee/opt/tribblix/seaweedfs/bin
+cp pkg/mod/github.com/seaweedfs/seaweedfs@v*/README.md pkg/mod/github.com/seaweedfs/seaweedfs@v*/LICENSE /tmp/ee/opt/tribblix/seaweedfs
 ${THOME}/build/create_pkg TRIBblix-seaweedfs /tmp/ee
 rm -fr /tmp/ee
