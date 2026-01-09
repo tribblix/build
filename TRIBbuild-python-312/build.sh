01861522d873c6a7315cebd3156f186a4fbb621a
Peter Tribble <peter.tribble@gmail.com>
2026-01-09 13:40:28 +0000

    Update python build 1.4.0

--- TRIBbuild-python-312/build.sh
+++ TRIBbuild-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # this is more internal, so inline pyproject_hooks
 #
-${THOME}/build/pkg_pep518 -N TRIBbuild-python-312 build-1.3.0 pyproject_hooks-1.2.0
+${THOME}/build/pkg_pep518 -N TRIBbuild-python-312 build-1.4.0 pyproject_hooks-1.2.0
