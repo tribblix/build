b921e572313be6e0c926ee142dca807c1ee24dfb
Peter Tribble <peter.tribble@gmail.com>
2026-01-09 16:02:31 +0000

    Update fonttosfnt 1.2.5

--- TRIBx11-fonttosfnt/build.sh
+++ TRIBx11-fonttosfnt/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only fonttosfnt-1.2.4
-${THOME}/build/genpkg TRIBx11-fonttosfnt fonttosfnt-1.2.4
+${THOME}/build/dobuild -64only fonttosfnt-1.2.5
+${THOME}/build/genpkg TRIBx11-fonttosfnt fonttosfnt-1.2.5
