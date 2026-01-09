df18f5b58e97ee8a219109949dccec15319fb915
Peter Tribble <peter.tribble@gmail.com>
2026-01-09 11:58:09 +0000

    Update PyNaCl 1.6.2

--- TRIBpynacl-python-312/build.sh
+++ TRIBpynacl-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env SODIUM_INSTALL=system ${THOME}/build/pkg_pep518 -N TRIBpynacl-python-312 pynacl-1.6.1
+env SODIUM_INSTALL=system ${THOME}/build/pkg_pep518 -N TRIBpynacl-python-312 pynacl-1.6.2
