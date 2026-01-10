e3580f15556cda2c2b01000e035bb108ce872d01
Peter Tribble <peter.tribble@gmail.com>
2026-01-10 09:46:55 +0000

    Update Perl::Tidy 20260109

--- TRIBlib-perl-5-perl-tidy/build.sh
+++ TRIBlib-perl-5-perl-tidy/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Perl-Tidy-20250912
-cd Perl-Tidy-20250912
+${THOME}/build/unpack Perl-Tidy-20260109
+cd Perl-Tidy-20260109
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-perl-tidy Perl-Tidy-20250912
+${THOME}/build/genpkg TRIBlib-perl-5-perl-tidy Perl-Tidy-20260109
