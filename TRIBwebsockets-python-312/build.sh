a639812c544b275f8aa24dc97336625000add4e9
Peter Tribble <peter.tribble@gmail.com>
2026-01-11 11:05:45 +0000

    Update websockets 16.0

--- TRIBwebsockets-python-312/build.sh
+++ TRIBwebsockets-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwebsockets-python-312 websockets-15.0.1
+${THOME}/build/pkg_pep518 -N TRIBwebsockets-python-312 websockets-16.0
