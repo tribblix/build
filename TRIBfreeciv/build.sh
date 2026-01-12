49d57d521b8d03027cedfd33fc0cbbeacd9180a7
Peter Tribble <peter.tribble@gmail.com>
2026-01-12 13:41:29 +0000

    Update freeciv 3.1.6

--- TRIBfreeciv/build.sh
+++ TRIBfreeciv/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/dobuild -64only freeciv-3.1.2 -C "--enable-ipv6=test --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBfreeciv freeciv-3.1.2
+$THOME/build/dobuild -64only freeciv-3.1.6 -C "--enable-ipv6=test --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBfreeciv freeciv-3.1.6
