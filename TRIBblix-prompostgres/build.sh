918e0d5a47fa53046bbe54485aa7924a0c79dfdc
Peter Tribble <peter.tribble@gmail.com>
2026-01-12 14:04:40 +0000

    Rebuild postgres_exporter with go 1.25

--- TRIBblix-prompostgres/build.sh
+++ TRIBblix-prompostgres/build.sh
@@ -12,12 +12,12 @@ cd p1
 #
 git clone https://github.com/prometheus/promu.git
 cd promu
-env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.24/bin:$PATH gmake build
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.25/bin:$PATH gmake build
 cd ..
 
 $THOME/build/unpack postgres_exporter-0.18.1
 cd postgres_exporter-0.18.1
-env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.24/bin:$PATH gmake build
+env GOPATH=`pwd`/.. PATH=/usr/versions/go-1.25/bin:$PATH gmake build
 
 ${THOME}/build/genpkg TRIBblix-prompostgres
 cd ..
