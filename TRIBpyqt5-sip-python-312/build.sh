ca235518eac311f70fd07b8b668ecc9d40834ceb
Peter Tribble <peter.tribble@gmail.com>
2026-01-14 09:21:49 +0000

    Update PyQt5_sip 12.18.0

--- TRIBpyqt5-sip-python-312/build.sh
+++ TRIBpyqt5-sip-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpyqt5-sip-python-312 pyqt5_sip-12.17.2
+${THOME}/build/pkg_pep518 -N TRIBpyqt5-sip-python-312 pyqt5_sip-12.18.0
