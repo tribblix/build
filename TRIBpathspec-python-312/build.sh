59f1a48a45d7704e74a49ddf036d53f3f2ad3880
Peter Tribble <peter.tribble@gmail.com>
2026-01-15 10:49:29 +0000

    Update pathspec 1.0.3

--- TRIBpathspec-python-312/build.sh
+++ TRIBpathspec-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBpathspec-python-312 pathspec-0.12.1
+${THOME}/build/pkg_pep518 -N TRIBpathspec-python-312 pathspec-1.0.3
--- TRIBpathspec-python-313/build.sh
+++ TRIBpathspec-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpathspec-python-313 pathspec-0.12.1
+${THOME}/build/pkg_pep518 -N TRIBpathspec-python-313 pathspec-1.0.3
