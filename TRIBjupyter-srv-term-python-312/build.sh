54ecd9c093a952f82e33bfd8120dc25433ee0eb9
Peter Tribble <peter.tribble@gmail.com>
2026-01-15 10:13:35 +0000

    Update jupyter_server_terminals 0.5.4

--- TRIBjupyter-srv-term-python-312/build.sh
+++ TRIBjupyter-srv-term-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBjupyter-srv-term-python-312 jupyter_server_terminals-0.5.3
+${THOME}/build/pkg_pep518 -N TRIBjupyter-srv-term-python-312 jupyter_server_terminals-0.5.4
