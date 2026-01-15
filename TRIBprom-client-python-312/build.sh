5bf436070413ec3be0e7e72acf74ab574ec52b99
Peter Tribble <peter.tribble@gmail.com>
2026-01-15 10:15:21 +0000

    Update python prometheus_client 0.24.1

--- TRIBprom-client-python-312/build.sh
+++ TRIBprom-client-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBprom-client-python-312 prometheus_client-0.24.0
+${THOME}/build/pkg_pep518 -N TRIBprom-client-python-312 prometheus_client-0.24.1
