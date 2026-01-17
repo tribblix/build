31a8618cfc1b679ad2c2cfcc9d4365248d4aed2e
Peter Tribble <peter.tribble@gmail.com>
2026-01-17 12:15:59 +0000

    Update gmake 4.1.1

--- TRIBdev-build-gnu-make/build.sh
+++ TRIBdev-build-gnu-make/build.sh
@@ -2,8 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# Note that 3.82 introduced major incompatibilities; these have been
-# reverted in 4.1 so that 4.1 should be safe
+# There's a patch so that CC defaults to gcc rather than cc
 #
-${THOME}/build/dobuild -64only make-4.3 -P /usr/gnu
-${THOME}/build/genpkg TRIBdev-build-gnu-make make-4.3
+${THOME}/build/dobuild -64only make-4.4.1 -P /usr/gnu
+${THOME}/build/genpkg TRIBdev-build-gnu-make make-4.4.1
---
+++ patches/make-4.4.1.pls
@@ -0,0 +1,1 @@
+-p1 make-gcc-default.patch
