27fdb150e704940f37f389fb9081f6da52b5f66c
Peter Tribble <peter.tribble@gmail.com>
2026-01-17 12:00:35 +0000

    Update gnu gettext 0.26

--- TRIBgnu-gettext/build.sh
+++ TRIBgnu-gettext/build.sh
@@ -6,5 +6,5 @@
 # embedded, but fortunately I haven't (yet) found any consumers so updating
 # should be safe
 #
-${THOME}/build/dobuild -64only gettext-0.22.5 -P /usr/gnu -C "--with-included-glib --with-included-libcroco --disable-curses --without-libiconv-prefix --disable-libasprintf --disable-openmp --with-included-libunistring --disable-static"
-${THOME}/build/genpkg TRIBgnu-gettext gettext-0.22.5
+${THOME}/build/dobuild -64only gettext-0.26 -P /usr/gnu -C "--with-included-glib --with-included-libcroco --disable-curses --without-libiconv-prefix --disable-libasprintf --disable-openmp --with-included-libunistring --disable-static"
+${THOME}/build/genpkg TRIBgnu-gettext gettext-0.26
