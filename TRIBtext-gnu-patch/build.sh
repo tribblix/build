1c6bb007bd8cd20f06176386165836f54e06e857
Peter Tribble <peter.tribble@gmail.com>
2026-01-17 12:07:52 +0000

    Update gnu patch 2.8

--- TRIBtext-gnu-patch/build.sh
+++ TRIBtext-gnu-patch/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -gnu +64only patch-2.7.6 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-patch patch-2.7.6
+${THOME}/build/dobuild -gnu +64only patch-2.8 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-patch patch-2.8
