e0204c0f483593b235fcabec83f7c905f0214ad9
Peter Tribble <peter.tribble@gmail.com>
2026-01-18 18:48:54 +0000

    Update poetry-core 2.3.0

--- TRIBpoetry-core-python-313/build.sh
+++ TRIBpoetry-core-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpoetry-core-python-313 poetry_core-2.2.1
+${THOME}/build/pkg_pep518 -N TRIBpoetry-core-python-313 poetry_core-2.3.0
