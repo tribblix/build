41b9b64356e3cf4b750c37818c472cbdf504474f
Peter Tribble <peter.tribble@gmail.com>
2026-01-19 10:03:58 +0000

    Update pytokens 0.4.0

--- TRIBpytokens-python-312/build.sh
+++ TRIBpytokens-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBpytokens-python-312 pytokens-0.3.0
+# PYTOKENS_USE_MYPYC turns off mypyc which doesn't build
+#
+env PYTOKENS_USE_MYPYC=0 ${THOME}/build/pkg_pep518 -N TRIBpytokens-python-312 pytokens-0.4.0
