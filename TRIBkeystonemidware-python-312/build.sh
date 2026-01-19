fbaea28edecee2e4f6df62d2f7577eddb05d263c
Peter Tribble <peter.tribble@gmail.com>
2026-01-19 10:23:55 +0000

    Update keystonemiddleware 11.0.0

--- TRIBkeystonemidware-python-312/build.sh
+++ TRIBkeystonemidware-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBkeystonemidware-python-312 keystonemiddleware-10.12.0
+${THOME}/build/pkg_pep518 -N TRIBkeystonemidware-python-312 keystonemiddleware-11.0.0
