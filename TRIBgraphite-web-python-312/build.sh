9cc85e6cc8550b131d27b48287ec07e61aec287f
Peter Tribble <peter.tribble@gmail.com>
2026-01-19 18:08:54 +0000

    Update graphite-web, better dependencies, create users

--- TRIBgraphite-web-python-312/build.sh
+++ TRIBgraphite-web-python-312/build.sh
@@ -8,6 +8,6 @@
 # script that moves that to /opt/tribblix/graphite
 #
 # note also that we use the latest source, as the versions on PyPI
-# are very old, the g0 in the package version reflects that
+# are very old, the g suffix in the package version reflects that
 #
 ${THOME}/build/pkg_setup_py TRIBgraphite-web-python-312 https://github.com/graphite-project/graphite-web
