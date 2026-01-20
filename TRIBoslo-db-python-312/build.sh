5324d8e24e7c3b58e1dbc9044a96b13f6971e4b6
Peter Tribble <peter.tribble@gmail.com>
2026-01-20 18:23:46 +0000

    Update oslo.db 18.0.0

--- TRIBoslo-db-python-312/build.sh
+++ TRIBoslo-db-python-312/build.sh
@@ -5,4 +5,4 @@
 # to simplify matters, bundle the 3 test modules into this packages as well,
 # as nothing else (that I can see) uses them
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-db-python-312 oslo_db-17.4.0 testresources-2.0.2 testscenarios-0.5.0 testtools-2.8.2
+${THOME}/build/pkg_pep518 -N TRIBoslo-db-python-312 oslo_db-18.0.0 testresources-2.0.2 testscenarios-0.5.0 testtools-2.8.2
