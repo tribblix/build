8082041c5455bcbebcead43e10e72f97654c11c9
Peter Tribble <peter.tribble@gmail.com>
2026-01-20 18:26:34 +0000

    Update pgBackRest 2.58.0

--- TRIBpgbackrest/build.sh
+++ TRIBpgbackrest/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_LDFLAGS="-R/opt/tribblix/postgres17/lib -lsocket -lnsl" TRIBBLIX_CFLAGS=-D__EXTENSIONS__ PATH=/opt/tribblix/postgres17/bin:$PATH TRIBBLIX_PKG_CONFIG_PATH=/opt/tribblix/postgres17/lib/pkgconfig $THOME/build/mesonbuild -64only pgbackrest-release-2.55.1
-${THOME}/build/genpkg TRIBpgbackrest pgbackrest-release-2.55.1
+env TRIBBLIX_LDFLAGS="-R/opt/tribblix/postgres17/lib -lsocket -lnsl" TRIBBLIX_CFLAGS=-D__EXTENSIONS__ PATH=/opt/tribblix/postgres17/bin:$PATH TRIBBLIX_PKG_CONFIG_PATH=/opt/tribblix/postgres17/lib/pkgconfig $THOME/build/mesonbuild -64only pgbackrest-release-2.58.0
+${THOME}/build/genpkg TRIBpgbackrest pgbackrest-release-2.58.0
