ad6e017741e0919d064a8cc330c6e7612f8dc823
Peter Tribble <peter.tribble@gmail.com>
2026-01-23 11:30:10 +0000

    Update wheel 0.46.3

--- TRIBwheel-python-312/build.sh
+++ TRIBwheel-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwheel-python-312 wheel-0.46.2
+${THOME}/build/pkg_pep518 -N TRIBwheel-python-312 wheel-0.46.3
--- TRIBwheel-python-313/build.sh
+++ TRIBwheel-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwheel-python-313 wheel-0.46.2
+${THOME}/build/pkg_pep518 -N TRIBwheel-python-313 wheel-0.46.3
