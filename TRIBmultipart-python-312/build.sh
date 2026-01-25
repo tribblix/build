2ae9d6bdfc66724ee1936bdb76e5581ce7599f8b
Peter Tribble <peter.tribble@gmail.com>
2026-01-25 11:32:36 +0000

    Update python-multipart 0.0.22

--- TRIBmultipart-python-312/build.sh
+++ TRIBmultipart-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBmultipart-python-312 python_multipart-0.0.21
+${THOME}/build/pkg_pep518 -N TRIBmultipart-python-312 python_multipart-0.0.22
