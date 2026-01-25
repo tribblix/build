4eece4da09d39dfc67f0839a03edc2699689d803
Peter Tribble <peter.tribble@gmail.com>
2026-01-25 11:39:55 +0000

    Update xfd 1.1.5

--- TRIBx11-xfd/build.sh
+++ TRIBx11-xfd/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xfd-1.1.4
-${THOME}/build/genpkg TRIBx11-xfd xfd-1.1.4
+${THOME}/build/dobuild -64only xfd-1.1.5
+${THOME}/build/genpkg TRIBx11-xfd xfd-1.1.5
