4fb305ff6d50210498bd0a92fded655ebc0c5b2e
Peter Tribble <peter.tribble@gmail.com>
2026-01-25 18:37:01 +0000

    Update stunnel 5.76

--- TRIBstunnel/build.sh
+++ TRIBstunnel/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only stunnel-5.74 -C "--sysconfdir=/etc --localstatedir=/var --disable-libwrap"
-${THOME}/build/genpkg TRIBstunnel stunnel-5.74
+env LIBS="-lsocket -lnsl" ${THOME}/build/dobuild -64only stunnel-5.76 -C "--sysconfdir=/etc --localstatedir=/var --disable-libwrap"
+${THOME}/build/genpkg TRIBstunnel stunnel-5.76
