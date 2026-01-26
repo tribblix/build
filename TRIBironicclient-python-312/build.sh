d76a6bee92d6a9fe9a0e06331880de87d305a430
Peter Tribble <peter.tribble@gmail.com>
2026-01-26 21:13:27 +0000

    Update python-ironicclient 5.15.0

--- TRIBironicclient-python-312/build.sh
+++ TRIBironicclient-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # the [cli] extra adds openstackclient as a dependency
 #
-${THOME}/build/pkg_setup_py TRIBironicclient-python-312 python_ironicclient-5.14.0
+${THOME}/build/pkg_setup_py TRIBironicclient-python-312 python_ironicclient-5.15.0
