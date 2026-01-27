326085f01c3370e1eb5b77162dd63f8fb263fdc2
Peter Tribble <peter.tribble@gmail.com>
2026-01-27 08:38:44 +0000

    Update xkill 1.0.7

--- TRIBx11-xkill/build.sh
+++ TRIBx11-xkill/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xkill-1.0.6
-${THOME}/build/genpkg TRIBx11-xkill xkill-1.0.6
+${THOME}/build/dobuild -64only xkill-1.0.7
+${THOME}/build/genpkg TRIBx11-xkill xkill-1.0.7
