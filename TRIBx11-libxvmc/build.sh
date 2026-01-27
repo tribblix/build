a9f7f0fcd74d9238da4f027c2452b9d68e2ec1ae
Peter Tribble <peter.tribble@gmail.com>
2026-01-27 09:09:49 +0000

    Update libXvMC 1.0.15

--- TRIBx11-libxvmc/build.sh
+++ TRIBx11-libxvmc/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/mesonbuild -64only libXvMC-1.0.14
-${THOME}/build/genpkg TRIBx11-libxvmc libXvMC-1.0.14
+${THOME}/build/mesonbuild -64only libXvMC-1.0.15
+${THOME}/build/genpkg TRIBx11-libxvmc libXvMC-1.0.15
