6b9d33aea89b34ca6a89769275a05b1869a3e47c
Peter Tribble <peter.tribble@gmail.com>
2026-01-27 09:07:01 +0000

    Update libXxf86vm 1.1.7

--- TRIBx11-libxxf86vm/build.sh
+++ TRIBx11-libxxf86vm/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXxf86vm-1.1.6
-${THOME}/build/genpkg TRIBx11-libxxf86vm libXxf86vm-1.1.6
+${THOME}/build/dobuild -64 libXxf86vm-1.1.7
+${THOME}/build/genpkg TRIBx11-libxxf86vm libXxf86vm-1.1.7
