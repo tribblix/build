be18e4240b931282d0db60454d134d06d3104da6
Peter Tribble <peter.tribble@gmail.com>
2026-01-27 08:58:08 +0000

    Update bitmap 1.1.2

--- TRIBx11-bitmap/build.sh
+++ TRIBx11-bitmap/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only bitmap-1.1.1
-${THOME}/build/genpkg TRIBx11-bitmap bitmap-1.1.1
+${THOME}/build/dobuild -64only bitmap-1.1.2
+${THOME}/build/genpkg TRIBx11-bitmap bitmap-1.1.2
