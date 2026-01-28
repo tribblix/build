c3da91c0103455a24f1220a4b7fc92dbda6e093b
Peter Tribble <peter.tribble@gmail.com>
2026-01-28 10:44:40 +0000

    Update Apache Arrow 23.0.0

--- TRIBapache-arrow/build.sh
+++ TRIBapache-arrow/build.sh
@@ -9,7 +9,7 @@
 # as in https://www.illumos.org/issues/16523
 #
 # as the SONAME gets bumped, consumers (ie pyarrow) will need to
-# be update and rebuilt at the same time as this package
+# be updated and rebuilt at the same time as this package
 #
-$THOME/build/cmbuild -64only -S cpp apache-arrow-22.0.0 -C "-DARROW_JEMALLOC=OFF -DARROW_BUILD_STATIC=OFF -DARROW_COMPUTE=ON -DARROW_FILESYSTEM=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_JSON=ON -DARROW_PARQUET=ON -DCMAKE_PREFIX_PATH=/usr/lib/`$THOME/build/getarch`/cmake"
-${THOME}/build/genpkg TRIBapache-arrow apache-arrow-22.0.0
+$THOME/build/cmbuild -64only -S cpp apache-arrow-23.0.0 -C "-DARROW_JEMALLOC=OFF -DARROW_BUILD_STATIC=OFF -DARROW_COMPUTE=ON -DARROW_FILESYSTEM=ON -DARROW_CSV=ON -DARROW_DATASET=ON -DARROW_JSON=ON -DARROW_PARQUET=ON -DCMAKE_PREFIX_PATH=/usr/lib/`$THOME/build/getarch`/cmake"
+${THOME}/build/genpkg TRIBapache-arrow apache-arrow-23.0.0
