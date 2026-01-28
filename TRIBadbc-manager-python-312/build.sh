7c008af0d4beb424545fc72a05c3d2a2d4ac40d8
Peter Tribble <peter.tribble@gmail.com>
2026-01-28 11:44:25 +0000

    Update adbc python modules 1.10.0

--- TRIBadbc-manager-python-312/build.sh
+++ TRIBadbc-manager-python-312/build.sh
@@ -7,4 +7,4 @@
 #
 # needs a patch. just like adbc itself
 #
-${THOME}/build/pkg_setup_py TRIBadbc-manager-python-312 adbc_driver_manager-1.9.0
+${THOME}/build/pkg_pep518 -N TRIBadbc-manager-python-312 adbc_driver_manager-1.10.0
--- TRIBadbc-postgresql-python-312/build.sh
+++ TRIBadbc-postgresql-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # dependencies added for the [dbapi] extension
 #
-env ADBC_POSTGRESQL_LIBRARY=/usr/lib/amd64/libadbc_driver_postgresql.so ${THOME}/build/pkg_setup_py TRIBadbc-postgresql-python-312 adbc_driver_postgresql-1.9.0
+env ADBC_POSTGRESQL_LIBRARY=/usr/lib/amd64/libadbc_driver_postgresql.so ${THOME}/build/pkg_pep518 -N TRIBadbc-postgresql-python-312 adbc_driver_postgresql-1.10.0
