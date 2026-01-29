bcd61104a2fbfb4745bae1e7d76802143e6f2ef0
Peter Tribble <peter.tribble@gmail.com>
2026-01-29 09:08:41 +0000

    Update forgejo 14.0.2

--- TRIBblix-forgejo/build.sh
+++ TRIBblix-forgejo/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/unpack forgejo-src-13.0.4
-cd forgejo-src-13.0.4
+$THOME/build/unpack forgejo-src-14.0.2
+cd forgejo-src-14.0.2
 #
 # download the proper tarball, not the automated source dump
 # this way, the assets are pregenerated and all you have to do
