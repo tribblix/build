a4cd1eb14004eb7eef4791fc7446745287293e77
Peter Tribble <peter.tribble@gmail.com>
2026-01-29 09:48:40 +0000

    Update libXcomposite 0.4.7

--- TRIBx11-libxcomposite/build.sh
+++ TRIBx11-libxcomposite/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXcomposite-0.4.6
-${THOME}/build/genpkg TRIBx11-libxcomposite libXcomposite-0.4.6
+${THOME}/build/dobuild -64 libXcomposite-0.4.7
+${THOME}/build/genpkg TRIBx11-libxcomposite libXcomposite-0.4.7
