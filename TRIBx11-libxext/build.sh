8ad5b1efb8a5f36653ec40ef595ece1183cec9a7
Peter Tribble <peter.tribble@gmail.com>
2026-01-29 10:41:57 +0000

    Update libXext 1.3.7

--- TRIBx11-libxext/build.sh
+++ TRIBx11-libxext/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXext-1.3.6 -C CPPFLAGS=-DSUNSOFT
-${THOME}/build/genpkg TRIBx11-libxext libXext-1.3.6
+${THOME}/build/dobuild -64 libXext-1.3.7 -C CPPFLAGS=-DSUNSOFT
+${THOME}/build/genpkg TRIBx11-libxext libXext-1.3.7
