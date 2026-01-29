dc6d780bc13cb8c0223f98f7cf6f65a9998a47cb
Peter Tribble <peter.tribble@gmail.com>
2026-01-29 10:01:13 +0000

    Update libXinerama 1.1.6

--- TRIBx11-libxinerama/build.sh
+++ TRIBx11-libxinerama/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXinerama-1.1.5
-${THOME}/build/genpkg TRIBx11-libxinerama libXinerama-1.1.5
+${THOME}/build/dobuild -64 libXinerama-1.1.6
+${THOME}/build/genpkg TRIBx11-libxinerama libXinerama-1.1.6
