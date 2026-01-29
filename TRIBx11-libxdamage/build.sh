dfdfdddc10a57804460fd58658791a8c3017d849
Peter Tribble <peter.tribble@gmail.com>
2026-01-29 10:20:52 +0000

    Update libXdamage 1.1.7

--- TRIBx11-libxdamage/build.sh
+++ TRIBx11-libxdamage/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXdamage-1.1.6
-${THOME}/build/genpkg TRIBx11-libxdamage libXdamage-1.1.6
+${THOME}/build/dobuild -64 libXdamage-1.1.7
+${THOME}/build/genpkg TRIBx11-libxdamage libXdamage-1.1.7
