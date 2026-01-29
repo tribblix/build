b5fe269d0f3d26fb29755eed56f6951199335913
Peter Tribble <peter.tribble@gmail.com>
2026-01-29 10:16:46 +0000

    Update libXrandr 1.5.5

--- TRIBx11-libxrandr/build.sh
+++ TRIBx11-libxrandr/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXrandr-1.5.4
-${THOME}/build/genpkg TRIBx11-libxrandr libXrandr-1.5.4
+${THOME}/build/dobuild -64 libXrandr-1.5.5
+${THOME}/build/genpkg TRIBx11-libxrandr libXrandr-1.5.5
