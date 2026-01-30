fc232e51ae31d2b3c0dbb166f22da60210837742
Peter Tribble <peter.tribble@gmail.com>
2026-01-30 10:38:31 +0000

    Update pdm-backend 2.4.7

--- TRIBpdm-backend-python-312/build.sh
+++ TRIBpdm-backend-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpdm-backend-python-312 pdm_backend-2.4.6
+${THOME}/build/pkg_pep518 -N TRIBpdm-backend-python-312 pdm_backend-2.4.7
