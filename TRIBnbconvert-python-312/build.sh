e62cef1e51bc7cb07065ce793a580dfd99cc17cc
Peter Tribble <peter.tribble@gmail.com>
2026-02-02 09:08:18 +0000

    Update nbconvert 7.17.0

--- TRIBnbconvert-python-312/build.sh
+++ TRIBnbconvert-python-312/build.sh
@@ -5,4 +5,4 @@
 # we add an explicit dependency on tinycss2 to satisfy the declared
 # dependency on bleach[css]
 #
-${THOME}/build/pkg_pep518 -N TRIBnbconvert-python-312 nbconvert-7.16.6
+${THOME}/build/pkg_pep518 -N TRIBnbconvert-python-312 nbconvert-7.17.0
