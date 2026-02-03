4e24cac7bcda984258626354ae672a884eeba1f9
Peter Tribble <peter.tribble@gmail.com>
2026-02-03 09:18:03 +0000

    Update proto-plus 1.27.1

--- TRIBproto-plus-python-312/build.sh
+++ TRIBproto-plus-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBproto-plus-python-312 proto_plus-1.27.0
+${THOME}/build/pkg_pep518 -N TRIBproto-plus-python-312 proto_plus-1.27.1
