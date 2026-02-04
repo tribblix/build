5797b4211e9021b2bb25476e2294a5ea553ee72f
Peter Tribble <peter.tribble@gmail.com>
2026-02-04 19:50:39 +0000

    Udpate fastfetch 2.58.0

--- TRIBfastfetch/build.sh
+++ TRIBfastfetch/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only fastfetch-2.30.1 -C -DENABLE_DRM=OFF
-${THOME}/build/genpkg TRIBfastfetch fastfetch-2.30.1
+${THOME}/build/cmbuild -64only fastfetch-2.58.0 -C -DENABLE_DRM=OFF
+${THOME}/build/genpkg TRIBfastfetch fastfetch-2.58.0
