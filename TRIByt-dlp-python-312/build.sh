8ad8ef873bfc1cca10dbbb1d3d646d92fea869a2
Peter Tribble <peter.tribble@gmail.com>
2026-02-04 08:56:58 +0000

    Update yt-dlp 2026.2.4

--- TRIByt-dlp-python-312/build.sh
+++ TRIByt-dlp-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # dependencies are for the [default] extra
 #
-${THOME}/build/pkg_pep518 -N TRIByt-dlp-python-312 yt_dlp-2026.1.31
+${THOME}/build/pkg_pep518 -N TRIByt-dlp-python-312 yt_dlp-2026.2.4
