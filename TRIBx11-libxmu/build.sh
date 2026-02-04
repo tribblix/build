865ee50ce6559c693ff33ef01f2ed16f5e378913
Peter Tribble <peter.tribble@gmail.com>
2026-02-04 19:37:54 +0000

    Update libXmu 1.3.1

--- TRIBx11-libxmu/build.sh
+++ TRIBx11-libxmu/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/X11/xcms ${THOME}/build/dobuild -64 libXmu-1.2.1
-${THOME}/build/genpkg TRIBx11-libxmu libXmu-1.2.1
+env TRIBBLIX_CFLAGS=-I/usr/include/X11/xcms ${THOME}/build/dobuild -A -64 libXmu-1.3.1
+${THOME}/build/genpkg TRIBx11-libxmu libXmu-1.3.1
