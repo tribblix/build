9a8dd6c82314989c062222a3cd73cc44844a23b6
Peter Tribble <peter.tribble@gmail.com>
2026-02-05 20:01:10 +0000

    Update pip 26.0.1

--- TRIBpip-python-313/build.sh
+++ TRIBpip-python-313/build.sh
@@ -5,4 +5,4 @@
 # see bootstrap.sh for how to build pip the first time, as pkg_pep518
 # assumes that pip is already installed
 #
-${THOME}/build/pkg_pep518 TRIBpip-python-313 pip-26.0
+${THOME}/build/pkg_pep518 TRIBpip-python-313 pip-26.0.1
