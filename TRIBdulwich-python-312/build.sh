d609305b2b9fc64b9886862627221d76162796de
Peter Tribble <peter.tribble@gmail.com>
2026-02-07 12:15:56 +0000

    Update breezy 3.3.21 dulwich 1.0.0

--- TRIBdulwich-python-312/build.sh
+++ TRIBdulwich-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # breezy sometimes pins a specific version
 #
-${THOME}/build/pkg_pep518 -N TRIBdulwich-python-312 dulwich-0.24.10
+${THOME}/build/pkg_pep518 -N TRIBdulwich-python-312 dulwich-1.0.0
