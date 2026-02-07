cea26fee7435127aeab7ea64c1adbb1441e0016f
Peter Tribble <peter.tribble@gmail.com>
2026-02-07 12:09:35 +0000

    Update python gnureadline 8.3.3

--- TRIBgnureadline-python-312/build.sh
+++ TRIBgnureadline-python-312/build.sh
@@ -5,4 +5,4 @@
 # we need -m64 in CFLAGS here because it's passed through to the
 # build of the private copy of readline
 #
-env CFLAGS="-m64 -I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/pkg_setup_py TRIBgnureadline-python-312 gnureadline-8.2.13
+env CFLAGS="-m64 -I/usr/gnu/include" LDFLAGS="-L/usr/gnu/lib/`isainfo -k` -R/usr/gnu/lib/`isainfo -k`" ${THOME}/build/pkg_pep518 -N TRIBgnureadline-python-312 gnureadline-8.3.3
