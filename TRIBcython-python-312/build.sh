c864f315935099e580276ee12b5609a9c54c44c0
Peter Tribble <peter.tribble@gmail.com>
2026-02-07 20:25:03 +0000

    Update Cython 3.1.8

--- TRIBcython-python-312/build.sh
+++ TRIBcython-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBcython-python-312 cython-3.0.12
+# there's a limited range supported by the scipy stack
+#
+${THOME}/build/pkg_pep518 -N TRIBcython-python-312 cython-3.1.8
