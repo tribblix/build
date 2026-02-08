788515db02897f07682d7d3cac761db96fea44be
Peter Tribble <peter.tribble@gmail.com>
2026-02-08 09:39:19 +0000

    Update xcalc 1.1.3

--- TRIBx11-xcalc/build.sh
+++ TRIBx11-xcalc/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xcalc-1.1.2
-${THOME}/build/genpkg TRIBx11-xcalc xcalc-1.1.2
+${THOME}/build/dobuild -64only xcalc-1.1.3
+${THOME}/build/genpkg TRIBx11-xcalc xcalc-1.1.3
