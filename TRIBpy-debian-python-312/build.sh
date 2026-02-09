907acb3be48ce1d8f0429ad7e6fc7f60ad825338
Peter Tribble <peter.tribble@gmail.com>
2026-02-09 09:27:43 +0000

    Update python-debian 1.1.0

--- TRIBpy-debian-python-312/build.sh
+++ TRIBpy-debian-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpy-debian-python-312 python-debian-1.0.1
+${THOME}/build/pkg_pep518 -N TRIBpy-debian-python-312 python_debian-1.1.0
