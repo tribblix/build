544fe4fb8c304a3faa334fdba4e110cd8deb99ed
Peter Tribble <peter.tribble@gmail.com>
2026-02-09 19:36:49 +0000

    Update gnutls 3.8.12

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -5,5 +5,8 @@
 # disable tpm because the code isn't even self-consistent and autoconf
 # can't work out what to do on its own
 #
-${THOME}/build/dobuild -64only gnutls-3.6.16 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs"
-${THOME}/build/genpkg TRIBgnutls gnutls-3.6.16
+# --enable-srp-authentication out of extreme caution over
+# binary compatibility
+#
+${THOME}/build/dobuild -64only gnutls-3.8.12 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs --enable-srp-authentication"
+${THOME}/build/genpkg TRIBgnutls gnutls-3.8.12
