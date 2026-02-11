86efb85e21979e78bf52f039f342ef26be94da8c
Peter Tribble <peter.tribble@gmail.com>
2026-02-11 15:38:16 +0000

    Update jupyterlab 4.5.4

--- TRIBjupyterlab-python-312/build.sh
+++ TRIBjupyterlab-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBjupyterlab-python-312 jupyterlab-4.5.3
+${THOME}/build/pkg_pep518 -N TRIBjupyterlab-python-312 jupyterlab-4.5.4
