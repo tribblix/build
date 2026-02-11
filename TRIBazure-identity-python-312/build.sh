d6864a641766d765fc7b9f41f22659c1d990fff1
Peter Tribble <peter.tribble@gmail.com>
2026-02-11 14:02:26 +0000

    Update azure-identity 1.25.2

--- TRIBazure-identity-python-312/build.sh
+++ TRIBazure-identity-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBazure-identity-python-312 azure_identity-1.25.1
+${THOME}/build/pkg_pep518 -N TRIBazure-identity-python-312 azure_identity-1.25.2
