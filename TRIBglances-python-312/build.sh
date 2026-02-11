1401de9090cba03d3134222fe9c4d46ddaa1f430
Peter Tribble <peter.tribble@gmail.com>
2026-02-11 15:36:13 +0000

    Update Glances 4.5.0.4

--- TRIBglances-python-312/build.sh
+++ TRIBglances-python-312/build.sh
@@ -2,11 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# orjson needs rust, but is now an optional dependency (not
-# even declared)
-#
-# the "ip" extra declares a dependency on netifaces2 which is
-# non-portable and doesn't work, but netifaces might actually
-# work well enough
-#
-${THOME}/build/pkg_pep518 -N TRIBglances-python-312 glances-4.5.0.1
+${THOME}/build/pkg_pep518 -N TRIBglances-python-312 glances-4.5.0.4
