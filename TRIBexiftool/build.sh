c38ca1a368d5437c75583e61f9f590c22f4fb650
Peter Tribble <peter.tribble@gmail.com>
2026-02-11 21:14:04 +0000

    Update ExifTool 13.50

--- TRIBexiftool/build.sh
+++ TRIBexiftool/build.sh
@@ -6,8 +6,8 @@
 # Archive::Zip we already had, Compress::Raw:Lzma and POSIX::strptime were easy
 # others send you down the rabbit hole
 #
-${THOME}/build/unpack Image-ExifTool-13.44
-cd Image-ExifTool-13.44
+${THOME}/build/unpack Image-ExifTool-13.50
+cd Image-ExifTool-13.50
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBexiftool
