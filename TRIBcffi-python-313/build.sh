10c681a609aef99e8539511534cbfa1ca34b7eb5
Peter Tribble <peter.tribble@gmail.com>
2026-02-16 14:57:11 +0000

    Package cffi for Python 3.13

---
+++ TRIBcffi-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBcffi-python-313 cffi-2.0.0
---
+++ TRIBpycparser-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpycparser-python-313 pycparser-3.0
