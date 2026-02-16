a1b0105c5411c1fe3e7966055a4628c7a97dc90a
Peter Tribble <peter.tribble@gmail.com>
2026-02-16 19:41:00 +0000

    Update oqs-provider 0.11.0

--- TRIBoqs-provider/build.sh
+++ TRIBoqs-provider/build.sh
@@ -5,5 +5,5 @@
 # there's an assumption here that by the time this gets to being deployed,
 # all relevant software will be 64-bit
 #
-${THOME}/build/cmbuild -64only oqs-provider-0.10.0 -C -DCMAKE_PREFIX_PATH=/usr/lib/`$THOME/build/getarch`/cmake
-${THOME}/build/genpkg TRIBoqs-provider oqs-provider-0.10.0
+${THOME}/build/cmbuild -64only oqs-provider-0.11.0 -C -DCMAKE_PREFIX_PATH=/usr/lib/`$THOME/build/getarch`/cmake
+${THOME}/build/genpkg TRIBoqs-provider oqs-provider-0.11.0
