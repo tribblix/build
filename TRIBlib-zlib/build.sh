74259a3fabf460efafc2c27086e0f2a219ae8edc
Peter Tribble <peter.tribble@gmail.com>
2026-02-18 09:51:37 +0000

    Update zlib 1.3.2

--- TRIBlib-zlib/build.sh
+++ TRIBlib-zlib/build.sh
@@ -8,13 +8,13 @@
 # -Wl,-z -Wl,direct -Wl,-z -Wl,text -Wl,-z -Wl,defs -Wl,-h -Wl,libz.so.1
 # It's not a real configure script
 #
-${THOME}/build/unpack -64 zlib-1.3.1
-cd zlib-1.3.1
+${THOME}/build/unpack -64 zlib-1.3.2
+cd zlib-1.3.2
 env CFLAGS="-DNO_VIZ -O2" ./configure --prefix=/usr --sharedlibdir=/lib
 gmake LDSHARED="gcc -shared -nostdlib -Wl,-z -Wl,direct -Wl,-z -Wl,text -Wl,-z -Wl,defs -Wl,-h -Wl,libz.so.1 -Wl,-M -Wl,${THOME}/build/patches/zlib.mapfile"
 cd ..
-cd zlib-1.3.1-64bit
+cd zlib-1.3.2-64bit
 env CFLAGS="-DNO_VIZ -O2" ./configure --64 --prefix=/usr --libdir=/usr/lib/`isainfo -k` --sharedlibdir=/lib/`isainfo -k`
 gmake LDSHARED="gcc -shared -nostdlib -Wl,-z -Wl,direct -Wl,-z -Wl,text -Wl,-z -Wl,defs -Wl,-h -Wl,libz.so.1 -Wl,-M -Wl,${THOME}/build/patches/zlib.mapfile"
 cd ..
-${THOME}/build/genpkg TRIBlib-zlib zlib-1.3.1
+${THOME}/build/genpkg TRIBlib-zlib zlib-1.3.2
