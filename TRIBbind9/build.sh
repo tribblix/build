dd761e457ff7ff162b2bc7409584347def4a31f7
Peter Tribble <peter.tribble@gmail.com>
2026-02-18 11:55:42 +0000

    Update bind 9.18.45

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -12,5 +12,5 @@
 zap i TRIBlibtool
 zap d TRIBjson-c
 zap d TRIBlibedit
-env LIBUV_CFLAGS=-I/tmp/libuv-v1.51.0-64bit/include LIBUV_LIBS="/tmp/libuv-v1.51.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_CFLAGS=-D_XPG4_2 TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64only bind-9.18.44 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBbind9 bind-9.18.44
+env LIBUV_CFLAGS=-I/tmp/libuv-v1.51.0-64bit/include LIBUV_LIBS="/tmp/libuv-v1.51.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_CFLAGS=-D_XPG4_2 TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64only bind-9.18.45 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBbind9 bind-9.18.45
