bbea61d1a17a5f9f9b74d132aa375386da0ffdb6
Peter Tribble <peter.tribble@gmail.com>
2026-02-19 09:06:59 +0000

    Update badkeys 0.0.17

--- TRIBbadkeys-python-312/build.sh
+++ TRIBbadkeys-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # dependencies include the [ssh,dkim] extras
 #
-${THOME}/build/pkg_pep518 -N TRIBbadkeys-python-312 badkeys-0.0.16
+${THOME}/build/pkg_pep518 -N TRIBbadkeys-python-312 badkeys-0.0.17
