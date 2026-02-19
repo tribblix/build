a8fbd6ccae5281f5d6e6aebe458a78dd5698978d
Peter Tribble <peter.tribble@gmail.com>
2026-02-19 09:44:00 +0000

    Update oslo.messaging 17.3.0

--- TRIBoslo-messaging-python-312/build.sh
+++ TRIBoslo-messaging-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-messaging-python-312 oslo_messaging-17.2.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-messaging-python-312 oslo_messaging-17.3.0
