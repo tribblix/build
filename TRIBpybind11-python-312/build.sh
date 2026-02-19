5dbac455dc7615b709edf6940610f63a1d82325a
Peter Tribble <peter.tribble@gmail.com>
2026-02-19 09:58:37 +0000

    Update pybind11 3.0.2

--- TRIBpybind11-python-312/build.sh
+++ TRIBpybind11-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/pkg_pep518 TRIBpybind11-python-312 pybind11-3.0.1 pybind11_global-3.0.1
+$THOME/build/pkg_pep518 TRIBpybind11-python-312 pybind11-3.0.2 pybind11_global-3.0.2
