d055d791644e11f971bb3f8e52cb00f8ce67df84
Peter Tribble <peter.tribble@gmail.com>
2026-02-20 13:05:43 +0000

    Update OpenJPH 0.26.3

--- TRIBopenjph/build.sh
+++ TRIBopenjph/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only OpenJPH-0.25.0
-${THOME}/build/genpkg TRIBopenjph OpenJPH-0.25.0
+${THOME}/build/cmbuild -64only OpenJPH-0.26.3
+${THOME}/build/genpkg TRIBopenjph OpenJPH-0.26.3
