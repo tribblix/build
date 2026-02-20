d8f75e50b5d7741f775c4608b29247f0ec1a2a4b
Peter Tribble <peter.tribble@gmail.com>
2026-02-20 13:08:58 +0000

    Update libpaper 2.2.7

--- TRIBlibpaper/build.sh
+++ TRIBlibpaper/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libpaper-2.2.5
-${THOME}/build/genpkg TRIBlibpaper libpaper-2.2.5
+${THOME}/build/dobuild -64only libpaper-2.2.7
+${THOME}/build/genpkg TRIBlibpaper libpaper-2.2.7
