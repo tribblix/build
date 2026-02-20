fc5102925893a31cc9f06dddcc38a789d075f542
Peter Tribble <peter.tribble@gmail.com>
2026-02-20 12:49:41 +0000

    Update socat 1.8.1.1

--- TRIBsocat/build.sh
+++ TRIBsocat/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only socat-1.8.1.0
-${THOME}/build/genpkg TRIBsocat socat-1.8.1.0
+${THOME}/build/dobuild -64only socat-1.8.1.1
+${THOME}/build/genpkg TRIBsocat socat-1.8.1.1
