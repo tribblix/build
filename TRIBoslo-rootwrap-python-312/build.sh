68f9387837ec066e2ea3db55a1a24eebede36018
Peter Tribble <peter.tribble@gmail.com>
2026-02-21 10:25:50 +0000

    Update oslo.rootwrap 7.9.0

--- TRIBoslo-rootwrap-python-312/build.sh
+++ TRIBoslo-rootwrap-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-rootwrap-python-312 oslo_rootwrap-7.8.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-rootwrap-python-312 oslo_rootwrap-7.9.0
