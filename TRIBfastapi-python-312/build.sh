309df54cc728f9dfc41db058dd768826047e2a5e
Peter Tribble <peter.tribble@gmail.com>
2026-02-22 10:11:21 +0000

    Update fastapi 0.129.2

--- TRIBfastapi-python-312/build.sh
+++ TRIBfastapi-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # bundle with the cli https://pypi.org/project/fastapi-cli/
 #
-${THOME}/build/pkg_pep518 -N TRIBfastapi-python-312 fastapi-0.129.0 fastapi_cli-0.0.23
+${THOME}/build/pkg_pep518 -N TRIBfastapi-python-312 fastapi-0.129.2 fastapi_cli-0.0.23
