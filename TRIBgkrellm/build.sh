8fcabe4f9ddd7cb487411ae96fdab1127691d965
Peter Tribble <peter.tribble@gmail.com>
2026-02-23 14:39:52 +0000

    Update gkrellm 2.5.1

--- TRIBgkrellm/build.sh
+++ TRIBgkrellm/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack gkrellm-2.3.11
-cd gkrellm-2.3.11
+${THOME}/build/unpack gkrellm-2.5.1
+cd gkrellm-2.5.1
 # CPPFLAGS, as CFLAGS is overridden by the Makefiles
 env PATH=/usr/bin/`$THOME/build/getarch`:$PATH PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig gmake -j 4 solaris LOCALEDIR=/usr/share/locale CPPFLAGS=-m64 LDFLAGS=-m64
 rm -fr /tmp/gk
