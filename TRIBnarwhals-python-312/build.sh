5ae8c730f86debea58957b75c16450ada25ef998
Peter Tribble <peter.tribble@gmail.com>
2026-02-23 21:39:19 +0000

    Update narwhals 2.17.0

--- TRIBnarwhals-python-312/build.sh
+++ TRIBnarwhals-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # there are a lot of possible dependencies, but all are in extras
 #
-${THOME}/build/pkg_pep518 -N TRIBnarwhals-python-312 narwhals-2.16.0
+${THOME}/build/pkg_pep518 -N TRIBnarwhals-python-312 narwhals-2.17.0
