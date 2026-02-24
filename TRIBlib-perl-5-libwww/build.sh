d5df6cb6bbbbc0aa83ea8595e60bb2dc14990d62
Peter Tribble <peter.tribble@gmail.com>
2026-02-24 08:28:48 +0000

    Update libwww-perl 6.81

--- TRIBlib-perl-5-libwww/build.sh
+++ TRIBlib-perl-5-libwww/build.sh
@@ -12,15 +12,15 @@
 # libwww needs http-daemon
 # libwww needs http-negotiate
 #
-${THOME}/build/unpack libwww-perl-6.77
-cd libwww-perl-6.77
+${THOME}/build/unpack libwww-perl-6.81
+cd libwww-perl-6.81
 perl Makefile.PL
 make
 cd ..
 # add the https protocol handler
-${THOME}/build/unpack LWP-Protocol-https-6.14
-cd LWP-Protocol-https-6.14
+${THOME}/build/unpack LWP-Protocol-https-6.15
+cd LWP-Protocol-https-6.15
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-libwww libwww-perl-6.77 LWP-Protocol-https-6.14
+${THOME}/build/genpkg TRIBlib-perl-5-libwww libwww-perl-6.81 LWP-Protocol-https-6.15
