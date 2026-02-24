b0c5aecb4914f8015cb37780681914f479939285
Peter Tribble <peter.tribble@gmail.com>
2026-02-24 09:33:12 +0000

    Update scipy 1.17.1

--- TRIBscipy-python-312/build.sh
+++ TRIBscipy-python-312/build.sh
@@ -2,11 +2,11 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# 1.14.1 build pins some dependencies
-# meson-python>=0.15.0,<0.19.0
-# Cython>=3.0.8,<3.1.0
-# pybind11>=2.12.0,<2.13.0
-# pythran>=0.14.0,<0.17.0
+# 1.17.1 build pins some dependencies
+# meson-python>=0.15.0,<0.21.0
+# Cython>=3.0.8,<3.3.0
+# pybind11>=2.13.2,<3.1.0
+# pythran>=0.14.0,<0.19.0
 # but we can try above the upper bound if necessary
 #
 # it's relatively hard work to get the build to be consistently 64-bit
@@ -16,4 +16,6 @@
 #
 # ideally that would be fixed further up
 #
-env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscipy-python-312 scipy-1.16.3
+# there's a patch to fix some conflicting enum names
+#
+env CXXFLAGS="-m64 -D__STDC_NO_ATOMICS__=1" CFLAGS=-m64 CC="gcc -m64" FFLAGS=-m64 FORTRANFLAGS=-m64 LDFLAGS=-m64 PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N TRIBscipy-python-312 scipy-1.17.1
