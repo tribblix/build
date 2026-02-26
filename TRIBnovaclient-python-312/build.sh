1a0b551120b5472306cb7d544ad0ef36835f286a
Peter Tribble <peter.tribble@gmail.com>
2026-02-26 08:48:42 +0000

    Update python-novaclient 18.12.0

--- TRIBnovaclient-python-312/build.sh
+++ TRIBnovaclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBnovaclient-python-312 python_novaclient-18.11.0
+${THOME}/build/pkg_pep518 -N TRIBnovaclient-python-312 python_novaclient-18.12.0
