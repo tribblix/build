4db43ea59aa4a04a6dc9bf4fa4ebe167656fd39d
Peter Tribble <peter.tribble@gmail.com>
2026-02-27 15:36:40 +0000

    Update nsd 4.14.1

--- TRIBnsd/build.sh
+++ TRIBnsd/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only nsd-4.14.0 -C --disable-dnstap
-${THOME}/build/genpkg TRIBnsd nsd-4.14.0
+${THOME}/build/dobuild -64only nsd-4.14.1 -C --disable-dnstap
+${THOME}/build/genpkg TRIBnsd nsd-4.14.1
