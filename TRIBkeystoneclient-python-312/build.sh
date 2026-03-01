1522e7248206f0ed83f590287920e369f0f5058c
Peter Tribble <peter.tribble@gmail.com>
2026-03-01 18:10:07 +0000

    Update python-keystoneclient 5.8.0

--- TRIBkeystoneclient-python-312/build.sh
+++ TRIBkeystoneclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBkeystoneclient-python-312 python_keystoneclient-5.7.0
+${THOME}/build/pkg_pep518 -N TRIBkeystoneclient-python-312 python_keystoneclient-5.8.0
