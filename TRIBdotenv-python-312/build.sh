bdfcd536530462b38b5bb55997939b886224d352
Peter Tribble <peter.tribble@gmail.com>
2026-03-01 18:04:23 +0000

    Update python-dotenv 1.2.2

--- TRIBdotenv-python-312/build.sh
+++ TRIBdotenv-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBdotenv-python-312 python_dotenv-1.2.1
+# adds click as a dependency for the [cli] extra
+#
+${THOME}/build/pkg_pep518 -N TRIBdotenv-python-312 python_dotenv-1.2.2
