8547b6fa67af80eb5d14451778810fab94127fe1
Peter Tribble <peter.tribble@gmail.com>
2026-03-01 18:15:13 +0000

    Update python-troveclient 8.10.0

--- TRIBtroveclient-python-312/build.sh
+++ TRIBtroveclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBtroveclient-python-312 python_troveclient-8.9.0
+${THOME}/build/pkg_pep518 -N TRIBtroveclient-python-312 python_troveclient-8.10.0
