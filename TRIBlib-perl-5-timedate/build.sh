d8f347a97acb876cb45c166e01ba66990366e7bc
Peter Tribble <peter.tribble@gmail.com>
2026-03-02 09:19:36 +0000

    Update TimeDate 2.34

--- TRIBlib-perl-5-timedate/build.sh
+++ TRIBlib-perl-5-timedate/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack TimeDate-2.33
-cd TimeDate-2.33
+${THOME}/build/unpack TimeDate-2.34
+cd TimeDate-2.34
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-timedate TimeDate-2.33
+${THOME}/build/genpkg TRIBlib-perl-5-timedate TimeDate-2.34
