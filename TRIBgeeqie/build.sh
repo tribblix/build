285f358b51bfb97109cf8fd075c6d54fce683ab2
Peter Tribble <peter.tribble@gmail.com>
2026-03-02 22:00:48 +0000

    Update geeqie 2.4

--- TRIBgeeqie/build.sh
+++ TRIBgeeqie/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_LDFLAGS="-lsocket" $THOME/build/mesonbuild -64only -gnu geeqie-2.1 -C -Dlua=disabled
-${THOME}/build/genpkg TRIBgeeqie geeqie-2.1
+env TRIBBLIX_CXXFLAGS="-lsocket" $THOME/build/mesonbuild -64only -gnu geeqie-2.4 -C -Dlua=disabled
+${THOME}/build/genpkg TRIBgeeqie geeqie-2.4
--- patches/geeqie-2.1.pls
+++
@@ -1,1 +0,0 @@
--p1 geeqie-includes.patch
