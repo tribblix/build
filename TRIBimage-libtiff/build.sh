378f43243e96fd37bd265b636120ca5d9f6e3475
Peter Tribble <peter.tribble@gmail.com>
2026-03-07 21:48:46 +0000

    Update libtiff 4.7.1 (with 4.4.0 compat libs)

--- TRIBimage-libtiff/build.sh
+++ TRIBimage-libtiff/build.sh
@@ -2,14 +2,14 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# see TRIBlibtiff-compat for the 3.x legacy files
-#
-# stick at 4.4.0; 4.5.0 revs the SONAME
+# 4.5.0 revs the SONAME, so ship the 4.4.0 shared libraries
+# until all consumers are rebuilt
 #
 # we disable webp as a nonstandard extensions
 #
 # freeglut must not be installed, there's no option
 # to disable it in the build
 #
-${THOME}/build/dobuild -64 tiff-4.4.0 -C "--disable-webp --disable-cxx --disable-static"
-${THOME}/build/genpkg TRIBimage-libtiff tiff-4.4.0
+${THOME}/build/dobuild -64 tiff-4.4.0 -C "--disable-webp --disable-cxx --disable-static --disable-docs --disable-libdeflate"
+${THOME}/build/dobuild -64 tiff-4.7.1 -C "--disable-webp --disable-cxx --disable-static --disable-libdeflate"
+${THOME}/build/genpkg TRIBimage-libtiff tiff-4.4.0 tiff-4.7.1
