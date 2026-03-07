cb08e077225c06c703d1f3f827e59ff60c4fcf4e
Peter Tribble <peter.tribble@gmail.com>
2026-03-07 10:18:01 +0000

    Update lazy_loader 0.5

--- TRIBlazyloader-python-312/build.sh
+++ TRIBlazyloader-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBlazyloader-python-312 lazy_loader-0.4
+${THOME}/build/pkg_pep518 -N TRIBlazyloader-python-312 lazy_loader-0.5
