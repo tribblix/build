9bef18e19340f68312191d3a2cee585b9467a457
Peter Tribble <peter.tribble@gmail.com>
2026-03-08 10:19:33 +0000

    Rebuild xplanet against updated libtiff

--- TRIBxplanet/build.sh
+++ TRIBxplanet/build.sh
@@ -6,5 +6,7 @@
 # svn checkout https://svn.code.sf.net/p/xplanet/code/trunk xplanet-code
 # tar czf ${THOME}/tarballs/xplanet-code.tar.gz xplanet-code
 #
-env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -64only xplanet-code
+# hits an autoconf version mismatch, so regenerate with -A
+#
+env TRIBBLIX_CXXFLAGS=-fpermissive ${THOME}/build/dobuild -A -64only xplanet-code
 ${THOME}/build/genpkg TRIBxplanet xplanet-code
