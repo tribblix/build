44ce62a2739b548916b279d9fb4e2b478ab6daaa
Peter Tribble <peter.tribble@gmail.com>
2026-03-08 20:39:58 +0000

    Rebuild lcms2 against updated libtiff

--- TRIBlib-lcms2/build.sh
+++ TRIBlib-lcms2/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# the dependencies are pulled in by the cli tools, not by the library
+#
 ${THOME}/build/dobuild -64 lcms2-2.16
 ${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.16
