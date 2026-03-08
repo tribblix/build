9a648f03e48aed09976de486967866ab464f0888
Peter Tribble <peter.tribble@gmail.com>
2026-03-08 20:46:37 +0000

    Rebuild openjpeg against updated libtiff

--- TRIBlib-openjpeg/build.sh
+++ TRIBlib-openjpeg/build.sh
@@ -7,6 +7,8 @@
 # need to not build static libs in the first place so the installed
 # cmake config is consistent
 #
+# the dependencies are pulled in by the cli tools, not by the library
+#
 env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +64 openjpeg-2.5.4 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF -DOPENJPEG_INSTALL_BIN_DIR=bin/`$THOME/build/getarch` -DOPENJPEG_INSTALL_LIB_DIR=lib/`$THOME/build/getarch`"
 env CFLAGS="-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64" $THOME/build/cmbuild +32 openjpeg-2.5.4 -C "-DBUILD_STATIC_LIBS=OFF -DBUILD_THIRDPARTY=OFF"
 ${THOME}/build/genpkg TRIBlib-openjpeg openjpeg-2.5.4
