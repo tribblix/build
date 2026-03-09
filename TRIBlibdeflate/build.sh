ac6508e6f0d7b66f68e064b957d6fa9e5ed8aa04
Peter Tribble <peter.tribble@gmail.com>
2026-03-09 19:22:04 +0000

    Fix libdeflate installed cmake files

--- TRIBlibdeflate/build.sh
+++ TRIBlibdeflate/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only libdeflate-1.23
+${THOME}/build/cmbuild -64only libdeflate-1.23 -C "-DLIBDEFLATE_BUILD_STATIC_LIB=OFF"
 ${THOME}/build/genpkg TRIBlibdeflate libdeflate-1.23
