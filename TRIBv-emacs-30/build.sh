ee02db6d41e18bc5a2eedf7929ce25cbe1ebe80b
Peter Tribble <peter.tribble@gmail.com>
2026-03-09 13:16:32 +0000

    Rebuild emacs30 against updated libtiff, bump to 30.2

--- TRIBv-emacs-30/build.sh
+++ TRIBv-emacs-30/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-30.1 -P /usr/versions/emacs-30 -C "--with-xft --with-xpm --without-sqlite3"
-${THOME}/build/genpkg TRIBv-emacs-30 emacs-30.1
+env LIBS=-lcurses ${THOME}/build/dobuild -64only emacs-30.2 -P /usr/versions/emacs-30 -C "--with-xft --with-xpm --without-sqlite3"
+${THOME}/build/genpkg TRIBv-emacs-30 emacs-30.2
