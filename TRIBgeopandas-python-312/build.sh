658f1c183fd60b624dadf8899a946e37a9bd2482
Peter Tribble <peter.tribble@gmail.com>
2026-03-09 22:23:23 +0000

    Update geopandas 1.1.3

--- TRIBgeopandas-python-312/build.sh
+++ TRIBgeopandas-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBgeopandas-python-312 geopandas-1.1.2
+${THOME}/build/pkg_pep518 -N TRIBgeopandas-python-312 geopandas-1.1.3
