960e611ca0b91840374ab5207b22faf4314e2ffe
Peter Tribble <peter.tribble@gmail.com>
2026-03-10 11:44:11 +0000

    Update Zabbix 7.0.23

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -5,7 +5,10 @@
 # we need a temporary copy of libevent
 # avoid tying it to the normal preconf as it's not relevant for the agent
 #
+# needs a server with php, but the packages for that are declared as
+# part of the zabbix-server overlay rather than as dependencies here
+#
 ${THOME}/build/patches/zabbix-server.preconf
-${THOME}/build/dobuild -64only zabbix-6.0.43 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres17/bin/pg_config --with-libpcre2 --with-libevent=/tmp/mc"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-6.0.43
+${THOME}/build/dobuild -64only zabbix-7.0.23 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres17/bin/pg_config --with-libpcre2 --with-libevent=/tmp/mc"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-7.0.23
 rm -fr /tmp/mc
