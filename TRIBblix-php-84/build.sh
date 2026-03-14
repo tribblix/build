cfa02848841e980f1eea8700b3ef4c4dfafae4e1
Peter Tribble <peter.tribble@gmail.com>
2026-03-14 15:03:36 +0000

    Update PHP 8.4.19

--- TRIBblix-php-84/build.sh
+++ TRIBblix-php-84/build.sh
@@ -13,8 +13,8 @@
 # needs a tsrm patch to avoid crashes
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-8.4.18
-cd php-8.4.18
+${THOME}/build/unpack php-8.4.19
+cd php-8.4.19
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
