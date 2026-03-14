7b56b801c9a4427f05f671b7e7d6c9e4e5c22ce1
Peter Tribble <peter.tribble@gmail.com>
2026-03-14 08:54:01 +0000

    Update sbom4python 0.12.5

--- TRIBsbom-4-python-312/build.sh
+++ TRIBsbom-4-python-312/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_wheel TRIBsbom-4-python-312 sbom4python-0.12.4-py2.py3-none-any.whl
+# it needs magic, but doesn't declare it because there are multiple
+# modules (bin or not) that might sastify it
+#
+${THOME}/build/pkg_wheel TRIBsbom-4-python-312 sbom4python-0.12.5-py3-none-any.whl
