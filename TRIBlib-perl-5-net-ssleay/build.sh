8e6917889cae74771d1b13b4af9e37a81bc9c143
Peter Tribble <peter.tribble@gmail.com>
2026-03-22 09:40:59 +0000

    Update Net::SSLeay 1.96

--- TRIBlib-perl-5-net-ssleay/build.sh
+++ TRIBlib-perl-5-net-ssleay/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Net-SSLeay-1.94
-cd Net-SSLeay-1.94
+${THOME}/build/unpack Net-SSLeay-1.96
+cd Net-SSLeay-1.96
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-net-ssleay Net-SSLeay-1.94
+${THOME}/build/genpkg TRIBlib-perl-5-net-ssleay Net-SSLeay-1.96
