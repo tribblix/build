e6bcdb41d56da809d857ee75d9c4181aff670f30
Peter Tribble <peter.tribble@gmail.com>
2026-03-22 09:47:29 +0000

    Update XML::Parser 2.51

--- TRIBlib-perl-5-xml-parser/build.sh
+++ TRIBlib-perl-5-xml-parser/build.sh
@@ -11,7 +11,7 @@
 ${THOME}/build/unpack XML-SAX-Base-1.09
 ${THOME}/build/unpack XML-NamespaceSupport-1.12
 ${THOME}/build/unpack XML-SAX-1.02
-${THOME}/build/unpack XML-Parser-2.47
+${THOME}/build/unpack XML-Parser-2.51
 ${THOME}/build/unpack XML-SAX-Expat-0.51
 ${THOME}/build/unpack XML-Simple-2.25
 cd XML-SAX-Base-1.09
@@ -26,7 +26,7 @@ cd XML-SAX-1.02
 perl Makefile.PL
 make
 cd ..
-cd XML-Parser-2.47
+cd XML-Parser-2.51
 perl Makefile.PL
 make
 cd ..
@@ -38,4 +38,4 @@ cd XML-Simple-2.25
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-xml-parser XML-SAX-Base-1.09 XML-NamespaceSupport-1.12 XML-SAX-1.02 XML-Parser-2.47 XML-SAX-Expat-0.51 XML-Simple-2.25
+${THOME}/build/genpkg TRIBlib-perl-5-xml-parser XML-SAX-Base-1.09 XML-NamespaceSupport-1.12 XML-SAX-1.02 XML-Parser-2.51 XML-SAX-Expat-0.51 XML-Simple-2.25
