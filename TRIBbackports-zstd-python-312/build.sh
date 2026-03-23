d68c9ed41176f3ad3a1db4cb1d5a12bd19939eb0
Peter Tribble <peter.tribble@gmail.com>
2026-03-23 10:23:51 +0000

    Use system library for backports.zstd

--- TRIBbackports-zstd-python-312/build.sh
+++ TRIBbackports-zstd-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBbackports-zstd-python-312 backports_zstd-1.3.0
+# use the system zstd
+#
+${THOME}/build/pkg_pep518 -N -C "--config-settings=--build-option=--system-zstd" TRIBbackports-zstd-python-312 backports_zstd-1.3.0
