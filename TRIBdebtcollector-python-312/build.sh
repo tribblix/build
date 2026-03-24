c23383814b230d272f1ef6ae9ff9d1eed9ab419a
Peter Tribble <peter.tribble@gmail.com>
2026-03-24 14:46:31 +0000

    Update debtcollector 3.1.0

--- TRIBdebtcollector-python-312/build.sh
+++ TRIBdebtcollector-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBdebtcollector-python-312 debtcollector-3.0.0
+${THOME}/build/pkg_pep518 -N TRIBdebtcollector-python-312 debtcollector-3.1.0
