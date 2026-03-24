8b3030b9b7811437aa570810caa32707f3e23462
Peter Tribble <peter.tribble@gmail.com>
2026-03-24 09:48:47 +0000

    Update pipx 1.11.0

--- TRIBpipx-python-312/build.sh
+++ TRIBpipx-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpipx-python-312 pipx-1.10.1
+${THOME}/build/pkg_pep518 -N TRIBpipx-python-312 pipx-1.11.0
--- TRIBpipx-python-313/build.sh
+++ TRIBpipx-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpipx-python-313 pipx-1.10.1
+${THOME}/build/pkg_pep518 -N TRIBpipx-python-313 pipx-1.11.0
