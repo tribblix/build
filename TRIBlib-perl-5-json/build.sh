d236b5718a94ddf2d4c661d348a16fbc755d6619
Peter Tribble <peter.tribble@gmail.com>
2026-03-25 09:52:54 +0000

    Update perl JSON 4.11

--- TRIBlib-perl-5-json/build.sh
+++ TRIBlib-perl-5-json/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack JSON-4.10
-cd JSON-4.10
+${THOME}/build/unpack JSON-4.11
+cd JSON-4.11
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-json JSON-4.10
+${THOME}/build/genpkg TRIBlib-perl-5-json JSON-4.11
