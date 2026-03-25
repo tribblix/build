366d704c7e9e50b891e30613b1092ddb4324203d
Peter Tribble <peter.tribble@gmail.com>
2026-03-25 09:49:56 +0000

    Update perl IO::Tty 1.23

--- TRIBlib-perl-5-io-tty/build.sh
+++ TRIBlib-perl-5-io-tty/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack IO-Tty-1.20
-cd IO-Tty-1.20
+${THOME}/build/unpack IO-Tty-1.23
+cd IO-Tty-1.23
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-io-tty IO-Tty-1.20
+${THOME}/build/genpkg TRIBlib-perl-5-io-tty IO-Tty-1.23
