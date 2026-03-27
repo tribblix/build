090fd0c34c4a69801f2cc65bf965e1384fcbebd9
Peter Tribble <peter.tribble@gmail.com>
2026-03-27 19:02:15 +0000

    Update vcs-versioning 1.1.0

--- TRIBvcs-versioning-python-313/build.sh
+++ TRIBvcs-versioning-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBvcs-versioning-python-313 vcs_versioning-1.0.1
+${THOME}/build/pkg_pep518 -N TRIBvcs-versioning-python-313 vcs_versioning-1.1.0
