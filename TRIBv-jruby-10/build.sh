7cfb42bc52cd97c28579a1b7978d6de3db003730
Peter Tribble <peter.tribble@gmail.com>
2026-03-28 21:33:07 +0000

    Update JRuby 10.0.4.0

--- TRIBv-jruby-10/build.sh
+++ TRIBv-jruby-10/build.sh
@@ -4,4 +4,4 @@
 #
 # not available on sparc due to jdk21 requirement
 #
-${THOME}/build/pkg_tarball TRIBv-jruby-10 jruby-dist-10.0.3.0-bin
+${THOME}/build/pkg_tarball TRIBv-jruby-10 jruby-dist-10.0.4.0-bin
