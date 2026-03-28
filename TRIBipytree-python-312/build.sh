e511b5a5b32deb8df064ea331703b9fb61b42da1
Peter Tribble <peter.tribble@gmail.com>
2026-03-28 13:43:02 +0000

    Drop unused ipytree

--- TRIBipytree-python-312/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# along with notebook and ipywidgets, supplies zarr[jupyter]
-#
-${THOME}/build/pkg_pep518 -N TRIBipytree-python-312 ipytree-0.2.2
