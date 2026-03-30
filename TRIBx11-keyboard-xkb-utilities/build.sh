09e9440c4da193c3ba4640280995c25ae4fe1e0f
Peter Tribble <peter.tribble@gmail.com>
2026-03-30 18:10:39 +0100

    Update setxkbmap 1.3.5

--- TRIBx11-keyboard-xkb-utilities/build.sh
+++ TRIBx11-keyboard-xkb-utilities/build.sh
@@ -7,5 +7,5 @@
 #
 ${THOME}/build/dobuild -64only xkbutils-1.0.6
 ${THOME}/build/dobuild -64only xkbprint-1.0.7
-${THOME}/build/dobuild -64only setxkbmap-1.3.4
-${THOME}/build/genpkg TRIBx11-keyboard-xkb-utilities xkbutils-1.0.6 xkbprint-1.0.7 setxkbmap-1.3.4
+${THOME}/build/dobuild -64only setxkbmap-1.3.5
+${THOME}/build/genpkg TRIBx11-keyboard-xkb-utilities xkbutils-1.0.6 xkbprint-1.0.7 setxkbmap-1.3.5
