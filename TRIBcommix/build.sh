de50f65cb0a963ee6b0723474ac206b954ba2b47
Peter Tribble <peter.tribble@gmail.com>
2026-04-03 21:13:05 +0100

    Update commix 4.1

--- TRIBcommix/build.sh
+++ TRIBcommix/build.sh
@@ -5,6 +5,4 @@
 # depends on python
 # needs the gnureadline module
 #
-# needs a fix to make the version string compatible with setuptools
-#
-${THOME}/build/pkg_setup_py -v 312 TRIBcommix commix-4.0
+${THOME}/build/pkg_pep518 -v 312 TRIBcommix commix-4.1
