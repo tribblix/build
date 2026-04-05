7d5e6cbeb4d1c45cd0946ac8c035e6709f66d5b4
Peter Tribble <peter.tribble@gmail.com>
2026-04-05 22:11:17 +0100

    Rebuild libva to fix mangled RPATH

--- TRIBlibva/build.sh
+++ TRIBlibva/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/mesonbuild +64 libva-2.23.0
+# use autotools, meson mangles the installed RPATH
+#
+${THOME}/build/dobuild +64 libva-2.23.0
 ${THOME}/build/genpkg TRIBlibva libva-2.23.0
