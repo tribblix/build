1db6c16ccabd904c4ef95d3ccf624779957d94bf
Peter Tribble <peter.tribble@gmail.com>
2026-04-06 12:53:44 +0100

    Update bind 9.20.22

--- TRIBbind9/build.sh
+++ TRIBbind9/build.sh
@@ -12,8 +12,8 @@
 zap i TRIBlibtool
 zap d TRIBjson-c
 zap d TRIBlibedit
-env LIBUV_CFLAGS=-I/tmp/libuv-v1.51.0-64bit/include LIBUV_LIBS="/tmp/libuv-v1.51.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_PKG_CONFIG_PATH=/tmp/urcu/lib/`$THOME/build/getarch`/pkgconfig TRIBBLIX_CFLAGS=-D_XPG4_2 TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64only bind-9.20.21 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBbind9 bind-9.20.21
+env LIBUV_CFLAGS=-I/tmp/libuv-v1.51.0-64bit/include LIBUV_LIBS="/tmp/libuv-v1.51.0-64bit/.libs/libuv.a -lkstat -lsendfile" TRIBBLIX_PKG_CONFIG_PATH=/tmp/urcu/lib/`$THOME/build/getarch`/pkgconfig TRIBBLIX_CFLAGS=-D_XPG4_2 TRIBBLIX_LDFLAGS=-lsocket ${THOME}/build/dobuild -64only bind-9.20.22 -C "--sysconfdir=/etc --localstatedir=/var --without-python --disable-geoip --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBbind9 bind-9.20.22
 
 #
 # clean up
