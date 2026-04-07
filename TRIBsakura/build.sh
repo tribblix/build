a3b41f1fa6e315a5e49246eb81216025f557c193
Peter Tribble <peter.tribble@gmail.com>
2026-04-07 08:49:29 +0100

    Update sakura 3.8.9

--- TRIBsakura/build.sh
+++ TRIBsakura/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only -gnu sakura-3.8.7
-${THOME}/build/genpkg TRIBsakura sakura-3.8.7
+${THOME}/build/cmbuild -64only -gnu sakura-3.8.9
+${THOME}/build/genpkg TRIBsakura sakura-3.8.9
