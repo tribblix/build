2a56c80f0b7e1c87a05397834d1fa0181d5c0239
Peter Tribble <peter.tribble@gmail.com>
2026-04-07 10:06:57 +0100

    Update DBD::Pg 3.20.0

--- TRIBlib-perl-5-dbd-pg/build.sh
+++ TRIBlib-perl-5-dbd-pg/build.sh
@@ -2,9 +2,11 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack DBD-Pg-3.18.0
-cd DBD-Pg-3.18.0
+${THOME}/build/unpack DBD-Pg-3.20.0
+cd DBD-Pg-3.20.0
+# https://github.com/bucardo/dbdpg/issues/170
+sed -i s:7.64:7.62: Makefile.PL
 env POSTGRES_HOME=/opt/tribblix/postgres17 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-dbd-pg DBD-Pg-3.18.0
+${THOME}/build/genpkg TRIBlib-perl-5-dbd-pg DBD-Pg-3.20.0
