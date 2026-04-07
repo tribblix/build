6f61a2250c11a1335c201c0266d763bc66950856
Peter Tribble <peter.tribble@gmail.com>
2026-04-07 17:54:03 +0100

    Update libpcap 1.10.6

--- TRIBlibpcap/build.sh
+++ TRIBlibpcap/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libpcap-1.10.5
-${THOME}/build/genpkg TRIBlibpcap libpcap-1.10.5
+${THOME}/build/dobuild -64 libpcap-1.10.6
+${THOME}/build/genpkg TRIBlibpcap libpcap-1.10.6
