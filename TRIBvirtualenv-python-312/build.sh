b26ed0535e7735cd407a06c00a8337abc8552b8a
Peter Tribble <peter.tribble@gmail.com>
2026-04-08 09:34:28 +0100

    Update python-discovery 1.2.2

--- TRIBvirtualenv-python-312/build.sh
+++ TRIBvirtualenv-python-312/build.sh
@@ -4,4 +4,4 @@
 #
 # python-discovery spun out into a separate module as of release 21
 #
-${THOME}/build/pkg_pep518 -N TRIBvirtualenv-python-312 virtualenv-21.2.0 python_discovery-1.2.1
+${THOME}/build/pkg_pep518 -N TRIBvirtualenv-python-312 virtualenv-21.2.0 python_discovery-1.2.2
