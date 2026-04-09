44361425e0166f9f0e2b6b03c9b3604a0920addc
Peter Tribble <peter.tribble@gmail.com>
2026-04-09 12:02:32 +0100

    Package WebOb for python 3.13

---
+++ TRIBlegacy-cgi-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBlegacy-cgi-python-313 legacy_cgi-2.6.4
---
+++ TRIBwebob-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBwebob-python-313 webob-1.8.9
