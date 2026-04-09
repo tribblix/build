8da845a319944e4ab1ce4549625a25a7637e79e8
Peter Tribble <peter.tribble@gmail.com>
2026-04-09 14:17:26 +0100

    Package futurist for python 3.13

---
+++ TRIBfuturist-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBfuturist-python-313 futurist-3.3.0
---
+++ TRIBwrapt-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBwrapt-python-313 wrapt-2.1.2
