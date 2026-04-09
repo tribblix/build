feb4e5671e62c7d382cb89f2764bd2b01f508172
Peter Tribble <peter.tribble@gmail.com>
2026-04-09 12:54:53 +0100

    Package alembic for python 3.13

---
+++ TRIBmako-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBmako-python-313 mako-1.3.10
---
+++ TRIBsqlalchemy-python-313/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# it does depend on greenlet, although pip doesn't pick that up as
+# there's an architecture switch that doesn't work the way they
+# expect on solaris
+#
+${THOME}/build/pkg_pep518 -N TRIBsqlalchemy-python-313 sqlalchemy-2.0.49
