59c25feb68079de34b5475f46341ac386ec61c96
Peter Tribble <peter.tribble@gmail.com>
2026-04-12 10:07:32 +0100

    Update ant 1.10.17

--- TRIBv-apache-ant-110/build.sh
+++ TRIBv-apache-ant-110/build.sh
@@ -6,4 +6,4 @@
 # apache-ivy-2.5.3-bin-with-deps.zip
 # svnant-1.3.1.zip
 #
-${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.16-bin
+${THOME}/build/pkg_tarball TRIBv-apache-ant-110 apache-ant-1.10.17-bin
