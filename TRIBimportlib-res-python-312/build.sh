6de14f9dc62b40d0f78528002b09d67c2e62eb55
Peter Tribble <peter.tribble@gmail.com>
2026-04-12 18:32:21 +0100

    Update importlib_resources 7.1.0

--- TRIBimportlib-res-python-312/build.sh
+++ TRIBimportlib-res-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBimportlib-res-python-312 importlib_resources-6.5.2
+${THOME}/build/pkg_pep518 -N TRIBimportlib-res-python-312 importlib_resources-7.1.0
