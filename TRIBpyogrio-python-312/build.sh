0b9d978cfd0513a247e668e2b18600c353a00f99
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 17:26:25 +0100

    Rebuild pyogrio for GDAL bump

--- TRIBpyogrio-python-312/build.sh
+++ TRIBpyogrio-python-312/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# pulls in libgdal which bumps the SONAME every update
+#
 ${THOME}/build/pkg_pep518 TRIBpyogrio-python-312 pyogrio-0.12.1
--- TRIBshapely-python-312/build.sh
+++ TRIBshapely-python-312/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# this depends on GEOS, but only via the C api, which is stable, so
+# shapely doesn't need to rebuilt if GEOS is updated
+#
 ${THOME}/build/pkg_pep518 -N TRIBshapely-python-312 shapely-2.1.2
