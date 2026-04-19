4717c86b538f8a8377489680e6177a9f32eeefc9
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 13:16:55 +0100

    Update iceauth 1.0.11

--- TRIBx11-iceauth/build.sh
+++ TRIBx11-iceauth/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only iceauth-1.0.10
-${THOME}/build/genpkg TRIBx11-iceauth iceauth-1.0.10
+${THOME}/build/mesonbuild -64only iceauth-1.0.11
+${THOME}/build/genpkg TRIBx11-iceauth iceauth-1.0.11
