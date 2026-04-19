24a237761d6b6b09e67541f23f469616eb18a8aa
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 21:24:50 +0100

    Add spdx-tools 0.8.5

---
+++ TRIBspdx-tools-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBspdx-tools-python-313 spdx_tools-0.8.5
---
+++ TRIBuritools-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBuritools-python-313 uritools-6.0.1
