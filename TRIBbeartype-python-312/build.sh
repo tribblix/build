d4bf8b8ebae450c2d8797191bc0c91781cbee635
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 20:48:28 +0100

    Add beartype 0.22.9

---
+++ TRIBbeartype-python-312/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBbeartype-python-312 beartype-0.22.9
---
+++ TRIBbeartype-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBbeartype-python-313 beartype-0.22.9
