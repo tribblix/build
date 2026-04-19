4ff4ef65ea482fb69043d484c1f3bcb9cab4a07a
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 16:53:42 +0100

    Update GEOS 3.14.1

--- TRIBgeos/build.sh
+++ TRIBgeos/build.sh
@@ -4,5 +4,5 @@
 #
 # any update bumps the SONAME
 #
-${THOME}/build/cmbuild -64only geos-3.13.0 -C "-DBUILD_TESTING=OFF -DBUILD_DOCUMENTATION=OFF"
-${THOME}/build/genpkg TRIBgeos geos-3.13.0
+${THOME}/build/cmbuild -64only geos-3.14.1 -C "-DBUILD_TESTING=OFF -DBUILD_DOCUMENTATION=OFF"
+${THOME}/build/genpkg TRIBgeos geos-3.14.1
