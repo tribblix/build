873ae0b442bf399821a68fca764681d824bb3c9c
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 12:47:52 +0100

    Update listres 1.0.7

--- TRIBx11-listres/build.sh
+++ TRIBx11-listres/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only listres-1.0.6
-${THOME}/build/genpkg TRIBx11-listres listres-1.0.6
+${THOME}/build/mesonbuild -64only listres-1.0.7
+${THOME}/build/genpkg TRIBx11-listres listres-1.0.7
