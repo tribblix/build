3f32c00f24b703d5575d51bbbfb6a7a5aedec612
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 13:57:56 +0100

    Update citus 14.0.0

--- TRIBblix-citus17/build.sh
+++ TRIBblix-citus17/build.sh
@@ -4,7 +4,5 @@
 #
 # this is a postgres extension, so it's specific to the postgres version
 #
-# there is a citus-stat patch
-#
-${THOME}/build/dobuild +64only citus-13.2.0 -C PG_CONFIG=/opt/tribblix/postgres17/bin/pg_config
-${THOME}/build/genpkg TRIBblix-citus17 citus-13.2.0
+${THOME}/build/dobuild +64only citus-14.0.0 -C PG_CONFIG=/opt/tribblix/postgres17/bin/pg_config
+${THOME}/build/genpkg TRIBblix-citus17 citus-14.0.0
