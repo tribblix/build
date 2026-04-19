9522cdd4278a7331df039a3ac5d2b35b97aa4e32
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 12:54:06 +0100

    Update ico 1.0.7

--- TRIBx11-demo/build.sh
+++ TRIBx11-demo/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only ico-1.0.6
+${THOME}/build/mesonbuild -64only ico-1.0.7
 ${THOME}/build/dobuild -64only xeyes-1.3.1
 ${THOME}/build/dobuild -64only xgc-1.0.7
 ${THOME}/build/dobuild -64only xlogo-1.0.7
-${THOME}/build/genpkg TRIBx11-demo ico-1.0.6 xeyes-1.3.1 xgc-1.0.7 xlogo-1.0.7
+${THOME}/build/genpkg TRIBx11-demo ico-1.0.7 xeyes-1.3.1 xgc-1.0.7 xlogo-1.0.7
