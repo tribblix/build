549e7002b7f304886d4c8aaf2e400cd7c1dc8777
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 13:56:43 +0100

    Update constype 1.0.6

--- TRIBdiagnostic-constype/build.sh
+++ TRIBdiagnostic-constype/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only constype-1.0.5
-${THOME}/build/genpkg TRIBdiagnostic-constype constype-1.0.5
+${THOME}/build/mesonbuild -64only constype-1.0.6
+${THOME}/build/genpkg TRIBdiagnostic-constype constype-1.0.6
