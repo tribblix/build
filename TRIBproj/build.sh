11759ab361a66862895e6a8a8cda637533944539
Peter Tribble <peter.tribble@gmail.com>
2026-04-19 15:40:09 +0100

    Update PROJ 9.8.1

--- TRIBproj/build.sh
+++ TRIBproj/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/cmbuild -64only proj-9.5.1
-${THOME}/build/genpkg TRIBproj proj-9.5.1
+${THOME}/build/cmbuild -64only proj-9.8.1 -C -DBUILD_TESTING=OFF
+${THOME}/build/genpkg TRIBproj proj-9.8.1
