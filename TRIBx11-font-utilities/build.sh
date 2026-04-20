77036c94ca16aae6f9c535bd7de2107e86440e33
Peter Tribble <peter.tribble@gmail.com>
2026-04-20 19:52:07 +0100

    Update mkfontscale 1.2.4

--- TRIBx11-font-utilities/build.sh
+++ TRIBx11-font-utilities/build.sh
@@ -5,11 +5,11 @@
 # this is a merge of several smaller packages, that was the way
 # it was inherited.
 #
-# font-utils is in the font directory, not the app directory
+# font-util is in the font directory, not the app directory
 #
 # do not build font-util -64only, we need both the 32 and 64 bit pc file
 #
 ${THOME}/build/dobuild -64only bdftopcf-1.1.2
-${THOME}/build/dobuild -64only mkfontscale-1.2.3
+${THOME}/build/mesonbuild -64only mkfontscale-1.2.4
 ${THOME}/build/dobuild -64 font-util-1.4.1
-${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1.2 mkfontscale-1.2.3 font-util-1.4.1
+${THOME}/build/genpkg TRIBx11-font-utilities bdftopcf-1.1.2 mkfontscale-1.2.4 font-util-1.4.1
