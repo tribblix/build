52fff5b9fb4a8888773f879bcbb260150ddcd3d6
Peter Tribble <peter.tribble@gmail.com>
2026-04-20 09:58:22 +0100

    Update lxml 6.1.0, and package for python 3.13

--- TRIBlxml-python-313/build.sh
+++ TRIBlxml-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # lxml is also an optional dependency for the pelican importer, via bs4
 #
-${THOME}/build/pkg_pep518 -N TRIBlxml-python-312 lxml-6.1.0
+${THOME}/build/pkg_pep518 -N TRIBlxml-python-313 lxml-6.1.0
