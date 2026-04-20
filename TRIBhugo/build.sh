c3bffcf3e61e65438825b1a93bbb547fadf11790
Peter Tribble <peter.tribble@gmail.com>
2026-04-20 18:17:34 +0100

    Update hugo 0.160.1

--- TRIBhugo/build.sh
+++ TRIBhugo/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env PATH=/usr/versions/go-1.25/bin:$PATH GOPATH=`pwd` go install github.com/gohugoio/hugo@v0.152.2
+env PATH=/usr/versions/go-1.26/bin:$PATH GOPATH=`pwd` go install github.com/gohugoio/hugo@v0.160.1
 
 rm -fr /tmp/ll
 mkdir -p /tmp/ll/usr/bin
