c1408e0c9236bacc891ce15d41f18eae84166c85
Peter Tribble <peter.tribble@gmail.com>
2026-04-21 12:49:09 +0100

    Update libgcrypt 1.12.2

--- TRIBlibgcrypt/build.sh
+++ TRIBlibgcrypt/build.sh
@@ -7,5 +7,5 @@
 # specify -O2 as the build tries to turn off optimization for jitterentropy.c
 # and doesn't understand the simplest case of -O
 #
-env TRIBBLIX_CFLAGS=-O2 ${THOME}/build/dobuild -64only libgcrypt-1.10.3 -C "--disable-amd64-as-feature-detection gcry_cv_gcc_amd64_platform_as_ok=no --disable-asm"
-${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.10.3
+env TRIBBLIX_CFLAGS=-O2 ${THOME}/build/dobuild -64only libgcrypt-1.12.2 -C "--disable-amd64-as-feature-detection gcry_cv_gcc_amd64_platform_as_ok=no --disable-asm"
+${THOME}/build/genpkg TRIBlibgcrypt libgcrypt-1.12.2
