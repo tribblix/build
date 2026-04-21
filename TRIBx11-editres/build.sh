597f6043c105a5b253eacab2db8fead2ebcff189
Peter Tribble <peter.tribble@gmail.com>
2026-04-21 13:55:42 +0100

    Update editres 1.1.0

--- TRIBx11-editres/build.sh
+++ TRIBx11-editres/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only editres-1.0.9
-${THOME}/build/genpkg TRIBx11-editres editres-1.0.9
+${THOME}/build/dobuild -64only editres-1.1.0
+${THOME}/build/genpkg TRIBx11-editres editres-1.1.0
