35ea58eb7d80925594f9ac51b04ecbb91157d900
Peter Tribble <peter.tribble@gmail.com>
2026-04-21 08:23:32 +0100

    Update apache-libcloud 3.9.1

--- TRIBlibcloud-python-312/build.sh
+++ TRIBlibcloud-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBlibcloud-python-312 apache_libcloud-3.9.0
+${THOME}/build/pkg_pep518 -N TRIBlibcloud-python-312 apache_libcloud-3.9.1
