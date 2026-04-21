b3c7580bc4cc125987ca2c8333d0555779db8060
Peter Tribble <peter.tribble@gmail.com>
2026-04-21 12:51:39 +0100

    Update libksba 1.6.8

--- TRIBlibksba/build.sh
+++ TRIBlibksba/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libksba-1.6.7
-${THOME}/build/genpkg TRIBlibksba libksba-1.6.7
+${THOME}/build/dobuild -64only libksba-1.6.8
+${THOME}/build/genpkg TRIBlibksba libksba-1.6.8
