fd9036a5d8cd5826c3cf70c21a90dbb8aa0b1301
Peter Tribble <peter.tribble@gmail.com>
2026-04-22 08:53:43 +0100

    Update openjdk 26.0.1

--- TRIBopenjdk26/build.sh
+++ TRIBopenjdk26/build.sh
@@ -6,8 +6,8 @@
 # go to https://github.com/openjdk/jdk/tags
 #
 
-${THOME}/build/unpack jdk-jdk-26-35
-cd jdk-jdk-26-35
+${THOME}/build/unpack jdk26u-jdk-26.0.1-ga
+cd jdk26u-jdk-26.0.1-ga
 
 #
 # jdk26 needs autoconf installed
@@ -44,7 +44,7 @@ env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin bash ./configure \
 --with-vendor-vm-bug-url=https://www.tribblix.org/feedback.html \
 --with-version-opt=Tribblix \
 --with-jobs=4 \
---with-version-pre=35 \
+--with-version-pre=ga \
 DATE=/usr/gnu/bin/date \
 LOCALE=/bin/true \
 STRIP=/usr/gnu/bin/strip
@@ -56,9 +56,9 @@ env PATH=/usr/bin:/usr/sbin:/usr/sfw/bin:/usr/gnu/bin gmake product-images
 #
 # ./build/solaris-x86_64-server-release/images/jdk/bin/java -version
 #
-# openjdk version "26-35" 2026-03-17
-# OpenJDK Runtime Environment (build 26-35-Tribblix)
-# OpenJDK 64-Bit Server VM (build 26-35-Tribblix, mixed mode, sharing)
+# openjdk version "26.0.1-ga" 2026-04-21
+# OpenJDK Runtime Environment (build 26.0.1-ga-Tribblix)
+# OpenJDK 64-Bit Server VM (build 26.0.1-ga-Tribblix, mixed mode, sharing)
 #
 
 rm -fr /tmp/jdk
