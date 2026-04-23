28289baea8bd9671dfd6171d7ba787fa73d65476
Peter Tribble <peter.tribble@gmail.com>
2026-04-23 10:02:46 +0100

    Update pyeclib 1.8.0, and package for python 3.13

--- TRIBpyeclib-python-312/build.sh
+++ TRIBpyeclib-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_setup_py TRIBpyeclib-python-312 pyeclib-1.7.0
+${THOME}/build/pkg_pep518 -N TRIBpyeclib-python-312 pyeclib-1.8.0
---
+++ TRIBpyeclib-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpyeclib-python-313 pyeclib-1.8.0
