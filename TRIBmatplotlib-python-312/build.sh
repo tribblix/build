a9481a04f85c841c492e8df477027fbbf5815130
Peter Tribble <peter.tribble@gmail.com>
2026-04-24 17:46:19 +0100

    Update matplotlib 3.10.9

--- TRIBmatplotlib-python-312/build.sh
+++ TRIBmatplotlib-python-312/build.sh
@@ -4,4 +4,9 @@
 #
 # force the use of the system freetype
 #
-env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C --config-settings=setup-args=-Dsystem-freetype=true TRIBmatplotlib-python-312 matplotlib-3.10.8
+# note that if pyqt5 is installed it will try and use that as the
+# default backend, and it likely won't work
+# either uninstall pyqt5, or make sure tkinter is installed and invoke python
+# with env MPLBACKEND=tkagg ...
+#
+env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" PATH=/usr/versions/python-3.12/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "--config-settings=setup-args=-Dsystem-freetype=true --config-settings=setup-args=-Db_lto=false" TRIBmatplotlib-python-312 matplotlib-3.10.9
