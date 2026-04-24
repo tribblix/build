7d6bb2ae26148a6c567be23f48f6161a8e9f442c
Peter Tribble <peter.tribble@gmail.com>
2026-04-24 09:57:04 +0100

    Update s3transfer 0.16.1

--- TRIBs3transfer-python-313/build.sh
+++ TRIBs3transfer-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBs3transfer-python-313 s3transfer-0.16.0
+${THOME}/build/pkg_pep518 -N TRIBs3transfer-python-313 s3transfer-0.16.1
