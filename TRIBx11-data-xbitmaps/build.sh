5ea801ee77010b403cc6a50b562583cf932fff43
Peter Tribble <peter.tribble@gmail.com>
2026-04-26 11:18:05 +0100

    Update xbitmaps 1.1.4

--- TRIBx11-data-xbitmaps/build.sh
+++ TRIBx11-data-xbitmaps/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild xbitmaps-1.1.3
-${THOME}/build/genpkg TRIBx11-data-xbitmaps xbitmaps-1.1.3
+${THOME}/build/mesonbuild xbitmaps-1.1.4
+${THOME}/build/genpkg TRIBx11-data-xbitmaps xbitmaps-1.1.4
