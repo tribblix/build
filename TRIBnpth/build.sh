93f6231f13b3e9b1921b8d820a88b617c5f51194
Peter Tribble <peter.tribble@gmail.com>
2026-04-26 20:27:57 +0100

    Update nPth 1.8

--- TRIBnpth/build.sh
+++ TRIBnpth/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_CFLAGS="-std=c99" ${THOME}/build/dobuild +64 npth-1.6
-${THOME}/build/genpkg TRIBnpth npth-1.6
+env TRIBBLIX_CFLAGS="-std=c99" ${THOME}/build/dobuild +64 npth-1.8
+${THOME}/build/genpkg TRIBnpth npth-1.8
