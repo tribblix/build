c128f3459f98e1dc39312a621f24e588354c999e
Peter Tribble <peter.tribble@gmail.com>
2026-04-26 20:43:52 +0100

    Update libassuan 3.0.2

--- TRIBlibassuan/build.sh
+++ TRIBlibassuan/build.sh
@@ -4,5 +4,8 @@
 #
 # libassuan
 #
+# still ship the old library for binary compatibility
+#
 ${THOME}/build/dobuild +64 libassuan-2.5.7
-${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.7
+${THOME}/build/dobuild +64 libassuan-3.0.2
+${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.7 libassuan-3.0.2
