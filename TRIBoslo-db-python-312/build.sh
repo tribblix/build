40e7eafaade14dd3d8ca7af4295fcd942d0a38ca
Peter Tribble <peter.tribble@gmail.com>
2026-04-26 11:07:09 +0100

    Update testresources 2.1.2 testtools 2.9.1

--- TRIBoslo-db-python-312/build.sh
+++ TRIBoslo-db-python-312/build.sh
@@ -5,4 +5,4 @@
 # to simplify matters, bundle the 3 test modules into this package as well,
 # as nothing else (that I can see) uses them
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-db-python-312 oslo_db-18.0.0 testresources-2.0.2 testscenarios-0.6.1 testtools-2.9.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-db-python-312 oslo_db-18.0.0 testresources-2.1.2 testscenarios-0.6.1 testtools-2.9.1
