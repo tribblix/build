310a9d5df9f6d4c0e4c01cc03fa984251bec2427
Peter Tribble <peter.tribble@gmail.com>
2026-04-26 19:16:28 +0100

    Update uharfbuzz 0.53.7

--- TRIBuharfbuzz-python-312/build.sh
+++ TRIBuharfbuzz-python-312/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# cannot use -N due to cython version mismatch
-#
-${THOME}/build/pkg_pep518 TRIBuharfbuzz-python-312 uharfbuzz-0.53.6
+${THOME}/build/pkg_pep518 -N TRIBuharfbuzz-python-312 uharfbuzz-0.53.7
