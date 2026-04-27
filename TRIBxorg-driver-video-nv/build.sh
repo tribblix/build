b40d39a7808f3167817086e664b6e18fd4fead1f
Peter Tribble <peter.tribble@gmail.com>
2026-04-27 09:55:48 +0100

    Update nv driver 2.1.24

--- TRIBxorg-driver-video-nv/build.sh
+++ TRIBxorg-driver-video-nv/build.sh
@@ -4,8 +4,8 @@
 #
 # need to have the matching Xorg server that you're building for installed
 #
-$THOME/build/unpack xf86-video-nv-2.1.23
-cd xf86-video-nv-2.1.23
+$THOME/build/unpack xf86-video-nv-2.1.24
+cd xf86-video-nv-2.1.24
 env PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig \
 ./configure --prefix=/usr \
 --with-xorg-module-dir=/usr/lib/xorg/modules/amd64 \
@@ -16,4 +16,4 @@ CXXFLAGS="-m64 -O2"
 gmake -j
 cd ..
 
-${THOME}/build/genpkg TRIBxorg-driver-video-nv xf86-video-nv-2.1.23
+${THOME}/build/genpkg TRIBxorg-driver-video-nv xf86-video-nv-2.1.24
