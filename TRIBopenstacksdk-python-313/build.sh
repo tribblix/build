01b4508b75ab431c2108521c8bdd5c4c2fc9d314
Peter Tribble <peter.tribble@gmail.com>
2026-04-27 17:28:58 +0100

    Update openstacksdk 4.12.0

--- TRIBopenstacksdk-python-313/build.sh
+++ TRIBopenstacksdk-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBopenstacksdk-python-313 openstacksdk-4.11.0
+${THOME}/build/pkg_pep518 -N TRIBopenstacksdk-python-313 openstacksdk-4.12.0
