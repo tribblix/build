cff5931e582a1aeeba6f489e77db5d3ec8d0afca
Peter Tribble <peter.tribble@gmail.com>
2026-04-27 11:32:18 +0100

    Update minio client, and build with go 1.26

--- TRIBminio-client/build.sh
+++ TRIBminio-client/build.sh
@@ -4,7 +4,10 @@
 #
 mkdir ~/ud/c
 cd ~/ud/c
-env GOPATH=`pwd` PATH=/usr/versions/go-1.25/bin:$PATH go install github.com/minio/mc@RELEASE.2025-07-21T05-28-08Z
+#
+# this appears to be the final version
+#
+env GOPATH=`pwd` PATH=/usr/versions/go-1.26/bin:$PATH go install github.com/minio/mc@RELEASE.2025-08-13T08-35-41Z
 
 #
 # install under a different name as Midnight Commander is also called mc
@@ -13,7 +16,7 @@ rm -fr /tmp/ee
 mkdir -p /tmp/ee/usr/bin
 mkdir -p /tmp/ee/usr/share/minio-client
 cp bin/mc /tmp/ee/usr/bin/minio-client
-cp pkg/mod/github.com/minio/mc@v0.0.0-20250721*/README.md /tmp/ee/usr/share/minio-client
-cp pkg/mod/github.com/minio/mc@v0.0.0-20250721*/LICENSE /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20250813*/README.md /tmp/ee/usr/share/minio-client
+cp pkg/mod/github.com/minio/mc@v0.0.0-20250813*/LICENSE /tmp/ee/usr/share/minio-client
 ${THOME}/build/create_pkg TRIBminio-client /tmp/ee
 rm -fr /tmp/ee
