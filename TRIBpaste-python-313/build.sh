72ba74134c7d75871766d6ed6e9a9b003495d064
Peter Tribble <peter.tribble@gmail.com>
2026-04-27 11:24:55 +0100

    Package routes paste pastedeploy for python 3.13

---
+++ TRIBpaste-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBpaste-python-313 paste-3.10.1
---
+++ TRIBpastedeploy-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBpastedeploy-python-313 PasteDeploy-3.1.0
