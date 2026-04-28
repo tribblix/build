f2af06c5ab0e7fdf9daca1c12ff771b11c44ca37
Peter Tribble <peter.tribble@gmail.com>
2026-04-28 08:30:35 +0100

    Update pyOpenSSL 26.1.0, and package for python 3.13

--- TRIBpyopenssl-313/build.sh
+++ TRIBpyopenssl-313/build.sh
@@ -5,4 +5,4 @@
 # despite the name, the actual openssl bindings are in the
 # cryptography module, not this one
 #
-${THOME}/build/pkg_pep518 -N TRIBpyopenssl-312 pyopenssl-26.1.0
+${THOME}/build/pkg_pep518 -N TRIBpyopenssl-313 pyopenssl-26.1.0
