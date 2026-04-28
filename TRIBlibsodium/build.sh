b4906c68494c250e4e2e00e3ee26e36521601aed
Peter Tribble <peter.tribble@gmail.com>
2026-04-28 08:37:17 +0100

    Update libsodium 1.0.22

--- TRIBlibsodium/build.sh
+++ TRIBlibsodium/build.sh
@@ -5,10 +5,7 @@
 # the intention is to build pynacl against the system libsodium provided here,
 # to reduce fragility in the pynacl build
 #
-# the downloaded tarball unpacks into libsodium-stable, so
-# repack it to create something more useful
-#
 # there's a postconf patch, absolutely necessary on sparc
 #
-${THOME}/build/dobuild +64 libsodium-1.0.19
-${THOME}/build/genpkg TRIBlibsodium libsodium-1.0.19
+${THOME}/build/dobuild +64 libsodium-1.0.22
+${THOME}/build/genpkg TRIBlibsodium libsodium-1.0.22
