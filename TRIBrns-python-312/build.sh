d6f3436e1a362927ec33f9690aaf8987969881eb
Peter Tribble <peter.tribble@gmail.com>
2026-04-29 09:21:32 +0100

    Update rns 1.2.0, and package for python 3.13

--- TRIBrns-python-312/build.sh
+++ TRIBrns-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBrns-python-312 rns-1.1.9
+${THOME}/build/pkg_pep518 -N TRIBrns-python-312 rns-1.2.0
---
+++ TRIBrns-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBrns-python-313 rns-1.2.0
