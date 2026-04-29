676106b4d1cfce0c7c1d46f9097371410027dcf4
Peter Tribble <peter.tribble@gmail.com>
2026-04-29 19:19:38 +0100

    Update opentelemetry-collector 0.151.0

--- TRIBblix-otel-collector/build.sh
+++ TRIBblix-otel-collector/build.sh
@@ -2,10 +2,10 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/unpack opentelemetry-collector-0.147.0
-cd opentelemetry-collector-0.147.0
+$THOME/build/unpack opentelemetry-collector-0.151.0
+cd opentelemetry-collector-0.151.0
 
-env PATH=/usr/versions/go-1.25/bin:$PATH gmake otelcorecol
+env PATH=/usr/versions/go-1.26/bin:$PATH gmake otelcorecol
 
 rm -fr /tmp/ee
 mkdir -p /tmp/ee/opt/tribblix/otel/bin
