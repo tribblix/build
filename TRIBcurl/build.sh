cd8c46073f3c62a2a769c2336a755428721e6550
Peter Tribble <peter.tribble@gmail.com>
2026-04-29 09:43:02 +0100

    Update curl 8.20.0, and enable brotli for the 64-bit build

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 curl-8.19.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli --disable-ldap"
-${THOME}/build/genpkg TRIBcurl curl-8.19.0
+# disable brotle in the 32-bit build, as our brotli is 64-bit only
+#
+${THOME}/build/dobuild -64 curl-8.20.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli --disable-ldap" +C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --disable-ldap"
+${THOME}/build/genpkg TRIBcurl curl-8.20.0
