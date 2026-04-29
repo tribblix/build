52862df556465c1a1cc88e40f1aec3d794af0d68
Peter Tribble <peter.tribble@gmail.com>
2026-04-29 18:24:36 +0100

    Update agedu 20260410

--- TRIBagedu/build.sh
+++ TRIBagedu/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_LDFLAGS=-lsocket $THOME/build/cmbuild -64only agedu-20241013.3622eda
-${THOME}/build/genpkg TRIBagedu agedu-20241013.3622eda
+env TRIBBLIX_LDFLAGS=-lsocket $THOME/build/cmbuild -64only agedu-20260410.3622eda
+${THOME}/build/genpkg TRIBagedu agedu-20260410.3622eda
