ed647822e7a125b400fc5780676f22b41000db28
Peter Tribble <peter.tribble@gmail.com>
2026-04-30 16:25:48 +0100

    Package httpcore for python 3.13

---
+++ TRIBh11-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBh11-python-313 h11-0.16.0
---
+++ TRIBh2-python-313/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# ship these 3 together to avoid exploding the number of packages
+# hpack and hyperframe are used by h2, and all come from the same
+# main project
+#
+${THOME}/build/pkg_pep518 -N TRIBh2-python-313 hpack-4.1.0 hyperframe-6.1.0 h2-4.3.0
