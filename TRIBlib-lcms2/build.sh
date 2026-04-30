78690aaa0431de7ad208acc16998cfe5e903c01d
Peter Tribble <peter.tribble@gmail.com>
2026-04-30 09:13:16 +0100

    Update lcms2 2.19

--- TRIBlib-lcms2/build.sh
+++ TRIBlib-lcms2/build.sh
@@ -4,5 +4,5 @@
 #
 # the dependencies are pulled in by the cli tools, not by the library
 #
-${THOME}/build/dobuild -64 lcms2-2.16
-${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.16
+${THOME}/build/dobuild -64 lcms2-2.19
+${THOME}/build/genpkg TRIBlib-lcms2 lcms2-2.19
