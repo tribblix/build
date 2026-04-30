31e3c80521c1c67181198618341ac67a1c84f882
Peter Tribble <peter.tribble@gmail.com>
2026-04-30 11:08:28 +0100

    Update gnutls 3.8.13

--- TRIBgnutls/build.sh
+++ TRIBgnutls/build.sh
@@ -8,5 +8,5 @@
 # --enable-srp-authentication out of extreme caution over
 # binary compatibility
 #
-${THOME}/build/dobuild -64only gnutls-3.8.12 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs --enable-srp-authentication"
-${THOME}/build/genpkg TRIBgnutls gnutls-3.8.12
+${THOME}/build/dobuild -64only gnutls-3.8.13 -C "--disable-doc --without-tpm --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-dependency-tracking --with-default-trust-store-file=/etc/ssl/cacert.pem --with-default-trust-store-dir=/etc/ssl/certs --enable-srp-authentication"
+${THOME}/build/genpkg TRIBgnutls gnutls-3.8.13
