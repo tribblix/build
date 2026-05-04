0d37a19df36b53c92f7b2c0f597060a0f742f373
Peter Tribble <peter.tribble@gmail.com>
2026-05-04 13:52:27 +0100

    Update file 5.47

--- TRIBfile-magic/build.sh
+++ TRIBfile-magic/build.sh
@@ -2,7 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
+# remember to bump the version in fixit
 # if the version gets updated, rebuild TRIBmog to match
 #
-${THOME}/build/dobuild -64only file-5.45 -C --disable-lzlib
-${THOME}/build/genpkg TRIBfile-magic file-5.45
+${THOME}/build/dobuild -64only file-5.47 -C --disable-lzlib
+${THOME}/build/genpkg TRIBfile-magic file-5.47
