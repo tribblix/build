eb05189197d2bbf745c190bc0ea5a380c0e88695
Peter Tribble <peter.tribble@gmail.com>
2026-05-05 16:24:35 +0100

    Drop now-unsupported Django 4

--- TRIBdjango4-python-312/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# pytz is deprecated, but marked as a dependency for transitional purposes
-#
-${THOME}/build/pkg_pep518 -N TRIBdjango4-python-312 django-4.2.30
