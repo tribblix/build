49a0c1431871f295839c7a5091924bedaf420bac
Peter Tribble <peter.tribble@gmail.com>
2026-05-06 19:47:58 +0100

    Switch asciinema to python 3.13

--- TRIBasciinema-python-312/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_pep518 -N TRIBasciinema-python-312 asciinema-2.4.0
---
+++ TRIBasciinema-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBasciinema-python-313 asciinema-2.4.0
