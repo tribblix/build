111dbf93da7e258a641cd9e8509c881f996e5bf6
Peter Tribble <peter.tribble@gmail.com>
2026-05-06 19:22:48 +0100

    Update osprofiler 4.4.0

--- TRIBosprofiler-python-313/build.sh
+++ TRIBosprofiler-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBosprofiler-python-313 osprofiler-4.3.0
+${THOME}/build/pkg_pep518 -N TRIBosprofiler-python-313 osprofiler-4.4.0
