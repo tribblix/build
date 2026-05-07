bc1e98c7906677009538005aefda66e3841a994a
Peter Tribble <peter.tribble@gmail.com>
2026-05-07 09:33:37 +0100

    Update proto-plus 1.28.0

--- TRIBproto-plus-python-313/build.sh
+++ TRIBproto-plus-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBproto-plus-python-313 proto_plus-1.27.2
+${THOME}/build/pkg_pep518 -N TRIBproto-plus-python-313 proto_plus-1.28.0
