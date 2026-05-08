f45a093770073b89ddb4e8ba968e3cdcb54b3ae0
Peter Tribble <peter.tribble@gmail.com>
2026-05-08 12:04:25 +0100

    Update azure-core 1.41.0

--- TRIBazure-core-python-313/build.sh
+++ TRIBazure-core-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBazure-core-python-313 azure_core-1.40.0
+${THOME}/build/pkg_pep518 -N TRIBazure-core-python-313 azure_core-1.41.0
