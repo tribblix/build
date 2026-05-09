42e14f6fea38c96ddbca066eee90300a4ec481dd
Peter Tribble <peter.tribble@gmail.com>
2026-05-09 08:42:49 +0100

    Update matplotlib-inline 0.2.2

--- TRIBmatplot-inline-python-313/build.sh
+++ TRIBmatplot-inline-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBmatplot-inline-python-313 matplotlib_inline-0.2.1
+${THOME}/build/pkg_pep518 -N TRIBmatplot-inline-python-313 matplotlib_inline-0.2.2
