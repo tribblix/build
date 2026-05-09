ff6529694ed75d6bceb7671d5b5a91e7ac7e5f02
Peter Tribble <peter.tribble@gmail.com>
2026-05-09 11:34:39 +0100

    A few more python 3.13 modules

---
+++ TRIBec2-metadata-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBec2-metadata-python-313 ec2_metadata-3.0.0
---
+++ TRIBipgw-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBipgw-python-313 iampoliciesgonewild-1.0.6.2
