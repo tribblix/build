e8691ae75ed7c6f9e6d2d944ea2a050c66ff7c68
Peter Tribble <peter.tribble@gmail.com>
2026-05-09 09:01:15 +0100

    Update perl Test::Most 0.42

--- TRIBlib-perl-5-test-most/build.sh
+++ TRIBlib-perl-5-test-most/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Test-Most-0.38
-cd Test-Most-0.38
+${THOME}/build/unpack Test-Most-0.42
+cd Test-Most-0.42
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-test-most Test-Most-0.38
+${THOME}/build/genpkg TRIBlib-perl-5-test-most Test-Most-0.42
