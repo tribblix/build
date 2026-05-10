aba1642044bb21d9d9e9fc66642f2fc5962cf743
Peter Tribble <peter.tribble@gmail.com>
2026-05-10 18:19:09 +0100

    Use getperlver to abstract perl version

--- TRIBirssi/build.sh
+++ TRIBirssi/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only irssi-1.4.5 -C "--with-perl=yes --with-perl-lib=/usr/perl5/site_perl/5.34"
+env TRIBBLIX_CFLAGS=-I/usr/gnu/include/ncurses TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only irssi-1.4.5 -C "--with-perl=yes --with-perl-lib=/usr/perl5/site_perl/`${THOME}/build/getperlver`"
 ${THOME}/build/genpkg TRIBirssi irssi-1.4.5
