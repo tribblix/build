c12b07bff4b5ceb69b626d28561ff03acf6561d5
Peter Tribble <peter.tribble@gmail.com>
2026-05-10 19:30:54 +0100

    Rebuild nginx exporter with go 1.26

--- TRIBblix-promnginx/build.sh
+++ TRIBblix-promnginx/build.sh
@@ -5,7 +5,7 @@
 
 $THOME/build/unpack nginx-prometheus-exporter-1.5.1
 cd nginx-prometheus-exporter-1.5.1
-env PATH=/usr/versions/go-1.25/bin:/usr/gnu/bin:$PATH gmake nginx-prometheus-exporter
+env PATH=/usr/versions/go-1.26/bin:/usr/gnu/bin:$PATH gmake nginx-prometheus-exporter
 
 ${THOME}/build/genpkg TRIBblix-promnginx
 cd ..
