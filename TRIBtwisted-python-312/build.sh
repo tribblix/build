07336f4c4edba0092b8fb70e482421383d4772da
Peter Tribble <peter.tribble@gmail.com>
2026-05-11 21:13:58 +0100

    Update Twisted 26.4.0

--- TRIBtwisted-python-312/build.sh
+++ TRIBtwisted-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBtwisted-python-312 twisted-25.5.0
+${THOME}/build/pkg_pep518 -N TRIBtwisted-python-312 twisted-26.4.0
--- TRIBtwisted-python-313/build.sh
+++ TRIBtwisted-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBtwisted-python-313 twisted-25.5.0
+${THOME}/build/pkg_pep518 -N TRIBtwisted-python-313 twisted-26.4.0
