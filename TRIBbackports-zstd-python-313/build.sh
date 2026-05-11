9e5c50b56f0ca10f959d2b14ce0bff2f72333325
Peter Tribble <peter.tribble@gmail.com>
2026-05-11 21:11:01 +0100

    Update backports.zstd 1.5.0

--- TRIBbackports-zstd-python-313/build.sh
+++ TRIBbackports-zstd-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # use the system zstd
 #
-${THOME}/build/pkg_pep518 -N -C "--config-settings=--build-option=--system-zstd" TRIBbackports-zstd-python-313 backports_zstd-1.4.0
+${THOME}/build/pkg_pep518 -N -C "--config-settings=--build-option=--system-zstd" TRIBbackports-zstd-python-313 backports_zstd-1.5.0
