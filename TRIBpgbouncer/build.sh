d41e46d148f6e14787cd01acaff2dcdf150983d8
Peter Tribble <peter.tribble@gmail.com>
2026-05-11 22:32:36 +0100

    Update pgBouncer 1.25.2

--- TRIBpgbouncer/build.sh
+++ TRIBpgbouncer/build.sh
@@ -4,6 +4,9 @@
 #
 # there's a sacrificial static libevent built by the preconf script
 #
-env TRIBBLIX_PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig ${THOME}/build/dobuild -64only pgbouncer-1.25.1
-${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.25.1
+# pandoc is needed for the man pages; if it's not present then the build
+# fails, although a working package will still be generated
+#
+env TRIBBLIX_PKG_CONFIG_PATH=/tmp/mc/lib/pkgconfig ${THOME}/build/dobuild -64only pgbouncer-1.25.2
+${THOME}/build/genpkg TRIBpgbouncer pgbouncer-1.25.2
 rm -fr /tmp/mc
