17f9aa12999b9acedffb1458735fc11ef9acab42
Peter Tribble <peter.tribble@gmail.com>
2026-05-12 18:04:40 +0100

    Update texinfo 7.3

--- TRIBgnu-texinfo/build.sh
+++ TRIBgnu-texinfo/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only texinfo-7.2
-${THOME}/build/genpkg TRIBgnu-texinfo texinfo-7.2
+${THOME}/build/dobuild -64only texinfo-7.3
+${THOME}/build/genpkg TRIBgnu-texinfo texinfo-7.3
