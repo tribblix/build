706621b4a214e3f863ada33e3309e3ab1cf0e06f
Peter Tribble <peter.tribble@gmail.com>
2026-05-12 20:04:05 +0100

    Switch meson over to python 3.13

---
+++ TRIBmesonpy-python-313/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# note that this isn't meson, it's the meson-python aka mesonpy
+# module
+#
+# and note that MesonPy on PyPi is something completely different
+#
+# used by the pandas and scikit-image build
+#
+${THOME}/build/pkg_pep518 TRIBmesonpy-python-313 meson_python-0.19.0
