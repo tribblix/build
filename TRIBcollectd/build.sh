747d56396122b1d6e9e7817ea2fcd04d8bb85147
Peter Tribble <peter.tribble@gmail.com>
2026-05-12 19:05:00 +0100

    Rebuild perl consumers

--- TRIBcollectd/build.sh
+++ TRIBcollectd/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only collectd-5.12.0 -C "--disable-static --localstatedir=/var --sysconfdir=/etc --localstatedir=/var --enable-rrdtool --enable-write_prometheus --disable-werror"
+env TRIBBLIX_LDFLAGS=-lgcrypt ${THOME}/build/dobuild -64only collectd-5.12.0 -C "--disable-static --localstatedir=/var --sysconfdir=/etc --localstatedir=/var --enable-rrdtool --enable-write_prometheus --disable-werror"
 ${THOME}/build/genpkg TRIBcollectd collectd-5.12.0
