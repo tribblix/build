d5938f911dc0930d3663820c08cc45d07fde756b
Peter Tribble <peter.tribble@gmail.com>
2026-05-13 08:58:52 +0100

    Update gnu sed 4.10

--- TRIBtext-gnu-sed/build.sh
+++ TRIBtext-gnu-sed/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only -gnu sed-4.9 -P /usr/gnu
-${THOME}/build/genpkg TRIBtext-gnu-sed sed-4.9
+${THOME}/build/dobuild -64only -gnu sed-4.10 -P /usr/gnu
+${THOME}/build/genpkg TRIBtext-gnu-sed sed-4.10
