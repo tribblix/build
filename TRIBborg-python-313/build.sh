7201e50b136e427cc3167f469ca53959aef1304b
Peter Tribble <peter.tribble@gmail.com>
2026-05-13 17:41:41 +0100

    Almost the last modules for python 3.13

---
+++ TRIBborg-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBborg-python-313 borgbackup-1.4.4
---
+++ TRIBglances-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBglances-python-313 glances-4.5.4
