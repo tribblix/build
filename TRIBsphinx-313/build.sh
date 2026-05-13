2af693c42a43b8b65046c02da1b3b3a031e572cd
Peter Tribble <peter.tribble@gmail.com>
2026-05-13 17:11:58 +0100

    Switch sphinx to python 3.13

---
+++ TRIBsphinx-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBsphinx-313 sphinx-9.1.0
---
+++ TRIBsphinx-ws-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 TRIBsphinx-ws-python-313 sphinxcontrib_websupport-2.0.0
