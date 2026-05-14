02363508409c98aedbeac457f1b3c39b2e78cf30
Peter Tribble <peter.tribble@gmail.com>
2026-05-14 19:06:27 +0100

    Update globus-sdk 4.6.0

--- TRIBglobus-sdk-python-313/build.sh
+++ TRIBglobus-sdk-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBglobus-sdk-python-313 globus_sdk-4.5.0
+${THOME}/build/pkg_pep518 -N TRIBglobus-sdk-python-313 globus_sdk-4.6.0
