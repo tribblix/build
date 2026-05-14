5b3fc26f31111e78229e3857928be700bcd5c976
Peter Tribble <peter.tribble@gmail.com>
2026-05-14 17:23:45 +0100

    Switch durdraw to python 3.13

--- TRIBdurdraw/build.sh
+++ TRIBdurdraw/build.sh
@@ -2,8 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# (built in python)
-#
-# only works in 3.12 due to the way it uses curses
-#
-${THOME}/build/pkg_pep518 -N -v 312 TRIBdurdraw durdraw-0.29.0
+${THOME}/build/pkg_pep518 -N -v 313 TRIBdurdraw durdraw-0.29.0
