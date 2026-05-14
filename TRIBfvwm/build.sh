160d7b6a9e73c99ed84a90a4e0a5e45b042156e2
Peter Tribble <peter.tribble@gmail.com>
2026-05-14 21:11:21 +0100

    Rebuild fvwm for python 3.13

--- TRIBfvwm/build.sh
+++ TRIBfvwm/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# This version requires pyxdg (TRIBxdg-python-312)
+# This version requires pyxdg (TRIBxdg-python-313)
 #
 ${THOME}/build/dobuild -64only fvwm-2.7.0 -C "--disable-bidi"
 ${THOME}/build/genpkg TRIBfvwm fvwm-2.7.0
