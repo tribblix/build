299a36ba9e214ced276bceaa378b1749f49cda88
Peter Tribble <peter.tribble@gmail.com>
2026-05-14 17:43:39 +0100

    Switch fail2ban to python 3.13

--- TRIBfail2ban/build.sh
+++ TRIBfail2ban/build.sh
@@ -5,4 +5,4 @@
 # there's both a fixit and fixinstall file to put everything in the right
 # place as it's not just a python application
 #
-${THOME}/build/pkg_pep518 -N -v 312 TRIBfail2ban fail2ban-1.1.0
+${THOME}/build/pkg_pep518 -N -v 313 TRIBfail2ban fail2ban-1.1.0
