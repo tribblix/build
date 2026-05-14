023baba61de330b281aad5a70c836ec7861abf93
Peter Tribble <peter.tribble@gmail.com>
2026-05-14 16:27:47 +0100

    Update keystoneauth1 5.14.0

--- TRIBkeystoneauth1-python-313/build.sh
+++ TRIBkeystoneauth1-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBkeystoneauth1-python-313 keystoneauth1-5.13.1
+${THOME}/build/pkg_pep518 -N TRIBkeystoneauth1-python-313 keystoneauth1-5.14.0
