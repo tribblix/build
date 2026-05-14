500330eda1284ce88f662551863cb671f1f6b695
Peter Tribble <peter.tribble@gmail.com>
2026-05-14 17:52:20 +0100

    Switch SCons to python 3.13

--- TRIBscons/build.sh
+++ TRIBscons/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# (built in python)
-#
-${THOME}/build/pkg_pep518 -N -v 312 TRIBscons scons-4.10.1
+${THOME}/build/pkg_pep518 -N -v 313 TRIBscons scons-4.10.1
