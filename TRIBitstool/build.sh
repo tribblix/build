f18c1273e15ac645e84e56676219bc52b512fe3a
Peter Tribble <peter.tribble@gmail.com>
2026-05-15 20:25:09 +0100

    Rebuild itstool with python 3.13

--- TRIBitstool/build.sh
+++ TRIBitstool/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild itstool-2.0.7 -C PYTHON=/usr/versions/python-3.12/bin/python3
+${THOME}/build/dobuild itstool-2.0.7 -C PYTHON=/usr/versions/python-3.13/bin/python3
 ${THOME}/build/genpkg TRIBitstool itstool-2.0.7
