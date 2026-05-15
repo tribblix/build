4a67a3c4ca629d745348d18eab09de27e7a63779
Peter Tribble <peter.tribble@gmail.com>
2026-05-15 19:06:43 +0100

    Use python 3.13 to build ninja

--- TRIBninja/build.sh
+++ TRIBninja/build.sh
@@ -4,7 +4,7 @@
 #
 $THOME/build/unpack ninja-1.13.2
 cd ninja-1.13.2
-env CFLAGS=-m64 python3.12 ./configure.py --bootstrap
+env CFLAGS=-m64 python3.13 ./configure.py --bootstrap
 rm -fr /tmp/n1
 mkdir -p /tmp/n1/usr/bin
 cp ninja /tmp/n1/usr/bin
