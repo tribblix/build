fcaff85f57d21d978d7a33755a4ab8a2a9667521
Peter Tribble <peter.tribble@gmail.com>
2026-05-15 17:54:12 +0100

    Update jsonargparse 4.49.0

--- TRIBjsonargparse-python-313/build.sh
+++ TRIBjsonargparse-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBjsonargparse-python-313 jsonargparse-4.48.0
+${THOME}/build/pkg_pep518 -N TRIBjsonargparse-python-313 jsonargparse-4.49.0
