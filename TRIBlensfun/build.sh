b3b55ebe02170e0002c91f4e352aa9b6e88309eb
Peter Tribble <peter.tribble@gmail.com>
2026-05-15 19:23:59 +0100

    Rebuild lensfun against python 3.13

--- TRIBlensfun/build.sh
+++ TRIBlensfun/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# there are python pieces, although we don't declare a dependency
+# there are python pieces, although we don't declare a runtime dependency
 # these are installed manually - see fixit and fixinstall - as the
 # cmake build installs a deprecated egg and puts it in the wrong place
 #
