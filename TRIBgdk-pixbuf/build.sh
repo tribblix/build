1939244a619d4b92c87bd5e3acacb13b2467e088
Peter Tribble <peter.tribble@gmail.com>
2026-05-16 20:56:50 +0100

    Rebuild gdk-pixbuf with python 3.13

--- TRIBgdk-pixbuf/build.sh
+++ TRIBgdk-pixbuf/build.sh
@@ -4,5 +4,5 @@
 #
 # needs python in the path to find rst2man to generate man pages
 #
-env AR=/usr/bin/ar PATH=${PATH}:/usr/versions/python-3.12/bin ${THOME}/build/mesonbuild -64 -gnu gdk-pixbuf-2.42.12 -C '-Dothers=enabled -Dtests=false -Dinstalled_tests=false -Dintrospection=disabled'
+env AR=/usr/bin/ar PATH=${PATH}:/usr/versions/python-3.13/bin ${THOME}/build/mesonbuild -64 -gnu gdk-pixbuf-2.42.12 -C '-Dothers=enabled -Dtests=false -Dinstalled_tests=false -Dintrospection=disabled'
 ${THOME}/build/genpkg TRIBgdk-pixbuf gdk-pixbuf-2.42.12
