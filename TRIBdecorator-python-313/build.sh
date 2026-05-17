50cb1bee94e7f9ca3c83febb9aa3a38a1583eb5b
Peter Tribble <peter.tribble@gmail.com>
2026-05-17 18:12:45 +0100

    Update decorator 5.3.0

--- TRIBdecorator-python-313/build.sh
+++ TRIBdecorator-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBdecorator-python-313 decorator-5.2.1
+${THOME}/build/pkg_pep518 -N TRIBdecorator-python-313 decorator-5.3.0
