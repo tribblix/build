fb98b9674d44ef562dfa37d9afc8410aac826aff
Peter Tribble <peter.tribble@gmail.com>
2026-05-24 10:53:53 +0100

    Update oslo.config 10.4.0

--- TRIBoslo-config-python-313/build.sh
+++ TRIBoslo-config-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-config-python-313 oslo_config-10.3.0
+${THOME}/build/pkg_pep518 -N TRIBoslo-config-python-313 oslo_config-10.4.0
