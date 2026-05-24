fafe6cb02bfaa960db7f6b917fd038ac6d02ff82
Peter Tribble <peter.tribble@gmail.com>
2026-05-24 11:14:39 +0100

    Update oslo.metrics 0.16.0

--- TRIBoslo-metrics-python-312/build.sh
+++ TRIBoslo-metrics-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-metrics-python-312 oslo_metrics-0.15.1
+${THOME}/build/pkg_pep518 -N TRIBoslo-metrics-python-312 oslo_metrics-0.16.0
