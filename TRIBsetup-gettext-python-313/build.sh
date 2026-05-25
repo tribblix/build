7ea7890f1f5f3afc7266330f2b669c032a1d3bb3
Peter Tribble <peter.tribble@gmail.com>
2026-05-25 11:09:54 +0100

    Update setuptools-gettext 0.1.18

--- TRIBsetup-gettext-python-313/build.sh
+++ TRIBsetup-gettext-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBsetup-gettext-python-313 setuptools_gettext-0.1.16
+${THOME}/build/pkg_pep518 -N TRIBsetup-gettext-python-313 setuptools_gettext-0.1.18
