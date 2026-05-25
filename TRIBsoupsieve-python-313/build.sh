dc03616d23f89ccd5128896546b657e83b5eb0c0
Peter Tribble <peter.tribble@gmail.com>
2026-05-25 09:29:39 +0100

    Update soupsieve 2.8.4

--- TRIBsoupsieve-python-313/build.sh
+++ TRIBsoupsieve-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBsoupsieve-python-313 soupsieve-2.8.3
+${THOME}/build/pkg_pep518 -N TRIBsoupsieve-python-313 soupsieve-2.8.4
