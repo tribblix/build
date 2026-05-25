c7961bd91c08060b9f004b2c3c352bf6e52327f2
Peter Tribble <peter.tribble@gmail.com>
2026-05-25 21:27:02 +0100

    Update libXi 1.8.3

--- TRIBx11-libxi/build.sh
+++ TRIBx11-libxi/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 libXi-1.7.10
-${THOME}/build/genpkg TRIBx11-libxi libXi-1.7.10
+${THOME}/build/dobuild -64 libXi-1.8.3
+${THOME}/build/genpkg TRIBx11-libxi libXi-1.8.3
