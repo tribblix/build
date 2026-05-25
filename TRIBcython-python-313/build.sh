dcbe7508fe698d88f968a278f943b461fa45878a
Peter Tribble <peter.tribble@gmail.com>
2026-05-25 10:58:39 +0100

    Update Cython 3.2.5

--- TRIBcython-python-313/build.sh
+++ TRIBcython-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # there's a limited range supported by the scipy stack
 #
-${THOME}/build/pkg_pep518 -N TRIBcython-python-313 cython-3.2.4
+${THOME}/build/pkg_pep518 -N TRIBcython-python-313 cython-3.2.5
