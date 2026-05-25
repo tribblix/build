8bbf1793cf07454f5a601af2931a44b9420cf083
Peter Tribble <peter.tribble@gmail.com>
2026-05-25 10:06:51 +0100

    Update python-glanceclient 4.12.0

--- TRIBglanceclient-python-312/build.sh
+++ TRIBglanceclient-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBglanceclient-python-312 python_glanceclient-4.11.0
+${THOME}/build/pkg_pep518 -N TRIBglanceclient-python-312 python_glanceclient-4.12.0
