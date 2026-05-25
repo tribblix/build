9d44d7fe87164fa6a69d0074635d7b35c4ef7421
Peter Tribble <peter.tribble@gmail.com>
2026-05-25 15:25:06 +0100

    Update Xapian 1.4.31

--- TRIBxapian-bindings/build.sh
+++ TRIBxapian-bindings/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xapian-bindings-1.4.30 -C --without-ruby
-${THOME}/build/genpkg TRIBxapian-bindings xapian-bindings-1.4.30
+${THOME}/build/dobuild -64only xapian-bindings-1.4.31 -C --without-ruby
+${THOME}/build/genpkg TRIBxapian-bindings xapian-bindings-1.4.31
--- TRIBxapian-omega/build.sh
+++ TRIBxapian-omega/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xapian-omega-1.4.30 -C "--localstatedir=/var --sysconfdir=/etc"
-${THOME}/build/genpkg TRIBxapian-omega xapian-omega-1.4.30
+${THOME}/build/dobuild -64only xapian-omega-1.4.31 -C "--localstatedir=/var --sysconfdir=/etc"
+${THOME}/build/genpkg TRIBxapian-omega xapian-omega-1.4.31
