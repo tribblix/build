9e251fe25ba6db751b8fcd92b9898aa3df100615
Peter Tribble <peter.tribble@gmail.com>
2026-05-26 13:27:42 +0100

    Rebuild libxcb with python 3.13

--- TRIBlibxcb/build.sh
+++ TRIBlibxcb/build.sh
@@ -9,13 +9,18 @@
 # increment the new components one at a time until done
 #
 # if you update the python version, you need to build the whole cycle
-# from scratch, as xcb-proto is tied to a given python version
+# from scratch, as xcb-proto is tied to a given python version, but a
+# quick workaround is to build twice:
+# 0. Extend fixit to correct the old and new python versions
+# 1. Build everything except libxcb as below, but add -C "PYTHON=python3.X"
+#    for the libxcb step to use the *old* python corresponding to the
+#    currently installed version of xcb-proto
+# 2. Install that package, then rebuild against the newly installed version
+# 3. Just have the new python version in fixit
 #
 ${THOME}/build/dobuild xcb-proto-1.16.0
 ${THOME}/build/dobuild libpthread-stubs-0.5
 # [install]
-# we had xevie and xprint before, but no consumers
-# if switching python, can add -C "PYTHON=python3.9" to select
 ${THOME}/build/dobuild -64 libxcb-1.16.1
 # [install]
 ${THOME}/build/dobuild -64 xcb-util-0.4.1
