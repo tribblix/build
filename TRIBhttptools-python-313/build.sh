642075574696a253eae490171046c0c4c540d4e8
Peter Tribble <peter.tribble@gmail.com>
2026-05-26 09:34:42 +0100

    Update httptools 0.8.0

--- TRIBhttptools-python-313/build.sh
+++ TRIBhttptools-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBhttptools-python-313 httptools-0.7.1
+${THOME}/build/pkg_pep518 -N TRIBhttptools-python-313 httptools-0.8.0
