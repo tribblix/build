d60f4ef61e1b9f568b338539aee6cf52dd3e8e5d
Peter Tribble <peter.tribble@gmail.com>
2026-05-27 19:17:18 +0100

    Update LibRaw 0.22.1

--- TRIBlibraw/build.sh
+++ TRIBlibraw/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only LibRaw-0.21.1 -L -C "--enable-jasper --enable-zlib --enable-lcms --disable-openmp --enable-shared --disable-static --disable-examples"
-${THOME}/build/genpkg TRIBlibraw LibRaw-0.21.1
+${THOME}/build/dobuild -64only LibRaw-0.22.1 -L -C "--enable-zlib --enable-lcms --enable-jpeg --disable-openmp --enable-shared --disable-static --disable-examples"
+${THOME}/build/genpkg TRIBlibraw LibRaw-0.22.1
