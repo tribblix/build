e8eb2dd4b4c607d4a921cd3093bae9202cdfc0dc
Peter Tribble <peter.tribble@gmail.com>
2026-05-27 10:06:42 +0100

    No longer need libassuan v2

--- TRIBlibassuan/build.sh
+++ TRIBlibassuan/build.sh
@@ -4,8 +4,5 @@
 #
 # libassuan
 #
-# still ship the old library for binary compatibility
-#
-${THOME}/build/dobuild +64 libassuan-2.5.7
 ${THOME}/build/dobuild +64 libassuan-3.0.2
-${THOME}/build/genpkg TRIBlibassuan libassuan-2.5.7 libassuan-3.0.2
+${THOME}/build/genpkg TRIBlibassuan libassuan-3.0.2
