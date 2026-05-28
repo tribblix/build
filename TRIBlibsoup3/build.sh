a12e3dda42a2ffa6b3f7e030694a8d41012d7e80
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 20:39:35 +0100

    Enable introspection and vapi for libsoup3

--- TRIBlibsoup3/build.sh
+++ TRIBlibsoup3/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/mesonbuild -64only libsoup-3.6.6 -C "-Dsysprof=disabled -Dintrospection=disabled -Dpkcs11_tests=disabled"
+# needs introspection so that gssdp can build with introspection enabled
+#
+$THOME/build/mesonbuild -64only libsoup-3.6.6 -C "-Dsysprof=disabled -Dpkcs11_tests=disabled"
 ${THOME}/build/genpkg TRIBlibsoup3 libsoup-3.6.6
