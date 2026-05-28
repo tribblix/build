11a85f38ae4d08f97aaf5d509c201ee5bdeec455
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 15:10:32 +0100

    Rebuild glib2 with introspection enabled

--- TRIBlib-glib2/build.sh
+++ TRIBlib-glib2/build.sh
@@ -8,9 +8,21 @@
 #
 
 #
+# there's a deadly embrace at bootstrapping, in the gobject-introspection
+# needs glib2, but the glib2 gir modules are supplied from here and need
+# gobject-introspection. so you may need to add -Dintrospection=disabled
+# here, build glib2, build gobject-introspection against this, and then
+# rebuild glib2 fully
+#
+
+#
+# also the existence of dconf breaks the install of the gir files
+#
+
+#
 # our libelf doesn't have large-file support in 32-bit mode
 # need docutils to be in the default path to find rst2man
 #
-env TRIBBLIX_LDFLAGS="-lsocket" TRIBBLIX_CFLAGS="-Wno-error=format -Wno-error=format-nonliteral -D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT" $THOME/build/mesonbuild +gnu -64 glib-2.82.5 -C "-Dxattr=false -Dman-pages=enabled -Ddtrace=false -Dsysprof=disabled -Dlibelf=disabled -Dtests=false -Dintrospection=disabled" +C "-Dxattr=false -Dman-pages=enabled -Ddtrace=false -Dsysprof=disabled -Dtests=false -Dintrospection=disabled"
+env TRIBBLIX_LDFLAGS="-lsocket" TRIBBLIX_CFLAGS="-Wno-error=format -Wno-error=format-nonliteral -D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT" $THOME/build/mesonbuild +gnu -64 glib-2.82.5 -C "-Dxattr=false -Dman-pages=enabled -Ddtrace=false -Dsysprof=disabled -Dlibelf=disabled -Dtests=false" +C "-Dxattr=false -Dman-pages=enabled -Ddtrace=false -Dsysprof=disabled -Dtests=false"
 
 $THOME/build/genpkg TRIBlib-glib2 glib-2.82.5
