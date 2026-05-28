d42c2fd710ff9a97a59d3314cf1d30ce717e0dda
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 12:06:58 +0100

    Add gst-editing-services

---
+++ TRIBgstreamer1-editing-services/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu +64 gst-editing-services-1.28.1 -C "-Dtests=disabled -Dintrospection=disabled"
+${THOME}/build/genpkg TRIBgstreamer1-editing-services gst-editing-services-1.28.1
