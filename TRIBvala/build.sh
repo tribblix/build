0a7512187b7b7c2846ff42bb6b64d4536d8bc696
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 12:04:59 +0100

    Update vala 0.56.19

--- TRIBvala/build.sh
+++ TRIBvala/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only vala-0.56.1 -C --disable-valadoc
-${THOME}/build/genpkg TRIBvala vala-0.56.1
+${THOME}/build/dobuild -64only vala-0.56.19 -C --disable-valadoc
+${THOME}/build/genpkg TRIBvala vala-0.56.19
