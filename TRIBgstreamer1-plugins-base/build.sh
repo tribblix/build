51abfdada1fe0e21ec0df629b68e77975f422c96
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 21:51:56 +0100

    Update GStreamer 1.28.3

--- TRIBgstreamer1-plugins-base/build.sh
+++ TRIBgstreamer1-plugins-base/build.sh
@@ -7,5 +7,5 @@
 #
 # sparc needs -Dgl-graphene=disabled
 #
-env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu -64 gst-plugins-base-1.28.1 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dgl_api=opengl -Dgl-graphene=disabled"
-${THOME}/build/genpkg TRIBgstreamer1-plugins-base gst-plugins-base-1.28.1
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu -64 gst-plugins-base-1.28.3 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dgl_api=opengl -Dgl-graphene=disabled" +C "-Dexamples=disabled -Dtests=disabled -Dgl_api=opengl -Dgl-graphene=disabled"
+${THOME}/build/genpkg TRIBgstreamer1-plugins-base gst-plugins-base-1.28.3
--- TRIBgstreamer1/build.sh
+++ TRIBgstreamer1/build.sh
@@ -8,5 +8,7 @@
 # there are 32 and 64-bit postconf patches to ensure that there are different
 # registries for 32 and 64-bit applications, otherwise one will crash
 #
-env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu -64 gstreamer-1.28.1 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled" +C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled -Dlibexecdir=/usr/libexec/`$THOME/build/getarch`"
-${THOME}/build/genpkg TRIBgstreamer1 gstreamer-1.28.1
+# enable introspection as gupnp-dlna needs it
+#
+env TRIBBLIX_LDFLAGS="-lsocket" AR=/usr/bin/ar ${THOME}/build/mesonbuild +gnu -64 gstreamer-1.28.3 -C "-Dintrospection=disabled -Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled" +C "-Dexamples=disabled -Dtests=disabled -Dbenchmarks=disabled -Dlibexecdir=/usr/libexec/`$THOME/build/getarch`"
+${THOME}/build/genpkg TRIBgstreamer1 gstreamer-1.28.3
