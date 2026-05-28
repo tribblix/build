f9931022a4d1ce8acae71e7456ca40712c7a3138
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 13:27:56 +0100

    Update glib 2.82.5

--- TRIBlib-glib2/build.sh
+++ TRIBlib-glib2/build.sh
@@ -10,3 +10,9 @@
 env TRIBBLIX_LDFLAGS="-lsocket" TRIBBLIX_CFLAGS="-Wno-error=format -Wno-error=format-nonliteral -D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT" $THOME/build/mesonbuild +gnu -64 glib-2.76.6 -C "-Dxattr=false -Dman=true -Ddtrace=false"
 
 $THOME/build/genpkg TRIBlib-glib2 glib-2.76.6
+#
+# our libelf doesn't have large-file support in 32-bit mode
+# need docutils to be in the default path to find rst2man
+#
+env TRIBBLIX_LDFLAGS="-lsocket" TRIBBLIX_CFLAGS="-Wno-error=format -Wno-error=format-nonliteral -D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT" $THOME/build/mesonbuild +gnu -64 glib-2.82.5 -C "-Dxattr=false -Dman-pages=enabled -Ddtrace=false -Dsysprof=disabled -Dlibelf=disabled -Dtests=false -Dintrospection=disabled" +C "-Dxattr=false -Dman-pages=enabled -Ddtrace=false -Dsysprof=disabled -Dtests=false -Dintrospection=disabled"
+$THOME/build/genpkg TRIBlib-glib2 glib-2.82.5
