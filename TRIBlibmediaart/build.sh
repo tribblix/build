d40d0a33dbe644601b857c1b3bbe35346332dc23
Peter Tribble <peter.tribble@gmail.com>
2026-05-28 21:15:49 +0100

    Enable introspection and vapi for libmediaart

--- TRIBlibmediaart/build.sh
+++ TRIBlibmediaart/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/mesonbuild -64only libmediaart-1.9.7 -C -Dintrospection=false
+${THOME}/build/mesonbuild -64only libmediaart-1.9.7
 ${THOME}/build/genpkg TRIBlibmediaart libmediaart-1.9.7
