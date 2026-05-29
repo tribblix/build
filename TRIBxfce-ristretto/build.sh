0b3092922d9a1d85e644f60a20284df303a1ac76
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 20:36:37 +0100

    Update ristretto 0.14.0

--- TRIBxfce-ristretto/build.sh
+++ TRIBxfce-ristretto/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -gnu -64only ristretto-0.13.2
-${THOME}/build/genpkg TRIBxfce-ristretto ristretto-0.13.2
+${THOME}/build/mesonbuild -gnu -64only ristretto-0.14.0
+${THOME}/build/genpkg TRIBxfce-ristretto ristretto-0.14.0
