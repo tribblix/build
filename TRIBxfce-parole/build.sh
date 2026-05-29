50099d410ca1202bea8ae21d2d07151b69a09f55
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 20:29:21 +0100

    Update parole 4.18.2

--- TRIBxfce-parole/build.sh
+++ TRIBxfce-parole/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only -gnu parole-4.18.1
-${THOME}/build/genpkg TRIBxfce-parole parole-4.18.1
+${THOME}/build/dobuild -64only -gnu parole-4.18.2
+${THOME}/build/genpkg TRIBxfce-parole parole-4.18.2
