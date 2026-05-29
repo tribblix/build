bb8d7ce7764719d1841ccce82b3fff0d61c6e62e
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 15:43:37 +0100

    Add azure-storage-blob 12.29.0

---
+++ TRIBazure-storage-python-313/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# together with azure-identity, gives us the [azure] extra for barman
+#
+${THOME}/build/pkg_pep518 -N TRIBazure-storage-python-313 azure_storage_blob-12.29.0
