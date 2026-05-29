0229263ae0770f173682711c9fbc3a8c90a6ec19
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 20:44:25 +0100

    Update xfce4-screenshooter 1.11.3

--- TRIBxfce-screenshooter/build.sh
+++ TRIBxfce-screenshooter/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -gnu -64only xfce4-screenshooter-1.10.0
-${THOME}/build/genpkg TRIBxfce-screenshooter xfce4-screenshooter-1.10.0
+env AR=/usr/bin/ar ${THOME}/build/mesonbuild -gnu -64only xfce4-screenshooter-1.11.3
+${THOME}/build/genpkg TRIBxfce-screenshooter xfce4-screenshooter-1.11.3
