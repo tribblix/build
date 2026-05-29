3bd4114fc654e7d96346e16402bb9f967fff80b6
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 20:48:17 +0100

    Update xfce weather-plugin 0.12.0

--- TRIBxfce-weather-plugin/build.sh
+++ TRIBxfce-weather-plugin/build.sh
@@ -2,7 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# 0.11.2 requires json-c, which we don't have in 32-bit
-#
-${THOME}/build/dobuild -64only -gnu xfce4-weather-plugin-0.11.3
-${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.11.3
+${THOME}/build/mesonbuild -64only -gnu xfce4-weather-plugin-0.12.0
+${THOME}/build/genpkg TRIBxfce-weather-plugin xfce4-weather-plugin-0.12.0
