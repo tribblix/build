a4d47b8014770a1e79e5d82afb7ce540f1e0ea8d
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 20:19:40 +0100

    Update mousepad 0.7.0

--- TRIBxfce-mousepad/build.sh
+++ TRIBxfce-mousepad/build.sh
@@ -4,5 +4,5 @@
 #
 # -L seems to be needed to fix crashes
 #
-env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/dobuild -64only -gnu -L mousepad-0.6.2
-${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.6.2
+env TRIBBLIX_CFLAGS=-I/usr/include/gio-unix-2.0 ${THOME}/build/mesonbuild -64only -gnu -L mousepad-0.7.0
+${THOME}/build/genpkg TRIBxfce-mousepad mousepad-0.7.0
