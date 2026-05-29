f7fa9b48c6cf996fadd2aadc9ab9ec830bac3ecb
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 19:51:37 +0100

    Update slang pre2.3.4-22, switch to pcre2

--- TRIBslang/build.sh
+++ TRIBslang/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only slang-pre2.3.4-14 -J 1 -C --without-onig
-${THOME}/build/genpkg TRIBslang slang-pre2.3.4-14
+# build from a recent snapshot
+#
+${THOME}/build/dobuild -64only slang-pre2.3.4-22 -J 1 -C "--without-onig --with-pcre2-8 --without-pcre"
+${THOME}/build/genpkg TRIBslang slang-pre2.3.4-22
