b9510789ac8406ecfd60e75dd7c1fd779ca23df6
Peter Tribble <peter.tribble@gmail.com>
2026-05-29 20:26:13 +0100

    Update orage 4.18.0

--- TRIBxfce-orage/build.sh
+++ TRIBxfce-orage/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only -gnu orage-4.16.0
-${THOME}/build/genpkg TRIBxfce-orage orage-4.16.0
+${THOME}/build/dobuild -64only -gnu orage-4.18.0
+${THOME}/build/genpkg TRIBxfce-orage orage-4.18.0
