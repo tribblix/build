38e43f036ef5ef3c21b47e364cee0dbb5512d571
Peter Tribble <peter.tribble@gmail.com>
2026-05-31 12:18:07 +0100

    Update HTML::Parser 3.85

--- TRIBlib-perl-5-html-parser/build.sh
+++ TRIBlib-perl-5-html-parser/build.sh
@@ -4,9 +4,9 @@
 #
 # HTML-Parser needs html-tagset
 #
-${THOME}/build/unpack HTML-Parser-3.83
-cd HTML-Parser-3.83
+${THOME}/build/unpack HTML-Parser-3.85
+cd HTML-Parser-3.85
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-html-parser HTML-Parser-3.83
+${THOME}/build/genpkg TRIBlib-perl-5-html-parser HTML-Parser-3.85
