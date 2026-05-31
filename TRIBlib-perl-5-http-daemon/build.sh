7d5f5e914d58b509bf2d455249afd85330e5fc08
Peter Tribble <peter.tribble@gmail.com>
2026-05-31 12:15:56 +0100

    Update HTTP::Daemon 6.17

--- TRIBlib-perl-5-http-daemon/build.sh
+++ TRIBlib-perl-5-http-daemon/build.sh
@@ -2,9 +2,12 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack HTTP-Daemon-6.16
-cd HTTP-Daemon-6.16
-perl Makefile.PL
-make
+${THOME}/build/unpack HTTP-Daemon-6.17
+cd HTTP-Daemon-6.17
+perl Build.PL
+./Build
+rm -fr /tmp/hoax
+./Build install -destdir /tmp/hoax
+${THOME}/build/create_pkg TRIBlib-perl-5-http-daemon /tmp/hoax
+rm -fr /tmp/hoax
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-http-daemon HTTP-Daemon-6.16
