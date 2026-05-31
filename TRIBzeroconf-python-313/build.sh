5f2754e4cf8800c263edc187ca82eb9723c5c8cc
Peter Tribble <peter.tribble@gmail.com>
2026-05-31 09:37:30 +0100

    Update zeroconf 0.149.16

--- TRIBzeroconf-python-313/build.sh
+++ TRIBzeroconf-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBzeroconf-python-313 zeroconf-0.148.0
+${THOME}/build/pkg_pep518 -N TRIBzeroconf-python-313 zeroconf-0.149.16
