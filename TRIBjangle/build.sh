0bce25be7390406959db03a6dab4dcf1a89d09ea
Peter Tribble <peter.tribble@gmail.com>
2026-05-31 13:13:41 +0100

    Update jangle

--- TRIBjangle/build.sh
+++ TRIBjangle/build.sh
@@ -11,5 +11,5 @@
 # then copy the package into our own format
 #
 mkdir -p /tmp/pct
-mv /tmp/TRIBjangle.pkg /tmp/pct/TRIBjangle.0.19.pkg
-${THOME}/build/pkg2zap /tmp/pct/TRIBjangle.0.19.pkg /tmp/pct
+mv /tmp/TRIBjangle.pkg /tmp/pct/TRIBjangle.0.20.0.pkg
+${THOME}/build/pkg2zap /tmp/pct/TRIBjangle.0.20.0.pkg /tmp/pct
