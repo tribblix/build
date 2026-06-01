3c572e6bdf0f38f526f25545f07e97116569ef99
Peter Tribble <peter.tribble@gmail.com>
2026-06-01 21:23:00 +0100

    Update tor 0.4.8.25

--- TRIBtor/build.sh
+++ TRIBtor/build.sh
@@ -6,6 +6,6 @@
 #
 # uses the same trick as memcached to build a static libevent
 #
-${THOME}/build/dobuild -64only tor-0.4.8.24 -C "--enable-static-libevent --with-libevent-dir=/tmp/mc --sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking"
-${THOME}/build/genpkg TRIBtor tor-0.4.8.24
+${THOME}/build/dobuild -64only tor-0.4.8.25 -C "--enable-static-libevent --with-libevent-dir=/tmp/mc --sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking"
+${THOME}/build/genpkg TRIBtor tor-0.4.8.25
 rm -fr /tmp/mc
