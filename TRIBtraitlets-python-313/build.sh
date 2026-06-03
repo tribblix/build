edb9ae6dfc20514d9275a5a7e6055ae8eb25349c
Peter Tribble <peter.tribble@gmail.com>
2026-06-03 13:59:21 +0100

    Update traitlets 5.15.1

--- TRIBtraitlets-python-313/build.sh
+++ TRIBtraitlets-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBtraitlets-python-313 traitlets-5.15.0
+${THOME}/build/pkg_pep518 -N TRIBtraitlets-python-313 traitlets-5.15.1
