61cfd971f12141fad127e8bb79b2e5106d92c017
Peter Tribble <peter.tribble@gmail.com>
2026-06-04 09:40:31 +0100

    Update imagecodecs 2026.5.10

--- TRIBimagecodecs-python-313/build.sh
+++ TRIBimagecodecs-python-313/build.sh
@@ -5,6 +5,4 @@
 # there's a patch for setup.py to disable lerc (we don't have), jpegxr
 # (won't build) and add in brotli, bz2, gif, snappy
 #
-# downrev due to cython version dependency
-#
-${THOME}/build/pkg_pep518 -N -q TRIBimagecodecs-python-313 imagecodecs-2025.8.2
+${THOME}/build/pkg_pep518 -N -q TRIBimagecodecs-python-313 imagecodecs-2026.5.10
