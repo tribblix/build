4fb1abfddda0e66ac1c9bfc0fbb70dea20fbad0d
Peter Tribble <peter.tribble@gmail.com>
2026-06-04 09:18:12 +0100

    Update plumbum 2.0.0

--- TRIBplumbum-python-313/build.sh
+++ TRIBplumbum-python-313/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBplumbum-python-313 plumbum-1.10.0
+# depends on paramiko for the [ssh] extra
+#
+${THOME}/build/pkg_pep518 -N TRIBplumbum-python-313 plumbum-2.0.0
