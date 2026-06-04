a57e3b137460f33b93374282f99b86056b88a175
Peter Tribble <peter.tribble@gmail.com>
2026-06-04 18:03:00 +0100

    Update OpenBLAS 0.3.33

--- TRIBopenblas/build.sh
+++ TRIBopenblas/build.sh
@@ -5,14 +5,8 @@
 # we only build 64-bit as the only consumers are scipy/numpy which are
 # in a 64-bit python
 #
-${THOME}/build/unpack OpenBLAS-0.3.21
-cd OpenBLAS-0.3.21
-#
-# this generates an assembler error on sparc, so put in the old
-# 0.3.20 version, caused by
-# https://github.com/OpenMathLib/OpenBLAS/commit/06ef015234584f57d6743a78df7a7a05f6b39c11
-#
-cp ${THOME}/build/patches/dnrm2.S kernel/sparc
+${THOME}/build/unpack OpenBLAS-0.3.33
+cd OpenBLAS-0.3.33
 #
 # Intel - TARGET=PRESCOTT
 # Sparc - TARGET=SPARC
