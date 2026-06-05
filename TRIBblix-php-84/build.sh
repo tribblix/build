5c2cc5b4c55ba6ff85ce5f9118ddb6210474e5b5
Peter Tribble <peter.tribble@gmail.com>
2026-06-05 09:56:43 +0100

    Update PHP 8.4.22

--- TRIBblix-php-84/build.sh
+++ TRIBblix-php-84/build.sh
@@ -13,8 +13,8 @@
 # needs a tsrm patch to avoid crashes
 #
 zap uninstall TRIBlibtool
-${THOME}/build/unpack php-8.4.21
-cd php-8.4.21
+${THOME}/build/unpack php-8.4.22
+cd php-8.4.22
 #
 # CFLAGS because Zend.m4 hasn't a hope of correctness
 #
@@ -29,8 +29,8 @@ cd php-8.4.21
 # --build gets the architecture right on x86, otherwise it uses 32-bit asm
 # on sparc, need --without-pcre-jit
 #
-env CFLAGS="-std=gnu99" PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig ./configure --prefix=/opt/tribblix/php --with-ldap=no --with-apxs2=/opt/tribblix/apache2/bin/apxs --enable-bcmath --enable-mbstring --enable-mysqlnd --with-pgsql=shared,/opt/tribblix/postgres17 --with-pdo-pgsql=shared,/opt/tribblix/postgres17 --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --without-sqlite3 --without-pdo-sqlite --with-curl=/usr --enable-intl=shared --enable-gd --enable-exif --with-jpeg --with-bz2 --with-zlib --with-freetype --enable-sockets --enable-fpm --with-zip --with-openssl --with-sodium --with-gmp --enable-ftp --disable-mbregex --build=x86_64-pc-solaris2.11 CFLAGS=-m64 LDFLAGS=-m64 ICU_CFLAGS=-m64
-gsed -i 's:ext/sockets/ \$:ext/sockets/ -D_XPG4_2 \$:' Makefile
+env PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig ./configure --prefix=/opt/tribblix/php --with-ldap=no --with-apxs2=/opt/tribblix/apache2/bin/apxs --enable-bcmath --enable-mbstring --enable-mysqlnd --with-pgsql=shared,/opt/tribblix/postgres17 --with-pdo-pgsql=shared,/opt/tribblix/postgres17 --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --without-sqlite3 --without-pdo-sqlite --with-curl=/usr --enable-intl=shared --enable-gd --enable-exif --with-jpeg --with-bz2 --with-zlib --with-freetype --enable-sockets --enable-fpm --with-zip --with-openssl --with-sodium --with-gmp --enable-ftp --disable-mbregex --build=x86_64-pc-solaris2.11 CFLAGS="-m64 -D_XPG6 -D__EXTENSIONS__" LDFLAGS=-m64 ICU_CFLAGS=-m64
+
 gmake -j 6
 
 rm -fr /tmp/m4 /tmp/m4g /tmp/m4i /tmp/pear
