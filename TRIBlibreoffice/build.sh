f38b783ec4e9de1890892671654bbbc9bbd9e076
Peter Tribble <peter.tribble@gmail.com>
2026-06-10 19:24:51 +0100

    Update LibreOffice 26.2.4.2

--- TRIBlibreoffice/build.sh
+++ TRIBlibreoffice/build.sh
@@ -2,23 +2,17 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/unpack libreoffice-7.6.7.2
-cd libreoffice-7.6.7.2
-$THOME/build/patches/libreoffice-7.6.7.2.preconf
+$THOME/build/unpack libreoffice-26.2.4.2
+cd libreoffice-26.2.4.2
+$THOME/build/patches/libreoffice-26.2.4.2.preconf
 
 #
 # if you have a previous build, copy the contents of external/tarballs
 # to reduce the amount of stuff that needs to be downloaded
 #
 # mkdir external/tarballs
-# cp ../libreoffice-7.2.7.2/external/tarballs/* external/tarballs/
+# cp ../libreoffice-7.6.7.2/external/tarballs/* external/tarballs/
 #
-
-#
-# despite us now having a native SSP, the build falls over in that area
-# with an unresolved symbol, even if I pass the library along
-#
-sed -i '/stack-protector-strong/d' ./solenv/gbuild/platform/com_GCC_defs.mk
 
 #
 # there are a number of external libraries that we need to build
@@ -37,22 +31,19 @@ sed -i '/stack-protector-strong/d' ./solenv/gbuild/platform/com_GCC_defs.mk
 #
 
 #
-# we can now use our openssl again
-#
-
-#
-# need to ensure we find python3.12, which is 64-bit
+# need to ensure we find python3.13, which is 64-bit
 #
 # note that we specify 64-bit in multiple ways, as there are multiple
 # components which pick up on different things
 #
+env NOCONFIGURE=y ./autogen.sh 
 
 env CFLAGS="-m64" LDFLAGS="-m64" CXXFLAGS="-m64" \
 CC="gcc -m64" CXX="g++ -m64" AFLAGS="-m64" \
 PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig \
-PATH=/usr/gnu/bin:/usr/versions/python-3.12/bin:/usr/bin/amd64:$PATH \
+PATH=/usr/gnu/bin:/usr/versions/python-3.13/bin:/usr/bin/amd64:$PATH \
 bash ./configure \
---prefix=/usr/versions/libreoffice-7 \
+--prefix=/usr/versions/libreoffice-26 \
 --without-java \
 --without-doxygen \
 --with-tls="openssl" \
@@ -77,7 +68,12 @@ bash ./configure \
 --with-vendor="Tribblix" \
 --enable-release-build=yes \
 --build=amd64-pc-solaris2.11 \
---with-system-icu
+--with-system-icu \
+--enable-gstreamer-1-0 \
+--enable-gtk3 \
+--with-system-cairo \
+--with-system-expat \
+--with-system-libxml
 
 #
 # a regular make will run the checks; these will almost always fail,
@@ -86,11 +82,11 @@ bash ./configure \
 env CFLAGS="-m64" LDFLAGS="-m64" CXXFLAGS="-m64" \
 CC="gcc -m64" CXX="g++ -m64" AFLAGS="-m64" \
 PKG_CONFIG_PATH=/usr/lib/amd64/pkgconfig \
-PATH=/usr/gnu/bin:/usr/versions/python-3.12/bin:/usr/bin/amd64:$PATH \
+PATH=/usr/gnu/bin:/usr/versions/python-3.13/bin:/usr/bin/amd64:$PATH \
 /usr/gnu/bin/make -j 6 VERBOSE=1 gb_SUPPRESS_TESTS=x
 
 #
 # need distro-pack-install to get things assembled the right way
 #
-env PATH=/usr/gnu/bin:/usr/versions/python-3.12/bin:$PATH /usr/gnu/bin/make distro-pack-install DESTDIR=/tmp/loo
+env PATH=/usr/gnu/bin:/usr/versions/python-3.13/bin:$PATH /usr/gnu/bin/make distro-pack-install DESTDIR=/tmp/loo
 ${THOME}/build/create_pkg TRIBlibreoffice /tmp/loo
