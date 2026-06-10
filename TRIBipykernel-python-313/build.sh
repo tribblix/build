bf850ee28b487b81d5099206f282017554971b4a
Peter Tribble <peter.tribble@gmail.com>
2026-06-10 13:21:48 +0100

    Update ipykernel 7.3.0, and switch to nest-ayncio2

--- TRIBipykernel-python-313/build.sh
+++ TRIBipykernel-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBipykernel-python-313 ipykernel-7.2.0
+${THOME}/build/pkg_pep518 -N TRIBipykernel-python-313 ipykernel-7.3.0
--- TRIBnest-asyncio-python-313/build.sh
+++ TRIBnest-asyncio-python-313/build.sh
@@ -2,4 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBnest-asyncio-python-313 nest_asyncio-1.6.0
+# this is actually nest-asyncio, a fork of the seemingly unmaintained
+# nest-aysncio, but ipykernel is the only consumer in Tribblix and it
+# switched dependency
+#
+${THOME}/build/pkg_pep518 -N TRIBnest-asyncio-python-313 nest_asyncio2-1.7.2
