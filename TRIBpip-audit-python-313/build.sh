dd15ff2ad96d3310a479f2456fbe88aaef73c4d4
Peter Tribble <peter.tribble@gmail.com>
2026-06-11 18:42:09 +0100

    Update pip_audit 2.10.1

--- TRIBpip-audit-python-313/build.sh
+++ TRIBpip-audit-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpip-audit-python-313 pip_audit-2.10.0
+${THOME}/build/pkg_pep518 -N TRIBpip-audit-python-313 pip_audit-2.10.1
