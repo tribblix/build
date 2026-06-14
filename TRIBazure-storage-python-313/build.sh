093c0f3de31d280311d69ff5096d6c7fa2688d8f
Peter Tribble <peter.tribble@gmail.com>
2026-06-14 16:16:41 +0100

    Update azure-storage-blob 12.30.0

--- TRIBazure-storage-python-313/build.sh
+++ TRIBazure-storage-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # together with azure-identity, gives us the [azure] extra for barman
 #
-${THOME}/build/pkg_pep518 -N TRIBazure-storage-python-313 azure_storage_blob-12.29.0
+${THOME}/build/pkg_pep518 -N TRIBazure-storage-python-313 azure_storage_blob-12.30.0
