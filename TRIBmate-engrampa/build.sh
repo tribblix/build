7f5a57b19c1cde214caf762e72cf5983ac03b755
Peter Tribble <peter.tribble@gmail.com>
2026-06-14 18:09:20 +0100

    Update engrampa 1.28.3

--- TRIBmate-engrampa/build.sh
+++ TRIBmate-engrampa/build.sh
@@ -2,5 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only +gnu engrampa-1.28.2
-${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.28.2
+# there's a patch to fix standards-noncompliance
+#
+${THOME}/build/dobuild -64only +gnu engrampa-1.28.3
+${THOME}/build/genpkg TRIBmate-engrampa engrampa-1.28.3
---
+++ patches/engrampa-1.28.3.pls
@@ -0,0 +1,1 @@
+-p1 engrampa-strptime.patch
