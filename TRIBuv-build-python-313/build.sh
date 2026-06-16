53f83ea2283cbfed313d180488c3e23c235a7d00
Peter Tribble <peter.tribble@gmail.com>
2026-06-16 17:11:32 +0100

    Update uv-build 0.11.21

--- TRIBuv-build-python-313/build.sh
+++ TRIBuv-build-python-313/build.sh
@@ -2,9 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# uses the fs2 crate which is broken
-# in ~/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fs2-0.4.3
-# (or similar), edit src/unix.rs and add illumos to the list of platforms
-# for the allocate function, at line ~100
-#
-${THOME}/build/pkg_pep518 -N TRIBuv-build-python-313 uv_build-0.11.19
+${THOME}/build/pkg_pep518 -N TRIBuv-build-python-313 uv_build-0.11.21
