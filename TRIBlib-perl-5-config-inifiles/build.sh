8a7fe710523cd26c808a1ab49de140d0a01ef783
Peter Tribble <peter.tribble@gmail.com>
2026-06-16 10:57:49 +0100

    Update Config::IniFiles 3.002000

--- TRIBlib-perl-5-config-inifiles/build.sh
+++ TRIBlib-perl-5-config-inifiles/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Config-IniFiles-3.001000
-cd Config-IniFiles-3.001000
+${THOME}/build/unpack Config-IniFiles-3.002000
+cd Config-IniFiles-3.002000
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-config-inifiles Config-IniFiles-3.001000
+${THOME}/build/genpkg TRIBlib-perl-5-config-inifiles Config-IniFiles-3.002000
