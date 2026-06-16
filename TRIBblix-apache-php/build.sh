8538e04f212efb025c3409ba4e3d1ba3f322303b
Peter Tribble <peter.tribble@gmail.com>
2026-06-16 15:29:54 +0100

    Switch apache-php to newer php

--- TRIBblix-apache-php/build.sh
+++ TRIBblix-apache-php/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# this is a package that sets up apache+php8 for a flat-file php
+# this is a package that sets up apache+php for a flat-file php
 # application hosted under /var/phpweb. Think grav, for example.
 #
 # everything is done by the fixit script
