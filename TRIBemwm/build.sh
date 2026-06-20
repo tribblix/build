cd3e3893e23a2608cc216f2205afb2e5c0046e09
Peter Tribble <peter.tribble@gmail.com>
2026-06-20 10:45:25 +0100

    Update emwm 2.0

--- TRIBemwm/build.sh
+++ TRIBemwm/build.sh
@@ -4,6 +4,7 @@
 #
 # the Makefile is patched to enable 64-bit
 #
-$THOME/build/dobuild emwm-src-1.3
-$THOME/build/dobuild emwm-utils-src-1.3
-${THOME}/build/genpkg TRIBemwm emwm-src-1.3 emwm-utils-src-1.3
+$THOME/build/dobuild emwm-src-2.0
+$THOME/build/dobuild emwm-utils-src-2.0
+$THOME/build/dobuild -n tellmwm tellmwm-src
+${THOME}/build/genpkg TRIBemwm emwm-src-2.0 emwm-utils-src-2.0 tellmwm-src
