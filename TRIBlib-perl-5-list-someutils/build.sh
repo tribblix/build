78d85a8fb03ec4483ed4ea7d52840af1e8e6a269
Peter Tribble <peter.tribble@gmail.com>
2026-06-22 17:49:24 +0100

    Update List::SomeUtils::XS 0.59

--- TRIBlib-perl-5-list-someutils/build.sh
+++ TRIBlib-perl-5-list-someutils/build.sh
@@ -4,8 +4,8 @@
 #
 # this builds and packages *both* parts together, the vanilla and XS
 #
-${THOME}/build/unpack List-SomeUtils-XS-0.58
-cd List-SomeUtils-XS-0.58
+${THOME}/build/unpack List-SomeUtils-XS-0.59
+cd List-SomeUtils-XS-0.59
 perl Makefile.PL
 make
 cd ..
@@ -14,5 +14,5 @@ cd List-SomeUtils-0.59
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-list-someutils List-SomeUtils-0.59 List-SomeUtils-XS-0.58
+${THOME}/build/genpkg TRIBlib-perl-5-list-someutils List-SomeUtils-0.59 List-SomeUtils-XS-0.59
 echo "Now install and rebuild a second time"
