48cc3f81d00fc12ebf6f1a45ddb9d78014478efa
Peter Tribble <peter.tribble@gmail.com>
2026-06-22 09:43:56 +0100

    Update xkeyboard-config 2.48

--- TRIBx11-keyboard-data-xkb/build.sh
+++ TRIBx11-keyboard-data-xkb/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -gnu xkeyboard-config-2.24
-${THOME}/build/genpkg TRIBx11-keyboard-data-xkb xkeyboard-config-2.24
+${THOME}/build/mesonbuild xkeyboard-config-2.48
+${THOME}/build/genpkg TRIBx11-keyboard-data-xkb xkeyboard-config-2.48
