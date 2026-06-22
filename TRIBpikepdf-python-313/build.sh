021908d320a9666bd5c8bd9403bf27a824523df3
Peter Tribble <peter.tribble@gmail.com>
2026-06-22 11:16:20 +0100

    Update pikepdf 10.9.1

--- TRIBpikepdf-python-313/build.sh
+++ TRIBpikepdf-python-313/build.sh
@@ -5,4 +5,4 @@
 # as of v10.6 shifted from pybind to nanobind
 # must use -N as we have a patched nanobind to make this work
 #
-env CXXFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBpikepdf-python-313 pikepdf-10.8.0
+env CXXFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBpikepdf-python-313 pikepdf-10.9.1
