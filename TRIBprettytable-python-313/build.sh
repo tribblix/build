623b1b51fe5e0f4726d4545565ecc904331988d5
Peter Tribble <peter.tribble@gmail.com>
2026-06-22 17:59:07 +0100

    Update prettytable 3.18.0

--- TRIBprettytable-python-313/build.sh
+++ TRIBprettytable-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBprettytable-python-313 prettytable-3.17.0
+${THOME}/build/pkg_pep518 -N TRIBprettytable-python-313 prettytable-3.18.0
