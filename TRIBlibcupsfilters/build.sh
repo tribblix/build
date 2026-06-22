50dcab064c504f004d6c5a6a09bdc668812b0256
Peter Tribble <peter.tribble@gmail.com>
2026-06-22 11:14:18 +0100

    Update pqdf 12.3.2, rebuild consumers

--- TRIBlibcupsfilters/build.sh
+++ TRIBlibcupsfilters/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-$THOME/build/dobuild -64only libcupsfilters-2.1.1 -C --disable-dbus
+env TRIBBLIX_CFLAGS=-D_XPG6 $THOME/build/dobuild -64only libcupsfilters-2.1.1 -C --disable-dbus
 ${THOME}/build/genpkg TRIBlibcupsfilters libcupsfilters-2.1.1
