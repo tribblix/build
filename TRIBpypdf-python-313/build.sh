e268ea28f212ab3b990dda53adad5ce5cabb592d
Peter Tribble <peter.tribble@gmail.com>
2026-06-23 20:41:58 +0100

    Update pypdf 6.14.2

--- TRIBpypdf-python-313/build.sh
+++ TRIBpypdf-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # dependencies cover the [full,crypto,fonts,image] extras
 #
-${THOME}/build/pkg_pep518 -N TRIBpypdf-python-313 pypdf-6.14.0
+${THOME}/build/pkg_pep518 -N TRIBpypdf-python-313 pypdf-6.14.2
