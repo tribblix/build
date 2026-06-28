85871e29de4244fa3c1add805dd48c94e5994e35
Peter Tribble <peter.tribble@gmail.com>
2026-06-28 21:32:20 +0100

    Update pdftotext 4.0.0

--- TRIBpdftotext-python-313/build.sh
+++ TRIBpdftotext-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpdftotext-python-313 pdftotext-3.0.0
+${THOME}/build/pkg_pep518 -N TRIBpdftotext-python-313 pdftotext-4.0.0
