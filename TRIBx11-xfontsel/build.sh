479afac2f4048a3b4cd009f758b8883e21baa0c3
Peter Tribble <peter.tribble@gmail.com>
2026-06-29 08:36:37 +0100

    Update xfontsel 1.1.2

--- TRIBx11-xfontsel/build.sh
+++ TRIBx11-xfontsel/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xfontsel-1.1.1
-${THOME}/build/genpkg TRIBx11-xfontsel xfontsel-1.1.1
+${THOME}/build/dobuild -64only xfontsel-1.1.2
+${THOME}/build/genpkg TRIBx11-xfontsel xfontsel-1.1.2
