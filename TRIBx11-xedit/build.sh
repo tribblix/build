c9159b1526103373dbb52b73a38d309e7b105eb2
Peter Tribble <peter.tribble@gmail.com>
2026-06-29 08:34:41 +0100

    Update xedit 1.2.5

--- TRIBx11-xedit/build.sh
+++ TRIBx11-xedit/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xedit-1.2.4
-${THOME}/build/genpkg TRIBx11-xedit xedit-1.2.4
+${THOME}/build/dobuild -64only xedit-1.2.5
+${THOME}/build/genpkg TRIBx11-xedit xedit-1.2.5
