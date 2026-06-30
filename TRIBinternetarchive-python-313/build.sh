6825a945275e6ba5201ac5bf3e64b67b4c32dea1
Peter Tribble <peter.tribble@gmail.com>
2026-06-30 08:44:00 +0100

    Update internetarchive 5.10.1

--- TRIBinternetarchive-python-313/build.sh
+++ TRIBinternetarchive-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBinternetarchive-python-313 internetarchive-5.9.0
+${THOME}/build/pkg_pep518 -N TRIBinternetarchive-python-313 internetarchive-5.10.1
