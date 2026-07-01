efcbc7c22406ecd45f09734bbd47701e2eac2afd
Peter Tribble <peter.tribble@gmail.com>
2026-07-01 18:39:35 +0100

    Update zabbix 7.0.27

--- TRIBblix-zabbix/build.sh
+++ TRIBblix-zabbix/build.sh
@@ -9,6 +9,6 @@
 # part of the zabbix-server overlay rather than as dependencies here
 #
 ${THOME}/build/patches/zabbix-server.preconf
-${THOME}/build/dobuild -64only zabbix-7.0.23 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres17/bin/pg_config --with-libpcre2 --with-libevent=/tmp/mc"
-${THOME}/build/genpkg TRIBblix-zabbix zabbix-7.0.23
+env TRIBBLIX_CFLAGS=-Wno-incompatible-pointer-types ${THOME}/build/dobuild -64only zabbix-7.0.27 -P /opt/tribblix/zabbix -C "--enable-server --with-libcurl --with-postgresql=/opt/tribblix/postgres17/bin/pg_config --with-libpcre2 --with-libevent=/tmp/mc"
+${THOME}/build/genpkg TRIBblix-zabbix zabbix-7.0.27
 rm -fr /tmp/mc
--- TRIBzabbix-agent/build.sh
+++ TRIBzabbix-agent/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only zabbix-7.0.23 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre2"
-${THOME}/build/genpkg TRIBzabbix-agent zabbix-7.0.23
+env TRIBBLIX_CFLAGS=-Wno-incompatible-pointer-types ${THOME}/build/dobuild -64only zabbix-7.0.27 -P /opt/tribblix/zabbix -C "--sysconfdir=/etc --localstatedir=/var --enable-agent --with-libpcre2"
+${THOME}/build/genpkg TRIBzabbix-agent zabbix-7.0.27
