fcd9ff5c23ecfbed8031b083cbb741b268b6f2ed
Peter Tribble <peter.tribble@gmail.com>
2026-07-01 18:04:22 +0100

    Update tmux 3.7a

--- TRIBtmux/build.sh
+++ TRIBtmux/build.sh
@@ -4,7 +4,7 @@
 #
 # tmux needs a sacrificial libevent
 #
-${THOME}/build/dobuild +64only libevent-2.1.12-stable -C "--enable-static --disable-shared --disable-libevent-install"
-env CPPFLAGS="-I`pwd`/libevent-2.1.12-stable-64bit/include -I`pwd`/libevent-2.1.12-stable-64bit/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.1.12-stable-64bit/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild -64only tmux-3.6b
-${THOME}/build/genpkg TRIBtmux tmux-3.6b
-rm -fr libevent-2.1.12-stable-64bit
+${THOME}/build/dobuild +64only libevent-2.1.13-stable -C "--enable-static --disable-shared --disable-libevent-install"
+env CPPFLAGS="-I`pwd`/libevent-2.1.13-stable-64bit/include -I`pwd`/libevent-2.1.13-stable-64bit/include/event2" TRIBBLIX_LDFLAGS="-L`pwd`/libevent-2.1.13-stable-64bit/.libs -lsocket -lnsl -lsendfile" ${THOME}/build/dobuild -64only tmux-3.7a
+${THOME}/build/genpkg TRIBtmux tmux-3.7a
+rm -fr libevent-2.1.13-stable-64bit
