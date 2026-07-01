9fc5a86b650e72bbc45a12803ca719b25d445182
Peter Tribble <peter.tribble@gmail.com>
2026-07-01 22:37:27 +0100

    Update clamav 1.4.5

--- TRIBblix-clamav/build.sh
+++ TRIBblix-clamav/build.sh
@@ -6,10 +6,12 @@
 #
 # there's a preconf to disable clamdtop, which needs curses but can't find it
 #
+# there's a preconf and postconf to fix up linker map files
+#
 # disable tests and milter as they don't build
 #
 # there are strange failures in rust - just delete the .so it complains about
 # and rerun gmake
 #
-${THOME}/build/cmbuild +gnu -64only clamav-1.0.9 -P /opt/tribblix/clamav -C "-DENABLE_MILTER=OFF -DENABLE_TESTS=OFF"
-${THOME}/build/genpkg TRIBblix-clamav clamav-1.0.9
+${THOME}/build/cmbuild +gnu -64only clamav-1.4.5 -P /opt/tribblix/clamav -C "-DENABLE_MILTER=OFF -DENABLE_TESTS=OFF"
+${THOME}/build/genpkg TRIBblix-clamav clamav-1.4.5
