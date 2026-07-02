42e1e17be8624070b01c2e304234ebdf7003f3e0
Peter Tribble <peter.tribble@gmail.com>
2026-07-02 21:23:27 +0100

    Update keystone 29.0.2

--- TRIBkeystone-python-313/build.sh
+++ TRIBkeystone-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # not available for sparc due to the jsonschema4 dependency
 #
-${THOME}/build/pkg_pep518 -N TRIBkeystone-python-313 keystone-29.0.1
+${THOME}/build/pkg_pep518 -N TRIBkeystone-python-313 keystone-29.0.2
