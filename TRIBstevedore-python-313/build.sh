e81b0b60ffda3ff35d9c885b66d45ba6a36e9577
Peter Tribble <peter.tribble@gmail.com>
2026-07-02 21:10:45 +0100

    Update stevedore 5.9.0

--- TRIBstevedore-python-313/build.sh
+++ TRIBstevedore-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBstevedore-python-313 stevedore-5.8.0
+${THOME}/build/pkg_pep518 -N TRIBstevedore-python-313 stevedore-5.9.0
