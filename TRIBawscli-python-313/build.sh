cb534f41821aa696a1b7eaa9a5e23201d555009c
Peter Tribble <peter.tribble@gmail.com>
2026-07-04 09:59:56 +0100

    Update botocore 1.43.40 boto3 1.43.40 awscli 1.45.40

--- TRIBawscli-python-313/build.sh
+++ TRIBawscli-python-313/build.sh
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# needs botocore colorama s3transfer docutils rsa yaml
-#
-${THOME}/build/pkg_pep518 -N TRIBawscli-python-313 awscli-1.45.36
+${THOME}/build/pkg_pep518 -N TRIBawscli-python-313 awscli-1.45.40
