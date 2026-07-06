fe854a575ffc5ab6bf7cf270b41ffff9b55dac3e
Peter Tribble <peter.tribble@gmail.com>
2026-07-06 11:32:51 +0100

    Update Garage 2.3.0

--- TRIBblix-garage/build.sh
+++ TRIBblix-garage/build.sh
@@ -4,8 +4,8 @@
 #
 
 #
-$THOME/build/unpack -n garage garage-v2.1.0
-cd garage-v2.1.0
+$THOME/build/unpack -n garage garage-v2.3.0
+cd garage-v2.3.0
 env CARGO_PROFILE_RELEASE_LTO=false PKG_CONFIG_PATH=/usr/lib/`$THOME/build/getarch`/pkgconfig SODIUM_USE_PKG_CONFIG=1 cargo build --release
 
 rm -fr /tmp/ll
