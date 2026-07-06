1142123b595018f41e214409d64f9a3e67b133ce
Peter Tribble <peter.tribble@gmail.com>
2026-07-06 13:25:08 +0100

    Update bzip3 1.5.3

--- TRIBcompress-bzip3/build.sh
+++ TRIBcompress-bzip3/build.sh
@@ -4,5 +4,5 @@
 #
 # uses clang if available
 #
-${THOME}/build/dobuild -64only bzip3-1.4.0 -C --disable-dependency-tracking
-${THOME}/build/genpkg TRIBcompress-bzip3 bzip3-1.4.0
+${THOME}/build/dobuild -64only bzip3-1.5.3 -C --disable-dependency-tracking
+${THOME}/build/genpkg TRIBcompress-bzip3 bzip3-1.5.3
