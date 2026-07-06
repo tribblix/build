01bdf6de552c62487c2d696514e224af13f0a8f2
Peter Tribble <peter.tribble@gmail.com>
2026-07-06 10:11:39 +0100

    Update libansilove 1.4.2 ansilove 4.2.2

--- TRIBansilove/build.sh
+++ TRIBansilove/build.sh
@@ -5,6 +5,8 @@
 # package both libansilove and ansilove together
 # first time through, just package libansilove
 #
-${THOME}/build/cmbuild -64only libansilove-1.4.1
-${THOME}/build/cmbuild -64only ansilove-4.2.0
-${THOME}/build/genpkg TRIBansilove libansilove-1.4.1 ansilove-4.2.0
+# _XPG7 for strndup()
+#
+env TRIBBLIX_CFLAGS=-D_XPG7 ${THOME}/build/cmbuild -64only libansilove-1.4.2
+env TRIBBLIX_CFLAGS=-D__EXTENSIONS__ ${THOME}/build/cmbuild -64only ansilove-4.2.2
+${THOME}/build/genpkg TRIBansilove libansilove-1.4.2 ansilove-4.2.2
