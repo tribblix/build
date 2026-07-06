14c652463d33076dd32216a71f9540188b41f70a
Peter Tribble <peter.tribble@gmail.com>
2026-07-06 10:24:24 +0100

    Update bgpq4 1.16

--- TRIBbgpq4/build.sh
+++ TRIBbgpq4/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -A -64only bgpq4-1.15
-${THOME}/build/genpkg TRIBbgpq4 bgpq4-1.15
+${THOME}/build/dobuild -A -64only bgpq4-1.16
+${THOME}/build/genpkg TRIBbgpq4 bgpq4-1.16
