6cd99b71b6fa39fce1c40715c138262e740f672f
Peter Tribble <peter.tribble@gmail.com>
2026-07-06 12:34:36 +0100

    Update PostGIS 3.6.4

--- TRIBblix-postgis18/build.sh
+++ TRIBblix-postgis18/build.sh
@@ -8,5 +8,5 @@
 # installs into the same prefix as the postgres version it's built
 # against, and is versioned to match
 #
-$THOME/build/dobuild +64only postgis-3.6.3 -P /opt/tribblix/postgres18 -C "--with-pgconfig=/opt/tribblix/postgres18/bin/pg_config --sysconfdir=/etc"
-$THOME/build/genpkg TRIBblix-postgis18 postgis-3.6.3
+env TRIBBLIX_CFLAGS="-D_XPG6 -D__EXTENSIONS__" $THOME/build/dobuild +64only postgis-3.6.4 -P /opt/tribblix/postgres18 -C "--with-pgconfig=/opt/tribblix/postgres18/bin/pg_config --sysconfdir=/etc"
+$THOME/build/genpkg TRIBblix-postgis18 postgis-3.6.4
