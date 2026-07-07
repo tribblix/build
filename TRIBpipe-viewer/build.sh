aae788cf809ad6e267103ba607df4a9cc83222bd
Peter Tribble <peter.tribble@gmail.com>
2026-07-07 16:45:14 +0100

    Update pv 1.11.0

--- TRIBpipe-viewer/build.sh
+++ TRIBpipe-viewer/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only pv-1.10.3 -C --disable-nls
-${THOME}/build/genpkg TRIBpipe-viewer pv-1.10.3
+env TRIBBLIX_CFLAGS="-D_XOPEN_SOURCE -D_XOPEN_VERSION=4" ${THOME}/build/dobuild -64only pv-1.11.0 -C --disable-nls
+${THOME}/build/genpkg TRIBpipe-viewer pv-1.11.0
