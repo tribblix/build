4d0d399e571e269afda28afe77e8b1bac4957c05
Peter Tribble <peter.tribble@gmail.com>
2026-07-07 19:22:36 +0100

    Update less 704

--- TRIBtext-less/build.sh
+++ TRIBtext-less/build.sh
@@ -2,5 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only less-685
-${THOME}/build/genpkg TRIBtext-less less-685
+# it's not happy about the signature of the system tputs()
+#
+# there's also a postconf file that's essential for 691-704
+#
+env TRIBBLIX_CFLAGS="-Wno-incompatible-pointer-types" ${THOME}/build/dobuild -64only less-704
+${THOME}/build/genpkg TRIBtext-less less-704
