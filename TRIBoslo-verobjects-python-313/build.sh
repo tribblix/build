99264a954e1fc98b32c5aca95fb4f1811fe1a450
Peter Tribble <peter.tribble@gmail.com>
2026-07-09 13:54:42 +0100

    Update oslo.versionedobjects 3.11.0

--- TRIBoslo-verobjects-python-313/build.sh
+++ TRIBoslo-verobjects-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-verobjects-python-313 oslo_versionedobjects-3.10.2
+${THOME}/build/pkg_pep518 -N TRIBoslo-verobjects-python-313 oslo_versionedobjects-3.11.0
