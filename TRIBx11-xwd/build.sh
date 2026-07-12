0dec4d6e0c61d8a3f13cdb575f61130b6ba2b458
Peter Tribble <peter.tribble@gmail.com>
2026-07-12 21:28:59 +0100

    Update xwd 1.0.10 xwud 1.0.8

--- TRIBx11-xwd/build.sh
+++ TRIBx11-xwd/build.sh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xwd-1.0.9
-${THOME}/build/dobuild -64only xwud-1.0.7
+${THOME}/build/mesonbuild -64only xwd-1.0.10
+${THOME}/build/mesonbuild -64only xwud-1.0.8
 ${THOME}/build/dobuild -64only xpr-1.2.0
-${THOME}/build/genpkg TRIBx11-xwd xwd-1.0.9 xwud-1.0.7 xpr-1.2.0
+${THOME}/build/genpkg TRIBx11-xwd xwd-1.0.10 xwud-1.0.8 xpr-1.2.0
