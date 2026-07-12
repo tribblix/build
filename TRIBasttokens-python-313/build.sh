9707c841318646b038b6c6d99bb8c10e6cf0ad55
Peter Tribble <peter.tribble@gmail.com>
2026-07-12 09:54:12 +0100

    Update asttokens 3.0.2

--- TRIBasttokens-python-313/build.sh
+++ TRIBasttokens-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBasttokens-python-313 asttokens-3.0.1
+${THOME}/build/pkg_pep518 -N TRIBasttokens-python-313 asttokens-3.0.2
