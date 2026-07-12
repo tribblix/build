c28fa960a89dc5f66552fa04fe54f6714b2ce17c
Peter Tribble <peter.tribble@gmail.com>
2026-07-12 10:37:13 +0100

    Update jupyter_builder 1.1.0

--- TRIBjupyter-builder-python-313/build.sh
+++ TRIBjupyter-builder-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBjupyter-builder-python-313 jupyter_builder-1.0.2
+${THOME}/build/pkg_pep518 TRIBjupyter-builder-python-313 jupyter_builder-1.1.0
