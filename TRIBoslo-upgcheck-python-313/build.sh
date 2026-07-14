c79099feed9cea516af3d7df5702dc510163cca9
Peter Tribble <peter.tribble@gmail.com>
2026-07-14 16:09:08 +0100

    Update oslo.service 4.7.0 oslo.upgradecheck 2.8.0

--- TRIBoslo-upgcheck-python-313/build.sh
+++ TRIBoslo-upgcheck-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBoslo-upgcheck-python-313 oslo_upgradecheck-2.7.1
+${THOME}/build/pkg_pep518 -N TRIBoslo-upgcheck-python-313 oslo_upgradecheck-2.8.0
