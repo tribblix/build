5b156f065b0d18ddcbc980f42191d3e6f300ed77
Peter Tribble <peter.tribble@gmail.com>
2026-07-16 21:32:20 +0100

    Update oqs-provider

--- TRIBoqs-provider/build.sh
+++ TRIBoqs-provider/build.sh
@@ -5,5 +5,7 @@
 # there's an assumption here that by the time this gets to being deployed,
 # all relevant software will be 64-bit
 #
-${THOME}/build/cmbuild -64only oqs-provider-0.11.0 -C -DCMAKE_PREFIX_PATH=/usr/lib/`$THOME/build/getarch`/cmake
-${THOME}/build/genpkg TRIBoqs-provider oqs-provider-0.11.0
+# build from git as the latest release won't build against current liboqs
+#
+${THOME}/build/cmbuild -64only https://github.com/open-quantum-safe/oqs-provider -C -DCMAKE_PREFIX_PATH=/usr/lib/`$THOME/build/getarch`/cmake
+${THOME}/build/genpkg TRIBoqs-provider oqs-provider
