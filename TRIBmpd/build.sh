258ba1f4ace7ee33992390479f63495a448a1477
Peter Tribble <peter.tribble@gmail.com>
2026-07-16 22:06:11 +0100

    Drop mpd, unused and doesn't build

--- TRIBmpd/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-# modplug and sndfile are incompatible and break the build
-#
-${THOME}/build/mesonbuild -64only mpd-0.23.15 -C -Dmodplug=disabled
-${THOME}/build/genpkg TRIBmpd mpd-0.23.15
