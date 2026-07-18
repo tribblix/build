8a0b4be0991657bee61e6f6ee73b9d02e9355bcc
Peter Tribble <peter.tribble@gmail.com>
2026-07-18 20:24:05 +0100

    Update xclock 1.2.1

--- TRIBx11-xclock/build.sh
+++ TRIBx11-xclock/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xclock-1.2.0
-${THOME}/build/genpkg TRIBx11-xclock xclock-1.2.0
+${THOME}/build/mesonbuild -64only xclock-1.2.1
+${THOME}/build/genpkg TRIBx11-xclock xclock-1.2.1
