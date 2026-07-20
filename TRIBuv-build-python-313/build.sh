5304b479df3c9384444556b9377d296185f62ead
Peter Tribble <peter.tribble@gmail.com>
2026-07-20 09:48:55 +0100

    Update uv-build 0.11.29

--- TRIBuv-build-python-313/build.sh
+++ TRIBuv-build-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBuv-build-python-313 uv_build-0.11.26
+${THOME}/build/pkg_pep518 -N TRIBuv-build-python-313 uv_build-0.11.29
