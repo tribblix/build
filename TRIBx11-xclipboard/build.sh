a7ae8cf8ef3d838bcc0177d88eedb1d54ea03f56
Peter Tribble <peter.tribble@gmail.com>
2026-07-20 09:57:26 +0100

    Update xclipboard 1.1.6

--- TRIBx11-xclipboard/build.sh
+++ TRIBx11-xclipboard/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only xclipboard-1.1.5
-${THOME}/build/genpkg TRIBx11-xclipboard xclipboard-1.1.5
+${THOME}/build/mesonbuild -64only xclipboard-1.1.6
+${THOME}/build/genpkg TRIBx11-xclipboard xclipboard-1.1.6
