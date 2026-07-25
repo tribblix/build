8c26a13960c5fb0cb378cfd4558fa79cdc6bf9e3
Peter Tribble <peter.tribble@gmail.com>
2026-07-25 12:48:41 +0100

    Update annotated-types 0.8.0

--- TRIBannot-types-python-313/build.sh
+++ TRIBannot-types-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBannot-types-python-313 annotated_types-0.7.0
+${THOME}/build/pkg_pep518 -N TRIBannot-types-python-313 annotated_types-0.8.0
