3b80006117bd9fc57d330a2852a18b66e6134358
Peter Tribble <peter.tribble@gmail.com>
2026-07-25 11:57:08 +0100

    Update HTTP::Message 7.04

--- TRIBlib-perl-5-http-message/build.sh
+++ TRIBlib-perl-5-http-message/build.sh
@@ -7,9 +7,9 @@
 # http-message need encode-locale
 # http-message need lwp-mediatypes
 #
-${THOME}/build/unpack HTTP-Message-7.02
-cd HTTP-Message-7.02
+${THOME}/build/unpack HTTP-Message-7.04
+cd HTTP-Message-7.04
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-http-message HTTP-Message-7.02
+${THOME}/build/genpkg TRIBlib-perl-5-http-message HTTP-Message-7.04
