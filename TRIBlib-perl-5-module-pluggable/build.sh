32f6bf48ef1b693c932e19fdc31c7f6761bb09e4
Peter Tribble <peter.tribble@gmail.com>
2026-07-27 15:22:38 +0100

    Update Module::Pluggable 6.4

--- TRIBlib-perl-5-module-pluggable/build.sh
+++ TRIBlib-perl-5-module-pluggable/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Module-Pluggable-6.3
-cd Module-Pluggable-6.3
+${THOME}/build/unpack Module-Pluggable-6.4
+cd Module-Pluggable-6.4
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-module-pluggable Module-Pluggable-6.3
+${THOME}/build/genpkg TRIBlib-perl-5-module-pluggable Module-Pluggable-6.4
