69dc410a3574db62b90f3a4ebab88aa4f32bdd0d
Peter Tribble <peter.tribble@gmail.com>
2026-07-27 09:01:25 +0100

    Update reportlab 5.0.0

--- TRIBreportlab-python-313/build.sh
+++ TRIBreportlab-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBreportlab-python-313 reportlab-4.5.1
+${THOME}/build/pkg_pep518 -N TRIBreportlab-python-313 reportlab-5.0.0
