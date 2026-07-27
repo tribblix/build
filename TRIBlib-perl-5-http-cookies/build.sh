be18483c4d1f6873e9244a97620a4776e0cda7f8
Peter Tribble <peter.tribble@gmail.com>
2026-07-27 15:17:59 +0100

    Update HTTP::Cookies 6.12

--- TRIBlib-perl-5-http-cookies/build.sh
+++ TRIBlib-perl-5-http-cookies/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack HTTP-Cookies-6.11
-cd HTTP-Cookies-6.11
+${THOME}/build/unpack HTTP-Cookies-6.12
+cd HTTP-Cookies-6.12
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-http-cookies HTTP-Cookies-6.11
+${THOME}/build/genpkg TRIBlib-perl-5-http-cookies HTTP-Cookies-6.12
