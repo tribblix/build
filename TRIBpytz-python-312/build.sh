ce4088bd688c346cf5d19b65b071ba1f424d0493
Peter Tribble <peter.tribble@gmail.com>
2026-07-27 08:56:33 +0100

    Update pytz 2026.3.post1

--- TRIBpytz-python-312/build.sh
+++ TRIBpytz-python-312/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpytz-python-312 pytz-2026.3
+${THOME}/build/pkg_pep518 -N TRIBpytz-python-312 pytz-2026.3.post1
--- TRIBpytz-python-313/build.sh
+++ TRIBpytz-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpytz-python-313 pytz-2026.3
+${THOME}/build/pkg_pep518 -N TRIBpytz-python-313 pytz-2026.3.post1
