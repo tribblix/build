0e4c745a0b7c2b5191c56175c8259528f0fa4eb6
Peter Tribble <peter.tribble@gmail.com>
2026-07-28 21:20:57 +0100

    Update annotated-doc 0.0.5

--- TRIBannotated-doc-python-313/build.sh
+++ TRIBannotated-doc-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBannotated-doc-python-313 annotated_doc-0.0.4
+${THOME}/build/pkg_pep518 -N TRIBannotated-doc-python-313 annotated_doc-0.0.5
