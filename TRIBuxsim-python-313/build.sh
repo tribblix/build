df54d4a979e696301e0d675ee0e455e43d476cc8
Peter Tribble <peter.tribble@gmail.com>
2026-07-28 09:14:39 +0100

    Update uxsim 1.14.0

--- TRIBuxsim-python-313/build.sh
+++ TRIBuxsim-python-313/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBuxsim-python-313 uxsim-1.13.0
+# as of v1.14.0 shifted to nanobind
+# must use -N as we have a patched nanobind to make this work
+#
+env CXXFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBuxsim-python-313 uxsim-1.14.0
