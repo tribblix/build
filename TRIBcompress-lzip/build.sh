9c890f3f5050989fea6658ea08398288092213a0
Peter Tribble <peter.tribble@gmail.com>
2026-07-28 12:31:22 +0100

    Update lzip 1.26 lzlib 1.16 plzip 1.13 lunzip 1.16

--- TRIBcompress-lzip/build.sh
+++ TRIBcompress-lzip/build.sh
@@ -4,8 +4,8 @@
 #
 # lzip and friends
 #
-${THOME}/build/dobuild lzip-1.25 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
-${THOME}/build/dobuild -64only lzlib-1.15 -C "CFLAGS+=-m64 --disable-static --enable-shared"
-${THOME}/build/dobuild plzip-1.12 -C "CPPFLAGS=-I../lzlib-1.15-64bit LDFLAGS=-L../lzlib-1.15-64bit CXXFLAGS+=-m64"
-${THOME}/build/dobuild lunzip-1.15 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
-${THOME}/build/genpkg TRIBcompress-lzip lzip-1.25 lzlib-1.15 plzip-1.12 lunzip-1.15
+${THOME}/build/dobuild lzip-1.26 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
+${THOME}/build/dobuild -64only lzlib-1.16 -C "CFLAGS+=-m64 --disable-static --enable-shared"
+${THOME}/build/dobuild plzip-1.13 -C "CPPFLAGS=-I../lzlib-1.16-64bit LDFLAGS=-L../lzlib-1.16-64bit CXXFLAGS+=-m64"
+${THOME}/build/dobuild lunzip-1.16 -C "LDFLAGS=-m64 CPPFLAGS=-m64"
+${THOME}/build/genpkg TRIBcompress-lzip lzip-1.26 lzlib-1.16 plzip-1.13 lunzip-1.16
