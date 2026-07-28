2eeb6680796509afbcf38050ca7e7f97fb85f07c
Peter Tribble <peter.tribble@gmail.com>
2026-07-28 09:20:04 +0100

    Update python-lsp-server 1.15.0

--- TRIBpylsp-server-python-313/build.sh
+++ TRIBpylsp-server-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpylsp-server-python-313 python_lsp_server-1.14.0
+${THOME}/build/pkg_pep518 -N TRIBpylsp-server-python-313 python_lsp_server-1.15.0
