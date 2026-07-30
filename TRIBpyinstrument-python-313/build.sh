16ae668fa9ab5ff17dac6840f88d7fdfec1b6693
Peter Tribble <peter.tribble@gmail.com>
2026-07-30 09:17:01 +0100

    Update pyinstrument 5.1.3

--- TRIBpyinstrument-python-313/build.sh
+++ TRIBpyinstrument-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpyinstrument-python-313 pyinstrument-5.1.2
+${THOME}/build/pkg_pep518 -N TRIBpyinstrument-python-313 pyinstrument-5.1.3
