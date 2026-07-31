4ae13f8bc1a9b3161c311d12db57ea942972cc26
Peter Tribble <peter.tribble@gmail.com>
2026-07-31 09:43:08 +0100

    Update uxsim 1.14.2

--- TRIBuxsim-python-313/build.sh
+++ TRIBuxsim-python-313/build.sh
@@ -5,4 +5,4 @@
 # as of v1.14.0 shifted to nanobind
 # must use -N as we have a patched nanobind to make this work
 #
-env CXXFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBuxsim-python-313 uxsim-1.14.0
+env CXXFLAGS=-m64 ${THOME}/build/pkg_pep518 -N TRIBuxsim-python-313 uxsim-1.14.2
