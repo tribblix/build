24c3643bdea602593bb6d07288ef349fb9661d0a
Peter Tribble <peter.tribble@gmail.com>
2026-08-02 20:54:57 +0100

    Update goaccess 1.11

--- TRIBgoaccess/build.sh
+++ TRIBgoaccess/build.sh
@@ -3,8 +3,7 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # needs ncurses
-# cannot enable utf8 as that requires ncursesw
 # patch to disable use of gmtoff
 #
-env TRIBBLIX_CFLAGS="-std=gnu99 -I/usr/gnu/include" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -64only goaccess-1.9.1 -C "--sysconfdir=/etc"
-${THOME}/build/genpkg TRIBgoaccess goaccess-1.9.1
+env TRIBBLIX_CFLAGS="-std=gnu99 -I/usr/gnu/include -I/usr/gnu/include/ncursesw" TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`${THOME}/build/getarch` -R/usr/gnu/lib/`${THOME}/build/getarch`" ${THOME}/build/dobuild -A -64only goaccess-1.11 -C "--sysconfdir=/etc --enable-utf8"
+${THOME}/build/genpkg TRIBgoaccess goaccess-1.11
