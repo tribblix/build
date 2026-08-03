384a8955f5bf6e3257b8e2cd577d3a50dd68704a
Peter Tribble <peter.tribble@gmail.com>
2026-08-03 17:43:15 +0100

    Update openconnect 9.21

--- TRIBopenconnect/build.sh
+++ TRIBopenconnect/build.sh
@@ -4,7 +4,7 @@
 #
 # It wants a vpnc-script installed first; the argument here is to tell it
 # where it's going to be packaged.
-# And it needs tun/tap installed (the headers at least)
+# And it needs tun/tap installed (the header at least)
 #
-${THOME}/build/dobuild openconnect-8.20 -C "--with-vpnc-script=/etc/vpnc/vpnc-script --disable-dependency-tracking --without-gnutls --with-system-cafile=/etc/ssl/cacert.pem"
-${THOME}/build/genpkg TRIBopenconnect openconnect-8.20
+env TRIBBLIX_CFLAGS=-D_POSIX_PTHREAD_SEMANTICS ${THOME}/build/dobuild openconnect-9.21 -C "--with-vpnc-script=/etc/vpnc/vpnc-script --disable-dependency-tracking --without-gnutls --with-system-cafile=/etc/ssl/cacert.pem"
+${THOME}/build/genpkg TRIBopenconnect openconnect-9.21
