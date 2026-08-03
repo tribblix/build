376cbf14ff011d998b897928253e929a0ee76982
Peter Tribble <peter.tribble@gmail.com>
2026-08-03 10:01:43 +0100

    Update pygraphviz 2.0.1

--- TRIBpygraphviz-python-313/build.sh
+++ TRIBpygraphviz-python-313/build.sh
@@ -2,4 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBpygraphviz-python-313 pygraphviz-1.14
+# there's a preconf script to manage linking
+#
+${THOME}/build/pkg_pep518 -N TRIBpygraphviz-python-313 pygraphviz-2.0.1
