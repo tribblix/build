90e40290a404cc661066c6ee70ef2a6d7ec0ec91
Peter Tribble <peter.tribble@gmail.com>
2026-08-05 16:20:46 +0100

    Drop unused python modules

--- TRIBsimplejson-python-312/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_setup_py TRIBsimplejson-python-312 simplejson-3.17.2
--- TRIBtzlocal-python-313/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/pkg_pep518 -N TRIBtzlocal-python-313 tzlocal-5.4.4
