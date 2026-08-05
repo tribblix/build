44158f96758ef5bcf9ba824d308ba44fd96970ab
Peter Tribble <peter.tribble@gmail.com>
2026-08-05 06:53:42 +0100

    Update libXfont2 2.0.9, disable fontserver support

--- TRIBx11-libxfont2/build.sh
+++ TRIBx11-libxfont2/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only libXfont2-2.0.8 +P /usr/lib/xorg -C "--enable-freetype --enable-fc --enable-builtins --with-bzip2"
-${THOME}/build/genpkg TRIBx11-libxfont2 libXfont2-2.0.8
+${THOME}/build/dobuild -64only libXfont2-2.0.9 +P /usr/lib/xorg -C "--enable-freetype --disable-fc --enable-builtins --with-bzip2"
+${THOME}/build/genpkg TRIBx11-libxfont2 libXfont2-2.0.9
