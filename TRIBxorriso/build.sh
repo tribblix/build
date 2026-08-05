971898128f6f9fb3f07e79b25c73625c68ae8815
Peter Tribble <peter.tribble@gmail.com>
2026-08-05 07:13:00 +0100

    Update xorriso 1.5.8.pl02

--- TRIBxorriso/build.sh
+++ TRIBxorriso/build.sh
@@ -4,5 +4,5 @@
 #
 # the tarball is misnamed
 #
-${THOME}/build/dobuild -64only -n xorriso-1.5.6 xorriso-1.5.6.pl02
-${THOME}/build/genpkg TRIBxorriso xorriso-1.5.6.pl02
+${THOME}/build/dobuild -64only -n xorriso-1.5.8 xorriso-1.5.8.pl02
+${THOME}/build/genpkg TRIBxorriso xorriso-1.5.8.pl02
