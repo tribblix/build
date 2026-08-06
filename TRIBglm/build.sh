9db5096cc0089b734c21cde703847651845b4625
Peter Tribble <peter.tribble@gmail.com>
2026-08-06 14:51:40 +0100

    Remove long-deprecated glm package

--- TRIBglm/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# SPDX-License-Identifier: CDDL-1.0
-#
-${THOME}/build/cmbuild -64only glm-1.0.2 -C "-DGLM_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON"
-${THOME}/build/genpkg TRIBglm glm-1.0.2
