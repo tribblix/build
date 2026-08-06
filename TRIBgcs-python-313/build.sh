94202d3f62a69e7d0ada3069258daa16e2d9dbc4
Peter Tribble <peter.tribble@gmail.com>
2026-08-06 09:52:19 +0100

    Update google-cloud-storage 3.13.1

--- TRIBgcs-python-313/build.sh
+++ TRIBgcs-python-313/build.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 # dependencies include the [protobuf] extra, currently pinning protobuf
-# to protobuf<7.0.0,>=3.20.2
+# to protobuf<8.0.0,>=6.33.5
 #
-${THOME}/build/pkg_pep518 -N TRIBgcs-python-313 google_cloud_storage-3.13.0
+${THOME}/build/pkg_pep518 -N TRIBgcs-python-313 google_cloud_storage-3.13.1
