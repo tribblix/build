9cfa02ecd9b84f1eeff2d5cc61ff5f6ddfcfa969
Peter Tribble <peter.tribble@gmail.com>
2026-08-07 08:56:30 +0100

    Update flit_core 4.0.2

--- TRIBflit-core-python-313/build.sh
+++ TRIBflit-core-python-313/build.sh
@@ -5,4 +5,4 @@
 # there's a potential bootstrapping issue, so using -N here
 # isn't advised
 #
-${THOME}/build/pkg_pep518 TRIBflit-core-python-313 flit_core-3.12.0
+${THOME}/build/pkg_pep518 TRIBflit-core-python-313 flit_core-4.0.2
