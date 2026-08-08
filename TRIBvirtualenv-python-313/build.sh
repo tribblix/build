d6c17df90aa56fc93bae6063a0819d76f3239702
Peter Tribble <peter.tribble@gmail.com>
2026-08-08 09:10:34 +0100

    Update virtualenv 21.7.2

--- TRIBvirtualenv-python-313/build.sh
+++ TRIBvirtualenv-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # python-discovery spun out into a separate module as of release 21
 #
-${THOME}/build/pkg_pep518 -N TRIBvirtualenv-python-313 virtualenv-21.7.1 python_discovery-1.5.1
+${THOME}/build/pkg_pep518 -N TRIBvirtualenv-python-313 virtualenv-21.7.2 python_discovery-1.5.1
