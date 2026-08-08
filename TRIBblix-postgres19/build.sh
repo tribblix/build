b001e0b2261bb7a5e7851e72f29dd3d25341a8f1
Peter Tribble <peter.tribble@gmail.com>
2026-08-08 13:12:29 +0100

    Add postgres 19beta2

---
+++ TRIBblix-postgres19/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# postgres 19
+#
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres19 postgresql-19beta2 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres19 postgresql-19beta2
