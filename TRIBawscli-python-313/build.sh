91935b0258f0e0f86415c13913c8d60963ebcebf
Peter Tribble <peter.tribble@gmail.com>
2026-08-08 09:34:11 +0100

    Update awscli 1.46.0

--- TRIBawscli-python-313/build.sh
+++ TRIBawscli-python-313/build.sh
@@ -2,4 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBawscli-python-313 awscli-1.45.62
+# now in maintenance mode, we're supposed to use TRIBawscli-v2
+#
+# as of 1.46.0, vendors botocore and s3transfer
+#
+${THOME}/build/pkg_pep518 -N TRIBawscli-python-313 awscli-1.46.0
