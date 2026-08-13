e4daf8a4cc48794dea6c72a3d31789c733cdbe8f
Peter Tribble <peter.tribble@gmail.com>
2026-08-13 10:30:42 +0100

    Update feedparser 6.0.14

--- TRIBfeedparser-python-313/build.sh
+++ TRIBfeedparser-python-313/build.sh
@@ -2,4 +2,10 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBfeedparser-python-313 feedparser-6.0.12
+# this module used to depend on sgmllib3k, but that's been replaced
+# by feedparser-sgmllib which is private to feedparser, so package
+# it together
+#
+# feedparser is built with setuptool, sgmllib with poetry
+#
+${THOME}/build/pkg_pep518 -N TRIBfeedparser-python-313 feedparser-6.0.14 feedparser_sgmllib-2.1.0
