a7042a35b0361d49325a283a46e30d93263d5c89
Peter Tribble <peter.tribble@gmail.com>
2026-08-13 11:55:07 +0100

    Update 7-zip 26.02

--- TRIBcompress-p7zip/build.sh
+++ TRIBcompress-p7zip/build.sh
@@ -5,8 +5,8 @@
 # replaces the old p7zip, but keep the package and command names
 # for compatibility
 #
-$THOME/build/unpack -N 7z2500-src
-cd 7z2500-src/CPP/7zip/Bundles/Alone2
+$THOME/build/unpack -N 7z2602-src
+cd 7z2602-src/CPP/7zip/Bundles/Alone2
 gmake -j8 -f ../../cmpl_gcc.mak CC=gcc CXX=g++ LOCAL_FLAGS_SYS=-m64 LDFLAGS=-m64 LIB2=
 #
 # see fixit and fixinstall for the actual installation
---
+++ patches/7z2602-src.pls
@@ -0,0 +1,1 @@
+-p1 7-zip-sha.patch 
