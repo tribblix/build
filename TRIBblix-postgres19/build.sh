65d76166b76fd8433639e1eada99ccce01448d9e
Peter Tribble <peter.tribble@gmail.com>
2026-08-14 11:19:46 +0100

    Update PostgreSQL 19beta3

--- TRIBblix-postgres19/build.sh
+++ TRIBblix-postgres19/build.sh
@@ -4,5 +4,5 @@
 #
 # postgres 19
 #
-${THOME}/build/dobuild +64only -P /opt/tribblix/postgres19 postgresql-19beta2 -C "--without-readline --with-perl --with-openssl" -M world
-${THOME}/build/genpkg -I install-world TRIBblix-postgres19 postgresql-19beta2
+${THOME}/build/dobuild +64only -P /opt/tribblix/postgres19 postgresql-19beta3 -C "--without-readline --with-perl --with-openssl" -M world
+${THOME}/build/genpkg -I install-world TRIBblix-postgres19 postgresql-19beta3
