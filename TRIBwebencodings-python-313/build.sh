3ce4ed618ff2e0f55dbb4251ee519e88327eba8d
Peter Tribble <peter.tribble@gmail.com>
2026-08-16 09:28:35 +0100

    Udpate webencodings 0.6.1

--- TRIBwebencodings-python-313/build.sh
+++ TRIBwebencodings-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwebencodings-python-313 webencodings-0.5.1
+${THOME}/build/pkg_pep518 -N TRIBwebencodings-python-313 webencodings-0.6.1
