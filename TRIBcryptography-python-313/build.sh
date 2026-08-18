fbacc450551b04a13f8129d40856edcf1950e358
Peter Tribble <peter.tribble@gmail.com>
2026-08-18 09:43:08 +0100

    Update python cryptography 50.0.0

--- TRIBcryptography-python-313/build.sh
+++ TRIBcryptography-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # current versions require a rust toolchain to build
 #
-${THOME}/build/pkg_pep518 -N TRIBcryptography-python-313 cryptography-49.0.0
+${THOME}/build/pkg_pep518 -N TRIBcryptography-python-313 cryptography-50.0.0
