b5069abfc19ba8225449e61f0f83d8491edc194b
Peter Tribble <peter.tribble@gmail.com>
2026-08-18 09:23:41 +0100

    Update xpr 1.2.1

--- TRIBx11-xwd/build.sh
+++ TRIBx11-xwd/build.sh
@@ -4,5 +4,5 @@
 #
 ${THOME}/build/mesonbuild -64only xwd-1.0.10
 ${THOME}/build/mesonbuild -64only xwud-1.0.8
-${THOME}/build/dobuild -64only xpr-1.2.0
-${THOME}/build/genpkg TRIBx11-xwd xwd-1.0.10 xwud-1.0.8 xpr-1.2.0
+${THOME}/build/mesonbuild -64only xpr-1.2.1
+${THOME}/build/genpkg TRIBx11-xwd xwd-1.0.10 xwud-1.0.8 xpr-1.2.1
