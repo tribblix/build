ff6b815ec6bed3d69b43639dc0db01d385a40539
Peter Tribble <peter.tribble@gmail.com>
2026-08-19 12:26:37 +0100

    procmail should be 64-bit

--- TRIBprocmail/build.sh
+++ TRIBprocmail/build.sh
@@ -2,8 +2,11 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64only procmail-3.24 -M LOCKINGTEST=110
+# easiest way to get 64-bit is to set CC
+# no-implicit because gcc14 is stricter
+#
+env CC="gcc -m64 -Wno-implicit" ${THOME}/build/dobuild -64only procmail-3.24 -M LOCKINGTEST=110
 #
 # see also the fixit script which relocates the install into the right place
 #
-${THOME}/build/genpkg -I BASENAME TRIBprocmail procmail-3.24
+${THOME}/build/genpkg -D BASENAME TRIBprocmail procmail-3.24
