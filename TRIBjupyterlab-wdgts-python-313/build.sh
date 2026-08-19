957de4691133394dce95552d5e4a9ad13149f842
Peter Tribble <peter.tribble@gmail.com>
2026-08-19 09:10:45 +0100

    Update ipywidgets 8.1.9 jupyterlab_widgets 3.0.17 widgetsnbextension 4.0.16

--- TRIBjupyterlab-wdgts-python-313/build.sh
+++ TRIBjupyterlab-wdgts-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBjupyterlab-wdgts-python-313 jupyterlab_widgets-3.0.16
+${THOME}/build/pkg_pep518 -N TRIBjupyterlab-wdgts-python-313 jupyterlab_widgets-3.0.17
--- TRIBwdgtsnbextension-python-313/build.sh
+++ TRIBwdgtsnbextension-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwdgtsnbextension-python-313 widgetsnbextension-4.0.15
+${THOME}/build/pkg_pep518 -N TRIBwdgtsnbextension-python-313 widgetsnbextension-4.0.16
