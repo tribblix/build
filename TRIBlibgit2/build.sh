2ad9fa0154ec1091a07744b35192e6ccbcbd6964
Peter Tribble <peter.tribble@gmail.com>
2026-08-20 12:08:51 +0100

    Update libgit2 1.9.7

--- TRIBlibgit2/build.sh
+++ TRIBlibgit2/build.sh
@@ -4,5 +4,5 @@
 #
 # usually bumps the SONAME, so upgrades require consumers to be rebuilt
 #
-${THOME}/build/cmbuild -64only libgit2-1.7.2 -C "-DBUILD_CLI=OFF -DBUILD_TESTS=OFF -DUSE_SSH=ON"
-${THOME}/build/genpkg TRIBlibgit2 libgit2-1.7.2
+${THOME}/build/cmbuild -64only libgit2-1.9.7 -C "-DBUILD_CLI=OFF -DBUILD_TESTS=OFF -DUSE_SSH=ON"
+${THOME}/build/genpkg TRIBlibgit2 libgit2-1.9.7
