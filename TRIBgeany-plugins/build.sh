5f63b75bdceff2eaa25a93b4bff8d45efd65eed5
Peter Tribble <peter.tribble@gmail.com>
2026-08-20 12:09:53 +0100

    Rebuild geany plugins for libgit2 update, enable lsp

--- TRIBgeany-plugins/build.sh
+++ TRIBgeany-plugins/build.sh
@@ -2,7 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# the lsp plugin needs glib2 2.68
-#
-${THOME}/build/dobuild -64only -gnu geany-plugins-2.1 -C "--disable-scope --disable-utilslib --disable-lsp"
+${THOME}/build/dobuild -64only -gnu geany-plugins-2.1 -C "--disable-scope --disable-utilslib"
 ${THOME}/build/genpkg TRIBgeany-plugins geany-plugins-2.1
