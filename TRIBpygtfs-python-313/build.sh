3116a18646ceffbf7c7f7d40f735ccf428c06edf
Peter Tribble <peter.tribble@gmail.com>
2026-08-20 19:17:58 +0100

    Add pygtfs 0.1.11

---
+++ TRIBpygtfs-python-313/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# package docopt with this, as I don't have other consumers
+#
+${THOME}/build/pkg_pep518 -N TRIBpygtfs-python-313 docopt-0.6.2 pygtfs-0.1.11
