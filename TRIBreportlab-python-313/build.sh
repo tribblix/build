1caa999b4895d544395140c2ecd29e427b0100b9
Peter Tribble <peter.tribble@gmail.com>
2026-08-21 20:05:50 +0100

    Update reportlab 5.0.1, add rl-accel

--- TRIBreportlab-python-313/build.sh
+++ TRIBreportlab-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBreportlab-python-313 reportlab-5.0.0
+${THOME}/build/pkg_pep518 -N TRIBreportlab-python-313 reportlab-5.0.1
---
+++ TRIBrlaccel-python-313/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+${THOME}/build/pkg_pep518 -N TRIBrlaccel-python-313 rl_accel-0.9.1
