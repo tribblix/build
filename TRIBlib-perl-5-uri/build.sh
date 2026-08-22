91f9c9418397faa2c2cfed0bbb9ef5d866d4c495
Peter Tribble <peter.tribble@gmail.com>
2026-08-22 13:37:00 +0100

    Update perl URI 5.36

--- TRIBlib-perl-5-uri/build.sh
+++ TRIBlib-perl-5-uri/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack URI-5.35
-cd URI-5.35
+${THOME}/build/unpack URI-5.36
+cd URI-5.36
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-uri URI-5.35
+${THOME}/build/genpkg TRIBlib-perl-5-uri URI-5.36
