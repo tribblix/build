d71caa0c634aaba8e161308a00198acfdabe6194
Peter Tribble <peter.tribble@gmail.com>
2026-08-26 17:37:17 +0100

    Update automaton 3.5.0

--- TRIBautomaton-python-313/build.sh
+++ TRIBautomaton-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBautomaton-python-313 automaton-3.4.0
+${THOME}/build/pkg_pep518 -N TRIBautomaton-python-313 automaton-3.5.0
