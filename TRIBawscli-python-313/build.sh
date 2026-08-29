3f4db55c188c2d40bd759eb83d4236bcadd898a0
Peter Tribble <peter.tribble@gmail.com>
2026-08-29 08:50:45 +0100

    Update awscli 1.46.1

--- TRIBawscli-python-313/build.sh
+++ TRIBawscli-python-313/build.sh
@@ -4,6 +4,7 @@
 #
 # now in maintenance mode, we're supposed to use TRIBawscli-v2
 #
-# as of 1.46.0, vendors botocore and s3transfer
+# as of 1.46.0, vendors botocore and s3transfer, and we should
+# transition to v2
 #
-${THOME}/build/pkg_pep518 -N TRIBawscli-python-313 awscli-1.46.0
+${THOME}/build/pkg_pep518 -N TRIBawscli-python-313 awscli-1.46.1
