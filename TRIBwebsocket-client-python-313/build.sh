04eb97c4278880b22fddbd988feb7ace60e6a348
Peter Tribble <peter.tribble@gmail.com>
2026-08-31 09:14:29 +0100

    Update websocket-client 1.9.1

--- TRIBwebsocket-client-python-313/build.sh
+++ TRIBwebsocket-client-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBwebsocket-client-python-313 websocket_client-1.9.0
+${THOME}/build/pkg_pep518 -N TRIBwebsocket-client-python-313 websocket_client-1.9.1
