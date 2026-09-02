afda5da7b31d3e300196d06580e2f337cc594d61
Peter Tribble <peter.tribble@gmail.com>
2026-09-02 09:21:52 +0100

    Update go 1.27.1

--- TRIBv-go-127/build.sh
+++ TRIBv-go-127/build.sh
@@ -13,7 +13,7 @@
 #
 chmod -R u+w go
 rm -fr go
-env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.27.0.src
+env LANG=en_GB.UTF-8 ${THOME}/build/unpack go1.27.1.src
 #
 cd go/src
 # one of the new tests in 1.26.5 fails, use make.bash not all.bash
