ec224e51876e5a5a39bea2abc94b062dd59881c1
Peter Tribble <peter.tribble@gmail.com>
2026-09-03 08:57:32 +0100

    Update nab modules to 0.0.16

--- TRIBnab-python-313/build.sh
+++ TRIBnab-python-313/build.sh
@@ -4,7 +4,8 @@
 #
 # pipdeptree depends on nab-index and nab-project
 # nab-project depends on nab-index nab-resolver nab-provider
+# nab-project and nab-provider depend on nab-markersets
 # to simplify matters, create a merged nab package
 # the pyproject_hooks dependency is provided by the build package
 #
-${THOME}/build/pkg_pep518 -N TRIBnab-python-313 nab_project-0.0.14 nab_index-0.0.14 nab_provider-0.0.14 nab_resolver-0.0.14
+${THOME}/build/pkg_pep518 -N TRIBnab-python-313 nab_project-0.0.16 nab_index-0.0.16 nab_provider-0.0.16 nab_resolver-0.0.16 nab_markersets-0.0.16
