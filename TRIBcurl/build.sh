66b2d285bbda60ba1583974dd37d48dedd348c05
Peter Tribble <peter.tribble@gmail.com>
2026-09-03 16:07:16 +0100

    Update curl 8.22.0

--- TRIBcurl/build.sh
+++ TRIBcurl/build.sh
@@ -4,5 +4,5 @@
 #
 # disable brotli in the 32-bit build, as our brotli is 64-bit only
 #
-${THOME}/build/dobuild -64 curl-8.21.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli --disable-ldap" +C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --disable-ldap"
-${THOME}/build/genpkg TRIBcurl curl-8.21.0
+${THOME}/build/dobuild -64 curl-8.22.0 -C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --without-brotli --disable-ldap" +C "--with-ca-bundle=/etc/openssl/cacert.pem --with-libssh2 --enable-static=no --disable-dependency-tracking --with-openssl --disable-ldap"
+${THOME}/build/genpkg TRIBcurl curl-8.22.0
