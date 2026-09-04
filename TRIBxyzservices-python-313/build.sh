ec6011ba83f6151757b078b6bd4f8498f8c5fc4f
Peter Tribble <peter.tribble@gmail.com>
2026-09-04 13:50:53 +0100

    Update xyzservices 2026.9.1

--- TRIBxyzservices-python-313/build.sh
+++ TRIBxyzservices-python-313/build.sh
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N TRIBxyzservices-python-313 xyzservices-2026.3.0
+${THOME}/build/pkg_pep518 -N TRIBxyzservices-python-313 xyzservices-2026.9.1
