e6b624dd1d44c3d2cac481d38a75feeb4e98ac63
Peter Tribble <peter.tribble@gmail.com>
2026-09-06 13:39:51 +0100

    No longer need the exiv2 workaround on sparc

--- TRIBexiv2/build.sh
+++ TRIBexiv2/build.sh
@@ -3,16 +3,4 @@
 # SPDX-License-Identifier: CDDL-1.0
 #
 $THOME/build/cmbuild -64only exiv2-0.28.9 -C "-DEXIV2_ENABLE_INIH=OFF"
-
-#
-# SPARC is a bit messy, because it has gcc7 and needs libstdc++fs, but
-# doesn't actually do it right. With meson, the library is linked correctly
-# but something else is wrong with the installation as consumers won't build
-#
-# The fix is to cd to tribblix_build/src, edit CMakeFiles/exiv2lib.dir/link.txt
-# and add -lstdc++fs to the *end* of the link, then source that to rebuild
-# the library correctly (the position matters, because it resolves symbols
-# in order)
-#
-
 ${THOME}/build/genpkg TRIBexiv2 exiv2-0.28.9
--- patches/exiv2-0.28.8.pls
+++
@@ -1,1 +0,0 @@
--p1 exiv2-sparc.patch
