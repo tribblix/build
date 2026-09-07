cf2914a071f27150d8690e5b1c0c09cccada59ed
Peter Tribble <peter.tribble@gmail.com>
2026-09-07 19:45:10 +0100

    Update LWP::MediaTypes 6.05

--- TRIBlib-perl-5-lwp-mediatypes/build.sh
+++ TRIBlib-perl-5-lwp-mediatypes/build.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack LWP-MediaTypes-6.04
-cd LWP-MediaTypes-6.04
+${THOME}/build/unpack LWP-MediaTypes-6.05
+cd LWP-MediaTypes-6.05
 perl Makefile.PL
 make
 ${THOME}/build/genpkg TRIBlib-perl-5-lwp-mediatypes
