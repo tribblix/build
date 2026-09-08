a3c9fa7a688fc8ab9e9682481efa889362231dd9
Peter Tribble <peter.tribble@gmail.com>
2026-09-08 09:54:59 +0100

    Update lynx 2.9.3

--- TRIBlynx/build.sh
+++ TRIBlynx/build.sh
@@ -8,5 +8,5 @@
 # don't have gnu iconv installed, as it will only be partially detected
 # and won't link
 #
-env TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only lynx2.9.2 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines --with-screen=ncurses --with-curses-dir=/usr/gnu"
-${THOME}/build/genpkg -I "install install-help" TRIBlynx lynx2.9.2
+env TRIBBLIX_LDFLAGS="-L/usr/gnu/lib/`$THOME/build/getarch` -R/usr/gnu/lib/`$THOME/build/getarch`" ${THOME}/build/dobuild -64only lynx2.9.3 -E /etc -C "--with-ssl --with-zlib --enable-gzip-help --enable-internal-links --enable-ipv6 --enable-local-docs --enable-nsl-fork --enable-underlines --with-screen=ncursesw --with-curses-dir=/usr/gnu"
+${THOME}/build/genpkg -I "install install-help" TRIBlynx lynx2.9.3
