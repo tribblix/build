76a67e5a6d5bba3e956df0c2ceebed97fb956ec0
Peter Tribble <peter.tribble@gmail.com>
2026-09-10 11:03:42 +0100

    Remove sparc comment for openexr

--- TRIBopenexr/build.sh
+++ TRIBopenexr/build.sh
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-# sparc downrev as we need a newer gcc
-#
 # 3.2 bumped the SONAME
 #
 # note that we need to package a full copy of Imath, as the cmake and
