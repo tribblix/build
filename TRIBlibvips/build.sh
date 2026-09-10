8ba11112c4ae40a9e8f6693a0ec2d465129bca7e
Peter Tribble <peter.tribble@gmail.com>
2026-09-10 11:55:46 +0100

    Update vips 8.18.6

--- TRIBlibvips/build.sh
+++ TRIBlibvips/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-env AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only vips-8.16.0 -C "-Dintrospection=disabled -Dimagequant=disabled"
-${THOME}/build/genpkg TRIBlibvips vips-8.16.0
+env AR=/usr/bin/ar ${THOME}/build/mesonbuild -64only vips-8.18.6 -C "-Dintrospection=disabled -Dimagequant=disabled"
+${THOME}/build/genpkg TRIBlibvips vips-8.18.6
