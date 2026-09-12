7f0dcd45503caa187758457daecef7889ba1aab9
Peter Tribble <peter.tribble@gmail.com>
2026-09-12 22:02:07 +0100

    Update Barman 3.20.0

--- TRIBpgbarman/build.sh
+++ TRIBpgbarman/build.sh
@@ -2,4 +2,8 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 -N -v 313 TRIBpgbarman barman-3.19.1
+# dependencies give the extras [argcomplete,aws-snapshots,cloud,lz4,zstandard]
+# depend on snappy, and cramjam only on x86
+# packages are available for [azure,google]
+#
+${THOME}/build/pkg_pep518 -N -v 313 TRIBpgbarman barman-3.20.0
