01b8b041bd33700214218670c86749718c9d3d3a
Peter Tribble <peter.tribble@gmail.com>
2026-09-12 14:56:09 +0100

    Update matplotlib 3.11.2

--- TRIBmatplotlib-python-313/build.sh
+++ TRIBmatplotlib-python-313/build.sh
@@ -10,4 +10,4 @@
 # either uninstall pyqt5, or make sure tkinter is installed and invoke python
 # with env MPLBACKEND=tkagg ...
 #
-env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" PATH=/usr/versions/python-3.13/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "--config-settings=setup-args=-Dsystem-freetype=true --config-settings=setup-args=-Db_lto=false" TRIBmatplotlib-python-313 matplotlib-3.11.1
+env CXXFLAGS=-m64 CFLAGS=-m64 CC="gcc -m64" PATH=/usr/versions/python-3.13/bin:$PATH ${THOME}/build/pkg_pep518 -N -C "--config-settings=setup-args=-Dsystem-freetype=true --config-settings=setup-args=-Db_lto=false" TRIBmatplotlib-python-313 matplotlib-3.11.2
