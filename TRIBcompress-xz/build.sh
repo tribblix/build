0db0c70252bbc5d4acd1ebcb92b27c965cd17ff4
Peter Tribble <peter.tribble@gmail.com>
2026-09-13 10:06:25 +0100

    Update xz 5.8.4

--- TRIBcompress-xz/build.sh
+++ TRIBcompress-xz/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -64 xz-5.8.3 -C --disable-static
-${THOME}/build/genpkg TRIBcompress-xz xz-5.8.3
+${THOME}/build/dobuild -64 xz-5.8.4 -C --disable-static
+${THOME}/build/genpkg TRIBcompress-xz xz-5.8.4
