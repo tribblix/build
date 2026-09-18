c585c80e851b00601531b269d7b8db87f994a4bf
Peter Tribble <peter.tribble@gmail.com>
2026-09-18 19:14:44 +0100

    Put hatchling back to 1.32.0, update is broken

--- TRIBhatchling-python-313/build.sh
+++ TRIBhatchling-python-313/build.sh
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/pkg_pep518 TRIBhatchling-python-313 hatchling-1.32.3 hatch_fancy_pypi_readme-25.1.0
+# 1.32.3 breaks builds, eg virtualenv
+# https://github.com/pypa/hatch/issues/2437
+#
+${THOME}/build/pkg_pep518 TRIBhatchling-python-313 hatchling-1.32.0 hatch_fancy_pypi_readme-25.1.0
