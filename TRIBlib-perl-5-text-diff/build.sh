00de9f3cf0d41b7567836b9a6392cf0946306fcf
Peter Tribble <peter.tribble@gmail.com>
2026-09-18 17:08:33 +0100

    Update Text::Diff 1.46

--- TRIBlib-perl-5-text-diff/build.sh
+++ TRIBlib-perl-5-text-diff/build.sh
@@ -2,9 +2,9 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/unpack Text-Diff-1.45
-cd Text-Diff-1.45
+${THOME}/build/unpack Text-Diff-1.46
+cd Text-Diff-1.46
 perl Makefile.PL
 make
 cd ..
-${THOME}/build/genpkg TRIBlib-perl-5-text-diff Text-Diff-1.45
+${THOME}/build/genpkg TRIBlib-perl-5-text-diff Text-Diff-1.46
