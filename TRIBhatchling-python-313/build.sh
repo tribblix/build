6b94295f8b92a5ef29e7b95bc3d616faac9074c3
Peter Tribble <peter.tribble@gmail.com>
2026-09-23 13:15:36 +0100

    Update hatchling 1.32.4

--- TRIBhatchling-python-313/build.sh
+++ TRIBhatchling-python-313/build.sh
@@ -5,4 +5,4 @@
 # 1.32.3 breaks builds, eg virtualenv
 # https://github.com/pypa/hatch/issues/2437
 #
-${THOME}/build/pkg_pep518 TRIBhatchling-python-313 hatchling-1.32.0 hatch_fancy_pypi_readme-25.1.0
+${THOME}/build/pkg_pep518 TRIBhatchling-python-313 hatchling-1.32.4 hatch_fancy_pypi_readme-25.1.0
