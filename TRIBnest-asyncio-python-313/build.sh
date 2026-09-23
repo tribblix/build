9c3a9ad77abf07be36253db661c2554a243d8b9b
Peter Tribble <peter.tribble@gmail.com>
2026-09-23 13:19:11 +0100

    Update nest-asyncio2 1.7.3

--- TRIBnest-asyncio-python-313/build.sh
+++ TRIBnest-asyncio-python-313/build.sh
@@ -6,4 +6,4 @@
 # nest-aysncio, but ipykernel is the only consumer in Tribblix and it
 # switched dependency
 #
-${THOME}/build/pkg_pep518 -N TRIBnest-asyncio-python-313 nest_asyncio2-1.7.2
+${THOME}/build/pkg_pep518 -N TRIBnest-asyncio-python-313 nest_asyncio2-1.7.3
