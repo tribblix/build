9f203575c9fbf797488ee08928e016893a51b7bc
Peter Tribble <peter.tribble@gmail.com>
2026-09-24 18:25:18 +0100

    Update networkx 3.7

--- TRIBnetworkx-python-313/build.sh
+++ TRIBnetworkx-python-313/build.sh
@@ -4,4 +4,4 @@
 #
 # the dependencies we declare are those for networkx[default]
 #
-${THOME}/build/pkg_pep518 -N TRIBnetworkx-python-313 networkx-3.6.1
+${THOME}/build/pkg_pep518 -N TRIBnetworkx-python-313 networkx-3.7
