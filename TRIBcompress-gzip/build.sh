63f691414e0a4ed05848283cd9576a17b579efed
Peter Tribble <peter.tribble@gmail.com>
2026-09-24 20:28:23 +0100

    Update gzip 1.15

--- TRIBcompress-gzip/build.sh
+++ TRIBcompress-gzip/build.sh
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: CDDL-1.0
 #
-${THOME}/build/dobuild -gnu -64only gzip-1.14
-${THOME}/build/genpkg TRIBcompress-gzip gzip-1.14
+${THOME}/build/dobuild -gnu -64only gzip-1.15
+${THOME}/build/genpkg TRIBcompress-gzip gzip-1.15
